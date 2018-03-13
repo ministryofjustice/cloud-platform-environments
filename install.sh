3239be51dd2911d5e949e747751e6aa9ce24962c
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-03-13 15:27:31 +0000

    bump

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install -y awscli
+sudo snap install kubectl --classic
+
 wget https://github.com/kubernetes/kops/releases/download/1.8.0/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
+
+cp /kube/config ~/.kube/config 
