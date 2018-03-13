a13c64724606caa08d9f7ab777c2c2405c75b5f8
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-03-13 15:35:19 +0000

    fix

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo snap install kubectl --classic
 wget https://github.com/kubernetes/kops/releases/download/1.8.0/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
-
+mkdir ~/.kube/ 
 cp ./kube/config ~/.kube/config 
