ce75d16fb9b2d6a0ccf9c4c589644c7ecf7b8ec2
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-03-13 15:31:33 +0000

    fix

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ wget https://github.com/kubernetes/kops/releases/download/1.8.0/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 
-cp /kube/config ~/.kube/config 
+cp ./kube/config ~/.kube/config 
