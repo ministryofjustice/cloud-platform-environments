f4faca70f726c48930a6071a69e1b5f737d4bea7
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-03-28 16:33:12 +0100

    moved to python

---
+++ namespace.py
@@ -0,0 +1,30 @@
+ #!/usr/local/bin/python3
+
+import subprocess, json
+
+default_namespaces = ['default', 'kube-public', 'kube-system']
+k8_namespaces = []
+dir_namespaces = str(subprocess.check_output(['ls', 'namespaces']).decode('utf-8')).split()
+k8_json = json.loads(str(subprocess.check_output(["kubectl", "get", "namespaces", "-o", "json"]).decode('utf-8')))
+
+for item in dir_namespaces:
+  try:
+    print(subprocess.check_output(['kubectl', 'create', '-f', 'namespaces/' + item + '/namespace.yaml']) )
+  except subprocess.CalledProcessError as e:
+    print('exit code 1') 
+
+  print(subprocess.check_output(['kubectl', 'apply', '-f', 'namespaces/' + item, '--namespace=' + item]) )
+
+for item in k8_json['items']:
+  k8_namespaces.append(item['metadata']['name'])
+
+for item in dir_namespaces:
+  k8_namespaces.remove(item)
+
+for item in default_namespaces:
+  k8_namespaces.remove(item)
+
+for item in k8_namespaces:
+  if item:
+    print('kubectl delete namespaces', item)
+    print(subprocess.check_output(['kubectl', 'delete', 'namespaces', item]) )
