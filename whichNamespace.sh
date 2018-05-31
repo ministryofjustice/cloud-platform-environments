3b5270089c234cafd1a37ae4d442baa58b6c1afe
Lukasz Raczylo <lukasz@raczylo.com>
2018-05-31 14:28:03 +0100

    Create and apply environment changes
    Script checks files changed within last commit.
    If any of the files lays within namespace definition directory, it'll pick
    the cluster it should run the update on and execute python script.

---
+++ whichNamespace.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This script determines which environment / cluster should we deploy to
+LIST_OF_CHANGED_FILES=`git diff-tree --no-commit-id --name-only -r $(git log --pretty=format:"%h" -1) | grep namespaces | cut -d "/" -f2 | uniq`
+# LIST_OF_CHANGED_FILES=`git diff-tree --no-commit-id --name-only -r afed721 | grep namespaces | cut -d "/" -f2 | uniq`
+if [ `echo $LIST_OF_CHANGED_FILES | wc -l` -lt 1 ]; then
+  echo "No clusters changes to apply"
+else
+  for CLUSTER in $LIST_OF_CHANGED_FILES; do
+    if [[ "$CLUSTER" =~ non-production\.k8s.* ]]; then
+      KEYSTORE='non-production-cluster-keystore'
+    elif [[ "$CLUSTER" =~ cloud-platforms-test\.k8s.* ]]; then
+      KEYSTORE="cloud-platform-test-cluster-keystore"
+    fi
+    echo "Running aws s3 cp s3://$KEYSTORE/kubecfg.yaml ~/.kube/config"
+    aws s3 cp s3://$KEYSTORE/kubecfg.yaml ~/.kube/config
+    echo "Running kubectl config use-context $CLUSTER"
+    kubectl config use-context $CLUSTER
+    echo "Running python3 namespace.py -c $CLUSTER"
+  done
+fi
+
+# Dummy change to trigger the build
