d9c089375a308ec9386e28e7a3ed79c834b36cd1
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-07-04 14:49:02 +0100

    added platforms dev manifest and terraform files

---
+++ namespaces/cloud-platform-test-1.k8s.integration.dsd.io/platforms-dev/resources/main.tf
@@ -0,0 +1,14 @@
+
+module "example_team_s3" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=master"
+
+  team_name              = "cloudplatform"
+  bucket_identifier      = "platform-dev"
+  acl                    = "private"
+  versioning             = false
+  business-unit          = "mojdigital"
+  application            = "pipeline-example"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "platform@digtal.justice.gov.uk"
+}
