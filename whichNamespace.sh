408356585ed13a60f69f8b7a279c1aa34335be71
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-07-04 15:14:27 +0100

    amended file to look for terraform files

--- whichNamespace.sh
+++ whichNamespace.sh
@@ -2,6 +2,7 @@
 
 # This script determines which environment / cluster should we deploy to
 LIST_OF_CHANGED_FILES=`git diff-tree --no-commit-id --name-only -r $(git log --pretty=format:"%h" -1) | grep namespaces | cut -d "/" -f2 | uniq`
+LIST_OF_CHANGED_DIR=`git diff-tree --no-commit-id --name-only -r $(git log --pretty=format:"%h" -1) | grep namespaces | cut -d "/" -f3 | uniq`
 if [ `echo $LIST_OF_CHANGED_FILES | wc -l` -lt 1 ]; then
   echo "No clusters changes to apply"
 else
@@ -10,5 +11,11 @@ else
     kubectl config use-context $CLUSTER
     echo "Running python3 namespace.py -c $CLUSTER"
     python3 namespace.py -c $CLUSTER
+    if [ -z "$(ls -A ./namespaces/$CLUSTER/$DIR/resources)" ]; then
+      echo "directory does not exist"
+    else
+      terraform init ./namespaces/$CLUSTER/$DIR/resources
+      terraform apply ./namespaces/$CLUSTER/$DIR/resources
+    fi
   done
 fi
