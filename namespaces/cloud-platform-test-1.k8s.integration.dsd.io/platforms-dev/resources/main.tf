cf4868af57dd879f65ebecb6489a44579a6bb544
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-07-05 17:21:23 +0100

    amended file to trigger pipeline

--- namespaces/cloud-platform-test-1.k8s.integration.dsd.io/platforms-dev/resources/main.tf
+++ namespaces/cloud-platform-test-1.k8s.integration.dsd.io/platforms-dev/resources/main.tf
@@ -6,11 +6,11 @@ module "example_team_s3" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=master"
 
   team_name              = "cloudplatform"
-  bucket_identifier      = "platform-devops"
+  bucket_identifier      = "platform-buckets"
   acl                    = "private"
   versioning             = false
   business-unit          = "mojdigital"
-  application            = "pipeline-example-app"
+  application            = "pipeline-example"
   is-production          = "false"
   environment-name       = "development"
   infrastructure-support = "platform@digtal.justice.gov.uk"
