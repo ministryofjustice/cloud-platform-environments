eb78d44e1ffebce5d03e8658dd26021a148ef85c
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-07-05 16:08:44 +0100

    changed terraform file to run pipeline

--- namespaces/cloud-platform-test-1.k8s.integration.dsd.io/platforms-dev/resources/main.tf
+++ namespaces/cloud-platform-test-1.k8s.integration.dsd.io/platforms-dev/resources/main.tf
@@ -10,7 +10,7 @@ module "example_team_s3" {
   acl                    = "private"
   versioning             = false
   business-unit          = "mojdigital"
-  application            = "pipeline-example-app"
+  application            = "pipeline-example"
   is-production          = "false"
   environment-name       = "development"
   infrastructure-support = "platform@digtal.justice.gov.uk"
