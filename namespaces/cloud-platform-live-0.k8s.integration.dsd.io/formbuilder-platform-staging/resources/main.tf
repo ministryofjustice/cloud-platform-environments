6369adc34fddbbfcf9b819d98f41a6006ef7c158
Al Davidson <apdavidson@gmail.com>
2018-09-03 15:06:24 +0100

    add RDS instance to FB staging

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-platform-staging/resources/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "rds-instance" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=master"
+
+  team_name                   = "formbuilder-staging"
+  db_backup_retention_period  = "2"
+  application                 = "formbuilderpublisher"
+  environment-name            = "staging"
+  is-production               = "false"
+  infrastructure-support      = "Form Builder form-builder-team@digital.justice.gov.uk"
+}
+
+resource "kubernetes_secret" "rds-instance" {
+  metadata {
+    name      = "rds-instance-formbuilder-publisher-staging"
+    namespace = "formbuilder-platform-staging"
+  }
+
+  data {
+    instance_id         = "${module.rds-instance.rds_instance_id}"
+    arn                 = "${module.rds-instance.rds_instance_arn}"
+    # postgres://USER:PASSWORD@HOST:PORT/NAME
+    url                 = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+    kms_key_id          = "${module.rds-instance.kms_key_id}"
+    access_key_id       = "${module.rds-instance.access_key_id}"
+    secret_access_key   = "${module.rds-instance.secret_access_key}"
+  }
+}
