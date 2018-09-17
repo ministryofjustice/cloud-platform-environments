2ff98cc600184d23e34e1cab747584e642bf6ff8
Nayo Akinyele <nayoa@hotmail.co.uk>
2018-09-17 15:52:41 +0100

    Pin down module version to 1.0

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/kuberos/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/kuberos/resources/ecr.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=master"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
 
   team_name = "cloud-platform"
   repo_name = "kuberos"
