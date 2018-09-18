ed553b2df7dbeb7d36192cd4e4c55e587f57061b
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-09-18 15:54:30 +0100

    terraform format

--- namespace-resources/variables.tf
+++ namespace-resources/variables.tf
@@ -1,11 +1,11 @@
 variable "cluster" {
-    description = "What cluster are you deploying your namespace. I.E cloud-platform-test-1 "
+  description = "What cluster are you deploying your namespace. I.E cloud-platform-test-1 "
 }
 
 variable "namespace" {
-    description = "Namespace you would like to create on cluster <application>-<environment>. I.E myapp-dev"
+  description = "Namespace you would like to create on cluster <application>-<environment>. I.E myapp-dev"
 }
 
 variable "github_team" {
-    description = "Your github team"
+  description = "Your github team"
 }
