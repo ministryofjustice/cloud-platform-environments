88428b928d30f3ff8ce7b3369fead3cd939c9ec6
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-09-18 15:50:23 +0100

    create terraform files to dynamically create kubernetes manifest files

---
+++ namespace-resources/main.tf
@@ -0,0 +1,82 @@
+locals {
+    cluster = "${var.cluster}.k8s.integration.dsd.io"
+}
+
+data "template_file" "namespace" {
+    template = "${file("${path.module}/00-namespace.yaml")}"
+
+
+    vars {
+        namespace = "${var.namespace}"
+    }
+}
+
+resource "local_file" "00-namespace" {
+    content     = "${data.template_file.namespace.rendered}"
+    filename = "../namespaces/${local.cluster}/${var.namespace}/00-namespace.yaml"
+}
+
+data "template_file" "rbac" {
+    template = "${file("${path.module}/01-rbac.yaml")}"
+
+
+    vars {
+        namespace = "${var.namespace}"
+        github_team = "${var.github_team}"
+    }
+}
+
+resource "local_file" "01-rbac" {
+    content     = "${data.template_file.rbac.rendered}"
+    filename = "../namespaces/${local.cluster}/${var.namespace}/01-rbac.yaml"
+}
+
+data "template_file" "limitrange" {
+    template = "${file("${path.module}/02-limitrange.yaml")}"
+
+
+    vars {
+        namespace = "${var.namespace}"
+    }
+}
+
+resource "local_file" "02-limitrange" {
+    content     = "${data.template_file.limitrange.rendered}"
+    filename = "../namespaces/${local.cluster}/${var.namespace}/02-limitrange.yaml"
+}
+
+data "template_file" "resourcequota" {
+    template = "${file("${path.module}/03-resourcequota.yaml")}"
+
+
+    vars {
+        namespace = "${var.namespace}"
+    }
+}
+
+resource "local_file" "03-resourcequota" {
+    content     = "${data.template_file.resourcequota.rendered}"
+    filename = "../namespaces/${local.cluster}/${var.namespace}/03-resourcequota.yaml"
+}
+
+data "template_file" "networkpolicy" {
+    template = "${file("${path.module}/04-networkpolicy.yaml")}"
+
+
+    vars {
+        namespace = "${var.namespace}"
+    }
+}
+
+resource "local_file" "04-networkpolicy" {
+    content     = "${data.template_file.networkpolicy.rendered}"
+    filename = "../namespaces/${local.cluster}/${var.namespace}/04-networkpolicy.yaml"
+}
+
+
+
+
+
+
+
+
---
+++ namespace-resources/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster" {
+    description = "What cluster are you deploying your namespace. I.E cloud-platform-test-1 "
+}
+
+variable "namespace" {
+    description = "Namespace you would like to create on cluster <application>-<environment>. I.E myapp-dev"
+}
+
+variable "github_team" {
+    description = "Your github team"
+}
