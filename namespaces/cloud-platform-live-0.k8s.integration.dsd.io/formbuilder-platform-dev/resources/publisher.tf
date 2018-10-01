29bb315eddcde24b9806a34bf1a5134f1c73ae2b
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-10-01 13:25:27 +0100

    Upgrade formbuilder-platform-dev RDS module

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-platform-dev/resources/publisher.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-platform-dev/resources/publisher.tf
@@ -1,15 +1,17 @@
 ##################################################
 # Publisher RDS
+
 module "publisher-rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=2.1"
 
+  cluster_name               = "${var.cluster_name}"
+  cluster_state_bucket       = "${var.cluster_state_bucket}"
   db_backup_retention_period = "2"
-
-  application            = "formbuilderpublisher"
-  environment-name       = "${var.environment-name}"
-  is-production          = "${var.is-production}"
-  infrastructure-support = "${var.infrastructure-support}"
-  team_name              = "${var.team_name}"
+  application                = "formbuilderpublisher"
+  environment-name           = "${var.environment-name}"
+  is-production              = "${var.is-production}"
+  infrastructure-support     = "${var.infrastructure-support}"
+  team_name                  = "${var.team_name}"
 }
 
 resource "kubernetes_secret" "publisher-rds-instance" {
@@ -19,14 +21,8 @@ resource "kubernetes_secret" "publisher-rds-instance" {
   }
 
   data {
-    instance_id = "${module.publisher-rds-instance.rds_instance_id}"
-    arn         = "${module.publisher-rds-instance.rds_instance_arn}"
-
     # postgres://USER:PASSWORD@HOST:PORT/NAME
-    url               = "postgres://${module.publisher-rds-instance.database_username}:${module.publisher-rds-instance.database_password}@${module.publisher-rds-instance.rds_instance_endpoint}/${module.publisher-rds-instance.database_name}"
-    kms_key_id        = "${module.publisher-rds-instance.kms_key_id}"
-    access_key_id     = "${module.publisher-rds-instance.access_key_id}"
-    secret_access_key = "${module.publisher-rds-instance.secret_access_key}"
+    url = "postgres://${module.publisher-rds-instance.database_username}:${module.publisher-rds-instance.database_password}@${module.publisher-rds-instance.rds_instance_endpoint}/${module.publisher-rds-instance.database_name}"
   }
 }
 
