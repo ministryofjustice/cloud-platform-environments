de222bf8b2ac04d3cd4b065d582c5ae8736a37c0
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-12-07 10:53:02 +0000

    Fix broken build with terraform fmt

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cica-development/resources/main.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cica-development/resources/main.tf
@@ -1,22 +1,26 @@
 terraform {
   backend "s3" {}
 }
+
 provider "aws" {
   region = "eu-west-2"
 }
+
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
   team_name = "cica"
   repo_name = "cicadevelopment"
 }
+
 resource "kubernetes_secret" "ecr-repo" {
   metadata {
-    name = "cica"
+    name      = "cica"
     namespace = "cica-development"
   }
+
   data {
-    repo_url = "${module.ecr-repo.repo_url}"
-    access_key_id = "${module.ecr-repo.access_key_id}"
+    repo_url          = "${module.ecr-repo.repo_url}"
+    access_key_id     = "${module.ecr-repo.access_key_id}"
     secret_access_key = "${module.ecr-repo.secret_access_key}"
   }
 }
