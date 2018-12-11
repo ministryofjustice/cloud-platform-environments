3f6e9074359a8aeb8ea8d30f2ae499663f72f262
David Elliott <david.elliott@justice.gov.uk>
2018-12-11 22:22:22 +0000

    Update ecr-credentials to latest release

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/laa-great-ideas-uat/resources/main.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/laa-great-ideas-uat/resources/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
 
   team_name = "laa-great-ideas"
   repo_name = "laa-great-ideas"
