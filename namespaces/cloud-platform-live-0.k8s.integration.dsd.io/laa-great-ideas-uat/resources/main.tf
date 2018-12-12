a5d6871aa2e9aae5a229cdd3a2a174c2cefff07b
David Elliott <david.elliott@digital.justice.gov.uk>
2018-12-12 10:32:42 +0000

    Create ECR repository for laa-great-ideas-uat (#287)
    * Create ECR repository for laa-great-ideas-uat
    
    * Update ecr-credentials to latest release

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/laa-great-ideas-uat/resources/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "ecr-repo" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+
+  team_name = "laa-great-ideas"
+  repo_name = "laa-great-ideas"
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-laa-great-ideas"
+    namespace = "laa-great-ideas-uat"
+  }
+
+  data {
+    repo_url          = "${module.ecr-repo.repo_url}"
+    access_key_id     = "${module.ecr-repo.access_key_id}"
+    secret_access_key = "${module.ecr-repo.secret_access_key}"
+  }
+}
