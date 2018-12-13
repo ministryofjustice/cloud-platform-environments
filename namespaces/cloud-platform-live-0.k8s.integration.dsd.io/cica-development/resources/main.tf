3b7b2f0d7a87ff5af8d04cad92e2c1dacdc75fd8
Razvan Cosma <36888774+razvan-moj@users.noreply.github.com>
2018-12-13 14:30:38 +0000

    fmt

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cica-development/resources/main.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cica-development/resources/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
   team_name = "cica"
   repo_name = "cicadevelopment"
 }
