38e17242b1f96c70a8c071072fc6f79bc84a4100
Razvan Cosma <36888774+razvan-moj@users.noreply.github.com>
2018-12-13 12:04:37 +0000

    aws region duplicate (#295)
    * aws region duplicate
    
    * Update rds-allocations-api.tf

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/offender-management-staging/resources/rds-allocations-api.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/offender-management-staging/resources/rds-allocations-api.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {}
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
 /*
  * When using this module through the cloud-platform-environments, the following
  * two variables are automatically supplied by the pipeline.
