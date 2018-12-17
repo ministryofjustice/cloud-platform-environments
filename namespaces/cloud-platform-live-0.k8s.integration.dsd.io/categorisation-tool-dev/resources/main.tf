bcd1738b7f6366f1652b95ae15bb1383581ca2b5
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2018-12-17 11:48:54 +0000

    categorisation tool dev environment

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/categorisation-tool-dev/resources/main.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/categorisation-tool-dev/resources/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
 
   team_name = "omic"
   repo_name = "categorisation-tool"
