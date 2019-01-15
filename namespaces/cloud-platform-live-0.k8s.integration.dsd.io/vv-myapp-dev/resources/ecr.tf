b1c807dc44ba3ac7cf29235b3a63f79965f87333
Vijay Veeranki <vijayveeranki@mojl0101lm.ws.local>
2019-01-15 15:22:18 +0000

    Created an ECR repository vv-myapp-dev

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/vv-myapp-dev/resources/ecr.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vv-ecr-cred" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=1.0"
+  repo_name = "vv-k8s-deploy-test-app"
+  team_name = "test-webops" 
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-vv-myapp-dev"
+    namespace = "vv-myapp-dev"
+  }
+
+  data {
+    repo_url          = "${module.ecr-repo.repo_url}"
+    access_key_id     = "${module.ecr-repo.access_key_id}"
+    secret_access_key = "${module.ecr-repo.secret_access_key}"
+  }
+}
