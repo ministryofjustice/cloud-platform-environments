cd5055d0edd1e1f472d2c522159ec2c00566c434
Alex Robinson <github@alex.fu2k.org>
2019-01-29 13:26:22 +0000

    Add formbuilder integration-staging namespaces (platform and services)

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-platform-integration-staging/resources/variables.tf
@@ -0,0 +1,29 @@
+# auto-generated from fb-cloud-platforms-environments
+variable "environment-name" {
+  default = "integration-staging"
+}
+
+variable "team_name" {
+  default = "formbuilder"
+}
+
+variable "db_backup_retention_period_submitter" {
+  default = "2"
+}
+
+variable "db_backup_retention_period_user_datastore" {
+  default = "2"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "infrastructure-support" {
+  default = "Form Builder form-builder-team@digital.justice.gov.uk"
+}
+
+// The following two variables are provided at runtime by the pipeline.
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
