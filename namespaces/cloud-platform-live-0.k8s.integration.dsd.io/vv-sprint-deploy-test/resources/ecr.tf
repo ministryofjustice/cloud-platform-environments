8da1bd78c3d8f3cccdd21454b6c259ad339ca2c3
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-01-29 13:47:40 +0000

    updated ecr.tf file to sort out errors in CI pipeline

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/vv-sprint-deploy-test/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/vv-sprint-deploy-test/resources/ecr.tf
@@ -6,20 +6,22 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module vv-ecr-repo {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
-  repo_name = "vv-k8s-sprint-deploy-test"
+module "ecr-repo" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+
   team_name = "test-webops"
+  repo_name = "test-webops"
 }
 
-resource "kubernetes_secret" "vv-ecr-repo" {
+resource "kubernetes_secret" "ecr-repo" {
   metadata {
-    name      = "ecr-repo-vv-sprint-deploy-test"
+    name      = "ecr-repo-test-webops"
     namespace = "vv-sprint-deploy-test"
   }
+
   data {
-    repo_url          = "${module.vv-ecr-repo.repo_url}"
-    access_key_id     = "${module.vv-ecr-repo.access_key_id}"
-    secret_access_key = "${module.vv-ecr-repo.secret_access_key}"
+    repo_url          = "${module.ecr-repo.repo_url}"
+    access_key_id     = "${module.ecr-repo.access_key_id}"
+    secret_access_key = "${module.ecr-repo.secret_access_key}"
   }
 }
