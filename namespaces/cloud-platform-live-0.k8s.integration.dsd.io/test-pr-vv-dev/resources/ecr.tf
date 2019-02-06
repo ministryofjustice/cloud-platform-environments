ab4cb147717b408c58f12c099f5b465efd0cadf2
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-02-06 18:59:57 +0000

    added ecr file

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/test-pr-vv-dev/resources/ecr.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "ecr-repo-test" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+
+  team_name = "test-webops"
+  repo_name = "test-webops"
+}
+
+resource "kubernetes_secret" "ecr-repo-test" {
+  metadata {
+    name      = "ecr-repo-test-webops"
+    namespace = "test-pr-vv-dev"
+  }
+
+  data {
+    repo_url          = "${module.ecr-repo-test.repo_url}"
+    access_key_id     = "${module.ecr-repo-test.access_key_id}"
+    secret_access_key = "${module.ecr-repo_test.secret_access_key}"
+  }
+}
