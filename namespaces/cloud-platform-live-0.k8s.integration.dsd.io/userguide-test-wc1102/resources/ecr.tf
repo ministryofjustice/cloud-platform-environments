5078698e24cd8c4c144f8de735ec9ca5f43d62e6
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-02-11 14:30:10 +0000

    Creating an ECR repository

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/userguide-test-wc1102/resources/ecr.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vv-ecr-wc-1102" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+  repo_name = "vv-k8s-deploy-test-app"
+  team_name = "test-webops"
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-vv-ecr-wc-1102"
+    namespace = "userguide-test-wc1102"
+  }
+
+  data {
+    repo_url          = "${module.vv-ecr-wc-1102.repo_url}"
+    access_key_id     = "${module.vv-ecr-wc-1102.access_key_id}"
+    secret_access_key = "${module.vv-ecr-wc-1102.secret_access_key}"
+  }
+}
