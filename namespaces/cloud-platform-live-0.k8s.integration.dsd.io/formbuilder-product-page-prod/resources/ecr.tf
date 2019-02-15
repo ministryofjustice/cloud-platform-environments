c9f3b8dfe27b7c8f45e77202d94865049fd481f8
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-02-15 05:44:21 +0000

    Change module name and metadata name

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-product-page-prod/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/formbuilder-product-page-prod/resources/ecr.tf
@@ -6,22 +6,22 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "formbuilder_ecr_credentials" {
+module "formbuilder_product_page_ecr_credentials" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
   repo_name = "formBuilder-product-page"
   team_name = "formbuilder"
 }
 
-resource "kubernetes_secret" "formbuilder_ecr_credentials" {
+resource "kubernetes_secret" "formbuilder_product_page_ecr_credentials" {
   metadata {
-    name      = "formbuilder_ecr_credentials_output"
+    name      = "formbuilder-product-page-ecr-credentials-output"
     namespace = "formbuilder-product-page-prod"
   }
 
   data {
-    access_key        = "${module.formbuilder_ecr_credentials.access_key_id}"
-    secret_access_key = "${module.formbuilder_ecr_credentials.secret_access_key}"
-    repo_arn          = "${module.formbuilder_ecr_credentials.repo_arn}"
-    repo_url          = "${module.formbuilder_ecr_credentials.repo_url}"
+    access_key        = "${module.formbuilder_product_page_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.formbuilder_product_page_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.formbuilder_product_page_ecr_credentials.repo_arn}"
+    repo_url          = "${module.formbuilder_product_page_ecr_credentials.repo_url}"
   }
 }
