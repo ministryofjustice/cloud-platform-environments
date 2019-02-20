14bc7b25f66f6572f871408c803cf3babee3c1f7
npreddy <n_preddy@hotmail.com>
2019-02-20 15:49:25 +0000

    Add PQ Staging ECR.

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/pq-staging/resources/rds_instance.tf
@@ -0,0 +1,29 @@
+#############################################
+# Parliamentary Questions (postgres engine) #
+#############################################
+
+module "rds-instance" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=3.1"
+
+  cluster_name         = "${var.cluster_name}"
+  cluster_state_bucket = "${var.cluster_state_bucket}"
+
+  db_backup_retention_period = "${var.db_backup_retention_period}"
+  application                = "${var.application}"
+  environment-name           = "${var.environment-name}"
+  is-production              = "${var.is-production}"
+  infrastructure-support     = "${var.infrastructure-support}"
+  team_name                  = "${var.team_name}"
+}
+
+resource "kubernetes_secret" "rds-instance" {
+  metadata {
+    name      = "rds-instance-pq-staging"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    # postgres://USER:PASSWORD@HOST:PORT/NAME
+    url = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+  }
+}
