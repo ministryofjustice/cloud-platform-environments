f7199b2a7095ec6e6f5726029a1007b023e69eb7
Toby Retallick <toby.retallick@gmail.com>
2019-02-26 11:56:34 +0000

    Create Elasticache instance for Prison Visits Booking Staff

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/prison-visits-booking-staging/resources/elasticache.tf
@@ -0,0 +1,15 @@
+module "ec-cluster-prison-visits-booking-staff" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster"
+  cluster_name                  = "cloud-platform-live-0"
+  cluster_state_bucket          = "live-0-state-bucket"
+  team_name                     = "prison-visits-booking"
+  engine_version                = "4.0.10"
+  parameter_group_name          = "default.redis4.0"
+  node_type                     = "cache.m3.medium"
+  number_cache_clusters         = "3"
+  replication_group_description = "prison-visits-booking-staff-staging"
+  application                   = "prison-visits-booking-staff"
+  is-production                 = "false"
+  environment-name              = "staging"
+  infrastructure-support        = "pvb-technical-support@digtal.justice.gov.uk"
+}
