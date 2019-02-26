bb6e8237fbaa01d29ccdf816e453e6bf752b035b
Toby Retallick <toby.retallick@gmail.com>
2019-02-26 13:39:00 +0000

    Add auth configuration to elasticache.tf

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/prison-visits-booking-staging/resources/elasticache.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/prison-visits-booking-staging/resources/elasticache.tf
@@ -8,3 +8,15 @@ module "ec-cluster-prison-visits-booking-staff" {
   environment-name       = "${var.environment-name}"
   infrastructure-support = "${var.infrastructure-support}"
 }
+
+resource "kubernetes_secret" "ec-cluster-prison-visits-booking-staff" {
+  metadata {
+    name      = "elasticache-prison-visits-booking-token-cache-${var.environment-name}"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    primary_endpoint_address = "${module.ec-cluster-prison-visits-booking-staff.primary_endpoint_address}"
+    auth_token               = "${module.ec-cluster-prison-visits-booking-staff.auth_token}"
+  }
+}
