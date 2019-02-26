fafb804cf0f3c491dbdf423c4daa29c43ab3deaa
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-02-26 16:30:41 +0000

    Commented out DummyApp creation

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/dummy-app-dev/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/dummy-app-dev/resources/ecr.tf
@@ -1,19 +1,16 @@
 # provider "aws" {
 #   region = "eu-west-2"
 # }
-
 # module "example_team_ecr_credentials" {
 #   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
 #   repo_name = "dummy-app-mourad"
 #   team_name = "cloud-platform-test"
 # }
-
 # resource "kubernetes_secret" "dummy_app_mourad_ecr_credentials" {
 #   metadata {
 #     name      = "dummy-app-mourad-ecr-credentials-output"
 #     namespace = "dummy-app-dev"
 #   }
-
 #   data {
 #     access_key_id     = "${module.example_team_ecr_credentials.access_key_id}"
 #     secret_access_key = "${module.example_team_ecr_credentials.secret_access_key}"
@@ -21,3 +18,4 @@
 #     repo_url          = "${module.example_team_ecr_credentials.repo_url}"
 #   }
 # }
+
