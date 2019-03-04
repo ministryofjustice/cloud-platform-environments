f6d1121621fd2cb835c79ddb6311e769afb216b1
David Salgado <david.salgado@digital.justice.gov.uk>
2019-03-04 14:00:39 +0000

    Add an RDS instance to David S' sandbox

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/davids-dummy-dev/resources/multicontainer-demo-app-rds.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {}
+variable "cluster_state_bucket" {}
+
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "example_team_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=3.1"
+
+  application            = "${var.application}"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "${var.team_name}"
+  is-production          = "${var.is-production}"
+  environment-name       = "${var.environment-name}"
+  infrastructure-support = "${var.infrastructure-support}"
+}
+
+resource "kubernetes_secret" "multi_container_demo_rds" {
+  metadata {
+    name      = "rds-instance-${var.application}-${var.environment-name}"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    # postgres://USER:PASSWORD@HOST:PORT/NAME
+    url = "postgres://${module.submitter-rds-instance.database_username}:${module.submitter-rds-instance.database_password}@${module.submitter-rds-instance.rds_instance_endpoint}/${module.submitter-rds-instance.database_name}"
+  }
+}
---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/davids-dummy-dev/resources/variables.tf
@@ -0,0 +1,26 @@
+# auto-generated from fb-cloud-platforms-environments
+
+variable "application" {
+  default = "multi-container-demo"
+}
+
+variable "environment-name" {
+  default = "dev"
+}
+
+variable "team_name" {
+  default = "davids-dummy-dev"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "infrastructure-support" {
+  default = "David Salgado david.salgado@digital.justice.gov.uk"
+}
+
+// The following two variables are provided at runtime by the pipeline.
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
