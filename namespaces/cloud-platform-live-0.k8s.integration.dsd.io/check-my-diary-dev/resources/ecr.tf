fc3df61c54ed879797ab4042bcaa1b9147c090f8
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-03-05 13:13:00 +0000

    Removed duplicate provider

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/check-my-diary-dev/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/check-my-diary-dev/resources/ecr.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {}
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
 module "checkmydiary-service-dev" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.1"
   repo_name = "check-my-diary-dev"
