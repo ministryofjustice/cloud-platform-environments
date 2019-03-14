f07593851c18592f282ec1989acf1b1692a68287
Anthony McKernan <anthonymckernan@gmail.com>
2019-03-14 15:23:15 +0000

    add disclosure-checker-staging namespace

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/disclosure-checker-staging/resources/variables.tf
@@ -0,0 +1,36 @@
+variable "team_name" {
+  default = "family-justice"
+}
+
+variable "environment-name" {
+  default = "staging"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "infrastructure-support" {
+  default = "Family Justice: family-justice-team@digital.justice.gov.uk"
+}
+
+variable "application" {
+  default = "Disclosure Checker"
+}
+
+variable "namespace" {
+  default = "disclosure-checker-staging"
+}
+
+variable "repo_name" {
+  default = "disclosure-checker"
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+// The following two variables are provided at runtime by the pipeline.
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
