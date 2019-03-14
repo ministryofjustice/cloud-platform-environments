c09f5a4819ca6d4f85632dc0c45b9a2f09d03b49
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2019-03-14 11:45:37 +0000

    Add ecr to cccd-staging (#508)

---
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cccd-staging/resources/ecr.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "cbo_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
+  repo_name = "cccd"
+  team_name = "laa-get-paid"
+}
+
+resource "kubernetes_secret" "cbo_ecr_credentials" {
+  metadata {
+    name      = "cbo-credentials-output"
+    namespace = "cccd-staging"
+  }
+
+  data {
+    access_key_id     = "${module.cbo_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.cbo_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.cbo_ecr_credentials.repo_arn}"
+    repo_url          = "${module.cbo_ecr_credentials.repo_url}"
+  }
+}
