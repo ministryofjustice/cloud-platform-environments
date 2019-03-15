943e617eddb82926179212b41d810a38e3dd6832
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2019-03-15 09:59:41 +0000

    Update rds cccd staging (#512)
    * Add the engine version and db_name
    
    * Update the secrets to point at the generated output

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cccd-staging/resources/rds.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/cccd-staging/resources/rds.tf
@@ -4,7 +4,7 @@
  * releases page of this repository.
  *
  */
-module "example_team_rds" {
+module "cccd_staging_rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=3.1"
 
   cluster_name           = "${var.cluster_name}"
@@ -15,19 +15,21 @@ module "example_team_rds" {
   is-production          = "false"
   environment-name       = "staging"
   infrastructure-support = "crowncourtdefence@digtal.justice.gov.uk"
+  db_engine_version      = "10.6"
+  db_name                = "adp_staging"
 }
 
-resource "kubernetes_secret" "cbo-rds-credentials-output" {
+resource "kubernetes_secret" "cccd_staging_rds" {
   metadata {
-    name      = "cbo-rds-credentials-output"
+    name      = "cccd-staging-rds-output"
     namespace = "cccd-staging"
   }
 
   data {
-    rds_instance_endpoint = "${module.example_team_rds.rds_instance_endpoint}"
-    database_name         = "${module.example_team_rds.database_name}"
-    database_username     = "${module.example_team_rds.database_username}"
-    database_password     = "${module.example_team_rds.database_password}"
-    rds_instance_address  = "${module.example_team_rds.rds_instance_address}"
+    rds_instance_endpoint = "${module.cccd_staging_rds.rds_instance_endpoint}"
+    database_name         = "${module.cccd_staging_rds.database_name}"
+    database_username     = "${module.cccd_staging_rds.database_username}"
+    database_password     = "${module.cccd_staging_rds.database_password}"
+    rds_instance_address  = "${module.cccd_staging_rds.rds_instance_address}"
   }
 }
