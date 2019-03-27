9d76aa574e370f95c016110e412c2402b4d11960
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-03-27 12:20:58 +0000

    Remove _checkenv in favour of nounset

--- bin/.common.sh
+++ bin/.common.sh
@@ -9,23 +9,3 @@ log() {
   # shellcheck disable=SC2145
   echo -e "${_fg}>>> ${@}\033[0m"
 }
-
-_checkenv(){
-  missing=""
-  for v in \
-      PIPELINE_CLUSTER \
-      PIPELINE_STATE_BUCKET \
-      PIPELINE_STATE_KEY_PREFIX \
-      PIPELINE_CLUSTER_STATE_BUCKET \
-      PIPELINE_CLUSTER_STATE_KEY_PREFIX; do
-    fv=""
-    eval fv="\${$v+xyz}"
-    if [ -z "${fv}" ]; then
-      missing="${missing}\n\t${v}"
-    fi
-  done
-  if [ ! -z "${missing}" ]; then
-    echo -e "missing environment variables:${missing}"
-    exit 1
-  fi
-}
