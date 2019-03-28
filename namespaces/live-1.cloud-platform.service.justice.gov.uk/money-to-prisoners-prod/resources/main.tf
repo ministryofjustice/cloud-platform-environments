d7afb9018e20ef85f1fbee2300dbf5c5cdf3cb0e
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-03-28 15:39:00 +0000

    Re-create live-1 MTP resources in AWS eu-west-2 …excluding prod RDS which is expensive and not yet needed

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/money-to-prisoners-prod/resources/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "namespace" {
+  default = "money-to-prisoners-prod"
+}
+
+variable "business-unit" {
+  default = "HMPPS"
+}
+
+variable "team_name" {
+  default = "prisoner-money"
+}
+
+variable "application" {
+  default = "money-to-prisoners"
+}
+
+variable "email" {
+  default = "money-to-prisoners@digital.justice.gov.uk"
+}
+
+variable "environment-name" {
+  default = "prod"
+}
+
+variable "is-production" {
+  default = "true"
+}
