caeaff294a24a499a5a22ccfeed06cbfc57d0a27
ermiasworku <ermis.worku@cica.gov.uk>
2019-04-12 20:06:14 +0000

    Updated ecr.tf file

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-uat/resources/ecr.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "cica-repo" {
+  source     = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=$
+  repo_name  = "cica"
+  team_name  = "cica"
+  aws_region = "eu-west-2"                                                                $
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-claim-criminal-injuries-compensation-uat"
+    namespace = "claim-criminal-injuries-compensation-uat"
+  }
+
+  data {
+    access_key_id     = "${module.cica-repo.access_key_id}"
+    secret_access_key = "${module.cica-repo.secret_access_key}"
+    repo_url          = "${module.cica-repo.repo_url}"
+  }
+}
+
