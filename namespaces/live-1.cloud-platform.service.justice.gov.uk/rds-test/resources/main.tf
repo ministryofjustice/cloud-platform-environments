04ac6be8af8a424d297c9dca4e8d1d46cabf09dc
ollieanwyll <oliver.anwyll@digital.justice.gov.uk>
2019-04-15 15:31:39 +0100

    terraform fmt

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/rds-test/resources/main.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/rds-test/resources/main.tf
@@ -23,7 +23,7 @@ variable "cluster_state_bucket" {}
  *
  */
 module "example_team_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.0"
   cluster_name           = "${var.cluster_name}"
   cluster_state_bucket   = "${var.cluster_state_bucket}"
   team_name              = "cloud-platform"
