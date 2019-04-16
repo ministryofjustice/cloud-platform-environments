b6cf0aa3c6d147af40bbb674d7d858f4e157c178
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-04-16 17:08:18 +0100

    Updated ecr.tf to run the plan pipeline

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/live0-to-live1/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/live0-to-live1/resources/ecr.tf
@@ -1,3 +1,9 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
 module "ecr_live0_to_live1" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.1"
   repo_name = "ecr-live0-to-live1"
