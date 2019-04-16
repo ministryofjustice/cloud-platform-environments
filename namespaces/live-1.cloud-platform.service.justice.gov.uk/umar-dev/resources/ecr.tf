9b659ba21fe16120dba9e6731b00d2f39cdf06f7
Umar Hansa <umar.hansa@gmail.com>
2019-04-16 13:59:03 +0100

    Adds new namespace

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/umar-dev/resources/ecr.tf
@@ -0,0 +1,20 @@
+module "umar-dev-ecr-credentials" {
+  source     = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.1"
+  repo_name  = "hello-world-demo-app"
+  team_name  = "umar-dev-ecr-team"
+  aws_region = "eu-west-2"
+}
+
+resource "kubernetes_secret" "umar-dev-ecr-credentials" {
+  metadata {
+    name      = "umar-dev-ecr-credentials-output"
+    namespace = "umar-dev"
+  }
+
+  data {
+    access_key_id     = "${module.umar-dev-ecr-credentials.access_key_id}"
+    secret_access_key = "${module.umar-dev-ecr-credentials.secret_access_key}"
+    repo_arn          = "${module.umar-dev-ecr-credentials.repo_arn}"
+    repo_url          = "${module.umar-dev-ecr-credentials.repo_url}"
+  }
+}
