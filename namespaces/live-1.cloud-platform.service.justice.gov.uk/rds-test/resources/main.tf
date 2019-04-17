b3512c853f82df43c3c4f7bac2f83846026f6639
ollieanwyll <oliver.anwyll@digital.justice.gov.uk>
2019-04-17 19:50:13 +0100

    increase rds storage size

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/rds-test/resources/main.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/rds-test/resources/main.tf
@@ -33,7 +33,7 @@ module "example_team_rds" {
   environment-name       = "rds-test"
   infrastructure-support = "oliver.anwyll@digtal.justice.gov.uk"
   aws_region             = "eu-west-2"
-  db_allocated_storage   = "50"
+  db_allocated_storage   = "90"
 }
 
 resource "kubernetes_secret" "example_team_rds" {
