d6f6e59632b7330a61aac9d7034fb81c16bede12
JamieDraperUK <draper65@googlemail.com>
2019-04-24 15:08:42 +0100

    Add an rbac entry to support circleci and remove whereabouts-api-postgres ecr

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/whereabouts-dev/resources/main.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/whereabouts-dev/resources/main.tf
@@ -13,12 +13,6 @@ module "ecr-repo" {
   repo_name = "whereabouts-api"
 }
 
-module "whereabouts-api-postgres-ecr-repo" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.1"
-  team_name = "omic"
-  repo_name = "whereabouts-api-postgres"
-}
-
 resource "kubernetes_secret" "ecr-repo" {
   metadata {
     name      = "ecr-repo-whereabouts-api"
@@ -29,18 +23,5 @@ resource "kubernetes_secret" "ecr-repo" {
     repo_url          = "${module.ecr-repo.repo_url}"
     access_key_id     = "${module.ecr-repo.access_key_id}"
     secret_access_key = "${module.ecr-repo.secret_access_key}"
-  }
-}
-
-resource "kubernetes_secret" "whereabouts-api-postgres-ecr-repo" {
-  metadata {
-    name      = "whereabouts-api-postgres-ecr-repo"
-    namespace = "whereabouts-dev"
-  }
-
-  data {
-    repo_url          = "${module.whereabouts-api-postgres-ecr-repo.repo_url}"
-    access_key_id     = "${module.whereabouts-api-postgres-ecr-repo.access_key_id}"
-    secret_access_key = "${module.whereabouts-api-postgres-ecr-repo.secret_access_key}"
   }
 }
