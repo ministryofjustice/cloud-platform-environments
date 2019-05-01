ad525db7b727d6943ae0e3391a71cdb922e53cd6
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2019-05-01 11:17:28 +0100

    Update to latest version of ecr.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-api-sandbox/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-api-sandbox/resources/ecr.tf
@@ -1,19 +1,23 @@
-module "cccd_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.1"
+module "cbo_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.2"
   repo_name = "cccd"
   team_name = "laa-get-paid"
+
+  providers = {
+    aws = "aws.london"
+  }
 }
 
-resource "kubernetes_secret" "cccd_ecr_credentials" {
+resource "kubernetes_secret" "cbo_ecr_credentials" {
   metadata {
-    name      = "cccd-ecr-credentials-output"
+    name      = "cbo-credentials-output"
     namespace = "cccd-api-sandbox"
   }
 
   data {
-    access_key_id     = "${module.cccd_ecr_credentials.access_key_id}"
-    secret_access_key = "${module.cccd_ecr_credentials.secret_access_key}"
-    repo_arn          = "${module.cccd_ecr_credentials.repo_arn}"
-    repo_url          = "${module.cccd_ecr_credentials.repo_url}"
+    access_key_id     = "${module.cbo_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.cbo_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.cbo_ecr_credentials.repo_arn}"
+    repo_url          = "${module.cbo_ecr_credentials.repo_url}"
   }
 }
