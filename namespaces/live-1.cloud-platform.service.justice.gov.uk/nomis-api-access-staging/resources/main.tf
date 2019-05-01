eb8b7e80cbb8b9931681d0ef86b81e987d046c29
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2019-05-01 13:43:50 +0100

    nomis-api-access-staging fix provider config

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/nomis-api-access-staging/resources/main.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/nomis-api-access-staging/resources/main.tf
@@ -2,6 +2,14 @@ terraform {
   backend "s3" {}
 }
 
+# To be use in case the resources need to be created in London
 provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+# To be use in case the resources need to be created in Ireland
+provider "aws" {
+  alias  = "ireland"
   region = "eu-west-1"
 }
