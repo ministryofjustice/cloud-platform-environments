bfdde219c7e259147adc327a45162d70cf5a4ad8
ermiasworku <ermis.worku@cica.gov.uk>
2019-05-08 05:14:04 +0000

    update ecr.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/uat-claim-criminal-injuries-compensation/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/uat-claim-criminal-injuries-compensation/resources/ecr.tf
@@ -1,14 +1,19 @@
-*
+/*
  * Make sure that you use the latest version of the module by changing the
  * `ref=` value in the `source` attribute to the latest version listed on the
  * releases page of this repository.
  *
  */
-module "cica_ecr_credentials" {
-  source     = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.1"
-  repo_name  = "cica-repo-uat"
-  team_name  = "cica"
-  aws_region = "${var.aws_region}"
+module "example_team_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.2"
+  repo_name = "cica-repo-uat"
+  team_name = "cica"
+
+  # aws_region = "eu-west-2"     # This input is deprecated from version 3.2 of this module
+
+  providers = {
+    aws = "aws.london"
+  }
 }
 
 resource "kubernetes_secret" "ecr_repo" {
@@ -18,9 +23,9 @@ resource "kubernetes_secret" "ecr_repo" {
   }
 
   data {
-    access_key_id     = "${module.cica_ecr_credentials.access_key_id}"
-    secret_access_key = "${module.cica_ecr_credentials.secret_access_key}"
-    repo_arn          = "${module.cica_ecr_credentials.repo_arn}"
-    repo_url          = "${module.cica_ecr_credentials.repo_url}"
+    access_key_id     = "${module.example_team_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.example_team_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.example_team_ecr_credentials.repo_arn}"
+    repo_url          = "${module.example_team_ecr_credentials.repo_url}"
   }
 }
