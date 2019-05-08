0d29665cea7ee037e3cdbd18ad4e2496467e79c2
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2019-05-08 09:49:21 +0100

    nomis-api-access production

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/nomis-api-access-production/resources/rds.tf
@@ -0,0 +1,35 @@
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
+
+module "nomis-api-access_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.2"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "Digital Prison Services/New Nomis"
+  business-unit          = "HMPPS"
+  application            = "nomis-api-access"
+  is-production          = "true"
+  environment-name       = "production"
+  infrastructure-support = "matt.whittaker@digtal.justice.gov.uk"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "nomis-api-access_rds" {
+  metadata {
+    name      = "nomis-api-access-rds-instance-output"
+    namespace = "nomis-api-access-production"
+  }
+
+  data {
+    rds_instance_endpoint = "${module.nomis-api-access_rds.rds_instance_endpoint}"
+    database_name         = "${module.nomis-api-access_rds.database_name}"
+    database_username     = "${module.nomis-api-access_rds.database_username}"
+    database_password     = "${module.nomis-api-access_rds.database_password}"
+    rds_instance_address  = "${module.nomis-api-access_rds.rds_instance_address}"
+    url                   = "postgres://${module.nomis-api-access_rds.database_username}:${module.nomis-api-access_rds.database_password}@${module.nomis-api-access_rds.rds_instance_endpoint}/${module.nomis-api-access_rds.database_name}"
+  }
+}
