54f80f51aa63ba19be4dfdafda9eafa59da1f595
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2019-05-13 14:49:30 +0100

    Add RDS to cccd dev in live-1 cluster (#746)
    * Add RDS to cccd dev in live-1 cluster
    
    Create main.tf to reduce duplication of region setup

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/rds.tf
@@ -0,0 +1,40 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+
+module "cccd_dev_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.0"
+
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "laa-get-paid"
+  business-unit          = "legal-aid-agency"
+  application            = "cccd"
+  is-production          = "false"
+  environment-name       = "dev"
+  infrastructure-support = "crowncourtdefence@digtal.justice.gov.uk"
+  db_engine_version      = "10.6"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "cccd_dev_rds" {
+  metadata {
+    name      = "cccd-dev-rds-output"
+    namespace = "cccd-dev"
+  }
+
+  data {
+    rds_instance_endpoint = "${module.cccd_dev_rds.rds_instance_endpoint}"
+    database_name         = "${module.cccd_dev_rds.database_name}"
+    database_username     = "${module.cccd_dev_rds.database_username}"
+    database_password     = "${module.cccd_dev_rds.database_password}"
+    rds_instance_address  = "${module.cccd_dev_rds.rds_instance_address}"
+    url                   = "postgres://${module.cccd_dev_rds.database_username}:${module.cccd_dev_rds.database_password}@${module.cccd_dev_rds.rds_instance_endpoint}/${module.cccd_dev_rds.database_name}"
+  }
+}
