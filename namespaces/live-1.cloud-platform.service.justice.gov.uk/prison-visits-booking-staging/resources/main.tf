3af12d5c5a1ac867dfc1ec77a441ba894e056436
David Salgado <david@digitalronin.com>
2019-05-13 16:42:31 +0100

    Add london & ireland aws providers

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/main.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/main.tf
@@ -6,6 +6,16 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
 variable "cluster_name" {}
 
 variable "cluster_state_bucket" {}
