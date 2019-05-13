9e06378daf1db59abbe67b5a31df0ba2b48782be
ermiasworku <ermis.worku@cica.gov.uk>
2019-05-13 13:25:04 +0000

    modify variable.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-uat/resources/variable.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-uat/resources/variable.tf
@@ -1,40 +1,28 @@
 variable "namespace" {
-  default = "claim-criminal-injuries-compensation"
+  default = "claim-criminal-injuries-compensation-uat"
 }
-
 variable "business-unit" {
   default = "cica"
 }
-
 variable "team_name" {
   default = "cica-dev-team"
 }
-
 variable "application" {
-  default = "claim-criminal-injuries-compensation"
+  default = "uat-claim-criminal-injuries-compensation"
 }
-
 variable "email" {
   default = "Infrastructure@cica.gov.uk"
 }
-
 variable "environment-name" {
   default = "uat"
 }
-
 variable "is-production" {
   default = "false"
 }
-
 variable "domain" {
   default = "uat-claim-criminal-injuries-compensation.service.justice.gov.uk"
 }
-
-variable "namespace" {
-  default = "claim-criminal-injuries-compensation-uat"
-}
-
 variable "infrastructure-support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "Infrastructure@cica.gov.uk"
+  default = "Infrastructure@cica.gov.uk"
 }
