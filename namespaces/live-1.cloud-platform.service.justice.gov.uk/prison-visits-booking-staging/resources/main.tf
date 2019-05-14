541251ee6e023eee89ecb10bacd8a42077fbdc29
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-05-14 08:02:56 +0100

    Setup new RDS instance for PVB staging

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/main.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/main.tf
@@ -5,17 +5,3 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
-
-provider "aws" {
-  alias  = "london"
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
-}
-
-variable "cluster_name" {}
-
-variable "cluster_state_bucket" {}
