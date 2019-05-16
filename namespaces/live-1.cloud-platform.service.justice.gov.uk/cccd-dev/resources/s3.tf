9c11f30d59727de1587236883c05a9745bede569
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-05-16 15:05:55 +0100

    Add s3.tf to cccd-dev

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/s3.tf
@@ -0,0 +1,29 @@
+module "cccd_s3_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=3.2"
+
+  team_name              = "laa-get-paid"
+  business-unit          = "legal-aid-agency"
+  application            = "cccd"
+  is-production          = "false"
+  environment-name       = "dev"
+  infrastructure-support = "crowncourtdefence@digtal.justice.gov.uk"
+
+  # This is a new input. 
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "cccd_s3_bucket" {
+  metadata {
+    name      = "cccd-s3-bucket-output"
+    namespace = "cccd-dev"
+  }
+
+  data {
+    access_key_id     = "${module.cccd_s3_bucket.access_key_id}"
+    secret_access_key = "${module.cccd_s3_bucket.secret_access_key}"
+    bucket_arn        = "${module.cccd_s3_bucket.bucket_arn}"
+    bucket_name       = "${module.cccd_s3_bucket.bucket_name}"
+  }
+}
