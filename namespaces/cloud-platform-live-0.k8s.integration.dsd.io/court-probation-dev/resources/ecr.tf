0215b443babb5bcad974dcf96ab6fcd145f2b32b
Andy Marke <andy.marke@gmail.com>
2019-05-20 16:21:39 +0100

    additional repo for parsing pack service

--- namespaces/cloud-platform-live-0.k8s.integration.dsd.io/court-probation-dev/resources/ecr.tf
+++ namespaces/cloud-platform-live-0.k8s.integration.dsd.io/court-probation-dev/resources/ecr.tf
@@ -25,3 +25,23 @@ resource "kubernetes_secret" "probation_services_ecr_credentials" {
     repo_url          = "${module.probation_services_ecr_credentials.repo_url}"
   }
 }
+
+module "ps_cps_pack_parser_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.1"
+  repo_name = "cps-pack-parser"
+  team_name = "probation-services"
+}
+
+resource "kubernetes_secret" "ps_cps_pack_parser_ecr_credentials" {
+  metadata {
+    name      = "ps_cps_pack_parser_ecr_credentials-output"
+    namespace = "court-probation-dev"
+  }
+
+  data {
+    access_key_id     = "${module.ps_cps_pack_parser_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.ps_cps_pack_parser_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.ps_cps_pack_parser_ecr_credentials.repo_arn}"
+    repo_url          = "${module.ps_cps_pack_parser_ecr_credentials.repo_url}"
+  }
+}
