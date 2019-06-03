3bc552b6d8cb2d97e6e28e143a573fdcb3797f7b
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-06-03 17:12:06 +0100

    tf fmt

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/sqs.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/sqs.tf
@@ -1,19 +1,16 @@
 # module "responses_for_cccd" {
 #   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=1.0"
-
 #   environment-name       = "dev"
 #   team_name              = "laa-get-paid"
 #   infrastructure-support = "crowncourtdefence@digtal.justice.gov.uk"
 #   application            = "cccd"
 #   aws_region             = "eu-west-2"
 # }
-
 # resource "kubernetes_secret" "responses_for_cccd" {
 #   metadata {
 #     name      = "responses-for-cccd-sqs"
 #     namespace = "cccd-dev"
 #   }
-
 #   data {
 #     access_key_id     = "${module.responses_for_cccd.access_key_id}"
 #     secret_access_key = "${module.responses_for_cccd.secret_access_key}"
@@ -21,3 +18,4 @@
 #     sqs_arn           = "${module.responses_for_cccd.sqs_arn}"
 #   }
 # }
+
