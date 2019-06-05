289d6687efa22c2661091dcdf2955d4b80c61165
beccapearce <rebecca.pearce.92@gmail.com>
2019-06-05 17:00:47 +0100

    add name and separate variables for pq dev database

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/parliamentary-questions-development/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/parliamentary-questions-development/resources/rds.tf
@@ -7,6 +7,9 @@ module "rds_instance" {
   cluster_name               = "${var.cluster_name}"
   cluster_state_bucket       = "${var.cluster_state_bucket}"
   db_backup_retention_period = "${var.db_backup_retention_period}"
+  db_engine                  = "postgres"
+  db_engine_version          = "11"
+  db_name                    = "parliamentary-questions_dev"
   environment-name           = "${var.environment-name}"
   infrastructure-support     = "${var.infrastructure-support}"
   is-production              = "${var.is-production}"
@@ -21,7 +24,11 @@ resource "kubernetes_secret" "rds_instance" {
   }
 
   data {
-    # postgres://USER:PASSWORD@HOST:PORT/NAME
-    url = "postgres://${module.rds_instance.database_username}:${module.rds_instance.database_password}@${module.rds_instance.rds_instance_endpoint}/${module.rds_instance.database_name}"
+    database_name         = "${module.rds_instance.database_name}"
+    database_password     = "${module.rds_instance.database_password}"
+    database_username     = "${module.rds_instance.database_username}"
+    rds_instance_address  = "${module.rds_instance.rds_instance_address}"
+    rds_instance_endpoint = "${module.rds_instance.rds_instance_endpoint}"
+    url                   = "postgres://${module.rds_instance.database_username}:${module.rds_instance.database_password}@${module.rds_instance.rds_instance_endpoint}/${module.rds_instance.database_name}"
   }
 }
