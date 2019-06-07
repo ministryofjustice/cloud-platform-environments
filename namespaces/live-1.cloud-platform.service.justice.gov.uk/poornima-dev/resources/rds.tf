2524ab85518de65dcbe1cdc7778ad60710aaf042
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2019-06-07 10:09:57 +0100

    Creating a test RDS repository
    Added rds.tf file to namespace poornima-dev to test multi container application in my namespace

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/rds.tf
@@ -0,0 +1,58 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
+
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "cp_team_test_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.3"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "cp-team-test-repo"
+  business-unit          = "cp-team-test-bu"
+  application            = "cpteamtestapp"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "poornima.krishnasamy@digtal.justice.gov.uk"
+  force_ssl              = "true"
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "true"
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "cp_team_test_rds" {
+  metadata {
+    name      = "cp-team-test-rds-instance-output"
+    namespace = "poornima-dev"
+  }
+
+  data {
+    rds_instance_endpoint = "${module.cp_team_test_rds.rds_instance_endpoint}"
+    database_name         = "${module.cp_team_test_rds.rds.database_name}"
+    database_username     = "${module.cp_team_test_rds.database_username}"
+    database_password     = "${module.cp_team_test_rds.database_password}"
+    rds_instance_address  = "${module.cp_team_test_rds.rds_instance_address}"
+
+    /* You can replace all of the above with the following, if you prefer to
+     * use a single database URL value in your application code:
+     *
+     * url = "postgres://${module.cp_team_test_rds.database_username}:${module.cp_team_test_rds.database_password}@${module.cp_team_test_rds.rds_instance_endpoint}/${module.cp_team_test_rds.database_name}"
+     *
+     */
+  }
+}
