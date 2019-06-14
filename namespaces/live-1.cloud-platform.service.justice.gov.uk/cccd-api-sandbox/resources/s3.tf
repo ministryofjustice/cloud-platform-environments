aa714b634c26ccb8f6d23813fb8323fad88fda21
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-06-14 11:53:10 +0100

    add s3 to cccd-api-sandbox

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-api-sandbox/resources/s3.tf
@@ -0,0 +1,28 @@
+module "cccd_s3_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=3.2"
+
+  team_name              = "laa-get-paid"
+  business-unit          = "legal-aid-agency"
+  application            = "cccd"
+  is-production          = "false"
+  environment-name       = "api-sandbox"
+  infrastructure-support = "crowncourtdefence@digtal.justice.gov.uk"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "cccd_s3_bucket" {
+  metadata {
+    name      = "cccd-s3-bucket"
+    namespace = "cccd-api-sandbox"
+  }
+
+  data {
+    access_key_id     = "${module.cccd_s3_bucket.access_key_id}"
+    secret_access_key = "${module.cccd_s3_bucket.secret_access_key}"
+    bucket_arn        = "${module.cccd_s3_bucket.bucket_arn}"
+    bucket_name       = "${module.cccd_s3_bucket.bucket_name}"
+  }
+}
