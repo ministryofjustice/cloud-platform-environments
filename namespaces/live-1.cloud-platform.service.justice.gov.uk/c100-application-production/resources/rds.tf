4f1ae8f26fb6cf2a5a54dfcbbcd2352b2ac83e03
Jesus Laiz <zheileman@users.noreply.github.com>
2019-06-19 11:36:05 +0100

    Bump RDS module version in C100 production
    * Using the new providers mechanism.
    * Enable force SSL.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-production/resources/rds.tf
@@ -3,7 +3,7 @@
 ########################################
 
 module "rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.4"
 
   cluster_name         = "${var.cluster_name}"
   cluster_state_bucket = "${var.cluster_state_bucket}"
@@ -13,6 +13,11 @@ module "rds-instance" {
   is-production          = "${var.is-production}"
   infrastructure-support = "${var.infrastructure-support}"
   team_name              = "${var.team_name}"
+  force_ssl              = true
+
+  providers = {
+    aws = "aws.london"
+  }
 }
 
 resource "kubernetes_secret" "rds-instance" {
