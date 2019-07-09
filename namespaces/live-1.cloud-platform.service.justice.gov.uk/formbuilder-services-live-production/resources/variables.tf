03fbda9ba904e7a4790fb1a5172e42cdc4c77037
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-07-09 12:23:54 +0100

    rework form builder route53 terraforms

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-services-live-production/resources/variables.tf
@@ -0,0 +1,15 @@
+variable "is-production" {
+  default = "true"
+}
+
+variable "environment-name" {
+  default = "live-production"
+}
+
+variable "team_name" {
+  default = "formbuilder"
+}
+
+variable "infrastructure-support" {
+  default = "Form Builder form-builder-team@digital.justice.gov.uk"
+}
