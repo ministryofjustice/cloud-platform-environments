9ab2c1e52e2868f0b4118381dc43d65d772a288c
Steve Hook <steve.hook@gmail.com>
2019-07-17 16:33:49 +0100

    Add `hmpps-book-secure-move-api-production` env

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/ecr.tf
@@ -0,0 +1,23 @@
+module "ecr-repo" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+
+  team_name = "${var.team_name}"
+  repo_name = "${var.repo_name}"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-hmpps-book-secure-move-api"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    repo_url          = "${module.ecr-repo.repo_url}"
+    access_key_id     = "${module.ecr-repo.access_key_id}"
+    secret_access_key = "${module.ecr-repo.secret_access_key}"
+  }
+}
