a6abaf670f3fd269dd4e0e2e8724382e4f32b238
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-07-30 14:51:22 +0100

    Add jason-lab RDS instance with variables

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/jason-lab/resources/jason-lab-rds.tf
@@ -0,0 +1,24 @@
+module "jason-lab-rds-instance" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.5"
+
+  cluster_name               = "${var.cluster_name}"
+  cluster_state_bucket       = "${var.cluster_state_bucket}"
+  db_backup_retention_period = "${var.db_backup_retention_period_user_datastore}"
+  application                = "${var.application}"
+  environment-name           = "${var.environment-name}"
+  is-production              = "${var.is-production}"
+  infrastructure-support     = "${var.infrastructure-support}"
+  team_name                  = "${var.team_name}"
+}
+
+resource "kubernetes_secret" "jason-lab-rds-instance" {
+  metadata {
+    name      = "jason-lab-${var.environment-name}"
+    namespace = "jason-lab"
+  }
+
+  data {
+    # postgres://USER:PASSWORD@HOST:PORT/NAME
+    url = "postgres://${module.jason-lab-rds-instance.database_username}:${module.jason-lab-rds-instance.database_password}@${module.jason-lab-rds-instance.rds_instance_endpoint}/${module.jason-lab-rds-instance.database_name}"
+  }
+}
