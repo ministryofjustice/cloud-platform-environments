bfc3e606206cef23d6c86c4fa11dd3bc713d386d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-07-30 14:56:37 +0100

    Run terraform fmt to jason-lab

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/jason-lab/resources/jason-lab-rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/jason-lab/resources/jason-lab-rds.tf
@@ -1,13 +1,13 @@
 module "jason-lab-rds-instance" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.5"
 
-  cluster_name               = "${var.cluster_name}"
-  cluster_state_bucket       = "${var.cluster_state_bucket}"
-  application                = "${var.application}"
-  environment-name           = "${var.environment-name}"
-  is-production              = "${var.is-production}"
-  infrastructure-support     = "${var.infrastructure-support}"
-  team_name                  = "${var.team_name}"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  application            = "${var.application}"
+  environment-name       = "${var.environment-name}"
+  is-production          = "${var.is-production}"
+  infrastructure-support = "${var.infrastructure-support}"
+  team_name              = "${var.team_name}"
 }
 
 resource "kubernetes_secret" "jason-lab-rds-instance" {
