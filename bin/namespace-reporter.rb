ab65827f8c6f9ddfd004e3a2f0c987fd2d4671de
David Salgado <david.salgado@digital.justice.gov.uk>
2019-08-07 14:18:34 +0100

    Enable parsing memory values like '999Ki'

--- bin/namespace-reporter.rb
+++ bin/namespace-reporter.rb
@@ -168,7 +168,7 @@ class Namespace
     case str
     when /^(\d+)$/
       $1.to_i / 1_000
-    when /^(\d+)k$/
+    when /^(\d+)k$/, /^(\d+)Ki$/
       $1.to_i / 1024
     when /^(\d+)m$/ # e.g. 6.4Gi in yaml => 6871947673600m in the JSON kubectl output
       $1.to_i / 1_000_000_000
