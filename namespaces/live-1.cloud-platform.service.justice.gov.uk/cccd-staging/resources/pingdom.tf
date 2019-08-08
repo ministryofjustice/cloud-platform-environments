14ba4bc1c955d9a75353dfc3fbe8b20e15e8a135
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-08-08 12:24:21 +0100

    Add pingdom/slack integrationid
    To output alerts in #laa-get-paid-alerts

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-staging/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-staging/resources/pingdom.tf
@@ -18,5 +18,5 @@ resource "pingdom_check" "claim-crown-court-defence-staging" {
   tags                     = "businessunit_${var.business-unit},application_${var.application},component_ping,isproduction_${var.is-production},environment_${var.environment-name},infrastructuresupport_${var.application}"
   probefilters             = "region:EU"
   publicreport             = "true"
-  integrationids           = []
+  integrationids           = [94703]
 }
