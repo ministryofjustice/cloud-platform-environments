f030e331ca7942f36b4bda2c2fcd3eac9eeceef1
steverendell <steve.rendell@syscon.net>
2019-08-08 16:06:10 +0100

    Create risk profiler database for cat tool - PR comment

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
@@ -19,23 +19,6 @@ resource "random_id" "risk_profiler_role_password" {
   byte_length = 32
 }
 
-resource "kubernetes_secret" "dps_rds" {
-  metadata {
-    name      = "dps-rds-instance-output"
-    namespace = "${var.namespace}"
-  }
-
-  data {
-    rds_instance_endpoint  = "${module.dps_rds.rds_instance_endpoint}"
-    database_name          = "${module.dps_rds.database_name}"
-    database_username      = "${module.dps_rds.database_username}"
-    database_password      = "${module.dps_rds.database_password}"
-    rds_instance_address   = "${module.dps_rds.rds_instance_address}"
-    url                    = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
-    risk_profiler_password = "${random_id.risk_profiler_role_password.b64}"
-  }
-}
-
 provider "postgresql" {
   host     = "${module.dps_rds.rds_instance_address}"
   port     = "${module.dps_rds.rds_instance_port}"
@@ -61,4 +44,22 @@ resource postgresql_grant "risk_profiler_tables" {
   schema      = "public"
   object_type = "table"
   privileges  = ["SELECT", "UPDATE", "INSERT", "CREATE"]
+}
+
+resource "kubernetes_secret" "dps_rds" {
+  metadata {
+    name      = "dps-rds-instance-output"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    rds_instance_endpoint  = "${module.dps_rds.rds_instance_endpoint}"
+    database_name          = "${module.dps_rds.database_name}"
+    database_username      = "${module.dps_rds.database_username}"
+    database_password      = "${module.dps_rds.database_password}"
+    rds_instance_address   = "${module.dps_rds.rds_instance_address}"
+    risk_profiler_name     = "${postgresql_database.risk_profiler.name}"
+    risk_profiler_username = "${postgresql_role.risk_profiler.name}"
+    risk_profiler_password = "${random_id.risk_profiler_role_password.b64}"
+  }
 }
