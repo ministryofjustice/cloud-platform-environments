3af049e17adc87e21e494e144b4d730be458502d
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-08-20 15:51:42 +0100

    Add cccd-production namespace
    Apply terraform templates, a variables.tf to reduce duplication in future resource definitions and limitrange
    tweaks.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-production/resources/variables.tf
@@ -0,0 +1,31 @@
+variable "application" {
+  default = "cccd"
+}
+
+variable "namespace" {
+  default = "cccd-production"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "legal-aid-agency"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "laa-get-paid"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "production"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "crowncourtdefence@digtal.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "true"
+}
