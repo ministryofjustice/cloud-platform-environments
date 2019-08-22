2143fa81fd0b4994deed2746b6057e0803ed9a62
Mark Berridge <mark.berridge@digital.justice.gov.uk>
2019-08-22 16:55:12 +0100

    Create ECR for tutorial

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/markberridge-dev/resources/ecr.tf
@@ -0,0 +1,31 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "markberridge-dev-ecr-credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  repo_name = "markberridge-dev-repo"
+  team_name = "markberridge-dev-team"
+
+  # aws_region = "eu-west-2"     # This input is deprecated from version 3.2 of this module
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "markberridge-dev-ecr-credentials" {
+  metadata {
+    name      = "markberridge-dev-team-ecr-credentials-output"
+    namespace = "markberridge-dev"
+  }
+
+  data {
+    access_key_id     = "${module.markberridge-dev-ecr-credentials.access_key_id}"
+    secret_access_key = "${module.markberridge-dev-ecr-credentials.secret_access_key}"
+    repo_arn          = "${module.markberridge-dev-ecr-credentials.repo_arn}"
+    repo_url          = "${module.markberridge-dev-ecr-credentials.repo_url}"
+  }
+}
