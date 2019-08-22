4369bc88936ba8df0fb8fe58ff6dafcc9fb07e4a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-08-22 09:04:51 +0100

    Modify cccd-production RDS terraform
    - the db config represents current config for TD cccd production.
    
    - IOPS I have taken default from template. TD cccd
      IOPS write and read seem to peak at 100 roughly but not
      sure how this correlates exactly to what we might need.
    
    - engine in TD cccd prod is currently 9.6 but could
      use 10.6 and have more testing to ensure the migration
      and db upgrade (combined) do not have an impact - more complexity.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-production/resources/rds.tf
@@ -16,23 +16,20 @@ variable "cluster_state_bucket" {}
  */
 
 module "cccd_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.5"
-  cluster_name           = "${var.cluster_name}"
-  cluster_state_bucket   = "${var.cluster_state_bucket}"
-  team_name              = "${var.team_name}"
-  business-unit          = "${var.business-unit}"
-  application            = "${var.application}"
-  is-production          = "${var.is-production}"
-  db_engine_version      = "9.6"
-  environment-name       = "${var.environment-name}"
-  infrastructure-support = "${var.infrastructure-support}"
-  force_ssl              = "true"
-
-  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
-  # Pick the one that defines the postgres version the best
-  rds_family = "postgres9.6"
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.5"
+  cluster_name                = "${var.cluster_name}"
+  cluster_state_bucket        = "${var.cluster_state_bucket}"
+  team_name                   = "${var.team_name}"
+  business-unit               = "${var.business-unit}"
+  application                 = "${var.application}"
+  is-production               = "${var.is-production}"
+  environment-name            = "${var.environment-name}"
+  infrastructure-support      = "${var.infrastructure-support}"
+  db_allocated_storage        = "50"
+  db_instance_class           = "db.t3.medium"
+  db_iops                     = "1000"
+  db_engine_version           = "9.6"
+  rds_family                  = "postgres9.6"
   allow_major_version_upgrade = "true"
 
   providers = {
@@ -48,16 +45,11 @@ resource "kubernetes_secret" "cccd_rds" {
   }
 
   data {
-    /*
-    * NOT NEEDED - APP USES URL ONLY
-    *
-    * rds_instance_endpoint = "${module.cccd_rds.rds_instance_endpoint}"
-    * database_name         = "${module.cccd_rds.database_name}"
-    * database_username     = "${module.cccd_rds.database_username}"
-    * database_password     = "${module.cccd_rds.database_password}"
-    * rds_instance_address  = "${module.cccd_rds.rds_instance_address}"
-    */
-
-    url = "postgres://${module.cccd_rds.database_username}:${module.cccd_rds.database_password}@${module.cccd_rds.rds_instance_endpoint}/${module.cccd_rds.database_name}"
+    rds_instance_endpoint = "${module.cccd_rds.rds_instance_endpoint}"
+    database_name         = "${module.cccd_rds.database_name}"
+    database_username     = "${module.cccd_rds.database_username}"
+    database_password     = "${module.cccd_rds.database_password}"
+    rds_instance_address  = "${module.cccd_rds.rds_instance_address}"
+    url                   = "postgres://${module.cccd_rds.database_username}:${module.cccd_rds.database_password}@${module.cccd_rds.rds_instance_endpoint}/${module.cccd_rds.database_name}"
   }
 }
