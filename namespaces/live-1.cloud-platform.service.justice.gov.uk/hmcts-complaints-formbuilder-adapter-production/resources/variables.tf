1aba98ad63c9c4a60258fa474db5e0039ad2e8b6
Emile Swarts <korpzilla@Emiles-MacBook-Air.local>
2019-09-04 13:57:03 +0100

    Declare missing variables for team name and environment

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-production/resources/variables.tf
@@ -0,0 +1,9 @@
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "form-builder"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "production"
+}
