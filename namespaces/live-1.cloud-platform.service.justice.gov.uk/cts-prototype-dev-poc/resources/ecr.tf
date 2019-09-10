9efbc23b44988720635999bd94e3a52ef0987434
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-09-10 12:25:02 +0100

    Correspondence Tool Staff - ECR Terraform Config file

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cts-prototype-dev-poc/resources/ecr.tf
@@ -0,0 +1,29 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "track_a_query_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  repo_name = "track-a-query-ecr"                                                             # Arbitrary module name does not need to reference any existing modules
+  team_name = "correspondence"                                                                # Github team name
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "track_a_query_ecr_credentials" {
+  metadata {
+    name      = "track-a-query-ecr-credentials-output"
+    namespace = "cts-prototype-dev-poc"
+  }
+
+  data {
+    access_key_id     = "${module.track_a_query_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.track_a_query_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.track_a_query_ecr_credentials.repo_arn}"
+    repo_url          = "${module.track_a_query_ecr_credentials.repo_url}"
+  }
+}
