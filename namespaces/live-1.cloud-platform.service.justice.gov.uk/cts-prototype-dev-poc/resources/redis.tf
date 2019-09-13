d143a62165622753b7513838647c870031cccc38
mseedat-moj <47113046+mseedat-moj@users.noreply.github.com>
2019-09-13 10:45:39 +0100

    Track A Query - add elasticache (#1212)
    * Add redis terraform config
    
    * Copy example with provider and member cluster
    
    * Remove unnecessary settings
    
    * Run formatting check

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cts-prototype-dev-poc/resources/redis.tf
@@ -0,0 +1,40 @@
+################################################################################
+# Track a Query (Correspondence Tool Staff)
+# Application Elasticache for ReDiS (for sidekiq background job processing)
+#################################################################################
+
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ */
+
+variable "cluster_name" {}
+variable "cluster_state_bucket" {}
+
+module "track_a_query_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.1"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "correspondence"
+  business-unit          = "Central Digital"
+  application            = "track-a-query"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "mohammed.seedat@digtal.justice.gov.uk"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "track_a_query_elasticache_redis" {
+  metadata {
+    name      = "track-a-query-elasticache-redis-output"
+    namespace = "cts-prototype-dev-poc"
+  }
+
+  data {
+    primary_endpoint_address = "${module.track_a_query_elasticache_redis.primary_endpoint_address}"
+    auth_token               = "${module.track_a_query_elasticache_redis.auth_token}"
+  }
+}
