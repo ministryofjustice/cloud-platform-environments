8a9dcebdae18ae629b4fc2e6569a2ea76fe732b0
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-09-13 11:23:46 +0100

    Add elasticache to cccd-staging
    TD elasticache uses a old node size of cache.m1.small
    in production which most closely maps to cache.t2.small
    for current standard nodes.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-staging/resources/elasticache.tf
@@ -0,0 +1,35 @@
+################################################################################
+# CCCD Application Elasticache for ReDiS
+# for sidekiq background job processing and internal API cache
+################################################################################
+
+module "cccd_elasticache_redis" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.1"
+
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  application            = "${var.application}"
+  environment-name       = "${var.environment-name}"
+  is-production          = "${var.is-production}"
+  infrastructure-support = "${var.infrastructure-support}"
+  team_name              = "${var.team_name}"
+
+  engine_version        = "4.0.10"
+  parameter_group_name  = "default.redis4.0"
+  number_cache_clusters = "3"
+  node_type             = "cache.t2.small"
+}
+
+resource "kubernetes_secret" "cccd_elasticache_redis" {
+  metadata {
+    name      = "cccd-elasticache-redis"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    primary_endpoint_address = "${module.cccd_elasticache_redis.primary_endpoint_address}"
+    auth_token               = "${module.cccd_elasticache_redis.auth_token}"
+    member_clusters          = "${jsonencode(module.cccd_elasticache_redis.member_clusters)}"
+    url                      = "rediss://dummyuser:${module.cccd_elasticache_redis.auth_token}@${module.cccd_elasticache_redis.primary_endpoint_address}:6379"
+  }
+}
