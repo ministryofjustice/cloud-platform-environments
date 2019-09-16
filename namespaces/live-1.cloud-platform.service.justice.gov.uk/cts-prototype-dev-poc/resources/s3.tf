0a57c09cecfb6561cde6055a7022d3f6e42a2e56
mseedat-moj <47113046+mseedat-moj@users.noreply.github.com>
2019-09-16 10:13:44 +0100

    Add simple S3 bucket to terraform config (#1220)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cts-prototype-dev-poc/resources/s3.tf
@@ -0,0 +1,33 @@
+################################################################################
+# Track a Query (Correspondence Tool Staff)
+# S3 Bucket for file uploads
+#################################################################################
+
+module "track_a_query_s3" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=3.3"
+
+  team_name              = "correspondence"
+  business-unit          = "Central Digital"
+  application            = "track-a-query"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "mohammed.seedat@digtal.justice.gov.uk"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "track_a_query_s3" {
+  metadata {
+    name      = "track-a-query-s3-output"
+    namespace = "cts-prototype-dev-poc"
+  }
+
+  data {
+    access_key_id     = "${module.track_a_query_s3.access_key_id}"
+    secret_access_key = "${module.track_a_query_s3.secret_access_key}"
+    bucket_arn        = "${module.track_a_query_s3.bucket_arn}"
+    bucket_name       = "${module.track_a_query_s3.bucket_name}"
+  }
+}
