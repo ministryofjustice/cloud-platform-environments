6c02c353d17f80391b699376faed08b65862503b
David Salgado <david.salgado@digital.justice.gov.uk>
2019-09-20 14:56:50 +0100

    Reverse change to check-my-diary-prod namespace
    * Replace the route53.tf file
    * Remove the ingress.yaml file
    
    I checked with John Hackett, and the original
    change should not have been merged.
    
    They have the ingress.yaml file in their app.
    repo, and they don't want to delete the route53
    zone.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/check-my-diary-prod/resources/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_zone" "check-my-diary-prod" {
+  name = "checkmydiary.service.justice.gov.uk"
+
+  tags {
+    business-unit          = "Check My Diary"
+    application            = "Check My Diary"
+    is-production          = "true"
+    environment-name       = "prod"
+    owner                  = "Lauren Darby"
+    infrastructure-support = "digital-studio-operations-team@digital.justice.gov.uk"
+  }
+}
+
+resource "kubernetes_secret" "check-my-diary-prod_sec" {
+  metadata {
+    name      = "check-my-diary-prod-zone-output"
+    namespace = "check-my-diary-prod"
+  }
+
+  data {
+    zone_id = "${aws_route53_zone.check-my-diary-prod.zone_id}"
+  }
+}
