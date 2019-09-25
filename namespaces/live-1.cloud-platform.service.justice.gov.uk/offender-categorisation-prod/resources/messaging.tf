58370d7a18382cae4da9b83f558f90867decd4b4
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-09-25 09:34:25 +0100

    apply dead letter config to preprod and prod envs

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-categorisation-prod/resources/messaging.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-categorisation-prod/resources/messaging.tf
@@ -69,3 +69,18 @@ resource "kubernetes_secret" "risk_profiler_change" {
     sqs_rpc_dlq_name  = "${module.risk_profiler_change_dead_letter_queue.sqs_name}"
   }
 }
+
+resource "kubernetes_secret" "risk_profiler_change_dead_letter_queue" {
+  metadata {
+    name      = "rp-sqs-dl-instance-output"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    access_key_id     = "${module.risk_profiler_change_dead_letter_queue.access_key_id}"
+    secret_access_key = "${module.risk_profiler_change_dead_letter_queue.secret_access_key}"
+    sqs_rpc_url       = "${module.risk_profiler_change_dead_letter_queue.sqs_id}"
+    sqs_rpc_arn       = "${module.risk_profiler_change_dead_letter_queue.sqs_arn}"
+    sqs_rpc_name      = "${module.risk_profiler_change_dead_letter_queue.sqs_name}"
+  }
+}
