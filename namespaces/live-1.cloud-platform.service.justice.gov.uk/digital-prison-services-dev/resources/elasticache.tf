d66f143185873247faf4636d2fe75f5e98e6b2f7
JamieDraperUK <draper65@googlemail.com>
2019-10-09 15:46:48 +0100

    Provision redis

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-dev/resources/elasticache.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {}
+
+variable "cluster_state_bucket" {}
+
+module "dps_redis" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.1"
+  cluster_name = "${var.cluster_name}"
+  cluster_state_bucket = "${var.cluster_state_bucket}"
+  application = "${var.application}"
+  environment-name = "${var.environment-name}"
+  is-production = "${var.is-production}"
+  infrastructure-support = "${var.infrastructure-support}"
+  team_name = "${var.team_name}"
+  number_cache_clusters = "${var.number_cache_clusters}"
+  node_type = "${var.node-type}"
+}
+
+resource "kubernetes_secret" "dps_redis" {
+  metadata {
+    name      = "dps_redis"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    primary_endpoint_address = "${module.dps_redis.primary_endpoint_address}"
+    member_clusters          = "${jsonencode(module.dps_redis.member_clusters)}"
+    auth_token               = "${module.dps_redis.auth_token}"
+  }
+}
---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-dev/resources/variables.tf
@@ -0,0 +1,44 @@
+variable "domain" {
+  default = "digital-dev.prison.service.justice.gov.uk"
+}
+
+variable "application" {
+  default = "digital-prison-services-dev"
+}
+
+variable "namespace" {
+  default = "digital-prison-services-dev"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Digital Prison Services/New Nomis"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "matt.whittaker@digtal.justice.gov.uk"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "node-type" {
+  default = "cache.t2.small"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
