16ee8cf7c9110dd9cb0d268d24912988a2febf82
Jamie Draper <draper65@googlemail.com>
2019-10-09 16:15:10 +0100

    Provision redis (#1326)
    * Provision redis
    
    * Fix formatting
    
    * Remove unused domain variable name
    
    * Fix linting issue
    
    * Change support

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-dev/resources/variables.tf
@@ -0,0 +1,39 @@
+variable "application" {
+  default = "digital-prison-services-dev"
+}
+
+variable "namespace" {
+  default = "digital-prison-services-dev"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Digital Prison Services/New Nomis"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "node-type" {
+  default = "cache.t2.small"
+}
+
+variable "is-production" {
+  default = "false"
+}
