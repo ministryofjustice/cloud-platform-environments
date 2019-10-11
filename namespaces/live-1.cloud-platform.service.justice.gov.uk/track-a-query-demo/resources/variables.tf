ef3280b33d5a912b6b4d17f1585a143a6111b300
mseedat-moj <47113046+mseedat-moj@users.noreply.github.com>
2019-10-11 12:00:22 +0100

    Track a Query - Add new environments (#1339)
    * Add new environments
    
    * Remove extra spaces (typo)
    
    * Update all environments so only Production has is-production: true

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-demo/resources/variables.tf
@@ -0,0 +1,7 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ */
+
+variable "cluster_name" {}
+variable "cluster_state_bucket" {}
