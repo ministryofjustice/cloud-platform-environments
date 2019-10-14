4b16480e8c0fd99ca9264e448c8761c6a22c29d3
JamieDraperUK <draper65@googlemail.com>
2019-10-14 07:33:57 +0100

    Rename redis variable names

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-dev/resources/elasticache.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-dev/resources/elasticache.tf
@@ -26,8 +26,8 @@ resource "kubernetes_secret" "dps_redis" {
   }
 
   data {
-    primary_endpoint_address = "${module.dps_redis.primary_endpoint_address}"
-    member_clusters          = "${jsonencode(module.dps_redis.member_clusters)}"
-    auth_token               = "${module.dps_redis.auth_token}"
+    REDIS_HOST      = "${module.dps_redis.primary_endpoint_address}"
+    REDIS_PASSWORD  = "${module.dps_redis.auth_token}"
+    member_clusters = "${jsonencode(module.dps_redis.member_clusters)}"
   }
 }
