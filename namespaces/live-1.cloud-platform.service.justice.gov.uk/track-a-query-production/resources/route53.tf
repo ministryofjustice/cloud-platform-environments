337117212197ae9c3e8d6853b55080d27d30b76c
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-10-15 13:03:10 +0100

    Add new Route53 terraform module settings

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/route53.tf
@@ -0,0 +1,28 @@
+################################################################################
+# Track a Query (Correspondence Tool Staff)
+# Route53 domain zone settings
+#################################################################################
+
+resource "aws_route53_zone" "track_a_query_route53_zone" {
+  name = "track-a-query-production.apps.live-1.cloud-platform.service.justice.gov.uk"
+
+  tags {
+    business-unit          = "Central Digital"
+    application            = "track-a-query"
+    is-production          = "true"
+    environment-name       = "production"
+    owner                  = "staff-services"
+    infrastructure-support = "correspondence-support@digital.justice.gov.uk"
+  }
+}
+
+resource "kubernetes_secret" "track_a_query_route53_zone_sec" {
+  metadata {
+    name      = "track-a-query-route53-zone-output"
+    namespace = "track-a-query-production"
+  }
+
+  data {
+    zone_id   = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
+  }
+}
