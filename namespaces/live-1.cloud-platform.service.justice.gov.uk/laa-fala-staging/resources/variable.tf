2f04b37231d9f107d10831f1ef130d67a277befe
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-10-21 13:36:42 +0100

    Add ecr resource

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/variable.tf
@@ -0,0 +1,31 @@
+variable "namespace" {
+  default = "laa-fala-staging"
+}
+
+variable "business-unit" {
+  default = "LAA"
+}
+
+variable "team_name" {
+  default = "laa-get-access"
+}
+
+variable "application" {
+  default = "LAA Legal Adviser API"
+}
+
+variable "repo_name" {
+  default = "laa-legal-adviser-api"
+}
+
+variable "email" {
+  default = "civil-legal-advice@digital.justice.gov.uk"
+}
+
+variable "environment-name" {
+  default = "staging"
+}
+
+variable "is-production" {
+  default = "false"
+}
