db5174b9574fd85f13cbbdf1106b399917e89f22
Matt Whittaker <matt.whittaker@digital.justice.gov.uk>
2019-10-31 15:01:14 +0000

    list to string fix

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-prod/resources/route53.tf
@@ -19,6 +19,6 @@ resource "kubernetes_secret" "route53_zone_sec" {
 
   data {
     zone_id      = "${aws_route53_zone.route53_zone.zone_id}"
-    name_servers = "${aws_route53_zone.route53_zone.name_servers}"
+    name_servers = "${join(", ", aws_route53_zone.route53_zone.name_servers)}"
   }
 }
