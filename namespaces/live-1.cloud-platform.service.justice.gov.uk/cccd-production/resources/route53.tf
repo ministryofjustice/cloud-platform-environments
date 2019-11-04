152fb020b96c3115fc49a3f21bb015f506bc146f
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2019-11-04 16:51:31 +0000

    Add cccd production DNS records (#1478)
    Add cccd production DNS record to route to TD load balancer
    
    In preparation for GDS delegating control to our
    hosted zone.
    
    Also, add name server outputs to secrets, for information that
    can be used with `dig`.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-production/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-production/resources/route53.tf
@@ -18,6 +18,20 @@ resource "kubernetes_secret" "cccd_route53_zone_sec" {
   }
 
   data {
-    zone_id = "${aws_route53_zone.cccd_route53_zone.zone_id}"
+    zone_id      = "${aws_route53_zone.cccd_route53_zone.zone_id}"
+    name_servers = "${join("\n", aws_route53_zone.cccd_route53_zone.name_servers)}"
+  }
+}
+
+resource "aws_route53_record" "cccd_route53_A_record_1" {
+  name    = "claim-crown-court-defence.service.gov.uk."
+  zone_id = "${aws_route53_zone.cccd_route53_zone.zone_id}"
+  type    = "A"
+
+  alias {
+    name    = "dualstack.advocated-elbgamma-17de5vdfln9zg-1726434105.eu-west-1.elb.amazonaws.com."
+    zone_id = "Z32O12XQLNTSW2"
+
+    evaluate_target_health = false
   }
 }
