722d791466eb4cb7d820ebcb68818a6f69192690
Vijay Veeranki <46675900+vijay-veeranki-moj@users.noreply.github.com>
2019-11-05 14:58:20 +0000

    Commented out court_case_dynamodb (#1485)
    * Commented out court_case_dynamodb
    
    As this is causing pipeline failure.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/dynamodb.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/dynamodb.tf
@@ -12,30 +12,40 @@ provider "aws" {
  * releases page of this repository.
  *
  */
-module "court_case_dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=2.0"
 
-  team_name              = "${var.team_name}"
-  application            = "Court Case Service"
-  business-unit          = "${var.business-unit}"
-  environment-name       = "${var.environment-name}"
-  infrastructure-support = "${var.infrastructure-support}"
-  is-production          = "false"
 
-  hash_key  = "pk"
-  range_key = "sk"
-}
+/* commented out below module as it is failing pipeline*/
 
-resource "kubernetes_secret" "court_case_dynamodb" {
-  metadata {
-    name      = "court-case-dynamodb-output"
-    namespace = "${var.namespace}"
-  }
 
-  data {
-    table_name        = "${module.court_case_dynamodb.table_name}"
-    table_arn         = "${module.court_case_dynamodb.table_arn}"
-    access_key_id     = "${module.court_case_dynamodb.access_key_id}"
-    secret_access_key = "${module.court_case_dynamodb.secret_access_key}"
-  }
-}
+# module "court_case_dynamodb" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=2.0"
+
+
+#   team_name              = "${var.team_name}"
+#   application            = "Court Case Service"
+#   business-unit          = "${var.business-unit}"
+#   environment-name       = "${var.environment-name}"
+#   infrastructure-support = "${var.infrastructure-support}"
+#   is-production          = "false"
+
+
+#   hash_key  = "pk"
+#   range_key = "sk"
+# }
+
+
+# resource "kubernetes_secret" "court_case_dynamodb" {
+#   metadata {
+#     name      = "court-case-dynamodb-output"
+#     namespace = "${var.namespace}"
+#   }
+
+
+#   data {
+#     table_name        = "${module.court_case_dynamodb.table_name}"
+#     table_arn         = "${module.court_case_dynamodb.table_arn}"
+#     access_key_id     = "${module.court_case_dynamodb.access_key_id}"
+#     secret_access_key = "${module.court_case_dynamodb.secret_access_key}"
+#   }
+# }
+
