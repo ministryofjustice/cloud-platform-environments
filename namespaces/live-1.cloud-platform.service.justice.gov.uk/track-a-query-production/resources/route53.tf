870b8ea66e4d4119bc0cd14827f8e7917b064e10
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-06 10:53:12 +0000

    Remove QA a record

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/route53.tf
@@ -79,10 +79,3 @@ resource "aws_route53_record" "track_a_query_route53_A_record_demo" {
     evaluate_target_health = true
   }
 }
-
-# QA is a brand new environment that does not exist in Template Deploy
-resource "aws_route53_record" "track_a_query_route53_A_record_qa" {
-  name    = "qa.track-a-query.service.gov.uk."
-  zone_id = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
-  type    = "A"
-}
