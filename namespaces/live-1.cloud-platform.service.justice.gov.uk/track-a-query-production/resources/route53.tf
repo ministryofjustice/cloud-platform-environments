865b753b8ad6b4b89d4f1598a0a28018f83e1067
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-06 10:33:27 +0000

    Add existing development, staging, demo DNS entries and new QA A record

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/route53.tf
@@ -40,3 +40,49 @@ resource "aws_route53_record" "track_a_query_route53_A_record_production" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "track_a_query_route53_A_record_development" {
+  name    = "development.track-a-query.service.gov.uk."
+  zone_id = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
+  type    = "A"
+
+  alias {
+    name    = "haproxy-track-a-query-1357848984.eu-west-1.elb.amazonaws.com."
+    zone_id = "Z32O12XQLNTSW2"
+
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "track_a_query_route53_A_record_staging" {
+  name    = "staging.track-a-query.service.gov.uk."
+  zone_id = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
+  type    = "A"
+
+  alias {
+    name    = "haproxy-track-a-query-1357848984.eu-west-1.elb.amazonaws.com."
+    zone_id = "Z32O12XQLNTSW2"
+
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "track_a_query_route53_A_record_demo" {
+  name    = "demo.track-a-query.service.gov.uk."
+  zone_id = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
+  type    = "A"
+
+  alias {
+    name    = "haproxy-track-a-query-1357848984.eu-west-1.elb.amazonaws.com."
+    zone_id = "Z32O12XQLNTSW2"
+
+    evaluate_target_health = true
+  }
+}
+
+# QA is a brand new environment that does not exist in Template Deploy
+resource "aws_route53_record" "track_a_query_route53_A_record_qa" {
+  name    = "qa.track-a-query.service.gov.uk."
+  zone_id = "${aws_route53_zone.track_a_query_route53_zone.zone_id}"
+  type    = "A"
+}
