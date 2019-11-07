8845fd7612d57c0afc715b9e8f9a183ae16e85db
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-11-07 09:13:08 +0000

    formbuilder upgrade test rds module and tweaks
    also apply rds changes immediately

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/publisher.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/publisher.tf
@@ -1,9 +1,5 @@
-# auto-generated from fb-cloud-platforms-environments
-##################################################
-# Publisher RDS
-
 module "publisher-rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.7"
 
   cluster_name               = "${var.cluster_name}"
   cluster_state_bucket       = "${var.cluster_state_bucket}"
@@ -14,6 +10,7 @@ module "publisher-rds-instance" {
   infrastructure-support     = "${var.infrastructure-support}"
   team_name                  = "${var.team_name}"
   db_engine_version          = "10.9"
+  apply_method               = "immediate"
 
   providers = {
     aws = "aws.london"
