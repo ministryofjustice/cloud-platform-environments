a39bd7dcd9ec43def68801c00a0b39da0e6f4d91
said-moj <said.shire@digital.justice.gov.uk>
2019-11-11 11:51:21 +0000

    Change tags to lower case

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-staging/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-staging/resources/pingdom.tf
@@ -11,7 +11,7 @@ resource "pingdom_check" "laa-legal-adviser-api-search" {
   url                      = "/ping.json"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_${var.business-unit},application_${var.application},component_ping,isproduction_${var.is-production},environment_${var.environment-name},infrastructuresupport_${var.application}"
+  tags                     = "businessunit_${lower(var.business-unit)},application_${lower(var.application)},component_ping,isproduction_${var.is-production},environment_${lower(var.environment-name)},infrastructuresupport_${lower(var.application)}"
   probefilters             = "region:EU"
   integrationids           = [87631]
 }
