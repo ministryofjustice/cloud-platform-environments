825bfd57e6831a928350ac65559c4337149a7413
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-11-11 14:58:19 +0000

    Add resources for laa-fala-production

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-fala-production/resources/variable.tf
@@ -0,0 +1,31 @@
+variable "namespace" {
+  default = "laa-fala-production"
+}
+
+variable "business-unit" {
+  default = "LAA"
+}
+
+variable "team_name" {
+  default = "laa-get-access"
+}
+
+variable "application" {
+  default = "Find A Legal Advisor"
+}
+
+variable "repo_name" {
+  default = "fala"
+}
+
+variable "email" {
+  default = "civil-legal-advice@digital.justice.gov.uk"
+}
+
+variable "environment-name" {
+  default = "production"
+}
+
+variable "is-production" {
+  default = "true"
+}
