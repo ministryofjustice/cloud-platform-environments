67293e86639df0efdc64af0140038b02f7eb6cc0
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-12 17:13:07 +0000

    Upgrade module to 3.4

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/s3.tf
@@ -4,7 +4,7 @@
 #################################################################################
 
 module "track_a_query_s3" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=3.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=3.4"
 
   team_name              = "correspondence"
   business-unit          = "Central Digital"
@@ -17,7 +17,7 @@ module "track_a_query_s3" {
     {
       allowed_headers = ["*"]
       allowed_methods = ["GET", "POST", "PUT"]
-      allowed_origins = ["https://track-a-query.service.justice.gov.uk", "https://track-a-query-production.apps.live-1.cloud-platform.service.justice.gov.uk"]
+      allowed_origins = ["https://track-a-query.service.justice.gov.uk", "https://track-a-query.apps.live-1.cloud-platform.service.justice.gov.uk"]
       expose_headers  = ["ETag"]
       max_age_seconds = 3000
     },
