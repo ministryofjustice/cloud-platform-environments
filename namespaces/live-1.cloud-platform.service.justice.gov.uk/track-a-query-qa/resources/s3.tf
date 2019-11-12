92f9924a509ff68ede2f4034db363fd2ceea7db7
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-12 15:00:50 +0000

    Add qa S3 Bucket CORS

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/s3.tf
@@ -13,6 +13,22 @@ module "track_a_query_s3" {
   environment-name       = "qa"
   infrastructure-support = "correspondence-support@digital.justice.gov.uk"
 
+  cors_rule = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET", "POST", "PUT"]
+      allowed_origins = ["https://qa.track-a-query.service.justice.gov.uk", "https://track-a-query-qa.apps.live-1.cloud-platform.service.justice.gov.uk"]
+      expose_headers  = ["ETag"]
+      max_age_seconds = 3000
+    },
+    {
+      allowed_headers = ["Authorization"]
+      allowed_methods = ["GET"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+    },
+  ]
+
   providers = {
     aws = "aws.london"
   }
