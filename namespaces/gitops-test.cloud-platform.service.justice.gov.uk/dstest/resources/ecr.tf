2cadcae421d3229b755169c44c88c6eabde08728
David Salgado <david.salgado@digital.justice.gov.uk>
2019-11-13 15:10:10 +0000

    Create a dstest namespace, to test ruby apply

---
+++ namespaces/gitops-test.cloud-platform.service.justice.gov.uk/dstest/resources/ecr.tf
@@ -0,0 +1,23 @@
+module "becca_test_app_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  repo_name = "becca-test-app"
+  team_name = "webops"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "becca_test_app_ecr_credentials" {
+  metadata {
+    name      = "becca-test-app-ecr-credentials-output"
+    namespace = "dstest"
+  }
+
+  data {
+    access_key_id     = "${module.becca_test_app_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.becca_test_app_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.becca_test_app_ecr_credentials.repo_arn}"
+    repo_url          = "${module.becca_test_app_ecr_credentials.repo_url}"
+  }
+}
