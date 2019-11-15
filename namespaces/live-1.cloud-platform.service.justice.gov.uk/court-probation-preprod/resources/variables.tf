2794a7e39204c1e8f4e32ea5fa2e2703d91fbed5
Mark Berridge <mark.berridge@digital.justice.gov.uk>
2019-11-15 14:26:37 +0000

    Added DynamoDB for court-probation preprod and prod

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/variables.tf
@@ -0,0 +1,23 @@
+variable "namespace" {
+  default = "court-probation-preprod"
+}
+
+variable "business-unit" {
+  default = "HMPPS"
+}
+
+variable "team_name" {
+  default = "Probation in Court Team"
+}
+
+variable "environment-name" {
+  default = "preprod"
+}
+
+variable "infrastructure-support" {
+  default = "Probation in Court Team: probation-in-court-team@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
