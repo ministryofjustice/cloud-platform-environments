0bf92aceb4f9884af75c975a44e81b6a798b0469
David Salgado <david.salgado@digital.justice.gov.uk>
2019-11-18 14:21:37 +0000

    Please ignore this PR.
    Making a terraform change to test the plan pipeline.

---
+++ namespaces/gitops-test.cloud-platform.service.justice.gov.uk/dstest-gitops/resources/ecr.tf
@@ -0,0 +1,23 @@
+module "dstest_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  repo_name = "ds-test-app"
+  team_name = "webops"
+
+  providers = {
+    aws = "aws.london"
+  }
+}
+
+resource "kubernetes_secret" "dstest_ecr_credentials" {
+  metadata {
+    name      = "dstest-ecr-credentials-output"
+    namespace = "dstest-dev"
+  }
+
+  data {
+    access_key_id     = "${module.dstest_ecr_credentials.access_key_id}"
+    secret_access_key = "${module.dstest_ecr_credentials.secret_access_key}"
+    repo_arn          = "${module.dstest_ecr_credentials.repo_arn}"
+    repo_url          = "${module.dstest_ecr_credentials.repo_url}"
+  }
+}
