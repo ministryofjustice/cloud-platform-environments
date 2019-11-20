2e84bd783d1bc9c207a0b5dc576f4f59b8b27d93
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-11-20 10:08:40 +0000

    formbuilder upgrade prod terraform modules

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-production/resources/service_token_cache.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-production/resources/service_token_cache.tf
@@ -1,8 +1,5 @@
-# auto-generated from fb-cloud-platforms-environments
-########################################################
-# Service Token Cache Elasticache Redis (for resque + job logging)
 module "service-token-cache-elasticache" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.2"
 
   cluster_name         = "${var.cluster_name}"
   cluster_state_bucket = "${var.cluster_state_bucket}"
