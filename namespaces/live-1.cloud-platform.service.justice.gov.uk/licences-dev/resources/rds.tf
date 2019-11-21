1656bbf0fef78276a22dfcd9980005edeb4fd7c9
bell-pa <peter.bell@digital.justice.gov.uk>
2019-11-21 09:28:10 +0000

    DCS-251 Create skeleton probation-teams-service (dev): Generate a random password for the probation_teams database role.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-dev/resources/rds.tf
@@ -15,6 +15,10 @@ module "dps_rds" {
   }
 }
 
+resource "random_id" "probation_teams_password" {
+  byte_length = 32
+}
+
 resource "kubernetes_secret" "dps_rds" {
   metadata {
     name      = "dps-rds-instance-output"
@@ -22,10 +26,11 @@ resource "kubernetes_secret" "dps_rds" {
   }
 
   data {
-    rds_instance_endpoint = "${module.dps_rds.rds_instance_endpoint}"
-    database_name         = "${module.dps_rds.database_name}"
-    database_username     = "${module.dps_rds.database_username}"
-    database_password     = "${module.dps_rds.database_password}"
-    rds_instance_address  = "${module.dps_rds.rds_instance_address}"
+    rds_instance_endpoint    = "${module.dps_rds.rds_instance_endpoint}"
+    database_name            = "${module.dps_rds.database_name}"
+    database_username        = "${module.dps_rds.database_username}"
+    database_password        = "${module.dps_rds.database_password}"
+    rds_instance_address     = "${module.dps_rds.rds_instance_address}"
+    probation_teams_password = "${random_id.probation_teams_password.b64}"
   }
 }
