034528370bb3b76b68657698db6083c7110ab8d4
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-25 15:21:50 +0000

    Update RDS to latest v4.8 from v4.4
    This is for all the non-production namespaces
    
    This is pre-requisite to get env repo upgraded to tf0.12

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/whereabouts-api-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/whereabouts-api-preprod/resources/rds.tf
@@ -3,7 +3,7 @@ variable "cluster_name" {}
 variable "cluster_state_bucket" {}
 
 module "dps_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.4"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=4.8"
   cluster_name           = "${var.cluster_name}"
   cluster_state_bucket   = "${var.cluster_state_bucket}"
   team_name              = "${var.team_name}"
