7b50aa06b38a43dce1a64238cd896dfb61eb230d
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2019-11-25 16:26:53 +0000

    Create namespace for tf12 module testing

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cp-terraform-module-testing/resources/sqs.tf
@@ -0,0 +1,35 @@
+module "example_sqs" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=TF12-DO_NOT-USE"
+
+  environment-name       = "test"
+  team_name              = "cp"
+  infrastructure-support = "example-team@digtal.justice.gov.uk"
+  application            = "cptf12-app"
+  sqs_name               = "cptf12sqsname"
+
+  # Set encrypt_sqs_kms = "true", to enable SSE for SQS using KMS key.
+  encrypt_sqs_kms = "false"
+
+  # existing_user_name     = "${module.another_sqs_instance.user_name}"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "example_sqs" {
+  metadata {
+    name      = "cptf12-sqs"
+    namespace = "cp-terraform-module-testing"
+  }
+
+  data = {
+    access_key_id     = module.example_sqs.access_key_id
+    secret_access_key = module.example_sqs.secret_access_key
+    # the above will not be set if existing_user_name is defined
+    sqs_id   = module.example_sqs.sqs_id
+    sqs_arn  = module.example_sqs.sqs_arn
+    sqs_name = module.example_sqs.sqs_name
+  }
+}
+
