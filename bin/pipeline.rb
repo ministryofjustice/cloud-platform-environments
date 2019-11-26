95fedcbd92e5d3a76f0227126929af6a7a6879ae
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-26 14:28:59 +0000

    Add silent argument to the execute method

--- bin/pipeline.rb
+++ bin/pipeline.rb
@@ -175,8 +175,8 @@ def apply_terraform(cluster, namespace, dir)
   Terraform.new(cluster: cluster, namespace: namespace, dir: dir).apply
 end
 
-def execute(cmd, can_fail: false)
-  log("blue", "executing: #{cmd}")
+def execute(cmd, can_fail: false, silent: false)
+  log("blue", "executing: #{cmd}") unless silent
   stdout, stderr, status = Open3.capture3(cmd)
 
   unless can_fail || status.success?
@@ -185,7 +185,7 @@ def execute(cmd, can_fail: false)
     raise
   end
 
-  puts stdout
+  puts stdout unless silent
 
   [stdout, stderr, status]
 end
