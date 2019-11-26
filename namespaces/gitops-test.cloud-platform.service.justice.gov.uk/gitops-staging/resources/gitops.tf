94db129059163a4f19d2ed905bd6a2eb6bcc6591
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-26 10:32:28 +0000

    Add gitops-staging environment and remove the gitops-dev

--- namespaces/gitops-test.cloud-platform.service.justice.gov.uk/gitops-staging/resources/gitops.tf
+++ namespaces/gitops-test.cloud-platform.service.justice.gov.uk/gitops-staging/resources/gitops.tf
@@ -2,7 +2,7 @@ module "concourse-gitops" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-gitops"
   source_code_url               = "https://github.com/ministryofjustice/cloud-platform-helloworld-ruby-app"
   github_team                   = "webops"
-  namespace                     = "gitops-dev"
+  namespace                     = "gitops-staging"
   branch                        = "master"
   concourse_basic_auth_username = "${var.concourse_basic_auth_username}"
   concourse_url                 = "${var.concourse_url}"
