eaa7932221c368bd1b8bb4f68d0ca168c1db54ac
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-11-26 14:04:52 +0000

    Add dns zone and cert for new fala staging custom domain

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/route53.tf
@@ -0,0 +1,22 @@
+resource "aws_route53_zone" "route53_zone" {
+  name = "staging.find-legal-advice.justice.gov.uk"
+
+  tags {
+    business-unit    = "${var.business-unit}"
+    application      = "${var.application}"
+    is-production    = "${var.is-production}"
+    environment-name = "${var.environment-name}"
+    owner            = "${var.team_name}"
+  }
+}
+
+resource "kubernetes_secret" "route53_zone_sec" {
+  metadata {
+    name      = "route53-zone-output"
+    namespace = "${var.namespace}"
+  }
+
+  data {
+    zone_id = "${aws_route53_zone.route53_zone.zone_id}"
+  }
+}
