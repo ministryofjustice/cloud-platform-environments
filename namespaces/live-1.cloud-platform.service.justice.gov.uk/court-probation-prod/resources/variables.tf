d08db82f1e2a4b2d562cd09fcc5fbd0be29177ea
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2019-11-27 13:49:25 +0000

    Added rds.tf to add RDS instance and updated variables

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/variables.tf
@@ -1,23 +1,35 @@
+variable "application" {
+  default = "court-case-service"
+}
+
 variable "namespace" {
   default = "court-probation-prod"
 }
 
 variable "business-unit" {
-  default = "HMPPS"
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
 }
 
 variable "team_name" {
-  default = "Probation in Court Team"
+  description = "The name of your development team"
+  default     = "probation-services"
 }
 
 variable "environment-name" {
-  default = "production"
+  description = "The type of environment you're deploying to."
+  default     = "prod"
 }
 
 variable "infrastructure-support" {
-  default = "Probation in Court Team: probation-in-court-team@digital.justice.gov.uk"
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "probation-in-court-team@digital.justice.gov.uk"
 }
 
 variable "is-production" {
   default = "true"
+}
+
+variable "rds-family" {
+  default = "postgres10"
 }
