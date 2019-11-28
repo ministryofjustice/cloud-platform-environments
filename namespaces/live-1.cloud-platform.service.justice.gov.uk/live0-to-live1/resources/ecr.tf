e5abb572061d5e7ebd3514aaf96761b1feaa2559
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-28 09:36:23 +0000

    Update live0-to-live1 namespace
    Run tf0.12 upgrade and update modules to use the tf0.12 version

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/live0-to-live1/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/live0-to-live1/resources/ecr.tf
@@ -1,10 +1,10 @@
 module "live0_to_live1_ecr_cred" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.0"
   repo_name = "live0-to-live1"
   team_name = "webops-cp"
 
   providers = {
-    aws = "aws.london"
+    aws = aws.london
   }
 }
 
@@ -14,10 +14,11 @@ resource "kubernetes_secret" "live0_to_live1_ecr_cred" {
     namespace = "live0-to-live1"
   }
 
-  data {
-    access_key_id     = "${module.live0_to_live1_ecr_cred.access_key_id}"
-    secret_access_key = "${module.live0_to_live1_ecr_cred.secret_access_key}"
-    repo_arn          = "${module.live0_to_live1_ecr_cred.repo_arn}"
-    repo_url          = "${module.live0_to_live1_ecr_cred.repo_url}"
+  data = {
+    access_key_id     = module.live0_to_live1_ecr_cred.access_key_id
+    secret_access_key = module.live0_to_live1_ecr_cred.secret_access_key
+    repo_arn          = module.live0_to_live1_ecr_cred.repo_arn
+    repo_url          = module.live0_to_live1_ecr_cred.repo_url
   }
 }
+
