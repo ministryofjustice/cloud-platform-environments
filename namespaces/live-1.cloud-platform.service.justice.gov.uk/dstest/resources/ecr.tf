1cb33fe919688ec8e7e0c2ad7fb90e736bfe5cfd
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-28 13:45:17 +0000

    Update namespace dstest
    Terraform 0.12 upgrade and update modules version to use the tf0.12
    
    Tf 12 upgrade environment repo for non-prod namespaces

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/dstest/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/dstest/resources/ecr.tf
@@ -1,10 +1,10 @@
 module "dstest_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.0"
   repo_name = "dstest"
   team_name = "webops"
 
   providers = {
-    aws = "aws.london"
+    aws = aws.london
   }
 }
 
@@ -14,10 +14,11 @@ resource "kubernetes_secret" "dstest_ecr_credentials" {
     namespace = "dstest"
   }
 
-  data {
-    access_key_id     = "${module.dstest_ecr_credentials.access_key_id}"
-    secret_access_key = "${module.dstest_ecr_credentials.secret_access_key}"
-    repo_arn          = "${module.dstest_ecr_credentials.repo_arn}"
-    repo_url          = "${module.dstest_ecr_credentials.repo_url}"
+  data = {
+    access_key_id     = module.dstest_ecr_credentials.access_key_id
+    secret_access_key = module.dstest_ecr_credentials.secret_access_key
+    repo_arn          = module.dstest_ecr_credentials.repo_arn
+    repo_url          = module.dstest_ecr_credentials.repo_url
   }
 }
+
