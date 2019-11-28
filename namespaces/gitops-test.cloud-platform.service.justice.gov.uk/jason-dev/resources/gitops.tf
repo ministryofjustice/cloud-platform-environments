1df912c3ac4226e8eff89c953ecd7ebfe062355e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-11-28 17:46:48 +0000

    Add main.tf file from master

--- namespaces/gitops-test.cloud-platform.service.justice.gov.uk/jason-dev/resources/gitops.tf
+++ namespaces/gitops-test.cloud-platform.service.justice.gov.uk/jason-dev/resources/gitops.tf
@@ -3,7 +3,7 @@ module "concourse-gitops" {
   source_code_url               = "https://github.com/ministryofjustice/cloud-platform-helloworld-ruby-app"
   github_team                   = "webops"
   namespace                     = "jason-dev"
-  branch                        = "master"
+  branch                        = "add/gitops"
   concourse_basic_auth_username = var.concourse_basic_auth_username
   concourse_url                 = var.concourse_url
   concourse_basic_auth_password = var.concourse_basic_auth_password
