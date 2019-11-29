f2d6b8d0da68a896ad3150738ca75f73f1ddf060
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-11-29 14:07:36 +0000

    Apply suggestions from code review
    New format for replaced variables for TF 12
    
    Co-Authored-By: Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/route53.tf
@@ -4,13 +4,13 @@
 #################################################################################
 
 resource "aws_route53_zone" "peoplefinder_route53_zone" {
-  name = "${var.domain}"
+  name = var.domain
 
   tags {
     business-unit          = "Central Digital"
     application            = "peoplefinder"
-    is-production          = "${var.is-production}"
-    environment-name       = "${var.environment-name}"
+    is-production          = var.is-production
+    environment-name       = var.environment-name
     owner                  = "peoplefinder"
     infrastructure-support = "people-finder-support@digital.justice.gov.uk"
   }
@@ -19,19 +19,19 @@ resource "aws_route53_zone" "peoplefinder_route53_zone" {
 resource "kubernetes_secret" "peoplefinder_route53_zone_sec" {
   metadata {
     name      = "peoplefinder-route53-zone-output"
-    namespace = "${var.namespace}"
+    namespace = var.namespace
   }
 
   data {
-    zone_id      = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
-    name_servers = "${join("\n", aws_route53_zone.peoplefinder_route53_zone.name_servers)}"
+    zone_id      = aws_route53_zone.peoplefinder_route53_zone.zone_id
+    name_servers = join("\n", aws_route53_zone.peoplefinder_route53_zone.name_servers)
   }
 }
 
 # Production alias to the template deploy loadbalancer
 resource "aws_route53_record" "peoplefinder_route53_A_record_production" {
   name    = "."
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   type    = "A"
 
   alias {
@@ -44,7 +44,7 @@ resource "aws_route53_record" "peoplefinder_route53_A_record_production" {
 
 # Sendgrid records copied from template deploy - to keep until switch to Notify
 resource "aws_route53_record" "peoplefinder_route53_sendgrid_1495674" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "1495674.peoplefinder.service.gov.uk."
   type    = "CNAME"
   ttl     = "300"
@@ -52,7 +52,7 @@ resource "aws_route53_record" "peoplefinder_route53_sendgrid_1495674" {
 }
 
 resource "aws_route53_record" "peoplefinder_route53_sendgrid_s1" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "s1._domainkey.peoplefinder.service.gov.uk."
   type    = "CNAME"
   ttl     = "300"
@@ -60,7 +60,7 @@ resource "aws_route53_record" "peoplefinder_route53_sendgrid_s1" {
 }
 
 resource "aws_route53_record" "peoplefinder_route53_sendgrid_s2" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "s2._domainkey.peoplefinder.service.gov.uk."
   type    = "CNAME"
   ttl     = "300"
@@ -68,7 +68,7 @@ resource "aws_route53_record" "peoplefinder_route53_sendgrid_s2" {
 }
 
 resource "aws_route53_record" "peoplefinder_route53_sendgrid_email" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "email.peoplefinder.service.gov.uk."
   type    = "CNAME"
   ttl     = "300"
@@ -76,7 +76,7 @@ resource "aws_route53_record" "peoplefinder_route53_sendgrid_email" {
 }
 
 resource "aws_route53_record" "peoplefinder_route53_sendgrid_mail" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "mail.peoplefinder.service.gov.uk."
   type    = "CNAME"
   ttl     = "300"
@@ -85,7 +85,7 @@ resource "aws_route53_record" "peoplefinder_route53_sendgrid_mail" {
 
 # Other records copied from template deploy - Do we need this one? Get rid?
 resource "aws_route53_record" "peoplefinder_route53_mx" {
-  zone_id = "${aws_route53_zone.peoplefinder_route53_zone.zone_id}"
+  zone_id = aws_route53_zone.peoplefinder_route53_zone.zone_id
   name    = "peoplefinder.service.gov.uk."
   type    = "MX"
   ttl     = "300"
