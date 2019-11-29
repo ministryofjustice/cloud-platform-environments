c8caa8e588a683527540f8e79ab5a80ca6856013
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-29 13:26:22 +0000

    Update namespace c100-application-production
    Terraform 0.12 upgrade and update modules version to use the tf0.12
    
    Tf 12 upgrade environment repo for prod namespaces

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-production/resources/redis.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-production/resources/redis.tf
@@ -3,30 +3,31 @@
 ########################################################
 
 module "redis-elasticache" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=3.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.0"
 
-  cluster_name         = "${var.cluster_name}"
-  cluster_state_bucket = "${var.cluster_state_bucket}"
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
 
-  application            = "${var.application}"
-  environment-name       = "${var.environment-name}"
-  is-production          = "${var.is-production}"
-  infrastructure-support = "${var.infrastructure-support}"
-  team_name              = "${var.team_name}"
+  application            = var.application
+  environment-name       = var.environment-name
+  is-production          = var.is-production
+  infrastructure-support = var.infrastructure-support
+  team_name              = var.team_name
 
   providers = {
-    aws = "aws.london"
+    aws = aws.london
   }
 }
 
 resource "kubernetes_secret" "redis-elasticache" {
   metadata {
     name      = "elasticache-c100-production"
-    namespace = "${var.namespace}"
+    namespace = var.namespace
   }
 
-  data {
-    primary_endpoint_address = "${module.redis-elasticache.primary_endpoint_address}"
-    auth_token               = "${module.redis-elasticache.auth_token}"
+  data = {
+    primary_endpoint_address = module.redis-elasticache.primary_endpoint_address
+    auth_token               = module.redis-elasticache.auth_token
   }
 }
+
