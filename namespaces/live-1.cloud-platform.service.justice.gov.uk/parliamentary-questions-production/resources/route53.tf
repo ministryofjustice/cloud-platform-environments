72ea01d2ca0945b1054e30e03558d1a0af37df1c
Mourad <mourad.trabelsi@digital.justice.gov.uk>
2019-12-02 14:36:26 +0000

    add k8s secret, rename resource

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/route53.tf
@@ -1,5 +1,5 @@
 
-resource "aws_route53_zone" "peoplefinder_route53_zone" {
+resource "aws_route53_zone" "parliamentary_questions" {
   name = var.domain
   tags = {
     business-unit          = var.team_name
@@ -8,5 +8,17 @@ resource "aws_route53_zone" "peoplefinder_route53_zone" {
     environment-name       = var.environment-name
     owner                  = var.team_name
     infrastructure-support = var.infrastructure-support
+  }
+}
+
+resource "kubernetes_secret" "parliamentary_questions_route53" {
+  metadata {
+    name      = "parliamentary_questions_route53"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id      = aws_route53_zone.parliamentary_questions.zone_id
+    name_servers = join("\n", aws_route53_zone.parliamentary_questions.name_servers)
   }
 }
