afa6e4c8823eac219b4b5c1a70e6ea26915686bf
David Salgado <david.salgado@digital.justice.gov.uk>
2019-12-04 15:09:04 +0000

    Require additional libraries and gems

--- lib/cp_env.rb
+++ lib/cp_env.rb
@@ -1,5 +1,8 @@
 require "bundler/setup"
+require "kubeclient"
 require "open3"
+require "open-uri"
+require "aws-sdk-s3"
 
 require File.join(File.dirname(__FILE__), "cp_env", "pipeline")
 require File.join(File.dirname(__FILE__), "cp_env", "terraform")
