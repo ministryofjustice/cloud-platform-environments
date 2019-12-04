68fd82387a47fc78426d716e486cdc8c5a23f612
David Salgado <david.salgado@digital.justice.gov.uk>
2019-12-04 15:13:42 +0000

    Add the `Kubeconfig` class inside CpEnv:: scope
    This is copied from https://github.com/ministryofjustice/cloud-platform-environments-checker/blob/2d4b146fce94d00a78e94569dd168961ea30658d/lib/orphaned_namespace_checker/kubeconfig.rb
    
    A nicer approach might be to assume the kubeconfig
    is available in the local filesystem, and have the
    pipeline put it there before invoking whatever
    script it's running. But, this works (as long as
    the required environment variables have been set).

--- lib/cp_env.rb
+++ lib/cp_env.rb
@@ -4,5 +4,9 @@ require "open3"
 require "open-uri"
 require "aws-sdk-s3"
 
+class CpEnv
+end
+
 require File.join(File.dirname(__FILE__), "cp_env", "pipeline")
 require File.join(File.dirname(__FILE__), "cp_env", "terraform")
+require File.join(File.dirname(__FILE__), "cp_env", "kubeconfig")
