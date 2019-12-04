9b54b8997eac92b21daf8092e77e980d786e64b6
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2019-12-04 11:04:56 +0000

    removed 'is production' attribute

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
@@ -11,7 +11,6 @@ module "court_case_rds" {
   team_name              = var.team_name
   business-unit          = var.business-unit
   application            = var.application
-  is-production          = var.is-production
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure-support
   rds_family             = var.rds-family
