9e6aee421c8f5dc2792868fff2c7895b431a9ccb
steverendell <steve.rendell@syscon.net>
2019-12-05 10:42:15 +0000

    PAT-361 Pathfinder - create S3 api client interface and buckets

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/s3.tf
@@ -0,0 +1,59 @@
+module "pathfinder_document_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.0"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = true
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "pathfinder_reporting_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.0"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "pathfinder_document_s3_bucket" {
+  metadata {
+    name      = "pathfinder_document_s3_bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_document_s3_bucket.access_key_id
+    secret_access_key = module.pathfinder_document_s3_bucket.secret_access_key
+    bucket_arn        = module.pathfinder_document_s3_bucket.bucket_arn
+    bucket_name       = module.pathfinder_document_s3_bucket.bucket_name
+  }
+}
+
+resource "kubernetes_secret" "pathfinder_reporting_s3_bucket" {
+  metadata {
+    name      = "pathfinder_reporting_s3_bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_reporting_s3_bucket.access_key_id
+    secret_access_key = module.pathfinder_reporting_s3_bucket.secret_access_key
+    bucket_arn        = module.pathfinder_reporting_s3_bucket.bucket_arn
+    bucket_name       = module.pathfinder_reporting_s3_bucket.bucket_name
+  }
+}
