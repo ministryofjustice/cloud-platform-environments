1d417fb67e29610709476c9bd87a84027a14f3b7
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-12-05 15:51:27 +0000

    Bump terraform template version to permit outdated ES version to run
    1.5 doesn't support logging

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
@@ -4,7 +4,7 @@
 #################################################################################
 
 module "peoplefinder_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.1"
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
   application            = "peoplefinder"
