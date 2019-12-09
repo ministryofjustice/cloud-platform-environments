6a54ade1c9e845fe90ed5c3d912af04a26e9988d
mseedat-moj <47113046+mseedat-moj@users.noreply.github.com>
2019-12-09 13:42:03 +0000

    Add pingdom to QA (#1728)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/pingdom.tf
@@ -0,0 +1,22 @@
+################################################################################
+# Track a Query (Correspondence Tool Staff)
+# Pingdom check for uptime monitoring
+#################################################################################
+
+provider "pingdom" {}
+
+resource "pingdom_check" "track-a-query-qa-healthcheck" {
+  type                     = "http"
+  name                     = "Track a Query QA - healthcheck"
+  host                     = "qa.track-a-query.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ping"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_central_digital,application_track_a_query,component_ping,isproduction_${var.is-production},environment_${var.environment-name},infrastructuresupport_track_a_query"
+  probefilters             = "region:EU"
+  publicreport             = "true"
+}
