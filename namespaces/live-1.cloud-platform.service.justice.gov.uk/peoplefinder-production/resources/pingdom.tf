3bc4addf8dd8e4b454404d803e763994eacbebbc
Javid Ali <javid.ali@digital.justice.gov.uk>
2019-12-11 12:28:00 +0000

    peoplefinder-production Adjust pingdom tf file

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/pingdom.tf
@@ -1,18 +1,14 @@
-terraform {
-  backend "s3" {}
-}
-
 provider "pingdom" {}
 
 resource "pingdom_check" "peoplefinder-production-cloud-platform-healthcheck" {
    type                     = "http"
    name                     = "Peoplefinder - Production - cloud-platform - Healthcheck"
-   host                     = "https://peoplefinder.service.gov.uk/"
+   host                     = "peoplefinder.service.gov.uk"
    resolution               = 1
    notifywhenbackup         = true
    sendnotificationwhendown = 3
    notifyagainevery         = 0
-   url                      = "/"
+   url                      = "/healthcheck"
    encryption               = true
    port                     = 443
    tags                     = "businessunit_central_digital,application_peoplefinder,component_healthcheck,isproduction_true,environment_prod,infrastructuresupport_staff_tools_and_services"
