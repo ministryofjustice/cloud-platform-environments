671365b736184f9e54bde809a71f92c477890e9e
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2019-12-16 15:10:54 +0000

    add new namespaces

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/oasys-keycloak-prod/resources/rds.tf
@@ -0,0 +1,39 @@
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+module "oasys-keycloak_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  cluster_name           = var.cluster_name
+  cluster_state_bucket   = var.cluster_state_bucket
+  team_name              = var.team_name
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  force_ssl              = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "oasys-keycloak_rds" {
+  metadata {
+    name      = "oasys-keycloak-rds-instance-output"
+    namespace = "oasys-keycloak-prod"
+  }
+
+  data = {
+    rds_instance_endpoint = module.oasys-keycloak_rds.rds_instance_endpoint
+    database_name         = module.oasys-keycloak_rds.database_name
+    database_username     = module.oasys-keycloak_rds.database_username
+    database_password     = module.oasys-keycloak_rds.database_password
+    rds_instance_address  = module.oasys-keycloak_rds.rds_instance_address
+    url                   = "postgres://${module.oasys-keycloak_rds.database_username}:${module.oasys-keycloak_rds.database_password}@${module.oasys-keycloak_rds.rds_instance_endpoint}/${module.oasys-keycloak_rds.database_name}"
+  }
+}
+
