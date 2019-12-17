2814715e13f83f665c642a7e36a3c7c220c517ee
David Salgado <david@digitalronin.com>
2019-12-17 17:29:16 +0000

    Add spec & fixtures for make gitops-namespace
    These invoke the script directly, since we can't
    run docker commands in the github action which 
    runs the specs on push.

---
+++ spec/make_gitops_namespace_spec.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+
+def make_gitops_namespace(filename)
+  cmd = "./bin/create-gitops-namespace-files.rb #{filename}"
+  system(cmd)
+end
+
+describe "make gitops namespace" do
+  let(:answers_file) { "spec/fixtures/make-gitops-namespace-answers.yaml" }
+  let(:namespace) { "make-gitops-namespace-test-dev" } # Defined in the answers_file
+  let(:namespace_dir) { "namespaces/gitops-test.cloud-platform.service.justice.gov.uk/#{namespace}" }
+  let(:fixture_dir) { "spec/fixtures/make-gitops-namespace-test-dev" }
+
+  let(:fixture_dir) { "spec/fixtures/make-gitops-namespace-test-dev" }
+
+  after do
+    system("rm -rf #{namespace_dir}")
+  end
+
+  it "creates namespace directory" do
+    expect {
+      make_gitops_namespace(answers_file)
+    }.to change { FileTest.directory?(namespace_dir) }.from(false).to(true)
+  end
+
+  it "creates all the namespace files" do
+    make_gitops_namespace(answers_file)
+
+    fixture_files = Dir["#{fixture_dir}/**/*"].map { |f| f.sub(fixture_dir, "") }
+    created_files = Dir["#{namespace_dir}/**/*"].map { |f| f.sub(namespace_dir, "") }
+
+    expect(created_files).to eq(fixture_files)
+  end
+
+  it "creates files with correct contents" do
+    make_gitops_namespace(answers_file)
+
+    files = Dir["#{fixture_dir}/**/*"].map { |f| f.sub(fixture_dir, "") }
+
+    files.each do |file|
+      fixture_file = File.join(fixture_dir, file)
+      namespace_file = File.join(namespace_dir, file)
+
+      unless FileTest.directory?(fixture_file)
+        fixture = File.read(fixture_file)
+        namespace = File.read(namespace_file)
+
+        expect(fixture).to eq(namespace)
+      end
+    end
+  end
+end
