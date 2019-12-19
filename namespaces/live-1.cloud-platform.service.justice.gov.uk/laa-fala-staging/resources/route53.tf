7d4d89a637d642e74646bc9cb05ac6da45061649
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-12-19 16:07:29 +0000

    Update zone resource definition for newer tf version

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/route53.tf
@@ -1,22 +1,22 @@
 resource "aws_route53_zone" "route53_zone" {
   name = "staging.find-legal-advice.justice.gov.uk"
 
-  tags {
-    business-unit    = "${var.business-unit}"
-    application      = "${var.application}"
-    is-production    = "${var.is-production}"
-    environment-name = "${var.environment-name}"
-    owner            = "${var.team_name}"
+  tags = {
+    business-unit    = var.business-unit
+    application      = var.application
+    is-production    = var.is-production
+    environment-name = var.environment-name
+    owner            = var.team_name
   }
 }
 
 resource "kubernetes_secret" "route53_zone_sec" {
   metadata {
     name      = "route53-zone-output"
-    namespace = "${var.namespace}"
+    namespace = var.namespace
   }
 
-  data {
-    zone_id = "${aws_route53_zone.route53_zone.zone_id}"
+  data = {
+    zone_id = aws_route53_zone.route53_zone.zone_id
   }
 }
