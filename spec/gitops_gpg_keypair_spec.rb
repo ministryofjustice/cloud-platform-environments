2466f05a249a5dc1cc5428c97a85cfac4a5affd8
David Salgado <david.salgado@digital.justice.gov.uk>
2019-12-19 16:17:55 +0000

    Improve gitops gpg keypair specs
    By making the execution expectations more explicit
    and copying them into each test, I was able to get
    the specs to work.

--- spec/gitops_gpg_keypair_spec.rb
+++ spec/gitops_gpg_keypair_spec.rb
@@ -14,7 +14,9 @@ describe CpEnv::GitopsGpgKeypair do
 
   let(:team_name) { "my-team" }
   let(:namespace) { "my-namespace" }
-  let(:pub_key_name) { "#{team_name}-gpg-pubkey" }
+
+  let(:sec_response) { [%({ "items": [ { "metadata": { "name": "#{sec_key_name}" } } ] }), "stderr", success] }
+  let(:pub_response) { [%({ "items": [ { "metadata": { "name": "#{pub_key_name}" } } ] }), "stderr", success] }
 
   let(:params) {
     {
@@ -28,59 +30,17 @@ describe CpEnv::GitopsGpgKeypair do
   subject(:gpg) { described_class.new(params) }
 
   before do
-    allow(executor).to receive(:execute).and_return(["stdout", "stderr", success])
     allow($stdout).to receive(:puts)
-
-    # See if secret key is in the concourse-team namespace
-    cmd = "kubectl -n concourse-#{team_name} get secrets -o json"
-    json = %({ "items": [ { "metadata": { "name": "#{sec_key_name}" } } ] })
-    allow(executor).to receive(:execute).with(cmd, silent: true).and_return(json)
-
-    # See if public key is in the app namespace
-    cmd = "kubectl -n #{namespace} get secrets -o json"
-    json = %({ "items": [ { "metadata": { "name": "#{pub_key_name}" } } ] })
-    allow(executor).to receive(:execute).with(cmd, silent: true).and_return(json)
-  end
-
-  context "when secret key exists" do
-    let(:sec_key_name) { "#{team_name}-gpg-seckey" }
-
-    it "does not generate a keypair" do
-      expect(key_generator).to_not receive(:generate)
-      gpg.generate_and_store
-    end
-
-    context "when public key does not exist in app namespace" do
-      let(:pub_key_name) { "does-not-match-public-key-name" }
-
-      # TODO: Fix this. I can't figure out how to make this test work correctly. With this version, rspec insists
-      # that I'm expecting the command to be called 3 times - I suspect this is something to do with having multiple
-      # different expectations on the executor object. I tried several variants, but couldn't get it to work, so I'm
-      # giving up, for now.
-      #
-      # it "copies public key from concourse-team namespace" do
-      #   cmd = "kubectl -n concourse-#{team_name} get secrets #{team_name}-gpg-pubkey -o yaml | sed 's/namespace: concourse-#{team_name}/namespace: #{namespace}/'  | kubectl create -f -"
-      #   expect(executor).to receive(:execute).with(cmd, silent: true).at_least(:once).and_return("stdout", "stderr", success)
-      #
-      #   gpg.generate_and_store
-      # end
-    end
-
-    context "when public key exists in app namespace" do
-      it "does not copy the public key from the concourse-team namespace" do
-        cmd = "kubectl -n concourse-#{team_name} get secrets #{team_name}-gpg-pubkey -o yaml | sed 's/namespace: concourse-#{team_name}/namespace: #{namespace}/'  | kubectl create -f -"
-        expect(executor).to_not receive(:execute).with(cmd, silent: true)
-
-        gpg.generate_and_store
-      end
-    end
   end
 
   context "when secret key does not exist" do
     let(:sec_key_name) { "wibble" }
 
     it "generates a keypair" do
+      allow(executor).to receive(:execute).and_return(sec_response)
+
       expect(key_generator).to receive(:generate).and_return(keypair)
+
       gpg.generate_and_store
     end
 
@@ -88,5 +48,51 @@ describe CpEnv::GitopsGpgKeypair do
     # it "stores public key in app namespace"
     # it "stores public key in concourse team namespace"
     # it "stores private key in concourse team namespace"
+  end
+
+  context "when secret key exists" do
+    let(:sec_key_name) { "#{team_name}-gpg-seckey" }
+
+    it "does not generate a keypair" do
+      allow(executor).to receive(:execute).and_return(sec_response)
+
+      expect(key_generator).to_not receive(:generate)
+
+      gpg.generate_and_store
+    end
+
+    context "when public key does not exist in app namespace" do
+      let(:pub_key_name) { "does-not-match" }
+
+      it "copies public key from concourse-team namespace" do
+        allow(executor).to receive(:execute).and_return(
+          sec_response,
+          pub_response,
+        )
+
+        cmd = "kubectl -n concourse-#{team_name} get secrets #{team_name}-gpg-pubkey -o yaml | sed 's/namespace: concourse-#{team_name}/namespace: #{namespace}/'  | kubectl create -f -"
+
+        expect(executor).to receive(:execute).with(cmd, silent: true)
+
+        gpg.generate_and_store
+      end
+    end
+
+    context "when public key exists in app namespace" do
+      let(:pub_key_name) { "#{team_name}-gpg-pubkey" }
+
+      it "does not copy the public key from the concourse-team namespace" do
+        allow(executor).to receive(:execute).and_return(
+          sec_response,
+          pub_response,
+        )
+
+        cmd = "kubectl -n concourse-#{team_name} get secrets #{team_name}-gpg-pubkey -o yaml | sed 's/namespace: concourse-#{team_name}/namespace: #{namespace}/'  | kubectl create -f -"
+
+        expect(executor).to_not receive(:execute).with(cmd, silent: true)
+
+        gpg.generate_and_store
+      end
+    end
   end
 end
