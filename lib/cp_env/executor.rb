91eced5849c2729b063879d697afefa63402f512
David Salgado <david.salgado@digital.justice.gov.uk>
2019-12-19 13:52:57 +0000

    Use an Executor class to execute commands
    This change makes no difference yet, but later
    changes will enable us to override the executor
    class so that our tests can pass in a mock object
    and check that the right commands would be 
    executed, without actually executing anything.

---
+++ lib/cp_env/executor.rb
@@ -0,0 +1,18 @@
+class CpEnv
+  class Executor
+    def execute(cmd, can_fail: false, silent: false)
+      log("blue", "executing: #{cmd}") unless silent
+      stdout, stderr, status = Open3.capture3(cmd)
+
+      unless can_fail || status.success?
+        log("red", "Command: #{cmd} failed.")
+        puts stderr
+        raise
+      end
+
+      puts stdout unless silent
+
+      [stdout, stderr, status]
+    end
+  end
+end
--- lib/cp_env/pipeline.rb
+++ lib/cp_env/pipeline.rb
@@ -96,18 +96,7 @@ def apply_terraform(cluster, namespace, dir)
 end
 
 def execute(cmd, can_fail: false, silent: false)
-  log("blue", "executing: #{cmd}") unless silent
-  stdout, stderr, status = Open3.capture3(cmd)
-
-  unless can_fail || status.success?
-    log("red", "Command: #{cmd} failed.")
-    puts stderr
-    raise
-  end
-
-  puts stdout unless silent
-
-  [stdout, stderr, status]
+  CpEnv::Executor.new.execute(cmd, can_fail: can_fail, silent: silent)
 end
 
 def log(colour, message)
