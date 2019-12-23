ba1cf8c9968c0c502b5231c80253ac38f59bbe0e
David Salgado <david.salgado@digital.justice.gov.uk>
2019-12-23 12:48:34 +0000

    Get the specs to pass

--- spec/namespace_dir_spec.rb
+++ spec/namespace_dir_spec.rb
@@ -4,7 +4,11 @@ describe CpEnv::NamespaceDir do
   let(:namespace) { "mynamespace" }
   let(:dir) { "/namespaces/live1/#{namespace}" }
   let(:cluster) { "my-cluster" }
-  let(:executor) { double(CpEnv::Executor) }
+
+  let(:success) { double(success?: true) }
+  let(:resp) { ["stdout", "stderr", success] }
+  let(:executor) { double(CpEnv::Executor, execute: resp) }
+
   let(:terraform) { double(CpEnv::Terraform) }
   let(:kubectl_apply) { "kubectl -n #{namespace} apply -f #{dir}" }
   let(:yaml_files) { [] }
@@ -30,13 +34,11 @@ describe CpEnv::NamespaceDir do
 
     it "does not update the working copy" do
       expect(executor).to_not receive(:execute).with("git pull")
-
       namespace_dir.apply
     end
 
     it "does not call terraform apply" do
       expect(terraform).to_not receive(:apply)
-
       namespace_dir.apply
     end
   end
@@ -44,19 +46,16 @@ describe CpEnv::NamespaceDir do
   context "when given a directory" do
     before do
       allow(FileTest).to receive(:directory?).with(dir).and_return(true)
-      allow(executor).to receive(:execute).with("git pull")
       allow(terraform).to receive(:apply)
     end
 
     it "updates the working copy" do
       expect(executor).to receive(:execute).with("git pull")
-
       namespace_dir.apply
     end
 
     it "calls terraform apply" do
       expect(terraform).to receive(:apply)
-
       namespace_dir.apply
     end
 
@@ -65,7 +64,6 @@ describe CpEnv::NamespaceDir do
 
       it "applies kubernetes files" do
         expect(executor).to receive(:execute).with(kubectl_apply)
-
         namespace_dir.apply
       end
     end
@@ -75,15 +73,47 @@ describe CpEnv::NamespaceDir do
 
       it "does not run kubectl apply" do
         expect(executor).to_not receive(:execute).with(kubectl_apply)
-
         namespace_dir.apply
       end
     end
 
     context "with a gitops namespace" do
-      it "applies kubernetes files"
-      it "calls terraform apply"
-      it "applies gitops kubernetes files"
+      let(:team_name) { "webops" }
+      let(:kubectl_apply_gitops) { "kubectl -n concourse-#{team_name} apply -f #{dir}/gitops-resources" }
+      let(:yaml) { <<~YAML
+                   subjects:
+                     - kind: Group
+                       name: "github:#{team_name}"
+                       apiGroup: rbac.authorization.k8s.io
+                   YAML
+      }
+
+      let(:keypair) { double(CpEnv::GitopsGpgKeypair, generate_and_store: nil) }
+
+      before do
+        allow(FileTest).to receive(:exists?).with("#{dir}/gitops-resources").and_return(true)
+        allow(File).to receive(:read).with("#{dir}/01-rbac.yaml").and_return(yaml)
+        allow(CpEnv::GitopsGpgKeypair).to receive(:new).and_return(keypair)
+      end
+
+      context "with kubernetes files" do
+        let(:yaml_files) { [1,2,3] } # just has to be a non-empty array that responds to 'any?'
+
+        it "applies kubernetes files" do
+          expect(executor).to receive(:execute).with(kubectl_apply)
+          namespace_dir.apply
+        end
+
+        it "calls terraform apply" do
+          expect(terraform).to receive(:apply)
+          namespace_dir.apply
+        end
+
+        it "applies gitops kubernetes files" do
+          expect(executor).to receive(:execute).with(kubectl_apply_gitops)
+          namespace_dir.apply
+        end
+      end
     end
   end
 end
