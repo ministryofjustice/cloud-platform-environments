a36983b2f534ccda5fd58ab20973055384dcc6d3
David Salgado <david@digitalronin.com>
2020-01-07 15:24:42 +0000

    Add a 'silent' flag to the execute method
    This allows sending `silent: true` to this method, to suppress logging the output of the command, e.g. if it is very long, and/or
    there is anything in the output that we wouldn't
    want in a log.

--- lib/cp_env/pipeline.rb
+++ lib/cp_env/pipeline.rb
@@ -81,8 +81,9 @@ def apply_terraform(cluster, namespace, dir)
   Terraform.new(cluster: cluster, namespace: namespace, dir: dir).apply
 end
 
-def execute(cmd, can_fail: false)
-  log("blue", "executing: #{cmd}")
+def execute(cmd, can_fail: false, silent: false)
+  log("blue", "executing: #{cmd}") unless silent
+
   stdout, stderr, status = Open3.capture3(cmd)
 
   unless can_fail || status.success?
@@ -91,7 +92,7 @@ def execute(cmd, can_fail: false)
     raise
   end
 
-  puts stdout
+  puts stdout unless silent
 
   [stdout, stderr, status]
 end
