58aa062829d289c51b4b74a9e27ac6988b5cdbbb
David Salgado <david@digitalronin.com>
2020-01-07 15:27:49 +0000

    Add CpEnv::Executor class
    This will allow us to use dependency injection to
    test for things we expect to be executed in a 
    simpler way than intercepting messages to Open3.

--- lib/cp_env.rb
+++ lib/cp_env.rb
@@ -7,6 +7,7 @@ require "aws-sdk-s3"
 class CpEnv
 end
 
+require File.join(File.dirname(__FILE__), "cp_env", "executor")
 require File.join(File.dirname(__FILE__), "cp_env", "pipeline")
 require File.join(File.dirname(__FILE__), "cp_env", "terraform")
 require File.join(File.dirname(__FILE__), "cp_env", "namespace_deleter")
