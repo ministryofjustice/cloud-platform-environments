2625bcaf56dc728d192fb4b2dc7d455c23b76ed8
Matt Whittaker <ops@mattops.it>
2020-01-16 15:41:12 +0000

    update sqs message retention to 14 days

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/pathfinder-sub-queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/pathfinder-sub-queue.tf
@@ -1,12 +1,13 @@
 module "pathfinder_events_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
 
-  environment-name       = var.environment-name
-  team_name              = var.team_name
-  infrastructure-support = var.infrastructure-support
-  application            = var.application
-  sqs_name               = "pathfinder_events_queue"
-  encrypt_sqs_kms        = "true"
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "pathfinder_events_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
 
   redrive_policy = <<EOF
   {
