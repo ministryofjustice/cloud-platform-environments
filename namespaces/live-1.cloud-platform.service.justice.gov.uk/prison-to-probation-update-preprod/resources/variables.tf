1c7a6f0617fcfe5cf5ccff7730571d7b3771ab4f
Andy Marke <andy.marke@gmail.com>
2020-01-20 14:18:54 +0000

    Create prison-to-probation-update-preprod and prod namespace and add new queue used by service in those namespaces

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/variables.tf
@@ -0,0 +1,42 @@
+variable "domain" {
+  default = "prison-to-probation-update-preprod.prison.service.justice.gov.uk"
+}
+
+variable "application" {
+  default = "prison-to-probation-update"
+}
+
+variable "namespace" {
+  default = "prison-to-probation-update-preprod"
+}
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Digital Prison Services/Tech"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
