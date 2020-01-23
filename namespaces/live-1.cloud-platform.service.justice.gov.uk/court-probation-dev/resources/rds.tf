dedc9578b44cf2fecb2a155e915d6c2abc08ab46
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2020-01-23 09:53:40 +0000

    add new rds instance for court-probation-dev

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/rds.tf
@@ -37,3 +37,37 @@ resource "kubernetes_secret" "court_case_rds" {
   }
 }
 
+module "court_case_service_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  cluster_name           = var.cluster_name
+  cluster_state_bucket   = var.cluster_state_bucket
+  team_name              = var.team_name
+  business-unit          = var.business-unit
+  application            = var.application
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  rds_family             = var.rds-family
+  force_ssl              = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "court_case_service_rds" {
+  metadata {
+    name      = "court-case-service-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.court_case_service_rds.rds_instance_endpoint
+    database_name         = module.court_case_service_rds.database_name
+    database_username     = module.court_case_service_rds.database_username
+    database_password     = module.court_case_service_rds.database_password
+    rds_instance_address  = module.court_case_service_rds.rds_instance_address
+    url                   = "postgres://${module.court_case_service_rds.database_username}:${module.court_case_service_rds.database_password}@${module.court_case_service_rds.rds_instance_endpoint}/${module.court_case_service_rds.database_name}"
+  }
+}
+
+
