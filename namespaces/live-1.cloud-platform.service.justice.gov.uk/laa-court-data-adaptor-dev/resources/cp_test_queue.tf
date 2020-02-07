e78a45ba7daa9929cb2aa621a2b740fd8958fa28
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-02-07 12:37:02 +0000

    Add policy to allow aws legacy to recieve message

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/cp_test_queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/cp_test_queue.tf
@@ -25,10 +25,27 @@ resource "aws_sqs_queue_policy" "cp_test_queue" {
     "Statement":
       [
         {
+          "Sid": "first"
           "Effect": "Allow",
           "Principal": {"AWS": "*"},
           "Resource": "${module.cp_test_queue.sqs_arn}",
           "Action": "SQS:*"
+        },
+        {
+          "Sid": "Second",
+          "Effect": "Allow",
+          "Principal": {
+          "AWS": [
+            "902837325998"
+              ]
+          }
+          "Action": "sqs:ReceiveMessage",
+          "Resource": "arn:aws:sqs:eu-west-2:754256621582:crimeapps-development-cp-test-queue",
+          "Condition": {
+              "ArnEquals": {
+                   "aws:SourceArn": "arn:aws:sqs:us-east-1:111045819866:test-queue"
+                     }
+              }
         }
       ]
   }
