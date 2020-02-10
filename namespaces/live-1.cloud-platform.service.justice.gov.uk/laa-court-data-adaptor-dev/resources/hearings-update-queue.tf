4cdff6f3f2c89f4f6aed0043e2942955e6577eba
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-02-10 08:57:25 +0000

    Add hearings update queue for crimeapps

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/hearings-update-queue.tf
@@ -0,0 +1,100 @@
+module "hearings_update_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name          = var.environment_name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure_support
+  application               = var.application
+  sqs_name                  = var.sqs_queue_name
+  encrypt_sqs_kms           = var.encrypt_sqs_kms
+  message_retention_seconds = var.message_retention_seconds
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.hearings_update_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+  EOF
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "hearings_update_queue_policy" {
+  queue_url = module.hearings_update_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.hearings_update_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Sid": "PublishPolicy",
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.hearings_update_queue.sqs_arn}",
+          "Action": "sqs:SendMessage"
+        },
+        {
+          "Sid": "ConsumePolicy",
+          "Effect": "Allow",
+          "Principal": {
+          "AWS": [
+            "902837325998"
+              ]
+          },
+          "Resource": "${module.hearings_update_queue.sqs_arn}",
+          "Action": "sqs:ReceiveMessage",
+        }
+      ]
+  }
+   EOF
+}
+
+module "hearings_update_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment_name       = var.environment_name
+  team_name              = var.team_name
+  namespace              = var.namespace
+  infrastructure-support = var.infrastructure_support
+  application            = var.application
+  sqs_name               = var.sqs_queue_name_dl
+  encrypt_sqs_kms        = var.encrypt_sqs_kms
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hearings_update_queue" {
+  metadata {
+    name      = "hearings_update-sqs-instance-output"
+    namespace = module.hearings_update_queue.namespace
+  }
+
+  data = {
+    access_key_id     = module.hearings_update_queue.access_key_id
+    secret_access_key = module.hearings_update_queue.secret_access_key
+    sqs_url           = module.hearings_update_queue.sqs_id
+    sqs_arn           = module.hearings_update_queue.sqs_arn
+    sqs_name          = module.hearings_update_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "hearings_update_dead_letter_queue" {
+  metadata {
+    name      = "hearings_update-sqs-dl-instance-output"
+    namespace = module.hearings_update_dead_letter_queue.namespace
+  }
+
+  data = {
+    access_key_id     = module.hearings_update_dead_letter_queue.access_key_id
+    secret_access_key = module.hearings_update_dead_letter_queue.secret_access_key
+    sqs_url           = module.hearings_update_dead_letter_queue.sqs_id
+    sqs_arn           = module.hearings_update_dead_letter_queue.sqs_arn
+    sqs_name          = module.hearings_update_dead_letter_queue.sqs_name
+  }
+}
+
---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/variables.tf
@@ -0,0 +1,53 @@
+variable "application" {}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "mojdigital"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default = "crime-apps-team"
+}
+
+variable "namespace" {
+  description = "The name of your development team"
+  default = "laa-court-data-adaptor-dev"
+}
+
+variable "environment_name" {
+  description = "The type of environment you're deploying to."
+  default = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default = "example-team@digtal.justice.gov.uk"
+}
+
+variable "sqs_queue_name" {
+  description = ""
+  default = "hearings-update-queue"
+}
+
+variable "sqs_queue_name_dl" {
+  description = ""
+  default = "hearings-update-queue-dl"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "encrypt_sqs_kms" {
+  description = "Encrypt sqs keys."
+  default     = "false"
+}
+
+variable "message_retention_seconds" {
+variable "message_retention_seconds" {
+  description = "The number of seconds Amazon SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days)."
+  default     = "1209600"
+}
+
+
