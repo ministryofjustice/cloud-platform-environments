eea1c1eff7fec30592d6b3640ba1b4ba1d021d20
G <gurnank@outlook.com>
2020-02-11 11:18:17 +0000

    Changed github group to github:dps-shared

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/resources/ecr.tf
@@ -0,0 +1,25 @@
+module "example_team_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.0"
+  repo_name = "cloud-platform-environments"
+  team_name = "HDC licences"
+
+  # aws_region = "eu-west-2"     # This input is deprecated from version 3.2 of this module
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "example_team_ecr_credentials" {
+  metadata {
+    name      = "example-team-ecr-credentials-output"
+    namespace = "learning-cloud-deployments-dev"
+  }
+
+  data = {
+    access_key_id     = module.example_team_ecr_credentials.access_key_id
+    secret_access_key = module.example_team_ecr_credentials.secret_access_key
+    repo_arn          = module.example_team_ecr_credentials.repo_arn
+    repo_url          = module.example_team_ecr_credentials.repo_url
+  }
+}
