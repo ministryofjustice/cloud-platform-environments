3ad1def068fab5b19fa08fb3014a89324fd7561c
Robin Sillem <robin.sillem@digital.justiice.gov.uk>
2020-02-12 13:29:57 +0000

    Adds a role that can be assumed from this namespace

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-core-services-tooling/resources/cprole.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_role" "pcst-role" {
+  name = "probation-core-services-tooling-role"
+  description = "A service-specific role which can be assumed by a suitably configured pod/namespace"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::754256621582:role/kiam-server"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_policy" "pcst-policy" {
+  name        = "probation-core-services-tooling-policy"
+  description = "Allows the role to assume any role in the hmpps-sandpit account"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Resource": "arn:aws:iam::434401102446:role/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "service" {
+  role       = aws_iam_role.pcst-role.name
+  policy_arn = aws_iam_policy.pcst-policy.arn
+}
+
