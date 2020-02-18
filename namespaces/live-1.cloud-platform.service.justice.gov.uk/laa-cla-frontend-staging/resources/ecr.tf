822cd2771b760018b37c6a3d4c2cf137e079f560
said-moj <45761276+said-moj@users.noreply.github.com>
2020-02-18 13:30:26 +0000

    Fixed missing equals sign after data

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/ecr.tf
@@ -15,9 +15,9 @@ resource "kubernetes_secret" "ecr-repo" {
     namespace = var.namespace
   }
 
-  data {
-    repo_url          = "${module.ecr-repo.repo_url}"
-    access_key_id     = "${module.ecr-repo.access_key_id}"
-    secret_access_key = "${module.ecr-repo.secret_access_key}"
+  data = {
+    repo_url          = module.ecr-repo.repo_url
+    access_key_id     = module.ecr-repo.access_key_id
+    secret_access_key = module.ecr-repo.secret_access_key
   }
 }
