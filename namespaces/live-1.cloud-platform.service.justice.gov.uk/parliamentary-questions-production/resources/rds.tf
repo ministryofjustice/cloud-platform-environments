757c5dab3d0f0b483112159b9058234ff8f3ecf7
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-02-19 16:56:58 +0000

    Parliamentary Questions production setup
    - Add rds.tf, versions.tf and serviceaccount-circleci.yaml files.
    - Amend formatting of main.tf and variables.tf files.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/rds.tf
@@ -0,0 +1,41 @@
+#############################################
+# Parliamentary Questions (postgres engine) #
+#############################################
+
+module "rds_instance" {
+  allow_major_version_upgrade = "true"
+  application                 = var.application
+  apply_method                = "pending-reboot"
+  cluster_name                = var.cluster_name
+  cluster_state_bucket        = var.cluster_state_bucket
+  db_backup_retention_period  = var.db_backup_retention_period
+  db_engine                   = "postgres"
+  db_engine_version           = "11"
+  db_name                     = "parliamentary_questions_production"
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure-support
+  is-production               = var.is-production
+  rds_family                  = "postgres11"
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  team_name                   = var.team_name
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "rds_instance" {
+  metadata {
+    name      = "parliamentary-questions-production-rds"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name         = module.rds_instance.database_name
+    database_password     = module.rds_instance.database_password
+    database_username     = module.rds_instance.database_username
+    rds_instance_address  = module.rds_instance.rds_instance_address
+    rds_instance_endpoint = module.rds_instance.rds_instance_endpoint
+    url                   = "postgres://${module.rds_instance.database_username}:${module.rds_instance.database_password}@${module.rds_instance.rds_instance_endpoint}/${module.rds_instance.database_name}"
+  }
+}
