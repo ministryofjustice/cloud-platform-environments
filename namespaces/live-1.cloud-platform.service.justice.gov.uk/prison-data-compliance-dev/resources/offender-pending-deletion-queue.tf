c7ef15abdb6be92e92fe9ae0bdbd8b418bb84b33
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-02-19 10:29:59 +0000

    GDPR-41 Shortening resource names for data compliance queues

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-dev/resources/offender-pending-deletion-queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-dev/resources/offender-pending-deletion-queue.tf
@@ -1,11 +1,11 @@
-module "offender_pending_deletion_queue" {
+module "off_pend_del_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
 
   environment-name           = var.environment-name
   team_name                  = var.team_name
   infrastructure-support     = var.infrastructure-support
   application                = var.application
-  sqs_name                   = "offender_pending_deletion_queue"
+  sqs_name                   = "off_pend_del_queue"
   encrypt_sqs_kms            = "true"
   visibility_timeout_seconds = 300
   message_retention_seconds  = 1209600
@@ -21,27 +21,27 @@ module "offender_pending_deletion_queue" {
   }
 }
 
-resource "aws_sqs_queue_policy" "offender_pending_deletion_queue_policy" {
-  queue_url = module.offender_pending_deletion_queue.sqs_id
+resource "aws_sqs_queue_policy" "off_pend_del_queue_policy" {
+  queue_url = module.off_pend_del_queue.sqs_id
 
   policy = <<EOF
   {
     "Version": "2012-10-17",
-    "Id": "${module.offender_pending_deletion_queue.sqs_arn}/SQSDefaultPolicy",
+    "Id": "${module.off_pend_del_queue.sqs_arn}/SQSDefaultPolicy",
     "Statement":
       [
         {
           "Sid": "PublishPolicy",
           "Effect": "Allow",
           "Principal": {"AWS": "*"},
-          "Resource": "${module.offender_pending_deletion_queue.sqs_arn}",
+          "Resource": "${module.off_pend_del_queue.sqs_arn}",
           "Action": "sqs:SendMessage"
         },
         {
           "Sid": "ConsumePolicy",
           "Effect": "Allow",
           "Principal": {"AWS": "*"},
-          "Resource": "${module.offender_pending_deletion_queue.sqs_arn}",
+          "Resource": "${module.off_pend_del_queue.sqs_arn}",
           "Action": "sqs:ReceiveMessage"
         }
       ]
@@ -56,7 +56,7 @@ module "offender_pending_deletion_dead_letter_queue" {
   team_name              = var.team_name
   infrastructure-support = var.infrastructure-support
   application            = var.application
-  sqs_name               = "offender_pending_deletion_queue_dl"
+  sqs_name               = "off_pend_del_queue_dl"
   encrypt_sqs_kms        = "true"
 
   providers = {
@@ -64,18 +64,18 @@ module "offender_pending_deletion_dead_letter_queue" {
   }
 }
 
-resource "kubernetes_secret" "offender_pending_deletion_queue" {
+resource "kubernetes_secret" "off_pend_del_queue" {
   metadata {
     name      = "offender-pending-deletion-sqs-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    access_key_id     = module.offender_pending_deletion_queue.access_key_id
-    secret_access_key = module.offender_pending_deletion_queue.secret_access_key
-    sqs_ocne_url      = module.offender_pending_deletion_queue.sqs_id
-    sqs_ocne_arn      = module.offender_pending_deletion_queue.sqs_arn
-    sqs_ocne_name     = module.offender_pending_deletion_queue.sqs_name
+    access_key_id     = module.off_pend_del_queue.access_key_id
+    secret_access_key = module.off_pend_del_queue.secret_access_key
+    sqs_ocne_url      = module.off_pend_del_queue.sqs_id
+    sqs_ocne_arn      = module.off_pend_del_queue.sqs_arn
+    sqs_ocne_name     = module.off_pend_del_queue.sqs_name
   }
 }
 
