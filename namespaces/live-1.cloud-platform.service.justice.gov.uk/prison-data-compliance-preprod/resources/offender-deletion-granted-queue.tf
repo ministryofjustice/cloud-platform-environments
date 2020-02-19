aabae983545fb9f6f535bba84f1737ae495725aa
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-02-19 12:26:28 +0000

    GDPR-41 Keeping preprod in line with dev

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-preprod/resources/offender-deletion-granted-queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-preprod/resources/offender-deletion-granted-queue.tf
@@ -21,34 +21,6 @@ module "offender_deletion_granted_queue" {
   }
 }
 
-resource "aws_sqs_queue_policy" "offender_deletion_granted_queue_policy" {
-  queue_url = module.offender_deletion_granted_queue.sqs_id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Id": "${module.offender_deletion_granted_queue.sqs_arn}/SQSDefaultPolicy",
-    "Statement":
-      [
-        {
-          "Sid": "PublishPolicy",
-          "Effect": "Allow",
-          "Principal": {"AWS": "*"},
-          "Resource": "${module.offender_deletion_granted_queue.sqs_arn}",
-          "Action": "sqs:SendMessage"
-        },
-        {
-          "Sid": "ConsumePolicy",
-          "Effect": "Allow",
-          "Principal": {"AWS": "*"},
-          "Resource": "${module.offender_deletion_granted_queue.sqs_arn}",
-          "Action": "sqs:ReceiveMessage"
-        }
-      ]
-  }
-   EOF
-}
-
 module "offender_deletion_granted_dead_letter_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
 
@@ -66,30 +38,30 @@ module "offender_deletion_granted_dead_letter_queue" {
 
 resource "kubernetes_secret" "offender_deletion_granted_queue" {
   metadata {
-    name      = "offender-deletion-granted-sqs-instance-output"
+    name      = "off-del-grant-sqs"
     namespace = var.namespace
   }
 
   data = {
     access_key_id     = module.offender_deletion_granted_queue.access_key_id
     secret_access_key = module.offender_deletion_granted_queue.secret_access_key
-    sqs_ocne_url      = module.offender_deletion_granted_queue.sqs_id
-    sqs_ocne_arn      = module.offender_deletion_granted_queue.sqs_arn
-    sqs_ocne_name     = module.offender_deletion_granted_queue.sqs_name
+    sqs_odg_url       = module.offender_deletion_granted_queue.sqs_id
+    sqs_odg_arn       = module.offender_deletion_granted_queue.sqs_arn
+    sqs_odg_name      = module.offender_deletion_granted_queue.sqs_name
   }
 }
 
 resource "kubernetes_secret" "offender_deletion_granted_dead_letter_queue" {
   metadata {
-    name      = "offender-deletion-granted-sqs-dl-instance-output"
+    name      = "off-del-grant-sqs-dl"
     namespace = var.namespace
   }
 
   data = {
     access_key_id     = module.offender_deletion_granted_dead_letter_queue.access_key_id
     secret_access_key = module.offender_deletion_granted_dead_letter_queue.secret_access_key
-    sqs_ocne_url      = module.offender_deletion_granted_dead_letter_queue.sqs_id
-    sqs_ocne_arn      = module.offender_deletion_granted_dead_letter_queue.sqs_arn
-    sqs_ocne_name     = module.offender_deletion_granted_dead_letter_queue.sqs_name
+    sqs_odg_url       = module.offender_deletion_granted_dead_letter_queue.sqs_id
+    sqs_odg_arn       = module.offender_deletion_granted_dead_letter_queue.sqs_arn
+    sqs_odg_name      = module.offender_deletion_granted_dead_letter_queue.sqs_name
   }
 }
