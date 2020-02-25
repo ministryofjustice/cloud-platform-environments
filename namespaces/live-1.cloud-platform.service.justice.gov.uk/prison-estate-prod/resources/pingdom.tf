ab2179f01a70911769bac541b790e4c963760f54
Peter Phillips <peter@greenthistle.co.uk>
2020-02-25 10:06:56 +0000

    DT-571: Add prison-estate-prod new service

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-prod/resources/pingdom.tf
@@ -0,0 +1,23 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 96624 = #dps_alerts
+# 96628 = DPS Pager duty
+
+resource "pingdom_check" "dps-production-check" {
+  type                     = "http"
+  name                     = "DPS - ${var.application}"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/estate.prison.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
