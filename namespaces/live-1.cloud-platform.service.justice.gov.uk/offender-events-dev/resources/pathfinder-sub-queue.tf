753f7a8cf5f22e6657683e1c70b33e4c3e341642
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-02-25 12:04:53 +0000

    2 new subscriber queues for the event topic

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/pathfinder-sub-queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/pathfinder-sub-queue.tf
@@ -20,6 +20,50 @@ module "pathfinder_events_queue" {
   }
 }
 
+module "pathfinder_tact_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "pathfinder_tact_events_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.pathfinder_tact_events_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+  EOF
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "pathfinder_offender_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "pathfinder_offender_events_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.pathfinder_offender_events_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+  EOF
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 resource "aws_sqs_queue_policy" "pathfinder_events_queue_policy" {
   queue_url = module.pathfinder_events_queue.sqs_id
 
@@ -33,6 +77,60 @@ resource "aws_sqs_queue_policy" "pathfinder_events_queue_policy" {
           "Effect": "Allow",
           "Principal": {"AWS": "*"},
           "Resource": "${module.pathfinder_events_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+                {
+                  "aws:SourceArn": "${module.offender_events.topic_arn}"
+                }
+            }
+        }
+      ]
+  }
+   EOF
+}
+
+resource "aws_sqs_queue_policy" "pathfinder_tact_events_queue_policy" {
+  queue_url = module.pathfinder_tact_events_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.pathfinder_tact_events_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.pathfinder_tact_events_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+                {
+                  "aws:SourceArn": "${module.offender_events.topic_arn}"
+                }
+            }
+        }
+      ]
+  }
+   EOF
+}
+
+resource "aws_sqs_queue_policy" "pathfinder_offender_events_queue_policy" {
+  queue_url = module.pathfinder_offender_events_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.pathfinder_offender_events_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.pathfinder_offender_events_queue.sqs_arn}",
           "Action": "SQS:SendMessage",
           "Condition":
             {
@@ -62,6 +160,36 @@ module "pathfinder_events_dead_letter_queue" {
   }
 }
 
+module "pathfinder_tact_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "pathfinder_tact_events_queue_dl"
+  encrypt_sqs_kms        = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "pathfinder_offender_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "pathfinder_offender_events_queue_dl"
+  encrypt_sqs_kms        = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 resource "kubernetes_secret" "pathfinder_events_queue" {
   metadata {
     name      = "pathfinder-events-sqs-instance-output"
@@ -74,6 +202,36 @@ resource "kubernetes_secret" "pathfinder_events_queue" {
     sqs_id            = module.pathfinder_events_queue.sqs_id
     sqs_arn           = module.pathfinder_events_queue.sqs_arn
     sqs_name          = module.pathfinder_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "pathfinder_tact_events_queue" {
+  metadata {
+    name      = "pathfinder-tact-events-sqs-instance-output"
+    namespace = "pathfinder-dev"
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_tact_events_queue.access_key_id
+    secret_access_key = module.pathfinder_tact_events_queue.secret_access_key
+    sqs_id            = module.pathfinder_tact_events_queue.sqs_id
+    sqs_arn           = module.pathfinder_tact_events_queue.sqs_arn
+    sqs_name          = module.pathfinder_tact_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "pathfinder_offender_events_queue" {
+  metadata {
+    name      = "pathfinder-offender-events-sqs-instance-output"
+    namespace = "pathfinder-dev"
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_offender_events_queue.access_key_id
+    secret_access_key = module.pathfinder_offender_events_queue.secret_access_key
+    sqs_id            = module.pathfinder_offender_events_queue.sqs_id
+    sqs_arn           = module.pathfinder_offender_events_queue.sqs_arn
+    sqs_name          = module.pathfinder_offender_events_queue.sqs_name
   }
 }
 
@@ -92,10 +250,56 @@ resource "kubernetes_secret" "pathfinder_events_dead_letter_queue" {
   }
 }
 
+resource "kubernetes_secret" "pathfinder_offender_events_dead_letter_queue" {
+  metadata {
+    name      = "pathfinder-offender-events-sqs-dl-instance-output"
+    namespace = "pathfinder-dev"
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_offender_events_dead_letter_queue.access_key_id
+    secret_access_key = module.pathfinder_offender_events_dead_letter_queue.secret_access_key
+    sqs_id            = module.pathfinder_offender_events_dead_letter_queue.sqs_id
+    sqs_arn           = module.pathfinder_offender_events_dead_letter_queue.sqs_arn
+    sqs_name          = module.pathfinder_offender_events_dead_letter_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "pathfinder_tact_events_dead_letter_queue" {
+  metadata {
+    name      = "pathfinder-tact-events-sqs-dl-instance-output"
+    namespace = "pathfinder-dev"
+  }
+
+  data = {
+    access_key_id     = module.pathfinder_tact_events_dead_letter_queue.access_key_id
+    secret_access_key = module.pathfinder_tact_events_dead_letter_queue.secret_access_key
+    sqs_id            = module.pathfinder_tact_events_dead_letter_queue.sqs_id
+    sqs_arn           = module.pathfinder_tact_events_dead_letter_queue.sqs_arn
+    sqs_name          = module.pathfinder_tact_events_dead_letter_queue.sqs_name
+  }
+}
+
 resource "aws_sns_topic_subscription" "pathfinder_subscription" {
   provider      = aws.london
   topic_arn     = module.offender_events.topic_arn
   protocol      = "sqs"
   endpoint      = module.pathfinder_events_queue.sqs_arn
   filter_policy = "{\"eventType\":[\"ALERT-INSERTED\", \"ALERT-UPDATED\"], \"code\":[\"XTACT\"]}"
+}
+
+resource "aws_sns_topic_subscription" "pathfinder_offender_events_subscription" {
+  provider      = aws.london
+  topic_arn     = module.offender_events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.pathfinder_offender_events_queue.sqs_arn
+  filter_policy = "{\"eventType\":[\"ALERT-INSERTED\", \"ALERT-UPDATED\"], \"code\":[\"XTACT\"]}"
+}
+
+resource "aws_sns_topic_subscription" "pathfinder_tact_events_subscription" {
+  provider      = aws.london
+  topic_arn     = module.offender_events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.pathfinder_offender_events_queue.sqs_arn
+  filter_policy = "{\"eventType\":[\"EXTERNAL_MOVEMENT_RECORD-INSERTED\", \"OFFENDER_ALIAS-CHANGED\"]}"
 }
