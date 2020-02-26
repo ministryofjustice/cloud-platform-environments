c6b6b973aae4eba01877c8a2991435af12216ba7
Peter Phillips <peter@greenthistle.co.uk>
2020-02-26 13:09:08 +0000

    DT-571: Temporarily remove copying of prod credentials into preprod

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-preprod/resources/rds.tf
@@ -33,21 +33,3 @@ resource "kubernetes_secret" "dps_rds" {
     secret_access_key     = module.dps_rds.secret_access_key
   }
 }
-
-# This places a secret for this preprod RDS instance in the production namespace,
-# this can then be used by a kubernetes job which will refresh the preprod data.
-resource "kubernetes_secret" "dps_rds_refresh_creds" {
-  metadata {
-    name      = "dps-rds-instance-output-preprod"
-    namespace = "prison-estate-prod"
-  }
-
-  data = {
-    rds_instance_endpoint = module.dps_rds.rds_instance_endpoint
-    database_name         = module.dps_rds.database_name
-    database_username     = module.dps_rds.database_username
-    database_password     = module.dps_rds.database_password
-    rds_instance_address  = module.dps_rds.rds_instance_address
-  }
-}
-
