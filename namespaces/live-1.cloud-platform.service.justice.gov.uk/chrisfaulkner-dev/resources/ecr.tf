c548655a61a03f03f31cb1e3790cfa4f358c7380
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-03-02 11:06:31 +0000

    adding ECR

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/chrisfaulkner-dev/resources/ecr.tf
@@ -0,0 +1,25 @@
+module "example_team_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.0"
+  repo_name = "chrisfaulkner-ruby-app"
+  team_name = "probation-in-court"
+
+  # aws_region = "eu-west-2"     # This input is deprecated from version 3.2 of this module
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "example_team_ecr_credentials" {
+  metadata {
+    name      = "pic-chrisfaulkner-dev-ecr-credentials-output"
+    namespace = "chrisfaulkner-dev"
+  }
+
+  data = {
+    access_key_id     = module.example_team_ecr_credentials.access_key_id
+    secret_access_key = module.example_team_ecr_credentials.secret_access_key
+    repo_arn          = module.example_team_ecr_credentials.repo_arn
+    repo_url          = module.example_team_ecr_credentials.repo_url
+  }
+}
