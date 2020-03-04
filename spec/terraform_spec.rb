22854c1ccfeb0b7d99be9ce7e3b1427fb0768ac0
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-03-04 15:19:08 +0000

    Fix the rspec tests for failure

--- spec/terraform_spec.rb
+++ spec/terraform_spec.rb
@@ -40,7 +40,7 @@ describe CpEnv::Terraform do
 
       tf_init = "cd #{tf_dir}; terraform init -backend-config=\"bucket=bucket\" -backend-config=\"key=key-prefix/live-1.cloud-platform.service.justice.gov.uk/mynamespace/terraform.tfstate\" -backend-config=\"dynamodb_table=lock-table\" -backend-config=\"region=region\""
 
-      tf_plan = "cd #{tf_dir}; terraform plan | grep -v password"
+      tf_plan = "cd #{tf_dir}; terraform plan | grep -v password  "
 
       expect_execute(tf_init, "", success)
       expect_execute(tf_plan, "", success)
