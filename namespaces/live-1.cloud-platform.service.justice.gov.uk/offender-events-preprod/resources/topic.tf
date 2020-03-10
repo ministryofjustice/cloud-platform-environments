c6653c029e48fb31516c27986f9d1d83c2267ab2
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-03-10 09:02:51 +0000

    Add probation-offender-events topic to preprod (#2022)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
@@ -47,3 +47,24 @@ resource "kubernetes_secret" "prison-data-compliance" {
     topic_arn         = module.offender_events.topic_arn
   }
 }
+
+module "probation_offender_events" {
+  source             = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.0"
+  team_name          = var.team_name
+  topic_display_name = "probation-offender-events"
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "probation_offender_events" {
+  metadata {
+    name      = "probation-offender-events-topic"
+    namespace = var.namespace
+  }
+  data = {
+    access_key_id     = module.probation_offender_events.access_key_id
+    secret_access_key = module.probation_offender_events.secret_access_key
+    topic_arn         = module.probation_offender_events.topic_arn
+  }
+}
