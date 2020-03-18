d6f5b426972ddebe221d93c7d038c0ed85d7ee09
David Salgado <david@digitalronin.com>
2020-03-18 15:39:14 +0000

    Update RDS module for laa-court-data-adaptor-dev

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/rds.tf
@@ -17,7 +17,7 @@ variable "cluster_state_bucket" {
  *
  */
 module "laa_crime_apps_team_rds" {
-  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
   cluster_name         = var.cluster_name
   cluster_state_bucket = var.cluster_state_bucket
   team_name            = "laa-crime-apps-team"
