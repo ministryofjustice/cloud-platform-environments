3c43373349846b613da52533e941e988e1d74d6d
David Salgado <david@digitalronin.com>
2020-03-18 16:10:31 +0000

    Update RDS module for laa-apply-for-legalaid-staging

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-staging/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-staging/resources/rds.tf
@@ -5,7 +5,7 @@
  *
  */
 module "apply-for-legal-aid-rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
 
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
