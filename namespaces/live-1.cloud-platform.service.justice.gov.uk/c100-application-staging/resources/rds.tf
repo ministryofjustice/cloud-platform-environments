a6eda6f77b713e42e2208fc1a4eb9bcbe7c15f7a
David Salgado <david@digitalronin.com>
2020-03-19 10:57:45 +0000

    Update RDS module for c100-application-staging

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-staging/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/c100-application-staging/resources/rds.tf
@@ -3,7 +3,7 @@
 ########################################
 
 module "rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
 
   cluster_name         = var.cluster_name
   cluster_state_bucket = var.cluster_state_bucket
