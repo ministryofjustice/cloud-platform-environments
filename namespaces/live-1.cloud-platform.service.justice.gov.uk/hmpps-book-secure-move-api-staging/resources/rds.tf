92f21772b686e5f2aebfac11cb4d77b3b3732208
Stephen Dicks <stephen@starswan.com>
2020-03-26 14:13:17 +0000

    Take II on putting an RDS snapshot from staging into dev

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-staging/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-staging/resources/rds.tf
@@ -48,6 +48,26 @@ resource "kubernetes_secret" "rds-instance" {
   }
 }
 
+module "rds-snapshot" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
+
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
+
+  application            = var.application
+  environment-name       = var.environment-name
+  is-production          = var.is-production
+  infrastructure-support = var.infrastructure-support
+  team_name              = var.team_name
+  force_ssl              = "false"
+  snapshot_identifier    = "rds:cloud-platform-e829e612552f9703-2020-03-24-00-59"
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
 resource "kubernetes_secret" "rds-snapshot" {
   metadata {
     name      = "rds-snapshot-staging"
@@ -57,7 +77,11 @@ resource "kubernetes_secret" "rds-snapshot" {
   data = {
     access_key_id     = module.rds-instance.access_key_id
     secret_access_key = module.rds-instance.secret_access_key
-    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+    username          = module.rds-instance.database_username
+    password          = module.rds-instance.database_password
+    endpoint          = module.rds-snapshot.rds_instance_endpoint
+    database_name     = module.rds-snapshot.database_name
+    url               = "postgres://${username}:${password}@${endpoint}/${database_name}"
   }
 }
 
