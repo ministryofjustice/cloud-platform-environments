e4f9e4fbc24e143197a6b5966fb348e58be0efd8
Stephen Dicks <stephen@starswan.com>
2020-03-26 07:58:42 +0000

    RDS snapshot, from production -> preprod for db repair testing

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/rds.tf
@@ -24,3 +24,15 @@ resource "kubernetes_secret" "rds-instance" {
   }
 }
 
+resource "kubernetes_secret" "rds-snapshot" {
+  metadata {
+    name      = "rds-snapshot-production"
+    namespace = var.preprod_namespace
+  }
+
+  data = {
+    access_key_id     = module.rds-instance.access_key_id
+    secret_access_key = module.rds-instance.secret_access_key
+    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+  }
+}
