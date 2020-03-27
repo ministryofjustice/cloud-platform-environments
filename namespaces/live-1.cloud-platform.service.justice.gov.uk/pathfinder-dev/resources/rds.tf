4565ae73aa4cc700aa7dc856ceffb9585e8ffd33
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2020-03-27 15:57:20 +0000

    PAT-708 - add missing iam Pass role

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/rds.tf
@@ -93,7 +93,8 @@ data "aws_iam_policy_document" "pathfinder-dev-rds-to-s3-export-policy" {
       "rds:StartExportTask",
       "rds:DescribeDBInstances",
       "kms:CreateGrant",
-      "kms:DescribeKey"
+      "kms:DescribeKey",
+      "iam:PassRole"
     ]
 
     resources = [
