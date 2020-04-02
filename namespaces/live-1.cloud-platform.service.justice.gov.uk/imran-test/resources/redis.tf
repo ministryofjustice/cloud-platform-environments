0bbcf3d844d9668db84fbb255a3c723fdb51846f
Imran Awan <imran.awan@gec.io>
2020-04-02 11:23:35 +0100

    imran - testing first deployment of resources to live

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/imran-test/resources/redis.tf
@@ -0,0 +1,33 @@
+#######################################################
+# C100 Application Redis Elasticache (for cache + jobs)
+#######################################################
+
+module "redis-elasticache" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.0"
+
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
+
+  application            = var.application
+  environment-name       = var.environment-name
+  is-production          = var.is-production
+  infrastructure-support = var.infrastructure-support
+  team_name              = var.team_name
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "redis-elasticache" {
+  metadata {
+    name      = "elasticache-imran"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.redis-elasticache.primary_endpoint_address
+    auth_token               = module.redis-elasticache.auth_token
+    url                      = "rediss://:${module.redis-elasticache.auth_token}@${module.redis-elasticache.primary_endpoint_address}:6379"
+  }
+}
