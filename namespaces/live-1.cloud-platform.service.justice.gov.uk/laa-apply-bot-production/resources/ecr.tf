77b310590dec9e6dfa5fde68c109214a42d97e7f
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2020-04-06 11:46:55 +0100

    Add ecr for the laa-apply-slack-bot (#2252)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-apply-bot-production/resources/ecr.tf
@@ -0,0 +1,23 @@
+module "laa_apply_bot_ecr_credentials" {
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.0"
+  repo_name = "laa-apply-bot"
+  team_name = "laa-apply-for-legal-aid"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "laa_apply_bot_ecr_credentials" {
+  metadata {
+    name      = "ecr-credentials-output"
+    namespace = "laa-apply-bot-production"
+  }
+
+  data = {
+    access_key_id     = module.laa_apply_bot_ecr_credentials.access_key_id
+    secret_access_key = module.laa_apply_bot_ecr_credentials.secret_access_key
+    repo_arn          = module.laa_apply_bot_ecr_credentials.repo_arn
+    repo_url          = module.laa_apply_bot_ecr_credentials.repo_url
+  }
+}
