30099fac55743f1c417f755aa35302207ee42f0d
David Salgado <david@digitalronin.com>
2020-04-14 12:25:58 +0100

    Update elasticache module to 4.1

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-management-production/resources/elasticache.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-management-production/resources/elasticache.tf
@@ -1,5 +1,5 @@
 module "ec-cluster-offender-management-allocation-manager" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.1"
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
   team_name              = var.team_name
@@ -8,6 +8,8 @@ module "ec-cluster-offender-management-allocation-manager" {
   node_type              = "cache.m4.large"
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure-support
+  engine_version         = "4.0.10"
+  parameter_group_name   = "default.redis4.0"
 
   providers = {
     aws = aws.london
