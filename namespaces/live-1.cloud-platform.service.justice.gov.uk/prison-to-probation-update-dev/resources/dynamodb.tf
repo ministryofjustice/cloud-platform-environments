6f15dcf26a363e0be2a8d3478732320b0a076d3a
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-04-17 11:22:45 +0100

    Dynomdb for prison to probation (#2343)
    * Add dynamodb for prison-to-probation service

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/dynamodb.tf
@@ -0,0 +1,31 @@
+
+module "message_dynamodb" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.0"
+
+  team_name              = var.team_name
+  application            = var.application
+  business-unit          = var.business-unit
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  is-production          = "false"
+
+  hash_key       = "id"
+  range_key      = "bookingId"
+  range_key_type = "N"
+  ttl_attribute  = "deleteBy"
+}
+
+resource "kubernetes_secret" "message_dynamodb" {
+  metadata {
+    name      = "message-dynamodb-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    table_name        = module.message_dynamodb.table_name
+    table_arn         = module.message_dynamodb.table_arn
+    access_key_id     = module.message_dynamodb.access_key_id
+    secret_access_key = module.message_dynamodb.secret_access_key
+  }
+}
+
