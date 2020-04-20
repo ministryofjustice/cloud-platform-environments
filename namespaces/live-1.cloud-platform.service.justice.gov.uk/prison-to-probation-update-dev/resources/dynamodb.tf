82448a290e14b20d04cab9f871e5e97b16eae1c4
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-04-20 14:34:31 +0100

    Dynomdb schedule for prison to probation (#2358)
    * Dynamodb table for use by scheduler

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/dynamodb.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/dynamodb.tf
@@ -29,3 +29,31 @@ resource "kubernetes_secret" "message_dynamodb" {
   }
 }
 
+module "schedule_dynamodb" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.0"
+
+  team_name              = var.team_name
+  application            = var.application
+  business-unit          = var.business-unit
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  is-production          = "false"
+
+  hash_key  = "_id"
+  range_key = "_id"
+}
+
+resource "kubernetes_secret" "schedule_dynamodb" {
+  metadata {
+    name      = "schedule-dynamodb-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    table_name        = module.schedule_dynamodb.table_name
+    table_arn         = module.schedule_dynamodb.table_arn
+    access_key_id     = module.schedule_dynamodb.access_key_id
+    secret_access_key = module.schedule_dynamodb.secret_access_key
+  }
+}
+
