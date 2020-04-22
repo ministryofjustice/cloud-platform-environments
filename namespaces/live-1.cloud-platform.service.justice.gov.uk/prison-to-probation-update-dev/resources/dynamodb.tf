b095e2852893a55b213789c57d3d6b46401c23cd
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-04-22 11:56:11 +0100

    Use release 3.1 and not specify range_key (#2375)
    * Use release 3.1.1 and not specify range_key

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/dynamodb.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/dynamodb.tf
@@ -1,6 +1,6 @@
 
 module "message_dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.1"
 
   team_name              = var.team_name
   application            = var.application
@@ -30,7 +30,7 @@ resource "kubernetes_secret" "message_dynamodb" {
 }
 
 module "schedule_dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.1"
 
   team_name              = var.team_name
   application            = var.application
@@ -39,8 +39,7 @@ module "schedule_dynamodb" {
   infrastructure-support = var.infrastructure-support
   is-production          = "false"
 
-  hash_key  = "_id"
-  range_key = "lockedBy"
+  hash_key = "_id"
 }
 
 resource "kubernetes_secret" "schedule_dynamodb" {
