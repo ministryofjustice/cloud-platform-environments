2f9810ef21267b932e1413b52c4b439b4898fc16
Imran Awan <imran.awan@gec.io>
2020-04-22 13:39:06 +0100

    Re-testing deployment of helm after updating it

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/imran-test/resources/k8s.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/imran-test/resources/k8s.tf
@@ -13,14 +13,13 @@ resource "kubernetes_namespace" "team-resources" {
     }
 
     annotations = {
-      "cloud-platform.justice.gov.uk/application"   = "helm-test"
-      "cloud-platform.justice.gov.uk/business-unit" = "cloud-platform"
-      "cloud-platform.justice.gov.uk/owner"         = "Cloud Platform: platforms@digital.justice.gov.uk"
-      "cloud-platform.justice.gov.uk/source-code"   = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
+      "cloud-platform.justice.gov.uk/application"                   = "helm-test"
+      "cloud-platform.justice.gov.uk/business-unit"                 = "cloud-platform"
+      "cloud-platform.justice.gov.uk/owner"                         = "Cloud Platform: platforms@digital.justice.gov.uk"
+      "cloud-platform.justice.gov.uk/source-code"                   = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
     }
   }
 }
-
 
 data "helm_repository" "cloud-platform" {
   name = "cloud-platform"
@@ -28,16 +27,16 @@ data "helm_repository" "cloud-platform" {
 }
 
 resource "helm_release" "team-resources" {
-  name       = "team-resources"
-  chart      = "team-resources"
-  repository = data.helm_repository.cloud-platform.metadata[0].name
-  namespace  = var.namespace
-  version    = local.team-resources
+  name          = "team-resources"
+  chart         = "team-resources"
+  repository    = data.helm_repository.cloud-platform.metadata[0].name
+  namespace     = var.namespace
+  version       = local.team-resources
 
   values = [
     <<EOF
         namespace: team-resources
-        rds.secret: "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+        rds.url: "${base64encode("postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}")}"
     EOF
     ,
   ]
