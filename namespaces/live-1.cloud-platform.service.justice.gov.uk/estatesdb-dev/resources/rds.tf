cd9a03039af9cc25eade2bd7fcbb129c99b5dfe7
MattStainsby <62069140+MattStainsby@users.noreply.github.com>
2020-04-23 14:07:46 +0100

    Add files via upload

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/estatesdb-dev/resources/rds.tf
@@ -0,0 +1,66 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+
+# IMP NOTE: Updating to module version 5.3, existing database password will be rotated.
+# Make sure you restart your pods which use this RDS secret to avoid any down time.
+
+module "programmeandperformance_rds" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
+  team_name            = "estatesprojects"
+  business-unit        = "Estates"
+  application          = "pdsdata"
+  is-production        = "false"
+
+  # enable performance insights
+  performance_insights_enabled = true  
+
+  # change the postgres version as you see fit.
+  db_engine_version      = "11"
+  environment-name       = "development"
+  infrastructure-support = "matthew.stainsby@digital.justice.gov.uk"
+
+  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
+  # Pick the one that defines the postgres version the best
+  rds_family = "postgres11"
+
+  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate). 
+  # You will need to specify "pending-reboot" here, as default is set to "immediate".
+  apply_method = "pending-reboot"
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "true"
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "programmeandperformance_rds" {
+  metadata {
+    name      = "programmeandperformance-rds-instance-output"
+    namespace = "estatesdb-dev"
+  }
+
+  data = {
+    url = "postgres://${module.programmeandperformance_rds.database_username}:${module.programmeandperformance_rds.database_password}@${module.programmeandperformance_rds.rds_instance_endpoint}/${module.programmeandperformance_rds.database_name}"
+  }
+}
