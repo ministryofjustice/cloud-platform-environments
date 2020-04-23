fc84f5463f28f3d6cebefc0ba32f64f2ba2b837f
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-04-23 17:07:56 +0100

    remove dl queue to create iam user

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-test/resources/messaging-queues.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-test/resources/messaging-queues.tf
@@ -9,12 +9,6 @@ module "create_link_queue" {
   encrypt_sqs_kms = var.encrypt_sqs_kms
   message_retention_seconds = var.message_retention_seconds
 
-  redrive_policy = <<EOF
-  {
-    "deadLetterTargetArn": "${module.create_link_queue_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
-  }
-  EOF
-
   providers = {
     aws = aws.london
   }
@@ -54,27 +48,9 @@ resource "aws_sqs_queue_policy" "create_link_queue_policy" {
 }
 
 
-module "create_link_queue_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
-
-  environment-name = var.environment_name
-  team_name = var.team_name
-  infrastructure-support = var.infrastructure_support
-  application = var.application
-  sqs_name = "create-link-queue-dl"
-  existing_user_name = module.create_link_queue.user_name
-  encrypt_sqs_kms = var.encrypt_sqs_kms
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-
-
 resource "kubernetes_secret" "create_link_queue" {
   metadata {
-    name = "ca-messaging-queues-output"
+    name = "cda-messaging-queues-output"
     namespace = var.namespace
   }
 
@@ -84,8 +60,5 @@ resource "kubernetes_secret" "create_link_queue" {
     sqs_url_link = module.create_link_queue.sqs_id
     sqs_arn_link = module.create_link_queue.sqs_arn
     sqs_name_link = module.create_link_queue.sqs_name
-    sqs_url_d_link = module.create_link_queue_dead_letter_queue.sqs_id
-    sqs_arn_d_link = module.create_link_queue_dead_letter_queue.sqs_arn
-    sqs_name_d_link = module.create_link_queue_dead_letter_queue.sqs_name
   }
 }
