2e7664667b7eb16dd40c638a32ae71fc530096fb
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-04-24 11:56:15 +0100

    Update all dynamo db modules to use latest 3.1.2
    This have a fix, not using team-name for creating a path in iam-user

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/dynamodb.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/dynamodb.tf
@@ -1,6 +1,6 @@
 
 module "message_dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.2"
 
   team_name              = var.team_name
   application            = var.application
@@ -28,7 +28,7 @@ resource "kubernetes_secret" "message_dynamodb" {
 }
 
 module "schedule_dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.1.2"
 
   team_name              = var.team_name
   application            = var.application
