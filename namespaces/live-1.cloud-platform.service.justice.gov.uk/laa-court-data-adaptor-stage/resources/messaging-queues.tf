f1228245c11a68ea195744f521058a74f015f16b
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-04-28 09:17:50 +0100

    create common platform initial queue(iamuser) in staging

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-stage/resources/messaging-queues.tf
@@ -0,0 +1,64 @@
+module "create_link_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name          = var.environment_name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure_support
+  application               = var.application
+  sqs_name                  = "create-link-queue"
+  encrypt_sqs_kms           = var.encrypt_sqs_kms
+  message_retention_seconds = var.message_retention_seconds
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+
+resource "aws_sqs_queue_policy" "create_link_queue_policy" {
+  queue_url = module.create_link_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.create_link_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Sid": "PublishPolicy",
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.create_link_queue.sqs_arn}",
+          "Action": "sqs:SendMessage"
+        },
+        {
+          "Sid": "ConsumePolicy",
+          "Effect": "Allow",
+          "Principal": {
+          "AWS": [
+            "484221692666"
+              ]
+          },
+          "Resource": "${module.create_link_queue.sqs_arn}",
+          "Action": "sqs:ReceiveMessage"
+        }
+      ]
+  }
+   EOF
+}
+
+
+resource "kubernetes_secret" "create_link_queue" {
+  metadata {
+    name      = "cda-messaging-queues-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.create_link_queue.access_key_id
+    secret_access_key = module.create_link_queue.secret_access_key
+    sqs_url_link      = module.create_link_queue.sqs_id
+    sqs_arn_link      = module.create_link_queue.sqs_arn
+    sqs_name_link     = module.create_link_queue.sqs_name
+  }
+}
