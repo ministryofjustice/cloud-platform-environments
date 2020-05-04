1123189bb41742cb4e77bb103d1715c4912cfe3d
Javid Ali <javid.ali@digital.justice.gov.uk>
2020-05-04 13:50:19 +0100

    Contact MOJ development - Add Redis resource

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/contact-moj-development/resources/elasticache.tf
@@ -0,0 +1,36 @@
+################################################################################
+# Contact MOJ (Correspondence Tool Public)
+# Application Elasticache for Redis (for Sidekiq background job processing)
+#################################################################################
+
+module "contact_moj_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.1"
+  cluster_name           = var.cluster_name
+  cluster_state_bucket   = var.cluster_state_bucket
+  team_name              = "correspondence"
+  business-unit          = "Central Digital"
+  application            = "contact-moj"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "staffservices@digital.justice.gov.uk"
+  engine_version         = "4.0.10"
+  parameter_group_name   = "default.redis4.0"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "contact_moj_elasticache_redis" {
+  metadata {
+    name      = "contact-moj-elasticache-redis-output"
+    namespace = "contact-moj-development"
+  }
+
+  data = {
+    primary_endpoint_address = module.contact_moj_elasticache_redis.primary_endpoint_address
+    auth_token               = module.contact_moj_elasticache_redis.auth_token
+    url                      = "rediss://appuser:${module.contact_moj_elasticache_redis.auth_token}@${module.contact_moj_elasticache_redis.primary_endpoint_address}:6379"
+  }
+}
+
