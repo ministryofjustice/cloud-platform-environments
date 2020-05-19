4054f9a67bce34bf73d01c4daa9bae14aa27a50a
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-05-19 14:40:03 +0100

    Added route 53 as per notes

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-dev/resources/route53.tf
@@ -0,0 +1,22 @@
+resource "aws_route53_zone" "example_team_route53_zone" {
+  name = "crime-portal-mirror-gateway.service.justice.gov.uk"
+
+  tags = {
+    application            = var.application
+    is-production          = var.is-production
+    environment-name       = var.environment-name
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure-support
+  }
+}
+
+resource "kubernetes_secret" "example_route53_zone_sec" {
+  metadata {
+    name      = "example-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id   = aws_route53_zone.example_team_route53_zone.zone_id
+  }
+}
