d8255b88137cdef65f7f11bd85c75499dd8c70dd
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2020-06-02 16:14:53 +0100

    Add pingdom checks to CDA for UAT and prod

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-uat/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-court-data-adaptor-uat" {
+  type                     = "http"
+  name                     = "LAA Court Data Adaptor - UAT"
+  host                     = "laa-court-data-adaptor-uat.apps.live-1.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 3
+  notifyagainevery         = 0
+  url                      = "/status"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_laa,application_court-data-adaptor,component_ping,isproduction_false,environment_uat"
+  probefilters             = "region:EU"
+  publicreport             = "true"
+}
