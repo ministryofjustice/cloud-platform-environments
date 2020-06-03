0ac45dc7a4435ae537c94408a9ccf6e38dc34c0a
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-06-03 13:49:57 +0100

    Add new manage HMPPS users service

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/manage-hmpps-auth-accounts-dev/resources/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# To be use in case the resources need to be created in London
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+# To be use in case the resources need to be created in Ireland
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
