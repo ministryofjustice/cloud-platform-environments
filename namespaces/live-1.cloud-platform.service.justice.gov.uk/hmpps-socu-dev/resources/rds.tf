ebea063e95a46225152cf56d57cba2836e37d040
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2020-06-10 14:03:48 +0100

    PAT-937: Creating DEV resources for new service hmpps-socu

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-socu-dev/resources/rds.tf
@@ -0,0 +1,97 @@
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+module "dps_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
+  cluster_name           = var.cluster_name
+  cluster_state_bucket   = var.cluster_state_bucket
+  team_name              = var.team_name
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  db_engine_version      = "11.4"
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  force_ssl              = "true"
+  rds_family             = "postgres11"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+data "aws_iam_policy_document" "hmpps_socu_dev_rds_to_s3_export_policy" {
+
+  statement {
+    sid = "AllowRdsExportUserToListS3Buckets"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    sid = "AllowRdsExportUserWriteToS3"
+    actions = [
+      "s3:PutObject*",
+      "s3:PutObjectAcl",
+      "s3:GetObject*",
+      "s3:DeleteObject*"
+    ]
+
+    resources = [
+      "${module.hmpps_socu_rds_to_s3_bucket.bucket_arn}",
+      "${module.hmpps_socu_rds_to_s3_bucket.bucket_arn}/*",
+      "arn:aws:s3:::mojap-land/hmpps/socu/",
+      "arn:aws:s3:::mojap-land/hmpps/socu/*"
+    ]
+  }
+}
+
+resource "aws_iam_user" "user" {
+  name = "hmpps-socu-rds-to-s3-snapshots-user-${random_id.id.hex}"
+  path = "/system/hmpps-socu-rds-to-s3-snapshots-user/"
+}
+
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name   = "hmpps-socu-rds-to-s3-snapshots-read-write"
+  policy = data.aws_iam_policy_document.hmpps_socu_dev_rds_to_s3_export_policy.json
+  user   = aws_iam_user.user.name
+}
+
+resource "kubernetes_secret" "dps_rds" {
+  metadata {
+    name      = "dps-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint       = module.dps_rds.rds_instance_endpoint
+    database_name               = module.dps_rds.database_name
+    database_username           = module.dps_rds.database_username
+    database_password           = module.dps_rds.database_password
+    rds_instance_address        = module.dps_rds.rds_instance_address
+    access_key_id               = module.dps_rds.access_key_id
+    secret_access_key           = module.dps_rds.secret_access_key
+    url                         = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
+    rds_to_s3_user_arn          = aws_iam_user.user.arn
+    rds_to_s3_access_key_id     = aws_iam_access_key.user.id
+    rds_to_s3_secret_access_key = aws_iam_access_key.user.secret
+  }
+}
+
