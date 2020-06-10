4e972198ab6eda43705e41bfdd6db341580de0b3
Ryan Brooks <ryan.brooks@digital.justice.gov.uk>
2020-06-10 14:47:43 +0100

    Change default region to eu-west-2 to resolve ElasticSearch service issue (#2537)
    * Change default region to eu-west-2 to resolve ElasticSearch service issues.
    See https://mojdt.slack.com/archives/C57UPMZLY/p1591794007430600\?thread_ts\=1591791526.429100\&cid\=C57UPMZLY
    
    * Fix region to Ireland, resolves build failure because the bucket already exits there (and we're changing the default region)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/s3.tf
@@ -8,6 +8,12 @@ module "drupal_content_storage" {
   is-production          = var.is-production
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure-support
+
+  # S3 was provisioned before we changed the default region to London
+  # so if this isn't set we get a 301 error when it tries to rebuild it
+  providers = {
+    aws = aws.ireland
+  }
 }
 
 resource "kubernetes_secret" "drupal_content_storage_secret" {
