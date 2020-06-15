3e5a4bad09f8c0cb6e8307fa266805fe216079d7
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-06-15 11:57:09 +0100

    Included route53.tf for domain
    Included certificates.yaml for the SSL certificate
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-prod/resources/variables.tf
@@ -0,0 +1,40 @@
+variable "domain" {
+  default = "crime-portal-mirror-gateway-probation.service.justice.gov.uk"
+}
+
+variable "application" {
+  default = "crime-portal-mirror-gateway"
+}
+
+variable "namespace" {
+  default = "crime-portal-mirror-gateway-prod"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Probation in court"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "probation-in-court-team@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "true"
+}
+
+variable "number_cache_clusters" {
+  default = "3"
+}
+
