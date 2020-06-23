5cc33c9a7a85329c077e2f60a1a6dab06b40aa91
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-06-23 11:49:17 +0100

    Update RDS module for check-my-diary-prod

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/check-my-diary-prod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/check-my-diary-prod/resources/rds.tf
@@ -10,7 +10,7 @@ variable "cluster_state_bucket" {
 }
 
 module "checkmydiary_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.3"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.6"
 
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
@@ -19,7 +19,7 @@ module "checkmydiary_rds" {
   is-production          = "true"
   environment-name       = "prod"
   infrastructure-support = "checkmydiary@digital.justice.gov.uk"
-  force_ssl              = "false"
+  db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
 
   providers = {
     aws = aws.london
