fd70bd41f32ced570b65826a978bbe3722b52d22
David Salgado <david@digitalronin.com>
2020-06-25 16:53:56 +0100

    Extract QUESTIONS to a shared file
    The list of questions for namespaces and gitops
    namespaces is identical, so it makes sense for 
    them to be defined in a file which both versions 
    of the script can refer to.

--- bin/create-namespace-files.rb
+++ bin/create-namespace-files.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require "yaml"
+require_relative "./namespace_questions.rb"
 
 # TODO: default
 # TODO: question N of M
@@ -117,56 +118,6 @@ def prompt(question)
 end
 
 ############################################################
-
-QUESTIONS = [
-  {
-    variable: "namespace",
-    description: "What is the name of your namespace? This should be of the form: <application>-<environment>. e.g. myapp-dev (lower-case letters and dashes only)",
-    validator: NamespaceNameValidator,
-  },
-
-  {
-    variable: "github_team",
-    description: "What is the name of your Github team? (this must be an exact match, or you will not have access to your namespace)",
-  },
-
-  {
-    variable: "business-unit",
-    description: "Which part of the MoJ is responsible for this service? (e.g HMPPS, Legal Aid Agency)",
-  },
-
-  {
-    variable: "is-production",
-    description: "Is this a production namespace? (please answer true or false)",
-    default: "false",
-    validator: TrueFalseValidator,
-  },
-
-  {
-    variable: "environment",
-    description: "What type of application environment is this namespace for? e.g. development, staging, production",
-  },
-
-  {
-    variable: "application",
-    description: "What is the name of your application/service? (e.g. Send money to a prisoner)",
-  },
-
-  {
-    variable: "owner",
-    description: "Which team in your organisation is responsible for this application? (e.g. Sentence Planning)",
-  },
-
-  {
-    variable: "contact_email",
-    description: "What is the email address for the team which owns the application? (this should not be a named individual's email address)",
-  },
-
-  {
-    variable: "source_code_url",
-    description: "What is the Github repository URL of the source code for this application?",
-  },
-]
 
 # For testing, we can supply on the command-line the name of a YAML
 # file containing the answers to the questions above. This enables us
