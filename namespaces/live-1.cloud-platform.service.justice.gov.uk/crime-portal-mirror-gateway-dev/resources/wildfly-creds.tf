ba4479dec74bb1286039b3277ec89eb291fc83da
John Evans <john.evans@digital.justice.gov.uk>
2020-06-29 14:33:33 +0100

    Update wildfly-creds.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-dev/resources/wildfly-creds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-dev/resources/wildfly-creds.tf
@@ -21,3 +21,18 @@ resource "kubernetes_secret" "pict_cpmg_wildfly_credentials" {
     user-password  = random_password.jmsuser_password.result
   }
 }
+
+
+resource "kubernetes_secret" "pict_cpmg_wildfly_credentials_for_offender_matcher" {
+  metadata {
+    name      = "pict_cpmg_wildfly_credentials_for_offender_matcher"
+    namespace = "court-probation-dev"
+  }
+
+  data = {
+    adminuser      = "adminuser"
+    owner-password = random_password.adminuser_password.result
+    jmsuser        = "jmsuser"
+    user-password  = random_password.jmsuser_password.result
+  }
+}
