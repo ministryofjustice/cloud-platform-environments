bb8bcb612c7a84100a8e127d603dfe866555ccd5
John Evans <john.evans@digital.justice.gov.uk>
2020-07-07 13:12:53 +0100

    🔧 Update NetworkPolicy for crime-portal-mirror-gateway-preprod and align crime-portal-mirror-gateway-prod with lower environments

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-prod/resources/rds.tf
@@ -0,0 +1,57 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+module "crime_portal_mirror_gateway_rds" {
+  source                       = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.6"
+  cluster_name                 = var.cluster_name
+  cluster_state_bucket         = var.cluster_state_bucket
+  team_name                    = "probation-in-court"
+  business-unit                = "hmpps"
+  application                  = "crime-portal-mirror-gateway"
+  is-production                = "false"
+  performance_insights_enabled = true
+  db_engine_version            = "10"
+  environment-name             = var.environment-name
+  infrastructure-support       = "probation-in-court-team@digital.justice.gov.uk"
+  rds_family                   = "postgres10"
+
+  allow_major_version_upgrade = "true"
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "1"
+      apply_method = "pending-reboot"
+    }
+  ]
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "crime_portal_mirror_gateway_rds" {
+  metadata {
+    name      = "crime-portal-mirror-gateway-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.crime_portal_mirror_gateway_rds.rds_instance_endpoint
+    database_name         = module.crime_portal_mirror_gateway_rds.database_name
+    database_username     = module.crime_portal_mirror_gateway_rds.database_username
+    database_password     = module.crime_portal_mirror_gateway_rds.database_password
+    rds_instance_address  = module.crime_portal_mirror_gateway_rds.rds_instance_address
+    access_key_id         = module.crime_portal_mirror_gateway_rds.access_key_id
+    secret_access_key     = module.crime_portal_mirror_gateway_rds.secret_access_key
+    database_url          = "jdbc:postgresql://${module.crime_portal_mirror_gateway_rds.rds_instance_endpoint}/${module.crime_portal_mirror_gateway_rds.database_name}?ssl=true&sslmode=require"
+  }
+}
