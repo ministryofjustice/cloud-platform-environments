18bbd33f3b71809f8c51bc4fdc1fdddcaf66f820
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2020-07-09 19:18:52 +0100

    Update replica to have similar settings as source db (#2742)
    * Update replica to have similar settings as source db
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/rds.tf
@@ -57,6 +57,16 @@ module "cla_backend_replica" {
   infrastructure-support = var.infrastructure-support
 
 
+  # Settings from current setup
+  db_instance_class    = "db.t2.medium"
+  db_allocated_storage = "100"
+
+  # change the postgres version as you see fit.
+  db_engine_version = "9.6"
+
+  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
+  # Pick the one that defines the postgres version the best
+  rds_family = "postgres9.6"
   # It is mandatory to set the below values to create read replica instance
 
   # Set the database_name of the source db
