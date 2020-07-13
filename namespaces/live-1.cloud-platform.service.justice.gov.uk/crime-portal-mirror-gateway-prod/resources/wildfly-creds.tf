5f88c88e4a5a5ba58c2addf4db9bef2b3e6da5f5
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-07-13 09:27:20 +0100

    :wrench: set production values

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-prod/resources/wildfly-creds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/crime-portal-mirror-gateway-prod/resources/wildfly-creds.tf
@@ -1,8 +1,3 @@
-resource "random_password" "adminuser_password" {
-  length  = 16
-  special = true
-}
-
 resource "random_password" "jmsuser_password" {
   length  = 16
   special = true
@@ -15,8 +10,6 @@ resource "kubernetes_secret" "pict_cpmg_wildfly_credentials" {
   }
 
   data = {
-    adminuser      = "adminuser"
-    owner-password = random_password.adminuser_password.result
     jmsuser        = "jmsuser"
     user-password  = random_password.jmsuser_password.result
   }
