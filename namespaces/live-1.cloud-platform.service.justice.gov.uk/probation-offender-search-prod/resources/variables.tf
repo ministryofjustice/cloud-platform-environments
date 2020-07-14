6c3137865f884f5f34c5175b0df282dc3e172588
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-07-14 08:29:10 +0100

    Part 1 = "rename" offender-search-prod/preprod to probation-offender-search-prod/preprod

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-prod/resources/variables.tf
@@ -0,0 +1,38 @@
+variable "application" {
+  default = "probation-offender-search-indexer"
+}
+
+variable "namespace" {
+  default = "probation-offender-search-prod"
+}
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "dps"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "true"
+}
+
