88261650cd5f918ec57dba8dd75e720f417ec2fe
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-07-14 11:52:21 +0100

    Add read-replica database

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
@@ -9,17 +9,38 @@ module "rds-instance" {
   is-production          = var.is-production
   infrastructure-support = var.infrastructure-support
   team_name              = var.team_name
-  db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
 
   providers = {
-    # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
   }
+
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "0"
+      apply_method = "immediate"
+    }
+  ]
+}
+
+provider "postgresql" {
+  host             = module.rds-instance.rds_instance_endpoint
+  database         = module.rds-instance.database_name
+  username         = module.rds-instance.database_username
+  password         = module.rds-instance.database_password
+  expected_version = "10.6"
+  sslmode          = "require"
+  connect_timeout  = 15
+}
+
+resource "random_password" "readonly-password" {
+  length  = 16
+  special = false
 }
 
 resource "kubernetes_secret" "rds-instance" {
   metadata {
-    name      = "rds-instance-hmpps-book-secure-move-api-preprod"
+    name      = "rds-instance-hmpps-book-secure-move-api-${var.environment-name}"
     namespace = var.namespace
   }
 
@@ -30,3 +51,49 @@ resource "kubernetes_secret" "rds-instance" {
   }
 }
 
+module "rds-read-replica" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.6"
+
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
+
+  application            = var.application
+  environment-name       = var.environment-name
+  is-production          = var.is-production
+  infrastructure-support = var.infrastructure-support
+  team_name              = var.team_name
+
+  db_name             = module.rds-instance.database_name
+  replicate_source_db = module.rds-instance.db_identifier
+
+  # Set to true for replica database. No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "0"
+      apply_method = "immediate"
+    }
+  ]
+}
+
+
+resource "kubernetes_secret" "rds-read-replica" {
+  metadata {
+    name      = "read-rds-instance-hmpps-book-secure-move-api-${var.environment-name}"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.rds-instance.access_key_id
+    secret_access_key = module.rds-instance.secret_access_key
+    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-read-replica.rds_instance_endpoint}/${module.rds-read-replica.database_name}"
+  }
+}
