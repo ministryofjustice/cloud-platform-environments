1ac1a504918e92a36ef52b0b6ca5b8ef69716b44
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-21 13:49:01 +0100

    Added the nginx ingress tf module

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
@@ -0,0 +1,8 @@
+
+
+module "ingress_controller" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller"
+
+  namespace     = "mogaal-test"
+  hostzone_name = "mogaal-test.cloud-platform.service.justice.gov.uk"
+}
