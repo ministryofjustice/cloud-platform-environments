680a25c63858f5850155b61263b96cdfa2b1fff3
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-21 12:34:43 +0100

    Fix duplicate variable

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/variables.tf
@@ -38,11 +38,6 @@ variable "is-production" {
   default = "false"
 }
 
-variable "is-production" {
-  description = "Team slack channel to use if we need to contact your team"
-  default     = "#cloud-platform"
-}
-
 variable "domain" {
   description = "Domain to be created"
   default     = "mogaal-test.cloud-platform.service.justice.gov.uk"
