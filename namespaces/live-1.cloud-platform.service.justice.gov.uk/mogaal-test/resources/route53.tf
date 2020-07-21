90d3866c018488ca805531cffadcaa719e7bda7c
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-21 12:44:04 +0100

    Fix duplicate variable

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/route53.tf
@@ -1,24 +1,24 @@
-resource "aws_route53_zone" "cccd_route53_zone" {
+resource "aws_route53_zone" "mogaal_test_route53_zone" {
   name = var.domain
 
   tags = {
     business-unit          = var.business-unit
     application            = var.application
     is-production          = var.is-production
-    environment-name       = var.environment-name
-    owner                  = var.team_name
+    environment-name       = var.environment
+    owner                  = var.team-name
     infrastructure-support = var.infrastructure-support
   }
 }
 
-resource "kubernetes_secret" "cccd_route53_zone_sec" {
+resource "kubernetes_secret" "mogaal_test_route53_zone_sec" {
   metadata {
     name      = "route53-zone-output"
     namespace = var.namespace
   }
 
   data = {
-    zone_id = aws_route53_zone.cccd_route53_zone.zone_id
+    zone_id = aws_route53_zone.mogaal_test_route53_zone.zone_id
   }
 }
 
