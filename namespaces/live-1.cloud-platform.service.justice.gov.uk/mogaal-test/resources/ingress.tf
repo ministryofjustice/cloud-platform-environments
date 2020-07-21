f446ce612835a10288ee3a2f813503924f69a25f
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-21 16:30:11 +0100

    Removed ingress controller for test

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
+++
@@ -1,8 +0,0 @@
-
-
-module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller"
-
-  namespace     = "mogaal-test"
-  hostzone_name = "mogaal-test.cloud-platform.service.justice.gov.uk"
-}
