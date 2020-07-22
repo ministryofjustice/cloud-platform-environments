0127693c92df05e5f090e5acbed8d959b104c641
Ryan Brooks <ryan@slatehorse.com>
2020-07-22 11:28:27 +0100

    add route53 zone for Prisoner Content Hub content-hub.prisoner.service.justice.gov.uk

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_zone" "content_hub_route53_zone" {
+  name = "content-hub.prisoner.service.justice.gov.uk"
+
+  tags = {
+    business-unit          = var.business-unit
+    application            = var.application
+    is-production          = var.is-production
+    environment-name       = var.environment-name
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure-support
+  }
+}
+
+resource "kubernetes_secret" "content_hub_route53_zone_secret" {
+  metadata {
+    name      = "${var.application}-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id   = aws_route53_zone.content_hub_route53_zone.zone_id
+  }
+}
