c85a5dfa4ac513bb3ad3b9615af6fa03f1fe01de
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-23 13:03:50 +0100

    Updated to the most recent module

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
@@ -1,6 +1,6 @@
 
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=main"
 
-  hostzone_name = "mogaal-test.cloud-platform.service.justice.gov.uk"
+  namespace = "mogaal-test"
 }
