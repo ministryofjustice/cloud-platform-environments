8eb0454de4ba87c22a62e40599bfa46af91010b0
Alejandro Garrido Mota <garridomota@gmail.com>
2020-07-23 12:19:33 +0100

    Revert network policy to its previously state

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
@@ -2,6 +2,5 @@
 module "ingress_controller" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.1"
 
-  namespace     = "mogaal-test"
   hostzone_name = "mogaal-test.cloud-platform.service.justice.gov.uk"
 }
