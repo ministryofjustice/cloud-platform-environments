91efa5672ca17a6cc86d1f9b432bcee3fd115979
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-07-30 18:28:58 +0100

    Create CompletedJobDeleter
    This script will delete completed jobs which are:
    *  not part of a Cronjobs
    * and job not set with ttlSecondsAfterFinished

---
+++ lib/cp_env/completed_job_deleter.rb
@@ -0,0 +1,53 @@
+class CpEnv
+  # This script will delete completed jobs which are:
+  #
+  #   * not part of a Cronjobs
+  #   * and job not set with ttlSecondsAfterFinished
+  #
+  # These completed jobs pods are taking up pod count on each node 
+  # creating too many pods per node get alerts (limit set by KOPS is 100 per node)
+  #
+  # To see a list of jobs that *would be* deleted, without actually
+  # deleting any, pass `dry_run: true` to the `initialize` method.
+  class CompletedJobDeleter
+    attr_reader :executor, :dry_run
+
+    def initialize(args = {})
+      @executor = args.fetch(:executor) { Executor.new }
+      @dry_run = args.fetch(:dry_run, false)
+    end
+
+    def delete
+      jobs_to_delete = get_completed_jobs
+        .reject { |job| job.fetch("metadata").key?("ownerReferences") }
+        .reject { |job| job.fetch("spec").key?("ttlSecondsAfterFinished") }
+
+      jobs_to_delete.map { |job| delete_job(job) }
+    end
+
+    private
+
+    def get_jobs
+      stdout, _stderr, _status = executor.execute("kubectl get jobs --all-namespaces -o json", silent: true)
+      JSON.parse(stdout).fetch("items")
+    end
+
+    def get_completed_jobs
+      get_jobs.find_all { |job| completed_jobs(job) }
+    end
+
+    def completed_jobs(job)
+      job.dig("status", "succeeded") == 1
+    end
+
+    def delete_job(job)
+      namespace = job.dig("metadata", "namespace")
+      name = job.dig("metadata", "name")
+      if dry_run
+        log("blue", "dry run: would delete job #{name} from namespace #{namespace}")
+      else
+        executor.execute("kubectl -n #{namespace} delete job #{name}")
+      end
+    end
+  end
+end
