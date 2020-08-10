89e5f94ee617927da6b7e2d5464d34b744a8e357
Alejandro Garrido Mota <garridomota@gmail.com>
2020-08-10 12:03:36 +0100

    Fix module version for custom ingress terraform module

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
@@ -1,6 +1,6 @@
 
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=main"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.2"
 
   namespace = "mogaal-test"
 }
