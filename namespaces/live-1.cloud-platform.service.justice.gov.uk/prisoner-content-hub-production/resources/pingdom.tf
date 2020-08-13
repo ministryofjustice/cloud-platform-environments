c57ce31cfcb6eebffcfee038520273da8860c45a
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-08-13 13:51:13 +0100

    Split the pingdom_check resource in to two sets
    To fix the error "Error: 403 Forbidden: No free uptime checks available"
    
    Adding 4 host names together causing this error, so split them in to 2.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/pingdom.tf
@@ -1,12 +1,10 @@
 provider "pingdom" {
 }
 
-resource "pingdom_check" "prisoner-content-hub-production-checks" {
+resource "pingdom_check" "prisoner-content-hub-production-checks-1" {
   for_each = {
     drupal-cms  = "manage.content-hub.prisoner.service.justice.gov.uk",
-    berwyn      = "berwyn.content-hub.prisoner.service.justice.gov.uk",
-    cookhamwood = "cookhamwood.content-hub.prisoner.service.justice.gov.uk",
-    wayland     = "wayland.content-hub.prisoner.service.justice.gov.uk",
+    berwyn      = "berwyn.content-hub.prisoner.service.justice.gov.uk"
   }
 
   type                     = "http"
@@ -28,3 +26,30 @@ resource "pingdom_check" "prisoner-content-hub-production-checks" {
     106310, # #pfs_alerts channel
   ]
 }
+
+resource "pingdom_check" "prisoner-content-hub-production-checks-2" {
+  for_each = {
+    cookhamwood = "cookhamwood.content-hub.prisoner.service.justice.gov.uk",
+    wayland     = "wayland.content-hub.prisoner.service.justice.gov.uk"
+  }
+
+  type                     = "http"
+  name                     = "Prisoner Content Hub Production ${each.key}"
+  host                     = each.value
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business-unit},application_${var.application},component_healthcheck,isproduction_${var.is-production},environment_${var.environment-name},infrastructuresupport_${var.team_name}"
+  probefilters             = "region:EU"
+  publicreport             = "true"
+
+  integrationids = [
+    106307, # #pfs_dev channel
+    106310, # #pfs_alerts channel
+  ]
+}
+
