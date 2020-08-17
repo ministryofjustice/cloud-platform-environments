cc3bb7accef567fd7457554e4b1e69f184bc36cc
David Salgado <david@digitalronin.com>
2020-08-17 15:05:00 +0100

    Use the snake-case variables from variables.tf
    We're moving to snake-case as a standard for
    terraform variable names, so we should use the
    snake-case versions, wherever possible.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/david-test/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/david-test/resources/s3.tf
@@ -2,12 +2,12 @@ module "s3_bucket" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.3"
   acl    = "private"
 
-  team_name              = var.team-name
-  business-unit          = var.business-unit
+  team_name              = var.team_name
+  business-unit          = var.business_unit
   application            = var.application
-  is-production          = var.is-production
-  infrastructure-support = var.infrastructure-support
+  is-production          = var.is_production
   environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
 
   providers = {
     aws = aws.london
