d972197fdc6cbf5a101920847d6376c810a4d29b
David Salgado <david@digitalronin.com>
2020-08-19 17:05:17 +0100

    Create dstest namespace for Helloworld walkthrough
    Create a namespace with an ECR.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/dstest/resources/variables.tf
@@ -0,0 +1,73 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "David Test"
+}
+
+variable "namespace" {
+  default = "dstest"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "Platforms"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "sentencing-and-oasys-development-team"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "david.salgado@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "cloud-platform"
+}
+
+# DEPRECATED: snake-case variables are the default. The definitions below
+# have been left in place until all code has been updated to use snake-case
+# variable names.
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "Platforms"
+}
+
+variable "team-name" {
+  description = "The name of your development team"
+  default     = "sentencing-and-oasys-development-team"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "david.salgado@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "slack-channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "cloud-platform"
+}
+
