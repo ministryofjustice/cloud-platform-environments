4417e2f562591f62a161f082d247ebef6ac5952e
David Salgado <david@digitalronin.com>
2020-08-26 16:22:47 +0100

    Pass is_production to ingress-controller module

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/ingress.tf
@@ -2,4 +2,5 @@ module "ingress_controller" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.6"
 
   namespace = "hmpps-book-secure-move-api-production"
+  is_production = var.is_production
 }
