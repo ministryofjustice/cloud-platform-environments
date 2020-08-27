c6ec8d1c1392ac45326390a65cce2e4e0ba61997
Peter Phillips <peter@greenthistle.co.uk>
2020-08-27 09:31:24 +0100

    DT-1086: 🐛 Fix is-production flag

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-prod/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-prod/resources/ingress.tf
@@ -2,5 +2,5 @@ module "ingress_controller" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.7"
 
   namespace     = "prison-estate-prod"
-  is_production = var.is_production
+  is_production = var.is-production
 }
