7417c8289b19e8b619104fba148c4964a5d54cd4
Peter Phillips <peter@greenthistle.co.uk>
2020-08-27 08:02:34 +0100

    DT-1086: 🔧 Add decided ingresses for prison estate and prison to nhs update

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-nhs-update-prod/resources/ingress.tf
@@ -0,0 +1,6 @@
+module "ingress_controller" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.7"
+
+  namespace     = "prison-to-nhs-update-prod"
+  is_production = var.is_production
+}
