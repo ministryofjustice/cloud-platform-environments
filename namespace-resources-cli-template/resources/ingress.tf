6f04ecdceda6627e8cd79b7e3626da74300d5196
David Read <david.read@digital.justice.gov.uk>
2020-08-27 14:02:37 +0000

    Use ingress tf module v0.0.7
    We should use the latest version https://github.com/ministryofjustice/cloud-platform-environments/pull/2991#discussion_r478330466

--- namespace-resources-cli-template/resources/ingress.tf
+++ namespace-resources-cli-template/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.7"
 
   namespace = "{{ .Namespace }}"
 }
