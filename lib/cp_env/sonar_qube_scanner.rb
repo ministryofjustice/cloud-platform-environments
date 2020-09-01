199f78e28de7e6ad1f181e7ecf0950460b404fc6
Imran Awan <imran.awan@digital.justice.gov.uk>
2020-09-01 16:01:49 +0100

    added logic so if a private repo is cloned then it is skipped. Also includes invalid repos

--- lib/cp_env/sonar_qube_scanner.rb
+++ lib/cp_env/sonar_qube_scanner.rb
@@ -22,20 +22,10 @@ class CpEnv
     def get_repos
       repo_full_url = get_namespaces.map {|namespace| namespace.dig('metadata','annotations','cloud-platform.justice.gov.uk/source-code') }.compact.uniq
     end
-  
-    def get_repos_ministryofjustice_org
-      repo_urls_ministry_org_only = []
-      get_repos.each do |each_repo_url|
-        split_repo_url = each_repo_url.split('/')
-        if split_repo_url[3] == 'ministryofjustice' # only if the repo is in the ministryofjustice org then add it to the new array.
-          repo_urls_ministry_org_only.push(each_repo_url)
-        end
-      end
-      return repo_urls_ministry_org_only
-    end
+
 
     def scan
-      get_repos_ministryofjustice_org.each do |each_repo_url|
+      get_repos.each do |each_repo_url|
         each_repo_url_list = each_repo_url.split('/')
         repo_name = ""
         repo_name = each_repo_url_list.last
@@ -47,14 +37,16 @@ class CpEnv
           if repo_name_with_git_suffix[1] != 'git'
             next
           end
-        end
+        end 
         puts 'REPO NAME: '+repo_name
-        if File.exists?(repo_name)
-          puts repo_name+': This repo is already scanned'
+        repo_url_with_token = 'https://'+ENV["GITHUB_TOKEN_PUB_ONLY"]+'@github.com/ministryofjustice/'+repo_name+'.git'
+        system("(git clone #{each_repo_url})")
+        if $?.exitstatus != 0 # If exit code is anything other than 0 then the reop is invalid, private or already cloned. Skip to next.
+          pp 'INVALID REPO'
+          pp 'The repo '+repo_name+' is private, invalid or already cloned'
         else
-          #repo_url_with_token = 'https://'+ENV["GITHUB_TOKEN"]+'@github.com/ministryofjustice/'+repo_name+'.git'
-          system("(git clone #{each_repo_url})")
           system("(cd #{repo_name})")
+          puts 'SCANNING REPO: '+repo_name
           system("(sonar-scanner -Dsonar.projectKey=#{repo_name} -Dsonar.sources=. -Dsonar.host.url=#{ENV["SONARQUBE_HOST_URL"]} -Dsonar.login=#{ENV["SONARQUBE_TOKEN"]})")
           system("(cd ..)")
           system("(rm -rf #{repo_name})") 
