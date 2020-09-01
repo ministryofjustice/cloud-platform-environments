3fe484fd7044b175b0504ba89e8f1ddb35f149d9
David Salgado <david@digitalronin.com>
2020-09-01 12:09:10 +0100

    Update ingress controller modules to v0.0.8
    ```
    ag -l
    "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller.ref=0.0.7"
    | xargs sed -i '' 's/ref=0.0.7/ref=0.0.8/'
    ```

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.7"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.8"
 
   namespace     = "pathfinder-dev"
   is_production = var.is_production
--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-prod/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-estate-prod/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.7"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.8"
 
   namespace     = "prison-estate-prod"
   is_production = var.is-production
