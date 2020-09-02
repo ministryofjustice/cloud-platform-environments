f839adb3668895cd14ac9bb70c707dfc6b3f88cd
David Salgado <david@digitalronin.com>
2020-09-02 10:04:39 +0100

    Refactor and make more ruby-ish
    co-authored with @imran-awan1

--- lib/cp_env/sonar_qube_scanner.rb
+++ lib/cp_env/sonar_qube_scanner.rb
@@ -1,55 +1,49 @@
 class CpEnv
   class SonarQubeScanner
-    attr_reader :executor, :dry_run
+    attr_reader :executor, :dry_run, :sonar_qube_host, :sonar_qube_token
+
+    REPO_REGEXP = %r[^https://github.com/[^\s,]+$]
 
     def initialize(args = {})
+      @sonar_qube_host = args.fetch(:sonar_qube_host, ENV.fetch("SONARQUBE_HOST_URL"))
+      @sonar_qube_token = args.fetch(:sonar_qube_token, ENV.fetch("SONARQUBE_TOKEN"))
       @executor = args.fetch(:executor) { Executor.new }
       @dry_run = args.fetch(:dry_run, false)
     end
 
     def scan_all_repos
-      scan
+      repo_urls.each do |url|
+        if checkout_repo(url)
+          dir = url.split("/").last.sub(/\..*$/, "")
+          scan_directory(dir)
+          system("rm -rf #{dir}")
+        end
+      end
     end
 
     private
 
+    def repo_urls
+      get_namespaces
+        .map { |namespace| namespace.dig("metadata", "annotations", "cloud-platform.justice.gov.uk/source-code") }
+        .compact
+        .uniq
+        .find_all { |url| REPO_REGEXP.match?(url) }
+    end
+
     def get_namespaces
       stdout, _stderr, _status = executor.execute("kubectl get ns -o json", silent: true)
-      namespaces = JSON.parse(stdout).fetch("items")
+      JSON.parse(stdout).fetch("items")
     end
 
-    def get_repos
-      repo_full_url = get_namespaces.map { |namespace| namespace.dig("metadata", "annotations", "cloud-platform.justice.gov.uk/source-code") }.compact.uniq
+    def checkout_repo(url)
+      system("GIT_TERMINAL_PROMPT=0 git clone #{url}")
+      $?.success? # If the command failed then the reop is invalid, private or already cloned. Skip to next.
     end
 
-    def scan
-      get_repos.each do |each_repo_url|
-        each_repo_url_list = each_repo_url.split("/")
-        repo_name = ""
-        repo_name = each_repo_url_list.last
-        # some repo urls have .git at the end. So we need to strip it out if it exists.
-        repo_name_with_git_suffix = repo_name.split(".")
-        if repo_name_with_git_suffix.count > 1 # then this repo url has .file_format (e.g .git, .tf etc) suffixed in the end
-          repo_name = repo_name_with_git_suffix.first
-          # If the suffix is anything other than .git then skip to next iteration as it is an invalid url
-          if repo_name_with_git_suffix[1] != "git"
-            next
-          end
-        end
-        puts "REPO NAME: " + repo_name
-        repo_url_with_token = "https://" + ENV["GITHUB_TOKEN_PUB_ONLY"] + "@github.com/ministryofjustice/" + repo_name + ".git"
-        system("(git clone #{repo_url_with_token})")
-        if $?.exitstatus != 0 # If exit code is anything other than 0 then the reop is invalid, private or already cloned. Skip to next.
-          pp "INVALID REPO"
-          pp "The repo " + repo_name + " is private, invalid or already cloned"
-        else
-          puts "SCANNING REPO: " + repo_name
-          Dir.chdir(repo_name) do
-            system("(sonar-scanner -Dsonar.projectKey=#{repo_name} -Dsonar.sources=. -Dsonar.host.url=#{ENV["SONARQUBE_HOST_URL"]} -Dsonar.login=#{ENV["SONARQUBE_TOKEN"]})")
-          end
-          system("(rm -rf #{repo_name})")
-        end
-      end
+    def scan_directory(dir)
+      puts "SCANNING REPO: " + dir
+      system("(cd #{dir}; sonar-scanner -Dsonar.projectKey=#{dir} -Dsonar.sources=. -Dsonar.host.url=#{sonar_qube_host} -Dsonar.login=#{sonar_qube_token})")
     end
   end
 end
