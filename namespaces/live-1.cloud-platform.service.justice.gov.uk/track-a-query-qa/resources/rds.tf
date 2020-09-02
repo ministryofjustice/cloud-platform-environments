9045aa292d0fe593300e01f3e8a1a521515c5b88
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-09-02 12:22:33 +0100

    CT-3062 Add keys to RDS secret on Track a Query QA

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/rds.tf
@@ -46,7 +46,11 @@ resource "kubernetes_secret" "track_a_query_rds" {
     database_username     = module.track_a_query_rds.database_username
     database_password     = module.track_a_query_rds.database_password
     rds_instance_address  = module.track_a_query_rds.rds_instance_address
-    url                   = "postgres://${module.track_a_query_rds.database_username}:${module.track_a_query_rds.database_password}@${module.track_a_query_rds.rds_instance_endpoint}/${module.track_a_query_rds.database_name}"
+
+    access_key_id     = module.track_a_query_rds.access_key_id
+    secret_access_key = module.track_a_query_rds.secret_access_key
+
+    url = "postgres://${module.track_a_query_rds.database_username}:${module.track_a_query_rds.database_password}@${module.track_a_query_rds.rds_instance_endpoint}/${module.track_a_query_rds.database_name}"
   }
 }
 
