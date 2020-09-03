95ff29fbe46d2e70ce85f0a6cf7d276fc6385517
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-09-03 14:28:10 +0100

    Update mogaal-test to use latest nginx module

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mogaal-test/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=upgrade-teams-nginx"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.0"
 
   namespace = "mogaal-test"
 }
