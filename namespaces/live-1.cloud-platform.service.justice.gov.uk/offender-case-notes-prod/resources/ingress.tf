3eca62a5e0f937172db1c54f2605385047675736
Peter Phillips <peter@greenthistle.co.uk>
2020-09-03 09:44:21 +0100

    DT-1086: ⬆️ Bump ingress controller module version

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-case-notes-prod/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-case-notes-prod/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.9"
 
   namespace     = "offender-case-notes-prod"
   is_production = var.is-production
