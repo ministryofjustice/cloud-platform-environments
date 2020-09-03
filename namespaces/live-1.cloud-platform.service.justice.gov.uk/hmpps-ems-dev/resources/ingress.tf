7bcc2d552c2fe872a881393746910352fed3fb17
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-09-03 15:02:28 +0100

    EMB-79 Updating ingress version

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-dev/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-dev/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.8"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.0"
 
   namespace     = var.namespace
   is_production = var.is_production
