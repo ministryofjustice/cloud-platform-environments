baffdff0c2cc50b6ba7f0dc4ad939a1d92138cff
David Salgado <david@digitalronin.com>
2020-09-04 14:32:45 +0100

    Update ingress modules to v0.1.0
    ```
    ag -l "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller.ref=0.0.9" | xargs sed -i '' 's/ref=0.0.9/ref=0.1.0/'
    ```

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.0.9"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.0"
 
   namespace     = "hmpps-book-secure-move-api-production"
   is_production = var.is_production
