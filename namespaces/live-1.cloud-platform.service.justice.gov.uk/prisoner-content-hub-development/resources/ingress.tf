5a723329373910254f7e7b5080927bc7a85a14c7
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2020-09-07 13:48:11 +0100

    Disabale validation webhook for non prod namespace
    This is causing teams to apply ingress to thier namespaces, i
    f one of the validation webhook failed

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.1"
 
   namespace = "prisoner-content-hub-development"
 }
