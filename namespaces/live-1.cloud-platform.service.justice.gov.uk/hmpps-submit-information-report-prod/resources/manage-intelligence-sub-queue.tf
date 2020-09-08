b225334c5a0e4e08dfaf602480d1507fdba31ce3
mattwhitfield <whitfield.mj@gmail.com>
2020-09-08 10:04:54 +0100

    added queue to hmpps-submit-information-report-prod

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-submit-information-report-prod/resources/manage-intelligence-sub-queue.tf
@@ -0,0 +1,67 @@
+module "information_report_submissions_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "information_report_submissions_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.information_report_submissions_dead_letter_queue.sqs_arn}","maxReceiveCount": 1
+  }
+  EOF
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "information_report_submissions_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "information_report_submissions_queue_dl"
+  encrypt_sqs_kms        = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "information_report_submissions_queue" {
+  metadata {
+    name      = "information-report-submission-sqs"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.information_report_submissions_queue.access_key_id
+    secret_access_key = module.information_report_submissions_queue.secret_access_key
+    sqs_dc_req_url    = module.information_report_submissions_queue.sqs_id
+    sqs_dc_req_arn    = module.information_report_submissions_queue.sqs_arn
+    sqs_dc_req_name   = module.information_report_submissions_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "information_report_submissions_dead_letter_queue" {
+  metadata {
+    name      = "information-report-submission-sqs-dl"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.information_report_submissions_dead_letter_queue.access_key_id
+    secret_access_key = module.information_report_submissions_dead_letter_queue.secret_access_key
+    sqs_dc_req_url    = module.information_report_submissions_dead_letter_queue.sqs_id
+    sqs_dc_req_arn    = module.information_report_submissions_dead_letter_queue.sqs_arn
+    sqs_dc_req_name   = module.information_report_submissions_dead_letter_queue.sqs_name
+  }
+}
