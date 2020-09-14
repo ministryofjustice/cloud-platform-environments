c7123c3443d1811a2673440214b0d826d29ffc49
David Salgado <david@digitalronin.com>
2020-09-14 10:07:06 +0100

    Non-prod ingress-controller module upgrade
    This change upgrades the ingress-controller module
    for non-production namespaces which were on v0.1.1
    to v0.1.2
    
    This change should not cause terraform to make any
    changes.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-dev/resources/ingress.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-data-compliance-dev/resources/ingress.tf
@@ -1,5 +1,5 @@
 module "ingress_controller" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-teams-ingress-controller?ref=0.1.2"
 
   namespace = "prison-data-compliance-dev"
 }
