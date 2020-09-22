23c8c2d1d19118f45adc4a9d9d57c20267f49e4b
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-09-22 11:52:41 +0100

    Remove replica db from initial resource creation
    To add it after the primary is created

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
@@ -40,43 +40,6 @@ module "cla_backend_rds" {
   }
 }
 
-module "cla_backend_replica" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.6"
-
-  cluster_name           = var.cluster_name
-  cluster_state_bucket   = var.cluster_state_bucket
-  team_name              = var.team_name
-  business-unit          = var.business-unit
-  application            = var.application
-  is-production          = var.is-production
-  environment-name       = var.environment-name
-  infrastructure-support = var.infrastructure-support
-
-
-  # It is mandatory to set the below values to create read replica instance
-
-  # Set the database_name of the source db
-  db_name = module.cla_backend_rds.database_name
-
-  # Set the db_identifier of the source db
-  replicate_source_db = module.cla_backend_rds.db_identifier
-
-  # Set to true. No backups or snapshots are created for read replica
-  skip_final_snapshot        = "true"
-  db_backup_retention_period = 0
-
-  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
-  # Pick the one that defines the postgres version the best
-  rds_family        = "postgres9.6"
-  db_engine_version = "9.6"
-
-  providers = {
-    # Can be either "aws.london" or "aws.ireland"
-    aws = aws.london
-  }
-
-}
-
 resource "kubernetes_secret" "cla_backend_rds" {
   metadata {
     name      = "database"
@@ -90,8 +53,6 @@ resource "kubernetes_secret" "cla_backend_rds" {
     name             = module.cla_backend_rds.database_name
     user             = module.cla_backend_rds.database_username
     password         = module.cla_backend_rds.database_password
-    replica_host     = module.cla_backend_replica.rds_instance_address
-    replica_endpoint = module.cla_backend_replica.rds_instance_endpoint
   }
 
 }
