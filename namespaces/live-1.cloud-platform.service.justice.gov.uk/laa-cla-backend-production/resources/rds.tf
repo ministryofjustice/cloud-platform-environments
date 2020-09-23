5d1dedc74626a5e3200420b0bda53fbc5d403347
said-moj <said.shire@digital.justice.gov.uk>
2020-09-23 06:54:19 +0100

    Add max_standby_archive_delay and max_standby_streaming_delay parameters to the replica

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/rds.tf
@@ -81,6 +81,18 @@ module "cla_backend_replica" {
   skip_final_snapshot        = "true"
   db_backup_retention_period = 0
 
+  parameter {
+    name  = "max_standby_archive_delay"
+    value = "3600000"
+  }
+  parameter {
+    name  = "max_standby_streaming_delay"
+    value = "3600000"
+  }
+  parameter {
+    name  = "log_statement"
+    value = "all"
+  }
   providers = {
     # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
