d313f57861fff8897907cfb21e5bce3683a113f1
David Salgado <david@digitalronin.com>
2020-09-30 15:38:39 +0100

    Update RDS module to 5.7

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-staging/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-staging/resources/rds.tf
@@ -1,5 +1,5 @@
 module "hmcts-complaints-adapter-rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.6"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.7"
 
   cluster_name               = var.cluster_name
   cluster_state_bucket       = var.cluster_state_bucket
@@ -7,6 +7,7 @@ module "hmcts-complaints-adapter-rds-instance" {
   application                = "hmcts-complaints-formbuilder-adapter"
   environment-name           = var.environment-name
   is-production              = var.is-production
+  namespace                  = var.namespace
   infrastructure-support     = var.infrastructure-support
   team_name                  = var.team_name
 
