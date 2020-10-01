30c58edb61a697b2f219ad587c2f42453e4885ce
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-01 16:25:52 +0100

    Added tags to IAM user

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/iam.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/iam.tf
@@ -15,6 +15,14 @@ data "aws_iam_policy_document" "grafana_platform" {
 resource "aws_iam_user" "grafana_platform" {
   name = "hmpps-ems-prod-grafana-platform"
   path = "/cloud-platform/hmpps-ems-prod/"
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
 }
 
 resource "aws_iam_user_policy" "grafana_platform" {
