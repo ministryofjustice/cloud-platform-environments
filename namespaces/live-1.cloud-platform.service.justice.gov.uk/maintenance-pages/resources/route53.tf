25146ffb314ea724f53da47f4935a48aaa64f73f
mtrbls <mourad.trabelsi@digital.justice.gov.uk>
2020-10-05 08:31:58 +0100

    remove tribunals.gov.uk as it is no longer necessary

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/maintenance-pages/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/maintenance-pages/resources/route53.tf
@@ -1,17 +1,3 @@
-resource "aws_route53_zone" "tribunals" {
-
-  name = "tribunals.gov.uk"
-
-  tags = {
-    business-unit          = "webops"
-    application            = "maintenance-pages"
-    is-production          = "true"
-    environment-name       = "production"
-    owner                  = "webops"
-    infrastructure-support = "platforms@digital.service.justice.gov.uk"
-  }
-}
-
 resource "aws_route53_zone" "legacy" {
   count = length(var.domains)
 
