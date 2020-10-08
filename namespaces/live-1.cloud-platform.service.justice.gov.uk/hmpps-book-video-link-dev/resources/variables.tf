1b095fffb01415463ec2a6909217b9ac83cc016d
G <gurnank@outlook.com>
2020-10-08 17:21:31 +0100

    DCS-644 creating new namespace for Book Video Link

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-video-link-dev/resources/variables.tf
@@ -0,0 +1,48 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Book video link"
+}
+
+variable "namespace" {
+  default = "hmpps-book-video-link-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "dps-shared"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "dps-shared"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
