6a867effa34c5e2b01724263ca3146348dcfb421
brenetic <brendan.butler@digital.justice.gov.uk>
2020-10-12 10:43:33 +0100

    Create formbuilder-saas namespace and Metadata API DB
    We are creating a new Metadata API application that will be backed by a
    postgres database.
    
    At the same time we need to create a new namespace
    `formbuilder-saas-test` which the api will live in along with some
    future apps.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/variables.tf
@@ -0,0 +1,30 @@
+variable "environment_name" {
+  default = "test"
+}
+
+variable "team_name" {
+  default = "formbuilder"
+}
+
+variable "is_production" {
+  default = "true"
+}
+
+variable "db_backup_retention_period" {
+  default = "2"
+}
+
+variable "infrastructure_support" {
+  default = "Form Builder form-builder-developers@digital.justice.gov.uk"
+}
+
+// The following two variables are provided at runtime by the pipeline.
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "namespace" {
+  default = "formbuilder-saas-test"
+}
