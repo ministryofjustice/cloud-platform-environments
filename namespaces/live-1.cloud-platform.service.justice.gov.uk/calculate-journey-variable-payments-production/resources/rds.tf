10f3bd3253e81816f1fdee87ed7e7c3b3b61ea3d
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-10-19 12:03:11 +0100

    Add JPC production environment (#3627)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/calculate-journey-variable-payments-production/resources/rds.tf
@@ -0,0 +1,42 @@
+module "rds-instance" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.9"
+
+  cluster_name         = var.cluster_name
+  cluster_state_bucket = var.cluster_state_bucket
+
+  application            = var.application
+  environment-name       = var.environment-name
+  is-production          = var.is-production
+  namespace              = var.namespace
+  infrastructure-support = var.infrastructure-support
+  team_name              = var.team_name
+
+  # enable performance insights
+  performance_insights_enabled = true
+
+  providers = {
+    aws = aws.london
+  }
+
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "0"
+      apply_method = "immediate"
+    }
+  ]
+}
+
+resource "kubernetes_secret" "rds-instance" {
+  metadata {
+    name      = "rds-instance-calculate-journey-variable-payments-${var.environment-name}"
+    namespace = var.namespace
+  }
+
+  data = {
+    resource_id       = module.rds-instance.resource_id
+    access_key_id     = module.rds-instance.access_key_id
+    secret_access_key = module.rds-instance.secret_access_key
+    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+  }
+}
