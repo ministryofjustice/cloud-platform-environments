11e7c9f7aa2f4892842eb67ac0098ed136b2fea5
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-10-22 14:16:06 +0100

    Add Pingdom check for Book Secure Move Frontend

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-production/resources/pingdom.tf
@@ -0,0 +1,21 @@
+  provider "pingdom" {
+  }
+
+    # Integration IDs
+    # 94314 = #pecs-alerts
+
+    resource "pingdom_check" "book-secure-move-frontend-check" {
+    type                     = "http"
+    name                     = "Book Secure Move Frontend - bookasecuremove.service.justice.gov.uk"
+    host                     = "bookasecuremove.service.justice.gov.uk"
+    resolution               = 1
+    notifywhenbackup         = true
+    sendnotificationwhendown = 6
+    notifyagainevery         = 0
+    url                      = "/healthcheck"
+    encryption               = true
+    port                     = 443
+    tags                     = "hmpps,cloudplatform-managed"
+    probefilters             = "region:EU"
+    integrationids           = [94314]
+  }
