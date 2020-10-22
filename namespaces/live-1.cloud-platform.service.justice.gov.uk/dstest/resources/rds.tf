35c9b4a405b66469fa3f511a2a84d0b6a136cb66
David Salgado <david@digitalronin.com>
2020-10-22 09:28:35 +0100

    Another attempt to create postgres9.5 RDS instance
    This is a copied and tweaked version of the peoplefinder-dev rds.tf
    file. Previous attempts to create a postgres9.5 RDS instance failed with
    various errors, so I'm hoping that copying and adapting an existing one
    will succeed.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/dstest/resources/rds.tf
@@ -0,0 +1,51 @@
+module "rds" {
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.9"
+  cluster_name               = var.cluster_name
+  cluster_state_bucket       = var.cluster_state_bucket
+  team_name                  = var.team_name
+  business-unit              = var.business_unit
+  application                = var.application
+  is-production              = var.is_production
+  namespace                  = var.namespace
+  db_engine                  = "postgres"
+  db_engine_version          = "9.5"
+  db_backup_retention_period = "7"
+  db_name                    = "dstest_rds"
+  environment-name           = var.environment
+  infrastructure-support     = var.infrastructure_support
+
+  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
+  # Pick the one that defines the postgres version the best
+  rds_family = "postgres9.5"
+
+  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
+  # You will need to specify "pending-reboot" here, as default is set to "immediate".
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "dstest_rds" {
+  metadata {
+    name      = "dstest-rds-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_name         = module.rds.database_name
+    database_username     = module.rds.database_username
+    database_password     = module.rds.database_password
+    rds_instance_address  = module.rds.rds_instance_address
+
+    access_key_id     = module.rds.access_key_id
+    secret_access_key = module.rds.secret_access_key
+
+    url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
+  }
+}
+
