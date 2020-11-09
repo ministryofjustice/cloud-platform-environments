09663cbbb75b22f34b48793bef3623711c794074
said-moj <said.shire@digital.justice.gov.uk>
2020-11-09 15:20:39 +0000

    Remove unused database migration celery broker

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-production/resources/elasticache.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-production/resources/elasticache.tf
@@ -17,25 +17,6 @@ module "celery-broker" {
   }
 }
 
-module "celery-broker-database-migration" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=4.2"
-  cluster_name           = var.cluster_name
-  cluster_state_bucket   = var.cluster_state_bucket
-  team_name              = var.team_name
-  application            = var.application
-  is-production          = var.is-production
-  node_type              = "cache.t2.medium"
-  environment-name       = var.environment-name
-  infrastructure-support = var.email
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
-  namespace              = var.namespace
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 resource "kubernetes_secret" "celery-broker" {
   metadata {
     name      = "celery-broker"
@@ -46,18 +27,5 @@ resource "kubernetes_secret" "celery-broker" {
     url                      = "redis://:${module.celery-broker.auth_token}@${module.celery-broker.primary_endpoint_address}:6379"
     primary_endpoint_address = module.celery-broker.primary_endpoint_address
     auth_token               = module.celery-broker.auth_token
-  }
-}
-
-resource "kubernetes_secret" "celery-broker-database-migration" {
-  metadata {
-    name      = "celery-broker-database-migration"
-    namespace = var.namespace
-  }
-
-  data = {
-    url                      = "redis://:${module.celery-broker-database-migration.auth_token}@${module.celery-broker-database-migration.primary_endpoint_address}:6379"
-    primary_endpoint_address = module.celery-broker-database-migration.primary_endpoint_address
-    auth_token               = module.celery-broker-database-migration.auth_token
   }
 }
