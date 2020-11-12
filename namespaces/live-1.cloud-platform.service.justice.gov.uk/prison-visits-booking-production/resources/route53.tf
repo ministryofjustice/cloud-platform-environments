9faa472643600c9b9f77d953021231bf6c158c3c
David Salgado <david@digitalronin.com>
2020-11-12 12:16:40 +0000

    Add a route53 zone for prisonvisits.service.just..
    This is a zone which has an A record for a production service:
    ```
    http://staff.prisonvisits.service.justice.gov.uk/
    ```
    But it's not being managed in terraform.
    
    This change adds a terraform resource for the zone. A manual `terraform
    import` will import the existing AWS route53 hosted zone into the
    terraform state.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-production/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-visits-booking-production/resources/route53.tf
@@ -23,3 +23,28 @@ resource "kubernetes_secret" "route53_zone_sec" {
   }
 }
 
+resource "aws_route53_zone" "route53_justice_zone" {
+  name = "prisonvisits.service.justice.gov.uk"
+
+  tags = {
+    business-unit          = "HMPPS"
+    application            = "PVB"
+    is-production          = var.is-production
+    environment-name       = var.environment-name
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure-support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "route53_justice_zone_sec" {
+  metadata {
+    name      = "route53-justice-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.route53_justice_zone.zone_id
+  }
+}
+
