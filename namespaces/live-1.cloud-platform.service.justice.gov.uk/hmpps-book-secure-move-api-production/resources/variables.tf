02dd394f3d4065546449370221d799c4bc616aae
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-11-17 09:32:00 +0000

    Update team email

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/variables.tf
@@ -15,7 +15,7 @@ variable "is_production" {
 }
 
 variable "infrastructure-support" {
-  default = "bookasecuremove@digital.justice.gov.uk"
+  default = "pecs-digital-tech@digital.justice.gov.uk"
 }
 
 variable "application" {
