af3a6dc518b4d085052f856423a8d2f08fcc75cc
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-11-23 15:53:30 +0000

    Upgrade basm dev database to  t3.medium

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-dev/resources/rds.tf
@@ -11,6 +11,8 @@ module "rds-instance" {
   infrastructure-support = var.infrastructure-support
   team_name              = var.team_name
 
+  db_instance_class      = "db.t3.medium"
+
   providers = {
     # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
