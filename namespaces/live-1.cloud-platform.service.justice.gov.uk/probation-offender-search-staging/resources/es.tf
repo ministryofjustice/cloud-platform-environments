6340b51c8d463a3244dcadc4f8d0b7cf7f8b633e
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2020-11-23 11:13:55 +0000

    S3 bucket for elastic search snapshots

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-staging/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-staging/resources/es.tf
@@ -1,5 +1,5 @@
 module "probation_offender_search_es" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.2"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.4"
   cluster_name               = var.cluster_name
   cluster_state_bucket       = var.cluster_state_bucket
   application                = var.application
@@ -14,4 +14,22 @@ module "probation_offender_search_es" {
   aws-es-proxy-replica-count = 2
   instance_type              = "t2.medium.elasticsearch"
   ebs_volume_size            = 15
+  s3_manual_snapshot_repository = module.es_snapshots_s3_bucket.bucket_arn
+}
+
+module "es_snapshots_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.5"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
 }
