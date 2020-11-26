5681c77191b7c9b3784e0a7e347a03910f816548
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2020-11-26 12:43:30 +0000

    Export separate k8s secret for role arn

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-staging/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-staging/resources/es.tf
@@ -1,5 +1,5 @@
 module "probation_offender_search_es" {
-  source                        = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
   cluster_name                  = var.cluster_name
   cluster_state_bucket          = var.cluster_state_bucket
   application                   = var.application
@@ -19,4 +19,15 @@ module "probation_offender_search_es" {
 
 data "aws_s3_bucket" "snapshot_bucket" {
   bucket = "cloud-platform-b98efe046f8e2974726adda74c50890e"
+}
+
+resource "kubernetes_secret" "es_snapshots_role" {
+  metadata {
+    name      = "es-snapshot-role"
+    namespace = var.namespace
+  }
+
+  data = {
+    snapshot_role_arn = module.probation_offender_search_es.snapshot_role_arn
+  }
 }
