eb44d307ab136ddcce31a96d46b06a0174e8d43e
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2020-11-26 11:54:00 +0000

    Add s3 bucket for snapshots probation-offender-search-preprod

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-preprod/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-preprod/resources/es.tf
@@ -1,17 +1,22 @@
 module "probation_offender_search_es" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.2"
-  cluster_name               = var.cluster_name
-  cluster_state_bucket       = var.cluster_state_bucket
-  application                = var.application
-  business-unit              = var.business-unit
-  environment-name           = var.environment-name
-  infrastructure-support     = var.infrastructure-support
-  is-production              = var.is-production
-  team_name                  = var.team_name
-  elasticsearch-domain       = "probation-search"
-  namespace                  = var.namespace
-  elasticsearch_version      = "7.4"
-  aws-es-proxy-replica-count = 2
-  instance_type              = "t2.medium.elasticsearch"
-  ebs_volume_size            = 15
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.2"
+  cluster_name                  = var.cluster_name
+  cluster_state_bucket          = var.cluster_state_bucket
+  application                   = var.application
+  business-unit                 = var.business-unit
+  environment-name              = var.environment-name
+  infrastructure-support        = var.infrastructure-support
+  is-production                 = var.is-production
+  team_name                     = var.team_name
+  elasticsearch-domain          = "probation-search"
+  namespace                     = var.namespace
+  elasticsearch_version         = "7.4"
+  aws-es-proxy-replica-count    = 2
+  instance_type                 = "t2.medium.elasticsearch"
+  ebs_volume_size               = 15
+  s3_manual_snapshot_repository = data.aws_s3_bucket.snapshot_bucket.arn
+}
+
+data "aws_s3_bucket" "snapshot_bucket" {
+  bucket = "cloud-platform-b98efe046f8e2974726adda74c50890e"
 }
