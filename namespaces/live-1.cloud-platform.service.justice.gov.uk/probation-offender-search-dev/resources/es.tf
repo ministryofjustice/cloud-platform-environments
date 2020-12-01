64fae84bf1786ea41912b42685ac244143a09c8f
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2020-12-01 11:26:46 +0000

    enable snapshots for probation-offender-search-dev

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/es.tf
@@ -1,18 +1,19 @@
 module "probation_offender_search_es" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
-  cluster_name               = var.cluster_name
-  cluster_state_bucket       = var.cluster_state_bucket
-  application                = var.application
-  business-unit              = var.business-unit
-  environment-name           = var.environment-name
-  infrastructure-support     = var.infrastructure-support
-  is-production              = var.is-production
-  team_name                  = var.team_name
-  elasticsearch-domain       = "probation-search"
-  namespace                  = var.namespace
-  elasticsearch_version      = "7.9"
-  aws-es-proxy-replica-count = 2
-  instance_type              = "t2.medium.elasticsearch"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
+  cluster_name                  = var.cluster_name
+  cluster_state_bucket          = var.cluster_state_bucket
+  application                   = var.application
+  business-unit                 = var.business-unit
+  environment-name              = var.environment-name
+  infrastructure-support        = var.infrastructure-support
+  is-production                 = var.is-production
+  team_name                     = var.team_name
+  elasticsearch-domain          = "probation-search"
+  namespace                     = var.namespace
+  elasticsearch_version         = "7.9"
+  aws-es-proxy-replica-count    = 2
+  instance_type                 = "t2.medium.elasticsearch"
+  s3_manual_snapshot_repository = module.es_snapshots_s3_bucket.bucket_arn
 }
 
 module "es_snapshots_s3_bucket" {
