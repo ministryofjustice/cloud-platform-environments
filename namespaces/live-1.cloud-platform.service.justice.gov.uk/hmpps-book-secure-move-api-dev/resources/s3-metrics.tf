aeac47a256d77bc597ce9d501956fc3fac479b89
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-12-07 14:40:47 +0000

    Revert "add metrics name to read-only user"
    This reverts commit 35f3811165b1d212a76d3dd2cede5f9aea8b015f.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-dev/resources/s3-metrics.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-dev/resources/s3-metrics.tf
@@ -34,20 +34,20 @@ resource "kubernetes_secret" "book_a_secure_move_metrics_s3_bucket" {
 }
 
 # Generate an additional IAM user with read-only access to the bucket
-resource "metrics_random_id" "id" {
+resource "random_id" "id" {
   byte_length = 16
 }
 
-resource "metrics_aws_iam_user" "user" {
-  name = "s3-bucket-user-${metrics_random_id.id.hex}"
+resource "aws_iam_user" "user" {
+  name = "s3-bucket-user-${random_id.id.hex}"
   path = "/system/s3-bucket-user/"
 }
 
-resource "metrics_aws_iam_access_key" "user" {
-  user = metrics_aws_iam_user.user.name
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
 }
 
-data "metrics_aws_iam_policy_document" "policy" {
+data "aws_iam_policy_document" "policy" {
   statement {
     actions = [
       "s3:GetBucketLocation",
@@ -77,10 +77,10 @@ data "metrics_aws_iam_policy_document" "policy" {
   }
 }
 
-resource "metrics_aws_iam_user_policy" "policy" {
+resource "aws_iam_user_policy" "policy" {
   name   = "s3-bucket-read-only"
-  policy = data.metrics_aws_iam_policy_document.policy.json
-  user   = metrics_aws_iam_user.user.name
+  policy = data.aws_iam_policy_document.policy.json
+  user   = aws_iam_user.user.name
 }
 
 resource "kubernetes_secret" "book_a_secure_move_metrics_s3_bucket_ro" {
@@ -90,8 +90,8 @@ resource "kubernetes_secret" "book_a_secure_move_metrics_s3_bucket_ro" {
   }
 
   data = {
-    access_key_id     = metrics_aws_iam_access_key.user.id
-    secret_access_key = metrics_aws_iam_access_key.user.secret
+    access_key_id     = aws_iam_access_key.user.id
+    secret_access_key = aws_iam_access_key.user.secret
     bucket_arn        = module.book_a_secure_move_metrics_s3_bucket.bucket_arn
     bucket_name       = module.book_a_secure_move_metrics_s3_bucket.bucket_name
   }
