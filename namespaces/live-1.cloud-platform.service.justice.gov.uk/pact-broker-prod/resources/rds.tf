b76d32745caf12087b126c068d7962b33a194b65
mtrbls <mourad.trabelsi@digital.justice.gov.uk>
2020-12-16 06:34:15 +0000

    bump RDS module version for prod env

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds.tf
@@ -1,5 +1,5 @@
 module "pact_broker_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.10"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.11"
 
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
