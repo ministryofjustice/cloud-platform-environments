0d27f20b12d5127380518d89a5dfd14c6e0049c5
David Salgado <david@digitalronin.com>
2020-12-16 18:05:09 +0900

    Update production RDS from 5.11 to 5.12
    This adds storage autoscaling

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/rds.tf
@@ -5,7 +5,7 @@
  *
  */
 module "check-financial-eligibility-rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.11"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.12"
 
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
