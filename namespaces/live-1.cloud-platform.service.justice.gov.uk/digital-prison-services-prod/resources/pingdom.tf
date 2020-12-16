9ad53c7d5506deafb7a7114a1ec41fb88898c654
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2020-12-16 09:20:39 +0000

    NN-3103 remove pingdom check and reference to new nomis ui repo

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-prod/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/digital-prison-services-prod/resources/pingdom.tf
@@ -20,20 +20,3 @@ resource "pingdom_check" "dps-production-check-digital" {
   probefilters             = "region:EU"
   integrationids           = [96624, 96628]
 }
-
-resource "pingdom_check" "dps-production-check-whereabouts" {
-  type                     = "http"
-  name                     = "DPS - whereabouts.${var.domain}"
-  host                     = "health-kick.prison.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/https/whereabouts.${var.domain}"
-  encryption               = true
-  port                     = 443
-  tags                     = "dps,hmpps,cloudplatform-managed"
-  probefilters             = "region:EU"
-  integrationids           = [96624, 96628]
-}
-
