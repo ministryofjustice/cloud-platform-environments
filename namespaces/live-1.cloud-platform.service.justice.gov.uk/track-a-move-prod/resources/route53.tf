d143236917cbdf36fa432c7951b0d7f634f63486
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-17 12:00:25 +0000

    Re enable conflicting resources

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/route53.tf
@@ -7,14 +7,14 @@ data "kubernetes_secret" "zone_id" {
 
 # Example DNS record using Route53.
 # Route53 is not specifically required; any DNS host can be used.
-# resource "aws_route53_record" "data" {
-#   name    = aws_api_gateway_domain_name.apigw_fqdn.domain_name
-#   type    = "A"
-#   zone_id = data.kubernetes_secret.zone_id.data["zone_id"]
-#
-#   alias {
-#     evaluate_target_health = true
-#     name                   = aws_api_gateway_domain_name.apigw_fqdn.regional_domain_name
-#     zone_id                = aws_api_gateway_domain_name.apigw_fqdn.regional_zone_id
-#   }
-# }
+resource "aws_route53_record" "data" {
+  name    = aws_api_gateway_domain_name.apigw_fqdn.domain_name
+  type    = "A"
+  zone_id = data.kubernetes_secret.zone_id.data["zone_id"]
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.apigw_fqdn.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.apigw_fqdn.regional_zone_id
+  }
+}
