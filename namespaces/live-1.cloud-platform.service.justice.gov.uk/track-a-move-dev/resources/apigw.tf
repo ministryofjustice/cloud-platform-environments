26a46d709766f4e4b720fc34f0284e3a38ccd0aa
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-17 17:29:13 +0000

    Wait for API deployment to complete before mapping name

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-dev/resources/apigw.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-dev/resources/apigw.tf
@@ -216,6 +216,4 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
   api_id      = aws_api_gateway_rest_api.apigw.id
   stage_name  = aws_api_gateway_deployment.live.stage_name
   domain_name = aws_api_gateway_domain_name.apigw_fqdn.domain_name
-
-  depends_on = [aws_api_gateway_deployment.live]
 }
--- namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-uat/resources/apigw.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/track-a-move-uat/resources/apigw.tf
@@ -216,4 +216,6 @@ resource "aws_api_gateway_base_path_mapping" "uat" {
   api_id      = aws_api_gateway_rest_api.apigw.id
   stage_name  = aws_api_gateway_deployment.live.stage_name
   domain_name = aws_api_gateway_domain_name.apigw_fqdn.domain_name
+
+  depends_on = [aws_api_gateway_deployment.live]
 }
