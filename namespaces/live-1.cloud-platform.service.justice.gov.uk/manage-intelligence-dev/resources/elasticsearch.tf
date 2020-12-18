2cfa71ff7ba972d4d1bed20a69c0dd48e29ddef1
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2020-12-18 11:09:28 +0000

    Upgrade to v7.9 and add encryption to cluster. (#3985)
    * Upgrade to v7.9 and add encryption to cluster.
    
    * Fix module url
    
    * Switch to use branch instead of tag

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/elasticsearch.tf
@@ -1,5 +1,5 @@
 module "manage_intelligence_elasticsearch" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=add-es-domain-encryption-support"
   cluster_name               = var.cluster_name
   cluster_state_bucket       = var.cluster_state_bucket
   application                = var.application
@@ -10,7 +10,7 @@ module "manage_intelligence_elasticsearch" {
   team_name                  = var.team_name
   elasticsearch-domain       = "manage-intelligence"
   namespace                  = var.namespace
-  elasticsearch_version      = "7.7"
+  elasticsearch_version      = "7.9"
   aws-es-proxy-replica-count = 2
   instance_type              = "t2.medium.elasticsearch"
 }
