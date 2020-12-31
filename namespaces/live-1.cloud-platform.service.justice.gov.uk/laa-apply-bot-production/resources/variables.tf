78c79cae5311bb99e0ae0711ecb71d27da3209ad
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-31 09:22:13 +0000

    Add rds resource and variables

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-apply-bot-production/resources/variables.tf
@@ -0,0 +1,15 @@
+variable "namespace" {
+  default = "laa-apply-bot-production"
+}
+
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
