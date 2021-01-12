5b2a42f531ca90a81d32532e1a7a945ef1f9cb1c
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2021-01-12 14:56:18 +0000

    Split out S3 queues in bucket notification resource

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/s3.tf
@@ -109,24 +109,24 @@ resource "aws_sqs_queue_policy" "mpps_pin_phone_monitor_s3_event_queue_policy" {
 resource "aws_s3_bucket_notification" "hmpps_pin_phone_monitor_s3_notification" {
   bucket = module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_name
 
-  queue = [
-    {
-      id        = "metadata-upload-event"
-      queue_arn = module.hmpps_pin_phone_monitor_s3_event_queue.sqs_arn
-      events = [
+  queue {
+    id = "metadata-upload-event"
+    queue_arn = module.hmpps_pin_phone_monitor_s3_event_queue.sqs_arn
+    events = [
       "s3:ObjectCreated:*"]
-      filter_suffix = ".json"
-      filter_prefix = "metadata/"
-    },
-    {
-      id        = "recording-deletion-event"
-      queue_arn = module.hmpps_pin_phone_monitor_s3_event_queue.sqs_arn
-      events = [
+    filter_suffix = ".json"
+    filter_prefix = "metadata/"
+  }
+
+  queue {
+    id = "recording-deletion-event"
+    queue_arn = module.hmpps_pin_phone_monitor_s3_event_queue.sqs_arn
+    events = [
       "s3:ObjectRemoved:Delete"]
-      filter_suffix = ".flac"
-      filter_prefix = "recordings/"
-    }
-  ]
+    filter_suffix = ".flac"
+    filter_prefix = "recordings/"
+  }
+
 }
 
 resource "kubernetes_secret" "hmpps_pin_phone_monitor_s3_event_queue" {
