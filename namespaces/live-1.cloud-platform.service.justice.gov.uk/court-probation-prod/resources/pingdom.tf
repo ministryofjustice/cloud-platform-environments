e638b494ce540dbe19421045f5fb7b4d044d3952
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-01-12 07:44:15 +0000

    Added integration id

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/pingdom.tf
@@ -4,7 +4,7 @@ provider "pingdom" {
 resource "pingdom_check" "prepare-a-case-production-check" {
   type                     = "http"
   name                     = "${var.application}"
-  host                     = "/https/${var.prepare-case-domain}"
+  host                     = "${var.prepare-case-domain}"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
@@ -14,6 +14,6 @@ resource "pingdom_check" "prepare-a-case-production-check" {
   port                     = 443
   tags                     = "hmpps, prepare-a-case, pic, cloudplatform-managed"
   probefilters             = "region:EU"
-  integrationids           = []
+  integrationids           = [110432]
 }
 
