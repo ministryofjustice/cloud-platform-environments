8eeccb47911e3440a1f2973f2af4c1bf8ee79597
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-01-19 11:51:04 +0000

    :wrench: change backup retention period to 0 days (temporary) (#4092)
    * :wrench: change backup retention period to 0 days (temporary)
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/rds.tf
@@ -29,14 +29,15 @@ resource "kubernetes_secret" "court_case_service_rds" {
   }
 
   data = {
-    rds_instance_endpoint = module.court_case_service_rds.rds_instance_endpoint
-    database_name         = module.court_case_service_rds.database_name
-    database_username     = module.court_case_service_rds.database_username
-    database_password     = module.court_case_service_rds.database_password
-    rds_instance_address  = module.court_case_service_rds.rds_instance_address
-    url                   = "postgres://${module.court_case_service_rds.database_username}:${module.court_case_service_rds.database_password}@${module.court_case_service_rds.rds_instance_endpoint}/${module.court_case_service_rds.database_name}"
-    access_key_id         = module.court_case_service_rds.access_key_id
-    secret_access_key     = module.court_case_service_rds.secret_access_key
+    rds_instance_endpoint      = module.court_case_service_rds.rds_instance_endpoint
+    database_name              = module.court_case_service_rds.database_name
+    database_username          = module.court_case_service_rds.database_username
+    database_password          = module.court_case_service_rds.database_password
+    rds_instance_address       = module.court_case_service_rds.rds_instance_address
+    url                        = "postgres://${module.court_case_service_rds.database_username}:${module.court_case_service_rds.database_password}@${module.court_case_service_rds.rds_instance_endpoint}/${module.court_case_service_rds.database_name}"
+    access_key_id              = module.court_case_service_rds.access_key_id
+    secret_access_key          = module.court_case_service_rds.secret_access_key
+    db_backup_retention_period = 0
   }
 }
 
