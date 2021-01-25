234d714b0a3d1ae79735bf245c276b0065b5a30f
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-01-25 12:16:31 +0000

    Test update to ES module, deploy a second ES domain with encrypted at reset enabled

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/es.tf
@@ -16,6 +16,26 @@ module "probation_offender_search_es" {
   s3_manual_snapshot_repository = module.es_snapshots_s3_bucket.bucket_arn
 }
 
+module "probation_offender_search_elasticsearch" {
+  source                          = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=fix_up_aws_es_proxy_deployment"
+  cluster_name                    = var.cluster_name
+  cluster_state_bucket            = var.cluster_state_bucket
+  application                     = var.application
+  business-unit                   = var.business-unit
+  environment-name                = var.environment-name
+  infrastructure-support          = var.infrastructure-support
+  is-production                   = var.is-production
+  team_name                       = var.team_name
+  elasticsearch-domain            = "probation-offender-search"
+  encryption_at_rest              = true
+  node_to_node_encryption_enabled = true
+  namespace                       = var.namespace
+  elasticsearch_version           = "7.9"
+  aws-es-proxy-replica-count      = 2
+  instance_type                   = "t2.medium.elasticsearch"
+  s3_manual_snapshot_repository   = module.es_snapshots_s3_bucket.bucket_arn
+}
+
 module "es_snapshots_s3_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.5"
   team_name              = var.team_name
