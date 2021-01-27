660c7d1d149eb9bf0a4bb7073471fd712e12f530
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-01-27 11:47:00 +0000

    Create delius-updates_dev

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/delius-updates-dev/resources/variables.tf
@@ -0,0 +1,31 @@
+variable "application" {
+  default = "delius-updates"
+}
+
+variable "namespace" {
+  default = "delius-updates-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Manage a Sentence Team"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
