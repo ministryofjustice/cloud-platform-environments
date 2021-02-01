d160078fcd53899d3accce5d52a5db403c7e2b7d
David Salgado <david@digitalronin.com>
2021-02-01 17:13:06 +0900

    Set repository for github actions secrets

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/moj-prototype/resources/serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/moj-prototype/resources/serviceaccount.tf
@@ -3,7 +3,5 @@ module "serviceaccount" {
 
   namespace = var.namespace
 
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["moj-prototype-template"]
 }
