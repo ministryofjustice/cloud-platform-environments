4f211bf0188ece9f97ad6496f42876560b56c562
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-02-04 13:47:17 +0000

    prisoner-offender-search-dev - new ES domain

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-offender-search-dev/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-offender-search-dev/resources/es.tf
@@ -16,6 +16,27 @@ module "prisoner_offender_search_es" {
   s3_manual_snapshot_repository = module.es_snapshots_s3_bucket.bucket_arn
 }
 
+module "prisoner_offender_search_elasticsearch" {
+  source                          = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.7.0"
+  cluster_name                    = var.cluster_name
+  cluster_state_bucket            = var.cluster_state_bucket
+  application                     = var.application
+  business-unit                   = var.business-unit
+  environment-name                = var.environment-name
+  infrastructure-support          = var.infrastructure-support
+  is-production                   = var.is-production
+  team_name                       = var.team_name
+  elasticsearch-domain            = "search-prisoner"
+  aws_es_proxy_service_name       = "es-proxy"
+  encryption_at_rest              = true
+  node_to_node_encryption_enabled = true
+  namespace                       = var.namespace
+  elasticsearch_version           = "7.9"
+  aws-es-proxy-replica-count      = 2
+  instance_type                   = "t3.medium.elasticsearch"
+  s3_manual_snapshot_repository   = module.es_snapshots_s3_bucket.bucket_arn
+}
+
 module "es_snapshots_s3_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.5"
   team_name              = var.team_name
@@ -41,6 +62,7 @@ resource "kubernetes_secret" "es_snapshots_role" {
 
   data = {
     snapshot_role_arn = module.prisoner_offender_search_es.snapshot_role_arn
+    snapshot_role_arn2 = module.prisoner_offender_search_elasticsearch.snapshot_role_arn
   }
 }
 
