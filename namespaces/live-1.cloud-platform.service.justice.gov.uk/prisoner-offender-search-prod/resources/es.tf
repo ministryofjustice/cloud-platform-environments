11adc3d0c9a0b0f47bfd09e5661e61962878e456
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-02-08 13:20:38 +0000

    prisoner-offender-search-prod - new ES domain

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-offender-search-prod/resources/es.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prisoner-offender-search-prod/resources/es.tf
@@ -16,6 +16,28 @@ module "prisoner_offender_search_es" {
   s3_manual_snapshot_repository = module.es_snapshots_s3_bucket.bucket_arn
 }
 
+module "prisoner_offender_search_elasticsearch" {
+  source                          = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.7.0"
+  cluster_name                    = var.cluster_name
+  cluster_state_bucket            = var.cluster_state_bucket
+  application                     = var.application
+  business-unit                   = var.business-unit
+  environment-name                = var.environment-name
+  infrastructure-support          = var.infrastructure-support
+  is-production                   = var.is-production
+  team_name                       = var.team_name
+  elasticsearch-domain            = "search-prisoner"
+  aws_es_proxy_service_name       = "es-proxy"
+  encryption_at_rest              = true
+  node_to_node_encryption_enabled = true
+  namespace                       = var.namespace
+  elasticsearch_version           = "7.9"
+  aws-es-proxy-replica-count      = 4
+  instance_type                   = "t3.medium.elasticsearch"
+  s3_manual_snapshot_repository   = module.es_snapshots_s3_bucket.bucket_arn
+  es_ns_annotation                = false
+}
+
 module "es_snapshots_s3_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.5"
   team_name              = var.team_name
@@ -40,7 +62,8 @@ resource "kubernetes_secret" "es_snapshots_role" {
   }
 
   data = {
-    snapshot_role_arn = module.prisoner_offender_search_es.snapshot_role_arn
+    snapshot_role_arn  = module.prisoner_offender_search_es.snapshot_role_arn
+    snapshot_role_arn2 = module.prisoner_offender_search_elasticsearch.snapshot_role_arn
   }
 }
 
@@ -68,3 +91,13 @@ resource "kubernetes_secret" "es_snapshots_preprod" {
   }
 }
 
+resource "kubernetes_secret" "elasticsearch" {
+  metadata {
+    name      = "elasticsearch"
+    namespace = var.namespace
+  }
+
+  data = {
+    aws_es_proxy_url = module.prisoner_offender_search_elasticsearch.aws_es_proxy_url
+  }
+}
