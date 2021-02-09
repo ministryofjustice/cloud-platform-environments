f039feefd18c4d2efef636573b00bd426823d2ee
reallydontask <agoney.garcia-deniz@digital.justice.gov.uk>
2021-02-09 15:09:08 +0000

    New SNS for Offender Assessments Events

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
@@ -68,3 +68,24 @@ resource "kubernetes_secret" "probation_offender_events" {
     topic_arn         = module.probation_offender_events.topic_arn
   }
 }
+
+module "offender_assessments_events" {
+  source             = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.1"
+  team_name          = var.team_name
+  topic_display_name = "offender-assessments-events"
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "offender_assessments_events" {
+  metadata {
+    name      = "offender-assessments-events-topic"
+    namespace = var.namespace
+  }
+  data = {
+    access_key_id     = module.offender_assessments_events.access_key_id
+    secret_access_key = module.offender_assessments_events.secret_access_key
+    topic_arn         = module.offender_assessments_events.topic_arn
+  }
+}
