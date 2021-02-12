0c98822b81db457aae4419387e4d50f52ddbf167
David Lantos <david.lantos@gmail.com>
2021-02-12 12:57:37 +0000

    Refer to new global topic in interventions secret
    We want to remove the previous topic but the application is still
    referencing the secret name. So we keep the secret name for now but
    delete the old topic.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-intervention-events-topic.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-intervention-events-topic.tf
@@ -1,14 +1,3 @@
-module "intervention_events" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.1"
-
-  team_name          = var.team_name
-  topic_display_name = "intervention-events"
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 resource "kubernetes_secret" "intervention_events_sns" {
   metadata {
     name      = "intervention-events-topic"
@@ -16,8 +5,8 @@ resource "kubernetes_secret" "intervention_events_sns" {
   }
 
   data = {
-    access_key_id     = module.intervention_events.access_key_id
-    secret_access_key = module.intervention_events.secret_access_key
-    topic_arn         = module.intervention_events.topic_arn
+    access_key_id     = module.hmpps-domain-events.access_key_id
+    secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
   }
 }
