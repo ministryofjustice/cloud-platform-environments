4edb59abeb4e6bf5317d865db649b122310dbb8d
brenetic <brendan.butler@digital.justice.gov.uk>
2021-02-15 18:24:56 +0000

    Form Builder - Fix Using MoJ Forms Pingdom URL
    usingmojforms instead of using-moj-forms

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-monitoring/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/formbuilder-monitoring/resources/pingdom.tf
@@ -13,7 +13,7 @@ locals {
     miscarriages-of-justice  = "miscarriages-of-justice.form.service.justice.gov.uk",
     publisher                = "fb-publisher-live.apps.live-1.cloud-platform.service.justice.gov.uk",
     report-security-incident = "report-security-incident.form.service.justice.gov.uk",
-    using-moj-forms          = "using-moj-forms.form.service.justice.gov.uk"
+    using-moj-forms          = "usingmojforms.form.service.justice.gov.uk"
   }
   names = keys(local.forms)
   hosts = values(local.forms)
