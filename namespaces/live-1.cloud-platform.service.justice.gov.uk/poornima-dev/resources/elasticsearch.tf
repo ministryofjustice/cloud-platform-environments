86471d399c3cac1f80195953e1370b3ce3a0834a
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-02-16 17:56:34 +0000

    Create 2 ES clusters and test the namespace annotation (#4312)
    * Add test ES for 3.7.0 upgrade testing
    
    * Create 2 ES module and test the namespace annotation
    
    * Input 2 different aws_es_proxy names
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/elasticsearch.tf
@@ -3,17 +3,46 @@
 # CP Test Elasticsearch cluster
 #################################################################################
 
-module "peoplefinder_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
-  cluster_name           = var.cluster_name
-  cluster_state_bucket   = var.cluster_state_bucket
-  application            = "cloud-platform-esupg"
-  business-unit          = "Platforms"
-  environment-name       = "dev"
-  infrastructure-support = "platforms@digital.justice.gov.uk"
-  is-production          = "false"
-  team_name              = "webops"
-  elasticsearch-domain   = "es"
-  namespace              = "poornima-dev"
-  elasticsearch_version  = "6.8"
+module "test_es_1" {
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=remove-ns-annotation"
+  cluster_name              = var.cluster_name
+  cluster_state_bucket      = var.cluster_state_bucket
+  application               = "cloud-platform-esupg"
+  business-unit             = "Platforms"
+  environment-name          = "dev"
+  infrastructure-support    = "platforms@digital.justice.gov.uk"
+  is-production             = "false"
+  team_name                 = "webops"
+  elasticsearch-domain      = "es-1"
+  namespace                 = var.namespace
+  elasticsearch_version     = "6.8"
+  aws_es_proxy_service_name = "aws-es-proxy-es-1"
 }
+
+module "test_es_2" {
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=remove-ns-annotation"
+  cluster_name              = var.cluster_name
+  cluster_state_bucket      = var.cluster_state_bucket
+  application               = "cloud-platform-esupg"
+  business-unit             = "Platforms"
+  environment-name          = "dev"
+  infrastructure-support    = "platforms@digital.justice.gov.uk"
+  is-production             = "false"
+  team_name                 = "webops"
+  elasticsearch-domain      = "es-2"
+  namespace                 = var.namespace
+  elasticsearch_version     = "6.8"
+  aws_es_proxy_service_name = "aws-es-proxy-es-2"
+}
+
+
+# dependency with es module. pass the role name from the es module
+
+
+module "ns_annotation" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-ns-annotation"
+  ns_annotation_roles = [module.test_es_1.aws_iam_role_name, module.test_es_2.aws_iam_role_name]
+  namespace           = var.namespace
+}
+
+
