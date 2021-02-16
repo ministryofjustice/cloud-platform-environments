c027833a23587a38e73e9c51a1da748f4856876f
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-02-16 14:53:22 +0000

    Create 2 ES module and test the namespace annotation

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/elasticsearch.tf
@@ -3,8 +3,8 @@
 # CP Test Elasticsearch cluster
 #################################################################################
 
-module "peoplefinder_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
+module "test_es_1" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=remove-ns-annotation"
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
   application            = "cloud-platform-esupg"
@@ -13,7 +13,34 @@ module "peoplefinder_es" {
   infrastructure-support = "platforms@digital.justice.gov.uk"
   is-production          = "false"
   team_name              = "webops"
-  elasticsearch-domain   = "es"
-  namespace              = "poornima-dev"
+  elasticsearch-domain   = "es-1"
+  namespace              = var.namespace
   elasticsearch_version  = "6.8"
 }
+
+module "test_es_2" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=remove-ns-annotation"
+  cluster_name           = var.cluster_name
+  cluster_state_bucket   = var.cluster_state_bucket
+  application            = "cloud-platform-esupg"
+  business-unit          = "Platforms"
+  environment-name       = "dev"
+  infrastructure-support = "platforms@digital.justice.gov.uk"
+  is-production          = "false"
+  team_name              = "webops"
+  elasticsearch-domain   = "es-2"
+  namespace              = var.namespace
+  elasticsearch_version  = "6.8"
+}
+
+
+# dependency with es module. pass the role name from the es module
+
+
+module "ns_annotation" {
+  source                 = "https://github.com/ministryofjustice/cloud-platform-terraform-ns-annotation?ref=main"
+  ns_annotation_roles = [module.test_es_1.aws_iam_role_name, module.test_es_2.aws_iam_role_name]
+  namespace = var.namespace
+}
+
+
