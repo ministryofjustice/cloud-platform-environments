13aced66bc5017022d2ab6f7b9702f9f5e83c83d
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-18 13:27:26 +0000

    create hmpps-audit-prod namespace

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/pingdom.tf
@@ -5,7 +5,7 @@ provider "pingdom" {
 # 96624 = #dps_alerts
 # 96628 = DPS Pager duty
 
-resource "pingdom_check" "court-register-production-check" {
+resource "pingdom_check" "hmpps-audit-api-production-check" {
   type                     = "http"
   name                     = "DPS - HMPPS Audit API"
   host                     = "health-kick.prison.service.justice.gov.uk"
@@ -20,20 +20,3 @@ resource "pingdom_check" "court-register-production-check" {
   probefilters             = "region:EU"
   integrationids           = [96624, 96628]
 }
-
-resource "pingdom_check" "prison-register-production-check" {
-  type                     = "http"
-  name                     = "DPS - prison-register"
-  host                     = "health-kick.prison.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/https/${var.domain_prison_register}"
-  encryption               = true
-  port                     = 443
-  tags                     = "dps,hmpps,cloudplatform-managed"
-  probefilters             = "region:EU"
-  integrationids           = [96624, 96628]
-}
-
