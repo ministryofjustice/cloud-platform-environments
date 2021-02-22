7a58cad87774366103a86017dda04a7224da67eb
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-02-22 15:11:42 +0000

    Upgrade peoplefinder-production elasticsearch modules to 3.8.0 (#4353)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/elasticsearch.tf
@@ -4,7 +4,7 @@
 #################################################################################
 
 module "peoplefinder_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.5.1"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.8.0"
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
   application            = "peoplefinder"
@@ -16,4 +16,10 @@ module "peoplefinder_es" {
   elasticsearch-domain   = "es"
   namespace              = "peoplefinder-production"
   elasticsearch_version  = "6.8"
+}
+
+module "ns_annotation" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-ns-annotation?ref=0.0.2"
+  ns_annotation_roles = [module.peoplefinder_es.aws_iam_role_name]
+  namespace           = var.namespace
 }
