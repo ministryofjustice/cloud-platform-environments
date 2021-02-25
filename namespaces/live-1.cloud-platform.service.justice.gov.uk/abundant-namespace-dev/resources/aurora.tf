c26f942ee515dcd8e4a750ae48215cbfef0d28f5
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-02-25 11:21:31 +0000

    Remove incorrect protocol in front of source url

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/aurora.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/aurora.tf
@@ -8,7 +8,7 @@
 
 
 module "aurora_db" {
-  source                 = "https://github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.1"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.1"
   cluster_name           = var.cluster_name
   cluster_state_bucket   = var.cluster_state_bucket
   team_name              = "webops"
