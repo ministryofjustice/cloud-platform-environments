5919dba633a77a04d3784537f0d25e00ad11cfe9
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-02-25 14:36:58 +0000

    Add pingdom for testing the pingdom provider (#4380)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {}
+
+resource "pingdom_check" "dev-pk-poornima-dev-pingdom-check" {
+  type                     = "http"
+  name                     = "poornima-dev - cloud-platform - Healthcheck"
+  host                     = "dev-pk.service.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 3
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_platforms,application_poornima_dev_app,component_healthcheck,isproduction_false,environment_dev,infrastructuresupport_platforms"
+  probefilters             = "region:EU"
+  publicreport             = "true"
+  integrationids           = [111647]
+}
