1b8aa30e7adfdcd5b8535c58da9d1a7d89586269
itzcookiie <mayowaadeniyi@hotmail.com>
2021-02-25 09:39:54 +0000

    Add generated files from running the create command

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/mayowa-hello-world-test-dev/resources/variables.tf
@@ -0,0 +1,54 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Hello World"
+}
+
+variable "namespace" {
+  default = "mayowa-hello-world-test-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "laa-get-access"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "mayowaadeniyi@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "laa-cla-fala"
+}
+
+variable "github_owner" {
+  description = "Required by the github terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
