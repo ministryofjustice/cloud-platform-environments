c045963e1d6f16068207835f95099246697ab0ca
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-02-25 10:25:44 +0000

    Add namespace with all modules attached for Terraform testing
    As we look to upgrade our Terraform stack it would be useful for the
    first namespace to include all Terraform resources. Hopefully, this will
    catch any syntax errors when moving from Terraform 0.12 to 13/14/15.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/amq.tf
@@ -0,0 +1,28 @@
+module "test_amq_broker" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-amq-broker?ref=3.2"
+
+  team_name              = "test-names"
+  business-unit          = "HQ"
+  application            = "cloud platform"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "platforms@digital.justice.gov.uk"
+
+  providers = {
+     aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "example_team_broker" {
+  metadata {
+    name      = "example-team-broker-output"
+    namespace = "abundant-namespace-dev"
+  }
+
+  data = {
+    primary_amqp_ssl_endpoint  = module.test_amq_broker.primary_amqp_ssl_endpoint
+    primary_stomp_ssl_endpoint = module.test_amq_broker.primary_stomp_ssl_endpoint
+    username                   = module.test_amq_broker.username
+    password                   = module.test_amq_broker.password
+  }
+}
