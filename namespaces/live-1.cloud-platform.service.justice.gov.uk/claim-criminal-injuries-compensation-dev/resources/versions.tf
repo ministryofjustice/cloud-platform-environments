577816f6481bef16ad74a94d27fc8db2a539c1de
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-03-03 10:49:06 +0000

    Add new Pingdom provider to namespace
    Pingdom has a new provider 1.1.3, which has been uploaded to the
    Terraform registry. This means we have to define the provider block in
    the `main.tf` and remove it from any `pingdom.tf` files.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/versions.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/versions.tf
@@ -1,3 +1,19 @@
+
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    pingdom = {
+      source = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
 }
--- namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-prod/resources/versions.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/licences-prod/resources/versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    pingdom = {
+    random = {
+      source = "hashicorp/random"
+      source = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
 }
