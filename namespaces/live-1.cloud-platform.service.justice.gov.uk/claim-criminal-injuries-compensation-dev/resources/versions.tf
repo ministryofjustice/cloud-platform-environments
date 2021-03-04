743d6cf167bf0cdf8acf3b71937f29b49812c3f2
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-03-04 11:58:17 +0000

    upgrade tf0.13 changes to versions.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/versions.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/versions.tf
@@ -1,3 +1,15 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    pingdom = {
+      source = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
 }
