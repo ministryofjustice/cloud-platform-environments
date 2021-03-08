c9e2d13a6905bc3703a72611eeae811af0bf8a8a
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-03-08 17:09:51 +0000

    Update ecr template to latest version

--- namespace-resources-cli-template/resources/prototype/ecr.tf
+++ namespace-resources-cli-template/resources/prototype/ecr.tf
@@ -1,5 +1,5 @@
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.4"
 
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
@@ -17,5 +17,6 @@ resource "kubernetes_secret" "ecr-repo" {
     repo_url          = module.ecr-repo.repo_url
     access_key_id     = module.ecr-repo.access_key_id
     secret_access_key = module.ecr-repo.secret_access_key
+    repo_arn          = module.ecr-repo.repo_arn
   }
 }
