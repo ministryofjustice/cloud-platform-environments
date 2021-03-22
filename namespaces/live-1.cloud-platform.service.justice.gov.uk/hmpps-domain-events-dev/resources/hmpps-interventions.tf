5b0f48b0b704a6849bcd85c9d5ddaaa976405bb1
David Lantos <david.lantos@gmail.com>
2021-03-22 12:31:53 +0000

    Add the topic ARN to interventions research env
    We created a separate environment to use in user research, which needs
    separate configuration

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-interventions.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-interventions.tf
@@ -10,3 +10,16 @@ resource "kubernetes_secret" "intervention_global_events_sns" {
     topic_arn         = module.hmpps-domain-events.topic_arn
   }
 }
+
+resource "kubernetes_secret" "intervention_research_global_events_sns" {
+  metadata {
+    name      = "hmpps-domain-events-topic"
+    namespace = "hmpps-interventions-research"
+  }
+
+  data = {
+    access_key_id     = module.hmpps-domain-events.access_key_id
+    secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
+  }
+}
