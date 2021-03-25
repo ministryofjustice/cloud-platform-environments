5d3a5c326b917c7daa1ed9a3b2c3cc9dce80a257
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2021-03-25 09:37:34 +0000

    Remove non prod zones

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/route53.tf
@@ -29,38 +29,13 @@ resource "aws_route53_record" "grafana_platform_amazonses_dkim_record" {
   records = ["${element(aws_ses_domain_dkim.grafana_platform.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
-resource "aws_route53_record" "hmpps_ems_ac_dev_zone" {
-  zone_id = aws_route53_zone.route53_zone.zone_id
-  name    = "acquisitive-crime.dev.${var.domain}"
-  type    = "NS"
-  ttl     = "600"
-  records = ["ns-1596.awsdns-07.co.uk.", "ns-1273.awsdns-31.org.", "ns-940.awsdns-53.net.", "ns-446.awsdns-55.com."]
-}
-
-#### Commented out while we test the development zone
-
-# resource "aws_route53_record" "hmpps_ems_ac_test_zone" {
-#   zone_id = aws_route53_zone.route53_zone.zone_id
-#   name    = "acquisitive-crime.test.${var.domain}"
-#   type    = "NS"
-#   ttl     = "600"
-#   records = ["ns-1536.awsdns-00.co.uk.", "ns-0.awsdns-00.com.", "ns-1024.awsdns-00.org.", "ns-512.awsdns-00.net."]
-# }
-
-# resource "aws_route53_record" "hmpps_ems_ac_preprod_zone" {
-#   zone_id = aws_route53_zone.route53_zone.zone_id
-#   name    = "acquisitive-crime.preprod.${var.domain}"
-#   type    = "NS"
-#   ttl     = "600"
-#   records = ["ns-1536.awsdns-00.co.uk.", "ns-0.awsdns-00.com.", "ns-1024.awsdns-00.org.", "ns-512.awsdns-00.net."]
-# }
-
+# # Acquisitive Crime Production
 # resource "aws_route53_record" "hmpps_ems_ac_prod_zone" {
 #   zone_id = aws_route53_zone.route53_zone.zone_id
 #   name    = "acquisitive-crime.${var.domain}"
 #   type    = "NS"
 #   ttl     = "600"
-#   records = ["ns-1536.awsdns-00.co.uk.", "ns-0.awsdns-00.com.", "ns-1024.awsdns-00.org.", "ns-512.awsdns-00.net."]
+#   records = ["TBC", "TBC", "TBC", "TBC"]
 # }
 
 resource "kubernetes_secret" "route53_zone_sec" {
