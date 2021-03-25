25f107ac41142be47da6b1b0e0ede3be9c68449a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2021-03-25 10:34:27 +0000

    Adding HMPPS EMS AC Test NS

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-dev/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-dev/resources/route53.tf
@@ -21,14 +21,14 @@ resource "aws_route53_zone" "route53_zone" {
 #   records = ["TBC", "TBC", "TBC", "TBC"]
 # }
 
-# # Acquisitive Crime Test
-# resource "aws_route53_record" "hmpps_ems_ac_test_zone" {
-#   zone_id = aws_route53_zone.route53_zone.zone_id
-#   name    = "ac.test.${var.domain}"
-#   type    = "NS"
-#   ttl     = "600"
-#   records = ["TBC", "TBC", "TBC", "TBC"]
-# }
+# Acquisitive Crime Test
+resource "aws_route53_record" "hmpps_ems_ac_test_zone" {
+  zone_id = aws_route53_zone.route53_zone.zone_id
+  name    = "ac.test.${var.domain}"
+  type    = "NS"
+  ttl     = "600"
+  records = ["ns-1390.awsdns-45.org.", "ns-99.awsdns-12.com.", "ns-535.awsdns-02.net.", "ns-1979.awsdns-55.co.uk"]
+}
 
 # # Acquisitive Crime PreProd
 # resource "aws_route53_record" "hmpps_ems_ac_preprod_zone" {
