2c3117d35637ff64fdf3c5f6d19dc9e9fc929db7
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2021-03-25 09:25:19 +0000

    Update Application tag Add EM short domain Placeholder for AC Dev zone

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-dev/resources/route53.tf
@@ -0,0 +1,22 @@
+resource "aws_route53_zone" "route53_zone" {
+  name = var.domain
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+# Acquisitive Crime Development
+resource "aws_route53_record" "hmpps_ems_ac_dev_zone" {
+  zone_id = aws_route53_zone.route53_zone.zone_id
+  name    = "ac.dev.${var.domain}"
+  type    = "NS"
+  ttl     = "600"
+  records = ["TBC", "TBC", "TBC", "TBC"]
+}
