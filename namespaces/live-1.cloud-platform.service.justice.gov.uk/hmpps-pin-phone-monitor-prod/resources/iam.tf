bcd6b0fbf5f5efee5c1493047ec17861103da6c0
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2021-03-30 10:17:26 +0100

    PPD-21 - add production namespace for prod pin phone monitoring service (#4553)
    * PPD-21 - add production namespace for prod pin phone monitoring service
    
    * PPD-21 - Remove xml from filter list
    
    * PPD-21 - Remove ounused ibject deletion event notification
    
    * PPD-21 - Temporarily remove prometheus until production environment is running.

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-prod/resources/iam.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_user" "bt_upload_user" {
+  name = "hmpps-pin-phone-monitor-prod-bt-upload-user"
+  path = "/system/hmpps-pin-phone-monitor-prod-upload-users/"
+}
+
+resource "aws_iam_access_key" "bt_upload_user_key" {
+  user = aws_iam_user.bt_upload_user.name
+}
+
+data "aws_iam_policy_document" "bt_upload_policy" {
+  statement {
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*.mp3",
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*.flac",
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*.wav",
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*.json"
+    ]
+  }
+
+  statement {
+    effect = "Deny"
+
+    actions = ["s3:*"]
+
+    resources = [
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}",
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_iam_user_policy" "bt_upload_policy" {
+  name   = "hmpps-pin-phone-monitor-bt-upload-policy-prod"
+  policy = data.aws_iam_policy_document.bt_upload_policy.json
+  user   = aws_iam_user.bt_upload_user.name
+}
+
+resource "kubernetes_secret" "hmpps_pin_phone_monitor_bt_upload_user" {
+  metadata {
+    name      = "hmpps-pin-phone-monitor-bt-upload-user-prod"
+    namespace = var.namespace
+  }
+
+  data = {
+    hmpps_pin_phone_monitor_upload_user_arn               = aws_iam_user.bt_upload_user.arn
+    hmpps_pin_phone_monitor_upload_user_access_key_id     = aws_iam_access_key.bt_upload_user_key.id
+    hmpps_pin_phone_monitor_upload_user_secret_access_key = aws_iam_access_key.bt_upload_user_key.secret
+  }
+}
+
