d776e11db204c8dd161110b7bdb234cb985c0c1a
mayowaadeniyi <64010092+mayowaadeniyi@users.noreply.github.com>
2021-04-13 17:22:52 +0100

    LAA CLA Frontend - Create route53 zone (#4602)
    * Create route53 resource
    
    * Add 'A' record and use correct domain name

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/route53.tf
@@ -0,0 +1,34 @@
+resource "aws_route53_zone" "cla_frontend_route53_zone" {
+  name = "cases.civillegaladvice.service.gov.uk"
+
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business-unit
+    application            = var.application
+    is-production          = var.is-production
+    environment-name       = var.environment-name
+    infrastructure-support = var.infrastructure-support
+  }
+}
+
+resource "kubernetes_secret" "cla_frontend_route53_zone_sec" {
+  metadata {
+    name      = "cla-frontend-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.cla_frontend_route53_zone.zone_id
+  }
+}
+
+resource "aws_route53_record" "add_a_record" {
+  name    = "."
+  zone_id = aws_route53_zone.cla_frontend_route53_zone.zone_id
+  type    = "A"
+  alias {
+    name                   = "dualstack.cla-front-elbprodf-1o815cnz2w3lh-1554019512.eu-west-1.elb.amazonaws.com."
+    zone_id                = aws_route53_zone.cla_frontend_route53_zone.zone_id
+    evaluate_target_health = true
+  }
+}
