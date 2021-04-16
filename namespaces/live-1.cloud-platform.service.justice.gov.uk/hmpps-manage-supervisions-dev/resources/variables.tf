9c63d54fb107edbd7f8551b7fcab4531accd8bff
Ed Brindley <ed@maidavale.org>
2021-04-16 17:01:34 +0100

    Add Manage Supervisions dev namespace (#4634)

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-manage-supervisions-dev/resources/variables.tf
@@ -0,0 +1,54 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Manage Supervisions"
+}
+
+variable "namespace" {
+  default = "hmpps-manage-supervisions-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-probation-end-to-end"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "steel-thread@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "probation-end-to-end-tech"
+}
+
+variable "github_owner" {
+  description = "Required by the github terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
