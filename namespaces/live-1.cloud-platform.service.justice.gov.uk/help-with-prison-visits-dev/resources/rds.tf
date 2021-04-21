10956de4ac42237672bdece9383ac7a7ba5ec531
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-04-21 17:22:10 +0100

    Try running a sqlserver rds instance

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
@@ -17,6 +17,26 @@ module "hwpv_rds" {
   }
 }
 
+module "hwpv_rds_sqlserver" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.14.1"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  db_engine              = "sqlserver-se"
+  db_engine_version      = "15.00.4073.23.v1"
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+
+  rds_family = "sqlserver-se-15.0"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 resource "kubernetes_secret" "hwpv_rds" {
   metadata {
     name      = "rds-instance-output"
@@ -35,3 +55,19 @@ resource "kubernetes_secret" "hwpv_rds" {
   }
 }
 
+resource "kubernetes_secret" "hwpv_rds_sqlserver" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hwpv_rds_sqlserver.rds_instance_endpoint
+    database_name         = module.hwpv_rds_sqlserver.database_name
+    database_username     = module.hwpv_rds_sqlserver.database_username
+    database_password     = module.hwpv_rds_sqlserver.database_password
+    rds_instance_address  = module.hwpv_rds_sqlserver.rds_instance_address
+    access_key_id         = module.hwpv_rds_sqlserver.access_key_id
+    secret_access_key     = module.hwpv_rds_sqlserver.secret_access_key
+  }
+}
