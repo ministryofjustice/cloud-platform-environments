c995d3ef54cb9217d6552915463fbcf4cde2d26e
Matt <34448412+mattops@users.noreply.github.com>
2021-04-22 15:09:23 +0100

    Test sqlserver rds instance (#4664)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
@@ -17,7 +17,7 @@ module "hwpv_rds" {
   }
 }
 
-module "hwpv_rds_sqlserver" {
+module "hwpv_sqlserver" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=support_sqlserver"
   cluster_name           = var.cluster_name
   team_name              = var.team_name
@@ -25,13 +25,13 @@ module "hwpv_rds_sqlserver" {
   application            = var.application
   is-production          = var.is_production
   namespace              = var.namespace
-  db_engine              = "sqlserver-se"
+  db_engine              = "sqlserver-web"
   db_engine_version      = "15.00"
-  db_instance_class      = "db.m5.large"
-  db_allocated_storage   = "200"
+  db_instance_class      = "db.t3.small"
+  db_allocated_storage   = "20"
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure-support
-  rds_family             = "sqlserver-se-15.0"
+  rds_family             = "sqlserver-web-15.0"
   db_parameter           = []
   license_model          = "license-included"
 
@@ -58,18 +58,18 @@ resource "kubernetes_secret" "hwpv_rds" {
   }
 }
 
-resource "kubernetes_secret" "hwpv_rds_sqlserver" {
+resource "kubernetes_secret" "hwpv_sqlserver" {
   metadata {
     name      = "rds-sqlserver-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    rds_instance_endpoint = module.hwpv_rds_sqlserver.rds_instance_endpoint
-    database_username     = module.hwpv_rds_sqlserver.database_username
-    database_password     = module.hwpv_rds_sqlserver.database_password
-    rds_instance_address  = module.hwpv_rds_sqlserver.rds_instance_address
-    access_key_id         = module.hwpv_rds_sqlserver.access_key_id
-    secret_access_key     = module.hwpv_rds_sqlserver.secret_access_key
+    rds_instance_endpoint = module.hwpv_sqlserver.rds_instance_endpoint
+    database_username     = module.hwpv_sqlserver.database_username
+    database_password     = module.hwpv_sqlserver.database_password
+    rds_instance_address  = module.hwpv_sqlserver.rds_instance_address
+    access_key_id         = module.hwpv_sqlserver.access_key_id
+    secret_access_key     = module.hwpv_sqlserver.secret_access_key
   }
 }
