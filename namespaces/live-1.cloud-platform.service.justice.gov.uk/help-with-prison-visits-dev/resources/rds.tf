686e7da3cd04f5ae811142f739097be45fe12189
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2021-04-22 10:50:01 +0100

    try exact engine version

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-dev/resources/rds.tf
@@ -26,7 +26,7 @@ module "hwpv_rds_sqlserver" {
   is-production          = var.is_production
   namespace              = var.namespace
   db_engine              = "sqlserver-se"
-  db_engine_version      = "15.00"
+  db_engine_version      = "15.00.4073.23.v1"
   db_instance_class      = "db.m5.large"
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure-support
