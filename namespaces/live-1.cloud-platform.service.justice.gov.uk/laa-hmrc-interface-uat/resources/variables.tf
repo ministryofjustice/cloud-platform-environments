dde594e83e6a83f88f669d5cddc521d8d055b255
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-05-13 09:35:19 +0100

    Remove unused variable "cluster_state_bucket"
    In namespace laa-hmrc-interface-uat

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-hmrc-interface-uat/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-hmrc-interface-uat/resources/variables.tf
@@ -2,9 +2,6 @@
 variable "cluster_name" {
 }
 
-variable "cluster_state_bucket" {
-}
-
 variable "application" {
   description = "Name of Application you are deploying"
   default     = "LAA-HMRC Interface Service API"
