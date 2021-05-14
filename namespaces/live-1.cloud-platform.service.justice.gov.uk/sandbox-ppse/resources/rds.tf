c0ccb3e4477d1e8e6314e5dee5933a1f9bc092cf
Ken Friesen <ken.friesen@equalexperts.com>
2021-05-14 15:40:14 +0100

    Use latest RDS module

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/sandbox-ppse/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/sandbox-ppse/resources/rds.tf
@@ -1,6 +1,6 @@
 
 module "rds" {
-  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.13.1"
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16"
   cluster_name         = var.cluster_name
   cluster_state_bucket = var.cluster_state_bucket
   team_name            = var.team_name
