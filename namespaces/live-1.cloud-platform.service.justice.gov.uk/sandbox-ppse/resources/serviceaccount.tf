3e56f9263be6679e297261080c573057d17b69c4
mikedunford73 <mike.dunford@digital.justice.gov.uk>
2021-05-19 16:58:17 +0100

    Adding circleci service account for sandbox-ppse

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/sandbox-ppse/resources/serviceaccount.tf
@@ -0,0 +1,10 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+
+  namespace = var.namespace
+  serviceaccount_name = "circleci"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  # github_repositories = ["my-repo"]
+}
