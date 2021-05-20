d2f67ae79bdf103aae599fc4cfdc1f6a4556d024
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-05-20 18:04:37 +0100

    Remove github_repositories to fix the failing pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/vijay-namespace-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vijay-namespace-dev/resources/serviceaccount.tf
@@ -3,5 +3,4 @@ module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
 
   namespace           = var.namespace
-  github_repositories = [""]
 }
