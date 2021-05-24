93357f0682f7f28459d09700d34cd8c8baf1de0a
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-05-24 16:49:09 +0100

    DT-2074: updated names for the redis elasticache in offender-event-ui

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/elasticache.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/elasticache.tf
@@ -2,7 +2,7 @@
 # registers Elasticache for ReDiS
 ################################################################################
 
-module "offender_events_elasticache_redis" {
+module "offender_events_ui_elasticache_redis" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.1"
   cluster_name           = var.cluster_name
   application            = var.application
@@ -21,21 +21,21 @@ module "offender_events_elasticache_redis" {
   }
 }
 
-resource "kubernetes_secret" "offender_events_elasticache_redis" {
+resource "kubernetes_secret" "offender_events_ui_elasticache_redis" {
   metadata {
-    name      = "elasticache-redis"
+    name      = "offender-events-ui-elasticache-redis"
     namespace = var.namespace
   }
 
   data = {
-    primary_endpoint_address = module.offender_events_elasticache_redis.primary_endpoint_address
-    auth_token               = module.offender_events_elasticache_redis.auth_token
-    member_clusters          = jsonencode(module.offender_events_elasticache_redis.member_clusters)
+    primary_endpoint_address = module.offender_events_ui_elasticache_redis.primary_endpoint_address
+    auth_token               = module.offender_events_ui_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.offender_events_ui_elasticache_redis.member_clusters)
   }
 }
 
 resource "aws_elasticache_parameter_group" "event_store" {
-  name   = "offender-events-event-parameter-group"
+  name   = "offender-events-ui-event-parameter-group"
   family = "redis6.x"
 
   # Needed in order to get spring boot to expire items from the redis cache
