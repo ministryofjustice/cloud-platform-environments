14e2fa79721752a708288213505ff7d50eac1ebc
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-05-28 10:08:16 +0100

    Set up a secondary/read-replicat SQL Server for sandbox-ppse.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/sandbox-ppse/resources/sql-server.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/sandbox-ppse/resources/sql-server.tf
@@ -1,3 +1,7 @@
+##
+## main/primary database
+##
+
 module "sql-server-main" {
   source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.1"
   cluster_name  = var.cluster_name
@@ -30,13 +34,13 @@ resource "kubernetes_secret" "sql-server-main" {
   }
 
   data = {
-    rds_instance_endpoint = module.rds.rds_instance_endpoint
-    database_name         = module.rds.database_name
-    database_username     = module.rds.database_username
-    database_password     = module.rds.database_password
-    rds_instance_address  = module.rds.rds_instance_address
-    access_key_id         = module.rds.access_key_id
-    secret_access_key     = module.rds.secret_access_key
+    rds_instance_endpoint = module.sql-server-main.rds_instance_endpoint
+    database_name         = module.sql-server-main.database_name
+    database_username     = module.sql-server-main.database_username
+    database_password     = module.sql-server-main.database_password
+    rds_instance_address  = module.sql-server-main.rds_instance_address
+    access_key_id         = module.sql-server-main.access_key_id
+    secret_access_key     = module.sql-server-main.secret_access_key
   }
 }
 
@@ -47,7 +51,65 @@ resource "kubernetes_config_map" "sql-server-main" {
   }
 
   data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
+    database_name = module.sql-server-main.database_name
+    db_identifier = module.sql-server-main.db_identifier
+  }
+}
+
+##
+## secondary/read-replica database
+##
+
+module "sql-server-rr" {
+  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.1"
+  cluster_name  = var.cluster_name
+  team_name     = var.team_name
+  business-unit = var.business_unit
+  application   = var.application
+  is-production = var.is_production
+  namespace     = var.namespace
+
+  db_engine              = "sqlserver-ee"
+  db_engine_version      = "11.00"
+  db_instance_class      = "db.t3.xlarge"
+  db_allocated_storage   = "20"
+  db_parameter           = []
+  license_model          = "license-included"
+  rds_family             = "sqlserver-ee-11.0"
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "sql-server-rr" {
+  metadata {
+    name      = "sql-server-rr"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.sql-server-rr.rds_instance_endpoint
+    database_name         = module.sql-server-rr.database_name
+    database_username     = module.sql-server-rr.database_username
+    database_password     = module.sql-server-rr.database_password
+    rds_instance_address  = module.sql-server-rr.rds_instance_address
+    access_key_id         = module.sql-server-rr.access_key_id
+    secret_access_key     = module.sql-server-rr.secret_access_key
+  }
+}
+
+resource "kubernetes_config_map" "sql-server-rr" {
+  metadata {
+    name      = "sql-server-rr"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name = module.sql-server-rr.database_name
+    db_identifier = module.sql-server-rr.db_identifier
   }
 }
