e53f64ba352dfb16ae8abe70e491636e42bec4c2
Mark Berridge <mark.berridge@digital.justice.gov.uk>
2021-06-02 11:24:36 +0100

    Provision RDS SQL Server to wmt-dev namespace (#4876)
    Provision RDS SQL Server to wmt-dev

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/wmt-dev/resources/rds.tf
@@ -0,0 +1,61 @@
+module "rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.3"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  # performance insights
+  performance_insights_enabled = false
+
+  # RDS SQL Server
+  db_engine            = "sqlserver-web"
+  db_engine_version    = "15.00"
+  rds_family           = "sqlserver-web-15.00"
+  db_parameter         = []
+  db_instance_class    = "db.t3.small"
+  db_allocated_storage = "10"
+  license_model        = "license-included"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_name         = module.rds.database_name
+    database_username     = module.rds.database_username
+    database_password     = module.rds.database_password
+    rds_instance_address  = module.rds.rds_instance_address
+    access_key_id         = module.rds.access_key_id
+    secret_access_key     = module.rds.secret_access_key
+  }
+  /* You can replace all of the above with the following, if you prefer to
+   * use a single database URL value in your application code:
+   *
+   * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
+   */
+}
+
+resource "kubernetes_config_map" "rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name = module.rds.database_name
+    db_identifier = module.rds.db_identifier
+  }
+}
