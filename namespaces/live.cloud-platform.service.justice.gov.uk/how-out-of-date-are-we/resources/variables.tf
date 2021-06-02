5d3b2e53167464343bbf8f684e9dab4c8f0cbf89
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-06-02 12:36:56 +0300

    live hoodaw (#4875)
    * live hoodaw

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/how-out-of-date-are-we/resources/variables.tf
@@ -0,0 +1,44 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "How Out Of Date Are We"
+}
+
+variable "namespace" {
+  default = "how-out-of-date-are-we"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "Platforms"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "webops"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "platforms@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "cloud-platform"
+}
