39d326e161c3cf0764427da6a361e96935d96941
Peter Phillips <peter@greenthistle.co.uk>
2021-06-09 17:47:37 +0100

    DT-2115: ✨ Add back in rds password

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-nhs-update-prod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/prison-to-nhs-update-prod/resources/rds.tf
@@ -21,6 +21,10 @@ module "dps_rds" {
   }
 }
 
+resource "random_id" "prison_to_nhs_role_password" {
+  byte_length = 32
+}
+
 resource "kubernetes_secret" "dps_rds" {
   metadata {
     name      = "dps-rds-instance-output"
@@ -35,6 +39,7 @@ resource "kubernetes_secret" "dps_rds" {
     rds_instance_address  = module.dps_rds.rds_instance_address
     access_key_id         = module.dps_rds.access_key_id
     secret_access_key     = module.dps_rds.secret_access_key
+    prison_to_nhs_update_password = random_id.prison_to_nhs_role_password.b64_std
   }
 }
 
