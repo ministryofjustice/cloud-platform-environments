2560176924e0c14aca8bfc297df7e7ef54177870
Siva Sighakolli <Siva.Sighakolli@digital.justice.gov.uk>
2021-06-09 10:17:05 +0100

    Removed NS and fixed the domain variable

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-prod/resources/route53.tf
@@ -36,16 +36,8 @@ resource "aws_route53_zone" "justicedata_route53_zone_ns" {
   name = "data.justice.gov.uk"
 }
 
-resource "aws_route53_record" "justicedata_route53_zone_ns" {
-  allow_overwrite = true
-  name            = "data.justice.gov.uk"
-  ttl             = 172800
-  type            = "NS"
-  zone_id         = aws_route53_zone.justicedata_route53_zone.zone_id
-}
-
 resource "aws_route53_record" "justicedata_route53_zone_cname0_record" {
-  name    = "_0202d4e448b9dc698a779ca6faa760a8.$var.domain"
+  name    = "_0202d4e448b9dc698a779ca6faa760a8.${var.domain}"
   zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
   type    = "CNAME"
   records = ["F5C15BF318A1459FA7FD064E46A8C336.4651BB299FC506838CC583D3533979EF.22ab6168adc1c019f687.comodoca.com."]
@@ -53,7 +45,7 @@ resource "aws_route53_record" "justicedata_route53_zone_cname0_record" {
 }
 
 resource "aws_route53_record" "justicedata_route53_zone_cname1_record" {
-  name    = "_6e66bde5693afcf24e3c6f6f1eb5d62a.$var.domain"
+  name    = "_6e66bde5693afcf24e3c6f6f1eb5d62a.${var.domain}"
   zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
   type    = "CNAME"
   records = ["27D48CD4FAA246277AD492FE0854993E.949CF41E1AFB5F686396AA072AB12A09.21c34fc0440021bd4f98.comodoca.com."]
