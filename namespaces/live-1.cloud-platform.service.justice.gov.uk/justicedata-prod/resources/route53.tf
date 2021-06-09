052cc1de67e17efe7c57c58afc05229c2f854357
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2021-06-09 09:16:21 +0100

    Justicedata - added A NS CNAME records (#4933)
    * Added route records
    
    * updated the cname records

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-prod/resources/route53.tf
@@ -23,3 +23,40 @@ resource "kubernetes_secret" "justicedata_route53_zone_sec" {
     zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
   }
 }
+
+resource "aws_route53_record" "justicedata_route53_zone_a_1" {
+  zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
+  name    = "data.justice.gov.uk"
+  type    = "A"
+  records = ["51.179.195.142"]
+  ttl     = "300"
+}
+
+resource "aws_route53_zone" "justicedata_route53_zone_ns" {
+  name = "data.justice.gov.uk"
+}
+
+resource "aws_route53_record" "justicedata_route53_zone_ns" {
+  allow_overwrite = true
+  name            = "data.justice.gov.uk"
+  ttl             = 172800
+  type            = "NS"
+  zone_id         = aws_route53_zone.justicedata_route53_zone.zone_id
+}
+
+resource "aws_route53_record" "justicedata_route53_zone_cname0_record" {
+  name    = "_0202d4e448b9dc698a779ca6faa760a8.$var.domain"
+  zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
+  type    = "CNAME"
+  records = ["F5C15BF318A1459FA7FD064E46A8C336.4651BB299FC506838CC583D3533979EF.22ab6168adc1c019f687.comodoca.com."]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "justicedata_route53_zone_cname1_record" {
+  name    = "_6e66bde5693afcf24e3c6f6f1eb5d62a.$var.domain"
+  zone_id = aws_route53_zone.justicedata_route53_zone.zone_id
+  type    = "CNAME"
+  records = ["27D48CD4FAA246277AD492FE0854993E.949CF41E1AFB5F686396AA072AB12A09.21c34fc0440021bd4f98.comodoca.com."]
+  ttl     = "300"
+}
+
