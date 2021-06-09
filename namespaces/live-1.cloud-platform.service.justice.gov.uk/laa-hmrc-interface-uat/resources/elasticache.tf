3b9a60beee17ba02557018f864277837df1a1b19
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-06-09 11:54:39 +0100

    Add elasticache to laa-hmrc-interface-uat
    This will be used for caching and temp storage of key-values pairs

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-hmrc-interface-uat/resources/elasticache.tf
@@ -0,0 +1,34 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "elasticache" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.1"
+
+  cluster_name  = var.cluster_name
+  team_name     = var.team_name
+  business-unit = var.business_unit
+  application   = var.application
+  is-production = var.is_production
+  namespace     = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "elasticache" {
+  metadata {
+    name      = "elasticache"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.elasticache.primary_endpoint_address
+    member_clusters          = jsonencode(module.elasticache.member_clusters)
+    auth_token               = module.elasticache.auth_token
+  }
+}
+
