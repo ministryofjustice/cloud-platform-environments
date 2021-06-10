5d36073639b208071c6678d32e30f635f014c75b
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-06-10 07:51:56 +0100

    Correct vars file

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/variables.tf
@@ -33,9 +33,3 @@ variable "is_production" {
 variable "cluster_name" {
 }
 
-variable "number_cache_clusters" {
-  description = "The number of cache clusters (primary and replicas) this replication group will have. Default is 2"
-  type        = string
-  default     = "2"
-}
-
