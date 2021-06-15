9b08ef116e7bd8868fc318a4304226ac110aca13
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-06-15 10:13:07 +0100

    Add additional serviceaccount_rules for circleci

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount.tf
@@ -3,4 +3,74 @@ module "serviceaccount" {
 
   namespace           = var.namespace
   serviceaccount_name = "circleci"
+
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+        "serviceaccounts",
+        "configmaps",
+      ]
+      verbs = [
+        "update",
+        "patch",
+        "get",
+        "create",
+        "delete",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "networking.k8s.io",
+        "monitoring.coreos.com",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "servicemonitors",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "networking.k8s.io",
+        "monitoring.coreos.com",
+        "certmanager.k8s.io"
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "servicemonitors",
+        "certificates"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+  ]
 }
