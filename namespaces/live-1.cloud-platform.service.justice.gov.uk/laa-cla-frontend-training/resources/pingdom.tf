25b23aaf7923bdcf6082c9dc5e159b8041dbc416
said-moj <said.shire@digital.justice.gov.uk>
2021-06-21 09:32:13 +0100

    Adding integration id

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-frontend-training/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/laa-cla-frontend-training/resources/pingdom.tf
@@ -14,5 +14,5 @@ resource "pingdom_check" "laa-cla-frontend-pingdom" {
   port                     = 443
   tags                     = "businessunit_${var.business-unit},application_cla-frontend,component_ping,isproduction_${var.is-production},environment_${var.environment-name},infrastructuresupport_cla-frontend"
   probefilters             = "region:EU"
-  integrationids           = []
+  integrationids           = [107761]
 }
