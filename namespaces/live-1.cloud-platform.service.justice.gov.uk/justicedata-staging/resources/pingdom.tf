8a6017c7a27d6433f59d047922b37cc722e8b738
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2021-06-21 10:46:17 +0100

    Justicedata - Remove Staging pingdom changes (#5018)
    * removed staging pingdom
    
    * added provider config
    
    * Removed the pingdom section.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-staging/resources/pingdom.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-staging/resources/pingdom.tf
@@ -1,18 +1,2 @@
 provider "pingdom" {
 }
-
-resource "pingdom_check" "justicedata-pingdom" {
-  type                     = "http"
-  name                     = "justicedata - staging - cloud-platform"
-  host                     = "staging.data.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/"
-  encryption               = true
-  port                     = 443
-  tags                     = "businessunit_hmpps,application_justicedata,component_ping,isproduction_false,environment_staging,performance_hub_dev"
-  probefilters             = "region:EU"
-  integrationids           = [114774]
-}
