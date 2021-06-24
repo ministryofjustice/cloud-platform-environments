886c469401a1d675504889272231e440b09ff8a2
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-06-24 14:40:25 +0100

    Migrate cloud-platform-migration namespace from live-1 to live
    Connects to
    https://github.com/ministryofjustice/cloud-platform/issues/2956

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-migration/resources/serviceaccount.tf
@@ -0,0 +1,9 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+
+  namespace = var.namespace
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["cloud-platform-reference-app"]
+}
