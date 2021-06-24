8c89f69e07fadba43ab0ba1e14551293ebc0d8f3
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-06-24 12:49:25 +0100

    WFP-324 create s3 bucket for extract

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/s3.tf
@@ -0,0 +1,32 @@
+module "hmpps-workload-dev-s3-bucket" {
+
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  versioning             = true
+
+  providers = {
+    aws = aws.london
+  }
+  
+}
+
+resource "kubernetes_secret" "hmpps-workload-dev-s3-bucket"  {
+  metadata {
+    name      = "s3-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.hmpps-workload-dev-s3-bucket.access_key_id
+    secret_access_key = module.hmpps-workload-dev-s3-bucket.secret_access_key
+    bucket_arn        = module.hmpps-workload-dev-s3-bucket.bucket_arn
+    bucket_name       = module.hmpps-workload-dev-s3-bucket.bucket_name
+  }
+}
