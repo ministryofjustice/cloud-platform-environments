50102e4a00380bfa0b5f4519c69f3c8050674740
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2021-06-25 15:35:30 +0100

    PAT-1832 - adds an IAM user to allow selected calls to be processed by AWS transcribe & translate (#5071)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/iam.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/iam.tf
@@ -61,3 +61,51 @@ resource "kubernetes_secret" "hmpps_pin_phone_monitor_upload_user" {
   }
 }
 
+resource "aws_iam_user" "call_processing_user" {
+  name = "hmpps-pin-phone-monitor-call-processing-user-${random_id.upload.hex}"
+  path = "/system/hmpps-pin-phone-monitor-call-processing-users/"
+}
+
+resource "aws_iam_access_key" "call_processing_user_key" {
+  user = aws_iam_user.call_processing_user.name
+}
+
+data "aws_iam_policy_document" "call_processing_policy" {
+  statement {
+    actions = ["s3:*Object"]
+
+    resources = [
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "transcribe:*",
+      "translate:*",
+      "comprehend:DetectDominantLanguage"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "call_processing_policy" {
+  name   = "hmpps-pin-phone-monitor-call-processing-policy"
+  policy = data.aws_iam_policy_document.call_processing_policy.json
+  user   = aws_iam_user.call_processing_user.name
+}
+
+resource "kubernetes_secret" "hmpps_pin_phone_monitor_call_processing_user" {
+  metadata {
+    name      = "hmpps-pin-phone-monitor-call-processing-user"
+    namespace = var.namespace
+  }
+
+  data = {
+    hmpps_pin_phone_call_processing_user_access_key_id     = aws_iam_access_key.call_processing_user_key.id
+    hmpps_pin_phone_call_processing_user_secret_access_key = aws_iam_access_key.call_processing_user_key.secret
+  }
+}
