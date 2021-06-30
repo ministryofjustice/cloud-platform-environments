e353c19b03b31eeb6292c86ed1b60d02c00eec06
David Read <david.read@hackneyworkshop.com>
2021-06-30 18:16:24 +0100

    Enable ECR creds
    This should inject the ECR creds into Github secrets for my namespace.
    It didn't work last time because I didn't have the github_owner variable.
    I'm leaving out the github_token variable also mentioned, to see if it
    is really necessary.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/davidread-dev/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/davidread-dev/resources/ecr.tf
@@ -7,7 +7,7 @@ module "ecr-repo" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
-  # github_repositories = ["helloworld-ruby-app-davidread"]
+  github_repositories = ["helloworld-ruby-app-davidread"]
 }
 
 resource "kubernetes_secret" "ecr-repo" {
