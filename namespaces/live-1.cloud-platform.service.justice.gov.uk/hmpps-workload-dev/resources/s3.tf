dbd5055a5b60033a282e6ff86c6b02c8bd25b51a
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-07-01 07:16:33 +0100

    WFP-324 grant write permission to bucket for account to write NART extract

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/s3.tf
@@ -1,12 +1,12 @@
 module "hmpps-workload-dev-s3-bucket" {
 
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  namespace              = var.namespace
-  environment-name       = var.environment
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
+  team_name = var.team_name
+  business-unit = var.business_unit
+  application = var.application
+  is-production = var.is_production
+  namespace = var.namespace
+  environment-name = var.environment
   infrastructure-support = var.infrastructure_support
 
   versioning = true
@@ -18,8 +18,8 @@ module "hmpps-workload-dev-s3-bucket" {
   lifecycle_rule = [
     {
       enabled = true
-      id      = "retire extracts after 30 days"
-      prefix  = "extract/"
+      id = "retire extracts after 30 days"
+      prefix = "extract/"
       noncurrent_version_expiration = [
         {
           days = 30
@@ -33,17 +33,8 @@ module "hmpps-workload-dev-s3-bucket" {
     }
   ]
 
-  /*
-   * The following are exampls of bucket and user policies. They are treated as
-   * templates. Currently, the only available variable is `$${bucket_arn}`.
-   *
-   */
 
-  /*
- * Allow a user (foobar) from another account (012345678901) to get objects from
- * this bucket.
- *
-   bucket_policy = <<EOF
+  bucket_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -51,7 +42,7 @@ module "hmpps-workload-dev-s3-bucket" {
             "Sid": "S3PutPolicy",
             "Effect": "Allow",
             "Principal": {
-                "AWS": "arn:aws:iam::${AWS_ACCOUNT_ID}:root"
+                "AWS": "arn:aws:iam::050243167760:root"
             },
             "Action": [
                 "s3:PutObject",
@@ -71,7 +62,7 @@ module "hmpps-workload-dev-s3-bucket" {
             "Sid": "S3ListPolicy",
             "Effect": "Allow",
             "Principal": {
-                "AWS": "arn:aws:iam::${AWS_ACCOUNT_ID}:root"
+                "AWS": "arn:aws:iam::050243167760:root"
             },
             "Action": [
                 "s3:List*",
@@ -87,20 +78,20 @@ module "hmpps-workload-dev-s3-bucket" {
     ]
 }
 EOF
-*/
+
 
 }
 
 resource "kubernetes_secret" "hmpps-workload-dev-s3-bucket" {
   metadata {
-    name      = "s3-bucket-output"
+    name = "s3-bucket-output"
     namespace = var.namespace
   }
 
   data = {
-    access_key_id     = module.hmpps-workload-dev-s3-bucket.access_key_id
+    access_key_id = module.hmpps-workload-dev-s3-bucket.access_key_id
     secret_access_key = module.hmpps-workload-dev-s3-bucket.secret_access_key
-    bucket_arn        = module.hmpps-workload-dev-s3-bucket.bucket_arn
-    bucket_name       = module.hmpps-workload-dev-s3-bucket.bucket_name
+    bucket_arn = module.hmpps-workload-dev-s3-bucket.bucket_arn
+    bucket_name = module.hmpps-workload-dev-s3-bucket.bucket_name
   }
 }
