60e421b7e04a30fdda44d97977fa26e896816815
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-14 20:10:10 +0100

    Revert "Restrict access to the `manage-recalls` S3 bucket to the VPC."

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/s3.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/s3.tf
@@ -1,23 +1,3 @@
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = [var.cluster_name == "live" ? "live-1" : var.cluster_name]
-  }
-}
-
-data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.selected.id
-
-  tags = {
-    SubnetType = "Private"
-  }
-}
-
-data "aws_subnet" "private" {
-  for_each = data.aws_subnet_ids.private.ids
-  id       = each.value
-}
-
 # Based on https://github.com/ministryofjustice/cloud-platform-terraform-s3-bucket/tree/master/example
 module "manage_recalls_s3_bucket_preprod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
@@ -36,32 +16,6 @@ module "manage_recalls_s3_bucket_preprod" {
   }
 
   versioning = false
-}
-
-# This policy restricts access to the bucket to applications running within the VPC.
-resource "aws_s3_bucket_policy" "manage_recalls_s3_bucket_policy_preprod" {
-  bucket = module.manage_recalls_s3_bucket_preprod.bucket_name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "SourceIP"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource = [
-          module.manage_recalls_s3_bucket_preprod.bucket_arn,
-          "${module.manage_recalls_s3_bucket_preprod.bucket_arn}/*",
-        ]
-        Condition = {
-          "NotIpAddress" = {
-            "aws:SourceIp" = [for s in data.aws_subnet.private : s.cidr_block]
-          }
-        }
-      },
-    ]
-  })
 }
 
 resource "kubernetes_secret" "manage_recalls_s3_bucket_preprod" {
