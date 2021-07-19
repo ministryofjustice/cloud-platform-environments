0ff93c1d1dfaf75622892a719670d613289fa347
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-19 17:11:24 +0100

    Fix repo name (#5141)
    * Fix repo name
    
    * Fix repo path for serviceaccount

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
@@ -2,5 +2,5 @@ module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
 
   namespace           = var.namespace
-  github_repositories = ["https://github.com/ministryofjustice/helloworld-poornima-dev"]
+  github_repositories = ["helloworld-poornima-dev"]
 }
