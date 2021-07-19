5c2793c305d45b47b1f4d01b43f0b49b10bad20d
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-07-19 14:03:11 +0100

    Setup an S3 bucket and SQL Server DB to house transferred Lumen data.
    This sets up an S3 bucket for the data import from Lumen, and a SQL
    Server instance to house the transferred database content.
    
    refs PUD-278, PUD-279

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/rds.tf
@@ -1,3 +1,7 @@
+##
+## PostgreSQL - Application Database
+##
+
 module "ppud_replacement_preprod_rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.3"
 
@@ -33,5 +37,48 @@ resource "kubernetes_secret" "ppud_replacement_preprod_rds_secrets" {
     name     = module.ppud_replacement_preprod_rds.database_name
     username = module.ppud_replacement_preprod_rds.database_username
     password = module.ppud_replacement_preprod_rds.database_password
+  }
+}
+
+##
+## SQL Server - Lumen/PPUD Copy
+##
+
+module "ppud_replica_preprod_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.3"
+
+  cluster_name           = var.cluster_name
+  namespace              = var.namespace
+  application            = var.application
+  business-unit          = var.business_unit
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  is-production          = var.is_production
+  team_name              = var.team_name
+
+  rds_name             = "ppud-replica-preprod"
+  rds_family           = "sqlserver-web-11.0"
+  db_engine            = "sqlserver-web"
+  db_engine_version    = "11.00"
+  db_instance_class    = "db.t3.small"
+  db_allocated_storage = "20"
+  license_model        = "license-included"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "ppud_replica_preprod_rds_secrets" {
+  metadata {
+    name      = "ppud-replica-database"
+    namespace = var.namespace
+  }
+
+  data = {
+    host     = module.ppud_replica_preprod_rds.rds_instance_address
+    name     = module.ppud_replica_preprod_rds.database_name
+    username = module.ppud_replica_preprod_rds.database_username
+    password = module.ppud_replica_preprod_rds.database_password
   }
 }
