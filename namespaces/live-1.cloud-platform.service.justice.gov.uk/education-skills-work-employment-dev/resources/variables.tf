a570e5adc7d530f1ab75a43ad29addcdc752128b
akeemfaulkner <contact@ake3m.com>
2021-07-21 12:58:35 +0100

    PE-4: adds education-skills-work-employment-dev namespace

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/education-skills-work-employment-dev/resources/variables.tf
@@ -0,0 +1,40 @@
+variable "application" {
+  default = "education-skills-work-employment"
+}
+
+variable "namespace" {
+  default = "education-skills-work-employment-dev"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "dps"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "cluster_name" {
+}
+
+
