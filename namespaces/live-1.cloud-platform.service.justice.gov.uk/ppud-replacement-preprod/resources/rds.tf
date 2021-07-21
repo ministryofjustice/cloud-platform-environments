b5af3c7c96a286578cc7d1e6ae5ed489ce77c9b9
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-07-21 11:59:33 +0100

    Temporarily remove the PPUD replica RDS instance

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/rds.tf
@@ -44,43 +44,43 @@ resource "kubernetes_secret" "ppud_replacement_preprod_rds_secrets" {
 ## SQL Server - Lumen/PPUD Copy
 ##
 
-module "ppud_replica_preprod_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.3"
+# module "ppud_replica_preprod_rds" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.3"
 
-  cluster_name           = var.cluster_name
-  namespace              = var.namespace
-  application            = var.application
-  business-unit          = var.business_unit
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
-  is-production          = var.is_production
-  team_name              = var.team_name
+#   cluster_name           = var.cluster_name
+#   namespace              = var.namespace
+#   application            = var.application
+#   business-unit          = var.business_unit
+#   environment-name       = var.environment
+#   infrastructure-support = var.infrastructure_support
+#   is-production          = var.is_production
+#   team_name              = var.team_name
 
-  rds_name             = "ppud-replica-preprod"
-  rds_family           = "sqlserver-web-11.0"
-  db_engine            = "sqlserver-web"
-  db_engine_version    = "11.00"
-  db_instance_class    = "db.t3.small"
-  db_allocated_storage = "20"
-  db_name              = "PPUD_LIVE"
-  license_model        = "license-included"
+#   rds_name             = "ppud-replica-preprod"
+#   rds_family           = "sqlserver-web-11.0"
+#   db_engine            = "sqlserver-web"
+#   db_engine_version    = "11.00"
+#   db_instance_class    = "db.t3.small"
+#   db_allocated_storage = "20"
+#   db_name              = "PPUD_LIVE"
+#   license_model        = "license-included"
 
-  providers = {
-    aws = aws.london
-  }
-}
+#   providers = {
+#     aws = aws.london
+#   }
+# }
 
-resource "kubernetes_secret" "ppud_replica_preprod_rds_secrets" {
-  metadata {
-    name      = "ppud-replica-database"
-    namespace = var.namespace
-  }
+# resource "kubernetes_secret" "ppud_replica_preprod_rds_secrets" {
+#   metadata {
+#     name      = "ppud-replica-database"
+#     namespace = var.namespace
+#   }
 
-  data = {
-    host     = module.ppud_replica_preprod_rds.rds_instance_address
-    name     = module.ppud_replica_preprod_rds.database_name
-    username = module.ppud_replica_preprod_rds.database_username
-    password = module.ppud_replica_preprod_rds.database_password
-  }
-}
+#   data = {
+#     host     = module.ppud_replica_preprod_rds.rds_instance_address
+#     name     = module.ppud_replica_preprod_rds.database_name
+#     username = module.ppud_replica_preprod_rds.database_username
+#     password = module.ppud_replica_preprod_rds.database_password
+#   }
+# }
 
