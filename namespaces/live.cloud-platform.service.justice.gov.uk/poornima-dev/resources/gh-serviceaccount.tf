3f8389aef6eba100a1da147fe67cc45bd5d17db5
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-23 10:30:23 +0100

    Create seperate serviceaccount for live. Amending current one overwrites the current secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
@@ -3,8 +3,4 @@ module "serviceaccount" {
 
   namespace           = var.namespace
   github_repositories = ["helloworld-poornima-dev"]
-  github_actions_secret_kube_namespace = "KUBE_PROD_NAMESPACE"
-  github_actions_secret_kube_cert      = "KUBE_PROD_CERT"
-  github_actions_secret_kube_token     = "KUBE_PROD_TOKEN"
-  github_actions_secret_kube_cluster   = "KUBE_PROD_CLUSTER"
 }
