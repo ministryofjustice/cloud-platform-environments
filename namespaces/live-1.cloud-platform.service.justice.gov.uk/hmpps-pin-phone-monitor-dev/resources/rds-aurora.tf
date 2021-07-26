107027326dd12c91db3c282a05c1c1d21318ce43
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2021-07-26 14:21:31 +0100

    PAT-1981 - Create rds aurora cluster from db instance using replication

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
@@ -0,0 +1,56 @@
+module "rds_aurora" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.6"
+
+  team_name                   = var.team_name
+  business-unit               = var.business-unit
+  application                 = var.application
+  is-production               = var.is-production
+  namespace                   = var.namespace
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure-support
+  engine                      = "aurora-postgresql"
+  engine_version              = "10.16"
+  engine_mode                 = "provisioned"
+  replica_count               = 1
+  instance_type               = "db.t3.medium"
+  replication_source_identifier = "arn:aws:rds:eu-west-2:754256621582:db:cloud-platform-ba254827a34d9c54"
+  storage_encrypted           = true
+  apply_immediately           = true
+  cluster_name                = var.cluster_name
+  allow_major_version_upgrade = true
+  skip_final_snapshot         = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "random_id" "pin_phone_monitor_update_role_password" {
+  byte_length = 32
+}
+
+resource "random_id" "pin_phone_monitor_read_role_password" {
+  byte_length = 32
+}
+
+resource "kubernetes_secret" "pin_phone_monitor_rds_aurora" {
+  metadata {
+    name      = "pin-phone-monitor-rds-aurora-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_cluster_endpoint        = module.rds_aurora.rds_cluster_endpoint
+    rds_cluster_reader_endpoint = module.rds_aurora.rds_cluster_reader_endpoint
+    db_cluster_identifier       = module.rds_aurora.db_cluster_identifier
+    # refer to pin-phone-monitor-rds-aurora-migration secret for migrated db name and master user
+    # database_name               = module.rds_aurora.database_name
+    # database_username           = module.rds_aurora.database_username
+    database_password           = module.rds_aurora.database_password
+    database_update_password    = random_id.pin_phone_monitor_update_role_password.b64_url
+    database_read_password      = random_id.pin_phone_monitor_read_role_password.b64_url
+    access_key_id               = module.rds_aurora.access_key_id
+    secret_access_key           = module.rds_aurora.secret_access_key
+  }
+
+}
