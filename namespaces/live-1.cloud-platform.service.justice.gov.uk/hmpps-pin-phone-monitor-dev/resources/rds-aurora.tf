1a6250dd18d475045124bff7deeb44ed0391abde
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2021-07-26 17:54:52 +0100

    PAT-1981 - Create rds aurora cluster snapshot skipping migrated db at… (#5213)
    * PAT-1981 - Create rds aurora cluster snapshot skipping migrated db attributes
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
@@ -1,24 +1,24 @@
 module "rds_aurora" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.6"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=skip-name-var"
 
-  team_name                     = var.team_name
-  business-unit                 = var.business-unit
-  application                   = var.application
-  is-production                 = var.is-production
-  namespace                     = var.namespace
-  environment-name              = var.environment-name
-  infrastructure-support        = var.infrastructure-support
-  engine                        = "aurora-postgresql"
-  engine_version                = "10.16"
-  engine_mode                   = "provisioned"
-  replica_count                 = 1
-  instance_type                 = "db.t3.medium"
-  replication_source_identifier = "arn:aws:rds:eu-west-2:754256621582:db:cloud-platform-ba254827a34d9c54"
-  storage_encrypted             = true
-  apply_immediately             = true
-  cluster_name                  = var.cluster_name
-  allow_major_version_upgrade   = true
-  skip_final_snapshot           = true
+  team_name                   = var.team_name
+  business-unit               = var.business-unit
+  application                 = var.application
+  is-production               = var.is-production
+  namespace                   = var.namespace
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure-support
+  engine                      = "aurora-postgresql"
+  engine_version              = "10.16"
+  engine_mode                 = "provisioned"
+  replica_count               = 1
+  instance_type               = "db.t3.medium"
+  snapshot_identifier         = "arn:aws:rds:eu-west-2:754256621582:snapshot:hmpps-pin-phone-dev-pre-migration-20210723-1436"
+  storage_encrypted           = true
+  apply_immediately           = true
+  cluster_name                = var.cluster_name
+  allow_major_version_upgrade = true
+  skip_setting_when_migrated  = true
 
   providers = {
     aws = aws.london
@@ -43,9 +43,9 @@ resource "kubernetes_secret" "pin_phone_monitor_rds_aurora" {
     rds_cluster_endpoint        = module.rds_aurora.rds_cluster_endpoint
     rds_cluster_reader_endpoint = module.rds_aurora.rds_cluster_reader_endpoint
     db_cluster_identifier       = module.rds_aurora.db_cluster_identifier
-    # refer to pin-phone-monitor-rds-aurora-migration secret for migrated db name and master user
-    # database_name               = module.rds_aurora.database_name
-    # database_username           = module.rds_aurora.database_username
+    # refer to pin-phone-monitor-rds-aurora-migration secret for the migrated db name and master user
+    database_name            = module.rds_aurora.database_name
+    database_username        = module.rds_aurora.database_username
     database_password        = module.rds_aurora.database_password
     database_update_password = random_id.pin_phone_monitor_update_role_password.b64_url
     database_read_password   = random_id.pin_phone_monitor_read_role_password.b64_url
