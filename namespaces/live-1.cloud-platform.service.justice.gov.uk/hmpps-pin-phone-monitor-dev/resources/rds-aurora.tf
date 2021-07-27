78834f8b287d3e6d1d2418aea75750039cf5b612
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2021-07-27 11:31:55 +0100

    PAT-1981 - Update postgres db parameter group to v11.7 (#5220)
    * PAT-1981 - Update postgres db parameter group to v11.7
    
    * Commit changes made by code formatters
    
    * Trigger PR rebuild
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
@@ -1,24 +1,25 @@
 module "rds_aurora" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.7"
 
-  team_name                   = var.team_name
-  business-unit               = var.business-unit
-  application                 = var.application
-  is-production               = var.is-production
-  namespace                   = var.namespace
-  environment-name            = var.environment-name
-  infrastructure-support      = var.infrastructure-support
-  engine                      = "aurora-postgresql"
-  engine_version              = "11.7"
-  engine_mode                 = "provisioned"
-  replica_count               = 1
-  instance_type               = "db.t3.medium"
-  snapshot_identifier         = "arn:aws:rds:eu-west-2:754256621582:snapshot:hmpps-pin-phone-dev-pre-migration-20210723-1436"
-  storage_encrypted           = true
-  apply_immediately           = true
-  cluster_name                = var.cluster_name
-  allow_major_version_upgrade = true
-  skip_setting_when_migrated  = true
+  team_name                       = var.team_name
+  business-unit                   = var.business-unit
+  application                     = var.application
+  is-production                   = var.is-production
+  namespace                       = var.namespace
+  environment-name                = var.environment-name
+  infrastructure-support          = var.infrastructure-support
+  engine                          = "aurora-postgresql"
+  engine_version                  = "11.7"
+  engine_mode                     = "provisioned"
+  db_cluster_parameter_group_name = "default.aurora-postgresql11"
+  replica_count                   = 1
+  instance_type                   = "db.t3.medium"
+  snapshot_identifier             = "arn:aws:rds:eu-west-2:754256621582:snapshot:hmpps-pin-phone-dev-pre-migration-20210723-1436"
+  storage_encrypted               = true
+  apply_immediately               = true
+  cluster_name                    = var.cluster_name
+  allow_major_version_upgrade     = true
+  skip_setting_when_migrated      = true
 
   providers = {
     aws = aws.london
