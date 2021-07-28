6c5f021183eb811ba4c02c6a3539f8cea958a72d
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-07-28 12:27:05 +0300

    rds upgrade for parameter maintenance window (#5227)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds.tf
@@ -1,5 +1,5 @@
 module "pact_broker_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.4"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
 
   cluster_name           = var.cluster_name
   namespace              = var.namespace
