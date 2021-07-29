d8475d39f9e152eb6f52762f4514fc45f34203ed
carlov20 <carlov20@users.noreply.github.com>
2021-07-29 14:58:11 +0100

    WFP-405 fixed terraform warning

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/s3-dashboard-read-only-user.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/s3-dashboard-read-only-user.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "policy" {
 
     resources = [
       "${module.hmpps-workload-dev-s3-dashboard-bucket.bucket_arn}/*",
-      "${module.hmpps-workload-dev-s3-dashboard-bucket.bucket_arn}"
+      module.hmpps-workload-dev-s3-dashboard-bucket.bucket_arn
     ]
   }
 }
