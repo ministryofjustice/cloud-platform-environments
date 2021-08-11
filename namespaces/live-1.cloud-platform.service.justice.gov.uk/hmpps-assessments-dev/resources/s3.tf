ceb91048e69b548159de18ec83c47fde0eadd962
Lucas Cairns <lucas.cairns@gmail.com>
2021-08-11 12:01:45 +0100

    Add S3 storage to HMPPS Assessments Dev  (#5347)
    * Add S3 storage to HMPPS Assessments Dev
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-assessments-dev/resources/s3.tf
@@ -0,0 +1,47 @@
+module "hmpps_assessments_s3" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment_name
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+
+  /*
+   * The following is used to set a lifecycle for documents in S3. 
+   *  Documentation here "https://www.terraform.io/docs/providers/aws/r/s3_bucket.html#using-object-lifecycle"
+   *  "https://docs.aws.amazon.com/AmazonS3/latest/dev/object-lifecycle-mgmt.html"
+   *
+  */
+  lifecycle_rule = [
+    {
+      enabled = true
+      id      = "retire documents after 5 days"
+      prefix  = "documents/"
+      expiration = [
+        {
+          days = 5
+        },
+      ]
+    },
+  ]
+}
+
+resource "kubernetes_secret" "hmpps_assessments_s3_secret" {
+  metadata {
+    name      = "hmpps-assessments-s3"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.hmpps_assessments_s3.access_key_id
+    secret_access_key = module.hmpps_assessments_s3.secret_access_key
+    bucket_arn        = module.hmpps_assessments_s3.bucket_arn
+    bucket_name       = module.hmpps_assessments_s3.bucket_name
+  }
+}
