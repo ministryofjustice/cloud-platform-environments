7d0e9d7d77a6ee7e77bb806a393d0ca924521ce3
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-08-23 15:14:42 +0100

    Add s3 bucket, sns topic to subscribe to s3 events and a sqs queue to subscribe from sns

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/poornima-staging/resources/s3-messaging.tf
@@ -0,0 +1,167 @@
+module "cp_test_s3_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.6"
+  acl    = "private"
+
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+
+}
+
+module "cp_test_s3_object_created_topic" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.2"
+
+  team_name          = var.team_name
+  topic_display_name = "cp-test-s3-object-created-topic"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sns_topic_policy" "cp_test_s3_object_created_policy" {
+  arn = module.cp_test_s3_object_created_topic.topic_arn
+
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": { "Service": "s3.amazonaws.com" },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:"${module.cp_test_s3_object_created_topic.topic_arn}",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${module.cp_test_s3_bucket.bucket_arn}"}
+        }
+    }]
+}
+POLICY
+}
+ 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn     = module.cp_test_s3_object_created_topic.topic_arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
+
+
+module "s3_bucket_created_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.3"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "cp-test-s3-bucket-created-queue"
+  encrypt_sqs_kms        = "false"
+  namespace              = var.namespace
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.s3_bucket_dead_letter_queue.sqs_arn}","maxReceiveCount": 1
+  }
+
+EOF
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+
+module "s3_bucket_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.3"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "reponses-for-cp-test-s3-bucket-dlq"
+  existing_user_name     = module.s3_bucket_created_queue.user_name
+  encrypt_sqs_kms        = "false"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "s3_bucket_created_queue_policy" {
+  queue_url = module.s3_bucket_created_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.s3_bucket_created_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.s3_bucket_created_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+                {
+                  "aws:SourceArn": "${module.cp_test_s3_object_created_topic.topic_arn}"
+                }
+              }
+        }
+      ]
+  }
+
+EOF
+
+}
+
+
+resource "aws_sns_topic_subscription" "cp_test_s3_object_created" {
+  provider      = aws.london
+  topic_arn     = module.cp_test_s3_object_created_topic.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.cp_test_s3_object_created_queue.sqs_arn
+}
+
+
+
+resource "kubernetes_secret" "s3_bucket" {
+  metadata {
+    name      = "s3"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id               = module.cp_test_s3_bucket.access_key_id
+    secret_access_key           = module.cp_test_s3_bucket.secret_access_key
+  }
+}
+
+
+resource "kubernetes_secret" "cp_test_s3_messaging" {
+  metadata {
+    name      = "pk-staging-s3-messaging"
+    namespace = var.namespace
+  }
+  data = {
+    access_key_id     = module.cp_test_s3_object_created_topic.access_key_id
+    secret_access_key = module.cp_test_s3_object_created_topic.secret_access_key
+    topic_arn         = module.cp_test_s3_object_created_topic.topic_arn
+    sqs_ccr_name      = module.cp_test_s3_object_created_queue.sqs_name
+    sqs_ccr_url       = module.cp_test_s3_object_created_queue.sqs_id
+    sqs_ccr_arn       = module.cp_test_s3_object_created_queue.sqs_arn
+  }
+}
