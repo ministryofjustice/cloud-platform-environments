3c100d17da0043c1cc5ae60d76a9ada0670aa085
Matt <34448412+mattops@users.noreply.github.com>
2021-08-26 16:16:34 +0100

    help-with-prison-visits-prod - Remove DNS records pointing old infra - service going live in CP. (#5421)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/route53.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/route53.tf
@@ -11,23 +11,6 @@ resource "aws_route53_zone" "route53_zone_hwpv" {
   }
 }
 
-# Service hosted in Azure (is being migrated to CP)
-resource "aws_route53_record" "hwpv-azure" {
-  zone_id = aws_route53_zone.route53_zone_hwpv.zone_id
-  name    = "help-with-prison-visits.service.gov.uk"
-  type    = "A"
-  ttl     = "30"
-  records = ["51.140.33.178"]
-}
-
-resource "aws_route53_record" "caseworker-cname" {
-  zone_id = aws_route53_zone.route53_zone_hwpv.zone_id
-  name    = "caseworker.help-with-prison-visits.service.gov.uk"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["e0e8c2cb-80cc-4087-a192-3b8fb4b49b2a.cloudapp.net"]
-}
-
 resource "kubernetes_secret" "route53_zone" {
   metadata {
     name      = "route53-dns-zone-nameservers"
