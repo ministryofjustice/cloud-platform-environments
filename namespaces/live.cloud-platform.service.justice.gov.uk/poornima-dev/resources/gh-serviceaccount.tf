54eb823d74815da4930e4269c7f6b985bbd9ca20
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-09-01 12:27:27 +0100

    Add new serviceaccount (#5439)
    * Add new serviceaccount to test if that changes any of the existing github secrets
    
    * Change sa module name
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/gh-serviceaccount.tf
@@ -4,3 +4,15 @@ module "serviceaccount" {
   namespace           = var.namespace
   github_repositories = ["helloworld-poornima-dev"]
 }
+
+
+module "serviceaccount-live" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+
+  namespace                            = var.namespace
+  github_repositories                  = ["helloworld-poornima-dev"]
+  github_actions_secret_kube_namespace = "KUBE_NAMESPACE_LIVE"
+  github_actions_secret_kube_cert      = "KUBE_CERT_LIVE"
+  github_actions_secret_kube_token     = "KUBE_TOKEN_LIVE"
+  github_actions_secret_kube_cluster   = "KUBE_CLUSTER_LIVE"
+}
