a79085fc18baf0073bb9eee65347f41e48452782
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-02 15:16:19 +0100

    Pass environment variable to another step

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount3.tf
@@ -1,6 +1,7 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
 
+
   namespace           = var.namespace
   serviceaccount_name = "circleci"
 }
