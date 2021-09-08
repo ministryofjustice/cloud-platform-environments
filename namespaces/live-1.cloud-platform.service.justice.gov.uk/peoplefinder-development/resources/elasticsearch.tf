456831ffaf8255a6ddfa02c5f2947aec0e415d25
= <javid.ali@digital.justice.gov.uk>
2021-09-08 09:34:43 +0100

    peoplefinder-development Include all bits in file

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
@@ -35,3 +35,28 @@ resource "aws_cloudwatch_log_resource_policy" "elasticsearch_log_publishing_poli
   policy_document = data.aws_iam_policy_document.elasticsearch_log_publishing_policy_doc.json
   policy_name     = "peoplefinder-dev-elasticsearch-log-publishing-policy"
 }
+
+# Elastic search module
+module "peoplefinder_es" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.0"
+  cluster_name           = var.cluster_name
+  application            = "peoplefinder"
+  business-unit          = "Central Digital"
+  environment-name       = "development"
+  infrastructure-support = "people-finder-support@digital.justice.gov.uk"
+  is-production          = "false"
+  team_name              = "peoplefinder"
+  elasticsearch-domain   = "es"
+  namespace              = "peoplefinder-development"
+  elasticsearch_version  = "6.8"
+  instance_type          = "t2.small.elasticsearch"
+
+  log_publishing_application_cloudwatch_log_group_arn = aws_cloudwatch_log_group.peoplefinder_cloudwatch_log_group.arn
+  log_publishing_application_enabled                  = true
+}
+
+module "ns_annotation" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-ns-annotation?ref=0.0.3"
+  ns_annotation_roles = [module.peoplefinder_es.aws_iam_role_name]
+  namespace           = var.namespace
+}
