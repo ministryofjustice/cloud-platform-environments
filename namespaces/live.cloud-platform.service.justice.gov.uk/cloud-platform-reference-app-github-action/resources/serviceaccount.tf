8227605f810c8b19d027fe78236690fb73429fc4
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-09-10 09:33:43 +0100

    Use different var names for reference app
    This is to not over ride existing vars created for live-1,
    and maintain seperate vars for live

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app-github-action/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app-github-action/resources/serviceaccount.tf
@@ -2,6 +2,11 @@ module "serviceaccount" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
   namespace = var.namespace
 
+  github_actions_secret_kube_namespace = KUBE_NAMESPACE_LIVE
+  github_actions_secret_kube_cert      = KUBE_CERT_LIVE
+  github_actions_secret_kube_token     = KUBE_TOKEN_LIVE
+  github_actions_secret_kube_cluster   = KUBE_CLUSTER_LIVE
+
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["cloud-platform-reference-app-github-action"]
