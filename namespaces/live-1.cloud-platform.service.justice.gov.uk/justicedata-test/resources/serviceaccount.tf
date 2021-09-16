8798abfaeb6d6d4bb468a7029f27a63a18afb038
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-09-16 16:43:55 +0100

    Upgrade serviceaccount module of non-prod in live-1 cluster

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-test/resources/serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/justicedata-test/resources/serviceaccount.tf
@@ -1,6 +1,7 @@
 module "serviceaccount" {
-  source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+  source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.6"
   namespace                            = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
   github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
   github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
   github_actions_secret_kube_token     = var.github_actions_secret_kube_token
