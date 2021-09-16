f729c29a9d76f9e3d39d213e8ca9b4825fc8eaff
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-09-16 11:42:19 +0300

    Raz test DMS (#5572)
    * Raz test DMS

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/raz-test-one/resources/dms.tf
@@ -0,0 +1,23 @@
+module "raz_test_dms" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dms?ref=new-use"
+
+  cluster_name           = var.cluster_name
+  namespace              = var.namespace
+  application            = var.application
+  business-unit          = var.business_unit
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  is-production          = var.is_production
+  team_name              = var.team_name
+}
+
+resource "kubernetes_secret" "dms_instance" {
+  metadata {
+    name      = "dms-instance"
+    namespace = var.namespace
+  }
+
+  data = {
+    replication_instance_arn = module.raz_test_dms.replication_instance_arn
+  }
+}
