c7f01d43256a6339148b6abf9dc3d18263baa890
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-09-16 13:11:44 +0100

    Create redis cluster … which is used as the session store, for example.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-my-prison-dev/resources/elasticache.tf
@@ -0,0 +1,34 @@
+module "redis" {
+  source       = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.3"
+  cluster_name = var.cluster_name
+
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  node_type             = "cache.t2.small"
+  engine_version        = "5.0.6"
+  parameter_group_name  = "default.redis5.0"
+  number_cache_clusters = "2"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "redis" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.redis.primary_endpoint_address
+    member_clusters          = jsonencode(module.redis.member_clusters)
+    auth_token               = module.redis.auth_token
+  }
+}
