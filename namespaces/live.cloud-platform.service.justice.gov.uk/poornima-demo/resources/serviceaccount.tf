c5141d284086b143f4da3534bf46d3c4a6fd7cac
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2021-09-16 13:24:30 +0100

    Test serviceaccount branch to get the kubernetes_cluster from pipeleine (#5587)

--- namespaces/live.cloud-platform.service.justice.gov.uk/poornima-demo/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/poornima-demo/resources/serviceaccount.tf
@@ -1,16 +1,14 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=example"
 
   namespace = var.namespace
+
+  kubernetes_cluster = var.kubernetes_cluster
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["helloworld-poornima-dev"]
 
-  github_actions_secret_kube_namespace = "KUBE_NAMESPACE_GREEN"
-  github_actions_secret_kube_cert      = "KUBE_CERT_GREEN"
-  github_actions_secret_kube_token     = "KUBE_TOKEN_GREEN"
-  github_actions_secret_kube_cluster   = "KUBE_CLUSTER_GREEN"
 
 }
 
