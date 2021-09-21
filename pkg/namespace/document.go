541d82e252735f8e1b7514802366a5e1e2b6109f
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-21 15:48:07 +0100

    Fix environment repository link in document

--- pkg/namespace/document.go
+++ pkg/namespace/document.go
@@ -2,6 +2,6 @@
 Package namespace implements a simple library for MoJ Cloud Platform namespaces.
 
 A Cloud Platform namespace consists of a Kubernetes namespace resource and a number of accompanying annotations, outlined in:
-https://github.com/namespaces/live/<namespace_name>
+https://github.com/cloud-platform-environments/namespaces/live/<namespace_name>
 */
 package namespace // import "github.com/ministryofjustice/cloud-platform-environments/pkg/namespace"
