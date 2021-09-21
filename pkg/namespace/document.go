0805452514edab373338155743d30f5165f85d68
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-21 15:44:46 +0100

    Document/comment package in a single file

---
+++ pkg/namespace/document.go
@@ -0,0 +1,7 @@
+/*
+Package namespace implements a simple library for MoJ Cloud Platform namespaces.
+
+A Cloud Platform namespace consists of a Kubernetes namespace resource and a number of accompanying annotations, outlined in:
+https://github.com/namespaces/live/<namespace_name>
+*/
+package namespace // import "github.com/ministryofjustice/cloud-platform-environments/pkg/namespace"
