987b9968d1b44dab0684a44dc1935fc799bf1a74
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-23 14:15:31 +0100

    Add `ingress` package comments

---
+++ pkg/ingress/document.go
@@ -0,0 +1,6 @@
+/*
+Package ingress implements a library for MoJ Cloud Platform ingress resources.
+
+An ingress resource is just a Kubernetes ingress with annotations specific to the Cloud Platform.
+*/
+package ingress // import "github.com/ministryofjustice/cloud-platform-environments/pkg/ingress"
