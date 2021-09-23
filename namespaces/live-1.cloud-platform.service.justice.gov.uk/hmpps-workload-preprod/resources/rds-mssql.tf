d170db1d3039d90d1879bd4152f1f29e79ff0ce9
carlov20 <carlov20@users.noreply.github.com>
2021-09-23 14:53:41 +0100

    WFP-403 added rds mssql

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/rds-mssql.tf
@@ -0,0 +1,58 @@
+module "rds-mssql" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  # performance insights
+  performance_insights_enabled = false
+
+  # RDS SQL Server
+  db_engine            = "sqlserver-web"
+  db_engine_version    = "15.00"
+  rds_family           = "sqlserver-web-15.0"
+  db_parameter         = []
+  db_instance_class    = "db.t3.small"
+  db_allocated_storage = "20"
+  license_model        = "license-included"
+  db_name              = "hmpps_workload"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "rds-mssql" {
+  metadata {
+    name      = "rds-mssql-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds-mssql.rds_instance_endpoint
+    database_name         = module.rds-mssql.database_name
+    database_username     = module.rds-mssql.database_username
+    database_password     = module.rds-mssql.database_password
+    rds_instance_address  = module.rds-mssql.rds_instance_address
+    access_key_id         = module.rds-mssql.access_key_id
+    secret_access_key     = module.rds-mssql.secret_access_key
+  }
+
+}
+
+resource "kubernetes_config_map" "rds-mssql" {
+  metadata {
+    name      = "rds-mssql-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name = module.rds-mssql.database_name
+    db_identifier = module.rds-mssql.db_identifier
+  }
+}
