b603692067b49e23c15d38c9a21300855cf8e0ff
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-09-24 11:15:16 +0100

    Add kubeconfig auth to environments repository
    Previously this function sat in a package named `authenticate` in the
    hoodaw repo. After a discussion with a colleague, it made more sense for
    this code to sit here.
    
    It's difficult to test a file grab from S3, so I've just tested the
    expected failure if a user doesn't provide the correct creds or inputs.
    
    Test coverage for the whole package after this commit:
    
    ```bash
    go test -coverprofile=c.out
    PASS
    coverage: 28.0% of statements
    ok
    github.com/ministryofjustice/cloud-platform-environments/pkg/authenticate
    0.507s
    ```

--- pkg/authenticate/authenticate_test.go
+++ pkg/authenticate/authenticate_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-github/github"
+	"k8s.io/client-go/kubernetes"
 )
 
 func TestGitHubClient(t *testing.T) {
@@ -35,6 +36,41 @@ func TestGitHubClient(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GitHubClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKubeConfigFromS3Bucket(t *testing.T) {
+	type args struct {
+		bucket     string
+		s3FileName string
+		clusterCtx string
+		region     string
+	}
+	tests := []struct {
+		name          string
+		args          args
+		wantClientset *kubernetes.Clientset
+		wantErr       bool
+	}{
+		{
+			name: "Unauthenticated request to bucket should err",
+			args: args{
+				bucket:     "realbucketname",
+				s3FileName: "doesntMatter",
+				clusterCtx: "irrelevantCluster",
+				region:     "eu-west-2",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := KubeConfigFromS3Bucket(tt.args.bucket, tt.args.s3FileName, tt.args.clusterCtx, tt.args.region)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("KubeConfigFromS3Bucket() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
 		})
 	}
