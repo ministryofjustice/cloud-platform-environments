9ec28115df312a293b15ae011f868e3aef7cbf7e
= <javid.ali@digital.justice.gov.uk>
2021-09-28 11:44:20 +0100

    contact-moj-development copy namespace to live

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-development/resources/variables.tf
@@ -0,0 +1,22 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ */
+
+variable "cluster_name" {}
+
+variable "namespace" {
+  default = "contact-moj-development"
+}
+
+variable "domain" {
+  default = "development.contact-moj.service.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "environment-name" {
+  default = "development"
+}
