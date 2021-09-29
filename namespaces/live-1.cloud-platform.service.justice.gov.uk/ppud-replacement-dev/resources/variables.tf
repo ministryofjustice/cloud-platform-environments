f091d2a05f54e6c06b30bf18336aa8c139ea91d7
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-09-29 14:43:49 +0100

    Replace Ken's personal email with a group email list

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "environment" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "ken.friesen@digital.justice.gov.uk"
+  default     = "ppud-replacement-tech@digital.justice.gov.uk"
 }
 
 variable "is_production" {
