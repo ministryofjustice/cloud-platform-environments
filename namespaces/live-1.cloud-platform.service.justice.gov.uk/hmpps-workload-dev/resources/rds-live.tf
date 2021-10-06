88bc659c8c7a803404832214c42074d7d9777895
carlov20 <carlov20@users.noreply.github.com>
2021-10-06 11:44:45 +0100

    WFP-590 set backup window to before etl run but after dms run

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/rds-live.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/rds-live.tf
@@ -18,7 +18,7 @@ module "rds-live" {
   # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
   # Pick the one that defines the postgres version the best
   rds_family = "postgres11"
-
+  backup_window= "02:00-03:00"
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "true"
 
