6d55a728bd2c8e398564c4bceeb31fe07b02fd45
Carlos Fernandez <carlos.fernandez@digital.justice.gov.uk>
2021-10-08 10:56:30 +0100

    set allow_major_version_upgrade to false

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/soc-reporting-dev/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/soc-reporting-dev/resources/rds.tf
@@ -20,7 +20,7 @@ module "rds" {
   rds_family = "postgres11"
 
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "true"
+  allow_major_version_upgrade = "false"
 
   providers = {
     # Can be either "aws.london" or "aws.ireland"
