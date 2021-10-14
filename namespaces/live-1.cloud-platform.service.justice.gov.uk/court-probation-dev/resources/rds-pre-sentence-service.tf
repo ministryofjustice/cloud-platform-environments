a10baa1447631d3ba2a912645d5b6fbaf06e64db
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-10-14 09:03:39 +0100

    court-probation-dev: new rds instance for pre-sentence-service

---
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/rds-pre-sentence-service.tf
@@ -0,0 +1,40 @@
+variable "cluster_name" {
+}
+
+
+module "pre_sentence_service_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business-unit
+  namespace              = var.namespace
+  application            = "pre-sentence-service"
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+  rds_family             = var.rds-family
+  db_engine_version      = var.db_engine_version
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "pre_sentence_service_rds" {
+  metadata {
+    name      = "pre-sentence-service-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.pre_sentence_service_rds.rds_instance_endpoint
+    database_name         = module.pre_sentence_service_rds.database_name
+    database_username     = module.pre_sentence_service_rds.database_username
+    database_password     = module.pre_sentence_service_rds.database_password
+    rds_instance_address  = module.pre_sentence_service_rds.rds_instance_address
+    url                   = "postgres://${module.pre_sentence_service_rds.database_username}:${module.pre_sentence_service_rds.database_password}@${module.pre_sentence_service_rds.rds_instance_endpoint}/${module.pre_sentence_service_rds.database_name}"
+    access_key_id         = module.pre_sentence_service_rds.access_key_id
+    secret_access_key     = module.pre_sentence_service_rds.secret_access_key
+  }
+}
+
+
