731c1e6d78f9fc5e1906383a24f50abe4bd2e81f
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-10-21 09:25:00 +0100

    Reconfigure the bandiera db as a general utility db
    As the db is likely to be quite underutilised on dev, push more than one
    service into the instance, this makes the utility db work for both
    bandiera (or other ff service) and our dashboard service.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/versions.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/versions.tf
@@ -11,5 +11,13 @@ terraform {
     pingdom = {
       source = "russellcardullo/pingdom"
     }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.14.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
   }
 }
