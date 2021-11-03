c3581cabf633de0048e998c9a38940b16afae241
carlov20 <carlov20@users.noreply.github.com>
2021-11-03 09:27:12 +0000

    WFP-585 made specific target for preprod

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/dms-impact-testing.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/dms-impact-testing.tf
@@ -20,7 +20,7 @@ data "kubernetes_secret" "dms_impact_testing_secret" {
 }
 
 resource "aws_dms_endpoint" "source-prod-db" {
-  endpoint_id                 = "${var.team_name}-src-${random_id.dms_rand.hex}"
+  endpoint_id                 = "${var.team_name}-src-wmt-prod-${random_id.dms_rand.hex}"
   endpoint_type               = "source"
   engine_name                 = data.kubernetes_secret.dms_secret.data.src_engine
   extra_connection_attributes = ""
@@ -40,13 +40,34 @@ resource "aws_dms_endpoint" "source-prod-db" {
   }
 }
 
+resource "aws_dms_endpoint" "target-preprod-db" {
+  endpoint_id                 = "${var.team_name}-target-wmt-preprod-${random_id.dms_rand.hex}"
+  endpoint_type               = "source"
+  engine_name                 = data.kubernetes_secret.dms_secret.data.src_engine
+  extra_connection_attributes = ""
+  server_name                 = module.rds-live.rds_instance_address
+  database_name               = module.rds-live.database_name
+  username                    = module.rds-live.database_username
+  password                    = module.rds-live.database_password
+  port                        = module.rds-live.rds_instance_port
+  ssl_mode                    = data.kubernetes_secret.dms_secret.data.src_ssl
+
+  tags = {
+    Name        = "${var.team_name} Source Endpoint"
+    Description = "Current Prod WMT Live database"
+    Application = var.application
+    Owner       = var.team_name
+    Env         = var.environment
+  }
+}
+
 resource "aws_dms_replication_task" "replication_impact_testing_mastered_task" {
   migration_type           = "full-load"
   replication_instance_arn = module.hmpps-workload-impact-testing-dms.replication_instance_arn
   replication_task_id      = "${var.team_name}-repl-${random_id.dms_rand.hex}"
 
   source_endpoint_arn = aws_dms_endpoint.source-prod-db.endpoint_arn
-  target_endpoint_arn = aws_dms_endpoint.target-postgres.endpoint_arn
+  target_endpoint_arn = aws_dms_endpoint.target-preprod-db.endpoint_arn
 
   table_mappings            = trimspace(file("settings/dms-table-mappings.json"))
   replication_task_settings = trimspace(file("settings/dms-replication-task-settings.json"))
@@ -66,7 +87,7 @@ resource "aws_dms_replication_task" "replication_impact_testing_full_task" {
   replication_task_id      = "${var.team_name}-repl-${random_id.dms_rand.hex}"
 
   source_endpoint_arn = aws_dms_endpoint.source-prod-db.endpoint_arn
-  target_endpoint_arn = aws_dms_endpoint.target-postgres.endpoint_arn
+  target_endpoint_arn = aws_dms_endpoint.target-preprod-db.endpoint_arn
 
   table_mappings            = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"app\",\"table-name\":\"%\"},\"rule-action\":\"include\"}]}"
   replication_task_settings = trimspace(file("settings/dms-replication-task-settings.json"))
@@ -91,7 +112,7 @@ resource "kubernetes_secret" "dms_impact_testing_instance" {
     access_key_id            = module.hmpps-workload-impact-testing-dms.access_key_id
     secret_access_key        = module.hmpps-workload-impact-testing-dms.secret_access_key
     source                   = aws_dms_endpoint.source-prod-db.endpoint_arn
-    destination              = aws_dms_endpoint.target-postgres.endpoint_arn
+    destination              = aws_dms_endpoint.target-preprod-db.endpoint_arn
     task                     = aws_dms_replication_task.replication_impact_testing_mastered_task.replication_task_arn
   }
 }
