bcf4ca7ba2800be24caaf2cd66359343aac4d4b5
Jesus Laiz <zheileman@users.noreply.github.com>
2021-11-04 12:18:05 +0000

    Migrate elsa-relevant-search-production to live cluster
    Note: despite this being a "production" namespace, it is not publicly available yet or in use, and is mostly a static website so we are willing to accept any risks.
    
    Staging namespace already migrated and working fine so far.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/elsa-relevant-search-production/resources/versions.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    github = {
+      source = "integrations/github"
+    }
+    pingdom = {
+      source  = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
+}
