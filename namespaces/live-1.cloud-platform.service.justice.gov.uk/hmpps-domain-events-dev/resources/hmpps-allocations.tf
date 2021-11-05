2ec494a6c9af7f98b0cb243c3492f9a488f483c5
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-05 18:22:50 +0000

    Temporary remove cross reference namespace secrets during migration

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-allocations.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-allocations.tf
@@ -1,15 +1,3 @@
-resource "kubernetes_secret" "hmpps-allocations" {
-  metadata {
-    name      = "hmpps-domain-events-topic"
-    namespace = "workforce-management-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps-domain-events.access_key_id
-    secret_access_key = module.hmpps-domain-events.secret_access_key
-    topic_arn         = module.hmpps-domain-events.topic_arn
-  }
-}
 
 module "hmpps_allocation_required_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.4"
