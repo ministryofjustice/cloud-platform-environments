5ba78484559193ed1065975b5763a7c9bb400fb0
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-11-05 18:37:00 +0000

    Temporary remove cross reference namespace secrets during migration

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-allocations.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-allocations.tf
@@ -71,36 +71,6 @@ module "hmpps_allocation_required_dead_letter_queue" {
   }
 }
 
-resource "kubernetes_secret" "hmpps_allocation_required_queue" {
-  metadata {
-    name      = "sqs-hmpps-allocations-secret"
-    namespace = "workforce-management-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps_allocation_required_queue.access_key_id
-    secret_access_key = module.hmpps_allocation_required_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_allocation_required_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_allocation_required_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_allocation_required_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "hmpps_allocation_required_dead_letter_queue" {
-  metadata {
-    name      = "sqs-hmpps-allocations-dl-secret"
-    namespace = "workforce-management-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps_allocation_required_dead_letter_queue.access_key_id
-    secret_access_key = module.hmpps_allocation_required_dead_letter_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_allocation_required_dead_letter_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_allocation_required_dead_letter_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_allocation_required_dead_letter_queue.sqs_name
-  }
-}
-
 resource "aws_sns_topic_subscription" "hmpps_allocation_required_subscription" {
   provider      = aws.london
   topic_arn     = module.hmpps-domain-events.topic_arn
