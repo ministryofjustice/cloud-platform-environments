525038e8d70658b1a087405a661d48adf22a37e8
Peter Phillips <peter@greenthistle.co.uk>
2021-11-05 10:48:37 +0000

    DT-2796: 🔧 Migrate to live context

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/nomis-user-roles-api-dev/resources/variables.tf
@@ -0,0 +1,42 @@
+
+variable "cluster_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "NOMIS User and Roles management API"
+}
+
+variable "namespace" {
+  default = "nomis-user-roles-api-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-tech"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "dps_tech_team"
+}
+
