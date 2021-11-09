5db500aba17452c7399e68460d068e6eab853c15
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-11-09 13:13:18 +0000

    Migrate ApplyBot to live

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-bot-production/resources/variables.tf
@@ -0,0 +1,13 @@
+variable "namespace" {
+  default = "laa-apply-bot-production"
+}
+
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ *
+ */
+
+variable "cluster_name" {
+}
+
