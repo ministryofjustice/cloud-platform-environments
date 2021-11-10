b22257d64396e5d3737eb098462326e5bdc396f3
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-11-10 16:59:22 +0000

    remove trailing commar

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-interventions-dev/resources/reporting-landing-access.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-interventions-dev/resources/reporting-landing-access.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "reporting_access" {
       "arn:aws:s3:::eu-west-2-delius-mis-dev-dfi-extracts/dfinterventions/dfi/*",
       "arn:aws:s3:::eu-west-2-delius-mis-dev-dfi-extracts/dfinterventions/dfi/",
       "arn:aws:s3:::eu-west-2-delius-mis-dev-dfi-extracts/exports/csv/reports/*",
-      "arn:aws:s3:::eu-west-2-delius-mis-dev-dfi-extracts/exports/csv/reports/",
+      "arn:aws:s3:::eu-west-2-delius-mis-dev-dfi-extracts/exports/csv/reports/"
     ]
   }
 }
