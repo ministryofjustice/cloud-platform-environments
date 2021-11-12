a0c53882d9fcd284413ce62ede3e710811be9b27
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2021-11-12 09:44:00 +0000

    Added staging site live cluster migration files (#6049)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justicedata-staging/resources/variables.tf
@@ -0,0 +1,53 @@
+
+variable "cluster_name" {
+}
+
+variable "kubernetes_cluster" {
+}
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Public facing publication portal for hmpps"
+}
+
+variable "namespace" {
+  default = "justicedata-staging"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "justicedata"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "hubusers@justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "performance-hub-dev"
+}
+
+variable "github_owner" {
+  description = "Required by the github terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
