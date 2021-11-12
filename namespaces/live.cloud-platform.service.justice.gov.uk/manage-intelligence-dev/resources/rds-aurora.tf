fc38da2ff23ac9be78f8409d82207b919623a39f
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2021-11-12 15:04:17 +0000

    DT-2796: 🔧 Migrate to live context for manage-intelligence-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/rds-aurora.tf
@@ -0,0 +1,56 @@
+module "rds_aurora" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=1.7"
+
+  team_name                   = var.team_name
+  business-unit               = var.business-unit
+  application                 = var.application
+  is-production               = var.is-production
+  namespace                   = var.namespace
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure-support
+  engine                      = "aurora-postgresql"
+  engine_version              = "12.4"
+  engine_mode                 = "provisioned"
+  replica_count               = 1
+  instance_type               = "db.t3.medium"
+  storage_encrypted           = true
+  apply_immediately           = true
+  cluster_name                = var.cluster_name
+  allow_major_version_upgrade = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "random_id" "manage_intelligence_update_role_password" {
+  byte_length = 32
+}
+
+resource "random_id" "manage_intelligence_read_role_password" {
+  byte_length = 32
+}
+
+resource "kubernetes_secret" "manage_intelligence_rds_aurora" {
+  metadata {
+    name      = "manage-intelligence-rds-aurora-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_cluster_endpoint                = module.rds_aurora.rds_cluster_endpoint
+    rds_cluster_reader_endpoint         = module.rds_aurora.rds_cluster_reader_endpoint
+    db_cluster_identifier               = module.rds_aurora.db_cluster_identifier
+    database_name                       = module.rds_aurora.database_name
+    database_username                   = module.rds_aurora.database_username
+    database_password                   = module.rds_aurora.database_password
+    manage_intelligence_update_password = random_id.manage_intelligence_update_role_password.b64
+    manage_intelligence_read_password   = random_id.manage_intelligence_read_role_password.b64
+    access_key_id                       = module.rds_aurora.access_key_id
+    secret_access_key                   = module.rds_aurora.secret_access_key
+    url                                 = "postgres://${module.rds_aurora.database_username}:${module.rds_aurora.database_password}@${module.rds_aurora.rds_cluster_endpoint}/${module.rds_aurora.database_name}"
+    reader_url                          = "postgres://${module.rds_aurora.database_username}:${module.rds_aurora.database_password}@${module.rds_aurora.rds_cluster_reader_endpoint}/${module.rds_aurora.database_name}"
+  }
+
+}
+
