a4e6e20c7d77970b004eed8ea182a9e1edeca2e6
Jesus Laiz <zheileman@users.noreply.github.com>
2021-11-16 16:06:02 +0000

    Replace CircleCI service account with GitHub
    For consistency between staging and production environments we are going to migrate this namespace to use github actions.

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/fj-cait-production/resources/variables.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/fj-cait-production/resources/variables.tf
@@ -30,8 +30,22 @@ variable "repo_name" {
   default = "pflr-cait"
 }
 
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
+
 // The following two variables are provided at runtime by the pipeline.
 variable "cluster_name" {
 }
 
+variable "cluster_state_bucket" {
+}
 
+variable "kubernetes_cluster" {
+}
