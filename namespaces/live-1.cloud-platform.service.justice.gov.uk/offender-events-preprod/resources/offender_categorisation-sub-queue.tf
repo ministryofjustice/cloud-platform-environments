b916546f6434e79d4bb46b2547c97cf43cc6de65
steverendell <steve.rendell@syscon.net>
2021-11-17 11:03:49 +0000

    PAT-2103 Migrate pathfinder to live cluster
    Temporarily remove event queue secrets

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/offender_categorisation-sub-queue.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/offender_categorisation-sub-queue.tf
@@ -64,35 +64,35 @@ module "offender_categorisation_events_dead_letter_queue" {
   }
 }
 
-resource "kubernetes_secret" "offender_categorisation_events_queue" {
-  metadata {
-    name      = "oc-events-sqs-instance-output"
-    namespace = "offender-categorisation-preprod"
-  }
-
-  data = {
-    access_key_id     = module.offender_categorisation_events_queue.access_key_id
-    secret_access_key = module.offender_categorisation_events_queue.secret_access_key
-    sqs_oce_url       = module.offender_categorisation_events_queue.sqs_id
-    sqs_oce_arn       = module.offender_categorisation_events_queue.sqs_arn
-    sqs_oce_name      = module.offender_categorisation_events_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "offender_categorisation_events_dead_letter_queue" {
-  metadata {
-    name      = "oc-events-sqs-dl-instance-output"
-    namespace = "offender-categorisation-preprod"
-  }
-
-  data = {
-    access_key_id     = module.offender_categorisation_events_dead_letter_queue.access_key_id
-    secret_access_key = module.offender_categorisation_events_dead_letter_queue.secret_access_key
-    sqs_oce_url       = module.offender_categorisation_events_dead_letter_queue.sqs_id
-    sqs_oce_arn       = module.offender_categorisation_events_dead_letter_queue.sqs_arn
-    sqs_oce_name      = module.offender_categorisation_events_dead_letter_queue.sqs_name
-  }
-}
+#resource "kubernetes_secret" "offender_categorisation_events_queue" {
+#  metadata {
+#    name      = "oc-events-sqs-instance-output"
+#    namespace = "offender-categorisation-preprod"
+#  }
+#
+#  data = {
+#    access_key_id     = module.offender_categorisation_events_queue.access_key_id
+#    secret_access_key = module.offender_categorisation_events_queue.secret_access_key
+#    sqs_oce_url       = module.offender_categorisation_events_queue.sqs_id
+#    sqs_oce_arn       = module.offender_categorisation_events_queue.sqs_arn
+#    sqs_oce_name      = module.offender_categorisation_events_queue.sqs_name
+#  }
+#}
+#
+#resource "kubernetes_secret" "offender_categorisation_events_dead_letter_queue" {
+#  metadata {
+#    name      = "oc-events-sqs-dl-instance-output"
+#    namespace = "offender-categorisation-preprod"
+#  }
+#
+#  data = {
+#    access_key_id     = module.offender_categorisation_events_dead_letter_queue.access_key_id
+#    secret_access_key = module.offender_categorisation_events_dead_letter_queue.secret_access_key
+#    sqs_oce_url       = module.offender_categorisation_events_dead_letter_queue.sqs_id
+#    sqs_oce_arn       = module.offender_categorisation_events_dead_letter_queue.sqs_arn
+#    sqs_oce_name      = module.offender_categorisation_events_dead_letter_queue.sqs_name
+#  }
+#}
 
 resource "aws_sns_topic_subscription" "offender_categorisation_subscription" {
   provider      = aws.london
@@ -171,35 +171,35 @@ module "offender_categorisation_ui_events_dead_letter_queue" {
   }
 }
 
-resource "kubernetes_secret" "offender_categorisation_ui_events_queue" {
-  metadata {
-    name      = "ocu-events-sqs-instance-output"
-    namespace = "offender-categorisation-preprod"
-  }
-
-  data = {
-    access_key_id     = module.offender_categorisation_ui_events_queue.access_key_id
-    secret_access_key = module.offender_categorisation_ui_events_queue.secret_access_key
-    url               = module.offender_categorisation_ui_events_queue.sqs_id
-    arn               = module.offender_categorisation_ui_events_queue.sqs_arn
-    name              = module.offender_categorisation_ui_events_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "offender_categorisation_ui_events_dead_letter_queue" {
-  metadata {
-    name      = "ocu-events-sqs-dl-instance-output"
-    namespace = "offender-categorisation-preprod"
-  }
-
-  data = {
-    access_key_id     = module.offender_categorisation_ui_events_dead_letter_queue.access_key_id
-    secret_access_key = module.offender_categorisation_ui_events_dead_letter_queue.secret_access_key
-    url               = module.offender_categorisation_ui_events_dead_letter_queue.sqs_id
-    arn               = module.offender_categorisation_ui_events_dead_letter_queue.sqs_arn
-    name              = module.offender_categorisation_ui_events_dead_letter_queue.sqs_name
-  }
-}
+#resource "kubernetes_secret" "offender_categorisation_ui_events_queue" {
+#  metadata {
+#    name      = "ocu-events-sqs-instance-output"
+#    namespace = "offender-categorisation-preprod"
+#  }
+#
+#  data = {
+#    access_key_id     = module.offender_categorisation_ui_events_queue.access_key_id
+#    secret_access_key = module.offender_categorisation_ui_events_queue.secret_access_key
+#    url               = module.offender_categorisation_ui_events_queue.sqs_id
+#    arn               = module.offender_categorisation_ui_events_queue.sqs_arn
+#    name              = module.offender_categorisation_ui_events_queue.sqs_name
+#  }
+#}
+#
+#resource "kubernetes_secret" "offender_categorisation_ui_events_dead_letter_queue" {
+#  metadata {
+#    name      = "ocu-events-sqs-dl-instance-output"
+#    namespace = "offender-categorisation-preprod"
+#  }
+#
+#  data = {
+#    access_key_id     = module.offender_categorisation_ui_events_dead_letter_queue.access_key_id
+#    secret_access_key = module.offender_categorisation_ui_events_dead_letter_queue.secret_access_key
+#    url               = module.offender_categorisation_ui_events_dead_letter_queue.sqs_id
+#    arn               = module.offender_categorisation_ui_events_dead_letter_queue.sqs_arn
+#    name              = module.offender_categorisation_ui_events_dead_letter_queue.sqs_name
+#  }
+#}
 
 resource "aws_sns_topic_subscription" "offender_categorisation_ui_subscription" {
   provider      = aws.london
