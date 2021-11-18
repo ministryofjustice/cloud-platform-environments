f60e062f7c0f737a2f57c2681aa2ded52957c303
Jesus Laiz <zheileman@users.noreply.github.com>
2021-11-18 11:19:37 +0000

    Migrate family-mediators-api-staging to live cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/family-mediators-api-staging/resources/variables.tf
@@ -0,0 +1,50 @@
+variable "team_name" {
+  default = "family-justice"
+}
+
+variable "business_unit" {
+  default = "HQ"
+}
+
+variable "environment_name" {
+  default = "staging"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "infrastructure_support" {
+  default = "crossjusticedelivery@digital.justice.gov.uk"
+}
+
+variable "application" {
+  default = "Family Mediators API"
+}
+
+variable "namespace" {
+  default = "family-mediators-api-staging"
+}
+
+variable "repo_name" {
+  default = "family-mediators-api"
+}
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
