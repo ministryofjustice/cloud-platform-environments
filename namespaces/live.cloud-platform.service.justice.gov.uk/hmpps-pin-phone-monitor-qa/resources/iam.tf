f400c3d57e4f2e3963e90651113c1cd9cb3176b4
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2021-11-24 16:21:44 +0000

    PPD-338 - promote namespace changes to QA environment (#6207)
    * PPD-338 - promote iam changes for call processing user to QA environment
    
    * PPD-338 - promote s3 sqs event changes to QA environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-qa/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-qa/resources/iam.tf
@@ -55,4 +55,55 @@ resource "kubernetes_secret" "pcms_bt_upload_user" {
     secret_access_key = aws_iam_access_key.bt_upload_user_key.secret
   }
 }
+resource "random_id" "upload" {
+  byte_length = 8
+}
 
+resource "aws_iam_user" "call_processing_user" {
+  name = "pcms-call-processing-user-${random_id.upload.hex}"
+  path = "/system/pcms-call-processing-users/"
+}
+
+resource "aws_iam_access_key" "call_processing_user_key" {
+  user = aws_iam_user.call_processing_user.name
+}
+
+data "aws_iam_policy_document" "call_processing_policy" {
+  statement {
+    actions = ["s3:*Object"]
+
+    resources = [
+      "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "transcribe:*",
+      "translate:*",
+      "comprehend:DetectDominantLanguage"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "call_processing_policy" {
+  name   = "pcms-call-processing-policy"
+  policy = data.aws_iam_policy_document.call_processing_policy.json
+  user   = aws_iam_user.call_processing_user.name
+}
+
+resource "kubernetes_secret" "pcms_call_processing_user" {
+  metadata {
+    name      = "pcms-call-processing-user"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = aws_iam_access_key.call_processing_user_key.id
+    secret_access_key = aws_iam_access_key.call_processing_user_key.secret
+  }
+}
