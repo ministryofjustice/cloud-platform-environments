10cf8e5c9e3ab35e1325e77910a2ec5f2161d603
petergphillips <peter@greenthistle.co.uk>
2021-11-25 10:37:10 +0000

    DT-2913: 🔨 Temporarily remove copying the pre-prod secret to prod until both have been migrated (#6214)

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
@@ -35,23 +35,3 @@ resource "kubernetes_secret" "dps_rds" {
     url                   = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
   }
 }
-
-# This places a secret for this preprod RDS instance in the production namespace,
-# this can then be used by a kubernetes job which will refresh the preprod data.
-resource "kubernetes_secret" "dps_rds_refresh_creds" {
-  metadata {
-    name      = "dps-rds-instance-output-preprod"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.dps_rds.rds_instance_endpoint
-    database_name         = module.dps_rds.database_name
-    database_username     = module.dps_rds.database_username
-    database_password     = module.dps_rds.database_password
-    access_key_id         = module.dps_rds.access_key_id
-    secret_access_key     = module.dps_rds.secret_access_key
-    rds_instance_address  = module.dps_rds.rds_instance_address
-  }
-}
-
