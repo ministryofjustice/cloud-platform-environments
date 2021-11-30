792fb3fb1834d2a8b50bf9909d04e088df5610eb
Barry Piccinni <barry.piccinni@cica.gov.uk>
2021-11-30 13:05:30 +0000

    migration of dev namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
+provider "kubernetes" {
+  version = "~> 1.11"
+}
+
+provider "helm" {
+  kubernetes {
+  }
+}
