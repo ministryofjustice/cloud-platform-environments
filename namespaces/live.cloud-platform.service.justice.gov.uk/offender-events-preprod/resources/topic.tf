a548b6c5264af4234061883a614cd21cbec19e53
petergphillips <peter@greenthistle.co.uk>
2021-12-01 16:03:15 +0000

    DT-2796: 🔨 Migrate offender events preprod to live cluster (#6290)
    * DT-2796: 🔨 Migrate offender events preprod to live cluster
    
    * DT-2796: 🔨 Stop creation of multiple secrets with the name name in the same namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
@@ -0,0 +1,94 @@
+module "offender_events" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.3"
+
+  team_name          = var.team_name
+  topic_display_name = "offender-events"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "offender_events" {
+  metadata {
+    name      = "offender-events-topic"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.offender_events.access_key_id
+    secret_access_key = module.offender_events.secret_access_key
+    topic_arn         = module.offender_events.topic_arn
+  }
+}
+
+resource "kubernetes_secret" "offender_case_notes" {
+  metadata {
+    name      = "offender-events-topic"
+    namespace = "offender-case-notes-preprod"
+  }
+
+  data = {
+    access_key_id     = module.offender_events.access_key_id
+    secret_access_key = module.offender_events.secret_access_key
+    topic_arn         = module.offender_events.topic_arn
+  }
+}
+
+resource "kubernetes_secret" "prison-data-compliance" {
+  metadata {
+    name      = "offender-events-topic-prison-data-compliance"
+    namespace = var.namespace
+    # Remove when namespace has been migrated
+    # name      = "offender-events-topic"
+    # namespace = "prison-data-compliance-preprod"
+  }
+
+  data = {
+    access_key_id     = module.offender_events.access_key_id
+    secret_access_key = module.offender_events.secret_access_key
+    topic_arn         = module.offender_events.topic_arn
+  }
+}
+
+module "probation_offender_events" {
+  source             = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.3"
+  team_name          = var.team_name
+  topic_display_name = "probation-offender-events"
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "probation_offender_events" {
+  metadata {
+    name      = "probation-offender-events-topic"
+    namespace = var.namespace
+  }
+  data = {
+    access_key_id     = module.probation_offender_events.access_key_id
+    secret_access_key = module.probation_offender_events.secret_access_key
+    topic_arn         = module.probation_offender_events.topic_arn
+  }
+}
+
+module "offender_assessments_events" {
+  source             = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.3"
+  team_name          = var.team_name
+  topic_display_name = "offender-assessments-events"
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "offender_assessments_events" {
+  metadata {
+    name      = "offender-assessments-events-topic"
+    namespace = var.namespace
+  }
+  data = {
+    access_key_id     = module.offender_assessments_events.access_key_id
+    secret_access_key = module.offender_assessments_events.secret_access_key
+    topic_arn         = module.offender_assessments_events.topic_arn
+  }
+}
