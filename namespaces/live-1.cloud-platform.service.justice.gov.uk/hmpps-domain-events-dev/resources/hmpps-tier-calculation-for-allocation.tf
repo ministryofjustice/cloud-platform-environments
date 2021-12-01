4d413bf84552cd8f4667192ff8f74fdc1c11187c
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-12-01 11:57:21 +0000

    WFP-639 add comment for future removal of secrets

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-tier-calculation-for-allocation.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-tier-calculation-for-allocation.tf
@@ -84,6 +84,8 @@ resource "aws_sns_topic_subscription" "hmpps_tier_calculation_allocation_subscri
 resource "kubernetes_secret" "hmpps_tier_calculation_allocation_queue_for_domain_events" {
   metadata {
     name      = "hmpps-tier-calculation-allocation-queue-for-domain-events-instance-output"
+    # Remove once domain events migrated
+    # namespace = "workforce-management-dev"
     namespace = var.namespace
   }
 
@@ -99,6 +101,8 @@ resource "kubernetes_secret" "hmpps_tier_calculation_allocation_queue_for_domain
 resource "kubernetes_secret" "hmpps_tier_calculation_allocation_queue_for_domain_events_dead_letter_queue" {
   metadata {
     name      = "hmpps-tier-calculation-allocation-queue-for-domain-events-dl-instance-output"
+    # Remove once domain events migrated
+    # namespace = "workforce-management-dev"
     namespace = var.namespace
   }
 
