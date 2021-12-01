e1e23e78a6597e324032461258feeea85140adcd
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-12-01 16:04:40 +0000

    Upgrades rds-pre-sentence-service from Postgres 11 to 13.3
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds-pre-sentence-service.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds-pre-sentence-service.tf
@@ -1,14 +1,15 @@
 module "pre_sentence_service_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
-  cluster_name           = var.cluster_name
-  team_name              = var.team_name
-  business-unit          = var.business-unit
-  namespace              = var.namespace
-  application            = "pre-sentence-service"
-  environment-name       = var.environment-name
-  infrastructure-support = var.infrastructure-support
-  rds_family             = var.rds-family
-  db_engine_version      = var.db_engine_version
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
+  cluster_name                = var.cluster_name
+  team_name                   = var.team_name
+  business-unit               = var.business-unit
+  namespace                   = var.namespace
+  application                 = "pre-sentence-service"
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure-support
+  rds_family                  = "postgres13"
+  db_engine_version           = "13.3"
+  allow_major_version_upgrade = true
 
   providers = {
     aws = aws.london
@@ -32,5 +33,3 @@ resource "kubernetes_secret" "pre_sentence_service_rds" {
     secret_access_key     = module.pre_sentence_service_rds.secret_access_key
   }
 }
-
-
