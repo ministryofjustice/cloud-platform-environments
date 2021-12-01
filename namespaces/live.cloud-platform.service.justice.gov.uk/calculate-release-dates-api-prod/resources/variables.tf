f17ea01d5eb6e78de83e4957b5add81995df6165
yasin <yasin.mustafa@gmail.com>
2021-12-01 12:14:30 +0000

    CRS-612 Add configuration for calculate-release-dates-api production environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/variables.tf
@@ -0,0 +1,45 @@
+variable "cluster_name" {
+}
+
+variable "domain" {
+  default = "calculate-release-dates-api.hmpps.service.justice.gov.uk"
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Calculate release dates API"
+}
+
+variable "namespace" {
+  default = "calculate-release-dates-api-prod"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "farsight-devs"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "true"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "calculate_release_dates"
+}
+
