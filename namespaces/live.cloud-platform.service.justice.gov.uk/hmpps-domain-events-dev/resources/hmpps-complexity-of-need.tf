2f798c5aee410886cfe1b1a1ae325f7e796251d4
Peter Phillips <peter@greenthistle.co.uk>
2021-12-01 14:42:50 +0000

    DT-2796: 🔨 Fix domain events dev to live cluster

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-complexity-of-need.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-complexity-of-need.tf
@@ -1,8 +1,9 @@
 resource "kubernetes_secret" "hmpps-complexity-of-need" {
   metadata {
-    name      = "hmpps-domain-events-topic"
+    name      = "hmpps-domain-events-topic-hmpps-complexity-of-need"
     namespace = var.namespace
     # Remove when namespace has been migrated
+    # name      = "hmpps-domain-events-topic"
     # namespace = "hmpps-complexity-of-need-staging"
   }
 
