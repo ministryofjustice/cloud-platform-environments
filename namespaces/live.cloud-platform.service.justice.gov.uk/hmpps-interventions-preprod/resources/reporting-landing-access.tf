f2cc317438fc8db9a0eb455cf64faad8a6b6614c
David Lantos <david.lantos@gmail.com>
2021-12-01 11:30:46 +0000

    Add IRSA modules for cross-account access

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-preprod/resources/reporting-landing-access.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-preprod/resources/reporting-landing-access.tf
@@ -1,3 +1,20 @@
+module "reporting_irsa" {
+  source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.0.2"
+  namespace        = var.namespace
+  role_policy_arns = [aws_iam_policy.ndmis_policy.arn]
+  service_account  = "${var.namespace}-to-ndmis-s3"
+}
+
+resource "aws_iam_policy" "ndmis_policy" {
+  name   = "${var.namespace}-ndmis-policy"
+  policy = data.aws_iam_policy_document.reporting_access.json
+}
+
+locals {
+  bucket_name   = "eu-west-2-delius-stage-dfi-extracts"
+  bucket_prefix = "dfinterventions/dfi"
+}
+
 data "aws_iam_policy_document" "reporting_access" {
   statement {
     sid = "AllowDataExportUserToListS3Buckets"
@@ -21,10 +38,8 @@ data "aws_iam_policy_document" "reporting_access" {
     ]
 
     resources = [
-      "arn:aws:s3:::eu-west-2-delius-stage-dfi-extracts/dfinterventions/dfi/*",
-      "arn:aws:s3:::eu-west-2-delius-stage-dfi-extracts/dfinterventions/dfi/",
-      "arn:aws:s3:::eu-west-2-delius-stage-dfi-extracts/exports/csv/reports/*",
-      "arn:aws:s3:::eu-west-2-delius-stage-dfi-extracts/exports/csv/reports/"
+      "arn:aws:s3:::${local.bucket_name}/${local.bucket_prefix}/*",
+      "arn:aws:s3:::${local.bucket_name}/${local.bucket_prefix}/"
     ]
   }
 }
@@ -55,10 +70,22 @@ resource "kubernetes_secret" "reporting_aws_secret" {
   }
 
   data = {
-    destination_bucket = "s3://eu-west-2-delius-stage-dfi-extracts/dfinterventions/dfi"
+    destination_bucket = "s3://${local.bucket_name}/${local.bucket_prefix}"
     user_arn           = aws_iam_user.reporting_user.arn
     access_key_id      = aws_iam_access_key.reporting_user.id
     secret_access_key  = aws_iam_access_key.reporting_user.secret
-    bucket_name        = "eu-west-2-delius-stage-dfi-extracts"
+    bucket_name        = local.bucket_name
+  }
+}
+
+resource "kubernetes_secret" "reporting_irsa" {
+  metadata {
+    name      = "to-ndmis-s3-irsa"
+    namespace = var.namespace
+  }
+
+  data = {
+    role           = module.reporting_irsa.aws_iam_role_name
+    serviceaccount = module.reporting_irsa.service_account_name.name
   }
 }
