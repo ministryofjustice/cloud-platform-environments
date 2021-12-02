2e4809a10f661c9d24916d3334938483bd5fc2fe
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-12-02 13:59:36 +0000

    Update rds.tf

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
@@ -11,7 +11,6 @@ module "court_case_service_rds" {
   application                 = var.application
   environment-name            = var.environment-name
   infrastructure-support      = var.infrastructure-support
-  rds_family                  = var.rds-family
   db_engine_version           = var.db_engine_version
   allow_major_version_upgrade = false
   engine_version              = "13.3"
