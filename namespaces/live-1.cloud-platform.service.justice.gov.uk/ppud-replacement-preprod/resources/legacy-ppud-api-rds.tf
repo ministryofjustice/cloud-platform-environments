fc82000e765d363a1c345799a15826899a8f6d91
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2021-12-03 10:11:37 +0000

    Clean up the deprecated `manage-recalls` resources
    These have been migrated to `manage-recalls-preprod`.
    
    ref PUD-1102

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/legacy-ppud-api-rds.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/ppud-replacement-preprod/resources/legacy-ppud-api-rds.tf
@@ -1,46 +1,4 @@
 ##
-## PostgreSQL - Application Database
-##
-
-module "ppud_replacement_preprod_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
-
-  cluster_name           = var.cluster_name
-  namespace              = var.namespace
-  application            = var.application
-  business-unit          = var.business_unit
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
-  is-production          = var.is_production
-  team_name              = var.team_name
-
-  rds_name          = "ppud-replacement-preprod"
-  rds_family        = "postgres13"
-  db_engine         = "postgres"
-  db_engine_version = "13"
-  db_instance_class = "db.t3.small"
-  db_name           = "manage_recalls"
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "ppud_replacement_preprod_rds_secrets" {
-  metadata {
-    name      = "manage-recalls-database"
-    namespace = var.namespace
-  }
-
-  data = {
-    host     = module.ppud_replacement_preprod_rds.rds_instance_address
-    name     = module.ppud_replacement_preprod_rds.database_name
-    username = module.ppud_replacement_preprod_rds.database_username
-    password = module.ppud_replacement_preprod_rds.database_password
-  }
-}
-
-##
 ## SQL Server - Lumen/PPUD Copy
 ##
 
