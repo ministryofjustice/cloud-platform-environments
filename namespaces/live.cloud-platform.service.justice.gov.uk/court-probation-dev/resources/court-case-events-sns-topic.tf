9630a69996bd34f4b2374d75f6208f55c25cd0e3
John Evans <john.evans@digital.justice.gov.uk>
2021-12-16 12:34:36 +0000

    🚚 PIC-1943: Migrate court-probation-dev to live

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/court-case-events-sns-topic.tf
@@ -0,0 +1,25 @@
+module "court-case-events" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.3"
+
+  team_name          = var.team_name
+  topic_display_name = "court-case-events"
+  encrypt_sns_kms    = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "court-case-events" {
+  metadata {
+    name      = "court-case-events-topic"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.court-case-events.access_key_id
+    secret_access_key = module.court-case-events.secret_access_key
+    topic_arn         = module.court-case-events.topic_arn
+  }
+}
+
