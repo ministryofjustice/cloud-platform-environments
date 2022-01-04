3b35faf1c4f4be27233cb7a99bf64cfa9d9e9f05
Lucas Cairns <lucas.cairns@gmail.com>
2022-01-04 13:55:30 +0000

    Migrate namespace hmpps-assess-risks-and-needs-dev to live (#6447)
    * Migrate namespace hmpps-assess-risks-and-needs-dev to live
    
    * dummy-ammendment
    
    Co-authored-by: PaulWyborn <paul.wyborn@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-dev/resources/rds.tf
@@ -0,0 +1,36 @@
+
+module "hmpps_assess_risks_and_needs_dev_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.7"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment_name
+  infrastructure-support = var.infrastructure_support
+  rds_family             = var.rds_family
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_assess_risks_and_needs_dev_rds_secret" {
+  metadata {
+    name      = "hmpps-assess-risks-and-needs-rds-instance"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_assess_risks_and_needs_dev_rds.rds_instance_endpoint
+    database_name         = module.hmpps_assess_risks_and_needs_dev_rds.database_name
+    database_username     = module.hmpps_assess_risks_and_needs_dev_rds.database_username
+    database_password     = module.hmpps_assess_risks_and_needs_dev_rds.database_password
+    rds_instance_address  = module.hmpps_assess_risks_and_needs_dev_rds.rds_instance_address
+    access_key_id         = module.hmpps_assess_risks_and_needs_dev_rds.access_key_id
+    secret_access_key     = module.hmpps_assess_risks_and_needs_dev_rds.secret_access_key
+    url                   = "postgres://${module.hmpps_assess_risks_and_needs_dev_rds.database_username}:${module.hmpps_assess_risks_and_needs_dev_rds.database_password}@${module.hmpps_assess_risks_and_needs_dev_rds.rds_instance_endpoint}/${module.hmpps_assess_risks_and_needs_dev_rds.database_name}"
+  }
+}
