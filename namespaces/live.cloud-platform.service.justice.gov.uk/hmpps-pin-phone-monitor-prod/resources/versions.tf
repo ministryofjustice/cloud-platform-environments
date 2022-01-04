10f282dfabb6679fa391d05c93e06eeab668f19a
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2022-01-04 15:54:54 +0000

    PAT-2124: 🔧 Migrate to live context for hmpps-pin-phone-monitor-prod (#6461)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-prod/resources/versions.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    pingdom = {
+      source  = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
+}
