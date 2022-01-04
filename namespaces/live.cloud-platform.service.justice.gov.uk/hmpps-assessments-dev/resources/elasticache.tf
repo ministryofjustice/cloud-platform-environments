4def09dbc8a9af257f83c98976a2b4fec2ed7bc7
Lucas Cairns <lucas.cairns@gmail.com>
2022-01-04 13:25:43 +0000

    Migrate namespace hmpps-assessments-dev to live (#6444)
    * Migrate namespace hmpps-assessments-dev to live
    
    * dummy-ammendment
    
    Co-authored-by: PaulWyborn <paul.wyborn@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assessments-dev/resources/elasticache.tf
@@ -0,0 +1,31 @@
+module "hmpps_assessments_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.3"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  namespace              = var.namespace
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment_name
+  infrastructure-support = var.infrastructure_support
+  node_type              = "cache.t2.small"
+  engine_version         = "6.x"
+  parameter_group_name   = "default.redis6.x"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_assessments_elasticache_redis" {
+  metadata {
+    name      = "hmpps-assessments-elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.hmpps_assessments_elasticache_redis.primary_endpoint_address
+    auth_token               = module.hmpps_assessments_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.hmpps_assessments_elasticache_redis.member_clusters)
+  }
+}
