8c75ecabccef33e8ae50c54a11993898d090ba34
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2022-01-05 14:20:09 +0000

    evidencelibrary-prod: add pingdom checks (#6466)
    * added pingdom resource
    
    * corrected the url
    
    * corrceted the names
    
    * added the integration ids

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/pingdom.tf
@@ -0,0 +1,34 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "evidencelibraryui-pingdom" {
+  type                     = "http"
+  name                     = "evidencelibraryui - production - cloud-platform"
+  host                     = "evidence-library-prod.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_hmpps,application_evidencelibraryui,component_ping,isproduction_true,environment_production,performance_hub_dev"
+  probefilters             = "region:EU"
+  integrationids           = [114774]
+}
+
+resource "pingdom_check" "evidencelibraryapi-pingdom" {
+  type                     = "http"
+  name                     = "evidencelibraryapi - production - cloud-platform"
+  host                     = "evidence-library-api-prod.apps.live.cloud-platform.service.justice.gov.uk/health"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_hmpps,application_evidencelibraryapi,component_ping,isproduction_true,environment_production,performance_hub_dev"
+  probefilters             = "region:EU"
+  integrationids           = [114774]
+}
