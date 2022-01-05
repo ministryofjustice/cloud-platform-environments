46f18cb1b1e92cabbef1ae088e7c16119d9e1783
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2022-01-05 16:27:39 +0000

    url change (#6474)

--- namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/pingdom.tf
@@ -20,15 +20,31 @@ resource "pingdom_check" "evidencelibraryui-pingdom" {
 resource "pingdom_check" "evidencelibraryapi-pingdom" {
   type                     = "http"
   name                     = "evidencelibraryapi - production - cloud-platform"
-  host                     = "evidence-library-api-prod.apps.live.cloud-platform.service.justice.gov.uk/health"
+  host                     = "evidence-library-api-prod.apps.live.cloud-platform.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/"
+  url                      = "/health"
   encryption               = true
   port                     = 443
   tags                     = "businessunit_hmpps,application_evidencelibraryapi,component_ping,isproduction_true,environment_production,performance_hub_dev"
+  probefilters             = "region:EU"
+  integrationids           = [114774]
+}
+
+resource "pingdom_check" "evidencelibraryidentity-pingdom" {
+  type                     = "http"
+  name                     = "evidencelibraryidentity - production - cloud-platform"
+  host                     = "evidence-library-identity-prod.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/.well-known/openid-configuration"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_hmpps,application_evidencelibraryidentity,component_ping,isproduction_true,environment_production,performance_hub_dev"
   probefilters             = "region:EU"
   integrationids           = [114774]
 }
