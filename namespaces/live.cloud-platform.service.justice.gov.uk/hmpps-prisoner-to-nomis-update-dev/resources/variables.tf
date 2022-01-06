222b3ff8ea64b9383704a41163fb1b952336ceb7
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2022-01-06 10:06:13 +0000

    new prisoner to nomis update service (dev)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/variables.tf
@@ -0,0 +1,42 @@
+
+variable "cluster_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Prisoner to NOMIS Update"
+}
+
+variable "namespace" {
+  default = "hmpps-prisoner-to-nomis-update-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "dps-syscon"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "dps_dev"
+}
+
