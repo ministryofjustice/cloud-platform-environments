188f082514a8d13dfd1fa613013736341c54d7a2
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-01-06 09:10:35 +0000

    EKS: Create laa hmrc interface staging environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-hmrc-interface-staging/resources/variables.tf
@@ -0,0 +1,53 @@
+
+variable "cluster_name" {
+}
+
+variable "kubernetes_cluster" {
+}
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "LAA-HMRC Interface Service API"
+}
+
+variable "namespace" {
+  default = "laa-hmrc-interface-staging"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "laa-apply-for-legal-aid"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "uat"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "apply@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "apply-dev"
+}
+
+variable "github_owner" {
+  description = "Required by the github terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
