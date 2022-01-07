2d00bf1855d09a48804a95e00302f6b83ce0c837
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-01-07 17:01:30 +0000

    Remove cloudwatch log group for development and staging (#6494)
    * Remove cloudwatch loggroup for dev and staging
    
    * Reduce retention to 60 days

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-staging/resources/elasticsearch.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/peoplefinder-staging/resources/elasticsearch.tf
@@ -2,38 +2,6 @@
 # Peoplefinder
 # Application Elasticsearch cluster
 #################################################################################
-# For logging elastic search on cloudwatch
-resource "aws_cloudwatch_log_group" "peoplefinder_cloudwatch_log_group" {
-  name              = "/aws/aes/domains/peoplefinder-staging-es/application-logs"
-  retention_in_days = 60
-
-  tags = {
-    Environment = "staging"
-    Application = "peoplefinder"
-  }
-}
-
-data "aws_iam_policy_document" "elasticsearch_log_publishing_policy_doc" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:PutLogEventsBatch",
-    ]
-
-    resources = [aws_cloudwatch_log_group.peoplefinder_cloudwatch_log_group.arn]
-
-    principals {
-      identifiers = ["es.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_resource_policy" "elasticsearch_log_publishing_policy" {
-  policy_document = data.aws_iam_policy_document.elasticsearch_log_publishing_policy_doc.json
-  policy_name     = "peoplefinder-dev-elasticsearch-log-publishing-policy"
-}
 
 # Elastic search module
 module "peoplefinder_es" {
@@ -50,9 +18,6 @@ module "peoplefinder_es" {
   elasticsearch_version      = "7.9"
   aws-es-proxy-replica-count = 1
   instance_type              = "t3.medium.elasticsearch"
-
-  log_publishing_application_cloudwatch_log_group_arn = aws_cloudwatch_log_group.peoplefinder_cloudwatch_log_group.arn
-  log_publishing_application_enabled                  = true
 }
 
 module "ns_annotation" {
