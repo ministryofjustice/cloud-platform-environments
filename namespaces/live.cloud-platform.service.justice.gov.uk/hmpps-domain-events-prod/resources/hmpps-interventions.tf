5b7bbf4556ffe6804b509f9fff66fe3b5fc8c7c4
Peter Phillips <peter@greenthistle.co.uk>
2022-01-07 15:53:33 +0000

    ✨ Migrate domain events production

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/hmpps-interventions.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_secret" "intervention_global_events_sns" {
+  metadata {
+    name      = "hmpps-domain-events-topic-hmpps-interventions"
+    namespace = var.namespace
+    # Remove when namespace has been migrated
+    # name      = "hmpps-domain-events-topic"
+    # namespace = "hmpps-interventions-prod"
+  }
+
+  data = {
+    access_key_id     = module.hmpps-domain-events.access_key_id
+    secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
+  }
+}
