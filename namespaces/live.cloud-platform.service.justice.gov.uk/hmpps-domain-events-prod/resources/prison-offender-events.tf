3290f68a79b49c3608c0c269cf4fb0f0f8726cc1
petergphillips <peter@greenthistle.co.uk>
2022-01-10 10:06:55 +0000

    ✨ Migrate domain events production (#6511)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/prison-offender-events.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_secret" "prison-offender-events" {
+  metadata {
+    name      = "hmpps-domain-events-topic-offender-events"
+    namespace = var.namespace
+    # Remove when namespace has been migrated
+    # name      = "hmpps-domain-events-topic"
+    # namespace = "offender-events-prod"
+  }
+
+  data = {
+    access_key_id     = module.hmpps-domain-events.access_key_id
+    secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
+  }
+}
