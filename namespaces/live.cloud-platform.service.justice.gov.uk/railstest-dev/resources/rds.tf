df41f9f10954ae0ff3e157e2594abdbd79ccfe8e
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2022-01-13 12:18:05 +0000

    environment_name to environment-name

--- namespaces/live.cloud-platform.service.justice.gov.uk/railstest-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/railstest-dev/resources/rds.tf
@@ -12,7 +12,7 @@ module "rds" {
   business-unit          = var.business_unit
   application            = var.application
   is-production          = var.is_production
-  environment-name       = var.environment_name
+  environment-name       = var.environment-name
   infrastructure-support = var.infrastructure_support
   namespace              = var.namespace
 
