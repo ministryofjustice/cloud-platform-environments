d51fad8b0b4e00019f651dd1d9179a9bcea86620
carlov20 <carlov20@users.noreply.github.com>
2022-01-18 14:27:34 +0000

    WFP-799 migrated namespace to live

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
@@ -22,8 +22,6 @@ module "rds-live" {
 
   db_allocated_storage = "59"
 
-  snapshot_identifier = "rds:cloud-platform-78407cd5fbd86ed5-2021-11-26-02-09"
-
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "true"
 
