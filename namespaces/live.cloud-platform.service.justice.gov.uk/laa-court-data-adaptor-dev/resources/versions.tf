8e600c1f556938b109cfe27990d1687d8438eaa1
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-01-18 16:09:24 +0100

    Migrate laa-court-data-adaptor-dev namespace env to 'live' cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/versions.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    github = {
+      source = "integrations/github"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    pingdom = {
+      source  = "russellcardullo/pingdom"
+      version = "1.1.3"
+    }
+  }
+}
