6cf53888c0615942e12fc70b2dd2b0c884c29ba1
said-moj <said.shire@digital.justice.gov.uk>
2022-01-19 11:48:25 +0000

    Migrate laa-legal-adviser-api-production to new live cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-production/resources/elasticache.tf
@@ -0,0 +1,30 @@
+module "celery-broker" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.3"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  application            = var.application
+  is-production          = var.is-production
+  node_type              = "cache.t2.medium"
+  environment-name       = var.environment-name
+  infrastructure-support = var.email
+  engine_version         = "4.0.10"
+  parameter_group_name   = "default.redis4.0"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "celery-broker" {
+  metadata {
+    name      = "celery-broker"
+    namespace = var.namespace
+  }
+
+  data = {
+    url                      = "redis://:${module.celery-broker.auth_token}@${module.celery-broker.primary_endpoint_address}:6379"
+    primary_endpoint_address = module.celery-broker.primary_endpoint_address
+    auth_token               = module.celery-broker.auth_token
+  }
+}
