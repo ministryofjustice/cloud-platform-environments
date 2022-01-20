5fb154afe9e163d65ea4a60cd54ffd7d244ff60d
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-01-20 14:55:02 +0000

    HAAR-9: 🔧 Migrate to live context for hmpps-registers-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-prod/resources/pingdom.tf
@@ -0,0 +1,88 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 96624 = #dps_alerts
+# 96628 = DPS Pager duty
+
+resource "pingdom_check" "court-register-production-check" {
+  type                     = "http"
+  name                     = "DPS - court-register"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain_court_register}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
+resource "pingdom_check" "prison-register-production-check" {
+  type                     = "http"
+  name                     = "DPS - prison-register"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain_prison_register}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
+resource "pingdom_check" "hmpps-registers-production-check" {
+  type                     = "http"
+  name                     = "DPS - hmpps-registers"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain_hmpps_registers}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
+resource "pingdom_check" "hmpps-registers-to-delius-production-check" {
+  type                     = "http"
+  name                     = "DPS - hmpps-registers-to-delius-update"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain_hmpps_registers_to_delius}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
+
+resource "pingdom_check" "hmpps-registers-to-nomis-production-check" {
+  type                     = "http"
+  name                     = "DPS - hmpps-registers-to-nomis-update"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain_hmpps_registers_to_nomis}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
+
