e2041061e981cd404df8e54b38b32449ba5a2784
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-01-20 13:18:19 +0000

    HAAR-9: 🔧 Migrate to live context for hmpps-audit-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/hmpps-audit-queue.tf
@@ -0,0 +1,73 @@
+
+
+module "hmpps_audit_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.5"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "hmpps_audit_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+  namespace                 = var.namespace
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.hmpps_audit_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+
+EOF
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+
+module "hmpps_audit_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.5"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "hmpps_audit_dlq"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_audit_queue_secret" {
+  metadata {
+    name      = "sqs-audit-queue-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.hmpps_audit_queue.access_key_id
+    secret_access_key = module.hmpps_audit_queue.secret_access_key
+    sqs_queue_url     = module.hmpps_audit_queue.sqs_id
+    sqs_queue_arn     = module.hmpps_audit_queue.sqs_arn
+    sqs_queue_name    = module.hmpps_audit_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "hmpps_audit_dead_letter_queue_secret" {
+  metadata {
+    name      = "sqs-audit-queue-dl-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.hmpps_audit_dead_letter_queue.access_key_id
+    secret_access_key = module.hmpps_audit_dead_letter_queue.secret_access_key
+    sqs_queue_url     = module.hmpps_audit_dead_letter_queue.sqs_id
+    sqs_queue_arn     = module.hmpps_audit_dead_letter_queue.sqs_arn
+    sqs_queue_name    = module.hmpps_audit_dead_letter_queue.sqs_name
+  }
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/variables.tf
@@ -0,0 +1,41 @@
+variable "namespace" {
+  default = "hmpps-audit-prod"
+}
+
+variable "cluster_name" {
+}
+
+variable "domain_audit_api" {
+  default = "audit-api.hmpps.service.justice.gov.uk"
+}
+
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "application" {
+  description = "The name of the application"
+  default     = "HMPPS-Audit-Service"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Digital-Prison-Services"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is-production" {
+  default = "true"
+}
+
