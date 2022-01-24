5fa096e623e1f3c7a9d37fc5fd54b7e45fcc560c
Carlos Fernandez <carlos.fernandez@digital.justice.gov.uk>
2022-01-24 11:43:04 +0000

    added service account for both soc entry and soc reporting and modified ecr.tf to enable git actions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/soc-reporting-dev/resources/serviceaccount.tf
@@ -0,0 +1,23 @@
+# to create github actions/service account for soc reporting
+module "serviceaccount-reporting" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["socreporting"]
+}
+
+# to create github actions/service account for soc entry
+module "serviceaccount-entry" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.5"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["socentry"]
+}
