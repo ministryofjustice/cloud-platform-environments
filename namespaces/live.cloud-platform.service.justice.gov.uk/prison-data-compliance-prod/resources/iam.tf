569fd33070c39453b6157a1401b336a31c27d583
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-01-24 17:57:56 +0000

    GDPR-340: 🔧 Migrate to live context for prison-data-compliance-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-data-compliance-prod/resources/iam.tf
@@ -0,0 +1,42 @@
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+data "aws_iam_policy_document" "data_compliance_prod_ap_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    resources = [
+      "arn:aws:iam::593291632749:role/pulumi_typescript_nomis_gdpr_role-aa611b4",
+    ]
+  }
+}
+
+resource "aws_iam_user" "user" {
+  name = "data-compliance-prod-ap-user-${random_id.id.hex}"
+  path = "/system/data-compliance-ap-users/"
+}
+
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name   = "data-compliance-ap-policy"
+  policy = data.aws_iam_policy_document.data_compliance_prod_ap_policy.json
+  user   = aws_iam_user.user.name
+}
+
+resource "kubernetes_secret" "data_compliance_ap_user" {
+  metadata {
+    name      = "data-compliance-ap-user"
+    namespace = var.namespace
+  }
+
+  data = {
+    data_compliance_ap_user_arn          = aws_iam_user.user.arn
+    data_compliance_ap_access_key_id     = aws_iam_access_key.user.id
+    data_compliance_ap_secret_access_key = aws_iam_access_key.user.secret
+  }
+}
