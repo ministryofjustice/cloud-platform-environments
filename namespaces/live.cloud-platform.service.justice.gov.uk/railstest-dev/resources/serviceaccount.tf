02e18a68b0f5facb3539d3b1b53e3a0f8f4930ad
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2022-01-25 12:18:09 +0000

    github service account with repository and env variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/railstest-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/railstest-dev/resources/serviceaccount.tf
@@ -6,5 +6,9 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["ministryofjustice/ALB-UnclaimedBalanceindex"]
+  github_actions_secret_ecr_name       = var.github_actions_secret_ecr_name
+  github_actions_secret_ecr_url        = var.github_actions_secret_ecr_url
+  github_actions_secret_ecr_access_key = var.github_actions_secret_ecr_access_key
+  github_actions_secret_ecr_secret_key = var.github_actions_secret_ecr_secret_key
 }
