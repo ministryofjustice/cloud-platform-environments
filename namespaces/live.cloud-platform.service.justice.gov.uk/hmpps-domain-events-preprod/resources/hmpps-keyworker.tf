b87e300632ea259d8e3578364c2b5fc5501c00bc
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-01-25 09:44:14 +0000

    NN-3922 copying domain event secrets into keyworker preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/hmpps-keyworker.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/hmpps-keyworker.tf
@@ -73,9 +73,7 @@ module "keyworker_api_dead_letter_queue" {
 resource "kubernetes_secret" "keyworker_api_queue" {
   metadata {
     name      = "kw-sqs-complexity-of-need-instance-output"
-    namespace = var.namespace
-    # Remove when namespace has been migrated
-    # namespace = "keyworker-api-preprod"
+    namespace = "keyworker-api-preprod"
   }
 
   data = {
@@ -90,9 +88,8 @@ resource "kubernetes_secret" "keyworker_api_queue" {
 resource "kubernetes_secret" "keyworker_api_dead_letter_queue" {
   metadata {
     name      = "kw-sqs-complexity-of-need-dl-instance-output"
-    namespace = var.namespace
-    # Remove when namespace has been migrated
-    # namespace = "keyworker-api-preprod"
+    Remove when namespace has been migrated
+    namespace = "keyworker-api-preprod"
   }
 
   data = {
