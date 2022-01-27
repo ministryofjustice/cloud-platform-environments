2ffc9d0f0d0222b9b232eadac6e4faa8e1f228c2
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2022-01-27 14:33:46 +0000

    PPD - promote transcribe iam and s3 permission changes to QA (#6874)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-qa/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-qa/resources/s3.tf
@@ -112,7 +112,11 @@ resource "aws_s3_bucket_policy" "hmpps_pin_phone_monitor_s3_ip_deny_policy" {
           },
           "Bool" : { "aws:ViaAWSService" : "false" },
           "StringNotEquals" : {
-            "aws:PrincipalArn" : ["${aws_iam_role.translate_s3_data_role.arn}", "${aws_iam_user.bt_upload_user.arn}"]
+            "aws:PrincipalArn" : [
+              "${aws_iam_role.translate_s3_data_role.arn}",
+              "${aws_iam_role.transcribe_s3_data_role.arn}",
+              "${aws_iam_user.bt_upload_user.arn}"
+            ]
           }
         }
       },
