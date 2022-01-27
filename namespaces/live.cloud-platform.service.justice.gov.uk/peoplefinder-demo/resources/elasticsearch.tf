280533a002dac8f08861950b74994267fecf484b
= <javid.ali@digital.justice.gov.uk>
2022-01-27 07:53:54 +0000

    peoplefinder-demo Migrate from live1 to live

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-demo/resources/elasticsearch.tf
@@ -0,0 +1,27 @@
+################################################################################
+# Peoplefinder
+# Application Elasticsearch cluster
+#################################################################################
+
+module "peoplefinder_es" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.2"
+  cluster_name           = var.cluster_name
+  application            = "peoplefinder"
+  business-unit          = "Central Digital"
+  environment-name       = "demo"
+  infrastructure-support = "people-finder-support@digital.justice.gov.uk"
+  is-production          = "false"
+  team_name              = "peoplefinder"
+  elasticsearch-domain   = "es"
+  namespace              = "peoplefinder-demo"
+  elasticsearch_version  = "6.8"
+  instance_type          = "t2.small.elasticsearch"
+  irsa_enabled           = true
+  assume_enabled         = false
+}
+
+module "ns_annotation" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-ns-annotation?ref=0.0.3"
+  ns_annotation_roles = [module.peoplefinder_es.aws_iam_role_name]
+  namespace           = var.namespace
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-demo/resources/variables.tf
@@ -0,0 +1,22 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * two variables are automatically supplied by the pipeline.
+ */
+
+variable "cluster_name" {}
+
+variable "namespace" {
+  default = "peoplefinder-demo"
+}
+
+variable "domain" {
+  default = "demo.peoplefinder.service.gov.uk"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "environment-name" {
+  default = "demo"
+}
