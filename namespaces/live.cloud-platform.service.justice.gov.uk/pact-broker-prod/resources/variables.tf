9c8b7abe61a33abe4f3082ec5253746c3e32bc48
David Lantos <david.lantos@gmail.com>
2022-01-27 11:14:47 +0000

    Add github provider configuration to pact-broker
    Missed this out from 351ce2b66746d4c7403f19b3b003b899fa1327af

--- namespaces/live.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/variables.tf
@@ -4,6 +4,16 @@ variable "cluster_name" {
 variable "kubernetes_cluster" {
 }
 
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
+
 variable "application" {
   description = "Name of Application you are deploying"
   default     = "Pact broker"
