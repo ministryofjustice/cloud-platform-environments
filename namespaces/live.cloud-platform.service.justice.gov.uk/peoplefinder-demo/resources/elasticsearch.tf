27fa157e8cda60647ab81cf72d7b5a309ef8848a
= <javid.ali@digital.justice.gov.uk>
2022-01-27 09:44:54 +0000

    peoplefinder-demo Update elastic search on live

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-demo/resources/elasticsearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-demo/resources/elasticsearch.tf
@@ -4,20 +4,22 @@
 #################################################################################
 
 module "peoplefinder_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.2"
-  cluster_name           = var.cluster_name
-  application            = "peoplefinder"
-  business-unit          = "Central Digital"
-  environment-name       = "demo"
-  infrastructure-support = "people-finder-support@digital.justice.gov.uk"
-  is-production          = "false"
-  team_name              = "peoplefinder"
-  elasticsearch-domain   = "es"
-  namespace              = "peoplefinder-demo"
-  elasticsearch_version  = "6.8"
-  instance_type          = "t2.small.elasticsearch"
-  irsa_enabled           = true
-  assume_enabled         = false
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.2"
+  cluster_name                = var.cluster_name
+  application                 = "peoplefinder"
+  business-unit               = "Central Digital"
+  environment-name            = "demo"
+  infrastructure-support      = "people-finder-support@digital.justice.gov.uk"
+  is-production               = "false"
+  team_name                   = "peoplefinder"
+  elasticsearch-domain        = "es"
+  namespace                   = "peoplefinder-demo"
+  elasticsearch_version       = "7.9"
+  instance_type               = "t2.small.elasticsearch"
+  aws-es-proxy-replica-count  = 2
+  instance_type               = "t3.medium.elasticsearch"
+  irsa_enabled                = true
+  assume_enabled              = false
 }
 
 module "ns_annotation" {
