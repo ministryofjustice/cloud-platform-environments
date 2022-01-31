c4412ea4186cbda8ffc299bc742399bd3e07bb14
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-01-31 16:54:11 +0000

    Create prison-visits-booking-dev namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-dev/resources/rds.tf
@@ -0,0 +1,36 @@
+module "prison-visits-rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.7"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  allow_major_version_upgrade = "true"
+  db_instance_class           = "db.m4.large"
+  rds_family                  = "postgres9.6"
+  db_engine_version           = "9.6"
+  db_allocated_storage        = "50"
+  db_engine                   = "postgres"
+  db_name                     = "visits"
+  db_parameter                = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "prison-visits-rds" {
+  metadata {
+    name      = "prison-visits-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    url = "postgres://${module.prison-visits-rds.database_username}:${module.prison-visits-rds.database_password}@${module.prison-visits-rds.rds_instance_endpoint}/${module.prison-visits-rds.database_name}"
+  }
+}
+
