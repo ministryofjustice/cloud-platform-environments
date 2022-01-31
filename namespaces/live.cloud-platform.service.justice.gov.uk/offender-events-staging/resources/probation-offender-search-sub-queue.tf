120df7cf0cef58a1d74a7faf6a022561c9c7ad0b
marcus.aspin <maspin@unilink.com>
2022-01-31 15:50:11 +0000

    Revert "PI-42 Migrate secrets for probation-offender-search-staging to correct namespace"
    This reverts commit b8728852c648f9c8880c63c7cc4bea696aeff9b1.

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-staging/resources/probation-offender-search-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-staging/resources/probation-offender-search-sub-queue.tf
@@ -71,7 +71,9 @@ module "probation_offender_search_event_dead_letter_queue" {
 resource "kubernetes_secret" "probation_offender_search_event_queue" {
   metadata {
     name      = "pose-sqs-instance-output"
-    namespace = "probation-offender-search-staging"
+    namespace = var.namespace
+    # Remove when namespace has been migrated
+    # namespace = "probation-offender-search-staging"
   }
 
   data = {
@@ -86,7 +88,9 @@ resource "kubernetes_secret" "probation_offender_search_event_queue" {
 resource "kubernetes_secret" "probation_offender_search_event_dead_letter_queue" {
   metadata {
     name      = "pose-sqs-dl-instance-output"
-    namespace = "probation-offender-search-staging"
+    namespace = var.namespace
+    # Remove when namespace has been migrated
+    # namespace = "probation-offender-search-staging"
   }
 
   data = {
