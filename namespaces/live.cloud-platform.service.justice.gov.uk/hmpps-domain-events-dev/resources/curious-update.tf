723a032eb1ef99756bde2aebbccd1fff82e60156
Richard Adams <richard.adams@digital.justice.gov.uk>
2022-02-08 14:35:06 +0000

    Add Curious SQS Queue to dev environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/curious-update.tf
@@ -0,0 +1,112 @@
+
+
+module "curious_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.5"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "curious_hmpps_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+  namespace                 = var.namespace
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.curious_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+
+EOF
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "curious_queue_policy" {
+  queue_url = module.curious_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.curious_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.curious_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+                      {
+                        "ArnEquals":
+                          {
+                            "aws:SourceArn": "${module.hmpps-domain-events.topic_arn}"
+                          }
+                        }
+        }
+      ]
+  }
+
+EOF
+
+}
+
+module "curious_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.5"
+
+  environment-name       = var.environment-name
+  team_name              = "MegaNexus"
+  infrastructure-support = var.infrastructure-support
+  application            = "Curious Synchronisation Service"
+  sqs_name               = "curious_hmpps_dlq"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "curious_queue" {
+  metadata {
+    name      = "sqs-hmpps-domain-events"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.curious_queue.access_key_id
+    secret_access_key = module.curious_queue.secret_access_key
+    sqs_queue_url     = module.curious_queue.sqs_id
+    sqs_queue_arn     = module.curious_queue.sqs_arn
+    sqs_queue_name    = module.curious_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "curious_dlq" {
+  metadata {
+    name      = "sqs-hmpps-domain-events-dlq"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.curious_dead_letter_queue.access_key_id
+    secret_access_key = module.curious_dead_letter_queue.secret_access_key
+    sqs_queue_url     = module.curious_dead_letter_queue.sqs_id
+    sqs_queue_arn     = module.curious_dead_letter_queue.sqs_arn
+    sqs_queue_name    = module.curious_dead_letter_queue.sqs_name
+  }
+}
+
+
+resource "aws_sns_topic_subscription" "curious_subscription" {
+  provider      = aws.london
+  topic_arn     = module.hmpps-domain-events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.curious_queue.sqs_arn
+  filter_policy = "{\"eventType\":[\"prison-offender-events.prisoner.received\"]}"
+}
+
+
