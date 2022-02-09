94c08d9b4f448f203ae58d1ea58faab80f8b6060
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-02-09 14:58:56 +0000

    Update serviceaccount.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/ubi-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ubi-staging/resources/serviceaccount.tf
@@ -3,7 +3,7 @@ module "serviceaccount" {
 
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
-  serviceaccount_name = sa-staging
+  serviceaccount_name = "sa-staging"
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories                  = ["ALB-UnclaimedBalanceindex"]
