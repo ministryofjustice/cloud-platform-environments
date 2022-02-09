2cad8a058b58944fef40e14606722d054adae271
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-02-09 10:17:17 +0000

    Update serviceaccount module (#7052)

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/jason-demo/resources/serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/jason-demo/resources/serviceaccount.tf
@@ -1,5 +1,5 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.3"
 
   namespace           = var.namespace
   github_repositories = [var.namespace]
