5212cb2297dfab1d75256510fc0e03a9bcc97973
Athar <atharmajeed@users.noreply.github.com>
2022-02-14 14:06:35 +0000

    Adding visibility_timeout_seconds param for a SQS. (#7083)
    * Adding visibility_timeout_seconds param for a SQS.
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/messaging-queues.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/messaging-queues.tf
@@ -280,16 +280,17 @@ module "hearing_resulted_dead_letter_queue" {
 module "prosecution_concluded_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.5"
 
-  environment-name          = var.environment_name
-  team_name                 = var.team_name
-  infrastructure-support    = var.infrastructure_support
-  application               = var.application
-  sqs_name                  = "prosecution-concluded-queue"
-  existing_user_name        = module.create_link_queue_m.user_name
-  encrypt_sqs_kms           = var.encrypt_sqs_kms
-  message_retention_seconds = var.message_retention_seconds
-  namespace                 = var.namespace
-  delay_seconds             = "120"
+  environment-name           = var.environment_name
+  team_name                  = var.team_name
+  infrastructure-support     = var.infrastructure_support
+  application                = var.application
+  sqs_name                   = "prosecution-concluded-queue"
+  existing_user_name         = module.create_link_queue_m.user_name
+  encrypt_sqs_kms            = var.encrypt_sqs_kms
+  message_retention_seconds  = var.message_retention_seconds
+  namespace                  = var.namespace
+  delay_seconds              = "120"
+  visibility_timeout_seconds = var.visibility_timeout_seconds
 
   redrive_policy = <<EOF
   {
