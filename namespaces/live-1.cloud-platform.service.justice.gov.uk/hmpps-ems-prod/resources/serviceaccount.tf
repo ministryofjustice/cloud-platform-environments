b48b5c8616ef13d604d08f16636f779721062000
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2022-02-14 12:50:53 +0000

    Remove GitHub Repository and Variable Override from Service Account Module (#7089)
    * Removing github_repositories as hmpps-ems-platform is private
    
    * Removing variable name overrides
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/serviceaccount.tf
@@ -1,11 +1,6 @@
 module "serviceaccount" {
-  source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
-  namespace                            = var.namespace
-  kubernetes_cluster                   = var.kubernetes_cluster
-  github_repositories                  = ["hmpps-ems-platform"]
-  serviceaccount_name                  = "moj-cloud-platform-github-actions-serviceaccount"
-  github_actions_secret_kube_cert      = "MOJ_CP_KUBE_CA_CERT"
-  github_actions_secret_kube_token     = "MOJ_CP_KUBE_TOKEN"
-  github_actions_secret_kube_cluster   = "MOJ_CP_KUBE_CLUSTER"
-  github_actions_secret_kube_namespace = "MOJ_CP_KUBE_NAMESPACE"
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  serviceaccount_name = "moj-cloud-platform-github-actions-serviceaccount"
 }
