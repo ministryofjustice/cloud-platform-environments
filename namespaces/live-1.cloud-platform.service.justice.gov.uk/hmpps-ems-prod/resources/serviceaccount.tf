90af46d9988b6d3067053ddc7e338545f4dff8cd
Jacob Woffenden <jacob@woffenden.io>
2022-02-14 12:44:11 +0000

    Removing github_repositories as hmpps-ems-platform is private

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/serviceaccount.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/serviceaccount.tf
@@ -2,7 +2,6 @@ module "serviceaccount" {
   source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
   namespace                            = var.namespace
   kubernetes_cluster                   = var.kubernetes_cluster
-  github_repositories                  = ["hmpps-ems-platform"]
   serviceaccount_name                  = "moj-cloud-platform-github-actions-serviceaccount"
   github_actions_secret_kube_cert      = "MOJ_CP_KUBE_CA_CERT"
   github_actions_secret_kube_token     = "MOJ_CP_KUBE_TOKEN"
