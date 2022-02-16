7e8ffbcd738bd727e426bba5b23c88778601b870
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-02-16 09:52:30 +0000

    Move prison-visits-booking-staging to live cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/pingdom_public.tf
@@ -0,0 +1,20 @@
+
+provider "pingdom" {
+}
+
+resource "pingdom_check" "prison-visits-booking-public-staging" {
+  type                     = "http"
+  name                     = "Prison Visits Public staging - Healthcheck"
+  host                     = "prison-visits-public-staging.apps.live-1.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ping"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_platforms,application_prometheus,component_healthcheck,isproduction_true,environment_uat,infrastructuresupport_platforms"
+  probefilters             = "region:EU"
+  integrationids           = [94618]
+}
+
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/variables.tf
@@ -0,0 +1,20 @@
+variable "environment-name" {
+  default = "staging"
+}
+
+variable "team_name" {
+  default = "prison-visits-booking"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "namespace" {
+  default = "prison-visits-booking-staging"
+}
+
+variable "infrastructure-support" {
+  default = "pvb-technical-support@digital.justice.gov.uk"
+}
+
