b20c73b7cb2a92768bd71d9f22cc2ec7ca03652e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-02-16 09:53:41 +0000

    Move prison-visits-booking-production to live cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-production/resources/variables.tf
@@ -0,0 +1,20 @@
+variable "environment-name" {
+  default = "production"
+}
+
+variable "team_name" {
+  default = "prison-visits-booking"
+}
+
+variable "is-production" {
+  default = "true"
+}
+
+variable "namespace" {
+  default = "prison-visits-booking-production"
+}
+
+variable "infrastructure-support" {
+  default = "pvb-technical-support@digital.justice.gov.uk"
+}
+
