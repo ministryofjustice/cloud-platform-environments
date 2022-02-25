c271eb1a50b0920dbde2366b2fe16501cadb8983
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-02-25 16:30:40 +0000

    Fix HMPPS Incentives testing S3 bucket policy …by using terraform for the policy document so that validation is easier, plus I'm pretty sure the policy resource was previously incorrect

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/s3.tf
@@ -10,32 +10,31 @@ module "analytical_platform_s3_bucket" {
   infrastructure-support = var.infrastructure_support
   namespace              = var.namespace
 
+  bucket_policy = data.aws_iam_policy_document.bucket-policy.json
+
   providers = {
     aws = aws.london
   }
-  bucket_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${module.analytical-platform.aws_iam_role_arn}"
-      },
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetBucketLocation",
-        "s3:ListObjectsV2",
-        "s3:GetObject",
-        "s3:GetObjectAcl"
-      ],
-      "Resource": [
-        "$${bucket_arn}/*"
-      ]
-    }
-  ]
 }
-EOF
+
+data "aws_iam_policy_document" "bucket-policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [module.analytical-platform.aws_iam_role_arn]
+    }
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListObjectsV2",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+    ]
+    resources = [
+      "${module.analytical_platform_s3_bucket.bucket_arn}/*",
+      module.analytical_platform_s3_bucket.bucket_arn
+    ]
+  }
 }
 
 resource "kubernetes_secret" "analytical_platform_s3_bucket" {
@@ -51,4 +50,3 @@ resource "kubernetes_secret" "analytical_platform_s3_bucket" {
     bucket_name       = module.analytical_platform_s3_bucket.bucket_name
   }
 }
-
