0f4250482d98c74d12e619371d7764c37a2a9ef5
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-03-03 19:29:26 +0000

    Update source argument

--- namespaces/live.cloud-platform.service.justice.gov.uk/courts-local-scorecard-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/courts-local-scorecard-dev/resources/serviceaccount.tf
@@ -1,6 +1,5 @@
 
 module "serviceaccount" {
-  source = "../"
-  namespace           = "courts-local-scorecard-dev"
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
   github_repositories = ["local-scorecard"]
 }
