fba78b4346fc85bc6ba03975e053fb9b6bb7e32c
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2022-03-03 10:10:06 +0000

    Update namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-rds-dev/resources/variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-rds-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-rds-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "environment" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "jacob.browning@digital.justice.gov.uk"
+  default     = "dataengineering@digital.justice.gov.uk"
 }
 
 variable "is_production" {
