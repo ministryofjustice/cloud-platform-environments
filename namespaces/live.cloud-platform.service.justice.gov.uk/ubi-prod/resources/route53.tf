951e84b94c9c31a9675edd5f14978376a25ab781
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2022-03-07 17:00:55 +0000

    removed old cname and a records

--- namespaces/live.cloud-platform.service.justice.gov.uk/ubi-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ubi-prod/resources/route53.tf
@@ -24,26 +24,3 @@ resource "kubernetes_secret" "fum_route53_zone_sec" {
   }
 }
 
-
-resource "aws_route53_record" "add_arecord_production" {
-  name    = "find-unclaimed-court-money.service.justice.gov.uk"
-  zone_id = aws_route53_zone.fum_route53_zone.zone_id
-  type    = "A"
-  records = ["dualstack.ubi-p-loadb-1rk7d55h8z4t3-1469044134.eu-west-2.elb.amazonaws.com."]
-  ttl     = "300"
-}
-
-resource "aws_route53_record" "add_cname_validation" {
-  name    = "_39556719e8d565ea45417d38b40889c7.find-unclaimed-court-money.service.justice.gov.uk"
-  zone_id = aws_route53_zone.fum_route53_zone.zone_id
-  type    = "CNAME"
-  records = ["_06c52dec40633865541b0ff381d9eeb1.chvvfdvqrj.acm-validations.aws."]
-  ttl     = "300"
-}
-resource "aws_route53_record" "add_cname_validation_www" {
-  name    = "_6f2592c897ce1cb57321ce86823bba01.www.find-unclaimed-court-money.service.justice.gov.uk"
-  zone_id = aws_route53_zone.fum_route53_zone.zone_id
-  type    = "CNAME"
-  records = ["_b889b9713a37c39694f437333e017428.chvvfdvqrj.acm-validations.aws."]
-  ttl     = "300"
-}
