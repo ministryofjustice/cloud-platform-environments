54a2455a8cc7470cfbd4086585cc79bcfb9d52ed
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-03-11 14:45:14 +0000

    Migrate multiple prototype namespace to live
    This is related to:
    https://github.com/ministryofjustice/cloud-platform/issues/3577
    https://github.com/ministryofjustice/cloud-platform/issues/3576
    https://github.com/ministryofjustice/cloud-platform/issues/3578

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/recalldecisions-prototype/resources/variables.tf
@@ -0,0 +1,54 @@
+
+variable "cluster_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Recall Decisions"
+}
+
+variable "namespace" {
+  default = "recalldecisions-prototype"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Making Recall Decisions"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "ryan.haney@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "making-recall-decisions"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
