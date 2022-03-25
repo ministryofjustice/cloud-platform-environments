c7b6c582989cd5f1646231aa22f59179e094e497
Andrew Larcombe <andrew.larcombe@mj003564.lan>
2022-03-25 11:34:50 +0000

    Creates new S3 bucket with access for data science role user

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-dev/resources/onnx_s3_bucket.tf
@@ -0,0 +1,61 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+/*
+ * An S3 bucket that the data science team can use to place updated ONNX files into
+ * for use by assess risks and needs
+ * 
+ */
+module "hmpps_assess_risks_and_needs_onnx_s3_bucket" {
+
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.7"
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+
+  bucket_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::593291632749:role/service-role/analytical-platform-hub-export"
+      },
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "$${bucket_arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+resource "kubernetes_secret" "hmpps_assess_risks_and_needs_onnx_s3_bucket" {
+  metadata {
+    name      = "hmpps-assess-risks-and-needs-onnx-s3-bucket"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.s3_bucket.access_key_id
+    secret_access_key = module.s3_bucket.secret_access_key
+    bucket_arn        = module.s3_bucket.bucket_arn
+    bucket_name       = module.s3_bucket.bucket_name
+  }
+}
