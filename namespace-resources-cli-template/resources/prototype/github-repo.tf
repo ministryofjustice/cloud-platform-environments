6eba224cbefa5ce6c78e77606305b718e30f61a5
Vijay Veeranki <46675900+vijay-veeranki@users.noreply.github.com>
2022-03-30 18:50:22 +0100

    Update github-repo.tf
    Remove secret in prototype, as this is already created in module
    https://github.com/ministryofjustice/cloud-platform-terraform-github-prototype/blob/main/main.tf#L56-L60

--- namespace-resources-cli-template/resources/prototype/github-repo.tf
+++ namespace-resources-cli-template/resources/prototype/github-repo.tf
@@ -7,9 +7,3 @@ module "github-prototype" {
 
   namespace = var.namespace
 }
-
-resource "github_actions_secret" "prototype" {
-  repository      = var.namespace
-  secret_name     = "PROTOTYPE_NAME"
-  plaintext_value = var.namespace
-}
