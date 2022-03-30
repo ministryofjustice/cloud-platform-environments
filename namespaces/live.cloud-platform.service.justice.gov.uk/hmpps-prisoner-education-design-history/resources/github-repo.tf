5c77c2e07eca6c7608cec04bee497aa8867b73f1
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2022-03-30 18:44:23 +0100

    Use design history terraform module

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/github-repo.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/github-repo.tf
@@ -3,13 +3,7 @@
 # continuous deployment (CD) workflow in prototype github repo.
 
 module "github-prototype" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-github-prototype?ref=0.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-github-design-history?ref=main"
 
   namespace = var.namespace
-}
-
-resource "github_actions_secret" "prototype" {
-  repository      = var.namespace
-  secret_name     = "PROTOTYPE_NAME"
-  plaintext_value = var.namespace
 }
