95adea6676ed3b06e272cd9ed06f2c481c9f669e
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2022-03-30 19:01:31 +0100

    Correct module name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/github-repo.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/github-repo.tf
@@ -2,7 +2,7 @@
 # This module creates files to build docker image and 
 # continuous deployment (CD) workflow in prototype github repo.
 
-module "github-prototype" {
+module "github-design-history" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-github-design-history?ref=main"
 
   namespace = var.namespace
