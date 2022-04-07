ae6738ace16453ba7d9fd29104493ec46e0294aa
jakemulley <jakemulley@users.noreply.github.com>
2022-04-07 10:25:47 +0100

    Fix Polygraph Offender Management kubernetes secret name

--- namespaces/live.cloud-platform.service.justice.gov.uk/polygraph-offender-management/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/polygraph-offender-management/resources/rds-postgresql.tf
@@ -43,7 +43,7 @@ module "rds_postgresql" {
 
 resource "kubernetes_secret" "rds_postgresql" {
   metadata {
-    name      = "rds-instance-output"
+    name      = "rds-postgresql-instance-output"
     namespace = var.namespace
   }
 
@@ -66,7 +66,7 @@ resource "kubernetes_secret" "rds_postgresql" {
 
 resource "kubernetes_config_map" "rds_postgresql" {
   metadata {
-    name      = "rds-instance-output"
+    name      = "rds-postgresql-instance-output"
     namespace = var.namespace
   }
 
