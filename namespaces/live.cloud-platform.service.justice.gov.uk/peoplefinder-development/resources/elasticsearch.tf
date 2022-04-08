8cc70549a2d85f07fc49ebb64c7215d8bff3bc1a
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-04-08 14:56:26 +0100

    Update branch to test the kiam removal (#7703)
    * Remove ns_annotation module
    
    * Upgrade elasticsearch module to latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/elasticsearch.tf
@@ -5,7 +5,7 @@
 
 # Elastic search module
 module "peoplefinder_es" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.2"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.3"
   cluster_name               = var.cluster_name
   application                = "peoplefinder"
   business-unit              = "Central Digital"
@@ -18,13 +18,4 @@ module "peoplefinder_es" {
   elasticsearch_version      = "7.9"
   aws-es-proxy-replica-count = 2
   instance_type              = "t3.medium.elasticsearch"
-
-  irsa_enabled   = true
-  assume_enabled = false
-}
-
-module "ns_annotation" {
-  source              = "github.com/ministryofjustice/cloud-platform-terraform-ns-annotation?ref=0.0.3"
-  ns_annotation_roles = [module.peoplefinder_es.aws_iam_role_name]
-  namespace           = var.namespace
 }
