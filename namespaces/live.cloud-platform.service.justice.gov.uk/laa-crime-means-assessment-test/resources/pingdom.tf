20d73fac10aac838fc6c2feaf0a4e44c7122bcff
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-04-12 14:13:31 +0100

    Update Pingdom target endpoint and port in test namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-means-assessment-test/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-means-assessment-test/resources/pingdom.tf
@@ -4,14 +4,14 @@ provider "pingdom" {
 resource "pingdom_check" "laa-crime-means-assessment-test" {
   type                     = "http"
   name                     = "LAA Crime Means Assessment - Test"
-  host                     = "test.laa-crime-means-assessment.service.justice.gov.uk"
+  host                     = "laa-crime-means-assessment-test.apps.live.cloud-platform.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 3
   notifyagainevery         = 0
   url                      = "/actuator/health"
   encryption               = true
-  port                     = 8096
+  port                     = 443
   tags                     = "businessunit_laa,application_laa-crime-means-assessment,component_ping,isproduction_false,environment_test,owner_laa-crime-apps-team"
   probefilters             = "region:EU"
   integrationids           = [121160]
