061ce7f8d64ac2427b7b08b659fae4703c7c58fc
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-04-19 11:17:41 +0100

    Prepare the `make-recall-decision` namespace for the UI app to be deployed into it.

--- namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/variables.tf
@@ -34,7 +34,7 @@ variable "environment" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "unknown@digital.justice.gov.uk"
+  default     = "making-recall-decisions-tech@digital.justice.gov.uk"
 }
 
 variable "is_production" {
