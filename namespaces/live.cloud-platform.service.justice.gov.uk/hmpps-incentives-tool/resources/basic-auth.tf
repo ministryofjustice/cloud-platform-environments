8ed806a46a20790e0ab0dc342d518452810596ba
reyrrep <justin.perryer@digital.justice.gov.uk>
2022-04-20 12:46:42 +0100

    added hmpps-incentives-tool prototype to cloud platform

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-tool/resources/basic-auth.tf
@@ -0,0 +1,19 @@
+# Username and password for the prototype kit website's http basic
+# authentication
+
+resource "random_password" "password" {
+  length  = 16
+  special = false
+}
+
+resource "kubernetes_secret" "basic-auth" {
+  metadata {
+    name      = "basic-auth"
+    namespace = var.namespace
+  }
+
+  data = {
+    username = var.basic-auth-username
+    password = random_password.password.result
+  }
+}
