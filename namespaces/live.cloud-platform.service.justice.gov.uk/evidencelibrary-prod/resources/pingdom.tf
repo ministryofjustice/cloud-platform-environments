f9752ebfa7001034a160c6605b9011d5e31c4129
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2022-04-27 10:02:54 +0100

    removed unwanted identity resources. (#7825)

--- namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/pingdom.tf
@@ -32,19 +32,3 @@ resource "pingdom_check" "evidencelibraryapi-pingdom" {
   probefilters             = "region:EU"
   integrationids           = [114774]
 }
-
-resource "pingdom_check" "evidencelibraryidentity-pingdom" {
-  type                     = "http"
-  name                     = "evidencelibraryidentity - production - cloud-platform"
-  host                     = "evidence-library-identity-prod.apps.live.cloud-platform.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/.well-known/openid-configuration"
-  encryption               = true
-  port                     = 443
-  tags                     = "businessunit_hmpps,application_evidencelibraryidentity,component_ping,isproduction_true,environment_production,performance_hub_dev"
-  probefilters             = "region:EU"
-  integrationids           = [114774]
-}
