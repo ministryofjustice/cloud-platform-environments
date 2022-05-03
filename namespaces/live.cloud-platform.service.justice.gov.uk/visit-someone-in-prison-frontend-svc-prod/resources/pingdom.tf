25c81deb09c0181db238ed4e00c8ceeb2112e622
Richard <70637410+rj-adams@users.noreply.github.com>
2022-05-03 17:10:55 +0100

    Create visit-someone-in-prison-frontend-svc-prod namespace (#7848)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-prod/resources/pingdom.tf
@@ -0,0 +1,22 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 96624 = #dps_alerts
+# 96628 = DPS Pager duty
+
+resource "pingdom_check" "visit-someone-in-prison-staff-ui-production-check" {
+  type                     = "http"
+  name                     = "Visit Someone in Prison Staff UI"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/visit-someone-in-prison-staff.hmpps.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
