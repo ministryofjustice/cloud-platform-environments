648097ba45ee893e7f07cdc12e7e00391868316d
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-05-04 14:37:04 +0100

    Setup pingdom checks for 'make-recall-decision-prod' applications

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-prod/resources/pingdom.tf
@@ -0,0 +1,47 @@
+resource "pingdom_check" "make_recall_decision" {
+  type                     = "http"
+  name                     = "make-recall-decision - PROD"
+  host                     = "make-recall-decision.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "hmpps,make-recall-decision,prod,isproduction_true,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [121587]
+}
+
+resource "pingdom_check" "make_recall_decision_ui_health" {
+  type                     = "http"
+  name                     = "make-recall-decision-ui /ping - PROD"
+  host                     = "make-recall-decision.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ping"
+  encryption               = true
+  port                     = 443
+  tags                     = "hmpps,make-recall-decision,prod,isproduction_true,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [121587]
+}
+
+resource "pingdom_check" "make_recall_decision_api_health" {
+  type                     = "http"
+  name                     = "make-recall-decision-api /health - PROD"
+  host                     = "make-recall-decision-api.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/health"
+  encryption               = true
+  port                     = 443
+  tags                     = "hmpps,make-recall-decision,prod,isproduction_true,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [121587]
+}
