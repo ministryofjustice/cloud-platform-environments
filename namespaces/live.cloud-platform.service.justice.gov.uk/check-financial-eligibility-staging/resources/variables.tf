bfc0e9b027026dca6794182c5786076764cbecbe
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-05-09 08:39:33 +0100

    Add pingdom config to cfe staging environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-staging/resources/variables.tf
@@ -1,3 +1,22 @@
 variable "namespace" {
   default = "check-financial-eligibility-staging"
 }
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "check-financial-eligibility"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "staging"
+}
+
+variable "is_production" {
+  default = "false"
+}
