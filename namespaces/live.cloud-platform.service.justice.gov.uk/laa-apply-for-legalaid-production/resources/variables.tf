4c503d6ea1189976449361c66266bd7488d738ad
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-05-09 11:00:25 +0100

    Add pingdom configuration for laa-apply-for-legalaid-production

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-production/resources/variables.tf
@@ -1,3 +1,22 @@
 variable "namespace" {
   default = "laa-apply-for-legalaid-production"
 }
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "laa-apply-for-legalaid"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "production"
+}
+
+variable "is_production" {
+  default = "true"
+}
