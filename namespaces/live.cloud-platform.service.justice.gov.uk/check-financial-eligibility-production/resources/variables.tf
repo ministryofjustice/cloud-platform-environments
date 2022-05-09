be4976a019100a6740c085c79f5d430dda7383b0
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2022-05-09 09:29:54 +0100

    Add pingdom configuration to cfe production

--- namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/variables.tf
@@ -1,3 +1,22 @@
 variable "namespace" {
   default = "check-financial-eligibility-production"
 }
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "check-financial-eligibility"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "production"
+}
+
+variable "is_production" {
+  default = "true"
+}
