ec27678288df9e7aaabfd15e72bd380f1eddcce0
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-05-11 15:44:00 +0100

    MRD-126: Setup postgres for make-recall-decision-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-prod/resources/postgresql.tf
@@ -0,0 +1,41 @@
+##
+## PostgreSQL - Application Database
+##
+
+module "make_recall_decision_api_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.10"
+
+  cluster_name           = var.cluster_name
+  namespace              = var.namespace
+  application            = var.application
+  business-unit          = var.business_unit
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  is-production          = var.is_production
+  team_name              = var.team_name
+
+  rds_name          = "make-recall-decision-${var.environment}"
+  rds_family        = "postgres13"
+  db_engine         = "postgres"
+  db_engine_version = "13"
+  db_instance_class = "db.t3.small"
+  db_name           = "make_recall_decision"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "make_recall_decision_api_rds" {
+  metadata {
+    name      = "make-recall-decision-api-database"
+    namespace = var.namespace
+  }
+
+  data = {
+    host     = module.make_recall_decision_api_rds.rds_instance_address
+    name     = module.make_recall_decision_api_rds.database_name
+    username = module.make_recall_decision_api_rds.database_username
+    password = module.make_recall_decision_api_rds.database_password
+  }
+}
