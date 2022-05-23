748574130801070bce448ee998913d1e8f4bcbe7
Lall, Kulwant <Kulwant.Lall@justice.gov.uk>
2022-05-23 12:21:42 +0100

    Fix for variable.tf file

--- namespaces/live.cloud-platform.service.justice.gov.uk/chapsdotnet-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chapsdotnet-dev/resources/variables.tf
@@ -86,4 +86,19 @@ variable "github_actions_secret_kube_cluster" {
   default     = "KUBE_CLUSTER"
 }
 
+variable "github_actions_secret_kube_namespace" {
+  description = "The name of the github actions secret containing the kubernetes namespace name"
+  default     = "KUBE_NAMESPACE"
+}
+
+variable "github_actions_secret_kube_cert" {
+  description = "The name of the github actions secret containing the serviceaccount ca.crt"
+  default     = "KUBE_CERT"
+}
+
+variable "github_actions_secret_kube_token" {
+  description = "The name of the github actions secret containing the serviceaccount token"
+  default     = "KUBE_TOKEN"
+}
+
 
