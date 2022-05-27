a5fc362249f5cd1aa976d872f27d7136c8e45ccb
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-27 14:50:48 +0100

    Add `kubernetes_cluster` var but leave empty

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/variables.tf
@@ -7,6 +7,10 @@ variable "repo_name" {
   default     = "laa-apply-for-legal-aid"
 }
 
+# no longer used but presence still required
+variable "kubernetes_cluster" {
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   default     = "ministryofjustice"
