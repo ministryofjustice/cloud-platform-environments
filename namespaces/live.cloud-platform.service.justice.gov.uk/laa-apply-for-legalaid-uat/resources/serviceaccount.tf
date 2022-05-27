243511f64fb2c82b895c3629b3a2fc0ea3f2ab48
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-27 14:21:39 +0100

    Remove unavailable and unused var `kubernetes_cluster`

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/serviceaccount.tf
@@ -2,7 +2,6 @@ module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
 
   namespace = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
   serviceaccount_name = "github-actions"
 
   # Uncomment and provide repository names to create github actions secrets
