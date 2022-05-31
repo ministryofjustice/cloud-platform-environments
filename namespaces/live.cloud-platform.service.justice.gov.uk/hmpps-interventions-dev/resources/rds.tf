b305aeb2142a2041c60f5f581d10cd7cbd18270f
Kumar Thangavel <kumar.thangavel@digital.justice.gov.uk>
2022-05-31 14:39:44 +0100

    provisioning new postgres env for interventions dev (#7983)
    * provisioning new postgres env for interventions dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-dev/resources/rds.tf
@@ -32,3 +32,38 @@ resource "kubernetes_secret" "hmpps_interventions_rds" {
     url                   = "postgres://${module.hmpps_interventions_rds.database_username}:${module.hmpps_interventions_rds.database_password}@${module.hmpps_interventions_rds.rds_instance_endpoint}/${module.hmpps_interventions_rds.database_name}"
   }
 }
+
+module "hmpps_interventions_split_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.10"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  rds_family             = var.rds_family
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_interventions_split_rds" {
+  metadata {
+    name      = "postgres.split"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_interventions_split_rds.rds_instance_endpoint
+    database_name         = module.hmpps_interventions_split_rds.database_name
+    database_username     = module.hmpps_interventions_split_rds.database_username
+    database_password     = module.hmpps_interventions_split_rds.database_password
+    rds_instance_address  = module.hmpps_interventions_split_rds.rds_instance_address
+    access_key_id         = module.hmpps_interventions_split_rds.access_key_id
+    secret_access_key     = module.hmpps_interventions_split_rds.secret_access_key
+    url                   = "postgres://${module.hmpps_interventions_split_rds.database_username}:${module.hmpps_interventions_split_rds.database_password}@${module.hmpps_interventions_split_rds.rds_instance_endpoint}/${module.hmpps_interventions_split_rds.database_name}"
+  }
+}
