0c9eacce3cff08c52a4eaf5699d7ece507d5ef33
sivafirst <siva.sighakolli@digital.justice.gov.uk>
2022-06-08 13:07:53 +0100

    Added route53

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/evidencelibrary-prod/resources/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "evidencelibrary_route53_zone" {
+  name = var.domain
+
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.owner   
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "evidencelibrary_route53_zone_sec" {
+  metadata {
+    name      = "evidencelibrary-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id   = aws_route53_zone.evidencelibrary_route53_zone.zone_id
+  }
+}
