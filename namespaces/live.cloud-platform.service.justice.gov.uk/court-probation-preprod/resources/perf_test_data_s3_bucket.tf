2ae2d4c8b8d1c9daa9dee48c608eaee718ae0245
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-06-13 10:59:58 +0100

    typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/perf_test_data_s3_bucket.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/perf_test_data_s3_bucket.tf
@@ -1,5 +1,5 @@
 module "perf-test-data-s3-bucket" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.7.1
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.7.1"
 
   team_name              = var.team_name
   business-unit          = var.business-unit
