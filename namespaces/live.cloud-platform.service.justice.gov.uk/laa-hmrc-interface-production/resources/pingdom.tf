38522c218f9d462b929559e0dc57b3c1d2909882
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2022-06-14 16:28:54 +0100

    HMRC Production: Add pingdom check (#8061)
    Change the application variable to prevent issues
    with concourse deploying the new pingdom check

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-hmrc-interface-production/resources/pingdom.tf
@@ -0,0 +1,14 @@
+resource "pingdom_check" "laa-hmrc-interface-production" {
+  type             = "http"
+  name             = "LAA HMRC Interface - Production"
+  host             = "laa-hmrc-interface.cloud-platform.service.justice.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  notifyagainevery = 0
+  url              = "/ping"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.application},laa,apply-for-legal-aid"
+  probefilters     = "region:EU"
+  integrationids   = [96996, 121629]
+}
