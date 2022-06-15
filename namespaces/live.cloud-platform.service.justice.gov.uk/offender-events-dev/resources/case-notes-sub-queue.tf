982c7275f3fe81b2969972fdf88fc5a4189b6b57
Marcus Aspin <maspin@unilink.com>
2022-06-15 11:45:57 +0100

    PI-277 Grant SQS permissions from Delius consumer to case-notes queue in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/case-notes-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/case-notes-sub-queue.tf
@@ -7,49 +7,59 @@ module "case_note_poll_pusher_queue" {
   application               = var.application
   sqs_name                  = "case_note_poll_pusher_queue"
   encrypt_sqs_kms           = "true"
-  message_retention_seconds = 1209600
+  message_retention_seconds = 1209600 # 2 weeks
   namespace                 = var.namespace
-
-  redrive_policy = <<EOF
-  {
-    "deadLetterTargetArn": "${module.case_note_poll_pusher_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
-  }
-  
-EOF
-
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.case_note_poll_pusher_dead_letter_queue.sqs_arn,
+    maxReceiveCount     = 3
+  })
 
   providers = {
     aws = aws.london
   }
 }
 
+data "aws_iam_policy_document" "case_note_poll_pusher_policy" {
+  version = "2012-10-17"
+  id      = "${module.case_note_poll_pusher_queue.sqs_arn}/SQSDefaultPolicy"
+  statement {
+    sid    = "TopicToQueue"
+    effect = "Allow"
+    actions = [
+      "SQS:SendMessage"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [module.offender_events.topic_arn]
+    }
+    resources = [module.case_note_poll_pusher_queue.sqs_arn]
+  }
+  statement {
+    sid    = "QueueToConsumer"
+    effect = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::728765553488:role/delius-test-ecs-sqs-consumer"
+      ]
+    }
+    resources = [module.case_note_poll_pusher_queue.sqs_arn]
+  }
+}
+
 resource "aws_sqs_queue_policy" "case_note_poll_pusher_queue_policy" {
   queue_url = module.case_note_poll_pusher_queue.sqs_id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Id": "${module.case_note_poll_pusher_queue.sqs_arn}/SQSDefaultPolicy",
-    "Statement":
-      [
-        {
-          "Effect": "Allow",
-          "Principal": {"AWS": "*"},
-          "Resource": "${module.case_note_poll_pusher_queue.sqs_arn}",
-          "Action": "SQS:SendMessage",
-          "Condition":
-                      {
-                        "ArnEquals":
-                          {
-                            "aws:SourceArn": "${module.offender_events.topic_arn}"
-                          }
-                        }
-        }
-      ]
-  }
-   
-EOF
-
+  policy    = data.aws_iam_policy_document.case_note_poll_pusher_policy.json
 }
 
 module "case_note_poll_pusher_dead_letter_queue" {
@@ -61,7 +71,7 @@ module "case_note_poll_pusher_dead_letter_queue" {
   application               = var.application
   sqs_name                  = "case_note_poll_pusher_queue_dl"
   encrypt_sqs_kms           = "true"
-  message_retention_seconds = 1209600
+  message_retention_seconds = 1209600 # 2 weeks
   namespace                 = var.namespace
 
   providers = {
@@ -100,10 +110,21 @@ resource "kubernetes_secret" "case_note_poll_pusher_dead_letter_queue" {
 }
 
 resource "aws_sns_topic_subscription" "case_note_poll_pusher_subscription" {
-  provider      = aws.london
-  topic_arn     = module.offender_events.topic_arn
-  protocol      = "sqs"
-  endpoint      = module.case_note_poll_pusher_queue.sqs_arn
-  filter_policy = "{\"eventType\":[ \"PRISON-RELEASE\", \"TRANSFER-FROMTOL\", \"GEN-OSE\", \"ALERT-ACTIVE\", \"ALERT-INACTIVE\", {\"prefix\": \"OMIC\"}, {\"prefix\": \"OMIC_OPD\"}, {\"prefix\": \"KA\"} ] }"
+  provider  = aws.london
+  topic_arn = module.offender_events.topic_arn
+  protocol  = "sqs"
+  endpoint  = module.case_note_poll_pusher_queue.sqs_arn
+  filter_policy = jsonencode({
+    eventType = [
+      "PRISON-RELEASE",
+      "TRANSFER-FROMTOL",
+      "GEN-OSE",
+      "ALERT-ACTIVE",
+      "ALERT-INACTIVE",
+      { prefix = "OMIC" },
+      { prefix = "OMIC_OPD" },
+      { prefix = "KA" }
+    ]
+  })
 }
 
