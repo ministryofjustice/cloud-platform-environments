6c4063dde04b3981eab78e3cd9d8c1244a5382be
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-06-21 12:31:13 +0100

    VB=866: Add secrets to vsip-dev for access to audit queue

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-dev/resources/sqs.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_secret" "vsip_audit_secret" {
+  metadata {
+    name      = "sqs-hmpps-audit-secret"
+    namespace = "visit-someone-in-prison-frontend-svc-dev"
+  }
+
+  data = {
+    access_key_id     = module.hmpps_audit_queue.access_key_id
+    secret_access_key = module.hmpps_audit_queue.secret_access_key
+    sqs_queue_url     = module.hmpps_audit_queue.sqs_id
+    sqs_queue_arn     = module.hmpps_audit_queue.sqs_arn
+    sqs_queue_name    = module.hmpps_audit_queue.sqs_name
+  }
+}
