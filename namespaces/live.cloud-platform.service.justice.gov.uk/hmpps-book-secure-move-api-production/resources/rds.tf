e68912860d3431a5353e9beb399c2382c44bd034
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-06-23 09:30:53 +0100

    Fixing refererences to the Basm AWS read replica DB.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-production/resources/rds.tf
@@ -34,7 +34,7 @@ resource "kubernetes_secret" "rds-instance" {
   data = {
     access_key_id     = module.rds-instance.access_key_id
     secret_access_key = module.rds-instance.secret_access_key
-    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-instance.rds_instance_endpoint}/${module.rds-instance.database_name}"
+    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-read-replica.rds_instance_endpoint}/${module.rds-read-replica.database_name}"
   }
 }
 
@@ -72,8 +72,8 @@ resource "kubernetes_secret" "rds-read-replica" {
   }
 
   data = {
-    access_key_id     = module.rds-instance.access_key_id
-    secret_access_key = module.rds-instance.secret_access_key
-    url               = "postgres://${module.rds-instance.database_username}:${module.rds-instance.database_password}@${module.rds-read-replica.rds_instance_endpoint}/${module.rds-read-replica.database_name}"
+    access_key_id     = module.rds-read-replica.access_key_id
+    secret_access_key = module.rds-read-replica.secret_access_key
+    url               = "postgres://${module.rds-read-replica.database_username}:${module.rds-read-replica.database_password}@${module.rds-read-replica.rds_instance_endpoint}/${module.rds-read-replica.database_name}"
   }
 }
