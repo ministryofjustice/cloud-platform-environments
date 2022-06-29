00dd31dc708a7aeda32ad62224c8fd579ab2e6cc
Danjiv Ramkhalawon <Danjiv.Ramkhalawon@digital.justice.gov.uk>
2022-06-29 11:50:48 +0100

    adding creds to github secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-dev/resources/serviceaccount.tf
@@ -0,0 +1,8 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.4"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  github_repositories = ["analytical-platform-uploader"]
+}
