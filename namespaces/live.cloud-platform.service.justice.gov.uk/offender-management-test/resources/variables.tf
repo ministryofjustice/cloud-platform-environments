8a41ec244b8221f287438c0e36fff9705e0a16fc
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2022-07-06 10:56:57 +0100

    New offender-management-test environment
    ## Purpose
    
    This new environment is to allow us to test WIP code on branches using
    the same synthetic data as our staging environment. This allows us to
    test WIP code with richer data than is available on our local machines
    without requiring security clearance to use live data.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-test/resources/variables.tf
@@ -0,0 +1,19 @@
+variable "environment-name" {
+  default = "test"
+}
+
+variable "team_name" {
+  default = "offender-management"
+}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "namespace" {
+  default = "offender-management-test"
+}
+
+variable "infrastructure-support" {
+  default = "omic@digital.justice.gov.uk"
+}
