b900a248bdac53aa0c4a51716bcad76339d91ea0
John Evans <john.evans@digital.justice.gov.uk>
2022-07-15 16:19:02 +0100

    ✨ PIC-2145: Create namespace hmpps-person-match-score-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-match-score-dev/resources/variables.tf
@@ -0,0 +1,37 @@
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Person Match Score"
+}
+
+variable "namespace" {
+  default = "hmpps-person-match-score-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "probation-in-court"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "probation-in-court-team@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "probation_in_court"
+}
