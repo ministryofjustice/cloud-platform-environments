5d7b1ac64c9abfb01a66942ee1764a9d8fc73d0b
carlov20 <carlov20@users.noreply.github.com>
2022-07-18 10:25:01 +0100

    INC1675061 updated storage to reflect what is in aws

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
@@ -20,7 +20,7 @@ module "rds-live" {
   rds_family    = "postgres11"
   backup_window = "02:00-03:00"
 
-  db_allocated_storage = "72"
+  db_allocated_storage = "110"
 
   snapshot_identifier = "rds:cloud-platform-78407cd5fbd86ed5-2022-07-18-02-12"
 
