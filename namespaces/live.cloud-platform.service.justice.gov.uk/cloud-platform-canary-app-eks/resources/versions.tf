a40b501ee977a265f304e068f44ba972fab3f39e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-07-26 09:05:16 +0100

    update providers

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/versions.tf
@@ -4,10 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.68.0"
+      version = "4.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.6.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "2.12.1"
     }
     pingdom = {
       source  = "russellcardullo/pingdom"
