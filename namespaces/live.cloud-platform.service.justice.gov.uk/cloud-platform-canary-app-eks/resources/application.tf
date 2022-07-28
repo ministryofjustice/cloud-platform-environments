4d68cc72ad5d795d26d1985b948f5243347b8415
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-07-28 09:41:46 +0100

    add checks for IP addresses

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/application.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/application.tf
@@ -1,11 +1,27 @@
 resource "helm_release" "cloud_platform_canary" {
-  name       = "cloud-platform-canary-application"
+  name       = "cloud-platform-canary-app"
   namespace  = "cloud-platform-canary-app-eks"
   repository = "https://stefanprodan.github.io/podinfo"
   chart      = "podinfo"
-  version    = "6.1.5"
+  version    = "6.1.7"
 
-  values = [
-    file("values.yaml")
-  ]
+  values = [templatefile("${path.module}/values.yaml", {
+    external_dns  = "cloud-platform-canary-app-podinfo-cloud-platform-canary-app-eks-green"
+    host_name     = var.host_name
+    ingress_class = "default"
+  })]
+}
+
+resource "helm_release" "cloud_platform_modsec_canary" {
+  name       = "cloud-platform-canary-app-modsec"
+  namespace  = "cloud-platform-canary-app-eks"
+  repository = "https://stefanprodan.github.io/podinfo"
+  chart      = "podinfo"
+  version    = "6.1.7"
+
+  values = [templatefile("${path.module}/values.yaml", {
+    external_dns  = "cloud-platform-canary-app-modsec-podinfo-cloud-platform-canary-app-eks-green"
+    host_name     = "modsec-${var.host_name}"
+    ingress_class = "modsec"
+  })]
 }
--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/versions.tf
@@ -6,12 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.23.0"
     }
+    dns = {
+      source  = "hashicorp/dns"
+      version = "3.2.3"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.6.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.12.1"
     }
     pingdom = {
