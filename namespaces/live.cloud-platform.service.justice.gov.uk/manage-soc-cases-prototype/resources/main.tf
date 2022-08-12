a90257ffb3df57938791b0e06a529eccb7a7d699
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-08-12 16:39:17 +0100

    Revert "Revert "[DSE-579] Add GH Teams to Dev Services and Prototypes (#8357)" (#8364)"
    This reverts commit 9c3ee783caa364a425007c843b3e34fc9b5789d1.

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-prototype/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-prototype/resources/main.tf
@@ -5,16 +5,34 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "secure-estate-digital-prototypes"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "secure-estate-digital-prototypes"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      GithubTeam = "secure-estate-digital-prototypes"
+    }
+  }
 }
 
 provider "github" {
