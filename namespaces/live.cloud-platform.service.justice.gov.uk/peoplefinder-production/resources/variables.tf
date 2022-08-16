43698a7bc7a74877cb587d0cfe05ef4d8fa5ad5b
digitalali-moj <javid.ali@digital.justice.gov.uk>
2022-08-16 10:57:44 +0100

    peoplefinder-production Add Github service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/variables.tf
@@ -32,3 +32,23 @@ variable "infrastructure-support" {
 variable "team_name" {
   default = "peoplefinder"
 }
+
+variable "cluster_state_bucket" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
+
+variable "repo_name" {
+  default = "peoplefinder"
+}
