d0f9d11b4d304788e2a645ab8e9f7c4cbe8bec43
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2022-08-16 14:32:40 +0100

    sdi-337 add offender events sub queue for prisoner from nomis synchronisation

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/hmpps-prisoner-from-nomis-sub-queue.tf
@@ -0,0 +1,108 @@
+module "prisoner_from_nomis_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "prisoner_from_nomis_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+  namespace                 = var.namespace
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.prisoner_from_nomis_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+
+EOF
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "prisoner_from_nomis_queue_policy" {
+  queue_url = module.prisoner_from_nomis_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.prisoner_from_nomis_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.prisoner_from_nomis_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+                      {
+                        "ArnEquals":
+                          {
+                            "aws:SourceArn": "${module.offender_events.topic_arn}"
+                          }
+                        }
+        }
+      ]
+  }
+
+EOF
+
+}
+
+module "prisoner_from_nomis_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "prisoner_from_nomis_dl_queue"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "prisoner_from_nomis_queue" {
+  metadata {
+    name      = "prisoner_from_nomis_queue_secret"
+    namespace = "hmpps-prisoner-from-nomis-migration-dev"
+  }
+
+  data = {
+    access_key_id     = module.prisoner_from_nomis_queue.access_key_id
+    secret_access_key = module.prisoner_from_nomis_queue.secret_access_key
+    sqs_id            = module.prisoner_from_nomis_queue.sqs_id
+    sqs_arn           = module.prisoner_from_nomis_queue.sqs_arn
+    sqs_name          = module.prisoner_from_nomis_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "prisoner_from_nomis_dead_letter_queue" {
+  metadata {
+    name      = "prisoner_from_nomis_dl_queue_secret"
+    namespace = "hmpps-prisoner-from-nomis-migration-dev"
+  }
+
+  data = {
+    access_key_id     = module.prisoner_from_nomis_dead_letter_queue.access_key_id
+    secret_access_key = module.prisoner_from_nomis_dead_letter_queue.secret_access_key
+    sqs_id            = module.prisoner_from_nomis_dead_letter_queue.sqs_id
+    sqs_arn           = module.prisoner_from_nomis_dead_letter_queue.sqs_arn
+    sqs_name          = module.prisoner_from_nomis_dead_letter_queue.sqs_name
+  }
+}
+
+resource "aws_sns_topic_subscription" "prisoner_from_nomis_subscription" {
+  provider      = aws.london
+  topic_arn     = module.offender_events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.prisoner_from_nomis_queue.sqs_arn
+  filter_policy = "{\"eventType\":[\"IEP_INSERTED\"]}"
+}
+
