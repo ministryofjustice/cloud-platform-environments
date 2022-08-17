61118b6a528409add7e24290e3d9f891a4f6dd6e
makl3 <lora.mak@digital.justice.gov.uk>
2022-08-17 11:20:52 +0100

    Add github environment name

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-preprod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-preprod/resources/serviceaccount.tf
@@ -4,7 +4,11 @@ module "serviceaccount" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  # Uncomment and provide repository names to create github actions secrets
+  # repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["analytical-platform-uploader"]
+
+  # list of github environments, to create the service account secrets as environment secrets
+  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  github_environments = ["preprod"]
 }
