d40e6a8b2f476cdb2e6851c68631416dde69416c
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-08-17 09:33:52 +0100

    EFS test

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/raz-test-not-one/resources/efs.tf
@@ -0,0 +1,28 @@
+
+module "efs" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-efs-pv?ref=frist"
+
+  cluster_name           = var.cluster_name
+  namespace              = var.namespace
+  application            = var.application
+  business_unit          = var.business_unit
+  environment            = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  team_name              = var.team_name
+  slack_channel          = var.slack_channel
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "efs_id" {
+  metadata {
+    name      = "efs-id"
+    namespace = var.namespace
+  }
+  data = {
+    efs-id = module.efs.efs_id
+  }
+}
