09de044e1aa780679167f61b7c4e50ffb2a7fee4
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-08-17 19:43:50 +0100

    feat(dev-alpha): copy a number of namespaces to new cluster

---
+++ namespaces/dev-alpha.cloud-platform.service.justice.gov.uk/cloud-platform-reports-prod/resources/serviceaccount.tf
@@ -0,0 +1,59 @@
+module "serviceaccount" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.5"
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  github_repositories = ["cloud-platform-how-out-of-date-are-we"]
+}
+
+variable "serviceaccount_rules" {
+  description = "The capabilities of this serviceaccount"
+
+  type = list(object({
+    api_groups = list(string),
+    resources  = list(string),
+    verbs      = list(string)
+  }))
+
+  # These values are usually sufficient for a CI/CD pipeline
+  default = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "delete",
+        "list",
+        "watch",
+        "update",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "networking.k8s.io",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+  ]
+}
