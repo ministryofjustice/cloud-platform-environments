5dc02e1ddc454c3231e745c3678003329b0c5e49
makl3 <lora.mak@digital.justice.gov.uk>
2022-08-24 11:48:33 +0100

    Add preprod ECR secrets as github environment secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-preprod/resources/variables.tf
@@ -72,3 +72,23 @@ variable "github_actions_secret_kube_cluster" {
   description = "The name of the github actions secret containing the serviceaccount cluster"
   default     = "KUBE_CLUSTER_PREPROD"
 }
+
+variable "github_actions_secret_ecr_name" {
+  description = "The name of the github actions secret containing the ECR name"
+  default     = "ECR_NAME_PREPROD"
+}
+
+variable "github_actions_secret_ecr_url" {
+  description = "The name of the github actions secret containing the ECR URL"
+  default     = "ECR_URL_PREPROD"
+}
+
+variable "github_actions_secret_ecr_access_key" {
+  description = "The name of the github actions secret containing the ECR AWS access key"
+  default     = "ECR_AWS_ACCESS_KEY_ID_PREPROD"
+}
+
+variable "github_actions_secret_ecr_secret_key" {
+  description = "The name of the github actions secret containing the ECR AWS secret key"
+  default     = "ECR_AWS_SECRET_ACCESS_KEY_PREPROD"
+}
