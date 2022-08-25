b9d77d886c04a91bc57401401aca09e7ac56cefd
makl3 <lora.mak@digital.justice.gov.uk>
2022-08-25 11:08:16 +0100

    Change names of ECR github actions secrets for dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-dev/resources/variables.tf
@@ -72,3 +72,23 @@ variable "github_actions_secret_kube_cluster" {
   description = "The name of the github actions secret containing the serviceaccount cluster"
   default     = "KUBE_CLUSTER_DEV"
 }
+
+variable "github_actions_secret_ecr_name" {
+  description = "The name of the github actions secret containing the ECR name"
+  default     = "ECR_NAME_DEV"
+}
+
+variable "github_actions_secret_ecr_url" {
+  description = "The name of the github actions secret containing the ECR URL"
+  default     = "ECR_URL_DEV"
+}
+
+variable "github_actions_secret_ecr_access_key" {
+  description = "The name of the github actions secret containing the ECR AWS access key"
+  default     = "ECR_AWS_ACCESS_KEY_ID_DEV"
+}
+
+variable "github_actions_secret_ecr_secret_key" {
+  description = "The name of the github actions secret containing the ECR AWS secret key"
+  default     = "ECR_AWS_SECRET_ACCESS_KEY_DEV"
+}
