5ded021e903c75d79408575b8633cb0945453cb0
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-08-31 13:53:56 +0100

    Update ECR module to use the latest version
    This version have fife cycle policy for ECR
    
    Lifecycle_policy provides a way to automate the cleaning up
    of your container images by expiring images based on age or count.

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloudopsbot-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloudopsbot-prod/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.7"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.8"
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
 
