5ed568fd78349f67a6a3162b3655c85696d05c4c
Leon Kessler <leon.nk@gmail.com>
2022-09-01 11:06:14 +0100

    Fixing module name and quotes.

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/s3.tf
@@ -35,41 +35,41 @@ module "db_backups_storage" {
 resource "kubernetes_secret" "db_backups_s3_production" {
   metadata {
     name      = "db-backups-s3"
-    namespace = prisoner-content-hub-production
+    namespace = "prisoner-content-hub-production"
   }
 
   data = {
-    access_key_id     = module.drupal_content_storage.access_key_id
-    secret_access_key = module.drupal_content_storage.secret_access_key
-    bucket_arn        = module.drupal_content_storage.bucket_arn
-    bucket_name       = module.drupal_content_storage.bucket_name
+    access_key_id     = module.db_backups_storage.access_key_id
+    secret_access_key = module.db_backups_storage.secret_access_key
+    bucket_arn        = module.db_backups_storage.bucket_arn
+    bucket_name       = module.db_backups_storage.bucket_name
   }
 }
 
 resource "kubernetes_secret" "db_backups_s3_staging" {
   metadata {
     name      = "db-backups-s3"
-    namespace = prisoner-content-hub-staging
+    namespace = "prisoner-content-hub-staging"
   }
 
   data = {
-    access_key_id     = module.drupal_content_storage.access_key_id
-    secret_access_key = module.drupal_content_storage.secret_access_key
-    bucket_arn        = module.drupal_content_storage.bucket_arn
-    bucket_name       = module.drupal_content_storage.bucket_name
+    access_key_id     = module.db_backups_storage.access_key_id
+    secret_access_key = module.db_backups_storage.secret_access_key
+    bucket_arn        = module.db_backups_storage.bucket_arn
+    bucket_name       = module.db_backups_storage.bucket_name
   }
 }
 
 resource "kubernetes_secret" "db_backups_s3_development" {
   metadata {
     name      = "db-backups-s3"
-    namespace = prisoner-content-hub-development
+    namespace = "prisoner-content-hub-development"
   }
 
   data = {
-    access_key_id     = module.drupal_content_storage.access_key_id
-    secret_access_key = module.drupal_content_storage.secret_access_key
-    bucket_arn        = module.drupal_content_storage.bucket_arn
-    bucket_name       = module.drupal_content_storage.bucket_name
+    access_key_id     = module.db_backups_storage.access_key_id
+    secret_access_key = module.db_backups_storage.secret_access_key
+    bucket_arn        = module.db_backups_storage.bucket_arn
+    bucket_name       = module.db_backups_storage.bucket_name
   }
 }
