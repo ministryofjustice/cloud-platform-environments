8e2f4d56b350c9e477c7e6ad5434b16672c828a4
yasin <yasin.mustafa@gmail.com>
2022-09-05 09:35:48 +0100

    OFFENCES-117 Delete old postgres 10 database and replace with postgres 14

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-offences-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-offences-api-preprod/resources/rds.tf
@@ -1,13 +1,17 @@
-module "manage_offences_api_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.11"
-  cluster_name           = var.cluster_name
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  namespace              = var.namespace
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
+module "manage_offences_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.11"
+  cluster_name                = var.cluster_name
+  team_name                   = var.team_name
+  business-unit               = var.business_unit
+  application                 = var.application
+  is-production               = var.is_production
+  namespace                   = var.namespace
+  environment-name            = var.environment
+  infrastructure-support      = var.infrastructure_support
+  rds_family                  = var.rds_family
+  allow_major_version_upgrade = "false"
+  db_instance_class           = "db.t3.small"
+  db_engine_version           = "14"
 
 
   providers = {
@@ -15,20 +19,19 @@ module "manage_offences_api_rds" {
   }
 }
 
-resource "kubernetes_secret" "manage_offences_api_rds" {
+resource "kubernetes_secret" "manage_offences_rds" {
   metadata {
     name      = "rds-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    rds_instance_endpoint = module.manage_offences_api_rds.rds_instance_endpoint
-    database_name         = module.manage_offences_api_rds.database_name
-    database_username     = module.manage_offences_api_rds.database_username
-    database_password     = module.manage_offences_api_rds.database_password
-    rds_instance_address  = module.manage_offences_api_rds.rds_instance_address
-    access_key_id         = module.manage_offences_api_rds.access_key_id
-    secret_access_key     = module.manage_offences_api_rds.secret_access_key
+    rds_instance_endpoint = module.manage_offences_rds.rds_instance_endpoint
+    database_name         = module.manage_offences_rds.database_name
+    database_username     = module.manage_offences_rds.database_username
+    database_password     = module.manage_offences_rds.database_password
+    rds_instance_address  = module.manage_offences_rds.rds_instance_address
+    access_key_id         = module.manage_offences_rds.access_key_id
+    secret_access_key     = module.manage_offences_rds.secret_access_key
   }
 }
-
