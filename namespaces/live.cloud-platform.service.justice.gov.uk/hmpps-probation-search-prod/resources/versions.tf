2883a14c68b74008c6750dfb2a237bdfd08b8f2e
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-09-06 14:49:54 +0100

    Added namespace and es config for prod person search

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/versions.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.29.0"
+    }
+    github = {
+      source = "integrations/github"
+    }
+    random = {
+      version = "3.3.2"
+    }
+  }
+}
