75f117cec352bd789a400619c595568e227b77a7
Marcus Aspin <maspin@unilink.com>
2022-09-07 12:51:38 +0100

    Re-added GitHub provider to allow resources to be deleted

--- namespaces/live.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/probation-offender-search-dev/resources/variables.tf
@@ -33,3 +33,9 @@ variable "infrastructure-support" {
 variable "is-production" {
   default = "false"
 }
+
+variable "github_token" {
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
