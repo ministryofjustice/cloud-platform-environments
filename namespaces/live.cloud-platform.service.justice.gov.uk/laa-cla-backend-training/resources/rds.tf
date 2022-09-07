50e395a2c5bef784c2563a9a6dc620f28df0b2e4
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-09-07 15:03:16 +0100

    create instance of postgres 11 rds

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
@@ -46,6 +46,47 @@ module "cla_backend_rds_postgres_10" {
   }
 }
 
+module "cla_backend_rds_postgres_11" {
+  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.11"
+  cluster_name  = var.cluster_name
+  team_name     = var.team_name
+  business-unit = var.business-unit
+  application   = var.application
+  is-production = var.is-production
+  namespace     = var.namespace
+
+  db_name = "cla_backend"
+  # change the postgres version as you see fit.
+  db_engine_version      = "11"
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+
+  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
+  # Pick the one that defines the postgres version the best
+  rds_family = "postgres11"
+
+  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
+  # You will need to specify "pending-reboot" here, as default is set to "immediate".
+
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "false"
+
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "1"
+      apply_method = "pending-reboot"
+    }
+  ]
+
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
 resource "kubernetes_secret" "cla_backend_rds_postgres_10" {
   metadata {
     name      = "database-10"
@@ -62,6 +103,26 @@ resource "kubernetes_secret" "cla_backend_rds_postgres_10" {
     access_key_id     = module.cla_backend_rds_postgres_10.access_key_id
     secret_access_key = module.cla_backend_rds_postgres_10.secret_access_key
     db_identifier     = module.cla_backend_rds_postgres_10.db_identifier
+  }
+
+}
+
+resource "kubernetes_secret" "cla_backend_rds_postgres_11" {
+  metadata {
+    name      = "database-11"
+    namespace = var.namespace
+  }
+
+  data = {
+    endpoint          = module.cla_backend_rds_postgres_11.rds_instance_endpoint
+    host              = module.cla_backend_rds_postgres_11.rds_instance_address
+    port              = module.cla_backend_rds_postgres_11.rds_instance_port
+    name              = module.cla_backend_rds_postgres_11.database_name
+    user              = module.cla_backend_rds_postgres_11.database_username
+    password          = module.cla_backend_rds_postgres_11.database_password
+    access_key_id     = module.cla_backend_rds_postgres_11.access_key_id
+    secret_access_key = module.cla_backend_rds_postgres_11.secret_access_key
+    db_identifier     = module.cla_backend_rds_postgres_11.db_identifier
   }
 
 }
