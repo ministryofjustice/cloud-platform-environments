f9eb41e86b8bccdd8b8440b2a32b19e56d4b921c
jemnery <jeremy.collins@digital.justice.gov.uk>
2022-09-13 13:11:15 +0100

    Adding repo to ecr.tf and updating some variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/cjs-dashboard-development/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cjs-dashboard-development/resources/variables.tf
@@ -24,7 +24,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "cjs-dashboard"
+  default     = "cjs-data-science"
 }
 
 variable "environment" {
@@ -43,12 +43,12 @@ variable "is_production" {
 
 variable "slack_channel" {
   description = "Team slack channel to use if we need to contact your team"
-  default     = "cjs-dashboard"
+  default     = "cjs-data-dashboard"
 }
 
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
-  default     = "ministryofjustice"
+  default     = "moj-analytical-services"
 }
 
 variable "github_token" {
