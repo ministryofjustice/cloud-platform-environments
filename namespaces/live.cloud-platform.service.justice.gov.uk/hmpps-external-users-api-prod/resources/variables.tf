de1309fa1d18fd1255ecd8b20e02d3766e89ffd9
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-09-15 14:37:06 +0100

    HAAR-784: new prod namespace for hmpps-external-users-api

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-users-api-prod/resources/variables.tf
@@ -0,0 +1,42 @@
+
+variable "cluster_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS external users api"
+}
+
+variable "namespace" {
+  default = "hmpps-external-users-api-prod"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "haar"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "hmpps auth audit registers"
+}
+
