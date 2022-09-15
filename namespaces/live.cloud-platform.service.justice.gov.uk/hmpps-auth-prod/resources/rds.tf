368a18727c87892151b985093a5e7c3bc98b6d87
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-09-15 16:14:41 +0100

    HAAR-784: inject hmpps-auth-prod rds secrets into hmpps-external-users-api-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-prod/resources/rds.tf
@@ -40,3 +40,20 @@ resource "kubernetes_secret" "dps_rds" {
   }
 }
 
+resource "kubernetes_secret" "dps_rds_external_users_api" {
+  metadata {
+    name      = "hmpps-auth-rds-instance-output"
+    namespace = "hmpps-external-users-api-prod"
+  }
+
+  data = {
+    rds_instance_endpoint = module.dps_rds.rds_instance_endpoint
+    database_name         = module.dps_rds.database_name
+    database_username     = module.dps_rds.database_username
+    database_password     = module.dps_rds.database_password
+    rds_instance_address  = module.dps_rds.rds_instance_address
+    access_key_id         = module.dps_rds.access_key_id
+    secret_access_key     = module.dps_rds.secret_access_key
+    url                   = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
+  }
+}
