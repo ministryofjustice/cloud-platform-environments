b3ac0d580bdc563c8fd89746b5a221cd3673fe2e
Olusola-Opencast <olusola.fadero@digital.justice.gov.uk>
2022-09-20 17:59:39 +0100

    CAT-1155 generate random password for categorisation-prototype
    this ensures that the password set in the kube cluster is in sync with the IaC

--- namespaces/live.cloud-platform.service.justice.gov.uk/categorisation-prototype/resources/basic-auth.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/categorisation-prototype/resources/basic-auth.tf
@@ -1,5 +1,10 @@
 # Username and password for the prototype kit website's http basic
 # authentication
+resource "random_password" "password" {
+  length  = 16
+  special = false
+}
+
 resource "kubernetes_secret" "basic-auth" {
   metadata {
     name      = "basic-auth"
@@ -8,6 +13,6 @@ resource "kubernetes_secret" "basic-auth" {
 
   data = {
     username = var.basic-auth-username
-    password = var.basic-auth-password
+    password = random_password.password.result
   }
 }
