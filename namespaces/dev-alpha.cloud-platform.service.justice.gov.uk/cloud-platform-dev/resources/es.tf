957f5fdb1a0cdb5ffbb3b62bdb8fc577d59c0145
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-09-21 13:35:08 +0100

    Update ES to latest version
    This is related to cold storage

--- namespaces/dev-alpha.cloud-platform.service.justice.gov.uk/cloud-platform-dev/resources/es.tf
+++ namespaces/dev-alpha.cloud-platform.service.justice.gov.uk/cloud-platform-dev/resources/es.tf
@@ -1,5 +1,5 @@
 module "cloud_platform_dev_es" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.6"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.1"
   cluster_name           = var.cluster_name
   team_name              = var.team_name
   business-unit          = var.business_unit
--- namespaces/live.cloud-platform.service.justice.gov.uk/probation-offender-search-prod/resources/es.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/probation-offender-search-prod/resources/es.tf
@@ -1,5 +1,5 @@
 module "probation_offender_search_elasticsearch" {
-  source                          = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=3.9.6"
+  source                          = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.1"
   cluster_name                    = var.cluster_name
   application                     = var.application
   business-unit                   = var.business-unit
