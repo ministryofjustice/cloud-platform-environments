23a5d06fc03c50208692e885e5db4ec242aad60f
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-09-21 13:45:19 +0100

    Update aws provider version to match with ES module

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/versions.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.68.0"
+      source   = "hashicorp/aws"
+      vversion = "~> 4.29.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
