60d5b14b1404a6ce30b255bedbe027d0df52bdfc
pezholio <pezholio@gmail.com>
2022-09-22 13:54:15 +0100

    Add new hmpps-approved-premises namespace
    This adds a new namespace for the approved premises project that has the
    correct naming convention and also combines the API and UI for ease of
    use. Once this is created and deployed, the old `approved-premises-dev`
    and `approved-premises-api-dev` namespaces can be deleted.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-approved-premises-dev/resources/variables.tf
@@ -0,0 +1,45 @@
+
+variable "cluster_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Approved Premises"
+}
+
+variable "namespace" {
+  default = "approved-premises-api-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-tech"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "approved-premises-team-dev"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
