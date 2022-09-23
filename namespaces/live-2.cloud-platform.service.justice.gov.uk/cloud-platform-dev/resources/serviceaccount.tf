4d66c094f4219d8af59c335f29a492e80bbdb1e0
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-09-23 10:07:40 +0100

    Create new namespace in live-2

---
+++ namespaces/live-2.cloud-platform.service.justice.gov.uk/cloud-platform-dev/resources/serviceaccount.tf
@@ -0,0 +1,7 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.5"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+}
