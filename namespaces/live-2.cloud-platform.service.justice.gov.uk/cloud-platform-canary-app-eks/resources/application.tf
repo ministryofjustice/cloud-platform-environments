35a6daaca20a214b01b15d5d569f1779dddb5a82
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-09-26 13:17:03 +0100

    Create cloud-platform-canary-app in live-2

---
+++ namespaces/live-2.cloud-platform.service.justice.gov.uk/cloud-platform-canary-app-eks/resources/application.tf
@@ -0,0 +1,27 @@
+resource "helm_release" "cloud_platform_canary" {
+  name       = "cloud-platform-canary-app"
+  namespace  = "cloud-platform-canary-app-eks"
+  repository = "https://stefanprodan.github.io/podinfo"
+  chart      = "podinfo"
+  version    = "6.1.7"
+
+  values = [templatefile("${path.module}/values.yaml", {
+    external_dns  = "cloud-platform-canary-app-podinfo-cloud-platform-canary-app-eks-blue"
+    host_name     = var.host_name
+    ingress_class = "default"
+  })]
+}
+
+resource "helm_release" "cloud_platform_modsec_canary" {
+  name       = "cloud-platform-canary-app-modsec"
+  namespace  = "cloud-platform-canary-app-eks"
+  repository = "https://stefanprodan.github.io/podinfo"
+  chart      = "podinfo"
+  version    = "6.1.7"
+
+  values = [templatefile("${path.module}/values.yaml", {
+    external_dns  = "cloud-platform-canary-app-modsec-podinfo-cloud-platform-canary-app-eks-blue"
+    host_name     = "modsec-${var.host_name}"
+    ingress_class = "modsec"
+  })]
+}
