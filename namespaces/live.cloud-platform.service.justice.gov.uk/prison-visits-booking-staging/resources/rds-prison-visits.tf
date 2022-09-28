7e412babace502f0e3f32d896eafc21d37361849
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-09-28 07:42:16 +0100

    12.8 rather than 12.7 as per docs

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/rds-prison-visits.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-staging/resources/rds-prison-visits.tf
@@ -32,10 +32,10 @@ module "prison-visits-rds" {
   environment-name       = "staging"
   infrastructure-support = "pvb-technical-support@digital.justice.gov.uk"
   db_engine              = "postgres"
-  db_engine_version      = "12.7"
+  db_engine_version      = "12.8"
   db_name                = "visits"
   db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
-  rds_family             = "postgres12.7"
+  rds_family             = "postgres12.8"
 
   providers = {
     aws = aws.london
