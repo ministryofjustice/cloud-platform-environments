5ad6f06c5c3e86d0c5d571eb58703fc153a7504d
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-09-29 17:20:13 +0100

    Fix github repo name to add SA secrets

--- namespaces/live-2.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app-github-action-dev/resources/serviceaccount.tf
+++ namespaces/live-2.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app-github-action-dev/resources/serviceaccount.tf
@@ -5,7 +5,7 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["cloud-platform-reference-app-github-action-"]
+  github_repositories = ["cloud-platform-reference-app-github-action"]
 
   github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
   github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
