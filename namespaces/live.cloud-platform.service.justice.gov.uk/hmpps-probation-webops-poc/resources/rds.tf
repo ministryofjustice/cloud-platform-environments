c06d45560de9348bb0c0a6dab08d515c9758b595
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-10-03 09:20:23 +0100

    pin version to unblock apply pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-webops-poc/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-webops-poc/resources/rds.tf
@@ -28,7 +28,7 @@ module "rds" {
   performance_insights_enabled = false
 
   # change the postgres version as you see fit.
-  db_engine_version = "10.5.12"
+  db_engine_version = "10.5.16"
 
   # change the instance class as you see fit.
   db_instance_class = "db.t3.micro" # Just used for a POC at this stage
@@ -49,6 +49,7 @@ module "rds" {
 
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "false"
 
   providers = {
     # Can be either "aws.london" or "aws.ireland"
