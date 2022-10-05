7f6a9c9d0fbc15563d387f900d0d7504007397cd
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-10-05 16:30:46 +0100

    SAA-84 refactoring file layout and naming.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-activities-management.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-activities-management.tf
@@ -1,31 +1,18 @@
-resource "kubernetes_secret" "domain-event-topic" {
-  metadata {
-    name      = "hmpps-domain-events-topic"
-    namespace = "activities-api-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps-domain-events.access_key_id
-    secret_access_key = module.hmpps-domain-events.secret_access_key
-    topic_arn         = module.hmpps-domain-events.topic_arn
-  }
-}
-
-module "hmpps_activities_management_queue" {
+module "activities_domain_events_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
 
   environment-name          = var.environment-name
   team_name                 = var.team_name
   infrastructure-support    = var.infrastructure-support
   application               = var.application
-  sqs_name                  = "hmpps_activities_management_queue"
+  sqs_name                  = "activities_domain_events_queue"
   encrypt_sqs_kms           = "true"
   message_retention_seconds = 1209600
   namespace                 = var.namespace
 
   redrive_policy = <<EOF
   {
-    "deadLetterTargetArn": "${module.hmpps_activities_management_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+    "deadLetterTargetArn": "${module.activities_domain_events_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
   }
 
 EOF
@@ -35,19 +22,35 @@ EOF
   }
 }
 
-resource "aws_sqs_queue_policy" "hmpps_activities_management_queue_policy" {
-  queue_url = module.hmpps_activities_management_queue.sqs_id
+module "activities_domain_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "activities_domain_events_dl"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "activities_domain_events_queue_policy" {
+  queue_url = module.activities_domain_events_queue.sqs_id
 
   policy = <<EOF
   {
     "Version": "2012-10-17",
-    "Id": "${module.hmpps_activities_management_queue.sqs_arn}/SQSDefaultPolicy",
+    "Id": "${module.activities_domain_events_queue.sqs_arn}/SQSDefaultPolicy",
     "Statement":
       [
         {
           "Effect": "Allow",
           "Principal": {"AWS": "*"},
-          "Resource": "${module.hmpps_activities_management_queue.sqs_arn}",
+          "Resource": "${module.activities_domain_events_queue.sqs_arn}",
           "Action": "SQS:SendMessage",
           "Condition":
                       {
@@ -64,56 +67,53 @@ EOF
 
 }
 
-module "hmpps_activities_management_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
-
-  environment-name       = var.environment-name
-  team_name              = var.team_name
-  infrastructure-support = var.infrastructure-support
-  application            = var.application
-  sqs_name               = "hmpps_activities_management_dlq"
-  encrypt_sqs_kms        = "true"
-  namespace              = var.namespace
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "activities_event_queue" {
-  metadata {
-    name      = "sqs-activities-management-secret"
-    namespace = "activities-api-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps_activities_management_queue.access_key_id
-    secret_access_key = module.hmpps_activities_management_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_activities_management_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_activities_management_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_activities_management_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "activities_event_dlq" {
-  metadata {
-    name      = "sqs-activities-management-dlq-secret"
-    namespace = "activities-api-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps_activities_management_dead_letter_queue.access_key_id
-    secret_access_key = module.hmpps_activities_management_dead_letter_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_activities_management_dead_letter_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_activities_management_dead_letter_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_activities_management_dead_letter_queue.sqs_name
-  }
-}
-
-resource "aws_sns_topic_subscription" "hmpps_activities_management_subscription" {
+resource "aws_sns_topic_subscription" "activities_domain_events_subscription" {
   provider      = aws.london
   topic_arn     = module.hmpps-domain-events.topic_arn
   protocol      = "sqs"
-  endpoint      = module.hmpps_activities_management_queue.sqs_arn
+  endpoint      = module.activities_domain_events_queue.sqs_arn
   filter_policy = "{\"eventType\":[\"prison-offender-events.prisoner.released\"]}"
+}
+
+resource "kubernetes_secret" "activities_domain_events_queue" {
+  metadata {
+    name      = "activities-domain-events-sqs-instance-output"
+    namespace = "activities-api-dev"
+  }
+
+  data = {
+    access_key_id     = module.activities_domain_events_queue.access_key_id
+    secret_access_key = module.activities_domain_events_queue.secret_access_key
+    sqs_queue_url     = module.activities_domain_events_queue.sqs_id
+    sqs_queue_arn     = module.activities_domain_events_queue.sqs_arn
+    sqs_queue_name    = module.activities_domain_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "activities_dlq" {
+  metadata {
+    name      = "activities-domain-events-sqs-dl-instance-output"
+    namespace = "activities-api-dev"
+  }
+
+  data = {
+    access_key_id     = module.activities_domain_events_dead_letter_queue.access_key_id
+    secret_access_key = module.activities_domain_events_dead_letter_queue.secret_access_key
+    sqs_queue_url     = module.activities_domain_events_dead_letter_queue.sqs_id
+    sqs_queue_arn     = module.activities_domain_events_dead_letter_queue.sqs_arn
+    sqs_queue_name    = module.activities_domain_events_dead_letter_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "activities_domain_events_topic" {
+  metadata {
+    name      = "activities-domain-events-sqs-topic-instance-output"
+    namespace = "activities-api-dev"
+  }
+
+  data = {
+    access_key_id     = module.hmpps-domain-events.access_key_id
+    secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
+  }
 }
