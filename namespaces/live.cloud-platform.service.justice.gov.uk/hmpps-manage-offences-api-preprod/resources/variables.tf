babc139df64256fc67b7dab1a1009a2f3ab27112
yasin <yasin.mustafa@gmail.com>
2022-10-11 13:20:54 +0100

    OFFENCES-128 Create new namespace from hmpps-manage-offences-api-preprod (copied manage-offences-api-preprod which will be deleted)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-offences-api-preprod/resources/variables.tf
@@ -0,0 +1,48 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Manage offences API"
+}
+
+variable "namespace" {
+  default = "hmpps-manage-offences-api-preprod"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "farsight-devs"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "preprod"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "manage_offences_public"
+}
+
+variable "rds_family" {
+  default = "postgres14"
+}
