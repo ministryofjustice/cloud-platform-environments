dbdf7e34f1ff42176aed5754f77ed190985d5842
Chidi Onyema <chidi.onyema@hmcts.net>
2022-10-17 18:46:13 +0100

    added variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/rds.tf
@@ -141,14 +141,14 @@ resource "kubernetes_secret" "read_replica" {
   # Uncomment if count > 0
 
   data = {
-    rds_instance_endpoint       = module.dps_rds.rds_instance_endpoint
-    rds_instance_address  =       module.read_replica.rds_instance_address
-    database_name               = module.dps_rds.database_name
-    database_username           = module.dps_rds.database_username
-    database_password           = module.dps_rds.database_password
-    rds_instance_address        = module.dps_rds.rds_instance_address
-    access_key_id               = module.dps_rds.access_key_id
-    secret_access_key           = module.dps_rds.secret_access_key
+  #  rds_instance_endpoint       = module.dps_rds.rds_instance_endpoint
+  #  rds_instance_address  =       module.read_replica.rds_instance_address
+  #  database_name               = module.dps_rds.database_name
+  #  database_username           = module.dps_rds.database_username
+  #  database_password           = module.dps_rds.database_password
+  #  rds_instance_address        = module.dps_rds.rds_instance_address
+  #  access_key_id               = module.dps_rds.access_key_id
+  #   secret_access_key           = module.dps_rds.secret_access_key
     url                         = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
   }
 }
