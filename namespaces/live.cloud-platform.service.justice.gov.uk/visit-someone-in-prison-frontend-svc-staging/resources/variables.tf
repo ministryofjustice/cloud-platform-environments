902c88e52d541493bc92124abb116ade3017921d
rj_adams <richard.adams@digital.justice.gov.uk>
2022-10-17 16:42:09 +0100

    VSIP add namespace for frontend staging environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-staging/resources/variables.tf
@@ -0,0 +1,49 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "visit-someone-in-prison"
+}
+
+variable "namespace" {
+  default = "visit-someone-in-prison-frontend-svc-staging"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "book-a-prison-visit"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "staging"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "prisonvisitsbooking@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "prison-visit-booking"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
