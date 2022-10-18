dd925e4d5a3c99bcc524a7d0380015d6612d90aa
Chidi Onyema <chidi.onyema@hmcts.net>
2022-10-18 16:21:09 +0100

    fixed vars

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/rds.tf
@@ -8,12 +8,12 @@ module "dps_rds" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.12"
   vpc_name               = var.vpc_name
   team_name              = var.team_name
-  business-unit          = var.business-unit
+  business-unit          = var.business_unit
   application            = var.application
-  is-production          = var.is-production
+  is-production          = var.is_production
   namespace              = var.namespace
-  environment-name       = var.environment-name
-  infrastructure-support = var.infrastructure-support
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
 
 
   providers = {
