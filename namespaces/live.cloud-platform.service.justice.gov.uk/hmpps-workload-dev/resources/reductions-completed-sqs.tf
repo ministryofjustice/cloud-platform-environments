2e4cae77b0cf870e2c4a41535f1831d4104fa507
carlov20 <carlov20@users.noreply.github.com>
2022-10-21 09:58:46 +0100

    WFP-1427 changed secret names to conform to other naming convention

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/reductions-completed-sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-dev/resources/reductions-completed-sqs.tf
@@ -40,30 +40,30 @@ module "hmpps_reductions_completed_dead_letter_queue" {
 
 resource "kubernetes_secret" "hmpps_reductions_completed_event_queue" {
   metadata {
-    name      = "hmpps-reductions-completed-sqs-output"
+    name      = "hmpps-reductions-completed-sqs-instance-output"
     namespace = var.namespace
   }
 
   data = {
     access_key_id     = module.hmpps_reductions_completed_queue.access_key_id
     secret_access_key = module.hmpps_reductions_completed_queue.secret_access_key
-    sqs_url           = module.hmpps_reductions_completed_queue.sqs_id
-    sqs_arn           = module.hmpps_reductions_completed_queue.sqs_arn
-    sqs_name          = module.hmpps_reductions_completed_queue.sqs_name
+    sqs_queue_url     = module.hmpps_reductions_completed_queue.sqs_id
+    sqs_queue_arn     = module.hmpps_reductions_completed_queue.sqs_arn
+    sqs_queue_name    = module.hmpps_reductions_completed_queue.sqs_name
   }
 }
 
 resource "kubernetes_secret" "hmpps_reductions_completed_dead_letter_queue" {
   metadata {
-    name      = "hmpps-reductions-completed-sqs-dl-output"
+    name      = "hmpps-reductions-completed-sqs-dl-instance-output"
     namespace = var.namespace
   }
 
   data = {
     access_key_id     = module.hmpps_reductions_completed_dead_letter_queue.access_key_id
     secret_access_key = module.hmpps_reductions_completed_dead_letter_queue.secret_access_key
-    sqs_url           = module.hmpps_reductions_completed_dead_letter_queue.sqs_id
-    sqs_arn           = module.hmpps_reductions_completed_dead_letter_queue.sqs_arn
-    sqs_name          = module.hmpps_reductions_completed_dead_letter_queue.sqs_name
+    sqs_queue_url     = module.hmpps_reductions_completed_dead_letter_queue.sqs_id
+    sqs_queue_arn     = module.hmpps_reductions_completed_dead_letter_queue.sqs_arn
+    sqs_queue_name    = module.hmpps_reductions_completed_dead_letter_queue.sqs_name
   }
 }
