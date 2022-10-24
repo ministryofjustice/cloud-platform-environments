2e905eb94645ed260fc0bc6f34afdd8364f8d9c3
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-10-24 09:48:06 +0100

    🔧 Create new namespace for hmpps-education-employment-prod
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-prod/resources/versions.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.68.0"
+    }
+    github = {
+      source = "integrations/github"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
