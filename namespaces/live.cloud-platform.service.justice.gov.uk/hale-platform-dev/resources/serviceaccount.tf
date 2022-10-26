fbb590c6b901b39c84532da5705c07eeefded384
adam <brown-a2@users.noreply.github.com>
2022-10-26 12:23:12 +0100

    Fix syntax error

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/serviceaccount.tf
@@ -7,7 +7,6 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["hale-platform"]
-}
 
 serviceaccount_rules = [
     {
@@ -66,3 +65,4 @@ serviceaccount_rules = [
       ]
     },
   ]
+}
