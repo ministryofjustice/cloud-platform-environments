2f9e95558d5ce2cc5fd04e0998989821c3cf3b0a
Stephen Dicks <github@starswan.com>
2022-10-26 15:14:45 +0100

    EL-413 Add Redis to LAA estimate-financial-eligibility

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-uat/resources/elasticache.tf
@@ -0,0 +1,38 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "laa-estimate-financial-eligibility-elasticache" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.4"
+
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  engine_version         = "4.0.10"
+  parameter_group_name   = "default.redis4.0"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "laa-estimate-financial-eligibility-elasticache" {
+  metadata {
+    name      = "laa-estimate-financial-eligibility-elasticache-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.laa-estimate-financial-eligibility-elasticache.primary_endpoint_address
+    member_clusters          = jsonencode(module.laa-estimate-financial-eligibility-elasticache.member_clusters)
+    auth_token               = module.laa-estimate-financial-eligibility-elasticache.auth_token
+  }
+}
+
