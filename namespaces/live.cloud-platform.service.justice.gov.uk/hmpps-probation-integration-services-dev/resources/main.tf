876a42fb163bd9ef227ee0898b95f45b0389d1ff
Marcus Aspin <maspin@unilink.com>
2022-10-31 13:09:48 +0000

    PI-377 Add prison-case-notes-to-probation SQS queues (dev)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/main.tf
@@ -1,20 +1,9 @@
 terraform {
-  backend "s3" {
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
   region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "london"
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
 }
 
 provider "github" {
