5bcdd8c8e2b90d667e885193d83d9a58702b7051
Marcus Aspin <maspin@unilink.com>
2022-11-02 14:22:41 +0000

    Fix cyclical reference

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
@@ -5,7 +5,7 @@ locals {
   ]
 }
 
-data "aws_iam_policy_document" "sqs_policy_document" {
+data "aws_iam_policy_document" "sqs_queue_policy_document" {
   statement {
     sid     = "TopicToQueue"
     effect  = "Allow"
@@ -19,8 +19,32 @@ data "aws_iam_policy_document" "sqs_policy_document" {
       test     = "ArnEquals"
       values   = [data.aws_sns_topic.hmpps-domain-events.arn]
     }
-    resources = local.managed_sqs_queues
+    resources = ["*"]
   }
+  statement {
+    sid    = "QueueManagement"
+    effect = "Allow"
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListDeadLetterSourceQueues",
+      "sqs:ListQueueTags",
+      "sqs:ListQueues",
+      "sqs:PurgeQueue",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.console_role.arn]
+    }
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "sqs_console_role_policy_document" {
   statement {
     sid    = "SQS"
     effect = "Allow"
@@ -31,16 +55,12 @@ data "aws_iam_policy_document" "sqs_policy_document" {
       "sqs:GetQueueUrl",
       "sqs:ListDeadLetterSourceQueues",
       "sqs:ListQueueTags",
+      "sqs:ListQueues",
       "sqs:PurgeQueue",
       "sqs:ReceiveMessage",
       "sqs:SendMessage",
-      "sqs:SetQueueAttributes"
     ]
     resources = local.managed_sqs_queues
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.console_role.arn]
-    }
   }
   statement {
     sid    = "KMS"
@@ -51,10 +71,6 @@ data "aws_iam_policy_document" "sqs_policy_document" {
       "kms:GenerateDataKey*"
     ]
     resources = ["*"]
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.console_role.arn]
-    }
   }
 }
 
@@ -73,7 +89,7 @@ resource "aws_iam_role" "console_role" {
   name               = "${var.namespace}-console"
   assume_role_policy = data.aws_iam_policy_document.console_assume_role_policy_document.json
   inline_policy {
-    policy = data.aws_iam_policy_document.sqs_policy_document.json
+    policy = data.aws_iam_policy_document.sqs_console_role_policy_document.json
   }
 }
 
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/prison-case-notes-to-probation-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/prison-case-notes-to-probation-queue.tf
@@ -35,7 +35,7 @@ module "prison-case-notes-to-probation-queue" {
 
 resource "aws_sqs_queue_policy" "prison-case-notes-to-probation-queue-policy" {
   queue_url = module.prison-case-notes-to-probation-queue.sqs_id
-  policy    = data.aws_iam_policy_document.sqs_policy_document.json
+  policy    = data.aws_iam_policy_document.sqs_queue_policy_document.json
 }
 
 module "prison-case-notes-to-probation-dlq" {
@@ -51,7 +51,7 @@ module "prison-case-notes-to-probation-dlq" {
 
 resource "aws_sqs_queue_policy" "prison-case-notes-to-probation-dlq-policy" {
   queue_url = module.prison-case-notes-to-probation-dlq.sqs_id
-  policy    = data.aws_iam_policy_document.sqs_policy_document.json
+  policy    = data.aws_iam_policy_document.sqs_queue_policy_document.json
 }
 
 resource "github_actions_environment_secret" "prison-case-notes-to-probation-secrets" {
