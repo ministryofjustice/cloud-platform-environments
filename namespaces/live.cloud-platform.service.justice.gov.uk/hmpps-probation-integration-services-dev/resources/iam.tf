faadfa9ac208dabf7230d4ffcd9bc266768bd662
Marcus Aspin <maspin@unilink.com>
2022-11-02 15:06:07 +0000

    PI-377 Simplified IAM policy for probation-integration SQS queue (#9079)
    * PI-377 Simplified IAM policy for probation-integration SQS queue
    
    * Fix cyclical reference

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
@@ -5,7 +5,46 @@ locals {
   ]
 }
 
-data "aws_iam_policy_document" "sqs_console_policy_document" {
+data "aws_iam_policy_document" "sqs_queue_policy_document" {
+  statement {
+    sid     = "TopicToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_sns_topic.hmpps-domain-events.arn]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid    = "QueueManagement"
+    effect = "Allow"
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListDeadLetterSourceQueues",
+      "sqs:ListQueueTags",
+      "sqs:ListQueues",
+      "sqs:PurgeQueue",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.console_role.arn]
+    }
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "sqs_console_role_policy_document" {
   statement {
     sid    = "SQS"
     effect = "Allow"
@@ -16,10 +55,10 @@ data "aws_iam_policy_document" "sqs_console_policy_document" {
       "sqs:GetQueueUrl",
       "sqs:ListDeadLetterSourceQueues",
       "sqs:ListQueueTags",
+      "sqs:ListQueues",
       "sqs:PurgeQueue",
       "sqs:ReceiveMessage",
       "sqs:SendMessage",
-      "sqs:SetQueueAttributes"
     ]
     resources = local.managed_sqs_queues
   }
@@ -50,7 +89,7 @@ resource "aws_iam_role" "console_role" {
   name               = "${var.namespace}-console"
   assume_role_policy = data.aws_iam_policy_document.console_assume_role_policy_document.json
   inline_policy {
-    policy = data.aws_iam_policy_document.sqs_console_policy_document.json
+    policy = data.aws_iam_policy_document.sqs_console_role_policy_document.json
   }
 }
 
