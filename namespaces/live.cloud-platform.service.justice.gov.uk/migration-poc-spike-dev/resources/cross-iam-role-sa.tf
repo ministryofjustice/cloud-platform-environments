b36b297f99b671a628428d92531ee651cd9a9cc8
Bogdan Mania <bogdan.mania@digital.justice.gov.uk>
2022-11-04 09:35:35 +0000

    update role policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/migration-poc-spike-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/migration-poc-spike-dev/resources/cross-iam-role-sa.tf
@@ -7,11 +7,29 @@ module "irsa" {
 
 data "aws_iam_policy_document" "allow_s3_access" {
   statement {
+    sid = "listbucket"
     actions = [
-      "sts:AssumeRole",
+      "s3:ListBucket",
     ]
     resources = [
-      "arn:aws:iam::684969100054:role/cp-app-migration-poc-dev",
+      "arn:aws:s3:::cp-app-migration-poc-dev",
+    ]
+  }
+
+  statement {
+    sid = "readwrite"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:RestoreObject",
+    ]
+    resources = [
+      "arn:aws:s3:::cp-app-migration-poc-dev/*",
     ]
   }
 }
