fd065d9c67b40070cfab6f4d07b9bb72aed058f2
Seb Norris <sebastian@automationlogic.com>
2022-11-07 13:50:58 +0000

    policy document

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-webops-poc/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-webops-poc/resources/s3.tf
@@ -190,21 +190,6 @@ data "aws_iam_policy_document" "efs_migration_policy_document" {
   }
 }
 
-data "aws_iam_policy" "efs_migration_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-    resources = ["${module.s3_bucket.bucket_arn}"]
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::356676313489:role/vcms-dev-efs-migration"]
-    }
-  }
-}
-
 resource "aws_iam_role" "efs_migration_role" {
   name = "efs_migration_role"
   path = "/"
@@ -220,5 +205,5 @@ resource "aws_iam_policy_attachment" "efs_migration_attachment" {
 
 resource "aws_iam_policy" "efs_migration_policy" {
   name        = "efs_migration_policy"
-  policy      = "${data.aws_iam_policy.efs_migration_policy.json}"
+  policy      = "${data.aws_iam_policy_document.efs_migration_policy_document.json}"
 }
