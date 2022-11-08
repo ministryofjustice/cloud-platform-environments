2b6780f1a6f9ab979fda72b42309248cee11f2a0
sj-williams <steve.williams@digital.justice.gov.uk>
2022-11-08 15:14:23 +0000

    Bump all dynamodb module resources to 3.2.1, remove abundant dynamodb resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/dynamodb.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/dynamodb.tf
@@ -1,5 +1,5 @@
 module "opseng_reports" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.2.1"
 
   team_name              = var.team_name
   application            = var.application
