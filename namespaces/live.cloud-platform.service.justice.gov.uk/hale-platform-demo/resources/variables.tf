928a73d8817f77ad135c6c2240b6ca25715df4d6
adam <brown-a2@users.noreply.github.com>
2022-11-08 10:57:43 +0000

    Create demo environment for Hale Platform
    Setup a demo environment for showcasing new features to stakeholders.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-demo/resources/variables.tf
@@ -0,0 +1,60 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "hale-platform"
+}
+
+variable "namespace" {
+  default = "hale-platform-demo"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HQ"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "jotw-content-devs"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "demo"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "wordpress@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "wordpress-gang"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
