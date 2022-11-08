7574dc5f24b44200a1c6e2a10aa58d19ba111d0f
sj-williams <steve.williams@digital.justice.gov.uk>
2022-11-08 12:49:21 +0000

    test: IAM policy module PR for dynamodb ListTables

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/dynamodb.tf
@@ -0,0 +1,49 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "example_team_dynamodb" {
+  source = "https://github.com/zheileman/cloud-platform-terraform-dynamodb-cluster?ref=list-tables-policy"
+
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = "false"
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  aws_region             = "eu-west-2"
+  namespace              = var.namespace
+
+  hash_key  = "example-hash"
+  range_key = "example-range"
+  attributes = [
+    {
+      name = "UserId"
+      type = "S"
+    },
+    {
+      name = "GameTitle"
+      type = "S"
+    },
+    {
+      name = "TopScore"
+      type = "N"
+    }
+  ]
+}
+
+resource "kubernetes_secret" "example_team_dynamodb" {
+  metadata {
+    name      = "example-team-dynamodb-output"
+    namespace = "my-namespace"
+  }
+
+  data = {
+    table_name        = module.example_team_dynamodb.table_name
+    table_arn         = module.example_team_dynamodb.table_arn
+    access_key_id     = module.example_team_dynamodb.access_key_id
+    secret_access_key = module.example_team_dynamodb.secret_access_key
+  }
+}
