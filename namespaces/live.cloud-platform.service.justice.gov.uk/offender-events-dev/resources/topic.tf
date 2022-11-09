aa14cbd194d64705d1f2de11f57c87207e2cbdf8
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-11-09 14:05:30 +0000

    PI-565_dev remove probation-offender-events

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/topic.tf
@@ -60,18 +60,6 @@ module "probation_offender_events" {
   }
 }
 
-resource "kubernetes_secret" "probation_offender_events" {
-  metadata {
-    name      = "probation-offender-events-topic"
-    namespace = var.namespace
-  }
-  data = {
-    access_key_id     = module.probation_offender_events.access_key_id
-    secret_access_key = module.probation_offender_events.secret_access_key
-    topic_arn         = module.probation_offender_events.topic_arn
-  }
-}
-
 module "offender_assessments_events" {
   source             = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.4"
   team_name          = var.team_name
