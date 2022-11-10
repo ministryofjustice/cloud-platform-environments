17c9869b4a4e073a5da3b8b6289aa4cf4ab9e8b3
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-11-10 16:20:52 +0000

    remove mssql read replica from hmpps jitbit dev rds

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-rehabilitation-ancilliary-jitbit-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-rehabilitation-ancilliary-jitbit-dev/resources/rds.tf
@@ -27,8 +27,6 @@ module "rds" {
   # change the instance class as you see fit.
   db_instance_class = "db.t2.small" # 1 vCPU, 2 GiB
 
-  # rds_family should be one of: postgres10, postgres11, postgres12, postgres13, postgres14
-  # Pick the one that defines the postgres version the best
   rds_family = "sqlserver-se-15.0"
 
   # Storage values
@@ -64,58 +62,10 @@ module "rds" {
   }
 }
 
-# To create a read replica, use the below code and update the values to specify the RDS instance
-# from which you are replicating. In this example, we're assuming that rds is the
-# source RDS instance and read-replica is the replica we are creating.
-
-module "read_replica" {
-  # default off
-  count  = 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.10"
-
-  vpc_name               = var.vpc_name
-  application            = var.application
-  environment-name       = var.environment
-  is-production          = var.is_production
-  infrastructure-support = var.infrastructure_support
-  team_name              = var.team_name
-
-  # If any other inputs of the RDS is passed in the source db which are different from defaults,
-  # add them to the replica
-
-
-  # It is mandatory to set the below values to create read replica instance
-
-  # Set the database_name of the source db
-  db_name = module.rds.database_name
-
-  # Set the db_identifier of the source db
-  replicate_source_db = module.rds.db_identifier
-
-  # Set to true. No backups or snapshots are created for read replica
-  skip_final_snapshot        = "true"
-  db_backup_retention_period = 0
-
-  providers = {
-    # Can be either "aws.london" or "aws.ireland"
-    aws = aws.london
-  }
-
-  # If db_parameter is specified in source rds instance, use the same values.
-  # If not specified you dont need to add any. It will use the default values.
-
-  # db_parameter = [
-  #   {
-  #     name         = "rds.force_ssl"
-  #     value        = "0"
-  #     apply_method = "immediate"
-  #   }
-  # ]
-}
 
 resource "kubernetes_secret" "rds" {
   metadata {
-    name      = "rds-postgresql-instance-output"
+    name      = "rds-mssql-instance-output"
     namespace = var.namespace
   }
 
@@ -128,43 +78,12 @@ resource "kubernetes_secret" "rds" {
     access_key_id         = module.rds.access_key_id
     secret_access_key     = module.rds.secret_access_key
   }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
 }
-
-
-resource "kubernetes_secret" "read_replica" {
-  # default off
-  count = 0
-
-  metadata {
-    name      = "rds-postgresql-read-replica-output"
-    namespace = var.namespace
-  }
-
-  # The database_username, database_password, database_name values are same as the source RDS instance.
-  # Uncomment if count > 0
-
-  /*
-  data = {
-    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-    rds_instance_address  = module.read_replica.rds_instance_address
-    access_key_id         = module.read_replica.access_key_id
-    secret_access_key     = module.read_replica.secret_access_key
-  }
-  */
-}
-
 
 # Configmap to store non-sensitive data related to the RDS instance
-
 resource "kubernetes_config_map" "rds" {
   metadata {
-    name      = "rds-postgresql-instance-output"
+    name      = "rds-mssql-instance-output"
     namespace = var.namespace
   }
 
