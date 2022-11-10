e8eca315355a8fa911b88860ae9f04ae33b01a39
Marcus Aspin <maspin@unilink.com>
2022-11-10 09:18:00 +0000

    PI-86 Remove queues from preprod
    These have been moved to the hmpps-probation-integration* namespaces

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/github.tf
+++
@@ -1,7 +0,0 @@
-data "github_repository" "hmpps-probation-integration-services" {
-  full_name = "ministryofjustice/hmpps-probation-integration-services"
-}
-
-data "github_actions_public_key" "hmpps-probation-integration-services" {
-  repository = data.github_repository.hmpps-probation-integration-services.name
-}
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/main.tf
@@ -18,9 +18,3 @@ provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
 }
-
-provider "github" {
-  owner = "ministryofjustice"
-  token = var.github_token
-}
-
