e44ec007edf161a1326ff4542e7c1a5e951a49c9
sj-williams <steve.williams@digital.justice.gov.uk>
2022-11-11 15:51:45 +0000

    Revert "resolving deleted namespaces"
    This reverts commit d79c94dbb8761cc6d313f016711d1fcd18b19ce6.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-backstage-dev/resources/variables.tf
@@ -0,0 +1,46 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "hmpps-backstage-dev"
+}
+
+variable "namespace" {
+  default = "hmpps-backstage-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-tech"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "hmpps_dev"
+}
+
