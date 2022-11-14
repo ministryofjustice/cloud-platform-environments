66dd96990017966e3cc632bba2553826a8bcd13a
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-11-14 10:48:47 +0000

    NN-4479: temporarily bump whereabouts-api-dev/rds to db.t3.large

--- namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
@@ -16,7 +16,7 @@ module "dps_rds" {
   environment-name            = var.environment-name
   infrastructure-support      = var.infrastructure-support
   allow_major_version_upgrade = "false"
-  db_instance_class           = "db.t2.small"
+  db_instance_class           = "db.t3.large"
   db_engine_version           = "10"
   rds_family                  = "postgres10"
 
