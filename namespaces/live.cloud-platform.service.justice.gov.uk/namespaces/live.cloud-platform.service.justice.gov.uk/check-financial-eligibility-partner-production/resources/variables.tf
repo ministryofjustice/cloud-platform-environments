d3b4b40d73a5a322670e89587948265d6097dc40
willc-work <william.clarke@digital.justice.gov.uk>
2022-11-16 19:01:26 +0000

    create prod env for CFEP

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-partner-production/resources/variables.tf
@@ -0,0 +1,60 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "estimate eligibility for legal aid"
+}
+
+variable "namespace" {
+  default = "check-financial-eligibility-partner-production"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "laa-estimate-eligibility"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "production"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "eligibility@justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "true"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "eligibility-team"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
