f5bff2e108996131d55249c9b6090a41376f4f54
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-11-17 09:21:37 +0000

    Create SNS subscription for search domain events (preprod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/prisoner-offender-search.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-preprod/resources/prisoner-offender-search.tf
@@ -10,3 +10,113 @@ resource "kubernetes_secret" "prisoner-offender-search" {
     topic_arn         = module.hmpps-domain-events.topic_arn
   }
 }
+
+
+
+module "prisoner_offender_search_domain_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
+
+  environment-name          = var.environment-name
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure-support
+  application               = var.application
+  sqs_name                  = "prisoner_offender_search_domain_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+  namespace                 = var.namespace
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.prisoner_offender_search_domain_dlq.sqs_arn}","maxReceiveCount": 3
+  }
+
+EOF
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "prisoner_offender_search_domain_queue_policy" {
+  queue_url = module.prisoner_offender_search_domain_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.prisoner_offender_search_domain_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.prisoner_offender_search_domain_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+                      {
+                        "ArnEquals":
+                          {
+                            "aws:SourceArn": "${module.hmpps-domain-events.topic_arn}"
+                          }
+                        }
+        }
+      ]
+  }
+
+EOF
+
+}
+
+module "prisoner_offender_search_domain_dlq" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.8"
+
+  environment-name       = var.environment-name
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure-support
+  application            = var.application
+  sqs_name               = "prisoner_offender_search_domain_dlq"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "prisoner_offender_search_domain_queue" {
+  metadata {
+    name      = "sqs-domain-event-secret"
+    namespace = "prisoner-offender-search-preprod"
+  }
+
+  data = {
+    access_key_id     = module.prisoner_offender_search_domain_queue.access_key_id
+    secret_access_key = module.prisoner_offender_search_domain_queue.secret_access_key
+    sqs_queue_url     = module.prisoner_offender_search_domain_queue.sqs_id
+    sqs_queue_arn     = module.prisoner_offender_search_domain_queue.sqs_arn
+    sqs_queue_name    = module.prisoner_offender_search_domain_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "prisoner_offender_search_domain_dlq" {
+  metadata {
+    name      = "sqs-domain-event-dlq-secret"
+    namespace = "prisoner-offender-search-preprod"
+  }
+
+  data = {
+    access_key_id     = module.prisoner_offender_search_domain_dlq.access_key_id
+    secret_access_key = module.prisoner_offender_search_domain_dlq.secret_access_key
+    sqs_queue_url     = module.prisoner_offender_search_domain_dlq.sqs_id
+    sqs_queue_arn     = module.prisoner_offender_search_domain_dlq.sqs_arn
+    sqs_queue_name    = module.prisoner_offender_search_domain_dlq.sqs_name
+  }
+}
+
+resource "aws_sns_topic_subscription" "prisoner_offender_search_domain_subscription" {
+  provider      = aws.london
+  topic_arn     = module.hmpps-domain-events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.prisoner_offender_search_domain_queue.sqs_arn
+  filter_policy = "{\"eventType\":[ \"incentives.iep-review.inserted\", \"incentives.iep-review.updated\", \"incentives.iep-review.deleted\"]}"
+}
