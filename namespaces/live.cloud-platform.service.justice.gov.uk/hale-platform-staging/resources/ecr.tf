d5fe4f5c3b6f0054ec60d4f4b3600266e5b6fef3
adam <brown-a2@users.noreply.github.com>
2022-11-22 11:43:20 +0000

    Target specific GitHub environment
    Create secrets in staging GitHub environment rather than global
    repository secrets.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/ecr.tf
@@ -22,6 +22,10 @@ module "ecr_credentials" {
   # github actions CI/CD pipelines
   github_repositories = ["hale-platform"]
 
+  # Create secrets in target GitHub environment
+  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  github_environments = ["staging"]
+
   github_actions_secret_ecr_name       = var.github_actions_secret_ecr_name
   github_actions_secret_ecr_url        = var.github_actions_secret_ecr_url
   github_actions_secret_ecr_access_key = var.github_actions_secret_ecr_access_key
