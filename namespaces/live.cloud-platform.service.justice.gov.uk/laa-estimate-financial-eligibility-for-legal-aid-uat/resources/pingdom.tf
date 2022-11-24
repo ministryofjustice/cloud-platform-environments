076c5f4313188dec84ecae3f704ebc0d5a7014b2
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-11-24 14:46:21 +0000

    Add integration ID

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-uat/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-uat/resources/pingdom.tf
@@ -13,4 +13,5 @@ resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-uat" 
   port             = 443
   tags             = "businessunit_laa,application_check-client-qualifies-for-legal-aid,component_ping,isproduction_false,environment_uat,infrastructuresupport_check-client-qualifies-for-legal-aid,laa,check-client-qualifies-for-legal-aid"
   probefilters     = "region:EU"
+  integrationids   = [125950]
 }
