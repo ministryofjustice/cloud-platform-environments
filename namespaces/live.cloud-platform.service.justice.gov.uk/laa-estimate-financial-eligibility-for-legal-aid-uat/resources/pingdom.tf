e28e9bfc54a40bd2086f256880e845df89a4148d
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-11-24 09:50:38 +0000

    Add initial Pingdom config for LAA EFE UAT

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-uat/resources/pingdom.tf
@@ -0,0 +1,16 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-uat" {
+  type             = "http"
+  name             = "Check if your client qualifies for legal aid - uat - ping"
+  host             = "main-estimate-financial-eligibility-uat.cloud-platform.service.justice.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  notifyagainevery = 0
+  url              = "/"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_laa,application_check-client-qualifies-for-legal-aid,component_ping,isproduction_false,environment_uat,infrastructuresupport_check-client-qualifies-for-legal-aid,laa,check-client-qualifies-for-legal-aid"
+  probefilters     = "region:EU"
+}
