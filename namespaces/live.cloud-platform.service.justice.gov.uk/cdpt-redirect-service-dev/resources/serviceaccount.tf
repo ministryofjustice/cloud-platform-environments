e8287d273b02b3efb7f80838fc1fe07db7b57831
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2022-11-24 13:31:10 +0000

    CDPT URL Redirect Service (dev) (#9310)
    * CDPT URL Redirect Service
    
    * Trigger status checks

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cdpt-redirect-service-dev/resources/serviceaccount.tf
@@ -0,0 +1,9 @@
+
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.5"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  github_repositories = ["cdpt-url-redirect-service"]
+}
