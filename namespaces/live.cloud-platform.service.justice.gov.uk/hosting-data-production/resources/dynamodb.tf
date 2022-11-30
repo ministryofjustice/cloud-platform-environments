6c3f85cbbc8d55512a28b50b4792a7985b0caf06
jakemulley <jakemulley@users.noreply.github.com>
2022-11-30 11:54:24 +0000

    Use vars for tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/hosting-data-production/resources/dynamodb.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hosting-data-production/resources/dynamodb.tf
@@ -1,13 +1,14 @@
 module "dynamodb" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.2.1"
 
-  team_name              = "example-team"
-  business-unit          = "example-bu"
-  application            = "exampleapp"
-  is-production          = "false"
-  environment-name       = "development"
-  infrastructure-support = "example-team@digtal.justice.gov.uk"
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
   aws_region             = "eu-west-2"
+  namespace              = var.namespace
 
   hash_key  = "example-hash"
   range_key = "example-range"
