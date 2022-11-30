6d45a8913e3eca4d45a54612f697d018b2227681
jakemulley <jakemulley@users.noreply.github.com>
2022-11-30 12:19:12 +0000

    Try legacy splat

--- namespaces/live.cloud-platform.service.justice.gov.uk/hosting-data-production/resources/dynamodb.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hosting-data-production/resources/dynamodb.tf
@@ -1,5 +1,5 @@
 module "dynamodb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=add-gsi"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=legacy-splat"
 
   team_name              = var.team_name
   business-unit          = var.business_unit
@@ -12,32 +12,4 @@ module "dynamodb" {
 
   hash_key  = "example-hash"
   range_key = "example-range"
-}
-
-module "dynamodb_with_gsi" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=add-gsi"
-
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
-  aws_region             = "eu-west-2"
-  namespace              = var.namespace
-
-  hash_key  = "example-hash"
-  range_key = "example-range"
-
-  global_secondary_indexes = [
-    {
-      name               = "GameTitleIndex"
-      hash_key           = "example-range"
-      range_key          = "example-hash"
-      write_capacity     = 10
-      read_capacity      = 10
-      projection_type    = "INCLUDE"
-      non_key_attributes = ["UserId"]
-    }
-  ]
 }
