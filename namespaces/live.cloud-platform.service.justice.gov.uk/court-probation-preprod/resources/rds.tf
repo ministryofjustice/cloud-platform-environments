812ccf88086f934f2a2b2e18f950b85e47697e7a
John Evans <john.evans@digital.justice.gov.uk>
2022-12-01 18:28:49 +0000

    🔥 PIC-2591: Remove redundant database instance

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
@@ -25,30 +25,6 @@ module "court_case_service_rds" {
   }
 }
 
-module "court_case_service_rds_prod_data" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.13"
-  vpc_name                    = var.vpc_name
-  team_name                   = var.team_name
-  business-unit               = var.business-unit
-  namespace                   = var.namespace
-  application                 = var.application
-  environment-name            = var.environment-name
-  infrastructure-support      = var.infrastructure-support
-  allow_major_version_upgrade = false
-  db_engine_version           = "11"
-  db_instance_class           = "db.t3.xlarge"
-  rds_family                  = "postgres11"
-  rds_name                    = "court-case-service-rds-prod-data"
-
-
-  snapshot_identifier         = "court-case-service-manual-snapshot-1669738195"
-  db_allocated_storage        = 38
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 resource "kubernetes_secret" "court_case_service_rds" {
   metadata {
     name      = "court-case-service-rds-instance-output"
@@ -63,24 +39,6 @@ resource "kubernetes_secret" "court_case_service_rds" {
     url                   = "postgres://${module.court_case_service_rds.database_username}:${module.court_case_service_rds.database_password}@${module.court_case_service_rds.rds_instance_endpoint}/${module.court_case_service_rds.database_name}"
     access_key_id         = module.court_case_service_rds.access_key_id
     secret_access_key     = module.court_case_service_rds.secret_access_key
-  }
-}
-
-resource "kubernetes_secret" "court_case_service_rds_prod_data" {
-  metadata {
-    name      = "court-case-service-rds-instance-output-prod-data"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.court_case_service_rds_prod_data.rds_instance_endpoint
-    database_name         = module.court_case_service_rds_prod_data.database_name
-    database_username     = module.court_case_service_rds_prod_data.database_username
-    database_password     = module.court_case_service_rds_prod_data.database_password
-    rds_instance_address  = module.court_case_service_rds_prod_data.rds_instance_address
-    url                   = "postgres://${module.court_case_service_rds_prod_data.database_username}:${module.court_case_service_rds_prod_data.database_password}@${module.court_case_service_rds_prod_data.rds_instance_endpoint}/${module.court_case_service_rds_prod_data.database_name}"
-    access_key_id         = module.court_case_service_rds_prod_data.access_key_id
-    secret_access_key     = module.court_case_service_rds_prod_data.secret_access_key
   }
 }
 
