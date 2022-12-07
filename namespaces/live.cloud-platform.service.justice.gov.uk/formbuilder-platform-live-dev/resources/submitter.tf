f9ff546d35c2281f924d4497bca7a14ef12e1f80
brenetic <brenetic@users.noreply.github.com>
2022-12-07 12:00:40 +0000

    Form Builder - remove old submitter DB live-dev
    Removes the old submitter DB in formbuilder-platform-live-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-dev/resources/submitter.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-dev/resources/submitter.tf
@@ -1,34 +1,3 @@
-module "submitter-rds-instance" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.13"
-
-  vpc_name                   = var.vpc_name
-  db_backup_retention_period = var.db_backup_retention_period_submitter
-  application                = "formbuildersubmitter"
-  environment-name           = var.environment-name
-  is-production              = var.is-production
-  namespace                  = var.namespace
-  infrastructure-support     = var.infrastructure-support
-  team_name                  = var.team_name
-
-  db_engine_version = "10"
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "submitter-rds-instance" {
-  metadata {
-    name      = "rds-instance-formbuilder-submitter-${var.environment-name}"
-    namespace = "formbuilder-platform-${var.environment-name}"
-  }
-
-  data = {
-    # postgres://USER:PASSWORD@HOST:PORT/NAME
-    url = "postgres://${module.submitter-rds-instance.database_username}:${module.submitter-rds-instance.database_password}@${module.submitter-rds-instance.rds_instance_endpoint}/${module.submitter-rds-instance.database_name}"
-  }
-}
-
 module "submitter-rds-instance-2" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.13"
 
