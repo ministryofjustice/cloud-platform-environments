ce9cedae2c2b4447b65c815ba651b1f61c5af413
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-12-08 14:59:28 +0000

    add new files

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vijay-test-prototype/resources/s3.tf
@@ -0,0 +1,36 @@
+/*
+ Based on https://github.com/ministryofjustice/cloud-platform-terraform-s3-bucket/tree/master/example
+ */
+module "calculate-journey-variable-payments_s3_bucket" {
+  source = "/Users/vijay.veeranki/moj/cloud-platform-terraform-s3-bucket"
+
+  team_name              = var.team_name
+  business-unit          = "Digital and Technology"
+  application            = var.application
+  infrastructure-support = var.infrastructure_support
+
+  is-production    = var.is_production
+  environment-name = var.environment
+  namespace        = var.namespace
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+
+  versioning = false
+}
+
+resource "kubernetes_secret" "calculate-journey-variable-payments_s3_bucket" {
+  metadata {
+    name      = "calculate-journey-variable-payments-bucket"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.calculate-journey-variable-payments_s3_bucket.access_key_id
+    secret_access_key = module.calculate-journey-variable-payments_s3_bucket.secret_access_key
+    bucket_arn        = module.calculate-journey-variable-payments_s3_bucket.bucket_arn
+    bucket_name       = module.calculate-journey-variable-payments_s3_bucket.bucket_name
+  }
+}
