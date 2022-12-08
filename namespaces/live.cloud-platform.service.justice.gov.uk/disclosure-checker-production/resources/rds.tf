0ac477b4cc275d0584c084baae93182f4a588548
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2022-12-08 18:48:47 +0000

    Upgrade to postgres 14 (#9437)

--- namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-production/resources/rds.tf
@@ -14,6 +14,11 @@ module "rds-instance" {
   infrastructure-support = var.infrastructure_support
   team_name              = var.team_name
   db_instance_class      = "db.t3.small"
+  db_engine              = "postgres"
+  db_engine_version      = "14"
+  rds_family             = "postgres14"
+
+  allow_major_version_upgrade = "true"
 
   providers = {
     aws = aws.london
