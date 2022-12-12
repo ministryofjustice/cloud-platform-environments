0f203bb4c92cb831ca5ba56b2c1ac61fcaca46da
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-12-12 16:02:10 +0000

    Update volume type var

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/es.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/es.tf
@@ -18,7 +18,7 @@ module "hmpps_historical_prisoner_elasticsearch" {
   instance_type                   = "t3.medium.elasticsearch"
   s3_manual_snapshot_repository   = module.es_snapshots_s3_bucket.bucket_arn
   ebs_iops                        = 0
-  volume_type                     = "gp3"
+  ebs_volume_type                 = "gp3"
 }
 
 module "es_snapshots_s3_bucket" {
