996081f997b398d03614094622b50cb38ba4c79a
Chidi Onyema <chidi.onyema@hmcts.net>
2022-12-14 12:49:12 +0000

    added rds

--- namespaces/live.cloud-platform.service.justice.gov.uk/grc-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/grc-preprod/resources/rds.tf
@@ -5,100 +5,41 @@
  *
  */
 
-module "rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.13"
+module "dps_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.12"
   vpc_name               = var.vpc_name
   team_name              = var.team_name
   business-unit          = var.business_unit
   application            = var.application
   is-production          = var.is_production
+  namespace              = var.namespace
+  db_engine_version      = "11"
   environment-name       = var.environment
   infrastructure-support = var.infrastructure_support
-  namespace              = var.namespace
 
-  # If the rds_name is not specified a random name will be generated ( cp-* )
-  # Changing the RDS name requires the RDS to be re-created (destroy + create)
-  # rds_name             = "my-rds-name"
-
-  # enable performance insights
-  performance_insights_enabled = true
-
-  # change the postgres version as you see fit.
-  db_engine_version = "14"
-
-  # change the instance class as you see fit.
-  db_instance_class = "db.t4g.small"
-
-  # rds_family should be one of: postgres10, postgres11, postgres12, postgres13, postgres14
-  # Pick the one that defines the postgres version the best
-  rds_family = "postgres14"
-
-  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
-  # You will need to specify "pending-reboot" here, as default is set to "immediate".
-  # db_parameter = [
-  #   {
-  #     name         = "rds.force_ssl"
-  #     value        = "0"
-  #     apply_method = "pending-reboot"
-  #   }
-  # ]
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "false"
-
-  # Enable auto start and stop of the RDS instances during 10:00 PM - 6:00 AM for cost saving, recommended for non-prod instances
-  # enable_rds_auto_start_stop  = true
+  rds_family = "postgres11"
 
   providers = {
-    # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
   }
 }
 
-resource "kubernetes_secret" "rds" {
+resource "kubernetes_secret" "dps_rds" {
   metadata {
-    name      = "rds-postgresql-instance-output"
+    name      = "dps-rds-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    rds_instance_endpoint = module.rds.rds_instance_endpoint
-    database_name         = module.rds.database_name
-    database_username     = module.rds.database_username
-    database_password     = module.rds.database_password
-    rds_instance_address  = module.rds.rds_instance_address
-    access_key_id         = module.rds.access_key_id
-    secret_access_key     = module.rds.secret_access_key
+    rds_instance_endpoint       = module.dps_rds.rds_instance_endpoint
+    database_name               = module.dps_rds.database_name
+    database_username           = module.dps_rds.database_username
+    database_password           = module.dps_rds.database_password
+    rds_instance_address        = module.dps_rds.rds_instance_address
+    access_key_id               = module.dps_rds.access_key_id
+    secret_access_key           = module.dps_rds.secret_access_key
+    url                         = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
   }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
-}
-
-
-resource "kubernetes_secret" "read_replica" {
-  # default off
-  count = 0
-
-  metadata {
-    name      = "rds-postgresql-read-replica-output"
-    namespace = var.namespace
-  }
-
-  # The database_username, database_password, database_name values are same as the source RDS instance.
-  # Uncomment if count > 0
-
-  /*
-  data = {
-    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-    rds_instance_address  = module.read_replica.rds_instance_address
-    access_key_id         = module.read_replica.access_key_id
-    secret_access_key     = module.read_replica.secret_access_key
-  }
-  */
 }
 
 
@@ -111,8 +52,7 @@ resource "kubernetes_config_map" "rds" {
   }
 
   data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
+    database_name = module.dps_rds.database_name
 
   }
 }
