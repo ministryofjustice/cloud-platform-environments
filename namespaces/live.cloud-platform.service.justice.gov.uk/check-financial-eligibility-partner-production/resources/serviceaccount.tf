2a731cc8c4bcb0948c3ccf5f3b1fdc32e6bd921d
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-12-14 12:06:37 +0000

    Trigger the pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-partner-production/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-partner-production/resources/serviceaccount.tf
@@ -54,8 +54,4 @@ module "serviceaccount" {
       ]
     },
   ]
-
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
 }
