41ec510aed34186b96300da5567b63cce38c699f
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-12-14 23:34:00 +0000

    Test es module removing override_main_response_version

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/elasticsearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/elasticsearch.tf
@@ -38,7 +38,7 @@ resource "aws_cloudwatch_log_resource_policy" "elasticsearch_log_publishing_poli
 
 # Elastic search module
 module "peoplefinder_es" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.2"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=remove-default"
   vpc_name                   = var.vpc_name
   eks_cluster_name           = var.eks_cluster_name
   application                = "peoplefinder"
