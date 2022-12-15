990165eeba352b4171bf0ac72d1243d51537497c
jakemulley <jakemulley@users.noreply.github.com>
2022-12-15 12:54:34 +0000

    Replace deprecated dot notation indice with square bracket notation

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-uat/resources/apigw.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-uat/resources/apigw.tf
@@ -37,7 +37,7 @@ EOF
 
 resource "aws_iam_role_policy" "api_gw_firehose_policy" {
 
-  count = length(aws_kinesis_firehose_delivery_stream.extended_s3_stream.*.arn)
+  count = length(aws_kinesis_firehose_delivery_stream.extended_s3_stream[*].arn)
   name  = "apigw-firehose-${count.index}"
 
   role = aws_iam_role.apigw_role.name
@@ -51,7 +51,7 @@ resource "aws_iam_role_policy" "api_gw_firehose_policy" {
       "Action": "firehose:PutRecordBatch",
 
       "Resource": [
-        "${element(aws_kinesis_firehose_delivery_stream.extended_s3_stream.*.arn, count.index)}"
+        "${element(aws_kinesis_firehose_delivery_stream.extended_s3_stream[*].arn, count.index)}"
       ]
     }
   ]
@@ -167,7 +167,7 @@ resource "random_id" "key" {
 resource "aws_api_gateway_api_key" "api_keys" {
   count = length(local.suppliers)
   name  = "${local.suppliers[count.index]}${var.environment_suffix}-key"
-  value = "${local.suppliers[count.index]}${var.environment_suffix}-${random_id.key.*.hex[count.index]}"
+  value = "${local.suppliers[count.index]}${var.environment_suffix}-${random_id.key[*].hex[count.index]}"
 }
 
 resource "kubernetes_secret" "apikeys" {
@@ -179,7 +179,7 @@ resource "kubernetes_secret" "apikeys" {
   }
 
   data = {
-    local.suppliers[count.index] = aws_api_gateway_api_key.api_keys.*.value[count.index]
+    local.suppliers[count.index] = aws_api_gateway_api_key.api_keys[*].value[count.index]
   }
 }
 
@@ -195,7 +195,7 @@ resource "aws_api_gateway_usage_plan" "default" {
 resource "aws_api_gateway_usage_plan_key" "main" {
   count = length(local.suppliers)
 
-  key_id        = aws_api_gateway_api_key.api_keys.*.id[count.index]
+  key_id        = aws_api_gateway_api_key.api_keys[*].id[count.index]
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.default.id
 }
