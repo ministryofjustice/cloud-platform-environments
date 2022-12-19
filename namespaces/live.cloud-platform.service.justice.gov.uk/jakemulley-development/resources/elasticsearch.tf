f8e4da2d2bba33efa0879e1093e23168673917ec
jakemulley <jakemulley@users.noreply.github.com>
2022-12-19 18:58:36 +0000

    Add ElasticSearch cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/elasticsearch.tf
@@ -0,0 +1,17 @@
+module "example" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.3"
+  vpc_name               = var.vpc_name
+  eks_cluster_name       = var.eks_cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  elasticsearch-domain = "jakemulley-development-example"
+
+  # change the elasticsearch version as you see fit.
+  elasticsearch_version = "7.10"
+}
