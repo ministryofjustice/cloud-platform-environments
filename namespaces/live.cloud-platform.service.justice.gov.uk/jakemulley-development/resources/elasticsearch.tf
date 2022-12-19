93afb97592619f9612fa6b1ff574f39ae7b5b17f
jakemulley <jakemulley@users.noreply.github.com>
2022-12-19 19:09:53 +0000

    Add example enforce ES cluster

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/elasticsearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/elasticsearch.tf
@@ -15,3 +15,23 @@ module "example" {
   # change the elasticsearch version as you see fit.
   elasticsearch_version = "7.10"
 }
+
+module "enforce" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=add-enforce-https-option"
+  vpc_name               = var.vpc_name
+  eks_cluster_name       = var.eks_cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  elasticsearch-domain = "enforce"
+
+  # change the elasticsearch version as you see fit.
+  elasticsearch_version = "7.10"
+
+  domain_endpoint_enforce_https = true
+}
