a11688cd6c8606962e3590cd3a26f51a7d338ac8
Marcus Aspin <maspin@unilink.com>
2022-12-19 15:29:52 +0000

    Update subscription

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/make-recall-decisions-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/make-recall-decisions-and-delius-queue.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic_subscription" "make-recall-decisions-and-delius-queue-su
   protocol  = "sqs"
   endpoint  = module.make-recall-decisions-and-delius-queue.sqs_arn
   filter_policy = jsonencode({
-    eventType = [] # TODO add event type filter
+    eventType = [ "prison-recall.recommendation.started" ]
   })
 }
 
