51d08d0f2922b763d7d0c505de3dd9f844e22df7
Kornel <kornel.berec@digital.justice.gov.uk>
2022-12-21 10:47:10 +0000

    Added allow_major_version_upgrade = "true"

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-preprod/resources/rds.tf
@@ -11,7 +11,8 @@ module "dps_rds" {
   rds_family             = var.rds-family
   db_instance_class      = "db.t3.small"
   db_engine_version      = "14"
-
+  allow_major_version_upgrade = "true"
+  
   providers = {
     aws = aws.london
   }
