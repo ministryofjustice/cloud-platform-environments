25c82e87193fc17da5f62416574600a6a697e4e2
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2022-12-22 11:40:47 +0000

    SDI-494 initial config for domain event logger namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-event-logger-dev/resources/variables.tf
@@ -0,0 +1,44 @@
+
+variable "cluster_name" {
+}
+
+variable "vpc_name" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Template Apps"
+}
+
+variable "namespace" {
+  default = "hmpps-domain-event-logger-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-developers"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "hmpps_dev"
+}
