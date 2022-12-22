31124cfe57a123bffdd12d878b53c58ac28ae9e0
Marcus Aspin <maspin@unilink.com>
2022-12-22 14:33:48 +0000

    PI-666 Fix Pingdom check for probation-offender-search (#9631)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/pingdom.tf
@@ -3,9 +3,9 @@ provider "pingdom" {}
 resource "pingdom_check" "search-api-check" {
   type                     = "http"
   name                     = "${var.application} API"
-  host                     = "probation-offender-search.hmpps.service.justice.gov.uk"
+  host                     = "health-kick.prison.service.justice.gov.uk"
   port                     = 443
-  url                      = "/health"
+  url                      = "/https/probation-offender-search.hmpps.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
@@ -15,20 +15,3 @@ resource "pingdom_check" "search-api-check" {
   probefilters             = "region:EU"
   integrationids           = [120233] # probation-integration-notifications
 }
-
-resource "pingdom_check" "indexer-ingress-check" {
-  type                     = "http"
-  name                     = "${var.application} Elasticsearch Ingress"
-  host                     = "probation-search-prod.hmpps.service.justice.gov.uk"
-  port                     = 443
-  url                      = "/health"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  encryption               = true
-  tags                     = "probation-integration,hmpps,cloudplatform-managed"
-  probefilters             = "region:EU"
-  integrationids           = [120233] # probation-integration-notifications
-}
-
