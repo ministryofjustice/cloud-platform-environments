dc671866c5f0a3e7dba0eab01d17c14fff0adb50
jakemulley <jakemulley@users.noreply.github.com>
2022-12-28 16:22:09 +0000

    -topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/sns.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/sns.tf
@@ -1,5 +1,5 @@
 module "example_sns_topic" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sns?ref=4.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=4.5"
 
   team_name          = var.team_name
   topic_display_name = "jakemulley-development"
