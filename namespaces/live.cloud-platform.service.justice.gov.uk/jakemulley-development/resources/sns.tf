92986b8b41c1446e61ea99e5ac5d59b01c26e067
jakemulley <jakemulley@users.noreply.github.com>
2022-12-28 16:42:32 +0000

    Add tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/sns.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/sns.tf
@@ -4,6 +4,14 @@ module "example_sns_topic" {
   team_name          = var.team_name
   topic_display_name = "jakemulley-development"
 
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  owner                  = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
   providers = {
     aws = aws.london
   }
