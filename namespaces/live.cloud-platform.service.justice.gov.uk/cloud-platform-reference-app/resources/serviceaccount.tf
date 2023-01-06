f039d64f6e9ec8b1a5a0446796f13a4ace6206d3
Jake Mulley <jakemulley@users.noreply.github.com>
2023-01-06 13:21:07 +0000

    Delete SA

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reference-app/resources/serviceaccount.tf
@@ -1,7 +1,7 @@
-module "service_account" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
+# module "service_account" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
 
-  namespace           = var.namespace
-  kubernetes_cluster  = var.kubernetes_cluster
-  serviceaccount_name = "circleci"
-}
+#   namespace           = var.namespace
+#   kubernetes_cluster  = var.kubernetes_cluster
+#   serviceaccount_name = "circleci"
+# }
