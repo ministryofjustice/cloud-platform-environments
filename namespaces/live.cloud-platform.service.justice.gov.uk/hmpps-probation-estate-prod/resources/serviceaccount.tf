7b0255064cc49a225df6853f41ec45e4dddc7d32
Jake Mulley <jakemulley@users.noreply.github.com>
2023-01-06 16:20:34 +0000

    Revert "Destroy service account for production environments"

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-estate-prod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-estate-prod/resources/serviceaccount.tf
@@ -1,7 +1,7 @@
-# module "serviceaccount" {
-#   source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
-#   serviceaccount_name = "circleci"
-#   namespace           = var.namespace
-#   kubernetes_cluster  = var.kubernetes_cluster
+module "serviceaccount" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
+  serviceaccount_name = "circleci"
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
 
-# }
+}
