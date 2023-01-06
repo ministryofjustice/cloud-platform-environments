cb065fc12161ca64e3bef119dca6e1498fb1f754
jakemulley <jakemulley@users.noreply.github.com>
2023-01-06 07:55:25 +0000

    Bump cloud-platform-terraform-ecr-credentials AWS access keys for -dev environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/cccd-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cccd-dev/resources/ecr.tf
@@ -1,6 +1,6 @@
 
 module "cccd_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.9"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=bump-key"
   repo_name = "cccd"
   team_name = "laa-get-paid"
 
--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "cica_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.9"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=bump-key"
   repo_name = "cica-repo-dev"
   team_name = "cica"
 
