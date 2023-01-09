07519c9a27bf4e34c472e702c9eaf278a4e43297
Jake Mulley <jakemulley@users.noreply.github.com>
2023-01-09 13:18:27 +0000

    Revert "Destroy service account for production environments"

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/serviceaccount-github.tf
@@ -0,0 +1,6 @@
+module "serviceaccount" {
+  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  github_repositories = ["hmpps-pact-broker"]
+}
