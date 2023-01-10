603f464df80b4639626412d8e4c1d885c5170c5a
jakemulley <jakemulley@users.noreply.github.com>
2023-01-10 06:54:32 +0000

    Remove IRSA SA from non-production environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/analytics-platform-rshiny-notebook-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/analytics-platform-rshiny-notebook-dev/resources/cross-iam-role-sa.tf
@@ -1,6 +1,6 @@
 module "irsa" {
   eks_cluster_name = var.eks_cluster_name
-  source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.0.6"
+  source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=rm-sa"
   namespace        = "analytics-platform-rshiny-notebook-dev"
   role_policy_arns = [aws_iam_policy.analytics-platform-rshiny-notebook-dev.arn]
 }
