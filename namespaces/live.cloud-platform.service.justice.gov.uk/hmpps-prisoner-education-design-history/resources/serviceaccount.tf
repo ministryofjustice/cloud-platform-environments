2bec8b060c83a143a7fb18824418998a6ab68ce2
jakemulley <jakemulley@users.noreply.github.com>
2023-01-10 18:27:00 +0000

    Destroy SA tokens for hmpps-prisoner-education-design-history

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-education-design-history/resources/serviceaccount.tf
@@ -1,8 +1,8 @@
-module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
+# module "serviceaccount" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
 
-  namespace          = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
+#   namespace          = var.namespace
+#   kubernetes_cluster = var.kubernetes_cluster
 
-  github_repositories = [var.namespace]
-}
+#   github_repositories = [var.namespace]
+# }
