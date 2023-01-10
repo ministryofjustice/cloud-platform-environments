094235506449e9b12be90c657b1a367d4282401b
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-01-10 11:51:33 +0000

    Add Elasticsearch to abundant namespace for testing

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/elasticsearch.tf
@@ -0,0 +1,18 @@
+module "abundance_es" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.4"
+  eks_cluster_name       = var.eks_cluster_name
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  elasticsearch-domain = "abundant-es"
+
+  # change the elasticsearch version as you see fit.
+  elasticsearch_version = "7.10"
+
+}
