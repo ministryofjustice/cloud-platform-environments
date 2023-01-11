365950e5adeb41a47153ff00824b3c105323cc67
jakemulley <jakemulley@users.noreply.github.com>
2023-01-11 13:13:48 +0000

    Bump ECR keys for production or unqualified environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-production/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "peoplefinder_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=4.9"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.0"
   repo_name = "peoplefinder-ecr"
   team_name = "peoplefinder"
 
