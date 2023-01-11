9f282954acf73af38bfd885a2ebf732bb87999a7
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-01-11 14:19:38 +0000

    feat(abundance-namespace): add another sqs module for testing circle breach

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/sqs.tf
@@ -0,0 +1,41 @@
+module "abundance_namespace_sqs" {
+  # remember to check the latest version
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.9.1"
+
+  sqs_name = "abundance_namespace_sqs"
+  # if true, the sqs_name above must end with ".fifo", it's an API quirk
+  fifo_queue             = false
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment_name
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  # Set encrypt_sqs_kms = "true", to enable SSE for SQS using KMS key.
+  encrypt_sqs_kms = "false"
+
+  # existing_user_name     = module.another_sqs_instance.user_name
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "abundance_namespace_sqs" {
+  metadata {
+    name      = "abundance_namespace_sqs"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.example_sqs.access_key_id
+    secret_access_key = module.example_sqs.secret_access_key
+    # the above will not be set if existing_user_name is defined
+    sqs_id   = module.example_sqs.sqs_id
+    sqs_arn  = module.example_sqs.sqs_arn
+    sqs_name = module.example_sqs.sqs_name
+  }
+}
+
