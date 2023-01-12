2a1d356e289f6d7875740e712569df9a34095c0d
Jake Mulley <jakemulley@users.noreply.github.com>
2023-01-12 13:46:09 +0000

    Remove workgroup

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/athena.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/athena.tf
@@ -3,18 +3,3 @@ resource "aws_athena_database" "database" {
   bucket = module.pathfinder_reporting_s3_bucket.bucket_name
   force_destroy = true
 }
-
-resource "aws_athena_workgroup" "queries" {
-  name = "pathfinder_${var.environment-name}"
-
-  configuration {
-    enforce_workgroup_configuration    = true
-    publish_cloudwatch_metrics_enabled = true
-
-    result_configuration {
-      output_location = "s3://${module.pathfinder_reporting_s3_bucket.bucket_name}/query_results/"
-    }
-  }
-
-  depends_on = [module.pathfinder_reporting_s3_bucket]
-}
