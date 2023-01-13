1464ea9796702ba6b1ff0dce8ab36863832fdaff
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-01-13 18:51:25 +0000

    Tidy up methods and main func

--- cmd/remove-skip-file/main.go
+++ cmd/remove-skip-file/main.go
@@ -16,23 +16,19 @@ import (
 )
 
 var (
-	// Required
-	sourceFiles = "namespaces/live.cloud-platform.service.justice.gov.uk/"
 	// Optional
-	authorName    = flag.String("author-name", "", "Name of the author of the commit.")
-	authorEmail   = flag.String("author-email", "", "Email of the author of the commit.")
-	prDescription = flag.String("pr-text", "", "Text to put in the description of the pull request.")
+	authorName   = flag.String("author-name", "", "Name of the author of the commit.")
+	authorEmail  = flag.String("author-email", "", "Email of the author of the commit.")
+	token        = flag.String("token", os.Getenv("GITHUB_AUTH_TOKEN"), "Personal Access token to push commit and Create PR.")
+	namespaceCSV = flag.String("csvfile", "", "Input CSV file to use for removing secret-rotator file")
 )
 
 var (
-	sourceOwner   = "ministryofjustice"
-	sourceRepo    = "cloud-platform-environments"
-	commitBranch  string
-	commitMessage = "fix(namespaces)Remove SECRET_ROTATE_BLOCK from" + commitBranch
-	baseBranch    = "main"
-	prTitle       string
-	prRepoOwner   = sourceOwner
-	prRepo        = sourceRepo
+	sourceOwner         = "ministryofjustice"
+	sourceRepo          = "cloud-platform-environments"
+	commitMessagePrefix = "fix(namespaces)Remove SECRET_ROTATE_BLOCK from "
+	baseBranch          = "main"
+	filePathBase        = "namespaces/live.cloud-platform.service.justice.gov.uk/"
 )
 
 var (
@@ -40,9 +36,67 @@ var (
 	ctx    = context.Background()
 )
 
+func main() {
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("Unauthorized: No Github access token present")
+	}
+	if *authorName == "" || *authorEmail == "" {
+		log.Fatal("You need to specify a non-empty value for the flags `-author-name` and `-author-email`")
+	}
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *token})
+	tc := oauth2.NewClient(ctx, ts)
+	client = github.NewClient(tc)
+
+	f, err := os.Open(*namespaceCSV)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(f)
+	for {
+		namespaces, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Unable to parse file - %s:%s\n", *namespaceCSV, err)
+		}
+
+		for _, ns := range namespaces {
+			commitBranch := "remove-secret-rotator" + ns
+
+			fmt.Printf("Namespace to rotate: %s\n", ns)
+			ref, err := getRef(commitBranch)
+			if err != nil {
+				log.Fatalf("Unable to get/create the commit reference: %s\n", err)
+			}
+			if ref == nil {
+				log.Fatalf("No error where returned but the reference is nil")
+			}
+
+			tree, err := getTree(ref, ns)
+			if err != nil {
+				log.Fatalf("Unable to create the tree based on the provided files: %s\n", err)
+			}
+
+			if err := pushCommit(ref, tree, ns); err != nil {
+				log.Fatalf("Unable to create the commit: %s\n", err)
+			}
+
+			if err := createPR(commitBranch, ns); err != nil {
+				log.Fatalf("Error while creating the pull request: %s", err)
+			}
+		}
+	}
+}
+
 // getRef returns the commit branch reference object if it exists or creates it
 // from the base branch before returning it.
-func getRef() (ref *github.Reference, err error) {
+func getRef(commitBranch string) (ref *github.Reference, err error) {
 	if ref, _, err = client.Git.GetRef(ctx, sourceOwner, sourceRepo, "refs/heads/"+commitBranch); err == nil {
 		return ref, nil
 	}
@@ -68,22 +122,20 @@ func getRef() (ref *github.Reference, err error) {
 
 // getTree generates the tree to commit based on the given files and the commit
 // of the ref you got in getRef.
-func getTree(ref *github.Reference, commitBranch string) (tree *github.Tree, err error) {
+func getTree(ref *github.Reference, namespace string) (tree *github.Tree, err error) {
 	// Create a tree with what to commit.
 	entries := []*github.TreeEntry{}
 
-	// Load each file into the tree.
-	ns := sourceFiles + commitBranch
-	file := ns + "/SECRET_ROTATE_BLOCK"
-	fmt.Printf("File Path: %s\n", file)
-	entries = append(entries, &github.TreeEntry{Path: github.String(file), Type: github.String("blob"), Content: nil, Mode: github.String("100644")})
+	secretRotatorFile := filePathBase + namespace + "/SECRET_ROTATE_BLOCK"
+	fmt.Println("File to rotate", secretRotatorFile)
+	entries = append(entries, &github.TreeEntry{Path: github.String(secretRotatorFile), Type: github.String("blob"), Content: nil, Mode: github.String("100644")})
 
 	tree, _, err = client.Git.CreateTree(ctx, sourceOwner, sourceRepo, *ref.Object.SHA, entries)
 	return tree, err
 }
 
 // pushCommit creates the commit in the given reference using the given tree.
-func pushCommit(ref *github.Reference, tree *github.Tree) (err error) {
+func pushCommit(ref *github.Reference, tree *github.Tree, namespace string) (err error) {
 	// Get the parent commit to attach the commit to.
 	parent, _, err := client.Repositories.GetCommit(ctx, sourceOwner, sourceRepo, *ref.Object.SHA, nil)
 	if err != nil {
@@ -94,6 +146,7 @@ func pushCommit(ref *github.Reference, tree *github.Tree) (err error) {
 
 	// Create the commit using the tree.
 	date := time.Now()
+	commitMessage := commitMessagePrefix + namespace
 	author := &github.CommitAuthor{Date: &date, Name: authorName, Email: authorEmail}
 	commit := &github.Commit{Author: author, Message: &commitMessage, Tree: tree, Parents: []*github.Commit{parent.Commit}}
 	newCommit, _, err := client.Git.CreateCommit(ctx, sourceOwner, sourceRepo, commit)
@@ -108,91 +161,22 @@ func pushCommit(ref *github.Reference, tree *github.Tree) (err error) {
 }
 
 // createPR creates a pull request. Based on: https://godoc.org/github.com/google/go-github/github#example-PullRequestsService-Create
-func createPR() (err error) {
-	prTitle = "Rotate Secrets in Namespace: " + commitBranch
-	if prTitle == "" {
-		return errors.New("missing Pull Request Title; skipping PR creation")
-	}
-
-	if prRepoOwner != "" && prRepoOwner != sourceOwner {
-		commitBranch = fmt.Sprintf("%s:%s", sourceOwner, commitBranch)
-	} else {
-		prRepoOwner = sourceOwner
-	}
-
-	if prRepo == "" {
-		prRepo = sourceRepo
-	}
+func createPR(commitBranch string, namespace string) (err error) {
+	prTitle := "Rotate Secrets in namespace:  " + namespace
+	prDescription := "Rotate Secrets in namespace: " + namespace
 
 	newPR := &github.NewPullRequest{
 		Title:               &prTitle,
 		Head:                &commitBranch,
 		Base:                &baseBranch,
-		Body:                prDescription,
+		Body:                &prDescription,
 		MaintainerCanModify: github.Bool(true),
 	}
-	pr, _, err := client.PullRequests.Create(ctx, prRepoOwner, prRepo, newPR)
+	pr, _, err := client.PullRequests.Create(ctx, sourceOwner, sourceRepo, newPR)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("PR created: %s\n", pr.GetHTMLURL())
 	return nil
-}
-
-func main() {
-	f, err := os.Open("namespace.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r := csv.NewReader(f)
-	for {
-		record, err := r.Read()
-
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, value := range record {
-			commitBranch = value
-			flag.Parse()
-			token := os.Getenv("GITHUB_AUTH_TOKEN")
-			if token == "" {
-				log.Fatal("Unauthorized: No token present")
-			}
-			if *authorName == "" || *authorEmail == "" {
-				log.Fatal("You need to specify a non-empty value for the flags `-author-name` and `-author-email`")
-			}
-			ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-			tc := oauth2.NewClient(ctx, ts)
-			client = github.NewClient(tc)
-
-			fmt.Printf("Namespace: %s\n", commitBranch)
-			ref, err := getRef()
-			if err != nil {
-				log.Fatalf("Unable to get/create the commit reference: %s\n", err)
-			}
-			if ref == nil {
-				log.Fatalf("No error where returned but the reference is nil")
-			}
-
-			tree, err := getTree(ref, commitBranch)
-			if err != nil {
-				log.Fatalf("Unable to create the tree based on the provided files: %s\n", err)
-			}
-
-			if err := pushCommit(ref, tree); err != nil {
-				log.Fatalf("Unable to create the commit: %s\n", err)
-			}
-
-			if err := createPR(); err != nil {
-				log.Fatalf("Error while creating the pull request: %s", err)
-			}
-		}
-	}
 }
