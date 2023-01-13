d00092523207580b1e7f2b4ec68e6680ebc8f1cf
tim <wright.tim.j@gmail.com>
2023-01-13 09:34:04 +0000

    NN-4710 manage adjudicationcs api pingdom

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "hmpps-manage-adjudications-api-prod-check" {
+  type                     = "http"
+  name                     = "DPS - Manage Adjudications API"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 96628]
+}
