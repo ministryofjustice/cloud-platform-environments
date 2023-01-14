68a53680596bdf408673d546931e91eaf80eba5a
Chidi Onyema <chidi.onyema@hmcts.net>
2023-01-14 23:02:37 +0000

    export db url

--- namespaces/live.cloud-platform.service.justice.gov.uk/grc-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/grc-prod/resources/rds.tf
@@ -54,6 +54,6 @@ resource "kubernetes_config_map" "rds" {
 
   data = {
     database_name = module.dps_rds.database_name
-
+    url = "postgres://${module.module_name.database_username}:${module.module_name.database_password}@${module.module_name.rds_instance_endpoint}/${module.module_name.database_name}"
   }
 }
