cf47889ecd2ee240412d007ca1686a32129be0b9
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-01-16 12:30:31 +0000

    Test comment resource

--- namespaces/live-2.cloud-platform.service.justice.gov.uk/vijay-beta/resources/variables.tf
+++ namespaces/live-2.cloud-platform.service.justice.gov.uk/vijay-beta/resources/variables.tf
@@ -33,7 +33,7 @@ variable "environment" {
 }
 
 variable "infrastructure_support" {
-  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  description = "The team responsible for managing the infrastructure."
   default     = "platforms@digital.justice.gov.uk"
 }
 
