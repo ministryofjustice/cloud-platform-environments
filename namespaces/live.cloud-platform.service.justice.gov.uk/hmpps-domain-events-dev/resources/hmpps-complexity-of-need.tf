d377f9c7069cd3c5758ee91ebcf1a12bc953e4de
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-01-19 16:40:19 +0000

    Adding secret management for MPC and CON in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-complexity-of-need.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-complexity-of-need.tf
@@ -1,10 +1,7 @@
 resource "kubernetes_secret" "hmpps-complexity-of-need" {
   metadata {
-    name      = "hmpps-domain-events-topic-hmpps-complexity-of-need"
-    namespace = var.namespace
-    # Remove when namespace has been migrated
-    # name      = "hmpps-domain-events-topic"
-    # namespace = "hmpps-complexity-of-need-staging"
+    name      = "hmpps-domain-events-topic"
+    namespace = "hmpps-complexity-of-need-staging"
   }
 
   data = {
