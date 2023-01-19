5957e6af0fe7f6ae5709bd9cb00b8854dcd075b9
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2023-01-19 12:54:03 +0000

    Create API Gateway with custom domain for HMPPS Integration API (#10262)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-development/resources/iam.tf
@@ -0,0 +1,48 @@
+# Generate an additional IAM user to manage API Gateway
+resource "random_id" "api_gateway_id" {
+  byte_length = 16
+}
+
+resource "aws_iam_user" "api_gateway_user" {
+  name = "api-gateway-user-${random_id.api_gateway_id.hex}"
+  path = "/system/api-gateway-user/"
+}
+
+resource "aws_iam_access_key" "api_gateway_user" {
+  user = aws_iam_user.api_gateway_user.name
+}
+
+data "aws_iam_policy_document" "api_gateway" {
+  statement {
+    actions = [
+      "apigateway:GET",
+      "apigateway:POST",
+      "apigateway:DELETE",
+    ]
+
+    resources = [
+      "${element(split("/", aws_api_gateway_rest_api.api_gateway.arn), 0)}/apikeys",
+      "${element(split("/", aws_api_gateway_rest_api.api_gateway.arn), 0)}/apikeys/*",
+      "${element(split("/", aws_api_gateway_rest_api.api_gateway.arn), 0)}/usageplans",
+      "${element(split("/", aws_api_gateway_rest_api.api_gateway.arn), 0)}/usageplans/**",
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "api_gateway_policy" {
+  name   = "${var.namespace}-api-gateway"
+  policy = data.aws_iam_policy_document.api_gateway.json
+  user   = aws_iam_user.api_gateway_user.name
+}
+
+resource "kubernetes_secret" "api_gateway_admin_user_credentials" {
+  metadata {
+    name      = "api-gateway-iam"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = aws_iam_access_key.api_gateway_user.id
+    secret_access_key = aws_iam_access_key.api_gateway_user.secret
+  }
+}
