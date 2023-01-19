2288d335adc4966162a73c9c39dd2a1e4f257597
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-01-19 17:25:15 +0000

    Add elasticache to test rotating tokens

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/paul-test/resources/elasticache.tf
@@ -0,0 +1,29 @@
+module "rotate_token_ec_cluster" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.5"
+
+  // The first two inputs are provided by the pipeline for cloud-platform. See the example for more detail.
+  vpc_name               = var.vpc_name
+  team_name              = "webops"
+  business-unit          = "platforms"
+  application            = "rotatetokenapp"
+  is-production          = "false"
+  environment-name       = "development"
+  infrastructure-support = "platforms@digtal.justice.gov.uk"
+  providers = {
+    aws = aws.london
+  }
+}
+
+
+resource "kubernetes_secret" "rotate_token_ec_cluster" {
+  metadata {
+    name      = "rotate-token-ec-cluster-output"
+    namespace = "paul-test"
+  }
+
+  data = {
+    primary_endpoint_address = module.rotate_token_ec_cluster.primary_endpoint_address
+    member_clusters          = jsonencode(module.rotate_token_ec_cluster.member_clusters)
+    auth_token               = module.rotate_token_ec_cluster.auth_token
+  }
+}
