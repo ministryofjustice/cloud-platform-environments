aef306654c55cda6157bd3304a541a225e30e85d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-01-19 15:23:41 +0000

    feat(cloud-platform-go-get-module): add new namespace with default values

---
+++ namespaces/live-2.cloud-platform.service.justice.gov.uk/cloud-platform-go-get-module/resources/versions.tf
@@ -0,0 +1,13 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.27.0"
+    }
+    github = {
+      source = "integrations/github"
+    }
+  }
+}
