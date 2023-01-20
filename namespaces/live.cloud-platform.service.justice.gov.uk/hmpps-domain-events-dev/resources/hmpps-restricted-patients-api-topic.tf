2fcfd3db1dd7165abe3a7416e53c28794da29698
Peter Phillips <peter@greenthistle.co.uk>
2023-01-20 15:17:51 +0000

    SDI-590: 🔧 Separate topic id / key for restricted patients

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-restricted-patients-api-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-restricted-patients-api-topic.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_access_key" "hmpps-domain-events-rp" {
+  user = module.hmpps-domain-events.user_name
+}
+
+# TODO: Remove me once secret underneath in use
 resource "kubernetes_secret" "hmpps-restricted-patients-api-topic" {
   metadata {
     name      = "hmpps-domain-events-topic"
@@ -7,6 +12,19 @@ resource "kubernetes_secret" "hmpps-restricted-patients-api-topic" {
   data = {
     access_key_id     = module.hmpps-domain-events.access_key_id
     secret_access_key = module.hmpps-domain-events.secret_access_key
+    topic_arn         = module.hmpps-domain-events.topic_arn
+  }
+}
+
+resource "kubernetes_secret" "hmpps-restricted-patients-api-topic-rp" {
+  metadata {
+    name      = "hmpps-domain-events-topic-rp"
+    namespace = "hmpps-restricted-patients-api-dev"
+  }
+
+  data = {
+    access_key_id     = aws_iam_access_key.hmpps-domain-events-rp.id
+    secret_access_key = aws_iam_access_key.hmpps-domain-events-rp.secret
     topic_arn         = module.hmpps-domain-events.topic_arn
   }
 }
