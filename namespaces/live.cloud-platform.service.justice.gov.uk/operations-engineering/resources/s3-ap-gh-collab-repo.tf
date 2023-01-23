53991a9cb9438ff68258f7be072d96805d31177e
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-23 14:36:15 +0000

    Update s3-ap-gh-collab-repo.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/s3-ap-gh-collab-repo.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/s3-ap-gh-collab-repo.tf
@@ -1,4 +1,4 @@
-module "s3_bucket" {
+module "ap_gh_collab_repo_s3_bucket" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.7.3"
   acl    = "private"
 
@@ -16,20 +16,14 @@ module "s3_bucket" {
 
 }
 
-resource "kubernetes_secret" "s3_bucket" {
+resource "kubernetes_secret" "ap_gh_collab_repo_s3_bucket" {
   metadata {
     name      = "tfstate-s3-bucket-ap-gh-collab-repo"
     namespace = var.namespace
   }
 
   data = {
-    access_key_id               = module.s3_bucket.access_key_id
-    secret_access_key           = module.s3_bucket.secret_access_key
-    reports_bucket_arn          = module.s3_bucket.bucket_arn
-    reports_bucket_name         = module.s3_bucket.bucket_name
-    deleted_objects_bucket_arn  = module.s3_bucket.bucket_arn
-    deleted_objects_bucket_name = module.s3_bucket.bucket_name
-    static_files_bucket_name    = module.s3_bucket.bucket_name
-    static_files_bucket_arn     = module.s3_bucket.bucket_arn
+    access_key_id               = module.ap_gh_collab_repo_s3_bucket.access_key_id
+    secret_access_key           = module.ap_gh_collab_repo_s3_bucket.secret_access_key
   }
 }
