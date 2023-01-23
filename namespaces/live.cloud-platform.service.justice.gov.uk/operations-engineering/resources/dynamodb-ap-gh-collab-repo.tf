814fdecfbf93a58154f5da736c9cd861e5475800
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-23 14:29:19 +0000

    Create dynamodb-ap-gh-collab-repo.tf

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/dynamodb-ap-gh-collab-repo.tf
@@ -0,0 +1,30 @@
+module "ap_gh_collab_repo_tf_state_lock" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=3.2.4"
+
+  team_name              = var.team_name
+  application            = var.application
+  business-unit          = var.business_unit
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  is-production          = "true"
+  namespace              = var.namespace
+
+  hash_key          = "LockID"
+  enable_encryption = "false"
+  enable_autoscaler = "true"
+  aws_region        = "eu-west-2"
+}
+
+resource "kubernetes_secret" "ap_gh_collab_repo_tf_state_lock" {
+  metadata {
+    name      = "ap-gh-collab-repo-tf-state-lock-table"
+    namespace = var.namespace
+  }
+
+  data = {
+    table_name        = module.ap_gh_collab_repo_tf_state_lock.table_name
+    table_arn         = module.ap_gh_collab_repo_tf_state_lock.table_arn
+    access_key_id     = module.ap_gh_collab_repo_tf_state_lock.access_key_id
+    secret_access_key = module.ap_gh_collab_repo_tf_state_lock.secret_access_key
+  }
+}
