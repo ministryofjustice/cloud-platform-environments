5fb371ae0962629d89fdcceea753a3441c0ad46d
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-01-24 12:03:40 +0000

    Add connection between API gateway and our custom domain

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-development/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-development/resources/api_gateway.tf
@@ -156,3 +156,8 @@ resource "kubernetes_secret" "api_keys" {
     "${var.team_name}" = aws_api_gateway_api_key.team.value
   }
 }
+
+resource "aws_api_gateway_base_path_mapping" "hostname" {
+  api_id      = aws_api_gateway_rest_api.api_gateway.id
+  domain_name = aws_api_gateway_domain_name.api_gateway_fqdn.domain_name
+}
