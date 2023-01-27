4f9b57e93ad4cbfc09bca4e2ce78c42173d590a2
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-01-27 14:27:28 +0000

    Claim-criminal-injuries-compensation-dev add sqs and policies

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/claim-criminal-injuries-application-queue.tf
@@ -0,0 +1,35 @@
+module "claim-criminal-injuries-application-queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.9.1"
+
+  sqs_name = "claim-criminal-injuries-application-queue"
+  fifo_queue             = false
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  # Set encrypt_sqs_kms = "true", to enable SSE for SQS using KMS key.
+  encrypt_sqs_kms = "true"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "claim-criminal-injuries-application-sqs" {
+  metadata {
+    name      = "sqs"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id = module.claim-criminal-injuries-application-queue.access_key_id
+    secret_access_key = module.claim-criminal-injuries-application-queue.secret_access_key
+    sqs_id = module.claim-criminal-injuries-application-queue.sqs_id
+    sqs_arn = module.claim-criminal-injuries-application-queue.sqs_arn
+    sqs_name = module.claim-criminal-injuries-application-queue.sqs_name
+  }
+}
