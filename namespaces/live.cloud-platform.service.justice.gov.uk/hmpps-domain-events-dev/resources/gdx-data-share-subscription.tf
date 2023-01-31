6948c9f0ad56ca195920c20f106502cd5d66b6d2
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-01-31 15:33:48 +0000

    Allow GDS data share AWS account access to queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/gdx-data-share-subscription.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/gdx-data-share-subscription.tf
@@ -1,4 +1,3 @@
-
 module "gdx_data_share_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.9.1"
 
@@ -7,7 +6,7 @@ module "gdx_data_share_queue" {
   infrastructure-support    = "michael.willis@digital.justice.gov.uk"
   application               = "GDX Data Share Platform"
   sqs_name                  = "gdx_data_share_queue"
-  encrypt_sqs_kms           = "true"
+  encrypt_sqs_kms           = "false"
   message_retention_seconds = 600
   namespace                 = var.namespace
 
@@ -24,35 +23,6 @@ EOF
   }
 }
 
-resource "aws_sqs_queue_policy" "gdx_data_share_queue_policy" {
-  queue_url = module.gdx_data_share_queue.sqs_id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Id": "${module.gdx_data_share_queue.sqs_arn}/SQSDefaultPolicy",
-    "Statement":
-      [
-        {
-          "Effect": "Allow",
-          "Principal": {"AWS": "*"},
-          "Resource": "${module.gdx_data_share_queue.sqs_arn}",
-          "Action": "SQS:SendMessage",
-          "Condition":
-                      {
-                        "ArnEquals":
-                          {
-                            "aws:SourceArn": "${module.hmpps-domain-events.topic_arn}"
-                          }
-                        }
-        }
-      ]
-  }
-
-EOF
-
-}
-
 module "gdx_data_share_dlq" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.9.1"
 
@@ -61,7 +31,7 @@ module "gdx_data_share_dlq" {
   infrastructure-support = "michael.willis@digital.justice.gov.uk"
   application            = "GDX Data Share Platform"
   sqs_name               = "gdx_data_share_dlq"
-  encrypt_sqs_kms        = "true"
+  encrypt_sqs_kms        = "false"
   namespace              = var.namespace
 
   providers = {
@@ -69,35 +39,102 @@ module "gdx_data_share_dlq" {
   }
 }
 
+# provide a policy to the GDX AWS account to be able to read these queues.
+resource "aws_sqs_queue_policy" "gdx_data_share_queue_policy" {
+  queue_url = module.gdx_data_share_queue.sqs_id
+
+  policy = <<EOF
+ {
+   "Version":"2012-10-17",
+   "Id":"${module.gdx_data_share_queue.sqs_arn}/SQSDefaultPolicy",
+   "Statement":[
+      {
+         "Effect":"Allow",
+         "Principal":{
+            "AWS":"*"
+         },
+         "Resource":"${module.gdx_data_share_queue.sqs_arn}",
+         "Action":"SQS:SendMessage",
+         "Condition":{
+            "ArnEquals":{
+               "aws:SourceArn":"${module.hmpps-domain-events.topic_arn}"
+            }
+         }
+      },
+      {
+         "Effect":"Allow",
+         "Principal":{
+            "AWS":[
+               "arn:aws:iam::776473272850:root"
+            ]
+         },
+         "Action":[
+            "SQS:SendMessage",
+            "SQS:ReceiveMessage",
+            "SQS:DeleteMessage",
+            "SQS:GetQueueUrl"
+         ],
+         "Resource":"${module.gdx_data_share_queue.sqs_arn}"
+      }
+   ]
+}
+
+EOF
+
+}
+
+resource "aws_sqs_queue_policy" "gdx_data_share_queue_policy" {
+  queue_url = module.gdx_data_share_dlq.sqs_id
+
+  policy = <<EOF
+ {
+   "Version":"2012-10-17",
+   "Id":"${module.gdx_data_share_dlq.sqs_arn}/SQSDefaultPolicy",
+   "Statement":[
+      {
+         "Effect":"Allow",
+         "Principal":{
+            "AWS":[
+               "arn:aws:iam::776473272850:root"
+            ]
+         },
+         "Action":[
+            "SQS:ReceiveMessage",
+            "SQS:DeleteMessage",
+            "SQS:GetQueueUrl"
+         ],
+         "Resource":"${module.gdx_data_share_dlq.sqs_arn}"
+      }
+   ]
+}
+
+EOF
+
+}
+
 resource "kubernetes_secret" "gdx_data_share_queue" {
   metadata {
-    name = "sqs-gdx-data-share-secret"
-    # injected here and then sent manually over to GDS - an external client of the consuming service
+    name      = "sqs-gdx-data-share-secret"
     namespace = var.namespace
   }
 
   data = {
-    access_key_id     = module.gdx_data_share_queue.access_key_id
-    secret_access_key = module.gdx_data_share_queue.secret_access_key
-    sqs_queue_url     = module.gdx_data_share_queue.sqs_id
-    sqs_queue_arn     = module.gdx_data_share_queue.sqs_arn
-    sqs_queue_name    = module.gdx_data_share_queue.sqs_name
+    sqs_queue_url  = module.gdx_data_share_queue.sqs_id
+    sqs_queue_arn  = module.gdx_data_share_queue.sqs_arn
+    sqs_queue_name = module.gdx_data_share_queue.sqs_name
   }
 }
 
 resource "kubernetes_secret" "gdx_data_share_dlq" {
   metadata {
-    name = "sqs-gdx-datashare-dl-secret"
-    # injected here and then sent manually over to GDS - an external client of the consuming service
+    name      = "sqs-gdx-datashare-dl-secret"
     namespace = var.namespace
   }
 
   data = {
-    access_key_id     = module.gdx_data_share_queue.access_key_id
-    secret_access_key = module.gdx_data_share_queue.secret_access_key
-    sqs_queue_url     = module.gdx_data_share_queue.sqs_id
-    sqs_queue_arn     = module.gdx_data_share_queue.sqs_arn
-    sqs_queue_name    = module.gdx_data_share_queue.sqs_name
+    sqs_queue_url  = module.gdx_data_share_queue.sqs_id
+    sqs_queue_arn  = module.gdx_data_share_queue.sqs_arn
+    sqs_queue_name = module.gdx_data_share_queue.sqs_name
   }
 }
 
