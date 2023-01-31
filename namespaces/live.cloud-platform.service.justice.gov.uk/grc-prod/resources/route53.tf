d3ed2b4b1ee6738dc2a8221c3532604cca131b1a
Chidi Onyema <chidi.onyema@codiance.com>
2023-01-31 23:38:14 +0000

    fixed vars

--- namespaces/live.cloud-platform.service.justice.gov.uk/grc-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/grc-prod/resources/route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "route53_zone" {
   tags = {
     business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
+    is-production          = var.is_production
     environment-name       = var.environment
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
@@ -29,7 +29,7 @@ resource "aws_route53_zone" "route53_justice_zone" {
   tags = {
     business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
+    is-production          = var.is_production
     environment-name       = var.environment
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
