391eea4bd5b110908c495875ef0c68ccc602ef36
Bogdan Mania <bogdan.mania@digital.justice.gov.uk>
2023-02-01 16:19:34 +0000

    allow list all buckets

--- namespaces/live.cloud-platform.service.justice.gov.uk/ap-gold-scorecard-form-prod/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ap-gold-scorecard-form-prod/resources/cross-iam-role-sa.tf
@@ -7,6 +7,16 @@ module "irsa" {
 
 data "aws_iam_policy_document" "ap-gold-scorecard-form-prod" {
   statement {
+    sid = "listallbuckets"
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
     sid = "listbucket"
     actions = [
       "s3:ListBucket",
@@ -20,7 +30,7 @@ data "aws_iam_policy_document" "ap-gold-scorecard-form-prod" {
   }
 
   statement {
-    sid = "readwritelistbuckets"
+    sid = "readwritebucket"
     actions = [
       "s3:GetObject",
       "s3:GetObjectAcl",
@@ -30,7 +40,6 @@ data "aws_iam_policy_document" "ap-gold-scorecard-form-prod" {
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:RestoreObject",
-      "s3:ListAllMyBuckets",
     ]
     resources = [
       "arn:aws:s3:::alpha-app-scorecard-form/*",
