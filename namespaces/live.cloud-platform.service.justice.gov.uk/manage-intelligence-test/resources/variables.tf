a47ecfec52a6d996654828d1ee5147158e13c58e
brian.carr <brian.carr@digital.justice.gov.uk>
2023-02-03 14:37:13 +0000

    Updated terraform environment value to test from development

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-test/resources/variables.tf
@@ -0,0 +1,63 @@
+
+
+variable "vpc_name" {
+}
+
+variable "cluster_state_bucket" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "manage-intelligence"
+}
+
+variable "namespace_suffix" {
+  description = "namespace suffix such as test or dev, used to ensure AWS resource names are unique"
+  default     = "test"
+}
+
+variable "namespace" {
+  default = "manage-intelligence-test"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "dps-soct-tech"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "test"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "ims-digital-team"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
