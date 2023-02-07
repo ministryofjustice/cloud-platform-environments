85e801b5792af9eb624b8f5ec1a1a421c6a70ec8
jakemulley <jakemulley@users.noreply.github.com>
2023-02-07 18:12:39 +0000

    Upgrade RDS module, remove unused var from ES

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-refactor/resources/elasticsearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-refactor/resources/elasticsearch.tf
@@ -1,6 +1,5 @@
 module "abundance_es" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticsearch?ref=4.0.4"
-  eks_cluster_name       = var.eks_cluster_name
   vpc_name               = var.vpc_name
   team_name              = var.team_name
   business-unit          = var.business_unit
