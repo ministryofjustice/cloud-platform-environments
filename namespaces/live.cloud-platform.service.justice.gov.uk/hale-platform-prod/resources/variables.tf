712b42af564d0e5a734ee7f79693d89e8ae9b4f6
adam <brown-a2@users.noreply.github.com>
2023-02-07 13:01:17 +0000

    Create new ECR secrets in GitActions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/variables.tf
@@ -76,3 +76,23 @@ variable "github_actions_secret_kube_token" {
   description = "The name of the github actions secret containing the serviceaccount token"
   default     = "KUBE_TOKEN_PROD"
 }
+
+variable "github_actions_secret_ecr_name" {
+  description = "The name of the github actions secret containing the ECR name"
+  default     = "ECR_NAME_PROD"
+}
+
+variable "github_actions_secret_ecr_url" {
+  description = "The name of the github actions secret containing the ECR URL"
+  default     = "ECR_URL_PROD"
+}
+
+variable "github_actions_secret_ecr_access_key" {
+  description = "The name of the github actions secret containing the ECR AWS access key"
+  default     = "ECR_AWS_ACCESS_KEY_ID_PROD"
+}
+
+variable "github_actions_secret_ecr_secret_key" {
+  description = "The name of the github actions secret containing the ECR AWS secret key"
+  default     = "ECR_AWS_SECRET_ACCESS_KEY_PROD"
+}
