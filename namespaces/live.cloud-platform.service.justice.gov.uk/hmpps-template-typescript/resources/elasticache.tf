663abf4df6bbd94e6cbfdd01f405c20e11f2590f
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-02-08 09:52:32 +0000

    Upgrade Elasticache Redis in template for HMPPS UI apps

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-template-typescript/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-template-typescript/resources/elasticache.tf
@@ -3,18 +3,20 @@
 ################################################################################
 
 module "elasticache_redis" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.5"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=5.6"
   vpc_name               = var.vpc_name
-  application            = var.application
-  environment-name       = var.environment
-  is-production          = var.is_production
-  infrastructure-support = var.infrastructure_support
   team_name              = var.team_name
-  number_cache_clusters  = var.number_cache_clusters
-  node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
   namespace              = var.namespace
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+
+  number_cache_clusters = var.number_cache_clusters
+  node_type             = "cache.t4g.small"
+  engine_version        = "6.x"
+  parameter_group_name  = "default.redis6.x"
 
   providers = {
     aws = aws.london
@@ -31,6 +33,8 @@ resource "kubernetes_secret" "elasticache_redis" {
     primary_endpoint_address = module.elasticache_redis.primary_endpoint_address
     auth_token               = module.elasticache_redis.auth_token
     member_clusters          = jsonencode(module.elasticache_redis.member_clusters)
+    replication_group_id     = module.elasticache_redis.replication_group_id
+    access_key_id            = module.elasticache_redis.access_key_id
+    secret_access_key        = module.elasticache_redis.secret_access_key
   }
 }
-
