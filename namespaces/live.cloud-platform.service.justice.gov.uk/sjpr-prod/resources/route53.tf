80cf56f2a6b898e29511a5fa192440591a2d3843
Amit Kohli <data@amitkohli.com>
2023-02-09 14:35:05 +0000

    correcting more variablenames
    Signed-off-by: Amit Kohli <data@amitkohli.com>

--- namespaces/live.cloud-platform.service.justice.gov.uk/sjpr-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/sjpr-prod/resources/route53.tf
@@ -3,19 +3,19 @@ resource "aws_route53_zone" "sjpr_route53_zone" {
 
   tags = {
     team_name              = var.team_name
-    business-unit          = var.business-unit
+    business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
-    environment-name       = var.environment-name
-    owner                  = var.owner    
-    infrastructure-support = var.infrastructure-support
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.githib_owner    
+    infrastructure_support = var.infrastructure_support
     namespace              = var.namespace
   }
 }
 
 resource "kubernetes_secret" "sjpr_route53_zone_sec" {
   metadata {
-    name      = "sjpr-route53-zone-output"
+    name      = "sjpr_route53_zone_output"
     namespace = var.namespace
   }
 
