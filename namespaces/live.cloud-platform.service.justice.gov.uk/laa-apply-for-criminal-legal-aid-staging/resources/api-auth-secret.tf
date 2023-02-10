b839170b5235e600e377cdcf6ecf53d191ad0b60
Jesus Laiz <zheileman@users.noreply.github.com>
2023-02-10 10:57:29 +0000

    Copy secret to another namespace
    This may not work until previous PR #10687 is applied.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-criminal-legal-aid-staging/resources/api-auth-secret.tf
@@ -0,0 +1,17 @@
+data "kubernetes_secret" "datastore-api-auth-secrets" {
+  metadata {
+    name      = "api-auth-secrets"
+    namespace = "laa-criminal-applications-datastore-staging"
+  }
+}
+
+resource "kubernetes_secret" "datastore-api-auth-secret" {
+  metadata {
+    name      = "datastore-api-auth-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    secret = data.kubernetes_secret.datastore-api-auth-secrets.data.crime_apply
+  }
+}
