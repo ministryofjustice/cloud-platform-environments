a129e6fd2e7218f1b273327cb3d6e3a88b7c50bb
Amit Kohli <data@amitkohli.com>
2023-02-10 17:08:40 +0000

    bug fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/sjpr-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/sjpr-prod/resources/route53.tf
@@ -26,7 +26,7 @@ resource "kubernetes_secret" "sjpr_route53_zone_sec" {
 
 resource "aws_route53_record" "add_cname_email" {
   name    = "sjpr-prod.apps.live.cloud-platform.service.justice.gov.uk"
-  zone_id = aws_route53_zone.example_team_route53_zone.zone_id
+  zone_id = aws_route53_zone.sjpr_route53_zone.zone_id
   type    = "CNAME"
   records = ["social-justice-problems.service.justice.gov.uk"]
   ttl     = "300"
