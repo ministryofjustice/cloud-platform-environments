22d16c87249d844faa7dd9fe056a6591c2edb416
jakemulley <jakemulley@users.noreply.github.com>
2023-02-14 09:30:32 +0000

    jakemulley-refactor: add default tags as an example

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-refactor/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-refactor/resources/main.tf
@@ -5,16 +5,52 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      application            = var.application
+      business-unit          = var.business_unit
+      environment-name       = var.environment
+      infrastructure-support = var.infrastructure_support
+      is-production          = var.is_production
+      namespace              = var.namespace
+      team_name              = var.team_name
+    }
+  }
 }
 
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      application            = var.application
+      business-unit          = var.business_unit
+      environment-name       = var.environment
+      infrastructure-support = var.infrastructure_support
+      is-production          = var.is_production
+      namespace              = var.namespace
+      team_name              = var.team_name
+    }
+  }
 }
 
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      application            = var.application
+      business-unit          = var.business_unit
+      environment-name       = var.environment
+      infrastructure-support = var.infrastructure_support
+      is-production          = var.is_production
+      namespace              = var.namespace
+      team_name              = var.team_name
+    }
+  }
 }
 
 provider "github" {
