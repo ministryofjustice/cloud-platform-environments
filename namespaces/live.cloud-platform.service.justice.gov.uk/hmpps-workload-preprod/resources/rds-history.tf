c55ac82cc9545d3a47bdbbc100d1225174361f65
sp-watson <77974320+sp-watson@users.noreply.github.com>
2023-02-20 15:21:48 +0000

    WFP-1758: Rotate RDS creds - workload preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/rds-history.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/rds-history.tf
@@ -7,6 +7,9 @@ module "rds-history" {
   is-production = var.is_production
   namespace     = var.namespace
 
+  # rotating creds
+  db_password_rotated_date = "20-02-2023"
+
   # enable performance insights
   performance_insights_enabled = true
 
