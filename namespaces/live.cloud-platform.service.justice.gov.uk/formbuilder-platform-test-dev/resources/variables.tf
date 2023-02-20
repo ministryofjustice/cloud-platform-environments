4491d2d6a14e15f59bdfcf7f5e667430d4b87bb8
matt tei <matt.tei@digital.justice.gov.uk>
2023-02-20 10:03:29 +0000

    Update contact email address for this namespace.

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/variables.tf
@@ -19,7 +19,7 @@ variable "is-production" {
 }
 
 variable "infrastructure-support" {
-  default = "Form Builder form-builder-team@digital.justice.gov.uk"
+  default = "Form Builder form-builder-developers@digital.justice.gov.uk"
 }
 
 # The following variable is provided at runtime by the pipeline.
