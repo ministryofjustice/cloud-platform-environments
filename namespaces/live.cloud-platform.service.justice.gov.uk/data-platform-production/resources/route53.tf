d3fd59da46679ac93df3f85d08a3b2efc1689735
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-02-22 12:37:32 +0000

    Updating variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/route53.tf
@@ -2,12 +2,12 @@ resource "aws_route53_zone" "data_platform_production_route53_zone" {
   name = "data-platform.service.justice.gov.uk"
 
   tags = {
-    business-unit          = var.business-unit
+    business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
-    environment-name       = var.environment-name
+    is-production          = var.is_production
+    environment-name       = var.environment
     owner                  = var.team_name
-    infrastructure-support = var.infrastructure-support
+    infrastructure-support = var.infrastructure_support
   }
 }
 
