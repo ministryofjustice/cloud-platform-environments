b33114124a77b9f36e33f88213781ffd5b205652
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2023-02-22 18:08:25 +0000

    feat(cmd): Add result output to pull request comment

--- cmd/compare-namespace/compare-namespace.go
+++ cmd/compare-namespace/compare-namespace.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -20,14 +21,13 @@ import (
 
 // github variables
 var (
-	client          *github.Client
-	ctx             context.Context
-	token           = flag.String("token", os.Getenv("GITHUB_OAUTH_TOKEN"), "GihHub Personel token string")
-	githubref       = flag.String("branch", os.Getenv("GITHUB_REF"), "GitHub branch reference.")
-	gitRepo         = os.Getenv("GITHUB_REPOSITORY")
-	repoNamespace   string
-	secretNamespace string
-	resourceName    []string
+	client    *github.Client
+	ctx       context.Context
+	token     = flag.String("token", os.Getenv("GITHUB_OAUTH_TOKEN"), "GihHub Personel token string")
+	githubref = flag.String("branch", os.Getenv("GITHUB_REF"), "GitHub branch reference.")
+	gitRepo   = os.Getenv("GITHUB_REPOSITORY")
+	mm        Mismatch
+	r         Result
 )
 
 var (
@@ -42,7 +42,19 @@ var (
 	bid, _     = strconv.Atoi(branch)
 )
 
-// listFiles will gather a list of tf files to be checked for namespace comparison
+type Mismatch struct {
+	RepositoryNamespace,
+	File,
+	ResourceTypeName,
+	ResourceName,
+	ResourceNamespace string
+}
+
+type Result struct {
+	Result string
+}
+
+// listFiles will gather a list of tf files to be checked for namespace comparisons
 func listFiles() []*github.CommitFile {
 	prs, _, err := client.PullRequests.ListFiles(ctx, owner, repo, bid, nil)
 	if err != nil {
@@ -51,74 +63,72 @@ func listFiles() []*github.CommitFile {
 	return prs
 }
 
-// decodeFile checks files for resources and if found runs resourceType function
-func decodeFile(filename string) error {
-	if filepath.Ext(filename) == ".tf" {
-		data, err := os.ReadFile(filename)
+// decodeFile for kube secrets and return namespaces that the secret is attached to
+func decodeFile() ([]*hclwrite.Block, error) {
+	var blocks []*hclwrite.Block
 
-		if err != nil {
-			return fmt.Errorf("error reading file %s", err)
-		}
+	data, err := os.ReadFile(mm.File)
 
-		f, diags := hclwrite.ParseConfig(data, filename, hcl.Pos{
-			Line:   0,
-			Column: 0,
-		})
-
-		if diags.HasErrors() {
-			return fmt.Errorf("error getting TF resource: %s", diags)
-		}
-
-		blocks := f.Body().Blocks()
-		for _, block := range blocks {
-			if block.Type() == "resource" {
-				resourceName = block.Labels()
-				for _, label := range block.Labels() {
-					resourceType(block, filename, label)
-				}
-			}
-		}
+	if err != nil {
+		return nil, fmt.Errorf("error reading file %s", err)
 	}
-	return nil
+
+	f, diags := hclwrite.ParseConfig(data, mm.File, hcl.Pos{
+		Line:   0,
+		Column: 0,
+	})
+
+	if diags.HasErrors() {
+		return nil, fmt.Errorf("error getting TF resource: %s", diags)
+	}
+	blocks = f.Body().Blocks()
+
+	return blocks, nil
 }
 
 // resouceType will search for namespace in all resources in a Pull Request
-func resourceType(block *hclwrite.Block, filename, label string) {
+func resourceType(block *hclwrite.Block) (string, string) {
+	var resourceName string
 	var namespaceBlock string
+	var namespaceVar string
+
 	metadata := block.Body().Blocks()
 	for _, m := range metadata {
 		if m.Type() == "metadata" {
 			for key, attr := range m.Body().Attributes() {
+				if key == "name" {
+					expr := attr.Expr()
+					exprTokens := expr.BuildTokens(nil)
+					var valueTokens hclwrite.Tokens
+					valueTokens = append(valueTokens, exprTokens...)
+					resourceName = strings.TrimSpace(string(valueTokens.Bytes()))
+				}
 				if key == "namespace" {
 					expr := attr.Expr()
 					exprTokens := expr.BuildTokens(nil)
 					var valueTokens hclwrite.Tokens
 					valueTokens = append(valueTokens, exprTokens...)
 					namespaceBlock = strings.TrimSpace(string(valueTokens.Bytes()))
+					if strings.Contains(namespaceBlock, "var.") {
+						ns := strings.SplitAfter(namespaceBlock, ".")
+						varNamespace, err := varFileSearch(ns[1])
+						if err != nil {
+							log.Fatal(err)
+						}
+						namespaceVar = varNamespace
+					} else {
+						namespaceVar = namespaceBlock
+					}
 				}
-			}
-			if strings.Contains(namespaceBlock, "var.") {
-				ns := strings.SplitAfter(namespaceBlock, ".")
-				varNamespace, err := varFileSearch(ns[1], filename)
-				if err != nil {
-					log.Fatal(err)
-				}
-				secretNamespace = varNamespace
-			} else {
-				secretNamespace = namespaceBlock
-			}
-
-			if !strings.Contains(secretNamespace, repoNamespace) {
-				githubaction.SetOutput("mismatch", "true")
-				fmt.Printf("\nNamespace Mismatch...\nRepository Namespace: %s\nFile: %s\nResource Name: %s\nResource Namespace: %s\n", repoNamespace, filename, resourceName[1], secretNamespace)
 			}
 		}
 	}
+	return namespaceVar, resourceName
 }
 
-// varFileSearch will search for the namespace in the variables.tf file if the resource metadata for namespace returns a var.
-func varFileSearch(ns string, filename string) (string, error) {
-	path := strings.SplitAfter(filename, "resources/")
+// varFileSearch will search for the namespace in the variables.tf file if the search returns a var.namespace
+func varFileSearch(ns string) (string, error) {
+	path := strings.SplitAfter(mm.File, "resources/")
 	data, err := os.ReadFile(path[0] + "variables.tf")
 	if err != nil {
 		return "", fmt.Errorf("error reading file %s", err)
@@ -157,7 +167,6 @@ func varFileSearch(ns string, filename string) (string, error) {
 }
 
 func main() {
-
 	if *token == "" {
 		client = github.NewClient(nil)
 	} else {
@@ -171,13 +180,39 @@ func main() {
 	}
 
 	prs := listFiles()
+	// setting stdout to a file
+	fname := filepath.Join(os.TempDir(), "stdout")
+	old := os.Stdout            // keep backup of the real stdout
+	temp, _ := os.Create(fname) // create temp file
+	os.Stdout = temp
+
 	for _, pr := range prs {
-		filename := *pr.Filename
-		filenameS := strings.Split(filename, "/")
-		repoNamespace = filenameS[2]
-		err := decodeFile(filename)
-		if err != nil {
-			log.Fatal(err)
+		mm.File = *pr.Filename
+		if filepath.Ext(mm.File) == ".tf" {
+			fileS := strings.Split(mm.File, "/")
+			mm.RepositoryNamespace = fileS[2]
+			blocks, err := decodeFile()
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, block := range blocks {
+				if block.Type() == "resource" {
+					rtn := block.Labels()
+					mm.ResourceTypeName = rtn[1]
+					mm.ResourceNamespace, mm.ResourceName = resourceType(block)
+					if !strings.Contains(mm.ResourceNamespace, mm.RepositoryNamespace) {
+						githubaction.SetOutput("mismatch", "true")
+						r.Result = fmt.Sprintf("\nRepository Namespace: %s\nFile: %s\nResosurce Type Name: %s\nResource Name: %s\nResource Namespace: %s\n", mm.RepositoryNamespace, mm.File, mm.ResourceTypeName, mm.ResourceName, mm.ResourceNamespace)
+						fmt.Println(r.Result)
+					}
+				}
+			}
 		}
 	}
+	// back to normal state
+	temp.Close()
+	os.Stdout = old // restoring the real stdout
+	// reading our temp stdout
+	out, _ := ioutil.ReadFile(fname)
+	githubaction.SetOutput("result", string(out))
 }
