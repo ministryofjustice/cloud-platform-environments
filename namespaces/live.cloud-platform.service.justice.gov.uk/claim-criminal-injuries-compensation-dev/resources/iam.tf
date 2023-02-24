9b05718bb3c00d94f5dc2429f5b2fce6a68f5f27
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-02-24 11:42:30 +0000

    claim-criminal-injuries-dev add notify queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/iam.tf
@@ -8,6 +8,16 @@ data "aws_iam_policy_document" "dcs_access" {
       module.claim-criminal-injuries-application-queue.sqs_arn
     ]
   }
+
+  statement {
+      sid = "AllowDCSToSendMessageToNotifyQueue"
+      actions = [
+        "sqs:SendMessage"
+      ]
+      resources = [
+        module.claim-criminal-injuries-notify-queue.sqs_arn
+      ]
+    }
 }
 
 resource "random_id" "id" {
@@ -133,5 +143,47 @@ resource "kubernetes_secret" "redrive-service-sqs-secret" {
     user_arn           = aws_iam_user.redrive_service.arn
     access_key_id      = aws_iam_access_key.redrive_service.id
     secret_access_key  = aws_iam_access_key.redrive_service.secret
+  }
+}
+
+data "aws_iam_policy_document" "notify_gateway_access" {
+  statement {
+    sid = "AllowNotifyGatewayToReadFromNotifyQueue"
+    actions = [
+      "sqs:DeleteMessage",
+      "sqs:ReceiveMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = [
+      module.claim-criminal-injuries-notify-queue.sqs_arn
+    ]
+  }
+}
+
+resource "aws_iam_user" "notify_gateway" {
+  name = "notify_gateway-${random_id.id.hex}"
+  path = "/system/cica-notify-gateway-user/"
+}
+
+resource "aws_iam_access_key" "notify_gateway" {
+  user = aws_iam_user.notify_gateway.name
+}
+
+resource "aws_iam_user_policy" "notify_gateway_policy" {
+  name   = "notify_gateway_access_policy"
+  policy = data.aws_iam_policy_document.notify_gateway_access.json
+  user   = aws_iam_user.notify_gateway.name
+}
+
+resource "kubernetes_secret" "notify-gateway-sqs-secret" {
+  metadata {
+    name      = "notify-gateway-sqs-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    user_arn          = aws_iam_user.notify_gateway.arn
+    access_key_id     = aws_iam_access_key.notify_gateway.id
+    secret_access_key = aws_iam_access_key.notify_gateway.secret
   }
 }
