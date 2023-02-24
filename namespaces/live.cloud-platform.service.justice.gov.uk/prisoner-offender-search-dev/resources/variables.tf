1e3c430f91c7d8bc3700def969f20253314cb101
petergphillips <peter.phillips@digital.justice.gov.uk>
2023-02-24 11:42:04 +0000

    Revert "SDI-642: ✨ Add github team to access AWS console for prisoner-offender-search-dev (#11041)"
    This reverts commit eb178248fe15aad58b2526cbcc55bfa5c1c08f45.

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-offender-search-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-offender-search-dev/resources/variables.tf
@@ -22,7 +22,7 @@ variable "business-unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "syscon-devs"
+  default     = "dps"
 }
 
 variable "environment-name" {
