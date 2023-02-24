b7b4a34bb2b856d1a034bbcb3e84d89a18928bcf
petergphillips <peter@greenthistle.co.uk>
2023-02-24 10:48:43 +0000

    SDI-642: ✨ Add github team to access AWS console for offender-events-prod (#11040)

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/variables.tf
@@ -22,7 +22,7 @@ variable "business-unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "Digital-Prison-Services"
+  default     = "syscon-devs"
 }
 
 variable "environment-name" {
