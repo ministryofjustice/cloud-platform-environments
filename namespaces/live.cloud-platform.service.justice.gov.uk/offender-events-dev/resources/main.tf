f560092a35e653bbbe886f6cb5128335d78ef5f9
petergphillips <peter@greenthistle.co.uk>
2023-02-24 11:47:55 +0000

    Revert "SDI-642: ✨ Add github team to access AWS console for offender-events-dev (#11038)" (#11051)
    This reverts commit 18f25633a9466774c908501b3b71723d433b2205.

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/main.tf
@@ -5,39 +5,18 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
-
-  default_tags {
-    tags = {
-      # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
-      GithubTeam = var.team_name
-    }
-  }
 }
 
 # To be use in case the resources need to be created in London
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
-
-  default_tags {
-    tags = {
-      # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
-      GithubTeam = var.team_name
-    }
-  }
 }
 
 # To be use in case the resources need to be created in Ireland
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
-
-  default_tags {
-    tags = {
-      # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
-      GithubTeam = var.team_name
-    }
-  }
 }
 
 provider "random" {
@@ -48,3 +27,4 @@ provider "github" {
   owner = "ministryofjustice"
   token = var.github_token
 }
+
