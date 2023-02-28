77cf52605d79f707ad56b3b2e7a204b39ac3b733
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-02-28 14:51:21 +0000

    Add named serviceaccount secrets
    Because we need to deploy to several environments
    via GHA this pattern allows secrets to be authomtically created
    with an "environment" specific name. Automatic or CP initiated
    rotation of these secrets, in the event of a breach, would then
    be transparent.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-staging/resources/serviceaccount.tf
@@ -4,7 +4,10 @@ module "serviceaccount" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["laa-assure-hmrc-data"]
+  github_repositories = [var.repo_name]
+
+  github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
+  github_actions_secret_kube_token     = var.github_actions_secret_kube_token
+  github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
+  github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
 }
