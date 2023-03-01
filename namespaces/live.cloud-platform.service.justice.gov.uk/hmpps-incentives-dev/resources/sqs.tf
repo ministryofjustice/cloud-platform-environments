4e25ed5c2f0a7214eaacbf3abe096f8cb3866cf3
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-03-01 16:46:25 +0000

    INC-1119: trial using AWS SSM for storage and retrieval of domain events topic ARN

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic_subscription" "prisoner_event_queue_subscription" {
-  topic_arn     = data.aws_sns_topic.hmpps-domain-events.arn
+  topic_arn     = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
   protocol      = "sqs"
   endpoint      = module.prisoner-event-queue.sqs_arn
   filter_policy = jsonencode({
@@ -50,7 +50,7 @@ resource "aws_sqs_queue_policy" "prisoner-event-queue-policy" {
                       {
                         "ArnEquals":
                           {
-                            "aws:SourceArn": "${data.aws_sns_topic.hmpps-domain-events.arn}"
+                            "aws:SourceArn": "${data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value}"
                           }
                         }
         }
