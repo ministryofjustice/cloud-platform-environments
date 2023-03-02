6b08d9c985fb1c4d9ffb3f87273aa64609e781a8
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-03-02 16:16:27 +0000

    Upgrade dms module to 2.3.0 (#11176)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/sqs.tf
@@ -1,7 +1,7 @@
 resource "aws_sns_topic_subscription" "prisoner_event_queue_subscription" {
-  topic_arn     = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
-  protocol      = "sqs"
-  endpoint      = module.prisoner-event-queue.sqs_arn
+  topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  protocol  = "sqs"
+  endpoint  = module.prisoner-event-queue.sqs_arn
   filter_policy = jsonencode({
     eventType = [
       "prison-offender-events.prisoner.merged",
