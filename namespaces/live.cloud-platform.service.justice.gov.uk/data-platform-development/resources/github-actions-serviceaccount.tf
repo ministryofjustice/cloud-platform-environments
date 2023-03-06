1d58994d6b453ebed21b3daa4fe535750173c45a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-03-06 17:24:56 +0000

    Added GitHub Actions service account Added RDS secret output

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-development/resources/github-actions-serviceaccount.tf
@@ -0,0 +1,12 @@
+module "github_actions_serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.8.0"
+
+  namespace = var.namespace
+
+  github_repositories = ["data-platform"]
+
+  github_actions_secret_kube_cluster   = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_CLUSTER"
+  github_actions_secret_kube_namespace = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_NAMESPACE"
+  github_actions_secret_kube_cert      = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_CERT"
+  github_actions_secret_kube_token     = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_TOKEN"
+}
