f44d0e032ed186a250c3bd1a6dbf2ac1f1febbd9
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-03-06 09:27:41 +0000

    PIC-2522 Add initial namespace for HMPPS Person Record Service

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/variables.tf
@@ -0,0 +1,29 @@
+variable "application" {
+  default = "HMPPS Person Record Service"
+}
+
+variable "namespace" {
+  default = "hmpps-person-record-preprod"
+}
+
+variable "business_unit" {
+  default = "HMPPS"
+}
+
+variable "team_name" {
+  default = "hmpps-developers"
+}
+
+variable "environment" {
+  default = "preprod"
+}
+
+variable "infrastructure_support" {
+  default = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/variables.tf
@@ -0,0 +1,29 @@
+variable "application" {
+  default = "HMPPS Person Record Service"
+}
+
+variable "namespace" {
+  default = "hmpps-person-record-prod"
+}
+
+variable "business_unit" {
+  default = "HMPPS"
+}
+
+variable "team_name" {
+  default = "hmpps-developers"
+}
+
+variable "environment" {
+  default = "prod"
+}
+
+variable "infrastructure_support" {
+  default = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "true"
+}
+
+
