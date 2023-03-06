d4328f8425e2f3658518f0ad4e8ce5f3346b254e
jakemulley <jakemulley@users.noreply.github.com>
2023-03-06 09:07:47 +0000

    Versions integrity: bump provider versions to match general consensus

--- namespaces/live.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ppud-replacement-dev/resources/versions.tf
@@ -16,7 +16,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.4.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
