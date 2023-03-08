e24e60a7687bad6b90c78335031b1acbc452fbda
adam <brown-a2@users.noreply.github.com>
2023-03-08 09:51:58 +0000

    Fix vars to use underscore rather then dash

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/haletest1-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/haletest1-route53.tf
@@ -2,12 +2,12 @@ resource "aws_route53_zone" "haletest1_route53_zone" {
   name = "haletest1.service.justice.gov.uk" 
 
   tags = {
-    business-unit          = var.business-unit
+    business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
-    environment-name       = var.environment-name
+    is-production          = var.is_production
+    environment-name       = var.environment
     owner                  = var.team_name
-    infrastructure-support = var.infrastructure-support
+    infrastructure-support = var.infrastructure_support
   }
 }
 
--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/haletest2-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/haletest2-route53.tf
@@ -2,12 +2,12 @@ resource "aws_route53_zone" "haletest2_route53_zone" {
   name = "haletest2.service.justice.gov.uk" 
 
   tags = {
-    business-unit          = var.business-unit
+    business-unit          = var.business_unit
     application            = var.application
-    is-production          = var.is-production
-    environment-name       = var.environment-name
+    is-production          = var.is_production
+    environment-name       = var.environment
     owner                  = var.team_name
-    infrastructure-support = var.infrastructure-support
+    infrastructure-support = var.infrastructure_support
   }
 }
 
