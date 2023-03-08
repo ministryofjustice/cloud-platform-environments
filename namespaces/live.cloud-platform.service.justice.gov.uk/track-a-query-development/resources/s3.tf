644e2cc12ed3d0333e68ebbd41c465aed2c97d83
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-03-08 16:27:50 +0000

    Update infra support email (#11301)

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-development/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-development/resources/s3.tf
@@ -11,7 +11,7 @@ module "track_a_query_s3" {
   application            = "track-a-query"
   is-production          = "false"
   environment-name       = "development"
-  infrastructure-support = "mohammed.seedat@digital.justice.gov.uk"
+  infrastructure-support = var.infrastructure_support
   namespace              = var.namespace
 
   cors_rule = [
