0b112fab8c59139b5d9589e6dbb0c6559877f7ae
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-03-09 15:49:38 +0000

    SLM-287: Move Redis to custom param group SLM dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-dev/resources/elasticache-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-dev/resources/elasticache-api.tf
@@ -13,8 +13,7 @@ module "slmtp_api_elasticache_redis" {
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
   engine_version         = "7.0"
-  parameter_group_name   = "default.redis7"
-  # parameter_group_name   = aws_elasticache_parameter_group.token_store.name
+  parameter_group_name   = aws_elasticache_parameter_group.token_store.name
   namespace              = var.namespace
 
   providers = {
