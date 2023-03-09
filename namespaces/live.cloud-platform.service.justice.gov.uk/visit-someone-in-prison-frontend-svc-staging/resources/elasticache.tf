8720b850bc9ea543366026b7e5bf402e26b3af7d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-03-09 08:01:50 +0000

    VB-1975: Upgrade Redis VSiP FE staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-staging/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-staging/resources/elasticache.tf
@@ -12,8 +12,8 @@ module "elasticache_redis" {
   team_name              = var.team_name
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
