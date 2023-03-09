13c1d689b5e5d12eb9efa9dfc52efe66fc714487
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-03-09 13:01:42 +0000

    SLM-287: Upgrade redis for SLM dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-dev/resources/elasticache-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-dev/resources/elasticache-api.tf
@@ -12,7 +12,7 @@ module "slmtp_api_elasticache_redis" {
   team_name              = var.team_name
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "5.0.6"
+  engine_version         = "7.0"
   parameter_group_name   = aws_elasticache_parameter_group.token_store.name
   namespace              = var.namespace
 
@@ -31,12 +31,14 @@ resource "kubernetes_secret" "slmtp_api_elasticache_redis" {
     primary_endpoint_address = module.slmtp_api_elasticache_redis.primary_endpoint_address
     auth_token               = module.slmtp_api_elasticache_redis.auth_token
     member_clusters          = jsonencode(module.slmtp_api_elasticache_redis.member_clusters)
+    access_key_id            = module.elasticache_redis.access_key_id
+    secret_access_key        = module.elasticache_redis.secret_access_key
   }
 }
 
 resource "aws_elasticache_parameter_group" "token_store" {
   name   = "slmtp-api-dev-token-store-parameter-group"
-  family = "redis5.0"
+  family = "redis7.0"
 
   # Needed in order to get spring boot to expire items from the redis cache
   parameter {
