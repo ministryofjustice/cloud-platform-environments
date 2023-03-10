b97bd5bc2cf2c7e9650335fe7621bb05bcd0b26d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-03-10 07:03:33 +0000

    SLM-287: SLM preprod, ui redis update, default params API

--- namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-preprod/resources/elasticache-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-preprod/resources/elasticache-api.tf
@@ -13,7 +13,8 @@ module "slmtp_api_elasticache_redis" {
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
   engine_version         = "5.0.6"
-  parameter_group_name   = aws_elasticache_parameter_group.token_store.name
+  parameter_group_name   = "default.redis5.x"
+  # parameter_group_name   = aws_elasticache_parameter_group.token_store.name
   namespace              = var.namespace
 
   providers = {
