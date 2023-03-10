ca02fb4ecfa91d3e88e73a1780099c160cec60cf
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-03-10 13:38:08 +0000

    HWPV-84: Update Redis HwPV preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/help-with-prison-visits-preprod/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/help-with-prison-visits-preprod/resources/elasticache.tf
@@ -12,8 +12,8 @@ module "hwpv_elasticache_redis" {
   team_name              = var.team_name
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
@@ -31,5 +31,7 @@ resource "kubernetes_secret" "hwpv_elasticache_redis" {
     primary_endpoint_address = module.hwpv_elasticache_redis.primary_endpoint_address
     auth_token               = module.hwpv_elasticache_redis.auth_token
     member_clusters          = jsonencode(module.hwpv_elasticache_redis.member_clusters)
+    access_key_id            = module.hwpv_elasticache_redis.access_key_id
+    secret_access_key        = module.hwpv_elasticache_redis.secret_access_key
   }
 }
