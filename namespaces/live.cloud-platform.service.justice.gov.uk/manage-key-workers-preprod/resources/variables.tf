ad1f86fdd2000a7d3da34182d75af0817a865411
jakemulley <jakemulley@users.noreply.github.com>
2023-03-13 16:17:20 +0000

    namespaces L-M: Move vpc_name variable to variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-preprod/resources/variables.tf
@@ -1,3 +1,11 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * variable is automatically supplied by the pipeline.
+ *
+ */
+variable "vpc_name" {
+}
+
 variable "application" {
   default = "manage-key-workers-preprod"
 }
