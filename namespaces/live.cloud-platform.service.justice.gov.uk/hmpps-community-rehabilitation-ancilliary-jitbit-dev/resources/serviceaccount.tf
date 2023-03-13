3579b928bd37463dc6a319634f024348bde9ff97
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-03-13 17:50:08 +0000

    Bump version of service account module

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-rehabilitation-ancilliary-jitbit-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-rehabilitation-ancilliary-jitbit-dev/resources/serviceaccount.tf
@@ -1,5 +1,5 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.7.6"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.8.0"
 
   serviceaccount_name = "github-actions-serviceaccount"
   namespace           = var.namespace
