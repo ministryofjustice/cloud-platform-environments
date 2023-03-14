092a44f651335cac090f7a6bbfcf786a2993b061
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-03-14 12:32:02 +0000

    INC-1119:  use AWS SSM to store hmpps-domain-events-prod topic ARN

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/hmpps-domain-events-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/hmpps-domain-events-topic.tf
@@ -15,6 +15,22 @@ module "hmpps-domain-events" {
     aws = aws.london
   }
 }
+resource "aws_ssm_parameter" "param-store-topic-arn" {
+  type        = "String"
+  name        = "/${var.namespace}/topic-arn"
+  value       = module.hmpps-domain-events.topic_arn
+  description = "SNS topic ARN for ${var.namespace}; use this parameter from other DPS dev namespaces"
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    owner                  = var.team_name
+    environment-name       = var.environment-name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
 
 resource "aws_iam_access_key" "key_2023" {
   user = module.hmpps-domain-events.user_name
