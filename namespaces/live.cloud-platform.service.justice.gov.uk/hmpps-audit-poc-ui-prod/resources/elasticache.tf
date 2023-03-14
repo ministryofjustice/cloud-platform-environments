385d3dea82f78a8803aeff0522dc8e79b55aacaf
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-03-14 15:33:00 +0000

    HAAR-1248: update redis audit-poc-ui-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-ui-prod/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-ui-prod/resources/elasticache.tf
@@ -5,6 +5,7 @@
 module "elasticache_redis" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=6.0.0"
   vpc_name               = var.vpc_name
+  business-unit          = var.business_unit
   application            = var.application
   environment-name       = var.environment
   is-production          = var.is_production
@@ -12,8 +13,8 @@ module "elasticache_redis" {
   team_name              = var.team_name
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
