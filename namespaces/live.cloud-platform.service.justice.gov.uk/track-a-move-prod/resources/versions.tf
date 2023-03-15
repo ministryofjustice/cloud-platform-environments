504f2b39587423995e984b94fa01e1345005dc09
jakemulley <jakemulley@users.noreply.github.com>
2023-03-15 11:13:28 +0000

    Bump hashicorp/kubernetes provider to v2.18.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/versions.tf
@@ -11,7 +11,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.3.0"
+      version = "~> 2.18.0"
     }
     random = {
       source  = "hashicorp/random"
