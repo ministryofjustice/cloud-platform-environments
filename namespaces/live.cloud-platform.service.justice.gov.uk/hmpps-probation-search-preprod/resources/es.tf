ad1746d8ccd7c125762630f1ed89964e0045e22f
Marcus Aspin <maspin@unilink.com>
2023-03-16 13:23:41 +0000

    Increase instance count and reduce instance size

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/es.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/es.tf
@@ -19,9 +19,8 @@ module "probation_search_elasticsearch" {
   dedicated_master_enabled        = true
   dedicated_master_count          = 3
   dedicated_master_type           = "m6g.large.elasticsearch"
-  instance_count                  = 6
-  instance_type                   = "m6g.xlarge.elasticsearch"
-  ebs_volume_size                 = 256
-  ebs_iops                        = 6000
+  instance_count                  = 12
+  instance_type                   = "m6g.large.elasticsearch"
+  ebs_volume_size                 = 128
 }
 
