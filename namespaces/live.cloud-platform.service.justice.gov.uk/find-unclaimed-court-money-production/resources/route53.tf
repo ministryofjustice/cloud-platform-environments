844a26b5476fe8f322b1ba7d7dd024d88e64fa64
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-03-20 11:45:11 +0000

    Create route53 config

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/find-unclaimed-court-money-production/resources/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "find_unclaimed_court_money_route53_zone" {
+  name = "find-unclaimed-court-money.service.justice.gov.uk"
+
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "find_unclaimed_court_money_route53_zone_sec" {
+  metadata {
+    name      = "find-unclaimed-court-money-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.find_unclaimed_court_money_route53_zone.zone_id
+  }
+}
