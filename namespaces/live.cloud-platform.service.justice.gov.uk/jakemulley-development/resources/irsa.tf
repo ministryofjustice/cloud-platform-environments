729e3668de4ef1f7eca884734d4a67ceafe1c591
jakemulley <jakemulley@users.noreply.github.com>
2023-03-20 13:04:10 +0000

    Try again

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/irsa.tf
@@ -1,3 +1,11 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+
+  eks_cluster_name = var.eks_cluster_name
+  namespace        = "jakemulley-development"
+  role_policy_arns = [aws_iam_policy.policy.arn]
+}
+
 data "aws_iam_policy_document" "document" {
   statement {
     actions = [
@@ -10,25 +18,16 @@ data "aws_iam_policy_document" "document" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "jm-dev-simple-policy-for-testing-irsa"
-  path        = "/cloud-platform/"
+  name        = "jm-dev-irsa-test"
   policy      = data.aws_iam_policy_document.document.json
-  description = "Policy for testing cloud-platform-terraform-irsa"
-}
-
-module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
-
-  eks_cluster_name = var.eks_cluster_name
-  namespace        = var.namespace
-  role_policy_arns = [aws_iam_policy.policy.arn]
 }
 
 resource "kubernetes_secret" "irsa" {
   metadata {
-    name      = "irsa"
-    namespace = var.namespace
+    name      = "irsa-output"
+    namespace = "jakemulley-development"
   }
+
   data = {
     role           = module.irsa.aws_iam_role_arn
     serviceaccount = module.irsa.service_account_name.name
