6629e4452ea5270c5162376fcae90d316cb26ce5
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-03-27 07:01:07 +0100

    PIC-2801
    . Added initial namespace for court probation e2e.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-e2e/resources/variables.tf
@@ -0,0 +1,32 @@
+variable "vpc_name" {
+}
+
+variable "application" {
+  default = "HMPPS  Court Probation"
+}
+
+variable "namespace" {
+  default = "court-probation-e2e"
+}
+
+variable "business_unit" {
+  default = "HMPPS"
+}
+
+variable "team_name" {
+  default = "hmpps-developers"
+}
+
+variable "environment" {
+  default = "development"
+}
+
+variable "infrastructure_support" {
+  default = "Probation in Court Team: probation-in-court-team@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+
