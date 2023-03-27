18018f4273b8d14cc03ef91434a1017e63b0fe50
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-03-27 07:31:14 +0100

    PIC-2801
    . Removed kubernetes from versions.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-e2e/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-e2e/resources/versions.tf
@@ -5,9 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.27.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.18.0"
-    }
   }
 }
