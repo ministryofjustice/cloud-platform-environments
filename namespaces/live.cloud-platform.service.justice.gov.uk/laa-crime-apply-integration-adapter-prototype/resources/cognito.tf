6c68c4570162595810a2a89991aad98bad2df944
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-03-28 09:44:50 +0100

    LASB-1819: Updated for tflint warnings

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/cognito.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/cognito.tf
@@ -4,7 +4,7 @@ resource "aws_cognito_user_pool" "pool" {
 
 resource "aws_cognito_user_pool_client" "client" {
   name                  = var.cognito_user_pool_client_name
-  user_pool_id          = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id          = aws_cognito_user_pool.pool.id
   explicit_auth_flows   = ["ALLOW_REFRESH_TOKEN_AUTH"]
   allowed_oauth_flows   = ["client_credentials"]
   generate_secret       = true
@@ -19,7 +19,7 @@ resource "aws_cognito_resource_server" "resource" {
     scope_description = var.resource_server_scope_description
   }
 
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 resource "aws_cognito_user_pool_domain" "domain" {
