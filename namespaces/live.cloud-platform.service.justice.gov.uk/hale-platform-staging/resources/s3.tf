cd04e746980f3caf52e8d0e247129c8115e1b63b
adam <brown-a2@users.noreply.github.com>
2023-03-30 11:22:58 +0100

    Fix syntax

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/s3.tf
@@ -21,7 +21,7 @@ module "s3_bucket" {
                     By default buckets are private, however to create a 'public' bucket add the following two variables when calling the module:
   */
 
- //acl                           = "public-read"
+ # acl                           = "public-read"
  enable_allow_block_pub_access = false
 
  /*
