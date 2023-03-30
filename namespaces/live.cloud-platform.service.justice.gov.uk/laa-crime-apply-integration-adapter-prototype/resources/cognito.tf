acd5f16bc4c582259ba4734faaa14119b44f4415
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-03-30 11:18:51 +0100

    LASB-1819:Update the aws_apigatewayv2_route - route_key and resource tag team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/cognito.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/cognito.tf
@@ -9,13 +9,7 @@ resource "aws_cognito_user_pool" "pool" {
   }
 
   tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    environment-name       = var.environment_name
-    owner                  = var.team_name
-    infrastructure-support = var.infrastructure_support
-    namespace              = var.namespace
+    GithubTeam = var.team_name
   }
   
 }
--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/main.tf
@@ -5,6 +5,18 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+      tags = {
+        business-unit          = var.business_unit
+        application            = var.application
+        is-production          = var.is_production
+        environment-name       = var.environment_name
+        owner                  = var.team_name
+        infrastructure-support = var.infrastructure_support
+        namespace              = var.namespace
+    }
+  }
 }
 
 provider "aws" {
