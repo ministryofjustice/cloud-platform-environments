4ffbdccf4a36de2db0a3c751639db79d0407d1c5
Emile <emile.swarts@digital.justice.gov.uk>
2023-03-31 12:13:17 +0100

    Add stage to usage plan

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-development/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-development/resources/api_gateway.tf
@@ -124,6 +124,7 @@ resource "aws_api_gateway_api_key" "team" {
 
 resource "aws_api_gateway_usage_plan" "default" {
   name = var.namespace
+  stage = aws_api_gateway_stage.development.id
 
   api_stages {
     api_id = aws_api_gateway_rest_api.api_gateway.id
