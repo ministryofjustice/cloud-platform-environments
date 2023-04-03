397f1a96bd8bbeb0c140ad8f2520cfd64be879ef
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-04-03 12:31:29 +0100

    LASB-1819:Enable cloudwatch log for api gateway stage

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-apply-integration-adapter-prototype/resources/api_gateway.tf
@@ -45,8 +45,3 @@ resource "aws_apigatewayv2_stage" "stage" {
   }
 
 }
-
-resource "aws_cloudwatch_log_group" "log_group" {
-  name              = var.log_group_name 
-  retention_in_days = var.log_retention_in_days
-}
