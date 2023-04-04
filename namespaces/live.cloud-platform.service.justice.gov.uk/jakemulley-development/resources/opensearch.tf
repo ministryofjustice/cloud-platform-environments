519c467c95a4b78093098422093d26688984ee65
jakemulley <jakemulley@users.noreply.github.com>
2023-04-04 15:33:50 +0100

    Add other instance_count variants

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/opensearch.tf
@@ -26,3 +26,61 @@ module "opensearch_non_production" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
+
+module "opensearch_production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=fix-ebs-sg" # use the latest release
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version = "OpenSearch_2.5"
+
+  cluster_config   = {
+    instance_count = 3
+    instance_type  = "m6g.large.search"
+  }
+
+  ebs_options = {
+    volume_size = 100
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+module "opensearch_production_huge" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=fix-ebs-sg" # use the latest release
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version = "OpenSearch_2.5"
+
+  cluster_config   = {
+    instance_count = 15
+    instance_type  = "m6g.xlarge.search"
+  }
+
+  ebs_options = {
+    volume_size = 100
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
