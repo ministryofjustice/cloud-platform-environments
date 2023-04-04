1c12d5a4592744eae1ba1d4d4dbf606d592d334e
jakemulley <jakemulley@users.noreply.github.com>
2023-04-04 13:54:50 +0100

    jakemulley-development: add opensearch

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/opensearch.tf
@@ -0,0 +1,24 @@
+module "opensearch" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=main" # use the latest release
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version = "OpenSearch_2.5"
+
+  cluster_config   = {
+    instance_count = 3
+    instance_type  = "t3.medium.search"
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
