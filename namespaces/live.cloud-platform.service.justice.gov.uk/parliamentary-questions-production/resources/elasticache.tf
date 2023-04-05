85436c0631d2462f9058f8176f4db8194a45715f
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-04-05 16:46:10 +0100

    Rotate elasticache auth token - parliamentary-questions-production (#12000)

--- namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/elasticache.tf
@@ -17,6 +17,8 @@ module "parliamentary_questions_elasticache_redis" {
   node_type              = "cache.t4g.small"
   namespace              = var.namespace
 
+  auth_token_rotated_date = "2023-04-05"
+
   providers = {
     aws = aws.london
   }
