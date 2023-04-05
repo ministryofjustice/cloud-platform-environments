55798e66aa91a19e18de798a01bf0175e216b3cb
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-04-05 15:16:00 +0100

    Upgrade Redis - parliamentary-questions-production (#11997)

--- namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/elasticache.tf
@@ -12,8 +12,8 @@ module "parliamentary_questions_elasticache_redis" {
   is-production          = var.is_production
   environment-name       = var.environment-name
   infrastructure-support = var.infrastructure_support
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
