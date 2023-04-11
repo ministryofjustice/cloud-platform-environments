0dc54632086c9dc7d724a180f2bd7361a9528cd0
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-04-11 16:30:48 +0100

    feat(aws): add dynamodb, serviceaccount and ecr registry

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-reports-dev/resources/ecr.tf
@@ -0,0 +1,22 @@
+module "ecr-repo" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.1.1"
+
+  team_name = var.team_name
+  repo_name = "${var.namespace}-ecr"
+
+  github_repositories = ["operations-engineering-reports"]
+}
+
+resource "kubernetes_secret" "ecr-repo" {
+  metadata {
+    name      = "ecr-repo-${var.namespace}"
+    namespace = var.namespace
+  }
+
+  data = {
+    repo_url          = module.ecr-repo.repo_url
+    access_key_id     = module.ecr-repo.access_key_id
+    secret_access_key = module.ecr-repo.secret_access_key
+  }
+}
+
