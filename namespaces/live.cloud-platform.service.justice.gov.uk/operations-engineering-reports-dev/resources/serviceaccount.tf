1b1c4ca683e51923e203de0b8b6f91d6c5f59793
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-04-11 17:10:17 +0100

    refactor(operations-engineering-reports-dev): rename env vars
    this was conflicting with production values named the same.

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-reports-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-reports-dev/resources/serviceaccount.tf
@@ -5,4 +5,10 @@ module "serviceaccount" {
   kubernetes_cluster = var.kubernetes_cluster
 
   github_repositories = ["operations-engineering-reports"]
+
+  # Using a DEV* prefix here to avoid conflicts with production.
+  github_actions_secret_kube_token     = "DEV_KUBE_TOKEN"
+  github_actions_secret_kube_cert      = "DEV_KUBE_CERT"
+  github_actions_secret_kube_namespace = "DEV_KUBE_NAMESPACE"
+  github_actions_secret_kube_cluster   = "DEV_KUBE_CLUSTER"
 }
