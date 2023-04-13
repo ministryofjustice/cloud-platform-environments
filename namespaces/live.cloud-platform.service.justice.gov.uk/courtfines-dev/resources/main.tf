05c2a379d9924e4bc9bbdff4a46368900b401919
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-04-13 16:53:15 +0100

    RST-5319: Add ecr to dev environment in cloud platform for court fines

--- namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/main.tf
@@ -34,6 +34,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = var.team_name
     }
   }
 }
