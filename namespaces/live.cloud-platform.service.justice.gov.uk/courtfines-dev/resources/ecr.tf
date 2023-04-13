1149c073cb7ba63a9bc651157a83448227ca6d78
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-04-13 16:57:56 +0100

    RST-5319: bump module version

--- namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.0"
+  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.1.1"
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
 
