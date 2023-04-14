ae6f3f3282220913642d7705ab819dc4c26d2af9
Emile <emile.swarts@digital.justice.gov.uk>
2023-04-14 10:47:46 +0100

    Add missing resources to production environment
    API gateway is required for client authentication

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-production/resources/versions.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = ">= 1.2.5"
   required_providers {
@@ -9,6 +8,10 @@ terraform {
     github = {
       source  = "integrations/github"
       version = "~> 5.17.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
