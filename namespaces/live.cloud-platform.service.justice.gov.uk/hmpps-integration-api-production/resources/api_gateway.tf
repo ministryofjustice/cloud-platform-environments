896de2ad633514b00d40b47d393302d62ff4a0ba
Emile <emile.swarts@digital.justice.gov.uk>
2023-04-17 13:26:27 +0100

    Fix toset syntax

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-production/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-production/resources/api_gateway.tf
@@ -126,7 +126,7 @@ resource "aws_api_gateway_deployment" "production" {
 }
 
 resource "aws_api_gateway_api_key" "team" {
-  for_each = to_set([var.team_name])
+  for_each = toset([var.team_name])
   name = each.key
 }
 
