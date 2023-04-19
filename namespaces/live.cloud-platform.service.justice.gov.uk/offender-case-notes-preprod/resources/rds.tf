5a009afd2a264f909585ec4141d357d03217d47a
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-04-19 16:26:27 +0100

    HAAR-1365 Increase max_allocated_storage to 10000

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
@@ -10,7 +10,7 @@ module "dps_rds" {
   infrastructure-support = var.infrastructure_support
   rds_family             = var.rds-family
   db_instance_class         = "db.t4g.small"
-  db_max_allocated_storage  = "500"
+  db_max_allocated_storage  = "10000"
   deletion_protection       = true
   prepare_for_major_upgrade = true
   db_engine                 = "postgres"
