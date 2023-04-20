6b8158ac260ce6878c9c18852b88624317f067dc
Asfand Qazi <asfand.qazi@digital.justice.gov.uk>
2023-04-20 14:21:37 +0100

    offender-management-staging: Enable auto start stop for RDS

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/rds.tf
@@ -7,20 +7,21 @@
 module "allocation-rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
 
-  vpc_name               = var.vpc_name
-  db_instance_class      = "db.t4g.small"
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  namespace              = var.namespace
-  environment-name       = var.environment_name
-  infrastructure-support = var.infrastructure_support
-  db_engine              = "postgres"
-  rds_family             = "postgres14"
-  db_engine_version      = "14"
-  db_name                = "allocations"
-  db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
+  vpc_name                   = var.vpc_name
+  db_instance_class          = "db.t4g.small"
+  team_name                  = var.team_name
+  business-unit              = var.business_unit
+  application                = var.application
+  is-production              = var.is_production
+  namespace                  = var.namespace
+  environment-name           = var.environment_name
+  infrastructure-support     = var.infrastructure_support
+  db_engine                  = "postgres"
+  rds_family                 = "postgres14"
+  db_engine_version          = "14"
+  db_name                    = "allocations"
+  db_parameter               = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
+  enable_rds_auto_start_stop = true
 
   db_password_rotated_date = "2023-04-05T11:31:27Z"
 
