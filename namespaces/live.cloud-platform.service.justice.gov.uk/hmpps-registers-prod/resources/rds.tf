a394f3ee44acd756cc46886aaeac1c326b34bbb9
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-04-21 13:38:21 +0100

    HAAR-1359 Remove skip file for registers-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-prod/resources/rds.tf
@@ -12,7 +12,7 @@ module "dps_rds" {
   db_instance_class          = "db.t4g.small"
   db_max_allocated_storage   = "10000"
   deletion_protection        = true
-  prepare_for_major_upgrade  = true
+  prepare_for_major_upgrade  = false
   rds_family                 = "postgres15"
   db_engine                  = "postgres"
   db_engine_version          = "15"
