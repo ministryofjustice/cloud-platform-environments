4ec88876859866c8a9c9e71550189bb158efc612
Peter Phillips <peter@greenthistle.co.uk>
2023-04-21 14:37:43 +0100

    SDIT-759: 🔧 Upgrade to postgres 15 for hmpps-restricted-patients-api-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-preprod/resources/rds.tf
@@ -1,23 +1,21 @@
 module "rp_rds" {
-  source                   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
-  vpc_name                 = var.vpc_name
-  team_name                = var.team_name
-  business-unit            = var.business_unit
-  application              = var.application
-  is-production            = var.is_production
-  namespace                = var.namespace
-  environment-name         = var.environment-name
-  infrastructure-support   = var.infrastructure_support
-  db_instance_class        = "db.t4g.small"
-  db_engine                = "postgres"
-  db_engine_version        = "14"
-  rds_family               = "postgres14"
-  db_password_rotated_date = "15-02-2023"
-  deletion_protection      = true
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
 
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "false"
-
+  vpc_name                  = var.vpc_name
+  team_name                 = var.team_name
+  business-unit             = var.business_unit
+  application               = var.application
+  is-production             = var.is_production
+  namespace                 = var.namespace
+  environment-name          = var.environment-name
+  infrastructure-support    = var.infrastructure_support
+  db_instance_class         = "db.t4g.small"
+  db_engine                 = "postgres"
+  db_engine_version         = "15"
+  rds_family                = "postgres15"
+  db_password_rotated_date  = "15-02-2023"
+  deletion_protection       = true
+  prepare_for_major_upgrade = true
 
   providers = {
     aws = aws.london
