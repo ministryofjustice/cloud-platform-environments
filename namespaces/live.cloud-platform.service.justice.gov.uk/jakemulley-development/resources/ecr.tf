6841dcbb0c4517251552e43b77d821436c14a0d1
jakemulley <jakemulley@users.noreply.github.com>
2023-04-21 16:19:36 +0100

    jakemulley-development: OIDC ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
@@ -1,8 +1,10 @@
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=oidc"
 
   repo_name = var.namespace
   team_name = var.team_name
 
   github_repositories = ["cloud-platform-ecr-oidc-test"]
 }
+
+# there is already an oidc provider in CP for GitHub from a firebreak test a while ago, need to remove it when rolling this out for real and create it in -infrastructure
