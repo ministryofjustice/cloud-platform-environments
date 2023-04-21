4d55b5e7ec58acf1a1e83cfc174c6d8ffffd19cf
brian.carr <brian.carr@digital.justice.gov.uk>
2023-04-21 15:19:50 +0100

    set redis to version 7

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mercury-submit-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mercury-submit-dev/resources/elasticache.tf
@@ -12,9 +12,9 @@ module "mercury_submit_elasticache_redis" {
   team_name              = var.team_name
   business-unit          = var.business_unit
   number_cache_clusters  = var.number_cache_clusters
-  node_type              = "cache.t2.small"
-  engine_version         = "5.0.6"
-  parameter_group_name   = "default.redis5.0"
+  node_type              = "cache.t4g.small"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
