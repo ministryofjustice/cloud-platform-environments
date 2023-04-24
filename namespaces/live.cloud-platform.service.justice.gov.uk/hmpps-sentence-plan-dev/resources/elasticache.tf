6fbecd0238afe7652efeb54bd5c0fec49d5fdc5d
Marcus Aspin <maspin@unilink.com>
2023-04-24 17:41:51 +0100

    small -> micro

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/elasticache.tf
@@ -9,7 +9,7 @@ module "elasticache_redis" {
   environment-name       = var.environment
   infrastructure-support = var.infrastructure_support
 
-  node_type            = "cache.t4g.small"
+  node_type            = "cache.t4g.micro"
   engine_version       = "7.0"
   parameter_group_name = "default.redis7"
 }
