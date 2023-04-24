08fb573b739c841338a2b44219e5a1e063dbeda5
Jesus Laiz <zheileman@users.noreply.github.com>
2023-04-24 10:50:25 +0100

    Comment out unused providers
    These will be required when adding the resources.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
@@ -9,17 +9,17 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.17.0"
     }
-    pingdom = {
-      source  = "DrFaust92/pingdom"
-      version = "~> 1.3.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.18.0"
-    }
+#    pingdom = {
+#      source  = "DrFaust92/pingdom"
+#      version = "~> 1.3.1"
+#    }
+#    random = {
+#      source  = "hashicorp/random"
+#      version = "~> 3.4.3"
+#    }
+#    kubernetes = {
+#      source  = "hashicorp/kubernetes"
+#      version = "~> 2.18.0"
+#    }
   }
 }
