40c911d3c42b5f4f33c05b8b17b8b01e397e3e6f
Marcus Aspin <maspin@unilink.com>
2023-04-24 17:31:50 +0100

    PI-987 Create ElastiCache and cert for hmpps-sentence-plan-ui
    Also updates the team name and sets the GitHubTeam tag for AWS Console access.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "probation-integration-team"
+  default     = "probation-integration"
 }
 
 variable "environment" {
