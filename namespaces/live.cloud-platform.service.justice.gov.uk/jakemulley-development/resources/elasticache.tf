dd3c73d4c97ad7f744fdcf36fb9b607f68e1be4d
jakemulley <jakemulley@users.noreply.github.com>
2023-04-24 17:19:53 +0100

    jakemulley-development: add ElastiCache cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/elasticache.tf
@@ -0,0 +1,36 @@
+module "redis_7" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=6.1.0"
+
+  # VPC configuration
+  vpc_name = var.vpc_name
+
+  # Redis configuration
+  engine_version       = "7.0"
+  parameter_group_name = "default.redis7"
+  node_type            = "cache.t4g.micro"
+
+  # Tags
+  team_name              = var.team_name
+  namespace              = var.namespace
+  business-unit          = var.business-unit
+  application            = var.application
+  is-production          = var.is-production
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure-support
+}
+
+resource "kubernetes_secret" "example_team_ec_cluster" {
+  metadata {
+    name      = "${var.team_name}-ec-cluster-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.example_team_ec_cluster.primary_endpoint_address
+    member_clusters          = jsonencode(module.example_team_ec_cluster.member_clusters)
+    auth_token               = module.example_team_ec_cluster.auth_token
+    access_key_id            = module.example_team_ec_cluster.access_key_id
+    secret_access_key        = module.example_team_ec_cluster.secret_access_key
+    replication_group_id     = module.example_team_ec_cluster.replication_group_id
+  }
+}
