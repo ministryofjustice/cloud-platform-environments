98bd7753fc483d96177aa1280ac85b77f49a0fee
Tom Hipkin <tomhipkin@gmail.com>
2023-04-24 16:43:59 +0100

    Fix CAS preprod deployment by using compatible elasticache node
    We'll move to t4g.micro later as a second operation.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-preprod/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-preprod/resources/elasticache.tf
@@ -12,7 +12,7 @@ module "elasticache_redis" {
   team_name              = var.team_name
   business-unit          = var.business_unit
   number_cache_clusters  = var.number_cache_clusters
-  node_type              = "cache.t4g.micro"
+  node_type              = "cache.t3.micro"
   engine_version         = "7.0"
   parameter_group_name   = "default.redis7"
   namespace              = var.namespace
