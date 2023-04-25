e9d485a3edaaaf23e3c50283671c04454490c503
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-04-25 13:56:35 +0100

    Magistrates Domain Records

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/magistrates-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/magistrates-route53.tf
@@ -21,3 +21,27 @@ resource "kubernetes_secret" "magistrates_route53_zone_sec" {
     zone_id = aws_route53_zone.magistrates_route53_zone.zone_id
   }
 }
+
+resource "aws_route53_record" "magistrates_route53_a_record" {
+  zone_id = aws_route53_zone.magistrates_route53_zone.zone_id
+  name    = "magistrates.judiciary.uk"
+  type    = "A"
+  ttl     = "300"
+  records = ["dualstack.jotwp-loadb-1mbwraz503eq6-1769122100.eu-west-2.elb.amazonaws.com."]
+}
+
+resource "aws_route53_record" "magistrates_route53_apply_a_record" {
+  zone_id = aws_route53_zone.magistrates_route53_zone.zone_id
+  name    = "apply.magistrates.judiciary.uk"
+  type    = "A"
+  ttl     = "300"
+  records = ["18.130.183.123"]
+}
+
+resource "aws_route53_record" "magistrates_route53_cname_record" {
+  zone_id = aws_route53_zone.magistrates_route53_zone.zone_id
+  name    = "_6f0e92da46509e479f24cc2bad43c06f.magistrates.judiciary.uk"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["_3ba2c01f2ad818f8e254df3786b6222e.zdxcnfdgtt.acm-validations.aws."]
+}
