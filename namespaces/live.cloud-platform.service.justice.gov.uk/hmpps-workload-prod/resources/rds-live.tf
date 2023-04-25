1c188f358602b82f6348355d9a9cb6d12517512b
AndySweilam <AndySweilam@users.noreply.github.com>
2023-04-25 16:53:36 +0100

    WFP-1861: upgrade to 11.18

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-prod/resources/rds-live.tf
@@ -13,10 +13,10 @@ module "rds-live" {
   # enable performance insights
   performance_insights_enabled = true
   # db instance class
-  db_instance_class = "db.t3.large"
+  db_instance_class = "db.t3.medium"
 
   # change the postgres version as you see fit.
-  db_engine_version      = "11"
+  db_engine_version      = "11.18"
   environment-name       = var.environment
   infrastructure-support = var.infrastructure_support
 
