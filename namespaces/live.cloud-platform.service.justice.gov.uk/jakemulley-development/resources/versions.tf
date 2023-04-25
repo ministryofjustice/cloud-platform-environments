8737d076eab8debb2b71306bc3278e98720dcfec
jakemulley <jakemulley@users.noreply.github.com>
2023-04-25 19:40:48 +0100

    Bump Terraform providers for hmpps-prisoner-education-laa-crown-court-contribution-staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/versions.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.59.0" # needed for elasticache_iam_user
+      version = "~> 4.64.0"
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.17.0"
+      version = "~> 5.23.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.18.0"
+      version = "~> 2.20.0"
     }
   }
 }
