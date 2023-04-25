824a32bac90e3103ff1c67e0ff414ec20b882ca9
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-04-25 15:17:45 +0100

    Upgrade to postgres v14 before upgrading to v15

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/rds.tf
@@ -15,14 +15,14 @@ module "rds" {
 
   # change the postgres version as you see fit.
   db_engine         = "postgres"
-  db_engine_version = "15"
+  db_engine_version = "14"
 
   # change the instance class as you see fit.
   db_instance_class        = "db.t4g.micro"
   db_max_allocated_storage = "500"
 
   # Pick the one that defines the postgres version the best
-  rds_family = "postgres15"
+  rds_family = "postgres14"
 
   # use "prepare_for_major_upgrade" when upgrading the major version of an engine
   prepare_for_major_upgrade = true
