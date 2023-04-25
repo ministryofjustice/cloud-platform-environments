32fd230ea373ad737e251353dbc0d87fd6d29a4d
Jesus Laiz <zheileman@users.noreply.github.com>
2023-04-25 13:43:57 +0100

    Add api secrets generation
    Both consumer namespaces have now been created and can receive the secret.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/api-auth-secrets.tf
@@ -0,0 +1,53 @@
+locals {
+  # Declare any consumers of the API here. If any existing secrets
+  # had to be rotated, change its `rotated_at` timestamp.
+  datastore_api_consumers = {
+    "crime_apply" = {
+      namespace  = "laa-apply-for-criminal-legal-aid-production"
+      rotated_at = "2023-04-25"
+    }
+    "crime_review" = {
+      namespace  = "laa-review-criminal-legal-aid-production"
+      rotated_at = "2023-04-25"
+    }
+  }
+}
+
+# No need to touch anything below this line. The above map
+# should declare all consumers and namespaces. Code below
+# will dynamically use the keys and values accordingly.
+
+resource "random_password" "passwords" {
+  for_each = local.datastore_api_consumers
+
+  length  = 32
+  special = true
+
+  keepers = {
+    last_changed = each.value.rotated_at
+  }
+}
+
+resource "kubernetes_secret" "api-auth-secrets" {
+  metadata {
+    name      = "api-auth-secrets"
+    namespace = var.namespace
+  }
+
+  data = {
+    for key in keys(local.datastore_api_consumers) : key => random_password.passwords[key].result
+  }
+}
+
+resource "kubernetes_secret" "injected-api-auth-secret" {
+  for_each = local.datastore_api_consumers
+
+  metadata {
+    name      = "datastore-api-auth-secret"
+    namespace = each.value.namespace
+  }
+
+  data = {
+    secret = random_password.passwords[each.key].result
+  }
+}
--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
@@ -13,10 +13,10 @@ terraform {
 #      source  = "DrFaust92/pingdom"
 #      version = "~> 1.3.1"
 #    }
-#    random = {
-#      source  = "hashicorp/random"
-#      version = "~> 3.4.3"
-#    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.18.0"
