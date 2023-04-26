64395604d0d81d52d3e65dc7ce03a77a2407e57c
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-04-26 14:58:34 +0100

    [laa-assure-hmrc-data-production] Add pingdom checks

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/versions.tf
@@ -13,5 +13,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.18.0"
     }
+    pingdom = {
+      source  = "DrFaust92/pingdom"
+      version = "~> 1.3.1"
+    }
   }
 }
