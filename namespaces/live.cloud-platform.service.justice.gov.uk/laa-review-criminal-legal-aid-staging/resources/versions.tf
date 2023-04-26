28690f0188e67e111e590e6ea910bdaab13d4c1d
Jesus Laiz <zheileman@users.noreply.github.com>
2023-04-26 10:14:09 +0100

    Add crime review pingdom check

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/versions.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.17.0"
     }
+    pingdom = {
+      source  = "DrFaust92/pingdom"
+      version = "~> 1.3.1"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.18.0"
