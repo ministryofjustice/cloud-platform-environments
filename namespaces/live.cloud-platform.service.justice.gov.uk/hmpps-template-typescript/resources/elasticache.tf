5e270f36837890b9995c0feed65d2a351e17268f
Peter Phillips <peter@greenthistle.co.uk>
2023-04-27 13:44:53 +0100

    SDIT-760: 🔧 Upgrade to redis 7 for hmpps-template-typescript

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-template-typescript/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-template-typescript/resources/elasticache.tf
@@ -14,9 +14,10 @@ module "elasticache_redis" {
   infrastructure-support = var.infrastructure_support
 
   number_cache_clusters = var.number_cache_clusters
-  node_type             = "cache.t4g.small"
-  engine_version        = "6.x"
-  parameter_group_name  = "default.redis6.x"
+  # sized for micro in dev, preprod, suggest small for production
+  node_type             = "cache.t4g.micro"
+  engine_version        = "7.0"
+  parameter_group_name  = "default.redis7"
 
   providers = {
     aws = aws.london
