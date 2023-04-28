41fe104a320b1809fbabdd7f89a1da80e0148ce8
Jesus Laiz <zheileman@users.noreply.github.com>
2023-04-28 10:59:26 +0100

    Add route53 to crime review

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-production/resources/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "route53_zone" {
+  name = "review-criminal-legal-aid.service.justice.gov.uk"
+
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.github_owner
+    infrastructure_support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "route53_zone_sec" {
+  metadata {
+    name      = "route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.route53_zone.zone_id
+  }
+}
