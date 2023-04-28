e526834488f57bc9e4636431f19bfec06c22b05e
carlov20 <carlov20@users.noreply.github.com>
2023-04-28 08:58:25 +0100

    WFP-1909 removed unused secrets and queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/hmpps-tier-offender-events-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/hmpps-tier-offender-events-sub-queue.tf
@@ -99,35 +99,6 @@ resource "kubernetes_secret" "hmpps_tier_offender_events_dead_letter_queue" {
   }
 }
 
-resource "kubernetes_secret" "hmpps_tier_sqs_tool_main_queue" {
-  metadata {
-    name      = "hmpps-tier-sqs-tool-main-queue"
-    namespace = "hmpps-tier-dev"
-  }
-
-  data = {
-    access_key_id     = module.hmpps_tier_offender_events_queue.access_key_id
-    secret_access_key = module.hmpps_tier_offender_events_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_tier_offender_events_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_tier_offender_events_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_tier_offender_events_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "hmpps_tier_sqs_tool_dead_letter_queue" {
-  metadata {
-    name      = "hmpps-tier-sqs-tool-dead-letter-queue"
-    namespace = "hmpps-tier-dev"
-  }
-  data = {
-    access_key_id     = module.hmpps_tier_offender_events_dead_letter_queue.access_key_id
-    secret_access_key = module.hmpps_tier_offender_events_dead_letter_queue.secret_access_key
-    sqs_queue_url     = module.hmpps_tier_offender_events_dead_letter_queue.sqs_id
-    sqs_queue_arn     = module.hmpps_tier_offender_events_dead_letter_queue.sqs_arn
-    sqs_queue_name    = module.hmpps_tier_offender_events_dead_letter_queue.sqs_name
-  }
-}
-
 resource "aws_sns_topic_subscription" "hmpps_tier_offender_events_subscription" {
   provider      = aws.london
   topic_arn     = module.probation_offender_events.topic_arn
