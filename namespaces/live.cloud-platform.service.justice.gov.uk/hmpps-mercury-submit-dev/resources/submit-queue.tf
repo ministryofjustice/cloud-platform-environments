1b4136653a51cb70bf2dac5dad19d108db109282
brian.carr <brian.carr@digital.justice.gov.uk>
2023-05-02 15:43:42 +0100

    added .fifo to sqs_name and switched off kms encryption

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mercury-submit-dev/resources/submit-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mercury-submit-dev/resources/submit-queue.tf
@@ -5,8 +5,8 @@ module "mercury_submit_queue" {
   team_name                  = var.team_name
   infrastructure-support     = var.infrastructure_support
   application                = var.application
-  sqs_name                   = "mercury_submit_queue"
-  encrypt_sqs_kms            = "true"
+  sqs_name                   = "mercury_submit_queue.fifo"
+  encrypt_sqs_kms            = "false"
   fifo_queue                 = "true"
   message_retention_seconds  = 1209600
   visibility_timeout_seconds = 120
@@ -32,8 +32,8 @@ module "mercury_submit_dead_letter_queue" {
   team_name              = var.team_name
   infrastructure-support = var.infrastructure_support
   application            = var.application
-  sqs_name               = "mercury_submit_dl_queue"
-  encrypt_sqs_kms        = "true"
+  sqs_name               = "mercury_submit_dl_queue.fifo"
+  encrypt_sqs_kms        = "false"
   fifo_queue             = "true"
   namespace              = var.namespace
 
