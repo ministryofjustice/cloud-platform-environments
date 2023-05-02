e8e3fde0b951781c0340ef8426406308a264f6db
Jesus Laiz <zheileman@users.noreply.github.com>
2023-05-02 14:56:34 +0100

    Update crime apply staging pingdom

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-criminal-legal-aid-staging/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-criminal-legal-aid-staging/resources/pingdom.tf
@@ -3,11 +3,11 @@ provider "pingdom" {
 
 resource "pingdom_check" "laa-apply-for-criminal-legal-aid-healthcheck" {
   type                     = "http"
-  name                     = "laa-apply-for-criminal-legal-aid-healthcheck - staging - cloud-platform"
-  host                     = "laa-apply-for-criminal-legal-aid-staging.apps.cloud-platform.service.justice.gov.uk"
+  name                     = "LAA Apply for criminal legal aid - Staging"
+  host                     = "staging.apply-for-criminal-legal-aid.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
-  sendnotificationwhendown = 6
+  sendnotificationwhendown = 4
   notifyagainevery         = 0
   url                      = "/ping"
   encryption               = true
