86a5230f52a433efa85ce582cce186cafff882b9
Jesus Laiz <zheileman@users.noreply.github.com>
2023-05-02 15:01:51 +0100

    Remove datastore staging pingdom
    This service will not have a public ingress. Removing pingdom check.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/versions.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.23.0"
     }
-    #    pingdom = {
-    #      source  = "DrFaust92/pingdom"
-    #      version = "~> 1.3.1"
-    #    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5.1"
