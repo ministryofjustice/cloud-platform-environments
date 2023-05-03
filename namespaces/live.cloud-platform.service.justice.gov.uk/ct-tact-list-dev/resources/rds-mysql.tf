eff79e192e282c7f5c272cee8a167d32f4b57140
Francesca von Braun-Bates <francesca.von.braun.bates@digital.justice.gov.uk>
2023-05-03 18:32:18 +0100

    Turn off RDS overnight while in development.

--- namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/rds-mysql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/rds-mysql.tf
@@ -15,8 +15,10 @@ module "rds_mysql" {
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+  # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  enable_rds_auto_start_stop = true
+  # Uncomment to rotate your database password.
+  # db_password_rotated_date     = "2023-04-17" 
 
   # MySQL specifics
   db_engine         = "mysql"
