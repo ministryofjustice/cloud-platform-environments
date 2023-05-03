79ff797b50a060359adc3b5a4506bf01c0ac13b6
Francesca von Braun-Bates <francesca.von.braun.bates@digital.justice.gov.uk>
2023-05-03 14:58:47 +0100

    Update to latest version.

--- namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/rds-mysql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/rds-mysql.tf
@@ -5,7 +5,7 @@
  *
 */
 module "rds_mysql" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.17.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
 
   # VPC configuration
   vpc_name = var.vpc_name
