939fc6a69d03517ae70a1a49a419ae07dd86e12c
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-05-03 09:38:43 +0100

    Remove CCRC A Record

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/ccrc-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/ccrc-route53.tf
@@ -22,19 +22,6 @@ resource "kubernetes_secret" "ccrc_route53_zone_sec" {
   }
 }
 
-resource "aws_route53_record" "ccrc_route53_a_record" {
-  zone_id = aws_route53_zone.ccrc_route53_zone.zone_id
-  name    = "ccrc.gov.uk"
-  type    = "A"
-
-  alias {
-    name                   = "dualstack.jotwp-loadb-1mbwraz503eq6-1769122100.eu-west-2.elb.amazonaws.com."
-    zone_id                = "ZHURV8PSTC4K8"
-    evaluate_target_health = false
-  }
-
-}
-
 resource "aws_route53_record" "ccrc_route53_a_record_connect" {
   zone_id = aws_route53_zone.ccrc_route53_zone.zone_id
   name    = "connect.ccrc.gov.uk"
