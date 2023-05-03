da774eca69f1a73c38e710f78373dc1d78b776f8
Marcus Aspin <maspin@unilink.com>
2023-05-03 16:21:24 +0100

    Dedicated access keys for hmpps-domain-events-prod topic (#12785)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/prisoner-offender-search.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/prisoner-offender-search.tf
@@ -1,18 +1,3 @@
-resource "kubernetes_secret" "prisoner-offender-search" {
-  metadata {
-    name      = "hmpps-domain-events-topic"
-    namespace = "prisoner-offender-search-prod"
-  }
-
-  data = {
-    access_key_id     = module.hmpps-domain-events.access_key_id
-    secret_access_key = module.hmpps-domain-events.secret_access_key
-    topic_arn         = module.hmpps-domain-events.topic_arn
-  }
-}
-
-
-
 module "prisoner_offender_search_domain_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.10.1"
 
