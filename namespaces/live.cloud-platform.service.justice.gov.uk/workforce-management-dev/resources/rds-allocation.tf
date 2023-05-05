9812b8f081536d6233e5c4f02ab31a12ff4d91ff
carlov20 <carlov20@users.noreply.github.com>
2023-05-05 08:55:22 +0100

    clean up

--- namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-dev/resources/rds-allocation.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-dev/resources/rds-allocation.tf
@@ -23,8 +23,7 @@ module "rds-allocation" {
   # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
   # Pick the one that defines the postgres version the best
   rds_family = "postgres14"
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "true"
+
   enable_rds_auto_start_stop = true
 
   providers = {
