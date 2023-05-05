b12f12439ac1248b833a0a965d015e4683aee9c7
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-05-05 11:29:08 +0100

    Claim-criminal-injuries-dev increase maxReceieveCount to 3

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/claim-criminal-injuries-application-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/claim-criminal-injuries-application-queue.tf
@@ -13,7 +13,7 @@ module "claim-criminal-injuries-application-queue" {
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.claim-criminal-injuries-application-dlq.sqs_arn
-    maxReceiveCount     = 1
+    maxReceiveCount     = 3
   })
 
   # Set encrypt_sqs_kms = "true", to enable SSE for SQS using KMS key.
