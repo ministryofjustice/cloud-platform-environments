da4e78d8b2208728c850b165ee84e81fc5675078
carlov20 <carlov20@users.noreply.github.com>
2023-05-05 08:51:01 +0100

    WFP-1932 switch off allocations during night

--- namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-dev/resources/rds-allocation.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-dev/resources/rds-allocation.tf
@@ -25,6 +25,7 @@ module "rds-allocation" {
   rds_family = "postgres14"
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "true"
+  enable_rds_auto_start_stop = true
 
   providers = {
     # Can be either "aws.london" or "aws.ireland"
