ad844b426cc20bbf3857e0c6dbb6108149018067
jakemulley <jakemulley@users.noreply.github.com>
2023-05-08 12:19:23 +0100

    OIDC 4 use cases: original, GitHub, CircleCI, both

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
@@ -1,5 +1,39 @@
+# Original (before OIDC)
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=oidc-github"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.1.1"
+
+  team_name = var.team_name
+  repo_name = "oidc-test"
+
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+  # oidc_providers      = ["github"]
+}
+
+# GitHub
+module "ecr_github" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=main"
+
+  team_name = var.team_name
+  repo_name = "oidc-test"
+
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+  oidc_providers      = ["github"]
+}
+
+# CircleCI
+module "ecr_circleci" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=main"
+
+  team_name = var.team_name
+  repo_name = "oidc-test"
+
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+  oidc_providers      = ["github"]
+}
+
+# Both
+module "ecr_github_circleci" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=main"
 
   team_name = var.team_name
   repo_name = "oidc-test"
