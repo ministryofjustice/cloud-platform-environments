ec1c22544f654a807bd61d9bc93412a463552ec4
Marcus Aspin <maspin@unilink.com>
2023-05-09 14:27:37 +0100

    PI-988 Add OpenSearch URL to kubernetes secrets (prod) (#12879)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/versions.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.23.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
     pingdom = {
       source  = "DrFaust92/pingdom"
       version = "~> 1.3.1"
