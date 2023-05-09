2769cda03ce149037872dfa4ce9a3036d6d22f06
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-09 15:08:02 +0100

    Ensure service account is unique name

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/offender_events_ui-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/offender_events_ui-sub-queue.tf
@@ -125,6 +125,6 @@ module "offender-events-ui-irsa" {
 
   eks_cluster_name = var.eks_cluster_name
   namespace        = var.namespace
-  service_account  = "offender-events-ui"
+  service_account  = "{var.namespace}-offender-events-ui"
   role_policy_arns = [module.offender_events_ui_dead_letter_queue.aws_iam_policy_arn, module.offender_events_ui_queue.aws_iam_policy_arn]
 }
