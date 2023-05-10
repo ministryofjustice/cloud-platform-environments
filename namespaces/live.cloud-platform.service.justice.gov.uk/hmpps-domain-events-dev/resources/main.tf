7a868ab49d283ee9ee622273a3ea6bdd98ea9efb
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-10 16:44:32 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/main.tf
@@ -34,18 +34,6 @@ provider "github" {
   token = var.github_token
 }
 
-locals {
-  tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    owner                  = var.team_name
-    environment-name       = var.environment-name
-    infrastructure-support = var.infrastructure_support
-    namespace              = var.namespace
-  }
-}
-
 # Add outputs that need to be consumed in other namespaces here
 resource "aws_ssm_parameter" "tf-outputs" {
   type = "String"
@@ -54,5 +42,4 @@ resource "aws_ssm_parameter" "tf-outputs" {
     "rp_domain-events_sqs_irsa_policy_arn" : module.restricted_patients_queue_for_domain_events.irsa_policy_arn
     "rp_domain-events_dlq_sqs_irsa_policy_arn" : module.restricted_patients_queue_for_domain_events_dead_letter_queue.irsa_policy_arn
   })
-  tags = local.tags
 }
