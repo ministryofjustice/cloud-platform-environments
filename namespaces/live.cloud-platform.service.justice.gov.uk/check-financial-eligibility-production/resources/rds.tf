b5b3be0da5bfe1d6bc403914503373cc4d398bc4
Adam Goldstone <adam.goldstone@digital.justice.gov.uk>
2023-05-10 17:12:16 +0100

    set instance class to db.t3.large and max allocated storage to 500

--- namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/check-financial-eligibility-production/resources/rds.tf
@@ -7,20 +7,22 @@
 module "check-financial-eligibility-rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
 
-  vpc_name               = var.vpc_name
-  team_name              = "apply-for-legal-aid"
-  business-unit          = "laa"
-  application            = "check-financial-eligibility"
-  is-production          = "true"
-  namespace              = var.namespace
-  environment-name       = "production"
-  infrastructure-support = "apply-for-civil-legal-aid@digital.justice.gov.uk"
-  db_engine              = "postgres"
-  db_engine_version      = "11"
-  db_name                = "check_financial_eligibility_production"
-  db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
-  rds_family             = "postgres11"
-  deletion_protection    = true
+  vpc_name                  = var.vpc_name
+  team_name                 = "apply-for-legal-aid"
+  business-unit             = "laa"
+  application               = "check-financial-eligibility"
+  is-production             = "true"
+  namespace                 = var.namespace
+  environment-name          = "production"
+  infrastructure-support    = "apply-for-civil-legal-aid@digital.justice.gov.uk"
+  db_engine                 = "postgres"
+  db_engine_version         = "11"
+  db_name                   = "check_financial_eligibility_production"
+  db_parameter              = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
+  rds_family                = "postgres11"
+  deletion_protection       = true
+  db_instance_class         = "db.t3.large"
+  db_max_allocated_storage  = "500"
 
   providers = {
     aws = aws.london
