32c72d95c310911430313fd77844bf64a8093093
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-10 15:09:40 +0100

    Fix names of outputs, tidy tags code

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-restricted-patients-api-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/hmpps-restricted-patients-api-queue.tf
@@ -96,24 +96,14 @@ resource "kubernetes_secret" "restricted_patients_queue_for_domain_events_dead_l
   }
 }
 
-resource "aws_ssm_parameter" "hmpps-restricted-patients-sqs" {
+resource "aws_ssm_parameter" "hmpps-restricted-patients-domain-events-sqs" {
   type = "String"
-  name = "/${var.namespace}/hmpps-restricted-patients-sqs"
+  name = "/${var.namespace}/hmpps-restricted-patients-domain-events-sqs"
   value = jsonencode({
     "irsa_policy_arn" : module.restricted_patients_queue_for_domain_events.irsa_policy_arn
     "irsa_policy_arn_dql" : module.restricted_patients_queue_for_domain_events_dead_letter_queue.irsa_policy_arn
   })
-  description = "Output from hmpps-restricted-patients sqs modules; use these parameters in other DPS dev namespaces"
-
-  tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    owner                  = var.team_name
-    environment-name       = var.environment-name
-    infrastructure-support = var.infrastructure_support
-    namespace              = var.namespace
-  }
+  tags = local.tags
 }
 
 resource "aws_sns_topic_subscription" "restricted_patients_queue_for_domain_events_subscription_details" {
