c97003d27ae4222b0f54417199570b38f113e199
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-10 18:02:38 +0100

    SNS module outputs to ssm param store

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/main.tf
@@ -53,6 +53,8 @@ resource "aws_ssm_parameter" "tf-outputs" {
   value = jsonencode({
     "rp_domain-events_sqs_irsa_policy_arn" : module.restricted_patients_queue_for_domain_events.irsa_policy_arn
     "rp_domain-events_dlq_sqs_irsa_policy_arn" : module.restricted_patients_queue_for_domain_events_dead_letter_queue.irsa_policy_arn
+    "hmpps_domain_events_topic_arn" : module.hmpps-domain-events.topic_arn
+    "hmpps_domain_events_irsa_policy_arn" : module.hmpps-domain-events.irsa_policy_arn
   })
   tags = local.tags
 }
