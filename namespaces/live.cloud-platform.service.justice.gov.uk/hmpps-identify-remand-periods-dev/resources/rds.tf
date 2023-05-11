38006d30a4897d1e8f6558e6b26e76bac49f8bc7
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-05-11 14:19:15 +0100

    MDWFP-119 Identify remand periods namespace dev.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/rds.tf
@@ -0,0 +1,36 @@
+module "idenfify_remand_periods_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business-unit               = var.business_unit
+  application                 = var.application
+  is-production               = var.is_production
+  namespace                   = var.namespace
+  environment-name            = var.environment_name
+  infrastructure-support      = var.infrastructure_support
+  rds_family                  = var.rds_family
+  allow_major_version_upgrade = "false"
+  db_instance_class           = "db.t3.small"
+  db_engine_version           = "14"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "idenfify_remand_periods_rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.idenfify_remand_periods_rds.rds_instance_endpoint
+    database_name         = module.idenfify_remand_periods_rds.database_name
+    database_username     = module.idenfify_remand_periods_rds.database_username
+    database_password     = module.idenfify_remand_periods_rds.database_password
+    rds_instance_address  = module.idenfify_remand_periods_rds.rds_instance_address
+    access_key_id         = module.idenfify_remand_periods_rds.access_key_id
+    secret_access_key     = module.idenfify_remand_periods_rds.secret_access_key
+  }
+}
