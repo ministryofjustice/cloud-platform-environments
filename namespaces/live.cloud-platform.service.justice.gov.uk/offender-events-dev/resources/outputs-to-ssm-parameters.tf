3a181d021f771352cefb1e6c866790557a9602f2
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-12 11:20:27 +0100

    fix Ambiguous attribute key

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/outputs-to-ssm-parameters.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/outputs-to-ssm-parameters.tf
@@ -15,52 +15,52 @@ resource "aws_ssm_parameter" "tf-outputs-sqs-irsa-policies" {
   type = "String"
   name = "/${var.namespace}/tf-outputs/sqs-irsa-policies"
   value = jsonencode({
-    module.cfo_queue.sqs_name : module.cfo_queue.irsa_policy_arn
-    module.cfo_dead_letter_queue.sqs_name : module.cfo_dead_letter_queue.irsa_policy_arn
-    module.cvl_prison_events_queue.sqs_name : module.cvl_prison_events_queue.irsa_policy_arn
-    module.cvl_probation_events_queue.sqs_name : module.cvl_probation_events_queue.irsa_policy_arn
-    module.cvl_prison_events_dead_letter_queue.sqs_name : module.cvl_prison_events_dead_letter_queue.irsa_policy_arn
-    module.cvl_probation_events_dead_letter_queue.sqs_name : module.cvl_probation_events_dead_letter_queue.irsa_policy_arn
-    module.hmpps_allocations_offender_events_queue.sqs_name : module.hmpps_allocations_offender_events_queue.irsa_policy_arn
-    module.hmpps_allocations_offender_events_dead_letter_queue.sqs_name : module.hmpps_allocations_offender_events_dead_letter_queue.irsa_policy_arn
-    module.prisoner_from_nomis_sentencing_queue.sqs_name : module.prisoner_from_nomis_sentencing_queue.irsa_policy_arn
-    module.prisoner_from_nomis_sentencing_dead_letter_queue.sqs_name : module.prisoner_from_nomis_sentencing_dead_letter_queue.irsa_policy_arn
-    module.prisoner_from_nomis_visits_queue.sqs_name : module.prisoner_from_nomis_visits_queue.irsa_policy_arn
-    module.prisoner_from_nomis_visits_dead_letter_queue.sqs_name : module.prisoner_from_nomis_visits_dead_letter_queue.irsa_policy_arn
-    module.restricted_patients_queue.sqs_name : module.restricted_patients_queue.irsa_policy_arn
-    module.restricted_patients_dead_letter_queue.sqs_name : module.restricted_patients_dead_letter_queue.irsa_policy_arn
-    module.hmpps_tier_offender_events_queue.sqs_name : module.hmpps_tier_offender_events_queue.irsa_policy_arn
-    module.hmpps_tier_offender_events_dead_letter_queue.sqs_name : module.hmpps_tier_offender_events_dead_letter_queue.irsa_policy_arn
-    module.hmpps_workload_offender_events_queue.sqs_name : module.hmpps_workload_offender_events_queue.irsa_policy_arn
-    module.hmpps_workload_offender_events_dead_letter_queue.sqs_name : module.hmpps_workload_offender_events_dead_letter_queue.irsa_policy_arn
-    module.keyworker_api_queue.sqs_name : module.keyworker_api_queue.irsa_policy_arn
-    module.keyworker_api_dead_letter_queue.sqs_name : module.keyworker_api_dead_letter_queue.irsa_policy_arn
-    module.manage_soc_cases_offender_events_queue.sqs_name : module.manage_soc_cases_offender_events_queue.irsa_policy_arn
-    module.manage_soc_cases_probation_offender_events_queue.sqs_name : module.manage_soc_cases_probation_offender_events_queue.irsa_policy_arn
-    module.manage_soc_cases_offender_events_dead_letter_queue.sqs_name : module.manage_soc_cases_offender_events_dead_letter_queue.irsa_policy_arn
-    module.manage_soc_cases_probation_offender_events_dead_letter_queue.sqs_name : module.manage_soc_cases_probation_offender_events_dead_letter_queue.irsa_policy_arn
-    module.offender_case_notes_events_queue.sqs_name : module.offender_case_notes_events_queue.irsa_policy_arn
-    module.offender_case_notes_events_dead_letter_queue.sqs_name : module.offender_case_notes_events_dead_letter_queue.irsa_policy_arn
-    module.offender_categorisation_events_queue.sqs_name : module.offender_categorisation_events_queue.irsa_policy_arn
-    module.offender_categorisation_events_dead_letter_queue.sqs_name : module.offender_categorisation_events_dead_letter_queue.irsa_policy_arn
-    module.offender_categorisation_ui_events_queue.sqs_name : module.offender_categorisation_ui_events_queue.irsa_policy_arn
-    module.offender_categorisation_ui_events_dead_letter_queue.sqs_name : module.offender_categorisation_ui_events_dead_letter_queue.irsa_policy_arn
-    module.offender_events_ui_queue.sqs_name : module.offender_events_ui_queue.irsa_policy_arn
-    module.offender_events_ui_dead_letter_queue.sqs_name : module.offender_events_ui_dead_letter_queue.irsa_policy_arn
-    module.pathfinder_offender_events_queue.sqs_name : module.pathfinder_offender_events_queue.irsa_policy_arn
-    module.pathfinder_probation_offender_events_queue.sqs_name : module.pathfinder_probation_offender_events_queue.irsa_policy_arn
-    module.pathfinder_offender_events_dead_letter_queue.sqs_name : module.pathfinder_offender_events_dead_letter_queue.irsa_policy_arn
-    module.pathfinder_probation_offender_events_dead_letter_queue.sqs_name : module.pathfinder_probation_offender_events_dead_letter_queue.irsa_policy_arn
-    module.pic_probation_offender_events_queue.sqs_name : module.pic_probation_offender_events_queue.irsa_policy_arn
-    module.pic_probation_offender_events_dead_letter_queue.sqs_name : module.pic_probation_offender_events_dead_letter_queue.irsa_policy_arn
-    module.prison_to_probation_update_queue.sqs_name : module.prison_to_probation_update_queue.irsa_policy_arn
-    module.prison_to_probation_update_dead_letter_queue.sqs_name : module.prison_to_probation_update_dead_letter_queue.irsa_policy_arn
-    module.prisoner_offender_events_queue.sqs_name : module.prisoner_offender_events_queue.irsa_policy_arn
-    module.prisoner_offender_events_dead_letter_queue.sqs_name : module.prisoner_offender_events_dead_letter_queue.irsa_policy_arn
-    module.prisoner_offender_search_queue.sqs_name : module.prisoner_offender_search_queue.irsa_policy_arn
-    module.prisoner_offender_search_dead_letter_queue.sqs_name : module.prisoner_offender_search_dead_letter_queue.irsa_policy_arn
-    module.whereabouts_api_queue.sqs_name : module.whereabouts_api_queue.irsa_policy_arn
-    module.whereabouts_api_dead_letter_queue.sqs_name : module.whereabouts_api_dead_letter_queue.irsa_policy_arn
+    (module.cfo_queue.sqs_name) : module.cfo_queue.irsa_policy_arn
+    (module.cfo_dead_letter_queue.sqs_name) : module.cfo_dead_letter_queue.irsa_policy_arn
+    (module.cvl_prison_events_queue.sqs_name) : module.cvl_prison_events_queue.irsa_policy_arn
+    (module.cvl_probation_events_queue.sqs_name) : module.cvl_probation_events_queue.irsa_policy_arn
+    (module.cvl_prison_events_dead_letter_queue.sqs_name) : module.cvl_prison_events_dead_letter_queue.irsa_policy_arn
+    (module.cvl_probation_events_dead_letter_queue.sqs_name) : module.cvl_probation_events_dead_letter_queue.irsa_policy_arn
+    (module.hmpps_allocations_offender_events_queue.sqs_name) : module.hmpps_allocations_offender_events_queue.irsa_policy_arn
+    (module.hmpps_allocations_offender_events_dead_letter_queue.sqs_name) : module.hmpps_allocations_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.prisoner_from_nomis_sentencing_queue.sqs_name) : module.prisoner_from_nomis_sentencing_queue.irsa_policy_arn
+    (module.prisoner_from_nomis_sentencing_dead_letter_queue.sqs_name) : module.prisoner_from_nomis_sentencing_dead_letter_queue.irsa_policy_arn
+    (module.prisoner_from_nomis_visits_queue.sqs_name) : module.prisoner_from_nomis_visits_queue.irsa_policy_arn
+    (module.prisoner_from_nomis_visits_dead_letter_queue.sqs_name) : module.prisoner_from_nomis_visits_dead_letter_queue.irsa_policy_arn
+    (module.restricted_patients_queue.sqs_name) : module.restricted_patients_queue.irsa_policy_arn
+    (module.restricted_patients_dead_letter_queue.sqs_name) : module.restricted_patients_dead_letter_queue.irsa_policy_arn
+    (module.hmpps_tier_offender_events_queue.sqs_name) : module.hmpps_tier_offender_events_queue.irsa_policy_arn
+    (module.hmpps_tier_offender_events_dead_letter_queue.sqs_name) : module.hmpps_tier_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.hmpps_workload_offender_events_queue.sqs_name) : module.hmpps_workload_offender_events_queue.irsa_policy_arn
+    (module.hmpps_workload_offender_events_dead_letter_queue.sqs_name) : module.hmpps_workload_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.keyworker_api_queue.sqs_name) : module.keyworker_api_queue.irsa_policy_arn
+    (module.keyworker_api_dead_letter_queue.sqs_name) : module.keyworker_api_dead_letter_queue.irsa_policy_arn
+    (module.manage_soc_cases_offender_events_queue.sqs_name) : module.manage_soc_cases_offender_events_queue.irsa_policy_arn
+    (module.manage_soc_cases_probation_offender_events_queue.sqs_name) : module.manage_soc_cases_probation_offender_events_queue.irsa_policy_arn
+    (module.manage_soc_cases_offender_events_dead_letter_queue.sqs_name) : module.manage_soc_cases_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.manage_soc_cases_probation_offender_events_dead_letter_queue.sqs_name) : module.manage_soc_cases_probation_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.offender_case_notes_events_queue.sqs_name) : module.offender_case_notes_events_queue.irsa_policy_arn
+    (module.offender_case_notes_events_dead_letter_queue.sqs_name) : module.offender_case_notes_events_dead_letter_queue.irsa_policy_arn
+    (module.offender_categorisation_events_queue.sqs_name) : module.offender_categorisation_events_queue.irsa_policy_arn
+    (module.offender_categorisation_events_dead_letter_queue.sqs_name) : module.offender_categorisation_events_dead_letter_queue.irsa_policy_arn
+    (module.offender_categorisation_ui_events_queue.sqs_name) : module.offender_categorisation_ui_events_queue.irsa_policy_arn
+    (module.offender_categorisation_ui_events_dead_letter_queue.sqs_name) : module.offender_categorisation_ui_events_dead_letter_queue.irsa_policy_arn
+    (module.offender_events_ui_queue.sqs_name) : module.offender_events_ui_queue.irsa_policy_arn
+    (module.offender_events_ui_dead_letter_queue.sqs_name) : module.offender_events_ui_dead_letter_queue.irsa_policy_arn
+    (module.pathfinder_offender_events_queue.sqs_name) : module.pathfinder_offender_events_queue.irsa_policy_arn
+    (module.pathfinder_probation_offender_events_queue.sqs_name) : module.pathfinder_probation_offender_events_queue.irsa_policy_arn
+    (module.pathfinder_offender_events_dead_letter_queue.sqs_name) : module.pathfinder_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.pathfinder_probation_offender_events_dead_letter_queue.sqs_name) : module.pathfinder_probation_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.pic_probation_offender_events_queue.sqs_name) : module.pic_probation_offender_events_queue.irsa_policy_arn
+    (module.pic_probation_offender_events_dead_letter_queue.sqs_name) : module.pic_probation_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.prison_to_probation_update_queue.sqs_name) : module.prison_to_probation_update_queue.irsa_policy_arn
+    (module.prison_to_probation_update_dead_letter_queue.sqs_name) : module.prison_to_probation_update_dead_letter_queue.irsa_policy_arn
+    (module.prisoner_offender_events_queue.sqs_name) : module.prisoner_offender_events_queue.irsa_policy_arn
+    (module.prisoner_offender_events_dead_letter_queue.sqs_name) : module.prisoner_offender_events_dead_letter_queue.irsa_policy_arn
+    (module.prisoner_offender_search_queue.sqs_name) : module.prisoner_offender_search_queue.irsa_policy_arn
+    (module.prisoner_offender_search_dead_letter_queue.sqs_name) : module.prisoner_offender_search_dead_letter_queue.irsa_policy_arn
+    (module.whereabouts_api_queue.sqs_name) : module.whereabouts_api_queue.irsa_policy_arn
+    (module.whereabouts_api_dead_letter_queue.sqs_name) : module.whereabouts_api_dead_letter_queue.irsa_policy_arn
   })
   tags = local.tags
 }
@@ -69,9 +69,9 @@ resource "aws_ssm_parameter" "tf-outputs-sns-irsa-policies" {
   type = "String"
   name = "/${var.namespace}/tf-outputs/sns-irsa-policies"
   value = jsonencode({
-    module.offender_events.topic_display_name : module.offender_events.irsa_policy_arn
-    module.probation_offender_events.topic_display_name : module.probation_offender_events.irsa_policy_arn
-    module.offender_assessments_events.topic_display_name : module.probation_offender_events.irsa_policy_arn
+    (module.offender_events.topic_display_name) : module.offender_events.irsa_policy_arn
+    (module.probation_offender_events.topic_display_name) : module.probation_offender_events.irsa_policy_arn
+    (module.offender_assessments_events.topic_display_name) : module.probation_offender_events.irsa_policy_arn
   })
   tags = local.tags
 }
