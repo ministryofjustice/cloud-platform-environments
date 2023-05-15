097ede3bcdec7448bc4aef098bec439bf5566752
jack-burt-is <jack.burt@informed.com>
2023-05-15 14:12:36 +0100

    Update to SecretString and change name to include namespeace

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ssm.tf
@@ -1,7 +1,7 @@
 # The value of this parameter is to be replaced manually, not wanting to put external acct IDs in public repository
 resource "aws_ssm_parameter" "cica_dev_account_id" {
-  name      = "cica_dev_account_id"
-  type      = "String"
+  name      = "/claim-criminal-injuries-compensation-dev/cica-dev-account-id"
+  type      = "SecureString"
   value     = "0123456789"
   overwrite = false
 
