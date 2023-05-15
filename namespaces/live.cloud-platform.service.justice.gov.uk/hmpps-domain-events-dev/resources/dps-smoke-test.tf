b568f6e1912ea4103e1e40f0e4c8a3e882e4c272
Matt <34448412+mattops@users.noreply.github.com>
2023-05-15 13:19:28 +0100

    Update all SQS/SNS modules to latest and output irsa policy arns to ssm parameters. (#13055)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/dps-smoke-test.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/dps-smoke-test.tf
@@ -1,6 +1,6 @@
 
 module "dps_smoketest_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.10.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.11.0"
 
   environment-name          = var.environment-name
   team_name                 = var.team_name
