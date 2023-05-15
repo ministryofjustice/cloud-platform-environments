70ca9aa21ac36f845befc893a6d1ab696b880880
Michael Clancy <92720613+mtac50@users.noreply.github.com>
2023-05-15 13:12:47 +0100

    LASB-1778: Updated infrastructure support email for CDA TEST. (#13048)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-test/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-test/resources/rds.tf
@@ -16,7 +16,7 @@ module "laa_crime_apps_team_rds" {
   # change the postgres version as you see fit.
   db_engine_version      = "11"
   environment-name       = "test"
-  infrastructure-support = "laa@digital.justice.gov.uk"
+  infrastructure-support = var.infrastructure_support
 
   # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
   # Pick the one that defines the postgres version the best
