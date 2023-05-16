1c9dc3ef438fb59925437c706b60020accd22471
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-05-16 16:18:11 +0100

    Rename preproduction to preprod
    This is to conform to hmpps naming conventions, whereby contracted forms
    (i.e., 'dev' rather than 'development') are preferred over their
    full counterparts.
    
    To rename our environments, we are first creating the replacement
    namespace before deleting the old.
    
    This change will take place over two steps. This is because the
    truststore public and private keys that comprise part of our API
    Gateway authentication process are stored as Kubernetes secrets. We must
    create and store these certificates before we are able to set up
    API Gateway in our namespace.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/kubernetes_secrets.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_secret" "aws_services" {
+  metadata {
+    name      = "aws-services"
+    namespace = var.namespace
+  }
+
+  data = {
+
+    "ecr" = jsonencode({
+      "access-credentials" = {
+        "access-key-id"     = module.ecr_credentials.access_key_id
+        "secret-access-key" = module.ecr_credentials.secret_access_key
+      }
+      "repo-arn"         = module.ecr_credentials.repo_arn
+      "repo-url"          = module.ecr_credentials.repo_url
+    })
+
+    "s3" = jsonencode({
+      "access-credentials" = {
+        "access-key-id"     = module.truststore_s3_bucket.access_key_id
+        "secret-access-key" = module.truststore_s3_bucket.secret_access_key
+      }
+      "bucket-arn"        = module.truststore_s3_bucket.bucket_arn
+      "bucket-name"       = module.truststore_s3_bucket.bucket_name
+    })
+  }
+}
+
+
