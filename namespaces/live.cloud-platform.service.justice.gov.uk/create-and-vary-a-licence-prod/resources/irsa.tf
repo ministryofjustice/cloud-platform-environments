94bcfa6f5e1300596874d2bff0334f583d12a8f9
Matt <34448412+mattops@users.noreply.github.com>
2023-05-16 12:58:50 +0100

    Add IRSA to create-and-vary-a-licence-prod (#13126)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-prod/resources/irsa.tf
@@ -0,0 +1,26 @@
+
+# Add the names of the SQS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-prod-cvl_domain_events_queue"       = "hmpps-domain-events-prod",
+    "Digital-Prison-Services-prod-cvl_domain_events_queue_dl"    = "hmpps-domain-events-prod",
+    "Digital-Prison-Services-prod-cvl_probation_events_queue"    = "offender-events-prod",
+    "Digital-Prison-Services-prod-cvl_probation_events_queue_dl" = "offender-events-prod"
+  }
+}
+
+module "app-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+
+  eks_cluster_name = var.eks_cluster_name
+  namespace        = var.namespace
+  service_account  = "${var.application}-${var.environment}"
+  role_policy_arns = [for item in data.aws_ssm_parameter.irsa_policy_arns : item.value]
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
