57b321745a43bcee4f1fdec378e6e1ba84f78b4d
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-17 16:03:55 +0100

    prison-to-probation-update-dev: add irsa

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/irsa.tf
@@ -0,0 +1,25 @@
+
+# Add the names of the SQS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-prison_to_probation_update_queue"    = "offender-events-dev",
+    "Digital-Prison-Services-dev-prison_to_probation_update_queue_dl" = "offender-events-dev"
+  }
+  sqs_policies = [for item in data.aws_ssm_parameter.irsa_policy_arns : item.value]
+}
+
+module "app-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+
+  eks_cluster_name = var.eks_cluster_name
+  namespace        = var.namespace
+  service_account  = "${var.application}-${var.environment}"
+  role_policy_arns = concat(local.sqs_policies, [module.message_dynamodb.irsa_policy_arn])
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
