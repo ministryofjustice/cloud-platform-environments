dbeb6d1827a37bdca2748dd6ddcda553735739aa
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-05-17 08:54:31 +0100

    Adding new test env for create and vary a licence

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/rds.tf
@@ -0,0 +1,38 @@
+module "create_and_vary_a_licence_api_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business-unit               = var.business_unit
+  application                 = var.application
+  is-production               = var.is_production
+  namespace                   = var.namespace
+  environment-name            = var.environment
+  infrastructure-support      = var.infrastructure_support
+  allow_major_version_upgrade = "false"
+  db_instance_class           = "db.t4g.small"
+  db_engine_version           = "14"
+  rds_family                  = "postgres14"
+  db_password_rotated_date    = "14-02-2023"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "create_and_vary_a_licence_api_rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.create_and_vary_a_licence_api_rds.rds_instance_endpoint
+    database_name         = module.create_and_vary_a_licence_api_rds.database_name
+    database_username     = module.create_and_vary_a_licence_api_rds.database_username
+    database_password     = module.create_and_vary_a_licence_api_rds.database_password
+    rds_instance_address  = module.create_and_vary_a_licence_api_rds.rds_instance_address
+    access_key_id         = module.create_and_vary_a_licence_api_rds.access_key_id
+    secret_access_key     = module.create_and_vary_a_licence_api_rds.secret_access_key
+  }
+}
+
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/variables.tf
@@ -0,0 +1,51 @@
+
+
+variable "vpc_name" {
+}
+
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "create-and-vary-a-licence"
+}
+
+variable "namespace" {
+  default = "create-and-vary-a-licence-test1"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "create-and-vary-a-licence-devs"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "create-and-vary-a-licence"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
