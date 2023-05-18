ec5958bcfb09df3b01203d9559bb80b46bdfef47
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-18 09:37:57 +0100

    Upgrade hmcts-adapter-prod to postgres 14.4

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-production/resources/rds.tf
@@ -10,8 +10,8 @@ module "hmcts-complaints-adapter-rds-instance" {
   infrastructure-support     = var.infrastructure_support
   team_name                  = var.team_name
 
-  db_engine_version    = "11"
-  rds_family           = "postgres11"
+  db_engine_version    = "14.4"
+  rds_family           = "postgres14"
   db_instance_class    = "db.t3.medium"
   db_allocated_storage = "100"
 
