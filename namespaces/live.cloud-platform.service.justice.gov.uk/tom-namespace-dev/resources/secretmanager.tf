0a7bd53ac267b75d19a4a921a56c392c08bce826
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-05-18 12:43:51 +0100

    Use latest SM module

--- namespaces/live.cloud-platform.service.justice.gov.uk/tom-namespace-dev/resources/secretmanager.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tom-namespace-dev/resources/secretmanager.tf
@@ -1,5 +1,5 @@
 module "secrets_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=main"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=1.0.2"
   team_name               = var.team_name
   application             = var.application
   business_unit           = var.business_unit
