4fbd275231cb493e8e68b7e54d38ccb7b092496c
Ashley Gyngell <96007411+ashleygyngell@users.noreply.github.com>
2023-05-18 15:45:39 +0100

    NON-2: Added hmpps-non-associations-dev namespaces (#13209)
    * NON-2: Added hmpps-non-associations-dev namespaces

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-dev/resources/ap-data.tf
@@ -0,0 +1,60 @@
+module "analytical-platform" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+
+  namespace        = var.namespace
+  eks_cluster_name = var.eks_cluster_name
+  role_policy_arns = [aws_iam_policy.analytical-platform.arn]
+  service_account  = "${var.namespace}-analytical-platform"
+  # NB: service account name must be unique within Cloud Platform (IAM role name is derived from it)
+}
+
+resource "aws_iam_policy" "analytical-platform" {
+  name   = "${var.namespace}-analytical-platform"
+  policy = data.aws_iam_policy_document.analytical-platform.json
+  # NB: IAM policy name must be unique within Cloud Platform
+
+  tags = {
+    business-unit          = var.business_unit
+    team_name              = var.team_name
+    application            = var.application
+    is-production          = var.is_production
+    namespace              = var.namespace
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
+data "aws_iam_policy_document" "analytical-platform" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      module.analytical_platform_s3_bucket.bucket_arn
+    ]
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:ListObjectsV2",
+    ]
+    resources = [
+      "${module.analytical_platform_s3_bucket.bucket_arn}/*"
+    ]
+  }
+}
+
+resource "kubernetes_secret" "analytical-platform" {
+  metadata {
+    name      = "analytical-platform"
+    namespace = var.namespace
+  }
+
+  data = {
+    role            = module.analytical-platform.aws_iam_role_name
+    role_arn        = module.analytical-platform.aws_iam_role_arn
+    service_account = module.analytical-platform.service_account_name.name
+  }
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-dev/resources/variables.tf
@@ -0,0 +1,45 @@
+variable "kubernetes_cluster" {}
+
+variable "vpc_name" {}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Non Associations Apps"
+}
+
+variable "namespace" {
+  default = "hmpps-non-associations-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-non-associations"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "development"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "non-associations-dev"
+}
