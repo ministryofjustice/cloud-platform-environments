1b7d87cc6f0bfe955b8a9ef8070cd8572f5a2ed5
AprilMD <95350189+AprilMD@users.noreply.github.com>
2023-05-18 14:47:50 +0100

    Create prod environment for HMPPS Integration API
    This is because we are renaming our environments to align with HMPPS
    naming conventions (i.e., 'prod' rather than 'production'). To do this
    we are creating a new namespace and then deleting the old.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/s3_truststore.tf
@@ -0,0 +1,15 @@
+module "truststore_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+  versioning             = true
+
+  providers = {
+    aws = aws.london_without_default_tags
+  }
+}
