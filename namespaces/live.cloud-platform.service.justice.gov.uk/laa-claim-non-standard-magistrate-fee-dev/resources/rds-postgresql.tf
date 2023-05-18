5196cf61bdda0bac190931d35ca362e119e410ca
Sandeep Mittal <Sandeep.Mittal@digital.justice.gov.uk>
2023-05-18 15:13:44 +0100

    updated from 5.17.1 to 5.18.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev/resources/rds-postgresql.tf
@@ -97,6 +97,8 @@ resource "kubernetes_secret" "rds" {
     rds_instance_address  = module.rds.rds_instance_address
     access_key_id         = module.rds.access_key_id
     secret_access_key     = module.rds.secret_access_key
+    
+    url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
   }
   /* You can replace all of the above with the following, if you prefer to
      * use a single database URL value in your application code:
