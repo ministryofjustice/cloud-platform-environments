f0bf191f107c6c4dc856a1314cc5effbef15de6b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-05-19 10:47:05 +0100

    Add API Gateway to prod namespace for HMPPS Integration API (#13214)
    This is the final infrastructure step for recreating/renaming our
    "production" environment to "prod".

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/kubernetes_secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/kubernetes_secrets.tf
@@ -5,6 +5,14 @@ resource "kubernetes_secret" "aws_services" {
   }
 
   data = {
+    "api-gateway" = jsonencode({
+      "cloudwatch-log-url" = "https://eu-west-2.console.aws.amazon.com/cloudwatch/home?region=eu-west-2#logsV2:log-groups/log-group/${aws_cloudwatch_log_group.api_gateway_access_logs.name}"
+      "access-credentials" = {
+        "access-key-id"   = aws_iam_access_key.api_gateway_user.id
+        "secret-access-key" = aws_iam_access_key.api_gateway_user.secret
+      }
+    })
+
     "ecr" = jsonencode({
       "access-credentials" = {
         "access-key-id"     = module.ecr_credentials.access_key_id
@@ -23,7 +31,31 @@ resource "kubernetes_secret" "aws_services" {
       "bucket-name"       = module.truststore_s3_bucket.bucket_name
     })
   }
+
+  depends_on = [
+    aws_cloudwatch_log_group.api_gateway_access_logs,
+    aws_api_gateway_api_key.clients
+  ]
 }
 
+resource "kubernetes_secret" "client_certificate_auth" {
+  metadata {
+    name      = "client-certificate-auth"
+    namespace = var.namespace
+  }
 
+  data = {
+    "ca.crt" = aws_api_gateway_client_certificate.api_gateway_client.pem_encoded_certificate
+  }
+}
 
+resource "kubernetes_secret" "consumer_api_keys" {
+  metadata {
+    name      = "consumer-api-keys"
+    namespace = var.namespace
+  }
+
+  data = {
+    for client in local.clients : client => aws_api_gateway_api_key.clients[client].value
+  }
+}
