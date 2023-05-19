5da21ce26da3eb5d89a1f9ed7d3ab169499d84f2
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-05-19 14:31:03 +0100

    Add rds for hmpps-service-catalogue app

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/dps-toolkit/resources/rds.tf
@@ -0,0 +1,35 @@
+module "hmpps_service_catalogue" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business-unit               = var.business_unit
+  application                 = "hmpps-service-catalogue"
+  is-production               = var.is_production
+  namespace                   = var.namespace
+  environment-name            = var.environment-name
+  infrastructure-support      = var.infrastructure_support
+  allow_major_version_upgrade = "false"
+  db_instance_class           = "db.t4g.micro"
+  db_max_allocated_storage    = "500" # maximum storage for autoscaling
+  db_engine_version           = "15"
+  rds_family                  = "postgres15"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_service_catalogue" {
+  metadata {
+    name      = "dps-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_service_catalogue.rds_instance_endpoint
+    database_name         = module.hmpps_service_catalogue.database_name
+    database_username     = module.hmpps_service_catalogue.database_username
+    database_password     = module.hmpps_service_catalogue.database_password
+    rds_instance_address  = module.hmpps_service_catalogue.rds_instance_address
+  }
+}
