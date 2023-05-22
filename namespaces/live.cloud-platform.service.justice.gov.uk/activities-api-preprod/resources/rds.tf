f9fb8b9876ed4e9ff29a9d878fdbb8d28e1b7df6
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2023-05-22 18:18:38 +0100

    SAA-609: API preprod namespace and RDS instance for Activities

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/activities-api-preprod/resources/rds.tf
@@ -0,0 +1,38 @@
+module "activities_api_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business-unit               = var.business_unit
+  application                 = var.application
+  is-production               = var.is_production
+  namespace                   = var.namespace
+  environment-name            = var.environment
+  infrastructure-support      = var.infrastructure_support
+  rds_family                  = var.rds_family
+  allow_major_version_upgrade = "false"
+  db_instance_class           = "db.t3.small"
+  db_engine_version           = "14"
+
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "activities_api_rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.activities_api_rds.rds_instance_endpoint
+    database_name         = module.activities_api_rds.database_name
+    database_username     = module.activities_api_rds.database_username
+    database_password     = module.activities_api_rds.database_password
+    rds_instance_address  = module.activities_api_rds.rds_instance_address
+    access_key_id         = module.activities_api_rds.access_key_id
+    secret_access_key     = module.activities_api_rds.secret_access_key
+  }
+}
+
