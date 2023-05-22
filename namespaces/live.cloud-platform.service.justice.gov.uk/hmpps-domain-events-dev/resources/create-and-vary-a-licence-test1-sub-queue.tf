b1bf110773d579d677aad3acf32f9f3cba807a55
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-05-22 08:47:52 +0100

    Removing key and secret from cvl test env domain events queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/create-and-vary-a-licence-test1-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/create-and-vary-a-licence-test1-sub-queue.tf
@@ -79,8 +79,6 @@ resource "kubernetes_secret" "create_and_vary_a_licence_test1_domain_events_queu
   }
 
   data = {
-    access_key_id     = module.cvl_test1_domain_events_queue.access_key_id
-    secret_access_key = module.cvl_test1_domain_events_queue.secret_access_key
     sqs_id            = module.cvl_test1_domain_events_queue.sqs_id
     sqs_arn           = module.cvl_test1_domain_events_queue.sqs_arn
     sqs_name          = module.cvl_test1_domain_events_queue.sqs_name
@@ -94,8 +92,6 @@ resource "kubernetes_secret" "create_and_vary_a_licence_test1_domain_events_dead
   }
 
   data = {
-    access_key_id     = module.cvl_test1_domain_events_dead_letter_queue.access_key_id
-    secret_access_key = module.cvl_test1_domain_events_dead_letter_queue.secret_access_key
     sqs_id            = module.cvl_test1_domain_events_dead_letter_queue.sqs_id
     sqs_arn           = module.cvl_test1_domain_events_dead_letter_queue.sqs_arn
     sqs_name          = module.cvl_test1_domain_events_dead_letter_queue.sqs_name
