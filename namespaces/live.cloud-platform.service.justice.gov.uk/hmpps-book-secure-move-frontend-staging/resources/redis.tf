ed3539a7e1dfba23eb6b8702470f52d39594a60a
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-05-22 10:31:54 +0100

    P4-4173 add access key values to redis secret BaSM Staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-staging/resources/redis.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-staging/resources/redis.tf
@@ -31,5 +31,7 @@ resource "kubernetes_secret" "redis-elasticache" {
   data = {
     primary_endpoint_address = module.redis-elasticache.primary_endpoint_address
     auth_token               = module.redis-elasticache.auth_token
+    access_key_id            = module.example_team_ec_cluster.access_key_id
+    secret_access_key        = module.example_team_ec_cluster.secret_access_key
   }
 }
