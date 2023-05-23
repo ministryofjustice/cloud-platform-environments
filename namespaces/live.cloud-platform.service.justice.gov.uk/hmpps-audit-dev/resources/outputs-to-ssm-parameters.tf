f288e97270d437cf2d96428db6fcfcfc53998727
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-05-23 14:27:24 +0100

    HEAT-65: param for hmpps_audit_dead_letter_queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/outputs-to-ssm-parameters.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/outputs-to-ssm-parameters.tf
@@ -3,6 +3,7 @@
 locals {
   sqs_irsa_policies = {
     (module.hmpps_audit_queue.sqs_name) = module.hmpps_audit_queue.irsa_policy_arn
+    (module.hmpps_audit_dead_letter_queue.sqs_name) = module.hmpps_audit_dead_letter_queue.irsa_policy_arn
   }
 }
 
