f9f2608e052a687364ae15b5707e71ad725c92c7
Peter Phillips <peter@greenthistle.co.uk>
2023-05-23 13:27:11 +0100

    SDIT-404: ✨ Add in policy for domain events topic too

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/index-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/index-queue.tf
@@ -63,13 +63,3 @@ resource "kubernetes_secret" "hmpps_prisoner_search_index_dead_letter_queue" {
     sqs_queue_name      = module.hmpps_prisoner_search_index_dead_letter_queue.sqs_name
   }
 }
-
-module "hmpps-prisoner-search-indexer-irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
-
-  eks_cluster_name = var.eks_cluster_name
-  namespace        = var.namespace
-  service_account  = "hmpps-prisoner-search-indexer-dev"
-  role_policy_arns = [module.hmpps_prisoner_search_index_queue.irsa_policy_arn, module.hmpps_prisoner_search_index_dead_letter_queue.irsa_policy_arn]
-}
-
