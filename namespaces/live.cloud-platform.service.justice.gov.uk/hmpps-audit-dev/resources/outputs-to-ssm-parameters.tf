2b69ffbd8e056dfaeedfecf5c455f383d7bf0b34
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-05-23 13:54:43 +0100

    HEAT-68: configure IRSA SSM params for hmpps-audit-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/outputs-to-ssm-parameters.tf
@@ -0,0 +1,15 @@
+# Add outputs that need to be consumed in other namespaces here.
+
+locals {
+  sqs_irsa_policies = {
+    (module.hmpps_audit_queue.sqs_name) = module.hmpps_audit_queue.irsa_policy_arn
+  }
+}
+
+resource "aws_ssm_parameter" "tf-outputs-sqs-irsa-policies" {
+  for_each = local.sqs_irsa_policies
+  type     = "String"
+  name     = "/${var.namespace}/sqs/${each.key}/irsa-policy-arn"
+  value    = each.value
+  tags     = local.tags
+}
