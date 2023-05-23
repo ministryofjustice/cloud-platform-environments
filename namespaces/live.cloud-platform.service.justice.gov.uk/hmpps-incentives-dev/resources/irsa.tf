99631bb9e5a6077fdaca3ddc573e624d528752b7
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-05-23 14:41:51 +0100

    INC-1263: configure IRSA for incentives-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/irsa.tf
@@ -0,0 +1,35 @@
+# Add the names of the SQS & SNS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "hmpps_audit_queue"   = "hmpps-audit-dev",
+  },
+  sns_topics = {
+    "hmpps-domain-events" = "hmpps-domain-events-dev"
+  }
+}
+
+module "app-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+
+  eks_cluster_name = var.eks_cluster_name
+  namespace        = var.namespace
+  service_account  = "hmpps-incentives-api-${var.environment}"
+  role_policy_arns = [
+    module.prisoner-event-queue.irsa_policy_arns,
+    module.prisoner-event-dlq.irsa_policy_arns,
+    [for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.value],
+    [for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.value]
+  ]
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
