7d2c85d7ecb0cf198927fc832f4dc2676d5a8eb2
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-05-23 14:59:44 +0100

    INC-1263: iterate through correct variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-dev/resources/irsa.tf
@@ -25,7 +25,7 @@ module "app-irsa" {
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
-  for_each = local.sns_topics
+  for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
 
