8db209255ea66991a39f9a288378d05ce35a591a
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-05-25 16:31:09 +0100

    Use variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/variables.tf
@@ -11,6 +11,14 @@ variable "namespace" {
   default = "peoplefinder-development"
 }
 
+variable "business_unit" {
+  default = "Central Digital"
+}
+
+variable "application" {
+  default = "peoplefinder"
+}
+
 variable "domain" {
   default = "development.peoplefinder.service.gov.uk"
 }
@@ -23,5 +31,14 @@ variable "environment" {
   default = "development"
 }
 
+variable "infrastructure_support" {
+  default = "people-finder-support@digital.justice.gov.uk"
+}
+
+variable "team_name" {
+  default = "peoplefinder"
+}
+
 variable "eks_cluster_name" {
 }
+
