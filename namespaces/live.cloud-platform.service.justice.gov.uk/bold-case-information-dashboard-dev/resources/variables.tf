ed5763dccc39b4b86aa142737810a4074dba6c59
William Martin <william.martin1@justice.gov.uk>
2023-05-25 12:01:19 +0100

    Update contact info for BOLD CID team
    Updating the contact information in the
    bold-case-information-dashboard-dev namespace for the BOLD CID team.

--- namespaces/live.cloud-platform.service.justice.gov.uk/bold-case-information-dashboard-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/bold-case-information-dashboard-dev/resources/variables.tf
@@ -41,7 +41,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "william.martin@digital.justice.gov.uk"
+  default     = "CaseInformationDashboard@justice.gov.uk"
 }
 
 variable "is_production" {
@@ -53,7 +53,7 @@ variable "is_production" {
 variable "slack_channel" {
   description = "Slack channel name for your team, if we need to contact you about this service"
   type        = string
-  default     = "ask-bold-rr-ops"
+  default     = "ask-bold-case-info-dashboard"
 }
 
 variable "repo_name" {
