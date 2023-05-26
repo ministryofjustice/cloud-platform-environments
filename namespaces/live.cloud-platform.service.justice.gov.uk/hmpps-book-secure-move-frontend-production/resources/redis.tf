20390bd33f845440193d6992b9292e7cd399e52e
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-05-26 13:04:05 +0100

    P4-4181 BaSM FE Production upgrade redis node to t4g

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-production/resources/redis.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-production/resources/redis.tf
@@ -3,17 +3,18 @@ module "redis-elasticache" {
 
   vpc_name = var.vpc_name
 
-  application            = var.application
-  environment-name       = var.environment-name
-  is-production          = var.is_production
-  infrastructure-support = var.infrastructure_support
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  engine_version         = "7.0"
-  parameter_group_name   = "default.redis7"
-  namespace              = var.namespace
-  snapshot_window        = "22:00-23:59"
-  maintenance_window     = "sun:00:00-sun:03:00"
+  application             = var.application
+  environment-name        = var.environment-name
+  is-production           = var.is_production
+  infrastructure-support  = var.infrastructure_support
+  team_name               = var.team_name
+  business-unit           = var.business_unit
+  engine_version          = "7.0"
+  parameter_group_name    = "default.redis7"
+  namespace               = var.namespace
+  snapshot_window         = "22:00-23:59"
+  maintenance_window      = "sun:00:00-sun:03:00"
+  node_type               = "cache.t4g.medium"
 
   providers = {
     aws = aws.london
