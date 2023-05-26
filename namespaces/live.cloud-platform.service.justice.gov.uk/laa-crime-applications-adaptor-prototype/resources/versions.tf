1e65784783d295691435bff7e64b8e686e34357a
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2023-05-26 13:26:59 +0100

    LASB-1918: Added required provider version for random and kubernetes.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-applications-adaptor-prototype/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-applications-adaptor-prototype/resources/versions.tf
@@ -10,5 +10,13 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.23.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
   }
 }
