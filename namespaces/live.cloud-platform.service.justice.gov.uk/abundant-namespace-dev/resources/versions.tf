089101fb8a32be1fa37cbf06a3bd8286896bdfef
jakemulley <jakemulley@users.noreply.github.com>
2023-05-31 18:05:12 +0100

    Bump terraform-provider-aws to v5.0.1 in abundant-namespace-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.64.0"
+      version = "~> 5.0.1"
     }
     github = {
       source  = "integrations/github"
