4ed0ec76fa3b20181c68cacc9fa51bf744d18aaf
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-06-01 12:57:13 +0100

    P4-4188 BaSM Front end staging Add github token variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-staging/resources/variables.tf
@@ -39,3 +39,8 @@ variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   default     = "ministryofjustice"
 }
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
