52e4c53d591d359d2f9e78ec7ed66873138e1e59
Stephen Dicks <github@starswan.com>
2023-06-01 08:06:25 +0100

    remove pingdom

--- namespaces/live.cloud-platform.service.justice.gov.uk/cfe-crime-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfe-crime-dev/resources/versions.tf
@@ -14,9 +14,5 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20.0"
     }
-    pingdom = {
-      source  = "DrFaust92/pingdom"
-      version = "~> 1.3.1"
-    }
   }
 }
