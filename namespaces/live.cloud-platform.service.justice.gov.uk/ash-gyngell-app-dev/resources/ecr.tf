0e2d58d568506845269503f84b604e1a0c2b3bd0
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2023-06-01 15:15:19 +0100

    reattempt namespace apply pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/ash-gyngell-app-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ash-gyngell-app-dev/resources/ecr.tf
@@ -4,19 +4,36 @@ module "ecr_credentials" {
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
 
-  github_repositories = [var.namespace]
+/*
+  By default scan_on_push is set to true. When this is enabled then all images pushed to the repo are scanned for any security
+  / software vulnerabilities in your image and the results can be viewed in the console. For further details, please see:
+  https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html
+  To disable 'scan_on_push', set it to false as below:
+scan_on_push = "false"
+*/
+
+# Uncomment and provide repository names to create github actions secrets
+# containing the ECR name, AWS access key, and AWS secret key, for use in
+# github actions CI/CD pipelines
+# github_repositories = ["my-repo"]
+
+# list of github environments, to create the ECR secrets as environment secrets
+# https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+# github_environments = ["my-environment"]
+
+github_repositories = [var.namespace]
 }
 
 resource "kubernetes_secret" "ecr_credentials" {
-  metadata {
-    name      = "ecr-repo-${var.namespace}"
-    namespace = var.namespace
-  }
+metadata {
+  name      = "ecr-repo-${var.namespace}"
+  namespace = var.namespace
+}
 
-  data = {
-    access_key_id     = module.ecr_credentials.access_key_id
-    secret_access_key = module.ecr_credentials.secret_access_key
-    repo_arn          = module.ecr_credentials.repo_arn
-    repo_url          = module.ecr_credentials.repo_url
-  }
+data = {
+  access_key_id     = module.ecr_credentials.access_key_id
+  secret_access_key = module.ecr_credentials.secret_access_key
+  repo_arn          = module.ecr_credentials.repo_arn
+  repo_url          = module.ecr_credentials.repo_url
+}
 }
