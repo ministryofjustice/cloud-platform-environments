77872a86097e4f50daf13141a45c8e8c25ef6534
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-06-02 15:01:14 +0100

    Change instance type (#13578)

--- namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-staging/resources/rds.tf
@@ -12,7 +12,8 @@ module "rds-instance" {
   environment-name           = var.environment_name
   infrastructure-support     = var.infrastructure_support
   namespace                  = var.namespace
-  db_instance_class          = "db.t3.small"
+  db_instance_class          = "db.t4g.micro"
+  db_max_allocated_storage   = "500"
   db_engine                  = "postgres"
   db_engine_version          = "14"
   rds_family                 = "postgres14"
