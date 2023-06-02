a657d5340a57833789ae1b3bd792a209daa857dd
sarahyjja <44409053+sarahyjja@users.noreply.github.com>
2023-06-02 16:38:26 +0100

    Update rds-postgresql.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-express-prototype/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-express-prototype/resources/rds-postgresql.tf
@@ -5,7 +5,7 @@
  *
  */
 module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.18.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.17.1"
 
   # VPC configuration
   vpc_name = var.vpc_name
