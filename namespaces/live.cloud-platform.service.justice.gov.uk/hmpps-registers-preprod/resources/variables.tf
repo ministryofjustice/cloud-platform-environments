55323cc65d07d9517dae2735fe7c8bafdd1c6550
jakemulley <jakemulley@users.noreply.github.com>
2023-06-04 20:00:25 +0100

    preprod namespaces: add github provider

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-preprod/resources/variables.tf
@@ -46,3 +46,15 @@ variable "is_production" {
 variable "number-cache-clusters" {
   default = "2"
 }
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
