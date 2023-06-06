24d86507605b1cfb32a0c7f414a91d03882501f7
jack-burt-is <jack.burt@informed.com>
2023-06-06 11:52:55 +0000

    Remove prometheus for now, move to OIDC for ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-stag/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-stag/resources/variables.tf
@@ -59,7 +59,7 @@ variable "slack_channel" {
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
-  default     = "ministryofjustice"
+  default     = "CriminalInjuriesCompensationAuthority"
 }
 
 variable "github_token" {
