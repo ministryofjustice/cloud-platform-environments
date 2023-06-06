2d605c0be6fccc08e7b11e9854247bf79787162c
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-06-06 16:20:17 +0100

    HAAR-1491: re-add aws alias london/ireland

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-api-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-api-dev/resources/main.tf
@@ -6,6 +6,17 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
