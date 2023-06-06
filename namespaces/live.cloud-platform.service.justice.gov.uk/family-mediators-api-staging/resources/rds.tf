fbd3eeb8ae2cb4baa93f97b97dbb3f7d25c91890
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-06-06 09:47:50 +0100

    Update instance type

--- namespaces/live.cloud-platform.service.justice.gov.uk/family-mediators-api-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/family-mediators-api-staging/resources/rds.tf
@@ -12,6 +12,8 @@ module "rds-instance" {
   environment-name           = var.environment_name
   infrastructure-support     = var.infrastructure_support
   namespace                  = var.namespace
+  db_instance_class          = "db.t4g.micro"
+  db_max_allocated_storage   = "500"
   rds_family                 = "postgres14"
   db_engine                  = "postgres"
   db_engine_version          = "14"
