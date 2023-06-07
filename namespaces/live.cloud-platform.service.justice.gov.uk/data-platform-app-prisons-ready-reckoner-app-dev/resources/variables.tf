5f5ea16da9fcd35c3f372cb25fc1b7fa57660851
Tom Webber <80110358+tom-webber@users.noreply.github.com>
2023-06-07 13:00:40 +0100

    Update variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-prisons-ready-reckoner-app-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-prisons-ready-reckoner-app-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "mh-cjs-priority-projects"
+  default     = "mh_cjs_priority_projects"
 }
 
 variable "environment" {
