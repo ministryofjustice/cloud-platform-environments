622c0473778e75c98cb81141c63237881c9af701
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-06-07 09:32:11 +0100

    AP migration task: - modify namespace for ccma-gold-scorecards-app - modify ecr for ccma-gold-scorecards-app (prod) - modify serviceaccount for ccma-gold-scorecards-app (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-ccma-gold-scorecards-app-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-ccma-gold-scorecards-app-prod/resources/ecr.tf
@@ -1,7 +1,8 @@
 module "ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.2.0"
-  team_name = var.team_name
-  repo_name = "${var.namespace}-ecr"
+  source         = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.2.0"
+  team_name      = var.team_name
+  repo_name      = "${var.namespace}-ecr"
+  oidc_providers = ["github"]
 
   /*
     By default scan_on_push is set to true. When this is enabled then all images pushed to the repo are scanned for any security
@@ -19,10 +20,7 @@ module "ecr_credentials" {
   # list of github environments, to create the ECR secrets as environment secrets
   # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
   github_environments   = ["prod"]
-  oidc_providers        = ["github"]
   github_actions_prefix = "prod"
-
-
 
   /*
   # Lifecycle_policy provides a way to automate the cleaning up of your container images by expiring images based on age or count.
