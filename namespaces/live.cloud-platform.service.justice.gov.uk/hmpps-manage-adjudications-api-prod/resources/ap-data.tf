e48e2291968b3ce259971edbfe0ac47b68543716
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-06-07 15:24:37 +0100

    NN-5194: use latest IRSA module

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/ap-data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/ap-data.tf
@@ -1,5 +1,5 @@
 module "analytical-platform" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   namespace        = var.namespace
   eks_cluster_name = var.eks_cluster_name
