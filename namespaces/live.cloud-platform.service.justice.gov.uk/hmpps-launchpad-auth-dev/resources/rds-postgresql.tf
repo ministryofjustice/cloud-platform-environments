d07ee52254c4692cd857afb63752ccdf71d05e93
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2023-06-08 09:13:28 +0100

    updating postgresql version based on checks error message in creating pull request

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-auth-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-auth-dev/resources/rds-postgresql.tf
@@ -5,7 +5,7 @@
  *
  */
 module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.17.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
 
   # VPC configuration
   vpc_name = var.vpc_name
