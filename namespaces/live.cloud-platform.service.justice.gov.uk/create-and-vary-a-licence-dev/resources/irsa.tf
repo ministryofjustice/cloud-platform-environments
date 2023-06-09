139b314555aa6962c0d61ec87f6804cfac79a4b6
Matt <34448412+mattops@users.noreply.github.com>
2023-06-09 16:13:06 +0100

    create-and-vary-a-licence-dev: update to latest irsa module (#13743)

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-dev/resources/irsa.tf
@@ -11,8 +11,26 @@ locals {
     "Digital-Prison-Services-dev-cvl_prison_events_queue"       = "offender-events-dev",
     "Digital-Prison-Services-dev-cvl_prison_events_queue_dl"    = "offender-events-dev"
   }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
 }
 
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = var.application
+  role_policy_arns     = local.sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+# For deletion when app updated.
 module "app-irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
 
