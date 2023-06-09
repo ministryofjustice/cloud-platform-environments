104177fd489b3f97744de470d8322d5f25aa4176
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-06-09 11:44:04 +0100

    prison-to-probation-update-dev: update to latest irsa module

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/irsa.tf
@@ -7,17 +7,35 @@ locals {
     "Digital-Prison-Services-dev-prison_to_probation_update_queue"    = "offender-events-dev",
     "Digital-Prison-Services-dev-prison_to_probation_update_queue_dl" = "offender-events-dev"
   }
-  sqs_policies      = [for item in data.aws_ssm_parameter.irsa_policy_arns : item.value]
-  dynamodb_policies = [module.message_dynamodb.irsa_policy_arn, module.schedule_dynamodb.irsa_policy_arn]
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
+  dynamodb_policies = {
+    message_dynamodb  = module.message_dynamodb.irsa_policy_arn,
+    schedule_dynamodb = module.schedule_dynamodb.irsa_policy_arn
+  }
 }
 
+module "app-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = var.application
+  role_policy_arns     = merge(local.sqs_policies, local.dynamodb_policies)
+}
+
+#  For deletion
+locals {
+  sqs_policies_old      = [for item in data.aws_ssm_parameter.irsa_policy_arns : item.value]
+  dynamodb_policies_old = [module.message_dynamodb.irsa_policy_arn, module.schedule_dynamodb.irsa_policy_arn]
+}
+#  For deletion
 module "app-irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
 
   eks_cluster_name = var.eks_cluster_name
   namespace        = var.namespace
   service_account  = "${var.application}-${var.environment-name}"
-  role_policy_arns = concat(local.sqs_policies, local.dynamodb_policies)
+  role_policy_arns = concat(local.sqs_policies_old, local.dynamodb_policies_old)
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns" {
