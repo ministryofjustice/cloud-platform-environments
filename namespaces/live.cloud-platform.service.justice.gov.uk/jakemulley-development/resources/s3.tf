1a3c6005738cca2777f0effd7e9fd4489be8fb7a
jakemulley <jakemulley@users.noreply.github.com>
2023-06-09 15:04:32 +0100

    jakemulley-development: test CDN

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/s3.tf
@@ -9,3 +9,25 @@ module "s3" {
   namespace              = var.namespace
   team_name              = var.team_name
 }
+
+module "cdn" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=initial"
+
+  # Configuration
+  enabled         = true
+  is_ipv6_enabled = true
+
+  # Origin
+  origin = {
+    domain_name = "https://${module.s3.bucket_name}.s3.eu-west-2.amazonaws.com" # tidy up
+  }
+
+  # Tags
+  application            = var.application
+  business-unit          = var.business_unit
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  is-production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+}
