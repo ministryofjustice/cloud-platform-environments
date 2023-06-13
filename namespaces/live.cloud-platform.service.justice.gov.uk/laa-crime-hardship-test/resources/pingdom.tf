0b4fc53b154e47e3aef41aaff7249642c3e16687
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-06-13 14:40:00 +0100

    setup of new laa-crime-hardship-test environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-hardship-test/resources/pingdom.tf
@@ -0,0 +1,15 @@
+resource "pingdom_check" "laa-crime-hardship-dev" {
+  type                     = "http"
+  name                     = "LAA Crime Hardship - Test"
+  host                     = "laa-crime-hardship-test.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 3
+  notifyagainevery         = 0
+  url                      = "/actuator/health"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_laa,application_laa-crime-hardship,component_ping,isproduction_false,environment_dev,owner_laa-crime-apps-team"
+  probefilters             = "region:EU"
+  integrationids           = [121160]
+}
