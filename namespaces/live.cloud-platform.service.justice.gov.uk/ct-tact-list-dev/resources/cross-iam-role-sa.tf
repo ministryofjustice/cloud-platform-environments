5190e1f1421abf6cb4e53f98ba67526ee97468b1
Francesca von Braun-Bates <francesca.von.braun.bates@digital.justice.gov.uk>
2023-06-14 14:21:42 +0100

    Add IRSA module.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/cross-iam-role-sa.tf
@@ -0,0 +1,42 @@
+
+    module "irsa" {
+      source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.0.2"
+      namespace        = "<namespace>"
+      role_policy_arns = ["<aws_iam_policy.<namespace>_<policy_name>.arn>"]
+    }
+    data "aws_iam_policy_document" "<namespace>_<policy_name>" {
+      # Provide list of permissions and target AWS account resources to allow access to
+      statement {
+        actions = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+        ]
+        resources = [
+          "<ARN of resource in target AWS account>/*",
+        ]
+      }
+    }
+    resource "aws_iam_policy" "<namespace>_<policy_name>" {
+      name   = "<namespace>-<policy-name>"
+      policy = data.aws_iam_policy_document.<namespace>_<policy_name>.json
+
+      tags = {
+        business-unit          = "<Which part of the MoJ is responsible for this service? (e.g HMPPS, Legal Aid Agency)>"
+        application            = "<Application name>"
+        is-production          = "<true/false>"
+        environment-name       = "<dev/test/staging/prod>"
+        owner                  = "<team responsible for this application>"
+        infrastructure-support = "<Email address for contact/support>"
+      }
+    }
+    resource "kubernetes_secret" "irsa" {
+      metadata {
+        name      = "irsa-output"
+        namespace = "<namespace>"
+      }
+      data = {
+        role = module.irsa.aws_iam_role_name
+        serviceaccount = module.irsa.service_account_name.name
+      }
+    }
+
