44b1a78e6b6a4e1ecc59e757a023181b2dcb3d0d
Peter Phillips <peter@greenthistle.co.uk>
2023-06-14 22:53:47 +0100

    SDIT-887: 🔧 Add in irsa for prisoner-offender-search-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-offender-search-prod/resources/index-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-offender-search-prod/resources/index-queue.tf
@@ -1,7 +1,7 @@
 module "prisoner_offender_index_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.11.0"
 
-  environment-name          = var.environment-name
+  environment-name          = var.environment
   team_name                 = var.team_name
   infrastructure-support    = var.infrastructure_support
   application               = var.application
@@ -25,7 +25,7 @@ EOF
 module "prisoner_offender_index_dead_letter_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.11.0"
 
-  environment-name       = var.environment-name
+  environment-name       = var.environment
   team_name              = var.team_name
   infrastructure-support = var.infrastructure_support
   application            = var.application
