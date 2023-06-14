ed3523a7bf329548c47b0da1e13548e02c728a7c
Francesca von Braun-Bates <francesca.von.braun.bates@digital.justice.gov.uk>
2023-06-14 14:32:35 +0100

    Update tags.

--- namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/cross-iam-role-sa.tf
@@ -21,12 +21,12 @@
       policy = data.aws_iam_policy_document.${var.namespace}_<policy_name>.json
 
       tags = {
-        business-unit          = "<Which part of the MoJ is responsible for this service? (e.g HMPPS, Legal Aid Agency)>"
-        application            = "<Application name>"
-        is-production          = "<true/false>"
-        environment-name       = "<dev/test/staging/prod>"
-        owner                  = "<team responsible for this application>"
-        infrastructure-support = "<Email address for contact/support>"
+        business-unit          = "${var.business_unit}"
+        application            = "${var.application}"
+        is-production          = "${var.is_production}"
+        environment-name       = "${var.environment}"
+        owner                  = "${var.team_name}"
+        infrastructure-support = "${var.infrastructure_support}"
       }
     }
     resource "kubernetes_secret" "irsa" {
