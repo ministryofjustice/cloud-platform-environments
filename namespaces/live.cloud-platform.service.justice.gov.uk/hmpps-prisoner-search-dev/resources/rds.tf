24ac4b19484c65139e5c1bb245d54c154ae1a1a5
petergphillips <peter@greenthistle.co.uk>
2023-06-14 09:21:55 +0100

    SDIT-887: 🔧 Remove IAM access keys for hmpps-prisoner-search-dev (#13869)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/rds.tf
@@ -34,7 +34,5 @@ resource "kubernetes_secret" "hmpps_prisoner_search_rds" {
     database_username     = module.hmpps_prisoner_search_rds.database_username
     database_password     = module.hmpps_prisoner_search_rds.database_password
     rds_instance_address  = module.hmpps_prisoner_search_rds.rds_instance_address
-    access_key_id         = module.hmpps_prisoner_search_rds.access_key_id
-    secret_access_key     = module.hmpps_prisoner_search_rds.secret_access_key
   }
 }
