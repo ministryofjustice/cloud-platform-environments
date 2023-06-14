e0da2a5bee6642a35bb3b022ca600973b5ed15d9
Peter Phillips <peter@greenthistle.co.uk>
2023-06-14 08:45:33 +0100

    SDIT-887: 🔧 Remove IAM access keys for hmpps-restricted-patients-api-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-prod/resources/rds.tf
@@ -34,8 +34,6 @@ resource "kubernetes_secret" "dps_rds" {
     database_username     = module.rp_rds.database_username
     database_password     = module.rp_rds.database_password
     rds_instance_address  = module.rp_rds.rds_instance_address
-    access_key_id         = module.rp_rds.access_key_id
-    secret_access_key     = module.rp_rds.secret_access_key
     url                   = "postgres://${module.rp_rds.database_username}:${module.rp_rds.database_password}@${module.rp_rds.rds_instance_endpoint}/${module.rp_rds.database_name}"
   }
 }
