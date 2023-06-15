5c1cefbfc8c40e2719277a3184fb40e7c7907ea8
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-06-15 07:18:46 +0100

    HWPV-104: Add IRSA s3 creds for HwPV Prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/help-with-prison-visits-prod/resources/variables.tf
@@ -30,7 +30,6 @@ variable "is_production" {
   default = "true"
 }
 
-
 variable "vpc_name" {
 }
 
@@ -39,6 +38,11 @@ variable "number_cache_clusters" {
   type        = string
   default     = "2"
 }
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
@@ -50,4 +54,3 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
-
