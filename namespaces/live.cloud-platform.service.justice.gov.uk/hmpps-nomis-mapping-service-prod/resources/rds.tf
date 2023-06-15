795a7ff41078ee86a3f1395ae11738e68ec11d03
petergphillips <peter@greenthistle.co.uk>
2023-06-15 11:51:34 +0100

    SDIT-887: 🔧 Remove IAM access keys for hmpps-nomis-mapping-service-prod (#13865)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-nomis-mapping-service-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-nomis-mapping-service-prod/resources/rds.tf
@@ -34,8 +34,6 @@ resource "kubernetes_secret" "dps_rds" {
     database_username     = module.dps_rds.database_username
     database_password     = module.dps_rds.database_password
     rds_instance_address  = module.dps_rds.rds_instance_address
-    access_key_id         = module.dps_rds.access_key_id
-    secret_access_key     = module.dps_rds.secret_access_key
     url                   = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
   }
 }
