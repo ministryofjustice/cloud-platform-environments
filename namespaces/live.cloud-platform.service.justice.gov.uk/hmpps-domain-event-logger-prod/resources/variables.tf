2599a4ac8d95838da2eb96704ece4c6320bd3a71
petergphillips <peter@greenthistle.co.uk>
2023-06-15 14:37:16 +0100

    SDIT-887: 🔧 Add in irsa for hmpps-domain-event-logger-prod (#13894)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-event-logger-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-event-logger-prod/resources/variables.tf
@@ -1,5 +1,6 @@
-variable "vpc_name" {
-}
+variable "vpc_name" {}
+
+variable "kubernetes_cluster" {}
 
 variable "application" {
   description = "Name of Application you are deploying"
@@ -38,6 +39,9 @@ variable "slack_channel" {
   description = "Team slack channel to use if we need to contact your team"
   default     = "syscon_team"
 }
+
+variable "eks_cluster_name" {}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
