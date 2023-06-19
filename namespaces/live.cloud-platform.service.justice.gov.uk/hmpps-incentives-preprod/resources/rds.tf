610a6fd2a38a2070f41dce6593602fc086b4dd2d
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2023-06-19 16:28:51 +0100

    INC-1246: remove namespace skip and set prepare_for_major_update in rds.tf to false.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-preprod/resources/rds.tf
@@ -9,7 +9,7 @@ module "dps_rds" {
   environment-name       = var.environment
   infrastructure-support = var.infrastructure_support
 
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
   db_instance_class           = "db.t4g.small"
   rds_family                  = "postgres15"
   db_engine_version           = "15"
