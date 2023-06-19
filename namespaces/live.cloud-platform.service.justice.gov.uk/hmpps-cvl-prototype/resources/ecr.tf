3d4f3b823afd017d1691f0b276f45879279f7447
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2023-06-19 16:57:10 +0100

    Added hmpps-cvl-prototype

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cvl-prototype/resources/ecr.tf
@@ -0,0 +1,9 @@
+module "ecr-repo" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.2.0"
+
+  team_name = var.team_name
+  repo_name = "${var.namespace}-ecr"
+
+  oidc_providers      = ["github"]
+  github_repositories = [var.namespace]
+}
