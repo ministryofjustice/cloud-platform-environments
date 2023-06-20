6d8bb01bed329cc945325a789bfc97202b89ba51
yasin <yasin.mustafa@gmail.com>
2023-06-20 15:16:04 +0100

    MOP-80 Add mandatory irsa variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-offences-api-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-offences-api-dev/resources/cross-iam-role-sa.tf
@@ -2,17 +2,25 @@ locals {
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  sns_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value}
 }
 
 module "irsa" {
   source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
   namespace        = var.namespace
   eks_cluster_name = var.eks_cluster_name
+  service_account_name = "hmpps-manage-offences-api"
   role_policy_arns = merge(
     aws_iam_policy.hmpps_manage_offences_api_dev_ap_policy.arn,
     local.sns_policies,
   )
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
@@ -90,9 +98,9 @@ resource "kubernetes_secret" "ap_aws_secret" {
   }
 
   data = {
-    bucket_arn         = "arn:aws:s3:::mojap-manage-offences"
-    user_arn           = aws_iam_user.manage_offences_api_ap_user.arn
-    access_key_id      = aws_iam_access_key.manage_offences_api_ap_user.id
-    secret_access_key  = aws_iam_access_key.manage_offences_api_ap_user.secret
+    bucket_arn        = "arn:aws:s3:::mojap-manage-offences"
+    user_arn          = aws_iam_user.manage_offences_api_ap_user.arn
+    access_key_id     = aws_iam_access_key.manage_offences_api_ap_user.id
+    secret_access_key = aws_iam_access_key.manage_offences_api_ap_user.secret
   }
 }
