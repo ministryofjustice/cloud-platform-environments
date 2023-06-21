32caa311498b5e92413760d8c653669fd6a6f9b2
Marcus Aspin <maspin@unilink.com>
2023-06-21 10:00:26 +0100

    PI-1092 Create topic access key for integration services (dev) (#13985)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/variables.tf
@@ -34,6 +34,7 @@ variable "additional_topic_clients" {
     "hmpps-manage-offences-api-dev",
     "hmpps-manage-adjudications-api-dev",
     "hmpps-prisoner-search-dev",
+    "hmpps-probation-integration-services-dev",
     "hmpps-registers-dev",
     "hmpps-restricted-patients-api-dev",
     "hmpps-tier-dev",
