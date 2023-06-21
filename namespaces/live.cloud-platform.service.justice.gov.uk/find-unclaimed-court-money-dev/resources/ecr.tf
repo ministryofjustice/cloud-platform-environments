1b027401e9d3236685b709fa40a827700abdcaaf
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-06-21 12:06:31 +0100

    cdpt 766 FUM migrate to short lived credentials

--- namespaces/live.cloud-platform.service.justice.gov.uk/find-unclaimed-court-money-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/find-unclaimed-court-money-dev/resources/ecr.tf
@@ -17,6 +17,13 @@ module "ecr_credentials" {
   scan_on_push = "false"
   */
 
+  # enable the oidc implementation for CircleCI
+  oidc_providers = ["circleci"]
+
+  # set your namespace name to set a ConfigMap
+  # of credentials you need in CircleCI
+  namespace = var.namespace
+
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
