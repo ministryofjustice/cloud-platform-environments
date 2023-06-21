cc61aae6aa2d6ee5b335c0423c914fb8ba463bac
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-06-21 12:08:02 +0100

    HAAR-1491: 🔧 update irsa

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-api-dev/resources/isra.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-poc-api-dev/resources/isra.tf
@@ -5,24 +5,26 @@ locals {
   sqs_queues = {
     "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
   }
+  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value}
 }
 
-module "app-irsa" {
+module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
-   eks_cluster_name     = var.eks_cluster_name
-   namespace            = var.namespace
-   service_account_name = "${var.team_name}-${var.environment}"
-   role_policy_arns = {
-    hmpps_audit_dlq = module.hmpps_audit_dlq.irsa_policy_arn,
-    hmpps_audit_queue = module.hmpps_audit_queue.irsa_policy_arn
-   }
+  eks_cluster_name       = var.eks_cluster_name
+  namespace              = var.namespace
+  service_account_name   = "hmpps-audit-poc-api"
+  role_policy_arns       = local.sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
 
-    # Tags
-     business_unit          = var.business_unit
-     application            = var.application
-     is_production          = var.is_production
-     team_name              = var.team_name
-     environment_name       = var.environment
-     infrastructure_support = var.infrastructure_support
+data "aws_ssm_parameter" "irsa_policy_arns" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
