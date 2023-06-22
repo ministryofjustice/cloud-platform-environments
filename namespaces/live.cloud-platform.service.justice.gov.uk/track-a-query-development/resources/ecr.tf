bafdcaf0a5ff1412482b10b99335bb09319ac7f1
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-06-22 15:07:01 +0100

    save in sublime

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-development/resources/ecr.tf
@@ -1,1 +1,19 @@
-/* * Make sure that you use the latest version of the module by changing the * `ref=` value in the `source` attribute to the latest version listed on the * releases page of this repository. * */module "track_a_query_ecr_credentials" {  # enable the oidc implementation for CircleCI  oidc_providers = ["circleci"]  # specify which GitHub repository your CircleCI job runs from  github_repositories = ["correspondence_tool_staff"]  # set your namespace name to create a ConfigMap  # of credentials you need in CircleCI  namespace = var.namespace}
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "track_a_query_ecr_credentials" {
+
+  # enable the oidc implementation for CircleCI
+  oidc_providers = ["circleci"]
+
+  # specify which GitHub repository your CircleCI job runs from
+  github_repositories = ["correspondence_tool_staff"]
+
+  # set your namespace name to create a ConfigMap
+  # of credentials you need in CircleCI
+  namespace = var.namespace
+
+}
