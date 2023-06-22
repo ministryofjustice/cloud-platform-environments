76a07eab821b9e86fae6717440c6ad96f1e32242
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-06-22 15:30:22 +0100

    MAP-32 remove duplicates

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-video-link-preprod/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-video-link-preprod/resources/elasticache.tf
@@ -13,8 +13,6 @@ module "hmpps_book_video_link_elasticache_redis" {
   business-unit          = var.business_unit
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
   namespace              = var.namespace
   engine_version         = "7.0"
   parameter_group_name   = "default.redis7"
