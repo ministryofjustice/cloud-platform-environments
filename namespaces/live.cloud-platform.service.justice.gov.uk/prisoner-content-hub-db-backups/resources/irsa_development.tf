4ff58f5a75909cbdf4f13b3596b5f97029d1c3b4
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-06-26 11:57:49 +0100

    Fix IRSA module names

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/irsa_development.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/irsa_development.tf
@@ -1,4 +1,4 @@
-module "irsa" {
+module "irsa-development" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   # EKS configuration
--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/irsa_staging.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-db-backups/resources/irsa_staging.tf
@@ -1,4 +1,4 @@
-module "irsa" {
+module "irsa-staging" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   # EKS configuration
