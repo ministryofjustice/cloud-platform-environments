d3b6671c47ebf924d8e188a608a30a848f648aed
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2023-06-27 11:21:32 +0100

    Create regional custom domain for supplier api gateway (#14152)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-prod/resources/route53.tf
@@ -112,10 +112,26 @@ resource "kubernetes_secret" "route53_zone_sec" {
   }
 }
 
+########################
+# RUBY (Telephony G4S) #
+########################
+
 resource "aws_route53_record" "ruby_preprod_dns_validation_record" {
   zone_id = aws_route53_zone.route53_zone.zone_id
   name    = "_f32f12ae73a733d24edd43c85757d18b.ruby.preprod.electronic-monitoring.service.justice.gov.uk."
   type    = "CNAME"
   ttl     = "7200"
   records = ["_48d1a9b6ff3eb34ffef392d38d9b1d65.hnyzmxtzsz.acm-validations.aws."]
+}
+
+resource "aws_route53_record" "ruby_preprod_record" {
+  zone_id = aws_route53_zone.route53_zone.zone_id
+  name    = "ruby.preprod.electronic-monitoring.service.justice.gov.uk"
+  type    = "A"
+
+  alias {
+    name                   = "d-pb1rucdbf4.execute-api.eu-west-2.amazonaws.com"
+    zone_id                = "ZJ5UAJN8Y3Z2Q"
+    evaluate_target_health = false
+  }
 }
