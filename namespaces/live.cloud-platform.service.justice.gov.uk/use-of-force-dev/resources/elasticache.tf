c3518e2fed331ceaacc3ce1c4c95816e52f0f2d3
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-06-27 15:53:41 +0100

    MAP-38-add skip file and secrets update redis engine

--- namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-dev/resources/elasticache.tf
@@ -13,8 +13,8 @@ module "uof_elasticache_redis" {
   business-unit          = var.business_unit
   number_cache_clusters  = var.number_cache_clusters
   node_type              = "cache.t2.small"
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
@@ -32,5 +32,9 @@ resource "kubernetes_secret" "uof_elasticache_redis" {
     primary_endpoint_address = module.uof_elasticache_redis.primary_endpoint_address
     auth_token               = module.uof_elasticache_redis.auth_token
     member_clusters          = jsonencode(module.uof_elasticache_redis.member_clusters)
+    access_key_id            = module.uof_elasticache_redis.access_key_id
+    secret_access_key        = module.uof_elasticache_redis.secret_access_key
+    replication_group_id     = module.uof_elasticache_redis.replication_group_id
+
   }
 }
