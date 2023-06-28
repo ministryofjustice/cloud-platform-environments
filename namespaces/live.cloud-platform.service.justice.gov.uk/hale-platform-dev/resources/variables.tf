dd1ab0b34b24b4018d7004c63f88b63a6fee8c3c
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-06-28 10:23:00 +0100

    Hale platform dev eks_cluster_name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/variables.tf
@@ -31,6 +31,10 @@ variable "environment" {
   default     = "development"
 }
 
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
   default     = "wordpress@digital.justice.gov.uk"
@@ -57,22 +61,22 @@ variable "github_token" {
 
 variable "github_actions_secret_kube_cluster" {
   description = "The name of the github actions secret containing the kubernetes cluster name"
-  default     = "KUBE_CLUSTER_DEV"
+  default     = "KUBE_CLUSTER"
 }
 
 variable "github_actions_secret_kube_namespace" {
   description = "The name of the github actions secret containing the kubernetes namespace name"
-  default     = "KUBE_NAMESPACE_DEV"
+  default     = "KUBE_NAMESPACE"
 }
 
 variable "github_actions_secret_kube_cert" {
   description = "The name of the github actions secret containing the serviceaccount ca.crt"
-  default     = "KUBE_CERT_DEV"
+  default     = "KUBE_CERT"
 }
 
 variable "github_actions_secret_kube_token" {
   description = "The name of the github actions secret containing the serviceaccount token"
-  default     = "KUBE_TOKEN_DEV"
+  default     = "KUBE_TOKEN"
 }
 
 variable "github_actions_secret_ecr_name" {
