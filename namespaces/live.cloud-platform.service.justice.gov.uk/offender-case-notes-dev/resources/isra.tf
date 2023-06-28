d9c0be4c67bfe5fd9f0269a67bccdf60e28e9334
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-06-28 11:28:20 +0100

    HAAR-1491: 🔧 add topic to irsa file for offender-case-notes-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/isra.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/isra.tf
@@ -1,12 +1,18 @@
 # Add the names of the SQS which the app needs permissions to access.
 # The value of each item should be the namespace where the SQS was created.
-# This information is used to collect the IAM policies which are used by the IRSA module.
+# This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
   sqs_queues = {
     "Digital-Prison-Services-dev-offender_case_notes_events_queue" = "offender-events-dev",
     "Digital-Prison-Services-dev-offender_case_notes_events_queue_dl" = "offender-events-dev",
   }
-  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value}
+   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  }
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
 module "irsa" {
@@ -15,7 +21,12 @@ module "irsa" {
   eks_cluster_name       = var.eks_cluster_name
   namespace              = var.namespace
   service_account_name   = var.application
-  role_policy_arns       = local.sqs_policies
+    role_policy_arns     = merge(
+      local.sqs_policies,
+      local.sns_policies,
+      { prisoner-event-queue = module.prisoner-event-queue.irsa_policy_arn },
+      { prisoner-event-dlq   = module.prisoner-event-dlq.irsa_policy_arn }
+    )
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -25,7 +36,12 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
-data "aws_ssm_parameter" "irsa_policy_arns" {
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
