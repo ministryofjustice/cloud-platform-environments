9bbcce9651fd207d36daf86c5409c4cbdb1ff6d5
jakemulley <jakemulley@users.noreply.github.com>
2023-06-29 09:57:18 +0100

    jakemulley-development: test canned lifecycle policies

--- namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jakemulley-development/resources/ecr.tf
@@ -1,6 +1,6 @@
 # No canned lifecycle policies
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=add-irsa"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=canned-lifecycle-policies"
 
   # REQUIRED: Repository configuration
   team_name = var.team_name
@@ -12,6 +12,101 @@ module "ecr" {
 
   # REQUIRED: GitHub repositories that push to this container repository
   github_repositories = ["cloud-platform-ecr-oidc-test"]
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+}
+
+# Custom lifecycle policies
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=canned-lifecycle-policies"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = var.namespace
+  namespace = var.namespace
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github", "circleci"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+
+  lifecycle_policy = jsonencode({})
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+}
+
+# One canned lifecycle policies (days)
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=canned-lifecycle-policies"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = var.namespace
+  namespace = var.namespace
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github", "circleci"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+
+  canned_lifecycle_policy = {
+    count = 5
+    type = "days"
+  }
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+}
+
+# One canned lifecycle policies (count)
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=canned-lifecycle-policies"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = var.namespace
+  namespace = var.namespace
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github", "circleci"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+
+  canned_lifecycle_policy = {
+    count = 5
+    type = "imageCount"
+  }
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+}
+
+# Canned lifecycle & custom lifecycle (custom should take precedence)
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=canned-lifecycle-policies"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = var.namespace
+  namespace = var.namespace
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github", "circleci"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["cloud-platform-ecr-oidc-test"]
+
+  canned_lifecycle_policy = {
+    count = 5
+    type = "imageCount"
+  }
+
+  lifecycle_policy = jsonencode({})
 
   # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
   # github_environments = ["production"]
