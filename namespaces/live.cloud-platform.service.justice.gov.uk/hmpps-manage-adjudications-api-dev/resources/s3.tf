f63c8fff2c0711230a582ecdc807a2c8ce1393d8
tim <wright.tim.j@gmail.com>
2023-06-29 15:23:02 +0100

    latest version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
@@ -17,14 +17,23 @@ module "analytical_platform_s3_bucket" {
   }
 }
 
+
 module "analytical-platform" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
-  namespace        = var.namespace
-  eks_cluster_name = var.eks_cluster_name
-  role_policy_arns = [aws_iam_policy.analytical-platform.arn]
-  service_account  = "${var.namespace}-analytical-platform"
-  # NB: service account name must be unique within Cloud Platform (IAM role name is derived from it)
+  namespace              = var.namespace
+  eks_cluster_name       = var.eks_cluster_name
+  role_policy_arns       = {
+    analytical-platform = aws_iam_policy.analytical-platform.arn
+  }
+  service_account_name   = "${var.namespace}-analytical-platform"
+  # Tags
+  business_unit          = var.business_unit
+  team_name              = var.team_name
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
 
 resource "aws_iam_policy" "analytical-platform" {
