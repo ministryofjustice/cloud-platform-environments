aa2add5e9b9266029395d2b9194cf7bf4e72a096
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-06-30 12:20:20 +0100

    HAAR-1491: 🔧 add irsa to hmpps-audit-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/irsa.tf
@@ -0,0 +1,18 @@
+module "hmpps-audit-api-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name       = var.eks_cluster_name
+  namespace              = var.namespace
+  service_account_name   = "hmpps-audit-api"
+  role_policy_arns       = {
+  (module.hmpps_audit_queue.sqs_name)             = module.hmpps_audit_queue.irsa_policy_arn
+  (module.hmpps_audit_dead_letter_queue.sqs_name) = module.hmpps_audit_dead_letter_queue.irsa_policy_arn
+  }
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
