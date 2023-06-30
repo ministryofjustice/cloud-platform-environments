c516cce1b7e7c9d09fa835b2e1d5cc714d149b17
Tim Wright <wright.tim.j@gmail.com>
2023-06-30 09:15:32 +0100

    s3 bucket for dev (#14251)
    * s3 bucket for dev
    
    * use latest irsa version
    
    * latest version
    
    * rename
    
    * use correct name
    
    * use same names
    
    * reorder
    
    * fix
    
    * policy output
    
    * use local
    
    * testing
    
    * random arn name due to cyclical nature of terraform setup
    
    * testing

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
@@ -0,0 +1,111 @@
+module "analytical_platform_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  bucket_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+      ],
+      "Resource": [
+        "$${bucket_arn}/*"
+      ]
+    },
+  {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        $${bucket_arn}
+      ]
+    }
+  ]
+}
+EOF
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+data "aws_iam_policy_document" "bucket-policy" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      module.analytical_platform_s3_bucket.bucket_arn
+    ]
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+    ]
+    resources = [
+      "${module.analytical_platform_s3_bucket.bucket_arn}/*"
+    ]
+  }
+}
+
+module "analytical-platform" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  namespace              = var.namespace
+  eks_cluster_name       = var.eks_cluster_name
+  role_policy_arns       = {
+    analytical-platform = aws_iam_policy.analytical-platform.arn
+  }
+  service_account_name   = "${var.namespace}-analytical-platform"
+  # Tags
+  business_unit          = var.business_unit
+  team_name              = var.team_name
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "aws_iam_policy" "analytical-platform" {
+  name   = "${var.namespace}-analytical-platform"
+  policy = data.aws_iam_policy_document.bucket-policy.json
+  # NB: IAM policy name must be unique within Cloud Platform
+
+  tags = {
+    business-unit          = var.business_unit
+    team_name              = var.team_name
+    application            = var.application
+    is-production          = var.is_production
+    namespace              = var.namespace
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
+
+resource "kubernetes_secret" "analytical-platform" {
+  metadata {
+    name      = "analytical-platform-s3-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn        = module.analytical_platform_s3_bucket.bucket_arn
+    bucket_name       = module.analytical_platform_s3_bucket.bucket_name
+  }
+}
