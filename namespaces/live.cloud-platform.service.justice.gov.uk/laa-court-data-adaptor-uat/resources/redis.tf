7c189d1517c98792818c841a78f9e32005437e79
Alessandro De Simone <alexdesi@gmail.com>
2023-07-03 15:54:09 +0100

    LASB-1987: Upgrade to ElastiCache for Redis 7.0 - UAT env Rotate Elasticache AUTH token.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-uat/resources/redis.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-uat/resources/redis.tf
@@ -10,6 +10,9 @@ module "crime_apps_ec_cluster" {
   environment-name       = var.environment_name
   infrastructure-support = var.infrastructure_support
   namespace              = var.namespace
+  engine_version         = "7.0"
+  node_type              = "cache.t4g.micro"
+  auth_token_rotated_date = "2023-07-03"
   providers = {
     aws = aws.london
   }
@@ -25,5 +28,8 @@ resource "kubernetes_secret" "crime_apps_ec_cluster" {
     primary_endpoint_address = module.crime_apps_ec_cluster.primary_endpoint_address
     auth_token               = module.crime_apps_ec_cluster.auth_token
     url                      = "rediss://:${module.crime_apps_ec_cluster.auth_token}@${module.crime_apps_ec_cluster.primary_endpoint_address}:6379"
+    access_key_id            = module.crime_apps_ec_cluster.access_key_id
+    secret_access_key        = module.crime_apps_ec_cluster.secret_access_key
+    replication_group_id     = module.crime_apps_ec_cluster.replication_group_id
   }
 }
