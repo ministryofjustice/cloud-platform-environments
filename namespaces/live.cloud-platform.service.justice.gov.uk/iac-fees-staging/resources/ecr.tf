54a56d762ff496b8a77e8718b7bc39da23b6bd89
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-04 17:06:34 +0100

    fix github_repository value

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-staging/resources/ecr.tf
@@ -13,10 +13,10 @@ module "ecr" {
   namespace = var.namespace
 
   # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
-  oidc_providers = ["github"]
+  oidc_providers = ["circleci", "github"]
 
   # REQUIRED: GitHub repositories that push to this container repository
-  github_repositories = ["example-repository"]
+  # github_repositories = ["example-repository"]
 
   # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
   # github_environments = ["production"]
