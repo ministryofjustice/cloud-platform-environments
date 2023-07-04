9974aa85f808c9fa8c1e447b24a54ae1e6421fb6
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-04 15:25:32 +0100

    removed unecessary data block

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/topic.tf
@@ -1,7 +1,3 @@
 data "aws_sns_topic" "prison-offender-events" {
   name = "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573"
 }
-
-data "aws_ssm_parameter" "probation-offender-events-policy-arn" {
-  name = "/hmpps-domain-events-dev-${var.environment}/sns/${data.aws_sns_topic.prison-offender-events.name}/irsa-policy-arn"
-}
