1ecef73394e434ade9e817394ab7e1f707d30599
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-04 10:44:05 +0100

    PI-944 add queue to namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/topic.tf
@@ -1,7 +1,7 @@
 data "aws_sns_topic" "probation-offender-events" {
-  name = "cloud-platform-Digital-Prison-Services-453cac1179377186788c5fcd12525870"
+  name = "cloud-platform-Digital-Prison-Services-dbe10e8d9c1f4d100f0c723d5d9b754e"
 }
 
 data "aws_ssm_parameter" "probation-offender-events-policy-arn" {
-  name = "/offender-events-${var.environment}/sns/${data.aws_sns_topic.probation-offender-events.name}/irsa-policy-arn"
+  name     = "/offender-events-${var.environment}/sns/${data.aws_sns_topic.probation-offender-events.name}/irsa-policy-arn"
 }
