dddb9322f456cc7eb41d0adb9d0e1b0a61a5830d
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-04 16:10:04 +0100

    PI-944 correct topic name

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-preprod/resources/topic.tf
@@ -1,3 +1,3 @@
 data "aws_sns_topic" "prison-offender-events" {
-  name = "cloud-platform-Digital-Prison-Services-dbe10e8d9c1f4d100f0c723d5d9b754e"
+  name = "cloud-platform-Digital-Prison-Services-d448bb61bb0b69b82fb19a3fa574e7f9"
 }
