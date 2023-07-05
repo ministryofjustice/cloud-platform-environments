7627b51cdf68887919a4089f10409b020328c3db
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-07-05 17:52:40 +0100

    Add Resuable Secgroup, Enable for Activities API

--- namespaces/live.cloud-platform.service.justice.gov.uk/activities-api-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/activities-api-dev/resources/rds.tf
@@ -1,3 +1,7 @@
+data "aws_security_group" "mp_dps_sg" {
+  name = var.mp_dps_sg_name
+}
+
 module "activities_api_rds" {
   source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
   vpc_name                    = var.vpc_name
@@ -12,7 +16,7 @@ module "activities_api_rds" {
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t3.small"
   db_engine_version           = "14"
-  vpc_security_group_ids      = [aws_security_group.rds.id]
+  vpc_security_group_ids      = [aws_security_group.mp_dps_sg.id]
 
 
   providers = {
@@ -35,32 +39,4 @@ resource "kubernetes_secret" "activities_api_rds" {
     access_key_id         = module.activities_api_rds.access_key_id
     secret_access_key     = module.activities_api_rds.secret_access_key
   }
-}
-
-# Get VPC id
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name == "live" ? "live-1" : var.vpc_name]
-  }
-}
-
-# Additional RDS SG
-resource "aws_security_group" "rds" {
-  name_prefix = "${module.activities_api_rds.database_name}-mp-sg"
-  description = "RDS VPC Security Group for MP Ingress Traffic"
-  vpc_id      = aws_vpc.selected.id
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group_rule" "rule" {
-  cidr_blocks       = ["10.26.24.0/21"]
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 5432
-  to_port           = 5432
-  security_group_id = aws_security_group.rds.id
 }
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/digital-prison-reporting-mi-prototype/resources/sg.tf
@@ -0,0 +1,27 @@
+# Get VPC id
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name == "live" ? "live-1" : var.vpc_name]
+  }
+}
+
+# Additional RDS SG
+resource "aws_security_group" "rds" {
+  name_prefix = "cloudplatform-mp-dps-sg"
+  description = "RDS VPC Security Group for MP Ingress Traffic"
+  vpc_id      = aws_vpc.selected.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "rule" {
+  cidr_blocks       = ["10.26.24.0/21"]
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 5432
+  to_port           = 5432
+  security_group_id = aws_security_group.rds.id
+}
