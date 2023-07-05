d4a4fa5a041ed060a89877528d570a955ccf86f9
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-05 12:49:44 +0100

    add iam role for api gateway

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -25,6 +25,8 @@ resource "aws_api_gateway_integration" "proxy" {
   integration_http_method = "PUT"
   uri                     = "arn:aws:apigateway:eu-west-2:s3:action/PutObject/cloud-platform-d3ad47215cc1ffea9eff85a1aa2575b6/"
 
+  credentials = aws_iam_role.api_gateway_role.arn
+
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
@@ -49,4 +51,48 @@ resource "aws_api_gateway_integration_response" "proxy" {
   response_templates = {
     "application/json" = ""
   }
+}
+
+resource "aws_iam_role" "api_gateway_role" {
+  name = "api-gateway-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "s3_put_object_policy" {
+  name        = "s3-put-object-policy"
+  description = "Allows API Gateway to put objects in S3 bucket"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowPutObject",
+      "Effect": "Allow",
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::cloud-platform-d3ad47215cc1ffea9eff85a1aa2575b6/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_s3_policy_attachment" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = aws_iam_policy.s3_put_object_policy.arn
 }
