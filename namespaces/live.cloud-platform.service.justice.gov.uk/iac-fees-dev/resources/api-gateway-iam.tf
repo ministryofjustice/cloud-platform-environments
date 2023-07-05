b4f91479efbc32fb58664aba0a8cb875bfa72455
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-05 16:59:47 +0100

    create api gateway user with iam permissions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway-iam.tf
@@ -0,0 +1,43 @@
+# Generate an additional IAM user to manage APIGW a
+resource "random_id" "apigw-id" {
+  byte_length = 16
+}
+
+resource "aws_iam_user" "apigw-user" {
+  name = "apigw-user-${random_id.apigw-id.hex}"
+  path = "/system/apigw-user/"
+}
+
+resource "aws_iam_access_key" "apigw-user" {
+  user = aws_iam_user.apigw-user.name
+}
+
+data "aws_iam_policy_document" "apigw" {
+  statement {
+    actions = [
+      "apigateway:PUT"
+    ]
+
+    resources = [
+      "${aws_api_gateway_rest_api.upload_pdf_api.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "apigw-policy" {
+  name   = "${var.namespace}-apigw"
+  policy = data.aws_iam_policy_document.apigw.json
+  user   = aws_iam_user.apigw-user.name
+}
+
+resource "kubernetes_secret" "iac_fees_apigw_iam" {
+  metadata {
+    name      = "apigateway-iam"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = aws_iam_access_key.apigw-user.id
+    secret_access_key = aws_iam_access_key.apigw-user.secret
+  }
+}
