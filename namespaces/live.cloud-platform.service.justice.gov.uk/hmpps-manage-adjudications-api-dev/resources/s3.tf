da0d2a0da4e5f2218f9590c2446a6da2dbd677bf
tim <wright.tim.j@gmail.com>
2023-07-06 13:30:15 +0100

    reorder

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
@@ -1,21 +1,3 @@
-module "analytical_platform_s3_bucket" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
-  team_name              = var.team_name
-  acl                    = "private"
-  versioning             = false
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
-  namespace              = var.namespace
-  bucket_policy          = data.aws_iam_policy_document.bucket-policy.json
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 
 module "analytical-platform" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
@@ -35,8 +17,30 @@ module "analytical-platform" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "analytical_platform_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+  bucket_policy          = data.aws_iam_policy_document.bucket-policy.json
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 data "aws_iam_policy_document" "bucket-policy" {
   statement {
+    principals {
+      type        = "AWS"
+      identifiers = [module.analytical-platform.aws_iam_role_arn]
+    }
     actions = [
       "s3:GetObject"
     ]
