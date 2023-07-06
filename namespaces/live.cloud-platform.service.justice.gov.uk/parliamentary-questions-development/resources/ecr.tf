2f84b03dd10a30afc0492b596a34611261503df7
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-07-06 14:54:45 +0100

    add attributes

--- namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-development/resources/ecr.tf
@@ -10,7 +10,18 @@ module "pq_ecr_credentials" {
   providers = {
     aws = aws.london
   }
+
+  # enable the oidc implementation for CircleCI
+  oidc_providers = ["circleci"]
+
+  # specify which GitHub repository your CircleCI job runs from
   github_repositories = [var.repo_name]
+
+  # set your namespace name to create a ConfigMap
+  # of credentials you need in CircleCI
+  namespace = var.namespace
+
+
 }
 
 resource "kubernetes_secret" "pq_ecr_credentials" {
