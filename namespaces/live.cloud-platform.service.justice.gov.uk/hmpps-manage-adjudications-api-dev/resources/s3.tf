cb8201d954e9202fd0264c761810ddfeb3735f4f
Tim Wright <wright.tim.j@gmail.com>
2023-07-06 13:49:26 +0100

    NN-5273 incentives hardcoded approach (#14394)
    * NN-5273 incentives hardcoded approach
    
    * remove principal
    
    * reorder
    
    * move to file staring with a
    
    * use output name
    
    * refactor

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/s3.tf
@@ -1,39 +1,3 @@
-module "analytical_platform_s3_bucket" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
-  team_name              = var.team_name
-  acl                    = "private"
-  versioning             = false
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
-  namespace              = var.namespace
-  bucket_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::012345678901:user/foobar"
-      },
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Resource": [
-        "$${bucket_arn}/*"
-      ]
-    }
-  ]
-}
-EOF
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 
 module "analytical-platform" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
@@ -53,13 +17,35 @@ module "analytical-platform" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "analytical_platform_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+  bucket_policy          = data.aws_iam_policy_document.bucket-policy.json
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 data "aws_iam_policy_document" "bucket-policy" {
   statement {
+    principals {
+      type        = "AWS"
+      identifiers = [module.analytical-platform.role_arn]
+    }
     actions = [
       "s3:GetObject"
     ]
     resources = [
-      "${module.analytical_platform_s3_bucket.bucket_arn}/*"
+      "arn:aws:s3:::cloud-platform-403569db5b294899ffe32e696b1c4ab1/*"
     ]
   }
 }
