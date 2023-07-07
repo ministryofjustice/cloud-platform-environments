b533bf524e206a06333e093eb21244ef8a20cf17
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-07 15:34:48 +0100

    fix zone_id reference

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/acm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/acm.tf
@@ -52,7 +52,7 @@ resource "kubernetes_secret" "zone_id" {
 resource "aws_route53_record" "data" {
   name    = aws_api_gateway_domain_name.apigw_fqdn.domain_name
   type    = "A"
-  zone_id = data.kubernetes_secret.zone_id.data["zone_id"]
+  zone_id = aws_route53_zone.iac_fees_dev_route53_zone.zone_id
 
   alias {
     evaluate_target_health = true
