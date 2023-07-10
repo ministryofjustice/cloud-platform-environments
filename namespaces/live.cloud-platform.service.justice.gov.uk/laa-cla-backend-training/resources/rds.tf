d339daa64c9d407f7512feb4a0b6f6988fa143b8
Mish <mishkat.najam@digital.justice.gov.uk>
2023-07-10 12:08:33 +0100

    LGA-2465 create postgres v14 rds instance module

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
@@ -39,7 +39,6 @@ module "cla_backend_rds_postgres_11" {
     }
   ]
 
-  snapshot_identifier = "manual-backup-1679001807"
   providers = {
     # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
@@ -72,8 +71,6 @@ module "cla_backend_rds_postgres_11_replica" {
   rds_family        = "postgres11"
   db_engine_version = "11"
 
-  snapshot_identifier = "manual-backup-1679001807"
-
   providers = {
     # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
@@ -98,5 +95,64 @@ resource "kubernetes_secret" "cla_backend_rds_postgres_11" {
     db_identifier     = module.cla_backend_rds_postgres_11.db_identifier
     replica_host      = module.cla_backend_rds_postgres_11_replica.rds_instance_address
     replica_endpoint  = module.cla_backend_rds_postgres_11_replica.rds_instance_endpoint
+  }
+}
+
+module "cla_backend_rds_postgres_14" {
+  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
+  vpc_name      = var.vpc_name
+  team_name     = var.team_name
+  business-unit = var.business_unit
+  application   = var.application
+  is-production = var.is_production
+  namespace     = var.namespace
+
+  db_name = "cla_backend"
+  # change the postgres version as you see fit.
+  db_engine_version      = "14"
+  environment-name       = var.environment-name
+  infrastructure-support = var.infrastructure_support
+
+  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
+  # Pick the one that defines the postgres version the best
+  rds_family = "postgres14"
+
+  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
+  # You will need to specify "pending-reboot" here, as default is set to "immediate".
+
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "false"
+
+  db_parameter = [
+    {
+      name         = "rds.force_ssl"
+      value        = "1"
+      apply_method = "pending-reboot"
+    }
+  ]
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "cla_backend_rds_postgres_14" {
+  metadata {
+    name      = "database-14"
+    namespace = var.namespace
+  }
+
+  data = {
+    endpoint          = module.cla_backend_rds_postgres_14.rds_instance_endpoint
+    host              = module.cla_backend_rds_postgres_14.rds_instance_address
+    port              = module.cla_backend_rds_postgres_14.rds_instance_port
+    name              = module.cla_backend_rds_postgres_14.database_name
+    user              = module.cla_backend_rds_postgres_14.database_username
+    password          = module.cla_backend_rds_postgres_14.database_password
+    access_key_id     = module.cla_backend_rds_postgres_14.access_key_id
+    secret_access_key = module.cla_backend_rds_postgres_14.secret_access_key
+    db_identifier     = module.cla_backend_rds_postgres_14.db_identifier
   }
 }
