03edf0ce0d0897a076315e9848f05ea374d8fb36
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-10 09:33:27 +0100

    revert kubernetes_secret to be a resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/acm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/acm.tf
@@ -30,7 +30,7 @@ resource "aws_route53_record" "cert-validations" {
   depends_on = [kubernetes_secret.zone_id]
   count      = length(aws_acm_certificate.apigw_custom_hostname.domain_validation_options)
 
-  zone_id = data.kubernetes_secret.zone_id.data["zone_id"]
+  zone_id = kubernetes_secret.zone_id.data["zone_id"]
 
   name    = element(aws_acm_certificate.apigw_custom_hostname.domain_validation_options[*].resource_record_name, count.index)
   type    = element(aws_acm_certificate.apigw_custom_hostname.domain_validation_options[*].resource_record_type, count.index)
@@ -38,7 +38,7 @@ resource "aws_route53_record" "cert-validations" {
   ttl     = 60
 }
 
-data "kubernetes_secret" "zone_id" {
+resource "kubernetes_secret" "zone_id" {
   metadata {
     name      = "route53-zone-output"
     namespace = var.base_domain_route53_namespace
@@ -48,7 +48,7 @@ data "kubernetes_secret" "zone_id" {
 resource "aws_route53_record" "data" {
   name    = aws_api_gateway_domain_name.apigw_fqdn.domain_name
   type    = "A"
-  zone_id = data.kubernetes_secret.zone_id.data["zone_id"]
+  zone_id = kubernetes_secret.zone_id.data["zone_id"]
 
   alias {
     evaluate_target_health = true
