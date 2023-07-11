02f21eb2b9dca692bc0805fd91dd182313ddce0a
Marcus Aspin <maspin@unilink.com>
2023-07-11 16:38:02 +0100

    Separate IAM policies to avoid LimitExceeded error (preprod) (#14539)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/iam.tf
@@ -1,36 +1,4 @@
-locals {
-  managed_sqs_queues = [
-    module.approved-premises-and-delius-dlq,
-    module.approved-premises-and-delius-queue,
-    module.custody-key-dates-and-delius-dlq,
-    module.custody-key-dates-and-delius-queue,
-    module.make-recall-decisions-and-delius-dlq,
-    module.make-recall-decisions-and-delius-queue,
-    module.manage-pom-cases-and-delius-dlq,
-    module.manage-pom-cases-and-delius-queue,
-    module.person-search-index-from-delius-contact-dlq,
-    module.person-search-index-from-delius-contact-queue,
-    module.person-search-index-from-delius-person-dlq,
-    module.person-search-index-from-delius-person-queue,
-    module.pre-sentence-reports-to-delius-dlq,
-    module.pre-sentence-reports-to-delius-queue,
-    module.prison-case-notes-to-probation-dlq,
-    module.prison-case-notes-to-probation-queue,
-    module.prison-custody-status-to-delius-dlq,
-    module.prison-custody-status-to-delius-queue,
-    module.refer-and-monitor-and-delius-dlq,
-    module.refer-and-monitor-and-delius-queue,
-    module.risk-assessment-scores-to-delius-dlq,
-    module.risk-assessment-scores-to-delius-queue,
-    module.tier-to-delius-dlq,
-    module.tier-to-delius-queue,
-    module.unpaid-work-and-delius-dlq,
-    module.unpaid-work-and-delius-queue,
-    module.workforce-allocations-to-delius-dlq,
-    module.workforce-allocations-to-delius-queue,
-  ]
-}
-
+# Policy to allow SNS -> SQS
 data "aws_iam_policy_document" "sqs_queue_policy_document" {
   statement {
     sid     = "DomainEventsToQueue"
@@ -79,7 +47,42 @@ data "aws_iam_policy_document" "sqs_queue_policy_document" {
   }
 }
 
+# Policies to manage queues e.g. view and redrive messages
 data "aws_iam_policy_document" "sqs_management_policy_document" {
+  for_each = {
+    queue = [
+      module.approved-premises-and-delius-queue,
+      module.custody-key-dates-and-delius-queue,
+      module.make-recall-decisions-and-delius-queue,
+      module.manage-pom-cases-and-delius-queue,
+      module.person-search-index-from-delius-contact-queue,
+      module.person-search-index-from-delius-person-queue,
+      module.pre-sentence-reports-to-delius-queue,
+      module.prison-case-notes-to-probation-queue,
+      module.prison-custody-status-to-delius-queue,
+      module.refer-and-monitor-and-delius-queue,
+      module.risk-assessment-scores-to-delius-queue,
+      module.tier-to-delius-queue,
+      module.unpaid-work-and-delius-queue,
+      module.workforce-allocations-to-delius-queue,
+    ]
+    dlq = [
+      module.approved-premises-and-delius-dlq,
+      module.custody-key-dates-and-delius-dlq,
+      module.make-recall-decisions-and-delius-dlq,
+      module.manage-pom-cases-and-delius-dlq,
+      module.person-search-index-from-delius-contact-dlq,
+      module.person-search-index-from-delius-person-dlq,
+      module.pre-sentence-reports-to-delius-dlq,
+      module.prison-case-notes-to-probation-dlq,
+      module.prison-custody-status-to-delius-dlq,
+      module.refer-and-monitor-and-delius-dlq,
+      module.risk-assessment-scores-to-delius-dlq,
+      module.tier-to-delius-dlq,
+      module.unpaid-work-and-delius-dlq,
+      module.workforce-allocations-to-delius-dlq,
+    ]
+  }
   statement {
     sid    = "QueueManagementList"
     effect = "Allow"
@@ -97,7 +100,7 @@ data "aws_iam_policy_document" "sqs_management_policy_document" {
       "sqs:ListDeadLetterSourceQueues",
       "sqs:ListQueueTags",
     ]
-    resources = local.managed_sqs_queues[*].sqs_arn
+    resources = each.value[*].sqs_arn
   }
   statement {
     sid    = "QueueManagementWrite"
@@ -109,7 +112,7 @@ data "aws_iam_policy_document" "sqs_management_policy_document" {
       "sqs:ReceiveMessage",
       "sqs:SendMessage",
     ]
-    resources = local.managed_sqs_queues[*].sqs_arn
+    resources = each.value[*].sqs_arn
   }
   statement {
     sid    = "KMS"
@@ -124,6 +127,7 @@ data "aws_iam_policy_document" "sqs_management_policy_document" {
 }
 
 resource "aws_iam_policy" "sqs_management_policy" {
-  name   = "${var.namespace}-sqs-management-policy"
-  policy = data.aws_iam_policy_document.sqs_management_policy_document.json
+  for_each = data.aws_iam_policy_document.sqs_management_policy_document
+  name     = "${var.namespace}-${each.key}-policy"
+  policy   = each.value.json
 }
