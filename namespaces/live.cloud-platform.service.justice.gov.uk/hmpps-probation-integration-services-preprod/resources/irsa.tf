0cdc759ac2db4e85dd95a37b3af4af617ab8fb23
Marcus Aspin <maspin@unilink.com>
2023-07-11 15:28:03 +0100

    Bump serviceaccount module to 0.8.2 (preprod) (#14530)
    * Bump serviceaccount module to 0.8.2 (preprod)
    
    * Remove unused console role

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/irsa.tf
@@ -1,10 +1,5 @@
 # Shared service account for use by the probation integration team to access all queues in the namespace
 # Note: each service in the namespace also has a dedicated service account
-resource "aws_iam_policy" "irsa_policy" {
-  name   = "${var.namespace}-irsa"
-  policy = data.aws_iam_policy_document.sqs_console_role_policy_document.json
-}
-
 module "shared-service-account" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
   application            = var.application
@@ -17,5 +12,5 @@ module "shared-service-account" {
   team_name              = var.team_name
 
   service_account_name = var.application
-  role_policy_arns     = { sqs = aws_iam_policy.irsa_policy.arn }
+  role_policy_arns     = { sqs = aws_iam_policy.sqs_management_policy.arn }
 }
