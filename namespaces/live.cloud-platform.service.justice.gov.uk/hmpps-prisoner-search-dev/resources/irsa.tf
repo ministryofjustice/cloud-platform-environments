989a9db47ef9445b84f15f0de7a9e4619ee6d7de
Peter Phillips <peter@greenthistle.co.uk>
2023-07-11 17:26:54 +0100

    SDIT-405: 🔧 Add in listening to incentives domain events

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-dev/resources/irsa.tf
@@ -1,14 +1,23 @@
+locals {
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sns_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value}
+}
+
 module "hmpps-prisoner-search-indexer" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-prisoner-search-indexer"
-  role_policy_arns = {
+  role_policy_arns     = merge({
     hmpps_prisoner_search_index_queue             = module.hmpps_prisoner_search_index_queue.irsa_policy_arn,
     hmpps_prisoner_search_index_dead_letter_queue = module.hmpps_prisoner_search_index_dead_letter_queue.irsa_policy_arn,
-    domain-events-sns-topic                       = data.aws_ssm_parameter.domain-events-sns-topic.value
-  }
+    hmpps_prisoner_search_domain_queue            = module.hmpps_prisoner_search_domain_queue.irsa_policy_arn,
+    hmpps_prisoner_search_domain_dlq              = module.hmpps_prisoner_search_domain_dlq.irsa_policy_arn,
+  }, local.sns_policies)
+
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -18,7 +27,7 @@ module "hmpps-prisoner-search-indexer" {
   infrastructure_support = var.infrastructure_support
 }
 
-data "aws_ssm_parameter" "domain-events-sns-topic" {
-  name = "/hmpps-domain-events-dev/sns/cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573/irsa-policy-arn"
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
-
