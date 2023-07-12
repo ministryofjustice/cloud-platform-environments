f752a8d5d03fb23e683ebe1eb4e2fa88b817718f
Tim Wright <wright.tim.j@gmail.com>
2023-07-12 11:17:47 +0100

    NN-5295 upgrade pt2 (#14498)

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-dev/resources/elasticache.tf
@@ -9,8 +9,8 @@ module "dps_redis" {
   business-unit          = var.business_unit
   number_cache_clusters  = var.number_cache_clusters
   node_type              = var.node-type
-  engine_version         = "4.0.10"
-  parameter_group_name   = "default.redis4.0"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   providers = {
