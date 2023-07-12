cee94312b26dfb12779ec9030ffbdfd8efc672d7
tim <wright.tim.j@gmail.com>
2023-07-12 10:17:22 +0100

    NN-5295 region

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-dev/resources/main.tf
@@ -4,18 +4,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
-
-  default_tags {
-    tags = {
-      GithubTeam = var.team_name
-    }
-  }
-}
-
-# To be use in case the resources need to be created in London
-provider "aws" {
-  alias  = "london"
   region = "eu-west-2"
 
   default_tags {
@@ -25,17 +13,7 @@ provider "aws" {
   }
 }
 
-# To be use in case the resources need to be created in Ireland
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
 
-  default_tags {
-    tags = {
-      GithubTeam = var.team_name
-    }
-  }
-}
 provider "github" {
   token = var.github_token
   owner = var.github_owner
