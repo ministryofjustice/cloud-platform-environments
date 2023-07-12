82ada80e2af025d55f5bc23c39dd9414a05e599d
theDustRoom <paul.massey@digital.justice.gov.uk>
2023-07-12 09:36:03 +0100

    Add pingdom for work readiness
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-prod/resources/pingdom.tf
@@ -0,0 +1,37 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 130487 = #prison-education-alerts
+
+resource "pingdom_check" "hmpps-get-ready-for-work-production-check" {
+  type                     = "http"
+  name                     = "HMPPS - Get someone ready to work"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/get-ready-for-work.hmpps.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_hmpps,application_get-ready-for-work,component_front-end,isproduction_true,environment_prod"
+  probefilters             = "region:EU"
+  integrationids           = [130487]
+}
+
+resource "pingdom_check" "hmpps-education-employment-api-check" {
+  type                     = "http"
+  name                     = "HMPPS - Education Employment API"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/education-employment-api.hmpps.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_hmpps,application_education-employment-api,component_api,isproduction_true,environment_prod"
+  probefilters             = "region:EU"
+  integrationids           = [130487]
+}
