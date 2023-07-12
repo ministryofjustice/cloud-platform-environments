391e98b3435d25be158784a963a5190cf2c9af8a
Marcus Aspin <maspin@unilink.com>
2023-07-12 17:30:49 +0000

    PI-1281 Grant access to domain events topic (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-prod/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-prod/resources/topic.tf
@@ -1,3 +1,23 @@
 data "aws_sns_topic" "prison-offender-events" {
   name = "cloud-platform-Digital-Prison-Services-160f3055cc4e04c4105ee85f2ed1fccb"
 }
+
+data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
+  name = "/hmpps-domain-events-${var.environment}/topic-arn"
+}
+
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+}
+
+data "aws_ssm_parameter" "hmpps-domain-events-policy-arn" {
+  name = "/hmpps-domain-events-${var.environment}/sns/${data.aws_sns_topic.hmpps-domain-events.name}/irsa-policy-arn"
+}
+
+resource "kubernetes_secret" "hmpps-domain-events-secret" {
+  metadata {
+    name      = "hmpps-domain-events-topic"
+    namespace = var.namespace
+  }
+  data = { TOPIC_ARN = data.aws_sns_topic.hmpps-domain-events.arn }
+}
