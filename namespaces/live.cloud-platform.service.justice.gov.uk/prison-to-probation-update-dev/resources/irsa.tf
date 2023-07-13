a443cc6c0b80f611ac31d27955016800b0da207b
Marcus Aspin <maspin@unilink.com>
2023-07-13 10:17:49 +0100

    PI-1281 Grant access to domain events topic (dev) (#14606)
    * PI-1281 Grant access to domain events topic (dev)
    
    * Fix topic ARN

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/irsa.tf
@@ -4,11 +4,12 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = {
+  role_policy_arns = {
     message_dynamodb  = module.message_dynamodb.irsa_policy_arn
     schedule_dynamodb = module.schedule_dynamodb.irsa_policy_arn
     sqs               = module.prison-to-probation-update-queue.irsa_policy_arn
     dlq               = module.prison-to-probation-update-dlq.irsa_policy_arn
+    sns               = data.aws_ssm_parameter.hmpps-domain-events-policy-arn.value
   }
   # Tags
   business_unit          = var.business_unit
