83d5340bd8552908df3a57c075a81a25a12486bd
Marcus Aspin <maspin@unilink.com>
2023-07-13 09:48:26 +0100

    Fix topic ARN

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-to-probation-update-dev/resources/topic.tf
@@ -2,12 +2,8 @@ data "aws_sns_topic" "prison-offender-events" {
   name = "cloud-platform-Digital-Prison-Services-f221e27fcfcf78f6ab4f4c3cc165eee7"
 }
 
-data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
-  name = "/hmpps-domain-events-${var.environment}/topic-arn"
-}
-
 data "aws_sns_topic" "hmpps-domain-events" {
-  name = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  name = "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573"
 }
 
 data "aws_ssm_parameter" "hmpps-domain-events-policy-arn" {
