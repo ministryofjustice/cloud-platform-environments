22dfb6b4214b17b21a6c3233cdfbc9d299d3aaed
Tim Wright <wright.tim.j@gmail.com>
2023-07-13 10:53:45 +0100

    NN-5295 tidyup (#14616)
    * NN-5295 tidyup
    
    * force build

--- namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
@@ -13,7 +13,7 @@ module "dps_rds" {
   db_engine                   = "postgres"
   rds_family                  = "postgres15"
   db_engine_version           = "15"
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   db_password_rotated_date    = "15-02-2023"
 
 
