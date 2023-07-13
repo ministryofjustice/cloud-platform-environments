6d8afd48f10fbd1af7df25f73d798828990134ee
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-13 13:05:39 +0100

    treat all media types as binary in api gateway

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -82,6 +82,7 @@ resource "aws_route53_record" "data" {
 resource "aws_api_gateway_rest_api" "api_gateway" {
   name                         = var.namespace
   disable_execute_api_endpoint = false
+  binary_media_types           = ["*/*"]
 
   endpoint_configuration {
     types = ["REGIONAL"]
