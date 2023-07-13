c83ddf6bb52d4fa37fd627f31371a63751b38b7c
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-13 16:55:38 +0100

    remove duplicate resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -219,18 +219,6 @@ resource "aws_api_gateway_method_settings" "all" {
   method_path = "*/*"
 
   settings {
-    metrics_enabled = true
-    logging_level   = "INFO"
-  }
-}
-
-
-resource "aws_api_gateway_method_settings" "all" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = aws_api_gateway_stage.main.stage_name
-  method_path = "*/*"
-
-  settings {
     metrics_enabled        = true
     logging_level          = "INFO"
     throttling_burst_limit = 5000
