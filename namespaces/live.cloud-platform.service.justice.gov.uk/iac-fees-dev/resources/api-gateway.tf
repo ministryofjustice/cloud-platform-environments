66f0d03031eccd7429de867b8520a0874628ca5c
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-13 16:59:06 +0100

    fix environment-name tag

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
     business-unit          = var.business_unit
     application            = var.application
     is-production          = var.is_production
-    environment-name       = var.environment_name
+    environment-name       = var.environment
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
     namespace              = var.namespace
