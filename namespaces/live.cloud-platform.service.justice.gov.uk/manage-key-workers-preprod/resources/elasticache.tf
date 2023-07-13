cc4a61da7683c9dbb6cf391aaadf63a2b9e8811a
Tim Wright <wright.tim.j@gmail.com>
2023-07-13 12:07:28 +0100

    NN-5295 tidy up + token rotate (#14621)
    * NN-5295 tidy up + token rotate
    
    * wrong date

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-preprod/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-key-workers-preprod/resources/elasticache.tf
@@ -1,17 +1,18 @@
 module "dps_redis" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=6.2.0"
-  vpc_name               = var.vpc_name
-  application            = var.application
-  environment-name       = var.environment-name
-  is-production          = var.is_production
-  infrastructure-support = var.infrastructure_support
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  number_cache_clusters  = var.number_cache_clusters
-  node_type              = var.node-type
-  engine_version         = "7.0"
-  parameter_group_name   = "default.redis7"
-  namespace              = var.namespace
+  source                  = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=6.2.0"
+  vpc_name                = var.vpc_name
+  application             = var.application
+  environment-name        = var.environment-name
+  is-production           = var.is_production
+  infrastructure-support  = var.infrastructure_support
+  team_name               = var.team_name
+  business-unit           = var.business_unit
+  number_cache_clusters   = var.number_cache_clusters
+  node_type               = var.node-type
+  engine_version          = "7.0"
+  parameter_group_name    = "default.redis7"
+  namespace               = var.namespace
+  auth_token_rotated_date = "2023-07-13"
 
   providers = {
     aws = aws.london
