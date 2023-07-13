fdf376e1af25cd1da31806ba1a25645f06046108
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-13 10:20:14 +0100

    Iac fees update api gateway iam (#14610)

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/rds-postgresql.tf
@@ -1,9 +1,3 @@
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
 module "rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
 
