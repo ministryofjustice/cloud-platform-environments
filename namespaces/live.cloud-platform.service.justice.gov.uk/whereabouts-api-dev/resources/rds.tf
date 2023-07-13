be1585057987172a79cc5358ba6d890b0e82f994
tim <wright.tim.j@gmail.com>
2023-07-13 10:05:30 +0100

    NN-5295 missing engine

--- namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/whereabouts-api-dev/resources/rds.tf
@@ -12,6 +12,7 @@ module "dps_rds" {
   db_instance_class           = "db.t4g.small"
   db_engine                   = "postgres"
   rds_family                  = "postgres15"
+  db_engine_version           = "15"
   prepare_for_major_upgrade   = true
   db_password_rotated_date    = "15-02-2023"
 
