2ce25006e02a6074561fa5271997e744f7369d39
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-07-14 16:16:50 +0100

    HAAR-1618: add elasicache redis for hmpps-authorization

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-authorization-server-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-authorization-server-dev/resources/variables.tf
@@ -53,6 +53,11 @@ variable "db_instance_class" {
 variable "db_engine_version" {
   default = "14"
 }
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
