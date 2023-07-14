81ed3e928d8836d3105b033ba61ef46f4ac92c56
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-14 16:08:33 +0100

    change aws_api_gateway_method to PUT

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -63,7 +63,7 @@ resource "aws_api_gateway_resource" "filename" {
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.filename.id
-  http_method   = "ANY"
+  http_method   = "PUT"
   authorization = "NONE"
 
   request_parameters = {
