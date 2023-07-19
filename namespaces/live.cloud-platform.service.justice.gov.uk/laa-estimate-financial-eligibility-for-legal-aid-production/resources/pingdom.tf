4bba3dfddee5f26b80be5cd4a8ecbac8c9bf722f
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-07-19 11:31:48 +0100

    New check for public beta URL (#14749)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-production/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-production/resources/pingdom.tf
@@ -15,3 +15,18 @@ resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-produ
   probefilters     = "region:EU"
   integrationids   = [125952]
 }
+
+resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-public-production" {
+  type             = "http"
+  name             = "Check if your client qualifies for legal aid - public production - ping"
+  host             = "check-your-client-qualifies-for-legal-aid.service.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  notifyagainevery = 0
+  url              = "/"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_laa,application_check-client-qualifies-for-legal-aid,component_ping,isproduction_true,environment_production,infrastructuresupport_check-client-qualifies-for-legal-aid,laa,check-client-qualifies-for-legal-aid"
+  probefilters     = "region:EU"
+  integrationids   = [125952]
+}
