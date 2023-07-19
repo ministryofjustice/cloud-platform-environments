adf7d69243ec20dc27a864089b5f71e762e7699e
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-07-19 15:21:24 +0100

    NN-5295 db engine version added

--- namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/rds.tf
@@ -10,6 +10,7 @@ module "dps_rds" {
   infrastructure-support      = var.infrastructure_support
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t4g.small"
+  db_engine_version           = "15"
   db_engine                   = "postgres"
   rds_family                  = "postgres15"
   prepare_for_major_upgrade   = true
