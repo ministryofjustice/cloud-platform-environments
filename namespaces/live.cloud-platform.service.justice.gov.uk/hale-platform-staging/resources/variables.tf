59c9e77e3a4030f96ec8ab2d3fa8a7b7addd81ee
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-07-19 15:52:00 +0100

    Hale Platform Staging - Keep old keys for now

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/variables.tf
@@ -88,3 +88,13 @@ variable "github_actions_secret_ecr_url" {
   description = "The name of the github actions secret containing the ECR URL"
   default     = "ECR_URL"
 }
+
+variable "github_actions_secret_ecr_access_key" {
+  description = "The name of the github actions secret containing the ECR AWS access key"
+  default     = "ECR_AWS_ACCESS_KEY_ID_STAGING"
+}
+
+variable "github_actions_secret_ecr_secret_key" {
+  description = "The name of the github actions secret containing the ECR AWS secret key"
+  default     = "ECR_AWS_SECRET_ACCESS_KEY_STAGING"
+}
