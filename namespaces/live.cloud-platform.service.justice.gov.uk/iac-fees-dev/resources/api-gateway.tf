1e2ab3151f8ff8c485ed45c30aac032cd9314842
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-19 14:27:08 +0100

    revert previous change

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/api-gateway.tf
@@ -48,7 +48,7 @@ resource "aws_api_gateway_rest_api_policy" "api_policy" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "{proxy}"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
