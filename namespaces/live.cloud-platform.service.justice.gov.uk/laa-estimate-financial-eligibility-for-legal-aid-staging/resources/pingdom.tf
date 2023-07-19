280fa4d3d2605bf6016f99acab8a8cba2c30c353
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-07-19 11:05:47 +0100

    Add second staging check (#14747)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-staging/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-estimate-financial-eligibility-for-legal-aid-staging/resources/pingdom.tf
@@ -15,3 +15,18 @@ resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-stagi
   probefilters     = "region:EU"
   integrationids   = [125951]
 }
+
+resource "pingdom_check" "laa-estimate-financial-eligibility-for-legal-aid-public-staging" {
+  type             = "http"
+  name             = "Check if your client qualifies for legal aid - public staging - ping"
+  host             = "staging.check-your-client-qualifies-for-legal-aid.service.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  notifyagainevery = 0
+  url              = "/"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_laa,application_check-client-qualifies-for-legal-aid,component_ping,isproduction_false,environment_staging,infrastructuresupport_check-client-qualifies-for-legal-aid,laa,check-client-qualifies-for-legal-aid"
+  probefilters     = "region:EU"
+  integrationids   = [125951]
+}
