f400ace2e4dbf83a0d178df209da948651e92307
Stephen Dicks <github@starswan.com>
2023-07-20 10:16:05 +0100

    LEP-202 - OIDC ECR change for cfe-crime

--- namespaces/live.cloud-platform.service.justice.gov.uk/cfe-crime-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfe-crime-dev/resources/ecr.tf
@@ -20,13 +20,13 @@ module "ecr_credentials" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["cfe-crime"]
+  namespace = var.namespace
 
   # list of github environments, to create the ECR secrets as environment secrets
   # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
   # github_environments = ["my-environment"]
 
-  /*
   # Lifecycle_policy provides a way to automate the cleaning up of your container images by expiring images based on age or count.
   # To apply multiple rules, combined them in one policy JSON.
   # https://docs.aws.amazon.com/AmazonECR/latest/userguide/lifecycle_policy_examples.html
@@ -75,8 +75,6 @@ module "ecr_credentials" {
     ]
 }
 EOF
-*/
-
 }
 
 
