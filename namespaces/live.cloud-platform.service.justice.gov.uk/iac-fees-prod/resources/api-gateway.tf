76b08e388824b42da77279d56f5eea7efe523a74
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-07-20 16:37:29 +0100

    fix api-gateway.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/api-gateway.tf
@@ -1,97 +1,140 @@
-# Generate an additional IAM user to manage APIGW
-resource "random_id" "apigw-id" {
-  byte_length = 16
-}
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name                         = var.namespace
+  disable_execute_api_endpoint = false
+  binary_media_types           = ["*/*"]
 
-resource "aws_iam_user" "apigw-user" {
-  name = "apigw-user-${random_id.apigw-id.hex}"
-  path = "/system/apigw-user/"
-}
-
-resource "aws_iam_access_key" "apigw-user" {
-  user = aws_iam_user.apigw-user.name
-}
-
-data "aws_iam_policy_document" "apigw" {
-  statement {
-    actions = [
-      "apigateway:*",
-    ]
-    resources = [
-      "${aws_api_gateway_rest_api.api_gateway.arn}/*",
-      aws_api_gateway_rest_api.api_gateway.arn
-    ]
-    effect = "Allow"
+  endpoint_configuration {
+    types = ["REGIONAL"]
   }
 
-  statement {
-    actions = [
-      "apigateway:*",
-    ]
-    resources = [
-      "arn:aws:apigateway:eu-west-2::/restapis"
-    ]
-    effect = "Allow"
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
   }
 }
 
-resource "aws_iam_user_policy" "apigw-policy" {
-  name   = "${var.namespace}-apigw"
-  policy = data.aws_iam_policy_document.apigw.json
-  user   = aws_iam_user.apigw-user.name
-}
-
-resource "aws_iam_role" "api_gateway_role" {
-  name               = "${var.namespace}-apigw"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "api_gw_s3" {
-  name = "${var.namespace}-apigw-s3"
-  role = aws_iam_role.api_gateway_role.name
+resource "aws_api_gateway_rest_api_policy" "api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
   policy = <<EOF
-{
-  "Version" : "2012-10-17",
-  "Statement" : [
-    {
-      "Sid": "AllowPutObject",
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": "arn:aws:execute-api:eu-west-2:754256621582:${aws_api_gateway_rest_api.api_gateway.id}/*"
+      },
+      {
       "Effect": "Allow",
+      "Principal": "*",
       "Action": "s3:PutObject",
-
       "Resource": [
         "${module.s3_bucket.bucket_arn}/*",
         "${module.s3_bucket.bucket_arn}"
       ]
     }
-  ]
-}
-EOF
+    ]
+  }
+  EOF
 }
 
-resource "kubernetes_secret" "iac_fees_apigw_iam" {
-  metadata {
-    name      = "apigateway-iam"
-    namespace = var.namespace
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "PUT"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "proxy_http_proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  type                    = "AWS"
+  integration_http_method = "PUT"
+  uri                     = "arn:aws:apigateway:eu-west-2:s3:path/${module.s3_bucket.bucket_name}/{proxy}"
+
+  credentials = aws_iam_role.api_gateway_role.arn
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
   }
 
-  data = {
-    access_key_id     = aws_iam_access_key.apigw-user.id
-    secret_access_key = aws_iam_access_key.apigw-user.secret
-    invoke_url        = aws_api_gateway_deployment.main.invoke_url
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "api_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "api_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.api_method_response.status_code
+  response_templates = {
+    "application/json" = ""
+  }
+  depends_on = [
+    aws_api_gateway_integration.proxy_http_proxy
+  ]
+}
+
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+
+  #hack to force recreate of the deployment resource
+  stage_description = md5(file("api-gateway.tf"))
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.proxy_http_proxy
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "live" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  stage_name    = "upload"
+}
+
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = aws_api_gateway_stage.live.stage_name
+  method_path = "*/*"
+  settings {
+    metrics_enabled        = true
+    logging_level          = "INFO"
+    throttling_burst_limit = 5000
+    throttling_rate_limit  = 10000
+    data_trace_enabled     = false
   }
 }
