35d1d8d4d0a9806089659aff63796ed0af7f9514
AndySweilam <AndySweilam@users.noreply.github.com>
2023-07-20 13:02:59 +0100

    WFP-1542: edit health url for workforce-management.

--- namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-prod/resources/pingdom.tf
@@ -9,7 +9,7 @@ resource "pingdom_check" "workforce-management-prod" {
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/health"
+  url                      = "/https/workforce-management.hmpps.service.justice.gov.uk"
   encryption               = true
   port                     = 443
   tags                     = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
