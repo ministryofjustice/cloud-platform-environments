991c7ff181088f3ec684d16182c5257d929d4fbd
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-07-20 10:07:34 +0100

    NN-5295 changed postfix db upgrade changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/rds.tf
@@ -13,7 +13,7 @@ module "dps_rds" {
   db_engine_version           = "15"
   db_engine                   = "postgres"
   rds_family                  = "postgres15"
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   db_password_rotated_date    = "15-02-2023"
 
   providers = {
