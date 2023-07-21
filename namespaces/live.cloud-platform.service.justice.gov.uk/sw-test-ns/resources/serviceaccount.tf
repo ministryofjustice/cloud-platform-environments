ecbf82bf24a1cfaae0991ee7282db3f7233015ca
Steve Williams <105657964+sj-williams@users.noreply.github.com>
2023-07-21 16:29:28 +0100

    switch to test module for sa (#14817)

--- namespaces/live.cloud-platform.service.justice.gov.uk/sw-test-ns/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/sw-test-ns/resources/serviceaccount.tf
@@ -4,7 +4,7 @@
  *
 */
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.8.2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=token-update"
 
   namespace           = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
