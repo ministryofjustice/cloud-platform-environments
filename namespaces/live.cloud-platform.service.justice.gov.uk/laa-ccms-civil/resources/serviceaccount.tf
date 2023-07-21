470e24f02b0659ed5073cebc7eb4e4ea5996ca6e
keith-h <keith.humphreys@digital.justice.gov.uk>
2023-07-21 17:16:58 +0100

    chore(*): add closing brace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/serviceaccount.tf
@@ -1,9 +1,10 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.8.2"
 
-  namespace = var.namespace
+  namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["laa-ccms-caab","laa-ccms-data-api","laa-ccms-soa-gateway-api"]
+  github_repositories = ["laa-ccms-caab", "laa-ccms-data-api", "laa-ccms-soa-gateway-api"]
+}
