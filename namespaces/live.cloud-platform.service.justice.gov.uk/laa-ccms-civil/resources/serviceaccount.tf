1803d2afa9f572b80db79c98170ec2ac8fed3bf2
keith-h <keith.humphreys@digital.justice.gov.uk>
2023-07-21 17:10:40 +0100

    fix(ccls-1704): remove prepopulated variables from service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/serviceaccount.tf
@@ -7,8 +7,3 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["laa-ccms-caab","laa-ccms-data-api","laa-ccms-soa-gateway-api"]
-  github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
-  github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
-  github_actions_secret_kube_token     = var.github_actions_secret_kube_token
-  github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
-}
