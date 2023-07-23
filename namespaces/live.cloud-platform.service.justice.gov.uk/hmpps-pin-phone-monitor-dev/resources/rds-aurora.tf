735242922fcf22e69d2416ecaaab8ffc8780a2a6
jakemulley <jakemulley@users.noreply.github.com>
2023-07-23 16:12:48 +0100

    rds-aurora: bump to v2.4.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/rds-aurora.tf
@@ -1,25 +1,29 @@
 module "rds_aurora" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=2.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-aurora?ref=2.4.0"
 
-  team_name                   = var.team_name
-  business-unit               = var.business_unit
-  application                 = var.application
-  is-production               = var.is_production
-  namespace                   = var.namespace
-  environment-name            = var.environment-name
-  infrastructure-support      = var.infrastructure_support
-  engine                      = "aurora-postgresql"
-  engine_version              = "14.6"
-  engine_mode                 = "provisioned"
-  replica_count               = 1
-  instance_type               = "db.t4g.medium"
-  snapshot_identifier         = "arn:aws:rds:eu-west-2:754256621582:snapshot:hmpps-pin-phone-dev-pre-migration-20210727-1400"
-  storage_encrypted           = true
-  apply_immediately           = true
-  vpc_name                    = var.vpc_name
+  # VPC configuration
+  vpc_name = var.vpc_name
+
+  # Database configuration
+  engine         = "aurora-postgresql"
+  engine_version = "14.6"
+  engine_mode    = "provisioned"
+  instance_type  = "db.t4g.medium"
+  replica_count  = 1
+
   allow_major_version_upgrade = true
+  snapshot_identifier         = "arn:aws:rds:eu-west-2:754256621582:snapshot:hmpps-pin-phone-dev-pre-migration-20210727-1400"
   auto_minor_version_upgrade  = true
   skip_setting_when_migrated  = true
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
 
   providers = {
     aws = aws.london
