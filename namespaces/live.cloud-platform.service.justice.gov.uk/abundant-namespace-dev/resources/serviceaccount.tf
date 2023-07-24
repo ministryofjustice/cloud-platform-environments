cb660d7b730ee8655294b07df24e65f2ad5f97b9
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-07-24 14:50:14 +0100

    fix: 🐛 wip (#14843)

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
@@ -1,5 +1,5 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=output-update"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.1"
 
   namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
