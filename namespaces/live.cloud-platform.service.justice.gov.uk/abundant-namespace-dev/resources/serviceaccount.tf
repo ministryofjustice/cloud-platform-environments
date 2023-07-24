86b2112c6738312ae63cc197e33b5303da5fed71
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-07-24 11:53:31 +0100

    fix: 🐛 wip

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
@@ -7,5 +7,5 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["my-repo"]
-  # test-changes
+  # test-changes 1
 }
