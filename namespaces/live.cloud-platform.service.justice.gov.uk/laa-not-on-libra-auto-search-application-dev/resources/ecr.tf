d051863fe955d816452eb5d4ce258870d5a4c075
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-07-24 14:34:26 +0100

    Adds ECR to namespace laa-not-on-libra-auto-search-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-dev/resources/ecr.tf
@@ -0,0 +1,13 @@
+module "ecr" {
+  ...
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["circleci"]
+
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["laa-nolasa"]
+
+  # This is the namespace to create a ConfigMap
+  # of credentials you need in CircleCI
+  namespace = var.namespace
+  ...
+}
