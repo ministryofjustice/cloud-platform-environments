d076a351ca6f2c5b92dfec9e80397bc755d5c6c5
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-07-24 14:45:24 +0100

    Missing the module def.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-dev/resources/ecr.tf
@@ -1,13 +1,14 @@
 module "ecr" {
-  ...
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
+
+  # REQUIRED: Repository configuration
+  team_name = var.team_name
+  repo_name = var.namespace
+  namespace = var.namespace
+
   # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
   oidc_providers = ["circleci"]
 
   # REQUIRED: GitHub repositories that push to this container repository
   github_repositories = ["laa-nolasa"]
-
-  # This is the namespace to create a ConfigMap
-  # of credentials you need in CircleCI
-  namespace = "laa-not-on-libra-auto-search-application-dev"
-  ...
 }
