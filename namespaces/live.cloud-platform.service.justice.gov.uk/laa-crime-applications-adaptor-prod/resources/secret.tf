21e59db4cbd2c66469d3365347d7ecf5f811f1e4
liam.murphy <liam.murphy@digital.justice.gov.uk>
2023-07-25 16:33:40 +0100

    LASB-2237 - crime applications adaptor Slack webhook secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-applications-adaptor-prod/resources/secret.tf
@@ -0,0 +1,20 @@
+module "secrets_manager" {
+  source = "../"
+  // source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=1.2.0"
+  team_name               = var.team_name
+  application             = var.application
+  business_unit           = var.business_unit
+  is_production           = var.is_production
+  namespace               = var.namespace
+  environment             = var.environment
+  infrastructure_support  = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "crime-apps-adaptor-alert-webhook-prod" = {
+      description             = "Crime Applications Adaptor Webhook",
+      recovery_window_in_days = 7,
+      k8s_secret_name        = "crime-apps-adaptor-alert-webhook-prod"
+    },
+  }
+}
