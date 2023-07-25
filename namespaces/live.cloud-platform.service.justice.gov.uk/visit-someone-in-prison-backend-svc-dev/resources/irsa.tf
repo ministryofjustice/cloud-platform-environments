5e55aabb18b17d6c7f527f9cebc62b63c63a829f
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-07-25 16:47:12 +0100

    VB-2564 Orchestration service, Update SQS/SNS queue updates

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/irsa.tf
@@ -1,8 +1,34 @@
-module "app-irsa" {
-  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+# Add the names of the SQS/SNS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS/SNS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_prison_visits_event_queue" = "hmpps_prison_visits_event_queue",
+    "Digital-Prison-Services-dev-hmpps_prison_visits_event_dlq" = "hmpps_prison_visits_event_dlq",
+  }
+  sns_queues = {
+    "Digital-Prison-Services-dev-hmpps_domain_events_dev"    = "hmpps-domain-events-dev",
+  }
+
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_queues
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = { sns = data.aws_ssm_parameter.irsa_policy_arns_sns.value }
+  role_policy_arns     = merge(local.sqs_policies,local.sns_policies)
 
   # Tags
   business_unit          = var.business_unit
@@ -12,8 +38,4 @@ module "app-irsa" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
   eks_cluster_name       = var.eks_cluster_name
-}
-
-data "aws_ssm_parameter" "irsa_policy_arns_sns" {
-  name = "/hmpps-domain-events-dev/sns/cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573/irsa-policy-arn"
 }
