96b5326900640f7006efa5091692ea5b941f8710
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-07-25 10:09:06 +0100

    Add IRSA config (#14803)

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/service-account-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/service-account-github.tf
@@ -2,7 +2,7 @@ module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.4"
 
   namespace          = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
+  kubernetes_cluster = var.eks_cluster_name
 
   github_repositories = [var.repo_name]
 
