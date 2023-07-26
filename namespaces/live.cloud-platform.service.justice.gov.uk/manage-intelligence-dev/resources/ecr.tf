7f1264441ea7b50decb9ba12a6b3b8de4ca18f06
brian.carr <brian.carr@digital.justice.gov.uk>
2023-07-26 15:33:25 +0100

    corrected module ref

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/ecr.tf
@@ -15,9 +15,9 @@ resource "kubernetes_secret" "manage_intelligence_ecr" {
   }
 
   data = {
-    access_key_id     = module.manage_intelligence_ecr.access_key_id
-    secret_access_key = module.manage_intelligence_ecr.secret_access_key
-    repo_arn          = module.manage_intelligence_ecr.repo_arn
-    repo_url          = module.manage_intelligence_ecr.repo_url
+    access_key_id     = module.ecr-repo.access_key_id
+    secret_access_key = module.ecr-repo.secret_access_key
+    repo_arn          = module.ecr-repo.repo_arn
+    repo_url          = module.ecr-repo.repo_url
   }
 }
