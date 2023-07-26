a5166d7a347d1774da92d7e6fccd01fd6dbbc52a
brian.carr <brian.carr@digital.justice.gov.uk>
2023-07-26 15:25:24 +0100

    update to ecr to add output

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/ecr.tf
@@ -7,3 +7,17 @@ module "ecr-repo" {
   oidc_providers      = ["github"]
   github_repositories = ["hmpps-mercury-data-monorepo"]
 }
+
+resource "kubernetes_secret" "manage_intelligence_ecr" {
+  metadata {
+    name      = "manage-intelligence-ecr-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    access_key_id     = module.manage_intelligence_ecr.access_key_id
+    secret_access_key = module.manage_intelligence_ecr.secret_access_key
+    repo_arn          = module.manage_intelligence_ecr.repo_arn
+    repo_url          = module.manage_intelligence_ecr.repo_url
+  }
+}
