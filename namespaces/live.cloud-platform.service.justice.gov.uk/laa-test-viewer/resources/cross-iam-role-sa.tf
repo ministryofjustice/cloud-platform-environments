af3fab56128baa7865577054c1c5481d7078b018
Anil Dhiman <anil.dhiman@digital.justice.gov.uk>
2023-07-27 13:52:04 +0100

    LCAM-830 added cross-irsa version ref

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-test-viewer/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-test-viewer/resources/cross-iam-role-sa.tf
@@ -1,5 +1,5 @@
 module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
   namespace        = var.namespace
   eks_cluster_name = var.eks_cluster_name
   service_account_name = "${var.namespace}-sa"
