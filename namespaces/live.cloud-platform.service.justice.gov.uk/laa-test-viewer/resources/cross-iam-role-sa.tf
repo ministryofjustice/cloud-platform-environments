fe9ed97c5964bba8aee87004414b28ca1ff9c73e
Anil Dhiman <anil.dhiman@digital.justice.gov.uk>
2023-07-27 13:48:53 +0100

    LCAM-830 added cross-irsa

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-test-viewer/resources/cross-iam-role-sa.tf
@@ -0,0 +1,48 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
+  namespace        = var.namespace
+  eks_cluster_name = var.eks_cluster_name
+  service_account_name = "${var.namespace}-sa"
+  role_policy_arns = [aws_iam_policy.allow_s3_access_policy.arn]
+}
+
+data "aws_iam_policy_document" "allow_s3_access_policy" {
+  # Provide list of permissions and target AWS account resources to allow access to
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectAcl"
+    ]
+    resources = [
+      "arn:aws:s3:::s3-bucket-output/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "allow_s3_access_policy" {
+  name   = "allow_s3_access_policy"
+  policy = data.aws_iam_policy_document.allow_s3_access_policy.json
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "irsa-output"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.aws_iam_role_name
+    serviceaccount = module.irsa.service_account_name.name
+  }
+}
