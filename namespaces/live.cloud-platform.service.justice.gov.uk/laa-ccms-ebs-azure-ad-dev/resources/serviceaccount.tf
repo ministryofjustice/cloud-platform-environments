a2377fe6d79a05b93a1a1863956e26857df12f79
sj-williams <steve.williams@digital.justice.gov.uk>
2023-07-27 13:03:25 +0100

    bump sa module to latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-ebs-azure-ad-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-ebs-azure-ad-dev/resources/serviceaccount.tf
@@ -1,6 +1,6 @@
 module "serviceaccount" {
 
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
 
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
--- namespaces/live.cloud-platform.service.justice.gov.uk/sw-test-ns/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/sw-test-ns/resources/serviceaccount.tf
@@ -4,7 +4,7 @@
  *
 */
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
 
   namespace           = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
