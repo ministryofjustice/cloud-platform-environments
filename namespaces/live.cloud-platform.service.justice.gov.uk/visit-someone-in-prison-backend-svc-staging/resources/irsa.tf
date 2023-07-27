e4e7d6725e63e5f100b2d0561c5fb50f227d1208
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-07-27 14:36:04 +0100

    VB-2572 STAGING - Orchestration service, Update SQS/SNS queue updates IRSA, update to see if CI passes

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/irsa.tf
@@ -4,12 +4,12 @@
 locals {
 
   sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev",
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
 
   sqs_policies = {
-    module.hmpps_prison_visits_event_queue.sqs_name = module.hmpps_prison_visits_event_queue.irsa_policy_arn
-    module.hmpps_prison_visits_event_dead_letter_queue.sqs_name = module.hmpps_prison_visits_event_dead_letter_queue.irsa_policy_arn
+    hmpps_prison_visits_event_queue = module.hmpps_prison_visits_event_queue.irsa_policy_arn
+    hmpps_prison_visits_event_dlq = module.hmpps_prison_visits_event_dead_letter_queue.irsa_policy_arn
   }
 
   sns_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value}
