ad560bd7ecf0d864b25995873c1e7f05ebe98d3c
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-07-28 15:13:24 +0100

    add irsa module to hmpps-tier dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/irsa.tf
@@ -0,0 +1,21 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name      = var.eks_cluster_name
+  service_account_name  = var.application
+  namespace             = var.namespace
+
+  role_policy_arns = {
+    sqs = module.hmpps_tier_domain_events_queue.irsa_policy_arn
+    dlq = module.hmpps_tier_domain_events_dead_letter_queue.irsa_policy_arn
+    sns = data.aws_ssm_parameter.hmpps-domain-events-policy-arn.value
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
