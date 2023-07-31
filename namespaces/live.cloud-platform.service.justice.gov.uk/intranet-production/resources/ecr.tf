2eb0856a8c459636d6192d8ef97f6196ab3fec26
digitalali-moj <javid.ali@digital.justice.gov.uk>
2023-07-31 11:36:25 +0100

    intranet-production add image deletion policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-production/resources/ecr.tf
@@ -19,8 +19,56 @@ module "intranet_ecr_credentials" {
 
   # specify which GitHub repository you're pushing from
   github_repositories = [var.app_repo]
-}
 
+ # Lifecycle policies
+  # Uncomment the below to automatically tidy up old Docker images
+  lifecycle_policy = <<EOF
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Expire untagged images older than 14 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 14
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 2,
+          "description": "Keep last 30 dev and staging images",
+          "selection": {
+            "tagStatus": "tagged",
+            "tagPrefixList": ["dev", "staging"],
+            "countType": "imageCountMoreThan",
+            "countNumber": 30
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 3,
+          "description": "Keep the newest 100 images and mark the rest for expiration",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 100,
+            "tagPrefixList": ["production"],
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+    EOF
+}
+/*
 resource "kubernetes_secret" "intranet_ecr_credentials" {
   metadata {
     name      = "intranet-ecr-credentials-output"
@@ -33,4 +81,4 @@ resource "kubernetes_secret" "intranet_ecr_credentials" {
     repo_arn          = module.intranet_ecr_credentials.repo_arn
     repo_url          = module.intranet_ecr_credentials.repo_url
   }
-}
+}*/
