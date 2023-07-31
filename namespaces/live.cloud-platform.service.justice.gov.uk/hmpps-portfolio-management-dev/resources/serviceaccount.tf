c00b6a9424ca96b93cebfe89b24ecca8326ff2c7
Matt <34448412+mattops@users.noreply.github.com>
2023-07-31 14:09:24 +0100

    hmpps-portfolio-management-dev: Add service account, redis, and rds. (#15084)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/serviceaccount.tf
@@ -0,0 +1,14 @@
+
+module "service_account" {
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
+  namespace                         = var.namespace
+  kubernetes_cluster                = var.kubernetes_cluster
+  serviceaccount_name               = "hmpps-portfolio-management"
+  github_environments               = [var.environment]
+  github_repositories               = ["hmpps-service-catalogue", "hmpps-health-ping", "hmpps-developer-portal"]
+  serviceaccount_token_rotated_date = time_rotating.weekly.unix
+}
+
+resource "time_rotating" "weekly" {
+  rotation_days = 7
+}
