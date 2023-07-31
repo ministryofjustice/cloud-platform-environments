b3844c41e827ad2de245cfd822dd294e57869a3d
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-07-31 16:08:03 +0100

    hmpps-portfolio-management-dev: Add github environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/serviceaccount.tf
@@ -1,14 +1,34 @@
+local {
+  github_repos = ["hmpps-service-catalogue", "hmpps-health-ping", "hmpps-developer-portal"]
+}
 
 module "service_account" {
-  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
-  namespace                         = var.namespace
-  kubernetes_cluster                = var.kubernetes_cluster
-  serviceaccount_name               = "hmpps-portfolio-management"
-  github_environments               = [var.environment]
-  github_repositories               = ["hmpps-service-catalogue", "hmpps-health-ping", "hmpps-developer-portal"]
-  serviceaccount_token_rotated_date = time_rotating.weekly.unix
+  source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
+  namespace                            = var.namespace
+  kubernetes_cluster                   = var.kubernetes_cluster
+  serviceaccount_name                  = "hmpps-portfolio-management"
+  github_environments                  = [github_repository_environment.env.name]
+  github_repositories                  = local.github_repos
+  github_actions_secret_kube_cert      = "${var.environment}-KUBE_CERT"
+  github_actions_secret_kube_token     = "${var.environment}-KUBE_TOKEN"
+  github_actions_secret_kube_cluster   = "${var.environment}-KUBE_CLUSTER"
+  github_actions_secret_kube_namespace = "${var.environment}-KUBE_NAMESPACE"
+  serviceaccount_token_rotated_date    = time_rotating.weekly.unix
 }
 
 resource "time_rotating" "weekly" {
   rotation_days = 7
+}
+
+resource "github_repository_environment" "env" {
+  for_each    = toset(local.github_repos)
+  environment = var.environment
+  repository  = each.key
+  reviewers {
+    teams = ["dps-tech"]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
 }
