d1bc104637ab415c52b7d7e0e6ada3d295c0454e
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-07-31 13:35:45 +0100

    terraform fmt

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/serviceaccount.tf
@@ -1,11 +1,11 @@
 
 module "service_account" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
-  namespace          = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
-  serviceaccount_name  = "hmpps-portfolio-management"
-  github_environments = [var.environment]
-  github_repositories = ["hmpps-service-catalogue", "hmpps-health-ping", "hmpps-developer-portal"]
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.6"
+  namespace                         = var.namespace
+  kubernetes_cluster                = var.kubernetes_cluster
+  serviceaccount_name               = "hmpps-portfolio-management"
+  github_environments               = [var.environment]
+  github_repositories               = ["hmpps-service-catalogue", "hmpps-health-ping", "hmpps-developer-portal"]
   serviceaccount_token_rotated_date = time_rotating.weekly.unix
 }
 
