37798f4c0e39ac2455bdce9b66bf7eb2ae0ad44a
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-08-01 15:30:07 +0100

    update sns irsa policy for hmpps-tier in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/irsa.tf
@@ -1,3 +1,10 @@
+locals {
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+}
+
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
@@ -8,7 +15,7 @@ module "irsa" {
   role_policy_arns = {
     sqs = module.hmpps_tier_domain_events_queue.irsa_policy_arn
     dlq = module.hmpps_tier_domain_events_dead_letter_queue.irsa_policy_arn
-    sns = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+    sns = local.sns_policies
   }
 
   # Tags
@@ -18,4 +25,9 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
