a052de3460cc554d84759189a114f4f6aaf36a9e
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-08-01 13:03:21 +0100

    fixes to namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/ap-firebreak-superset/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ap-firebreak-superset/resources/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = "ap-firebreak-superset-admin-team"
+      GithubTeam    = "analytics-hq"
     }
   }
 }
@@ -23,7 +23,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = "ap-firebreak-superset-admin-team"
+      GithubTeam    = "analytics-hq"
     }
   }
 }
@@ -36,7 +36,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = "ap-firebreak-superset-admin-team"
+      GithubTeam    = "analytics-hq"
     }
   }
 }
--- namespaces/live.cloud-platform.service.justice.gov.uk/ap-firebreak-superset/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ap-firebreak-superset/resources/variables.tf
@@ -32,7 +32,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "Analytical Platform"
+  default     = "analytics-hq"
 }
 
 variable "environment" {
