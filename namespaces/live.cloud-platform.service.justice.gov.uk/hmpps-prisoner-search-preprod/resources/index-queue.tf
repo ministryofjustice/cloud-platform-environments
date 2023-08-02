b716edc86210c8f639fc7aa80fe910820f239073
Peter Phillips <peter@greenthistle.co.uk>
2023-08-02 16:42:06 +0100

    SDIT-987: ✨ New preprod prisoner search environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-preprod/resources/index-queue.tf
@@ -0,0 +1,63 @@
+module "hmpps_prisoner_search_index_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.11.0"
+
+  environment-name          = var.environment
+  team_name                 = var.team_name
+  infrastructure-support    = var.infrastructure_support
+  application               = var.application
+  sqs_name                  = "hmpps_prisoner_search_index_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+  namespace                 = var.namespace
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.hmpps_prisoner_search_index_dead_letter_queue.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "hmpps_prisoner_search_index_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.11.0"
+
+  environment-name       = var.environment
+  team_name              = var.team_name
+  infrastructure-support = var.infrastructure_support
+  application            = var.application
+  sqs_name               = "hmpps_prisoner_search_index_queue_dl"
+  encrypt_sqs_kms        = "true"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_prisoner_search_index_queue" {
+  metadata {
+    name      = "prisoner-search-indexer-queue"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url       = module.hmpps_prisoner_search_index_queue.sqs_id
+    sqs_queue_arn       = module.hmpps_prisoner_search_index_queue.sqs_arn
+    sqs_queue_name      = module.hmpps_prisoner_search_index_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "hmpps_prisoner_search_index_dead_letter_queue" {
+  metadata {
+    name      = "prisoner-search-indexer-dlq"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url       = module.hmpps_prisoner_search_index_dead_letter_queue.sqs_id
+    sqs_queue_arn       = module.hmpps_prisoner_search_index_dead_letter_queue.sqs_arn
+    sqs_queue_name      = module.hmpps_prisoner_search_index_dead_letter_queue.sqs_name
+  }
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-preprod/resources/opensearch.tf
@@ -0,0 +1,75 @@
+module "hmpps_prisoner_search_opensearch" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=snapshot-management"
+  application            = var.application
+  business_unit          = var.business_unit
+  eks_cluster_name       = var.eks_cluster_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+  vpc_name               = var.vpc_name
+
+  engine_version = "OpenSearch_2.7"
+
+  cluster_config = {
+    instance_count = 2
+    instance_type  = "t3.small.search"
+  }
+
+  ebs_options = {
+    volume_size = 20
+  }
+  snapshot_bucket_arn = module.os_snapshots_s3_bucket.bucket_arn
+}
+
+module "os_snapshots_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "os_snapshots_role" {
+  metadata {
+    name      = "os-snapshot-role"
+    namespace = var.namespace
+  }
+
+  data = {
+    snapshot_role_arn = module.hmpps_prisoner_search_opensearch.snapshot_role_arn
+  }
+}
+
+resource "kubernetes_secret" "os_snapshots" {
+  metadata {
+    name      = "os-snapshot-bucket"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn  = module.os_snapshots_s3_bucket.bucket_arn
+    bucket_name = module.os_snapshots_s3_bucket.bucket_name
+  }
+}
+
+resource "kubernetes_secret" "opensearch" {
+  metadata {
+    name      = "opensearch"
+    namespace = var.namespace
+  }
+
+  data = {
+    url = module.hmpps_prisoner_search_opensearch.proxy_url
+  }
+}
