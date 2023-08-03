cffd598bf2c6a9da36663c85cedda29814336252
jakemulley <jakemulley@users.noreply.github.com>
2023-08-03 11:07:29 +0100

    justicedata-(staging|test): remove container repository access keys

--- namespaces/live.cloud-platform.service.justice.gov.uk/justicedata-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justicedata-test/resources/ecr.tf
@@ -1,8 +1,18 @@
 module "ecr" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
-  team_name              = var.team_name
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.0.0"
+
   repo_name              = "${var.namespace}-ecr"
-  github_repositories    = ["justice-data"]
+
   oidc_providers         = ["github"]
+  github_repositories    = ["justice-data"]
   github_actions_prefix  = "test"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
