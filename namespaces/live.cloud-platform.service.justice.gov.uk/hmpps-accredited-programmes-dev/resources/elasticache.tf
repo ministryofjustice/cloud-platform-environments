1818f2a71f54a27a13a52793f01ad01b70fa677c
George Eaton <george@dxw.com>
2023-08-03 10:36:50 +0100

    Update Elasticache for Redis to v7
    for `hmpps-accredited-programmes-dev`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/elasticache.tf
@@ -15,8 +15,8 @@ module "elasticache_redis" {
 
   number_cache_clusters = var.number_cache_clusters
   node_type             = "cache.t4g.small"
-  engine_version        = "6.x"
-  parameter_group_name  = "default.redis6.x"
+  engine_version        = "7.0"
+  parameter_group_name  = "default.redis7"
 
   providers = {
     aws = aws.london
