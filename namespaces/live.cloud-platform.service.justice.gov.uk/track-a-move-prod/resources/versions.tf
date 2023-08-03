1eca2309bdee4ca69a9afd0f0bf8309b43270389
jakemulley <jakemulley@users.noreply.github.com>
2023-08-03 09:14:52 +0100

    chore: bump cyrilgdn/postgresql, hashicorp/helm, hashicorp/http, hashicorp/random and lagging aws/kubernetes providers

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-move-prod/resources/versions.tf
@@ -11,7 +11,7 @@ terraform {
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 3.3.0"
+      version = "~> 3.4.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
