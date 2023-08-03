eb4800ae9c4cf761ca2b476a56e84a6765a71e52
jakemulley <jakemulley@users.noreply.github.com>
2023-08-03 09:55:30 +0100

    try main

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/opensearch.tf
@@ -1,12 +1,25 @@
+# module "opensearch_snapshot_bucket" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
+
+#   team_name              = var.team_name
+#   business-unit          = var.business_unit
+#   application            = var.application
+#   is-production          = var.is_production
+#   environment-name       = var.environment
+#   infrastructure-support = var.infrastructure_support
+#   namespace              = var.namespace
+# }
+
 module "opensearch" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=dependabot//terraform//irsa--github--ministryofjustice//cloud-platform-terraform-irsa--1.1.0-2.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=main"
 
   # VPC/EKS configuration
   vpc_name         = var.vpc_name
   eks_cluster_name = var.eks_cluster_name
 
   # Cluster configuration
-  engine_version = "OpenSearch_2.5"
+  engine_version      = "OpenSearch_2.5"
+  # snapshot_bucket_arn = module.opensearch_snapshot_bucket.bucket_arn
 
   cluster_config = {
     instance_count = 2
