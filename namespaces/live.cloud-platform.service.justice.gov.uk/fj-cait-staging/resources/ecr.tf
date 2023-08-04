12409a68084c6ab549337e7dca70d8f464d58a24
digitalali-moj <javid.ali@digital.justice.gov.uk>
2023-08-04 18:55:32 +0100

    fj-cait-staging add oidc ref

--- namespaces/live.cloud-platform.service.justice.gov.uk/fj-cait-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/fj-cait-staging/resources/ecr.tf
@@ -1,9 +1,11 @@
 module "ecr-repo" {
+
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
 
   team_name = var.team_name
   repo_name = var.repo_name
-
+  
+  oidc_providers = ["github"]
   github_repositories = [var.repo_name]
 }
 
