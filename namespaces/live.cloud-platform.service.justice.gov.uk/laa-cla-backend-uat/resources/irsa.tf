5589bda3f90fa53844dcf9a247eaf99ec4b32081
said-moj <45761276+said-moj@users.noreply.github.com>
2023-08-07 15:25:25 +0100

    Adding sqs to irsa role_policy_arns

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-uat/resources/irsa.tf
@@ -15,8 +15,9 @@ module "irsa" {
     rds_cla_backend_rds_postgres_11    = module.cla_backend_rds_postgres_11.irsa_policy_arn
     rds_cla_backend_rds_postgres_14    = module.cla_backend_rds_postgres_14.irsa_policy_arn
     s3_cla_backend_private_reports_bucket     = module.cla_backend_private_reports_bucket.irsa_policy_arn
-    cla_backend_deleted_objects_bucket     = module.cla_backend_deleted_objects_bucket.irsa_policy_arn
-    cla_backend_static_files_bucket     = module.cla_backend_static_files_bucket.irsa_policy_arn
+    s3_cla_backend_deleted_objects_bucket     = module.cla_backend_deleted_objects_bucket.irsa_policy_arn
+    s3_cla_backend_static_files_bucket     = module.cla_backend_static_files_bucket.irsa_policy_arn
+    sqs_laa_cla_backend_uat_sqs = module.laa_cla_backend_uat_sqs.irsa_policy_arn
   }
 
   # Tags
