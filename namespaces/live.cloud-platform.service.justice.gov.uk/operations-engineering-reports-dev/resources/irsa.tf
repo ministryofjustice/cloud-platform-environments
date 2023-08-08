7c08fc5f3332eae9475cc4512631f4e8622592c6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-08-08 13:43:38 +0100

    :green_heart: Use short-lived credentials in ECR configuration

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-reports-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-reports-dev/resources/irsa.tf
@@ -12,7 +12,7 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    dynamo  = module.opseng_reports.irsa_policy_arn
+    dynamo = module.opseng_reports.irsa_policy_arn
   }
 
   # Tags
