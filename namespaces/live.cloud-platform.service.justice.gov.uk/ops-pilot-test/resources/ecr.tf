e6759ad87b6b2ae65219281aee28eb9424d87677
William Martin <william.martin1@justice.gov.uk>
2023-08-08 11:26:40 +0100

    Remove ECR protection from ops-pilot-test
    In preparation for removing the ops-pilot-test namespace, remove the ECR deletion protection.

--- namespaces/live.cloud-platform.service.justice.gov.uk/ops-pilot-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ops-pilot-test/resources/ecr.tf
@@ -6,6 +6,7 @@
  */
 module "ecr_credentials" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
+  deletion_protection = false
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
 
