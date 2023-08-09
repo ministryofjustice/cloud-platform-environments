f6dd61b2d43aa7f30e31dfbf5041f018420771c8
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-08-09 13:11:14 +0100

    update queues in irsa config for workforce-management-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-prod/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-prod/irsa.tf
@@ -1,7 +1,7 @@
 locals {
   sqs_queues = {
-    "Digital-Prison-Services-prod-hmpps-allocations-offender-events-sqs-instance-output" = "offender_events-prod"
-    "Digital-Prison-Services-prod-hmpps-allocations-offender-events-sqs-dl-instance-output" = "offender_events-prod"
+    "Digital-Prison-Services-prod-hmpps_allocations_offender_events_queue" = "offender_events-prod"
+    "Digital-Prison-Services-prod-hmpps_allocations_offender_events_dead_letter_queue" = "offender_events-prod"
   }
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08" = "hmpps-domain-events-prod"
