ddede31970e025373b74ef16a4cb3e43ba899e53
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-10 16:25:02 +0100

    update to bucket policy condition

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
@@ -67,7 +67,7 @@ module "manage_intelligence_logging_bucket" {
         "Condition": {
             "ArnLike": {
                 "aws:SourceArn": 
- ["${module.manage_intelligence_extractor_bucket.bucket_arn}", "${module.manage_intelligence_transformer_bucket.bucket_arn}"]
+ ["${module.manage_intelligence_extractor_bucket.bucket_arn}/*", "${module.manage_intelligence_transformer_bucket.bucket_arn}/*"]
             }
         }
     }
