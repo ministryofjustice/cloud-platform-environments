06343f844eb5bc03201d21369ada964b5a17678d
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-10 15:43:54 +0100

    adding bucket policy for s3 logging

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
@@ -41,7 +41,7 @@ module "manage_intelligence_transformer_bucket" {
 module "manage_intelligence_logging_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
   team_name              = var.team_name
-  acl                    =  "log-delivery-write"
+  acl                    = "private"
   versioning             = false
   business-unit          = var.business_unit
   application            = var.application
@@ -49,6 +49,32 @@ module "manage_intelligence_logging_bucket" {
   environment-name       = var.environment
   infrastructure-support = var.infrastructure_support
   namespace              = var.namespace
+
+  bucket_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Sid": "S3ServerAccessLogsPolicy",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "logging.s3.amazonaws.com"
+        },
+        "Action": [
+            "s3:PutObject"
+        ],
+        "Resource": "$${bucket_arn}",
+        "Condition": {
+            "ArnLike": {
+                "aws:SourceArn": "$${module.manage_intelligence_extractor_bucket.bucket_arn}",
+                "aws:SourceArn": "$${module.manage_intelligence_transformer_bucket.bucket_arn}"
+            }
+        }
+    }
+  ]
+}
+EOF
+
 
   providers = {
     aws = aws.london
@@ -63,8 +89,8 @@ resource "kubernetes_secret" "ims_extractor_s3_bucket" {
   }
 
   data = {
-    bucket_arn        = module.manage_intelligence_extractor_bucket.bucket_arn
-    bucket_name       = module.manage_intelligence_extractor_bucket.bucket_name
+    bucket_arn  = module.manage_intelligence_extractor_bucket.bucket_arn
+    bucket_name = module.manage_intelligence_extractor_bucket.bucket_name
   }
 }
 
@@ -75,8 +101,8 @@ resource "kubernetes_secret" "ims_transformer_s3_bucket" {
   }
 
   data = {
-    bucket_arn        = module.manage_intelligence_transformer_bucket.bucket_arn
-    bucket_name       = module.manage_intelligence_transformer_bucket.bucket_name
+    bucket_arn  = module.manage_intelligence_transformer_bucket.bucket_arn
+    bucket_name = module.manage_intelligence_transformer_bucket.bucket_name
   }
 }
 
@@ -87,7 +113,7 @@ resource "kubernetes_secret" "ims_logging_s3_bucket" {
   }
 
   data = {
-    bucket_arn        = module.manage_intelligence_logging_bucket.bucket_arn
-    bucket_name       = module.manage_intelligence_logging_bucket.bucket_name
+    bucket_arn  = module.manage_intelligence_logging_bucket.bucket_arn
+    bucket_name = module.manage_intelligence_logging_bucket.bucket_name
   }
 }
