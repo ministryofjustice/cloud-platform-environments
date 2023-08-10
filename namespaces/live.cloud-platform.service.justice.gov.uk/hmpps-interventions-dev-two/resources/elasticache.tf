e7ac9128c178bc41e7382ade50c9902388340ba9
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2023-08-10 14:22:59 +0100

    Configure resources required to run up the service / ui in the dev-two environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-dev-two/resources/elasticache.tf
@@ -0,0 +1,37 @@
+################################################################################
+# Interventions Elasticache for ReDiS
+################################################################################
+
+module "hmpps_interventions_elasticache_redis" {
+  source                  = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=6.2.0"
+  vpc_name                = var.vpc_name
+  application             = var.application
+  environment-name        = var.environment
+  is-production           = var.is_production
+  infrastructure-support  = var.infrastructure_support
+  team_name               = var.team_name
+  business-unit           = var.business_unit
+  number_cache_clusters   = var.number_cache_clusters
+  node_type               = "cache.t4g.micro"
+  engine_version          = "7.0"
+  parameter_group_name    = "default.redis7"
+  namespace               = var.namespace
+  auth_token_rotated_date = "2023-08-02"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_interventions_elasticache_redis" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.hmpps_interventions_elasticache_redis.primary_endpoint_address
+    auth_token               = module.hmpps_interventions_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.hmpps_interventions_elasticache_redis.member_clusters)
+  }
+}
