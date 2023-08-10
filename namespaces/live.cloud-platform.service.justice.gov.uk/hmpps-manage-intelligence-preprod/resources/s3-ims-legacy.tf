d6a2d627706e499095b24eece5ca4f142f1516e8
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-10 12:28:13 +0100

    creating logging bucket with private acl first

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/s3-ims-legacy.tf
@@ -3,9 +3,9 @@ module "manage_intelligence_extractor_bucket" {
   team_name              = var.team_name
   acl                    = "private"
   versioning             = false
-  logging_enabled        = true
-  log_target_bucket      = module.manage_intelligence_logging_bucket.bucket_name
-  log_path               = "extractor/"
+  #logging_enabled        = true
+  #log_target_bucket      = module.manage_intelligence_logging_bucket.bucket_name
+  #log_path               = "extractor/"
   business-unit          = var.business_unit
   application            = var.application
   is-production          = var.is_production
@@ -23,9 +23,9 @@ module "manage_intelligence_transformer_bucket" {
   team_name              = var.team_name
   acl                    = "private"
   versioning             = false
-  logging_enabled        = true
-  log_target_bucket      = module.manage_intelligence_logging_bucket.bucket_name
-  log_path               = "transformer/"
+  #logging_enabled        = true
+  #log_target_bucket      = module.manage_intelligence_logging_bucket.bucket_name
+  #log_path               = "transformer/"
   business-unit          = var.business_unit
   application            = var.application
   is-production          = var.is_production
@@ -41,7 +41,7 @@ module "manage_intelligence_transformer_bucket" {
 module "manage_intelligence_logging_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
   team_name              = var.team_name
-  acl                    = "log-delivery-write"
+  acl                    =  "private" #"log-delivery-write"
   versioning             = false
   business-unit          = var.business_unit
   application            = var.application
