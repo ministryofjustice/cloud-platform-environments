dc15a7656030788ad6be0049e3af1fab017ddc8d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-11 10:38:32 +0100

    Correct aqa github repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-aqa-website-prod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-aqa-website-prod/resources/serviceaccount.tf
@@ -6,7 +6,7 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["aqa_website"]
+  github_repositories = ["aqa-website"]
 
   github_environments = ["prod"]
 
