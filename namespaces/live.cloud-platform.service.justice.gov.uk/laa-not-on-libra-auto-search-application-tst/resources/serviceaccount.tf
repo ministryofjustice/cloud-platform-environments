11658009babcc26906c887945597f63b229b00ba
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-08-11 09:41:23 +0100

    Create serviceaccount with cli instead

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/serviceaccount.tf
@@ -9,7 +9,4 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["my-repo"]
-
-  serviceaccount_name = "circleci"
-
 }
