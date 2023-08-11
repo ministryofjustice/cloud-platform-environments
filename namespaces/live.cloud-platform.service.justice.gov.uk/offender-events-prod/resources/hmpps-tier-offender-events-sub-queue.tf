d946dc92e5e84aab041a4ad5c8c2a74c8e33545b
Marcus Aspin <maspin@unilink.com>
2023-08-11 15:36:21 +0000

    PI-1360 Add GitHub team tag to tier offender-events queue (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/hmpps-tier-offender-events-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-prod/resources/hmpps-tier-offender-events-sub-queue.tf
@@ -21,7 +21,7 @@ EOF
 
 
   providers = {
-    aws = aws.london
+    aws = aws.probation-integration
   }
 }
 
@@ -66,7 +66,7 @@ module "hmpps_tier_offender_events_dead_letter_queue" {
   namespace              = var.namespace
 
   providers = {
-    aws = aws.london
+    aws = aws.probation-integration
   }
 }
 
@@ -100,7 +100,7 @@ resource "kubernetes_secret" "hmpps_tier_offender_events_dead_letter_queue" {
 }
 
 resource "aws_sns_topic_subscription" "hmpps_tier_offender_events_subscription" {
-  provider      = aws.london
+  provider      = aws.probation-integration
   topic_arn     = module.probation_offender_events.topic_arn
   protocol      = "sqs"
   endpoint      = module.hmpps_tier_offender_events_queue.sqs_arn
