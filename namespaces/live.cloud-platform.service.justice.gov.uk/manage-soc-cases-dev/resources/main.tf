15a9c595116b3bc63c6a806da456f2b5e57f106a
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-08-15 14:05:59 +0100

    Adding irsa to manage-soc-cases

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-dev/resources/main.tf
@@ -4,31 +4,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
-
-  default_tags {
-    tags = {
-      GithubTeam = "secure-estate-digital-team"
-    }
-  }
-}
-
-# To be use in case the resources need to be created in London
-provider "aws" {
-  alias  = "london"
   region = "eu-west-2"
-
-  default_tags {
-    tags = {
-      GithubTeam = "secure-estate-digital-team"
-    }
-  }
-}
-
-# To be use in case the resources need to be created in Ireland
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
 
   default_tags {
     tags = {
@@ -42,3 +18,14 @@ provider "github" {
   owner = var.github_owner
 }
 
+locals {
+  default_tags = {
+    namespace              = var.namespace
+    business-unit          = var.business-unit
+    application            = var.application
+    is-production          = var.is-production
+    environment-name       = var.environment_name
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure-support
+  }
+}
