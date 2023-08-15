45123d5f0035a9ad07a3747d28944122f309c37a
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2023-08-15 08:59:06 +0100

    Update namespaces/live.cloud-platform.service.justice.gov.uk/sqstesting-dev/resources/ecr.tf
    Co-authored-by: Jake Mulley <jakemulley@users.noreply.github.com>

--- namespaces/live.cloud-platform.service.justice.gov.uk/sqstesting-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/sqstesting-dev/resources/ecr.tf
@@ -14,7 +14,7 @@ module "ecr" {
   oidc_providers = ["github"]
 
   # REQUIRED: GitHub repositories that push to this container repository
-  github_repositories = ["https://github.com/ministryofjustice/sqs-testing-application"]
+  github_repositories = ["sqs-testing-application"]
 
   # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
   # github_environments = ["production"]
