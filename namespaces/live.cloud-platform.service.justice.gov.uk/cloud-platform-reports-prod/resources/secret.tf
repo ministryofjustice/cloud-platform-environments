d50d04e231b351e4eb88c9e37b6ee9bc69ec16c4
jakemulley <jakemulley@users.noreply.github.com>
2023-08-15 14:52:52 +0100

    cloud-platform-terraform-secrets-manager: update module

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reports-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reports-prod/resources/secret.tf
@@ -1,19 +1,20 @@
 module "secrets_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=1.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=std-tags"
+
   team_name               = var.team_name
   application             = var.application
   business_unit           = var.business_unit
   is_production           = var.is_production
   namespace               = var.namespace
-  environment             = var.environment
+  environment_name        = var.environment
   infrastructure_support  = var.infrastructure_support
-  eks_cluster_name       = var.eks_cluster_name
+  eks_cluster_name        = var.eks_cluster_name
 
   secrets = {
     "cloud-platform-reports-api-key" = {
-      description             = "cloud-platform-reports HOODAW POST request validation API key", 
-      recovery_window_in_days = 7, 
-      k8s_secret_name        = "hoodaw-api-key" 
+      description             = "cloud-platform-reports HOODAW POST request validation API key",
+      recovery_window_in_days = 7,
+      k8s_secret_name        = "hoodaw-api-key"
     },
   }
 }
