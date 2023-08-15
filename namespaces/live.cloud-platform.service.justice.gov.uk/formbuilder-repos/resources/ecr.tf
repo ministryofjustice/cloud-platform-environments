3f489ae787e7d044fa7b8e62007153f6448748ff
Natalie Seeto <njseeto@users.noreply.github.com>
2023-08-15 11:09:15 +0100

    Add ecr source to file

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-repos/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-repos/resources/ecr.tf
@@ -1,4 +1,6 @@
 module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
+
   oidc_providers = ["circleci"]
 
   github_repositories = ["fb-deploy"]
