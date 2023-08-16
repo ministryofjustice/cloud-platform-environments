adf2eb8521d88156c4f7ec9900e74e3a782a0a96
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-08-16 12:44:48 +0100

    Update ecr.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/ecr.tf
@@ -1,9 +1,10 @@
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
 
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
 
+  oidc_providers      = ["github"]
   github_repositories = ["operations-engineering-reports"]
 }
 
