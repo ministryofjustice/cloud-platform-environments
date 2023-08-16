4db3f9c51bd1cce8d182725dd58b3acc46b42b18
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-08-16 10:16:04 +0100

    Add irsa to manage-soc-cases-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-prod/resources/rds.tf
@@ -3,28 +3,25 @@ resource "random_id" "id" {
 }
 
 module "dps_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
-  vpc_name               = var.vpc_name
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  namespace              = var.namespace
-  db_instance_class      = "db.t4g.small"
+  source                   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
+  vpc_name                 = var.vpc_name
+  team_name                = var.team_name
+  business-unit            = var.business_unit
+  application              = var.application
+  is-production            = var.is_production
+  namespace                = var.namespace
+  db_engine_version        = "15.2"
+  db_instance_class        = "db.t4g.small"
   db_max_allocated_storage = "10000" # maximum storage for autoscaling
-  db_engine_version      = "15.2"
-  environment-name       = var.environment-name
-  infrastructure-support = var.infrastructure_support
+  environment-name         = var.environment_name
+  infrastructure-support   = var.infrastructure_support
 
-  rds_family = "postgres15"
+  rds_family                = "postgres15"
   prepare_for_major_upgrade = false
 
-  providers = {
-    aws = aws.london
-  }
 }
 
-data "aws_iam_policy_document" "manage_soc_cases_prod_rds_to_s3_export_policy" {
+data "aws_iam_policy_document" "manage_soc_cases_preprod_rds_to_s3_export_policy" {
 
   statement {
     sid = "AllowRdsExportUserToListS3Buckets"
@@ -67,7 +64,7 @@ resource "aws_iam_access_key" "user" {
 
 resource "aws_iam_user_policy" "policy" {
   name   = "manage-soc-cases-rds-to-s3-snapshots-read-write"
-  policy = data.aws_iam_policy_document.manage_soc_cases_prod_rds_to_s3_export_policy.json
+  policy = data.aws_iam_policy_document.manage_soc_cases_preprod_rds_to_s3_export_policy.json
   user   = aws_iam_user.user.name
 }
 
@@ -83,8 +80,6 @@ resource "kubernetes_secret" "dps_rds" {
     database_username           = module.dps_rds.database_username
     database_password           = module.dps_rds.database_password
     rds_instance_address        = module.dps_rds.rds_instance_address
-    access_key_id               = module.dps_rds.access_key_id
-    secret_access_key           = module.dps_rds.secret_access_key
     url                         = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
     rds_to_s3_user_arn          = aws_iam_user.user.arn
     rds_to_s3_access_key_id     = aws_iam_access_key.user.id
