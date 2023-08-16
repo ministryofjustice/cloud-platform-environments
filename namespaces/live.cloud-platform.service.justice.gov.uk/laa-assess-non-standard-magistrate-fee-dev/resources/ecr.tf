137c0a48289151d54bdf20edbf01f09da7f43a4c
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-08-16 17:04:31 +0100

    cleanup ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/ecr.tf
@@ -4,6 +4,7 @@
  * releases page of this repository.
  *
  */
+
 module "ecr" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
   
@@ -67,11 +68,6 @@ module "ecr" {
 
 
   # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
   team_name              = var.team_name # also used for naming the container repository
   namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
 }
