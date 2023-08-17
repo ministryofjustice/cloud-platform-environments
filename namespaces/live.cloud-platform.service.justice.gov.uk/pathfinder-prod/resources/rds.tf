4493204df412547d6feb6e47817729c8ff2f40a0
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-08-17 12:33:59 +0100

    Add IRSA to pathfinder-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/rds.tf
@@ -3,24 +3,21 @@ resource "random_id" "id" {
 }
 
 module "dps_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
-  vpc_name               = var.vpc_name
-  team_name              = var.team_name
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  namespace              = var.namespace
-  db_instance_class      = "db.t4g.small"
+  source                   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.19.0"
+  vpc_name                 = var.vpc_name
+  team_name                = var.team_name
+  business-unit            = var.business_unit
+  application              = var.application
+  is-production            = var.is_production
+  namespace                = var.namespace
+  db_instance_class        = "db.t4g.small"
   db_max_allocated_storage = "10000" # maximum storage for autoscaling
-  db_engine_version      = "15.2"
-  environment-name       = var.environment-name
-  infrastructure-support = var.infrastructure_support
+  db_engine_version        = "15.2"
+  environment-name         = var.environment_name
+  infrastructure-support   = var.infrastructure_support
 
-  rds_family = "postgres15"
+  rds_family                = "postgres15"
   prepare_for_major_upgrade = false
-  providers = {
-    aws = aws.london
-  }
 }
 
 data "aws_iam_policy_document" "pathfinder_dev_rds_to_s3_export_policy" {
@@ -82,8 +79,6 @@ resource "kubernetes_secret" "dps_rds" {
     database_username           = module.dps_rds.database_username
     database_password           = module.dps_rds.database_password
     rds_instance_address        = module.dps_rds.rds_instance_address
-    access_key_id               = module.dps_rds.access_key_id
-    secret_access_key           = module.dps_rds.secret_access_key
     url                         = "postgres://${module.dps_rds.database_username}:${module.dps_rds.database_password}@${module.dps_rds.rds_instance_endpoint}/${module.dps_rds.database_name}"
     rds_to_s3_user_arn          = aws_iam_user.user.arn
     rds_to_s3_access_key_id     = aws_iam_access_key.user.id
--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/variables.tf
@@ -28,7 +28,7 @@ variable "team_name" {
   default     = "Digital Prison Services/New Nomis"
 }
 
-variable "environment-name" {
+variable "environment_name" {
   description = "The type of environment you're deploying to."
   default     = "prod"
 }
@@ -49,6 +49,7 @@ variable "number_cache_clusters" {
 variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
 }
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
@@ -60,4 +61,3 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
-
