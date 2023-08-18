a215807f2649bbb58ea2edca863261cd5d629e8b
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-08-18 12:53:08 +0100

    NN-5495 Sperate Service pod added to the environment.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/irsa.tf
@@ -42,12 +42,3 @@ data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
-
-# set up the service pod
-module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
-
-  # Configuration
-  namespace            = var.namespace
-  service_account_name = "hmpps_manage_adjudications"
-}
