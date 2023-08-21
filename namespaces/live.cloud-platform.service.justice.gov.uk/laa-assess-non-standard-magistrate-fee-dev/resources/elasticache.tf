a1c6546fd3cdc3858826b2b53cc180e2420ac3dd
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2023-08-21 15:52:10 +0100

    Update elasticache.tf
    to kickstart concourse

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/elasticache.tf
@@ -9,7 +9,6 @@ module "nsm_elasticache" {
   team_name              = var.team_name
   namespace              = var.namespace
   business-unit          = var.business_unit
-
   engine_version         = "7.0"
   parameter_group_name   = "default.redis7"
   node_type              = "cache.t4g.micro"
