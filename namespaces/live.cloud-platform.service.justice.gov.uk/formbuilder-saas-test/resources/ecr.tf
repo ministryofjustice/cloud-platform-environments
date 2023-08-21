c06f833f6058a0eea5a307cc899076323924761a
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-08-21 10:09:21 +0100

    Set repo name to try to work with existing fb-editor-web ecr repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/ecr.tf
@@ -2,7 +2,7 @@ module "ecr" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
 
   team_name = var.team_name
-  repo_name = "fb-editor-ecr"
+  repo_name = "fb-editor-web"
 
   oidc_providers = ["circleci"]
 
