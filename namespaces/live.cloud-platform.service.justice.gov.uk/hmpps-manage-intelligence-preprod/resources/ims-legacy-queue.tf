fd686bd0a50e318fedff9d1216b3c8d0f04cf007
jakemulley <jakemulley@users.noreply.github.com>
2023-08-21 15:40:46 +0100

    hmpps-manage-intelligence-preprod: bump cloud-platform-terraform-sqs to v4.12.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/ims-legacy-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/ims-legacy-queue.tf
@@ -1,17 +1,12 @@
 module "ims_extractor_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name           = var.environment
-  team_name                  = var.team_name
-  infrastructure-support     = var.infrastructure_support
-  application                = var.application
-  sqs_name                   = "ims_extractor_queue_${var.environment}.fifo"
+  # Queue configuration
+  sqs_name                   = "ims_extractor_queue_${var.environment}"
   fifo_queue                 = true
   encrypt_sqs_kms            = "true"
   message_retention_seconds  = 1209600
   visibility_timeout_seconds = 120
-  namespace                  = var.namespace
-
 
   redrive_policy = <<EOF
   {
@@ -20,22 +15,36 @@ module "ims_extractor_queue" {
 
 EOF
 
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
   providers = {
     aws = aws.london
   }
 }
 
 module "ims_extractor_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name       = var.environment
-  team_name              = var.team_name
-  infrastructure-support = var.infrastructure_support
+  # Queue configuration
+  sqs_name        = "ims_extractor_dl_queue_${var.environment}"
+  fifo_queue      = true
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
   application            = var.application
-  sqs_name               = "ims_extractor_dl_queue_${var.environment}.fifo"
-    fifo_queue           = true
-  encrypt_sqs_kms        = "true"
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
   namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 
   providers = {
     aws = aws.london
@@ -44,19 +53,14 @@ module "ims_extractor_dead_letter_queue" {
 
 
 module "ims_transformer_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name           = var.environment
-  team_name                  = var.team_name
-  infrastructure-support     = var.infrastructure_support
-  application                = var.application
-  sqs_name                   = "ims_transformer_queue_${var.environment}.fifo"
-    fifo_queue               = true
+  # Queue configuration
+  sqs_name                   = "ims_transformer_queue_${var.environment}"
+  fifo_queue                 = true
   encrypt_sqs_kms            = "true"
   message_retention_seconds  = 1209600
   visibility_timeout_seconds = 120
-  namespace                  = var.namespace
-
 
   redrive_policy = <<EOF
   {
@@ -65,22 +69,36 @@ module "ims_transformer_queue" {
 
 EOF
 
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
   providers = {
     aws = aws.london
   }
 }
 
 module "ims_transformer_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name       = var.environment
-  team_name              = var.team_name
-  infrastructure-support = var.infrastructure_support
+  # Queue configuration
+  sqs_name        = "ims_transformer_dl_queue_${var.environment}"
+  fifo_queue      = true
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
   application            = var.application
-  sqs_name               = "ims_transformer_dl_queue_${var.environment}.fifo"
-  fifo_queue             = true
-  encrypt_sqs_kms        = "true"
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
   namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 
   providers = {
     aws = aws.london
@@ -88,19 +106,14 @@ module "ims_transformer_dead_letter_queue" {
 }
 
 module "ims_lastupdate_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name           = var.environment
-  team_name                  = var.team_name
-  infrastructure-support     = var.infrastructure_support
-  application                = var.application
-  sqs_name                   = "ims_lastupdate_queue_${var.environment}.fifo"
-    fifo_queue               = true
+  # Queue configuration
+  sqs_name                   = "ims_lastupdate_queue_${var.environment}"
+  fifo_queue                 = true
   encrypt_sqs_kms            = "true"
   message_retention_seconds  = 1209600
   visibility_timeout_seconds = 120
-  namespace                  = var.namespace
-
 
   redrive_policy = <<EOF
   {
@@ -109,22 +122,36 @@ module "ims_lastupdate_queue" {
 
 EOF
 
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
   providers = {
     aws = aws.london
   }
 }
 
 module "ims_lastupdate_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=fifo-fix"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.12.0"
 
-  environment-name       = var.environment
-  team_name              = var.team_name
-  infrastructure-support = var.infrastructure_support
+  # Queue configuration
+  sqs_name        = "ims_lastupdate_dl_queue_${var.environment}"
+  fifo_queue      = true
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
   application            = var.application
-  sqs_name               = "ims_lastupdate_dl_queue_${var.environment}.fifo"
-  fifo_queue             = true
-  encrypt_sqs_kms        = "true"
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
   namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 
   providers = {
     aws = aws.london
