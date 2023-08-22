6164710af6d60fc539c98faa82fc60f63d637f4b
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-08-22 16:20:26 +0100

    changed case of github_repositories

--- namespaces/live.cloud-platform.service.justice.gov.uk/soc-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/soc-production/resources/ecr.tf
@@ -19,7 +19,7 @@ module "ecr_credentials" {
   # enable the oidc implementation for github
   oidc_providers = ["github"]
 
-  github_repositories = ["socreporting", "socentry"]
+  github_repositories = ["SOCReporting", "SOCEentry"]
 
 
   github_actions_secret_ecr_name       = var.github_actions_secret_ecr_name
