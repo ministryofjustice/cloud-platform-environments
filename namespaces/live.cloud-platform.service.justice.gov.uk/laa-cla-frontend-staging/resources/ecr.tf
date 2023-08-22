10a30e229d24810f1ba5d4119b57d3418c4fad7a
said-moj <45761276+said-moj@users.noreply.github.com>
2023-08-22 13:26:24 +0100

    Allow laa-cla-frontend-staging repo to access other cla ecr repos

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/ecr.tf
@@ -11,7 +11,7 @@ module "cla_frontend_app_credentials" {
   oidc_providers = ["circleci"]
 
   # specify which GitHub repository your CircleCI job runs from
-  github_repositories = [var.repo_name]
+  github_repositories = [var.repo_name, "cla-end-to-end-tests", "cla_backend", "cla_public", "fala"]
 
   # set your namespace name to create a ConfigMap
   # of credentials you need in CircleCI
