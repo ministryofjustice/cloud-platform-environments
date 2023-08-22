d0d905f76a22bdc5eea63c981ea2bdf8023dbe45
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-08-22 09:38:15 +0100

    Fix namespace config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-prod/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "hmpps-launchpad"
+  default     = "hmpps-launchpad-prod"
 }
 
 variable "business_unit" {
