c8e080ba3ce7b290bbb8cf9eefeb16352932241d
adam <brown-a2@users.noreply.github.com>
2023-08-23 16:09:02 +0100

    Add policy to both ECR modules in our namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/irsa.tf
@@ -13,7 +13,8 @@ module "irsa" {
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
     s3 = module.s3_bucket.irsa_policy_arn
-    ecr = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    ecr = module.ecr_credentials.irsa_policy_arn,
+    ecr2 = module.ecr_feed_parser.irsa_policy_arn,
   }
 
   # Tags
