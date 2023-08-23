bca60763627d7ac471cb5d7fb5d0bf1424acf8a1
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-23 08:50:25 +0100

    updating env name

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa-legacy.tf
@@ -5,7 +5,7 @@ module "irsa-legacy" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = "${var.application}-legacy-${var.environment}"
+  service_account_name = "${var.application}-legacy-${var.environment-name}"
   namespace            = var.namespace # this is also used as a tag
 
   # Attach the approprate policies using a key => value map
@@ -28,7 +28,7 @@ module "irsa-legacy" {
   application            = var.application
   is_production          = var.is_production
   team_name              = var.team_name
-  environment_name       = var.environment
+  environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
 }
 
