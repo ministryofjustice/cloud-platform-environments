e7cdbf1d3c14ebfb6044eac8f38fbf25f7b657cf
Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
2023-08-24 16:35:47 +0100

    PF - remove access keys from preprod. (#15891)

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-preprod/resources/analytical-platform-access.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-preprod/resources/analytical-platform-access.tf
@@ -1,11 +1,3 @@
-module "ap_irsa" {
-  source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=1.1.0"
-  namespace        = var.namespace
-  eks_cluster_name = var.eks_cluster_name
-  role_policy_arns = [aws_iam_policy.ap_policy.arn]
-  service_account  = "${var.namespace}-to-ap-s3"
-}
-
 resource "aws_iam_policy" "ap_policy" {
   name   = "${var.namespace}-ap-register-my-data-policy"
   policy = data.aws_iam_policy_document.pathfinder_ap_access.json
@@ -30,25 +22,6 @@ data "aws_iam_policy_document" "pathfinder_ap_access" {
   }
 }
 
-resource "random_id" "pathfinder-ap-id" {
-  byte_length = 16
-}
-
-resource "aws_iam_user" "pathfinder_ap_user" {
-  name = "pathfinder-ap-s3-bucket-user-${random_id.pathfinder-ap-id.hex}"
-  path = "/system/pathfinder-ap-s3-bucket-user/"
-}
-
-resource "aws_iam_access_key" "pathfinder_ap_user" {
-  user = aws_iam_user.pathfinder_ap_user.name
-}
-
-resource "aws_iam_user_policy" "pathfinder_ap_policy" {
-  name   = "${var.namespace}-ap-s3-snapshots"
-  policy = data.aws_iam_policy_document.pathfinder_ap_access.json
-  user   = aws_iam_user.pathfinder_ap_user.name
-}
-
 resource "kubernetes_secret" "ap_aws_secret" {
   metadata {
     name      = "pathfinder-analytical-platform-reporting-s3-bucket"
@@ -57,20 +30,5 @@ resource "kubernetes_secret" "ap_aws_secret" {
 
   data = {
     destination_bucket = "s3://moj-reg-preprod/landing/hmpps-pathfinder-preprod/"
-    user_arn           = aws_iam_user.pathfinder_ap_user.arn
-    access_key_id      = aws_iam_access_key.pathfinder_ap_user.id
-    secret_access_key  = aws_iam_access_key.pathfinder_ap_user.secret
-  }
-}
-
-resource "kubernetes_secret" "pathfinder_to_ap_irsa" {
-  metadata {
-    name      = "pathfinder-to-ap-s3-irsa"
-    namespace = var.namespace
-  }
-
-  data = {
-    role           = module.ap_irsa.aws_iam_role_name
-    serviceaccount = module.ap_irsa.service_account_name.name
   }
 }
