e62a89e70a78b35d2611dc3917676a078501cd29
gareth.davies <gareth.m.davies@gmail.com>
2023-08-24 16:59:42 +0100

    PF - remove access keys and unused resources from prod.

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-prod/resources/s3.tf
@@ -31,19 +31,6 @@ resource "aws_iam_policy" "irsa_additional_s3_policy" {
   tags   = local.default_tags
 }
 
-module "pathfinder_rds_to_s3_bucket" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=4.8.2"
-  team_name              = var.team_name
-  acl                    = "private"
-  versioning             = false
-  business-unit          = var.business_unit
-  application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment_name
-  infrastructure-support = var.infrastructure_support
-  namespace              = var.namespace
-}
-
 resource "kubernetes_secret" "pathfinder_document_s3_bucket" {
   metadata {
     name      = "pathfinder-document-s3-bucket-output"
@@ -51,23 +38,7 @@ resource "kubernetes_secret" "pathfinder_document_s3_bucket" {
   }
 
   data = {
-    access_key_id     = module.pathfinder_document_s3_bucket.access_key_id
-    secret_access_key = module.pathfinder_document_s3_bucket.secret_access_key
     bucket_arn        = module.pathfinder_document_s3_bucket.bucket_arn
     bucket_name       = module.pathfinder_document_s3_bucket.bucket_name
-  }
-}
-
-resource "kubernetes_secret" "pathfinder_rds_to_s3_bucket" {
-  metadata {
-    name      = "pathfinder-rds-to-s3-bucket-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    access_key_id     = module.pathfinder_rds_to_s3_bucket.access_key_id
-    secret_access_key = module.pathfinder_rds_to_s3_bucket.secret_access_key
-    bucket_arn        = module.pathfinder_rds_to_s3_bucket.bucket_arn
-    bucket_name       = module.pathfinder_rds_to_s3_bucket.bucket_name
   }
 }
