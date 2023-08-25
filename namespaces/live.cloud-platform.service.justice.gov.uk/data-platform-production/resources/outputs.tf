e0b2798fbe8fe022f937071fb182524b3dde5504
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-08-25 13:26:21 +0100

    Fixing zone name

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/outputs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/outputs.tf
@@ -1,9 +1,9 @@
 output "alpha_zone_name_servers" {
-  value       = try(aws_route53_zone.default[0].name_servers, [])
+  value       = try(aws_route53_zone.data_platform_apps_alpha_route53_zone.name_servers, [])
   description = "Alpha Route53 DNS Zone Name Servers"
 }
 
 output "alpha_fqdn" {
-  value       = join("", aws_route53_zone.default.*.name)
+  value       = join("", aws_route53_zone.data_platform_apps_alpha_route53_zone.*.name)
   description = "Alpha Fully-qualified domain name"
 }
