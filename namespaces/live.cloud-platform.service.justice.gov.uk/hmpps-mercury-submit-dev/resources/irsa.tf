c87ca63c4051ab9a63b38412900e016ffdad1f90
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-25 12:04:16 +0100

    adding irsa

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mercury-submit-dev/resources/irsa.tf
@@ -0,0 +1,39 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "hmpps-mercury-submit-${var.environment}"
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the approprate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = {
+    sqs_submit_queue  = module.mercury_submit_queue.irsa_policy_arn
+    sqs_submit_dlq    = module.mercury_submit_dead_letter_queue.irsa_policy_arn
+    db_report_id      = module.report_id_dynamodb.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "irsa-output"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+    rolearn        = module.irsa.role_arn
+  }
+}
