10b0d4fb20ba96b9567be2232034a4a88169bf1d
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-08-25 10:36:04 +0100

    MAP-149 JPC Preprod access BaSM Reporting bucket IRSA policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-journey-variable-payments-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-journey-variable-payments-preprod/resources/irsa.tf
@@ -24,3 +24,21 @@ module "irsa" {
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
 }
+
+data "aws_ssm_parameter" "basm-bucket" {
+  name     = "/hmpps-book-secure-move-api-preprod/reporting-bucket/irsa-policy-arn"
+}
+data "aws_ssm_parameter" "basm-bucket-name" {
+  name     = "/hmpps-book-secure-move-api-preprod/reporting-bucket/bucket-arn"
+}
+
+resource "kubernetes_secret" "basm_reporting_bucket" {
+  metadata {
+    name      = "basm-reporting-bucket"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_name = data.aws_ssm_parameter.basm-bucket-name.value
+  }
+}
