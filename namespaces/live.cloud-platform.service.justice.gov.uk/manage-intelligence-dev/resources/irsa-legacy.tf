f538f2fa68188691c7edfcdb19fe04e4e6a01206
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2023-08-29 11:23:51 +0100

    update to ims sa policy (#15940)
    * update to ims sa policy
    
    * update to irsa policy
    
    * adding aws resources
    
    * added hashicorp/time version
    
    * fix to env var name
    
    * adding k8s cluster env var
    
    ---------
    
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa-legacy.tf
@@ -1,29 +1,26 @@
-module "irsa" {
+module "irsa-legacy" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   # EKS configuration
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = "${var.application}-${var.environment-name}"
+  service_account_name = "${var.application}-legacy-${var.environment-name}"
   namespace            = var.namespace # this is also used as a tag
 
   # Attach the approprate policies using a key => value map
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    sqs_batch           = module.ims_index_batch_queue.irsa_policy_arn
-    sqs_batch_dlq       = module.ims_index_batch_dead_letter_queue.irsa_policy_arn
-    sqs_update          = module.ims_index_update_queue.irsa_policy_arn
-    sqs_update_dlq      = module.ims_index_update_dead_letter_queue.irsa_policy_arn
+    sqs_extractor       = module.ims_extractor_queue.irsa_policy_arn
+    sqs_extractor_dlq   = module.ims_extractor_dead_letter_queue.irsa_policy_arn
     sqs_transformer     = module.ims_transformer_queue.irsa_policy_arn
     sqs_transformer_dlq = module.ims_transformer_dead_letter_queue.irsa_policy_arn
     sqs_lastupdate      = module.ims_lastupdate_queue.irsa_policy_arn
     sqs_lastupdate_dlq  = module.ims_lastupdate_dead_letter_queue.irsa_policy_arn
-    s3_ims              = module.manage_intelligence_storage_bucket.irsa_policy_arn
-    s3_rds              = module.manage_intelligence_rds_to_s3_bucket.irsa_policy_arn
+    s3_extractor        = module.manage_intelligence_extractor_bucket.irsa_policy_arn
     s3_transformer      = module.manage_intelligence_transformer_bucket.irsa_policy_arn
-    rds                 = module.rds_aurora.irsa_policy_arn
+    rds                 = module.rds_aurora_legacy.irsa_policy_arn
   }
 
   # Tags
@@ -35,14 +32,14 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
-resource "kubernetes_secret" "irsa" {
+resource "kubernetes_secret" "irsa-legacy" {
   metadata {
-    name      = "irsa-output"
+    name      = "irsa-legacy-output"
     namespace = var.namespace
   }
   data = {
-    role           = module.irsa.role_name
-    serviceaccount = module.irsa.service_account.name
-    rolearn        = module.irsa.role_arn
+    role           = module.irsa-legacy.role_name
+    serviceaccount = module.irsa-legacy.service_account.name
+    rolearn        = module.irsa-legacy.role_arn
   }
 }
