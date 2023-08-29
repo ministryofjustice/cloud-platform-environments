fbb02d57e14f1614f1df97a6a7220c860509f84b
jakemulley <jakemulley@users.noreply.github.com>
2023-08-29 13:12:13 +0100

    data-platform-app-*: bump cloud-platform-terraform-ecr-credentials to v6.1.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-bold-case-info-dashboard-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-bold-case-info-dashboard-prod/resources/ecr.tf
@@ -1,6 +1,5 @@
 module "ecr_credentials" {
-  source         = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
-  team_name      = var.team_name
+  source         = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
   repo_name      = "${var.namespace}-ecr"
   oidc_providers = ["github"]
 
@@ -73,6 +72,14 @@ module "ecr_credentials" {
 EOF
 */
 
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
 
 
@@ -89,4 +96,3 @@ resource "kubernetes_secret" "ecr_credentials" {
     repo_url          = module.ecr_credentials.repo_url
   }
 }
-
