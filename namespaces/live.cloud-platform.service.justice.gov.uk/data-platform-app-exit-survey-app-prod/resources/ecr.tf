ceb1814ce4ba8753fca63f8c06d8bdd5b3248859
jakemulley <jakemulley@users.noreply.github.com>
2023-08-29 13:32:26 +0100

    data-platform-app-*: remove scan on push notice as removed

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-exit-survey-app-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-exit-survey-app-prod/resources/ecr.tf
@@ -3,14 +3,6 @@ module "ecr_credentials" {
   repo_name      = "${var.namespace}-ecr"
   oidc_providers = ["github"]
 
-  /*
-    By default scan_on_push is set to true. When this is enabled then all images pushed to the repo are scanned for any security
-    / software vulnerabilities in your image and the results can be viewed in the console. For further details, please see:
-    https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html
-    To disable 'scan_on_push', set it to false as below:
-  scan_on_push = "false"
-  */
-
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
