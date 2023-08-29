39b673c4e9cabc6c7a8cdb549d314d322dc952c8
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-08-29 11:04:31 +0100

    fixed typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/soc-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/soc-staging/resources/irsa.tf
@@ -5,7 +5,7 @@ module "irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = ${var.namespace}-service-pod"
+  service_account_name = "${var.namespace}-service-pod"
   namespace            = var.namespace # this is also used as a tag
 
   # Attach the approprate policies using a key => value map
