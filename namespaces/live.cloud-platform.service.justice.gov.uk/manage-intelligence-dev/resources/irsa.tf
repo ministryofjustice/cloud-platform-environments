8d6316d93c96df1ec1ec68e25a01d74c0eb4b3e0
brian.carr <brian.carr@digital.justice.gov.uk>
2023-08-29 10:40:20 +0100

    update to irsa policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/irsa.tf
@@ -13,11 +13,13 @@ module "irsa" {
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
     sqs_batch           = module.ims_index_batch_queue.irsa_policy_arn
-    sqs_batch_dlg       = module.ims_index_batch_dead_letter_queue.irsa_policy_arn
+    sqs_batch_dlq       = module.ims_index_batch_dead_letter_queue.irsa_policy_arn
     sqs_update          = module.ims_index_update_queue.irsa_policy_arn
-    sqs_update_dlq      = module.ms_index_update_dead_letter_queue.irsa_policy_arn
+    sqs_update_dlq      = module.ims_index_update_dead_letter_queue.irsa_policy_arn
     sqs_transformer     = module.ims_transformer_queue.irsa_policy_arn
     sqs_transformer_dlq = module.ims_transformer_dead_letter_queue.irsa_policy_arn
+    sqs_lastupdate      = module.ims_lastupdate_queue.irsa_policy_arn
+    sqs_lastupdate_dlq  = module.ims_lastupdate_dead_letter_queue.irsa_policy_arn
     s3_ims              = module.manage_intelligence_storage_bucket.irsa_policy_arn
     s3_rds              = module.manage_intelligence_rds_to_s3_bucket.irsa_policy_arn
     s3_transformer      = module.manage_intelligence_transformer_bucket.irsa_policy_arn
