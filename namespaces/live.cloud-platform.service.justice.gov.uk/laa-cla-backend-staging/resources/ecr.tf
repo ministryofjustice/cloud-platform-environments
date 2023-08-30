16bb2adda3aed03fc216e94c870ab9312a5ebb90
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2023-08-30 10:52:41 +0100

    Remove secrets from CLA Backend

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/ecr.tf
@@ -27,7 +27,5 @@ resource "kubernetes_secret" "ecr-repo" {
 
   data = {
     repo_url          = module.ecr-repo.repo_url
-    access_key_id     = module.ecr-repo.access_key_id
-    secret_access_key = module.ecr-repo.secret_access_key
   }
 }
