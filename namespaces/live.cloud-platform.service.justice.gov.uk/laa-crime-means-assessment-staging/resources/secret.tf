662297a6b9dc65028e547955df09f5d629c56469
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-08-30 16:18:50 +0100

    added config for cma secrets in staging

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-means-assessment-staging/resources/secret.tf
@@ -0,0 +1,24 @@
+module "secrets_manager" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.0"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "maat_api_oauth_client_id" = {
+      description             = "MAAT API oauth client ID for CMA Staging",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "maat-api-oauth-client-id"
+    },
+    "maat_api_oauth_client_secret" = {
+      description             = "MAAT API oauth client secret for CMA Staging",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "maat-api-oauth-client-secret"
+    },
+  }
+}
