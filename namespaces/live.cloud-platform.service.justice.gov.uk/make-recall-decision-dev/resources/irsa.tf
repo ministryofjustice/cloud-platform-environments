2c6e43691712bfdd163a22dca0babe7d40159620
Bill Sclater <bill.sclater@justice.gov.uk>
2023-08-31 20:38:05 +0100

    missing quote

--- namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/irsa.tf
@@ -3,7 +3,7 @@ locals {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
   sqs_queues = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue" = hmpps-audit-dev"
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
   }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
