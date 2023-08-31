723b56b2f3a08cfce5d6e1179ec85281f1aad135
Bill Sclater <bill.sclater@justice.gov.uk>
2023-08-31 20:29:58 +0100

    mrd-1670_irsa-preprod-make-recall-decisions: preprod irsa

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-preprod/resources/irsa.tf
@@ -0,0 +1,67 @@
+locals {
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd" = "hmpps-domain-events-preprod"
+  }
+  sqs_queues = {
+    "Digital-Prison-Services-preprod-hmpps_audit_queue" = "hmpps-audit-preprod"
+  }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+}
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "make-recall-decision"
+  role_policy_arns     = merge(
+                           local.sqs_policies,
+                           local.sns_policies
+  )
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd"
+}
+
+resource "kubernetes_secret" "hmpps_domain_events_topic" {
+  metadata {
+    name      = "hmpps-domain-events-topic"
+    namespace = var.namespace
+  }
+  data = {
+    topic_arn = data.aws_sns_topic.hmpps-domain-events.arn
+  }
+}
+
+data "aws_sqs_queue" "hmpps-audit-queue" {
+  name = "Digital-Prison-Services-preprod-hmpps_audit_queue"
+}
+
+resource "kubernetes_secret" "sqs-hmpps-audit-secret" {
+  metadata {
+    name      = "sqs-hmpps-audit-secret"
+    namespace = var.namespace
+  }
+  data = {
+    sqs_queue_url = data.aws_sqs_queue.hmpps-audit-queue.url
+    sqs_queue_name = data.aws_sqs_queue.hmpps-audit-queue.name
+  }
+}
