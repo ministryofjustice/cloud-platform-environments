85445c5aee823639b0be687c465cc6826d5b69a6
jack-burt-is <jack.burt@informed.com>
2023-08-31 09:01:19 +0000

    Fix naming for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-prod/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-prod/resources/ssm.tf
@@ -1,6 +1,6 @@
 # The value of this parameter is to be replaced manually, not wanting to put external acct IDs in public repository
 resource "aws_ssm_parameter" "cica_prod_account_id" {
-  name      = "/claim-criminal-injuries-compensation-uat/cica-prod-account-id"
+  name      = "/claim-criminal-injuries-compensation-prod/cica-prod-account-id"
   type      = "SecureString"
   value     = "0123456789"
   overwrite = false
