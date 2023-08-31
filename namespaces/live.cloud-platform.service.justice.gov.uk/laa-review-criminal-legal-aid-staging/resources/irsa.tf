720c3ef43a66fcb5414a3113a210ec4dd2c94428
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-08-31 08:58:47 +0100

    Remove ecr irsa policy arn

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-staging/resources/irsa.tf
@@ -12,7 +12,6 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    ecr = module.ecr-repo.irsa_policy_arn
     rds = module.rds.irsa_policy_arn
   }
 
