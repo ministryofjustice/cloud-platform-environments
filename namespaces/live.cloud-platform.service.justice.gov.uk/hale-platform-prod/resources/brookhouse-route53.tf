b215d8763119f86e1ac148ac77652acc2a402bdc
adam <brown-a2@users.noreply.github.com>
2023-08-31 11:24:12 +0100

    Remove A records pointing to old LB

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/brookhouse-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/brookhouse-route53.tf
@@ -22,31 +22,6 @@ resource "kubernetes_secret" "brookhouse_route53_zone_sec" {
   }
 }
 
-resource "aws_route53_record" "brookhouse_route53_a_record" {
-  zone_id = aws_route53_zone.brookhouse_route53_zone.zone_id
-  name    = "brookhouseinquiry.org.uk"
-  type    = "A"
-
-  alias {
-    name                   = "dualstack.brook-loadb-ubcen73kpkww-1507091893.eu-west-2.elb.amazonaws.com."
-    zone_id                = "ZHURV8PSTC4K8"
-    evaluate_target_health = false
-  }
-}
-
-
-resource "aws_route53_record" "brookhouse_route53_a_record_www" {
-  zone_id = aws_route53_zone.brookhouse_route53_zone.zone_id
-  name    = "www.brookhouseinquiry.org.uk"
-  type    = "A"
-
-  alias {
-    name                   = "dualstack.brook-loadb-ubcen73kpkww-1507091893.eu-west-2.elb.amazonaws.com."
-    zone_id                = "ZHURV8PSTC4K8"
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_route53_record" "brookhouse_route53_mx_record_mail" {
   zone_id = aws_route53_zone.brookhouse_route53_zone.zone_id
   name    = "brookhouseinquiry.org.uk"
