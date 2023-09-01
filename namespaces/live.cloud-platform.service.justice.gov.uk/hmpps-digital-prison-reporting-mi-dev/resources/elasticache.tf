321dd3a45ae0068645eac9d097deda031edd8d37
jakemulley <jakemulley@users.noreply.github.com>
2023-09-01 18:55:38 +0100

    Remove access key ID and secret access key outputs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/elasticache.tf
@@ -29,8 +29,6 @@ resource "kubernetes_secret" "hmpps_digital_prison_reporting_mi_ui_ec_cluster" {
     primary_endpoint_address = module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.primary_endpoint_address
     member_clusters          = jsonencode(module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.member_clusters)
     auth_token               = module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.auth_token
-    access_key_id            = module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.access_key_id
-    secret_access_key        = module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.secret_access_key
     replication_group_id     = module.hmpps_digital_prison_reporting_mi_ui_ec_cluster.replication_group_id
   }
 }
