c32546360819d65e72b1aa89688be00b56d4997f
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-09-01 16:27:54 +0100

    :fire: Remove ECR Kubernetes secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/maintenance-pages/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/maintenance-pages/resources/ecr.tf
@@ -26,17 +26,3 @@ module "ecr_credentials" {
   github_repositories = ["cloud-platform-maintenance-pages"]
 }
 
-resource "kubernetes_secret" "ecr_credentials" {
-  metadata {
-    name      = "ecr-credentials-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    access_key_id     = module.ecr_credentials.access_key_id
-    secret_access_key = module.ecr_credentials.secret_access_key
-    repo_arn          = module.ecr_credentials.repo_arn
-    repo_url          = module.ecr_credentials.repo_url
-  }
-}
-
