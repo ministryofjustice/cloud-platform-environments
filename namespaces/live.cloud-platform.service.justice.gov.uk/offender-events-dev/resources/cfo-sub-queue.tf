7fac8430f06552a2570a6abd521ecf64d3d4a1ba
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-09-01 15:28:07 +0100

    Update CFO queue for IRSA

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/cfo-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/cfo-sub-queue.tf
@@ -78,7 +78,7 @@ module "cfo_dead_letter_queue" {
 }
 
 resource "aws_iam_user" "user" {
-  name = "cfo-queue-user-${random_id.id.hex}"
+  name = "cfo-queue-user"
   path = "/system/cfo-queue-user/"
 }
 
