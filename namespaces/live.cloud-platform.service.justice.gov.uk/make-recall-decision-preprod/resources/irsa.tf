f0fb8e1a9c605863c5b244971b6481c5d40c29fe
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-09-01 14:21:48 +0100

    mrd-1670_irsa-preprod-make-recall-decisions-remove-duplicate-key: removed duplicate key

--- namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-preprod/resources/irsa.tf
@@ -36,17 +36,3 @@ data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
-
-data "aws_sns_topic" "hmpps-domain-events" {
-  name = "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd"
-}
-
-resource "kubernetes_secret" "hmpps_domain_events_topic" {
-  metadata {
-    name      = "hmpps-domain-events-topic"
-    namespace = var.namespace
-  }
-  data = {
-    topic_arn = data.aws_sns_topic.hmpps-domain-events.arn
-  }
-}
