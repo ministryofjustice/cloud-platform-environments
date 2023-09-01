471331cba91f750baf7d69b466ffd2945f25c0de
Michael Clancy <92720613+mtac50@users.noreply.github.com>
2023-09-01 12:33:52 +0100

    LASB-2396: Limit SQS access to IAM Role from Landing Zone (#16121)
    * Revert "LASB-2396: Opened up all DEV SQS queues to the LAA LandingZone DEV account for read access."
    
    This reverts commit 3429b94f1ff0cbc19d968b96695ba8a93e946fea.
    
    * Dummy commit

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/messaging-queues.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/messaging-queues.tf
@@ -42,8 +42,7 @@ resource "aws_sqs_queue_policy" "create_link_queue_m_policy" {
           "Effect": "Allow",
           "Principal": {
           "AWS": [
-            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0",
-            "411213865113"
+            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0"
               ]
           },
           "Resource": "${module.create_link_queue_m.sqs_arn}",
@@ -120,8 +119,7 @@ resource "aws_sqs_queue_policy" "unlink_queue_m_policy" {
           "Effect": "Allow",
           "Principal": {
           "AWS": [
-            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0",
-            "411213865113"
+            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0"
               ]
           },
           "Resource": "${module.unlink_queue_m.sqs_arn}",
@@ -227,8 +225,7 @@ resource "aws_sqs_queue_policy" "laa_status_update_queue_policy" {
           "Effect": "Allow",
           "Principal": {
           "AWS": [
-            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0",
-            "411213865113"
+            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0"
               ]
           },
           "Resource": "${module.laa_status_update_queue.sqs_arn}",
@@ -275,8 +272,7 @@ resource "aws_sqs_queue_policy" "hearing_resulted_queue_policy" {
           "Effect": "Allow",
           "Principal": {
           "AWS": [
-            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0",
-            "411213865113"
+            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0"
               ]
           },
           "Resource": "${module.hearing_resulted_queue.sqs_arn}",
@@ -375,8 +371,7 @@ resource "aws_sqs_queue_policy" "prosecution_concluded_queue_policy" {
           "Effect": "Allow",
           "Principal": {
           "AWS": [
-            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0",
-            "411213865113"
+            "arn:aws:iam::411213865113:role/LAA-maat-cd-api-development-ECSTaskExecutionRole-14XJSFYJHCVF0"
               ]
           },
           "Resource": "${module.prosecution_concluded_queue.sqs_arn}",
