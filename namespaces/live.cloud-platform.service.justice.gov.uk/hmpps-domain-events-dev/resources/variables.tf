d163b806ab44d8f7c695f3eb0b6225e7c6370013
Bill Sclater <bill.sclater@justice.gov.uk>
2023-09-01 10:24:43 +0100

    mrd-1670_remove-duplicate-event-topic-key: another duplicate key removal

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-dev/resources/variables.tf
@@ -34,7 +34,6 @@ variable "additional_topic_clients" {
     "hmpps-manage-offences-api-dev",
     "hmpps-manage-adjudications-api-dev",
     "hmpps-non-associations-dev",
-    "hmpps-probation-integration-services-dev",
     "hmpps-registers-dev",
     "hmpps-tier-dev",
     "hmpps-workload-dev",
