19304219907f4c3e6bc90e1f4f5e1d8cc1ddd63b
Bill Sclater <bill.sclater@justice.gov.uk>
2023-09-01 12:20:04 +0100

    mrd-1670_irsa-removed-duplicate-key: duplicate secret keys removed for qs-hmpps-audit-secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/make-recall-decision.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/make-recall-decision.tf
@@ -5,8 +5,6 @@ resource "kubernetes_secret" "make-recall-decision_secret" {
   }
 
   data = {
-    access_key_id     = module.hmpps_audit_queue.access_key_id
-    secret_access_key = module.hmpps_audit_queue.secret_access_key
     sqs_queue_url     = module.hmpps_audit_queue.sqs_id
     sqs_queue_arn     = module.hmpps_audit_queue.sqs_arn
     sqs_queue_name    = module.hmpps_audit_queue.sqs_name
