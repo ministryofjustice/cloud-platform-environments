af34c332fcd9b15c83559ba670298e0e21b40771
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-09-05 17:13:15 +0100

    PI-1429

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-domain-events-prod/resources/access-key-delius.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_user" "delius-domain-event-topic-user" {
+  name = "delius-domain-event-topic-user-prod"
+  path = "/system/delius-domain-event-topic-user/"
+}
+
+resource "aws_iam_access_key" "delius-domain-event-access" {
+  user = aws_iam_user.delius-domain-event-topic-user.name
+}
+
+resource "aws_iam_user_policy_attachment" "topic-policy" {
+  policy_arn = module.hmpps-domain-events.irsa_policy_arn
+  user       = aws_iam_user.delius-domain-event-topic-user.name
+}
+
+resource "kubernetes_secret" "delius-hmpps-domain-events-topic-credentials" {
+  metadata {
+    name      = "delius-hmpps-domain-events-topic-credentials"
+    namespace = var.namespace
+  }
+  data = {
+    topic_arn         = module.hmpps-domain-events.topic_arn
+    access_key_id     = aws_iam_access_key.delius-domain-event-access.id
+    secret_access_key = aws_iam_access_key.delius-domain-event-access.secret
+  }
+}
