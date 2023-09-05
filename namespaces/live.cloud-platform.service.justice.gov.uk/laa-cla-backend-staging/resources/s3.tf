489622e2cc2459967953291054f6beb6734f3ce6
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-05 17:10:39 +0100

    Removed user policy from S3 buckets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/s3.tf
@@ -14,39 +14,6 @@ module "cla_backend_private_reports_bucket" {
     aws = aws.london
   }
 
-  user_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket"
-    ],
-    "Resource": [
-      "$${bucket_arn}",
-      "arn:aws:s3:::cloud-platform-b88610a8f07bc115d7d038d15f6170c5",
-      "arn:aws:s3:::cloud-platform-9025c5a1a81bca7eaefd78a38df7d7de"
-    ]
-  },
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:*"
-    ],
-    "Resource": [
-      "$${bucket_arn}/*",
-      "arn:aws:s3:::cloud-platform-b88610a8f07bc115d7d038d15f6170c5/*",
-      "arn:aws:s3:::cloud-platform-9025c5a1a81bca7eaefd78a38df7d7de/*"
-    ]
-  }
-]
-}
-EOF
-
 }
 
 module "cla_backend_deleted_objects_bucket" {
@@ -64,35 +31,6 @@ module "cla_backend_deleted_objects_bucket" {
   providers = {
     aws = aws.london
   }
-
-  user_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket"
-    ],
-    "Resource": [
-      "$${bucket_arn}"
-    ]
-  },
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:*"
-    ],
-    "Resource": [
-      "$${bucket_arn}/*"
-    ]
-  }
-]
-}
-EOF
 
 }
 
