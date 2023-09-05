8cbe1d9222d98fcd64cde5280f5b41e58b754034
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-05 17:00:49 +0100

    Keed the user policy alongside until we know it works

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service-metadata-bucket.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service-metadata-bucket.tf
@@ -14,6 +14,46 @@ module "service-metadata-s3-bucket" {
   providers = {
     aws = aws.london
   }
+
+  user_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Sid": "",
+    "Effect": "Allow",
+    "Action": [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions"
+    ],
+    "Resource": "$${bucket_arn}"
+  },
+  {
+    "Sid": "",
+    "Effect": "Allow",
+    "Action": [
+      "s3:AbortMultipartUpload",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectTagging",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:PutObjectTagging",
+      "s3:PutObjectVersionAcl",
+      "s3:PutObjectVersionTagging",
+      "s3:RestoreObject"
+    ],
+    "Resource": "$${bucket_arn}/*"
+  }
+]
+}
+EOF
 }
 
 module "irsa" {
@@ -26,6 +66,8 @@ module "irsa" {
   role_policy_arns = {
     s3  = module.s3.irsa_policy_arn
   }
+
+  # Tags
 
   team_name              = var.team_name
   acl                    = "private"
