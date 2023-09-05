ad5327b09dc52f901fd7afd7aa54081efcd55555
Peter Phillips <peter@greenthistle.co.uk>
2023-09-05 09:30:33 +0100

    SDIT-1066: ✨ Add missing kubernetes version constraint

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-events-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-events-dev/resources/versions.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.23.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
   }
 }
 
