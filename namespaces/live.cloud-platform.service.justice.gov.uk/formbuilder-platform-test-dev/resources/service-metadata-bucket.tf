85c68693ad5d9c642c19e1d5633b7ca7d88b8aa2
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-05 15:57:06 +0100

    Replace s3 user policies with irsa definitions in formbuilder-platform-test-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service-metadata-bucket.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service-metadata-bucket.tf
@@ -14,46 +14,27 @@ module "service-metadata-s3-bucket" {
   providers = {
     aws = aws.london
   }
-
-  user_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListBucketVersions"
-    ],
-    "Resource": "$${bucket_arn}"
-  },
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:AbortMultipartUpload",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PutObjectTagging",
-      "s3:PutObjectVersionAcl",
-      "s3:PutObjectVersionTagging",
-      "s3:RestoreObject"
-    ],
-    "Resource": "$${bucket_arn}/*"
-  }
-]
 }
-EOF
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # IRSA configuration
+  service_account_name = "service-metadata-irsa-${var.environment-name}"
+  namespace            = var.namespace # this is also used as a tag
+
+  role_policy_arns = {
+    s3  = module.s3.irsa_policy_arn
+  }
+
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business_unit          = "transformed-department"
+  application            = "formbuilderservicemetadata"
+  is_production          = var.is_production
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
 }
 
 resource "kubernetes_secret" "service-metadata-s3-bucket" {
--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/user-filestore.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/user-filestore.tf
@@ -18,53 +18,6 @@ module "user-filestore-s3-bucket" {
     aws = aws.london
   }
 
-  user_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListBucketVersions",
-      "s3:GetLifecycleConfiguration",
-      "s3:PutLifecycleConfiguration"
-    ],
-    "Resource": "$${bucket_arn}"
-  },
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:AbortMultipartUpload",
-      "s3:DeleteObject",
-      "s3:DeleteObjectTagging",
-      "s3:DeleteObjectVersion",
-      "s3:DeleteObjectVersionTagging",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PutObjectTagging",
-      "s3:PutObjectVersionAcl",
-      "s3:PutObjectVersionTagging",
-      "s3:RestoreObject"
-    ],
-    "Resource": "$${bucket_arn}/*"
-  }
-]
-}
-EOF
-
-
   lifecycle_rule = [
     {
       enabled                                = true
@@ -83,6 +36,27 @@ EOF
       ]
     },
   ]
+}
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # IRSA configuration
+  service_account_name = "user-filestore-irsa-${var.environment-name}"
+  namespace            = var.namespace # this is also used as a tag
+
+  role_policy_arns = {
+    s3  = module.s3.irsa_policy_arn
+  }
+
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business_unit          = "transformed-department"
+  application            = "formbuilderuserfilestore"
+  is_production          = var.is_production
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
 }
 
 resource "kubernetes_secret" "user-filestore-s3-bucket" {
