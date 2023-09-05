64ad1bb44dfe99e20c459a1a460e808b008d0783
gareth.davies <gareth.m.davies@gmail.com>
2023-09-05 21:52:32 +0100

    SI - changes statement to resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/s3.tf
@@ -56,7 +56,7 @@ data "aws_iam_policy_document" "pin_phone_s3_policy" {
     actions = [
       "s3:ListBucket"
     ]
-    resource = module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn
+    resources = [module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn]
   }
 
   statement {
@@ -70,7 +70,7 @@ data "aws_iam_policy_document" "pin_phone_s3_policy" {
       "s3:PutObjectTagging",
       "s3:DeleteObject"
     ]
-    resource = "${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*"
+    resources = ["${module.hmpps_pin_phone_monitor_document_s3_bucket.bucket_arn}/*"]
   }
 }
 
