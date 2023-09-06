abc528b7606a4a113e7403ebea606963b0b25479
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-09-06 12:16:39 +0100

    Updating secret name.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-report-service-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-report-service-prod/resources/secret.tf
@@ -10,7 +10,7 @@ module "secrets_manager" {
   eks_cluster_name        = var.eks_cluster_name
 
   secrets = {
-    "crime-apps-adaptor-alert-webhook-prod" = {
+    "dces-report-service-alert-webhook-prod" = {
       description             = "DCES Report Service Slack Webhook",
       recovery_window_in_days = 7,
       k8s_secret_name         = "dces-report-service-alert-webhook-prod"
