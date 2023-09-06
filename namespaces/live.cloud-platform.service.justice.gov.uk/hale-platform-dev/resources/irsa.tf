02f3519c20dff58073c305f109ef2e020e5e413e
adam <brown-a2@users.noreply.github.com>
2023-09-06 14:00:52 +0100

    Add lawcom s3 bucket policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/irsa.tf
@@ -13,8 +13,54 @@ module "irsa" {
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
     s3 = module.s3_bucket.irsa_policy_arn
+    s3_lawcom = aws_iam_policy.tacticalproducts_lawcom_s3_policy.arn,
     ecr = module.ecr_credentials.irsa_policy_arn,
     ecr2 = module.ecr_feed_parser.irsa_policy_arn,
+  }
+
+  data "aws_iam_policy_document" "tacticalproducts_lawcom_s3_policy" {
+    # Provide list of permissions and target AWS account resources to allow access to
+    statement {
+      actions = [
+        "s3:ListBucket",
+      ]
+      resources = [
+        "arn:aws:s3:::lawcom-prod-storage-11jsxou24uy7q",
+     ]
+    }
+    statement {
+      actions = [
+        "s3:*",
+      ]
+      resources = [
+        "arn:aws:s3:::lawcom-prod-storage-11jsxou24uy7q/*"
+      ]
+    }
+  }
+
+  resource "aws_iam_policy" "tacticalproducts_lawcom_s3_policy" {
+  name   = "tacticalproducts_lawcom_s3_policy"
+  policy = data.aws_iam_policy_document.tacticalproducts_lawcom_s3_policy.json
+
+  tags = {
+    business_unit          = var.business_unit
+    application            = var.application
+    is_production          = var.is_production
+    team_name              = var.team_name
+    environment_name       = var.environment
+    infrastructure_support = var.infrastructure_support
+    }
+  }
+
+  resource "kubernetes_secret" "lawcom_aws_secret" {
+    metadata {
+    name      = "lawcom-prod-s3-bucket"
+    namespace = var.namespace
+  }
+
+  data = {
+      bucket_arn = "arn:aws:s3:::lawcom-prod-storage-11jsxou24uy7q"
+    }
   }
 
   # Tags
