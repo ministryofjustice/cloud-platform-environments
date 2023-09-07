55c6335347a6ff49c4224eb0684b7415963138bd
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-09-07 10:52:50 +0100

    Removing unneeded section.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-report-service-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-report-service-dev/resources/ecr.tf
@@ -15,18 +15,6 @@ module "ecr_credentials" {
   # specify which GitHub repository you're pushing from
   github_repositories = ["laa-dces-report-service"]
 
-  # set this if you use one GitHub repository to push to multiple container repositories
-  # this ensures the variable key used in the workflow is unique
-  github_actions_prefix = "dev"
-
-  /*
-    By default scan_on_push is set to true. When this is enabled then all images pushed to the repo are scanned for any security
-    / software vulnerabilities in your image and the results can be viewed in the console. For further details, please see:
-    https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html
-    To disable 'scan_on_push', set it to false as below:
-  scan_on_push = "false"
-  */
-
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
