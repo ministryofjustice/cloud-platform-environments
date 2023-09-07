4892967af96b6fe14141ff6be744aa9462eae7f4
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-09-07 14:12:29 +0100

    REM-1 renamed namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-preprod/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "remand-and-sentencing-preprod"
+  default     = "hmpps-remand-and-sentencing-preprod"
 }
 
 variable "business_unit" {
