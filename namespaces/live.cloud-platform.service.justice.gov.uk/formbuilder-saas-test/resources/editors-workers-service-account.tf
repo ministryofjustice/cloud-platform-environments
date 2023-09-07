3c37e32793918451a8bdf142c9b4b34863521dc1
jakemulley <jakemulley@users.noreply.github.com>
2023-09-07 16:23:31 +0100

    formbuilder-saas-test: get namespace secret and secret custom sa that has a token

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/editors-workers-service-account.tf
@@ -0,0 +1,73 @@
+local {
+  sa_name = "formbuilder-editor-workers-test"
+}
+
+data "kubernetes_secret" "s3_bucket_arns" {
+  metadata {
+    name      = "policy-arns"
+    namespace = var.namespace
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = var.eks_cluster_name
+}
+
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.30.0"
+
+  allow_self_assume_role     = false
+  assume_role_condition_test = "StringEquals"
+  create_role                = true
+  force_detach_policies      = true
+  role_name                  = "formbuilder-saas-test-live"
+  role_policy_arns = {
+    s3 = data.kubernetes_secret.s3_bucket_arns.data.service_metadata_bucket_irsa
+  }
+
+  oidc_providers = {
+    (data.aws_eks_cluster.eks_cluster.name) : {
+      provider_arn               = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")}"
+      namespace_service_accounts = ["${var.namespace}:${local.sa_name}"]
+    }
+  }
+
+  tags = local.default_tags
+}
+
+resource "kubernetes_service_account" "editors_workers" {
+  metadata {
+    name      = local.sa_name
+    namespace = var.namespace
+  }
+
+  secret {
+    name = "${local.sa_name}-token"
+  }
+
+  annotations = {
+    "eks.amazonaws.com/role-arn" = module.iam_assumable_role.iam_role_name
+  }
+
+  automount_service_account_token = true
+}
+
+resource "kubernetes_secret_v1" "editors_workers_token" {
+  metadata {
+    name      = "formbuilder-editor-workers-test-token"
+    namespace = var.namespace
+    annotations = {
+      "kubernetes.io/service-account.name" = local.sa_name
+    }
+  }
+
+  type = "kubernetes.io/service-account-token"
+
+  depends_on = [
+    kubernetes_service_account.editors_workers
+  ]
+}
