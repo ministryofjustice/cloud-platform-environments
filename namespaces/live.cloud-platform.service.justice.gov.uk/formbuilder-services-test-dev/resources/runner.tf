2a87e070c6210d1f3bb746742629cab8e8939288
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-08 12:35:24 +0100

    Add IRSA module and variable for runner to pick up service metadata S3 bucket policy arn

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-services-test-dev/resources/runner.tf
@@ -0,0 +1,26 @@
+module "runner-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name = var.eks_cluster_name
+
+  service_account_name = "runner-irsa-${var.environment-name}"
+  namespace            = var.namespace # this is also used as a tag
+
+  role_policy_arns = {
+    metadatas3  = data.kubernetes_secret.service-metadata-s3.service-metadata-s3-policy-arn
+  }
+
+  team_name              = var.team_name
+  business_unit          = "transformed-department"
+  application            = "formbuilderrunner"
+  is_production          = var.is_production
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
+data "kubernetes_secret" "service-metadata-s3" {
+  metadata {
+    name      = "service-metadata-s3-policy-arn"
+    namespace = var.namespace
+  }
+}
