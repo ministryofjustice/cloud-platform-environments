eefbe852148c81f38d6b5b5f396935427a928c28
adam <brown-a2@users.noreply.github.com>
2023-09-11 21:22:43 +0100

    Add missed Zone ID for LO

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/layobservers-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/layobservers-route53.tf
@@ -29,7 +29,7 @@ resource "aws_route53_record" "layobservers_route53_a_record" {
 
   alias {
     name                   = "dualstack.layob-loadb-1ihnwblvc4lwo-1400211917.eu-west-2.elb.amazonaws.com."
-    zone_id                = ""
+    zone_id                = "ZHURV8PSTC4K8"
     evaluate_target_health = false
   }
 }
