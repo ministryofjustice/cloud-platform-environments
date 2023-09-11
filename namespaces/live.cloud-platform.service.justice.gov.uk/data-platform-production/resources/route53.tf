20e58e0b0f7f4f42bf98ec1a1e27d401d3b10ca9
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-11 09:45:10 +0000

    Remove Data Platform
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/route53.tf
@@ -1,16 +1,3 @@
-resource "aws_route53_zone" "data_platform_production_route53_zone" {
-  name = "data-platform.service.justice.gov.uk"
-
-  tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    environment-name       = var.environment
-    owner                  = var.team_name
-    infrastructure-support = var.infrastructure_support
-  }
-}
-
 resource "aws_route53_zone" "data_platform_apps_alpha_route53_zone" {
   name = "apps.alpha.mojanalytics.xyz"
 
@@ -35,32 +22,4 @@ resource "aws_route53_zone" "data_platform_test_alpha_route53_zone" {
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
   }
-}
-
-/* Delegating to data-platform-development */
-resource "aws_route53_record" "data_platform_development_zone" {
-  zone_id = aws_route53_zone.data_platform_production_route53_zone.zone_id
-  name    = "development.data-platform.service.justice.gov.uk"
-  type    = "NS"
-  ttl     = "600"
-  records = ["ns-1741.awsdns-25.co.uk.", "ns-446.awsdns-55.com.", "ns-1406.awsdns-47.org.", "ns-952.awsdns-55.net."]
-}
-
-resource "kubernetes_secret" "data_platform_production_route53_zone_id" {
-  metadata {
-    name      = "cloud-platform-data-platform-production-route53-zone-id"
-    namespace = var.namespace
-  }
-
-  data = {
-    zone_id = aws_route53_zone.data_platform_production_route53_zone.zone_id
-  }
-}
-
-resource "aws_route53_record" "data_platform_technical_documentation" {
-  zone_id = aws_route53_zone.data_platform_production_route53_zone.zone_id
-  name    = "technical-documentation.data-platform.service.justice.gov.uk"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["ministryofjustice.github.io."]
 }
