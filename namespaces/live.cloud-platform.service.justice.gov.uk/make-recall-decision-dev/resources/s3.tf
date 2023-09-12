2a4f134cc335cd03c31c74a0ad15e68023bc3fbb
Robin de Villiers <robindevilliers@me.com>
2023-09-12 13:48:27 +0100

    Consider a recall s3 bucket for dev, renamed properties

--- namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/s3.tf
@@ -2,11 +2,11 @@ module "s3_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.0.0"
 
   team_name              = var.team_name
-  business-unit          = var.business_unit
+  business_unit          = var.business_unit
   application            = var.application
-  is-production          = var.is_production
-  environment-name       = var.environment
-  infrastructure-support = var.infrastructure_support
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 
   providers = {
