c6aabe68b31112de691b290d074f577248e7a782
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-13 10:06:47 +0100

    Add kubernetes to required providers

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-services-live-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-services-live-production/resources/versions.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.23.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
   }
 }
 
