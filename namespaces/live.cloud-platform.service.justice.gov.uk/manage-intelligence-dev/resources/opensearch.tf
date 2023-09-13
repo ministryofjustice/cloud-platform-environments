cd61c21e58ff9fa627742e5d3816f81fa51de5eb
brian.carr <brian.carr@digital.justice.gov.uk>
2023-09-13 15:21:29 +0100

    updated opensearch version to 2.7

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/opensearch.tf
@@ -1,12 +1,26 @@
+module "s3" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.0.0"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
 module "opensearch" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.1.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.2.0" # use the latest release
 
   # VPC/EKS configuration
   vpc_name         = var.vpc_name
   eks_cluster_name = var.eks_cluster_name
 
   # Cluster configuration
-  engine_version = "OpenSearch_2.5"
+  engine_version = "OpenSearch_2.7"
+  snapshot_bucket_arn = module.s3.bucket_arn
 
   cluster_config = {
     instance_count = 2
