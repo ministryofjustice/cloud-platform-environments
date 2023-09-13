1119d751e960115dbcb151ff3f25619c2b8f26ed
Mark Berridge <mark.berridge@digital.justice.gov.uk>
2023-09-13 13:25:44 +0100

    Renamed namespace hmpps-person-links-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-links-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-links-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "moj-person-links-dev"
+  default     = "hmpps-person-links-dev"
 }
 
 variable "business_unit" {
