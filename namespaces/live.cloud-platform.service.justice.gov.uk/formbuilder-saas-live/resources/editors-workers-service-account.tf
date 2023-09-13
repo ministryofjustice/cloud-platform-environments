016e400326956d70bfe617dedd081ed96836780f
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-13 11:02:12 +0100

    Use the exported data name, even though it is poorly named it is in the right namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-live/resources/editors-workers-service-account.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-live/resources/editors-workers-service-account.tf
@@ -11,7 +11,7 @@ data "kubernetes_secret" "s3_bucket_arns" {
 
 data "kubernetes_secret" "s3_bucket_arns_live" {
   metadata {
-    name      = "service-metadata-live-production-policy-arns"
+    name      = "service-metadata-test-production-policy-arns"
     namespace = var.namespace
   }
 }
