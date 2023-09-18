8b7f752a94aa5213ec9b197a5b04119299d1b002
adam <brown-a2@users.noreply.github.com>
2023-09-18 11:08:31 +0100

    Add Brookhouse domainkey DNS record

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/brookhouse-route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/brookhouse-route53.tf
@@ -197,3 +197,11 @@ resource "aws_route53_record" "brookhouse_route53_srv_record_autodiscover" {
   ttl     = "3600"
   records = ["10 10 443 autodiscover.homeoffice.gov.uk"]
 }
+
+resource "aws_route53_record" "brookhouse_route53_txt_smtp" {
+  zone_id = aws_route53_zone.brookhouse_route53_zone.zone_id
+  name    = "07092023._domainkey.brookhouseinquiry.org.uk."
+  type    = "TXT"
+  ttl     = "300"
+  records = ["v=DKIM1; t=s; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApFBvkc4d43rDfJNqF4PxbaTKY3j6hfLVcPrF5TC4IdvHvhMLRE0QSbLi/nJLXskM0MM0ClSUlnPV16HahDgkQdSwRc3nZleV2KqO2fKLdRque2ddG7K+huY9IvAtyqPD9CeCxuxXeakVYFqUYUyM5uhPoDrtQsHT2lhZ/faAN/e6FpdtBbOIpxiITbuZxanaWaTcn3GoN/A0RW;", "Ht0LeLpitU23tepDbysfuZFk7ZCc+qhNKUL+rNd5yWeaHHbs4Ge1KH6rMN7pXgHUMpvNl8YMIR2I1OvTz/R3IKn6M73MdUV8vlgjsdrZ4s+jOYUCBxQL3ukc+ogfWy9babqxgtaQIDAQAB"]
+}
