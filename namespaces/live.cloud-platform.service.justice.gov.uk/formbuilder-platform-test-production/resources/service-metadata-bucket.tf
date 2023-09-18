c1432e05b819d9f3572010dfd64798a9387ccfe1
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-18 10:05:39 +0100

    Remove long lived S3 access keys from formbuilder-platform-test-production

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-production/resources/service-metadata-bucket.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-production/resources/service-metadata-bucket.tf
@@ -14,46 +14,6 @@ module "service-metadata-s3-bucket" {
   providers = {
     aws = aws.london
   }
-
-  user_policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListBucketVersions"
-    ],
-    "Resource": "$${bucket_arn}"
-  },
-  {
-    "Sid": "",
-    "Effect": "Allow",
-    "Action": [
-      "s3:AbortMultipartUpload",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PutObjectTagging",
-      "s3:PutObjectVersionAcl",
-      "s3:PutObjectVersionTagging",
-      "s3:RestoreObject"
-    ],
-    "Resource": "$${bucket_arn}/*"
-  }
-]
-}
-EOF
 }
 
 module "service-metadata-irsa" {
@@ -86,10 +46,8 @@ resource "kubernetes_secret" "service-metadata-s3-bucket" {
   }
 
   data = {
-    access_key_id     = module.service-metadata-s3-bucket.access_key_id
     bucket_arn        = module.service-metadata-s3-bucket.bucket_arn
     bucket_name       = module.service-metadata-s3-bucket.bucket_name
-    secret_access_key = module.service-metadata-s3-bucket.secret_access_key
   }
 }
 
