9a8059021b5b4516484e2a7edb853b329590c994
Peter Phillips <peter@greenthistle.co.uk>
2023-09-19 17:50:19 +0100

    SDIT-988: ✨ Production instance of prisoner search with opensearch and postgres

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-prod/resources/rds.tf
@@ -0,0 +1,38 @@
+module "hmpps_prisoner_search_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.0"
+
+  vpc_name                  = var.vpc_name
+  team_name                 = var.team_name
+  business_unit             = var.business_unit
+  application               = var.application
+  is_production             = var.is_production
+  namespace                 = var.namespace
+  environment_name          = var.environment
+  infrastructure_support    = var.infrastructure_support
+  db_instance_class         = "db.t4g.micro"
+  db_engine                 = "postgres"
+  db_engine_version         = "15"
+  rds_family                = "postgres15"
+  deletion_protection       = true
+  prepare_for_major_upgrade = false
+  db_max_allocated_storage  = "500"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_prisoner_search_rds" {
+  metadata {
+    name      = "rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_prisoner_search_rds.rds_instance_endpoint
+    database_name         = module.hmpps_prisoner_search_rds.database_name
+    database_username     = module.hmpps_prisoner_search_rds.database_username
+    database_password     = module.hmpps_prisoner_search_rds.database_password
+    rds_instance_address  = module.hmpps_prisoner_search_rds.rds_instance_address
+  }
+}
