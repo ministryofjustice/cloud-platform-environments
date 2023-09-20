3197f9c468e5d78ef61e8d10dbf1d99969ce8ef3
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-09-20 14:01:18 +0100

    Removed ECR as DEV ECR will be re-used

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-orchestration-test/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-orchestration-test/resources/pingdom.tf
@@ -9,7 +9,7 @@ resource "pingdom_check" "laa-maat-orchestration-test" {
   url                      = "/actuator/health"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_laa,application_laa-maat-orchestration,component_ping,isproduction_false,environment_dev,owner_laa-crime-apps-team"
+  tags                     = "businessunit_laa,application_laa-maat-orchestration,component_ping,isproduction_false,environment_test,owner_laa-crime-apps-team"
   probefilters             = "region:EU"
   integrationids           = [121160]
 }
