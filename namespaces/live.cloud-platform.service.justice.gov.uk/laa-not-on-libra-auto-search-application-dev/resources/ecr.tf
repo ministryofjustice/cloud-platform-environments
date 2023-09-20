504387847330e3717bea0fce80d51dadef2afb98
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-09-20 12:06:38 +0100

    Using GHA for application pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-dev/resources/ecr.tf
@@ -1,87 +1,86 @@
-module "ecr_credentials" {
-  source         = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
-  team_name      = var.team_name
-  repo_name      = "${var.namespace}-ecr"
-  oidc_providers = ["circleci"]
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.0.0"
 
-  # This is the namespace to create a ConfigMap of credentials you need in CircleCI
-  namespace = var.namespace
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ECR name, AWS access key, and AWS secret key, for use in
-  # github actions CI/CD pipelines
-  github_repositories = ["laa-nolasa"]
+  # REQUIRED: Repository configuration
+  repo_name = var.namespace
 
-  # list of github environments, to create the ECR secrets as environment secrets
-  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["github"]
 
+  # REQUIRED: GitHub repositories that push to this container repository
+  github_repositories = ["example-repository"]
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+
+  # Lifecycle policies
+  # Uncomment the below to automatically tidy up old Docker images
   /*
-  # Lifecycle_policy provides a way to automate the cleaning up of your container images by expiring images based on age or count.
-  # To apply multiple rules, combined them in one policy JSON.
-  # https://docs.aws.amazon.com/AmazonECR/latest/userguide/lifecycle_policy_examples.html
-
   lifecycle_policy = <<EOF
-{
-    "rules": [
+    {
+      "rules": [
         {
-            "rulePriority": 1,
-            "description": "Expire untagged images older than 14 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 14
-            },
-            "action": {
-                "type": "expire"
-            }
+          "rulePriority": 1,
+          "description": "Expire untagged images older than 14 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 14
+          },
+          "action": {
+            "type": "expire"
+          }
         },
         {
-            "rulePriority": 2,
-            "description": "Keep last 30 dev and staging images",
-            "selection": {
-                "tagStatus": "tagged",
-                "tagPrefixList": ["dev", "staging"],
-                "countType": "imageCountMoreThan",
-                "countNumber": 30
-            },
-            "action": {
-                "type": "expire"
-            }
+          "rulePriority": 2,
+          "description": "Keep last 30 dev and staging images",
+          "selection": {
+            "tagStatus": "tagged",
+            "tagPrefixList": ["dev", "staging"],
+            "countType": "imageCountMoreThan",
+            "countNumber": 30
+          },
+          "action": {
+            "type": "expire"
+          }
         },
         {
-            "rulePriority": 3,
-            "description": "Keep the newest 100 images and mark the rest for expiration",
-            "selection": {
-                "tagStatus": "any",
-                "countType": "imageCountMoreThan",
-                "countNumber": 100
-            },
-            "action": {
-                "type": "expire"
-            }
+          "rulePriority": 3,
+          "description": "Keep the newest 100 images and mark the rest for expiration",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 100
+          },
+          "action": {
+            "type": "expire"
+          }
         }
-    ]
-}
-EOF
-*/
+      ]
+    }
+    EOF
+  */
 
-  # Tags
+  # OPTIONAL: Add deletion_protection = false parameter if you are planning on either deleting your environment namespace or ECR resource.
+  # IMPORTANT: It is the PR owners responsibility to ensure that no other environments are sharing this ECR registry.
+  # This flag will allow a non-empty ECR to be deleted.
+  # Defaults to true
+
+  # deletion_protection = false
+
+  # Tags (commented out until release)
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-
-resource "kubernetes_secret" "ecr_credentials" {
-  metadata {
-    name      = "ecr-repo-${var.namespace}"
-    namespace = var.namespace
-  }
-
-  data = {
-    repo_arn = module.ecr_credentials.repo_arn
-    repo_url = module.ecr_credentials.repo_url
-  }
 }
