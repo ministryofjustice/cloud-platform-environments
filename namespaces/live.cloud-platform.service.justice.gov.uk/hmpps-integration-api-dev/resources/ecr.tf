e2b2339af5b90843bf24e0199f38c454e0facba9
Emile <emile.swarts@digital.justice.gov.uk>
2023-09-20 10:18:28 +0100

    Bump Cloud Platform ECR credentials module
    Also remove default tags from this module as it leads to duplicated
    tags.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/ecr.tf
@@ -1,8 +1,12 @@
 module "ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=5.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
   oidc_providers = ["circleci"]
   github_repositories = [var.github_repo_name]
   namespace = var.namespace
+
+  providers = {
+    aws = aws.london_without_default_tags
+  }
 }
