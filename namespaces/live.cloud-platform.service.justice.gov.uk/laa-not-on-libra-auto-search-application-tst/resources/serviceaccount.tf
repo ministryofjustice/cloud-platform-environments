85709390bf546e27cc78299c4ae220fc6a7b70e0
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-09-21 15:04:00 +0100

    Add SA for GHA for TST env

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/serviceaccount.tf
@@ -13,3 +13,22 @@ module "serviceaccount" {
   serviceaccount_name = "circleci"
   
 }
+
+module "serviceaccount-github" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=0.9.7"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "01-01-2000"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["laa-nolasa"]
+
+  serviceaccount_name = "github-actions"
+
+  role_name = "serviceaccount-role-github"
+  rolebinding_name = "serviceaccount-rolebinding-github"
+
+}
