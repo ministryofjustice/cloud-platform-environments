5c0ef9fc86c0885265ec6d372084a63ffb68dc24
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-22 16:07:48 +0100

    Fixed multiline strng

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notifiy-orchestrator-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notifiy-orchestrator-staging/resources/variables.tf
@@ -35,7 +35,7 @@ variable "team_name" {
 variable "environment" {
   description = "Name of the environment type for this service"
   type        = string
-  default     = "staging""
+  default     = "staging"
 }
 
 variable "infrastructure_support" {
@@ -57,7 +57,7 @@ variable "slack_channel" {
 }
 
 variable "github_owner" {
-  description = "The GitHub organization or individual user account containing the app's code repo."
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
   default     = "ministryofjustice"
 }
