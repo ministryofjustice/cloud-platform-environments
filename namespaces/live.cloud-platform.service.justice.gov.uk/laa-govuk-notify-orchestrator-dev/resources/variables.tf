43ae910f3fc2305725f2879cc65731761d37a188
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-22 10:53:08 +0100

    Fix typo in namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notify-orchestrator-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notify-orchestrator-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "laa-govuk-notifiy-orchestrator-dev"
+  default     = "laa-govuk-notify-orchestrator-dev"
 }
 
 variable "business_unit" {
