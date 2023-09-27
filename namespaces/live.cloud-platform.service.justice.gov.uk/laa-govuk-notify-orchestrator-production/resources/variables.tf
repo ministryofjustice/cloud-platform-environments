14a25329b030a79a224ec0fe12069bb1f5d499ef
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-27 10:10:50 +0100

    Fixed namespace mismatch

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notify-orchestrator-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-govuk-notify-orchestrator-production/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "laa-govuk-notify-orchestrator-staging"
+  default     = "laa-govuk-notify-orchestrator-production"
 }
 
 variable "business_unit" {
