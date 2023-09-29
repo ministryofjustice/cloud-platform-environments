37565969009955e59397a3d481191d952e8738c1
Mark Berridge <mark.berridge@digital.justice.gov.uk>
2023-09-29 10:13:37 +0100

    Creating namespace hmpps-person-links-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-links-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-links-preprod/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "hmpps-person-links-dev"
+  default     = "hmpps-person-links-preprod"
 }
 
 variable "business_unit" {
