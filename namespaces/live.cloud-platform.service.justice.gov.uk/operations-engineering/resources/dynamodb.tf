9e275e1853c719e0a567f7ffa1bb468db19c79f9
ben-al <ben.ashton@digital.justice.gov.uk>
2023-10-03 14:34:23 +0100

    Merge collab resources and mark other resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/dynamodb.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/dynamodb.tf
@@ -1,3 +1,31 @@
+module "opseng_tf_state_lock" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=4.0.0"
+
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+
+  hash_key          = "LockID"
+  enable_encryption = "false"
+  enable_autoscaler = "true"
+}
+
+resource "kubernetes_secret" "opseng_tf_state_lock" {
+  metadata {
+    name      = "terraform-state-lock-table"
+    namespace = var.namespace
+  }
+
+  data = {
+    table_name = module.opseng_tf_state_lock.table_name
+    table_arn  = module.opseng_tf_state_lock.table_arn
+  }
+}
+
 module "ap_gh_collab_repo_tf_state_lock" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=4.0.0"
 
@@ -25,3 +53,4 @@ resource "kubernetes_secret" "ap_gh_collab_repo_tf_state_lock" {
     table_arn  = module.ap_gh_collab_repo_tf_state_lock.table_arn
   }
 }
+
