500fade04e06292b1507b6566d76a3fdcfb719fc
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-10-04 00:14:14 +0100

    Rename the repo name for ap-rshiny-notesbook app

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-ap-rshiny-notesbook-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-app-ap-rshiny-notesbook-dev/resources/serviceaccount.tf
@@ -6,7 +6,7 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["ap-rshiny-notebook"]
+  github_repositories = ["ap-rshiny-notebooks"]
 
   github_environments = ["dev"]
 
