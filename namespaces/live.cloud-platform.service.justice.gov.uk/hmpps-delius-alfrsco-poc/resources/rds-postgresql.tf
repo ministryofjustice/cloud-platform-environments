ac0096a8cc07dcbbb1db6b5ee78bc6fe4c9274d4
George Taylor <george.taylor@digital.justice.gov.uk>
2023-10-03 10:14:51 +0100

    rds version and family (#16837)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds-postgresql.tf
@@ -20,8 +20,8 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "14.7"
-  rds_family        = "postgres14"
+  db_engine_version = "11.17"
+  rds_family        = "postgres11"
   db_instance_class = "db.t3.micro"
 
   # snapshot restore
