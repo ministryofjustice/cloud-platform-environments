b1cc71e95d4714a0221009691dc60d864d7844e6
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-10-03 14:09:51 +0100

    Update application names

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-development/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-development/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "laa-infox-dev"
+  default     = "laa-information-exchange-dev"
 }
 
 variable "business_unit" {
