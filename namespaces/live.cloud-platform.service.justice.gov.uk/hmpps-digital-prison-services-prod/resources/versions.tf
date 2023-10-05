bcfe50dedc88bd691dfbea8670b6166e5e65208e
Dani Burnley <dan@danops.uk>
2023-10-05 12:06:25 +0100

    CDPS-427: Add pingdom for the DPS Homepage

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-services-prod/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-services-prod/resources/versions.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20.0"
     }
+    pingdom = {
+      source  = "DrFaust92/pingdom"
+      version = "~> 1.3.1"
+    }
   }
 }
 
