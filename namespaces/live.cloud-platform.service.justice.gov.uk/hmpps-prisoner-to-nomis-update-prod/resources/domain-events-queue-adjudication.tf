4565ba8ae8c43247ea7a7cca6d9c481a07776243
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2023-10-06 12:40:42 +0100

    SDIT-1021 enable hearing deleted create filtering - prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-prod/resources/domain-events-queue-adjudication.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-prod/resources/domain-events-queue-adjudication.tf
@@ -11,7 +11,8 @@ resource "aws_sns_topic_subscription" "hmpps_prisoner_to_nomis_adjudication_subs
       "adjudication.hearing.created",
       "adjudication.hearing.updated",
       "adjudication.hearing.deleted",
-      "adjudication.hearingCompleted.created"
+      "adjudication.hearingCompleted.created",
+      "adjudication.hearingCompleted.deleted"
     ]
   })
 }
