60014e38a67d22eafed874fd6557ddd092a537af
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-10-11 10:27:05 +0100

    Initial commit of DCES-DRC-Integration-Dev namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-dev/resources/pingdom.tf
@@ -0,0 +1,15 @@
+resource "pingdom_check" "laa-dces-report-service-dev" {
+  type                     = "http"
+  name                     = "LAA DCES DRC Integration - Dev"
+  host                     = "laa-dces-drc-integration-dev.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 3
+  notifyagainevery         = 0
+  url                      = "/actuator/health"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_laa,application_laa-dces-drc-integration,component_ping,isproduction_false,environment_dev,owner_laa-crime-apps-team"
+  probefilters             = "region:EU"
+  integrationids           = [121160]
+}
