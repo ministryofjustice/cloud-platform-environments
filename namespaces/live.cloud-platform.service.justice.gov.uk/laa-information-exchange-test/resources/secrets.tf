5259aa5e096f7b7870ae1b768661d4c3c4a0dab5
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-10-11 15:26:27 +0100

    Missed a reference to "dev".

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-test/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-test/resources/secrets.tf
@@ -13,7 +13,7 @@ module "secrets_manager" {
     "laa-infox-db-password" = {
       description             = "InfoX Soap user database password", # Required
       recovery_window_in_days = 7, # Required - number of days that AWS Secrets Manager waits before it can delete the secret
-      k8s_secret_name        = "laa-infox-db-password-dev" # The name of the secret in k8s
+      k8s_secret_name        = "laa-infox-db-password-test" # The name of the secret in k8s
     },
   }
 }
