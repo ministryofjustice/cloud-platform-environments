1887f8a55e4b7ace5e8f978449dd291842087067
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-10-12 14:02:16 +0100

    Update pingdom for Content Hub prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/pingdom.tf
@@ -1,29 +1,37 @@
 provider "pingdom" {
 }
 
-resource "pingdom_check" "prisoner-content-hub-production-checks" {
-  for_each = {
-    drupal-cms  = "manage.content-hub.prisoner.service.justice.gov.uk",
-    berwyn      = "berwyn.content-hub.prisoner.service.justice.gov.uk",
-    cookhamwood = "cookhamwood.content-hub.prisoner.service.justice.gov.uk",
-    wayland     = "wayland.content-hub.prisoner.service.justice.gov.uk",
-  }
+# Integration IDs
+# 122531 = #launchpad-alerts
 
+resource "pingdom_check" "prisoner-content-hub-frontend-production" {
   type                     = "http"
-  name                     = "Prisoner Content Hub Production ${each.key}"
+  name                     = "Prisoner Content Hub Frontend"
   host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/https/${each.value}"
+  url                      = "/https/content-hub.prisoner.service.justice.gov.uk"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_${var.business_unit},application_prisoner_content_hub,component_healthcheck,isproduction_${var.is_production},environment_${var.environment-name},infrastructuresupport_platforms"
+  tags                     = "dps,hmpps,cloudplatform-managed,businessunit_${var.business_unit},application_prisoner_content_hub,component_healthcheck,isproduction_${var.is_production},environment_${var.environment-name},infrastructuresupport_platforms"
   probefilters             = "region:EU"
+  integrationids           = [122531]
+}
 
-  integrationids = [
-    106307 # #pfs_dev channel
-    # 106310, # #pfs_alerts channel
-  ]
+resource "pingdom_check" "prisoner-content-hub-backend-production" {
+  type                     = "http"
+  name                     = "Prisoner Content Hub Backend"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/manage.content-hub.prisoner.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed,businessunit_${var.business_unit},application_prisoner_content_hub,component_healthcheck,isproduction_${var.is_production},environment_${var.environment-name},infrastructuresupport_platforms"
+  probefilters             = "region:EU"
+  integrationids           = [122531]
 }
