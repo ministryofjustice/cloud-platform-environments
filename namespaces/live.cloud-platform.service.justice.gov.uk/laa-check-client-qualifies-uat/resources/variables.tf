9c67a7d90e823aca00d6bd06d539ffc47b40a71c
Hettie Street <hettie.street@digital.justice.gov.uk>
2023-10-17 12:25:53 +0100

    Add UAT namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-uat/resources/variables.tf
@@ -0,0 +1,65 @@
+
+
+variable "vpc_name" {
+}
+
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "Check if your client qualifies for legal aid"
+}
+
+variable "namespace" {
+  default = "laa-check-client-qualifies-uat"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "LAA"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "check-client-qualifies"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "uat"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "eligibility@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "laa-ccq-team"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
+
+variable "repo_name" {
+  description = "The name of github repo"
+  default     = "laa-check-client-qualifies"
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
