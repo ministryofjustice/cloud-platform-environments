2600018c7c91fc818dfa87f9bda70faf2da01322
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-10-18 12:20:01 +0100

    Rename namespace to include -dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-archiver-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-archiver-dev/resources/variables.tf
@@ -13,13 +13,13 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "justice-gov-uk-archiver"
+  default     = "justice-archiver-dev"
 }
 
 variable "archive_date" {
   description = "The date we want to see in the archive"
   type        = string
-  default     = "justice-gov-uk-archiver"
+  default     = "justice-archiver-dev"
 }
 
 variable "business_unit" {
