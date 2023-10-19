209b6b01d32affbfef91ccd00d008b520cbcc7c3
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-10-19 13:28:31 +0100

    Adding drc-integration uat environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-uat/resources/pingdom.tf
@@ -0,0 +1,15 @@
+resource "pingdom_check" "laa-dces-drc-integration-uat" {
+  type                     = "http"
+  name                     = "LAA DCES DRC Integration - uat"
+  host                     = "laa-dces-drc-integration-uat.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 3
+  notifyagainevery         = 0
+  url                      = "/actuator/health"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_laa,application_laa-dces-drc-integration,component_ping,isproduction_false,environment_uat,owner_laa-crime-apps-team"
+  probefilters             = "region:EU"
+  integrationids           = [121160]
+}
