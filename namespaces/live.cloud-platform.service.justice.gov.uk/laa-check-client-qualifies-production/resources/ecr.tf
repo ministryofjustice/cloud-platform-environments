db5ae719125d7c8e957a19492f9e8102757175b8
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-10-19 10:50:36 +0100

    update to repo name and adding in skip

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/ecr.tf
@@ -13,7 +13,8 @@ module "ecr_credentials" {
   oidc_providers = ["circleci"]
 
   # specify which GitHub repository your CircleCI job runs from
-  github_repositories = ["laa-check-client-qualifies"]
+  # TODO this needs to be updated when we change repo name
+  github_repositories = ["laa-estimate-financial-eligibility-for-legal-aid"]
 
   # set your namespace name to create a ConfigMap of credentials you need in CircleCI
   namespace = var.namespace
