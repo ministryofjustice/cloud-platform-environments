265b9a6af19bd1f22deff241846d480062d14439
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-10-20 13:20:39 +0100

    Add privilege Role binding to each namespace

---
+++ cmd/addPrivRoleBinding/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	cpCliEnv "github.com/ministryofjustice/cloud-platform-cli/pkg/environment"
+	cpCliUtils "github.com/ministryofjustice/cloud-platform-cli/pkg/util"
+)
+
+func main() {
+
+	home, _ := os.UserHomeDir()
+	repoPath := filepath.Join(home, "go/src/ministryofjustice/cloud-platform-environments/namespaces/live.cloud-platform.service.justice.gov.uk/")
+
+	folders, err := cpCliUtils.ListFolderPaths(repoPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var nsFolders []string
+	nsFolders = append(nsFolders, folders[1:]...)
+	for _, folder := range nsFolders {
+
+		fmt.Println("Folder:", folder)
+
+		if _, err := os.Stat(folder); os.IsNotExist(err) {
+			fmt.Printf("Namespace %s does not exist, skipping \n", folder)
+			panic(err)
+		}
+
+		namespace := folder[strings.LastIndex(folder, ",")+1:]
+
+		if err := os.Chdir(filepath.Join(namespace)); err != nil {
+			panic(err)
+		}
+
+		ns := cpCliEnv.Namespace{
+			Namespace: folder,
+		}
+
+		err := ns.ReadYaml()
+		if err != nil {
+			panic(err)
+		}
+
+		println("Name:", ns.Namespace, "IsProd:", ns.IsProduction, "/n")
+
+	}
+
+}
