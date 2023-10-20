1852562235ea097e686be7883f0373eebfc1305f
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-10-20 10:50:32 +0100

    test: 💍 test terraform plan (new var auto_tune_options)

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/opensearch.tf
@@ -12,7 +12,7 @@ module "s3" {
 }
 
 module "opensearch" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.2.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=pass-auto-tune-options-in" # use the latest release
 
   # VPC/EKS configuration
   vpc_name         = var.vpc_name
