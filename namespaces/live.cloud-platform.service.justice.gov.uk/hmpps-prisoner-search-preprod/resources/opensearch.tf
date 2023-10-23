34a42f5744d4ddbaf023c5181049964a643b6671
petergphillips <peter@greenthistle.co.uk>
2023-10-23 17:47:56 +0100

    SDIT-723: 🔧 Turn on auto tune for pre-prod (#17193)
    * SDIT-723: 🔧 Turn on auto tune for pre-prod
    
    * SDIT-723: 🔧 Remove cron and amend duration

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-preprod/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-preprod/resources/opensearch.tf
@@ -14,12 +14,22 @@ module "hmpps_prisoner_search_opensearch" {
 
   cluster_config = {
     instance_count = 2
-    instance_type  = "t3.medium.search"
+    instance_type  = "m6g.large.search"
   }
 
   ebs_options = {
     volume_size = 20
   }
+
+  auto_tune_config = {
+    desired_state                  = "ENABLED"
+    start_at                       = "2023-10-23T20:00:00.00Z"
+    duration_value                 = 10
+    duration_unit                  = "HOURS"
+    cron_expression_for_recurrence = ""
+    rollback_on_disable            = "NO_ROLLBACK"
+  }
+
   snapshot_bucket_arn = data.aws_s3_bucket.snapshot_bucket.arn
 }
 
