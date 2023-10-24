e79df533cdc1275e5fb9e14c21282ea7d1af8694
Lucas Cairns <lucas.cairns@gmail.com>
2023-10-24 11:00:57 +0100

    Add hmpps-audit queue secret to hmpps-assess-risks-and-needs-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/hmpps-assess-risks-and-needs.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_secret" "assess-risks-and-needs-secret" {
+  metadata {
+    name      = "hmpps-sqs-audit-queue"
+    namespace = "hmpps-assess-risks-and-needs-prod"
+  }
+
+  data = {
+    sqs_queue_url  = module.hmpps_audit_queue.sqs_id
+    sqs_queue_arn  = module.hmpps_audit_queue.sqs_arn
+    sqs_queue_name = module.hmpps_audit_queue.sqs_name
+  }
+}
