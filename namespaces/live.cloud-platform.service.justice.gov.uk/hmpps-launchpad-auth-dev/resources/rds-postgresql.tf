b1a1eede96b1217af370c6b723ab3a893e60ad47
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2023-10-24 10:17:56 +0100

    pushing empty commit to trigger code scan

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-auth-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-auth-dev/resources/rds-postgresql.tf
@@ -13,8 +13,8 @@ module "rds" {
   # RDS configuration
   prepare_for_major_upgrade    = false
   allow_minor_version_upgrade  = true
-  #  this will be set false after major version upgrade
-  #  allow_major_version_upgrade  = false
+  # this will be set false after major version upgrade
+  # allow_major_version_upgrade  = false
   performance_insights_enabled = true
   # db_max_allocated_storage     = "500"
   enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
