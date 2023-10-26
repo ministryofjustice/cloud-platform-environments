18913e16aa0161651595715adf51134a2074dec4
brian.carr <brian.carr@digital.justice.gov.uk>
2023-10-26 16:12:45 +0100

    Enable rds performance insights

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/rds-legacy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/rds-legacy.tf
@@ -13,9 +13,9 @@ module "rds_aurora_legacy" {
     min_capacity = 1
     max_capacity = 10
   }
-  replica_count = 1
-
-  allow_major_version_upgrade = true
+  replica_count                = 1
+  performance_insights_enabled = true
+  allow_major_version_upgrade  = true
 
   # Tags
   business_unit          = var.business_unit
