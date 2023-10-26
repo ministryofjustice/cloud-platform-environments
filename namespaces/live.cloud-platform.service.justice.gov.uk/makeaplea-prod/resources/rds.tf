6ccd2a9623672ecc9d3910529b6e4eac269d7434
itoulou <touloumbadjiani@gmail.com>
2023-10-26 12:05:11 +0100

    revert instance type

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-prod/resources/rds.tf
@@ -14,7 +14,7 @@ module "dps_rds" {
   is_production          = var.is_production
   namespace              = var.namespace
   db_engine_version      = "14.7"
-  db_instance_class      = "db.t4g.micro"
+  db_instance_class      = "db.t3.micro"
   db_max_allocated_storage = "500"
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
