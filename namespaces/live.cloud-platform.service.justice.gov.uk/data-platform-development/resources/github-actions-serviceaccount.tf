dbda664ff8c1371de0e2bdd90c3fa3b80cb6ac46
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-26 14:22:13 +0000

    revert changes to development
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-development/resources/github-actions-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-development/resources/github-actions-serviceaccount.tf
@@ -4,4 +4,11 @@ module "github_actions_serviceaccount" {
   kubernetes_cluster  = var.kubernetes_cluster
   namespace           = var.namespace
   serviceaccount_name = "github-actions"
+
+  github_repositories = ["data-platform"]
+
+  github_actions_secret_kube_cluster   = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_CLUSTER"
+  github_actions_secret_kube_namespace = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_NAMESPACE"
+  github_actions_secret_kube_cert      = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_CERT"
+  github_actions_secret_kube_token     = "CLOUD_PLATFORM_DATA_PLATFORM_DEVELOPMENT_KUBE_TOKEN"
 }
