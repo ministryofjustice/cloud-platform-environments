277fe8a680bd6a6557c33936bbbc5d7e3a144658
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-10-27 16:06:51 +0100

    MRD-2005 Use health kick for pingdom Prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ppud-automation-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ppud-automation-prod/resources/pingdom.tf
@@ -1,12 +1,12 @@
 resource "pingdom_check" "ppud_automation_api_health" {
   type                     = "http"
   name                     = "hmpps-ppud-automation-api /health - PROD"
-  host                     = "hmpps-ppud-automation-api.hmpps.service.justice.gov.uk"
+  host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/health"
+  url                      = "/https/hmpps-ppud-automation-api.hmpps.service.justice.gov.uk/health"
   encryption               = true
   port                     = 443
   tags                     = "hmpps,make-recall-decision,isproduction_true,cloudplatform-managed"
