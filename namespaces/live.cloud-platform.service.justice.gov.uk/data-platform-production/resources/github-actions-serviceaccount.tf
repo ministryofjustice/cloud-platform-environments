0f0f6f16bee27de0691a41bcebc2cf097bd562d2
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-27 10:12:04 +0100

    🛂 Update Data Platform Production's SA rules (#17343)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/github-actions-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-production/resources/github-actions-serviceaccount.tf
@@ -4,4 +4,73 @@ module "github_actions_serviceaccount" {
   kubernetes_cluster  = var.kubernetes_cluster
   namespace           = var.namespace
   serviceaccount_name = "github-actions"
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "configmaps",
+        "pods",
+        "serviceaccounts",
+        "configmaps",
+        "persistentvolumeclaims"
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "update",
+        "delete",
+        "list",
+        "watch"
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "batch",
+        "networking.k8s.io",
+        "rbac.authorization.k8s.io",
+        "policy"
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "cronjobs",
+        "jobs",
+        "replicasets",
+        "statefulsets",
+        "networkpolicies",
+        "servicemonitors",
+        "roles",
+        "rolebindings",
+        "poddisruptionbudgets"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "monitoring.coreos.com",
+      ]
+      resources = [
+        "prometheusrules",
+        "servicemonitors",
+      ]
+      verbs = [
+        "*",
+      ]
+    },
+  ]
 }
