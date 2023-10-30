5bde5fbaeb890a6c52cc76c1705e134974f3dfac
Prakash Subramani <prakash.subramani@digital.justice.gov.uk>
2023-10-30 16:40:47 +0000

    Try deploying demo app

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prakash-test/resources/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.2.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.39.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+}
