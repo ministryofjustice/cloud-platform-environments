f6d3f0f2b3b0090eaea5cd15e35186da8242bab8
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-10-31 08:43:43 +0000

    cccd-staging: Update Elasticache Redis to 7.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/cccd-staging/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cccd-staging/resources/elasticache.tf
@@ -15,8 +15,8 @@ module "cccd_elasticache_redis" {
   namespace              = var.namespace
   business_unit          = var.business_unit
 
-  engine_version        = "6.x"
-  parameter_group_name  = "default.redis6.x"
+  engine_version        = "7.0"
+  parameter_group_name  = "default.redis7"
   number_cache_clusters = "3"
   node_type             = "cache.t4g.small"
 }
