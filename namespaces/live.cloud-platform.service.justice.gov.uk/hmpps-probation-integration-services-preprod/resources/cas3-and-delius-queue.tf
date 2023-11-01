4e3ed4a9b5b28f0f032b5bed5367aaab4c66d2f9
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-11-01 14:37:59 +0000

    PI-1599 addeded .updated events

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/cas3-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/cas3-and-delius-queue.tf
@@ -1,14 +1,19 @@
 resource "aws_sns_topic_subscription" "cas3-and-delius-queue-subscription" {
-  topic_arn = data.aws_sns_topic.hmpps-domain-events.arn
-  protocol  = "sqs"
-  endpoint  = module.cas3-and-delius-queue.sqs_arn
+  topic_arn     = data.aws_sns_topic.hmpps-domain-events.arn
+  protocol      = "sqs"
+  endpoint      = module.cas3-and-delius-queue.sqs_arn
   filter_policy = jsonencode({
-    eventType = ["accommodation.cas3.referral.submitted",
+    eventType = [
+      "accommodation.cas3.referral.submitted",
       "accommodation.cas3.booking.provisionally-made",
       "accommodation.cas3.booking.cancelled",
       "accommodation.cas3.person.arrived",
       "accommodation.cas3.booking.confirmed",
-      "accommodation.cas3.person.departed"]
+      "accommodation.cas3.person.departed",
+      "accommodation.cas3.person.arrived.updated",
+      "accommodation.cas3.booking.cancelled.updated",
+      "accommodation.cas3.person.departed.updated"
+    ]
   })
 }
 
