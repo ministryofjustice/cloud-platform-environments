7be33db401fa5ad033caf601342a986c80a5012c
Andrew Tulloch <1846916+atulloch@users.noreply.github.com>
2023-11-01 19:10:41 +0000

    Add random provider to versions.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.67.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 5.39.0"
