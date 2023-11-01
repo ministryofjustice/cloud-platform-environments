726a9b1f42a08c44aded9c0ef6376e41428b6f49
adam <brown-a2@users.noreply.github.com>
2023-11-01 15:55:49 +0000

    Add bucket permissions to AWS legacy bucket PPO
    Allow for the permissions to transfer bucket contents from old AWS
    account to CP.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/irsa.tf
@@ -41,8 +41,9 @@ module "irsa" {
         "arn:aws:s3:::justicejobs-prod-storage-u1mo8w50uvqm", #tacticalproducts legacy account
         "arn:aws:s3:::sifocc-prod-storage-7f6qtyoj7wir", #tacticalproducts legacy account
         "arn:aws:s3:::npm-prod-storage-19n0nag2nk8xk", #tacticalproducts legacy account
-        "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd" #tacticalproducts legacy account
-      ]
+        "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd", #tacticalproducts legacy account
+        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw" #tacticalproducts legacy account
+       ]
     }
     statement {
       actions = [
@@ -61,8 +62,9 @@ module "irsa" {
         "arn:aws:s3:::justicejobs-prod-storage-u1mo8w50uvqm/*", #tacticalproducts legacy account
         "arn:aws:s3:::sifocc-prod-storage-7f6qtyoj7wir/*", #tacticalproducts legacy account
         "arn:aws:s3:::npm-prod-storage-19n0nag2nk8xk/*", #tacticalproducts legacy account
-        "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd/*" #tacticalproducts legacy account
-     ]
+        "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd/*", #tacticalproducts legacy account
+        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw/*" #tacticalproducts legacy account
+      ]
     }
   }
 
