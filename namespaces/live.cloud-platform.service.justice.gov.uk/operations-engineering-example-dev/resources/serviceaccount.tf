2616e8e33e24a34b77b2023644a5ab38203b4126
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-11-02 14:19:29 +0000

    revert changes to dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-example-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-example-dev/resources/serviceaccount.tf
@@ -1,7 +1,7 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
-  namespace          = var.namespace
+  namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
   serviceaccount_token_rotated_date = "01-01-2000"
@@ -9,10 +9,4 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["operations-engineering-example"]
-
-  github_actions_secret_kube_token     = "DEV_KUBE_TOKEN"
-  github_actions_secret_kube_cert      = "DEV_KUBE_CERT"
-  github_actions_secret_kube_namespace = "DEV_KUBE_NAMESPACE"
-  github_actions_secret_kube_cluster   = "DEV_KUBE_CLUSTER"
-
 }
