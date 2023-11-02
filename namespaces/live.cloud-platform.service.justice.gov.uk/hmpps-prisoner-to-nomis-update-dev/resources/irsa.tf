27a8d5b1892593151cc6c7344e8074dd8bf76e70
petergphillips <peter@greenthistle.co.uk>
2023-11-02 10:57:30 +0000

    SDIT-670: 🚚 Create domain events queues in own namespace for hmpps-prisoner-to-nomis-update-dev (#17496)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
@@ -14,7 +14,7 @@ locals {
     "Digital-Prison-Services-dev-hmpps_prisoner_to_nomis_visit_dlq"        = "hmpps-domain-events-dev"
     "Digital-Prison-Services-dev-hmpps_prisoner_to_nomis_visit_queue"      = "hmpps-domain-events-dev"
   }
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
+  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value}
 }
 
 module "irsa" {
@@ -24,10 +24,20 @@ module "irsa" {
   namespace            = var.namespace
   service_account_name = "hmpps-prisoner-to-nomis-update"
   role_policy_arns     = merge({
-    hmpps_prisoner_to_nomis_adjudication_queue             = module.hmpps_prisoner_to_nomis_adjudication_queue.irsa_policy_arn,
-    hmpps_prisoner_to_nomis_adjudication_dead_letter_queue = module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_adjudication_queue               = module.hmpps_prisoner_to_nomis_adjudication_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_adjudication_dead_letter_queue   = module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.irsa_policy_arn,
     hmpps_prisoner_to_nomis_nonassociation_queue             = module.hmpps_prisoner_to_nomis_nonassociation_queue.irsa_policy_arn,
     hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue = module.hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_activity_queue                   = module.hmpps_prisoner_to_nomis_activity_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_activity_dead_letter_queue       = module.hmpps_prisoner_to_nomis_activity_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_appointment_queue                = module.hmpps_prisoner_to_nomis_appointment_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_appointment_dead_letter_queue    = module.hmpps_prisoner_to_nomis_appointment_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_incentive_queue                  = module.hmpps_prisoner_to_nomis_incentive_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_incentive_dead_letter_queue      = module.hmpps_prisoner_to_nomis_incentive_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_sentencing_queue                 = module.hmpps_prisoner_to_nomis_sentencing_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_sentencing_dead_letter_queue     = module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_visit_queue                      = module.hmpps_prisoner_to_nomis_visit_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_visit_queue                      = module.hmpps_prisoner_to_nomis_visit_queue.irsa_policy_arn,
   }, local.sqs_policies)
 
   # Tags
