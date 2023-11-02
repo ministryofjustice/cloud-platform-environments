d38687287382ddf3d4eadc3745b13b8a648a27a3
petergphillips <peter@greenthistle.co.uk>
2023-11-02 15:12:28 +0000

    SDIT-670: 🚚 Fix irsa permissions as hit limit (second time lucky) (#17534)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "combined_local_sqs" {
       module.hmpps_prisoner_to_nomis_sentencing_queue.sqs_arn,
       module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.sqs_arn,
       module.hmpps_prisoner_to_nomis_visit_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_visit_queue.sqs_arn
+      module.hmpps_prisoner_to_nomis_visit_dead_letter_queue.sqs_arn
     ]
   }
 }
