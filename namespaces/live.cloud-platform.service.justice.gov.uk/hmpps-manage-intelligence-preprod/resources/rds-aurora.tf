40499e2c50d3e4f3046697899ecc6e8014d4c933
brian.carr <brian.carr@digital.justice.gov.uk>
2023-11-06 13:27:28 +0000

    adding read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/rds-aurora.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-preprod/resources/rds-aurora.tf
@@ -13,7 +13,9 @@ module "rds_aurora" {
     min_capacity = 2
     max_capacity = 20
   }
-  replica_count                = 1
+  replica_count                = 2
+  replica_scale_enabled        = true
+  replica_scale_min            = 1
   performance_insights_enabled = true
   allow_major_version_upgrade  = true
 
