373380882c51f8a5b7d897e58fe359546c8a03fe
petergphillips <peter@greenthistle.co.uk>
2023-11-07 18:47:20 +0000

    SDIT-670: 🚚 Remove irsa policy for old domain events queue for hmpps-prisoner-to-nomis-update-prod (#17583)
    * SDIT-670: 🚚 Remove irsa policy for old domain events queue for hmpps-prisoner-to-nomis-update-prod
    
    * SDIT-670: 🚚 Fix incorrect irsa policy definition

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-prod/resources/irsa.tf
@@ -1,62 +1,30 @@
 # Add the names of the SQS which the app needs permissions to access.
 # The value of each item should be the namespace where the SQS was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
-locals {
-  sqs_queues = {
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_activity_dlq"     = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_activity_queue"   = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-prisoner_to_nomis_appointment_dlq"        = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-prisoner_to_nomis_appointment_queue"      = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_incentive_dlq"    = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_incentive_queue"  = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_sentencing_dlq"   = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_sentencing_queue" = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_visit_dlq"        = "hmpps-domain-events-prod"
-    "Digital-Prison-Services-prod-hmpps_prisoner_to_nomis_visit_queue"      = "hmpps-domain-events-prod"
-  }
-  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value}
-}
-
-data "aws_iam_policy_document" "combined_local_sqs" {
-  version = "2012-10-17"
-  statement {
-    sid       = "hmppsPrisonerToNomisUpdateSqs"
-    effect    = "Allow"
-    actions   = ["sqs:*"]
-    resources = [
-      module.hmpps_prisoner_to_nomis_adjudication_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_nonassociation_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_activity_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_activity_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_appointment_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_appointment_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_incentive_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_incentive_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_sentencing_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_visit_queue.sqs_arn,
-      module.hmpps_prisoner_to_nomis_visit_dead_letter_queue.sqs_arn
-    ]
-  }
-}
-
-resource "aws_iam_policy" "combined_local_sqs" {
-  policy = data.aws_iam_policy_document.combined_local_sqs.json
-  tags   = local.default_tags
-}
-
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-prisoner-to-nomis-update"
-  role_policy_arns     = merge(
-    local.sqs_policies,
-    { combined_local_sqs = aws_iam_policy.combined_local_sqs.arn }
-  )
+  # we've almost hit the limit for policies here (limit is 15).  If wanting to add any more then switch to the
+  # approach used in hmpps-prisoner-from-nomis-migration-prod/resources/irsa.tf instead.
+  role_policy_arns     = {
+    hmpps_prisoner_to_nomis_adjudication_queue               = module.hmpps_prisoner_to_nomis_adjudication_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_adjudication_dead_letter_queue   = module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_nonassociation_queue             = module.hmpps_prisoner_to_nomis_nonassociation_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue = module.hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_activity_queue                   = module.hmpps_prisoner_to_nomis_activity_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_activity_dead_letter_queue       = module.hmpps_prisoner_to_nomis_activity_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_appointment_queue                = module.hmpps_prisoner_to_nomis_appointment_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_appointment_dead_letter_queue    = module.hmpps_prisoner_to_nomis_appointment_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_incentive_queue                  = module.hmpps_prisoner_to_nomis_incentive_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_incentive_dead_letter_queue      = module.hmpps_prisoner_to_nomis_incentive_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_sentencing_queue                 = module.hmpps_prisoner_to_nomis_sentencing_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_sentencing_dead_letter_queue     = module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_visit_queue                      = module.hmpps_prisoner_to_nomis_visit_queue.irsa_policy_arn,
+    hmpps_prisoner_to_nomis_visit_dead_letter_queue          = module.hmpps_prisoner_to_nomis_visit_dead_letter_queue.irsa_policy_arn
+  }
 
   # Tags
   business_unit          = var.business_unit
@@ -65,21 +33,4 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-data "aws_ssm_parameter" "irsa_policy_arns" {
-  for_each = local.sqs_queues
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
-}
-
-locals {
-  default_tags = {
-    namespace              = var.namespace
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    environment-name       = var.environment
-    owner                  = var.team_name
-    infrastructure-support = var.infrastructure_support
-  }
 }
