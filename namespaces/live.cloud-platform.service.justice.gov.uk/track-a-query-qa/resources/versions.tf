60f3b69771db0b554371aa92bf5376dd0285ff3b
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-11-07 15:04:58 +0000

    remove k8s

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-qa/resources/versions.tf
@@ -10,9 +10,5 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.39.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
   }
 }
