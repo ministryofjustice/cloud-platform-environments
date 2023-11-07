5fb527e14de2ed6d8d2ecbafda9397570ed051f1
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-11-07 14:24:23 +0000

    CPR-60-Dev
    . Removed SQS queue's and topic from local.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/irsa.tf
@@ -1,13 +1,9 @@
 locals {
   # *** Placeholder for incoming SQS queues ***
   sqs_queues = {
-    "Digital-Prison-Services-dev-cpr_court_case_events_queue"    = "cpr-court-case-events-queue-dev"
-    "Digital-Prison-Services-dev-cpr_court_case_events_dead_letter_queue"    = "cpr-court-case-events-dead-letter-queue-dev"
-
   }
   # *** Placeholder for incoming SNS topics ***
   sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
