b4f82bac65ea140238e00adedf71059d7b4f310a
Mike Bell <mike.bell1@digital.justice.gov.uk>
2023-11-08 08:48:03 +0000

    Use correct github repo name in ecr

--- namespaces/live.cloud-platform.service.justice.gov.uk/mikebell-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mikebell-test/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["ministryofjustice"]
+  github_repositories = ["mikebell-test"]
 
   # Tags
   business_unit          = var.business_unit
