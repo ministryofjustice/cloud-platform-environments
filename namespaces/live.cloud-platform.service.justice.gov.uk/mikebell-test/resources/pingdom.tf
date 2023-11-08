5c2814d490f910d14469c15188b2fc2ef65423d9
Mike Bell <mike.bell1@digital.justice.gov.uk>
2023-11-08 14:31:50 +0000

    Add slack channel to pingdom alert

--- namespaces/live.cloud-platform.service.justice.gov.uk/mikebell-test/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mikebell-test/resources/pingdom.tf
@@ -6,7 +6,7 @@ resource "pingdom_check" "mikebell-test-check" {
   type                     = "http"
   name                     = "mikebell-test"
   host                     = "mikebell-test.apps.live.cloud-platform.service.justice.gov.uk"
-  resolution               = 5
+  resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
@@ -15,5 +15,6 @@ resource "pingdom_check" "mikebell-test-check" {
   port                     = 443
   tags                     = "businessunit_platforms,application_mikebell_test,component_healthcheck,isproduction_false,environment_development,infrastructuresupport_platforms"
   probefilters             = "region:EU"
+  integrationids           = ["C015JL16L8Z"]
 }
 
