9058e1e4b2a858ad22ad19e2894589f08e770a78
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-11-10 10:56:58 +0000

    Remove GHA

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-staging/resources/serviceaccount.tf
@@ -73,34 +73,3 @@ module "circleci-sa" {
   kubernetes_cluster   = var.kubernetes_cluster
   serviceaccount_rules = local.sa_rules
 }
-
-# Service account used by github actions
-module "service_account" {
-  source                               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
-  namespace                            = var.namespace
-  kubernetes_cluster                   = var.kubernetes_cluster
-  serviceaccount_name                  = "hmpps-launchpad"
-  github_environments                  = [var.environment]
-  github_repositories                  = local.github_repos
-  github_actions_secret_kube_cert      = "${upper(var.environment)}_KUBE_CERT"
-  github_actions_secret_kube_token     = "${upper(var.environment)}_KUBE_TOKEN"
-  github_actions_secret_kube_cluster   = "${upper(var.environment)}_KUBE_CLUSTER"
-  github_actions_secret_kube_namespace = "${upper(var.environment)}_KUBE_NAMESPACE"
-  serviceaccount_rules                 = local.sa_rules
-  serviceaccount_token_rotated_date    = time_rotating.weekly.unix
-  depends_on                           = [github_repository_environment.env]
-}
-
-resource "time_rotating" "weekly" {
-  rotation_days = 7
-}
-
-resource "github_repository_environment" "env" {
-  for_each    = toset(local.github_repos)
-  environment = var.environment
-  repository  = each.key
-  deployment_branch_policy {
-    protected_branches     = true
-    custom_branch_policies = false
-  }
-}
