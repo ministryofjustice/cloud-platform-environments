194502dcedca77c09ac9b451090e392c28f5012b
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2023-11-14 16:16:37 +0000

    MAP-16: Add change-someones-cell-dev namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-change-someones-cell-dev/resources/variables.tf
@@ -0,0 +1,68 @@
+/*
+ * When using this module through the cloud-platform-environments, the following
+ * variable is automatically supplied by the pipeline.
+ *
+ */
+variable "vpc_name" {
+}
+
+variable "application" {
+  default = "hmpps-change-someones-cell-dev"
+}
+
+variable "namespace" {
+  default = "hmpps-change-someones-cell-dev"
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "Move a Prisoner"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "moveaprisoner@digital.justice.gov.uk"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "node-type" {
+  default = "cache.t2.small"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "move-a-prisoner-digital"
+}
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
