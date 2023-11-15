7cd17616caf77661305e0a08054707c72caac2c2
psweeting <patrick.sweeting@digital.justice.gov.uk>
2023-11-15 13:07:25 +0000

    LGA-2766 changing db_instance_class to db.t4g.small

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-legal-adviser-api-staging/resources/rds.tf
@@ -35,7 +35,7 @@ module "laa_laa_rds_postgres_14" {
   infrastructure_support = var.email
   db_engine              = "postgres"
   db_engine_version      = "14"
-  db_instance_class      = "db.t2.small"
+  db_instance_class      = "db.t4g.small"
   db_allocated_storage   = "5"
   db_name                = "laalaa"
   db_parameter           = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
