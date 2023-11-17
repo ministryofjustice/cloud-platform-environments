11090b525b9f96ecfbce7ec11aef946580fb914c
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-11-17 10:25:55 +0000

    Add github team tags (#17784)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/main.tf
@@ -5,16 +5,34 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "check-client-qualifies"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "check-client-qualifies"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      GithubTeam = "check-client-qualifies"
+    }
+  }
 }
 
 provider "github" {
