5578adfd3041130cf20ab5c3cbc83518471f648d
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-11-17 15:38:14 +0000

    Claim-criminal-injuries-stag add secrets manager

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-stag/resources/secrets.tf
@@ -0,0 +1,29 @@
+module "secrets_manager_multiple_secrets" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.0"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "cica-web-aws-secrets" = {
+      description             = "Cica web secrets STAGING",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "cica-web-secrets-aws"
+    },
+    "data-capture-service-aws-secrets" = {
+      description             = "Data capture service secrets STAGING",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "data-capture-service-secrets-aws"
+    },
+    "shared-aws-secrets" = {
+      description             = "Shared secrets STAGING",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "shared-secrets-aws"
+    },
+  }
+}
