f5ec2b2830cf272aaededc79a58cd4e62ed37ddb
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-11-20 21:42:52 +0000

    :wrench: Add a github action scaleset
    This commit connects to https://github.com/ministryofjustice/operations-engineering/issues/3759 and relates to the requirement to self-host GitHub actions local runners on the Cloud Platform.

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-runners-poc/resources/github-actions-controller.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-runners-poc/resources/github-actions-controller.tf
@@ -9,3 +9,21 @@ resource "helm_release" "actions_scale_set_controller" {
     value = "0.7.0"
   }
 }
+
+
+resource "helm_release" "operations_engineering_runners" {
+  name       = "operations-engineering-private-runners"
+  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
+  chart      = "gha-runner-scale-set"
+  namespace  = "operations-engineering-github-runners-poc"
+
+  set {
+    name  = "githubConfigUrl"
+    value = "https://github.com/ministryofjustice"
+  }
+
+  set {
+    name  = "githubConfigSecret"
+    value = "poc-classic-token"
+  }
+}
