194bf029c61ef12f3883164735a171fd6e923629
adam <brown-a2@users.noreply.github.com>
2023-11-21 20:55:42 +0000

    Add missing commas

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-staging/resources/irsa.tf
@@ -42,7 +42,7 @@ module "irsa" {
         "arn:aws:s3:::sifocc-prod-storage-7f6qtyoj7wir", #tacticalproducts legacy account
         "arn:aws:s3:::npm-prod-storage-19n0nag2nk8xk", #tacticalproducts legacy account
         "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd", #tacticalproducts legacy account
-        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw" #tacticalproducts legacy account
+        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw", #tacticalproducts legacy account
         "arn:aws:s3:::imbmembers-prod-storage-k98pxkemaqp0" #tacticalproducts legacy account
       ]
     }
@@ -64,7 +64,7 @@ module "irsa" {
         "arn:aws:s3:::sifocc-prod-storage-7f6qtyoj7wir/*", #tacticalproducts legacy account
         "arn:aws:s3:::npm-prod-storage-19n0nag2nk8xk/*", #tacticalproducts legacy account
         "arn:aws:s3:::layobservers-prod-storage-nu2yj19yczbd/*", #tacticalproducts legacy account
-        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw/*" #tacticalproducts legacy account
+        "arn:aws:s3:::ppo-prod-storage-1g9rkhjhkjmgw/*", #tacticalproducts legacy account
         "arn:aws:s3:::imbmembers-prod-storage-k98pxkemaqp0/*" #tacticalproducts legacy account
      ]
     }
