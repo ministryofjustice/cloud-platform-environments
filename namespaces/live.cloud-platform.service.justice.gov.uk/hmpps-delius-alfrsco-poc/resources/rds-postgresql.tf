d52a5370df18dcdd383dd3ad038e2b7793d3c9bf
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2023-11-21 10:43:34 +0000

    NIT-946 upgrade postgres version (14.10) for alfresco in CP

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds-postgresql.tf
@@ -24,7 +24,7 @@ module "rds_alfresco" {
   # PostgreSQL specifics
   db_engine         = "postgres"
   prepare_for_major_upgrade = false
-  db_engine_version = "14.9"
+  db_engine_version = "14.10"
   rds_family        = "postgres14"
   db_instance_class = "db.t3.micro"
 
@@ -63,7 +63,7 @@ module "read_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "14.9"
+  db_engine_version = "14.10"
   rds_family        = "postgres14"
   db_instance_class = "db.t3.micro"
   # It is mandatory to set the below values to create read replica instance
