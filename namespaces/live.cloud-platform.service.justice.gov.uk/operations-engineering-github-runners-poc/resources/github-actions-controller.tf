c6c79b0918d393ce6b38a6b4849ad7eef8915404
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-11-22 10:00:05 +0000

    :wrench: Add forked image to runnersset as this uses non-root

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-runners-poc/resources/github-actions-controller.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-runners-poc/resources/github-actions-controller.tf
@@ -9,3 +9,25 @@ resource "helm_release" "actions_scale_set_controller" {
     value = "0.7.0"
   }
 }
+
+resource "helm_release" "operations_engineering_runners" {
+  name       = "operations-engineering-private-runners"
+  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
+  chart      = "gha-runner-scale-set"
+  namespace  = "operations-engineering-github-runners-poc"
+
+  set {
+    name  = "githubConfigUrl"
+    value = "https://github.com/ministryofjustice"
+  }
+
+  set {
+    name  = "githubConfigSecret"
+    value = "poc-classic-token"
+  }
+
+  set {
+    name  = "template.containers.image[0]"
+    value = "json0/actions-runner:latest"
+  }
+}
