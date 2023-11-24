bfa1d503254d1f5f8da67f0da059b71031ba0f11
rajindersinghsandhu <102977930+rajindersinghsandhu@users.noreply.github.com>
2023-11-24 13:23:40 +0000

    Upadted team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-provider-details-api-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-provider-details-api-uat/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "laa-cwa-data-api"
+  default     = "laa-cwa-data-api-admin-team"
 }
 
 variable "environment" {
