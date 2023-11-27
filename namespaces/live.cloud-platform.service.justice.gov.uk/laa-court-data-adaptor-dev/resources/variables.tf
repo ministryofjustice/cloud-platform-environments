144625ebeb7dbb50a290468ad93a3fa193b8898e
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-11-27 15:31:40 +0000

    [LASB-2427] Add secrets to SM to access Maat API from CDA

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-adaptor-dev/resources/variables.tf
@@ -18,7 +18,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "crime-apps"
+  default     = "laa-crime-apps-team"
 }
 
 variable "environment_name" {
@@ -72,5 +72,11 @@ variable "github_token" {
 
 variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
+variable "slack_channel" {
+  description = "Slack channel name for your team, if we need to contact you about this service"
+  type        = string
+  default     = "laa-crimeapps-core"
 }
 
