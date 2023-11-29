4fe52a7cad5ce3224e81cba262a5559884a82550
brian.carr <brian.carr@digital.justice.gov.uk>
2023-11-29 13:13:19 +0000

    deleted module in error

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/s3.tf
@@ -15,6 +15,23 @@ module "manage_intelligence_storage_bucket" {
   }
 }
 
+module "manage_intelligence_rds_to_s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+  team_name              = var.team_name
+  acl                    = "private"
+  versioning             = false
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 module "manage_intelligence_csv_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
   team_name              = var.team_name
