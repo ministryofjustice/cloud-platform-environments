0429b9432ed4d50b9af89c21328d275e5bc01841
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-12-05 11:53:41 +0000

    This was causing an issue in the apply pipeline but we don't need to reference the name again in this file anyway, so just going to not define the variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/circleci-service-account.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-saas-test/resources/circleci-service-account.tf
@@ -1,13 +1,9 @@
-locals {
-  circleci_sa_name = "circleci-terraform-formbuilder-saas-test"
-}
-
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
-  serviceaccount_name = locals.circleci_sa_name
+  serviceaccount_name = "circleci-terraform-formbuilder-saas-test"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
