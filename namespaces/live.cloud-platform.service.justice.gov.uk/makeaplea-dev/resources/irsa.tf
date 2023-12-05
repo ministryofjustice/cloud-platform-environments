0ae054fa64eed47345aa808a2464f7aadaef939c
itoulou <touloumbadjiani@gmail.com>
2023-12-05 15:02:30 +0000

    add locals sqs policy to irsa modulei

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/irsa.tf
@@ -1,3 +1,13 @@
+# Add the names of the SQS & SNS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "pet-development-makeaplea_queue" = "makeaplea-dev"
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+}
+
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
@@ -11,11 +21,12 @@ module "irsa" {
   # Attach the approprate policies using a key => value map
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = {
-    s3  = module.s3_bucket.irsa_policy_arn
-    sqs_map_queue = module.makeaplea_queue.irsa_policy_arn
-    sqs_map_queue_dead_letter = module.makeaplea_dead_letter_queue.irsa_policy_arn
-  }
+  role_policy_arns = merge(
+    local.sqs_policies,
+    { s3  = module.s3_bucket.irsa_policy_arn },
+    { sqs_map_queue = module.makeaplea_queue.irsa_policy_arn },
+    { sqs_map_queue_dead_letter = module.makeaplea_dead_letter_queue.irsa_policy_arn },
+  )
 
   # Tags
   business_unit          = var.business_unit
@@ -24,4 +35,9 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
