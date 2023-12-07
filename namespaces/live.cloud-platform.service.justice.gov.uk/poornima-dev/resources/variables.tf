a37c5f4c11c1343940daa57d98ba3094458f0330
Prakash Subramani <prakash.subramani@digital.justice.gov.uk>
2023-12-07 13:02:08 +0000

    TFSec tweak

--- namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/poornima-dev/resources/variables.tf
@@ -67,10 +67,3 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
-
-variable "random_string" {
-  type        = string
-  description = "Required by the GitHub Terraform provider"
-  default     = "1234"
-}
-
