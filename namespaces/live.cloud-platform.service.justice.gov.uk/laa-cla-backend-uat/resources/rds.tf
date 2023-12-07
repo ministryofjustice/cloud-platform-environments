dcdb674ac64394e61fed8305bbf516530d7ee00b
said-moj <45761276+said-moj@users.noreply.github.com>
2023-12-07 13:45:46 +0000

    Remove instances that was created from a snapshot, restoring from snapshot is only intended to be done on the same instance

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-uat/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-uat/resources/rds.tf
@@ -49,49 +49,6 @@ module "cla_backend_rds_postgres_14" {
   }
 }
 
-module "cla_backend_eligibility_integration_rds_postgres_14" {
-  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.0"
-  vpc_name      = var.vpc_name
-  team_name     = var.team_name
-  business_unit = var.business_unit
-  application   = var.application
-  is_production = var.is_production
-  namespace     = var.namespace
-
-  db_name = "cla_backend"
-  # change the postgres version as you see fit.
-  db_engine_version      = "14"
-  db_instance_class      = "db.t4g.small"
-  environment_name       = var.environment-name
-  infrastructure_support = var.infrastructure_support
-
-  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
-  # Pick the one that defines the postgres version the best
-  rds_family = "postgres14"
-
-  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
-  # You will need to specify "pending-reboot" here, as default is set to "immediate".
-
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "false"
-
-  snapshot_identifier = "eligibility-int-uat"
-
-  db_parameter = [
-    {
-      name         = "rds.force_ssl"
-      value        = "1"
-      apply_method = "pending-reboot"
-    }
-  ]
-
-  providers = {
-    # Can be either "aws.london" or "aws.ireland"
-    aws = aws.london
-  }
-}
-
 resource "kubernetes_secret" "cla_backend_rds_postgres_14" {
   metadata {
     name      = "database-14"
@@ -159,20 +116,3 @@ resource "kubernetes_secret" "cla_backend_metabase_rds" {
   }
 }
 
-resource "kubernetes_secret" "cla_backend_eligibility_integration_rds_postgres_14" {
-  metadata {
-    name      = "eligibility-integration-database-14"
-    namespace = var.namespace
-  }
-
-  data = {
-    endpoint          = module.cla_backend_eligibility_integration_rds_postgres_14.rds_instance_endpoint
-    host              = module.cla_backend_eligibility_integration_rds_postgres_14.rds_instance_address
-    port              = module.cla_backend_eligibility_integration_rds_postgres_14.rds_instance_port
-    name              = module.cla_backend_eligibility_integration_rds_postgres_14.database_name
-    user              = module.cla_backend_eligibility_integration_rds_postgres_14.database_username
-    password          = module.cla_backend_eligibility_integration_rds_postgres_14.database_password
-    db_identifier     = module.cla_backend_eligibility_integration_rds_postgres_14.db_identifier
-  }
-
-}
