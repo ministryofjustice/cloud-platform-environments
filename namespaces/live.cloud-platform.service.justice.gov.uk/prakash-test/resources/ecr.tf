f0365c0ec658b63aa6fc956d2c76f425a9975a16
Prakash Subramani <prakash.subramani@digital.justice.gov.uk>
2023-12-07 16:52:50 +0000

    Use shared static-analysis

--- namespaces/live.cloud-platform.service.justice.gov.uk/prakash-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prakash-test/resources/ecr.tf
@@ -71,7 +71,7 @@ module "ecr" {
   # This flag will allow a non-empty ECR to be deleted.
   # Defaults to true
 
-  deletion_protection = false
+  # deletion_protection = false
 
   # Tags (commented out until release)
   business_unit          = var.business_unit
