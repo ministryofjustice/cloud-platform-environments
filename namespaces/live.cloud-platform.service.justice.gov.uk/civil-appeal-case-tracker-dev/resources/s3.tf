5d9a20b87f261075c97ece86f091eece7e4e0436
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-12-08 13:23:37 +0000

    fix formatting

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/s3.tf
@@ -6,16 +6,16 @@
  */
 module "s3_bucket" {
 
-  source                        = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
-  team_name                     = var.team_name
-  business_unit                 = var.business_unit
-  application                   = var.application
-  is_production                 = var.is_production
-  environment_name              = var.environment
-  infrastructure_support        = var.infrastructure_support
-  namespace                     = var.namespace
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
 
-    bucket_policy = <<EOF
+  bucket_policy = <<EOF
     {
       "Version": "2012-10-17",
       "Statement": [
