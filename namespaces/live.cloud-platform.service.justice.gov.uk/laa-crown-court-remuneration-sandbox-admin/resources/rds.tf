0f9592d0e790fd0a1e50ffb2cf994ccb4950ded6
RahulMehta <rahul.mehta@digital.justice.gov.uk>
2023-12-11 16:03:57 +0000

    change url which handles the Oracle in list of db with url change env name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-sandbox-admin/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-sandbox-admin/resources/rds.tf
@@ -3,7 +3,7 @@ module "rds-instance" {
   vpc_name = var.vpc_name
 
   application            = var.application
-  environment_name       = var.environment-name
+  environment_name       = var.environment
   is_production          = var.is_production
   namespace              = var.namespace
   infrastructure_support = var.infrastructure_support
