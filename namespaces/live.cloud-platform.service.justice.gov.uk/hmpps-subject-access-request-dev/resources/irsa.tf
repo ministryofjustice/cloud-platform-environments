f11adc7b1333854b901da52742c014342451acdd
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2023-12-13 12:49:30 +0000

    apply to correct irsa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/irsa.tf
@@ -24,10 +24,6 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-
-   providers = {
-    aws = aws.london_without_default_tags
-  }
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
