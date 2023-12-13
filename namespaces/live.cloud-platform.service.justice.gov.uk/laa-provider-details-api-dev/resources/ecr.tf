545ad4fb8b612ff9482d77c533e293b34baabba1
rajindersinghsandhu <102977930+rajindersinghsandhu@users.noreply.github.com>
2023-12-13 06:44:11 +0000

    Added files for new namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-provider-details-api-dev/resources/ecr.tf
@@ -0,0 +1,77 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
+
+  # Repository configuration
+  team_name = var.team_name
+  repo_name = "${var.namespace}-ecr"
+  namespace = var.namespace
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github"]
+  github_repositories = ["laa-cwa-data-api"]
+  github_actions_prefix = "dev"
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  github_environments = ["dev"]
+
+  # Lifecycle policies
+  # Uncomment the below to automatically tidy up old Docker images
+
+  lifecycle_policy = <<EOF
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Expire untagged images older than 14 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 14
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 2,
+          "description": "Keep last 30 dev and staging images",
+          "selection": {
+            "tagStatus": "tagged",
+            "tagPrefixList": ["dev", "staging"],
+            "countType": "imageCountMoreThan",
+            "countNumber": 30
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 3,
+          "description": "Keep the newest 100 images and mark the rest for expiration",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 100
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+    EOF
+    
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-provider-details-api-dev/resources/secret.tf
@@ -0,0 +1,19 @@
+module "secrets_manager" {
+  source                  = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.0"
+  team_name               = var.team_name
+  application             = var.application
+  business_unit           = var.business_unit
+  is_production           = var.is_production
+  namespace               = var.namespace
+  environment_name        = var.environment
+  infrastructure_support  = var.infrastructure_support
+  eks_cluster_name        = var.eks_cluster_name
+
+  secrets = {
+    "laa-provider-details-api-secrets-dev" = {
+      description             = "Db urls and credentials for laa-provider-details-api dev environment", # Required
+      recovery_window_in_days = 7, # Required
+      k8s_secret_name         = "laa-provider-details-api-secrets"                    # The name of the secret in k8s
+    },
+  }
+}
