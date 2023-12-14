d9957cd6046f907f1d818fb571469c233828afe7
LavMatt <mattlaverty@gmail.com>
2023-12-14 08:41:53 +0000

    add redis elasticache resource

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-firebreak-daap-api/resources/elasticache.tf
@@ -0,0 +1,31 @@
+module "elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=7.0.0"
+  vpc_name               = var.vpc_name
+  application            = var.application
+  environment_name       = var.environment
+  is_production          = var.is_production
+  infrastructure_support = var.infrastructure_support
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  node_type              = "cache.t4g.micro"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
+  namespace              = var.namespace
+
+  auth_token_rotated_date = "2024-01-14"
+
+}
+
+resource "kubernetes_secret" "elasticache_redis_secrets" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.elasticache_redis.primary_endpoint_address
+    auth_token               = module.elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.elasticache_redis.member_clusters)
+    replication_group_id     = module.elasticache_redis.replication_group_id
+  }
+}
