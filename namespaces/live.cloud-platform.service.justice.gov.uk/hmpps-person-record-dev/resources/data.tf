ea006a3e71850d1f102c52a043867076fa640575
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-12-14 15:47:05 +0000

    CPR-63-dev
    . Added subscription queue for delius offender domain events.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/data.tf
@@ -1,3 +1,7 @@
 data "aws_ssm_parameter" "court-case-events-topic-arn" {
   name = "/court-probation-dev/topic-arn"
 }
+
+data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
+  name = "/hmpps-domain-events-dev/topic-arn"
+}
