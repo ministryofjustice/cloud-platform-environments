7e4e705344e5f2b3d8deb826f672c82c276c4d4a
Jesus Laiz <zheileman@users.noreply.github.com>
2023-12-18 13:27:27 +0000

    Add additional rules to SA (#18354)
    In particular we need horizontalpodautoscalers.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-production/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-complaints-formbuilder-adapter-production/resources/serviceaccount.tf
@@ -7,4 +7,72 @@ module "serviceaccount" {
   serviceaccount_name = "circleci-hmcts-complaints-formbuilder-adapter"
 
   serviceaccount_token_rotated_date = "18-12-2023"
+
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "configmaps",
+        "pods"
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "update",
+        "delete",
+        "list",
+        "watch"
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "monitoring.coreos.com",
+        "networking.k8s.io",
+        "batch"
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "cronjobs",
+        "jobs",
+        "replicasets",
+        "statefulsets",
+        "poddisruptionbudgets",
+        "networkpolicies",
+        "servicemonitors"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch"
+      ]
+    },
+    {
+      api_groups = [
+        "autoscaling"
+      ]
+      resources = [
+        "hpa",
+        "horizontalpodautoscalers"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch"
+      ]
+    }
+  ]
 }
