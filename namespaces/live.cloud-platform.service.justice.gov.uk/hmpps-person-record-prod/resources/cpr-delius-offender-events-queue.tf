7cb950c241aa08234872b86af830f67f4ec3fef2
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-12-19 08:49:27 +0000

    CPR-63-prod
    . Added delius domain event subscription
    . Added domain event topic secret.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/cpr-delius-offender-events-queue.tf
@@ -0,0 +1,125 @@
+######################################## Delius offender events subscription
+
+resource "aws_sns_topic_subscription" "cpr_delius_offender_events_subscription" {
+  provider  = aws.london
+  topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  protocol  = "sqs"
+  endpoint  = module.cpr_delius_offender_events_queue.sqs_arn
+  filter_policy = jsonencode({
+    eventType = [
+      "probation-case.engagement.created",
+      "probation-case.merge.completed",
+      "probation-case.unmerge.completed"
+    ]
+  })
+}
+
+module "cpr_delius_offender_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name                   = "cpr_delius_offender_events_queue"
+  encrypt_sqs_kms            = "true"
+  message_retention_seconds  = 1209600
+  visibility_timeout_seconds = 120
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.cpr_delius_offender_events_dead_letter_queue.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "cpr_delius_offender_events_queue_policy" {
+  queue_url = module.cpr_delius_offender_events_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.cpr_delius_offender_events_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.cpr_delius_offender_events_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+              {
+                "aws:SourceArn": "${data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value}"
+              }
+            }
+        }
+      ]
+  }
+EOF
+}
+
+######## Dead letter queue
+
+module "cpr_delius_offender_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name        = "cpr_delius_offender_events_dlq"
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+########  Secrets
+
+resource "kubernetes_secret" "cpr_delius_offender_events_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "sqs-cpr-delius-offender-events-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.cpr_delius_offender_events_queue.sqs_id
+    sqs_queue_arn  = module.cpr_delius_offender_events_queue.sqs_arn
+    sqs_queue_name = module.cpr_delius_offender_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "cpr_delius_offender_events_dead_letter_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "sqs-cpr-delius-offender-events-dlq-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.cpr_delius_offender_events_dead_letter_queue.sqs_id
+    sqs_queue_arn  = module.cpr_delius_offender_events_dead_letter_queue.sqs_arn
+    sqs_queue_name = module.cpr_delius_offender_events_dead_letter_queue.sqs_name
+  }
+}
