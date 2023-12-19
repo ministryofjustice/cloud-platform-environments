2e5d6a652191aa4c72d53e4123ba01e161bccdb0
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-12-19 11:04:50 +0000

    PI-1736 remove dlqs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/person-search-index-from-delius-person-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/person-search-index-from-delius-person-queue.tf
@@ -21,11 +21,6 @@ module "person-search-index-from-delius-person-queue" {
   # Queue configuration
   sqs_name = "person-search-index-from-delius-person-queue"
 
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = module.person-search-index-from-delius-person-dlq.sqs_arn
-    maxReceiveCount     = 3
-  })
-
   # Tags
   business_unit          = var.business_unit
   application            = "person-search-index-from-delius"
@@ -41,28 +36,6 @@ resource "aws_sqs_queue_policy" "person-search-index-from-delius-person-queue-po
   policy    = data.aws_iam_policy_document.sqs_queue_policy_document.json
 }
 
-module "person-search-index-from-delius-person-dlq" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
-
-  # Queue configuration
-  sqs_name                  = "person-search-index-from-delius-person-dlq"
-  message_retention_seconds = 86400 # 1 day
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = "person-search-index-from-delius"
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
-  environment_name       = var.environment_name
-  infrastructure_support = var.infrastructure_support
-}
-
-resource "aws_sqs_queue_policy" "person-search-index-from-delius-person-dlq-policy" {
-  queue_url = module.person-search-index-from-delius-person-dlq.sqs_id
-  policy    = data.aws_iam_policy_document.sqs_queue_policy_document.json
-}
-
 resource "kubernetes_secret" "person-search-index-from-delius-person-queue-secret" {
   metadata {
     name      = "person-search-index-from-delius-person-queue"
@@ -70,15 +43,5 @@ resource "kubernetes_secret" "person-search-index-from-delius-person-queue-secre
   }
   data = {
     QUEUE_NAME = module.person-search-index-from-delius-person-queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "person-search-index-from-delius-person-dlq-secret" {
-  metadata {
-    name      = "person-search-index-from-delius-person-dlq"
-    namespace = var.namespace
-  }
-  data = {
-    QUEUE_NAME = module.person-search-index-from-delius-person-dlq.sqs_name
   }
 }
