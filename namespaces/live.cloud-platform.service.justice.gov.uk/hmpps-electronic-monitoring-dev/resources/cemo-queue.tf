c5e278d00184ff19545e1d30460204c8c38b5b81
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2023-12-19 11:09:55 +0000

    Create SQS .tf file for electronic monitoring CEMO. Rename CEMO RDS & S3.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-dev/resources/cemo-queue.tf
@@ -0,0 +1,42 @@
+module "cemo_submit_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name        = "cemo_submit_queue"
+  encrypt_sqs_kms = "true"
+  message_retention_seconds  = 1209600
+  visibility_timeout_seconds = 120
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.cemo_submit_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+
+  EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+module "cemo_submit_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name        = "cemo_submit_dl_queue"
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
