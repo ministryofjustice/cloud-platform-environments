6fe8a02cf42d465d1a7f13c141f5561791f25791
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-12-19 08:52:51 +0000

    CPR-63-preprod
    . corrected variable name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-delius-offender-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-delius-offender-events-queue.tf
@@ -34,7 +34,7 @@ module "cpr_delius_offender_events_queue" {
   is_production          = var.is_production
   team_name              = var.team_name # also used for naming the queue
   namespace              = var.namespace
-  environment_name       = var.environment-name
+  environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
   providers = {
@@ -84,7 +84,7 @@ module "cpr_delius_offender_events_dead_letter_queue" {
   is_production          = var.is_production
   team_name              = var.team_name # also used for naming the queue
   namespace              = var.namespace
-  environment_name       = var.environment-name
+  environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
   providers = {
