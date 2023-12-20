e63f6a46a895f464fa3a4c682f384556f79538a2
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-12-20 14:59:12 +0000

    Add secrets for certs in infox tesT

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-test/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-test/resources/secrets.tf
@@ -15,5 +15,10 @@ module "secrets_manager" {
       recovery_window_in_days = 7, # Required - number of days that AWS Secrets Manager waits before it can delete the secret
       k8s_secret_name        = "laa-infox-db-password-test" # The name of the secret in k8s
     },
+    "laa-infox-mtls-certs" = {
+      description             = "InfoX MTLS Certificates (base64-encoded). These certificates are used to secure the Libra endpoint.", # Required
+      recovery_window_in_days = 7, # Required - number of days that AWS Secrets Manager waits before it can delete the secret
+      k8s_secret_name        = "app-libra-mtls-certs" # The name of the secret in k8s
+    },
   }
 }
