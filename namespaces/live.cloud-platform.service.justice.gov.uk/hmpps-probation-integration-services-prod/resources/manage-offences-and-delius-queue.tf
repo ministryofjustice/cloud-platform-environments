d0016d701b00e4feef806f2d6c137e38ab53b825
Marcus Aspin <maspin@unilink.com>
2023-12-29 14:52:24 +0000

    PI-1759 Extend DLQ retention period to 1 week (prod) (#18459)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-prod/resources/manage-offences-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-prod/resources/manage-offences-and-delius-queue.tf
@@ -12,20 +12,19 @@ module "manage-offences-and-delius-queue" {
 
   # Queue configuration
   sqs_name = "manage-offences-and-delius-queue"
-
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.manage-offences-and-delius-dlq.sqs_arn
     maxReceiveCount     = 3
   })
 
   # Tags
-  business_unit          = var.business_unit
   application            = "manage-offences-and-delius"
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
+  business_unit          = var.business_unit
   environment_name       = var.environment_name
   infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name # also used as queue name prefix
 }
 
 resource "aws_sqs_queue_policy" "manage-offences-and-delius-queue-policy" {
@@ -37,16 +36,17 @@ module "manage-offences-and-delius-dlq" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
 
   # Queue configuration
-  sqs_name = "manage-offences-and-delius-dlq"
+  sqs_name                  = "manage-offences-and-delius-dlq"
+  message_retention_seconds = 7 * 24 * 3600 # 1 week
 
   # Tags
-  business_unit          = var.business_unit
   application            = "manage-offences-and-delius"
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
+  business_unit          = var.business_unit
   environment_name       = var.environment_name
   infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name # also used as queue name prefix
 }
 
 resource "aws_sqs_queue_policy" "manage-offences-and-delius-dlq-policy" {
