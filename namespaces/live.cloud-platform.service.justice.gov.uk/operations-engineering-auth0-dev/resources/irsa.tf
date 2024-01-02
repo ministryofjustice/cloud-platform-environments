5ab198fe71b74858a621374d4b0aa7c86b595294
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-02 15:58:47 +0000

    :sparkles: Add new namespace with s3 and irsa permissions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-auth0-dev/resources/irsa.tf
@@ -0,0 +1,25 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "operations-engineering-auth0-dev"
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the approprate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = {
+    s3 = module.s3.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
