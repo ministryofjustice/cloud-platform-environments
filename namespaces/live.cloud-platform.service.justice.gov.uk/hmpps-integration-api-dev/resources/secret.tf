6c156ec6b9cb0b76cf8b92390d7be1f53efe74c3
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-01-03 13:42:39 +0000

    add secret manager module to store slack webhook url

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/secret.tf
@@ -0,0 +1,27 @@
+module "secret" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=version" # use the latest release
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # Secrets configuration
+  secrets = {
+    "integration-api-slack-webhook-url" = {
+      description             = "Slack webhook url for Integration API gateway api alerts",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "slack-webhook-url"
+    },
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+   providers = {
+    aws = aws.london_without_default_tags
+  }
+}
