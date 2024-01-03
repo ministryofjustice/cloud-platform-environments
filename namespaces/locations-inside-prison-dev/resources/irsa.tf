93457728f96d0220d64120f2ca25ac3445f7242b
mjwillis <michael.willis@digital.justice.gov.uk>
2024-01-03 15:34:29 +0000

    CELL-47 Setup new Inside prison locations namespace.

---
+++ namespaces/locations-inside-prison-dev/resources/irsa.tf
@@ -0,0 +1,41 @@
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
+  }
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+}
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "locations-inside-prison-api"
+  role_policy_arns = merge(
+    local.sqs_policies,
+    local.sns_policies,
+    { prisoner-event-queue = module.prisoner-event-queue.irsa_policy_arn },
+    { prisoner-event-dlq = module.prisoner-event-dlq.irsa_policy_arn }
+  )
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
