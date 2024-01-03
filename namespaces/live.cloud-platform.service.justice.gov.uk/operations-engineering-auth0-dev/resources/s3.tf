6023c259c584d5f5330d0d6613dcbfba3330bf2c
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-03 15:45:03 +0000

    :wrench: Add GitHub Actions OIDC configuration for s3 bucket

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-auth0-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-auth0-dev/resources/s3.tf
@@ -12,7 +12,48 @@ module "s3_bucket" {
 resource "github_actions_secret" "role_arn" {
   repository      = var.repository_name
   secret_name     = "TERRAFORM_AUTH0_S3_ROLE_ARN_DEV"
-  plaintext_value = module.s3_bucket.irsa_policy_arn
+  plaintext_value = aws_iam_role.github.arn
+}
+locals {
+  oidc_provider = "token.actions.githubusercontent.com"
+}
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://${local.oidc_provider}"
+}
+
+resource "aws_iam_role" "github" {
+  name               = "operations-engineering-s3-oidc-role"
+  assume_role_policy = data.aws_iam_policy_document.github.json
+}
+data "aws_iam_policy_document" "github" {
+  version = "2012-10-17"
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "${local.oidc_provider}:sub"
+      values   = "repo:ministryofjustice/operations-engineering:*"
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_provider}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "github" {
+  role       = aws_iam_role.github.name
+  policy_arn = module.s3_bucket.irsa_policy_arn
 }
 
 resource "kubernetes_secret" "s3_bucket" {
