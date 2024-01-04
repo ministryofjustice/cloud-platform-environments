307edbd8dc0f425abd7fc3cd4ecafdae8245390f
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-01-04 21:41:19 +0000

    Amend service account name

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-dev/resources/servicepod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-dev/resources/servicepod.tf
@@ -3,5 +3,5 @@ module "service_pod" {
 
   # Configuration
   namespace            = var.namespace
-  service_account_name = module.irsa.service_account_name # this uses the service account name from the irsa module
+  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
 }
