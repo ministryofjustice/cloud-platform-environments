5d9603035afb36f9eccfa376d39c449a39cfd2e3
nataliewood <natalie.wood@digital.justice.gov.uk>
2024-01-05 15:29:47 +0000

    add service pod to hmpps-workload-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-workload-preprod/resources/irsa.tf
@@ -76,5 +76,5 @@ module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
 
   namespace            = var.namespace
-  service_account_name = module.irsa.service_account_name
+  service_account_name = module.irsa.service_account.name
 }
