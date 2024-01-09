9a0507f107fdf055349e943147aa8637890af8e5
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-01-09 15:40:15 +0000

    Bump secretsmanager module to 3.0.2 (#18568)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/secret.tf
@@ -1,5 +1,5 @@
 module "secret" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.2"
   # EKS configuration
   eks_cluster_name = var.eks_cluster_name
 
--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-pay-for-la-dev/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-pay-for-la-dev/resources/secret.tf
@@ -1,7 +1,7 @@
 
 module "secrets_manager" {
   #   source = "../"
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.2"
   team_name              = var.team_name
   application            = var.application
   business_unit          = var.business_unit
