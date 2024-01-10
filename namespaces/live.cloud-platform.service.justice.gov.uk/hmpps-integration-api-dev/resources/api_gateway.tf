3b5e11e1f334ad185570ec185add10c6ac955acb
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-01-10 11:14:04 +0000

    Add additional cloud watch alarms

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
@@ -250,6 +250,75 @@ resource "aws_cloudwatch_metric_alarm" "gateway_4XX_error_rate" {
   ]
 }
 
+resource "aws_cloudwatch_metric_alarm" "gateway_5XX_error_rate" {
+  alarm_name          = "${var.namespace}-gateway-5XX-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  alarm_description   = "Gateway 5xx error greater than 0"
+  treat_missing_data  = "notBreaching"
+  metric_name         = "5XXError"
+  namespace           = "AWS/ApiGateway"
+  period              = 30
+  evaluation_periods  = 1
+  threshold           = 1
+  statistic           = "Sum"
+  unit                = "Count"
+  actions_enabled     = true
+  alarm_actions       = [module.sns_topic.topic_arn]
+  dimensions = {
+    ApiName = var.namespace
+  }
+
+   depends_on = [
+    module.sns_topic
+  ]
+}
+
+resource "aws_cloudwatch_metric_alarm" "gateway_integration_latency" {
+  alarm_name          = "${var.namespace}-gateway-integration-latency-greater-than-3-seconds"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  alarm_description   = "Gateway 5xx error greater than 0"
+  treat_missing_data  = "notBreaching"
+  metric_name         = "IntegrationLatency"
+  namespace           = "AWS/ApiGateway"
+  period              = 60
+  evaluation_periods  = 1
+  threshold           = 3000
+  statistic           = "Maximum"
+  unit                = "Count"
+  actions_enabled     = true
+  alarm_actions       = [module.sns_topic.topic_arn]
+  dimensions = {
+    ApiName = var.namespace
+  }
+
+   depends_on = [
+    module.sns_topic
+  ]
+}
+
+resource "aws_cloudwatch_metric_alarm" "gateway_latency" {
+  alarm_name          = "${var.namespace}-gateway-latency-greater-than-5-seconds"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  alarm_description   = "Gateway 5xx error greater than 0"
+  treat_missing_data  = "notBreaching"
+  metric_name         = "IntegrationLatency"
+  namespace           = "AWS/ApiGateway"
+  period              = 60
+  evaluation_periods  = 1
+  threshold           = 5000
+  statistic           = "Maximum"
+  unit                = "Count"
+  actions_enabled     = true
+  alarm_actions       = [module.sns_topic.topic_arn]
+  dimensions = {
+    ApiName = var.namespace
+  }
+
+   depends_on = [
+    module.sns_topic
+  ]
+}
+
 module "sns_topic" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=5.0.0" 
 
