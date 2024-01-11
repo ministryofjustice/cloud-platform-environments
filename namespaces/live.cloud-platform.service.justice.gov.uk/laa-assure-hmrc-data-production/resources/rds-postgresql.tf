6e5aff709fe390fb9906c230bdbdf4244ebb341c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-01-11 07:54:18 +0000

    HMRC Assure Production - update RDS minor version
    Update minor version to 14.10 and rationalise the DB
    config block to be easier to read and similar to
    other rds instances maintained by this team

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/rds-postgresql.tf
@@ -16,33 +16,17 @@ module "rds" {
   infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 
-  # If the rds_name is not specified a random name will be generated ( cp-* )
-  # Changing the RDS name requires the RDS to be re-created (destroy + create)
-  # rds_name             = "my-rds-name"
-
   # enable performance insights
   performance_insights_enabled = true
 
-  # change the postgres version as you see fit.
-  db_engine_version = "14"
-
-  # change the instance class as you see fit.
-  db_instance_class = "db.t4g.small"
-
-  # rds_family should be one of: postgres10, postgres11, postgres12, postgres13, postgres14
-  # Pick the one that defines the postgres version the best
-  rds_family = "postgres14"
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  # Database configuration
+  db_engine_version           = "14.10"
+  db_instance_class           = "db.t4g.small"
+  rds_family                  = "postgres14"
+  allow_minor_version_upgrade = "true"
   allow_major_version_upgrade = "false"
-
-  # Enable auto start and stop of the RDS instances during 10:00 PM - 6:00 AM for cost saving, recommended for non-prod instances
-  # enable_rds_auto_start_stop  = true
-
-  # This will rotate the db password. Update the value to the current date.
-  # db_password_rotated_date  = "dd-mm-yyyy"
-
-  deletion_protection = true
+  enable_rds_auto_start_stop  = false
+  deletion_protection         = true
 
   providers = {
     # Can be either "aws.london" or "aws.ireland"
