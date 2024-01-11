1800399bba13d2fce49503b63f81264adc46259f
LavMatt <mattlaverty@gmail.com>
2024-01-11 11:20:22 +0000

    opensearch and rds resources for datahub

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/opensearch.tf
@@ -0,0 +1,42 @@
+module "opensearch_snapshot_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+module "opensearch" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.4.0"
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version      = "OpenSearch_2.9"
+  snapshot_bucket_arn = module.opensearch_snapshot_bucket.bucket_arn
+
+  cluster_config = {
+    instance_count = 6
+    instance_type  = "r6g.large.search"
+  }
+
+  ebs_options = {
+    volume_size = 10
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
