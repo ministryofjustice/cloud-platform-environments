e424d4080e64eb8290f9c1af98c6b527baa9439e
LavMatt <mattlaverty@gmail.com>
2024-01-12 11:41:17 +0000

    add irsa module to service-pod.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/service-pod.tf
@@ -1,3 +1,31 @@
+# irsa configuration for the service pod
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "${var.namespace}-irsa-sevice-account"
+  namespace            = var.namespace
+
+  # Attach the approprate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = {
+    datahub_rds_postgres                  = module.rds.irsa_policy_arn
+    datahub_opensearch                    = module.opensearch.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
 module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
 
