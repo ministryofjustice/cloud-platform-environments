1505532a047a6d8006a42c2f94f322c09be1de5c
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-12 09:14:27 +0000

    update s3 bucket policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-dev/resources/gha_oidc.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-dev/resources/gha_oidc.tf
@@ -42,13 +42,21 @@ data "aws_iam_policy_document" "s3_access_policy_document" {
   statement {
     effect  = "Allow"
     actions = ["s3:*"]
-    resources = [module.s3_bucket.bucket_arn]
+    resources = [
+      module.s3_bucket.bucket_arn,
+      "${module.s3_bucket.bucket_arn}/*"
+    ]
   }
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3_access_policy"
+  policy      = data.aws_iam_policy_document.s3_access_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "github_role_perms_attachment" {
   role       = aws_iam_role.github.name
-  policy = data.aws_iam_policy_document.s3_access_policy_document.json
+  policy_arn = aws_iam_policy.s3_access_policy.arn
 } 
 
 resource "github_actions_secret" "role_arn" {
