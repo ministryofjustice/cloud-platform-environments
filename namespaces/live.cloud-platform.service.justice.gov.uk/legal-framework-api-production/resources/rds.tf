380dfb6b7f714b3bf40cbfa3e73d0cdb4e9d6a8b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-01-12 07:22:24 +0000

    Update Legal Framework Production RDS
    remove lock at minor version and revert auto stop start

--- namespaces/live.cloud-platform.service.justice.gov.uk/legal-framework-api-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/legal-framework-api-production/resources/rds.tf
@@ -14,12 +14,12 @@ module "rds" {
   performance_insights_enabled = true
 
   # Database configuration
-  db_engine_version           = "14.10"
+  db_engine_version           = "14"
   db_instance_class           = "db.t4g.small"
   rds_family                  = "postgres14"
   allow_minor_version_upgrade = "true"
   allow_major_version_upgrade = "false"
-  enable_rds_auto_start_stop  = true
+  enable_rds_auto_start_stop  = false
   deletion_protection         = true
 
   providers = {
