10c4d7e21bd7056f2121ab40059d3db9cad44956
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-01-16 14:44:40 +0000

    CVSL-1516 hardcode env for test2 arn name

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test2/resources/domain-events-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test2/resources/domain-events-topic.tf
@@ -10,5 +10,5 @@ resource "kubernetes_secret" "hmpps_domain_events_topic" {
 }
 
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
-  name = "/hmpps-domain-events-${var.environment}/topic-arn"
+  name = "/hmpps-domain-events-dev/topic-arn"
 }
