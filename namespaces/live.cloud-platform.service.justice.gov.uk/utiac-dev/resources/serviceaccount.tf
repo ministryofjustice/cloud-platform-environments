c3a9f07b1094b3549336757eb9e82d1885f8d642
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-01-16 16:11:28 +0000

    utiac-dev: add service pod (#18778)
    * add service pod
    
    * fix reference to s3

--- namespaces/live.cloud-platform.service.justice.gov.uk/utiac-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/utiac-dev/resources/serviceaccount.tf
@@ -5,7 +5,7 @@ module "serviceaccount" {
   kubernetes_cluster  = var.kubernetes_cluster
   serviceaccount_name = "circleci"
 
-  serviceaccount_token_rotated_date = "01-01-2000"
+  serviceaccount_token_rotated_date = "16-01-2024"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
