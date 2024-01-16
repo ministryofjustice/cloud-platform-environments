33352db107cb7b0b21b8083d87c0b66e4970a941
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-01-16 14:03:47 +0000

    Add IRSA

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/irsa.tf
@@ -11,7 +11,11 @@ locals {
     "Digital-Prison-Services-dev-cvl_prison_test1_events_queue"       = "offender-events-dev",
     "Digital-Prison-Services-dev-cvl_prison_test1_events_queue_dl"    = "offender-events-dev"
   }
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
 module "irsa" {
@@ -20,7 +24,7 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = local.sqs_policies
+  role_policy_arns     = merge(local.sqs_policies, local.sns_policies)
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -33,4 +37,9 @@ module "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arns" {
   for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
