47e0ae89cc30b1e3c7c16453c3d71e4443465904
petergphillips <peter@greenthistle.co.uk>
2024-01-16 11:06:16 +0000

    SDIT-1427: ✨ New prod prisoner download environment (#18769)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-download-prod/resources/variables.tf
@@ -0,0 +1,52 @@
+variable "domain" {
+  default = "prisoner-download.prison.service.justice.gov.uk"
+}
+
+variable "application" {
+  default = "hmpps-prisoner-download"
+}
+
+variable "namespace" {
+  default = "hmpps-prisoner-download-prod"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "syscon-devs"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "true"
+}
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
+variable "kubernetes_cluster" {}
+
+variable "eks_cluster_name" {}
+
+
