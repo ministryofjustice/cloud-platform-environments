f855bb43313aca5b600f24879e0afd80705c5e39
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2024-01-17 14:49:53 +0000

    CPR-63-prod
    . Removed offender merged/un merged event subscriptions.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/cpr-delius-offender-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/cpr-delius-offender-events-queue.tf
@@ -7,9 +7,7 @@ resource "aws_sns_topic_subscription" "cpr_delius_offender_events_subscription" 
   endpoint  = module.cpr_delius_offender_events_queue.sqs_arn
   filter_policy = jsonencode({
     eventType = [
-      "probation-case.engagement.created",
-      "probation-case.merge.completed",
-      "probation-case.unmerge.completed"
+      "probation-case.engagement.created"
     ]
   })
 }
