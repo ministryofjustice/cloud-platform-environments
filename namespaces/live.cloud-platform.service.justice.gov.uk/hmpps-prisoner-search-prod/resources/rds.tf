f421dd2e40858c8c70090f81ac66eb5829062b3d
petergphillips <peter@greenthistle.co.uk>
2024-01-17 10:03:28 +0000

    SDIT-1416: 🗃️ Finish off postgres16 upgrade for hmpps-prisoner-search-prod (#18786)
    * SDIT-1416: 🗃️ Finish off postgres16 upgrade for hmpps-prisoner-search-prod
    
    * SDIT-1416: 🔧 Empty commit to hopefully get checks to pass this time

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-search-prod/resources/rds.tf
@@ -14,7 +14,7 @@ module "hmpps_prisoner_search_rds" {
   db_engine_version            = "16"
   rds_family                   = "postgres16"
   deletion_protection          = true
-  prepare_for_major_upgrade    = true
+  prepare_for_major_upgrade    = false
   db_max_allocated_storage     = "500"
   performance_insights_enabled = true
 
