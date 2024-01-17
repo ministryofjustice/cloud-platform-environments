aeb5fb26d17ca768a908d6df053822a2c2ceabde
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-01-17 12:46:36 +0000

    assess-dev-crm4: Bump postgres 14.10 to 16.1

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev-crm4/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev-crm4/resources/rds-postgresql.tf
@@ -12,16 +12,16 @@ module "rds" {
 
   # RDS configuration
   allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
+  allow_major_version_upgrade  = true
+  performance_insights_enabled = true
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "14.10"
-  rds_family        = "postgres14"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
   # Tags
