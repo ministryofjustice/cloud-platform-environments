182ae9014ee46ed7a708fc54c542fe57e99d4293
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-01-17 07:41:12 +0000

    Remove prepare_for_major_upgrade
    Following guide note
    ```
    Note: If you need to upgrade major versions more than once, e.g. from Postgres 12 to 14, and then 14 to 16, you must turn off prepare_for_major_upgrade (step 6) after each upgrade and then turn it back on (step 5). Otherwise, any upgrade after the first will fail.
    ```

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev-crm4/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev-crm4/resources/rds-postgresql.tf
@@ -13,7 +13,7 @@ module "rds" {
   # RDS configuration
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = true
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
 
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
