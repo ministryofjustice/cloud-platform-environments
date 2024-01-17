80baa47278b1c03831d1a535f2bd24bd9a6dec06
Tom Hipkin <tomhipkin@gmail.com>
2024-01-17 16:31:12 +0000

    Update CAS support email on test
    There was only one person left on this inbox and since CAS is now a shared environment between three teams, we’ve made a new shared inbox to make sure platform requests are recieved in future.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/variables.tf
@@ -29,7 +29,7 @@ variable "environment" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "cas3@digital.justice.gov.uk"
+  default     = "casdev@digital.justice.gov.uk"
 }
 
 variable "is_production" {
