05392f2ca920adb411797f772fc3d58b12f55ada
ayeshamoj <ayesha.kholwadia@digital.justice.gov.uk>
2024-01-18 12:12:27 +0000

    lowercase update

--- namespaces/live.cloud-platform.service.justice.gov.uk/soc-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/soc-staging/resources/variables.tf
@@ -24,7 +24,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "Central Digital Product Team"
+  default     = "central-digital-product-team"
 }
 
 variable "environment" {
