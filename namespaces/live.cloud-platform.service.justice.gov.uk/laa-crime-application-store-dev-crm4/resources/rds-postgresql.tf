aebd99db16c4a05696152f194a3e731693b2b7c3
Paul King <84575729+pking-moj@users.noreply.github.com>
2024-01-18 16:06:01 +0000

    Set upgrade flag to false

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-dev-crm4/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-dev-crm4/resources/rds-postgresql.tf
@@ -13,7 +13,7 @@ module "rds" {
   # RDS configuration
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
@@ -58,8 +58,8 @@ module "read_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "14.7"
-  rds_family        = "postgres14"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
   # It is mandatory to set the below values to create read replica instance
 
