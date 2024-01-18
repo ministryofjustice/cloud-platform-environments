5e2e88f57f9b3b234ed3c8fc18e92479b1070ff4
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-18 14:56:17 +0000

    rename iam role and s3 access policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
@@ -7,7 +7,7 @@ data "aws_iam_openid_connect_provider" "github" {
 }
 
 resource "aws_iam_role" "github" {
-  name               = "operations-engineering-github-repos-role"
+  name               = "operations-engineering-github-repos-prod-role"
   assume_role_policy = data.aws_iam_policy_document.github.json
 }
 
@@ -49,14 +49,14 @@ data "aws_iam_policy_document" "s3_access_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "s3_access_policy"
+resource "aws_iam_policy" "github_role_prod_s3_access_policy" {
+  name        = "github_role_prod_s3_access_policy"
   policy      = data.aws_iam_policy_document.s3_access_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "github_role_perms_attachment" {
   role       = aws_iam_role.github.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
+  policy_arn = aws_iam_policy.github_role_prod_s3_access_policy.arn
 } 
 
 resource "github_actions_secret" "role_arn" {
