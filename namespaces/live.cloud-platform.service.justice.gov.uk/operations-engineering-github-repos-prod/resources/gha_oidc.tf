192e3046d46bb9f542efd1f6d14e2f4362e497f9
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-01-18 15:34:32 +0000

    revert name of terraform resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "github" {
   }
 }
 
-data "aws_iam_policy_document" "github_role_prod_s3_access_policy_document" {
+data "aws_iam_policy_document" "s3_access_policy_document" {
   version = "2012-10-17"
 
   statement {
@@ -49,14 +49,14 @@ data "aws_iam_policy_document" "github_role_prod_s3_access_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "github_role_prod_s3_access_policy" {
+resource "aws_iam_policy" "s3_access_policy" {
   name        = "github_role_prod_s3_access_policy"
-  policy      = data.aws_iam_policy_document.github_role_prod_s3_access_policy_document.json
+  policy      = data.aws_iam_policy_document.s3_access_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "github_role_perms_attachment" {
   role       = aws_iam_role.github.name
-  policy_arn = aws_iam_policy.github_role_prod_s3_access_policy.arn
+  policy_arn = aws_iam_policy.s3_access_policy.arn
 } 
 
 resource "github_actions_secret" "role_arn" {
