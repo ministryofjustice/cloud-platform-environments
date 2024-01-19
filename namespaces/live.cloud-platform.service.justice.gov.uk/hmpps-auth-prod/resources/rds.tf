9155b2eff96374b3736abf1d35625510b7cd90ad
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-01-19 14:54:00 +0000

    HAAR-2243: update to postgres 16 for hmpps-auth-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-prod/resources/rds.tf
@@ -11,10 +11,11 @@ module "dps_rds" {
   performance_insights_enabled = true
   db_instance_class            = "db.t4g.large"
   deletion_protection          = true
-  prepare_for_major_upgrade    = false
-  rds_family                   = "postgres15"
+  prepare_for_major_upgrade    = true
+  rds_family                   = "postgres16"
   db_engine                    = "postgres"
-  db_engine_version            = "15"
+  db_engine_version            = "16"
+  performance_insights_enabled  = true
 
   providers = {
     aws = aws.london
