ba7ac0fcbc163d172355f2c517568e98f9bbb6cb
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-01-22 16:50:55 +0000

    HAAR-2243: set prepare_for_major_upgrade to false and remove skip namespace file for offender-case-notes-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
@@ -11,7 +11,7 @@ module "dps_rds" {
   db_instance_class         = "db.t4g.small"
   db_max_allocated_storage  = "10000"
   deletion_protection       = true
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
   rds_family                = "postgres16"
   db_engine                 = "postgres"
   db_engine_version         = "16"
