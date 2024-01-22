0f8a50a21f12ee6dc723db51e74428e241b5d29a
brian.carr <brian.carr@digital.justice.gov.uk>
2024-01-22 17:43:42 +0000

    setting rds param group to default

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/rds-aurora.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/rds-aurora.tf
@@ -16,7 +16,7 @@ module "rds_aurora" {
   replica_count                = 1
   performance_insights_enabled = true
   #db_parameter_group_name      = resource.aws_db_parameter_group.default.name
-  #db_parameter_group_name       = default.aurora-postgres14
+  db_parameter_group_name       = default.aurora-postgres14
   allow_major_version_upgrade  = true
 
   # Tags
