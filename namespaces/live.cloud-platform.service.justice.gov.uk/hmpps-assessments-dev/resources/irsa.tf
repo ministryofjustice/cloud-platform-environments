dc092144f2f2e45cf82800c5fb3c52febb144f05
Lucas Cairns <lucas.cairns@gmail.com>
2024-01-22 16:36:39 +0000

    Update IRSA config in hmpps-assessments-dev to include audit queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assessments-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assessments-dev/resources/irsa.tf
@@ -3,6 +3,10 @@ locals {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
 module "irsa" {
@@ -16,7 +20,8 @@ module "irsa" {
       rds         = module.hmpps_assessments_rds.irsa_policy_arn
       elasticache = module.hmpps_assessments_elasticache_redis.irsa_policy_arn
     },
-    local.sns_policies
+    local.sns_policies,
+    local.sqs_policies
   )
   business_unit          = var.business_unit
   application            = var.application
@@ -29,6 +34,11 @@ module "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
 
 resource "kubernetes_secret" "irsa" {
