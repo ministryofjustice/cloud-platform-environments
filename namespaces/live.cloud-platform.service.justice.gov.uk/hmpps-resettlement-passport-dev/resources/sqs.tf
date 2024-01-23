0f6e85e8654e5daba68e0430d206d369e4322558
Emily Pritchard <emily.pritchard@hippodigital.co.uk>
2024-01-23 15:32:48 +0000

    refactored to use module and incorporate policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-dev/resources/sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-dev/resources/sqs.tf
@@ -1,8 +1,90 @@
-resource "aws_sqs_queue" "sh_queue" {
-  name                       = "dev-standard-queue"
-  delay_seconds              = 0
-  visibility_timeout_seconds = 30
-  max_message_size           = 2048
-  receive_wait_time_seconds  = 2
-  sqs_managed_sse_enabled = true
+module "prisoner-event-queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  sqs_name                  = "case-note-queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.case-note-dlq.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+data "aws_iam_policy_document" "sqs" {
+  policy_id = "${module.case-note-queue.sqs_arn}/SQSDefaultPolicy"
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [module.case-note-queue.sqs_arn]
+    actions   = ["SQS:SendMessage",
+                "sqs:ReceiveMessage"
+                ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "case-note-queue-policy" {
+  queue_url = module.case-note-queue.sqs_id
+  policy    = data.aws_iam_policy_document.sqs.json
+}
+
+module "prisoner-event-dlq" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  sqs_name        = "case-note-dlq"
+  encrypt_sqs_kms = "true"
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "prisoner-event-queue" {
+  metadata {
+    name      = "sqs-case-notes-queue-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.case-note-queue.sqs_id
+    sqs_queue_arn  = module.case-note-queue.sqs_arn
+    sqs_queue_name = module.case-note-queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "case-note-queue-dlq" {
+  metadata {
+    name      = "sqs-case-note-queue-dlq-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.case-note-dlq.sqs_id
+    sqs_queue_arn  = module.case-note-dlq.sqs_arn
+    sqs_queue_name = module.case-note-dlq.sqs_name
+  }
 }
