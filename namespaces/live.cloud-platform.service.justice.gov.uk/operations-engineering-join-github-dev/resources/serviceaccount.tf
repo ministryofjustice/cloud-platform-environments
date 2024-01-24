7c9f89d6727d71ddf166a4efbbe72061d8abcebc
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-01-24 15:57:57 +0000

    Update serviceaccount.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-join-github-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-join-github-dev/resources/serviceaccount.tf
@@ -6,6 +6,7 @@ module "serviceaccount" {
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
+
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories                  = ["operations-engineering-join-github"]
