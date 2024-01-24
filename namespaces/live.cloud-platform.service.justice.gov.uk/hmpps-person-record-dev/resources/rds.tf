2cd555034f7039c172f8b23b655dfcb1ef4edf9a
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-01-24 16:28:08 +0000

    CPR-125 disable prepare_for_major_upgrade

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
@@ -12,7 +12,7 @@ module "hmpps_person_record_rds" {
   db_instance_class      = "db.t3.small"
   db_engine              = "postgres"
   db_engine_version      = "16.1"
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
   allow_major_version_upgrade = "true"
 
   providers = {
