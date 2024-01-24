3d63b72a424d92a265868ffb5bf4bfc560d4e82b
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2024-01-24 15:36:28 +0000

    SAA-1496 Removed allow minor version from db engine

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
@@ -14,10 +14,9 @@ module "activities_api_rds" {
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
   rds_family                  = var.rds_family
-  allow_minor_version_upgrade = "false"
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t4g.small"
-  db_engine_version           = "14.10"
+  db_engine_version           = "14"
   
 
   # Add security groups for DPR
