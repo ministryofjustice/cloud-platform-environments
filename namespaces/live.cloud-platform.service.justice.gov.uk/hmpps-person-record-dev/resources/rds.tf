89decbfc483e273d8d80fa94598459bfdf922d54
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-01-24 14:09:41 +0000

    CPR-125 upgrade to postgres v16

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
@@ -8,11 +8,11 @@ module "hmpps_person_record_rds" {
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-  rds_family             = "postgres15"
+  rds_family             = "postgres16"
   db_instance_class      = "db.t3.small"
   db_engine              = "postgres"
-  db_engine_version      = "15.5"
-  prepare_for_major_upgrade = false
+  db_engine_version      = "16.1"
+  prepare_for_major_upgrade = true
   allow_major_version_upgrade = "true"
 
   providers = {
