c9e5e88bba90f6949e9b5f9648dca47b7fc15f8c
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2024-01-25 11:36:16 +0000

    upgrading rds postgresql to 15.5 (#19100)
    * upgrading rds postgresql to 15.5
    
    * adding lifecycle block to db param group
    
    ---------
    
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/rds-aurora.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/rds-aurora.tf
@@ -6,7 +6,7 @@ module "rds_aurora" {
 
   # Database configuration
   engine         = "aurora-postgresql"
-  engine_version = "14.6"
+  engine_version = "15.5"
   engine_mode    = "provisioned"
   instance_type  = "db.serverless"
   serverlessv2_scaling_configuration = {
@@ -34,7 +34,11 @@ module "rds_aurora" {
 
 resource "aws_db_parameter_group" "default" {
   name   = module.rds_aurora.db_cluster_identifier
-  family = "aurora-postgresql14"
+  family = "aurora-postgresql15"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   parameter {
     name  = "log_error_verbosity"
