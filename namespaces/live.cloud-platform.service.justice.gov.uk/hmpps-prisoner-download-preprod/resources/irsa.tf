7c2f7f1d574d46a5f0d722fad3e688e2cb09bec6
Peter Phillips <peter@greenthistle.co.uk>
2024-01-25 10:19:46 +0000

    SDIT-1432: 🔧 Move audit sqs access to backend for hmpps-prisoner-download-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-download-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-download-preprod/resources/irsa.tf
@@ -2,28 +2,12 @@ locals {
   sqs_queues = {
     "Digital-Prison-Services-${var.environment}-hmpps_audit_queue" = "hmpps-audit-${var.environment}",
   }
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value}
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
-}
-
-module "download_ui_irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-
-  eks_cluster_name     = var.eks_cluster_name
-  namespace            = var.namespace
-  service_account_name = "hmpps-prisoner-download"
-  role_policy_arns     = local.sqs_policies
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
 }
 
 module "download_api_irsa" {
@@ -36,9 +20,10 @@ module "download_api_irsa" {
   service_account_name = "hmpps-prisoner-download-api"
   namespace            = var.namespace
 
-  role_policy_arns = {
-    s3 = module.hmpps-prisoner-download_s3_bucket.irsa_policy_arn
-  }
+  role_policy_arns = merge(
+    local.sqs_policies,
+    { s3 = module.hmpps-prisoner-download_s3_bucket.irsa_policy_arn },
+  )
 
   # Tags
   business_unit          = var.business_unit
