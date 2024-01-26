1061a8eb68d5c4ec8f10beb44d4c3b7bb22eec57
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2024-01-26 11:34:55 +0000

    New environment for preprod.justice.gov.uk (#19134)
    * New environment for preprod.justice.gov.uk
    
    * Small update to bump concourse build
    
    * Remove preprod cert

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/cloudfront.tf
@@ -0,0 +1,29 @@
+module "cloudfront" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.2.0"
+
+  # Configuration
+  bucket_id          = module.s3_bucket.bucket_name
+  bucket_domain_name = "${module.s3_bucket.bucket_name}.s3.eu-west-2.amazonaws.com"
+
+  # Can we get a custom domain for the CDN?
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "cloudfront_url" {
+  metadata {
+    name      = "cloudfront-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    cloudfront_url = module.cloudfront.cloudfront_url
+  }
+}
