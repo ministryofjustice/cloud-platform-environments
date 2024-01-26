53bafc3fe299529c36d3cf801b72fdd8547db5bf
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2024-01-26 11:33:59 +0000

    New environment for justice.gov.uk (#19135)
    * New environment for preprod.justice.gov.uk
    
    * New environment for justice.gov.uk
    
    * Remove staging stack
    
    * Remove production cert
    
    * Use correct environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-production/resources/ecr.tf
@@ -0,0 +1,57 @@
+module "ecr_credentials" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
+
+  repo_name = "${var.namespace}-ecr"
+
+  lifecycle_policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep the newest 50 images and mark the rest for expiration",
+        "selection": {
+          "tagStatus": "any",
+          "countType": "imageCountMoreThan",
+          "countNumber": 50
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+
+  # enable the oidc implementation for GitHub
+  oidc_providers = ["github"]
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ECR name, AWS access key, and AWS secret key, for use in
+  # github actions CI/CD pipelines
+  github_repositories = ["justice-gov-uk"]
+
+  # Create secrets in target GitHub environment
+  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  github_environments = ["production"]
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "ecr_credentials" {
+  metadata {
+    name      = "ecr-${var.namespace}"
+    namespace = var.namespace
+  }
+
+  data = {
+    repo_arn = module.ecr_credentials.repo_arn
+    repo_url = module.ecr_credentials.repo_url
+  }
+}
