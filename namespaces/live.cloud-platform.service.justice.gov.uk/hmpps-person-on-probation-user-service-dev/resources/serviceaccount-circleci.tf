ca34a794c89298cdb51681ab80626427800fd5c1
Thomas Halson <thomas.halson@digital.justice.gov.uk>
2024-01-29 11:50:34 +0000

    change resource name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-on-probation-user-service-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-on-probation-user-service-dev/resources/serviceaccount-circleci.tf
@@ -1,4 +1,4 @@
-module "serviceaccount" {
+module "circleci-sa" {
   source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
