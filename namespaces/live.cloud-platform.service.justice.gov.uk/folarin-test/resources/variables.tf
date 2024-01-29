81a194db91881166607d4a5d7b5f85c7db16b006
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-01-29 17:19:38 +0000

    only reran the environment config command

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "current time app"
+  default     = "time checker"
 }
 
 variable "namespace" {
