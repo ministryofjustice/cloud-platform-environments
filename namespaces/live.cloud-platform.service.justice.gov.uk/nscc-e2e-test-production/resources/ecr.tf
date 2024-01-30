c2c161e32b2bc426a6814c30eac7c1cd61d9a4b8
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-01-30 13:42:05 +0000

    Adds container repository configuration

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/nscc-e2e-test-production/resources/ecr.tf
@@ -0,0 +1,57 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
+
+  # Repository configuration
+  repo_name = var.namespace
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github", "circleci"]
+  github_repositories = ["nsm-e2e-test"]
+
+   lifecycle_policy = <<EOF
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Expire untagged images older than 14 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 14
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 2,
+          "description": "Keep the newest 100 images and mark the rest for expiration",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 100
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+    EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
