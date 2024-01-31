47d9f75f63ecf1b21709c7f7a48448b582c4cea8
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2024-01-31 09:39:40 +0000

    add certificate and  elasticache redis to casenotes search prototype

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-casenotes-search-prototype/resources/elasticache.tf
@@ -0,0 +1,32 @@
+module "elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=7.0.0"
+  vpc_name               = var.vpc_name
+  application            = var.application
+  environment_name       = var.environment
+  is_production          = var.is_production
+  infrastructure_support = var.infrastructure_support
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  number_cache_clusters  = "2"
+  node_type              = "cache.t4g.micro"
+  engine_version         = "7.0"
+  parameter_group_name   = "default.redis7"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "elasticache_redis" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.elasticache_redis.primary_endpoint_address
+    auth_token               = module.elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.elasticache_redis.member_clusters)
+  }
+}
