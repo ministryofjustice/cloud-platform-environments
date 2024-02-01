998b42c64b392ab49a332efe4ccb139784360d8a
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-02-01 05:34:43 +0000

    reran the env config command trying to correct 2 errors out of 12 processes

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "time checker"
+  default     = "helloworld"
 }
 
 variable "namespace" {
