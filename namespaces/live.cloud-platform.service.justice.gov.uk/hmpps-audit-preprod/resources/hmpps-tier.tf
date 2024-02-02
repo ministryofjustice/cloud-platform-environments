78bc26e1d934593f06e03bd58ee413846bdf9e6b
Marcus Aspin <maspin@unilink.com>
2024-02-02 13:58:14 +0000

    PI-1848 Add IRSA policy ARN for audit queue (preprod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-preprod/resources/hmpps-tier.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-preprod/resources/hmpps-tier.tf
@@ -1,12 +1,13 @@
 resource "kubernetes_secret" "hmpps_tier" {
   metadata {
     name      = "sqs-hmpps-audit-secret"
-    namespace = "hmpps-tier-preprod"
+    namespace = "hmpps-tier-${var.environment-name}"
   }
 
   data = {
-    sqs_queue_url  = module.hmpps_audit_queue.sqs_id
-    sqs_queue_arn  = module.hmpps_audit_queue.sqs_arn
-    sqs_queue_name = module.hmpps_audit_queue.sqs_name
+    sqs_queue_url   = module.hmpps_audit_queue.sqs_id
+    sqs_queue_arn   = module.hmpps_audit_queue.sqs_arn
+    sqs_queue_name  = module.hmpps_audit_queue.sqs_name
+    irsa_policy_arn = module.hmpps_audit_queue.irsa_policy_arn
   }
 }
