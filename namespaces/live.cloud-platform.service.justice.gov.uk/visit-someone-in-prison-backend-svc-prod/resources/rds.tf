b211925a1d5f958ddc7482920b971994880dad51
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-02-02 11:59:22 +0000

    VB-3380 - update DB version to 15.5 for visit-someone-in-prison-backend-svc-prod namespace.

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
@@ -10,10 +10,10 @@ module "visit_scheduler_rds" {
   namespace              = var.namespace
 
   allow_major_version_upgrade = "false"
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
   db_engine                   = "postgres"
-  db_engine_version           = "14.10"
-  rds_family                  = "postgres14"
+  db_engine_version           = "15.5"
+  rds_family                  = "postgres15"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "10000"
   db_password_rotated_date    = "2023-05-11"
