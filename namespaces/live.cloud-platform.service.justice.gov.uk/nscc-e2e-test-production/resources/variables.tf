499ae0344d4091050ede22cbe89806ffcf610601
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-02-02 13:57:56 +0000

    Updates team name for e2e test

--- namespaces/live.cloud-platform.service.justice.gov.uk/nscc-e2e-test-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/nscc-e2e-test-production/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "nsm-e2e-test-admin-team"
+  default     = "crm7team"
 }
 
 variable "environment" {
