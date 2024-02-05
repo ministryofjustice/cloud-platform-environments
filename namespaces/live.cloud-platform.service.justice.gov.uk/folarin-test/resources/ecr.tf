620f666f058a1f3ac3de946345a117a1cc8d3369
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-02-05 14:05:41 +0000

    changed the ecr version to 6.1.1

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=tf-validation"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.1"
 
   # Repository configuration
   repo_name = var.namespace
