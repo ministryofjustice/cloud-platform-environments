4e23ca542a45ad5c6f47c3cb7e7db6733860815b
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-06 10:57:50 +0000

    Rebuild s3 and api gw

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-dev/resources/versions.tf
@@ -13,9 +13,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
-    # random = {
-    #   source  = "hashicorp/random"
-    #   version = "~> 3.5.1"
-    # }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
   }
 }
