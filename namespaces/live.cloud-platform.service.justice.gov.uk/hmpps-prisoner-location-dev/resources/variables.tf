d436787d1a5ceb291fb01d6fe4bdbb611f28ff2e
petergphillips <peter@greenthistle.co.uk>
2024-02-06 12:22:18 +0000

    SDIT-1467: 🚚 Rename prisoner download hmpps-prisoner-location-dev (#20561)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-location-dev/resources/variables.tf
@@ -0,0 +1,53 @@
+variable "domain" {
+  default = "prisoner-location-dev.prison.service.justice.gov.uk"
+}
+
+variable "application" {
+  default = "hmpps-prisoner-location"
+}
+
+variable "namespace" {
+  default = "hmpps-prisoner-location-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "syscon-devs"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
+variable "kubernetes_cluster" {}
+
+variable "eks_cluster_name" {}
+
+variable "vpc_name" {}
+
