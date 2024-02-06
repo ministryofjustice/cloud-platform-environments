1ddf1e464a701268464f4ca466eb9cf7d3059883
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-02-06 13:26:51 +0000

    Revert "changed the ecr version to 6.1.1"
    This reverts commit 620f666f058a1f3ac3de946345a117a1cc8d3369.

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-test/resources/ecr.tf
@@ -5,7 +5,7 @@
  *
  */
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.1"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=tf-validation"
 
   # Repository configuration
   repo_name = var.namespace
