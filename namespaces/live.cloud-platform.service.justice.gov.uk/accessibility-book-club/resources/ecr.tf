287509d7c6eb1f2f7809009cfb110ff1fc0a8e82
jackstockley89 <jstock8904@gmail.com>
2024-02-06 17:17:43 +0000

    feat(bump): ecr module to latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/accessibility-book-club/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/accessibility-book-club/resources/ecr.tf
@@ -1,5 +1,5 @@
 module "ecr-repo" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=6.1.1"
 
   repo_name = "${var.namespace}-ecr"
 
