ca6e3169c9757fc46ab535306d8fb746e46ac3db
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2024-02-06 12:36:02 +0000

    Adding role_name and rolebinding_name to avoid duplicate sa issue

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/serviceaccount-circleci-formbuilder-publisher-test.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/serviceaccount-circleci-formbuilder-publisher-test.tf
@@ -4,9 +4,12 @@ module "serviceaccount_circleci-formbuilder-publisher-test" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
+  role_name = "circleci-formbuilder-publisher-test-sa-migrated"
+  rolebinding_name = "circleci-formbuilder-publisher-sa-test-migrated"
+
   serviceaccount_token_rotated_date = "01-01-2000"
 
-  serviceaccount_name = "circleci-formbuilder-publisher-test-migrated"
+  serviceaccount_name = "circleci-formbuilder-publisher-test-sa-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/serviceaccount-formbuilder-publisher-workers-test.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-test/resources/serviceaccount-formbuilder-publisher-workers-test.tf
@@ -4,9 +4,12 @@ module "serviceaccount_formbuilder-publisher-workers-test" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
+  role_name = "formbuilder-publisher-workers-test-sa-migrated"
+  rolebinding_name = "formbuilder-publisher-workers-test-sa-migrated"
+
   serviceaccount_token_rotated_date = "01-01-2000"
 
-  serviceaccount_name = "formbuilder-publisher-workers-test-migrated"
+  serviceaccount_name = "formbuilder-publisher-workers-test-sa-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
