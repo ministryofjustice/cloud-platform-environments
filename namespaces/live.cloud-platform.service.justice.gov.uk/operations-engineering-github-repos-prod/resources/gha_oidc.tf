d3b590a69bff1e2d8269df0d302793cfccab5bb7
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-02-07 11:21:41 +0000

    Added policy for dynamodb state lock table to function correctly

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-github-repos-prod/resources/gha_oidc.tf
@@ -49,15 +49,42 @@ data "aws_iam_policy_document" "s3_access_policy_document" {
   }
 }
 
+data "aws_iam_policy_document" "dynamodb_state_lock_policy" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:DescribeTable",
+    ]
+    resources = [
+      module.github_repos_prod_state_lock_table.table_arn
+    ]
+  }
+}
+
 resource "aws_iam_policy" "s3_access_policy" {
   name        = "github_role_prod_s3_access_policy"
   policy      = data.aws_iam_policy_document.s3_access_policy_document.json
 }
 
+resource "aws_iam_policy" "dynamodb_state_lock" {
+  name   = "github-repos-prod-dynamodb-state-lock-policy"
+  policy = data.aws_iam_policy_document.dynamodb_state_lock_policy.json
+}
+
 resource "aws_iam_role_policy_attachment" "github_role_perms_attachment" {
   role       = aws_iam_role.github.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
-} 
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_state_lock_attachment" {
+  role       = aws_iam_role.github.name
+  policy_arn = aws_iam_policy.dynamodb_state_lock.arn
+}
 
 resource "github_actions_secret" "role_arn" {
   repository      = var.repository_name
