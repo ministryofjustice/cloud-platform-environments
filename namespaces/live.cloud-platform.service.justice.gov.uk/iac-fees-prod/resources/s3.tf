60e1e90ec8f1f7221442c0d2c79e63bab161355f
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-07 11:01:53 +0000

    rebuild commented out resources in new region (#20620)

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/s3.tf
@@ -1,86 +1,86 @@
-# module "s3_bucket" {
+module "s3_bucket" {
 
-#   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
-#   team_name              = var.team_name
-#   business_unit          = var.business_unit
-#   application            = var.application
-#   is_production          = var.is_production
-#   environment_name       = var.environment
-#   infrastructure_support = var.infrastructure_support
-#   namespace              = var.namespace
-#   versioning             = true
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+  versioning             = true
 
-#   providers = {
-#     aws = aws.ireland
-#   }
+  providers = {
+    aws = aws.ireland
+  }
 
-#   bucket_policy = <<EOF
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Sid": "AllowBucketAccess",
-#         "Effect": "Allow",
-#         "Principal": {
-#           "AWS": "${aws_iam_role.api_gateway_role.arn}"
-#         },
-#         "Action": "s3:PutObject",
-#         "Resource": [
-#           "$${bucket_arn}",
-#           "$${bucket_arn}/*"
-#         ]
-#       }
-#     ]
-#   }
-#   EOF
+  bucket_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "AllowBucketAccess",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "${aws_iam_role.api_gateway_role.arn}"
+        },
+        "Action": "s3:PutObject",
+        "Resource": [
+          "$${bucket_arn}",
+          "$${bucket_arn}/*"
+        ]
+      }
+    ]
+  }
+  EOF
 
-# }
+}
 
-# data "aws_iam_policy_document" "cgi_s3_access_policy" {
+data "aws_iam_policy_document" "cgi_s3_access_policy" {
 
-#   statement {
-#     sid = "AllowCgiUserToReadAndWriteS3"
-#     actions = [
-#       "s3:GetObject*",
-#       "s3:DeleteObject*",
-#       "s3:ListBucket",
-#       "s3:GetBucketLocation"
-#     ]
+  statement {
+    sid = "AllowCgiUserToReadAndWriteS3"
+    actions = [
+      "s3:GetObject*",
+      "s3:DeleteObject*",
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
 
-#     resources = [
-#       module.s3_bucket.bucket_arn,
-#       "${module.s3_bucket.bucket_arn}/*"
-#     ]
-#   }
-# }
+    resources = [
+      module.s3_bucket.bucket_arn,
+      "${module.s3_bucket.bucket_arn}/*"
+    ]
+  }
+}
 
-# resource "aws_iam_user" "user" {
-#   name = "cgi-s3-access-user-${var.environment}"
-#   path = "/system/cgi-s3-access-user/"
-# }
+resource "aws_iam_user" "user" {
+  name = "cgi-s3-access-user-${var.environment}"
+  path = "/system/cgi-s3-access-user/"
+}
 
-# resource "aws_iam_access_key" "user" {
-#   user = aws_iam_user.user.name
-# }
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
+}
 
-# resource "aws_iam_user_policy" "policy" {
-#   name   = "cgi-s3-read-write-policy"
-#   policy = data.aws_iam_policy_document.cgi_s3_access_policy.json
-#   user   = aws_iam_user.user.name
-# }
+resource "aws_iam_user_policy" "policy" {
+  name   = "cgi-s3-read-write-policy"
+  policy = data.aws_iam_policy_document.cgi_s3_access_policy.json
+  user   = aws_iam_user.user.name
+}
 
 
-# resource "kubernetes_secret" "s3_bucket" {
-#   metadata {
-#     name      = "s3-bucket-output"
-#     namespace = var.namespace
-#   }
+resource "kubernetes_secret" "s3_bucket" {
+  metadata {
+    name      = "s3-bucket-output"
+    namespace = var.namespace
+  }
 
-#   data = {
-#     bucket_arn               = module.s3_bucket.bucket_arn
-#     bucket_name              = module.s3_bucket.bucket_name
-#     cgi_s3_access_user_arn   = aws_iam_user.user.arn
-#     cgi_s3_access_key_id     = aws_iam_access_key.user.id
-#     cgi_s3_secret_access_key = aws_iam_access_key.user.secret
-#   }
-# }
+  data = {
+    bucket_arn               = module.s3_bucket.bucket_arn
+    bucket_name              = module.s3_bucket.bucket_name
+    cgi_s3_access_user_arn   = aws_iam_user.user.arn
+    cgi_s3_access_key_id     = aws_iam_access_key.user.id
+    cgi_s3_secret_access_key = aws_iam_access_key.user.secret
+  }
+}
