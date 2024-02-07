9f84366ba9555e121853be9bf9934646ebae5020
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-07 10:24:16 +0000

    comment out resources so they can be recreated in new region

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/api-gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/api-gateway.tf
@@ -1,150 +1,150 @@
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  provider = aws.ireland
-  name                         = var.namespace
-  disable_execute_api_endpoint = false
-  binary_media_types           = ["*/*"]
+# resource "aws_api_gateway_rest_api" "api_gateway" {
+#   provider = aws.ireland
+#   name                         = var.namespace
+#   disable_execute_api_endpoint = false
+#   binary_media_types           = ["*/*"]
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+#   endpoint_configuration {
+#     types = ["REGIONAL"]
+#   }
 
-  tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    environment-name       = var.environment
-    owner                  = var.team_name
-    infrastructure-support = var.infrastructure_support
-    namespace              = var.namespace
-  }
-}
+#   tags = {
+#     business-unit          = var.business_unit
+#     application            = var.application
+#     is-production          = var.is_production
+#     environment-name       = var.environment
+#     owner                  = var.team_name
+#     infrastructure-support = var.infrastructure_support
+#     namespace              = var.namespace
+#   }
+# }
 
-resource "aws_api_gateway_rest_api_policy" "api_policy" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+# resource "aws_api_gateway_rest_api_policy" "api_policy" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "execute-api:Invoke",
-        "Resource": "arn:aws:execute-api:eu-west-1:754256621582:${aws_api_gateway_rest_api.api_gateway.id}/*"
-      },
-      {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:PutObject",
-      "Resource": [
-        "${module.s3_bucket.bucket_arn}/*",
-        "${module.s3_bucket.bucket_arn}"
-      ]
-    }
-    ]
-  }
-  EOF
-}
+#   policy = <<EOF
+#   {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Principal": "*",
+#         "Action": "execute-api:Invoke",
+#         "Resource": "arn:aws:execute-api:eu-west-1:754256621582:${aws_api_gateway_rest_api.api_gateway.id}/*"
+#       },
+#       {
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": "s3:PutObject",
+#       "Resource": [
+#         "${module.s3_bucket.bucket_arn}/*",
+#         "${module.s3_bucket.bucket_arn}"
+#       ]
+#     }
+#     ]
+#   }
+#   EOF
+# }
 
-resource "aws_api_gateway_resource" "proxy" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "{proxy+}"
-}
+# resource "aws_api_gateway_resource" "proxy" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+#   path_part   = "{proxy+}"
+# }
 
-resource "aws_api_gateway_method" "proxy" {
-  provider = aws.ireland
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "PUT"
-  authorization = "NONE"
+# resource "aws_api_gateway_method" "proxy" {
+#   provider = aws.ireland
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id   = aws_api_gateway_resource.proxy.id
+#   http_method   = "PUT"
+#   authorization = "NONE"
 
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
-}
+#   request_parameters = {
+#     "method.request.path.proxy" = true
+#   }
+# }
 
-resource "aws_api_gateway_integration" "proxy_http_proxy" {
-  provider = aws.ireland
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.proxy.http_method
-  type                    = "AWS"
-  integration_http_method = "PUT"
-  uri                     = "arn:aws:apigateway:eu-west-1:s3:path/${module.s3_bucket.bucket_name}/{proxy}"
+# resource "aws_api_gateway_integration" "proxy_http_proxy" {
+#   provider = aws.ireland
+#   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id             = aws_api_gateway_resource.proxy.id
+#   http_method             = aws_api_gateway_method.proxy.http_method
+#   type                    = "AWS"
+#   integration_http_method = "PUT"
+#   uri                     = "arn:aws:apigateway:eu-west-1:s3:path/${module.s3_bucket.bucket_name}/{proxy}"
 
-  credentials = aws_iam_role.api_gateway_role.arn
+#   credentials = aws_iam_role.api_gateway_role.arn
 
-  request_parameters = {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
+#   request_parameters = {
+#     "integration.request.path.proxy" = "method.request.path.proxy"
+#   }
 
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
+#   request_templates = {
+#     "application/json" = "{\"statusCode\": 200}"
+#   }
+# }
 
-resource "aws_api_gateway_method_response" "api_method_response" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
+# resource "aws_api_gateway_method_response" "api_method_response" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id = aws_api_gateway_resource.proxy.id
+#   http_method = aws_api_gateway_method.proxy.http_method
+#   status_code = "200"
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+# }
 
-resource "aws_api_gateway_integration_response" "api_integration_response" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.api_method_response.status_code
-  response_templates = {
-    "application/json" = ""
-  }
-  depends_on = [
-    aws_api_gateway_integration.proxy_http_proxy
-  ]
-}
+# resource "aws_api_gateway_integration_response" "api_integration_response" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   resource_id = aws_api_gateway_resource.proxy.id
+#   http_method = aws_api_gateway_method.proxy.http_method
+#   status_code = aws_api_gateway_method_response.api_method_response.status_code
+#   response_templates = {
+#     "application/json" = ""
+#   }
+#   depends_on = [
+#     aws_api_gateway_integration.proxy_http_proxy
+#   ]
+# }
 
-resource "aws_api_gateway_deployment" "main" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+# resource "aws_api_gateway_deployment" "main" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
-  #hack to force recreate of the deployment resource
-  stage_description = md5(file("api-gateway.tf"))
+#   #hack to force recreate of the deployment resource
+#   stage_description = md5(file("api-gateway.tf"))
 
-  depends_on = [
-    aws_api_gateway_method.proxy,
-    aws_api_gateway_integration.proxy_http_proxy
-  ]
+#   depends_on = [
+#     aws_api_gateway_method.proxy,
+#     aws_api_gateway_integration.proxy_http_proxy
+#   ]
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_api_gateway_stage" "live" {
-  provider = aws.ireland
-  deployment_id = aws_api_gateway_deployment.main.id
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  stage_name    = "upload"
-}
+# resource "aws_api_gateway_stage" "live" {
+#   provider = aws.ireland
+#   deployment_id = aws_api_gateway_deployment.main.id
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+#   stage_name    = "upload"
+# }
 
-resource "aws_api_gateway_method_settings" "all" {
-  provider = aws.ireland
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = aws_api_gateway_stage.live.stage_name
-  method_path = "*/*"
-  settings {
-    metrics_enabled        = true
-    logging_level          = "OFF"
-    throttling_burst_limit = 5000
-    throttling_rate_limit  = 10000
-    data_trace_enabled     = false
-  }
-}
+# resource "aws_api_gateway_method_settings" "all" {
+#   provider = aws.ireland
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+#   stage_name  = aws_api_gateway_stage.live.stage_name
+#   method_path = "*/*"
+#   settings {
+#     metrics_enabled        = true
+#     logging_level          = "OFF"
+#     throttling_burst_limit = 5000
+#     throttling_rate_limit  = 10000
+#     data_trace_enabled     = false
+#   }
+# }
