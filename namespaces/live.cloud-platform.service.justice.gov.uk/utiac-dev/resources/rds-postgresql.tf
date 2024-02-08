a40a50ef0ad8e5c87d78b9872ea16b1dcf40f1f5
sj-williams <steve.williams@digital.justice.gov.uk>
2024-02-08 12:12:14 +0000

    postgres version fixes

--- namespaces/live.cloud-platform.service.justice.gov.uk/utiac-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/utiac-dev/resources/rds-postgresql.tf
@@ -14,7 +14,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "14.7"
+  db_engine_version = "14.10"
   rds_family        = "postgres14"
   db_instance_class = "db.t4g.micro"
 
