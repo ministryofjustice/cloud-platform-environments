d6312470a437c0bdf04cc046221f56828425e645
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-02-08 09:39:08 +0000

    VB-3379, PVB - staging, false after db upgrade

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-production/resources/rds-prison-visits.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-production/resources/rds-prison-visits.tf
@@ -1,32 +1,25 @@
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
-
 module "prison-visits-rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
-
+  source                   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
   vpc_name                 = var.vpc_name
   team_name                = "prison-visits-booking"
-  db_instance_class        = "db.m5.xlarge"
-  db_allocated_storage     = "50"
   business_unit            = "HMPPS"
   application              = "prison-visits-booking-production"
   is_production            = var.is_production
-  namespace                = var.namespace
   environment_name         = "production"
   infrastructure_support   = "pvb-technical-support@digital.justice.gov.uk"
-  db_engine                = "postgres"
-  db_engine_version        = "12.11"
-  db_name                  = "visits"
-  db_parameter             = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
-  rds_family               = "postgres12"
-  db_password_rotated_date = "2023-05-11"
+  namespace                = var.namespace
 
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_minor_version_upgrade  = "false"
+  allow_major_version_upgrade = "false"
+  prepare_for_major_upgrade   = false
+  db_engine                   = "postgres"
+  db_engine_version           = "14.4"
+  rds_family                  = "postgres14"
+  db_instance_class           = "db.m5.xlarge"
+  db_allocated_storage        = "50"
+  db_name                     = "visits"
+  db_parameter                = [{ name = "rds.force_ssl", value = "0", apply_method = "immediate" }]
+  db_password_rotated_date    = "2023-05-11"
+
   performance_insights_enabled = true
 
   providers = {
