0b0f98ed0f3ef37b9b4e780024f2e816cdb2f10c
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-02-08 11:50:30 +0000

    HAAR-2297: merge IRSA into one file

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/irsa.tf
@@ -8,7 +8,7 @@ locals {
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
-module "irsa" {
+module "irsa-authorization" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   eks_cluster_name     = var.eks_cluster_name
@@ -23,6 +23,23 @@ module "irsa" {
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
 }
+
+module "irsa-manage-users" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "hmpps-manage-users"
+  role_policy_arns     = local.sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
 
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   for_each = local.sqs_queues
