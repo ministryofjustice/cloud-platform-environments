d50d3a99112405788ee0efe3b7328854853ac197
JoelEfejuku <114004171+JoelEfejuku@users.noreply.github.com>
2024-02-09 13:59:13 +0000

    Update ecr.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-zap-dast/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-zap-dast/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["example-repository"]
+  github_repositories = laa-zap-dast
 
   # Tags
   business_unit          = var.business_unit
