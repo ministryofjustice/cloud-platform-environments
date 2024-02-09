dd4b1065fcfb11bd1b3726047cd66df3f3cc2ae1
petergphillips <peter@greenthistle.co.uk>
2024-02-09 10:13:14 +0000

    SDIT-1480: 🔧 Rename service account, role and rolebinding for offender-events-preprod (#20750)

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/serviceaccount-circleci.tf
@@ -1,15 +1,10 @@
 module "serviceaccount_circleci" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
-
-  namespace = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
-
-  serviceaccount_token_rotated_date = "01-01-2000"
-
-  serviceaccount_name = "circleci-migrated"
-
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+  namespace                         = var.namespace
+  kubernetes_cluster                = var.kubernetes_cluster
+  serviceaccount_token_rotated_date = "08-02-2024"
+  serviceaccount_name               = "circleci"
+  role_name                         = "circleci"
+  rolebinding_name                  = "circleci"
 }
 
