d31510ec5b890c40829a7e4ea0778fabc0342eca
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-02-12 14:34:45 +0000

    HAAR-2297: add IRSA service accout for hmpps-external-users-api in hmpps-auth-stage

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/irsa.tf
@@ -40,6 +40,22 @@ module "irsa-manage-users" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "irsa-external-users" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "hmpps-external-users-api"
+  role_policy_arns     = local.sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
 
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   for_each = local.sqs_queues
