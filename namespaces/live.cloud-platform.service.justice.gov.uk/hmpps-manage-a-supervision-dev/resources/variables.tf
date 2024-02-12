c4d1cf31f792542623d617f7a2f08aa76e7451a1
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-02-12 17:25:36 +0000

    PI-1875: Added elasticache for manage a supervision dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "Manage a Workforce"
+  default     = "Manage a Supervision"
 }
 
 variable "namespace" {
