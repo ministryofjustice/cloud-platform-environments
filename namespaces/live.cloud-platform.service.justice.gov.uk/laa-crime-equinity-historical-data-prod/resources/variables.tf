08455643ceec09b5594d11ff5ec1ce6edbb729d4
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-02-13 11:03:35 +0000

    EMP-72: force commit to re-run concourse-ci check

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/variables.tf
@@ -43,7 +43,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "laa-eq-data-migration -team@digital.justice.gov.uk"
+  default     = "laa-eq-data-migration-team@digital.justice.gov.uk"
 }
 
 variable "is_production" {
