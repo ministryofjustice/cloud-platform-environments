0826a93895b4cf47fed515c37036d7bdfc35da99
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-02-13 10:23:14 +0000

    DOC-66 add refresh clamav service account

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-dev/resources/serviceaccount-refreshclamav.tf
@@ -0,0 +1,27 @@
+locals {
+  sa_rules = [
+    {
+      api_groups = [
+        "apps",
+        "extensions",
+      ]
+      resources = [
+        "deployment",
+      ]
+      verbs = [
+        "patch",
+        "get",
+      ]
+    },
+  ]
+}
+
+module "serviceaccount" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+  namespace            = var.namespace
+  kubernetes_cluster   = var.kubernetes_cluster
+  serviceaccount_name  = "refreshclamav"
+  role_name            = "refreshclamav"
+  rolebinding_name     = "refreshclamav"
+  serviceaccount_rules = local.sa_rules
+}
