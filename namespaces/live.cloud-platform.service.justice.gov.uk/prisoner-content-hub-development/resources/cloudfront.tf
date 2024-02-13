5ce13de4704e52b86acbbd1fe1bafae369dbe390
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2024-02-13 11:45:08 +0000

    LNP-592: 🔧  enable cloudfront on normal dev s3 bucket. Remove proof of concept specific s3 bucket.

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/cloudfront.tf
@@ -2,8 +2,8 @@ module "cloudfront" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.0.0"
 
   # Configuration
-  bucket_id          = module.drupal_content_storage_cf.bucket_name
-  bucket_domain_name = "${module.drupal_content_storage_cf.bucket_name}.s3.eu-west-2.amazonaws.com"
+  bucket_id          = module.drupal_content_storage_2.bucket_name
+  bucket_domain_name = "${module.drupal_content_storage_2.bucket_name}.s3.eu-west-2.amazonaws.com"
 
   # Tags
   business_unit          = var.business_unit
