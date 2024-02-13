3741bd52d2dd54a8f73727a6243a82de69033650
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-02-13 11:08:20 +0000

    unique variable name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-dev/resources/serviceaccount-refreshclamav.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-dev/resources/serviceaccount-refreshclamav.tf
@@ -1,5 +1,5 @@
 locals {
-  refresh_clam_sa_rules = [
+  refresh_clamav_sa_rules = [
     {
       api_groups = [
         "apps",
@@ -16,12 +16,12 @@ locals {
   ]
 }
 
-module "serviceaccount" {
+module "refresh_clamav_serviceaccount" {
   source               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
   namespace            = var.namespace
   kubernetes_cluster   = var.kubernetes_cluster
   serviceaccount_name  = "refreshclamav"
   role_name            = "refreshclamav"
   rolebinding_name     = "refreshclamav"
-  serviceaccount_rules = local.refresh_clam_sa_rules
+  serviceaccount_rules = local.refresh_clamav_sa_rules
 }
