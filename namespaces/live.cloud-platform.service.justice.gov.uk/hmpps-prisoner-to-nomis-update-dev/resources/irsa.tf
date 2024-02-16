ac92a605813db9a8931146c9dd1b16c8aa2ff2b1
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2024-02-16 07:56:28 +0000

    SDIT-1507 add court sentencing update queues and refactor - dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/irsa.tf
@@ -6,24 +6,24 @@ data "aws_iam_policy_document" "combined_local_sqs" {
     effect  = "Allow"
     actions = ["sqs:*"]
     resources = [
-      module.hmpps_prisoner_to_nomis_adjudication_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_nonassociation_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_location_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_location_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_activity_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_activity_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_appointment_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_appointment_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_incentive_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_incentive_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_sentencing_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_court_sentencing_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_court_sentencing_dead_letter_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_visit_queue.irsa_policy_arn,
-      module.hmpps_prisoner_to_nomis_visit_dead_letter_queue.irsa_policy_arn
+      module.hmpps_prisoner_to_nomis_adjudication_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_adjudication_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_nonassociation_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_nonassociation_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_location_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_location_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_activity_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_activity_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_appointment_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_appointment_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_incentive_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_incentive_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_sentencing_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_sentencing_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_court_sentencing_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_court_sentencing_dead_letter_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_visit_queue.sqs_arn,
+      module.hmpps_prisoner_to_nomis_visit_dead_letter_queue.sqs_arn
     ]
   }
 }
