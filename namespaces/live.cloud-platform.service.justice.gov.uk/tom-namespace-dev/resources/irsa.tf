12344ee9d6b6f3b1cfa727a5b5756fbb7acdf76d
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2024-02-19 11:51:05 +0000

    Add service pod for RDS IRSA test

--- namespaces/live.cloud-platform.service.justice.gov.uk/tom-namespace-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tom-namespace-dev/resources/irsa.tf
@@ -23,3 +23,10 @@ module "irsa" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
+
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
+
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name
+}
