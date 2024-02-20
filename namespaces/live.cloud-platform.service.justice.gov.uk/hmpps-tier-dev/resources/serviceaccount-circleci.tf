c8f48624de2d8ba55b2b55d06d0bb5f0451a8e60
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-02-20 14:22:47 +0000

    PI-1864

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/serviceaccount-circleci.tf
@@ -1,4 +1,4 @@
-module "serviceaccount" {
+module "serviceaccount_circleci" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
   namespace          = var.namespace
