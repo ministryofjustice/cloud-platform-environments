a579473af93c379c3152060fd778dfd7c59ce7bb
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-02-22 10:11:38 +0000

    Re-apply reverted change (#21132)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-claim-non-standard-magistrate-fee-dev/resources/serviceaccount.tf
@@ -8,7 +8,6 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories                  = ["laa-submit-crime-forms"]
-  github_environments                  = ["development"]
   github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
   github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
   github_actions_secret_kube_token     = var.github_actions_secret_kube_token
