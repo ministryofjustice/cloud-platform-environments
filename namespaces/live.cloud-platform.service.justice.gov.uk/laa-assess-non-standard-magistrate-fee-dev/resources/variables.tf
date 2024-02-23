44f1e3b4154c19a72e8fdfe09003f80caac22d89
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-23 12:53:13 +0000

    update application name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-non-standard-magistrate-fee-dev/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "laa-assess-non-standard-magistrate-fee"
+  default     = "laa-assess-crime-forms"
 }
 
 variable "namespace" {
