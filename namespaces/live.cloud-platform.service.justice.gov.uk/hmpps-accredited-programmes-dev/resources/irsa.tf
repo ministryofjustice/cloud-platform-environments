2b46fb31bda1cc3569d95e2d1b4d61a9dc8f64ca
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2024-02-26 10:09:32 +0000

    1350 - Create sqs queue for writing audit messages in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
@@ -1,3 +1,16 @@
+# Add the names of the SQS queues & SNS topics which the app needs permissions to access.
+# The value of each item should be the namespace where the queue or topic was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  # The names of the queues used and the namespace which created them
+  sqs_queues = {
+    "accredited-programmes-dev-hmpps_audit_queue" = "hmpps-audit-dev",
+  }
+
+  # The names of the SNS topics used and the namespace which created them
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+}
+
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
@@ -23,4 +36,9 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
