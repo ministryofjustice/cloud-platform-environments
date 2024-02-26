edf4c817bf27fb1368fbd5cd849ac59fa50a5bcc
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-02-26 12:09:08 +0000

    added audit queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
@@ -1,16 +1,3 @@
-# Add the names of the SQS queues & SNS topics which the app needs permissions to access.
-# The value of each item should be the namespace where the queue or topic was created.
-# This information is used to collect the IAM policies which are used by the IRSA module.
-locals {
-  # The names of the queues used and the namespace which created them
-  sqs_queues = {
-    "accredited-programmes-dev-hmpps_audit_queue" = "hmpps-audit-dev"
-  }
-
-  # The names of the SNS topics used and the namespace which created them
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
-}
-
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
@@ -18,16 +5,14 @@ module "irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = "hmpps-accredited-programmes-service-account"
+  service_account_name = var.application
   namespace            = var.namespace
 
-  # Attach the approprate policies using a key => value map
-  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
-  # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = {
-    rds   = module.rds.irsa_policy_arn
-    redis = module.elasticache_redis.irsa_policy_arn
-  }
+  role_policy_arns = merge(
+    module.rds.irsa_policy_arn,
+    module.elasticache_redis.irsa_policy_arn,
+    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+  )
 
   # Tags
   business_unit          = var.business_unit
@@ -36,9 +21,4 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
-  for_each = local.sqs_queues
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
