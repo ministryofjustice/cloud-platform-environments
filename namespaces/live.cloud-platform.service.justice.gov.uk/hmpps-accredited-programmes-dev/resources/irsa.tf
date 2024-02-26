e16095e2859f07f48736f8d40569df43cea7f4e4
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-02-26 13:41:44 +0000

    added audit queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-dev/resources/irsa.tf
@@ -1,26 +1,44 @@
-module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-
-  # EKS configuration
-  eks_cluster_name = var.eks_cluster_name
-
-  # IRSA configuration
-  service_account_name = "hmpps-accredited-programmes-service-account"
-  namespace            = var.namespace
-
-  # Attach the approprate policies using a key => value map
-  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
-  # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = {
-    rds   = module.rds.irsa_policy_arn
-    redis = module.elasticache_redis.irsa_policy_arn
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
   }
+  sqs_policies = {for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value}
+}
 
-  # Tags
-  business_unit          = var.business_unit
+module "irsa" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
   application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
+  business_unit          = var.business_unit
+  eks_cluster_name       = var.eks_cluster_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  service_account_name = "hmpps-accredited-programmes-service-account"
+
+  role_policy_arns = merge(
+    { rds = module.rds.irsa_policy_arn },
+    { redis = module.elasticache_redis.irsa_policy_arn },
+    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+    local.sqs_policies
+  )
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "irsa-output"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+    rolearn        = module.irsa.role_arn
+  }
 }
