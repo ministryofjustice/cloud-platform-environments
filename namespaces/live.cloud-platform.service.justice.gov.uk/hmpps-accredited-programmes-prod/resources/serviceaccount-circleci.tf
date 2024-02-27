597d0984c90b74131c9015fd9f713cfc1bc92753
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-02-27 14:19:05 +0000

    migrated service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-prod/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-accredited-programmes-prod/resources/serviceaccount-circleci.tf
@@ -4,12 +4,8 @@ module "serviceaccount_circleci" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  serviceaccount_token_rotated_date = "01-01-2000"
+  serviceaccount_token_rotated_date = "27-02-2024"
 
-  serviceaccount_name = "circleci-migrated"
-
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  serviceaccount_name = "circleci"
 }
 
