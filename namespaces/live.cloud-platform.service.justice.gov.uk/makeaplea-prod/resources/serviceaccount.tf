935c484ae0f36be5af49a972067ade52ff054666
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-02-27 09:33:09 +0000

    Make a plea prod: add resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-prod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-prod/resources/serviceaccount.tf
@@ -6,5 +6,67 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["manchester_traffic_offences_pleas", "makeaplea_dx"]
+
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+        "serviceaccounts",
+        "configmaps",
+        "persistentvolumeclaims",
+        "horizontalpodautoscalers",
+        "statefulset"
+      ]
+      verbs = [
+        "update",
+        "patch",
+        "get",
+        "create",
+        "delete",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "batch",
+        "networking.k8s.io",
+        "monitoring.coreos.com",
+        "rbac.authorization.k8s.io",
+        "autoscaling",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "secrets",
+        "cronjobs",
+        "jobs",
+        "replicasets",
+        "statefulsets",
+        "networkpolicies",
+        "servicemonitors",
+        "prometheusrules",
+        "roles",
+        "rolebindings",
+        "horizontalpodautoscalers",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+  ]
 }
