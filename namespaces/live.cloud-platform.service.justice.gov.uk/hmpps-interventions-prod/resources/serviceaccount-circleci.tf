ff44ececb3fb02c7d6903a6ad09dabe939323eef
amol phalnikar <2399527+amolphalnikar@users.noreply.github.com>
2024-02-28 12:31:17 +0000

    Changed service account name to circleci

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-prod/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-prod/resources/serviceaccount-circleci.tf
@@ -4,9 +4,9 @@ module "serviceaccount_circleci" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  serviceaccount_token_rotated_date = "01-01-2000"
+  serviceaccount_token_rotated_date = "28-02-2024"
 
-  serviceaccount_name = "circleci-migrated"
+  serviceaccount_name = "circleci"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
