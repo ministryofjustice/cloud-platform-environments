e70240bfe452c52c39d6deaf6e668bc42a163ed7
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-29 16:25:38 +0000

    Fix Service Account Caseworker Dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-crime-forms-dev/resources/circleci-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-crime-forms-dev/resources/circleci-serviceaccount.tf
@@ -1,6 +1,7 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
+  serviceaccount_name  = "circleci"
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
@@ -8,5 +9,5 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = [laa-assess-crime-forms"]
+  # github_repositories = ["laa-submit-crime-forms"]
 }
