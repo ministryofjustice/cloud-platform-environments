d7665dbc2b68c4bb8f9757ed0209cf0588931cca
Johny Farrar <me@johnyfarrar.co.uk>
2024-02-29 12:09:59 +0000

    Update serviceaccount.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/serviceaccount.tf
@@ -6,7 +6,9 @@ module "serviceaccount" {
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
-  serviceaccount_name = "circleci-migrated"
+  serviceaccount_name               = "circleci"
+  role_name                         = "circleci"
+  rolebinding_name                  = "circleci"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
