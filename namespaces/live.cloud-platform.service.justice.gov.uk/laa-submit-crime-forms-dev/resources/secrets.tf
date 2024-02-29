a9df07b42b70946be52806a4e994d0bd8996a0cd
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-02-29 11:53:50 +0000

    Move secrets to AWS Secret Manager
    Don't need to use member clusters or replication group id

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-dev/resources/secrets.tf
@@ -0,0 +1,29 @@
+module "secrets" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4" # use the latest release
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # Secrets configuration
+  secrets = {
+    "elasticache_primary_endpoint_address" = {
+      description             = "Elasticache primary endpoint address"
+      recovery_window_in_days = 7               
+      k8s_secret_name         = "elasticache_primary_endpoint_address"
+    },
+    "elasticache_auth_token" = {
+      description             = "Elasticache auth token"
+      recovery_window_in_days = 7               
+      k8s_secret_name         = "elasticache_auth_token"
+    }
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
