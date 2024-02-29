b2a23a7a1d65dc7a506198889c0fd83caebad3fc
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-29 08:11:32 +0000

    remove temporary dns entries to complete migration (#21327)

--- namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/iac-fees-prod/resources/route53.tf
@@ -22,24 +22,3 @@ resource "kubernetes_secret" "route53_justice_zone_sec" {
     zone_id = aws_route53_zone.route53_justice_zone.zone_id
   }
 }
-
-resource "aws_route53_record" "aws_route53_record_prod_1" {
-  name    = "immigrationappealsonline.justice.gov.uk."
-  zone_id = aws_route53_zone.route53_justice_zone.zone_id
-  type    = "A"
-  alias {
-    zone_id                = "Z32O12XQLNTSW2"
-    name                   = "iacfees-prod-alb-waf-1928234551.eu-west-1.elb.amazonaws.com."
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "aws_route53_record_cname_1" {
-  name    = "_bbde749af95f9db428c5643e2a4bb710.immigrationappealsonline.justice.gov.uk."
-  zone_id = aws_route53_zone.route53_justice_zone.zone_id
-  type    = "CNAME"
-  ttl     = 60
-  records = [
-    "_aeb80b3f8f3963e3506d87496778dd1f.dlgthlwgnp.acm-validations.aws."
-  ]
-}
