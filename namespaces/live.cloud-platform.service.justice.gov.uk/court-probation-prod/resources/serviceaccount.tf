fa175f02840ac07a1c02a1384ef8b96cb05a0439
Johny Farrar <me@johnyfarrar.co.uk>
2024-02-29 12:52:48 +0000

    PIC-3663 - Update account name and remove old account [prod] (#21351)
    * Update account name and remove old account
    
    * Update serviceaccount.tf
    
    * Update serviceaccount.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/serviceaccount.tf
@@ -6,6 +6,8 @@ module "serviceaccount" {
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
+  serviceaccount_name               = "circleci"
+
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["my-repo"]
