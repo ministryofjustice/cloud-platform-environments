4dbdc10530ccdb64873f234b1dfb9d7b74f6d933
Mateusz Kolakowski <mateusz.kolakowski@justice.gov.uk>
2024-03-01 12:01:57 +0000

    Make a plea dev: update irsa with iam policy with s3 access (#21412)

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/s3.tf
@@ -203,6 +203,26 @@ EOF
   }
 }
 
+resource "aws_iam_policy" "s3_access_policy" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:GetObject*",
+        ]
+        Effect   = "Allow"
+        resources = [
+          module.s3_bucket.bucket_arn,
+          "${module.s3_bucket.bucket_arn}/*"
+        ]
+      },
+    ]
+  })
+}
+
 resource "kubernetes_secret" "s3_bucket" {
   metadata {
     name      = "s3-bucket-output"
