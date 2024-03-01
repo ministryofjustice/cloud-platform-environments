384c6a159731b76e4e96d0b0d8bf3d1611a409e5
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-03-01 16:01:57 +0000

    namespace: test -> preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-preprod/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "data-platform-datahub-catalogue-test"
+  default     = "data-platform-datahub-catalogue-preprod"
 }
 
 variable "business_unit" {
