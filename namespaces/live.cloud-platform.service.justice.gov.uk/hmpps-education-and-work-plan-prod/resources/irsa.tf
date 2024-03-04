65c3936bd2df9f98788c860a8396912b097942fb
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2024-03-04 16:13:45 +0000

    Adds rds policy arn to irsa role

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-prod/resources/irsa.tf
@@ -15,6 +15,10 @@ locals {
 
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+
+  rds_policies = {
+    rds = module.hmpps_education_work_plan_rds.irsa_policy_arn
+  }
 }
 
 module "irsa" {
@@ -23,7 +27,7 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-education-and-work-plan-api"
-  role_policy_arns     = merge(local.sqs_policies, local.sns_policies)
+  role_policy_arns     = merge(local.sqs_policies, local.sns_policies,  local.rds_policies)
 
   # Tags
   business_unit          = var.business_unit
