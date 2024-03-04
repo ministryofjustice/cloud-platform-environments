4be2c76f5c2419ddb4220ef5af696456c67a2f83
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2024-03-04 14:50:36 +0000

    CVL upgrade postgres DB version in test1

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/rds.tf
@@ -8,10 +8,12 @@ module "create_and_vary_a_licence_api_rds" {
   namespace                   = var.namespace
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
-  allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
+  allow_major_version_upgrade = "true"
   db_instance_class           = "db.t4g.small"
-  db_engine_version           = "14"
-  rds_family                  = "postgres14"
+  db_engine_version           = "15.2"
+  rds_family                  = "postgres15"
+  prepare_for_major_upgrade   = true
   db_password_rotated_date    = "14-02-2023"
 
   providers = {
