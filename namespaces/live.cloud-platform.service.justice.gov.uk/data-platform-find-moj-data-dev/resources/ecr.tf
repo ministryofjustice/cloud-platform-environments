47208be506cd4a2ee9663d0e16a74fa1b5a05d1d
Mat Moore <MatMoore@users.noreply.github.com>
2024-03-04 15:49:46 +0000

    Revert rename of find-moj-data repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["data-catalogue"]
+  github_repositories = ["find-moj-data"]
 
   # Tags
   business_unit          = var.business_unit
