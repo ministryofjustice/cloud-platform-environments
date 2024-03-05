5676b0d06435ffd3288211f3775192937cf73988
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-05 13:07:28 +0000

    Update serviceaccount-circleci.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-dev/resources/serviceaccount-circleci.tf
@@ -7,6 +7,8 @@ module "serviceaccount_circleci" {
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "circleci-migrated"
+  role_name = "circle-migrated"
+  rolebinding_name = "circle-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
