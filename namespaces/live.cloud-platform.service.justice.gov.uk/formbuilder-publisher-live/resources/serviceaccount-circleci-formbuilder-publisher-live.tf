cbebb47c3f6b7485f4d1e8541ff4a36f482e8c6e
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-05 15:53:10 +0000

    Update serviceaccount-circleci-formbuilder-publisher-live.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-live/resources/serviceaccount-circleci-formbuilder-publisher-live.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-live/resources/serviceaccount-circleci-formbuilder-publisher-live.tf
@@ -7,6 +7,8 @@ module "serviceaccount_circleci-formbuilder-publisher-live" {
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "circleci-formbuilder-publisher-live-migrated"
+  role_name = "circleci-formbuilder-publisher-live-migrated"
+  rolebinding_name = "circleci-formbuilder-publisher-live-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
