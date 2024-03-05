199ddaf1d9a582b75db1ad39cdce6e75344bc2a6
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-03-05 12:54:32 +0000

    PI-1880: Add audit to MAS

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/hmpps-manage-a-supervision.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/hmpps-manage-a-supervision.tf
@@ -5,9 +5,6 @@ resource "kubernetes_secret" "hmpps_audit_secret" {
   }
 
   data = {
-    sqs_queue_url   = module.hmpps_audit_queue.sqs_id
-    sqs_queue_arn   = module.hmpps_audit_queue.sqs_arn
-    sqs_queue_name  = module.hmpps_audit_queue.sqs_name
-    irsa_policy_arn = module.hmpps_audit_queue.irsa_policy_arn
+    sqs_queue_url = "https://sqs.eu-west-2.amazonaws.com/754256621582/Digital-Prison-Services-${var.environment}-hmpps_audit_queue"
   }
 }
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-a-supervision-dev/resources/irsa.tf
@@ -1,3 +1,14 @@
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-${var.environment}-hmpps_audit_queue" = "hmpps-audit-${var.environment}",
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
 
 module "manage-a-supervision-ui-service-account" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
@@ -13,6 +24,6 @@ module "manage-a-supervision-ui-service-account" {
   service_account_name = "manage-a-supervision-ui"
   role_policy_arns = merge(
     { elasticache = module.elasticache.irsa_policy_arn },
-    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+    local.sqs_policies,
   )
 }
