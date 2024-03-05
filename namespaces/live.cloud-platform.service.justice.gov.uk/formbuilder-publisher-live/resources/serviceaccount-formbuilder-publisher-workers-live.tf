b340d830055ae9c041ab3a5f8d1af4a646189582
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-05 15:53:47 +0000

    Update serviceaccount-formbuilder-publisher-workers-live.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-live/resources/serviceaccount-formbuilder-publisher-workers-live.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-publisher-live/resources/serviceaccount-formbuilder-publisher-workers-live.tf
@@ -7,6 +7,8 @@ module "serviceaccount_formbuilder-publisher-workers-live" {
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "formbuilder-publisher-workers-live-migrated"
+  role_name = "formbuilder-publisher-workers-live-migrated"
+  rolebinding_name = "formbuilder-publisher-workers-live-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
