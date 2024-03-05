449ca2fe4d9c5799867fc86cd633ac85c48e6430
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-05 13:28:52 +0000

    Update serviceaccount-formbuilder-user-datastore-live-dev.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-dev/resources/serviceaccount-formbuilder-user-datastore-live-dev.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-dev/resources/serviceaccount-formbuilder-user-datastore-live-dev.tf
@@ -7,6 +7,8 @@ module "serviceaccount_formbuilder-user-datastore-live-dev" {
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "formbuilder-user-datastore-live-dev-migrated"
+  role_name = "formbuilder-user-datastore-live-dev-migrated"
+  rolebinding_name = "formbuilder-user-datastore-live-dev-migrated"  
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
