fb5124d47a33c7bca47ab521141580bbe9d17bc5
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-05 17:01:54 +0000

    Revert "Add service account rules"
    This reverts commit 65c096b2a289b13f36e29107c7a6dbecbfc44722.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-crime-forms-dev/resources/circleci-serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assess-crime-forms-dev/resources/circleci-serviceaccount.tf
@@ -4,74 +4,10 @@ module "serviceaccount" {
   serviceaccount_name  = "circleci"
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
-  serviceaccount_rules = var.serviceaccount_rules
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["laa-submit-crime-forms"]
-}
-
-variable "serviceaccount_rules" {
-  description = "The capabilities of this serviceaccount"
-
-  type = list(object({
-    api_groups = list(string),
-    resources  = list(string),
-    verbs      = list(string)
-  }))
-
-  # These values are usually sufficient for a CI/CD pipeline
-  default = [
-    {
-      api_groups = [""]
-      resources = [
-        "pods/portforward",
-        "deployment",
-        "secrets",
-        "services",
-        "serviceaccounts",
-        "pods",
-        "pods/exec",
-        "configmaps",
-        "persistentvolumeclaims",
-      ]
-      verbs = [
-        "patch",
-        "get",
-        "create",
-        "update",
-        "delete",
-        "list",
-        "watch",
-        "update",
-      ]
-    },
-    {
-      api_groups = [
-        "extensions",
-        "apps",
-        "networking.k8s.io",
-        "batch",
-      ]
-      resources = [
-        "deployments",
-        "ingresses",
-        "cronjobs",
-        "jobs",
-        "replicasets",
-        "statefulsets",
-      ]
-      verbs = [
-        "get",
-        "update",
-        "delete",
-        "create",
-        "patch",
-        "list",
-        "watch",
-      ]
-    },
-  ]
 }
