f54d5bf5e7c7ccaff16ff7d9a9a18a4e6ab2ee35
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-03-05 09:25:43 +0000

    Upgrade db

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/rds.tf
@@ -4,7 +4,7 @@
 #################################################################################
 
 module "contact-moj_rds" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.2"
   vpc_name                   = var.vpc_name
   team_name                  = var.team_name
   business_unit              = var.business_unit
@@ -16,18 +16,13 @@ module "contact-moj_rds" {
   db_instance_class          = "db.t4g.small"
   db_max_allocated_storage   = "10000"
   db_engine                  = "postgres"
-  db_engine_version          = "12"
+  db_engine_version          = "14.11"
+  rds_family                 = "postgres14"
   db_backup_retention_period = "7"
   db_name                    = "contact_moj_production"
 
-  rds_family = "postgres12"
-
-  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
-  # You will need to specify "pending-reboot" here, as default is set to "immediate".
-
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "true"
+  allow_major_version_upgrade = true
+  prepare_for_major_upgrade   = true
 
   providers = {
     aws = aws.london
