b5e8c96e41a32e1f86d92acab8f0c4ae8156cc63
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-06 08:52:59 +0000

    ci: 🎡 unblock pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/dpr-nomis-port-forwarder/resources/serviceaccount-nomis-port-forwarder.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/dpr-nomis-port-forwarder/resources/serviceaccount-nomis-port-forwarder.tf
@@ -1,12 +1,14 @@
 module "serviceaccount_nomis-port-forwarder" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
-  namespace = var.namespace
+  namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "nomis-port-forwarder-migrated"
+  role_name           = "nomis-port-forwarder-migrated"
+  rolebinding_name    = "nomis-port-forwarder-migrated"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
