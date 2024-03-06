4b7270183927cfbd6d9b6e3c61d4968fd17c8307
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-03-06 16:25:22 +0000

    Upgrade DB

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-staging/resources/rds.tf
@@ -4,7 +4,7 @@
 #################################################################################
 
 module "track_a_query_rds" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.2"
   vpc_name                   = var.vpc_name
   team_name                  = var.team_name
   business_unit              = var.business_unit
@@ -14,21 +14,16 @@ module "track_a_query_rds" {
   db_instance_class          = "db.t4g.micro"
   db_max_allocated_storage   = "500"
   db_engine                  = "postgres"
-  db_engine_version          = "12"
+  rds_family                 = "postgres12"
+  db_engine_version          = "12.18"
   db_backup_retention_period = "7"
   db_name                    = "track_a_query_staging"
   environment_name           = var.environment
   infrastructure_support     = var.infrastructure_support
   enable_rds_auto_start_stop = true
 
-  rds_family = "postgres12"
 
-  # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
-  # You will need to specify "pending-reboot" here, as default is set to "immediate".
-
-
-  # use "allow_major_version_upgrade" when upgrading the major version of an engine
-  allow_major_version_upgrade = "true"
+  prepare_for_major_upgrade = false
 
   providers = {
     aws = aws.london
