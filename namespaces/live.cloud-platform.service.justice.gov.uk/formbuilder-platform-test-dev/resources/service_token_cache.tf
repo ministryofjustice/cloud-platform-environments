4491a9237a53e4d70513ccc6d9aaff341cc55daa
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-03-07 16:50:30 +0000

    This change snuck in from another branch, will be returned on merge

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service_token_cache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-test-dev/resources/service_token_cache.tf
@@ -51,7 +51,7 @@ resource "kubernetes_service_account" "service_token_cache_service_account" {
 
 resource "kubernetes_secret_v1" "service_token_cache_token" {
   metadata {
-    name      = "${local.sa_name}-token"
+    name      = "formbuilder-service-token-cache-test-dev-token"
     namespace = var.namespace
     annotations = {
       "kubernetes.io/service-account.name" = local.sa_name
