ae8ce16c915eab4fd3b54dfebbe651e7a9809f6c
brian.carr <brian.carr@digital.justice.gov.uk>
2024-03-07 13:09:39 +0000

    Setting rolebinding name in cci secret for ims test

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-test/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-test/resources/serviceaccount-circleci.tf
@@ -7,7 +7,8 @@ module "serviceaccount_circleci" {
   serviceaccount_token_rotated_date = "01-01-2000"
 
   serviceaccount_name = "circleci-migrated"
-  role_name = "serviceaccount-cci-role" 
+  role_name = "serviceaccount-cci-role"
+  rolebinding_name = "serviceaccount-cci-rolebinding"  
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
