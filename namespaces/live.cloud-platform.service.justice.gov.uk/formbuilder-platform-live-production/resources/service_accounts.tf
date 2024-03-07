fc31c73fe4c0462b2bf697aee6302caec111c35c
Jesus Laiz <zheileman@users.noreply.github.com>
2024-03-07 15:52:46 +0000

    Add new TF service accounts (live-production)
    Using different names to avoid clash, will get renamed later on once the old SA are deleted.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-platform-live-production/resources/service_accounts.tf
@@ -0,0 +1,154 @@
+locals {
+  sa_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "configmaps",
+        "pods",
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "update",
+        "delete",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "batch",
+        "networking.k8s.io",
+        "policy",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "cronjobs",
+        "jobs",
+        "replicasets",
+        "poddisruptionbudgets",
+        "networkpolicies",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "monitoring.coreos.com",
+      ]
+      resources = [
+        "prometheusrules",
+        "servicemonitors",
+      ]
+      verbs = [
+        "*",
+      ]
+    },
+    {
+      api_groups = [
+        "autoscaling"
+      ]
+      resources = [
+        "hpa",
+        "horizontalpodautoscalers"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch"
+      ]
+    }
+  ]
+}
+
+module "serviceaccount_formbuilder-av-live-production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "07-03-2024"
+
+  serviceaccount_rules = local.sa_rules
+
+  serviceaccount_name = "formbuilder-av-live-production-migrated"
+  role_name           = "formbuilder-av-live-production-migrated"
+  rolebinding_name    = "formbuilder-av-live-production-migrated"
+}
+
+module "serviceaccount_formbuilder-pdf-generator-live-production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "07-03-2024"
+
+  serviceaccount_rules = local.sa_rules
+
+  serviceaccount_name = "formbuilder-pdf-generator-live-production-migrated"
+  role_name           = "formbuilder-pdf-generator-live-production-migrated"
+  rolebinding_name    = "formbuilder-pdf-generator-live-production-migrated"
+}
+
+module "serviceaccount_formbuilder-submitter-workers-live-production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "07-03-2024"
+
+  serviceaccount_rules = local.sa_rules
+
+  serviceaccount_name = "formbuilder-submitter-workers-live-production-migrated"
+  role_name           = "formbuilder-submitter-workers-live-production-migrated"
+  rolebinding_name    = "formbuilder-submitter-workers-live-production-migrated"
+}
+
+module "serviceaccount_formbuilder-user-datastore-live-production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "07-03-2024"
+
+  serviceaccount_rules = local.sa_rules
+
+  serviceaccount_name = "formbuilder-user-datastore-live-production-migrated"
+  role_name           = "formbuilder-user-datastore-live-production-migrated"
+  rolebinding_name    = "formbuilder-user-datastore-live-production-migrated"
+}
+
+module "serviceaccount_formbuilder-user-filestore-live-production" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "07-03-2024"
+
+  serviceaccount_rules = local.sa_rules
+
+  serviceaccount_name = "formbuilder-user-filestore-live-production-migrated"
+  role_name           = "formbuilder-user-filestore-live-production-migrated"
+  rolebinding_name    = "formbuilder-user-filestore-live-production-migrated"
+}
