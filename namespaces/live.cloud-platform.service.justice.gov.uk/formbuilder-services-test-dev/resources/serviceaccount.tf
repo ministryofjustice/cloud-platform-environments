c693a213d49a51ffc8bc6930c03f58e255b0aa53
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-03-08 11:15:16 +0000

    Move the role binding to the target namespace, reference the service account that needs access

--- namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-services-test-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/formbuilder-services-test-dev/resources/serviceaccount.tf
@@ -98,3 +98,20 @@ resource "kubernetes_role" "get_configmaps_role" {
     verbs      = ["get", "list", "watch"]
   }
 }
+
+resource "kubernetes_role_binding" "get-configmaps-rolebinding" {
+  metadata {
+    name      = "serviceaccount_get_configmap_rolebinding"
+    namespace = var.namespace
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "formbuilder-services-test-dev-get-configmaps-role"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "formbuilder-service-token-cache-cross-namespace-test-dev"
+    namespace = "formbuilder-platform-test-dev"
+  }
+}
