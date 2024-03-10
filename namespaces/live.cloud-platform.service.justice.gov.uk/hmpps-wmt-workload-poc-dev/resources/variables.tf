11a881d9f651fc2d5f569269c90e3621fe5af873
Aldo Troiano <151734268+atroiano-ext@users.noreply.github.com>
2024-03-10 16:23:13 +0000

    Adding CP DPS Ingress

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-wmt-workload-poc-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-wmt-workload-poc-dev/resources/variables.tf
@@ -1,3 +1,9 @@
+variable "mp_dps_sg_name" {
+  type        = string
+  description = "Required for MP DPR Traffic ingress into CP DPS"
+  default     = "cloudplatform-mp-dps-sg"
+}
+
 variable "vpc_name" {
   description = "VPC name to create security groups in for the ElastiCache and RDS modules"
   type        = string
