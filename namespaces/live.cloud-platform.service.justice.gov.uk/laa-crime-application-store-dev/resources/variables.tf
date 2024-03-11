aca820567b25370cefddf060aab6cabab2dea2e6
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-11 12:59:10 +0000

    Revert "Change to New Team - App Store Dev"
    This reverts commit 53797e7397927616d9add108e34cd7a680fc2e0b.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-dev/resources/variables.tf
@@ -34,7 +34,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "laa-crime-forms-team"
+  default     = "crm7team"
 }
 
 variable "environment" {
