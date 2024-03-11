9beb0f77b0a6dd8cd18c6f7cc2ffd01806af403e
theDustRoom <paul.massey@digital.justice.gov.uk>
2024-03-11 16:10:07 +0000

    Upgrade to 15.6 before 16.2
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-dev/resources/variables.tf
@@ -62,9 +62,6 @@ variable "github_token" {
 variable "number_cache_clusters" {
   default = "2"
 }
-variable "rds_family" {
-  default = "postgres14"
-}
 
 variable "mp_dps_sg_name" {
   type        = string
