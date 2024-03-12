6831be2fc6bd7e5c94106648bc855b3735ef2f02
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-03-12 16:12:24 +0000

    fix: 🐛 rebase

--- namespaces/live.cloud-platform.service.justice.gov.uk/jaskaran-dev/resources/bad.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jaskaran-dev/resources/bad.tf
@@ -1,8 +1,15 @@
 module "bad_module" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
 
+<<<<<<< HEAD
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
   serviceaccount_name = "bad name"
+=======
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_name = var.application
+>>>>>>> 3a902c0606 (test: 💍 use var)
 }
 
