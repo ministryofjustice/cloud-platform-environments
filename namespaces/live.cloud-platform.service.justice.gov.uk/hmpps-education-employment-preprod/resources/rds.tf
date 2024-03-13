72069ec339283d0af73e027f690aa56124bb2740
theDustRoom <paul.massey@digital.justice.gov.uk>
2024-03-13 10:41:35 +0000

    Disable major version upgrade
    Remove skip namespace
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-employment-preprod/resources/rds.tf
@@ -9,8 +9,8 @@ module "edu_rds" {
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
   rds_family                  = "postgres16"
-  prepare_for_major_upgrade   = true
-  allow_major_version_upgrade = true
+  prepare_for_major_upgrade   = false
+  allow_major_version_upgrade = false
   db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500"
   db_engine_version           = "16.2"
