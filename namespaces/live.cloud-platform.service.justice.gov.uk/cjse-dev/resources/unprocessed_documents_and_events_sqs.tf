9b72956aa25ca25b203e07396ec4e306267d3416
Shahzad Chaudhry <shahzad.chaudhry@digital.justice.gov.uk>
2024-03-14 18:25:50 +0000

    CJS-56 create Unprocessed documents and events SQS queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/cjse-dev/resources/unprocessed_documents_and_events_sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cjse-dev/resources/unprocessed_documents_and_events_sqs.tf
@@ -1,5 +1,5 @@
 module "unprocessed_documents_and_events_sqs_dlq" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=version" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0" # use the latest release
 
   # Queue configuration
   sqs_name        = "unprocessed_documents_and_events_sqs_dlq"
