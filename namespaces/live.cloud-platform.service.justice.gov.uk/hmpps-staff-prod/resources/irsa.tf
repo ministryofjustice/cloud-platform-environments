4d41c4ef627efb12593d93c610444f5f3abc6033
wood-n <natalie.wood@digital.justice.gov.uk>
2024-03-14 12:10:36 +0000

    add service pod to hmpps-staff-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-staff-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-staff-prod/resources/irsa.tf
@@ -23,3 +23,12 @@ module "irsa" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
+
+# set up the service pod
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+}
