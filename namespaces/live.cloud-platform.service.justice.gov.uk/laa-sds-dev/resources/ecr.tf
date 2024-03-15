f7fb5030e8a86823ebbb54aa5c41b3cdfe32586c
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-03-15 15:57:00 +0000

    laa-sds adding env-prefix-to-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-sds-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-sds-dev/resources/ecr.tf
@@ -13,6 +13,7 @@ module "ecr" {
   # OpenID Connect configuration
   oidc_providers      = ["github"]
   github_repositories = ["laa-secure-document-storage-api"]
+  github_actions_prefix = "dev"
 
   # Tags
   business_unit          = var.business_unit
