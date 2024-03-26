1b0e5ace932dae2e805a78b57398749184bc53e8
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-03-26 17:32:21 +0000

    Update cross-iam-role-sa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/cross-iam-role-sa.tf
@@ -1,15 +1,15 @@
 module "cross_irsa" {
-  source                = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  business_unit         = var.business_unit
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  business_unit          = var.business_unit
   application            = var.application
-  eks_cluster_name      = var.eks_cluster_name
-  namespace             = var.namespace
-  service_account_name  = "${var.namespace}-cross-service"
+  eks_cluster_name       = var.eks_cluster_name
+  namespace              = var.namespace
+  service_account_name   = "${var.namespace}-cross-service"
   is_production          = var.is_production
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-  role_policy_arns      = { s3 = aws_iam_policy.intranet_dev_s3_staging.arn }
+  role_policy_arns       = { s3 = aws_iam_policy.intranet_dev_s3_staging.arn }
 }
 
 data "aws_iam_policy_document" "intranet_dev_s3_staging" {
