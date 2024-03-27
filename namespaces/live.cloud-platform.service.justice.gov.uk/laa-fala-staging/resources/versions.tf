a89dd35fa786969d088b0383b19d84670e7cfef5
Stephen Dicks <github@starswan.com>
2024-03-27 07:22:01 +0000

    EL-1400 add pingdom check for FALA staging (#22145)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/versions.tf
@@ -14,5 +14,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
+    pingdom = {
+      source  = "DrFaust92/pingdom"
+      version = "~> 1.3.1"
+    }
   }
 }
