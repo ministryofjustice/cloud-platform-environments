ad9158aee256477c451a2b6e85319c210a1edbf2
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-03-27 19:37:20 +0000

    SDIT-1628: 🔧 Rotate service account secrets for hmpps-restricted-patients-api-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-dev/resources/serviceaccount-circleci.tf
@@ -1,9 +1,9 @@
 module "serviceaccount_circleci" {
-  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.0"
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.0.1"
   namespace                         = var.namespace
   kubernetes_cluster                = var.kubernetes_cluster
-  serviceaccount_token_rotated_date = "08-02-2024"
   serviceaccount_name               = "circleci"
+  serviceaccount_token_rotated_date = "28-03-2024"
   role_name                         = "circleci"
   rolebinding_name                  = "circleci"
 }
