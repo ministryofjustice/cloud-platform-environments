7c9960417bf87b8405b25984809a423230f0a86c
Stephen Dicks <github@starswan.com>
2024-03-28 13:12:45 +0000

    Add pingdom to FALA production (#22217)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-fala-production/resources/pingdom.tf
@@ -0,0 +1,16 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "fala-staging" {
+  type                     = "http"
+  name                     = "fala - production - pingdom"
+  host                     = "find-legal-advice.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_platforms,application_prometheus,component_healthcheck,isproduction_true,environment_production,infrastructuresupport_platforms"
+  probefilters             = "region:EU"
+}
