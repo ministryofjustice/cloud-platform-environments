3de7eae26bbe5798b9e37ed6a85f8ce0562c2268
jackstockley89 <jstock8904@gmail.com>
2024-03-28 14:34:40 +0000

    feat(tf): add s3 policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reports-prod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cloud-platform-reports-prod/resources/s3.tf
@@ -24,3 +24,31 @@ resource "kubernetes_secret" "s3_bucket" {
     bucket_name = module.s3_bucket.bucket_name
   }
 }
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = data.aws_iam_user.manager_concourse.arn
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
+
+data "aws_iam_user" "manager_concourse" {
+  user_name = "manager-concourse"
+}
