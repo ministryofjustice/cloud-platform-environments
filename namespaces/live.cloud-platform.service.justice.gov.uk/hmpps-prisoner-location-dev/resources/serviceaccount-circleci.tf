d11f9b1d6bc03e3b4718a80c0cb3936508b5fd6a
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-02 12:42:35 +0100

    SDIT-1628: 🔧 Rotate circleci serviceaccount token (#22242)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-location-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-location-dev/resources/serviceaccount-circleci.tf
@@ -3,6 +3,6 @@ module "serviceaccount" {
   namespace                         = var.namespace
   kubernetes_cluster                = var.kubernetes_cluster
   serviceaccount_name               = "circleci"
-  serviceaccount_token_rotated_date = "28-03-2024"
+  serviceaccount_token_rotated_date = "02-04-2024"
   role_name                         = "circleci"
 }
