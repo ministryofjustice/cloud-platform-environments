bdf6111a55277b638705715323c95d342c158c40
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-04-03 11:15:52 +0100

    Bump SA to 1.1.0 to abundant-namespace-dev (#22257)

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/serviceaccount.tf
@@ -1,5 +1,5 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=remove-sa-secret-ref"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
   namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
