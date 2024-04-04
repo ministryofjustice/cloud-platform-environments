812d0ac734c6a068cd12c743e681509b699b1317
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2024-04-04 12:24:59 +0100

    Updated permission for s3
    Updated permission for EQ

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/iam.tf
@@ -11,6 +11,13 @@ data "aws_iam_policy_document" "upload_policy" {
   statement {
     actions = [
       "s3:PutObject",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListAllMyBuckets", 
+      "s3:AbortMultipartUpload",
+      "s3:ListBucket",
+      "s3:DeleteObject",
+      "s3:ListMultipartUploadParts",
       "s3:GetBucketLocation"
     ]
     
