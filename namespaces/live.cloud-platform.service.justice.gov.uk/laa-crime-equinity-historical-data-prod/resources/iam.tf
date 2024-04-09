e2636b96ca04f086c15086425346cc9124251e23
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-04-09 10:39:04 +0100

    EMP-198: Add new permission ListObjectsV2 to S3 Bucket in PROD (#22333)
    * EMP-198: Add new permissions to S3 Bucket
    
    EMP-198: Add new permissions to S3 Bucket in order to support data transmission during migration
    
    * EMP-198: Add new permission ListObjectsV2 to S3 Bucket in PROD
    
    EMP-198: Add new permission ListObjectsV2 to S3 Bucket in PROD

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-equinity-historical-data-prod/resources/iam.tf
@@ -11,14 +11,14 @@ data "aws_iam_policy_document" "upload_policy" {
   statement {
     actions = [
       "s3:PutObject",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:ListAllMyBuckets", 
-      "s3:AbortMultipartUpload",
-      "s3:ListBucket",
       "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:ListObjectsV2",
+      "s3:ListAllMyBuckets",
       "s3:ListMultipartUploadParts",
-      "s3:GetBucketLocation"
+      "s3:AbortMultipartUpload"
     ]
     
     resources = [
