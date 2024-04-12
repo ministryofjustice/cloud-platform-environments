bde65717c46c5ce13347ac5417c128c562323b84
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2024-04-12 16:27:04 +0100

    Ims dev s3 sqs (#22395)
    * updats to add new sqs queues and s3 buckets
    
    * adding irsa access
    
    * update to sqs policy name because of clash
    
    ---------
    
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/metadata-status-queue.tf
@@ -0,0 +1,76 @@
+module "metatdata_status_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name                   = "metatdata_status_queue"
+  encrypt_sqs_kms            = "true"
+  message_retention_seconds  = 1209600
+  visibility_timeout_seconds = 120
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.metatdata_status_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+
+EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+module "metatdata_status_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name        = "metatdata_status_dl_queue"
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "metatdata_status_queue" {
+  metadata {
+    name      = "attachment-metadata-extractor-queue-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_id   = module.metatdata_status_queue.sqs_id
+    sqs_arn  = module.metatdata_status_queue.sqs_arn
+    sqs_name = module.metatdata_status_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "metatdata_status_dead_letter_queue" {
+  metadata {
+    name      = "attachment-metadata-extractor-dlq-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_id   = module.metatdata_status_dead_letter_queue.sqs_id
+    sqs_arn  = module.metatdata_status_dead_letter_queue.sqs_arn
+    sqs_name = module.metatdata_status_dead_letter_queue.sqs_name
+  }
+}
