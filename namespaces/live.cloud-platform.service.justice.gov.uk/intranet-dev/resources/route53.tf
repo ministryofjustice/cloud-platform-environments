9164ed8f71e214c9c3137605987d82c501ec2917
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-15 18:09:11 +0100

    intranet-dev - remove hosted zone

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/route53.tf
@@ -1,16 +1,16 @@
-resource "aws_route53_zone" "intranet_route53_zone" {
-  name = var.base_domain
+# resource "aws_route53_zone" "intranet_route53_zone" {
+#   name = var.base_domain
 
-  tags = {
-    business_unit          = var.business_unit
-    application            = var.application
-    is_production          = var.is_production
-    team_name              = var.team_name
-    namespace              = var.namespace
-    environment_name       = var.environment
-    infrastructure_support = var.infrastructure_support
-  }
-}
+#   tags = {
+#     business_unit          = var.business_unit
+#     application            = var.application
+#     is_production          = var.is_production
+#     team_name              = var.team_name
+#     namespace              = var.namespace
+#     environment_name       = var.environment
+#     infrastructure_support = var.infrastructure_support
+#   }
+# }
 
 # CloudFront alias certificate validations
 
@@ -42,14 +42,14 @@ resource "aws_route53_zone" "intranet_route53_zone" {
 #   }
 # }
 
-resource "kubernetes_secret" "route53_zone_sec" {
-  metadata {
-    name      = "route53-zone-output"
-    namespace = var.namespace
-  }
+# resource "kubernetes_secret" "route53_zone_sec" {
+#   metadata {
+#     name      = "route53-zone-output"
+#     namespace = var.namespace
+#   }
 
-  data = {
-    zone_id      = aws_route53_zone.intranet_route53_zone.zone_id
-    name_servers = join("\n", aws_route53_zone.intranet_route53_zone.name_servers)
-  }
-}
+#   data = {
+#     zone_id      = aws_route53_zone.intranet_route53_zone.zone_id
+#     name_servers = join("\n", aws_route53_zone.intranet_route53_zone.name_servers)
+#   }
+# }
