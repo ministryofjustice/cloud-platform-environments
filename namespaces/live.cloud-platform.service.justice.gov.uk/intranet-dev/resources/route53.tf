33a548a1d25106771e42ec47de3d2607081ccf7f
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-15 12:11:30 +0100

    Comment the CloudFont alias code.

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/route53.tf
@@ -12,6 +12,36 @@ resource "aws_route53_zone" "intranet_route53_zone" {
   }
 }
 
+# CloudFront alias certificate validations
+
+# resource "aws_route53_record" "cert-validations" {
+#   count           = length(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options)
+
+#   zone_id         = aws_route53_zone.intranet_route53_zone.zone_id
+
+#   name            = element(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options[*].resource_record_name, count.index)
+#   type            = element(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options[*].resource_record_type, count.index)
+#   records         = [element(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options[*].resource_record_value, count.index)]
+#   ttl             = 60
+#   allow_overwrite = true
+
+#   depends_on      = [aws_acm_certificate.cloudfront_alias_cert]
+# }
+
+# For CloudFront alias A record
+
+# resource "aws_route53_record" "cloudfront_aws_route53_record" {
+#   name    = var.cloudfront_alias
+#   zone_id = aws_route53_zone.intranet_route53_zone.zone_id
+#   type    = "A"
+
+#   alias {
+#     name                   = module.cloudfront.cloudfront_url
+#     zone_id                = module.cloudfront.cloudfront_hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
+
 resource "kubernetes_secret" "route53_zone_sec" {
   metadata {
     name      = "route53-zone-output"
