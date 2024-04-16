3866ec69f735a1c701c37ba29c6e3547799eaa64
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-04-16 17:00:38 +0100

    LASB-3208: Added default service account permissions.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-applications-adaptor-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-applications-adaptor-dev/resources/variables.tf
@@ -146,47 +146,79 @@ variable "serviceaccount_rules" {
       verbs      = ["create"]
     },
     {
-      api_groups = [""]
-      resources = [
-        "pods/portforward",
-        "deployment",
-        "secrets",
-        "services",
-        "configmaps",
-        "pods",
-        "certificates"
-      ]
-      verbs = [
-        "patch",
-        "get",
-        "create",
-        "delete",
-        "list",
-        "watch",
-        "update",
-      ]
-    },
-    {
-      api_groups = [
-        "extensions",
-        "apps",
-        "networking.k8s.io",
-        "cert-manager.io"
-      ]
-      resources = [
-        "deployments",
-        "ingresses",
-        "certificates"
-      ]
-      verbs = [
-        "get",
-        "update",
-        "delete",
-        "create",
-        "patch",
-        "list",
-        "watch",
-      ]
-    },
+    "api_groups": [""],
+    "resources": [
+      "pods/portforward",
+      "deployment",
+      "secrets",
+      "services",
+      "configmaps",
+      "pods"
+    ],
+    "verbs": [
+      "patch",
+      "get",
+      "create",
+      "update",
+      "delete",
+      "list",
+      "watch"
+    ]
+  },
+  {
+    "api_groups": [
+      "extensions",
+      "apps",
+      "batch",
+      "networking.k8s.io",
+      "policy"
+    ],
+    "resources": [
+      "deployments",
+      "ingresses",
+      "cronjobs",
+      "jobs",
+      "replicasets",
+      "poddisruptionbudgets",
+      "networkpolicies"
+    ],
+    "verbs": [
+      "get",
+      "update",
+      "delete",
+      "create",
+      "patch",
+      "list",
+      "watch"
+    ]
+  },
+  {
+    "api_groups": [
+      "monitoring.coreos.com"
+    ],
+    "resources": [
+      "prometheusrules",
+      "servicemonitors"
+    ],
+    "verbs": [
+      "*"
+    ]
+  },
+  {
+    "api_groups": [
+      "autoscaling"
+    ],
+    "resources": [
+      "hpa",
+      "horizontalpodautoscalers"
+    ],
+    "verbs": [
+      "get",
+      "update",
+      "delete",
+      "create",
+      "patch"
+    ]
+  }
   ]
 }
