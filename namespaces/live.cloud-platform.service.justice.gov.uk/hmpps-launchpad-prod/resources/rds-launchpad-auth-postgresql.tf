7d3b0ad4d8704a4144205e48178bc10de637f68e
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2024-04-16 14:24:22 +0100

    setting db_allocated_storage and backup_window value for rds config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-prod/resources/rds-launchpad-auth-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-prod/resources/rds-launchpad-auth-postgresql.tf
@@ -14,7 +14,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = true
-  # db_max_allocated_storage     = "500"
+  db_allocated_storage = "200"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
@@ -22,6 +22,7 @@ module "rds" {
   db_engine         = "postgres"
   db_engine_version = "16.2"
   rds_family        = "postgres16"
+  backup_window = "02:00-03:00"
   db_instance_class = "db.t4g.large"
 
   # Tags
