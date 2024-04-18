202798027b4b6a624b677368a3c2fe2b821a67d8
vloruncg <victor.olorunleye@digital.justice.gov.uk>
2024-04-18 15:23:20 +0100

    Update irsa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-dev/resources/irsa.tf
@@ -5,7 +5,7 @@ module "irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name  = "${var.team_name}-${var.environment}"
+  service_account  = "${var.team_name}-${var.environment}"
   namespace            = var.namespace # this is also used as a tag
 
   # Attach the approprate policies using a key => value map
