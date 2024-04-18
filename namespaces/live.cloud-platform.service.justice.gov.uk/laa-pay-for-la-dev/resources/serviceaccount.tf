b866cd67688b76693764bfa42acde43a8b07d202
Luke J <134690131+Lukehub1@users.noreply.github.com>
2024-04-18 16:52:43 +0100

    Adding env

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-pay-for-la-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-pay-for-la-dev/resources/serviceaccount.tf
@@ -7,4 +7,6 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["payforlegalaid"]
+  github_environments = ["dev"]
+
 }
