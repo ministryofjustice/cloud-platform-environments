ee3e8edc4b8f8de467c546d4be02df01d4fa5c22
Mike Bell <mike.bell1@digital.justice.gov.uk>
2024-04-19 13:32:29 +0100

    Update default team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/mike-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mike-test/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "mike-test"
+  default     = "mikebell-test-admin-team"
 }
 
 variable "environment" {
