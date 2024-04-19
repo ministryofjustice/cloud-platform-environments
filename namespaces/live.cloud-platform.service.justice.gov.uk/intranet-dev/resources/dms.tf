163cd10f15f173fe79663cdc7c618ed212956414
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-19 17:34:00 +0100

    intranet-dev: add secret to fix broken build

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/dms.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/dms.tf
@@ -24,44 +24,58 @@ resource "random_id" "id" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dms_endpoint
 
-# resource "aws_dms_endpoint" "source" {
-#   endpoint_id                 = "${var.team_name}-src-${random_id.id.hex}"
-#   endpoint_type               = "source"
-#   engine_name                 = data.kubernetes_secret.dms_secret.data.src_engine
-#   extra_connection_attributes = ""
-#   server_name                 = data.kubernetes_secret.dms_secret.data.src_addr
-#   database_name               = data.kubernetes_secret.dms_secret.data.src_database
-#   username                    = data.kubernetes_secret.dms_secret.data.src_user
-#   password                    = data.kubernetes_secret.dms_secret.data.src_pass
-#   port                        = data.kubernetes_secret.dms_secret.data.src_port
-#   ssl_mode                    = data.kubernetes_secret.dms_secret.data.src_ssl
+resource "aws_dms_endpoint" "source" {
+  endpoint_id                 = "${var.team_name}-src-${random_id.id.hex}"
+  endpoint_type               = "source"
+  engine_name                 = data.kubernetes_secret.dms_secret.data.src_engine
+  extra_connection_attributes = ""
+  server_name                 = data.kubernetes_secret.dms_secret.data.src_addr
+  database_name               = data.kubernetes_secret.dms_secret.data.src_database
+  username                    = data.kubernetes_secret.dms_secret.data.src_user
+  password                    = data.kubernetes_secret.dms_secret.data.src_pass
+  port                        = data.kubernetes_secret.dms_secret.data.src_port
+  ssl_mode                    = data.kubernetes_secret.dms_secret.data.src_ssl
 
-#   tags = {
-#     Name        = "${var.team_name} Source Endpoint"
-#     Description = "Managed by Terraform"
-#     Application = var.application
-#     Owner       = var.team_name
-#     Env         = var.environment
-#   }
-# }
+  tags = {
+    Name        = "${var.team_name} Source Endpoint"
+    Description = "Managed by Terraform"
+    Application = var.application
+    Owner       = var.team_name
+    Env         = var.environment
+  }
+}
 
-# resource "aws_dms_endpoint" "target" {
-#   endpoint_id                 = "${var.team_name}-dst-${random_id.id.hex}"
-#   endpoint_type               = "target"
-#   engine_name                 = data.kubernetes_secret.dms_secret.data.dst_engine
-#   extra_connection_attributes = ""
-#   server_name                 = data.kubernetes_secret.dms_secret.data.dst_addr
-#   database_name               = data.kubernetes_secret.dms_secret.data.dst_database
-#   username                    = data.kubernetes_secret.dms_secret.data.dst_user
-#   password                    = data.kubernetes_secret.dms_secret.data.dst_pass
-#   port                        = data.kubernetes_secret.dms_secret.data.dst_port
-#   ssl_mode                    = data.kubernetes_secret.dms_secret.data.dst_ssl
+resource "aws_dms_endpoint" "target" {
+  endpoint_id                 = "${var.team_name}-dst-${random_id.id.hex}"
+  endpoint_type               = "target"
+  engine_name                 = data.kubernetes_secret.dms_secret.data.dst_engine
+  extra_connection_attributes = ""
+  server_name                 = data.kubernetes_secret.dms_secret.data.dst_addr
+  database_name               = data.kubernetes_secret.dms_secret.data.dst_database
+  username                    = data.kubernetes_secret.dms_secret.data.dst_user
+  password                    = data.kubernetes_secret.dms_secret.data.dst_pass
+  port                        = data.kubernetes_secret.dms_secret.data.dst_port
+  ssl_mode                    = data.kubernetes_secret.dms_secret.data.dst_ssl
 
-#   tags = {
-#     Name        = "${var.team_name} Destination Endpoint"
-#     Description = "Managed by Terraform"
-#     Application = var.application
-#     Owner       = var.team_name
-#     Env         = var.environment
-#   }
-# }
+  tags = {
+    Name        = "${var.team_name} Destination Endpoint"
+    Description = "Managed by Terraform"
+    Application = var.application
+    Owner       = var.team_name
+    Env         = var.environment
+  }
+}
+
+resource "kubernetes_secret" "dms_instance" {
+  metadata {
+    name      = "dms-instance"
+    namespace = var.namespace
+  }
+
+  data = {
+    replication_instance_arn = module.test_dms.replication_instance_arn
+    source                   = aws_dms_endpoint.source.endpoint_arn
+    destination              = aws_dms_endpoint.target.endpoint_arn
+    task                     = aws_dms_replication_task.replication_task.replication_task_arn
+  }
+}
