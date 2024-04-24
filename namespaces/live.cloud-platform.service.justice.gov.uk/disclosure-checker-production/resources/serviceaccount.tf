062762755d1134529f0ad698797471bc1b89be83
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-24 09:58:56 +0100

    Set secrets for environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-production/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-production/resources/serviceaccount.tf
@@ -5,12 +5,5 @@ module "serviceaccount" {
   kubernetes_cluster = var.kubernetes_cluster
 
   github_repositories = [var.repo_name]
-
-  # This module is also used in the staging namespace, so we must change the
-  # secret names here in order to not overwrite the existing ones.
-
-  github_actions_secret_kube_cert      = "KUBE_PROD_CERT"
-  github_actions_secret_kube_token     = "KUBE_PROD_TOKEN"
-  github_actions_secret_kube_cluster   = "KUBE_PROD_CLUSTER"
-  github_actions_secret_kube_namespace = "KUBE_PROD_NAMESPACE"
+  github_environments = ["production"]
 }
