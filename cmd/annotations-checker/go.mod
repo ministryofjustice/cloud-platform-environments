7d1f2ca33c0ebcc75105d97c9e6a5130cb073b05
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-04-26 17:10:14 +0100

    will adjust the org name

---
+++ cmd/annotations-checker/go.mod
@@ -0,0 +1,14 @@
+module github.com/ministryofjustice/cloud-platform-environments/cmd/annotations-checker
+
+go 1.22.1
+
+require (
+	github.com/google/go-github/v32 v32.1.0
+	golang.org/x/oauth2 v0.19.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
--- cmd/annotations-checker/validate-annotations.go
+++ cmd/annotations-checker/validate-annotations.go
@@ -3,17 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
-	"log" // logging library for error handling
 	"os"
-	"path/filepath" // for manipulating file paths and walking directories
+	"path/filepath"
 
 	"github.com/google/go-github/v32/github" // GitHub API client library
-	"golang.org/x/oauth2"                    // API requests authentication
+	// "github.com/ministryofjustice/cloud-platform-environments/pkg/namespace"
+	"golang.org/x/oauth2" // API requests authentication
+	// "google.golang.org/genproto/googleapis/api/annotations"
 	"gopkg.in/yaml.v2"
 )
 
 // Annotations structs to define the structure of annotations in the YAML file
 type Annotations struct {
+	Kind           string `yaml:"kind"`
 	SourceCodeRepo string `yaml:"cloud-platform.justice.gov.uk/source-code"`
 	TeamName       string `yaml:"cloud-platform.justice.gov.uk/team-name"`
 }
@@ -25,117 +27,93 @@ type KubernetesConfig struct {
 	} `yaml:"metadata"`
 }
 
-func main() {
-	// retrieve the GitHub token
-	githubToken := getEnv("GITHUB_TOKEN")
-	org := "https://github.com/ministryofjustice/cloud-platform-environments/"
-
-	// write the validation results to a file
-	outputFile := createFile("validation_results.txt")
-	defer outputFile.Close()
-
-	// setup the GitHub client
-	client := setupGitHubClient(githubToken)
-
-	// filter files in the namespace directory
-	processYAMLFiles(client, org, outputFile)
-}
-
-// retrieve an environment variables or throw an error if unfound
-func getEnv(key string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		log.Fatalf("%s environment variable is not set", key)
-	}
-	return value
-}
-
-// create aand write to a file, but throw an error if this fails
-func createFile(filename string) *os.File {
-	file, err := os.Create(filename)
-	if err != nil {
-		log.Fatalf("Failed to create an output file: %v.", err)
-	}
-	return file
-}
-
-// initialize a new gitHub client using the 0auth token
-func setupGitHubClient(token string) *github.Client {
+// gitHub client
+func NewGitHubClient(token string) *github.Client {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
-
 	return client
 }
 
-// process YAML files
-func processYAMLFiles(client *github.Client, org string, outputFile *os.File) {
-	err := filepath.Walk("namespaces", func(path string, info os.FileInfo, err error) error {
-		// error handling during Walking
-		if err != nil {
-			fmt.Fprintf(outputFile, "Error walking through the namespaces directory: %v\n", err)
-			return err
-		}
-
-		// skip anything that has nothing to do with YAML files
-		if info.IsDir() || filepath.Ext(path) != ".yaml" {
-			return nil
-		}
-		// read and parse the YAML file
-		configData, err := os.ReadFile(path)
-		if err != nil {
-			fmt.Fprintf(outputFile, "Error reading the config file %s: %v\n", path, err)
-			return nil
-		}
-		var config KubernetesConfig
-		err = yaml.Unmarshal(configData, &config)
-		if err != nil {
-			fmt.Fprintf(outputFile, "Error parsing config file %s: %v\n", path, err)
-			return nil
-		}
-		// validate annotations
-		validateAnnotations(client, org, path, config.Metadata.Annotations, outputFile)
-		return nil
-	})
-
+// validate team name and source code repo
+func ValidateAnnotations(client *github.Client, org string) (string, error) {
+	// get namespace directory
+	namespacesDir := "/path/to/namespaces"
+	files, err := os.ReadDir(namespacesDir)
 	if err != nil {
-		fmt.Fprintf(outputFile, "Failed to walk through the namespaces directory: %v\n", err)
+		return "", fmt.Errorf("Failed to read namespaces directory: %v", err)
 	}
 
-	// check if any validation message was written to the file
-	checkValidationResults(outputFile)
+	// check if kind:Namespace exists in any file
+	foundNamespace := false
+	for _, file := range files {
+		if !file.IsDir() {
+			path := filepath.Join(namespacesDir, file.Name())
+			annotations, err := getAnnotations(path)
+			if err != nil {
+				return "", fmt.Errorf("Failed to get annotation from %s: %v", path, err)
+			}
+
+			if annotations.Kind == "Namespace" {
+				foundNamespace = true
+				passed, message := validateFile(client, org, path, annotations)
+				if passed {
+					return message, nil
+				} else {
+					return "", fmt.Errorf(message)
+				}
+			}
+		}
+	}
+
+	// if kind:Namespace isn't found, do nothing
+	if !foundNamespace {
+		return "", nil
+	}
+
+	return "No files with 'kind:Namespace' annotation found. Check Passed.", nil
 }
 
-// validate team name and source code repo
-func validateAnnotations(client *github.Client, org, path string, annotations Annotations, outputFile *os.File) {
+// retrieve annotations from yaml file
+func getAnnotations(path string) (Annotations, error) {
+	// read yaml file
+	yamlFile, err := os.ReadFile(path)
+	if err != nil {
+		return Annotations{}, err
+	}
+
+	// parse yaml
+	var config KubernetesConfig
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		return Annotations{}, err
+	}
+
+	return config.Metadata.Annotations, nil
+}
+
+// validate yaml file
+func validateFile(client *github.Client, org, path string, annotations Annotations) (bool, string) {
 	repo := annotations.SourceCodeRepo
 	team := annotations.TeamName
 
-	// validate the github repo
-	_, _, err := client.Repositories.Get(context.Background(), org, repo)
+	// validate the repo
+	repository, _, err := client.Repositories.Get(context.Background(), org, repo)
 	if err != nil {
-		fmt.Fprintf(outputFile, "The repository either does not exist or has been set to private in %s: %v\n", org, repo, path, err)
+		return false, fmt.Sprintf("The repository, %s, does not exist.", repo)
 	}
 
-	// validate the github team
+	if repository.GetPrivate() {
+		return false, fmt.Sprintf("The repository %s is private.", repo)
+	}
+
+	// validate team
 	_, _, err = client.Teams.GetTeamBySlug(context.Background(), org, team)
 	if err != nil {
-		fmt.Fprintf(outputFile, "Team validation failed as the team could not be found in %s: %v\n", team, org, path, err)
-	}
-}
-
-// write a success message to the file if no validation messages were written to it
-func checkValidationResults(outputFile *os.File) {
-	// retrieve the file's info to check its size
-	info, err := outputFile.Stat()
-	if err != nil {
-		log.Printf("Could not stat the file: %v", err)
-		return
+		return false, fmt.Sprintf("The team %s does not exist or the PR owner isn't a member.", team)
 	}
 
-	// if the file size was 0, it would mean err == nil
-	if info.Size() == 0 {
-		fmt.Fprintf(outputFile, "All annotation validations were successful.")
-	}
+	// if everything checks out, return success message
+	return true, fmt.Sprintf("Annotations successfully validated for %s", path)
 }
