8a2088aabd352c6664da2af92febcf33ef6c4c30
Mark Butler <mgb777@outlook.com>
2024-04-26 15:51:52 +0100

    Add descriptions to outputs

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/outputs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/outputs.tf
@@ -1,7 +1,9 @@
 output "irsa_policy_arn" {
   value = module.makeaplea_queue.irsa_policy_arn
+  description = "IRSA Policy arn"
 }
 
 output "sqs_id_arn" {
   value = module.makeaplea_queue.assume_role_policy.arn
+  description = "sqs assume role policy arn"
 }
