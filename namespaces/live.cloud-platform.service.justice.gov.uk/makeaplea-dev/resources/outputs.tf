71dd637d760de05bcde0e3f43f896d33951705a6
Mark Butler <mgb777@outlook.com>
2024-04-26 15:54:40 +0100

    Remove outputs

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "irsa_policy_arn" {
-  value = module.makeaplea_queue.irsa_policy_arn
-  description = "IRSA Policy arn"
-}
-
-output "sqs_id_arn" {
-  value = module.makeaplea_queue.assume_role_policy.arn
-  description = "sqs assume role policy arn"
-}
