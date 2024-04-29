16ea6d4e1930d61f5597783fa1c688a1fed3641c
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2024-04-29 16:38:54 +0100

    Added AWS resources for new IMS staging env (#22733)
    * Added AWS resources for new IMS staging env
    
    * update to variable.tf to add missing
    
    * adding rds aurora legacy db
    
    * setting RDS aurora instance to latest minor version of 15.x
    
    ---------
    
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-stage/resources/iam.tf
@@ -0,0 +1,74 @@
+resource "aws_iam_user" "ims_extractor_user" {
+  name = "hmpps-manage-intelligence-stage-extractor-user"
+  path = "/system/hmpps-manage-intelligence-stage-users/"
+}
+
+resource "aws_iam_access_key" "key_2023" {
+  user = aws_iam_user.ims_extractor_user.name
+}
+
+data "aws_iam_policy_document" "ims_legacy_extractor_policy" {
+  statement {
+    actions = ["s3:PutObject", "s3:ListBucket", "s3:GetObject", "s3:DeleteObject"]
+
+    resources = [
+      module.ims_images_storage_bucket.bucket_arn,
+      "${module.ims_images_storage_bucket.bucket_arn}/*",
+      module.ims_attachments_storage_bucket.bucket_arn,
+      "${module.ims_attachments_storage_bucket.bucket_arn}/*"
+    ]
+  }
+
+  statement {
+    actions = ["s3:PutObject", "sqs:SendMessage", "sqs:GetQueueUrl"]
+
+    resources = [
+      module.manage_intelligence_extractor_bucket.bucket_arn,
+      "${module.manage_intelligence_extractor_bucket.bucket_arn}/*",
+      module.ims_extractor_queue.sqs_arn,
+      module.ims_extractor_dead_letter_queue.sqs_arn,
+      module.attachment_metadata_extractor_queue.sqs_arn,
+      module.attachment_metadata_extractor_dead_letter_queue.sqs_arn
+    ]
+  }
+
+  statement {
+    effect = "Deny"
+
+    actions = ["s3:*"]
+
+    resources = [
+      module.manage_intelligence_extractor_bucket.bucket_arn,
+      "${module.manage_intelligence_extractor_bucket.bucket_arn}/*",
+      module.ims_images_storage_bucket.bucket_arn,
+      "${module.ims_images_storage_bucket.bucket_arn}/*",
+      module.ims_attachments_storage_bucket.bucket_arn,
+      "${module.ims_attachments_storage_bucket.bucket_arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_iam_user_policy" "ims_extractor_policy" {
+  name   = "hmpps-manage-intelligence-legacy-extractor-policy-stage"
+  policy = data.aws_iam_policy_document.ims_legacy_extractor_policy.json
+  user   = aws_iam_user.ims_extractor_user.name
+}
+
+resource "kubernetes_secret" "ims_legacy_extractor_user" {
+  metadata {
+    name      = "ims-extractor-user-stage"
+    namespace = var.namespace
+  }
+
+  data = {
+    arn               = aws_iam_user.ims_extractor_user.arn
+    access_key_id     = aws_iam_access_key.key_2023.id
+    secret_access_key = aws_iam_access_key.key_2023.secret
+  }
+}
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-stage/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-stage/resources/versions.tf
@@ -13,5 +13,13 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
   }
 }
