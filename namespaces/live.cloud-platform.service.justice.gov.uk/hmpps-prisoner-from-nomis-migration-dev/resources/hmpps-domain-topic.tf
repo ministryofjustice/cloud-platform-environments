10d09885dc7b6cecc2ebeab9541fd42fd4f50689
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-04-30 12:36:30 +0100

    add subscription to hmpps-domain-events-topic for prisoner_from_nomis - dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-domain-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-domain-topic.tf
@@ -1,3 +1,3 @@
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
-  name = "/hmpps-domain-events-${var.environment}/topic-arn"
+  name = "/hmpps-domain-events-${var.environment_name}/topic-arn"
 }
