55deb995418088862aa6a3ac98af4dd41ae8e57c
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-04-30 16:23:44 +0100

    HIA-682
    Fix for failing build

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/iam.tf
@@ -174,9 +174,3 @@ resource "aws_iam_role_policy" "api_gateway_sqs_policy" {
     ]
   })
 }
-
-
-resource "aws_iam_role_policy_attachment" "api_gateway_sqs_policy_attachment" {
-  role       = aws_iam_role.api_gateway_sqs_role.name
-  policy_arn = aws_iam_policy.api_gateway_sqs_policy.arn
-}
