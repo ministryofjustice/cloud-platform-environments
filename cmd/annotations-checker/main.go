439a716c83ea866e6784ea100d077453cb0f048f
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-05-01 13:09:23 +0100

    add pagination and validate only newly added/modified files

--- cmd/annotations-checker/main.go
+++ cmd/annotations-checker/main.go
@@ -6,23 +6,25 @@ import (
 )
 
 func main() {
-	// retrieve the GitHub token
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
 		fmt.Println("GITHUB_TOKEN env var not set")
 		return
 	}
 
-	// set org
-	org := "https://github.com/ministryofjustice/cloud-platform-environments/"
+	org := "https://github.com/ministryofjustice/"
 
-	// create GitHub client
 	client := NewGitHubClient(token)
 
-	// validate annotations
-	message, err := ValidateAnnotations(client, org)
+	prEvent := os.Getenv("PR_EVENT_DATA")
+	if prEvent == "" {
+		fmt.Println("PR_EVENT_DATA environment variable is not set")
+		return
+	}
+
+	message, err := ValidateAnnotations(client, org, prEvent)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error during validation %v\n", err)
 		return
 	}
 
