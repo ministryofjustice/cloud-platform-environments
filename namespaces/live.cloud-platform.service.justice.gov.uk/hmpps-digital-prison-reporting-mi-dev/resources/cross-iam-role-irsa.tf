4e1889ef272f3db55912808d57b48291622712c3
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-05-02 09:20:59 +0100

    Add missing TAGS

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/cross-iam-role-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/cross-iam-role-irsa.tf
@@ -3,7 +3,7 @@ module "irsa" {
   eks_cluster_name     = "var.eks_cluster_name"
   service_account_name = "dpr-reporting-mi-${var.environment}-cross-iam"
   namespace            = var.namespace
-  role_policy_arns     = [aws_iam_policy.cross_iam_role_mp.arn]
+  role_policy_arns     = [aws_iam_policy.cross_iam_policy_mp.arn]
 
   # Tags
   business_unit          = var.business_unit
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "cross_iam_policy_mp" {
   }
 }
 
-resource "aws_iam_policy" "cross_iam_role_mp" {
+resource "aws_iam_policy" "cross_iam_policy_mp" {
   name   = "${var.namespace}-cross-iam-role-mp"
   policy = data.aws_iam_policy_document.cross_iam_policy_mp.json
 
