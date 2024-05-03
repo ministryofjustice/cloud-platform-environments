5dc3b99cb8de824c2adabfee43782f94959a2004
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-05-03 14:22:30 +0100

    NIT-1241 adding lifecycle config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/s3.tf
@@ -23,9 +23,12 @@ module "s3_bucket" {
           storage_class = "GLACIER"
         },
       ]
-      noncurrent_version_expiration = {
-        days = var.s3_lifecycle_config["noncurrent_version_expiration_days"]
-      }
+
+      noncurrent_version_expiration = [
+        {
+          days = var.s3_lifecycle_config["noncurrent_version_expiration_days"]
+        },
+      ]
     }
   ]
 }
