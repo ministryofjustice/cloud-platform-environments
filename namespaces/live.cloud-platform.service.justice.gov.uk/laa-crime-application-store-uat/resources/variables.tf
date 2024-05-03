8b11aeea36d40dcaaab09fa6980269107fc919b5
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-05-03 14:25:12 +0100

    Fix team name in UAT

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "non-standard-crime-claims"
+  default     = "laa-crime-forms-team"
 }
 
 variable "environment" {
