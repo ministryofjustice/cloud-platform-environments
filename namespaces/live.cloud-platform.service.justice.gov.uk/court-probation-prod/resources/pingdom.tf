6b37a6d69ef80860c23c68e328eb441a0b0db5df
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-05-07 15:22:33 +0100

    Add pingdom to crime-portal-mirror-gateway production
    Add pingdomm to crime-portal-mirror-gateway production

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/pingdom.tf
@@ -17,3 +17,17 @@ resource "pingdom_check" "prepare-a-case-production-check" {
   integrationids           = [110432]
 }
 
+resource "pingdom_check" "crime-portal-gateway-check" {
+  type                     = "http"
+  name                     = var.application
+  host                     = var.crime-portal-mirror-gateway-domain
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "hmpps, probation-in-court, crime-portal-gateway, cloudplatform-managed"
+  probefilters             = "region:EU"
+}
