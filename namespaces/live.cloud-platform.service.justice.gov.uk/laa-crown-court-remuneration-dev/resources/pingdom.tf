767d467120b4d8b91d8ff591c9e62a3a45154616
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-05-07 13:11:59 +0100

    Add pingdom to versions.tf and create a pingdom file

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-dev/resources/pingdom.tf
@@ -0,0 +1,17 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-crown-court-remuneration-dev" {
+  type                     = "http"
+  name                     = "cica - dev - cloud-platform - claim"
+  host                     = "dev.laa-crown-court-remuneration-dev.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_platforms,application_prometheus,component_healthcheck,isproduction_true,environment_uat,infrastructuresupport_platforms"
+  probefilters             = "region:EU"
+}
