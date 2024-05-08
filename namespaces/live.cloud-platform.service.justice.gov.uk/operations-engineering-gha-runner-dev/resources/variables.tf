961f2927505791e4f7fb9fff088b5a99e939ebb3
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-05-08 11:42:52 +0100

    change namespace name and add rbac

--- namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-gha-runner-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-gha-runner-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "operations-engineering-test-gha-runner"
+  default     = "operations-engineering-gha-runner-dev"
 }
 
 variable "business_unit" {
