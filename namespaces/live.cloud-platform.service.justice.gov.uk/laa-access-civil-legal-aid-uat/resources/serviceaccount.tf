cb817d2f4346fee6a562de9ce44c186580b419e5
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-05-13 15:56:41 +0100

    Remove service account from laa-access-civil-legal-aid-uat

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-uat/resources/serviceaccount.tf
+++
@@ -1,9 +0,0 @@
-module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
-
-  namespace = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
-
-  # Creates Kubernetes' GitHub actions secrets in the repository
-  github_repositories = ["laa-access-civil-legal-aid"]
-}
