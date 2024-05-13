653d282525826f970a48fcef47e07f7e2af1c2a3
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-05-13 10:58:28 +0100

    Removed serviceaccount_token_rotated_date

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/serviceaccount.tf
@@ -4,9 +4,6 @@ module "serviceaccount" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  serviceaccount_token_rotated_date = "01-01-2000"
-
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  # Creates Kubernetes' GitHub actions secrets in the repository
   github_repositories = ["laa-access-civil-legal-aid"]
 }
