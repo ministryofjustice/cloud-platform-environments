2529f059bca33fff6a41deeed2576e7ba71fd657
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-05-15 09:55:16 +0100

    Add Service POD

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/cross-iam-role-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-prison-reporting-mi-dev/resources/cross-iam-role-irsa.tf
@@ -122,3 +122,13 @@ resource "kubernetes_secret" "irsa" {
     serviceaccount = module.irsa.service_account.name
   }
 }
+
+## Service POD 
+# set up the service pod
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name 
+}
