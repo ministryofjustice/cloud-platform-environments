4d912503bf54eda5ed0801a4d447d2156d03683f
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2024-05-15 11:47:26 +0100

    Remove SQS lastupdate queue and irsa access (#23025)
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/ims-legacy-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/ims-legacy-queue.tf
@@ -104,59 +104,6 @@ module "ims_transformer_dead_letter_queue" {
   }
 }
 
-module "ims_lastupdate_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
-
-  # Queue configuration
-  sqs_name                   = "ims_lastupdate_queue_${var.environment}"
-  fifo_queue                 = false
-  encrypt_sqs_kms            = "true"
-  message_retention_seconds  = 1209600
-  visibility_timeout_seconds = 120
-
-  redrive_policy = <<EOF
-  {
-    "deadLetterTargetArn": "${module.ims_lastupdate_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
-  }
-
-EOF
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-module "ims_lastupdate_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
-
-  # Queue configuration
-  sqs_name        = "ims_lastupdate_dl_queue_${var.environment}"
-  fifo_queue      = false
-  encrypt_sqs_kms = "true"
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 module "ims_reprocess_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
 
@@ -260,32 +207,6 @@ resource "kubernetes_secret" "ims_transformer_dead_letter_queue" {
     sqs_id   = module.ims_transformer_dead_letter_queue.sqs_id
     sqs_arn  = module.ims_transformer_dead_letter_queue.sqs_arn
     sqs_name = module.ims_transformer_dead_letter_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "ims_lastupdate_queue" {
-  metadata {
-    name      = "ims-lastupdate-queue-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    sqs_id   = module.ims_lastupdate_queue.sqs_id
-    sqs_arn  = module.ims_lastupdate_queue.sqs_arn
-    sqs_name = module.ims_lastupdate_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "ims_lastupdate_dead_letter_queue" {
-  metadata {
-    name      = "ims-lastupdate-dlq-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    sqs_id   = module.ims_lastupdate_dead_letter_queue.sqs_id
-    sqs_arn  = module.ims_lastupdate_dead_letter_queue.sqs_arn
-    sqs_name = module.ims_lastupdate_dead_letter_queue.sqs_name
   }
 }
 
