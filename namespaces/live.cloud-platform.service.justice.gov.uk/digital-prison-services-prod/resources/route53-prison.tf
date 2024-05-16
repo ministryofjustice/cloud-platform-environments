d11e0ce3f21addce2342744f13208e740419762f
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-05-16 19:31:22 +0100

    Revert "SDIT-1751: 🔧 Remove preprod route (#23062)" (#23071)
    This reverts commit e9898fd316a1536f1fd330963602a1395ea9910a.

--- namespaces/live.cloud-platform.service.justice.gov.uk/digital-prison-services-prod/resources/route53-prison.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/digital-prison-services-prod/resources/route53-prison.tf
@@ -20,3 +20,13 @@ resource "aws_route53_record" "prison-api-prod" {
   ttl     = "30"
   records = ["51.141.55.10"]
 }
+
+
+# preprod DNS record for Azure based Prison API
+resource "aws_route53_record" "prison-api-preprod" {
+  zone_id = aws_route53_zone.route53_zone.zone_id
+  name    = "api-preprod.prison.service.justice.gov.uk"
+  type    = "A"
+  ttl     = "30"
+  records = ["52.142.149.4"]
+}
