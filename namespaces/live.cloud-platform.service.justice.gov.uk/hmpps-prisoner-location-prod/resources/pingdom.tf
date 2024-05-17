07ee549567448359abe48a1ddd5ed9a3cfcb944c
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-17 16:29:18 +0100

    SDIT-1751: 🔧 Add in pingdom alert for hmpps-prisoner-location-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-location-prod/resources/pingdom.tf
@@ -0,0 +1,38 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 126478 = #syscon-alerts
+
+resource "pingdom_check" "dps-production-check" {
+  type                     = "http"
+  name                     = "DPS - hmpps-prisoner-location-api"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/prisoner-location-api.prison.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [126478]
+}
+
+resource "pingdom_check" "dps-production-ui-check" {
+  type                     = "http"
+  name                     = "DPS - ${var.application}"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/${var.domain}"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [126478]
+}
+
