33a636dfdfb04d626b4dcec77c5e980166388c51
popey2700 <alex.pope@madetech.com>
2024-05-20 16:27:38 +0100

    Add fake password
    Adding a fake password to see if this gives any intel on current deployment error issue. Hopefully by the secret being set we ll get a more informative error

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/s3_truststore.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/s3_truststore.tf
@@ -97,7 +97,7 @@ resource "kubernetes_secret" "certificate_backup_secret" {
     bucket_name =  module.certificate_backup.bucket_name
     # Secrets require mannual setup after event service certficate uploaded:
     event_service_certificate_path = "event-service/client.p12"
-    # event_service_certificate_password
+    event_service_certificate_password = "DUMMY_TEST_FOR_DEBUGGING"
 
   }
 }
