bfdf2be7df5fc7e9ea46c932bdbf9db2fe03432b
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-05-22 12:39:57 +0100

    HIA-730
    Updating naming of an element as directed by Councourse CI

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/event-subscriber-pnd.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/event-subscriber-pnd.tf
@@ -101,7 +101,7 @@ resource "aws_sns_topic_subscription" "event_pnd_subscription" {
 
 resource "kubernetes_secret" "event_pnd_queue" {
   metadata {
-    name      = "pnd_filter_list"
+    name      = "pnd-filter-list"
     namespace = var.namespace
   }
 
