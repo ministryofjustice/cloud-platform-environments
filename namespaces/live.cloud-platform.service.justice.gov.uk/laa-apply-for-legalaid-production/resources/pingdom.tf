6d6f4d6c46670cb91b43b1c19224f31ce266d9dd
David Read <david.read@digital.justice.gov.uk>
2024-05-22 09:54:53 +0000

    Add pingdom tag
    Tag is for creation of a pingdom dashboard across laa production services

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-production/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-production/resources/pingdom.tf
@@ -11,7 +11,7 @@ resource "pingdom_check" "laa-apply-for-legalaid-production" {
   url              = "/ping"
   encryption       = true
   port             = 443
-  tags             = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.application},laa,apply-for-legal-aid"
+  tags             = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.application},laa,apply-for-legal-aid,laa_production_environment_dashboard"
   probefilters     = "region:EU"
   integrationids   = [129269]
 }
