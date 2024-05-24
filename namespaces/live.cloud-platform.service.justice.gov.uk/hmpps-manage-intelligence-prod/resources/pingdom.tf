d5635f033dbe42102a9a8cc0dd66c03855951c93
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2024-05-24 14:05:36 +0100

    correcting pingdom config to use health-kick.prison.service.justice.gov.uk (#23207)
    Co-authored-by: brian.carr <brian.carr@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-prod/resources/pingdom.tf
@@ -4,12 +4,12 @@ provider "pingdom" {
 resource "pingdom_check" "transformer" {
   type                     = "http"
   name                     = "DPS - ${var.application} - transformer"
-  host                     = "mercury-data-transform-load.hmpps.service.justice.gov.uk"
+  host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/"
+  url                      = "/https/mercury-data-transform-load.hmpps.service.justice.gov.uk"
   encryption               = true
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
@@ -20,12 +20,12 @@ resource "pingdom_check" "transformer" {
 resource "pingdom_check" "manage-intelligence" {
   type                     = "http"
   name                     = "DPS - ${var.application}"
-  host                     = "manage-intelligence.prison.service.justice.gov.uk"
+  host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/"
+  url                      = "/https/manage-intelligence.prison.service.justice.gov.uk"
   encryption               = true
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
@@ -36,12 +36,12 @@ resource "pingdom_check" "manage-intelligence" {
 resource "pingdom_check" "manage-intelligence-api" {
   type                     = "http"
   name                     = "DPS - ${var.application} - api"
-  host                     = "manage-intelligence-api.prison.service.justice.gov.uk"
+  host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/"
+  url                      = "/https/manage-intelligence-api.prison.service.justice.gov.uk/"
   encryption               = true
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
