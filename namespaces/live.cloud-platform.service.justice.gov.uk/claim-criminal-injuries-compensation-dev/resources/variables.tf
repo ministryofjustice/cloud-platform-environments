f78757d34d124c26d6c4299eb35f1e8d72f0bb75
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2024-05-24 08:41:39 +0100

    Rename migrated cica repos for ecr module registry

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/variables.tf
@@ -76,7 +76,7 @@ variable "slack_channel" {
 variable "repo_name" {
   description = "List of repos"
   type        = list
-  default     = [ "cica-gov-uk-application-service", "cica-gov-uk-data-capture-service", "cica-apply-web",
+  default     = [ "cica-apply-application-service", "cica-apply-data-capture-service", "cica-apply-web",
   "cica-apply-notify-gateway"]
 }
 variable "kubernetes_cluster" {}
