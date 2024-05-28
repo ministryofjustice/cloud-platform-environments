302b3eeb7e4eb4bcca5b5e6768b0786b4b53e80d
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-28 12:54:09 +0100

    SDIT-1751: 🔧 Add in pingdom check for old azure prison api

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-api-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-api-prod/resources/pingdom.tf
@@ -4,7 +4,7 @@ provider "pingdom" {
 # Integration IDs
 # 126478 = #syscon-alerts
 
-resource "pingdom_check" "dps-production-check" {
+resource "pingdom_check" "prison-api-production-check" {
   type                     = "http"
   name                     = "DPS - ${var.application}"
   host                     = "health-kick.prison.service.justice.gov.uk"
@@ -17,6 +17,22 @@ resource "pingdom_check" "dps-production-check" {
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
   probefilters             = "region:EU"
-  integrationids           = [126478]
+  integrationids           = [96624, 126478]
+}
+
+resource "pingdom_check" "azure-prison-api-production-check" {
+  type                     = "http"
+  name                     = "DPS - Azure ${var.application}"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/api.prison.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 126478]
 }
 
