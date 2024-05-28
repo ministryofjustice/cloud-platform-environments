dc9c312a5a0be752dfe760cfc7c91d5f0c990fa3
vonbraunbates <vonbraunbates@users.noreply.github.com>
2024-05-28 14:47:53 +0100

    Remove namespace.

--- namespaces/live.cloud-platform.service.justice.gov.uk/ct-tact-list-dev/resources/service-pod.tf
+++
@@ -1,8 +0,0 @@
-module "service_pod" {
-  # use the latest release"
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
-
-  # Configuration
-  namespace            = var.namespace
-  service_account_name = module.irsa.service_account.name # see irsa.tf for an example of irsa
-}
