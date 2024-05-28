a9602cef3c8571e2435bb8130cc6bb175a553378
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-05-28 15:17:00 +0100

    amend SNS for hmpps_prisoner_to_nomis_alerts_subscription"
    Add eventTypes for the 8 domain events in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/domain-events-queue-alerts.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/domain-events-queue-alerts.tf
@@ -110,7 +110,15 @@ resource "aws_sns_topic_subscription" "hmpps_prisoner_to_nomis_alerts_subscripti
     eventType = [
       "prisoner-alerts.alert-created",
       "prisoner-alerts.alert-updated",
-      "prisoner-alerts.alert-deleted"
+      "prisoner-alerts.alert-deleted",
+      "prisoner-alerts.alert-code-created",
+      "prisoner-alerts.alert-code-deactivated",
+      "prisoner-alerts.alert-code-reactivated",
+      "prisoner-alerts.alert-code-updated",
+      "prisoner-alerts.alert-type-created",
+      "prisoner-alerts.alert-type-deactivated",
+      "prisoner-alerts.alert-type-reactivated",
+      "prisoner-alerts.alert-type-updated"
     ]
   })
 }
