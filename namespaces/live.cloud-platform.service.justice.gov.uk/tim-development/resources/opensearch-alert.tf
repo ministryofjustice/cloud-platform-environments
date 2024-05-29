393885de180ae5a518ee87ec5e2a8d8980b61d73
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-05-29 11:57:59 +0100

    update variable and implement secret module

--- namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/opensearch-alert.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/opensearch-alert.tf
@@ -1,27 +1,27 @@
-module "opensearch_alert" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alerts?ref=opensearch-alert-for-user"
+# module "opensearch_alert" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alerts?ref=opensearch-alert-for-user"
 
-  opensearch_alert_name    = "test alert"
-  opensearch_alert_enabled = true
-  monitor_period_interval  = 1
-  monitor_period_unit      = "MINUTES"
-  query_start_time         = "{{period_end}}||-10m"
-  query_end_time           = "{{period_end}}"
-  query_source             = "ctx.results[0].hits.total.value > 1"
-  trigger_id               = "test test"
-  trigger_name             = "test test"
-  action_id                = "test test"
-  action_name              = "test test"
-  slack_channel_name       = "test test"
-  aws_secret_name          = "test"
-  k8s_secret_name          = "test"
-  slack_message_subject    = "test test"
-  team_name                = var.team_name
-  business_unit            = var.business_unit
-  application              = var.application
-  is_production            = var.is_production
-  environment_name         = var.environment
-  infrastructure_support   = var.infrastructure_support
-  namespace                = var.namespace
+#   opensearch_alert_name    = "test alert"
+#   opensearch_alert_enabled = true
+#   monitor_period_interval  = 1
+#   monitor_period_unit      = "MINUTES"
+#   query_start_time         = "{{period_end}}||-10m"
+#   query_end_time           = "{{period_end}}"
+#   query_source             = "ctx.results[0].hits.total.value > 1"
+#   trigger_id               = "test test"
+#   trigger_name             = "test test"
+#   action_id                = "test test"
+#   action_name              = "test test"
+#   slack_channel_name       = "test test"
+#   aws_secret_name          = "test"
+#   k8s_secret_name          = "test"
+#   slack_message_subject    = "test test"
+#   team_name                = var.team_name
+#   business_unit            = var.business_unit
+#   application              = var.application
+#   is_production            = var.is_production
+#   environment_name         = var.environment
+#   infrastructure_support   = var.infrastructure_support
+#   namespace                = var.namespace
 
-}
+# }
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/secret.tf
@@ -0,0 +1,24 @@
+module "secret" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4" # use the latest release
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # Secrets configuration
+  secrets = {
+    "tim" = {
+      description             = "tim" # required
+      recovery_window_in_days = 7                # required
+      k8s_secret_name         = "tim-example-secret" # the name of the secret in k8s
+    },
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
