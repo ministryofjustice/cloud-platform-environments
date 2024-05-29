a5cb44076e0e0db93a1caf8e8d195b0f4bc5b757
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-05-29 12:51:29 +0100

    style: 💄 tidy

--- cmd/annotations-checker/main.go
+++ cmd/annotations-checker/main.go
@@ -11,23 +11,21 @@ import (
 func main() {
 	org := "ministryofjustice"
 	token := os.Getenv("GITHUB_TOKEN")
-	prEvent := os.Getenv("PR_EVENT")
+	diffUrl := os.Getenv("DIFF_URL")
 
 	if token == "" {
 		fmt.Println("GITHUB_TOKEN environment variable is not set")
 		os.Exit(1)
 	}
 
-	if prEvent == "" {
-		fmt.Println("PR_EVENT environment variable is not set")
+	if diffUrl == "" {
+		fmt.Println("DIFF_URL environment variable is not set")
 		os.Exit(1)
 	}
 
 	client := init_github.NewGitHubClient(token)
 
-	// diff := "https://patch-diff.githubusercontent.com/raw/ministryofjustice/cloud-platform-environments/pull/21897.diff"
-
-	annotations, err := validate.Parse(client, org, prEvent)
+	annotations, err := validate.Parse(client, org, diffUrl)
 	if err != nil {
 		fmt.Printf("Error parsing pr files: %v\n", err)
 		os.Exit(1)
