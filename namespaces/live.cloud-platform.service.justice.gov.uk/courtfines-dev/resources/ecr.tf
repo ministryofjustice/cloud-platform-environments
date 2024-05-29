30a94b5111341375fd6221a318db8c2c2891e343
Mateusz Kolakowski <mateusz.kolakowski@justice.gov.uk>
2024-05-29 16:18:34 +0100

    Courtfines Dev: Add github_repositories (#23311)

--- namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/courtfines-dev/resources/ecr.tf
@@ -9,11 +9,7 @@ module "ecr_credentials" {
   team_name      = var.team_name
   repo_name      = "${var.namespace}-ecr"
   oidc_providers = ["circleci"]
-
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ECR name, AWS access key, and AWS secret key, for use in
-  # github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["hmcts-courtfines"]
 
   # list of github environments, to create the ECR secrets as environment secrets
   # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
