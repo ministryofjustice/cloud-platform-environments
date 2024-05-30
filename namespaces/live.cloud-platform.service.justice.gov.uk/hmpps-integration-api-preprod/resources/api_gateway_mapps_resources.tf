d67c5f3b557c126a46251073d9b0d5e4412cc15d
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-05-30 13:32:54 +0100

    Fix resource order

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/api_gateway_mapps_resources.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/api_gateway_mapps_resources.tf
@@ -30,42 +30,6 @@ resource "aws_api_gateway_method_response" "sqs_mapps_method_response" {
   }
 }
 
-resource "aws_api_gateway_integration" "sqs_mapps_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_resource.sqs_mapps_resource.id
-  http_method             = aws_api_gateway_method.sqs_mapps_method.http_method
-  type                    = "AWS"
-  integration_http_method = "GET"
-  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${data.aws_caller_identity.current.account_id}/${module.event_mapps_queue.sqs_name}?Action=ReceiveMessage"
-
-  depends_on = [
-    aws_api_gateway_rest_api.api_gateway,
-    aws_api_gateway_resource.sqs_parent_resource,
-    aws_api_gateway_resource.sqs_mapps_resource,
-    module.event_mapps_queue,
-    aws_api_gateway_method.sqs_mapps_method,
-    aws_api_gateway_method_response.sqs_mapps_method_response,
-    aws_iam_role.api_gateway_sqs_role
-  ]
-
-  credentials = aws_iam_role.api_gateway_sqs_role.arn
-}
-
-resource "aws_api_gateway_integration_response" "sqs_mapps_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.sqs_mapps_resource.id
-  http_method = aws_api_gateway_method.sqs_mapps_method.http_method
-  status_code = aws_api_gateway_method_response.sqs_mapps_method_response.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-  depends_on = [
-    aws_api_gateway_rest_api.api_gateway,
-    aws_api_gateway_integration.sqs_mapps_integration
-  ]
-}
-
 resource "aws_iam_role" "api_gateway_sqs_mapps_role" {
   name               = "${var.namespace}-api-gateway-sqs-mapps-role"
   assume_role_policy = <<EOF
@@ -102,5 +66,40 @@ resource "aws_iam_role_policy" "api_gateway_sqs_mapps_policy" {
 
   depends_on = [
     module.event_mapps_queue
+  ]
+}
+
+resource "aws_api_gateway_integration" "sqs_mapps_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.sqs_mapps_resource.id
+  http_method             = aws_api_gateway_method.sqs_mapps_method.http_method
+  type                    = "AWS"
+  integration_http_method = "GET"
+  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${data.aws_caller_identity.current.account_id}/${module.event_mapps_queue.sqs_name}?Action=ReceiveMessage"
+
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_resource.sqs_parent_resource,
+    aws_api_gateway_resource.sqs_mapps_resource,
+    module.event_mapps_queue,
+    aws_api_gateway_method.sqs_mapps_method,
+    aws_api_gateway_method_response.sqs_mapps_method_response
+  ]
+
+  credentials = aws_iam_role.api_gateway_sqs_mapps_role.arn
+}
+
+resource "aws_api_gateway_integration_response" "sqs_mapps_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.sqs_mapps_resource.id
+  http_method = aws_api_gateway_method.sqs_mapps_method.http_method
+  status_code = aws_api_gateway_method_response.sqs_mapps_method_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_integration.sqs_mapps_integration
   ]
 }
