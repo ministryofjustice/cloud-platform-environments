82b8792c3291ea1e9d1d06bdfce9a82361e855fa
Ky <ky.truong@digital.justice.gov.uk>
2024-05-31 09:13:24 +0100

    bumping cloudfront module

--- namespaces/live.cloud-platform.service.justice.gov.uk/ky-dev-test/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ky-dev-test/resources/cloudfront.tf
@@ -1,5 +1,5 @@
 module "cloudfront" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=cloudfront_functions_v2"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.3.0"
 
   # Configuration
   bucket_id          = module.s3_bucket.bucket_name
