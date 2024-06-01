58f630eab2bfb42ab30b76695e83dfa793a1c495
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-06-01 20:34:14 +0100

    update provider

--- namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/versions.tf
@@ -9,13 +9,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.31.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.25.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.0.0"
-    }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = "2.25.2"
+    # }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 2.0.0"
+    # }
   }
 }
