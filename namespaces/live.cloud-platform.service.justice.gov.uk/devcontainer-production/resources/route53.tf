cd7ab5deee6cb1f4c06a9768d3cde5f8cb6e9dc9
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-03 13:10:50 +0000

    :wrench: Add devcontainer namespace + DNS

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/devcontainer-production/resources/route53.tf
@@ -0,0 +1,30 @@
+resource "aws_route53_zone" "example_team_route53_zone" {
+  name = "devcontainer.service.justice.gov.uk"
+
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure_support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "aws_route53_record" "github_pages" {
+  zone_id = aws_route53_zone.devcontainer_service_justice_gov_uk.zone_id
+  name    = "devcontainer.service.justice.gov.uk"
+  type    = "A"
+  ttl     = "300"
+  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
+}
+
+resource "aws_route53_record" "github_pages_user_guide" {
+  zone_id = aws_route53_zone.devcontainer_service_justice_gov_uk.zone_id
+  name    = "user-guide.devcontainer.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["ministryofjustice.github.io"]
+}
