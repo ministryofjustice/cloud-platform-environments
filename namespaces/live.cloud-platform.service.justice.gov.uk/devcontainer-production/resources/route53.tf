1c291d25fb5bf4016a478ffd41cb39f3a37a5900
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-03 13:48:00 +0000

    Remove unneeded resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/devcontainer-production/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/devcontainer-production/resources/route53.tf
@@ -13,13 +13,6 @@ resource "aws_route53_zone" "devcontainer_service_justice_gov_uk" {
   }
 }
 
-resource "aws_route53_record" "github_pages" {
-  zone_id = aws_route53_zone.devcontainer_service_justice_gov_uk.zone_id
-  name    = "devcontainer.service.justice.gov.uk"
-  type    = "A"
-  ttl     = "300"
-  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
-}
 
 resource "aws_route53_record" "github_pages_user_guide" {
   zone_id = aws_route53_zone.devcontainer_service_justice_gov_uk.zone_id
