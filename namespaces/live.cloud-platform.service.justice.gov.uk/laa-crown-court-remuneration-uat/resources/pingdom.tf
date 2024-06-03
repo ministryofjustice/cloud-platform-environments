85830cfed9d5f2151fb6194ad21c42895c4beeb1
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2024-06-03 15:12:50 +0100

    Add CCR UAT Pingdom

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-uat/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-crown-court-remuneration-uat" {
+  type                     = "http"
+  name                     = "Crown Court Remuneration UAT"
+  host                     = "ccr-uat.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ccr/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  probefilters             = "region:EU"
+  integrationids           = [135401]
+}
