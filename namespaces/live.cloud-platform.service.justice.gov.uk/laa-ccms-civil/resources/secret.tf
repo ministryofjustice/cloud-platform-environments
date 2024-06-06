f27e6b82c3b035a82738c90539dde89d97e990a1
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-06-06 14:53:09 +0100

    add auth secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
@@ -11,14 +11,39 @@ module "secrets_manager" {
 
   secrets = {
     "saml-metadata-uri" = {
-      description             = "The URI for the SAML authentication", # Required
-      recovery_window_in_days = 7,                                     # Required
-      k8s_secret_name         = "saml-metadata-uri"                    # The name of the secret in k8s
+      description             = "The URI for the SAML authentication",              # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "saml-metadata-uri"                                 # The name of the secret in k8s
     },
     "caab-secrets" = {
-      description             = "SOA urls and database credentials for CAAB", # Required
-      recovery_window_in_days = 7,                                            # Required
-      k8s_secret_name         = "caab-secrets"                                # The name of the secret in k8s
+      description             = "SOA urls and database credentials for CAAB",       # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-secrets"                                      # The name of the secret in k8s
+    },
+    "caab-api-auth-secrets" = {
+      description             = "Authentication secrets for CAAB API",              # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-api-auth-secrets"                             # The name of the secret in k8s
+    },
+    "caab-assessment-api-auth-secrets" = {
+      description             = "Authentication secrets for CAAB Assessment API",   # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-assessment-api-auth-secrets"                  # The name of the secret in k8s
+    },
+    "caab-ebs-api-auth-secrets" = {
+      description             = "Authentication secrets for CAAB EBS API",          # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-ebs-api-auth-secrets"                         # The name of the secret in k8s
+    },
+    "caab-soa-api-auth-secrets" = {
+      description             = "Authentication secrets for CAAB SOA API",          # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-soa-api-auth-secrets"                         # The name of the secret in k8s
+    },
+    "caab-ui-secrets" = {
+      description             = "Secrets for CAAB UI",                              # Required
+      recovery_window_in_days = 7,                                                  # Required
+      k8s_secret_name         = "caab-ui-secrets"                                   # The name of the secret in k8s
     },
   }
 }
