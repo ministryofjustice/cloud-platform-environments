33f7e82cff54f4349b32e4ef2f8d10a1d5006d09
carlosmoj <carlos.fernandez@digital.justice.gov.uk>
2024-06-06 09:51:18 +0100

    github_repositories is case-sensitive so I fixed this

--- namespaces/live.cloud-platform.service.justice.gov.uk/mscproject-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mscproject-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["mscproject"]
+  github_repositories = ["MScProject"]
 
   # Tags
   business_unit          = var.business_unit
