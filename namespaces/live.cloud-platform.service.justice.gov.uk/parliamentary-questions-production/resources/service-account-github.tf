add092cfa30aa5257702d4c4d31708453cf3e511
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-06-10 08:28:02 +0100

    update github service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/service-account-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/parliamentary-questions-production/resources/service-account-github.tf
@@ -1,10 +1,12 @@
-module "serviceaccount" {
+module "serviceaccount-github" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
   namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  github_repositories = [var.repo_name]
+  serviceaccount_name = "github-serviceaccount"
+  role_name = "github-serviceaccount"
+  rolebinding_name = "github-serviceaccount"
 
   # This module is also used in the staging namespace, so we must change the
   # secret names here in order to not overwrite the existing ones.
@@ -13,4 +15,9 @@ module "serviceaccount" {
   github_actions_secret_kube_token     = "KUBE_PROD_TOKEN"
   github_actions_secret_kube_cluster   = "KUBE_PROD_CLUSTER"
   github_actions_secret_kube_namespace = "KUBE_PROD_NAMESPACE"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = [var.repo_name]
+  github_environments = ["production"]
 }
