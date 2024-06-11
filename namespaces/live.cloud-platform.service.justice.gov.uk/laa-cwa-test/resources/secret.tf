8e5c347c1ae1f6a85c748c58686657cb8fcb5481
Solomon Ahmed <51327770+solomonahmed@users.noreply.github.com>
2024-06-11 14:56:08 +0100

    version set to 3.0.4

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-test/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-test/resources/secret.tf
@@ -1,5 +1,5 @@
 module "secrets_manager" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=version" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4" # use the latest release
   team_name              = var.team_name
   application            = var.application
   business_unit          = var.business_unit
