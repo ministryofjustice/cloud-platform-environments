57ea3213447fdc399f0195c7c69a5514a1dfea44
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-06-11 12:04:30 +0100

    update version

--- namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = ">= 4.0."
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
