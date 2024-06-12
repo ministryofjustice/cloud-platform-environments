760484d9684d7645ce8e0443f1b63007dbfdd4ba
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-06-12 10:38:52 +0100

    ECR fix - Person-on-probation-user service

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-on-probation-user-service-preprod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-on-probation-user-service-preprod/resources/ecr.tf
@@ -8,11 +8,8 @@ module "ecr" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.0.0"
 
   # Repository configuration
-  repo_name = var.namespace
+  repo_name = "${var.namespace}-ecr"
 
-  # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = ["hmpps-person-on-probation-user-api"]
 
   # Tags
   business_unit          = var.business_unit
