0eb78b286acda569e659fd2747a6a1dc592591ac
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-06-13 15:22:01 +0100

    RASS-254 revert

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "hmpps_court_register"
+  default     = "HMPPS Court Register"
 }
 
 variable "namespace" {
