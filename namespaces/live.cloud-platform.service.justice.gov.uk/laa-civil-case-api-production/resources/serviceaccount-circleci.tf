6922a3742826b58f4350a257497b944b0f7a5347
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-06-13 10:16:49 +0100

    Amendments

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-case-api-production/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-case-api-production/resources/serviceaccount-circleci.tf
@@ -6,7 +6,7 @@ module "serviceaccount_circleci" {
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
-  serviceaccount_name = "circleci-migrated"
+  serviceaccount_name = "circleci"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
