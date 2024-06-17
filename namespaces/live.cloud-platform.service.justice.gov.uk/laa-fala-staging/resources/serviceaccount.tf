1975577b5182c02b558385537c26607a5a08e45b
Stephen Dicks <github@starswan.com>
2024-06-17 13:32:27 +0100

    EL-1463 remove github repo from servuce account

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-fala-staging/resources/serviceaccount.tf
@@ -58,5 +58,5 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["my-repo"]
-  github_repositories                  = [var.repo_name]
+#   github_repositories                  = [var.repo_name]
 }
