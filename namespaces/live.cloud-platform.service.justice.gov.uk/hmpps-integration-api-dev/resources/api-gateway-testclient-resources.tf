5d9a9bf950bd9b7aefd1ebac4f78d87859d467b4
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-06-19 14:14:54 +0100

    Create test client event api

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api-gateway-testclient-resources.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api-gateway-testclient-resources.tf
@@ -1,24 +1,39 @@
+resource "aws_api_gateway_rest_api" "api_gateway_test_client" {
+  name                         = "${var.namespace}_test_client_events"
+  disable_execute_api_endpoint = false
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "testclient_parent_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway_test_client.root_resource_id
+  path_part   = "events"
+}
+
 resource "aws_api_gateway_resource" "sqs_test_client_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_resource.sqs_parent_resource.id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
+  parent_id   = aws_api_gateway_resource.testclient_parent_resource.id
   path_part   = "test_client"
 }
 resource "aws_api_gateway_method" "sqs_test_client_method" {
-  rest_api_id      = aws_api_gateway_rest_api.api_gateway.id
+  rest_api_id      = aws_api_gateway_rest_api.api_gateway_test_client.id
   resource_id      = aws_api_gateway_resource.sqs_test_client_resource.id
   http_method      = "GET"
   authorization    = "NONE"
   api_key_required = true
 
   depends_on = [
-    aws_api_gateway_rest_api.api_gateway,
-    aws_api_gateway_resource.sqs_parent_resource,
+    aws_api_gateway_rest_api.api_gateway_test_client,
+    aws_api_gateway_resource.testclient_parent_resource,
     aws_api_gateway_resource.sqs_test_client_resource
   ]
 }
 
 resource "aws_api_gateway_method_response" "sqs_test_client_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
   resource_id = aws_api_gateway_resource.sqs_test_client_resource.id
   http_method = aws_api_gateway_method.sqs_test_client_method.http_method
   status_code = "200"
@@ -26,6 +41,8 @@ resource "aws_api_gateway_method_response" "sqs_test_client_method_response" {
     "application/json" = "Empty"
   }
 }
+
+
 
 resource "aws_iam_role" "api_gateway_sqs_test_client_role" {
   name               = "${var.namespace}-api-gateway-sqs-test-client-role"
@@ -45,6 +62,7 @@ resource "aws_iam_role" "api_gateway_sqs_test_client_role" {
 }
 EOF
 }
+
 
 resource "aws_iam_role_policy" "api_gateway_sqs_test_client_policy" {
   name = "${var.namespace}-api-gateway-sqs-test-client-policy"
@@ -67,7 +85,7 @@ resource "aws_iam_role_policy" "api_gateway_sqs_test_client_policy" {
 }
 
 resource "aws_api_gateway_integration" "sqs_test_client_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway_test_client.id
   resource_id             = aws_api_gateway_resource.sqs_test_client_resource.id
   http_method             = aws_api_gateway_method.sqs_test_client_method.http_method
   type                    = "AWS"
@@ -75,8 +93,7 @@ resource "aws_api_gateway_integration" "sqs_test_client_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${data.aws_caller_identity.current.account_id}/${module.event_test_client_queue.sqs_name}?Action=ReceiveMessage"
 
   depends_on = [
-    aws_api_gateway_rest_api.api_gateway,
-    aws_api_gateway_resource.sqs_parent_resource,
+    aws_api_gateway_rest_api.api_gateway_test_client,
     aws_api_gateway_resource.sqs_test_client_resource,
     module.event_test_client_queue,
     aws_api_gateway_method.sqs_test_client_method,
@@ -87,7 +104,7 @@ resource "aws_api_gateway_integration" "sqs_test_client_integration" {
 }
 
 resource "aws_api_gateway_integration_response" "sqs_test_client_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
   resource_id = aws_api_gateway_resource.sqs_test_client_resource.id
   http_method = aws_api_gateway_method.sqs_test_client_method.http_method
   status_code = aws_api_gateway_method_response.sqs_test_client_method_response.status_code
@@ -96,7 +113,92 @@ resource "aws_api_gateway_integration_response" "sqs_test_client_integration_res
     "application/json" = ""
   }
   depends_on = [
-    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_rest_api.api_gateway_test_client,
     aws_api_gateway_integration.sqs_test_client_integration
   ]
 }
+
+resource "aws_api_gateway_deployment" "test-client-deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      # "manual-deploy-trigger",
+      local.clients,
+      var.cloud_platform_integration_api_url,
+      md5(file("api-gateway-testclient-resources.tf"))
+    ]))
+  }
+
+  depends_on = [
+    aws_api_gateway_method.sqs_test_client_method,   
+    aws_api_gateway_integration.sqs_test_client_integration
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "test-client-event" {
+  deployment_id         = aws_api_gateway_deployment.test-client-deployment.id
+  rest_api_id           = aws_api_gateway_rest_api.api_gateway_test_client.id
+  stage_name            = var.namespace
+  client_certificate_id = aws_api_gateway_client_certificate.api_gateway_client_two.id
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_access_logs.arn
+    format = jsonencode({
+      extendedRequestId  = "$context.extendedRequestId"
+      ip                 = "$context.identity.sourceIp"
+      client             = "$context.identity.clientCert.subjectDN"
+      issuerDN           = "$context.identity.clientCert.issuerDN"
+      requestTime        = "$context.requestTime"
+      httpMethod         = "$context.httpMethod"
+      resourcePath       = "$context.resourcePath"
+      status             = "$context.status"
+      responseLength     = "$context.responseLength"
+      error              = "$context.error.message"
+      authenticateStatus = "$context.authenticate.status"
+      authenticateError  = "$context.authenticate.error"
+      integrationStatus  = "$context.integration.status"
+      integrationError   = "$context.integration.error"
+      apiKeyId           = "$context.identity.apiKeyId"
+    })
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_cloudwatch_log_group.api_gateway_access_logs]
+}
+resource "aws_api_gateway_usage_plan" "test_client_events" {
+  name = "${var.namespace}-events"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.api_gateway_test_client.id
+    stage  = aws_api_gateway_stage.test-client-event.stage_name
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "test_client_events_client" {
+
+  key_id        = aws_api_gateway_api_key.clients["event-service"].id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.test_client_events.id
+
+   depends_on = [
+    aws_api_gateway_api_key.clients
+  ]
+
+}
+
+resource "aws_api_gateway_base_path_mapping" "testclient-event-hostname" {
+  for_each = aws_api_gateway_domain_name.api_gateway_fqdn
+
+  api_id      = aws_api_gateway_rest_api.api_gateway_test_client.id
+  domain_name = aws_api_gateway_domain_name.api_gateway_fqdn[each.key].domain_name
+  stage_name  = aws_api_gateway_stage.test-client-event.stage_name
+}
+
