eef0064e472717750b29afd52bdf96b176e87993
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2024-06-20 09:06:00 +0100

    fetch diff content for each test case with GetDiff

--- cmd/annotations-checker/validate/parse_test.go
+++ cmd/annotations-checker/validate/parse_test.go
@@ -9,10 +9,16 @@ import (
 func TestParse(t *testing.T) {
 	client := &github.Client{}
 	org := "ministryofjustice"
+
 	validDiffURL := "https://github.com/ministryofjustice/cloud-platform-environments/pull/21897.diff"
+	validDiff, err := GetDiff(validDiffURL)
+	if err != nil {
+		t.Errorf("unexpected error fetching valid diff: %v", err)
+		return
+	}
 
 	// test case 1: passes passing a raw diff for a valid PR
-	annotations, err := Parse(client, org, validDiffURL)
+	annotations, err := Parse(client, org, validDiff)
 	if err != nil {
 		t.Errorf("unexpected error for valid PR: %v", err)
 	}
@@ -28,14 +34,24 @@ func TestParse(t *testing.T) {
 
 	// test case 2: invalid pr fails containing incorrect team name
 	invalidTeamNameDiffURL := "https://github.com/ministryofjustice/cloud-platform-environments/pull/23707.diff"
-	_, err = Parse(client, org, invalidTeamNameDiffURL)
+	invalidTeamNameDiff, err := GetDiff(invalidTeamNameDiffURL)
+	if err != nil {
+		t.Errorf("unexpected error fetching invalid team name dff: %v", err)
+		return
+	}
+	_, err = Parse(client, org, invalidTeamNameDiff)
 	if err == nil {
 		t.Errorf("Expected an error for invalid team name, but got none")
 	}
 
 	// test case 3: invalid PR fails containing incorrect source code repo
 	invalidSourceCodeDiffURL := "https://github.com/ministryofjustice/cloud-platform-environments/pull/23730.diff"
-	_, err = Parse(client, org, invalidSourceCodeDiffURL)
+	invalidSourceCodeDiff, err := GetDiff(invalidSourceCodeDiffURL)
+	if err != nil {
+		t.Errorf("unexpected error fetching invalid source code diff: %v", err)
+		return
+	}
+	_, err = Parse(client, org, invalidSourceCodeDiff)
 	if err == nil {
 		t.Errorf("Expected an error for invalid source code repo, but got none")
 	}
