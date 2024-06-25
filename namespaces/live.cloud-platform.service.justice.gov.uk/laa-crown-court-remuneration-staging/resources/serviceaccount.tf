8a8bd7200a8e371b84e65d5c7c0081256cb2b322
vloruncg <victor.olorunleye@digital.justice.gov.uk>
2024-06-25 14:25:29 +0100

    removed cross namespace changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-staging/resources/serviceaccount.tf
@@ -4,7 +4,7 @@ module "serviceaccount" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  serviceaccount_token_rotated_date = "01-01-2000"
+  serviceaccount_token_rotated_date = "25-06-2024"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
