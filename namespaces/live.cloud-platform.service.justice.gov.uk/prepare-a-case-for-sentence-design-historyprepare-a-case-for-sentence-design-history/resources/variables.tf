7d7aca35356f1ca17bd4b09abf1d693aab55a3a7
titlescreen <richardcooley@gmail.com>
2024-06-25 14:53:38 +0100

    Fix namespace for prepare-a-case-for-sentence-design-history

--- namespaces/live.cloud-platform.service.justice.gov.uk/prepare-a-case-for-sentence-design-historyprepare-a-case-for-sentence-design-history/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prepare-a-case-for-sentence-design-historyprepare-a-case-for-sentence-design-history/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "prepare-a-case-for-sentence-design-historyprepare-a-case-for-sentence-design-history"
+  default     = "prepare-a-case-for-sentence-design-history"
 }
 
 variable "business_unit" {
