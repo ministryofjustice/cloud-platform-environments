b336bbc9218cefbd5a28f71f3323f2d3e9857132
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-06-26 12:16:41 +0100

    Fix ECR setup Launchpad Dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/ecr.tf
@@ -13,7 +13,6 @@ module "ecr" {
   # OpenID Connect configuration
   oidc_providers      = ["github"]
   github_repositories = ["hmpps-launchpad-home-ui","hmpps-launchpad-auth"]
-  github_environments = ["dev"]
 
   # Tags
   business_unit          = var.business_unit
