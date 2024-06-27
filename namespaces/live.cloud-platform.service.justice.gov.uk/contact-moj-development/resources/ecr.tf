f5cb5f08c2a911078e8808ea353cb35b0ef19f4f
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-06-27 11:16:45 +0100

    use var repo name

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-development/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-development/resources/ecr.tf
@@ -6,7 +6,7 @@
  */
 module "contact-moj_ecr_credentials" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.0.0"
-  repo_name = "contact-moj-ecr"
+  repo_name = var.repo_name
 
   oidc_providers      = ["github", "circleci"]
   github_repositories = [var.repo_name]
