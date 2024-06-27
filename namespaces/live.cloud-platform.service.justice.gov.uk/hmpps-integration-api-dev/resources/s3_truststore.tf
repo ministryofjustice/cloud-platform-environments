451cd5840c0a7182f76fbb4dd3f5a065b235ce4f
popey2700 <alex.pope@madetech.com>
2024-06-27 14:20:52 +0100

    Remove delete access

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/s3_truststore.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/s3_truststore.tf
@@ -78,7 +78,6 @@ module "certificate_backup" {
         "s3:GetObject",
         "s3:PutObject",
         "s3:GetObjectVersion",
-        "s3:DeleteObject",
         "s3:ListObjectsV2"
       ],
       "Resource": [
