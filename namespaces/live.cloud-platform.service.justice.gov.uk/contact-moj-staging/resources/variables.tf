f79ba9a731bf2ef4ddc5ac318ec3e44a0d850c74
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-06-27 09:07:36 +0100

    repo name var

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-staging/resources/variables.tf
@@ -38,9 +38,12 @@ variable "github_token" {
   default     = ""
 }
 
-
 variable "team_name" {
   default = "correspondence"
+}
+
+variable "repo_name" {
+  default = "contact-moj"
 }
 
 variable "business_unit" {
