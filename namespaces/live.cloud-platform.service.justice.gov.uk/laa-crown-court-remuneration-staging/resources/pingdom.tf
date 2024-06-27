1ae8501dfe2d3eb2c57f8a3ad46fec1dacbd24c7
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2024-06-27 12:00:16 +0100

    Add CCR Staging Pingdom

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-staging/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-crown-court-remuneration-staging" {
+  type                     = "http"
+  name                     = "Crown Court Remuneration Staging"
+  host                     = "ccr-staging.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ccr/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  probefilters             = "region:EU"
+  integrationids           = [135401]
+}
