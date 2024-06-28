18a494d7b01dce0e9d09878242ca5afdc1f56b75
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-06-28 12:57:37 +0100

    remove comment

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/service-account-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/service-account-github.tf
@@ -4,8 +4,6 @@ module "serviceaccount_github" {
   namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  # This module is also used in the staging namespace, so we must change the
-  # secret names here in order to not overwrite the existing ones.
   serviceaccount_name = "github-serviceaccount"
   role_name = "github-serviceaccount"
   rolebinding_name = "github-serviceaccount"
