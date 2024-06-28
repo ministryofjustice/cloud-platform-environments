947b108e6aab1a29ca849e89b881d4cf4088cafb
carlosmoj <carlos.fernandez@digital.justice.gov.uk>
2024-06-28 14:32:31 +0100

    small changes to enable access to AP resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/mscproject-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mscproject-dev/resources/ecr.tf
@@ -13,6 +13,7 @@ module "ecr" {
   # OpenID Connect configuration
   oidc_providers      = ["github"]
   github_repositories = ["MScProject"]
+  github_environments = ["dev"]
 
   # Tags
   business_unit          = var.business_unit
