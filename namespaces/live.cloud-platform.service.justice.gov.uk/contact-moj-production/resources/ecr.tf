55af61b7a913380ff0cee85437a4345fa1b53196
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-06-28 13:34:07 +0100

    remove change

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-production/resources/ecr.tf
@@ -6,7 +6,7 @@
  */
 module "contact-moj_ecr_credentials" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.0.0"
-  repo_name = var.repo_name
+  repo_name = "contact-moj-ecr"
 
   providers = {
     aws = aws.london
