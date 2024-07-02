cbe850a0e034b7c20481f27e2a4895be09860c3b
james Hart <james.hart@digital.justice.gov.uk>
2024-07-02 17:03:09 +0100

    change module name

--- namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/serviceaccount-circleci.tf
@@ -62,7 +62,7 @@ locals {
   ]
 }
 
-module "serviceaccount" {
+module "serviceaccountcircleci" {
   source               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
   namespace            = var.namespace
   kubernetes_cluster   = var.kubernetes_cluster
