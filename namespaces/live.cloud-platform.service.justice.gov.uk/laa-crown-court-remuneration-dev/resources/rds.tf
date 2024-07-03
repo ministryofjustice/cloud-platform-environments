7787d49a7a9759cd9a884dc537e87973c2fb0503
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-07-03 12:06:27 +0100

    chore: bump cloud-platform-terraform-rds-instance module to 7.0.0 in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-dev/resources/rds.tf
@@ -1,5 +1,5 @@
 module "rds-mtn" {
-  source   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=migration"
+  source   = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.0.0"
   vpc_name = var.vpc_name
 
   application            = var.application
