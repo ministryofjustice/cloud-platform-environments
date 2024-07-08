585aed2c718ad465ee38d5275fae97fea71b0116
Jesus Laiz <zheileman@users.noreply.github.com>
2024-07-08 10:20:07 +0100

    Migrate service account token (#24164)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-complexity-of-need-preprod/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-complexity-of-need-preprod/resources/serviceaccount-circleci.tf
@@ -4,56 +4,11 @@ module "serviceaccount" {
   namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
+  serviceaccount_token_rotated_date = "08-07-2024"
+
+  serviceaccount_name = "circleci-migrated"
+
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   # github_repositories = ["my-repo"]
-
-  # This is a copy-and-paste of the default serviceaccount_rules
-  # See: https://github.com/ministryofjustice/cloud-platform-terraform-serviceaccount/blob/main/variables.tf
-  # The only difference is that we've also granted access to "configmaps" resources
-  serviceaccount_rules = [
-    {
-      api_groups = [""]
-      resources = [
-        "pods/portforward",
-        "deployment",
-        "secrets",
-        "services",
-        "pods",
-        "configmaps",
-      ]
-      verbs = [
-        "patch",
-        "get",
-        "create",
-        "update",
-        "delete",
-        "list",
-        "watch",
-      ]
-    },
-    {
-      api_groups = [
-        "extensions",
-        "apps",
-        "batch",
-        "networking.k8s.io",
-      ]
-      resources = [
-        "deployments",
-        "ingresses",
-        "cronjobs",
-        "jobs",
-      ]
-      verbs = [
-        "get",
-        "update",
-        "delete",
-        "create",
-        "patch",
-        "list",
-        "watch",
-      ]
-    },
-  ]
 }
