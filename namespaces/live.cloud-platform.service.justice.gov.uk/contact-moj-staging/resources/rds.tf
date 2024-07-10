46d6477a11d8942d7dca40e4a8e0e824da7f8bea
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-07-10 11:28:10 +0100

    upgrade to 13.14

--- namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/contact-moj-staging/resources/rds.tf
@@ -16,14 +16,14 @@ module "contact-moj_rds" {
   db_instance_class          = "db.t4g.micro"
   db_max_allocated_storage   = "500"
   db_engine                  = "postgres"
-  db_engine_version          = "12.18"
-  rds_family                 = "postgres12"
+  db_engine_version          = "13.14"
+  rds_family                 = "postgres13"
   db_backup_retention_period = "7"
   db_name                    = "contact_moj_staging"
   enable_rds_auto_start_stop = true
 
   allow_major_version_upgrade = true
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
 
   providers = {
     aws = aws.london
