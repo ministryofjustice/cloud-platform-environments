c36139c13778b910bec822f12bf8a814fa273456
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-07-12 14:03:09 +0100

    set to false prepare for major upgrade

--- namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/peoplefinder-development/resources/rds.tf
@@ -24,7 +24,7 @@ module "peoplefinder_rds" {
 
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "true"
-  prepare_for_major_upgrade = "true"
+  prepare_for_major_upgrade = "false"
 
   providers = {
     aws = aws.london
