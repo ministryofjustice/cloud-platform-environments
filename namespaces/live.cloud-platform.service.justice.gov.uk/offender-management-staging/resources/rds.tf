332c97751747d93936051e137b03d1bf06f35908
Jesus Laiz <zheileman@users.noreply.github.com>
2024-07-16 16:04:21 +0100

    Remove references to test2 namespace (#24486)
    The `offender-management-test2` namespace is about to be deleted as no longer used.
    
    Also prepare to remove ECR repo (`deletion_protection = false`) as it is not used.

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/rds.tf
@@ -69,22 +69,3 @@ resource "kubernetes_secret" "allocation-rds-test" {
     postgres_password     = module.allocation-rds.database_password
   }
 }
-
-resource "kubernetes_secret" "allocation-rds-test2" {
-  metadata {
-    name      = "allocation-rds-instance-output"
-    namespace = "offender-management-test2"
-  }
-
-  data = {
-    rds_instance_endpoint = module.allocation-rds.rds_instance_endpoint
-    rds_instance_address  = module.allocation-rds.rds_instance_address
-    database_name         = module.allocation-rds.database_name
-    database_username     = module.allocation-rds.database_username
-    database_password     = module.allocation-rds.database_password
-    postgres_name         = module.allocation-rds.database_name
-    postgres_host         = module.allocation-rds.rds_instance_address
-    postgres_user         = module.allocation-rds.database_username
-    postgres_password     = module.allocation-rds.database_password
-  }
-}
