7577200fba67d8f59a2beb0592f359c0a4ca6758
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-07-16 14:41:39 +0100

    Revert "CPR-388 Use correct sns topic for delius PROD (#24471)"
    This reverts commit 61e698adf27aabbae7ba3d7f17a3e7711b10c0e6.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/data.tf
@@ -5,7 +5,3 @@ data "aws_ssm_parameter" "court-case-events-topic-arn" {
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-prod/topic-arn"
 }
-
-data "aws_sns_topic" "probation-offender-events" {
-  name = "cloud-platform-Digital-Prison-Services-c2d997878cd24eef94e60f1404977153"
-}
