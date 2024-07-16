9e3396cb36cfca4c971842df615e3cffa85c324c
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-07-16 10:17:49 +0100

    Get the DB name

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-staging/resources/rds.tf
@@ -49,6 +49,7 @@ resource "kubernetes_secret" "rds" {
 
   data = {
     rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_name         = module.rds.database_name
     database_username     = module.rds.database_username
     database_password     = module.rds.database_password
     rds_instance_address  = module.rds.rds_instance_address
