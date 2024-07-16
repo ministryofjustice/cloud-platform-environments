78561dc107ef9d6a1121ecfcdd26eb362b71f837
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-07-16 14:06:17 +0100

    CPR-388 Use correct sns topic for probation

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
@@ -6,6 +6,6 @@ data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-preprod/topic-arn"
 }
 
-data "aws_sns_topic" "prison-offender-events" {
-  name = "cloud-platform-Digital-Prison-Services-d448bb61bb0b69b82fb19a3fa574e7f9"
+data "aws_sns_topic" "probation-offender-events" {
+  name = "cloud-platform-Digital-Prison-Services-dbe10e8d9c1f4d100f0c723d5d9b754e"
 }
