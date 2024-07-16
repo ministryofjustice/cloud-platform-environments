1912e694d0e5a124411b4a2ff0f36737b378731e
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-07-16 13:33:16 +0100

    CPR-388 Add sns topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "court-case-events-topic-arn" {
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-preprod/topic-arn"
 }
+
+data "aws_sns_topic" "prison-offender-events" {
+  name = "cloud-platform-Digital-Prison-Services-d448bb61bb0b69b82fb19a3fa574e7f9"
+}
