a349ec4f21b2e3df3d832d63b3413c7da616f49a
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2024-07-17 16:00:36 +0100

    Update github team for CCLF dev console access
    The GithubTeam value in default_tags in main.tf is set to the team
    previously responsible for this application. This updates the value
    to match the team defined in 01-rbac.yaml so that all relevant developers
    can access the console for this application.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-dev/resources/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam = "laa-aws-infrastructure"
+      GithubTeam = "laa-lz-cp-migration"
     }
   }
 }
