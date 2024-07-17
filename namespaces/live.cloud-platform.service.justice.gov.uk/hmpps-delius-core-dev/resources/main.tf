5d97dc6707b19e4032ea9e9003418a5ba31f13ea
Marcus Aspin <maspin@unilink.com>
2024-07-17 14:15:19 +0100

    DST-17030 Add hmpps-delius-core-dev namespace (#24534)
    * DST-17030 Add hmpps-delius-core-dev namespace
    
    * Remove unused provider

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-dev/resources/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      GithubTeam = var.team_name
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-dev/resources/variables.tf
@@ -0,0 +1,76 @@
+variable "vpc_name" {}
+variable "kubernetes_cluster" {}
+variable "eks_cluster_name" {}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "hmpps-delius-core"
+}
+
+variable "namespace" {
+  default = "hmpps-delius-core-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "unilink"
+}
+
+variable "environment_name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "hmpps-unilink-delius@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "ndelius_service_team"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the Github Terraform provider"
+  default     = ""
+}
+
+variable "github_environment" {
+  description = "Name of the GitHub environment linked to this namespace"
+  default     = "dev"
+}
+
+variable "github_actions_secret_kube_cluster" {
+  description = "The name of the github actions secret containing the kubernetes cluster name"
+  default     = "KUBE_CLUSTER"
+}
+
+variable "github_actions_secret_kube_namespace" {
+  description = "The name of the github actions secret containing the kubernetes namespace name"
+  default     = "KUBE_NAMESPACE"
+}
+
+variable "github_actions_secret_kube_cert" {
+  description = "The name of the github actions secret containing the serviceaccount ca.crt"
+  default     = "KUBE_CERT"
+}
+
+variable "github_actions_secret_kube_token" {
+  description = "The name of the github actions secret containing the serviceaccount token"
+  default     = "KUBE_TOKEN"
+}
