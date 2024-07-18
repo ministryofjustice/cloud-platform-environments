cc1cb94a61cbfeaf7adbb91d857febc4a3e1e2bc
Andrew Larcombe <andrew.larcombe@digital.justice.gov.uk>
2024-07-18 15:22:29 +0100

    moves lock config inside main module config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/s3.tf
@@ -13,15 +13,14 @@ module "s3" {
   infrastructure_support = var.infrastructure_support
   versioning = true
 
-}
+  resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+    bucket = module.s3.bucket_name
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = module.s3.bucket_name
+    versioning_configuration {
+      status = "Enabled"
+    }
 
-  versioning_configuration {
-    status = "Enabled"
   }
-
 }
 
 # Sets the governance mode to 1 day for testing on dev
