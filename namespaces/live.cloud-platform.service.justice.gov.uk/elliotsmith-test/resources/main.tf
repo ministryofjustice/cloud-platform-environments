7031a2c1697fc494d163abf5ed8291ccef57ddc6
Elliot Smith <elliot.smith2@justice.gov.uk>
2024-07-22 10:27:48 +0100

    Give research-and-development MoJ github team access to AWS

--- namespaces/live.cloud-platform.service.justice.gov.uk/elliotsmith-test/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/elliotsmith-test/resources/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = "research-and-development"
     }
   }
 }
@@ -22,6 +23,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = "research-and-development"
     }
   }
 }
@@ -34,6 +36,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = "research-and-development"
     }
   }
 }
