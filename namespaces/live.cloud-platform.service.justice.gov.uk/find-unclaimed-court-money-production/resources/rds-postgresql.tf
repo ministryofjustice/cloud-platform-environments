4b31cb12a3725d41d2210371b24c2fd4ccf7370d
N.Preddy <nick.preddy@digital.justice.gov.uk>
2024-07-22 16:58:21 +0100

    Update rds-postgresql.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/find-unclaimed-court-money-production/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/find-unclaimed-court-money-production/resources/rds-postgresql.tf
@@ -6,15 +6,17 @@
  */
 
 module "rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
-  vpc_name               = var.vpc_name
-  team_name              = var.team_name
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  namespace              = var.namespace
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.1"
+  vpc_name                  = var.vpc_name
+  team_name                 = var.team_name
+  business_unit             = var.business_unit
+  application               = var.application
+  is_production             = var.is_production
+  environment_name          = var.environment
+  infrastructure_support    = var.infrastructure_support
+  namespace                 = var.namespace
+  prepare_for_major_upgrade = true
+  db_engine                 = "postgres"
 
   # If the rds_name is not specified a random name will be generated ( cp-* )
   # Changing the RDS name requires the RDS to be re-created (destroy + create)
@@ -24,14 +26,14 @@ module "rds" {
   performance_insights_enabled = true
 
   # change the postgres version as you see fit.
-  db_engine_version = "14.11"
+  db_engine_version = "15.7"
 
   # change the instance class as you see fit.
   db_instance_class = "db.t4g.small"
 
   # rds_family should be one of: postgres10, postgres11, postgres12, postgres13, postgres14
   # Pick the one that defines the postgres version the best
-  rds_family = "postgres14"
+  rds_family = "postgres15"
 
   # Some engines can't apply some parameters without a reboot(ex postgres9.x cant apply force_ssl immediate).
   # You will need to specify "pending-reboot" here, as default is set to "immediate".
