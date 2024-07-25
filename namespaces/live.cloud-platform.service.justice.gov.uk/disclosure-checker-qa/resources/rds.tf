49f878fc29cef15a05fd9ddbe7952ef689b6bbff
MeenaModhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-07-25 10:25:35 +0100

    upgrade postgres16

--- namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-qa/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/disclosure-checker-qa/resources/rds.tf
@@ -15,11 +15,11 @@ module "rds-instance" {
   db_instance_class          = "db.t4g.micro"
   db_max_allocated_storage   = "500"
   db_engine                  = "postgres"
-  db_engine_version          = "15"
-  rds_family                 = "postgres15"
+  db_engine_version          = "16"
+  rds_family                 = "postgres16"
 
   enable_rds_auto_start_stop = true
-  prepare_for_major_upgrade = false
+  prepare_for_major_upgrade = true
 
   providers = {
     # Can be either "aws.london" or "aws.ireland"
