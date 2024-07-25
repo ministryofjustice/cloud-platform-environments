e1aed9feb29d31943b52ea64b7f71f6350bfd16f
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2024-07-25 10:07:30 +0100

    PI-2376: Add tags for SNS topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
@@ -383,6 +383,15 @@ module "sns_topic" {
   # Configuration
   topic_display_name = "integration-api-alert-topic"
   encrypt_sns_kms    = true
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the topic
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
 
 module "notify_slack" {
