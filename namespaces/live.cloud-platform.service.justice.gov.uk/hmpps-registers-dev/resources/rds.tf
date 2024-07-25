bdc2b359b90868697c80a5de3bb07b2e00ad4f93
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2024-07-25 10:44:49 +0100

    MAP-1419 (#24749)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-dev/resources/rds.tf
@@ -53,7 +53,7 @@ module "prisons_rds" {
   db_instance_class          = "db.t4g.micro"
   db_max_allocated_storage   = "500"
   deletion_protection        = true
-  prepare_for_major_upgrade  = true
+  prepare_for_major_upgrade  = false
   rds_family                 = "postgres16"
   db_engine                  = "postgres"
   db_engine_version          = "16.3"
