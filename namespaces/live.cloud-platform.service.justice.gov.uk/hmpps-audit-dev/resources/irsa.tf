344e4516d674c06482571904ea874de0bb2a66aa
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-07-26 10:51:52 +0100

    HAAR-2823 Added service account for audit S3 bucket in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
@@ -18,3 +18,45 @@ module "hmpps-audit-api-irsa" {
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
 }
+
+module "s3-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "audit-s3-${var.environment-name}"
+  namespace            = var.namespace # this is also used as a tag
+  role_policy_arns = {
+    s3 = aws_iam_policy.allow-irsa-read.arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "aws_iam_policy" "allow-irsa-read" {
+  name        = "audit-read-only"
+  path        = "/cloud-platform/"
+  policy      = data.aws_iam_policy_document.document.json
+  description = "Policy for reading audit json files from audit s3 bucket"
+}
+
+data "aws_iam_policy_document" "document" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      module.s3.bucket_arn,
+      "${module.s3.bucket_arn}/*",
+    ]
+  }
+}
