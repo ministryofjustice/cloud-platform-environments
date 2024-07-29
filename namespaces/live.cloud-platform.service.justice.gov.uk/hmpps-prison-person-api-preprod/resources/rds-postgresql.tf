8c334f5edf910590dd0079308c93c5c02fced99c
Jon Brighton <brightonsbox@hotmail.com>
2024-07-29 15:09:55 +0100

    CDPS-857: Using GP3 storage volume

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-preprod/resources/rds-postgresql.tf
@@ -5,7 +5,7 @@
  *
  */
 module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=upgrade-to-gp3"
 
   # VPC configuration
   vpc_name = var.vpc_name
