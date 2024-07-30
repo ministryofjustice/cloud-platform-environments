6d2f66be7e9be49a14ef3a87b1495421dec3ea09
Marcus Aspin <maspin@unilink.com>
2024-07-30 10:53:51 +0100

    PI-2359 Add bedrock policy to IRSA role (dev) (#24885)
    * PI-2359 Add bedrock policy to IRSA role (dev)
    
    * Swap role ARN for role name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-dev/resources/bedrock.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-dev/resources/bedrock.tf
@@ -41,3 +41,21 @@ resource "aws_iam_role_policy" "opensearch_bedrock_policy" {
     ]
   })
 }
+
+data "aws_arn" "opensearch_irsa_role" {
+  arn = module.opensearch.irsa_role_arn
+}
+
+resource "aws_iam_role_policy" "opensearch_bedrock_irsa_policy" {
+  role = trimprefix(data.aws_arn.opensearch_irsa_role.resource, "role/")
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "iam:PassRole",
+        Resource = aws_iam_role.opensearch_bedrock_role.arn
+      }
+    ]
+  })
+}
