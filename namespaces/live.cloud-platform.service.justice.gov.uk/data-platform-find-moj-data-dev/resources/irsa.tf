2ba044f2f66221554f04ad3334db23c22be91a87
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-08-01 10:45:32 +0100

    Removed references to route53

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/irsa.tf
@@ -12,9 +12,8 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    rds     = module.rds.irsa_policy_arn
-    ecr     = module.ecr.irsa_policy_arn
-    route53 = aws_iam_policy.route53_irsa.arn
+    rds = module.rds.irsa_policy_arn
+    ecr = module.ecr.irsa_policy_arn
   }
 
   # Tags
