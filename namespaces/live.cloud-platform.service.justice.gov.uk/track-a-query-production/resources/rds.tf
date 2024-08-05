fb3a01c3b9a01b619718fae83c0b6ea7b793297f
N.Preddy <nick.preddy@digital.justice.gov.uk>
2024-08-05 14:58:12 +0100

    Update rds.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-production/resources/rds.tf
@@ -16,11 +16,11 @@ module "track_a_query_rds" {
   db_instance_class          = "db.t4g.small"
   db_max_allocated_storage   = "10000"
   db_engine                  = "postgres"
-  db_engine_version          = "15.6"
+  db_engine_version          = "16.1"
   db_backup_retention_period = "7"
   db_name                    = "track_a_query_production"
   prepare_for_major_upgrade  = true
-  rds_family                 = "postgres15"
+  rds_family                 = "postgres16"
 
   # use "allow_major_version_upgrade" when upgrading the major version of an engine
   allow_major_version_upgrade = "false"
@@ -44,8 +44,8 @@ module "track_a_query_rds_replica" {
   namespace                = var.namespace
   db_instance_class        = "db.t4g.small"
   db_max_allocated_storage = "10000"
-  rds_family               = "postgres12"
-  db_engine_version        = "12"
+  rds_family               = "postgres16"
+  db_engine_version        = "16.1"
 
   db_name             = null # "db_name": conflicts with replicate_source_db
   replicate_source_db = module.track_a_query_rds.db_identifier
