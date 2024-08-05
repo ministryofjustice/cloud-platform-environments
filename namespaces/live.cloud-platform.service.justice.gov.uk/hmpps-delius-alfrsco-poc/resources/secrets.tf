7d78b407f2826b5dbb466f67ca18eea416953d79
George Taylor <george.taylor@digital.justice.gov.uk>
2024-08-05 14:48:12 +0100

    :sparkles: Add k8s service for RDS endpoint

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/secrets.tf
@@ -15,3 +15,22 @@ module "secret" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
+
+resource "kubernetes_service" "rds_endpoint" {
+  # set up a kube service to expose the RDS endpoint
+  metadata {
+    name      = "rds-endpoint"
+    namespace = var.namespace
+  }
+
+  spec {
+    external_name = module.rds_alfresco.rds_instance_endpoint
+
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+    session_affinity = "None"
+    type             = "ExternalName"
+  }
+}
