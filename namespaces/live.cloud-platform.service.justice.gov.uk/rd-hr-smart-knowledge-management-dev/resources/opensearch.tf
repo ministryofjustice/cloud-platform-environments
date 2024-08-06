b06af531c4dcce98d530274950785145c559855d
Elliot Smith <elliot.smith2@justice.gov.uk>
2024-08-06 17:49:43 +0100

    Add mandatory snapshot bucket

--- namespaces/live.cloud-platform.service.justice.gov.uk/rd-hr-smart-knowledge-management-dev/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/rd-hr-smart-knowledge-management-dev/resources/opensearch.tf
@@ -1,3 +1,17 @@
+module "s3_snapshot_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0" # use the latest release
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+# Create the domain
 module "opensearch" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.5.1"
 
@@ -7,6 +21,7 @@ module "opensearch" {
 
   # Cluster configuration
   engine_version      = "OpenSearch_2.9"
+  snapshot_bucket_arn = module.s3_snapshot_bucket.bucket_arn
   proxy_count         = 2
 
   # Non-production cluster configuration
