712ebb6c6ed5a5a995767beed59ccad5f141fb1c
Elliot Smith <elliot.smith2@justice.gov.uk>
2024-08-06 17:37:53 +0100

    Add opensearch resource

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/rd-hr-smart-knowledge-management-dev/resources/opensearch.tf
@@ -0,0 +1,42 @@
+module "opensearch" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.5.1"
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version      = "OpenSearch_2.9"
+  proxy_count         = 2
+
+  # Non-production cluster configuration
+  cluster_config = {
+    instance_count = 2
+    instance_type  = "t3.small.search"
+  }
+
+  ebs_options = {
+    volume_size = 10
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+# Output the proxy URL
+resource "kubernetes_secret" "opensearch" {
+  metadata {
+    name      = "${var.team_name}-opensearch-proxy-url"
+    namespace = var.namespace
+  }
+
+  data = {
+    proxy_url = module.opensearch.proxy_url
+  }
+}
