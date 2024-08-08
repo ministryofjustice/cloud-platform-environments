eb8c8764dc88e9f52373d2a5d9f2cac01cc5cd9e
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-08-08 09:29:12 +0100

    CVSL-2068: Update postgres version to 16.3.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-for-early-release-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-for-early-release-dev/resources/rds.tf
@@ -11,7 +11,7 @@ module "assess-for-early-release_rds" {
   allow_minor_version_upgrade = true
   allow_major_version_upgrade = false
   db_instance_class           = "db.t4g.small"
-  db_engine_version           = "15.6"
+  db_engine_version           = "16.3"
   rds_family                  = "postgres15"
   prepare_for_major_upgrade   = false
   db_password_rotated_date    = "14-02-2023"
