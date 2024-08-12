3c980fbb5a683d50f7bbfa802c7b8e91742e8955
George Taylor <george.taylor@digital.justice.gov.uk>
2024-08-12 19:03:58 +0100

    tidy

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/opensearch.tf
@@ -43,3 +43,48 @@ resource "kubernetes_secret" "opensearch" {
     proxy_url = module.opensearch.proxy_url
   }
 }
+
+#######################################
+# s3 bucket for OpenSearch snapshots
+#######################################
+
+module "s3_opensearch_snapshots_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0" # use the latest release
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "s3_opensearch_snapshots_bucket" {
+  metadata {
+    name      = "s3-opensearch-snapshots-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    BUCKET_ARN  = module.s3_opensearch_snapshots_bucket.bucket_arn
+    BUCKET_NAME = module.s3_opensearch_snapshots_bucket.bucket_name
+    ACCESSKEY   = aws_iam_access_key.opensearch_snapshots.id
+    SECRETKEY   = aws_iam_access_key.opensearch_snapshots.secret
+  }
+}
+
+resource "aws_iam_user" "opensearch_snapshots" {
+  name = "${var.namespace}-opensearch_snapshots_user"
+  path = "/system/${var.namespace}-opensearch_snapshots_user/"
+}
+
+resource "aws_iam_access_key" "opensearch_snapshots" {
+  user = aws_iam_user.opensearch_snapshots.name
+}
+
+resource "aws_iam_user_policy_attachment" "opensearch_snapshots" {
+  policy_arn = module.s3_opensearch_snapshots_bucket.irsa_policy_arn
+  user       = aws_iam_user.opensearch_snapshots.name
+}
