5c710b56584a459be36bde154f3ccc77bce9a254
Murdo <109604278+murdo-moj@users.noreply.github.com>
2024-08-14 09:51:13 +0100

    Updated team name variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-eng-uploader-dev/resources/variables.tf
@@ -23,7 +23,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "prison-probation-data-engineering"
+  default     = "data-engineering"
 }
 
 variable "environment" {
