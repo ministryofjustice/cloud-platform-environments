2c36de6265ab4e2c24c736b37e1b86501e4d11b5
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-08-14 05:54:18 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
@@ -28,7 +28,10 @@ module "s3-irsa" {
   # IRSA configuration
   service_account_name = "audit-s3-${var.environment-name}"
   namespace            = var.namespace # this is also used as a tag
-  role_policy_arns = [aws_iam_policy.allow-irsa-read-write.arn, module.s3.irsa_policy_arn]
+  role_policy_arns = {
+    s3_1 = aws_iam_policy.allow-irsa-read-write.arn,
+    s3_2 = module.s3.irsa_policy_arn
+  }
 
   # Tags
   business_unit          = var.business_unit
