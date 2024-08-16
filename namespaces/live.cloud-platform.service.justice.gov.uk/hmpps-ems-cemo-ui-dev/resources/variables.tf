adcaef2621cca544f2842a626a44a9ec19030308
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-08-16 14:08:57 +0100

    Remove secrets manager from hmpps-ems-cemo-ui-dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/variables.tf
@@ -71,7 +71,3 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
-
-variable "eks_cluster_name" {
-  description = "The name of the cluster"
-}
