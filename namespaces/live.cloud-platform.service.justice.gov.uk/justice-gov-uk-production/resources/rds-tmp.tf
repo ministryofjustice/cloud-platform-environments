d8db958d4fe6e16c0935ac16e7f96b82ac712e47
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-08-20 10:54:40 +0100

    Correct db_engine_version number.

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-production/resources/rds-tmp.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-production/resources/rds-tmp.tf
@@ -12,7 +12,7 @@ module "rds_tmp" {
 
   # general options
   db_engine                   = "mariadb"
-  db_engine_version           = "10.11.6"
+  db_engine_version           = "10.11.8"
   rds_family                  = "mariadb10.11"
   db_instance_class           = "db.t4g.medium"
   db_allocated_storage        = "20"
