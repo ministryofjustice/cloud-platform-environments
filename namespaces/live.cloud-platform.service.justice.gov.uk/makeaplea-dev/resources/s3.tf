ae2f5e03471e2303939d738533511d8ef15b7261
Mateusz Kolakowski <mateusz.kolakowski@justice.gov.uk>
2024-08-21 11:40:42 +0100

    Make a plea dev: delete acl from s3 bucket (#25405)

--- namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/makeaplea-dev/resources/s3.tf
@@ -13,7 +13,6 @@ module "s3_bucket" {
   environment_name              = var.environment
   infrastructure_support        = var.infrastructure_support
   namespace                     = var.namespace
-  acl                           = "public-read"
   enable_allow_block_pub_access = false
 
   bucket_policy = <<EOF
