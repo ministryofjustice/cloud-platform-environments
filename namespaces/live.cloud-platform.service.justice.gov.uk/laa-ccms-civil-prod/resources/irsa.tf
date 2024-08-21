9d3e47967152a01d0fbefd2c7e191e8b1db559b4
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-08-21 16:47:26 +0100

    remove ecr

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil-prod/resources/irsa.tf
@@ -12,7 +12,7 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    ecr = module.ecr.irsa_policy_arn
+    s3  = module.laa_ccms_documents.irsa_policy_arn
   }
 
   # Tags
