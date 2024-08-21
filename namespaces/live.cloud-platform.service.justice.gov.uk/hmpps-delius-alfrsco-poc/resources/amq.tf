4d5c6d5edab7c914e8869d9345c02036f5a7e83d
George Taylor <george.taylor@digital.justice.gov.uk>
2024-08-21 14:05:37 +0100

    Update amq.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/amq.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/amq.tf
@@ -12,7 +12,7 @@ module "alfresco_amq" {
   cluster_name = var.eks_cluster_name
 
   engine_type        = "ActiveMQ"
-  engine_version     = "5.18.4"
+  engine_version     = "5.18"
   host_instance_type = "mq.m5.large"
   deployment_mode    = "ACTIVE_STANDBY_MULTI_AZ"
 
