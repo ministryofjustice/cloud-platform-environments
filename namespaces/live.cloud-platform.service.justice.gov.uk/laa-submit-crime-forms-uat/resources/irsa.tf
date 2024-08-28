c2ac3c7926d7f4f1cb950c6a77e825f6aca3f466
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-28 12:00:07 +0100

    laa-submit-crime-forms-uat: Add service account pod

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-uat/resources/irsa.tf
@@ -24,6 +24,15 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
+# set up the service pod
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name
+}
+
 resource "kubernetes_secret" "irsa" {
   metadata {
     name      = "irsa-output"
