18554d52e12ff9e92f2c3152b9d65553ff9d41e3
David Conneely <899881+dconneely@users.noreply.github.com>
2024-08-28 23:25:42 +0100

    Add missing eks_cluster_name variable needed by secrets module

--- namespaces/live.cloud-platform.service.justice.gov.uk/davidconneely-sandbox-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/davidconneely-sandbox-dev/resources/variables.tf
@@ -3,6 +3,11 @@ variable "vpc_name" {
   type        = string
 }
 
+variable "eks_cluster_name" {
+  description = "Required by the cloud-platform-terraform-secrets-manager module"
+  type        = string
+}
+
 variable "kubernetes_cluster" {
   description = "Kubernetes cluster name for references to secrets for service accounts"
   type        = string
