6716c126a4fe80ecb96c77dc2f0726c5a17b6922
sameermoj <sameer.srivastava@digital.justice.gov.uk>
2024-08-28 16:12:00 +0100

    CPR-395 : Adding FIFO SQS Queues for Dev env. of hmpps-person-record

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/data.tf
@@ -2,6 +2,10 @@ data "aws_ssm_parameter" "court-case-events-topic-arn" {
   name = "/court-probation-dev/topic-arn"
 }
 
+data "aws_ssm_parameter" "court-cases-topic-arn" {
+  name = "/court-probation-dev/court-cases-topic-arn"
+}
+
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-dev/topic-arn"
 }
