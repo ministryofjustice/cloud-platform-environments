df7b2dd27d266da4e0c7cc9626b704e830203a1d
David Conneely <899881+dconneely@users.noreply.github.com>
2024-08-28 23:03:00 +0100

    Add secrets to davidconneely-sandbox

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/davidconneely-sandbox-dev/resources/secrets.tf
@@ -0,0 +1,29 @@
+module "secrets_manager" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "dcs-auth-tls-clkey" = {
+      description             = "Outgoing client private key",
+      recovery_window_in_days = 0
+      k8s_secret_name         = "dcs-auth-tls-clkey"
+    },
+    "dcs-auth-tls-clcrt" = {
+      description             = "Outgoing client public certificate",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "dcs-auth-tls-clcrt"
+    },
+    "dcs-auth-tls-cacrt" = {
+      description             = "Incoming client CA public certificate",
+      recovery_window_in_days = 30
+      k8s_secret_name         = "dcs-auth-tls-cacrt"
+    },
+  }
+}
