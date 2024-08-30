78d4e2532a0642eabf4645e5413c888f8ab2427a
Joel Stobart <>
2024-08-30 11:27:03 +0100

    Add a new RDS Instance using guides from:
    https://github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.0.0
    https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-to-a-new-major-database-version-upgrade-paths

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
@@ -1,36 +1,38 @@
-module "calculate_release_dates_api_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.0.0"
-  vpc_name               = var.vpc_name
-  db_instance_class      = "db.t3.small"
-  team_name              = var.team_name
+module "calculate_release_dates_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.1.0"
+
+  # VPC configuration
+  vpc_name = var.vpc_name
+
+  # Database configuration
+  db_engine                = "postgres"
+  db_engine_version        = "13.3"
+  rds_family               = "postgres13"
+  db_instance_class        = "db.t4g.xlarge"
+  db_max_allocated_storage = "1000"
+
+  # Tags
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
+  team_name              = var.team_name
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-  db_engine              = "postgres"
-  db_engine_version      = "13"
-  rds_family             = "postgres13"
-
-  db_password_rotated_date = "17-02-2023"
-
-  providers = {
-    aws = aws.london
-  }
 }
 
-resource "kubernetes_secret" "calculate_release_dates_api_rds" {
+resource "kubernetes_secret" "calculate_release_dates_rds" {
   metadata {
-    name      = "rds-instance-output"
+    name      = "calculate-release-dates-rds-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    rds_instance_endpoint = module.calculate_release_dates_api_rds.rds_instance_endpoint
-    database_name         = module.calculate_release_dates_api_rds.database_name
-    database_username     = module.calculate_release_dates_api_rds.database_username
-    database_password     = module.calculate_release_dates_api_rds.database_password
-    rds_instance_address  = module.calculate_release_dates_api_rds.rds_instance_address
+    rds_instance_endpoint = module.calculate_release_dates_rds.rds_instance_endpoint
+    database_name         = module.calculate_release_dates_rds.database_name
+    database_username     = module.calculate_release_dates_rds.database_username
+    database_password     = module.calculate_release_dates_rds.database_password
+    rds_instance_address  = module.calculate_release_dates_rds.rds_instance_address
+    url                   = "postgres://${module.calculate_release_dates_rds.database_username}:${module.calculate_release_dates_rds.database_password}@${module.calculate_release_dates_rds.rds_instance_endpoint}/${module.calculate_release_dates_rds.database_name}"
   }
 }
