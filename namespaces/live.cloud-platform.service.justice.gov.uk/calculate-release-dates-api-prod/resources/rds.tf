f4a4b1b37b5d62b3fad9bc9744f724e073dc386c
Joel Stobart <>
2024-08-30 11:30:52 +0100

    Tweaks to match previous file

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
@@ -1,15 +1,8 @@
-module "calculate_release_dates_rds" {
+module "calculate_release_dates_api_rds" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.1.0"
 
   # VPC configuration
   vpc_name = var.vpc_name
-
-  # Database configuration
-  db_engine                = "postgres"
-  db_engine_version        = "13.3"
-  rds_family               = "postgres13"
-  db_instance_class        = "db.t4g.xlarge"
-  db_max_allocated_storage = "1000"
 
   # Tags
   business_unit          = var.business_unit
@@ -19,6 +12,18 @@ module "calculate_release_dates_rds" {
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+
+  # Database configuration
+  db_engine                = "postgres"
+  db_engine_version        = "13"
+  rds_family               = "postgres13"
+  db_instance_class        = "db.t4g.xlarge"
+
+  db_password_rotated_date = "17-02-2023"
+
+  providers = {
+    aws = aws.london
+  }
 }
 
 resource "kubernetes_secret" "calculate_release_dates_rds" {
@@ -28,11 +33,11 @@ resource "kubernetes_secret" "calculate_release_dates_rds" {
   }
 
   data = {
-    rds_instance_endpoint = module.calculate_release_dates_rds.rds_instance_endpoint
-    database_name         = module.calculate_release_dates_rds.database_name
-    database_username     = module.calculate_release_dates_rds.database_username
-    database_password     = module.calculate_release_dates_rds.database_password
-    rds_instance_address  = module.calculate_release_dates_rds.rds_instance_address
-    url                   = "postgres://${module.calculate_release_dates_rds.database_username}:${module.calculate_release_dates_rds.database_password}@${module.calculate_release_dates_rds.rds_instance_endpoint}/${module.calculate_release_dates_rds.database_name}"
+    rds_instance_endpoint = module.calculate_release_dates_api_rds.rds_instance_endpoint
+    database_name         = module.calculate_release_dates_api_rds.database_name
+    database_username     = module.calculate_release_dates_api_rds.database_username
+    database_password     = module.calculate_release_dates_api_rds.database_password
+    rds_instance_address  = module.calculate_release_dates_api_rds.rds_instance_address
+    url                   = "postgres://${module.calculate_release_dates_api_rds.database_username}:${module.calculate_release_dates_api_rds.database_password}@${module.calculate_release_dates_api_rds.rds_instance_endpoint}/${module.calculate_release_dates_api_rds.database_name}"
   }
 }
