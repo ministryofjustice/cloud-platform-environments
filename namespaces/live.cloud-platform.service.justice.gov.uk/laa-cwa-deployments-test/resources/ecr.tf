275975f06f0e8fee21da8c9bf97517d58db16200
vimal <vimal.suresh@digital.justice.gov.uk>
2024-08-30 13:20:25 +0100

    remove github oidc provider

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-deployments-test/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-deployments-test/resources/ecr.tf
@@ -13,7 +13,7 @@ module "ecr" {
   namespace = var.namespace
 
   # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
-  oidc_providers = ["github", "circleci"]
+  oidc_providers = ["circleci"]
 
   # REQUIRED: GitHub repositories that push to this container repository
   github_repositories = ["laa-cwa-feature-tests", "laa-cwa"]
