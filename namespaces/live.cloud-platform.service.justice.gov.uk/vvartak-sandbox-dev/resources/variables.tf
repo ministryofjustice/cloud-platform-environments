f41cce912e8504580f1e0caec34c67d66d0cdaa9
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2024-09-02 17:01:18 +0100

    Added a role with required privileges to see if this makes the secrets available.

--- namespaces/live.cloud-platform.service.justice.gov.uk/vvartak-sandbox-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vvartak-sandbox-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "laa-dces-team"
+  default     = "vvartak_sandbox"
 }
 
 variable "environment" {
@@ -41,7 +41,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "laa-dces@digital.justice.gov.uk"
+  default     = "vidyanand.vartak@digital.justice.gov.uk"
 }
 
 variable "is_production" {
