f783073331c4e574950f37db6b7d3d11800b1790
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2024-09-02 17:16:49 +0100

    Put back the old team name.

--- namespaces/live.cloud-platform.service.justice.gov.uk/vvartak-sandbox-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vvartak-sandbox-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "vvartak_sandbox"
+  default     = "laa-dces-team"
 }
 
 variable "environment" {
