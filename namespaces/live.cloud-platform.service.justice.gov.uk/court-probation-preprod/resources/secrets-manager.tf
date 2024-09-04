5801c6b4db6702fc8a5f8d30fff9e03f535f5e62
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-09-04 09:15:52 +0100

    Add application insights secret
    Add application insights connection string secret for Preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/secrets-manager.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/secrets-manager.tf
@@ -14,6 +14,11 @@ module "secrets_manager" {
       description             = "Contains the slack webhook url to send notifications",
       recovery_window_in_days = 7,
       k8s_secret_name         = "probation-in-court-preprod-slack-webhook"
+    },
+    "applicationinsights" = {
+      description             = "Contains the application insights connection string for preprod",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "applicationinsights-connection-string"
     }
   }
 }
