c966d7ef754f43409535467184517b706e2a72b0
Peter Hunt <naseberry@users.noreply.github.com>
2024-09-09 10:29:07 +0100

    AP-5156 Add LFA UAT secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/legal-framework-api-uat/resources/secret.tf
@@ -0,0 +1,35 @@
+
+module "secrets" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "secret_key_base" = {
+      description             = "Rails application secret key base",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "secret-key-base"
+    },
+    "postgres_username" = {
+      description             = "PostgreSQL username",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "postgres-username"
+    },
+    "postgres_password" = {
+      description             = "PostgreSQL password",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "postgres-password"
+    },
+    "sentry_dsn" = {
+      description             = "Sentry DSN",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "sentry-dsn"
+    },
+  }
+}
