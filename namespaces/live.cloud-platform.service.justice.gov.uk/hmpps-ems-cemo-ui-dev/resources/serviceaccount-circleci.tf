582e769f0b24aedb77239d5a1fb00cd6cb1c08a0
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-09-10 12:20:39 +0100

    Resolve conflicting module names in hmpps-ems-cemo-ui-dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/serviceaccount-circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/serviceaccount-circleci.tf
@@ -62,7 +62,7 @@ locals {
   ]
 }
 
-module "serviceaccount" {
+module "serviceaccount_circleci" {
   source               = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
   namespace            = var.namespace
   kubernetes_cluster   = var.kubernetes_cluster
