0232359d25f196102b2a4f48ca1bf2e364f055ed
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-09-10 09:54:55 +0100

    Add a rule set

--- namespaces/live.cloud-platform.service.justice.gov.uk/stability-inspector-dev/resources/service_account.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/stability-inspector-dev/resources/service_account.tf
@@ -4,4 +4,64 @@ module "serviceaccount" {
   namespace           = var.namespace
   kubernetes_cluster  = var.kubernetes_cluster
   github_repositories = [var.app_repo]
+
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+        "serviceaccounts",
+        "configmaps",
+        "persistentvolumeclaims",
+        "horizontalpodautoscalers",
+      ]
+      verbs = [
+        "update",
+        "patch",
+        "get",
+        "create",
+        "delete",
+        "list",
+        "watch",
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "batch",
+        "networking.k8s.io",
+        "monitoring.coreos.com",
+        "rbac.authorization.k8s.io",
+        "autoscaling",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "cronjobs",
+        "jobs",
+        "replicasets",
+        "statefulsets",
+        "networkpolicies",
+        "servicemonitors",
+        "prometheusrules",
+        "roles",
+        "rolebindings",
+        "horizontalpodautoscalers",
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list",
+        "watch",
+      ]
+    }
+  ]
 }
