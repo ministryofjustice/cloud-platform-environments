d63674d3990c6990b1c0bf57456bd5394b36eddd
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2024-09-11 10:20:27 +0100

    CPR-339 add delius unmerge event DEV

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/cpr-delius-merge-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/cpr-delius-merge-events-queue.tf
@@ -7,7 +7,7 @@ resource "aws_sns_topic_subscription" "cpr_delius_merge_domain_events_subscripti
   filter_policy = jsonencode({
     eventType = [
       "probation-case.merge.completed",
-      "probation-case-unmerge-completed"
+      "probation-case.unmerge.completed"
     ]
   })
 }
