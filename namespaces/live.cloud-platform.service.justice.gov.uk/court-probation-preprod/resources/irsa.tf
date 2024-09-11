02a5f76070c752a3ec8219960a894ce83be7cb98
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-09-11 08:17:50 +0100

    PIC-4134 add new IRSA for court-facing-api and move S3 and SNS permissions

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/irsa.tf
@@ -17,6 +17,35 @@ module "irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
+  service_account_name = "court-facing-api"
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the appropriate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = merge(
+    { s3_cpg = module.crime-portal-gateway-s3-bucket.irsa_policy_arn },
+    { s3_pt = module.perf-test-data-s3-bucket.irsa_policy_arn },
+    { sns_cce = module.court-case-events.irsa_policy_arn },
+    { sns_cc = module.court-cases.irsa_policy_arn },
+  )
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
+module "court-case-service-irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
   service_account_name = var.application
   namespace            = var.namespace # this is also used as a tag
 
@@ -28,9 +57,6 @@ module "irsa" {
     local.sqs_policies,
     { rds_ccs = module.court_case_service_rds.irsa_policy_arn },
     { rds_pss = module.pre_sentence_service_rds.irsa_policy_arn },
-    { s3_cpg = module.crime-portal-gateway-s3-bucket.irsa_policy_arn },
-    { s3_pt = module.perf-test-data-s3-bucket.irsa_policy_arn },
-    { sns_cce = module.court-case-events.irsa_policy_arn },
     { sqs_cpg = module.crime-portal-gateway-queue.irsa_policy_arn },
     { sqs_cpg_dlq = module.crime-portal-gateway-dead-letter-queue.irsa_policy_arn },
     { sqs_ccm = module.court-case-matcher-queue.irsa_policy_arn },
