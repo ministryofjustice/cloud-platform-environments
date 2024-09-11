5350288309eb5bbe5ca52510a8b6d0eaf552ff3a
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-09-11 13:07:08 +0100

    CDPT-1854: Create a cloud platform stack to deploy storybook

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-design-system/resources/serviceaccount.tf
@@ -0,0 +1,19 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["justice-gov-uk"]
+
+  # list of github environments, to create the service account secrets as environment secrets
+  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  github_environments = ["development"]
+
+  github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
+  github_actions_secret_kube_token     = var.github_actions_secret_kube_token
+  github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
+  github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
+}
