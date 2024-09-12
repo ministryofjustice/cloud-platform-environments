95a10b4a5f6782960054d604e465a2ef8f154a20
jasper.jackson <jasper.jackson@madetech.com>
2024-09-12 13:13:36 +0100

    Changed erroneous variable from hmpps-electronic-monitoring-datastore-api-dev to hmpps-ems-datastore-api-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-datastore-api-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-datastore-api-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "hmpps-electronic-monitoring-datastore-api-dev"
+  default     = "hmpps-ems-datastore-api-dev"
 }
 
 variable "business_unit" {
