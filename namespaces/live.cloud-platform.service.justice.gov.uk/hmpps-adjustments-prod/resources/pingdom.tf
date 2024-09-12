dd0afd06b85df0f5d550b2799ba2a581cdc71051
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-09-12 14:56:09 +0100

    Implemented pingdom for hmpps adjustments

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-prod/resources/pingdom.tf
@@ -0,0 +1,21 @@
+provider "pingdom" {
+}
+
+# Integration IDs
+# 96624 = #dps_alerts
+
+resource "pingdom_check" "hmpps-adjustments-production-check" {
+  type                     = "http"
+  name                     = "DPS - HMPPS Adjustments"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/adjust-release-dates.hmpps.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624]
+}
