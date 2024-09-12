43a3f3326c31214d25ffeb9caf945f564edffd85
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-09-12 08:48:02 +0100

    PIC-4134 add new service account with access to court-cases topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/irsa.tf
@@ -10,7 +10,36 @@ locals {
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
+
 module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "court-facing-api"
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the appropriate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = merge(
+    { s3_cpg = module.crime-portal-gateway-s3-bucket.irsa_policy_arn },
+    { sns_cce = module.court-case-events.irsa_policy_arn },
+    { sns_cc = module.court-cases.irsa_policy_arn },
+  )
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+}
+
+module "court-case-service-irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   # EKS configuration
--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/service-pod.tf
@@ -1,6 +1,12 @@
+module "court-case-service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.1"
 
-module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  namespace            = var.namespace
+  service_account_name = module.court-case-service-irsa.service_account.name
+}
+
+module "court-facing-api-service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.1"
 
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name
