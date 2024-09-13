cc7084e2e72a04687a7b1147d29e06504ffa520f
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-09-13 13:43:36 +0100

    Added pingdom for API

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-prod/resources/pingdom.tf
@@ -19,3 +19,19 @@ resource "pingdom_check" "hmpps-adjustments-production-check" {
   probefilters             = "region:EU"
   integrationids           = [96624]
 }
+
+resource "pingdom_check" "hmpps-adjustments-api-production-check" {
+  type                     = "http"
+  name                     = "DPS - HMPPS Adjustments API"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/adjustments-api.hmpps.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624]
+}
