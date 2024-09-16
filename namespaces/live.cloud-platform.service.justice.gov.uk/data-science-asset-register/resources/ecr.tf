dc028fb6bc2cd238db6ea96f0f44dbe5f6433d09
William Miller <77627423+miller-william@users.noreply.github.com>
2024-09-16 14:36:03 +0100

    Update ecr.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-science-asset-register/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-science-asset-register/resources/ecr.tf
@@ -12,7 +12,9 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["example-repository"]
+  github_repositories = ["data-science-assets-app"]
+  github_environments = ["dev"]
+  github_actions_prefix = "dev"
 
   # Tags
   business_unit          = var.business_unit
