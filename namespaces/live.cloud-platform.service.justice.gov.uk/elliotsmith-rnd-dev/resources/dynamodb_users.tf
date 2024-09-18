8948abe4d7754d8c2831fcb4f2c2e2087d681947
Elliot Smith <elliot.smith2@justice.gov.uk>
2024-09-18 16:51:20 +0100

    Example in documentation is incorrect, fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/elliotsmith-rnd-dev/resources/dynamodb_users.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/elliotsmith-rnd-dev/resources/dynamodb_users.tf
@@ -5,7 +5,7 @@ module "dynamodb_users_table" {
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
-  environment_name       = var.environment_name
+  environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 
