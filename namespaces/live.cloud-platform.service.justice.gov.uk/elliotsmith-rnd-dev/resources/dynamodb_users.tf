82b8cbd49a012509ba60dbdd8c188d6b53096c76
Elliot Smith <elliot.smith2@justice.gov.uk>
2024-09-18 16:39:46 +0100

    Provision dynamodb table for users

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/elliotsmith-rnd-dev/resources/dynamodb_users.tf
@@ -0,0 +1,36 @@
+module "dynamodb_users_table" {
+  source = "https://github.com/ministryofjustice/cloud-platform-terraform-dynamodb-cluster?ref=version" # use the latest release
+
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment_name
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  hash_key  = "guid"
+  range_key = "created_at"
+  attributes = [
+    {
+      name = "guid"
+      type = "S"
+    },
+    {
+      name = "created_at"
+      type = "S"
+    }
+  ]
+}
+
+resource "kubernetes_secret" "dynamodb_users_table_output" {
+  metadata {
+    name      = "dynamodb_users_table_output"
+    namespace = var.namespace
+  }
+
+  data = {
+    table_name = module.dynamodb_users_table.table_name
+    table_arn  = module.dynamodb_users_table.table_arn
+  }
+}
