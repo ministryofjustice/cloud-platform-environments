bd8acf36ad45f0f028a7b6e532fee33973a36581
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-09-18 11:27:42 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-preprod/resources/irsa.tf
@@ -11,22 +11,23 @@ module "irsa" {
   # Attach the approprate policies using a key => value map
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = merge({
-    s3                   = module.s3_bucket.irsa_policy_arn
-    offender-event-queue = module.offender-event-queue.irsa_policy_arn
-    offender-event-dlq   = module.offender-event-dlq.irsa_policy_arn
-    document-storage-s3  = module.document_storage_s3_bucket.irsa_policy_arn
-  },
-  {
-    audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn
-  })
-}
+  role_policy_arns = merge(
+    {
+      s3                   = module.s3_bucket.irsa_policy_arn
+      offender-event-queue = module.offender-event-queue.irsa_policy_arn
+      offender-event-dlq   = module.offender-event-dlq.irsa_policy_arn
+      document-storage-s3  = module.document_storage_s3_bucket.irsa_policy_arn
+    },
+    {
+      audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn
+    }
+  )
 
-# Tags
-business_unit          = var.business_unit
-application            = var.application
-is_production          = var.is_production
-team_name              = var.team_name
-environment_name       = var.environment
-infrastructure_support = var.infrastructure_support
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 }
