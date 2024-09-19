64e039c5574abad0cdf2bf5cb8d63c58283e47e5
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-09-19 12:20:10 +0100

    increase db storage

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
@@ -9,6 +9,7 @@ module "dps_rds" {
   environment_name          = var.environment-name
   infrastructure_support    = var.infrastructure_support
   db_instance_class         = "db.t4g.xlarge"
+  db_allocated_storage      = "50"
   db_max_allocated_storage  = "100000"
   deletion_protection       = true
   prepare_for_major_upgrade = false
