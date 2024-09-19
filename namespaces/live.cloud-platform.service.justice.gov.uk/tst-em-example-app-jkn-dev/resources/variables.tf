7916000a74a7b6c257d2b45844e80c01307c0ca6
jasper.jackson <jasper.jackson@madetech.com>
2024-09-19 12:14:57 +0100

    added cache clusters to variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/tst-em-example-app-jkn-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tst-em-example-app-jkn-dev/resources/variables.tf
@@ -20,6 +20,10 @@ variable "namespace" {
   default     = "tst-em-example-app-jkn-dev"
 }
 
+variable "number_cache_clusters" {
+  default = "2"
+}
+
 variable "business_unit" {
   description = "Area of the MOJ responsible for this service"
   type        = string
