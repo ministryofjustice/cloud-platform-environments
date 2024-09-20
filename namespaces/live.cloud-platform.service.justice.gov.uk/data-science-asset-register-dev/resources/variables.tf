936e3a7cf08588c9f73c5e128f850e4933b23d48
William Miller <77627423+miller-william@users.noreply.github.com>
2024-09-20 11:00:33 +0100

    Update variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-science-asset-register-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-science-asset-register-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "data-science-asset-register"
+  default     = "data-science-asset-register-dev"
 }
 
 variable "business_unit" {
