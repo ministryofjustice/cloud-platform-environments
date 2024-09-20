c139154b508ac3423afda2bb65857b7e341770c6
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-09-20 14:07:36 +0100

    Add sqs subscriber to court case events topic

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/court-case-events-queue.tf
@@ -0,0 +1,116 @@
+resource "aws_sns_topic_subscription" "_court_case_events_subscription" {
+  provider  = aws.london
+  topic_arn = data.aws_ssm_parameter.court-case-events-topic-arn.value
+  protocol  = "sqs"
+  endpoint  = module.court_case_events_queue.sqs_arn
+}
+
+module "court_case_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name                   = "court_case_events_queue"
+  encrypt_sqs_kms            = "true"
+  message_retention_seconds  = 1209600
+  visibility_timeout_seconds = 120
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.court_case_events_dead_letter_queue.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "aws_sqs_queue_policy" "court_case_events_queue_policy" {
+  queue_url = module.court_case_events_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.court_case_events_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.court_case_events_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+              {
+                "aws:SourceArn": "${data.aws_ssm_parameter.court-case-events-topic-arn.value}"
+              }
+            }
+        }
+      ]
+  }
+EOF
+}
+
+######## Dead letter queue
+
+module "court_case_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+
+  # Queue configuration
+  sqs_name        = "court_case_events_dlq"
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+########  Secrets
+
+resource "kubernetes_secret" "court_case_events_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "sqs-cpr-court-case-events-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.court_case_events_queue.sqs_id
+    sqs_queue_arn  = module.court_case_events_queue.sqs_arn
+    sqs_queue_name = module.court_case_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "court_case_events_dead_letter_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "sqs-cpr-court-case-events-dlq-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.court_case_events_dead_letter_queue.sqs_id
+    sqs_queue_arn  = module.court_case_events_dead_letter_queue.sqs_arn
+    sqs_queue_name = module.court_case_events_dead_letter_queue.sqs_name
+  }
+}
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/irsa.tf
@@ -16,7 +16,11 @@ module "create_an_order_api_irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-electronic-monitoring-create-an-order-api"
-  role_policy_arns     = local.sqs_policies
+  role_policy_arns     = merge(
+    local.sqs_policies,
+    {court_case_events_queue             = module.court_case_events_queue.irsa_policy_arn},    
+    {court_case_events_dead_letter_queue = module.court_case_events_dead_letter_queue.irsa_policy_arn}
+  )
   # Tags
   business_unit          = var.business_unit
   application            = var.application
