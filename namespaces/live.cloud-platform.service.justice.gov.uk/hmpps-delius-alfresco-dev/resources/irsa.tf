fc151d6e9b7e94e7af7d107202b688406e8f5830
George Taylor <george.taylor@digital.justice.gov.uk>
2024-09-23 21:01:20 +0100

    Update irsa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-dev/resources/irsa.tf
@@ -1,3 +1,7 @@
+data "aws_iam_policy" "poc_env_bucket_policy" {
+  name = "cloud-platform-s3-5ce784402d8052fe1cd006f1e7329f70"
+}
+
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
@@ -8,6 +12,7 @@ module "irsa" {
   service_account_name = "${var.team_name}-${var.environment}"
   role_policy_arns = {
     s3                             = module.s3_bucket.irsa_policy_arn
+    s3_refresh_poc                     = data.aws_iam_policy.poc_env_bucket_policy.arn
     rds                            = module.rds_alfresco.irsa_policy_arn
     s3_opensearch_snapshots_bucket = module.s3_opensearch_snapshots_bucket.irsa_policy_arn
   }
