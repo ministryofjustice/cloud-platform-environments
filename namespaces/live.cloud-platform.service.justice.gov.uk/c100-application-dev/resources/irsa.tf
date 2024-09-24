4064545cd3dac3a1b9f2354e263cf9a7a8a55f93
Petr Zaparka <petr@zaparka.cz>
2024-09-24 14:45:36 +0100

    Updating version of service pod

--- namespaces/live.cloud-platform.service.justice.gov.uk/c100-application-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/c100-application-dev/resources/irsa.tf
@@ -25,7 +25,7 @@ module "irsa" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=2.0.0"
 
   # Configuration
   namespace            = var.namespace
