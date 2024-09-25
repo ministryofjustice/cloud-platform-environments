34e3179cc50456a814b12850db0bda0660ed22c0
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-09-25 15:23:53 +0100

    CVL preprod - rotate DB creds

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-preprod/resources/rds.tf
@@ -14,7 +14,7 @@ module "create_and_vary_a_licence_api_rds" {
   db_engine_version           = "15.7"
   rds_family                  = "postgres15"
   prepare_for_major_upgrade   = false
-  db_password_rotated_date    = "23-09-2024"
+  db_password_rotated_date    = "25-09-2024"
   db_allocated_storage        = 20
 
   providers = {
