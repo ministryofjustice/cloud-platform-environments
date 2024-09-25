0851d886ead3112642ad414caa4ab5ee494f474c
brian.carr <brian.carr@digital.justice.gov.uk>
2024-09-25 13:42:44 +0100

    adding missing comma

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-stage/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-intelligence-stage/resources/iam.tf
@@ -49,7 +49,7 @@ data "aws_iam_policy_document" "ims_legacy_extractor_policy" {
       module.ims_images_storage_bucket.bucket_arn,
       "${module.ims_images_storage_bucket.bucket_arn}/*",
       module.ims_attachments_storage_bucket.bucket_arn,
-      "${module.ims_attachments_storage_bucket.bucket_arn}/*"
+      "${module.ims_attachments_storage_bucket.bucket_arn}/*",
       module.ims_dissemination_storage_bucket.bucket_arn,
       "${module.ims_dissemination_storage_bucket.bucket_arn}/*",
       module.ims_audit_files_storage_bucket.bucket_arn,
