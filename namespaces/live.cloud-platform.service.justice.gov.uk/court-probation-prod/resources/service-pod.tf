1cafdcfc3baa45040944382fc9353d60e4125fb3
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-09-26 10:31:57 +0100

    Revert "PIC-4134 use latest version of service pod terraform"
    This reverts commit c59db843ec83200bf145bbb8350e8112c2922ef7.

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/service-pod.tf
@@ -1,12 +1,12 @@
 module "court-case-service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=2.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.1"
 
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name
 }
 
 module "court-facing-api-service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=2.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.1"
 
   namespace            = var.namespace
   service_account_name = module.court-facing-api-irsa.service_account.name
