2be98f2da62c2d434a3ac92bb7efa2d4e5441882
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-09-26 11:15:32 +0100

    chore: update storage

--- namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/rds.tf
@@ -11,7 +11,7 @@ module "rds" {
   db_max_allocated_storage     = "500"
   db_allocated_storage         = "80"
   storage_type                 = "io2"
-#   db_iops                      = "10000"
+  db_iops                      = "1000"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
