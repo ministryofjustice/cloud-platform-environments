8df2c46924103a9f2ca8c1d3beb18151aa1711c9
bcrawford-moj <billy.crawford@justice.gov.uk>
2024-10-02 16:52:29 +0100

    Fixed error with repo url instead of name

--- namespaces/live.cloud-platform.service.justice.gov.uk/wiggedout-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/wiggedout-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["https://github.com/ministryofjustice/wigged-out-app"]
+  github_repositories = ["wigged-out-app"]
   github_environments = ["dev"]
   github_actions_prefix = "dev"
 
