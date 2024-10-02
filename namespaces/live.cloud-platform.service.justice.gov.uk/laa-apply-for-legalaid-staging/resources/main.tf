950e7a7f6ea502ab6b41472d73dd50bf12d7c8b3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-10-02 11:18:52 +0100

    Civil Apply Staging: Add github tags to AWS

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-staging/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-staging/resources/main.tf
@@ -5,18 +5,36 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "laa-apply-for-legal-aid"
+    }
+  }
 }
 
 # To be use in case the resources need to be created in London
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "laa-apply-for-legal-aid"
+    }
+  }
 }
 
 # To be use in case the resources need to be created in Ireland
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      GithubTeam = "laa-apply-for-legal-aid"
+    }
+  }
 }
 provider "github" {
   token = var.github_token
