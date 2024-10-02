01cf15411b422a69509a4e67d032f1b8bf3d9a4b
Marcus Aspin <maspin@unilink.com>
2024-10-02 11:44:23 +0000

    DST-17959 New namespaces for breach-notice project (dev)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-breach-notice-dev/resources/variables.tf
@@ -0,0 +1,57 @@
+variable "kubernetes_cluster" {
+}
+
+
+variable "vpc_name" {
+}
+
+
+variable "application" {
+  default = "hmpps-breach-notice"
+}
+
+variable "namespace" {
+  default = "hmpps-breach-notice-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "unilink"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "hmpps-unilink-delius@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "github_owner" {
+  description = "Required by the github terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the github terraform provider"
+  default     = ""
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
+variable "maintenance_window" {
+  default = "sun:00:00-sun:03:00"
+}
