6591737aeca7daa3b95cd03f2fc8ae88fc7d549b
steverendell <steve.rendell@syscon.net>
2024-10-02 13:18:31 +0100

    SDIT-2062 Upgrade prod mapping DB
    Also dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-nomis-mapping-service-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-nomis-mapping-service-dev/resources/rds.tf
@@ -9,13 +9,16 @@ module "dps_rds" {
   namespace                 = var.namespace
   environment_name          = var.environment-name
   infrastructure_support    = var.infrastructure_support
-  db_instance_class         = "db.t4g.small"
+  db_instance_class         = "db.t4g.large"
   db_engine                 = "postgres"
   db_engine_version         = "16"
   rds_family                = "postgres16"
   db_password_rotated_date  = "2023-02-21"
   deletion_protection       = true
   prepare_for_major_upgrade = false
+  storage_type              = "gp3"
+  db_allocated_storage      = "50"
+  db_max_allocated_storage  = "2000"
 
   providers = {
     aws = aws.london
