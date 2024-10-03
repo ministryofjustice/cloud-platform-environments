8e2222012c107f35960915a063a4721f0fce2ac3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-03 10:14:30 +0100

    MIC-572 Update namespace email and slack notifications

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/variables.tf
@@ -35,7 +35,7 @@ variable "environment-name" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "dps-hmpps@digital.justice.gov.uk"
+  default     = "moveandimprove@justice.gov.uk"
 }
 
 variable "is_production" {
