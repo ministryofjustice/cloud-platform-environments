ccbaba62c125adc64f297bbfc219a2c096d81c3a
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-03 10:37:31 +0100

    MIC-572 Update team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-prod/resources/variables.tf
@@ -25,7 +25,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "Digital Prison Services/New Nomis"
+  default     = "hmpps-move-and-improve"
 }
 
 variable "environment-name" {
