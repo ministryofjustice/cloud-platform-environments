fa1c1c496d833b078c0edf0a2dac5c1b80ac09f5
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-03 10:30:18 +0100

    MIC-572 Update team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-prod/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "farsight-devs"
+  default     = "hmpps-move-and-improve"
 }
 
 variable "environment" {
