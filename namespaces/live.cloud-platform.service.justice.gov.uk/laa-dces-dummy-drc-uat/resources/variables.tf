6f4cb53e131c8ebeb18c35d9ec039d8c3ea020cb
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2024-10-03 16:38:40 +0100

    Dces 459 create ca crt secret uat (#26578)
    * DCES-459: Create a new secret ca-crt in UAT
    
    * Fixed typo.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-dummy-drc-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-dummy-drc-uat/resources/variables.tf
@@ -67,3 +67,7 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
