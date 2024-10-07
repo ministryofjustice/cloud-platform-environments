d947be164e0ebde3a5878a4cee721a09e7b2b64a
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-10-07 11:05:58 +0100

    Rotate tokens

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/rds-postgresql.tf
@@ -18,7 +18,7 @@ module "rds" {
   db_max_allocated_storage     = "10000"
   deletion_protection          = true
   enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+  db_password_rotated_date     = "07-10-2024"
 
   # PostgreSQL specifics
   db_engine         = "postgres"
--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-application-store-uat/resources/serviceaccount.tf
@@ -5,6 +5,7 @@ module "serviceaccount" {
   namespace            = var.namespace
   kubernetes_cluster   = var.kubernetes_cluster
   serviceaccount_rules = var.serviceaccount_rules
+  serviceaccount_token_rotated_date = "07-10-2024"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
