f0f3c7554272d298a28950ef3f57f0d7eb1fa6e6
David Read <david.read@digital.justice.gov.uk>
2024-10-07 10:21:40 +0100

    Create environment cfe-civil-cicd-poc-uat
    It is a copy of cfe-civil-uat, but renaming the app from cfe-civil to cfe-civil-cicd-poc

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfe-civil-cicd-poc-uat/resources/ecr.tf
@@ -0,0 +1,122 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "ecr_credentials" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.0.0"
+
+  repo_name = "cfe-civil-cicd-poc-ecr"
+
+  # REQUIRED: OIDC providers to configure, either "github", "circleci", or both
+  oidc_providers = ["circleci"]
+
+  # REQUIRED: GitHub repositories, whose CI will be provided with short-term credentials to access this container repository
+  github_repositories = ["cfe-civil-cicd-poc", "laa-check-client-qualifies"]
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  # github_environments = ["production"]
+
+  # list of github environments, to create the ECR secrets as environment secrets
+  # https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#environment-secrets
+  # github_environments = ["my-environment"]
+
+  # Lifecycle_policy provides a way to automate the cleaning up of your container images by expiring images based on age or count.
+  # To apply multiple rules, combined them in one policy JSON.
+  # https://docs.aws.amazon.com/AmazonECR/latest/userguide/lifecycle_policy_examples.html
+
+  lifecycle_policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep newest 50 uat images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["uat-"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 50
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep newest 5 staging images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["staging-"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 3,
+            "description": "Keep newest 5 staging_mtr images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["staging_mtr-"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 4,
+            "description": "Keep the newest 10 production images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["production-"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 5,
+            "description": "Deprecated tags - keep 5 newest",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["dev", "latest"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+
+resource "kubernetes_secret" "ecr_credentials" {
+  metadata {
+    name      = "ecr-repo-cfe-civil-cicd-poc"
+    namespace = var.namespace
+  }
+
+  data = {
+    repo_arn = module.ecr_credentials.repo_arn
+    repo_url = module.ecr_credentials.repo_url
+  }
+}
