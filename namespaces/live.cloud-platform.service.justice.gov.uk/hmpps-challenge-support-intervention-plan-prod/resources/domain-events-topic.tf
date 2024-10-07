5e194ff79e3c890c911d26b2b742f68c81ad0c39
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-07 17:30:06 +0100

    MIC-615

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-prod/resources/domain-events-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-prod/resources/domain-events-topic.tf
@@ -12,3 +12,7 @@ resource "kubernetes_secret" "hmpps_csip_hmpps_domain_events_topic" {
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-prod/topic-arn"
 }
+
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08"
+}
