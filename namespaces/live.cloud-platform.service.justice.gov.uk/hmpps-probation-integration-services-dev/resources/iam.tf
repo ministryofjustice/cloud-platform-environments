8ec2b6847cbe3a6397626920c5adf24e7e0da8ac
Marcus Aspin <maspin@unilink.com>
2024-10-08 16:30:44 +0100

    PI-2575 Add permissions to receive messages from the court topic (dev) (#26690)
    * PI-2575 Add permissions to receive messages from the court topic (dev)
    
    * fmt

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/iam.tf
@@ -45,6 +45,21 @@ data "aws_iam_policy_document" "sqs_queue_policy_document" {
     }
     resources = ["*"]
   }
+  statement {
+    sid     = "CourtTopicToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_ssm_parameter.court-topic.value]
+    }
+    resources = ["*"]
+  }
 }
 
 # Policies to manage queues e.g. view and redrive messages
