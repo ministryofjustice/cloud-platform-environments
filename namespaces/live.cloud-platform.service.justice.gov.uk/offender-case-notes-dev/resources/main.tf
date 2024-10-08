acbadb517cc092aeba9e79120b98a696d175a804
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-08 16:11:47 +0100

    MICN-184 add domain event queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-dev/resources/main.tf
@@ -4,7 +4,16 @@ terraform {
 }
 
 provider "aws" {
+  alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
+      slack-channel = var.slack_channel
+      GithubTeam = "hmpps-move-and-improve"
+    }
+  }
 }
 
 provider "github" {
