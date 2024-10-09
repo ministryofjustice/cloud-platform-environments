fdc6f9b871d66c232a913b70ff2d52b672840a17
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-10-09 15:56:04 +0100

    PRC-157 : Build the PROD environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-prod/resources/irsa.tf
@@ -9,7 +9,7 @@ locals {
 
   # The names of the SNS topics used and the namespace which created them
   sns_topics = {
-    "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd" = "hmpps-domain-events-prod"
+    "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08" = "hmpps-domain-events-prod"
   }
 
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
