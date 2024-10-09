e9155417108748f73eb3c8ce786f4588156f3d0f
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-10-09 10:17:08 +0100

    PRC-156 : Build the PREPROD environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-preprod/resources/irsa.tf
@@ -2,11 +2,17 @@
 # The value of each item should be the namespace where the queue or topic was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
-  # The names of the SNS topics used and the namespace which created them
-  sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  # The names of the queues used and the namespace which created them
+  sqs_queues = {
+    "Digital-Prison-Services-preprod-hmpps_audit_queue" = "hmpps-audit-preprod",
   }
 
+  # The names of the SNS topics used and the namespace which created them
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd" = "hmpps-domain-events-preprod"
+  }
+
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
@@ -18,7 +24,7 @@ module "irsa" {
 
   # IRSA configuration
   service_account_name = "hmpps-contacts-api"
-  role_policy_arns     = merge(local.sns_policies)
+  role_policy_arns     = merge(local.sqs_policies, local.sns_policies)
 
   # Tags
   business_unit          = var.business_unit
@@ -28,6 +34,11 @@ module "irsa" {
   namespace              = var.namespace # this is also used to attach your service account to your namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
