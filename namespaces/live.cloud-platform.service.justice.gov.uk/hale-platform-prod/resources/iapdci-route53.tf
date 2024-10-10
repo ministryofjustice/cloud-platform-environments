dd807bc1afac90894a3bddc7a8f7cb679cc7dc97
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2024-10-10 09:51:47 +0100

    Add iapdci certs and route53 zone

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/iapdci-route53.tf
@@ -0,0 +1,24 @@
+resource "aws_route53_zone" "iapdci_route53_zone" {
+  name = "iapdeathsincustody.independent.gov.uk"
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
+resource "kubernetes_secret" "iapdci_route53_zone_sec" {
+  metadata {
+    name      = "iapdci-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.iapdci_route53_zone.zone_id
+    nameservers = join(",", aws_route53_zone.advance_into_justice_route53_zone.name_servers)
+  }
+}
