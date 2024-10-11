91f1445695e0eb70cc2155bb5d9559779c8e3aec
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-11 13:04:17 +0100

    update default provider

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/main.tf
@@ -5,12 +5,6 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
-}
-
-# To be use in case the resources need to be created in London
-provider "aws" {
-  alias  = "london"
-  region = "eu-west-2"
 
   default_tags {
     tags = {
@@ -19,12 +13,6 @@ provider "aws" {
       GithubTeam = "hmpps-move-and-improve"
     }
   }
-}
-
-# To be use in case the resources need to be created in Ireland
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
 }
 
 provider "github" {
