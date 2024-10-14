f2e976e7cdc36c7550fb50b49ed30c25e5c29086
james Hart <james.hart@digital.justice.gov.uk>
2024-10-14 15:44:53 +0100

    create github service account for dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-templates-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-templates-dev/resources/variables.tf
@@ -23,10 +23,13 @@ variable "team_name" {
   default     = "hmpps-developers"
 }
 
+####################################################################################################################
+### Change this environment to the environment name corresponding to this namespace (as per helm/values-ENV.dev) ###
 variable "environment" {
   description = "The type of environment you're deploying to."
-  default     = "development"
+  default     = "dev"
 }
+####################################################################################################################
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
