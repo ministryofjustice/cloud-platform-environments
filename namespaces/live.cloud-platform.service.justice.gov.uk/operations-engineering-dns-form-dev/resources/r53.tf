d3ac0c78008a595c287527ce13614ff631c0ea99
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-10-18 11:18:33 +0100

    :wrench: Add custom domain dev.change-dns.service.justice.gov.uk

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering-dns-form-dev/resources/r53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_zone" "change_dns_route53_zone" {
+  name = var.domain
+  tags = {
+    team_name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure_support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+resource "kubernetes_secret" "change_dns_route53_zone_sec" {
+  metadata {
+    name      = "change-dns-route53-zone-output"
+    namespace = var.namespace
+  }
+  data = {
+    zone_id     = aws_route53_zone.change_dns_route53_zone.zone_id
+    nameservers = join("\n", aws_route53_zone.change_dns_route53_zone.name_servers)
+  }
+}
