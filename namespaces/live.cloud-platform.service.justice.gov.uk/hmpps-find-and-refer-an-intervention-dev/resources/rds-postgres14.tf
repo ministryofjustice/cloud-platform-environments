913ce86ad59ff909f82be81972a90205734a3a1e
amol phalnikar <2399527+amolphalnikar@users.noreply.github.com>
2024-10-22 14:43:34 +0100

    commented out read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-find-and-refer-an-intervention-dev/resources/rds-postgres14.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-find-and-refer-an-intervention-dev/resources/rds-postgres14.tf
@@ -40,48 +40,48 @@ resource "kubernetes_secret" "rds" {
 }
 
 
-module "rds_replica" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
-  vpc_name               = var.vpc_name
-  team_name              = var.team_name
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-
-  rds_family                  = "postgres16"
-  db_engine_version           = "16.1"
-  db_instance_class           = "db.t4g.small"
-  allow_major_version_upgrade = "false"
-  db_max_allocated_storage    = "10"
-
-  db_name             = null # "db_name": conflicts with replicate_source_db
-  replicate_source_db = module.rds.db_identifier
-
-  # Set to true for replica database. No backups or snapshots are created for read replica
-  skip_final_snapshot        = "true"
-  db_backup_retention_period = 0
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "rds_replica" {
-  metadata {
-    name      = "rds-replica-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.rds_replica.rds_instance_endpoint
-    rds_instance_address  = module.rds_replica.rds_instance_address
-    database_name         = module.rds_replica.database_name
-    database_username     = module.rds_replica.database_username
-    database_password     = module.rds_replica.database_password
-    url                   = "postgres://${module.rds_replica.database_username}:${module.rds_replica.database_password}@${module.rds_replica.rds_instance_endpoint}/${module.rds_replica.database_name}"
-
-  }
+#module "rds_replica" {
+#  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
+#  vpc_name               = var.vpc_name
+#  team_name              = var.team_name
+#  business_unit          = var.business_unit
+#  application            = var.application
+#  is_production          = var.is_production
+#  namespace              = var.namespace
+#  environment_name       = var.environment
+#  infrastructure_support = var.infrastructure_support
+#
+#  rds_family                  = "postgres16"
+#  db_engine_version           = "16.1"
+#  db_instance_class           = "db.t4g.small"
+#  allow_major_version_upgrade = "false"
+#  db_max_allocated_storage    = "10"
+#
+#  db_name             = null # "db_name": conflicts with replicate_source_db
+#  replicate_source_db = module.rds.db_identifier
+#
+#  # Set to true for replica database. No backups or snapshots are created for read replica
+#  skip_final_snapshot        = "true"
+#  db_backup_retention_period = 0
+#
+#  providers = {
+#    aws = aws.london
+#  }
+#}
+#
+#resource "kubernetes_secret" "rds_replica" {
+#  metadata {
+#    name      = "rds-replica-output"
+#    namespace = var.namespace
+#  }
+#
+#  data = {
+#    rds_instance_endpoint = module.rds_replica.rds_instance_endpoint
+#    rds_instance_address  = module.rds_replica.rds_instance_address
+#    database_name         = module.rds_replica.database_name
+#    database_username     = module.rds_replica.database_username
+#    database_password     = module.rds_replica.database_password
+#    url                   = "postgres://${module.rds_replica.database_username}:${module.rds_replica.database_password}@${module.rds_replica.rds_instance_endpoint}/${module.rds_replica.database_name}"
+#
+#  }
 }
