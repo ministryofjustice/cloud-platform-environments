6752290aa995d1f34981966fccb7e0f63bd1c307
David Conneely <899881+dconneely@users.noreply.github.com>
2024-10-24 08:52:03 +0100

    DCES-556 Re-order/re-describe secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-prod/resources/secret.tf
@@ -10,33 +10,33 @@ module "secrets_manager" {
   eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
-    "drc-client-auth-tls" = {
-      description             = "DRC mTLS client credentials [laa-dces-drc-integration-prod]"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "drc-client-auth-tls"
-    },
     "ca-crt" = {
-      description             = "ca.crt for mTLS client certificate validation [laa-dces-drc-integration-prod]"
+      description             = "[laa-dces-drc-integration-prod/ca-crt] ca.crt for mTLS client certificate validation"
       recovery_window_in_days = 7
       k8s_secret_name         = "ca-crt"
-    },
-    "feature" = {
-      description             = "Feature flag variables [laa-dces-drc-integration-prod]"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "feature"
-    },
+    }
     "dces-datasource" = {
-      description             = "DCES Database Connection Information [laa-dces-drc-integration-prod]"
+      description             = "[laa-dces-drc-integration-prod/dces-datasource] Database connection"
       recovery_window_in_days = 7
       k8s_secret_name         = "dces-datasource"
-    },
+    }
+    "drc-client-auth-tls" = {
+      description             = "[laa-dces-drc-integration-prod/drc-client-auth-tls] DRC mTLS client credentials"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "drc-client-auth-tls"
+    }
+    "feature" = {
+      description             = "[laa-dces-drc-integration-prod/feature] Feature flag variables"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "feature"
+    }
     "maat_api_oauth_client_id" = {
-      description             = "MAAT API OAuth2 client ID [laa-dces-drc-integration-prod]"
+      description             = "[laa-dces-drc-integration-prod/maat-api-oath-client-id] MAAT API OAuth2 client ID"
       recovery_window_in_days = 7
       k8s_secret_name         = "maat-api-oauth-client-id"
-    },
+    }
     "maat_api_oauth_client_secret" = {
-      description             = "MAAT API OAuth2 client secret [laa-dces-drc-integration-prod]"
+      description             = "[laa-dces-drc-integration-prod/maat-api-oauth-client-secret] MAAT API OAuth2 client secret"
       recovery_window_in_days = 7
       k8s_secret_name         = "maat-api-oauth-client-secret"
     }
