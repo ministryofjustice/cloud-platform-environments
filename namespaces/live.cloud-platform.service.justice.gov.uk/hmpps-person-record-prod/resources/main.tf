7d30f099ebb3c91c9b5bfc7b8c42b8632f1d04f2
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-10-24 13:27:05 +0100

    CPR-445 Add default tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/main.tf
@@ -16,8 +16,20 @@ provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
 }
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
 }
 
+locals {
+  default_tags = {
+    namespace              = var.namespace
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment_name
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
