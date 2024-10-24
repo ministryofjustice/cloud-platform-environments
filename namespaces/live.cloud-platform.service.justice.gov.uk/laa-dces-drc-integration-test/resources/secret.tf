2aecec4b5a73ca3608f848a5e848c25cf354cdd0
David Conneely <899881+dconneely@users.noreply.github.com>
2024-10-24 08:48:25 +0100

    DCES-556 Re-order/re-describe secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-test/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-test/resources/secret.tf
@@ -10,38 +10,38 @@ module "secrets_manager" {
   eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
-    "dces-drc-integration-service-alert-webhook-test" = {
-      description             = "DCES Report Service Slack Webhook Test"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "dces-drc-integration-service-alert-webhook-test"
-    },
-    "drc-client-auth-tls" = {
-      description             = "DRC mTLS client credentials [laa-dces-drc-integration-test]"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "drc-client-auth-tls"
-    },
     "ca-crt" = {
-      description             = "ca.crt for mTLS client certificate validation [laa-dces-drc-integration-test]"
+      description             = "[laa-dces-drc-integration-test/ca-crt] ca.crt for mTLS client certificate validation"
       recovery_window_in_days = 7
       k8s_secret_name         = "ca-crt"
-    },
-    "feature" = {
-      description             = "Feature flag variables [laa-dces-drc-integration-test]"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "feature"
     }
     "dces-datasource" = {
-      description             = "DCES Database Connection Information [laa-dces-drc-integration-test]"
+      description             = "[laa-dces-drc-integration-test/dces-datasource] Database connection"
       recovery_window_in_days = 7
       k8s_secret_name         = "dces-datasource"
     }
+    "dces-drc-integration-service-alert-webhook-test" = {
+      description             = "[laa-dces-drc-integration-test/dces-drc-integration-service-alert-webhook-test] Slack webhook"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "dces-drc-integration-service-alert-webhook-test"
+    }
+    "drc-client-auth-tls" = {
+      description             = "[laa-dces-drc-integration-test/drc-client-auth-tls] DRC mTLS client credentials"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "drc-client-auth-tls"
+    }
+    "feature" = {
+      description             = "[laa-dces-drc-integration-test/feature] Feature flag variables"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "feature"
+    }
     "maat_api_oauth_client_id" = {
-      description             = "MAAT API OAuth2 client ID [laa-dces-drc-integration-test]"
+      description             = "[laa-dces-drc-integration-test/maat-api-oauth-client-id] MAAT API OAuth2 client ID"
       recovery_window_in_days = 7
       k8s_secret_name         = "maat-api-oauth-client-id"
-    },
+    }
     "maat_api_oauth_client_secret" = {
-      description             = "MAAT API OAuth2 client secret [laa-dces-drc-integration-test]"
+      description             = "[laa-dces-drc-integration-test/maat-api-oauth-client-secret] MAAT API OAuth2 client secret"
       recovery_window_in_days = 7
       k8s_secret_name         = "maat-api-oauth-client-secret"
     }
