8248263f517e80d22dd76744be08fed7f917dac3
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-10-25 10:46:42 +0100

    PIC-4134 make ordering match dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/service-pod.tf
@@ -1,13 +1,13 @@
-module "court-case-service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.1.0"
-
-  namespace            = var.namespace
-  service_account_name = module.irsa.service_account.name
-}
-
 module "court-facing-api-service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.1.0"
 
   namespace            = var.namespace
   service_account_name = module.court-facing-api-irsa.service_account.name
+}
+
+module "court-case-service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.1.0"
+
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name
 }
