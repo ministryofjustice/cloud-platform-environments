1dfe6c55dcdb2c047f432404430ca6db02b35456
Joseph.Dundon <jdundon@unilink.com>
2024-10-28 10:50:25 +0000

    Update Common-Platform-and-delius-queue filter policy - Update the sqs queue filter policy to receive common-platform hearing messages and not libra messages

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/common-platform-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/common-platform-and-delius-queue.tf
@@ -2,11 +2,6 @@ resource "aws_sns_topic_subscription" "common-platform-and-delius-queue-subscrip
   topic_arn = data.aws_ssm_parameter.court-topic.value
   protocol  = "sqs"
   endpoint  = module.common-platform-and-delius-queue.sqs_arn
-  filter_policy = jsonencode({
-    messageType = [
-      "COMMON_PLATFORM_HEARING"
-    ]
-  })
 }
 
 module "common-platform-and-delius-queue" {
