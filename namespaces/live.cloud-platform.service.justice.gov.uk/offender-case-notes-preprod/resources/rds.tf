4bd81b9082ad53061bb2330dec679a1047bebf8a
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-10-28 16:42:47 +0000

    MICN-131 migrate to gp3

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-case-notes-preprod/resources/rds.tf
@@ -1,5 +1,5 @@
 module "dps_rds" {
-  source                    = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.2.0"
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
   vpc_name                  = var.vpc_name
   team_name                 = var.team_name
   business_unit             = var.business_unit
@@ -10,7 +10,6 @@ module "dps_rds" {
   infrastructure_support    = var.infrastructure_support
   db_instance_class         = "db.r6g.xlarge"
   db_allocated_storage      = "512"
-  db_max_allocated_storage  = "10000"
   deletion_protection       = true
   prepare_for_major_upgrade = false
   rds_family                = "postgres16"
