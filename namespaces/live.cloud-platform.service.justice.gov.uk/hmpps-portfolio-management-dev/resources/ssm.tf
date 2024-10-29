103966f39cd680e9ad4ce5cf06352c43a5d37a44
james Hart <james.hart@digital.justice.gov.uk>
2024-10-29 12:24:20 +0000

    add ssm values

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/ssm.tf
@@ -0,0 +1,41 @@
+resource "aws_ssm_parameter" "application_insights_key_t3" {
+  name  = "application_insights_key_t3"
+  type  = "String"
+  value = "T3 KEY TO BE MODIFIED"
+  description = "Application Insights key for t3"
+}
+
+resource "aws_ssm_parameter" "application_insights_connection_string_t3" {
+  name  = "application_insights_connection_string_t3"
+  type  = "String"
+  value = "T3 CONNECTION STRING TO BE MODIFIED"
+  description = "Application Insights connection string for t3"
+}
+
+resource "aws_ssm_parameter" "application_insights_key_preprod" {
+  name  = "application_insights_key_preprod"
+  type  = "String"
+  value = "PREPROD KEY TO BE MODIFIED"
+  description = "Application Insights key for preprod"
+}
+
+resource "aws_ssm_parameter" "application_insights_connection_string_preprod" {
+  name  = "application_insights_connection_string_preprod"
+  type  = "String"
+  value = "PREPROD CONNECTION STRING TO BE MODIFIED"
+  description = "Application Insights connection string for preprod"
+}
+
+resource "aws_ssm_parameter" "application_insights_key_prod" {
+  name  = "application_insights_key_prod"
+  type  = "String"
+  value = "PROD KEY TO BE MODIFIED"
+  description = "Application Insights key for prod"
+}
+
+resource "aws_ssm_parameter" "application_insights_connection_string_prod" {
+  name  = "application_insights_connection_string_prod"
+  type  = "String"
+  value = "PREPROD CONNECTION STRING TO BE MODIFIED"
+  description = "Application Insights connection string for preprod"
+}
