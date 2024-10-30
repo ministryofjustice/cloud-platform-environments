f468aa13b774383e476e5965de52a087478c444d
Jon Brighton <brightonsbox@hotmail.com>
2024-10-30 12:28:26 +0000

    CDPS-1016: Switching off Prison Person API (and db) in dev outside of working hours

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/rds-postgresql.tf
@@ -16,7 +16,7 @@ module "rds" {
   performance_insights_enabled = false
   db_allocated_storage      = "100"
   db_max_allocated_storage  = "2000"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  enable_rds_auto_start_stop   = true # Turns off database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
