bcdf42af3a0e839a6f62a30fdebd3cdd0f907927
james Hart <james.hart@digital.justice.gov.uk>
2024-10-30 11:25:43 +0000

    add service pod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/irsa.tf
@@ -0,0 +1,16 @@
+# irsa configuration is required to use the service pod
+module "irsa" {
+  role_policy_arns = {
+  # Here you must provide the policy arn(s) for the AWS resources you want to access via the service pod
+  ssm  = module.ssm.irsa_policy_arn
+  }
+}
+
+# set up the service pod
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+}
