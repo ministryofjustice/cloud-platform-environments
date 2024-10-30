0b632061243b6452b0a8936b07121112166edd8d
james Hart <james.hart@digital.justice.gov.uk>
2024-10-30 15:10:29 +0000

    add tags for access through AWS console

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/ssm.tf
@@ -1,24 +1,10 @@
 resource "aws_ssm_parameter" "application_insights_key_t3" {
-  name  = "/application_insights/key_t3"
-  type  = "String"
-  value = "T3 KEY TO BE MODIFIED"
+  name        = "/application_insights/key_t3"
+  type        = "String"
+  value       = "T3 KEY TO BE MODIFIED"
   description = "Application Insights key for t3"
-  overwrite = false
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
-
-resource "aws_ssm_parameter" "application_insights_connection_string_t3" {
-  name  = "/application_insights/connection_string_t3"
-  type  = "String"
-  value = "T3 CONNECTION STRING TO BE MODIFIED"
-  description = "Application Insights connection string for t3"
-  overwrite = false
-
+  overwrite   = false
+  tags        = local.tags
   lifecycle {
     ignore_changes = [
       value
@@ -27,26 +13,12 @@ resource "aws_ssm_parameter" "application_insights_connection_string_t3" {
 }
 
 resource "aws_ssm_parameter" "application_insights_key_preprod" {
-  name  = "/application_insights/key_preprod"
-  type  = "SecureString"
-  value = "PREPROD KEY TO BE MODIFIED"
+  name        = "/application_insights/key_preprod"
+  type        = "SecureString"
+  value       = "PREPROD KEY TO BE MODIFIED"
   description = "Application Insights key for preprod"
-  overwrite = false
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
-
-resource "aws_ssm_parameter" "application_insights_connection_string_preprod" {
-  name  = "/application_insights/connection_string_preprod"
-  type  = "SecureString"
-  value = "PREPROD CONNECTION STRING TO BE MODIFIED"
-  description = "Application Insights connection string for preprod"
-  overwrite = false
-
+  overwrite   = false
+  tags        = local.tags
   lifecycle {
     ignore_changes = [
       value
@@ -55,12 +27,12 @@ resource "aws_ssm_parameter" "application_insights_connection_string_preprod" {
 }
 
 resource "aws_ssm_parameter" "application_insights_key_prod" {
-  name  = "/application_insights/key_prod"
-  type  = "SecureString"
-  value = "PROD KEY TO BE MODIFIED"
+  name        = "/application_insights/key_prod"
+  type        = "SecureString"
+  value       = "PROD KEY TO BE MODIFIED"
   description = "Application Insights key for prod"
-  overwrite = false
-
+  overwrite   = false
+  tags        = local.tags
   lifecycle {
     ignore_changes = [
       value
@@ -68,16 +40,3 @@ resource "aws_ssm_parameter" "application_insights_key_prod" {
   }
 }
 
-resource "aws_ssm_parameter" "application_insights_connection_string_prod" {
-  name  = "/application_insights/connection_string_prod"
-  type  = "SecureString"
-  value = "PREPROD CONNECTION STRING TO BE MODIFIED"
-  description = "Application Insights connection string for preprod"
-  overwrite = false
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
