7eeb76d131c0840e238ed61faa17dd4f59d7693b
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-10-30 11:59:27 +0000

    MAP-1752 preprod db update cleanup

--- namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-preprod/resources/rds.tf
@@ -9,7 +9,7 @@ module "dps_rds" {
   environment_name            = var.environment-name
   infrastructure_support      = var.infrastructure_support
 
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t4g.small"
   db_engine_version           = "16.4"
