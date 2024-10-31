eafbecb2e7f069aec9afbb4a8b755ee770529197
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2024-10-31 14:57:52 +0000

    removing unneeded code

--- cmd/yaml-metadata-change-check/utils/github.go
+++ cmd/yaml-metadata-change-check/utils/github.go
@@ -14,21 +14,6 @@ var (
 
 func GitHubClient(token string) *github.Client {
 	client := github.NewClient(nil).WithAuthToken(token)
-
-	// _, resp, err := client.Users.Get(ctx, "")
-	// if err != nil {
-	// 	fmt.Printf("\nerror: %v\n", err)
-	// 	return nil
-	// }
-
-	// // Rate.Limit should most likely be 5000 when authorized.
-	// log.Printf("Rate: %#v\n", resp.Rate)
-
-	// // If a Token Expiration has been set, it will be displayed.
-	// if !resp.TokenExpiration.IsZero() {
-	// 	log.Printf("Token Expiration: %v\n", resp.TokenExpiration)
-	// }
-
 	return client
 }
 
