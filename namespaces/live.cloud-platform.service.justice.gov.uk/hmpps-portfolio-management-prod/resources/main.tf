2451882753fc0bc4e3c52690ba4b9ffba2600ac0
james Hart <james.hart@digital.justice.gov.uk>
2024-10-31 09:51:57 +0000

    fix - double quotes

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = 'hmpps-sre'
+      GithubTeam    = "hmpps-sre"
     }
   }
 }
@@ -23,7 +23,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = 'hmpps-sre'
+      GithubTeam    = "hmpps-sre"
     }
   }
 }
