d6b4344c04034330cb2b3f8e6cf4ede5d3714c1c
james Hart <james.hart@digital.justice.gov.uk>
2024-10-31 09:35:59 +0000

    specify github team this time

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam    = 'hmpps-sre'
     }
   }
 }
@@ -22,7 +23,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam    = var.team_name
+      GithubTeam    = 'hmpps-sre'
     }
   }
 }
