3e6a359f01d1610ee58d5db43ff95e065478a069
Marcus Aspin <maspin@unilink.com>
2024-11-01 16:04:02 +0000

    Update justice-email-and-delius-queue.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/justice-email-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/justice-email-and-delius-queue.tf
@@ -1,12 +1,3 @@
-resource "aws_sns_topic_subscription" "justice-email-and-delius-queue-subscription" {
-  topic_arn = data.aws_sns_topic.hmpps-domain-events.arn
-  protocol  = "sqs"
-  endpoint  = module.justice-email-and-delius-queue.sqs_arn
-  filter_policy = jsonencode({
-    eventType = [] # TODO add event type filter e.g ["prison.case-note.published"]
-  })
-}
-
 module "justice-email-and-delius-queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
 
