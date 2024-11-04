9dd1f2c8171a4946cd21b46d957c692f823122a0
james Hart <james.hart@digital.justice.gov.uk>
2024-11-04 15:58:52 +0000

    add appinsights secrets to template namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-templates-dev/resources/appinsights.tf
@@ -0,0 +1,25 @@
+########################### IMPORTANT ##################################
+## This parameter determines which application insights account is used
+## so you will need to change depending on environment
+
+data "aws_ssm_parameter" "application_insights_key" {
+
+## dev     = /application_insights/key-dev
+## preprod = /application_insights/key-preprod
+## prod    = /application_insights/key-prod
+
+  name = "/application_insights/key-dev"
+}
+
+########################################################################
+
+resource "kubernetes_secret" "application-insights" {
+  metadata {
+    name      = "application-insights"
+    namespace = var.namespace
+  }
+  data = {
+    APPINSIGHTS_INSTRUMENTATIONKEY = data.aws_ssm_parameter.application_insights_key.value
+    APPLICATIONINSIGHTS_CONNECTION_STRING = format("%s%s","InstrumentationKey=",data.aws_ssm_parameter.application_insights_key.value)
+  }
+}
