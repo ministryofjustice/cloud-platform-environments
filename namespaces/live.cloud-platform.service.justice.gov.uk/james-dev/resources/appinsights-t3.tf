f9a06c781d4aa0062409b8e246f4fa9365d05df5
james Hart <james.hart@digital.justice.gov.uk>
2024-11-04 09:18:34 +0000

    correct format info

--- namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/appinsights-t3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/appinsights-t3.tf
@@ -8,7 +8,7 @@ resource "kubernetes_secret" "application-insights" {
     namespace = var.namespace
   }
   data = {
-    APPINSIGHTS_INSTRUMENTATIONKEY = base64encode(data.aws_ssm_parameter.application_insights_key_t3.value)
-    APPLICATIONINSIGHTS_CONNECTION_STRING = base64encode(format("%s/%s","InstrumentationKey=",data.aws_ssm_parameter.application_insights_key_t3.value))
+    APPINSIGHTS_INSTRUMENTATIONKEY = data.aws_ssm_parameter.application_insights_key_t3.value
+    APPLICATIONINSIGHTS_CONNECTION_STRING = format("%s%s","InstrumentationKey=",data.aws_ssm_parameter.application_insights_key_t3.value)
   }
 }
