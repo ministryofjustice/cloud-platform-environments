7e918845fe1d8a6f6236134723c70f9e3d95e295
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-05 10:04:07 +0000

    SDIT-2186: ✨ Create pre-production historical prisoner application (#27479)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-hpa-preprod/resources/rds.tf
@@ -0,0 +1,40 @@
+module "hmpps_hpa_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  namespace              = var.namespace
+  db_engine              = "sqlserver-ex"
+  db_engine_version      = "16.00"
+  db_instance_class      = "db.t4g.small"
+  db_allocated_storage   = 120
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+  rds_family             = "sqlserver-ex-16.0"
+  db_parameter           = []
+  license_model          = "license-included"
+  prepare_for_major_upgrade = false
+
+  performance_insights_enabled = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_hpa_rds" {
+  metadata {
+    name      = "rds-sqlserver-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    DB_SERVER   = module.hmpps_hpa_rds.rds_instance_endpoint
+    DB_USER     = module.hmpps_hpa_rds.database_username
+    DB_PASS     = module.hmpps_hpa_rds.database_password
+    DB_NAME     = "iis-preprod"
+    rds_instance_address  = module.hmpps_hpa_rds.rds_instance_address
+  }
+}
