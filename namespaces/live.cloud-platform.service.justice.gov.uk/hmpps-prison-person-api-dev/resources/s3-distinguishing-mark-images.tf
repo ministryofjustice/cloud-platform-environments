334b82189f3d463fbdc6427e43983800eacfc36d
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-11-05 09:45:48 +0000

    CDPS-956: Add bucket policy allowing read/write access to the document api service account.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/s3-distinguishing-mark-images.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/s3-distinguishing-mark-images.tf
@@ -11,6 +11,41 @@ module "s3-distinguishing-mark-images" {
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+
+  bucket_policy = data.s3_read_write_access_policy.json
+}
+
+locals {
+  document_api_service_account_arn = "arn:aws:iam::754256621582:role/cloud-platform-irsa-c2bb1b0743e08206-live"
+}
+
+# Create policy document to allow read/rite access to the document API service account.
+data "aws_iam_policy_document" "s3_read_write_access_policy" {
+  statement {
+    sid = "BucketLevelPermisions"
+    principals {
+      type        = "AWS"
+      identifiers = [locals.document_api_service_account_arn]
+    }
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [module.s3-distinguishing-mark-images.bucket_arn]
+  }
+
+  statement {
+    sid = "ReadWriteObjectPermissions"
+    principals {
+      type        = "AWS"
+      identifiers = [locals.document_api_service_account_arn]
+    }
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+    resources = ["${module.s3-distinguishing-mark-images.bucket_arn}/*"]
+  }
 }
 
 resource "kubernetes_secret" "s3-distinguidhing-mark-images" {
