c43ad8f640953ac2358777d3e8a515d11780d1d2
Michael Clancy <92720613+mtac50@users.noreply.github.com>
2024-11-05 16:44:37 +0000

    CDPS-956: Create separate secret for s3 data injection (#27543)
    * CDPS-956: Create separate resource secret for the injection into the document api.
    
    * CDPS-956: Empty commit to trigger concourse

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/s3-distinguishing-mark-images.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-person-api-dev/resources/s3-distinguishing-mark-images.tf
@@ -17,12 +17,6 @@ module "s3-distinguishing-mark-images" {
 
 locals {
   document_api_service_account_arn = "arn:aws:iam::754256621582:role/cloud-platform-irsa-c2bb1b0743e08206-live"
-
-  # List of namespaces to inject S3 outputs into.
-  s3-distinguishing-mark-image-consumers = [
-    var.namespace,
-    "hmpps-document-management-dev"
-  ]
 }
 
 # Create policy document to allow read/write access to the document API service account.
@@ -55,12 +49,21 @@ data "aws_iam_policy_document" "s3_read_write_access_policy" {
 }
 
 resource "kubernetes_secret" "s3-distinguishing-mark-images" {
-  for_each = toset(local.s3-distinguishing-mark-image-consumers)
   metadata {
     name      = "s3-distinguishing-mark-images-output"
-    namespace = each.key
+    namespace = var.namespace
   }
+  data = {
+    images_bucket_arn  = module.s3-distinguishing-mark-images.bucket_arn
+    images_bucket_name = module.s3-distinguishing-mark-images.bucket_name
+  }
+}
 
+resource "kubernetes_secret" "s3-distinguishing-mark-images-s3-injected" {
+  metadata {
+    name      = "s3-distinguishing-mark-images-output-injected"
+    namespace = "hmpps-document-management-dev"
+  }
   data = {
     images_bucket_arn  = module.s3-distinguishing-mark-images.bucket_arn
     images_bucket_name = module.s3-distinguishing-mark-images.bucket_name
