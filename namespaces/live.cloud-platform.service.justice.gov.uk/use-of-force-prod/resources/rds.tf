3493e9e38b3da2e00bf15d51d269c3893aae30fb
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-11-05 14:12:34 +0000

    MAP-1752 production DB update

--- namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-prod/resources/rds.tf
@@ -9,12 +9,12 @@ module "dps_rds" {
   environment_name            = var.environment-name
   infrastructure_support      = var.infrastructure_support
 
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t4g.small"
   db_allocated_storage        = 400
-  db_engine_version           = "15.7"
-  rds_family                  = "postgres15"
+  db_engine_version           = "16.4"
+  rds_family                  = "postgres16"
   db_password_rotated_date    = "13-04-2023"
   db_iops                     = "12000"
 
