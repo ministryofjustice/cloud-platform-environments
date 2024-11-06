5b67738d6786de3e22aa758b7a9aebfe8d8f3db9
adam <brown-a2@users.noreply.github.com>
2024-11-06 15:45:32 +0000

    Update DB version and set option to true
    Even though this is upgrading from 10.4 -> 10.11 it is a major update. Following this teams lead #26924

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/rds.tf
@@ -11,15 +11,16 @@ module "rds" {
   performance_insights_enabled = false
 
   # general options
-  db_instance_class           = "db.t4g.small"
-  environment_name            = var.environment
-  infrastructure_support      = var.infrastructure_support
-  allow_major_version_upgrade = "false"
+  db_instance_class      = "db.t4g.small"
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
 
   # using mysql
   db_engine         = "mariadb"
-  db_engine_version = "10.4"
-  rds_family        = "mariadb10.4"
+  db_engine_version = "10.11"
+  rds_family        = "mariadb10.11"
+
+  prepare_for_major_upgrade = true
 
   # overwrite db_parameters
   db_parameter = [
