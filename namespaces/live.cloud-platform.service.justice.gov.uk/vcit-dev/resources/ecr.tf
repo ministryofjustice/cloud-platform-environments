3153f3a5cfee7da4129c18556d87d08541451618
oli-deane-moj <oliver.deane@justice.gov.uk>
2024-11-07 12:35:26 +0000

    Added vcit-dev namespace with ECR and service account configuration

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vcit-dev/resources/ecr.tf
@@ -0,0 +1,33 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.0.0"
+
+  # Repository configuration
+  repo_name = var.namespace
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github"]
+  github_repositories = ["vcit-test-app"]
+
+  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
+  github_environments = ["dev"]
+
+  # OPTIONAL:
+  # set this if you use one GitHub repository to push to multiple container repositories
+  # this ensures the variable key used in the workflow is unique
+  github_actions_prefix = "dev"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
