b1c73c2ec6f8a03de9441e873147b645ce3fc9be
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-11-08 10:09:59 +0000

    SDIT-2186: 🔒️ Switch to web version for encryption at rest

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/rds.tf
@@ -1,4 +1,4 @@
-module "hmpps_hpa_rds" {
+module "hpa_rds" {
   source                    = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
   vpc_name                  = var.vpc_name
   team_name                 = var.team_name
@@ -6,18 +6,19 @@ module "hmpps_hpa_rds" {
   application               = var.application
   is_production             = var.is_production
   namespace                 = var.namespace
-  db_engine                 = "sqlserver-ex"
+  db_engine                 = "sqlserver-web"
+  rds_family                = "sqlserver-web-16.0"
   db_engine_version         = "16.00"
   db_instance_class         = "db.t3.small"
   db_allocated_storage      = 20
   db_max_allocated_storage  = 100
   environment_name          = var.environment-name
   infrastructure_support    = var.infrastructure_support
-  rds_family                = "sqlserver-ex-16.0"
-  db_parameter = []
-  license_model             = "license-included"
-  prepare_for_major_upgrade = false
 
+  deletion_protection          = true
+  allow_minor_version_upgrade  = true
+  allow_major_version_upgrade  = false
+  prepare_for_major_upgrade    = false
   performance_insights_enabled = false
 
   providers = {
@@ -25,17 +26,16 @@ module "hmpps_hpa_rds" {
   }
 }
 
-resource "kubernetes_secret" "hmpps_hpa_rds" {
+resource "kubernetes_secret" "hpa_rds" {
   metadata {
     name      = "rds-sqlserver-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    DB_SERVER            = module.hmpps_hpa_rds.rds_instance_endpoint
-    DB_USER              = module.hmpps_hpa_rds.database_username
-    DB_PASS              = module.hmpps_hpa_rds.database_password
-    DB_NAME              = "iis-dev"
-    rds_instance_address = module.hmpps_hpa_rds.rds_instance_address
+    DB_SERVER            = module.hpa_rds.rds_instance_endpoint
+    DB_USER              = module.hpa_rds.database_username
+    DB_PASS              = module.hpa_rds.database_password
+    rds_instance_address = module.hpa_rds.rds_instance_address
   }
 }
