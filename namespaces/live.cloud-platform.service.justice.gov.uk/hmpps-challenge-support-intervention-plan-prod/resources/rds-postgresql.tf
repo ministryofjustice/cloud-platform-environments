3f53cee95f65bacfcc07049586f3420ce2a22fce
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-11-12 15:50:11 +0000

    MICN-210

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-prod/resources/rds-postgresql.tf
@@ -79,6 +79,38 @@ module "read_replica" {
   skip_final_snapshot        = "true"
   db_backup_retention_period = 0
 
+  vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40GB"
+      apply_method = "immediate"
+    }
+  ]
+}
+
+data "aws_security_group" "mp_dps_sg" {
+  name = var.mp_dps_sg_name
 }
 
 resource "kubernetes_secret" "read_replica" {
