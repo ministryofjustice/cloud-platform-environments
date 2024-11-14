e3057bf429586fd9300b1dae492be614d74fbdf4
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-11-14 12:50:53 +0000

    Added hmpps-person-integration-api-prod namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-integration-api-prod/resources/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.2.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.39.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+    pingdom = {
+      source  = "DrFaust92/pingdom"
+      version = "~> 1.3.1"
+    }
+  }
+}
