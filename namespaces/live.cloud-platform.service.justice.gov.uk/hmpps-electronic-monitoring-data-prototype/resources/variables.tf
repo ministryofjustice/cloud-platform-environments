84bbf4eedf372c8edf959681f802b33f75032270
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2024-11-14 16:03:20 +0000

    Renamed prototype and GH repo to more meaningful name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-prototype/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-prototype/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "em-data"
+  default     = "hmpps-electronic-monitoring-data-prototype"
 }
 
 variable "business_unit" {
