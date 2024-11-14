33d93eb8343ea1b907853b20e859c70e32d4d6a0
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2024-11-14 09:13:18 +0000

    CICA: update aws_iam_policy for irsa

--- namespaces/live.cloud-platform.service.justice.gov.uk/cica-copilot-dev/resources/bedrock.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-copilot-dev/resources/bedrock.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "guidance_bedrock_role" {
   name = "${var.namespace}-mod-platform-bedrock-connector"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           AWS = "arn:aws:iam::321388111150:role/BedrockAccessforCP"
         }
@@ -17,22 +17,14 @@ resource "aws_iam_role" "guidance_bedrock_role" {
 resource "aws_iam_policy" "access_bedrock_service" {
   name   = "AccessBedrockService"
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [ 
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
-          "bedrock:InvokeModel",
-          "bedrock:Get*",
-          "bedrock:List*",
+          "sts:AssumeRole"
         ],
-        Resource = [
-          "arn:aws:bedrock:eu-central-1:321388111150:model/amazon.titan-embed-text-v1",
-          "arn:aws:bedrock:eu-central-1:321388111150:model/anthropic.claude-instant-v1",
-          "arn:aws:bedrock:eu-central-1:321388111150:model/anthropic.claude-v2:1",
-          "arn:aws:bedrock:eu-central-1:321388111150:model/anthropic.claude-v2",
-          "arn:aws:bedrock:eu-central-1:321388111150:model/anthropic.claude-3-5-sonnet-20240620-v1:0",          
-        ]
+        Resource = "arn:aws:iam::321388111150:role/BedrockAccessforCP"
       }
     ]
   })
