1034854fadfa143582114e09f7e27aad11318f24
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2024-11-15 14:56:42 +0000

    Update hmpps-challenge-support-intervention-plan-ui.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-challenge-support-intervention-plan-ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-challenge-support-intervention-plan-ui.tf
@@ -8,6 +8,5 @@ resource "kubernetes_secret" "hmpps_challenge_support_intervention_plan_secret" 
     sqs_queue_url   = module.hmpps_audit_queue.sqs_id
     sqs_queue_arn   = module.hmpps_audit_queue.sqs_arn
     sqs_queue_name  = module.hmpps_audit_queue.sqs_name
-    irsa_policy_arn = module.hmpps_audit_queue.irsa_policy_arn
   }
 }
