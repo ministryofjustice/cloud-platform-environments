3f21798bbcb854efe4b380a6e8c0f6c3ae374eff
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-11-15 10:21:13 +0000

    Ignore - Raising to review plan
    Trying to confirm if changing k8s secret to new rds module would work

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
@@ -25,6 +25,32 @@ module "visit_scheduler_rds" {
   }
 }
 
+module "visit_scheduler_store_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.2.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  allow_major_version_upgrade  = "false"
+  prepare_for_major_upgrade    = false
+  db_engine                    = "postgres"
+  db_engine_version            = "15.7"
+  rds_family                   = "postgres15"
+  db_instance_class            = "db.t4g.small"
+  db_max_allocated_storage     = "100"
+  db_password_rotated_date     = "2023-05-11"
+  performance_insights_enabled = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 resource "kubernetes_secret" "visit_scheduler_rds" {
   metadata {
     name      = "visit-scheduler-rds"
@@ -32,11 +58,11 @@ resource "kubernetes_secret" "visit_scheduler_rds" {
   }
 
   data = {
-    rds_instance_endpoint = module.visit_scheduler_rds.rds_instance_endpoint
-    database_name         = module.visit_scheduler_rds.database_name
-    database_username     = module.visit_scheduler_rds.database_username
-    database_password     = module.visit_scheduler_rds.database_password
-    rds_instance_address  = module.visit_scheduler_rds.rds_instance_address
+    rds_instance_endpoint = module.visit_scheduler_store_rds.rds_instance_endpoint
+    database_name         = module.visit_scheduler_store_rds.database_name
+    database_username     = module.visit_scheduler_store_rds.database_username
+    database_password     = module.visit_scheduler_store_rds.database_password
+    rds_instance_address  = module.visit_scheduler_store_rds.rds_instance_address
   }
 }
 
