581c4c38be18051df0ece1786d64bbd9b9151bef
Jasper <54336703+JazJax@users.noreply.github.com>
2024-11-15 09:41:15 +0000

    updating irsa policy doc

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-datastore-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-datastore-dev/resources/cross-iam-role-sa.tf
@@ -18,40 +18,14 @@
     data "aws_iam_policy_document" "document" {
       # Provide list of permissions and target AWS account resources to allow access to
       statement {
-        actions = []
-        # actions = [
-        #   "athena:CancelQueryExecution",
-        #   "athena:GetQueryExecution",
-        #   "athena:GetQueryResults",
-        #   "athena:GetWorkGroup",
-        #   "athena:StartQueryExecution",
-        #   "athena:StopQueryExecution",
-        #   "s3:GetObject",
-        #   "s3:PutObject",
-        #   "s3:ListBucket",
-        #   "s3:ListAllMyBuckets",
-        #   "s3:GetBucketLocation",
-        #   "s3:ListBucketMultipartUploads",
-        #   "s3:ListMultipartUploadParts",
-        #   "s3:AbortMultipartUpload",
-        #   "s3:CreateBucket",
-        #   "glue:GetDatabase",
-        #   "glue:GetTable",
-        #   "glue:GetTables",
-        #   "glue:UpdateTable",
-        #   "glue:DeleteTable",
-        #   "glue:GetPartitions",
-        #   "glue:GetPartition",
-        #   "glue:BatchCreatePartition",
-        #   "glue:GetDatabases",
-        #   "glue:CreateTable",
-        #   "glue:CreateDatabase",
-        #   "glue:DeleteTable",
-        # ]
-        resources = []
-        # resources = [
-        #   "arn:aws:athena:eu-west-2:800964199911:workgroup/*",
-        # ]
+        actions = [
+          "sts:AssumeRole"
+        ]
+        # This is a placeholder principal - waiting on real values before merging~
+        principals {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::321388111150:role/BedrockAccessforCP"]
+        }
       }
     }
     resource "aws_iam_policy" "em_datastore_api_athena_general" {
