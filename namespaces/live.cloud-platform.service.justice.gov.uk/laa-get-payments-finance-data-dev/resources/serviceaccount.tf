807895643103cfe7740f1095e3d5a4553755651d
Robert Buczek <robert.buczek@digital.justice.gov.uk>
2024-11-15 14:20:08 +0000

    [PF-468]: Create New Dev Environment in Cloud Platform - add rules for service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
@@ -10,6 +10,57 @@ module "serviceaccount" {
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["payforlegalaid"]
   github_environments = ["dev"]
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources  = ["pods/exec"]
+      verbs      = ["create"]
+    },
+    {
+      api_groups = [""]
+      resources = [
+        "pods/portforward",
+        "deployment",
+        "secrets",
+        "services",
+        "pods",
+        "configmaps",
+      ]
+      verbs = [
+        "patch",
+        "get",
+        "create",
+        "update",
+        "delete",
+        "list",
+        "watch"
+      ]
+    },
+    {
+      api_groups = [
+        "extensions",
+        "apps",
+        "batch",
+        "networking.k8s.io",
+      ]
+      resources = [
+        "deployments",
+        "ingresses",
+        "cronjobs",
+        "jobs",
+        "statefulsets",
+        "replicasets"
+      ]
+      verbs = [
+        "get",
+        "update",
+        "delete",
+        "create",
+        "patch",
+        "list"
+      ]
+    },
+  ]
 
   github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
   github_actions_secret_kube_token     = var.github_actions_secret_kube_token
