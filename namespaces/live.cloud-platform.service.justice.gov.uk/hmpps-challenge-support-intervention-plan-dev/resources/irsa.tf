48596982f96f5453f2dd350cb22477000896a1af
James Reed <james.reed@digital.justice.gov.uk>
2024-11-15 15:51:01 +0000

    feat: add svc account for csip for audit access

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-dev/resources/irsa.tf
@@ -41,6 +41,24 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "hmpps-challenge-support-intervention-plan-ui-service-account" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  application            = var.application
+  business_unit          = var.business_unit
+  eks_cluster_name       = var.eks_cluster_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  service_account_name = "hmpps-challenge-support-intervention-plan-ui"
+  role_policy_arns = merge(
+    { elasticache = module.elasticache.irsa_policy_arn },
+    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+  )
+}
+
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
