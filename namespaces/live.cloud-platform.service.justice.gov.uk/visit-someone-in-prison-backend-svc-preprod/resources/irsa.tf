d3981fb4d8361b8218636edd2e219e815afe1e47
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-11-18 16:39:05 +0000

    temporarily remove rds policy in irsa.tf while database is rebuilt

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/irsa.tf
@@ -8,7 +8,7 @@ locals {
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 
   rds_policies = {
-    visit_scheduler_rds              = module.visit_scheduler_rds.irsa_policy_arn,
+    # visit_scheduler_rds              = module.visit_scheduler_rds.irsa_policy_arn,
     prison_visit_booker_registry_rds = module.prison_visit_booker_registry_rds.irsa_policy_arn
   }
 
