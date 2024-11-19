5af1ff97a0f56cdceb6f00a3f77698e4ddc2123a
karenmillermoj <karen.miller@digital.justice.gov.uk>
2024-11-19 10:29:36 +0000

    SDIT-2262 rename service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/irsa.tf
@@ -17,7 +17,7 @@ module "historical_prisoner_irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = "hmpps-historical_prisoner-api"
+  service_account_name = "hmpps-historical_prisoner"
   namespace            = var.namespace
 
   role_policy_arns     = local.sqs_policies
