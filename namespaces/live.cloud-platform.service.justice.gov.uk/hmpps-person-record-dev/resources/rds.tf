f9683bb367e784aa887ce79f0aa5aafa58d487ba
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-11-20 11:25:50 +0000

    CPR-503 Upgrade RDS DEV

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
@@ -1,5 +1,5 @@
 module "hmpps_person_record_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.2.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
   vpc_name               = var.vpc_name
   team_name              = var.team_name
   business_unit          = var.business_unit
@@ -11,7 +11,7 @@ module "hmpps_person_record_rds" {
   rds_family             = "postgres16"
   db_instance_class      = "db.t3.small"
   db_engine              = "postgres"
-  db_engine_version      = "16.1"
+  db_engine_version      = "17.1"
   prepare_for_major_upgrade = false
   allow_major_version_upgrade = "true"
   enable_rds_auto_start_stop   = true
