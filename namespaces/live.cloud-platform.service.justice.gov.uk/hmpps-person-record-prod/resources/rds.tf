45fae230ca776f1e7a93cfaa6992b5ff6b4c19e5
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-11-21 11:48:10 +0000

    CPR-503 Toggle off prepare for upgrade

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/rds.tf
@@ -12,7 +12,7 @@ module "hmpps_person_record_rds" {
   db_instance_class            = "db.r6g.2xlarge"
   db_engine                    = "postgres"
   db_engine_version            = "17.1"
-  prepare_for_major_upgrade    = true
+  prepare_for_major_upgrade    = false
   performance_insights_enabled = true
   allow_major_version_upgrade  = "true"
 
