8d294d29d1fdbf5d2ca53e6bf890336ddece1c8d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-11-21 13:11:56 +0000

    Sort GH teams

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "dps-tech"
+  default     = "hmpps-sre"
 }
 
 variable "environment" {
