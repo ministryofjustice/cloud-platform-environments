895b9618fc372b23a25b6f66ca7a12c365413eb6
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-11-27 17:04:06 +0000

    Rename variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-dev/resources/route53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "route53_zone" {
     business-unit          = var.business_unit
     application            = var.application
     is-production          = var.is_production
-    environment-name       = var.environment-name
+    environment-name       = var.environment_name
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
     namespace              = var.namespace
@@ -30,7 +30,7 @@ resource "aws_route53_zone" "route53_justice_zone" {
     business-unit          = "HMPPS"
     application            = "PVB"
     is-production          = var.is_production
-    environment-name       = var.environment-name
+    environment-name       = var.environment_name
     owner                  = var.team_name
     infrastructure-support = var.infrastructure_support
     namespace              = var.namespace
