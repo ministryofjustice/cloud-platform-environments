05407fced91cbed8a9c7581941b9dfb96211b06f
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-11-29 14:11:43 +0000

    Create new RDS instance for visit scheduler in Production to replace current

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-prod/resources/rds.tf
@@ -40,6 +40,49 @@ resource "kubernetes_secret" "visit_scheduler_rds" {
   }
 }
 
+module "visit_scheduler_pg_rds" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  allow_major_version_upgrade  = "false"
+  prepare_for_major_upgrade    = false
+  db_engine                    = "postgres"
+  db_engine_version            = "15.7"
+  rds_family                   = "postgres15"
+  db_instance_class            = "db.t4g.small"
+  db_max_allocated_storage     = "200"
+  db_allocated_storage         = "35"
+  storage_type                 = "gp3"
+  performance_insights_enabled = true
+  deletion_protection          = true
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "visit_scheduler_pg_rds" {
+  metadata {
+    name      = "visit-scheduler-pg-rds"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.visit_scheduler_pg_rds.rds_instance_endpoint
+    database_name         = module.visit_scheduler_pg_rds.database_name
+    database_username     = module.visit_scheduler_pg_rds.database_username
+    database_password     = module.visit_scheduler_pg_rds.database_password
+    rds_instance_address  = module.visit_scheduler_pg_rds.rds_instance_address
+  }
+}
+
 resource "kubernetes_secret" "prison_visit_booker_registry_rds" {
   metadata {
     name      = "prison-visit-booker-registry-rds"
