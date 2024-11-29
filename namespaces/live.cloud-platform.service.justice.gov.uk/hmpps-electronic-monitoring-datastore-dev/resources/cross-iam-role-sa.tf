8495727646576272b43f4c7b15ac1e0b9d39b8af
Jasper <54336703+JazJax@users.noreply.github.com>
2024-11-29 09:24:44 +0000

    renamed service-account to comply with max-length restrictions on service-pod name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-datastore-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-datastore-dev/resources/cross-iam-role-sa.tf
@@ -2,7 +2,7 @@
     module "irsa" {
       source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
       eks_cluster_name      = var.eks_cluster_name
-      service_account_name  = "${var.namespace}-athena-service-account"
+      service_account_name  = "${var.namespace}-athena"
       namespace             = var.namespace
       role_policy_arns = {
         athena = aws_iam_policy.athena_access.arn
