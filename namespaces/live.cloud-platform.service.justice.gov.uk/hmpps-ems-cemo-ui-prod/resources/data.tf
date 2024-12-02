118caeed9f3be8ddd9740eda45486d75bb06e90f
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-12-02 15:24:17 +0000

    PIC-4367 get secret from correct namespace for court-cases-topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-prod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-prod/resources/data.tf
@@ -3,5 +3,5 @@ data "aws_ssm_parameter" "court-case-events-topic-arn" {
 }
 
 data "aws_ssm_parameter" "court-cases-topic-arn" {
-  name = "/court-probation-preprod/court-cases-topic-arn"
+  name = "/court-probation-prod/court-cases-topic-arn"
 }
