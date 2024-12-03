a6a1fd05de60a7b31298aa70bb6d32aaa05011cf
Toby Privett <toby@snaplab.co.uk>
2024-12-03 16:13:07 +0000

    Add policies to the `rds-read-replica`, not the `rds-instance`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
@@ -42,32 +42,9 @@ module "rds-instance" {
     aws = aws.london
   }
 
-  # Add security groups for DPR
-  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
-
   db_parameter = [
     {
       name         = "rds.force_ssl"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-     {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
       value        = "0"
       apply_method = "immediate"
     }
@@ -142,9 +119,32 @@ module "rds-read-replica" {
     aws = aws.london
   }
 
+  # Add security groups for DPR
+  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
+
   db_parameter = [
     {
       name         = "rds.force_ssl"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+     {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
       value        = "0"
       apply_method = "immediate"
     }
