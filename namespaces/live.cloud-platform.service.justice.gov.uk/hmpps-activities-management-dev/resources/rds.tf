a022d0de809028481e7bb5c044fbd101b698529d
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-12-04 08:00:30 +0000

    Destroy hmpps Activities Dev RDS instance

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
@@ -3,55 +3,6 @@ data "aws_security_group" "mp_dps_sg" {
   name = var.mp_dps_sg_name
 }
 
-module "activities_api_rds" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
-  vpc_name                    = var.vpc_name
-  team_name                   = var.team_name
-  business_unit               = var.business_unit
-  application                 = var.application
-  is_production               = var.is_production
-  namespace                   = var.namespace
-  environment_name            = var.environment
-  infrastructure_support      = var.infrastructure_support
-  rds_family                  = var.rds_family
-  allow_major_version_upgrade = "false"
-  allow_minor_version_upgrade = "true"
-  db_instance_class           = "db.t4g.small"
-  db_engine_version           = "14"
-  
-
-  # Add security groups for DPR
-  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
-
-  # Add parameters to enable DPR team to configure replication
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-     {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    }
-  ]
-
-  providers = {
-    aws = aws.london
-  }
-}
-
 resource "kubernetes_secret" "activities_api_rds" {
   metadata {
     name      = "rds-instance-output"
@@ -66,7 +17,6 @@ resource "kubernetes_secret" "activities_api_rds" {
     rds_instance_address  = module.activities_rds.rds_instance_address
   }
 }
-
 
 module "activities_rds" {
   source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
