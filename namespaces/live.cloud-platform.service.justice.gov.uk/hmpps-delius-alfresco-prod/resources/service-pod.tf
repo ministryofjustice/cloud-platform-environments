24c168281655e73aca2925c3f78a0d00f6f2e491
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-12-04 11:12:34 +0000

    TM-127 upgrading module version for rds and service pod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-prod/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-prod/resources/service-pod.tf
@@ -1,5 +1,5 @@
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
 
   # Configuration
   namespace            = var.namespace
