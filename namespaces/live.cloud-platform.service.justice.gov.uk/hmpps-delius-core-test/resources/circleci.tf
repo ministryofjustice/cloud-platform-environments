790e009f605c8526f3ef63f7fd0b65784a8c761b
Marcus Aspin <maspin@unilink.com>
2024-12-10 13:57:08 +0000

    PI-2658 Initial setup for moving Community API to MOJ Cloud Platform (test) (#28340)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-test/resources/circleci.tf
@@ -0,0 +1,8 @@
+module "circleci_service_account" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace            = var.namespace
+  kubernetes_cluster   = var.kubernetes_cluster
+  serviceaccount_rules = var.serviceaccount_rules
+  serviceaccount_name  = "circleci"
+}
