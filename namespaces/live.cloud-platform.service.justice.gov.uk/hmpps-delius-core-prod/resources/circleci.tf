297034e3bdb91588079fc67385da8bfc73d5e8ac
Marcus Aspin <maspin@unilink.com>
2024-12-10 13:51:35 +0000

    PI-2658 Initial setup for moving Community API to MOJ Cloud Platform (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-prod/resources/circleci.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-prod/resources/circleci.tf
@@ -1,10 +1,8 @@
-module "github_actions_service_account" {
+module "circleci_service_account" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
   namespace            = var.namespace
   kubernetes_cluster   = var.kubernetes_cluster
-  github_repositories  = ["hmpps-delius-docker-images", "ndelius-um"]
   serviceaccount_rules = var.serviceaccount_rules
-  # This GitHub environment will need to be created manually first
-  github_environments = [var.environment_name]
+  serviceaccount_name  = "circleci"
 }
