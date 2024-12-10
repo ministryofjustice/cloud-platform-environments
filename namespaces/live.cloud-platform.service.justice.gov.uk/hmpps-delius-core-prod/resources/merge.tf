1ced8a869f0b235b2a60734070402ea4c8b25cf9
Marcus Aspin <maspin@unilink.com>
2024-12-10 15:56:04 +0000

    PI-2658 Add network policy for pathfinder (prod) (#28351)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-prod/resources/merge.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-prod/resources/merge.tf
@@ -12,7 +12,7 @@ module "merge_rds" {
   performance_insights_enabled = false
   db_max_allocated_storage     = 500
   db_allocated_storage         = 100
-  enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  enable_rds_auto_start_stop   = false
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
