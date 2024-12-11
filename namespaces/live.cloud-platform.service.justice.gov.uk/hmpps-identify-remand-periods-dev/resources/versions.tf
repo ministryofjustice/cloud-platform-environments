7eaa8489023bafccf0196a97ce795c4022baf32b
Luke Harper <luke.harper@digital.justice.gov.uk>
2024-12-11 10:59:54 +0000

    ADJST-929 Analytics Platfrom IRSA Identify Remand Dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/versions.tf
@@ -14,5 +14,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
   }
 }
