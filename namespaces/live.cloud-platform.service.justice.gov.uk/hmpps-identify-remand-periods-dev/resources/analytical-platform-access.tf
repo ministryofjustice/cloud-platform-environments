8f4125c88b65a9747a72c7ce4bd99ff20e05dbd8
Luke Harper <luke.harper@digital.justice.gov.uk>
2024-12-11 11:28:01 +0000

    Trigger Build

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/analytical-platform-access.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/analytical-platform-access.tf
@@ -1,11 +1,16 @@
 module "ap_irsa" {
-  source           = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  namespace        = var.namespace
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
   eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "${var.namespace}-to-ap-s3"
+  namespace            = var.namespace # this is also used as a tag
   role_policy_arns = {
     s3 = aws_iam_policy.ap_policy.arn
   }
-  service_account_name = "${var.namespace}-to-ap-s3"
+
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -13,6 +18,7 @@ module "ap_irsa" {
   team_name              = var.team_name
   environment_name       = var.environment_name
   infrastructure_support = var.infrastructure_support
+
 }
 
 resource "aws_iam_policy" "ap_policy" {
@@ -43,29 +49,10 @@ data "aws_iam_policy_document" "ap_access" {
     ]
 
     resources = [
-      "arn:aws:s3:::moj-reg-dev/landing/hmpps-identify-remand-periods-api-dev/*",
-      "arn:aws:s3:::moj-reg-dev/landing/hmpps-identify-remand-periods-api-dev/"
+      "arn:aws:s3:::moj-reg-dev/landing/${var.namespace}/*",
+      "arn:aws:s3:::moj-reg-dev/landing/${var.namespace}/"
     ]
   }
-}
-
-resource "random_id" "id" {
-  byte_length = 16
-}
-
-resource "aws_iam_user" "user" {
-  name = "ap-s3-bucket-user-${random_id.id.hex}"
-  path = "/system/ap-s3-bucket-user/"
-}
-
-resource "aws_iam_access_key" "user" {
-  user = aws_iam_user.user.name
-}
-
-resource "aws_iam_user_policy" "policy" {
-  name   = "${var.namespace}-ap-s3-snapshots"
-  policy = data.aws_iam_policy_document.ap_access.json
-  user   = aws_iam_user.user.name
 }
 
 resource "kubernetes_secret" "ap_aws_secret" {
@@ -75,21 +62,9 @@ resource "kubernetes_secret" "ap_aws_secret" {
   }
 
   data = {
-    destination_bucket = "s3://moj-reg-dev/landing/hmpps-identify-remand-periods-api-dev/"
-    user_arn           = aws_iam_user.user.arn
-    access_key_id      = aws_iam_access_key.user.id
-    secret_access_key  = aws_iam_access_key.user.secret
-  }
-}
-
-resource "kubernetes_secret" "ap_irsa" {
-  metadata {
-    name      = "to-ap-s3-irsa"
-    namespace = var.namespace
-  }
-
-  data = {
-    role           = module.ap_irsa.role_name
-    serviceaccount = module.ap_irsa.service_account.name
+    destination_bucket = "s3://moj-reg-dev/landing/${var.namespace}/"
+    role               = module.ap_irsa.role_name
+    serviceaccount     = module.ap_irsa.service_account.name
+    rolearn            = module.ap_irsa.role_arn
   }
 }
