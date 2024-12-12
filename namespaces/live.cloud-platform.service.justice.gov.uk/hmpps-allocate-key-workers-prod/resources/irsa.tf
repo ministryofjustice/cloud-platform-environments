2c644d961dfd1276f889bdee8292f70206c58053
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-12-12 16:47:21 +0000

    Create new namespace hmpps-allocate-key-workers-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-prod/resources/irsa.tf
@@ -0,0 +1,24 @@
+data "kubernetes_secret" "audit_secret" {
+  metadata {
+    name      = "sqs-hmpps-audit-secret"
+    namespace = var.namespace
+  }
+}
+
+module "hmpps-allocate-key-workers-ui-service-account" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  application            = var.application
+  business_unit          = var.business_unit
+  eks_cluster_name       = var.eks_cluster_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  service_account_name = "hmpps-allocate-key-workers-ui"
+  role_policy_arns = merge(
+    { elasticache = module.elasticache_redis.irsa_policy_arn },
+#     { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+  )
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-prod/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {}
+
+
+resource "pingdom_check" "pingdom_ui" {
+  type                     = "http"
+  name                     = "HMPPS CSIP UI"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  port                     = 443
+  url                      = "/https/allocate-key-workers.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  encryption               = true
+  tags                     = "hmpps,cloudplatform-managed,hmpps-allocate-key-workers-ui,isproduction_true"
+  probefilters             = "region:EU"
+  integrationids           = [136330] # move-and-improve-alerts
+}
