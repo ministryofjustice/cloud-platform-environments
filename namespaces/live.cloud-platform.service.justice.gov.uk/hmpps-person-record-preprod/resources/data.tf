e46c87533c5375dabd1ef4923eecd77fc49631fe
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-12-12 11:39:06 +0000

    CPR-421 create preprod FIFO queue subscribed to court-cases-topic in court-probation-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
@@ -2,6 +2,10 @@ data "aws_ssm_parameter" "court-case-events-topic-arn" {
   name = "/court-probation-preprod/topic-arn"
 }
 
+data "aws_ssm_parameter" "court-cases-topic-arn" {
+  name = "/court-probation-preprod/court-cases-topic-arn"
+}
+
 data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-preprod/topic-arn"
 }
