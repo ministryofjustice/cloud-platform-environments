a0788a1d65206b9887b950ee4a72f7c7b96b7c52
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-12-12 15:09:46 +0000

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-dev/resources/irsa.tf
@@ -11,7 +11,8 @@ module "hmpps-allocate-key-workers-ui-service-account" {
 
   service_account_name = "hmpps-allocate-key-workers-ui"
   role_policy_arns = merge(
-    { elasticache = module.elasticache_redis.irsa_policy_arn },
-    # { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
+    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
   )
+
+  depends_on = [data.kubernetes_secret.audit_secret.data.irsa_policy_arn]
 }
