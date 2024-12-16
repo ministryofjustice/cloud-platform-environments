4dc8fad7a03b88d81b253a9c3fd0ba3f162816c1
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-12-16 16:34:00 +0000

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-prod/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-prod/resources/versions.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9.0"
+    }
     pingdom = {
       source  = "DrFaust92/pingdom"
       version = "~> 1.3.1"
