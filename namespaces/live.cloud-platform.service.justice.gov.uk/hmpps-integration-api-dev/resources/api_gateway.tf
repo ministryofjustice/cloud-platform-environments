a518fbeb914d2ec44e859b44d78b65bea7d3fd86
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-12-16 10:33:45 +0000

    PI-2686 remove gateway alerts

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/api_gateway.tf
@@ -256,20 +256,3 @@ resource "aws_api_gateway_method_settings" "all" {
     data_trace_enabled = true
   }
 }
-
-module "sns_topic" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=5.0.2"
-
-  # Configuration
-  topic_display_name = "integration-api-alert-topic"
-  encrypt_sns_kms    = true
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the topic
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-}
