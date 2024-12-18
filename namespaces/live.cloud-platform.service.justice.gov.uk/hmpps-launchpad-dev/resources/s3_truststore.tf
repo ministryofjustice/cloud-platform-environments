01c432a3b73c33b6f205b4259749ea9fe84dcd90
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2024-12-18 13:00:17 +0000

    adding certifictae back up

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/s3_truststore.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/s3_truststore.tf
@@ -44,6 +44,41 @@ resource "aws_s3_object" "truststore" {
   content = data.kubernetes_secret.truststore.data["truststore-public-key"]
 }
 
+module "certificate_backup" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+  bucket_name            = "${var.namespace}-certificates-backup"
+  versioning             = true
+
+  bucket_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowBucketAccess",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_iam_user.api_gateway_user.arn}"
+      },
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:GetObjectVersion"
+      ],
+      "Resource": [
+        "$${bucket_arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
 
 resource "random_password" "event_service_certificate_password" {
   length           = 16
@@ -51,3 +86,14 @@ resource "random_password" "event_service_certificate_password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
+resource "kubernetes_secret" "certificate_backup_secret" {
+  metadata {
+    name      = "certificate-store"
+    namespace = var.namespace
+  }
+  data = {
+    bucket_name                        = module.certificate_backup.bucket_name
+    event_service_certificate_path     = "event-service/client-debug.p12"
+    event_service_certificate_password = random_password.event_service_certificate_password.result
+  }
+}
