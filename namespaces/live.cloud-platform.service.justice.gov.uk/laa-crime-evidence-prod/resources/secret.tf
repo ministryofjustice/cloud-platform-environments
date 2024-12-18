9db6bdff38f6ac70ce4f24eaea0b18850c4f9cec
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-12-18 08:13:59 +0000

    added config for new crime evidence secret for slack webhook

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-evidence-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-evidence-prod/resources/secret.tf
@@ -20,5 +20,10 @@ module "secrets_manager" {
       recovery_window_in_days = 7
       k8s_secret_name         = "maat-api-oauth-client-secret"
     },
+    "crime_evidence_alert_webhook_prod" = {
+      description             = "Crime Evidence Slack Webhook",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "crime-evidence-alert-webhook-prod"
+    }
   }
 }
