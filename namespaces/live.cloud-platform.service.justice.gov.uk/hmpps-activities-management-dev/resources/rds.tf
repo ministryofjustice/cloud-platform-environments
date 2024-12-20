6cd7dd4933ae129554d9fb513a0a1d3e9748a6cf
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2024-12-20 13:43:06 +0000

    share replication params between source and replica for DPR ingestion on activities-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
@@ -3,6 +3,42 @@ data "aws_security_group" "mp_dps_sg" {
   name = var.mp_dps_sg_name
 }
 
+resource "aws_db_parameter_group" "replication_params" {
+  name        = "shared-postgres-params"
+  family      = var.rds_family
+  description = "Custom parameter group for replication parameters thats shared between main rds and the read replica"
+
+  parameter {
+    name         = "rds.logical_replication"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "shared_preload_libraries"
+    value        = "pglogical"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "max_wal_size"
+    value        = "1024"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "wal_sender_timeout"
+    value        = "0"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "max_slot_wal_keep_size"
+    value        = "5000"
+    apply_method = "immediate"
+  }
+}
+
 resource "kubernetes_secret" "activities_api_rds" {
   metadata {
     name      = "rds-instance-output"
@@ -39,34 +75,8 @@ module "activities_rds" {
   # Add security groups for DPR
   vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
 
-  # Add parameters to enable DPR team to configure replication
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-     {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "max_slot_wal_keep_size"
-      value        = "5000"
-      apply_method = "immediate"
-    }
-  ]
+  # Add replication parameters
+  parameter_group_name  = aws_db_parameter_group.replication_params.name
 
   providers = {
     aws = aws.london
@@ -112,6 +122,9 @@ module "activities_rds_read_replica" {
   
   # Add security groups for DPR
   vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
+
+  # Add replication parameters
+  parameter_group_name  = aws_db_parameter_group.replication_params.name
 
   providers = {
     aws = aws.london
