d777007842c75f517508065c318c2da0cbd13dce
rickchoijd <rick.choi@digital.justice.gov.uk>
2024-12-31 14:50:18 +0000

    [ESWE-1187] remove comments
    Signed-off-by: rickchoijd <rick.choi@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-integration-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-integration-dev/resources/rds-postgresql.tf
@@ -14,7 +14,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "16" # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  db_engine_version = "16"
   rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
