284ab0f59ab40cf4b7853ccc33ded8fb93e83e8e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-12-31 16:49:07 +0000

    MIC-862 update db params

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-preprod/resources/rds-postgresql.tf
@@ -14,8 +14,8 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
-  db_max_allocated_storage     = "2000"
-  enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  db_max_allocated_storage     = "100"
+  enable_rds_auto_start_stop   = false
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
@@ -54,6 +54,11 @@ module "rds" {
     {
       name         = "wal_sender_timeout"
       value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "5000"
       apply_method = "immediate"
     }
   ]
