915e7df14423f156788e3820afc42b5f04f940e5
Marcus Aspin <maspin@unilink.com>
2025-01-02 13:09:38 +0000

    PI-2317 Update team name for hmpps-tier (prod) (#28577)
    * PI-2317 Update team name for hmpps-tier (prod)
    
    * Fix variables
    
    * Remove scheduled downtime serviceaccount

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-prod/resources/rds.tf
@@ -1,40 +1,25 @@
 module "rds" {
-  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.2.2"
-  vpc_name      = var.vpc_name
-  team_name     = var.team_name
-  business_unit = var.business_unit
-  application   = var.application
-  is_production = var.is_production
-  namespace     = var.namespace
-
-  # rotating creds
-  db_password_rotated_date = "21-02-2023"
-
-  # enable performance insights
+  source                       = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+  vpc_name                     = var.vpc_name
+  team_name                    = var.team_name
+  business_unit                = var.business_unit
+  application                  = var.application
+  is_production                = var.is_production
+  environment_name             = var.environment_name
+  infrastructure_support       = var.infrastructure_support
+  namespace                    = var.namespace
+  rds_name                     = "hmpps-tier-${var.environment_name}"
+  rds_family                   = "postgres16"
+  db_engine_version            = "16.3"
+  db_instance_class            = "db.t4g.small"
+  prepare_for_major_upgrade    = false
   performance_insights_enabled = true
-  # db instance class
-  db_instance_class = "db.t4g.small"
-
-  # change the postgres version as you see fit.
-  db_engine_version      = "16.3"
-  environment_name       = var.environment-name
-  infrastructure_support = var.infrastructure_support
-  maintenance_window = var.maintenance_window
-  backup_window = var.backup_window
-
-  prepare_for_major_upgrade = false
-
-  # rds_family should be one of: postgres9.4, postgres9.5, postgres9.6, postgres10, postgres11
-  # Pick the one that defines the postgres version the best
-  rds_family = "postgres16"
-
-  providers = {
-    # Can be either "aws.london" or "aws.ireland"
-    aws = aws.london
-  }
+  maintenance_window           = var.maintenance_window
+  backup_window                = var.backup_window
+  db_password_rotated_date     = "21-02-2023"
 }
 
-resource "kubernetes_secret" "rds" {
+resource "kubernetes_secret" "rds-secret" {
   metadata {
     name      = "rds-instance-output"
     namespace = var.namespace
@@ -47,12 +32,6 @@ resource "kubernetes_secret" "rds" {
     database_password     = module.rds.database_password
     rds_instance_address  = module.rds.rds_instance_address
   }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
 }
 
 resource "kubernetes_config_map" "rds" {
