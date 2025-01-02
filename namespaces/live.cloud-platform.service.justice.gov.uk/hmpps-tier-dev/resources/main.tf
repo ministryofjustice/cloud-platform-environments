2d12193986f0af11dd338baa9d0838422a15400c
Marcus Aspin <maspin@unilink.com>
2025-01-02 10:40:11 +0000

    PI-2317 Update team name for hmpps-tier (dev)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/main.tf
@@ -12,7 +12,6 @@ provider "aws" {
   }
 }
 
-# To be use in case the resources need to be created in London
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
@@ -23,7 +22,6 @@ provider "aws" {
   }
 }
 
-# To be use in case the resources need to be created in Ireland
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
@@ -32,9 +30,4 @@ provider "aws" {
       GithubTeam = "probation-integration"
     }
   }
-}
-
-provider "github" {
-  token = var.github_token
-  owner = var.github_owner
 }
