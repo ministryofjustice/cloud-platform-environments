936cc4c78ba75a43c3c796d838c87045ea65b935
liam.murphy <liam.murphy@digital.justice.gov.uk>
2025-01-03 15:44:42 +0000

    LASB-3701 - fix infox dev secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-development/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-development/resources/secrets.tf
@@ -23,7 +23,7 @@ module "secrets_manager" {
     "laa-infox-private-key-password" = {
       description             = "InfoX private key password",        # Required
       recovery_window_in_days = 7,                                   # Required - number of days that AWS Secrets Manager waits before it can delete the secret
-      k8s_secret_name         = "laa-infox-private-key-dev"          # The name of the secret in k8s
+      k8s_secret_name         = "laa-infox-private-key-password-dev"          # The name of the secret in k8s
     },
   }
 }
