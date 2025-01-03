994a03c4baec02b0de28c1ee22a16d5c6fea8c08
liam.murphy <liam.murphy@digital.justice.gov.uk>
2025-01-03 16:50:58 +0000

    LASB-3701 - fix infox preprod secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-preproduction/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-information-exchange-preproduction/resources/secrets.tf
@@ -23,7 +23,7 @@ module "secrets_manager" {
     "laa-infox-private-key-password" = {
       description             = "InfoX private key password preprod",        # Required
       recovery_window_in_days = 7,                                   # Required - number of days that AWS Secrets Manager waits before it can delete the secret
-      k8s_secret_name         = "laa-infox-private-key-preprod"          # The name of the secret in k8s
+      k8s_secret_name         = "laa-infox-private-key-password-preprod"          # The name of the secret in k8s
     },
   }
 }
