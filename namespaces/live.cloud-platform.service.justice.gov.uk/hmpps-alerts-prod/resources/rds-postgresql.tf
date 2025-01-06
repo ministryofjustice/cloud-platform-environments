fd4265aa813dea80fe2fa3b3678a2d78c67bee6d
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-01-06 10:21:39 +0000

    MIAD-285

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-prod/resources/rds-postgresql.tf
@@ -14,6 +14,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
+  db_allocated_storage         = "100"
   db_max_allocated_storage     = "250"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   deletion_protection          = true
@@ -57,7 +58,7 @@ module "rds" {
     },
     {
       name         = "max_slot_wal_keep_size"
-      value        = "5000"
+      value        = "40000"
       apply_method = "immediate"
     }
   ]
