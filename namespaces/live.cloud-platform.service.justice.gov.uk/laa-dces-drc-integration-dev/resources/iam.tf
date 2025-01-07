f4c020838fa1ea03572d912d4498f9e14c10d2ac
tariqhossain <tariq.hossain@digital.justice.gov.uk>
2025-01-07 15:12:54 +0000

    quick update

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-dev/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-dev/resources/iam.tf
@@ -331,5 +331,5 @@ resource "aws_iam_policy" "guard-duty_s3_custom" {
 ## Attach the IAM policy to the role
 resource "aws_iam_role_policy_attachment" "attach_guardduty_s3_custom" {
   role       = aws_iam_role.guardduty_malware_protection_role.name
-  policy_arn = aws_iam_policy.guardduty_s3_custom.arn
+  policy_arn = aws_iam_policy.guard-duty_s3_custom.arn
 }
