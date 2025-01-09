94bbb957c07a6573e5dd4950f42b696b352cbb3d
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2025-01-09 10:11:20 +0000

    reverting previous change (#28716)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/variables.tf
@@ -97,8 +97,3 @@ variable "github_actions_secret_kube_token" {
   type        = string
   sensitive   = true
 }
-
-variable "domain" {
-  default = "get-legal-aid-data.service.justice.gov.uk"
-  type    = string
-}
