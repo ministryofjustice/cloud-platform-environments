9101c8d04bd021816f3d4cac25b93ed992142814
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-10 03:20:18 -0800

    Remove standard sqs and sns resources
    Remove standard sqs and sns resources
    Remove references to standard sns and sqs resources
    
    This is because we've migrated to fifo sns/sqs resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/irsa.tf
@@ -26,7 +26,6 @@ module "court-facing-api-irsa" {
   role_policy_arns = merge(
     { s3_cpg = module.crime-portal-gateway-s3-bucket.irsa_policy_arn },
     { s3_pt = module.perf-test-data-s3-bucket.irsa_policy_arn },
-    { sns_cce = module.court-case-events.irsa_policy_arn },
     { sns_cc = module.court-cases.irsa_policy_arn },
   )
 
@@ -62,8 +61,6 @@ module "irsa" {
     { sqs_ccq = module.court-cases-queue.irsa_policy_arn },
     { sqs_ccq_dlq = module.court-cases-dlq.irsa_policy_arn },
     { sqs_cpg_dlq = module.crime-portal-gateway-dead-letter-queue.irsa_policy_arn },
-    { sqs_ccm = module.court-case-matcher-queue.irsa_policy_arn },
-    { sqs_ccm_dlq = module.court-case-matcher-dead-letter-queue.irsa_policy_arn },
     { sqs_ccs = module.pic_new_offender_events_queue.irsa_policy_arn },
     { sqs_ccs_dlq = module.pic_new_offender_events_dead_letter_queue.irsa_policy_arn },
     { elasticache = module.pac_elasticache_redis.irsa_policy_arn }
