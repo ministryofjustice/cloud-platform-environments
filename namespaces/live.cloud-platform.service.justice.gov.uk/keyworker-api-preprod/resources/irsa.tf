53db71c7bda3d7beeb7422e23bf8e43f23663353
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-01-13 14:03:27 +0000

    Fix sns access

--- namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/keyworker-api-preprod/resources/irsa.tf
@@ -6,7 +6,13 @@ locals {
     "Digital-Prison-Services-preprod-keyworker_api_queue"                       = "offender-events-preprod",
     "Digital-Prison-Services-preprod-keyworker_api_queue_dl"                    = "offender-events-preprod",
   }
+
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd" = "hmpps-domain-events-preprod"
+  }
+
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
 module "irsa" {
@@ -33,11 +39,17 @@ module "irsa" {
     {
       sqs_dlq = module.keyworker_domain_events_dlq.irsa_policy_arn
     },
-    local.sqs_policies
+    local.sqs_policies,
+    local.sns_policies
   )
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns" {
   for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
