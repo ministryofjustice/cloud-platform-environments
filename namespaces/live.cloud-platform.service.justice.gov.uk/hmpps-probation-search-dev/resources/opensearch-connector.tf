e8304ca9970d194621f724f881e8ad80e6572179
Marcus Aspin <maspin@unilink.com>
2025-01-13 18:31:05 +0000

    PI-2526 Revert connector changes + upgrade to OS 2.18

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-dev/resources/opensearch-connector.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-dev/resources/opensearch-connector.tf
@@ -14,43 +14,8 @@ resource "aws_iam_role_policy" "opensearch_connector_irsa_policy" {
       {
         Effect   = "Allow",
         Action   = "iam:PassRole",
-        Resource = aws_iam_role.cross_account_pass_role.arn
+        Resource = local.opensearch_connector_role_arn
       }
     ]
   })
-}
-
-## Role to allow passing a role from Account A (Modernisation Platform) to Account B (Cloud Platform)
-## See https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_passrole.html#:~:text=To%20pass%20a%20role%20in%20Account%20A%20to%20a%20service%20in%20Account%20B%2C%20you%20must%20first%20create%20an%20IAM%20role%20in%20Account%20B%20that%20can%20assume%20the%20role%20from%20Account%20A%2C%20and%20then%20the%20role%20in%20Account%20B%20can%20be%20passed%20to%20the%20service
-resource "aws_iam_role" "cross_account_pass_role" {
-  name = "${var.namespace}-xa-opensearch-to-sagemaker"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowOpenSearchServiceToAssume"
-        Effect = "Allow"
-        Principal = {
-          Service = "opensearchservice.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      },
-    ]
-  })
-  inline_policy {
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Sid    = "AllowToAssumeRemoteRole"
-          Effect = "Allow"
-          Action = [
-            "sts:AssumeRole",
-            "sts:TagSession",
-          ]
-          Resource = local.opensearch_connector_role_arn
-        }
-      ]
-    })
-  }
 }
