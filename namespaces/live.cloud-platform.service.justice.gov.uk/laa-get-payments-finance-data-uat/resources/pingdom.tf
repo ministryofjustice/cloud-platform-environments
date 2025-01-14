8fb295df26aac3f9c916e0bd98412c2a62384795
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2025-01-14 10:39:29 +0000

    adding pingdom (#28808)
    * adding pingdom
    
    * Empty - Commit
    
    * updating environment variable

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-uat/resources/pingdom.tf
@@ -0,0 +1,17 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "get-legal-aid-data-uat" {
+  type                     = "http"
+  name                     = "get legal aid data - uat - cloud platform"
+  host                     = "uat.get-legal-aid-data.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/actuator/health"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_glad,component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_glad"
+  probefilters             = "region:EU"
+}
