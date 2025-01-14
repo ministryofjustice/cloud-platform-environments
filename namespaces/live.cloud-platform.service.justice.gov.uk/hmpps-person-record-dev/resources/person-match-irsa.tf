f6570900aacda6ea47cef2818c1fe777b64c813d
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-01-14 11:54:50 +0000

    CPR-541-person-match-dev added  rds and irsa for person match

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/person-match-irsa.tf
@@ -0,0 +1,21 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0" # use the latest release
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "hmpps-person-match-${var.environment}"
+  role_policy_arns = {
+      rds = module.hmpps_person_match_rds.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace # this is also used to attach your service account to your namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
