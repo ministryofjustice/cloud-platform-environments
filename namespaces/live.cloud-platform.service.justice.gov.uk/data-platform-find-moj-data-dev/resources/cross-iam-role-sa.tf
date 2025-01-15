c6989af92230e3aac43a8746f8cbccd92f8f7d72
LavMatt <matthew.laverty@justice.gov.uk>
2025-01-15 09:25:05 +0000

    add missing tags to irsa module

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/cross-iam-role-sa.tf
@@ -4,6 +4,14 @@ module "quicksight_irsa" {
     namespace             = var.namespace
     service_account_name  = "find-moj-data-quicksight"
     role_policy_arns = [aws_iam_policy.find_moj_data_dev_quicksight.arn]
+
+    # Tags 
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = false
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = "findmojdata@justice.gov.uk"
 }
 data "aws_iam_policy_document" "find_moj_data_dev_quicksight" {
     # Provide list of permissions and target AWS account resources to allow access to
