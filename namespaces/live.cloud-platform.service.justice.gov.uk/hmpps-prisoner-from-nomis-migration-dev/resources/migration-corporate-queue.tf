c51e955a8d2db3375b50e63301d142b91ed5f136
andymarke <andy.marke@digital.justice.gov.uk>
2025-01-15 13:04:39 +0000

    Added migration_corporate_queue - dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/migration-corporate-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/migration-corporate-queue.tf
@@ -1,5 +1,5 @@
 module "migration_corporate_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.0"
 
   # Queue configuration
   sqs_name                   = "migration_corporate_queue"
@@ -25,7 +25,7 @@ EOF
 }
 
 module "migration_corporate_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.0"
 
   # Queue configuration
   sqs_name        = "migration_corporate_dlq"
