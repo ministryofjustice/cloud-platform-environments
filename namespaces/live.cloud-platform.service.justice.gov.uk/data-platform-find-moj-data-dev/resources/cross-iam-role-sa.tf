b7741c35249598eedbf6c306c742a5162d37cfc2
LavMatt <matthew.laverty@justice.gov.uk>
2025-01-15 10:24:20 +0000

    add a service pod

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/cross-iam-role-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-find-moj-data-dev/resources/cross-iam-role-sa.tf
@@ -60,3 +60,11 @@ resource "kubernetes_secret" "irsa" {
         serviceaccount = module.quicksight_irsa.service_account.name
     }
 }
+
+module "cross_service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.quicksight_irsa.service_account.name
+}
