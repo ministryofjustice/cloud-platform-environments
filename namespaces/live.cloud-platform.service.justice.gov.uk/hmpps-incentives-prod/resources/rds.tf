5ddd0064a823396ab8ad34f250a16dfd75633658
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-01-17 11:28:32 +0000

    INC-1385 [PROD] Upgrade postgres 15->16

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-prod/resources/rds.tf
@@ -14,10 +14,10 @@ module "dps_rds" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
   db_instance_class           = "db.t4g.large"
-  rds_family                  = "postgres15"
-  db_engine_version           = "15"
+  rds_family                  = "postgres16"
+  db_engine_version           = "16"
   allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
 
