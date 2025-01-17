d8f5084df25ab468b0c12590916072bc500ac691
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2025-01-17 14:58:39 +0000

    commenting non required tf code

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/api_gateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-dev/resources/api_gateway.tf
@@ -130,7 +130,7 @@ resource "aws_api_gateway_stage" "main" {
   deployment_id         = aws_api_gateway_deployment.main.id
   rest_api_id           = aws_api_gateway_rest_api.api_gateway.id
   stage_name            = var.namespace
-  client_certificate_id = aws_api_gateway_client_certificate.api_gateway_client_two.id
+  # client_certificate_id = aws_api_gateway_client_certificate.api_gateway_client_two.id
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway_access_logs.arn
