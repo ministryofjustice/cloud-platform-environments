2df5cf4479ad0e3530e48045485a4416317eba18
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-01-20 11:41:15 +0000

    Add rds (#28967)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cdpt-deploys-dashboard/resources/rds.tf
@@ -0,0 +1,59 @@
+module "rds" {
+  source                    = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+  vpc_name                  = var.vpc_name
+  team_name                 = var.team_name
+  business_unit             = var.business_unit
+  application               = var.application
+  is_production             = var.is_production
+  environment_name          = var.environment
+  infrastructure_support    = var.infrastructure_support
+  namespace                 = var.namespace
+  prepare_for_major_upgrade = false
+  db_engine                 = "postgres"
+
+  deletion_protection = true
+  db_engine_version = "16"
+  db_instance_class = "db.t4g.micro"
+  rds_family = "postgres16"
+
+  # use "allow_major_version_upgrade" when upgrading the major version of an engine
+  allow_major_version_upgrade = "false"
+
+  # This will rotate the db password. Update the value to the current date.
+  # db_password_rotated_date  = "dd-mm-yyyy"
+
+  providers = {
+    # Can be either "aws.london" or "aws.ireland"
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "rds" {
+  metadata {
+    name      = "rds-postgresql-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_name         = module.rds.database_name
+    database_username     = module.rds.database_username
+    database_password     = module.rds.database_password
+    rds_instance_address  = module.rds.rds_instance_address
+    url                   = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
+  }
+}
+
+# Configmap to store non-sensitive data related to the RDS instance
+
+resource "kubernetes_config_map" "rds" {
+  metadata {
+    name      = "rds-postgresql-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name = module.rds.database_name
+    db_identifier = module.rds.db_identifier
+  }
+}
