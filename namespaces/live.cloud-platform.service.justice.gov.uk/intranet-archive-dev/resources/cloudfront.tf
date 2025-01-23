edc37091104404239dcc9c1181092c94dbf28e1b
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-01-23 14:22:37 +0000

    intranet-archive-dev Custom error pages for CloudFront

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-archive-dev/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-archive-dev/resources/cloudfront.tf
@@ -1,5 +1,5 @@
 module "cloudfront" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=allow-var-for-custom-error"
 
   # Configuration
   bucket_id            = module.s3_bucket.bucket_name
@@ -13,6 +13,27 @@ module "cloudfront" {
   trusted_public_keys  = local.expiring_trusted_key.encoded_key == null ? [local.trusted_key] : [local.trusted_key, local.expiring_trusted_key]
   # Object to return when an end user requests the root URL
   default_root_object  = "index.html"
+  # Custom error pages for 403, 404, and 5xx errors.
+  custom_error_response = [
+    {
+      error_code            = 403
+      response_code         = 403
+      response_page_path    = "/error_pages/403.html"
+      error_caching_min_ttl = 0
+    },
+    {
+      error_code            = 404
+      response_code         = 404
+      response_page_path    = "/error_pages/404.html"
+      error_caching_min_ttl = 60
+    },
+    {
+      error_code            = 500
+      response_code         = 500
+      response_page_path    = "/error_pages/500.html"
+      error_caching_min_ttl = 0
+    }
+  ]
 
   # Tags
   business_unit          = var.business_unit
