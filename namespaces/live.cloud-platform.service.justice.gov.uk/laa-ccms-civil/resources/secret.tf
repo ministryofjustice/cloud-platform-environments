0b2befd50297b789d57f39cd3e176a867841bb5d
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-01-24 15:09:35 +0000

    update secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
@@ -10,45 +10,40 @@ module "secrets_manager" {
   eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
-    "caab-api-auth-secrets" = {
-      description             = "Authentication secrets for CAAB API",              # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-api-auth-secrets"                             # The name of the secret in k8s
+    "caab-api-secrets" = {
+      description             = "Secrets for CAAB API",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-api-auth-secrets"
     },
-    "caab-assessment-api-auth-secrets" = {
-      description             = "Authentication secrets for CAAB Assessment API",   # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-assessment-api-auth-secrets"                  # The name of the secret in k8s
+    "caab-assessment-api-secrets" = {
+      description             = "Secrets for CAAB Assessment API",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-assessment-api-auth-secrets"
     },
-    "caab-ebs-api-auth-secrets" = {
-      description             = "Authentication secrets for CAAB EBS API",          # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-ebs-api-auth-secrets"                         # The name of the secret in k8s
+    "caab-ebs-api-secrets" = {
+      description             = "Secrets for CAAB EBS API",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-ebs-api-auth-secrets"
     },
-    "caab-soa-api-auth-secrets" = {
-      description             = "Authentication secrets for CAAB SOA API",          # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-soa-api-auth-secrets"                         # The name of the secret in k8s
+    "caab-soa-api-secrets" = {
+      description             = "Secrets for CAAB SOA API",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-soa-api-auth-secrets"
     },
     "caab-ui-secrets" = {
-      description             = "Secrets for CAAB UI",                              # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-ui-secrets"                                   # The name of the secret in k8s
+      description             = "Secrets for CAAB UI",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-ui-secrets"
     },
     "caab-soa-gateway-secrets" = {
-      description             = "Secrets for CAAB SOA Gateway",                     # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-soa-gateway-secrets"                          # The name of the secret in k8s
-    },
-    "caab-datasource-secrets" = {
-      description             = "Secrets for CAAB Datasource",                      # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-datasource-secrets"                           # The name of the secret in k8s
-    },
+      description             = "Secrets for CAAB SOA Gateway",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-soa-gateway-secrets"
+    }
     "caab-regression-test-secrets" = {
-      description             = "Secrets for CAAB Regression Tests",                # Required
-      recovery_window_in_days = 7,                                                  # Required
-      k8s_secret_name         = "caab-regression-test-secrets"                      # The name of the secret in k8s
+      description             = "Secrets for CAAB Regression Tests",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "caab-regression-test-secrets"
     }
   }
 }
