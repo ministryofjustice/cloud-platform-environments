18d125f876d067dbdcfb0691449ba3a9c22528d0
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-01-24 15:17:25 +0000

    update names

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-civil/resources/secret.tf
@@ -13,32 +13,27 @@ module "secrets_manager" {
     "caab-api-secrets" = {
       description             = "Secrets for CAAB API",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "caab-api-auth-secrets"
+      k8s_secret_name         = "caab-api-secrets"
     },
     "caab-assessment-api-secrets" = {
       description             = "Secrets for CAAB Assessment API",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "caab-assessment-api-auth-secrets"
+      k8s_secret_name         = "caab-assessment-api-secrets"
     },
     "caab-ebs-api-secrets" = {
       description             = "Secrets for CAAB EBS API",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "caab-ebs-api-auth-secrets"
+      k8s_secret_name         = "caab-ebs-api-secrets"
     },
     "caab-soa-api-secrets" = {
       description             = "Secrets for CAAB SOA API",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "caab-soa-api-auth-secrets"
+      k8s_secret_name         = "caab-soa-api-secrets"
     },
     "caab-ui-secrets" = {
       description             = "Secrets for CAAB UI",
       recovery_window_in_days = 7,
       k8s_secret_name         = "caab-ui-secrets"
-    },
-    "caab-soa-gateway-secrets" = {
-      description             = "Secrets for CAAB SOA Gateway",
-      recovery_window_in_days = 7,
-      k8s_secret_name         = "caab-soa-gateway-secrets"
     }
     "caab-regression-test-secrets" = {
       description             = "Secrets for CAAB Regression Tests",
