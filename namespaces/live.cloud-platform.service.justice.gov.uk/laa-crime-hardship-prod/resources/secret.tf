27921617a879c7fe4eb3561930211610f6fa1801
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2025-01-27 15:24:42 +0000

    Add sentry secret to hardship prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-hardship-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-hardship-prod/resources/secret.tf
@@ -40,5 +40,10 @@ module "secrets_manager" {
       recovery_window_in_days = 7,
       k8s_secret_name         = "cma-api-oauth-client-secret"
     },
+    "sentry_dsn" = {
+      description             = "Sentry Data Source Name (DSN) for Hardship Prod",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "sentry-dsn"
+    },
   }
 }
