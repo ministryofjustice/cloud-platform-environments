20b6fd07fe0971401a3815e31a0e7e3abed7171a
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-01-27 21:35:18 +0000

    chore: 2 ns terraofrm plan

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/service-pod.tf
@@ -5,3 +5,11 @@ module "service_pod" {
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name
 }
+
+module "service_pod2" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name
+}
