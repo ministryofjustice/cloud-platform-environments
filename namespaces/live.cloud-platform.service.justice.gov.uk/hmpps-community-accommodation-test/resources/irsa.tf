699f32e37508dfc1e057ed3e2bb024972d47d775
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-01-29 15:59:20 +0000

    subscription to domain events for hmpps-community-accommodation-test

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/irsa.tf
@@ -1,29 +1,23 @@
-locals {
-  sqs_queues_ap = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
-  }
-  sqs_queues_cas2 = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
-  }
-  sqs_queues_ta = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
-  }
-  sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
-  }
 
-  sns_policies      = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
-  sqs_policies_ap   = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs_ap : item.name => item.value }
-  sqs_policies_cas2 = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs_cas2 : item.name => item.value }
-  sqs_policies_ta   = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs_ta : item.name => item.value }
+# Get the ARN of the IRSA policy for the SQS queue
+data "aws_ssm_parameter" "irsa_policy_arns_sns_domain_events" {
+  name     = "/hmpps-domain-events-dev/sns/cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573/irsa-policy-arn"
+}
+
+# Get the ARN of the IRSA policy for the SNS topic
+data "aws_ssm_parameter" "irsa_policy_arns_sqs_audit" {
+  name     = "/hmpps-audit-dev/sqs/Digital-Prison-Services-dev-hmpps_audit_queue/irsa-policy-arn"
 }
 
 module "irsa" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  eks_cluster_name       = var.eks_cluster_name
-  service_account_name   = "hmpps-community-accommodation-api-service-account"
-  namespace              = var.namespace
-  role_policy_arns       = local.sns_policies
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-community-accommodation-api-service-account"
+  namespace            = var.namespace
+  role_policy_arns = {
+    cas-2-sns           = module.cas-2-domain-events-queue.irsa_policy_arn,
+    domain_events_topic = data.aws_ssm_parameter.irsa_policy_arns_sns_domain_events.value
+  }
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -33,11 +27,13 @@ module "irsa" {
 }
 
 module "irsa_ap" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  eks_cluster_name       = var.eks_cluster_name
-  service_account_name   = "hmpps-approved-premises-service-account"
-  namespace              = var.namespace
-  role_policy_arns       = local.sqs_policies_ap
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-approved-premises-service-account"
+  namespace            = var.namespace
+  role_policy_arns = {
+    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
+  }
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -47,11 +43,13 @@ module "irsa_ap" {
 }
 
 module "irsa_cas2" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  eks_cluster_name       = var.eks_cluster_name
-  service_account_name   = "hmpps-community-accommodation-tier-2-service-account"
-  namespace              = var.namespace
-  role_policy_arns       = local.sqs_policies_cas2
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-community-accommodation-tier-2-service-account"
+  namespace            = var.namespace
+  role_policy_arns = {
+    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
+  }
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -61,11 +59,13 @@ module "irsa_cas2" {
 }
 
 module "irsa_ta" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  eks_cluster_name       = var.eks_cluster_name
-  service_account_name   = "hmpps-temporary-accommodation-service-account"
-  namespace              = var.namespace
-  role_policy_arns       = local.sqs_policies_ta
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-temporary-accommodation-service-account"
+  namespace            = var.namespace
+  role_policy_arns = {
+    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
+  }
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -74,19 +74,4 @@ module "irsa_ta" {
   infrastructure_support = var.infrastructure_support
 }
 
-data "aws_ssm_parameter" "irsa_policy_arns_sns" {
-  for_each = local.sns_topics
-  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
-}
-data "aws_ssm_parameter" "irsa_policy_arns_sqs_ap" {
-  for_each = local.sqs_queues_ap
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
-}
-data "aws_ssm_parameter" "irsa_policy_arns_sqs_cas2" {
-  for_each = local.sqs_queues_cas2
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
-}
-data "aws_ssm_parameter" "irsa_policy_arns_sqs_ta" {
-  for_each = local.sqs_queues_ta
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
-}
+
