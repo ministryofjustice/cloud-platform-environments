6a1e4f4c2608cc58019505eb6e885690c12206bb
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-01-29 15:50:19 +0000

    subscription to domain events for hmpps-community-accommodation-preprod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-preprod/resources/data.tf
@@ -0,0 +1,21 @@
+data "aws_iam_policy_document" "sns_to_sqs" {
+  statement {
+    sid     = "DomainEventsToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value]
+    }
+    resources = ["*"]
+  }
+}
+
+data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
+  name = "/hmpps-domain-events-preprod/topic-arn"
+}
