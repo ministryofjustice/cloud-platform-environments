8d41f1a9451cd423c9ed332e983a9a7696520296
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-01-29 10:30:03 +0000

    set backup window

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-a-query-staging/resources/rds.tf
@@ -20,7 +20,8 @@ module "track_a_query_rds" {
   db_name                    = "track_a_query_staging"
   environment_name           = var.environment
   infrastructure_support     = var.infrastructure_support
-  enable_rds_auto_start_stop = false
+  backup_window              = "06:00-08:00"
+  enable_rds_auto_start_stop = true
   prepare_for_major_upgrade  = false
 
   providers = {
