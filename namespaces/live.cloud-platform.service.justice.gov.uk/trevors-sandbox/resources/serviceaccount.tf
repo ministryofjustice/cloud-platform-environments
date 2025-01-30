97b971ef0c45518e4d7f1b84bba7ba50deb5179c
Trevas-MoJ <trevor.smith1@digital.justice.gov.uk>
2025-01-30 10:30:03 +0000

    IMSV2-5932: add service account resource in trevorS sandbox namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/trevors-sandbox/resources/serviceaccount.tf
@@ -0,0 +1,11 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_name = "manage-intelligence-plq"
+
+  github_repositories = [var.namespace]
+
+}
