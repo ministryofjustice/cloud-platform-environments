febe42e81e53dd231a15050ed3b1fb66578acbc2
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-30 15:36:20 +0000

    PIC-4500 Add new s3 bucket for dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/large_court_cases_s3.tf
@@ -0,0 +1,45 @@
+module "large-court-cases-s3-bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0"
+
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = "large-court-cases"
+  is_production          = var.is_production
+  environment_name       = var.environment-name
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+
+  lifecycle_rule = [
+    {
+      enabled                                = true
+      id                                     = "expire-large-court-cases"
+      abort_incomplete_multipart_upload_days = 90
+      expiration = [
+        {
+          days = 30
+        },
+      ]
+      noncurrent_version_expiration = [
+        {
+          days = 30
+        },
+      ]
+    },
+  ]
+}
+
+resource "kubernetes_secret" "large-court-cases-s3-secret" {
+  metadata {
+    name      = "large-court-cases-s3-credentials"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn  = module.large-court-cases-s3-bucket.bucket_arn
+    bucket_name = module.large-court-cases-s3-bucket.bucket_name
+  }
+}
