63b4b210216dc025b76a3b9362fe8779b0ad9e4a
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2025-01-31 15:11:01 +0000

    SAA-2302: remove read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/rds.tf
@@ -28,12 +28,12 @@ module "activities_rds" {
   namespace                   = var.namespace
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
-  prepare_for_major_upgrade   = true
-  rds_family                  = "postgres16"
+  prepare_for_major_upgrade   = false
+  rds_family                  = "postgres14"
   allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
   db_instance_class           = "db.t4g.small"
-  db_engine_version           = "16.3"
+  db_engine_version           = "14.12"
   storage_type                = "gp3"
   db_max_allocated_storage    = "50"
 
@@ -89,72 +89,72 @@ resource "kubernetes_secret" "activities_rds" {
   }
 }
 
-module "activities_rds_read_replica" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
-  vpc_name                    = var.vpc_name
-  team_name                   = var.team_name
-  business_unit               = var.business_unit
-  application                 = var.application
-  is_production               = var.is_production
-  namespace                   = var.namespace
-  environment_name            = var.environment
-  infrastructure_support      = var.infrastructure_support
-  rds_family                  = var.rds_family
-  allow_major_version_upgrade = "false"
-  allow_minor_version_upgrade = "true"
-  db_instance_class           = "db.t4g.small"
-  db_engine_version           = "14"
-  storage_type                = "gp3"
-  db_max_allocated_storage    = "50"
+#module "activities_rds_read_replica" {
+#  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+#  vpc_name                    = var.vpc_name
+#  team_name                   = var.team_name
+#  business_unit               = var.business_unit
+#  application                 = var.application
+#  is_production               = var.is_production
+#  namespace                   = var.namespace
+#  environment_name            = var.environment
+#  infrastructure_support      = var.infrastructure_support
+#  rds_family                  = var.rds_family
+#  allow_major_version_upgrade = "false"
+#  allow_minor_version_upgrade = "true"
+#  db_instance_class           = "db.t4g.small"
+#  db_engine_version           = "14"
+#  storage_type                = "gp3"
+#  db_max_allocated_storage    = "50"
+#
+#  replicate_source_db         = module.activities_rds.db_identifier
+#  skip_final_snapshot         = "true"
+#  db_backup_retention_period  = 0
+#
+#  # Add security groups for DPR
+#  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
+#
+#  db_parameter = [
+#    {
+#      name         = "rds.logical_replication"
+#      value        = "0"
+#      apply_method = "pending-reboot"
+#    },
+#     {
+#      name         = "shared_preload_libraries"
+#      value        = "pglogical"
+#      apply_method = "pending-reboot"
+#    },
+#    {
+#      name         = "max_wal_size"
+#      value        = "1024"
+#      apply_method = "immediate"
+#    },
+#    {
+#      name         = "wal_sender_timeout"
+#      value        = "0"
+#      apply_method = "immediate"
+#    },
+#    {
+#      name         = "max_slot_wal_keep_size"
+#      value        = "5000"
+#      apply_method = "immediate"
+#    }
+#  ]
+#
+#  providers = {
+#    aws = aws.london
+#  }
+#}
 
-  replicate_source_db         = module.activities_rds.db_identifier
-  skip_final_snapshot         = "true"
-  db_backup_retention_period  = 0
-  
-  # Add security groups for DPR
-  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
-
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "0"
-      apply_method = "pending-reboot"
-    },
-     {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "max_slot_wal_keep_size"
-      value        = "5000"
-      apply_method = "immediate"
-    }
-  ]
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "activities_rds_read_replica" {
-  metadata {
-    name      = "activities-rds-read-replica"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.activities_rds_read_replica.rds_instance_endpoint
-    rds_instance_address  = module.activities_rds_read_replica.rds_instance_address
-  }
-}
+#resource "kubernetes_secret" "activities_rds_read_replica" {
+#  metadata {
+#    name      = "activities-rds-read-replica"
+#    namespace = var.namespace
+#  }
+#
+#  data = {
+#    rds_instance_endpoint = module.activities_rds_read_replica.rds_instance_endpoint
+#    rds_instance_address  = module.activities_rds_read_replica.rds_instance_address
+#  }
+#}
