b5bf590d62bf84b1269e946231e646a9af9e572c
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-02-03 14:48:37 +0000

    Add permission to debug problem

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
@@ -58,6 +58,7 @@ data "aws_iam_policy_document" "service_pod_policy_document" {
       "athena:GetWorkGroup",
       "athena:StartQueryExecution",
       "athena:StopQueryExecution",
+      "athena:ListDataCatalogs",
       "s3:GetObject",
       "s3:PutObject",
       "s3:ListBucket",
