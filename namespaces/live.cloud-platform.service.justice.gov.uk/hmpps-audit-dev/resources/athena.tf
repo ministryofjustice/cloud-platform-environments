d02c1bdec595b86288de0752ad5dde054e1db413
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-02-03 10:07:54 +0000

    Revert Athena database to glue catalog database

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/athena.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/athena.tf
@@ -1,6 +1,6 @@
-resource "aws_athena_database" "audit_database" {
-  name   = "audit_${var.environment-name}"
-  bucket = module.s3.bucket_name
+resource "aws_glue_catalog_database" "audit_database" {
+  name = "audit_${var.environment-name}"
+  location_uri = "s3://${module.s3.bucket_name}/"
 }
 
 resource "aws_athena_workgroup" "queries" {
@@ -16,31 +16,87 @@ resource "aws_athena_workgroup" "queries" {
   }
 }
 
-resource "aws_athena_named_query" "audit_event_table" {
-  name        = "audit_event_table"
-  database    = aws_athena_database.audit_database.name
+resource "aws_glue_catalog_table" "audit_event_table" {
+  database_name = aws_glue_catalog_database.audit_database.name
+  name          = "audit_event"
 
-  query = <<EOT
-    CREATE EXTERNAL TABLE IF NOT EXISTS audit_event (
-      id STRING,
-      what STRING,
-      `when` STRING,
-      operationId STRING,
-      subjectId STRING,
-      subjectType STRING,
-      correlationId STRING,
-      who STRING,
-      service STRING,
-      details STRING
-    )
-    PARTITIONED BY (
-      year STRING,
-      month STRING,
-      day STRING,
-      user STRING
-    )
-    STORED AS PARQUET
-    LOCATION 's3://${module.s3.bucket_name}/'
-    TBLPROPERTIES ("parquet.compression"="SNAPPY");
-  EOT
+  table_type = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    columns {
+      name = "id"
+      type = "string"
+    }
+    columns {
+      name = "what"
+      type = "string"
+    }
+    columns {
+      name = "when"
+      type = "string"
+    }
+    columns {
+      name = "operationId"
+      type = "string"
+    }
+    columns {
+      name = "subjectId"
+      type = "string"
+    }
+    columns {
+      name = "subjectType"
+      type = "string"
+    }
+    columns {
+      name = "correlationId"
+      type = "string"
+    }
+    columns {
+      name = "who"
+      type = "string"
+    }
+    columns {
+      name = "service"
+      type = "string"
+    }
+    columns {
+      name = "details"
+      type = "string"
+    }
+
+    location      = "s3://${module.s3.bucket_name}/"
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    compressed    = true
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    }
+
+    stored_as_sub_directories = false
+  }
+
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+
+  partition_keys {
+    name = "day"
+    type = "string"
+  }
+
+  partition_keys {
+    name = "user"
+    type = "string"
+  }
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+    "parquet.compression" = "SNAPPY"
+  }
 }
