d6a1d1509be399e246ada41a22f88f85751ffd67
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-02-03 21:30:58 +0000

    update

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-templates-dev/resources/template-kotlin.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-templates-dev/resources/template-kotlin.tf
@@ -5,7 +5,8 @@ module "hmpps_template_kotlin" {
   github_team = "hmpps-sre"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Defaults to ["main"]
+  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
+  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-kotlin"
