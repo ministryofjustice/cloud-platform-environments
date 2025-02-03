7d06a49cce91e3b3a7d86226ada7b3ebc5fac9a4
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-02-03 12:05:33 +0000

    PIC-4500 refer to data using correct notation

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/large-court-cases-s3-bucket-secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/large-court-cases-s3-bucket-secret.tf
@@ -6,7 +6,7 @@ resource "kubernetes_secret" "large-court-cases-s3-bucket-secret" {
   }
 
   data = {
-    bucket_name = data.large-court-cases-s3-bucket-name
+    bucket_name = data.large-court-cases-s3-bucket-name.value
   }
 
 }
