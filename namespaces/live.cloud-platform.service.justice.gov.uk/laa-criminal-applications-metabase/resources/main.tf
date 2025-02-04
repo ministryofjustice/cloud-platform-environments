7a2583779a92b32e2d6e5e21b64af9f72883e56c
EdwinKruglov <edwin.kruglov@digital.justice.gov.uk>
2025-02-04 15:57:33 +0000

    CRIMAPP-1523 Config AWS Secrets Manager on crime metabase

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-metabase/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-metabase/resources/main.tf
@@ -5,19 +5,37 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = var.team_name
+    }
+  }
 }
 
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = var.team_name
+    }
+  }
 }
 
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      GithubTeam = var.team_name
+    }
+  }
 }
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
 }
-
