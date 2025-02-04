c9cddb0ce1a1eec385a7a5b16e6c78d95ffaa181
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-02-04 12:08:43 +0000

    CMG-195 fixing service account name.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-cases-release-dates-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-cases-release-dates-dev/resources/irsa.tf
@@ -20,7 +20,7 @@ module "irsa" {
 
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
-  service_account_name = var.application
+  service_account_name = "hmpps-court-cases-release-dates-service-account"
   role_policy_arns     = merge(local.sqs_policies, local.sns_policies)
   # Tags
   business_unit          = var.business_unit
