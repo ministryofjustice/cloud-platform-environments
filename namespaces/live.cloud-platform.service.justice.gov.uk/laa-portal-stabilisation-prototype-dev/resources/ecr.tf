fe8b8ada16b72689bae675bec8df0e7e7ab22f40
emorgan-moj1 <ethan.morgan1@justice.gov.uk>
2025-02-04 15:35:26 +0000

    laa-portal-stabilisation-prototype naming

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-portal-stabilisation-prototype-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-portal-stabilisation-prototype-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["example-repository"]
+  github_repositories = ["laa-portal-stabilisation-prototype"]
 
   # Tags
   business_unit          = var.business_unit
