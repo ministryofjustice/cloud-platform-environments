b5f730b82370c39f0aea3466c8464ad2854ad306
Prabash Balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-02-06 11:19:13 +0000

    PRC-446 : update preprod environment for hmpps-personal-relationships-api (#29379)
    PRC-446 : update preprod environment for hmpps-personal-relationships-api (#29379)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-personal-relationships-preprod/resources/domain-events-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-personal-relationships-preprod/resources/domain-events-topic.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_secret" "hmpps_personal_relationships_domain_events_topic" {
+resource "kubernetes_secret" "hmpps_domain_events_topic" {
   metadata {
     name      = "hmpps-domain-events-topic"
     namespace = var.namespace
