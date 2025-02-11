3f7bd03e5ba6b761adb7ab5387421b46cf9c7bf0
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2025-02-11 11:38:48 +0000

    Removing sa rules so it behaves like UAT

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
@@ -10,74 +10,12 @@ module "serviceaccount" {
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["payforlegalaid"]
   github_environments = ["development"]
-  serviceaccount_rules = [
-    {
-      api_groups = [""]
-      resources  = ["pods/exec"]
-      verbs      = ["create"]
-    },
-    {
-      api_groups = [""]
-      resources = [
-        "pods/portforward",
-        "deployment",
-        "secrets",
-        "services",
-        "pods",
-        "configmaps"
-      ]
-      verbs = [
-        "patch",
-        "get",
-        "create",
-        "update",
-        "delete",
-        "list",
-        "watch"
-      ]
-    },
-    {
-      api_groups = [
-        "extensions",
-        "apps",
-        "batch",
-        "networking.k8s.io"
-      ]
-      resources = [
-        "deployments",
-        "ingresses",
-        "cronjobs",
-        "jobs",
-        "statefulsets",
-        "replicasets",
-        "networkpolicies"
-      ]
-      verbs = [
-        "get",
-        "update",
-        "delete",
-        "create",
-        "patch",
-        "list",
-        "watch"
-      ]
-    },
-    {
-      "api_groups": [
-        "monitoring.coreos.com"
-      ],
-      "resources": [
-        "prometheusrules",
-        "servicemonitors"
-      ],
-      "verbs": [
-        "*"
-      ]
-    }
-  ]
+
 
   github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
   github_actions_secret_kube_token     = var.github_actions_secret_kube_token
   github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
   github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
+
+
 }
