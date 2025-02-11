a3016754cd92db835b8902e5274751a7802f5e69
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-02-11 15:59:46 +0000

    SDIT-2565: ✨ Add in new production environments (#29503)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-prod/resources/pingdom.tf
@@ -6,13 +6,30 @@ provider "pingdom" {
 
 resource "pingdom_check" "dps-production-check" {
   type                     = "http"
-  name                     = "DPS - hmpps-hpa"
+  name                     = "DPS - hmpps-historical-prisoner-api"
   host                     = "health-kick.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
   notifyagainevery         = 0
-  url                      = "/https/hpa.prison.service.justice.gov.uk"
+  url                      = "/https/historical-prisoner-api.prison.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [126478]
+}
+
+
+resource "pingdom_check" "dps-production-ui-check" {
+  type                     = "http"
+  name                     = "DPS - hmpps-historical-prisoner"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/historical-prisoner.prison.service.justice.gov.uk"
   encryption               = true
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
