fcd76cbc0bd71be6b07aa23407022e4f66a8fad3
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-02-12 16:02:18 +0000

    Update ecr.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-billing-controlled-work-proto/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-billing-controlled-work-proto/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["example-repository"]
+  github_repositories = ["laa-billing-controlled-work-proto"]
 
   # Tags
   business_unit          = var.business_unit
