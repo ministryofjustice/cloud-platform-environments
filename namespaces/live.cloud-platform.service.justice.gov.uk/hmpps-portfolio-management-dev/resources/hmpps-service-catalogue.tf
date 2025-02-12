df7d6d77235cfe95e0372a70c4a54cc049a97616
james Hart <james.hart@digital.justice.gov.uk>
2025-02-12 14:19:54 +0000

    rename the module to hmpps_service_catalogue_template

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-service-catalogue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-service-catalogue.tf
@@ -1,4 +1,4 @@
-module "hmpps_service_catalogue" {
+module "hmpps_service_catalogue_template" {
   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=0.0.6"
   github_repo = "hmpps-service-catalogue"
   application = "hmpps-service-catalogue"
