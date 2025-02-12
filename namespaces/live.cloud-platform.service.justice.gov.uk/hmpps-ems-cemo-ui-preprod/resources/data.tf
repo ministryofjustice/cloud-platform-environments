562aae393e99d2b5ff65dba93c12429e75e5b69e
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-02-12 14:39:01 +0000

    Add large court cases s3 bucket access in preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-preprod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-preprod/resources/data.tf
@@ -1,5 +1,9 @@
-data "aws_ssm_parameter" "court-case-events-topic-arn" {
-  name = "/court-probation-preprod/topic-arn"
+data "aws_ssm_parameter" "large-court-cases-s3-bucket-name" {
+  name = "/court-probation-preprod/large-court-cases-s3-bucket-name"
+}
+
+data "aws_ssm_parameter" "large-court-cases-s3-bucket-arn" {
+  name = "/court-probation-preprod/large-court-cases-s3-bucket-arn"
 }
 data "aws_ssm_parameter" "court-cases-topic-arn" {
   name = "/court-probation-preprod/court-cases-topic-arn"
