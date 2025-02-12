8bad8ce04e3e9a8b42090b1c343f3c205aea219e
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-02-12 14:41:05 +0000

    large court case s3 bucket access

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-prod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-prod/resources/data.tf
@@ -1,5 +1,11 @@
-data "aws_ssm_parameter" "court-case-events-topic-arn" {
-  name = "/court-probation-prod/topic-arn"
+
+
+data "aws_ssm_parameter" "large-court-cases-s3-bucket-name" {
+  name = "/court-probation-prod/large-court-cases-s3-bucket-name"
+}
+
+data "aws_ssm_parameter" "large-court-cases-s3-bucket-arn" {
+  name = "/court-probation-prod/large-court-cases-s3-bucket-arn"
 }
 
 data "aws_ssm_parameter" "court-cases-topic-arn" {
