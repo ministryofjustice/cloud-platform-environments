3b4e4a2f1c2308e7cea425edc452f4bda9649b78
devkokov <dm.kokov@gmail.com>
2025-02-13 18:58:15 +0000

    ARN-2494 Set up Github environments for Sentence Plan [PREPROD]

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-preprod/resources/sp-api.tf
@@ -0,0 +1,14 @@
+module "hmpps_sentence_plan_api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=0.0.7"
+  github_repo                   = "hmpps-sentence-plan"
+  application                   = "hmpps-sentence-plan"
+  github_team                   = "hmpps-sentence-planning"
+  reviewer_teams                = ["hmpps-sentence-planning"]
+  environment                   = var.environment
+  is_production                 = var.is_production
+  application_insights_instance = "preprod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
