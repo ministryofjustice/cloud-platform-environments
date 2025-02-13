3015dcefd3fb453a14c71c655a06ef806ca40b76
harryryan1 <harry.ryan@informed.com>
2025-02-13 14:01:50 +0000

    Add Role for Circle CI pipeline to access S3 bucket

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
@@ -0,0 +1,36 @@
+data "aws_eks_cluster" "eks_cluster" {
+  name = var.eks_cluster_name
+}
+
+data "aws_secretsmanager_secret" "circleci" {
+  name = "cloud-platform-circleci"
+}
+
+data "aws_secretsmanager_secret_version" "circleci" {
+  secret_id = data.aws_secretsmanager_secret.circleci.id
+}
+
+locals {
+  circleci_organisation_id = jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]
+}
+
+module "bankwizard_bucket_assumable_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.13.0"
+  create_role = true
+  role_name = "bankwizard-bucket-assumable-role"
+  provider_url = "https://oidc.circleci.com/org/${local.circleci_organisation_id}"
+  role_policy_arns = module.bankwizard_artifact_bucket.irsa_policy_arn
+  oidc_subjects_with_wildcards = ["org/${local.circleci_organisation_id}/*"]
+}
+
+resource "kubernetes_secret" "bankwizard_artifact_bucket_role" {
+  metadata {
+    name      = "bankwizard-artifact-bucket-role-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_role_arn = module.bankwizard_bucket_assumable_role.iam_role_arn
+  }
+}
