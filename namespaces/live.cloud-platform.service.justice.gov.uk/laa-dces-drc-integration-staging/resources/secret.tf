5ccb05a2a53cb14cbfb00945e3e6fee7d3f1997d
David Conneely <899881+dconneely@users.noreply.github.com>
2025-02-13 13:46:27 +0000

    Clean up unused secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-staging/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-staging/resources/secret.tf
@@ -10,41 +10,6 @@ module "secrets_manager" {
   eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
-    "ca-crt" = {
-      description             = "[laa-dces-drc-integration-staging/ca-crt] ca.crt for mTLS client certificate validation"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "ca-crt"
-    }
-    "dces-datasource" = {
-      description             = "[laa-dces-drc-integration-staging/dces-datasource] Database connection"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "dces-datasource"
-    }
-    "dces-drc-integration-service-alert-webhook-staging" = {
-      description             = "[dces-drc-integration-service-alert-webhook-staging] Slack webhook"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "dces-drc-integration-service-alert-webhook-staging"
-    }
-    "drc-client-auth-tls" = {
-      description             = "[laa-dces-drc-integration-staging/drc-client-auth-tls] DRC mTLS client credentials"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "drc-client-auth-tls"
-    }
-    "feature" = {
-      description             = "[laa-dces-drc-integration-staging/feature] Feature flag variables"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "feature"
-    }
-    "maat_api_oauth_client_id" = {
-      description             = "[laa-dces-drc-integration-staging/maat-api-oauth-client-id] MAAT API OAuth2 client ID"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "maat-api-oauth-client-id"
-    }
-    "maat_api_oauth_client_secret" = {
-      description             = "[laa-dces-drc-integration-staging/maat-api-oauth-client-secret] MAAT API OAuth2 client secret"
-      recovery_window_in_days = 7
-      k8s_secret_name         = "maat-api-oauth-client-secret"
-    }
     "client-ca" = {
       description             = "[client-ca] Client CA certificate file for mTLS client certificate validation"
       recovery_window_in_days = 7
@@ -59,6 +24,11 @@ module "secrets_manager" {
       description             = "[datasource] Database connection env-vars"
       recovery_window_in_days = 7
       k8s_secret_name         = "datasource"
+    }
+    "dces-drc-integration-service-alert-webhook-staging" = {
+      description             = "[dces-drc-integration-service-alert-webhook-staging] Slack webhook"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "dces-drc-integration-service-alert-webhook-staging"
     }
     "drc-client" = {
       description             = "[drc-client] Debt recovery company client credential files and env-vars"
