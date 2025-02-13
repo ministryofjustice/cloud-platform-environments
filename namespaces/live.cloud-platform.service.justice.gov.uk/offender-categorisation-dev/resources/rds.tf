41f1a76ddc474de768296d420015e741ccef88c8
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-02-13 13:51:55 +0000

    SI-395 - rds dev config changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
@@ -9,10 +9,13 @@ module "dps_rds" {
   environment_name            = var.environment_name
   infrastructure_support      = var.infrastructure_support
   rds_family                  = var.rds-family
-  db_instance_class           = "db.t4g.small"
+  db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500"
-  db_engine_version           = "14"
-  allow_major_version_upgrade = "false"
+  db_engine                   = "postgres"
+  db_engine_version           = "16.3"
+  allow_major_version_upgrade = "true"
+  prepare_for_major_upgrade   = true
+  enable_rds_auto_start_stop  = true
 }
 
 resource "random_id" "risk_profiler_role_password" {
