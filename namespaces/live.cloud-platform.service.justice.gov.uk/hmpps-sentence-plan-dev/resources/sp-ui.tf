a482e88cd0db95608b5245728e4a9df434856978
devkokov <dm.kokov@gmail.com>
2025-02-14 13:45:42 +0000

    ARN-2494 Allow any branch to be deployed to Sentence Plan [DEV]

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/sp-ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sentence-plan-dev/resources/sp-ui.tf
@@ -9,6 +9,6 @@ module "hmpps_sentence_plan_ui" {
   source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
-  protected_branches_only       = false
+  selected_branch_patterns      = ["*"]
   kubernetes_cluster            = var.kubernetes_cluster
 }
