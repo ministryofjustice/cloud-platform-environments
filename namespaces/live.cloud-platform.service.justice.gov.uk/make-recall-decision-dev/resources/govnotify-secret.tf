60f9856e30c9dec7278763a6a3bb1f2f92b2f5c8
Rick Livesey <rick.livesey@digital.justice.gov.uk>
2025-02-14 11:32:30 +0000

    additional secrets for govnotify

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/make-recall-decision-dev/resources/govnotify-secret.tf
@@ -0,0 +1,31 @@
+module "secrets_manager" {
+  source = "../" # use the latest release
+  // source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=version" # use the latest release
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    // the key "test-secret-01" is used to create kubernetes resource and must only contain lowercase alphanumeric characters, dots and dashes
+    "govnotify-webapp-pwd" = {
+      description             = "Password for logging in to the GovNotify management web app",   # Required
+      recovery_window_in_days = 7,               # Required
+      k8s_secret_name         = "govnotify-webapp-pwd" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "govnotify-teamandguest-api-id" = {
+      description             = "GovNotify API Key",   # Required
+      recovery_window_in_days = 7,               # Required
+      k8s_secret_name         = "govnotify-teamandguest-api-id" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "govnotify-teamandguest-api-secret" = {
+      description             = "GovNotify API secret",   # Required
+      recovery_window_in_days = 7,               # Required
+      k8s_secret_name         = "govnotify-teamandguest-api-secret" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+  }
+}
