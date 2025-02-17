66c83d550db2bdcdf7eca9e091653f8804654f26
RAGQYFTJ <raghavender.tangallapally@justice.gov.uk>
2025-02-17 10:44:22 +0000

    Revert "SI-395 downgrade to 16.3 compatible version" (#29692)
    This reverts commit 4b9d4e8e0d6f108a902a3495ae3b497d4bb800c0.

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
@@ -12,9 +12,9 @@ module "dps_rds" {
   db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500"
   db_engine                   = "postgres"
-  db_engine_version           = "16.3"
+  db_engine_version           = "17"
   allow_major_version_upgrade = "true"
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   enable_rds_auto_start_stop  = true
 }
 
