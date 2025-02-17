e4c0ef06f94441beab935eda1bbe2d3f78d13281
harryryan1 <harry.ryan@informed.com>
2025-02-17 11:50:13 +0000

    Swap OIDC issuer

--- namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
@@ -10,8 +10,8 @@ data "aws_secretsmanager_secret_version" "circleci" {
   secret_id = data.aws_secretsmanager_secret.circleci.id
 }
 
-locals {
-  circleci_organisation_id = jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]
+data "aws_eks_cluster" "eks_cluster" {
+  name = var.eks_cluster_name
 }
 
 module "bankwizard_bucket_assumable_role" {
@@ -19,7 +19,7 @@ module "bankwizard_bucket_assumable_role" {
   version = "5.13.0"
   create_role = true
   role_name = "bankwizard-bucket-assumable-role"
-  provider_url = "https://oidc.circleci.com/org/${local.circleci_organisation_id}"
+  provider_url = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
   role_policy_arns = [module.bankwizard_artifact_bucket.irsa_policy_arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.service_account_name}"]
 }
