9648391950513bf5c8ca3daa4bdb44a7e5d71ab8
jack-burt-is <jack.burt@informed.com>
2025-02-17 15:24:57 +0000

    Update provider to CircleCI

--- namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
@@ -2,12 +2,16 @@ data "aws_eks_cluster" "eks_cluster" {
   name = var.eks_cluster_name
 }
 
+locals {
+  circleci_organisation_id = jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]
+}
+
 module "bankwizard_bucket_assumable_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.13.0"
   create_role = true
   role_name = "bankwizard-bucket-assumable-role"
-  provider_url = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  provider_url = "https://oidc.circleci.com/org/${local.circleci_organisation_id}"
   role_policy_arns = [module.bankwizard_artifact_bucket.irsa_policy_arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.service_account_name}"]
 }
