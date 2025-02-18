7d4de604a99a2eb35f1ba8f8a1779d3710f55efe
ross75 <ross.callaghan@cica.gov.uk>
2025-02-18 12:45:02 +0000

    Update module

--- namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-dev/resources/s3-oidc-role.tf
@@ -26,12 +26,19 @@ data "aws_secretsmanager_secret_version" "circleci" {
 
 module "bankwizard_bucket_assumable_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.13.0"
+  version = "5.52.2
   create_role = true
   role_name = "bankwizard-bucket-assumable-role"
   provider_url = "https://oidc.circleci.com/org/${jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]}"
   role_policy_arns = [module.bankwizard_artifact_bucket.irsa_policy_arn]
   oidc_fully_qualified_subjects = ["${jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]}"]
+  provider_trust_policy_conditions = [
+    {
+      test     = "StringLike"
+      variable = "oidc.circleci.com/org/ORGANIZATION_ID:sub"
+      values   = ["org/${jsondecode(data.aws_secretsmanager_secret_version.circleci.secret_string)["organisation_id"]}/project/07922d32-a0bc-4e11-9e28-9d574f9e7a0e/user/*"]
+    }
+  ]
 
 }
 
