200b8eadd8b34a7927bfcbe3ee7d4c9049fe723e
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-02-19 12:18:46 +0000

    Temporarily add permission to delete object to remove corrupt files from audit dev bucket (will be reverted)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/irsa.tf
@@ -60,6 +60,7 @@ data "aws_iam_policy_document" "service_pod_policy_document" {
       "athena:StopQueryExecution",
       "athena:ListDataCatalogs",
       "s3:GetObject",
+      "s3:DeleteObject",
       "s3:PutObject",
       "s3:ListBucket",
       "s3:ListAllMyBuckets",
