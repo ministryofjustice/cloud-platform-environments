a1ed993e9b1030129f7a727a599e0bd76667af4b
Dave A <168838504+daveawc@users.noreply.github.com>
2025-02-19 17:07:38 +0000

    updated policies

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-test/resources/irsa.tf
@@ -1,12 +1,22 @@
-
-# Get the ARN of the IRSA policy for the SQS queue
-data "aws_ssm_parameter" "irsa_policy_arns_sns_domain_events" {
-  name     = "/hmpps-domain-events-dev/sns/cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573/irsa-policy-arn"
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
+  }
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
-# Get the ARN of the IRSA policy for the SNS topic
-data "aws_ssm_parameter" "irsa_policy_arns_sqs_audit" {
-  name     = "/hmpps-audit-dev/sqs/Digital-Prison-Services-dev-hmpps_audit_queue/irsa-policy-arn"
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
 
 module "irsa" {
@@ -14,10 +24,9 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   service_account_name = "hmpps-community-accommodation-api-service-account"
   namespace            = var.namespace
-  role_policy_arns = {
-    cas-2-sns           = module.cas-2-domain-events-listener-queue.irsa_policy_arn,
-    domain_events_topic = data.aws_ssm_parameter.irsa_policy_arns_sns_domain_events.value
-  }
+  role_policy_arns = merge(local.sqs_policies,
+    local.sns_policies,
+    { cas-2-sns = module.cas-2-domain-events-listener-queue.irsa_policy_arn })
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -31,9 +40,7 @@ module "irsa_ap" {
   eks_cluster_name     = var.eks_cluster_name
   service_account_name = "hmpps-approved-premises-service-account"
   namespace            = var.namespace
-  role_policy_arns = {
-    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
-  }
+  role_policy_arns = local.sqs_policies
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -47,9 +54,7 @@ module "irsa_cas2" {
   eks_cluster_name     = var.eks_cluster_name
   service_account_name = "hmpps-community-accommodation-tier-2-service-account"
   namespace            = var.namespace
-  role_policy_arns = {
-    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
-  }
+  role_policy_arns = local.sqs_policies
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -63,9 +68,7 @@ module "irsa_ta" {
   eks_cluster_name     = var.eks_cluster_name
   service_account_name = "hmpps-temporary-accommodation-service-account"
   namespace            = var.namespace
-  role_policy_arns = {
-    audit_sqs = data.aws_ssm_parameter.irsa_policy_arns_sqs_audit.value
-  }
+  role_policy_arns = local.sqs_policies
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -73,5 +76,4 @@ module "irsa_ta" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
-
 
