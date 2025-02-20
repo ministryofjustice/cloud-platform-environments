9cda2234c03c178d9b97537f5b92423ac63afc87
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-02-20 11:27:48 +0000

    replace the initial snapshot identifier
    relates to https://github.com/ministryofjustice/cloud-platform/issues/5401

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-mysql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-mysql.tf
@@ -25,7 +25,7 @@ module "rds_mysql" {
   db_instance_class = "db.t4g.micro"
   db_parameter      = []
 
-  snapshot_identifier = "cloud-platform-b3b919c90c5897c4-finalsnapshot"
+  snapshot_identifier = "rds:cloud-platform-b3b919c90c5897c4-2025-02-19-14-58"
 
   # Tags
   application            = var.application
