f617d8afe20a308037c7c19e9bd8442a2efd7663
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-02-21 16:22:06 +0000

    HAAR-3650: correct template-kotlin.tf module for SAR namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/template-kotlin.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/template-kotlin.tf
@@ -8,7 +8,7 @@ module "hmpps_template_kotlin" {
   protected_branches_only       = true
   is_production                 = var.is_production
   application_insights_instance = var.environment
-  source_template_repo          = "hmpps-subject-access-request-html-renderer"
+  source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
