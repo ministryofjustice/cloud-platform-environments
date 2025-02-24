f135f02ceb36e7fde4d41752007ada06774562a2
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-02-24 10:00:04 +0000

    CRS-2276 Preprod CRDS read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-preprod/resources/rds.tf
@@ -13,14 +13,13 @@ module "calculate_release_dates_api_rds" {
   db_engine_version      = "16.3"
   rds_family             = "postgres16"
   prepare_for_major_upgrade = false
+  allow_minor_version_upgrade = true
 
   db_password_rotated_date = "14-02-2023"
 
   providers = {
     aws = aws.london
   }
-
-  vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
 
   db_parameter = [
     {
@@ -58,6 +57,54 @@ resource "kubernetes_secret" "calculate_release_dates_api_rds" {
     database_username     = module.calculate_release_dates_api_rds.database_username
     database_password     = module.calculate_release_dates_api_rds.database_password
     rds_instance_address  = module.calculate_release_dates_api_rds.rds_instance_address
+  }
+}
+
+
+module "read_replica" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+
+  vpc_name               = var.vpc_name
+  allow_minor_version_upgrade  = true
+
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
+  db_instance_class = "db.t3.small"
+
+  # It is mandatory to set the below values to create read replica instance
+  # Set the db_identifier of the source db
+  replicate_source_db = module.calculate_release_dates_api_rds.db_identifier
+
+  # No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
+
+  vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
+}
+
+resource "kubernetes_secret" "read_replica" {
+  metadata {
+    name      = "rds-read-replica-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
+    database_name         = module.read_replica.database_name
+    database_username     = module.read_replica.database_username
+    database_password     = module.read_replica.database_password
+    rds_instance_address  = module.read_replica.rds_instance_address
   }
 }
 
