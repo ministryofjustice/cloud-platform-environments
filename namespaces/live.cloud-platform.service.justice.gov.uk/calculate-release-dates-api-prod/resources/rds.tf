614696db4423b0bd080a49a47f0eb050a9cb58a1
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-02-24 10:03:01 +0000

    CRS-2276 CRDS production readonly replica.

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-release-dates-api-prod/resources/rds.tf
@@ -18,12 +18,36 @@ module "calculate_release_dates_api_rds" {
   rds_family             = "postgres16"
   
   prepare_for_major_upgrade = false
+  allow_minor_version_upgrade  = true
 
   db_password_rotated_date = "17-02-2023"
 
   providers = {
     aws = aws.london
   }
+
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    }
+  ]
 }
 
 resource "kubernetes_secret" "calculate_release_dates_api_rds" {
@@ -39,4 +63,55 @@ resource "kubernetes_secret" "calculate_release_dates_api_rds" {
     database_password     = module.calculate_release_dates_api_rds.database_password
     rds_instance_address  = module.calculate_release_dates_api_rds.rds_instance_address
   }
+}
+
+module "read_replica" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+
+  vpc_name               = var.vpc_name
+  allow_minor_version_upgrade  = true
+
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
+  db_instance_class = "db.t3.small"
+
+  # It is mandatory to set the below values to create read replica instance
+  # Set the db_identifier of the source db
+  replicate_source_db = module.calculate_release_dates_api_rds.db_identifier
+
+  # No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
+
+  vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
+}
+
+resource "kubernetes_secret" "read_replica" {
+  metadata {
+    name      = "rds-read-replica-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
+    database_name         = module.read_replica.database_name
+    database_username     = module.read_replica.database_username
+    database_password     = module.read_replica.database_password
+    rds_instance_address  = module.read_replica.rds_instance_address
+  }
+}
+
+data "aws_security_group" "mp_dps_sg" {
+  name = var.mp_dps_sg_name
 }
