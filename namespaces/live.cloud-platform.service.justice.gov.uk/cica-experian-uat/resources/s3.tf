4e036dce30cce06da435f49dc727ed67f9869cc4
jack-burt-is <jack.burt@informed.com>
2025-02-25 14:48:27 +0000

    Create UAT environment for EBW

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-experian-uat/resources/s3.tf
@@ -0,0 +1,50 @@
+module "bankwizard_artifact_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.2.0"
+
+  # S3 configuration
+  versioning = true
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  bucket_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            aws_iam_role.bankwizard_bucket_assumable_role.arn
+          ]
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "$${bucket_arn}",
+          "$${bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "kubernetes_secret" "bankwizard_artifact_bucket" {
+  metadata {
+    name      = "bankwizard-artifact-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn  = module.bankwizard_artifact_bucket.bucket_arn
+    bucket_name = module.bankwizard_artifact_bucket.bucket_name
+  }
+}
