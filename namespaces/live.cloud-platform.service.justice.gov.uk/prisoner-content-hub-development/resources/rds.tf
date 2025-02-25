e39297dc2c49f4859a91b2dac00f75ca7c01d0c3
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-02-25 10:35:27 +0000

    Revert previous change, add new rds file

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/rds.tf
@@ -1,5 +1,5 @@
 module "drupal_rds" {
-  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
+  source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.2.2"
   vpc_name                   = var.vpc_name
   team_name                  = var.team_name
   business_unit              = var.business_unit
@@ -42,6 +42,9 @@ resource "kubernetes_secret" "drupal_rds" {
     database_username     = module.drupal_rds.database_username
     database_password     = module.drupal_rds.database_password
     rds_instance_address  = module.drupal_rds.rds_instance_address
+
+    # This may be a nicer way to represent the DB URL, but I don't _think_ we use it
+    # url                   = "postgres://${module.drupal_rds.database_username}:${module.drupal_rds.database_password}@${module.drupal_rds.rds_instance_endpoint}/${module.drupal_rds.database_name}"
   }
 }
 
@@ -59,47 +62,5 @@ resource "kubernetes_secret" "drupal_rds_refresh_creds_development" {
     database_username     = module.drupal_rds.database_username
     database_password     = module.drupal_rds.database_password
     rds_instance_address  = module.drupal_rds.rds_instance_address
-  }
-}
-
-module "prisoner_feedback_rds" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
-  vpc_name               = var.vpc_name
-  team_name              = var.team_name
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  namespace              = var.namespace
-
-  allow_major_version_upgrade = "false"
-  allow_minor_version_upgrade = "true"
-  prepare_for_major_upgrade   = false
-  db_engine                   = "postgres"
-  db_engine_version           = "17"
-  rds_family                  = "postgres17"
-  db_instance_class           = "db.t4g.micro"
-  db_max_allocated_storage    = "20"
-  enable_rds_auto_start_stop   = true
-  performance_insights_enabled = true
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "prisoner_feedback_rds" {
-  metadata {
-    name      = "prisoner-feedback-rds"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.prisoner_feedback_rds.rds_instance_endpoint
-    database_name         = module.prisoner_feedback_rds.database_name
-    database_username     = module.prisoner_feedback_rds.database_username
-    database_password     = module.prisoner_feedback_rds.database_password
-    rds_instance_address  = module.prisoner_feedback_rds.rds_instance_address
   }
 }
