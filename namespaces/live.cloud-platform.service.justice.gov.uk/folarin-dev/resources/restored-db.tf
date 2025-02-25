d0c44d975c4f1fdeaa163fb81e7bcad7b2a6e0db
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-02-25 16:40:46 +0000

    add missing arguments

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/restored-db.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/restored-db.tf
@@ -2,7 +2,6 @@ module "restored_db" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
 
   # Unique identifiers
-  application            = "restored-db"  # different from the original
   db_instance_name       = "restored-v16-instance"
   
   # Snapshot config
@@ -15,12 +14,28 @@ module "restored_db" {
   db_engine_version      = "16.3"
   rds_family             = "postgres16"
   db_instance_class      = "db.t4g.micro"
+
+  # Tags
+  application            = "restored-db"  # different from the original
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  # RDS configuration
+  allow_minor_version_upgrade  = true
+  allow_major_version_upgrade  = false
+  performance_insights_enabled = true
+  db_max_allocated_storage     = "500"
+  prepare_for_major_upgrade   = false
 }
 
 # Unique secret name
-resource "kubernetes_secret" "restored_db" {
+resource "kubernetes_secret" "restored_db_secret" {
   metadata {
-    name      = "restored-db-postgresql-output"  # unique name
+    name      = "restored-db-postgresql-output"
     namespace = var.namespace
   }
 
