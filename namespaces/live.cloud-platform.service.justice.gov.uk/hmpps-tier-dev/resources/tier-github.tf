ea9052013ea73f2f96415ac1e79477b659176b59
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-02-26 09:40:58 +0000

    PI-2770 move tier to gha

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-tier-dev/resources/tier-github.tf
@@ -0,0 +1,29 @@
+module "hmpps-tier-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-tier"
+  application                   = "hmpps-tier"
+  github_team                   = var.team_name
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment_name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "hmpps-tier-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-tier-ui"
+  application                   = "hmpps-tier-ui"
+  github_team                   = var.team_name
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment_name
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
