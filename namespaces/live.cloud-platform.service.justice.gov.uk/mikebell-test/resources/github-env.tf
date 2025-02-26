1c5baacd4caddf7106709d0be109873aec748e3d
Mike Bell <mike.bell1@digital.justice.gov.uk>
2025-02-26 19:13:35 +0000

    feat: test if github env tf resource works

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mikebell-test/resources/github-env.tf
@@ -0,0 +1,22 @@
+resource "github_repository_environment" "env" {
+  environment = "dev"
+  repository  = "mikebell-test"
+
+  #   reviewers {
+  #     teams = [for team in data.github_team.teams : team.id]
+  #   }
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+
+  prevent_self_review = false
+
+  # lifecycle {
+  #   precondition {
+  #     condition     = var.is_production == "true" ? length(var.reviewer_teams) > 0 : true
+  #     error_message = "Reviewer teams must be specified for production environments."
+  #   }
+  # }
+}
