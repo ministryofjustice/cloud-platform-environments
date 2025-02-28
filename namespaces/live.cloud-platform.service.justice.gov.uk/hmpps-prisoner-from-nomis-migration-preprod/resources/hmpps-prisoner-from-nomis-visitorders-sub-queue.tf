708e7482094c2e215290c6696d06431c491689cc
karenmillermoj <karen.miller@digital.justice.gov.uk>
2025-02-28 11:05:19 +0000

    SDIT-2634 Listen for events from OFFENDER_VISIT_BALANCES table

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-preprod/resources/hmpps-prisoner-from-nomis-visitorders-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-preprod/resources/hmpps-prisoner-from-nomis-visitorders-sub-queue.tf
@@ -109,9 +109,9 @@ resource "aws_sns_topic_subscription" "prisoner_from_nomis_visitorders_subscript
   endpoint  = module.prisoner_from_nomis_visitorders_queue.sqs_arn
   filter_policy = jsonencode({
     eventType = [
-      "OFFENDER_VISIT_BALANCE_ADJS-INSERTED",
-      "OFFENDER_VISIT_BALANCE_ADJS-UPDATED",
-      "OFFENDER_VISIT_BALANCE_ADJS-DELETED"
+      "OFFENDER_VISIT_BALANCES-INSERTED",
+      "OFFENDER_VISIT_BALANCES-UPDATED",
+      "OFFENDER_VISIT_BALANCES-DELETED"
     ]
   })
 }
