86ccbad0ba89e1a84955afc14c79ba46e4dbb270
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-03-03 09:38:18 +0000

    tf fmt

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-trivy-discovery.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-trivy-discovery.tf
@@ -1,9 +1,9 @@
 module "hmpps_trivy_discovery" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
-  github_repo = "hmpps-trivy-discovery"
-  application = "hmpps-trivy-discovery"
-  github_team = "hmpps-sre"
-  environment = "dev" # Should match environment name used in helm values file e.g. values-dev.yaml
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-trivy-discovery"
+  application                   = "hmpps-trivy-discovery"
+  github_team                   = "hmpps-sre"
+  environment                   = "dev" # Should match environment name used in helm values file e.g. values-dev.yaml
   protected_branches_only       = true
   is_production                 = var.is_production
   application_insights_instance = "dev"
