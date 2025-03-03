0c8f1f412c081e9132043b65f43bb4ae2396ac96
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-03-03 09:46:41 +0000

    fix for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-trivy-discovery.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-trivy-discovery.tf
@@ -3,10 +3,11 @@ module "hmpps_trivy_discovery" {
   github_repo                   = "hmpps-trivy-discovery"
   application                   = "hmpps-trivy-discovery"
   github_team                   = "hmpps-sre"
-  environment                   = "dev" # Should match environment name used in helm values file e.g. values-dev.yaml
+  environment                   = "prod" # Should match environment name used in helm values file e.g. values-dev.yaml
   protected_branches_only       = true
+  reviewer_teams                = ["hmpps-sre"]
   is_production                 = var.is_production
-  application_insights_instance = "dev"
+  application_insights_instance = "prod"
   source_template_repo          = "none"
   github_token                  = var.github_token
   namespace                     = var.namespace
