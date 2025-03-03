92b38f520cf940b7bd11f31e196f9ea087a87b66
Matt <34448412+mattops@users.noreply.github.com>
2025-03-03 12:37:42 +0000

    Update tf to use newer module for managing service accounts and github environments. (#30095)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-health-ping.tf
@@ -0,0 +1,14 @@
+module "hmpps_health_ping" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-health-ping"
+  application                   = "hmpps-health-ping"
+  github_team                   = "hmpps-sre"
+  environment                   = "dev" # Should match environment name used in helm values file e.g. values-dev.yaml
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
