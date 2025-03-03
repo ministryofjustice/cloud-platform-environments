f11513533ea356bcd1b0d0431875e2e8837f0be9
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-03-03 09:37:10 +0000

    Update tf to use newer module for managing service accounts and github environments.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-trivy-discovery.tf
@@ -0,0 +1,14 @@
+module "hmpps_trivy_discovery" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo = "hmpps-trivy-discovery"
+  application = "hmpps-trivy-discovery"
+  github_team = "hmpps-sre"
+  environment = "dev" # Should match environment name used in helm values file e.g. values-dev.yaml
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
