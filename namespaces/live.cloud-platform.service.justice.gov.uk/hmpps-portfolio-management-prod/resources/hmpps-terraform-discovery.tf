296e6135f2f359be20eebf47f184883ad28688b0
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-03-06 16:36:02 +0000

    Update tf to use newer module for managing service accounts and github environments.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-terraform-discovery.tf
@@ -0,0 +1,14 @@
+module "hmpps_terraform_discovery" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-terraform-discovery"
+  application                   = "hmpps-terraform-discovery"
+  github_team                   = "hmpps-sre"
+  environment                   = "prod" # Should match environment name used in helm values file e.g. values-dev.yaml
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = "prod"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
