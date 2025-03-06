a476f592a1c1d238e0e50dfff7db8b3ddea815dd
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2025-03-06 15:41:17 +0000

    Updated pingdom check

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-test/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-test/resources/pingdom.tf
@@ -3,7 +3,7 @@ provider "pingdom" {
 
 resource "pingdom_check" "hmpps-prisoner-profile-test-check" {
   type                     = "http"
-  name                     = "HMPPS Prisoner Profile - DEV"
+  name                     = "HMPPS Prisoner Profile - TEST"
   host                     = "prisoner-test.digital.prison.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
@@ -12,7 +12,7 @@ resource "pingdom_check" "hmpps-prisoner-profile-test-check" {
   url                      = "/ping"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_HMPPS,application_hmpps-prisoner-profile,component_healthcheck,isproduction_false,environment_dev"
+  tags                     = "businessunit_HMPPS,application_hmpps-prisoner-profile,component_healthcheck,isproduction_false,environment_test"
   probefilters             = "region:EU"
   integrationids           = [132508] #connect-dps-non-prod-alerts
 }
