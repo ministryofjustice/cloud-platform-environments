f1f8e73a218370b3fa70d05912ddbfc1f8bd3157
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-03-06 16:46:05 +0000

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-developer-portal.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-developer-portal.tf
@@ -80,6 +80,7 @@ module "hmpps_developer_portal" {
   application                   = "hmpps-developer-portal"
   github_team                   = "hmpps-sre"
   environment                   = "prod" # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-sre"]
   protected_branches_only       = true
   is_production                 = var.is_production
   application_insights_instance = "prod"
