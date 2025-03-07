079f52589f6f43fa1cd239ad11cb212a0d00592a
Michael Clancy <92720613+mtac50@users.noreply.github.com>
2025-03-07 09:31:41 +0000

    Create test env for prisoner profile (#30211)
    * DEV namespace cloned to form TEST namespace for prisoner profile.
    
    * Updated remaining references to dev
    
    * Updated pingdom check
    
    * Removed .git from source code link.
    
    * Updated team name.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-test/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "hmpps-prisoner-profile-test-check" {
+  type                     = "http"
+  name                     = "HMPPS Prisoner Profile - TEST"
+  host                     = "prisoner-test.digital.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/ping"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_HMPPS,application_hmpps-prisoner-profile,component_healthcheck,isproduction_false,environment_test"
+  probefilters             = "region:EU"
+  integrationids           = [132508] #connect-dps-non-prod-alerts
+}
