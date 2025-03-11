e651f39a385253270aa42a512b7af2f1b3b5fcd5
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-03-11 11:25:16 +0000

    Fix environment variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/hmpps-content-hub-feedback-reports.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-development/resources/hmpps-content-hub-feedback-reports.tf
@@ -3,7 +3,7 @@ module "hmpps_content-hub-feedback-reports" {
   github_repo = "hmpps-content-hub-feedback-reports"
   application = "hmpps-content-hub-feedback-reports"
   github_team = "hmpps-launchpad"
-  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  environment = var.environment-name # Should match environment name used in helm values file e.g. values-dev.yaml
   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
   #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
