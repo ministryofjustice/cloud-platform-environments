7775db924332196185bf15d18f65d854593b550b
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-03-11 15:46:09 +0000

    HAAR-3484: upgrade rds to postgres 16.6

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-dev/resources/rds.tf
@@ -11,8 +11,8 @@ module "subject_access_request_rds" {
   infrastructure_support      = var.infrastructure_support
   db_instance_class           = "db.t4g.small"
   db_engine                   = "postgres"
-  db_engine_version           = "15.8"
-  rds_family                  = "postgres15"
+  db_engine_version           = "16.6"
+  rds_family                  = "postgres16"
   prepare_for_major_upgrade   = true
   allow_major_version_upgrade = true
   allow_minor_version_upgrade = true
