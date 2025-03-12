5010c891db99792097b9eaa70a6fdb35f7a00b29
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2025-03-12 16:10:19 +0000

    Updated to check the plan after reverting variables.tf changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/variables.tf
@@ -38,7 +38,7 @@ variable "team_name" {
 variable "environment" {
   description = "Name of the environment type for this service"
   type        = string
-  default     = "prod"
+  default     = "production"
 }
 
 variable "infrastructure_support" {
