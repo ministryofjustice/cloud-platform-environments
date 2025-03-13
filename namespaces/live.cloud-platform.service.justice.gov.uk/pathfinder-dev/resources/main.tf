a0bc24245f9e1c51c7d77a4507009bb1efb2d2f5
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-03-13 09:08:10 +0000

    added in connection to domain events queue for pathfinder-api in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/main.tf
@@ -13,6 +13,18 @@ provider "aws" {
   }
 }
 
+# To be use in case the resources need to be created in London
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = var.team_name
+    }
+  }
+}
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
