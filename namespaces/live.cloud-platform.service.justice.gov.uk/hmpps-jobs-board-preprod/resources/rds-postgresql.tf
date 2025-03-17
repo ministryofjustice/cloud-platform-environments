5f4a3d22f7e34357c5f08b9e0667cf0ec8d0c80b
gorton.brown <gorton.brown@justice.gov.uk>
2025-03-17 10:33:55 +0000

    remove the minor upgrade request so it will be automanged

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-preprod/resources/rds-postgresql.tf
@@ -25,7 +25,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "16.2"
+  db_engine_version = "16"
   rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
