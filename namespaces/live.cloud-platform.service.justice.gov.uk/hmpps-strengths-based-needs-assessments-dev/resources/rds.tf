6ef84211f0facfd4723895a054d1d951dab8dd7c
Alex Warhurst <alex.warhurst@digital.justice.gov.uk>
2025-03-18 11:18:15 +0000

    Add Postgres FDW to the Assessments database

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-strengths-based-needs-assessments-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-strengths-based-needs-assessments-dev/resources/rds.tf
@@ -1,4 +1,3 @@
-
 module "hmpps_strengths_based_needs_assessments_dev_rds" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
   db_allocated_storage   = 10
@@ -22,7 +21,6 @@ module "hmpps_strengths_based_needs_assessments_dev_rds" {
   providers = {
     aws = aws.london
   }
-
 }
 
 resource "kubernetes_secret" "hmpps_strengths_based_needs_assessments_dev_rds_secret" {
@@ -52,7 +50,59 @@ provider "postgresql" {
   superuser        = false
 }
 
-# Installs postgres dblink extension
-resource "postgresql_extension" "dblink_new" {
-  name = "dblink"
+data "aws_ssm_parameter" "integrations_rds_database_name" {
+  name = "/hmpps-assess-risks-and-needs-integrations-dev/rds-database-name"
+}
+
+data "aws_ssm_parameter" "integrations_rds_database_username" {
+  name = "/hmpps-assess-risks-and-needs-integrations-dev/rds-database-username"
+}
+
+data "aws_ssm_parameter" "integrations_rds_database_password" {
+  name = "/hmpps-assess-risks-and-needs-integrations-dev/rds-database-password"
+}
+
+data "aws_ssm_parameter" "integrations_rds_instance_address" {
+  name = "/hmpps-assess-risks-and-needs-integrations-dev/rds-instance-address"
+}
+
+data "aws_ssm_parameter" "integrations_rds_instance_port" {
+  name = "/hmpps-assess-risks-and-needs-integrations-dev/rds-instance-port"
+}
+
+# Installs postgres_fdw extension
+resource "postgresql_extension" "postgres_fdw" {
+  name = "postgres_fdw"
+}
+
+# Create Foreign Server
+resource "postgresql_server" "myserver_postgres" {
+  server_name = "myserver_postgres"
+  fdw_name    = "postgres_fdw"
+  options = {
+    host   = data.aws_ssm_parameter.integrations_rds_instance_address # Other server
+    dbname = data.aws_ssm_parameter.integrations_rds_database_name # Other DB name
+    port   = data.aws_ssm_parameter.integrations_rds_instance_port # Other port
+  }
+
+  depends_on = [postgresql_extension.postgres_fdw]
+}
+
+# Create User Mapping
+resource "postgresql_role" "remote_role" {
+  name = "remote"
+}
+
+resource "postgresql_user_mapping" "remote_mapping" {
+  server_name = postgresql_server.myserver_postgres.server_name
+  user_name   = postgresql_role.remote_role.name
+  options = {
+    user = data.aws_ssm_parameter.integrations_rds_database_username # username for other server
+    password = data.aws_ssm_parameter.integrations_rds_database_password # password for other server
+  }
+}
+
+# Import Tables
+data "postgresql_tables" "tables" {
+  database = data.aws_ssm_parameter.integrations_rds_database_name
 }
