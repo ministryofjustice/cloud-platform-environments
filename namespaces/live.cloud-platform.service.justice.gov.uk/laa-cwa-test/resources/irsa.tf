0495587f316b52e13bd5d3270c0e4febddc60179
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-03-18 14:41:38 +0000

    fix: set cluster name statically

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-test/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-test/resources/irsa.tf
@@ -2,7 +2,7 @@ module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   # EKS configuration
-  eks_cluster_name = var.eks_cluster_name
+  eks_cluster_name = "live"
 
   # IRSA configuration
   service_account_name = "sa-laa-cwa-test"
