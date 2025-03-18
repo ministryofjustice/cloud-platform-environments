977defabe5648746e1fe96464e666172d648bae9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-03-18 13:24:40 +0000

    Add additional, team, tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/pingdom.tf
@@ -12,7 +12,7 @@ resource "pingdom_check" "laa-assure-hmrc-data-production-healthcheck" {
   url                      = "/ping"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_${var.business_unit},application_${var.repo_name},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name},laa_production_environment_dashboard"
+  tags                     = "businessunit_${var.business_unit},application_${var.repo_name},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name},laa,apply-for-legal-aid,laa_production_environment_dashboard"
   probefilters             = "region:EU"
   integrationids           = [129269]
 }
