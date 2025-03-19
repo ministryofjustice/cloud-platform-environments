20a48cdf167177e38374b4104457f11204976469
gorton.brown <gorton.brown@justice.gov.uk>
2025-03-19 10:36:10 +0000

    add version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-preprod/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-preprod/resources/secrets.tf
@@ -1,6 +1,6 @@
 
 module "secrets_manager" {
-  source = "../"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4"
   team_name              = var.team_name
   application            = var.application
   business_unit          = var.business_unit
@@ -14,7 +14,7 @@ module "secrets_manager" {
     "dpr-ingestion" = {
       description             = "dpr postgres user credentials",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "dpr-ingestion"
+      k8s_secret_name         = "dpr-ingestion",
     },
   }
 }
