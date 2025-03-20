8ad113050bae703614b1696cf016dc9a8ba1432e
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-03-20 14:44:55 +0000

    Fix case sensitivity for target repository

--- namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["cfo-caseassessmenttrackingsystem-cp"]
+  github_repositories = ["CFO-CaseAssessmentTrackingSystem-cp"]
 
   # Tags
   business_unit          = var.business_unit
