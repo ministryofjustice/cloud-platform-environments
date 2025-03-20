ecd83249271d71eb578168c74104d8e543e474e7
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-03-20 10:16:11 +0000

    Update email address

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-ui-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-court-data-ui-dev/resources/variables.tf
@@ -32,7 +32,7 @@ variable "environment-name" {
 
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
-  default     = "assessaclaim@digital.justice.gov.uk"
+  default     = "access-court-data-team@digital.justice.gov.uk"
 }
 
 variable "is_production" {
