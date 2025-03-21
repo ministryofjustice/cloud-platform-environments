c065621e321c009ce32cfc373e51f0da44bb3d14
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-03-21 14:04:40 +0000

    cfocats-dev: Add service pod and irsa

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/irsa.tf
@@ -0,0 +1,23 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+# EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+  
+
+  # IRSA configuration
+  service_account_name = var.namespace
+  namespace            = var.namespace # this is also used as a tag
+
+  role_policy_arns = {
+    s3 = module.s3_bucket.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
