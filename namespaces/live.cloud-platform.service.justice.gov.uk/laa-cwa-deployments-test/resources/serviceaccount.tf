4cd0c84be74af725af66704a9e946a1ff21cc64e
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-03-26 12:52:39 +0000

    fix: add back whitespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-deployments-test/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-deployments-test/resources/serviceaccount.tf
@@ -9,6 +9,7 @@ module "serviceaccount" {
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["laa-cwa-feature-tests", "laa-cwa"]
+
   serviceaccount_rules = [
     {
       api_groups = [""]
