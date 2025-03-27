7e02c372acf85c0be5021e50ed48b95edb834937
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-03-27 09:24:09 +0000

    fix: single namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-feature-tests-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-feature-tests-dev/resources/serviceaccount.tf
@@ -8,7 +8,7 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["laa-cwa-feature-tests"]
+  github_repositories = ["laa-cwa-feature-tests", "laa-cwa"]
   #github_environments = ["dev"]
   serviceaccount_rules = [
     {
