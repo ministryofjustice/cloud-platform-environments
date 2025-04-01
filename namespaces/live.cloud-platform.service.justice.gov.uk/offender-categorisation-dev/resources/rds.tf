fb674fd4be1b85294f0968cc4454046e441e6e31
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-04-01 16:12:56 +0100

    SI-1487 - Downgrade to 16

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
@@ -9,7 +9,7 @@ module "dps_rds" {
   environment_name            = var.environment_name
   infrastructure_support      = var.infrastructure_support
   rds_family                  = var.rds-family
-  dbs_instance_identifier     = "cloud-platform-9715beccbb718d4b-downgradedto16"
+  db_name     = "cloud-platform-9715beccbb718d4b-downgradedto16"
   snapshot_identifier         = "cloud-platform-9715beccbb718d4b-finalsnapshotMar312025"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "10000"
