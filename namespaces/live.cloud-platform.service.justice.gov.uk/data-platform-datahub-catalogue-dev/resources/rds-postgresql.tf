a2cc818a2ad624c31f0c25f4b7132eaceed9275f
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-04-02 13:18:40 +0100

    Remove read replica module configuration from RDS PostgreSQL setup

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/rds-postgresql.tf
@@ -7,7 +7,7 @@
 module "rds" {
   source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
   db_allocated_storage = var.db_allocated_storage
-  storage_type         = "gp2"
+  storage_type         = var.storage_type
 
   # VPC configuration
   vpc_name = var.vpc_name
@@ -38,68 +38,11 @@ module "rds" {
   team_name              = var.team_name
 }
 
-
-# To create a read replica, use the below code and update the values to specify the RDS instance
-# from which you are replicating. In this example, we're assuming that rds is the
-# source RDS instance and read-replica is the replica we are creating.
-
-module "read_replica" {
-  # default off
-  count                = 0
-  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.0.1"
-  db_allocated_storage = var.db_allocated_storage
-  storage_type         = "gp2"
-
-  vpc_name = var.vpc_name
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
-  # If any other inputs of the RDS is passed in the source db which are different from defaults,
-  # add them to the replica
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "16"
-  rds_family        = "postgres16"
-  db_instance_class = "db.t4g.small"
-  # It is mandatory to set the below values to create read replica instance
-
-  # Set the database_name of the source db
-  db_name = null # "db_name": conflicts with replicate_source_db
-
-  # Set the db_identifier of the source db
-  replicate_source_db = module.rds.db_identifier
-
-  # Set to true. No backups or snapshots are created for read replica
-  skip_final_snapshot        = "true"
-  db_backup_retention_period = 0
-
-  # If db_parameter is specified in source rds instance, use the same values.
-  # If not specified you dont need to add any. It will use the default values.
-
-  # db_parameter = [
-  #   {
-  #     name         = "rds.force_ssl"
-  #     value        = "0"
-  #     apply_method = "immediate"
-  #   }
-  # ]
-}
-
-
 resource "kubernetes_secret" "rds" {
   metadata {
     name      = "rds-postgresql-instance-output"
     namespace = var.namespace
   }
-
   data = {
     rds_instance_endpoint = module.rds.rds_instance_endpoint
     database_name         = module.rds.database_name
@@ -108,37 +51,7 @@ resource "kubernetes_secret" "rds" {
     rds_instance_address  = module.rds.rds_instance_address
     rds_url               = "jdbc:postgres://${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
   }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
 }
-
-
-resource "kubernetes_secret" "read_replica" {
-  # default off
-  count = 0
-
-  metadata {
-    name      = "rds-postgresql-read-replica-output"
-    namespace = var.namespace
-  }
-
-  # The database_username, database_password, database_name values are same as the source RDS instance.
-  # Uncomment if count > 0
-
-  /*
-  data = {
-    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-    rds_instance_address  = module.read_replica.rds_instance_address
-    access_key_id         = module.read_replica.access_key_id
-    secret_access_key     = module.read_replica.secret_access_key
-  }
-  */
-}
-
 
 # Configmap to store non-sensitive data related to the RDS instance
 
