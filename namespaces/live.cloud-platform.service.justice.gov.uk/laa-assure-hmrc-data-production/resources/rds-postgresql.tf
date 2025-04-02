2ef3ddbd43250f7d0744f0681e6ecade9077e88a
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2025-04-02 16:24:34 +0100

    AP-5630: Bump RDS postgresql 14.13 to 16.4

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-assure-hmrc-data-production/resources/rds-postgresql.tf
@@ -20,9 +20,10 @@ module "rds" {
   performance_insights_enabled = true
 
   # Database configuration
-  db_engine_version           = "14.13"
+  prepare_for_major_upgrade   = true
+  db_engine_version           = "16.4"
   db_instance_class           = "db.t4g.small"
-  rds_family                  = "postgres14"
+  rds_family                  = "postgres16"
   allow_minor_version_upgrade = "true"
   allow_major_version_upgrade = "false"
   enable_rds_auto_start_stop  = false
