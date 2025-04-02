2ddbe567ae86cb6716208db8f9f70c69d427c420
cloud-platform-moj <platforms+githubuser@digital.justice.gov.uk>
2025-04-02 21:52:05 +0100

    concourse: correcting rds version drift

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-dev/resources/rds.tf
@@ -13,7 +13,7 @@ module "dps_rds" {
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "10000"
   db_engine                   = "postgres"
-  db_engine_version           = "16"
+  db_engine_version = "17.2"
   allow_major_version_upgrade = "true"
   prepare_for_major_upgrade   = true
   enable_rds_auto_start_stop  = true
