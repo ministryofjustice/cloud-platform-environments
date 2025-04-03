dadaeec05f7c05ccad62005935d5376f80540a5a
robert.buczek <robert.buczek@digital.justice.gov.uk>
2025-04-03 09:46:47 +0100

    [LPF-535]: Integrate acceptance tests into dev pipeline

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/serviceaccount.tf
@@ -1,14 +1,14 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
-  namespace = var.namespace
+  namespace          = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["payforlegalaid", "payforlegalaid-ui"]
+  github_repositories = ["payforlegalaid", "payforlegalaid-ui", "payforlegalaid-tests"]
   github_environments = ["development"]
 
 
