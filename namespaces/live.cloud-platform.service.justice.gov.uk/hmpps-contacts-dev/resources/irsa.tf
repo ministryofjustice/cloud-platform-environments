c03d1955977fe0c145c9f05c061be63ac914b34f
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-04-03 16:28:23 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-dev/resources/irsa.tf
@@ -1,24 +1,30 @@
-data "kubernetes_secret" "audit_secret" {
-  metadata {
-    name      = "sqs-hmpps-audit-secret"
-    namespace = var.namespace
+# Add the names of the SQS which the app needs permissions to access.
+# The value of each item should be the namespace where the SQS was created.
+# This information is used to collect the IAM policies which are used by the IRSA module.
+locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
   }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
 }
 
-module "hmpps-contacts-ui-service-account" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  application            = var.application
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "hmpps-contacts-ui"
+  role_policy_arns     = local.sqs_policies
+  # Tags
   business_unit          = var.business_unit
-  eks_cluster_name       = var.eks_cluster_name
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
+}
 
-  service_account_name = "hmpps-contacts-ui"
-  role_policy_arns = merge(
-    { elasticache = module.elasticache_redis.irsa_policy_arn },
-    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
-  )
+data "aws_ssm_parameter" "irsa_policy_arns" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
