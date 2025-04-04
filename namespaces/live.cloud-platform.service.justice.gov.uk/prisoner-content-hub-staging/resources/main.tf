cc6438d33c2f945be3b5de3b087754bd08649b7c
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2025-04-04 13:50:08 +0100

    Add github team to all provider blocks for Content Hub Staging.

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-staging/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-staging/resources/main.tf
@@ -5,18 +5,36 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "prisoner-content-hub-developers"
+    }
+  }
 }
 
 # To be use in case the resources need to be created in London
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      GithubTeam = "prisoner-content-hub-developers"
+    }
+  }
 }
 
 # To be use in case the resources need to be created in Ireland
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      GithubTeam = "prisoner-content-hub-developers"
+    }
+  }
 }
 
 provider "github" {
