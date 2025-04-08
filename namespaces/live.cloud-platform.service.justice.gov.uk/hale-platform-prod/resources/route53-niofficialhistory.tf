7580a1a900147d2ea168ab726fa1b95dfe323e06
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2025-04-08 15:40:29 +0100

    Hale Platform - niofficialhistory dns records and certs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-niofficialhistory.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-niofficialhistory.tf
@@ -22,3 +22,28 @@ resource "kubernetes_secret" "niofficialhistory_route53_zone_sec" {
     nameservers = join(",", aws_route53_zone.niofficialhistory_route53_zone.name_servers)
   }
 }
+
+resource "aws_route53_record" "niofficialhistory_route53_mx_record_main" {
+  zone_id = aws_route53_zone.niofficialhistory_route53_zone.zone_id
+  name    = "niofficialhistory.org.uk"
+  type    = "MX"
+  ttl     = "300"
+  records = ["20 mx-02-eu-west-1.prod.hydra.sophos.com.", "10 mx-01-eu-west-1.prod.hydra.sophos.com."]
+}
+
+resource "aws_route53_record" "niofficialhistory_route53_txt_record_dmarc" {
+  zone_id = aws_route53_zone.niofficialhistory_route53_zone.zone_id
+  name    = "_dmarc.niofficialhistory.org.uk"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["v=DMARC1; p=quarantine; rua=mailto:dmarc-rua@finance-ni.gov.uk,mailto:dmarc-rua@dmarc.service.gov.uk; adkim=r; aspf=r; pct=0; sp=none"]
+}
+
+resource "aws_route53_record" "niofficialhistory_route53_txt_domainkey" {
+  zone_id = aws_route53_zone.niofficialhistory_route53_zone.zone_id
+  name    = "sophos2de752f943a0484491f5d897972a8f61._domainkey.niofficialhistory.org.uk"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuxlfukIPlNxBCe+RnfTXDhk1pub8rqscdImMHH679S9u0OQrm8+cumW0yr1ceqTgn2HBVcA83m+o+9CRUq2fvpxXDndhuD\"\"laqJi4uu7XFlsxK6oHxetyRTXmYCNq236JvK2etkfUDVmfKv3YkiW22Hqepqa3SG8bSC4xPmqlWW5ZQBKfvlYqMhxraznNhe72d
+mkJpZERp7qBYgkkfZnsj4HNqsnPP8\"\"i+a5XDW16OGnsMs9xhQkaU8Z6IaWCE8sjn4sxnn/CkPaj+ogQL1ehbgKOJwkVi14cthBGT92WStRIy7oKx7VnncdOk+a7wTVbuZA5qmwknCckNxAZA3vXrBQIDAQAB"]
+}
