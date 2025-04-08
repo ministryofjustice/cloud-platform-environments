dd5ffee373b3d67c2364aa9aa7879a800f337ff0
devkokov <dm.kokov@gmail.com>
2025-04-08 17:21:58 +0100

    ACE-25 Add Sentence Plan secret to hmpps-audit [PROD]

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/hmpps-sentence-plan.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_secret" "hmpps-sentence-plan-secret" {
+  metadata {
+    name      = "hmpps-sqs-audit-queue"
+    namespace = "hmpps-sentence-plan-prod"
+  }
+
+  data = {
+    sqs_queue_url  = module.hmpps_audit_queue.sqs_id
+    sqs_queue_arn  = module.hmpps_audit_queue.sqs_arn
+    sqs_queue_name = module.hmpps_audit_queue.sqs_name
+  }
+}
