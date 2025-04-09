6e26b2fc9d4f03ceaafa1ddeeba7e0d3b4128453
steverendell <steve.rendell@syscon.net>
2025-04-09 09:56:44 +0100

    SDIT-2692 Create reconciliation process between release and receive events and Nomis movements part 2
    Queue, topics, database and IRSA

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-dev/resources/irsa.tf
@@ -2,8 +2,6 @@ module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   eks_cluster_name = var.eks_cluster_name
-
-  # IRSA configuration
   service_account_name = "hmpps-dps-reconciliation"
   role_policy_arns     = merge(
     {
@@ -25,28 +23,4 @@ module "irsa" {
   namespace              = var.namespace # this is also used to attach your service account to your namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-data "kubernetes_secret" "audit_secret" {
-  metadata {
-    name      = "sqs-hmpps-audit-secret"
-    namespace = var.namespace
-  }
-}
-
-module "hmpps-dps-reconciliation-service-account" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
-  application            = var.application
-  business_unit          = var.business_unit
-  eks_cluster_name       = var.eks_cluster_name
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
-  service_account_name = "hmpps-dps-reconciliation"
-  role_policy_arns = merge(
-    { audit_sqs = data.kubernetes_secret.audit_secret.data.irsa_policy_arn },
-  )
 }
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-dev/resources/rds.tf
@@ -1,6 +1,6 @@
 module "hmpps_dps_reconciliation_rds" {
   source                     = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
-  db_allocated_storage       = 10
+  db_allocated_storage       = 20
   storage_type               = "gp3"
   vpc_name                   = var.vpc_name
   team_name                  = var.team_name
