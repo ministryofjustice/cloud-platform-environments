f7600eebd0d241610444f94f98fd92a5798ab405
Emterry <emma.terry@digital.justice.gov.uk>
2025-04-11 15:33:19 +0100

    change name for test

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/variables.tf
@@ -20,7 +20,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "webops"
+  default     = "test-workflow"
 }
 
 variable "environment" {
