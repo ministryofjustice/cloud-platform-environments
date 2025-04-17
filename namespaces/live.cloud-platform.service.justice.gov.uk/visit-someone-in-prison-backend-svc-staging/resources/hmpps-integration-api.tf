8f0b2dc68720e08b02b47b03511826a47895f882
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-17 12:42:39 +0100

    Add staging DLQ secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/hmpps-integration-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/hmpps-integration-api.tf
@@ -11,3 +11,16 @@ resource "kubernetes_secret" "integration-api-secret" {
     sqs_queue_name = module.hmpps_prison_visits_write_events_queue.sqs_name
   }
 }
+
+resource "kubernetes_secret" "integration-api-dlq-secret" {
+  metadata {
+    name      = "sqs-hmpps-prison-visits-write-events-staging-dlq-secret"
+    namespace = "hmpps-integration-api-dev"
+  }
+
+  data = {
+    sqs_queue_url  = module.hmpps_prison_visits_write_events_dead_letter_queue.sqs_id
+    sqs_queue_arn  = module.hmpps_prison_visits_write_events_dead_letter_queue.sqs_arn
+    sqs_queue_name = module.hmpps_prison_visits_write_events_dead_letter_queue.sqs_name
+  }
+}
