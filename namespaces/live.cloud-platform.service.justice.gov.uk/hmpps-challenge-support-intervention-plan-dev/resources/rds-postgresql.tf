b0ba4ec71f00d50fe75fe10235907c9a88daa612
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-04-17 11:10:08 +0100

    MIAD-361 upgrade db

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-dev/resources/rds-postgresql.tf
@@ -11,7 +11,7 @@ module "rds" {
   vpc_name = var.vpc_name
 
   # RDS configuration
-  prepare_for_major_upgrade    = true
+  prepare_for_major_upgrade    = false
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = true
   performance_insights_enabled = false
@@ -37,6 +37,16 @@ module "rds" {
   vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
 
   db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
     {
       name         = "max_wal_size"
       value        = "1024"
