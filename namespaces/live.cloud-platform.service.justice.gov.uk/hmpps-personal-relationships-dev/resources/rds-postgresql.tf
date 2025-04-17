dd510335271a69f2d5b9faf65028842f5403c83a
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-04-17 09:25:02 +0100

    PRC-612 : DPR access configurations

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-personal-relationships-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-personal-relationships-dev/resources/rds-postgresql.tf
@@ -68,7 +68,7 @@ module "rds" {
 module "read_replica" {
   # default off
   count  = 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=6.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
 
   vpc_name               = var.vpc_name
 
