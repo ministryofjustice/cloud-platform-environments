681c38df061f41836a65dd060865b49a74d0bd07
Tom Watkins <tom.watkins@justice.gov.uk>
2025-04-22 13:33:30 +0100

    Commit to force concourse rerun

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/irsa.tf
@@ -2,7 +2,7 @@
 # The value of each item should be the namespace where the SQS was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
-  # The names of the queues used and the namespace which created them
+  # The names of the queues used and the namespace which created them.
   sqs_queues = {
     "Digital-Prison-Services-prod-hmpps_audit_queue"                              = "hmpps-audit-prod",
     "education-skills-work-employment-prod-hmpps_jobs_board_integration_queue"    = "hmpps-jobs-board-integration-prod",
