afa3dd97bdd2f207ad38ee881d1b10a184bbc024
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-04-28 12:54:23 +0100

    Toggle the allow major version upgrades flag to true

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-dev/resources/rds.tf
@@ -11,7 +11,7 @@ module "adjustments_rds" {
   environment_name            = var.environment_name
   infrastructure_support      = var.infrastructure_support
   rds_family                  = var.rds_family
-  allow_major_version_upgrade = "false"
+  allow_major_version_upgrade = "true"
   db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500"
   db_engine_version           = "16"
