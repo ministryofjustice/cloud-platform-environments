0c23035334c2733363803b79c033b2d402125e49
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-04-28 13:25:12 +0100

    Update DB version to 16.8 on visit_allocation_rds instance on visit-someone-in-prison-backend-svc-dev namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/rds.tf
@@ -97,11 +97,11 @@ module "visit_allocation_rds" {
 
   allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
   db_engine                   = "postgres"
-  db_engine_version           = "15.8"
-  rds_family                  = "postgres15"
-  db_instance_class           = "db.t4g.micro"
+  db_engine_version           = "16.8"
+  rds_family                  = "postgres16"
+  db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "20"
   enable_rds_auto_start_stop   = true
   performance_insights_enabled = true
