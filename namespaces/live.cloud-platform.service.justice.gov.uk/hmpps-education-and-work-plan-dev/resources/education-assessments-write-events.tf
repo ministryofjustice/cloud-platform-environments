6471d465057cc13368778183972205c85cd3b2e4
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-04-29 15:01:37 +0100

    create queue for screening and assessments dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-dev/resources/education-assessments-write-events.tf
@@ -0,0 +1,86 @@
+# Prison visits write events for visit someone in prison
+
+module "eawp_assessment_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.1"
+
+  # Queue configuration
+  sqs_name                   = "eawp_assessment_events_queue"
+  encrypt_sqs_kms            = "true"
+  message_retention_seconds  = 43200 # 12 hours
+  visibility_timeout_seconds = 120   # 2 minutess
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.eawp_assessment_events_dead_letter_queue.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+# Dead letter queue
+
+module "eawp_assessment_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.1"
+
+  # Queue configuration
+  sqs_name                   = "eawp_assessment_events_dlq"
+  encrypt_sqs_kms            = "true"
+  message_retention_seconds  = 604800 # 7 days
+  visibility_timeout_seconds = 120    # 2 minutes
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+# Secrets
+
+resource "kubernetes_secret" "eawp_assessment_events_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "eawp-assessment-events-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.eawp_assessment_events_queue.sqs_id
+    sqs_queue_arn  = module.eawp_assessment_events_queue.sqs_arn
+    sqs_queue_name = module.eawp_assessment_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "eawp_assessment_events_dead_letter_queue" {
+  ## For metadata use - not _
+  metadata {
+    name = "eawp-assessment-events-dlq-secret"
+    ## Name space where the listening service is found
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url  = module.eawp_assessment_events_dead_letter_queue.sqs_id
+    sqs_queue_arn  = module.eawp_assessment_events_dead_letter_queue.sqs_arn
+    sqs_queue_name = module.eawp_assessment_events_dead_letter_queue.sqs_name
+  }
+}
