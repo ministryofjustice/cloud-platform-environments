22c41c6a9ae42bf8b76199b7b7dfca506f713c6b
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-04-29 14:46:38 +0100

    create queue for screening and assessments dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-dev/resources/irsa.tf
@@ -8,13 +8,12 @@ locals {
     "education-and-work-plan-assessments_queue"                               = "hmpps-education-and-work-plan-dev",
     "education-and-work-plan-assessments_dlq"                                 = "hmpps-education-and-work-plan-dev"
   }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 
   # The names of the SNS topics used and the namespace which created them
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
-
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
