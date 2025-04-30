b56d92a8ca4e3dc492bd50875ffa54e84fdc7ec0
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-04-30 11:30:47 +0100

    SDIT-2737: 🐛 Fix source repo for ui project (#32571)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/hmpps-historical-prisoner-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-historical-prisoner-dev/resources/hmpps-historical-prisoner-github.tf
@@ -23,7 +23,7 @@ module "hmpps-historical-prisoner-ui" {
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
   application_insights_instance = var.environment
-  source_template_repo          = "hmpps-template-kotlin"
+  source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
