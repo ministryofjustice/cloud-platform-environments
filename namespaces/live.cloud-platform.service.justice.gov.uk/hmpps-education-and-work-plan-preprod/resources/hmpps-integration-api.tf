a2627207efce8e00a194a6542deb0ea88f7e8730
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-05-02 20:27:01 +0100

    update to integration api secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-preprod/resources/hmpps-integration-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-preprod/resources/hmpps-integration-api.tf
@@ -20,8 +20,8 @@ resource "kubernetes_secret" "integration-api-dlq-secret" {
   }
 
   data = {
-    sqs_queue_url  = module.hmpps_eawp_domain_events_dlq.sqs_id
-    sqs_queue_arn  = module.hmpps_eawp_domain_events_dlq.sqs_arn
-    sqs_queue_name = module.hmpps_eawp_domain_events_dlq.sqs_name
+    sqs_queue_url  = module.eawp_assessment_events_dead_letter_queue.sqs_id
+    sqs_queue_arn  = module.eawp_assessment_events_dead_letter_queue.sqs_arn
+    sqs_queue_name = module.eawp_assessment_events_dead_letter_queue.sqs_name
   }
 }
