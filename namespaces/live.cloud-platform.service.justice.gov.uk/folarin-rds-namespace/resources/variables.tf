250b1658763cc91d7f90173eb24e81ba2e0978dc
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-05-06 10:44:51 +0100

    correct ns in the variables file

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "folarin-dev"
+  default     = "folarin-rds-namespace"
 }
 
 variable "business_unit" {
