2cc59a6192447ff348513333ae3ec288c34bb2d2
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-05-06 15:07:59 +0100

    added queue permission

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/irsa.tf
@@ -8,7 +8,7 @@ locals {
     "education-skills-work-employment-prod-hmpps_jobs_board_integration_queue"    = "hmpps-jobs-board-integration-prod",
     "book-a-prison-visit-prod-hmpps_prison_visits_write_events_queue"             = "visit-someone-in-prison-backend-svc-prod",
     "book-a-prison-visit-prod-hmpps_prison_visits_write_events_dlq"               = "visit-someone-in-prison-backend-svc-prod",
-    "hmpps-farsight-reduce-re-offend-prod-eawp_assessment_events_queue"           = "hmpps-education-and-work-plan-dev",
+    "hmpps-farsight-reduce-re-offend-prod-eawp_assessment_events_queue"           = "hmpps-education-and-work-plan-prod",
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 
