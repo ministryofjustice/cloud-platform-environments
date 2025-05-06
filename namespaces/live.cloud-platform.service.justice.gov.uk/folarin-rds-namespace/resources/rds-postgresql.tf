c9899ca4a8e06b51e5e1c09f4703a4a6367f468b
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-05-06 12:22:17 +0100

    add a read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
@@ -18,6 +18,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = true
+  db_allocated_storage         = "50"
   db_max_allocated_storage     = "500"
   prepare_for_major_upgrade   = false
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
@@ -25,8 +26,8 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "15.4"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres15"
+  db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
   # Tags
@@ -44,51 +45,53 @@ module "rds" {
 # source RDS instance and read-replica is the replica we are creating.
 
 
-# module "read_replica" {
-#   # default 0
-#   count = 1
-#   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+module "read_replica" {
+  # default 0
+  count = 1
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
 
-#   vpc_name               = var.vpc_name
+  vpc_name               = var.vpc_name
 
-#   # Tags
-#   application            = var.application
-#   business_unit          = var.business_unit
-#   environment_name       = var.environment
-#   infrastructure_support = var.infrastructure_support
-#   is_production          = var.is_production
-#   namespace              = var.namespace
-#   team_name              = var.team_name
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
 
-#   # If any other inputs of the RDS is passed in the source db which are different from defaults,
-#   # add them to the replica
+  # If any other inputs of the RDS is passed in the source db which are different from defaults,
+  # add them to the replica
 
-#   # PostgreSQL specifics
-#   db_engine         = "postgres"
-#   db_engine_version = "17.3"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-#   rds_family        = "postgres17"
-#   db_instance_class = "db.t4g.micro"
-#   # It is mandatory to set the below values to create read replica instance
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  rds_family        = "postgres16"
+  db_instance_class = "db.t4g.micro"
+  db_allocated_storage         = "50"
+  db_max_allocated_storage     = "500"
+  # It is mandatory to set the below values to create read replica instance
 
-#   # Set the db_identifier of the source db
-#   replicate_source_db = module.rds.db_identifier
-#   allow_major_version_upgrade = false ##inherit from the primary db
+  # Set the db_identifier of the source db
+  replicate_source_db = module.rds.db_identifier
+  allow_major_version_upgrade = false ##inherit from the primary db
 
-#   # Set to true. No backups or snapshots are created for read replica
-#   skip_final_snapshot        = "true"
-#   db_backup_retention_period = 0
+  # Set to true. No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
 
-#   # If db_parameter is specified in source rds instance, use the same values.
-#   # If not specified you dont need to add any. It will use the default values.
+  # If db_parameter is specified in source rds instance, use the same values.
+  # If not specified you dont need to add any. It will use the default values.
 
-#   # db_parameter = [
-#   #   {
-#   #     name         = "rds.force_ssl"
-#   #     value        = "0"
-#   #     apply_method = "immediate"
-#   #   }
-#   # ]
-# }
+  # db_parameter = [
+  #   {
+  #     name         = "rds.force_ssl"
+  #     value        = "0"
+  #     apply_method = "immediate"
+  #   }
+  # ]
+}
 
 resource "kubernetes_secret" "rds" {
   metadata {
@@ -114,7 +117,7 @@ resource "kubernetes_secret" "rds" {
 
 resource "kubernetes_secret" "read_replica" {
   # default off
-  count = 0
+  count = 1
 
   metadata {
     name      = "rds-postgresql-read-replica-output"
