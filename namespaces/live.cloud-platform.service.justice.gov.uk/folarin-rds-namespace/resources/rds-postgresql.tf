22a897631014c3d9163c5bc9bc4d024cd9de9ca5
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2025-05-06 13:54:09 +0100

    create a replica (#32710)
    * create a replica
    
    * use count to reference the appropriate element
    
    * remove secret and access key fields
    
    * remove mem storage params
    
    ---------
    
    Co-authored-by: folarin oyenuga <folarin.justice@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
@@ -45,53 +45,51 @@ module "rds" {
 # source RDS instance and read-replica is the replica we are creating.
 
 
-# module "read_replica" {
-#   # default 0
-#   count = 1
-#   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+module "read_replica" {
+  # default 0
+  count = 1
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
 
-#   vpc_name               = var.vpc_name
+  vpc_name               = var.vpc_name
 
-#   # Tags
-#   application            = var.application
-#   business_unit          = var.business_unit
-#   environment_name       = var.environment
-#   infrastructure_support = var.infrastructure_support
-#   is_production          = var.is_production
-#   namespace              = var.namespace
-#   team_name              = var.team_name
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
 
-#   # If any other inputs of the RDS is passed in the source db which are different from defaults,
-#   # add them to the replica
+  # If any other inputs of the RDS is passed in the source db which are different from defaults,
+  # add them to the replica
 
-#   # PostgreSQL specifics
-#   db_engine         = "postgres"
-#   db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-#   rds_family        = "postgres16"
-#   db_instance_class = "db.t4g.micro"
-#   db_allocated_storage         = "50"
-#   db_max_allocated_storage     = "500"
-#   # It is mandatory to set the below values to create read replica instance
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  rds_family        = "postgres16"
+  db_instance_class = "db.t4g.micro"
+  # It is mandatory to set the below values to create read replica instance
 
-#   # Set the db_identifier of the source db
-#   replicate_source_db = module.rds.db_identifier
-#   allow_major_version_upgrade = false ##inherit from the primary db
+  # Set the db_identifier of the source db
+  replicate_source_db = module.rds.db_identifier
+  allow_major_version_upgrade = false ##inherit from the primary db
 
-#   # Set to true. No backups or snapshots are created for read replica
-#   skip_final_snapshot        = "true"
-#   db_backup_retention_period = 0
+  # Set to true. No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
 
-#   # If db_parameter is specified in source rds instance, use the same values.
-#   # If not specified you dont need to add any. It will use the default values.
+  # If db_parameter is specified in source rds instance, use the same values.
+  # If not specified you dont need to add any. It will use the default values.
 
-#   # db_parameter = [
-#   #   {
-#   #     name         = "rds.force_ssl"
-#   #     value        = "0"
-#   #     apply_method = "immediate"
-#   #   }
-#   # ]
-# }
+  # db_parameter = [
+  #   {
+  #     name         = "rds.force_ssl"
+  #     value        = "0"
+  #     apply_method = "immediate"
+  #   }
+  # ]
+}
 
 resource "kubernetes_secret" "rds" {
   metadata {
@@ -115,27 +113,25 @@ resource "kubernetes_secret" "rds" {
 }
 
 
-# resource "kubernetes_secret" "read_replica" {
-#   # default off
-#   count = 1
+resource "kubernetes_secret" "read_replica" {
+  # default off
+  count = 1
 
-#   metadata {
-#     name      = "rds-postgresql-read-replica-output"
-#     namespace = var.namespace
-#   }
+  metadata {
+    name      = "rds-postgresql-read-replica-output"
+    namespace = var.namespace
+  }
 
-#   # The database_username, database_password, database_name values are same as the source RDS instance.
-#   # Uncomment if count > 0
+  # The database_username, database_password, database_name values are same as the source RDS instance.
+  # Uncomment if count > 0
 
-#   /*
-#   data = {
-#     rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-#     rds_instance_address  = module.read_replica.rds_instance_address
-#     access_key_id         = module.read_replica.access_key_id
-#     secret_access_key     = module.read_replica.secret_access_key
-#   }
-#   */
-# }
+  data = {
+    rds_instance_endpoint = module.read_replica[count.index].rds_instance_endpoint
+    rds_instance_address  = module.read_replica[count.index].rds_instance_address
+    # access_key_id         = module.read_replica[count.index].access_key_id
+    # secret_access_key     = module.read_replica[count.index].secret_access_key
+  }
+}
 
 
 # Configmap to store non-sensitive data related to the RDS instance
