0f79f2aa84a111fa95140e72fc39f3d03c02fbb5
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-05-07 14:46:08 +0100

    Reintroducing athena role usage without creating new resource, removing further ssm privileges.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/iam-policies.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/iam-policies.tf
@@ -3,8 +3,7 @@ data "aws_iam_policy_document" "ssm_policy" {
     actions = [
       "ssm:GetParameter",
       "ssm:GetParameters",
-      "ssm:PutParameter",
-      "ssm:DeleteParameter"
+      "ssm:PutParameter"
     ]
     resources = [
       "arn:aws:ssm:eu-west-2:754256621582:parameter/${var.namespace}/athena_general_role_arn"
@@ -15,5 +14,23 @@ data "aws_iam_policy_document" "ssm_policy" {
 resource "aws_iam_policy" "ssm_access" {
   name   = "${var.namespace}-ssm-policy"
   policy = data.aws_iam_policy_document.ssm_policy.json
+  tags = local.tags
+}
+
+
+data "aws_iam_policy_document" "athena_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    resources = [
+      data.aws_ssm_parameter.athena_general_role_arn.value
+    ]
+  }
+}
+
+resource "aws_iam_policy" "athena_access" {
+  name   = "${var.namespace}-athena-policy-general"
+  policy = data.aws_iam_policy_document.athena_policy.json
   tags = local.tags
 }
