6b30512e03be186d003cd425923ed45918ffac0d
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-05-07 14:03:01 +0100

    revert

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/athena-iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/athena-iam.tf
@@ -16,6 +16,7 @@ data "aws_iam_policy_document" "athena" {
     actions = [
       "athena:StartQueryExecution",
       "athena:GetQueryResults",
+      "athena:GetWorkGroup",
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
