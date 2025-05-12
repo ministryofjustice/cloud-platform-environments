0e05e5a4528a2cc3f71be18f83c64a886f978f99
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-05-12 22:32:25 +0000

    Add cdn-production
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cdn-production/resources/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "cdn" {
+  name = "cdn.service.justice.gov.uk"
+
+  tags = {
+    business_unit          = var.business_unit
+    application            = var.application
+    is_production          = var.is_production
+    team_name              = var.team_name
+    namespace              = var.namespace
+    environment_name       = var.environment
+    infrastructure_support = var.infrastructure_support
+  }
+}
+
+resource "kubernetes_secret" "cdn_route53_zone" {
+  metadata {
+    name      = "cdn-route53-zone"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id     = aws_route53_zone.cdn.zone_id
+    nameservers = join("\n", aws_route53_zone.cdn.name_servers)
+  }
+}
