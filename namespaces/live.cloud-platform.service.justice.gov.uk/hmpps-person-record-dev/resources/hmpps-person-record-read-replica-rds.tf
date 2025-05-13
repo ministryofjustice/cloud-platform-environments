51b59e52794363c68be653eeafd2a33ad2476251
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-05-13 09:02:35 +0100

    CPR-703 add a read replica database in dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/hmpps-person-record-read-replica-rds.tf
@@ -0,0 +1,39 @@
+module "hmpps_person_record_rds_read_replica" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business_unit               = var.business_unit
+  application                 = var.application
+  is_production               = var.is_production
+  namespace                   = var.namespace
+  environment_name            = var.environment
+  infrastructure_support      = var.infrastructure_support
+  rds_family                  = "postgres17"
+  db_instance_class           = "db.r6g.xlarge"
+  db_engine                   = "postgres"
+  db_engine_version           = "17.1"
+  prepare_for_major_upgrade   = false
+  allow_major_version_upgrade = "true"
+  enable_rds_auto_start_stop  = true
+  replicate_source_db         =  module.hmpps_person_record_rds.database_name
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_person_record_rds_read_replica" {
+  metadata {
+    name      = "hmpps-person-record-rds-read-replica-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_person_record_rds_read_replica.rds_instance_endpoint
+    database_name         = module.hmpps_person_record_rds_read_replica.database_name
+    database_username     = module.hmpps_person_record_rds_read_replica.database_username
+    database_password     = module.hmpps_person_record_rds_read_replica.database_password
+    rds_instance_address  = module.hmpps_person_record_rds_read_replica.rds_instance_address
+    url                   = "postgres://${module.hmpps_person_record_rds_read_replica.database_username}:${module.hmpps_person_record_rds_read_replica.database_password}@${module.hmpps_person_record_rds_read_replica.rds_instance_endpoint}/${module.hmpps_person_record_rds_read_replica.database_name}"
+  }
+}
