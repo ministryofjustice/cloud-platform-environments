61eec522c5b724e0609cf2247e8a797a1047d0fa
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-05-13 14:00:45 +0100

    CPR-703 remove unused secret values and make database smaller

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/rds.tf
@@ -9,7 +9,7 @@ module "hmpps_person_record_rds" {
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
   rds_family                  = "postgres17"
-  db_instance_class           = "db.r6g.xlarge"
+  db_instance_class           = "db.t4g.medium"
   db_engine                   = "postgres"
   db_engine_version           = "17.1"
   prepare_for_major_upgrade   = false
