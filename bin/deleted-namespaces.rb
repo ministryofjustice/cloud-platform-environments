c82d96df50ab4c9c30a26a21e446c0cb0a6a842d
Emterry <emma.terry@digital.justice.gov.uk>
2025-05-13 14:12:30 +0100

    update deleted namespace to detect prod only

--- bin/deleted-namespaces.rb
+++ bin/deleted-namespaces.rb
@@ -4,6 +4,7 @@ require File.join(".", File.dirname(__FILE__), "..", "lib", "cp_env")
 
 require "slack-notify"
 
+
 def send_notification(msg)
   webhook_url = "https://hooks.slack.com/services/#{ENV.fetch("SLACK_WEBHOOK")}"
 
@@ -15,13 +16,17 @@ def send_notification(msg)
   ).notify(msg)
 end
 
+# Fetch deleted namespaces
 namespaces = deleted_namespaces("live.cloud-platform.service.justice.gov.uk")
 
-if namespaces.any?
-  msg = <<~EOF
-    The following namespaces have been removed from the environments repository:
+# Filter namespaces to include only production-related ones
+production_namespaces = namespaces.select { |ns| ns.match?(/production/) }
 
-      - #{namespaces.join("\n  - ")}
+if production_namespaces.any?
+  msg = <<~EOF
+    The following production namespaces have been removed from the environments repository:
+
+      - #{production_namespaces.join("\n  - ")}
 
     Please delete them:
 
@@ -32,5 +37,5 @@ if namespaces.any?
   puts msg
   send_notification(msg)
 else
-  puts "No namespaces to delete"
+  puts "No production namespaces to delete"
 end
