4ff0262615bd4d526644cc8318885734f685d964
samz-justice <sam.zaroug@justice.gov.uk>
2025-05-14 08:33:33 +0100

    CPR-703 added read replica for db in prod env

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/hmpps-person-record-read-replica-rds.tf
@@ -0,0 +1,35 @@
+module "hmpps_person_record_rds_read_replica" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business_unit               = var.business_unit
+  application                 = var.application
+  is_production               = var.is_production
+  namespace                   = var.namespace
+  environment_name            = var.environment
+  infrastructure_support      = var.infrastructure_support
+  rds_family                  = "postgres17"
+  db_instance_class           = "db.t4g.small"
+  db_engine                   = "postgres"
+  db_engine_version           = "17.1"
+  prepare_for_major_upgrade   = false
+  allow_major_version_upgrade = "true"
+  enable_rds_auto_start_stop  = true
+  replicate_source_db         =  module.hmpps_person_record_rds.db_identifier
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "hmpps_person_record_rds_read_replica" {
+  metadata {
+    name      = "hmpps-person-record-rds-read-replica-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.hmpps_person_record_rds_read_replica.rds_instance_endpoint
+    rds_instance_address  = module.hmpps_person_record_rds_read_replica.rds_instance_address
+  }
+}
