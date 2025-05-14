6bdd6b31d22f6d7f2dd1182ea9687b121806edaa
Timi Olubusi <timiolubusi@icloud.com>
2025-05-14 12:16:21 +0100

    Create apex-s3.tf

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-dev/resources/apex-s3.tf
@@ -0,0 +1,19 @@
+ locals {
+  application = "apex certificated bailiffs"
+}
+
+module "apex-migration-s3" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.2.0"
+
+  # S3 configuration
+  versioning = true
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = local.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
