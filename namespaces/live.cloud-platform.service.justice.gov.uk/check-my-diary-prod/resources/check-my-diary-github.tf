83038aae5fa1012274d6427054f790019aa93f23
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-05-15 14:54:18 +0100

    SDIT-2737: 👷 Fix team name for check my diary (#32947)

--- namespaces/live.cloud-platform.service.justice.gov.uk/check-my-diary-prod/resources/check-my-diary-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/check-my-diary-prod/resources/check-my-diary-github.tf
@@ -2,7 +2,7 @@ module "cmd-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "cmd-api"
   application                   = "cmd-api"
-  github_team                   = var.team_name
+  github_team                   = "syscon-devs"
   environment                   = var.environment
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
@@ -12,14 +12,14 @@ module "cmd-api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
-  reviewer_teams                = [var.team_name]
+  reviewer_teams                = ["syscon-devs"]
 }
 
 module "csr-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "csr-api"
   application                   = "csr-api"
-  github_team                   = var.team_name
+  github_team                   = "syscon-devs"
   environment                   = var.environment
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
@@ -29,14 +29,14 @@ module "csr-api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
-  reviewer_teams                = [var.team_name]
+  reviewer_teams                = ["syscon-devs"]
 }
 
 module "check-my-diary" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "check-my-diary"
   application                   = "check-my-diary"
-  github_team                   = var.team_name
+  github_team                   = "syscon-devs"
   environment                   = var.environment
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
@@ -46,5 +46,5 @@ module "check-my-diary" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
-  reviewer_teams                = [var.team_name]
+  reviewer_teams                = ["syscon-devs"]
 }
