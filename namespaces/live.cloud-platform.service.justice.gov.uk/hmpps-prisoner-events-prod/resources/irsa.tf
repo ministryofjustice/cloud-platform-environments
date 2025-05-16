0ddbc3bd49ac03020ff2bcb549dbf8745df5a32e
karenmillermoj <karen.miller@digital.justice.gov.uk>
2025-05-16 12:27:52 +0100

    SDIT-2737 Change irsa module name to allow github action to use service name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-events-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-events-prod/resources/irsa.tf
@@ -2,16 +2,16 @@ locals {
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-160f3055cc4e04c4105ee85f2ed1fccb" = "offender-events-prod"
   }
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  irsa_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
 
-module "hmpps-prisoner-events" {
+module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
 
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-prisoner-events"
-  role_policy_arns     = local.sns_policies
+  role_policy_arns     = local.irsa_policies
 
   # Tags
   business_unit          = var.business_unit
