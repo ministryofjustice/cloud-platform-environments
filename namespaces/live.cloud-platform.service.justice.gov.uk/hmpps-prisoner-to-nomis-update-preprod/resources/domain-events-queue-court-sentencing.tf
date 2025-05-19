4883e0daff83d893b26347be342c5027413d5149
andymarke <andy.marke@digital.justice.gov.uk>
2025-05-19 14:24:03 +0100

    recall.inserted for hmpps_prisoner_to_nomis_court_sentencing - preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-preprod/resources/domain-events-queue-court-sentencing.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-preprod/resources/domain-events-queue-court-sentencing.tf
@@ -122,7 +122,8 @@ resource "aws_sns_topic_subscription" "hmpps_prisoner_to_nomis_court_sentencing_
       "sentence.deleted",
       "sentence.period-length.inserted",
       "sentence.period-length.updated",
-      "sentence.period-length.deleted"
+      "sentence.period-length.deleted",
+      "recall.inserted"
     ]
   })
 }
