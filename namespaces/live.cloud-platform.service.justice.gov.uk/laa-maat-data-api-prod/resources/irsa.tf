5299087fbf6f212cfb69782cfab958107a93a1ce
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2025-05-21 16:01:21 +0100

    Formatting changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-data-api-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-data-api-prod/resources/irsa.tf
@@ -12,9 +12,9 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    hearing_resulted_queue      = data.kubernetes_secret.queue_arn.data.hearing_resulted_queue_irsa_policy_arn
-    create_link_queue = data.kubernetes_secret.queue_arn.data.create_link_queue_m_irsa_policy_arn
-    unlink_queue = data.kubernetes_secret.queue_arn.data.unlink_queue_m_irsa_policy_arn
+    hearing_resulted_queue  = data.kubernetes_secret.queue_arn.data.hearing_resulted_queue_irsa_policy_arn
+    create_link_queue       = data.kubernetes_secret.queue_arn.data.create_link_queue_m_irsa_policy_arn
+    unlink_queue            = data.kubernetes_secret.queue_arn.data.unlink_queue_m_irsa_policy_arn
   }
 
   # Tags
