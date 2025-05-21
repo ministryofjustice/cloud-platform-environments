41859e4744b43e5ebf275dcf8f891ccd8436b080
Jesus Laiz <zheileman@users.noreply.github.com>
2025-05-21 11:27:52 +0100

    MO-1693 Add github service account to CNL staging (#33169)
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1693

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-complexity-of-need-staging/resources/serviceaccount-github.tf
@@ -0,0 +1,13 @@
+module "serviceaccount_github" {
+  source                   = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo              = "hmpps-complexity-of-need"
+  application              = "hmpps-complexity-of-need"
+  github_team              = var.team_name
+  environment              = var.environment
+  is_production            = var.is_production
+  selected_branch_patterns = ["main", "staging"]
+  source_template_repo     = "none"
+  github_token             = var.github_token
+  namespace                = var.namespace
+  kubernetes_cluster       = var.kubernetes_cluster
+}
