c6d6ad0bf03c70f1ec4317f9e0cc63ece114f427
Marcus Aspin <maspin@unilink.com>
2025-05-21 13:04:45 +0100

    PI-3102 Increase OpenSearch volume size (prod) (#33193)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-prod/resources/opensearch.tf
@@ -22,7 +22,8 @@ module "opensearch" {
   }
   proxy_count = 3
   ebs_options = {
-    volume_size = 368
+    volume_size = 1536
+    iops        = 5000 # must be greater than volume_size * 3
     throughput  = 250
   }
 }
