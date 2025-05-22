bd306b0d9eb519c32e84f5d74c3cbd8b3ac9dfdf
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-05-22 14:29:25 +0100

    Prepare deletion of namespace (#33266)

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-firebreak-aws-microservice-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-firebreak-aws-microservice-dev/resources/ecr.tf
@@ -14,6 +14,8 @@ module "ecr" {
   oidc_providers      = ["github"]
   github_repositories = ["data-platform-firebreak-aws-microservice"]
 
+  deletion_protection = false
+
   # Tags
   business_unit          = var.business_unit
   application            = var.application
