0699fa9524257f10e3eea601396f3ea189b83ec9
andymarke <andy.marke@digital.justice.gov.uk>
2025-05-22 11:32:18 +0100

    recall.updated and deleted for hmpps_prisoner_to_nomis_court_sentencing - dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/domain-events-queue-court-sentencing.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/domain-events-queue-court-sentencing.tf
@@ -125,6 +125,8 @@ resource "aws_sns_topic_subscription" "hmpps_prisoner_to_nomis_court_sentencing_
       "sentence.period-length.updated",
       "sentence.period-length.deleted",
       "recall.inserted",
+      "recall.updated",
+      "recall.deleted",
     ]
   })
 }
