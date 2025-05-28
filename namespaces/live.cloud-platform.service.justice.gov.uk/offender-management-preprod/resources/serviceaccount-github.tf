e2b6039235c0b73dd7fb4f06096e1188275f157d
Jesus Laiz <zheileman@users.noreply.github.com>
2025-05-28 12:18:12 +0100

    MO-1698 Add github service account to MPC preprod (#33477)
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1698

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-preprod/resources/serviceaccount-github.tf
@@ -0,0 +1,13 @@
+module "serviceaccount_github" {
+  source                   = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo              = "offender-management-allocation-manager"
+  application              = "offender-management-allocation-manager"
+  github_team              = var.team_name
+  environment              = var.environment_name
+  is_production            = var.is_production
+  selected_branch_patterns = ["main"]
+  source_template_repo     = "none"
+  github_token             = var.github_token
+  namespace                = var.namespace
+  kubernetes_cluster       = var.kubernetes_cluster
+}
