2c2f40e0848b653494858d5986efbc9891d4e956
Phil Payne <phil.payne@digital.justice.gov.uk>
2025-05-29 16:17:16 +0100

    Preprod
    Co-Authored-By: farrell-m <168534673+farrell-m@users.noreply.github.com>

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-preprod/resources/secret.tf
@@ -0,0 +1,44 @@
+module "secrets_manager" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "${var.environment}-ccms-pda-secrets" = {
+      description             = "Secrets for ccms pda",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "${var.environment}-ccms-pda-secrets"
+    },
+    "${var.environment}-ccms-assess-service-adaptor-secrets" = {
+        description             = "Secrets for ccms assess service adaptor",
+        recovery_window_in_days = 7,
+        k8s_secret_name         = "${var.environment}-ccms-assess-service-adaptor-secrets"
+    },
+    "${var.environment}-ccms-pui-secrets" = {
+        description             = "Secrets for ccms pui",
+        recovery_window_in_days = 7,
+        k8s_secret_name         = "${var.environment}-ccms-pui-secrets"
+    },
+    "${var.environment}-ccms-connector-secrets" = {
+        description             = "Secrets for ccms connector",
+        recovery_window_in_days = 7,
+        k8s_secret_name         = "${var.environment}-ccms-connector-secrets"
+    },
+    "${var.environment}-ccms-edrms-web-service-secrets" = {
+        description             = "Secrets for ccms edrms api",
+        recovery_window_in_days = 7,
+        k8s_secret_name         = "${var.environment}-ccms-edrms-web-service-secrets"
+    }
+    "${var.environment}-ccms-opa-hub-secrets" = {
+        description             = "Secrets for ccms opa hub",
+        recovery_window_in_days = 7,
+        k8s_secret_name         = "${var.environment}-ccms-opa-hub-secrets"
+    }
+  }
+}
