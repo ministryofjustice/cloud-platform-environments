cff2bc741d717226cd679cc18faa58ac2c6a2b28
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-06-02 14:12:01 +0100

    CVSL-2876: Add domain events queue for CVL API dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/irsa.tf
@@ -1,4 +1,11 @@
 locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-cvl_domain_events_queue" = "hmpps-domain-events-dev",
+    "Digital-Prison-Services-dev-cvl_domain_events_dead_letter_queue" = "hmpps-domain-events-dev",
+  }
+
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
@@ -11,7 +18,7 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = merge(local.sns_policies, { rds_policy = module.create_and_vary_a_licence_api_rds.irsa_policy_arn })
+  role_policy_arns     = merge(local.sns_policies, local.sqs_policies, { rds_policy = module.create_and_vary_a_licence_api_rds.irsa_policy_arn })
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -24,4 +31,9 @@ module "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
