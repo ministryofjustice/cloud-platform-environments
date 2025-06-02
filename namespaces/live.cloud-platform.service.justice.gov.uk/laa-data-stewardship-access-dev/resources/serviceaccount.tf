577c304e9f635db3c15bd1d4ea1b11d35b11712f
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-06-02 10:09:24 +0100

    added ecr and serviceaccount tf files

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-stewardship-access-dev/resources/serviceaccount.tf
@@ -0,0 +1,11 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "01-01-2000"
+
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["laa-data-stewardship-access-dev"]
+}
