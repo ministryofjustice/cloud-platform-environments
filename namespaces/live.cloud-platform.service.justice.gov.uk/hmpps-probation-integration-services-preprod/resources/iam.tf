c84e84196dca5ee786be9ffe49cc17f350c642e7
Marcus Aspin <maspin@unilink.com>
2025-06-03 14:53:23 +0100

    PI-3125 Re-enable "PI-3013 Copy prod events to preprod queue for testing (#32520)"
    This reverts commit 73adf5203d8854aa902e17a54744330a0bef70d7.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/iam.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/iam.tf
@@ -46,6 +46,21 @@ data "aws_iam_policy_document" "sqs_queue_policy_document" {
     resources = ["*"]
   }
   statement {
+    sid     = "ProbationOffenderEventsProdToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_sns_topic.probation-offender-events-prod.arn]
+    }
+    resources = ["*"]
+  }
+  statement {
     sid     = "CourtTopicToQueue"
     effect  = "Allow"
     actions = ["sqs:SendMessage"]
