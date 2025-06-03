15d9e4f5c6eb481822cb3c898341bac503fc7bde
mikeAdamsMOJ <michael.adams3@justice.gov.uk>
2025-06-03 10:39:04 +0100

    align naming

--- namespaces/live.cloud-platform.service.justice.gov.uk/mbpr-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mbpr-dev/resources/ecr.tf
@@ -4,7 +4,7 @@
  * releases page of this repository.
  *
  */
-module "ecr" {
+module "ecr_credentials" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
 
   # Repository configuration
