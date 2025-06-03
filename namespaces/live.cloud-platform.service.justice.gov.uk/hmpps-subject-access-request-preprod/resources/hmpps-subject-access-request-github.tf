c6696cd8612c65716580a1476f0d54dd221e4dc8
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-03 16:21:17 +0100

    update subject access preprod - update variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-preprod/resources/hmpps-subject-access-request-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-preprod/resources/hmpps-subject-access-request-github.tf
@@ -1,16 +1,16 @@
 module "hmpps_subject_access_request_html_renderer" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
-  github_repo = "hmpps-subject-access-request-html-renderer"
-  application = "hmpps-subject-access-request-html-renderer"
-  github_team = var.team_name
-  environment = var.environment
-  selected_branch_patterns      = ["main"]
-  protected_branches_only       = true
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-subject-access-request-html-renderer"
+  application                   = "hmpps-subject-access-request-html-renderer"
+  github_team                   = var.team_name
+  environment                   = var.environment
   is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
   application_insights_instance = var.environment
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
   reviewer_teams                = [var.team_name]
 }
