e1e88580aa51fd75b75afeaf1918076f27776139
steverendell <steve.rendell@syscon.net>
2025-06-03 11:09:20 +0100

    SDIT-2692 Create reconciliation process between release and receive events and Nomis movements 2

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-preprod/resources/hmpps-dps-reconciliation-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dps-reconciliation-preprod/resources/hmpps-dps-reconciliation-github.tf
@@ -1,10 +1,9 @@
-module "hmpps_template_kotlin" {
+module "hmpps-dps-reconciliation" {
   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo = "hmpps-dps-reconciliation"
   application = "hmpps-dps-reconciliation"
   github_team = "syscon-devs"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
   #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
@@ -13,4 +12,5 @@ module "hmpps_template_kotlin" {
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
+  reviewer_teams                = [var.team_name]
 }
