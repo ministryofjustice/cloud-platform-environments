8b25e94349b5c564ed6d98226dad3cb093664a38
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2025-06-04 09:43:53 +0100

    Create hosted zone (#33720)
    * Create hosted zone
    
    * Alter domain name

--- namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/variables.tf
@@ -56,6 +56,10 @@ variable "slack_channel" {
   default     = "cdpt-care-arrangement-plan"
 }
 
+variable "domain" {
+  default = "child-arrangements-plan.service.justice.gov.uk"
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
