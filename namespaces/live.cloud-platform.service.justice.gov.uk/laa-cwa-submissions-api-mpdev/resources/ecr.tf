05c17653213420e7f1abeced1d1dc8c7dc28d6ee
kkmanne <kirankumar.manne@justice.gov.uk>
2025-06-06 15:52:01 +0100

    multiple ECR modules

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-submissions-api-mpdev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cwa-submissions-api-mpdev/resources/ecr.tf
@@ -1,33 +1,29 @@
 module "ecr_cwa_submissions" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
-
-  repo_name     = var.namespace
+  source  = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
+  repo_name           = "cwa-submissions-api"
+  oidc_providers      = ["github"]
   github_repositories = ["cwa-submissions-api"]
 
-  # Tags
-  team_name              = var.team_name
-  namespace              = var.namespace
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
 
 module "ecr_csv_converter" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
-
-  repo_name     = var.namespace
-  repo_suffix   = "-csv"  # ✅ This differentiates the repo name
-
+  source  = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
+  repo_name           = "csv-converter"
+  oidc_providers      = ["github"]
   github_repositories = ["cwa-submissions-api"]
 
-  # Tags
-  team_name              = var.team_name
-  namespace              = var.namespace
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
