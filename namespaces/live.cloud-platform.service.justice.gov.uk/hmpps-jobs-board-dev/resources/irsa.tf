b88a62c6125b9ff6f3283f8bfb77bc559939acfa
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-06-06 15:46:58 +0100

    [ESWE-1330] Revise IRSA at `hmpps-jobs-board-dev`
    Signed-off-by: rickchoijd <rick.choi@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-dev/resources/irsa.tf
@@ -4,7 +4,7 @@
 locals {
   # The names of the queues used and the namespace which created them
   sqs_queues = {
-    "education-skills-work-employment-dev-hmpps_jobs_board_integration_queue" = "hmpps-jobs-board-integration-dev"
+    "education-skills-and-work-devs-dev-hmpps_jobs_board_integration_queue" = "hmpps-jobs-board-integration-dev"
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
