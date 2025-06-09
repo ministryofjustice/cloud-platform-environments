26923ed8e0ddf86efc15e280e02ce4b36f2bb56f
adam <brown-a2@users.noreply.github.com>
2025-06-09 11:44:45 +0100

    Add in 2 MX records to finucane.independent-inquiry.uk

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-finucane.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-finucane.tf
@@ -22,3 +22,15 @@ resource "kubernetes_secret" "finucane_route53_zone_sec" {
     nameservers = join(",", aws_route53_zone.finucane_route53_zone.name_servers)
   }
 }
+
+resource "aws_route53_record" "finucane_route53_mx_records" {
+  zone_id = aws_route53_zone.ppo_route53_zone.zone_id
+  name    = "finucane.independent-inquiry.uk"
+  type    = "MX"
+  ttl     = "300"
+  records = [
+    "10 mx-01-eu-west-1.prod.hydra.sophos.com.",
+    "20 mx-02-eu-west-1.prod.hydra.sophos.com.",
+  ]
+}
+
