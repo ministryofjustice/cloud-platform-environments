03608d504b8ccc7cf2996beacc33a2738d5a891d
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-06-09 14:34:43 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/cloudtrail.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "audit_s3_bucket_cloudtrail_logs" {
-  bucket = module.cloudtrail_s3_bucket.name
-
-  tags = {
-    name = "audit_s3_bucket_cloudtrail_logs"
-    environment_name = var.environment-name
-  }
-}
