d0f82b13917c366b18f3f94781ccd6b0799f85f6
Mark Butler <mark.butler@solirius.com>
2025-06-09 17:02:13 +0100

    Missing eks cluster

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-preprod/resources/secrets-manager.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-preprod/resources/secrets-manager.tf
@@ -7,6 +7,7 @@ module "secrets_manager" {
   namespace              = var.namespace
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
     "slack-webhook" = {
