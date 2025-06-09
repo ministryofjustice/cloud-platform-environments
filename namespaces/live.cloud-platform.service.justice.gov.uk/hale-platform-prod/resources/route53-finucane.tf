abb9be6254bb1ca36e9a75cf56f8514188e7cbbb
adam <brown-a2@users.noreply.github.com>
2025-06-09 12:08:36 +0100

    Add DMARC and DKIM records to finucane

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-finucane.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-prod/resources/route53-finucane.tf
@@ -24,13 +24,43 @@ resource "kubernetes_secret" "finucane_route53_zone_sec" {
 }
 
 resource "aws_route53_record" "finucane_route53_mx_records" {
-  zone_id = aws_route53_zone.ppo_route53_zone.zone_id
+  zone_id = aws_route53_zone.finucane_route53_zone.zone_id
   name    = "finucane.independent-inquiry.uk"
   type    = "MX"
   ttl     = "300"
   records = [
     "10 mx-01-eu-west-1.prod.hydra.sophos.com.",
     "20 mx-02-eu-west-1.prod.hydra.sophos.com.",
+  ]
+}
+
+resource "aws_route53_record" "finucane_route53_dmarc" {
+  zone_id = aws_route53_zone.finucane_route53_zone.zone_id
+  name    = "_dmarc.finucane.independent-inquiry.uk"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "v=DMARC1; p=reject; rua=mailto:dmarc-rua@finance-ni.gov.uk,mailto:dmarc-rua@dmarc.service.gov.uk; adkim=r; aspf=r; pct=100"
+  ]
+}
+
+resource "aws_route53_record" "finucane_route53_spf" {
+  zone_id = aws_route53_zone.finucane_route53_zone.zone_id
+  name    = "finucane.independent-inquiry.uk"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "v=spf1 ip4:194.32.29.0/24 ip4:194.32.31.0/24 ip4:52.208.126.243 ip4:52.31.106.198 ip4:198.154.180.128/26 include:_spf_euwest1.prod.hydra.sophos.com include:spf.protection.outlook.com -all"
+  ]
+}
+
+resource "aws_route53_record" "finucane_route53_dkim" {
+  zone_id = aws_route53_zone.finucane_route53_zone.zone_id
+  name    = "sophos6ccbafd5a548420e95312e0473634265._domainkey.finucane.independent-inquiry.uk"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAugfVMdz1tPmlB2oEiIhVLeZnRUzge+doPPTmRvbmhtkPzFGA+mkV49eRXbakmS4pTUgb4DF2A3M5duqLDbhxUDG9Jmg7Yavq6qqPYACVai/pPws+mekRMjlUhSxxa6tiqgWRd5Aw+b154uslY4GzaZCrioKd/l0WCKYD3pE2TQlo0"/"/u7eev9ViU7NtrieJiEbpfbhAeJx3DkI4zP991sducdz0p7q9Qk4ApuxwDbPnkXiZFWKKcf9KBNu3OYbQkVLAalwjihxKqSMPnbRLZMGbuS3/9DW/cHH9zRoKGS7yJjUVVHJ3clYTn7xzD8PFcwqoFVfPDnCnuOIniLrtgl51QIDAQAB"
   ]
 }
 
