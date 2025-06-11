c85c433a0e8c9e114a3a145ee904aedb5a18e051
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-06-11 12:14:40 +0100

    HAAR-4008 Added CloudTrail bucket policy (audit dev)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/s3-cloudtrail.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/s3-cloudtrail.tf
@@ -21,7 +21,60 @@ resource "aws_s3_bucket_ownership_controls" "cloudtrail_bucket_ownership_control
   }
 }
 
-resource "aws_s3_bucket_acl" "cloudtrail_bucket_acl" {
+resource "aws_s3_bucket_policy" "cloudtrail_logs" {
   bucket = module.cloudtrail_s3_bucket.bucket_name
-  acl    = "private"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailWrite",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "${module.cloudtrail_s3_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+      },
+      {
+        Sid    = "AWSCloudTrailGetBucketAcl",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action   = "s3:GetBucketAcl",
+        Resource = module.cloudtrail_s3_bucket.arn
+      }
+    ]
+  })
 }
+
+resource "aws_cloudtrail" "s3_data_trail" {
+  name                          = "s3-object-events-${var.namespace}"
+  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_name
+  is_multi_region_trail         = true
+  include_global_service_events = true
+  enable_log_file_validation    = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = false
+
+    data_resource {
+      type = "AWS::S3::Object"
+      values = ["arn:aws:s3:::${module.s3.bucket_name}/"]
+    }
+  }
+
+  tags = {
+    Environment = var.environment-name
+    Terraform   = "true"
+  }
+
+  depends_on = [
+    aws_s3_bucket_policy.cloudtrail_logs,
+    aws_s3_bucket_ownership_controls.cloudtrail_bucket_ownership_controls
+  ]
+}
+
+data "aws_caller_identity" "current" {}
