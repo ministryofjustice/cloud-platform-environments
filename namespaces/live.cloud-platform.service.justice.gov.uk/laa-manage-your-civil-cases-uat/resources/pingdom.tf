e4f329b1e365967963964750d3f68778c0ac15bf
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-06-11 11:06:10 +0100

    Update integration id

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-your-civil-cases-uat/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-your-civil-cases-uat/resources/pingdom.tf
@@ -13,5 +13,5 @@ resource "pingdom_check" "laa-manage-your-civil-cases-uat" {
   port                     = 443
   tags                     = "businessunit_${var.business_unit},application_${var.application},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.infrastructure_support}"
   probefilters             = "region:EU"
-  integrationids           = [0]
+  integrationids           = [143418]
 }
