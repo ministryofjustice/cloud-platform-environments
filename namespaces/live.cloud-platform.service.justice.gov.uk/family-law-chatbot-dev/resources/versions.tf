b1253be17ce45eeb6ae0dc32008f8c6b715be3bc
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-06-13 12:23:30 +0100

    Update according to failed test

--- namespaces/live.cloud-platform.service.justice.gov.uk/family-law-chatbot-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/family-law-chatbot-dev/resources/versions.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.78.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
     github = {
       source  = "integrations/github"
       version = "~> 5.39.0"
