fdf0f9877b7c95669645e8016250b835bc98d933
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-06-13 12:20:57 +0100

    `family-law-chatbot-dev` Provision environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/family-law-chatbot-dev/resources/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      GithubTeam    = var.team_name
+      slack-channel = var.slack_channel
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
