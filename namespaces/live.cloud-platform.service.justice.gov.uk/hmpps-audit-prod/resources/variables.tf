7f3545e2cea59b1e780005f2bad6dad8ba1116c2
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-13 12:44:25 +0100

    HAAR-3945: Add hmpps-template terraform module to hmpps-audit-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-prod/resources/variables.tf
@@ -1,24 +1,18 @@
-variable "namespace" {
-  default = "hmpps-audit-prod"
+variable "application" {
+  default     = "HMPPS-Audit-Service"
 }
 
-
-variable "vpc_name" {
+variable "namespace" {
+  default = "hmpps-audit-prod"
 }
 
 variable "domain_audit_api" {
   default = "audit-api.hmpps.service.justice.gov.uk"
 }
 
-
 variable "business_unit" {
   description = "Area of the MOJ responsible for the service."
   default     = "HMPPS"
-}
-
-variable "application" {
-  description = "The name of the application"
-  default     = "HMPPS-Audit-Service"
 }
 
 variable "team_name" {
@@ -40,6 +34,11 @@ variable "is_production" {
   default = "true"
 }
 
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "hmpps-auth-audit-registers"
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
@@ -52,10 +51,8 @@ variable "github_token" {
   default     = ""
 }
 
-variable "eks_cluster_name" {
-  description = "The name of the eks cluster to retrieve the OIDC information"
-}
-
-
-
 variable "kubernetes_cluster" {}
+
+variable "eks_cluster_name" {}
+
+variable "vpc_name" {}
