1b781ca559319a43f328479dd037de0c69f28972
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-06-13 14:44:37 +0100

    RASS-939 align environment with github actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-prod/resources/hmpps-court-register-api-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-prod/resources/hmpps-court-register-api-github.tf
@@ -7,6 +7,7 @@ module "hmpps_template_kotlin" {
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
   application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
+  reviewer_teams = [var.team_name]
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
