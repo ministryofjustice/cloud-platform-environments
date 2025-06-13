b0fc991f93dd43a4140447b0092be89031552798
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2025-06-13 11:06:30 +0100

    disabling db start off and on during night time as it prod db

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-prod/resources/rds-postgresql.tf
@@ -14,7 +14,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
-  enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
