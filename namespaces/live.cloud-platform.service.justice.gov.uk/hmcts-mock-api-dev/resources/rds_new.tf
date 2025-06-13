e2192d9c598097c343cba70ca5bb6b5d225aa227
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-06-13 14:21:44 +0100

    Switch off

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-mock-api-dev/resources/rds_new.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmcts-mock-api-dev/resources/rds_new.tf
@@ -14,7 +14,7 @@ module "hmcts_mock_api_rds_instance" {
   infrastructure_support = "laa@digital.justice.gov.uk"
   rds_family             = "postgres17"
 
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   allow_major_version_upgrade = "true"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "500"
