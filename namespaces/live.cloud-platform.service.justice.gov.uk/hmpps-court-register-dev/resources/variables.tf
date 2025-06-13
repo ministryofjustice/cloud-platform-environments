830e8d7e99b1bc32abb845333cf6bfed47723174
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-06-13 13:36:54 +0100

    RASS-939 align environment with github actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/variables.tf
@@ -29,13 +29,13 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "farsight-devs"
+  default     = "hmpps-farsight-sentence-offence"
 }
 
 variable "environment" {
   description = "Name of the environment type for this service"
   type        = string
-  default     = "development"
+  default     = "dev"
 }
 
 variable "infrastructure_support" {
