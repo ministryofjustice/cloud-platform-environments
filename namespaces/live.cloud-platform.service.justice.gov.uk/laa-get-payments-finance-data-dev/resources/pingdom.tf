22c329a75f1a31d3b31cdc6274c53ca9866e602f
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2025-06-13 17:08:20 +0100

    Remove pingdom check for the UI

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-dev/resources/pingdom.tf
@@ -1,22 +1,6 @@
 provider "pingdom" {
 }
 
-resource "pingdom_check" "ui-get-legal-aid-data-dev" {
-  type                     = "http"
-  name                     = "ui get legal aid data - dev - cloud platform"
-  host                     = "dev.get-legal-aid-data.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 2147483647
-  notifyagainevery         = 0
-  url                      = "/health"
-  encryption               = true
-  port                     = 443
-  tags                     = "businessunit_${var.business_unit},application_glad,component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_glad"
-  probefilters             = "region:EU"
-  integrationids           = [141868]
-}
-
 resource "pingdom_check" "api-get-legal-aid-data-dev" {
   type                     = "http"
   name                     = "api get legal aid data - dev - cloud platform"
