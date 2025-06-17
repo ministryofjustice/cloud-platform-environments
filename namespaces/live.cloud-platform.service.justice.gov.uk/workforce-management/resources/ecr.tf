c01fb416cf500574a901923c2b00a94d0b6726cb
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-06-17 14:14:40 +0100

    :wrench: WFP-3164: prepare deletion of namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management/resources/ecr.tf
@@ -6,6 +6,8 @@ module "ecr-repo" {
   oidc_providers      = ["github"]
   github_repositories = [var.namespace, "manage-a-workforce-design-history"]
 
+  deletion_protection = false
+
   # Tags
   business_unit          = var.business_unit
   application            = var.application
