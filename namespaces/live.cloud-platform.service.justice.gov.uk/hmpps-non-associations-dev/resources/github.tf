d9be97883f82cf8421783b666571db35b2fe4f07
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2025-06-18 17:48:34 +0100

    hmpps-non-associations-dev: Set up for GH Actions
    Added two instances of the cloud-platform-terraform-hmpps-template module, one for the UI and the other for the API.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-dev/resources/github.tf
@@ -0,0 +1,31 @@
+module "hmpps-non-associations" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-non-associations"
+  application                   = "hmpps-non-associations"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "hmpps-non-associations-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-non-associations-api"
+  application                   = "hmpps-non-associations-api"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
