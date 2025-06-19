7a7c83dc0c6df254f00ecef44fcaf064ca7d8cf3
Timi Olubusi <timiolubusi@icloud.com>
2025-06-19 16:16:36 +0100

    Adding route 53 hosting zone for apex

--- namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-prod/resources/route53.tf
@@ -22,3 +22,28 @@ resource "kubernetes_secret" "certbailiff_route53_zone_sec" {
     zone_id = aws_route53_zone.certbailiff_route53_zone.zone_id
   }
 }
+
+resource "aws_route53_zone" "apex_certbailiff_route53_zone" {
+  name = "apex-certificatedbailiffs.justice.gov.uk"
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "certbailiff_route53_zone_sec" {
+  metadata {
+    name      = "apex-certbailiff-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.apex_certbailiff_route53_zone.zone_id
+  }
+}
