e036265947adabaa8787c2b2bb14e8fa1a860831
AledWEvans <aled.evans1@justice.gov.uk>
2025-06-23 15:32:58 +0100

    CVSL-2944 Minor upgrade 15.12 to 15.13

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-prod/resources/rds.tf
@@ -11,10 +11,10 @@ module "create_and_vary_a_licence_api_rds" {
   infrastructure_support      = var.infrastructure_support
   allow_minor_version_upgrade = true
   allow_major_version_upgrade = false
-  db_instance_class           = "db.t4g.small"
-  db_engine_version = "15.12"
-  rds_family                  = "postgres15"
   prepare_for_major_upgrade   = false
+  db_instance_class           = "db.t4g.small"
+  db_engine_version           = "15.13"
+  rds_family                  = "postgres15"
   db_password_rotated_date    = "23-09-2024"
   db_allocated_storage        = 20
 
