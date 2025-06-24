ca340a837f81fc94722f4f16292a8279cfc07041
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-06-24 17:26:59 +0100

    [APG-1011] Add Terraform configuration for HMPPS manage-and-deliver domain events handling (DEV)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-dev/resources/sqs-policy.tf
@@ -0,0 +1,18 @@
+# Policy to allow SNS -> SQS
+data "aws_iam_policy_document" "sqs_queue_policy_document" {
+  statement {
+    sid     = "DomainEventsToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_sns_topic.hmpps-domain-events.arn]
+    }
+    resources = ["*"]
+  }
+}
