6a33483a8df6dc55beef1c657fde06fe36b2ce6e
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2025-06-24 13:47:48 +0100

    fix build issue

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-not-on-libra-auto-search-application-tst/resources/secret.tf
@@ -24,5 +24,6 @@ module "secrets_manager" {
       description             = "Test data source credentials",
       recovery_window_in_days = 7
       k8s_secret_name         = "datasource-credentials"
+    }
   }
 }
