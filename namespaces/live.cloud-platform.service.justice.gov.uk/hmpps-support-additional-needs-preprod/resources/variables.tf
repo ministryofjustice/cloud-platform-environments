ce6578fe4259091e513e29ab1fc4b433750f0209
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2025-06-24 11:10:09 +0100

    RR-1598 - Corrected tag to use team name var, and corrected team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-support-additional-needs-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-support-additional-needs-preprod/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "hmpps-support-additional-needs-devs"
+  default     = "hmpps-support-additional-needs-live"
 }
 
 variable "environment" {
