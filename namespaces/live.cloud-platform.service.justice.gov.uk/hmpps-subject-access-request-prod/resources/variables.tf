5d8beb0952a9df0c4af8c1ca41db2d815c8e82b4
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-26 13:42:26 +0100

    HAAR-2945: update github actions and add S3 bucket hmpps-subject-access-request-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-prod/resources/variables.tf
@@ -1,52 +1,29 @@
-variable "vpc_name" {
-  description = "VPC name to create security groups in for the ElastiCache and RDS modules"
-  type        = string
-}
-
-variable "kubernetes_cluster" {
-  description = "Kubernetes cluster name for references to secrets for service accounts"
-  type        = string
-}
-
 variable "application" {
-  description = "Name of the application you are deploying"
-  type        = string
   default     = "Subject Access Request"
 }
 
 variable "namespace" {
-  description = "Name of the namespace these resources are part of"
-  type        = string
   default     = "hmpps-subject-access-request-prod"
 }
 
 variable "business_unit" {
-  description = "Area of the MOJ responsible for this service"
-  type        = string
   default     = "HMPPS"
 }
 
 variable "team_name" {
-  description = "Name of the development team responsible for this service"
-  type        = string
-  default     = "hmpps-subject-access-requests"
+  default     = "hmpps-sar-live"
 }
 
 variable "environment" {
-  description = "Name of the environment type for this service"
-  type        = string
   default     = "production"
 }
 
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
-  type        = string
   default     = "subject-access-request-service@digital.justice.gov.uk"
 }
 
 variable "is_production" {
-  description = "Whether this environment type is production or not"
-  type        = string
   default     = "true"
 }
 
@@ -68,12 +45,8 @@ variable "github_token" {
   default     = ""
 }
 
-variable "number_cache_clusters" {
-  default = "2"
-}
-
-variable "rds_family" {
-  default = "postgres15"
-}
+variable "kubernetes_cluster" {}
 
 variable "eks_cluster_name" {}
+
+variable "vpc_name" {}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-subject-access-request-prod/s3.tf
@@ -0,0 +1,48 @@
+module "hmpps-subject-access-request_s3_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.2.0" # use the latest release
+
+  # S3 configuration
+  versioning = true
+
+  providers = {
+    aws = aws.london
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "s3_bucket_lock_configuration" {
+  bucket = module.hmpps-subject-access-request_s3_bucket.bucket_name
+
+  rule {
+    default_retention {
+      mode = "GOVERNANCE"
+        days = 14
+    }
+  }
+
+  depends_on = [
+    module.hmpps-subject-access-request_s3_bucket
+  ]
+
+}
+
+resource "kubernetes_secret" "hmpps-subject-access-request_s3_bucket" {
+  metadata {
+    name      = "hmpps-subject-access-request-s3"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn  = module.hmpps-subject-access-request_s3_bucket.bucket_arn
+    bucket_name = module.hmpps-subject-access-request_s3_bucket.bucket_name
+  }
+}
