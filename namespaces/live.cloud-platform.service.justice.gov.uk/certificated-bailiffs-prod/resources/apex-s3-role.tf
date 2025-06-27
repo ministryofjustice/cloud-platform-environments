ca8e765a0fa36d58ec035e1738e25d1e07f3a713
Timi Olubusi <timiolubusi@icloud.com>
2025-06-27 10:56:31 +0100

    Removing oracle apex database and assosiated roles temporarily

--- namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-prod/resources/apex-s3-role.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-prod/resources/apex-s3-role.tf
@@ -1,13 +1,13 @@
-data "aws_iam_role" "rds_s3_integration" {
-  name = "rds-s3-integration-role"
-}
+# data "aws_iam_role" "rds_s3_integration" {
+#   name = "rds-s3-integration-role"
+# }
 
-data "aws_iam_policy" "rds_s3_access_policy" {
-  name = "rds-s3-access-policy"
-}
+# data "aws_iam_policy" "rds_s3_access_policy" {
+#   name = "rds-s3-access-policy"
+# }
 
 
-resource "aws_iam_role_policy_attachment" "rds_s3_attach" {
-  role       = data.aws_iam_role.rds_s3_integration.name
-  policy_arn = data.aws_iam_policy.rds_s3_access_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "rds_s3_attach" {
+#   role       = data.aws_iam_role.rds_s3_integration.name
+#   policy_arn = data.aws_iam_policy.rds_s3_access_policy.arn
+# }
