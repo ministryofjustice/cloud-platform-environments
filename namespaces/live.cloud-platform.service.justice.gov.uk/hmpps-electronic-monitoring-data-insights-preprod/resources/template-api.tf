1222a1a6014255febd20ddb3fed52cd0ec90732b
John Asafo <John.Asafo@justice.gov.uk>
2025-07-01 15:06:18 +0100

    EPT-234: EMDI Preprod env
    Empty Commit
    
    EPT-234: EMDI fixed typo

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-preprod/resources/template-api.tf
@@ -0,0 +1,15 @@
+module "hmpps_electronic_monitoring_crime_matching_api" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo = "hmpps-electronic-monitoring-data-insights-api"
+  application = "hmpps-electronic-monitoring-data-insights-api"
+  github_team = "hmpps-em-probation"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-preprod.yaml
+  reviewer_teams                = ["hmpps-em-probation"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main"] # Optional
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
