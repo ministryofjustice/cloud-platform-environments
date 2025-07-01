18c2a291431289228bc3d29ba22d59b85dbd13f4
Mark Butler <mark.butler@solirius.com>
2025-07-01 12:12:11 +0100

    Add version constraint

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opensearch-destroy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opensearch-destroy.tf
@@ -1,18 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.48.0" # Match your original version
-    }
     opensearch = {
       source  = "opensearch-project/opensearch"
-      version = "2.2.1" # Match version from cloud-platform-terraform-opensearch-alert
+      version = "~> 2.1.0" # Match version from cloud-platform-terraform-opensearch-alert
     }
   }
-}
-
-provider "aws" {
-  region = "eu-west-2" # Adjust to your AWS region (e.g., eu-west-2 for MoJ Cloud Platform)
 }
 
 provider "opensearch" {
