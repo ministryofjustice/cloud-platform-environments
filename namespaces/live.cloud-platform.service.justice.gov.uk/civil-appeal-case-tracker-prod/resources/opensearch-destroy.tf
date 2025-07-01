715938512fa87b4c0fb07302956d26c0bcb23bb3
Mark Butler <mark.butler@solirius.com>
2025-07-01 12:00:08 +0100

    Revise cleanup tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opensearch-destroy.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opensearch-destroy.tf
@@ -1,31 +1,20 @@
-module "opensearch_alert" {
-
-    variable "aws_opensearch_domain" {
-        description = "The OpenSearch Cluster for alert creation. Set default to user application logs one."
-        type        = string
-        default     = "cp-live-app-logs"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0" # Match your original version
     }
-
-    variable "aws_iam_role" {
-        description = "AWS IAM role for alert creation. Set detault to user application logs one."
-        type        = string
-        default     = "opensearch-access-role-app-logs"
+    opensearch = {
+      source  = "opensearch-project/opensearch"
+      version = "2.2.1" # Match version from cloud-platform-terraform-opensearch-alert
     }
+  }
+}
 
-    data "aws_opensearch_domain" "live_app_logs" {
-        domain_name = var.aws_opensearch_domain
-    }
+provider "aws" {
+  region = "eu-west-2" # Adjust to your AWS region (e.g., eu-west-2 for MoJ Cloud Platform)
+}
 
-    data "aws_iam_role" "os_access_role_app_logs" {
-        name = var.aws_iam_role
-    }
-
-    provider "opensearch" {
-        alias               = "app_logs"
-        url                 = "https://${data.aws_opensearch_domain.live_app_logs.endpoint}"
-        aws_assume_role_arn = data.aws_iam_role.os_access_role_app_logs.arn
-        sign_aws_requests   = true
-        healthcheck         = false
-        sniff               = false
-    }
+provider "opensearch" {
+  url = "https://cp-live-app-logs.eu-west-2.es.amazonaws.com" # Replace with your OpenSearch endpoint
 }
