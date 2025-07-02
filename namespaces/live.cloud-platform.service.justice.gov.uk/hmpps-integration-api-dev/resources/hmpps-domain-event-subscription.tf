2fd5fc129a8ecc5ee1890de55241172a3c837ba7
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-02 09:16:16 +0100

    added SAN message types to plp filter

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/hmpps-domain-event-subscription.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/hmpps-domain-event-subscription.tf
@@ -90,6 +90,8 @@ resource "aws_sns_topic_subscription" "integration_api_domain_events_subscriptio
       "probation.staff.updated",
       "plp.induction-schedule.updated",
       "plp.review-schedule.updated",
+      "san.plan-creation-schedule.updated",
+      "san.review-schedule.updated",
       "create-and-vary-a-licence.licence.activated",
       "create-and-vary-a-licence.licence.inactivated",
       "person.alert.created",
