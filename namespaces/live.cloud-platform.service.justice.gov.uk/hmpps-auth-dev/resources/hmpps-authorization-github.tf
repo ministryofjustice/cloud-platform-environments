a38cff2d47bc19d85e3be6e192425bc5e0bf1acb
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-07-02 11:23:49 +0100

    HAAR-3945: Add hmpps-template terraform module to hmpps-auth-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-dev/resources/hmpps-authorization-github.tf
@@ -0,0 +1,31 @@
+module "hmpps_authorization" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-authorization"
+  application                   = "hmpps-authorization"
+  github_team                   = "haha-live"
+  environment                   = var.environment-name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "hmpps-authorization_api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-authorization-api"
+  application                   = "hmpps-authorization-api"
+  github_team                   = "haha-live"
+  environment                   = var.environment-name
+  selected_branch_patterns      = ["main"]
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
