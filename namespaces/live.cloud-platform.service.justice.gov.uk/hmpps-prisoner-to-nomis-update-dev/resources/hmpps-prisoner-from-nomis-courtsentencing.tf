6004c74e7cfc6c7123ba3b13353c29e3c53213fa
andymarke <andy.marke@digital.justice.gov.uk>
2025-07-03 14:26:45 +0100

    Access court sentencing migration event queue from update service - dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-to-nomis-update-dev/resources/hmpps-prisoner-from-nomis-courtsentencing.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_secret" "prisoner-from-nomis-courtsentencing_secret" {
+  metadata {
+    name      = "prisoner_from_nomis_courtsentencing_queue"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_name = "syscon-devs-${var.environment}-prisoner_from_nomis_courtsentencing_queue"
+  }
+}
+
