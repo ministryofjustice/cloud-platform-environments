0282d01fa24df2af0a204eaf63a3670c8f2e4760
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-07-04 11:12:30 +0100

    HAAR-3945: add GitHub action hmpps-template for token-verification-api prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/token-verification-api-prod/resources/token-verification-api-github.tf
@@ -0,0 +1,16 @@
+module "token_varification_api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "token-verification-api"
+  application                   = "token-verification-api"
+  github_team                   = "haha-live"
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = ["haha-live"]
+}
