b9c22ef76007bd05eeb61446a630e10fffee8208
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-07-04 11:21:58 +0100

    HAAR-3945: update environment variable  token-verification-api-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/token-verification-api-prod/resources/token-verification-api-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/token-verification-api-prod/resources/token-verification-api-github.tf
@@ -3,10 +3,10 @@ module "token_varification_api" {
   github_repo                   = "token-verification-api"
   application                   = "token-verification-api"
   github_team                   = "haha-live"
-  environment                   = var.environment
+  environment                   = var.environment-name
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
-  application_insights_instance = var.environment
+  application_insights_instance = var.environment-name
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
