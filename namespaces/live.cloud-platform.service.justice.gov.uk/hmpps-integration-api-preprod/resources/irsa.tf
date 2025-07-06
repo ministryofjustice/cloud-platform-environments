100be6678446cceecfc2231f710bcf03023ffd0c
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-07-06 16:48:13 +0100

    PI-3182: allow preprod service account access to pnd queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/irsa.tf
@@ -54,7 +54,8 @@ module "hmpps-integration-event-irsa" {
       s3                                              = module.certificate_backup.irsa_policy_arn,
       truststore                                      = module.truststore_s3_bucket.irsa_policy_arn,
       secrets                                         = aws_iam_policy.secrets_manager_access.arn,
-      event_topic                                     = module.hmpps-integration-events.irsa_policy_arn
+      event_topic                                     = module.hmpps-integration-events.irsa_policy_arn,
+      events_pnd_queue                                = module.events_pnd_queue.irsa_policy_arn
     }
   )
 
