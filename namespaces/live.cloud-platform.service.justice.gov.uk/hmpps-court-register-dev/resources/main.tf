6890500f26e8dfd15199e1171a1b2adb74fbb12d
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-07-07 16:55:42 +0100

    Remove reference to hmpps-farsight-offences team from main.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-register-dev/resources/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam = "hmpps-farsight-sentence-offence"
+      GithubTeam = "hmpps-calculate-release-dates-team-devs"
     }
   }
 }
@@ -23,7 +23,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam = "hmpps-farsight-sentence-offence"
+      GithubTeam = "hmpps-calculate-release-dates-team-devs"
     }
   }
 }
