7c70367c03ade8642efc8939384d2ae75409ad8a
Kumar Thangavel <kumar.thangavel@gmail.com>
2025-07-09 13:48:16 +0100

    PR for configuring audit for Refer and Monitor prod (#34630)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-prod/resources/irsa.tf
@@ -2,7 +2,13 @@ locals {
   sns_local = {
     "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08" = "hmpps-domain-events-prod"
   }
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  sqs_queues = {
+    #"Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
+    "Digital-Prison-Services-${var.environment_name}-hmpps_audit_queue" = "hmpps-audit-${var.environment_name}",
+  }
+  # The names of the SNS topics used and the namespace which created them
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
 module "irsa" {
@@ -16,7 +22,8 @@ module "irsa" {
   namespace            = var.namespace
   role_policy_arns = merge(local.sns_policies, {
     s3 = module.interventions_s3_bucket.irsa_policy_arn
-  })
+  },
+  local.sqs_policies)
 
   # Tags
   business_unit          = var.business_unit
@@ -27,7 +34,12 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
-data "aws_ssm_parameter" "irsa_policy_arns" {
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_local
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
