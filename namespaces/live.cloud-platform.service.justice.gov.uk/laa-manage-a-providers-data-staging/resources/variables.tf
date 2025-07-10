038af031d81e209612ba689a4514f722ce323b22
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-07-10 14:29:38 +0100

    Update email

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-staging/resources/variables.tf
@@ -41,7 +41,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "civil-legal-advice@justice.gov.uk"
+  default     = "civil.legal.advice@justice.gov.uk"
 }
 
 variable "is_production" {
