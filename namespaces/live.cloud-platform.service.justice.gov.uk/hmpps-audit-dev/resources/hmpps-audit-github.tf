29b0c814fa09f5349d148a863a41fe8649e25164
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-07-11 13:01:38 +0100

    HAAR-3945: revert github_team in hmpps-template - hmpps-audit-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-audit-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-audit-github.tf
@@ -2,7 +2,7 @@ module "hmpps-audit-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "hmpps-audit-api"
   application                   = "hmpps-audit-api"
-  github_team                   = "haha-audit"
+  github_team                   = var.team_name
   environment                   = var.environment-name
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
