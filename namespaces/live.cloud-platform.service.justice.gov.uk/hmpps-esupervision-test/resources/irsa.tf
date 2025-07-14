dba695267849737ee494ac04525b133b7b246144
Lee Kitching <lee.kitching@justice.gov.uk>
2025-07-14 15:56:28 +0100

    Define s3 bucket and IRSA roles for esupervision test namespace
    Add S3 bucket resource and IRSA configuration for the HMPPS
    esupervision test namespace.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-test/resources/irsa.tf
@@ -0,0 +1,34 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "hmpps-esupervision-api"
+  role_policy_arns = {
+    s3 = module.s3_data_bucket.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+# IRSA role used to authenticate bucket requests
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "hmpps-esupervision-api-irsa"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+  }
+}
+
