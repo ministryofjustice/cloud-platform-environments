611cce80a32dcad9c079839e2156d7dd56a1f05c
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-07-14 11:20:22 +0100

    Changing team from DCES to Crime Apps

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-dces-drc-integration-staging/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "laa-dces-team"
+  default     = "laa-crime-apps-team"
 }
 
 variable "environment" {
@@ -41,7 +41,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "laa-dces@digital.justice.gov.uk"
+  default     = "laa-crime-apps@digital.justice.gov.uk"
 }
 
 variable "is_production" {
