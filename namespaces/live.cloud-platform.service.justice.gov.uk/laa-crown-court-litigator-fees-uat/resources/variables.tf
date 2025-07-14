ee062969fbff1e796f7cbb80856843204641d466
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2025-07-14 16:37:10 +0100

    Confgure cognito for cclf-uat

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-uat/resources/variables.tf
@@ -126,3 +126,36 @@ variable "serviceaccount_rules" {
     },
   ]
 }
+
+variable "user_pool_name" {
+  description = "Cognito user pool name"
+  default     = "cclf-uat-userpool"
+}
+
+variable "cognito_user_pool_client_name" {
+  description = "Cognito user pool client name"
+  default     = "maat-uat"
+}
+
+variable "resource_server_identifier" {
+  description = "Cognito resource server identifier"
+  default     = "cclf-uat"
+}
+
+variable "resource_server_name" {
+  description = "Cognito resource server name"
+  default     = "cclf-uat-resource-server"
+}
+
+variable "resource_server_scope_name" {
+  description = "Resource server scope name"
+  default     = "standard"
+}
+
+variable "resource_server_scope_description" {
+  default = "Standard scope"
+}
+
+variable "cognito_user_pool_domain_name" {
+  default = "cclf-uat"
+}
