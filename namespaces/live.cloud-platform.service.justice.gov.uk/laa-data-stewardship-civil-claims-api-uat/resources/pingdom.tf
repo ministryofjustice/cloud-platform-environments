eee7314594bf0e0a6b46127719627d348e972bd6
Peter Hunt <naseberry@users.noreply.github.com>
2025-07-15 13:50:57 +0100

    DSTEW-103: Add LAA pingdom tags - DSTEW Civil Claims

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-stewardship-civil-claims-api-uat/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-stewardship-civil-claims-api-uat/resources/pingdom.tf
@@ -12,7 +12,7 @@ resource "pingdom_check" "laa-data-stewardship-civil-claims-api-uat" {
   url                      = "/actuator/health"
   encryption               = true
   port                     = 443
-  tags                     = "businessunit_${var.business_unit},application_${var.repo_name},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  tags                     = "businessunit_${var.business_unit},application_${var.repo_name},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name},laa,dstew_payments,laa_production_environment_dashboard"
   probefilters             = "region:EU"
   integrationids           = []
 }
