333c8b20569d2bb7b418e925504a2cb7b7319166
zahid.khan <Zahid.Khan1@justice.gov.uk>
2025-07-15 11:01:03 +0100

    [ESWE-1342] updates to integration events - remove PERSON_CHANGED with PERSON_STATUS_CHANGED

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/event-subscriber-plp.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/event-subscriber-plp.tf
@@ -93,7 +93,7 @@ resource "aws_sns_topic_subscription" "event_plp_subscription" {
       "SAN_REVIEW_SCHEDULE_CHANGED",
       "PRISONER_BASE_LOCATION_CHANGED",
       "PERSON_EDUCATION_ASSESSMENTS_CHANGED",
-      "PRISONER_CHANGED"
+      "PERSON_STATUS_CHANGED"
 
     ]
   })
