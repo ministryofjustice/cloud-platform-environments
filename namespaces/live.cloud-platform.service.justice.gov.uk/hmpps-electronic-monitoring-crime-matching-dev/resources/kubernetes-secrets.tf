f0db32be34adef1ef3871fd70947690005e2a65a
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-07-15 10:46:06 +0100

    Reintroducing ssm references to use newly updated parameter

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/kubernetes-secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/kubernetes-secrets.tf
@@ -9,13 +9,13 @@ resource "kubernetes_secret" "irsa" {
   }
 }
 
-# resource "kubernetes_secret" "athena_roles" {
-#   metadata {
-#     name      = "athena-roles"
-#     namespace = var.namespace
-#   }
-#   type = "Opaque"
-#   data = {
-#     general_role_arn = data.aws_ssm_parameter.athena_general_role_arn.value
-#   }
-# }
+resource "kubernetes_secret" "athena_roles" {
+  metadata {
+    name      = "athena-roles"
+    namespace = var.namespace
+  }
+  type = "Opaque"
+  data = {
+    general_role_arn = data.aws_ssm_parameter.athena_general_role_arn.value
+  }
+}
