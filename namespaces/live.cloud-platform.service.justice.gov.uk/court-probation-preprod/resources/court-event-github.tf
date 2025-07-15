918a0ab9ecbed11139837f690eab0c822bd0b9a3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-07-15 14:20:50 +0100

    PI-3278 move to gha

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/court-event-github.tf
@@ -0,0 +1,31 @@
+module "court-hearing-event" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "court-hearing-event-receiver"
+  application                   = "court-hearing-event-receiver"
+  github_team                   = "probation-integration-team"
+  environment                   = var.environment-name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "crime-portal-gateway" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "crime-portal-gateway"
+  application                   = "crime-portal-gateway"
+  github_team                   = "probation-integration-team"
+  environment                   = var.environment-name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
