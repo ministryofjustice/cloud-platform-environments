83f04feea73237dbffd5456cd87ced548bde0205
Timi Olubusi <timiolubusi@icloud.com>
2025-07-17 11:18:34 +0100

    Creating opensearch alert for bots

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-preprod/resources/opensearch.tf
@@ -0,0 +1,78 @@
+module "opensearch_alert" {
+    source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2" #use the latest version
+
+    secret_name                    = "live-certificated-bailiffs-preprod-a2bfb8b5b867cbf6"
+    secret_key                     = "OPENSEARCH_SLACK_URL"
+    slack_channel_name             = "dts-legacy-apps-system-alerts"
+    slack_channel_name_description = "DTS Legacy Service Pre-Production Alerts"
+    opensearch_alert_name          = "certificated-bailiffs-preprod-bots"
+    opensearch_alert_enabled       = true
+    monitor_period_interval        = "30"
+    monitor_period_unit            = "MINUTES"
+    index                          = ["live_kubernetes_ingress-*"]
+
+    trigger_name                   = "certificated-bailiffs-preprod-bots"
+    serverity                      = "1"
+    query_source                   = "ctx.results[0].hits.total.value > 10"
+    action_name                    = "certificated-bailiffs-preprod-send-alert"
+    slack_message_subject          = "Certificated Bailiffs Pre-prod Bot Alert"
+    slack_message_template         = "Monitor {{ctx.monitor.name}} just entered alert status. Please investigate the issue.\n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}"
+    alert_throttle_enabled         = true
+    throttle_value                 = 60
+    throttle_unit                  = "MINUTES"
+    environment_name               = var.environment
+    alert_query = jsonencode(
+      {
+         "size": 20,
+         "query": {
+            "bool": {
+              "must": [],
+              "filter": [
+                {
+                  "match_all": {}
+                },
+                {
+                  "bool": {
+                    "should": [
+                      {
+                        "match_phrase": {
+                          "log_processed.kubernetes_namespace": "certificated-bailiffs-preprod"
+                        }
+                      }
+                    ],
+                    "minimum_should_match": 1
+                  }
+                },
+                {
+                  "bool": {
+                    "should": [
+                      {
+                        "match_phrase": {
+                          "log_processed.http_user_agent": "bot"
+                        }
+                      }
+                    ],
+                    "minimum_should_match": 1
+                  }
+                },
+                {
+                  "range": {
+                    "@timestamp": {
+                    "from": "{{period_start}}",
+                    "to": "{{period_end}}",
+                    "include_lower": true,
+                    "include_upper": true,
+                    "format": "epoch_millis",
+                    "boost": 1
+                    }
+                  }
+                }
+              ],
+              "should": [],
+              "must_not": []
+            }
+        }
+    }
+    )
+  }
+  
