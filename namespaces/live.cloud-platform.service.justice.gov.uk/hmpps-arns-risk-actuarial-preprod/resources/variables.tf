1a42ca43a43bd414c43913139b5c6b3ea0598b75
riccardo-noviello-moj-2 <Riccardo.Noviello@justice.gov.uk>
2025-07-21 10:38:07 +0100

    ACT-35: create preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-risk-actuarial-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-risk-actuarial-preprod/resources/variables.tf
@@ -27,7 +27,7 @@ variable "team_name" {
 ### Change this environment to the environment name corresponding to this namespace (as per helm/values-ENV.dev) ###
 variable "environment" {
   description = "The type of environment you're deploying to."
-  default     = "dev"
+  default     = "preprod"
 }
 ####################################################################################################################
 
