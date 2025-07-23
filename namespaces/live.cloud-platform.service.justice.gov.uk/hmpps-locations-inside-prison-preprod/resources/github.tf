5d1bce320dbaeb5da90f0b94c4451521e90ba637
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-07-23 16:39:17 +0100

    MAP-2608 Remove CircleCI and GitHub service account configurations, update to use HMPPS template for repository and deployment settings.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-preprod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-preprod/resources/github.tf
@@ -1,3 +1,20 @@
+module "hmpps-locations-inside-prison" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-locations-inside-priso"
+  application                   = "hmpps-locations-inside-priso"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  reviewer_teams                = [var.team_name]
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = [var.reviewer_team_name]
+}
 
 module "hmpps-locations-inside-prison-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
