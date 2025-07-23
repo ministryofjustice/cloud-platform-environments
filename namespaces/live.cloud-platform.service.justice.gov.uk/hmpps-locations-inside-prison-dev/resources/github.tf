5adf84cde44fa7e5f43847028a5eb0b5c95e4713
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-07-23 16:46:03 +0100

    MAP-2608 Remove CircleCI and GitHub service account configurations, replace with new GitHub repository module for HMPPS Locations Inside Prison API.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
@@ -1,3 +1,18 @@
+module "hmpps-locations-inside-prison" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-locations-inside-prison"
+  application                   = "hmpps-locations-inside-prison"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
 
 module "hmpps-locations-inside-prison-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/versions.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "integrations/github"
       version = "~> 6.5.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
