99cb14d501e547eec68d917cf5fdf2f558fcccf6
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-07-23 15:58:28 +0100

    Remove `reviewer_teams` configuration from GitHub repository resource.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-prod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-prod/resources/github.tf
@@ -13,5 +13,4 @@ module "hmpps-locations-inside-prison-api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
-  reviewer_teams                = [var.team_name]
 }
