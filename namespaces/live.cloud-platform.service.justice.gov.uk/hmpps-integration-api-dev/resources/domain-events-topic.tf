b233fda255f417df595b66dc2017e62f82ff18c5
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-24 13:12:05 +0100

    added domain events topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/domain-events-topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/domain-events-topic.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_secret" "hmpps_san_hmpps_domain_events_topic" {
+resource "kubernetes_secret" "hmpps_domain_events_topic" {
   metadata {
     name      = "hmpps-domain-events-topic"
     namespace = var.namespace
