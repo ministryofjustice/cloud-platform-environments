697b6c87d23bb9df26105485bce6b39310007d4a
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-24 13:57:29 +0100

    added domain event topic

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-prod/resources/domain-events-topic.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_secret" "hmpps_domain_events_topic" {
+  metadata {
+    name      = "hmpps-domain-events-topic"
+    namespace = var.namespace
+  }
+
+  data = {
+    topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  }
+}
+
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08"
+}
