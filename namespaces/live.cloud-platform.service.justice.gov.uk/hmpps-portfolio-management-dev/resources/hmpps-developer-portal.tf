255efeefce1d57893ffa1ec8cce7bc3d223d4791
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-07-24 13:15:04 +0100

    Corrected variable name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-developer-portal.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-developer-portal.tf
@@ -93,7 +93,7 @@ module "hmpps_developer_portal" {
 module "hmpps_developer_portal_strapi5" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "hmpps-developer-portal"
-  application                   = "hmpps-developer-portal_strapi5"
+  application                   = "hmpps-developer-portal-strapi5"
   github_team                   = "hmpps-sre"
   environment                   = "stage" # Should match environment name used in helm values file e.g. values-dev.yaml
   selected_branch_patterns      = ["main", "release/*", "feature/*", "bug/*", "**"] # Optional but required if protected_branches_only is false
