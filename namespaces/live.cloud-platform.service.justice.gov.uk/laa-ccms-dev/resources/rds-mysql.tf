6687e98599eada7e154698ae1c2ceefb20fe5d58
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2025-07-24 17:38:26 +0100

    Update rds-mysql.tf (#35089)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-dev/resources/rds-mysql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-dev/resources/rds-mysql.tf
@@ -9,7 +9,7 @@ module "opa_hub_db" {
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
-  enable_rds_auto_start_stop   = true
+  enable_rds_auto_start_stop   = false
 
   # MySQL specifics
   db_engine         = "mysql"
