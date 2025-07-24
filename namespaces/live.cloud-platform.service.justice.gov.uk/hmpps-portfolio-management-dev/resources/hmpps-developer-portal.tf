87fd14c75c2c3d3a4997826dff78c0903a538faa
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-07-24 13:11:03 +0100

    Adding developer portal stage environment

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-developer-portal.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/hmpps-developer-portal.tf
@@ -89,3 +89,19 @@ module "hmpps_developer_portal" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
+
+module "hmpps_developer_portal_strapi5" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-developer-portal"
+  application                   = "hmpps-developer-portal_strapi5"
+  github_team                   = "hmpps-sre"
+  environment                   = "stage" # Should match environment name used in helm values file e.g. values-dev.yaml
+  selected_branch_patterns      = ["main", "release/*", "feature/*", "bug/*", "**"] # Optional but required if protected_branches_only is false
+  protected_branches_only       = false                                    # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
