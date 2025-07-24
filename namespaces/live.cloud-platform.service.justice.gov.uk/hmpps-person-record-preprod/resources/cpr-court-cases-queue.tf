2fed480073e2f4dec276cb8cfb66f783bfb87be1
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-24 15:34:26 +0100

    CPR-802 update queue policy to allow production topic to send data to preproduction queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-court-cases-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-court-cases-queue.tf
@@ -48,7 +48,7 @@ data "aws_iam_policy_document" "cpr_court_cases_sqs_queue_policy_document" {
     condition {
       variable = "aws:SourceArn"
       test     = "ArnEquals"
-      values   = [data.aws_ssm_parameter.court-cases-topic-arn.value]
+      values   = [data.aws_ssm_parameter.prod-court-cases-topic-arn.value]
     }
     resources = ["*"]
   }
