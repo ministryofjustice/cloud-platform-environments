9f9e6aa61e35b194a94f9727cf63cb856a89219e
Denis Davidov <denislav.davivod@justice.gov.uk>
2025-07-25 11:30:41 +0100

    fixing version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-e-surveillance-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-e-surveillance-dev/resources/irsa.tf
@@ -1,5 +1,5 @@
 module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=version" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0" # use the latest release
 
   # EKS configuration
   eks_cluster_name = var.eks_cluster_name
