794a82507f44f46477feb1c32e8e337d9f68dd79
samz-justice <sam.zaroug@justice.gov.uk>
2025-07-28 12:02:47 +0100

    CPR-802 removed hardcoded sns topic in preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-delius-offender-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/cpr-delius-offender-events-queue.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic_subscription" "cpr_delius_probation_domain_events_subscription" {
-  topic_arn = data.aws_sns_topic.hmpps-domain-events.arn
+  topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
   protocol  = "sqs"
   endpoint  = module.cpr_delius_offender_events_queue.sqs_arn
   filter_policy = jsonencode({
@@ -67,7 +67,7 @@ data "aws_iam_policy_document" "cpr_delius_sqs_queue_policy_document" {
     condition {
       variable = "aws:SourceArn"
       test     = "ArnEquals"
-      values   = [data.aws_sns_topic.hmpps-domain-events.arn]
+      values   = [data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value]
     }
     resources = ["*"]
   }
