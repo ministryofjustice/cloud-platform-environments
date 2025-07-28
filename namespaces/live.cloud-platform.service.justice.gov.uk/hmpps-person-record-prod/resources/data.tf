6b15c4d4a7de646c4941e9b0d964b90a913344f1
samz-justice <sam.zaroug@justice.gov.uk>
2025-07-28 11:40:59 +0100

    CPR-802 removed unused secret in prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-prod/resources/data.tf
@@ -14,10 +14,6 @@ data "aws_sns_topic" "probation-offender-events" {
   name = "cloud-platform-Digital-Prison-Services-c2d997878cd24eef94e60f1404977153"
 }
 
-data "aws_ssm_parameter" "large-court-cases-s3-bucket-name" {
-  name = "/court-probation-prod/large-court-cases-s3-bucket-name"
-}
-
 data "aws_ssm_parameter" "large-court-cases-s3-bucket-arn" {
   name = "/court-probation-prod/large-court-cases-s3-bucket-arn"
 }
