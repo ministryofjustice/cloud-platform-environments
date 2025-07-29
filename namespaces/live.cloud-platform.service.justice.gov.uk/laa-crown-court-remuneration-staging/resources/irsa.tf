fff8b61cf4da2bd8d6c4807ac458a4c4f75772d5
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2025-07-29 17:07:36 +0100

    Update terraform-service-pod reference version
    Update terraform-service-pod reference to latest version (1.2.0).

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-remuneration-staging/resources/irsa.tf
@@ -60,7 +60,7 @@ resource "aws_iam_policy" "ccr_stg_policy" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
 
   # Configuration
   namespace            = var.namespace
