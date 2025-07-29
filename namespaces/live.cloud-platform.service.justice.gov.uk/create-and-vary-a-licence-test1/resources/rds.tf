2d0532c7abbaf62947a51e73dce9c41086e86df4
AledWEvans <aled.evans1@justice.gov.uk>
2025-07-29 14:14:35 +0100

    CVSL-2942 test2 Tidy up Upgrade to 17.5

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test1/resources/rds.tf
@@ -13,11 +13,11 @@ module "create_and_vary_a_licence_api_rds" {
   allow_minor_version_upgrade = true
   allow_major_version_upgrade = false
   prepare_for_major_upgrade   = false
+  enable_rds_auto_start_stop  = true
   db_instance_class           = "db.t4g.small"
   db_engine_version           = "17.5"
   rds_family                  = "postgres17"
   db_password_rotated_date    = "14-02-2023"
-  enable_rds_auto_start_stop  = true
 
   providers = {
     aws = aws.london
