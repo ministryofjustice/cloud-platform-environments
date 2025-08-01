0792728f59d5819b7bff7427b3f588f69721eb91
Andy Welsh <andy.welsh@justice.gov.uk>
2025-08-01 17:38:10 +0100

    fix: add regexing

--- namespaces/live.cloud-platform.service.justice.gov.uk/rajinder-pocs/resources/locals.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/rajinder-pocs/resources/locals.tf
@@ -2,9 +2,8 @@ locals {
   sqs_allowed_applications = toset(concat([var.application], var.sqs_queue_subscriber_applications))
   sqs_matching_role_names = [
     for arn in data.aws_iam_roles.sqs_subscriber_roles.arns :
-    replace(arn, "arn:aws:iam::[0-9]+:role/", "")
+    replace(arn, "/^arn:aws:iam::[0-9]+:role\//", "")
   ]
-
   roles_with_app_tag = {
     for name, role in data.aws_iam_role.sqs_matching_roles :
     name => role
