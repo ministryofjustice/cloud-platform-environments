cf6bca4d29d4e4de6325a0659d415f73d1d3a8db
Marcus Aspin <maspin@unilink.com>
2025-08-04 13:28:34 +0100

    PI-3311 Service account for probation search repo (preprod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/probation-search-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/probation-search-github.tf
@@ -15,7 +15,7 @@ module "probation-search-api" {
 
 }
 
-module "hmpps-tier-ui" {
+module "probation-search-ui" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo                   = "probation-search-ui"
   application                   = "probation-search-ui"
@@ -25,6 +25,22 @@ module "hmpps-tier-ui" {
   selected_branch_patterns      = ["main"]
   application_insights_instance = var.environment
   source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "probation-search-services" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-probation-search-services"
+  application                   = "probation-search"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "none"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
