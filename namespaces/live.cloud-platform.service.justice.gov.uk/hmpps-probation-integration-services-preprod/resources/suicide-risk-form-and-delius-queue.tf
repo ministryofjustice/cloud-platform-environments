a7919253abec61e402d0115d8e03675cd24d9b6d
james.macbeth <james.macbeth@justice.gov.uk>
2025-08-05 09:03:53 +0100

    PI-3326 - Bump IRSA module to 2.1.0.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/suicide-risk-form-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/suicide-risk-form-and-delius-queue.tf
@@ -65,7 +65,7 @@ resource "kubernetes_secret" "suicide-risk-form-and-delius-queue-secret" {
 }
 
 module "suicide-risk-form-and-delius-service-account" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
   application            = var.application
   business_unit          = var.business_unit
   eks_cluster_name       = var.eks_cluster_name
