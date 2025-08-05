4ef86de22e78e6b49e14fc6e250aa5d016cec365
devkokov <dm.kokov@gmail.com>
2025-08-05 10:02:57 +0100

    ACE-428 Create a Test environment for the ARNS API

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-test/resources/arns-api.tf
@@ -0,0 +1,14 @@
+module "hmpps_assess_risks_and_needs" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=0.0.7"
+  github_repo                   = "hmpps-assess-risks-and-needs"
+  application                   = "hmpps-assess-risks-and-needs"
+  github_team                   = "hmpps-assessments"
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  selected_branch_patterns      = ["*"]
+  kubernetes_cluster            = var.kubernetes_cluster
+}
