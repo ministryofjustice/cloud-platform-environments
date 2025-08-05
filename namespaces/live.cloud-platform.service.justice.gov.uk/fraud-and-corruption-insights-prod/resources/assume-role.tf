db35fc01390e2163c00ca7e6be70d3aa35ce5a0a
jhenfrancis <jhenielle.francis@justice.gov.uk>
2025-08-05 15:47:36 +0100

    add assume role

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/fraud-and-corruption-insights-prod/resources/assume-role.tf
@@ -0,0 +1,26 @@
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.30.0"
+
+  allow_self_assume_role     = false
+  assume_role_condition_test = "StringEquals"
+  create_role                = true
+  force_detach_policies      = true
+  role_name                  = "${var.namespace}-live"
+  role_policy_arns = {
+    dynamodb                = module.dynamodb-cluster.irsa_policy_arn
+    dynamodb_prod           = module.dynamodb-fci-prod.irsa_policy_arn
+    xaccounts3              = aws_iam_policy.access_ap_s3_policy.arn
+  }
+
+  oidc_providers = {
+    (data.aws_eks_cluster.eks_cluster.name) : {
+      provider_arn               = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")}"
+      namespace_service_accounts = ["${var.namespace}-sa"]
+    }
+  }
+
+  tags = {
+    namespace = var.namespace
+  }
+}
