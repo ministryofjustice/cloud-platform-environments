320ad8cafd271192a9c1e61e1c1979b56099d6d1
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-08-06 14:10:34 +0100

    changes for GHA

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-estate-dev/resources/probation-estate-be.tf
@@ -0,0 +1,14 @@
+module "hmpps_probation_estate_dev" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-probation-estate-api"
+  application                   = "hmpps-probation-estate-api"
+  github_team                   = "manage-a-workforce"
+  environment                   = var.environment
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  selected_branch_patterns      = ["*"]
+  kubernetes_cluster            = var.kubernetes_cluster
+}
