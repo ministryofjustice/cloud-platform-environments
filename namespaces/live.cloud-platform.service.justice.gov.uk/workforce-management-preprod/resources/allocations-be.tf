68caa9e0eee114e86dd76d388905d161ba53b7b5
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-08-06 16:18:55 +0100

    changes for GHA

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/workforce-management-preprod/resources/allocations-be.tf
@@ -0,0 +1,15 @@
+module "hmpps_allocations_preprod" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-allocations"
+  application                   = "hmpps-allocations"
+  github_team                   = "manage-a-workforce"
+  reviewer_teams                = ["manage-a-workforce"]
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  application_insights_instance = "preprod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  selected_branch_patterns      = ["*"]
+  kubernetes_cluster            = var.kubernetes_cluster
+}
