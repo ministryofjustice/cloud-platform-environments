b2e586796e6f65d6ca3b9d3a3475c79c087e129a
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-08-07 09:08:57 +0100

    add github team tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-a-bulk-claim-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-a-bulk-claim-uat/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "laa-submit-a-bulk-claim"
+  default     = "cwa-bulkupload-app-team"
 }
 
 variable "environment" {
