ce82d464e37da240dfc9945b88f8cc8154aa477e
samz-justice <sam.zaroug@justice.gov.uk>
2025-08-07 13:25:08 +0100

    CPR-769 update to use cci actions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/person-record.tf
@@ -0,0 +1,13 @@
+module "hmpps_person_record" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-person-record"
+  application = "hmpps-person-record"
+  github_team = var.team_name
+  environment = "preprodß"
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
