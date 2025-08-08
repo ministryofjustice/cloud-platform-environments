7afde1f8fe799001134d0d805751abb90fbfae00
neil-stephen-mcgonigle <neil.mcgonigle@justice.gov.uk>
2025-08-08 16:35:55 +0100

    CICA: add GitHubTeam name tag to all aws providers

--- namespaces/live.cloud-platform.service.justice.gov.uk/cica-review-case-documents-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-review-case-documents-dev/resources/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = "cica-review-case-documents"
     }
   }
 }
@@ -35,6 +36,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam = "cica-review-case-documents"
     }
   }
 }
