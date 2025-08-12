ff3104a898e8adba781c2a4fc92e143ec18cb56a
miya-pniel-moj <miya.pniel@justice.gov.uk>
2025-08-12 17:04:29 +0100

    pingdom urls added and updated

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/pingdom.tf
@@ -1,10 +1,26 @@
 provider "pingdom" {
 }
 
-resource "pingdom_check" "laa-landing-page-dev" {
+resource "pingdom_check" "laa-landing-page-homepage" {
   type                     = "http"
-  name                     = "laa-landing-page - dev - cloud-platform"
-  host                     = "laa-landing-page-dev.apps.live.cloud-platform.service.justice.gov.uk"
+  name                     = "laa-landing-page-homepage - dev - cloud-platform"
+  host                     = "dev.your-legal-aid-services.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_${var.namespace},isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.namespace}"
+  probefilters             = "region:EU"
+  integrationids           = [142729]
+}
+
+resource "pingdom_check" "manage-users-srv" {
+  type                     = "http"
+  name                     = "manage-users-srv - dev - cloud-platform"
+  host                     = "dev.manage-your-legal-aid-users.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
