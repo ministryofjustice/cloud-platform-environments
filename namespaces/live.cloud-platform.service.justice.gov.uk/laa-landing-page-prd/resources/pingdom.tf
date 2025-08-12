dd97813204ffac0076861c9c5263c23b63ed93b9
miya-pniel-moj <miya.pniel@justice.gov.uk>
2025-08-12 15:56:58 +0100

    adding pingdom checks for both landing pages

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-prd/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-prd/resources/pingdom.tf
@@ -1,10 +1,26 @@
 provider "pingdom" {
 }
 
-resource "pingdom_check" "laa-landing-page-prd" {
+resource "pingdom_check" "laa-landing-page-homepage" {
   type                     = "http"
-  name                     = "laa-landing-page - production - cloud-platform"
-  host                     = "laa-landing-page-prd.apps.live.cloud-platform.service.justice.gov.uk"
+  name                     = "laa-landing-page-homepage - production - cloud-platform"
+  host                     = "your-legal-aid-services.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_${var.namespace},isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.namespace}"
+  probefilters             = "region:EU"
+  integrationids           = [144248]
+}
+
+resource "pingdom_check" "manage-users-srv" {
+  type                     = "http"
+  name                     = "manage-users-srv - production - cloud-platform"
+  host                     = "manage-your-legal-aid-users.service.justice.gov.uk"
   resolution               = 1
   notifywhenbackup         = true
   sendnotificationwhendown = 6
