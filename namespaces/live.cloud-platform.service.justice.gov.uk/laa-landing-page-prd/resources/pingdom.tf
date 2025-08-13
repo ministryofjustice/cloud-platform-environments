cce0ec28002d7cee7bb020c8557c803ab91e916e
miya-pniel-moj <miya.pniel@justice.gov.uk>
2025-08-13 10:57:40 +0100

    removing one of the domains for pingdom

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-prd/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-prd/resources/pingdom.tf
@@ -16,19 +16,3 @@ resource "pingdom_check" "laa-landing-page-homepage" {
   probefilters             = "region:EU"
   integrationids           = [144248]
 }
-
-resource "pingdom_check" "manage-users-srv" {
-  type                     = "http"
-  name                     = "manage-users-srv - production - cloud-platform"
-  host                     = "manage-your-legal-aid-users.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/"
-  encryption               = true
-  port                     = 443
-  tags                     = "businessunit_${var.business_unit},application_${var.namespace},isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.namespace}"
-  probefilters             = "region:EU"
-  integrationids           = [144248]
-}
