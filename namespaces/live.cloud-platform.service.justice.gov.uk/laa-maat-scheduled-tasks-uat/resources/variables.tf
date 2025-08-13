24e4f5d58bd2ef8401557f479361bae9dad6a54a
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2025-08-13 15:54:04 +0100

    added cognito config for maat scheduled tasks uat

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-scheduled-tasks-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-maat-scheduled-tasks-uat/resources/variables.tf
@@ -72,3 +72,41 @@ variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
 }
 
+variable "user_pool_name" {
+  description = "Cognito user pool name"
+  default     = "maat-scheduled-tasks-uat-userpool"
+}
+
+variable "cognito_user_pool_client_name_maat" {
+  description = "Cognito user pool client - MAAT"
+  default     = "maat-uat"
+}
+
+variable "cognito_user_pool_client_name_billing" {
+  description = "Cognito user pool client - Billing"
+  default     = "billing-uat"
+}
+
+variable "resource_server_identifier" {
+  description = "Cognito resource server identifier"
+  default     = "maat-scheduled-tasks-uat"
+}
+
+variable "resource_server_name" {
+  description = "Cognito resource server name"
+  default     = "maat-scheduled-tasks-uat-resource-server"
+}
+
+variable "resource_server_scope_name" {
+  description = "Resource server scope name"
+  default     = "standard"
+}
+
+variable "resource_server_scope_description" {
+  default = "Standard scope"
+}
+
+variable "cognito_user_pool_domain_name" {
+  default = "maat-scheduled-tasks-uat"
+}
+
