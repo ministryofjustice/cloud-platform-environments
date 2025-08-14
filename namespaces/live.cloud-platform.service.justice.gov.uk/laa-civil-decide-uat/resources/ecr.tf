814bc4247f01069a9d79a9f337d87e8b4f498976
Mark-Dodson-MT <Mark.Dodson@madetech.com>
2025-08-14 10:12:01 +0100

    [AD-531] Github renaming corrections

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-uat/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-uat/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["laa-decide"]
+  github_repositories = ["laa-civil-decide"]
 
   # Tags
   business_unit          = var.business_unit
