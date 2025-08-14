c4ae13a4a5d533b7e30d533b638e5002370d8520
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-08-14 16:27:18 +0100

    CPR-826 specify source template repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/person-match-score.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/person-match-score.tf
@@ -1,11 +1,12 @@
 module "hmpps_person_match_score" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
-  github_repo = "hmpps-person-match-score"
-  application = "hmpps-person-match-score"
-  github_team = var.team_name
-  environment = "dev"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-person-match-score"
+  application                   = "hmpps-person-match-score"
+  github_team                   = var.team_name
+  environment                   = "dev"
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "none"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
