09280122450f8a75bff5bdac4d23de8b0ac1de9c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-08-14 12:00:37 +0100

    CPR-777 set correct version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/person-match-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-dev/resources/person-match-irsa.tf
@@ -21,7 +21,7 @@ module "hmpps_person_match_irsa" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
 
   # Configuration
   namespace            = var.namespace
