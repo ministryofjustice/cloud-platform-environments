e52cf0afe22e1168e1c0623a322cf9bd19f37129
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-08-14 15:31:39 +0100

    Fix source and replace queue_name with sqs_name.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/sqs.tf
@@ -1,6 +1,6 @@
 module "sqs_queue" {
-  source                            = "./modules/sqs"
-  queue_name                        = "${var.namespace}-sqs-queue"
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+  sqs_name                        = "${var.namespace}-sqs-queue"
   sqs_queue_subscriber_namespaces = ["laa-data-claims-event-service-prod"]
   business_unit                     = var.business_unit
   application                       = var.application
