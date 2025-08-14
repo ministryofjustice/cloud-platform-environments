4ad39d4aef356cd313a49ccce38a143ed2adf22e
Richard Taylor <33220256+RichT900@users.noreply.github.com>
2025-08-14 15:41:22 +0100

    add module for pre-sentence-service

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/court-event-github.tf
@@ -29,3 +29,19 @@ module "crime-portal-gateway" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "pre-sentence-service" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "pre-sentence-service"
+  application                   = "pre-sentence-service"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = "dev"
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = "dev"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
