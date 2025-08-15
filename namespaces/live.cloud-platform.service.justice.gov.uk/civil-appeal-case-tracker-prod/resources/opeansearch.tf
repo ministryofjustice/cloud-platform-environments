fd03bd736ce228269d628554b4e45600b0949762
Timi Olubusi <timiolubusi@icloud.com>
2025-08-15 14:47:21 +0100

    Adding DoS search

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opeansearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-prod/resources/opeansearch.tf
@@ -75,3 +75,80 @@ module "opensearch_alert" {
     }
     )
   }
+
+    module "opensearch_dos_alert" {
+    source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2" #use the latest version
+
+    secret_name                    = "live-civil-appeal-case-tracker-prod-9b11d3c77b56564c"
+    secret_key                     = "OPENSEARCH_SLACK_URL"
+    slack_channel_name             = "dts-legacy-apps-system-alerts"
+    slack_channel_name_description = "DTS Legacy Services Production Alerts"
+    opensearch_alert_name          = "case-tracker-prod-bots"
+    opensearch_alert_enabled       = true
+    monitor_period_interval        = "10"
+    monitor_period_unit            = "MINUTES"
+    index                          = ["live_kubernetes_ingress-*"]
+    trigger_name                   = "case-tracker-prod-dos"
+    serverity                      = "2"
+    query_source                   = "ctx.results[0].hits.total.value > 50"
+    action_name                    = "case-tracker-prod-send-alert"
+    slack_message_subject          = "Case Tracker Prod Dos Alert"
+    alert_throttle_enabled         = true
+    throttle_value                 = 60
+    throttle_unit                  = "MINUTES"
+    environment_name               = var.environment
+    slack_message_template         = "Monitor {{ctx.monitor.name}} just entered alert status for DoS. Please investigate the issue.\n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}\n- Top offending IPs:\n{{#ctx.results.0.aggregations.top_ips.buckets}}  IP: {{key}} - Count: {{doc_count}}\n{{/ctx.results.0.aggregations.top_ips.buckets}}"
+                                      
+    alert_query = jsonencode(
+      {
+        "size": 20,
+        "query": {
+          "bool": {
+            "must_not": [
+              { "prefix": { "log_processed.request_uri.keyword": "/images" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/js" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/dg_scripts" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/asset" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/favicon.ico" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/css" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/search.jsp" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/search.do" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/getDetail.do" } },
+              { "match": { "log_processed.request_uri.keyword": { "query": "/" } } }
+            ],
+            "filter": [
+              {
+                "range": {
+                  "@timestamp": {
+                    "gte": "now-10m",
+                    "lte": "now"
+                  }
+                }
+              },
+              {
+                  "bool": {
+                    "should": [
+                      {
+                        "match_phrase": {
+                          "log_processed.kubernetes_namespace": "civil-appeal-case-tracker-prod"
+                        }
+                      }
+                    ],
+                    "minimum_should_match": 1
+                  }
+                },
+            ]
+          }
+        },
+        "aggs": {
+          "top_ips": {
+            "terms": {
+              "field": "log_processed.remote_addr.keyword",
+              "size": 10
+            }
+          }
+        }
+      }
+    )
+  }
+  
