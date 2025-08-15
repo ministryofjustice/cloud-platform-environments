7c9fcd3a8e9eab7bdf7eda2e6084386b7c129708
Timi Olubusi <81569910+Tim97eng@users.noreply.github.com>
2025-08-15 14:09:35 +0100

    RST-7477: Adding dos for case tracker in preprod (#35528)
    * Adding dos for case tracker in preprod
    
    * Commit to trigger plan

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-preprod/resources/opeansearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-preprod/resources/opeansearch.tf
@@ -75,3 +75,88 @@ module "opensearch_alert" {
     }
     )
   }
+
+  module "opensearch_dos_alert" {
+    source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2" #use the latest version
+
+    secret_name                    = "live-civil-appeal-case-tracker-preprod-cd48a1d8f6100258"
+    secret_key                     = "OPENSEARCH_SLACK_URL"
+    slack_channel_name             = "dts-legacy-apps-system-alerts"
+    slack_channel_name_description = "DTS Legacy Services Production Alerts"
+    opensearch_alert_name          = "case-tracker-preprod-dos"
+    opensearch_alert_enabled       = true
+    monitor_period_interval        = "10"
+    monitor_period_unit            = "MINUTES"
+    index                          = ["live_kubernetes_ingress-*"]
+    trigger_name                   = "case-tracker-preprod-dos"
+    serverity                      = "2"
+    query_source                   = "ctx.results[0].hits.total.value > 50"
+    action_name                    = "case-tracker-preprod-send-alert"
+    slack_message_subject          = "Case Tracker Pre-prod Dos Alert"
+    alert_throttle_enabled         = true
+    throttle_value                 = 60
+    throttle_unit                  = "MINUTES"
+    environment_name               = var.environment
+    slack_message_template         = "Monitor {{ctx.monitor.name}} just entered alert status for DoS. Please investigate the issue.\n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}\n- Top offending IPs:\n{{#ctx.results[0].aggregations.top_ips.buckets}}  IP: {{key}} - Count: {{doc_count}}\n{{/ctx.results[0].aggregations.top_ips.buckets}}"
+                                      
+    alert_query = jsonencode(
+      {
+        "size": 20,
+        "query": {
+          "bool": {
+            "must_not": [
+              { "prefix": { "log_processed.request_uri.keyword": "/images" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/js" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/dg_scripts" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/asset" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/favicon.ico" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/css" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/search.jsp" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/search.do" } },
+              { "prefix": { "log_processed.request_uri.keyword": "/getDetail.do" } },
+              { "match": { "log_processed.request_uri.keyword": { "query": "/" } } }
+            ],
+            "filter": [
+              {
+                "range": {
+                  "@timestamp": {
+                    "gte": "now-10m",
+                    "lte": "now"
+                  }
+                }
+              },
+              {
+                "bool": {
+                  "should": [
+                    {
+                      "match_phrase": {
+                        "log_processed.kubernetes_namespace": {
+                          "query": "civil-appeal-case-tracker-preprod",
+                          "slop": 0,
+                          "zero_terms_query": "NONE",
+                          "boost": 1
+                        }
+                      }
+                    }
+                  ],
+                  "adjust_pure_negative": true,
+                  "minimum_should_match": "1",
+                  "boost": 1
+                }
+              }
+            ]
+          }
+        },
+        "aggs": {
+          "top_ips": {
+            "terms": {
+              "field": "log_processed.remote_addr.keyword",
+              "size": 10
+            }
+          }
+        }
+      }
+    )
+    
+  }
+  
