f558ff97a46ea6461ac1a7ff296cf00d868995ad
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-08-18 16:57:48 +0100

    LPF-1021 - add s3 bucket to uat

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-uat/resources/s3.tf
@@ -0,0 +1,34 @@
+/*
+ * Make sure that you use the latest version of the module by changing the
+ * `ref=` value in the `source` attribute to the latest version listed on the
+ * releases page of this repository.
+ *
+ */
+module "s3_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
+
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+
+  # Buckets are private by default
+
+  bucket_name = var.file_store_bucket_name
+  versioning = true
+}
+
+resource "kubernetes_secret" "s3_bucket" {
+  metadata {
+    name      = "s3-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    bucket_arn  = module.s3_bucket.bucket_arn
+    bucket_name = module.s3_bucket.bucket_name
+  }
+}
