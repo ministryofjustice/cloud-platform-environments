1e0d1c71721a4af4576ffe4964afaf7d38b7c9ff
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-08-18 11:44:25 +0100

    environment creation

--- namespaces/live.cloud-platform.service.justice.gov.uk/srikanthapp-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srikanthapp-dev/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "new-creation"
+  default     = "prisoner"
 }
 
 variable "namespace" {
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "lss-sre-team"
+  default     = "check-my-diary"
 }
 
 variable "environment" {
