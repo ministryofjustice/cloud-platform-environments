e00dc933a1e82919b3acc46d9930cabbd4e7c407
Richard Taylor <33220256+RichT900@users.noreply.github.com>
2025-08-19 16:06:25 +0100

    added module to prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/court-event-github.tf
@@ -30,3 +30,19 @@ module "crime-portal-gateway" {
   kubernetes_cluster            = var.kubernetes_cluster
   reviewer_teams                = ["probation-integration"]
 }
+
+module "pre-sentence-service" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "pre-sentence-service"
+  application                   = "pre-sentence-service"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
