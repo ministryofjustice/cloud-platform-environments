0e0788144d3f93fc0e9c84f573d7a2cfd26a1d54
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-08-20 15:16:34 +0100

    Create IRSA resource to allow access to the SQS queue. (#35664)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/irsa.tf
@@ -0,0 +1,46 @@
+data "aws_ssm_parameter" "sqs_queue_arn" {
+  name = "/${var.namespace}/sqs-queue-arn"
+}
+
+data "aws_ssm_parameter" "sqs_policy_arn" {
+  name = "/${var.namespace}/sqs-policy-arn"
+}
+
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name = var.eks_cluster_name
+  service_account_name = "irsa-sqs-${var.namespace}"
+  namespace            = var.namespace
+
+  role_policy_arns = {
+    sqs = data.aws_ssm_parameter.sqs_policy_arn.value
+  }
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "${var.namespace}-irsa"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+    rolearn        = module.irsa.role_arn
+  }
+}
+
+resource "kubernetes_secret" "sqs_queue_arn" {
+  metadata {
+    name      = "${var.namespace}-sqs-arn"
+    namespace = var.namespace
+  }
+  data = {
+    arn = data.aws_ssm_parameter.sqs_queue_arn.value
+  }
+}
