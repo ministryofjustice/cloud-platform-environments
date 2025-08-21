c82e6e2aa74663d25f1f1447ac530ab539e1f48c
Mark-Dodson-MT <Mark.Dodson@madetech.com>
2025-08-21 13:21:24 +0100

    [AD-639] Add actions prefix for LAA Civil Decide staging github

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-staging/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-staging/resources/ecr.tf
@@ -11,8 +11,9 @@ module "ecr" {
   repo_name = var.namespace
 
   # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = ["laa-civil-decide"]
+  oidc_providers        = ["github"]
+  github_repositories   = ["laa-civil-decide"]
+  github_actions_prefix = "staging"
 
   # Tags
   business_unit          = var.business_unit
