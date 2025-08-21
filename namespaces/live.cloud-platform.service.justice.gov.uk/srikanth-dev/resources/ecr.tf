08f74bf5354b44503d2908a02b895b4747eceb90
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-08-21 16:36:57 +0100

    git update

--- namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["srikanth-hello-world.git"]
+  github_repositories = ["srikanth-hello-world"]
 
   # Tags
   business_unit          = var.business_unit
