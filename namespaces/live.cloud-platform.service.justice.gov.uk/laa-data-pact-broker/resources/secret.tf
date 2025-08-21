9382d661b76b7a8cc86840cde2838f0e5d14771f
Colm Lynch <colm.lynch@justice.gov.uk>
2025-08-21 16:14:27 +0100

    feat: setup aws secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-pact-broker/resources/secret.tf
@@ -0,0 +1,25 @@
+module "secrets_manager_multiple_secrets" {
+  source                 = "../" # use the latest release
+  // source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=version" # use the latest release
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "pact-broker-basic-auth-username" = {
+      description             = "Username required to authenticate with the Pact Broker.",
+      recovery_window_in_days = 0
+      k8s_secret_name         = "PACT_BROKER_BASIC_AUTH_USERNAME" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "pact-broker-basic-auth-password" = {
+      description             = "Password associated with the username for authentication.",
+      recovery_window_in_days = 0
+      k8s_secret_name         = "PACT_BROKER_BASIC_AUTH_PASSWORD" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+  }
+}
