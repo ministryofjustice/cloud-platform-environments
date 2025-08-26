a7adcc240aedf3ab30dde3e44ee294646f751b64
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-08-26 14:12:32 +0100

    Empty Commit

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-dev/resources/hmpps-auth-github.tf
@@ -0,0 +1,16 @@
+module "hmpps-auth" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-auth"
+  application                   = "hmpps-auth"
+  github_team                   = "haha-live"
+  environment                   = var.environment-name
+  selected_branch_patterns      = ["main"]
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
