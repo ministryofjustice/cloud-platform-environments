e169cbc2fc95bdc3506b06546234553656b0d8ba
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-08-26 16:47:35 +0100

    chore: cleanup unused resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/rds.tf
@@ -1,80 +1,29 @@
 
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
-module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
-
-  # VPC configuration
-  vpc_name = var.vpc_name
-
-  # RDS configuration
-  allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres16"
-  db_instance_class = "db.t4g.micro"
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-}
-
-
-module "rds_2" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
-
-  # VPC configuration
-  vpc_name = var.vpc_name
-
-  # RDS configuration
-  allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres16"
-  db_instance_class = "db.t4g.micro"
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-}
-# To create a read replica, use the below code and update the values to specify the RDS instance
-# from which you are replicating. In this example, we're assuming that rds is the
-# source RDS instance and read-replica is the replica we are creating.
-
-# module "read_replica" {
-#   # default off
-#   count  = 1
+# /*
+#  * Make sure that you use the latest version of the module by changing the
+#  * `ref=` value in the `source` attribute to the latest version listed on the
+#  * releases page of this repository.
+#  *
+#  */
+# module "rds" {
 #   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
 
-#   vpc_name               = var.vpc_name
+#   # VPC configuration
+#   vpc_name = var.vpc_name
+
+#   # RDS configuration
+#   allow_minor_version_upgrade  = true
+#   allow_major_version_upgrade  = false
+#   performance_insights_enabled = false
+#   db_max_allocated_storage     = "500"
+#   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+#   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+
+#   # PostgreSQL specifics
+#   db_engine         = "postgres"
+#   db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+#   rds_family        = "postgres16"
+#   db_instance_class = "db.t4g.micro"
 
 #   # Tags
 #   application            = var.application
@@ -84,154 +33,205 @@ module "rds_2" {
 #   is_production          = var.is_production
 #   namespace              = var.namespace
 #   team_name              = var.team_name
+# }
 
-#   # If any other inputs of the RDS is passed in the source db which are different from defaults,
-#   # add them to the replica
+
+# module "rds_2" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+
+#   # VPC configuration
+#   vpc_name = var.vpc_name
+
+#   # RDS configuration
+#   allow_minor_version_upgrade  = true
+#   allow_major_version_upgrade  = false
+#   performance_insights_enabled = false
+#   db_max_allocated_storage     = "500"
+#   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+#   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
 #   # PostgreSQL specifics
 #   db_engine         = "postgres"
-#   db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+#   db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
 #   rds_family        = "postgres16"
 #   db_instance_class = "db.t4g.micro"
-#   # It is mandatory to set the below values to create read replica instance
 
-#   # Set the db_identifier of the source db
-#   replicate_source_db = module.rds.db_identifier
+#   # Tags
+#   application            = var.application
+#   business_unit          = var.business_unit
+#   environment_name       = var.environment
+#   infrastructure_support = var.infrastructure_support
+#   is_production          = var.is_production
+#   namespace              = var.namespace
+#   team_name              = var.team_name
+# }
+# # To create a read replica, use the below code and update the values to specify the RDS instance
+# # from which you are replicating. In this example, we're assuming that rds is the
+# # source RDS instance and read-replica is the replica we are creating.
 
-#   # Set to true. No backups or snapshots are created for read replica
-#   skip_final_snapshot        = "true"
-#   db_backup_retention_period = 0
+# # module "read_replica" {
+# #   # default off
+# #   count  = 1
+# #   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
 
-#   # If db_parameter is specified in source rds instance, use the same values.
-#   # If not specified you dont need to add any. It will use the default values.
+# #   vpc_name               = var.vpc_name
 
-#   # db_parameter = [
-#   #   {
-#   #     name         = "rds.force_ssl"
-#   #     value        = "0"
-#   #     apply_method = "immediate"
-#   #   }
-#   # ]
+# #   # Tags
+# #   application            = var.application
+# #   business_unit          = var.business_unit
+# #   environment_name       = var.environment
+# #   infrastructure_support = var.infrastructure_support
+# #   is_production          = var.is_production
+# #   namespace              = var.namespace
+# #   team_name              = var.team_name
+
+# #   # If any other inputs of the RDS is passed in the source db which are different from defaults,
+# #   # add them to the replica
+
+# #   # PostgreSQL specifics
+# #   db_engine         = "postgres"
+# #   db_engine_version = "16.7"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+# #   rds_family        = "postgres16"
+# #   db_instance_class = "db.t4g.micro"
+# #   # It is mandatory to set the below values to create read replica instance
+
+# #   # Set the db_identifier of the source db
+# #   replicate_source_db = module.rds.db_identifier
+
+# #   # Set to true. No backups or snapshots are created for read replica
+# #   skip_final_snapshot        = "true"
+# #   db_backup_retention_period = 0
+
+# #   # If db_parameter is specified in source rds instance, use the same values.
+# #   # If not specified you dont need to add any. It will use the default values.
+
+# #   # db_parameter = [
+# #   #   {
+# #   #     name         = "rds.force_ssl"
+# #   #     value        = "0"
+# #   #     apply_method = "immediate"
+# #   #   }
+# #   # ]
+# # }
+
+# module "rds_3" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+
+#   # VPC configuration
+#   vpc_name = var.vpc_name
+
+#   # RDS configuration
+#   allow_minor_version_upgrade  = true
+#   allow_major_version_upgrade  = false
+#   performance_insights_enabled = false
+#   db_max_allocated_storage     = "500"
+#   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+#   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+
+#   # PostgreSQL specifics
+#   db_engine         = "postgres"
+#   db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+#   rds_family        = "postgres16"
+#   db_instance_class = "db.t4g.micro"
+
+#   # Tags
+#   application            = var.application
+#   business_unit          = var.business_unit
+#   environment_name       = var.environment
+#   infrastructure_support = var.infrastructure_support
+#   is_production          = var.is_production
+#   namespace              = var.namespace
+#   team_name              = var.team_name
 # }
 
-module "rds_3" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
-
-  # VPC configuration
-  vpc_name = var.vpc_name
-
-  # RDS configuration
-  allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "16.8"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres16"
-  db_instance_class = "db.t4g.micro"
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-}
-
-resource "kubernetes_secret" "rds" {
-  metadata {
-    name      = "rds-postgresql-instance-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.rds.rds_instance_endpoint
-    database_name         = module.rds.database_name
-    database_username     = module.rds.database_username
-    database_password     = module.rds.database_password
-    rds_instance_address  = module.rds.rds_instance_address
-  }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
-}
-
-resource "kubernetes_secret" "rds_2" {
-  metadata {
-    name      = "rds2-postgresql-instance-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.rds_2.rds_instance_endpoint
-    database_name         = module.rds_2.database_name
-    database_username     = module.rds_2.database_username
-    database_password     = module.rds_2.database_password
-    rds_instance_address  = module.rds_2.rds_instance_address
-  }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
-}
-# resource "kubernetes_secret" "read_replica" {
-#   # default off
-#   count = 0
-
+# resource "kubernetes_secret" "rds" {
 #   metadata {
-#     name      = "rds-postgresql-read-replica-output"
+#     name      = "rds-postgresql-instance-output"
 #     namespace = var.namespace
 #   }
 
-#   # The database_username, database_password, database_name values are same as the source RDS instance.
-#   # Uncomment if count > 0
-
-#   /*
 #   data = {
-#     rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-#     rds_instance_address  = module.read_replica.rds_instance_address
-#     access_key_id         = module.read_replica.access_key_id
-#     secret_access_key     = module.read_replica.secret_access_key
+#     rds_instance_endpoint = module.rds.rds_instance_endpoint
+#     database_name         = module.rds.database_name
+#     database_username     = module.rds.database_username
+#     database_password     = module.rds.database_password
+#     rds_instance_address  = module.rds.rds_instance_address
 #   }
-#   */
+#   /* You can replace all of the above with the following, if you prefer to
+#      * use a single database URL value in your application code:
+#      *
+#      * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
+#      *
+#      */
 # }
 
+# resource "kubernetes_secret" "rds_2" {
+#   metadata {
+#     name      = "rds2-postgresql-instance-output"
+#     namespace = var.namespace
+#   }
 
-# Configmap to store non-sensitive data related to the RDS instance
+#   data = {
+#     rds_instance_endpoint = module.rds_2.rds_instance_endpoint
+#     database_name         = module.rds_2.database_name
+#     database_username     = module.rds_2.database_username
+#     database_password     = module.rds_2.database_password
+#     rds_instance_address  = module.rds_2.rds_instance_address
+#   }
+#   /* You can replace all of the above with the following, if you prefer to
+#      * use a single database URL value in your application code:
+#      *
+#      * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
+#      *
+#      */
+# }
+# # resource "kubernetes_secret" "read_replica" {
+# #   # default off
+# #   count = 0
 
-resource "kubernetes_config_map" "rds" {
-  metadata {
-    name      = "rds-postgresql-instance-output"
-    namespace = var.namespace
-  }
+# #   metadata {
+# #     name      = "rds-postgresql-read-replica-output"
+# #     namespace = var.namespace
+# #   }
 
-  data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
-  }
-}
+# #   # The database_username, database_password, database_name values are same as the source RDS instance.
+# #   # Uncomment if count > 0
 
-resource "kubernetes_config_map" "rds_2" {
-  metadata {
-    name      = "rds2-postgresql-instance-output"
-    namespace = var.namespace
-  }
+# #   /*
+# #   data = {
+# #     rds_instance_endpoint = module.read_replica.rds_instance_endpoint
+# #     rds_instance_address  = module.read_replica.rds_instance_address
+# #     access_key_id         = module.read_replica.access_key_id
+# #     secret_access_key     = module.read_replica.secret_access_key
+# #   }
+# #   */
+# # }
 
-  data = {
-    database_name = module.rds_2.database_name
-    db_identifier = module.rds_2.db_identifier
-  }
-}
+
+# # Configmap to store non-sensitive data related to the RDS instance
+
+# resource "kubernetes_config_map" "rds" {
+#   metadata {
+#     name      = "rds-postgresql-instance-output"
+#     namespace = var.namespace
+#   }
+
+#   data = {
+#     database_name = module.rds.database_name
+#     db_identifier = module.rds.db_identifier
+#   }
+# }
+
+# resource "kubernetes_config_map" "rds_2" {
+#   metadata {
+#     name      = "rds2-postgresql-instance-output"
+#     namespace = var.namespace
+#   }
+
+#   data = {
+#     database_name = module.rds_2.database_name
+#     db_identifier = module.rds_2.db_identifier
+#   }
+# }
 
 
