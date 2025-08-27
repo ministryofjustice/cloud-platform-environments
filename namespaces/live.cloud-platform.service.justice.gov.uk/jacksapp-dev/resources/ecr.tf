aa62244a8b51652856e8cc342de04ec8edb30f90
jackstockley89 <jstock8904@gmail.com>
2025-08-27 15:15:04 +0100

    feat: remove deletion protection

--- namespaces/live.cloud-platform.service.justice.gov.uk/jacksapp-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jacksapp-dev/resources/ecr.tf
@@ -8,7 +8,7 @@ module "ecr_credentials" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.1"
   team_name = var.team_name
   repo_name = "${var.namespace}-ecr"
-
+  deletion_protection = false
   # Uncomment and provide repository names to create github actions secrets
   # containing the ECR name, AWS access key, and AWS secret key, for use in
   # github actions CI/CD pipelines
