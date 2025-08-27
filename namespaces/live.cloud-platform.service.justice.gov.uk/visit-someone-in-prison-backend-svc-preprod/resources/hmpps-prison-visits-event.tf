930b146a3bc6481226c43df6bd89be0bc1096bec
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-08-27 11:50:56 +0100

    Hardcode team names in queues

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/hmpps-prison-visits-event.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/hmpps-prison-visits-event.tf
@@ -50,7 +50,7 @@ module "hmpps_prison_visits_event_queue" {
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
+  team_name              = "book-a-prison-visit" # also used for naming the queue
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
@@ -100,7 +100,7 @@ module "hmpps_prison_visits_event_dead_letter_queue" {
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
+  team_name              = "book-a-prison-visit" # also used for naming the queue
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
