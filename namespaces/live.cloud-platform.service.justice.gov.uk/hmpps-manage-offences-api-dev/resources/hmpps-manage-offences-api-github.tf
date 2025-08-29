eb3a10234adc8c2a8d8b6929032802a495dd828f
dave-virden <151018159+dave-virden@users.noreply.github.com>
2025-08-29 15:27:02 +0100

    MOP-203 Move hmpps-manage-offences to github actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-offences-api-dev/resources/hmpps-manage-offences-api-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-offences-api-dev/resources/hmpps-manage-offences-api-github.tf
@@ -3,10 +3,10 @@ module "hmpps_manage_offences_ui" {
   github_repo = "hmpps-manage-offences"
   application = "hmpps-manage-offences"
   github_team = var.team_name
-  environment_name = var.environment_name
+  environment = var.environment
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
-  application_insights_instance = var.environment_name # Either "dev", "preprod" or "prod"
+  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
@@ -18,10 +18,10 @@ module "hmpps_manage_offences_api" {
   github_repo = "hmpps-manage-offences-api"
   application = "hmpps-manage-offences-api"
   github_team = var.team_name
-  environment_name = var.environment_name
+  environment = var.environment
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
-  application_insights_instance = var.environment_name # Either "dev", "preprod" or "prod"
+  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
