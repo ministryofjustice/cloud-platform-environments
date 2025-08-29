9984a02d0ec94ffa430b09b385b7f465f8c80934
dave-virden <151018159+dave-virden@users.noreply.github.com>
2025-08-29 16:43:41 +0100

    ADJST-1335 Add secrets for github actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/hmpps-identify-remand-periods-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/hmpps-identify-remand-periods-github.tf
@@ -1,22 +1,22 @@
-module "hmpps_manage_offences_ui" {
+module "hmpps_identify_remand_periods_ui" {
   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
-  github_repo = "hmpps-manage-offences"
-  application = "hmpps-manage-offences"
+  github_repo = "hmpps-identify-remand-periods"
+  application = "hmpps-identify-remand-periods"
   github_team = var.team_name
-  environment = var.environment
+  environment = var.environment_name
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
-  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
+  application_insights_instance = var.environment_name # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
 
-module "hmpps_manage_offences_api" {
+module "hmpps_identify_remand_periods_api" {
   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
-  github_repo = "hmpps-manage-offences-api"
-  application = "hmpps-manage-offences-api"
+  github_repo = "hmpps-identify-remand-periods-api"
+  application = "hmpps-identify-remand-periods-api"
   github_team = var.team_name
   environment = var.environment_name
   selected_branch_patterns      = ["main"]
