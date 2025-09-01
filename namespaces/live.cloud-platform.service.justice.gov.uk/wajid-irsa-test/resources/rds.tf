9152a416a70d59a4c485365730b839c3e8ad13ec
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2025-09-01 12:34:01 +0100

    Bumping rds module version for non prod namespaces to remove unused irsa policies

--- namespaces/live.cloud-platform.service.justice.gov.uk/wajid-irsa-test/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/wajid-irsa-test/resources/rds.tf
@@ -1,5 +1,5 @@
 module "rds_mariadb" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=main"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.0.0"
 
   # VPC configuration
   vpc_name = var.vpc_name
