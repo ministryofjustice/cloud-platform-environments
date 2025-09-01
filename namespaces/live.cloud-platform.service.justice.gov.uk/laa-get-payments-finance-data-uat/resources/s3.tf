b8bb500de3ead385e22caf7f2269c131268b05dc
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-09-01 14:44:09 +0100

    LPF-1029 - uat - setup oidc so acceptance test repo can also access, and that we can run ATs on main repo without needing to be inside dev/uat environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-uat/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-uat/resources/s3.tf
@@ -22,8 +22,8 @@ module "s3_bucket" {
 
   # This allows GitHub Actions to access the S3 bucket using OIDC.
   oidc_providers = ["github"]
-  github_repositories = ["payforlegalaid"]
-  github_environments = ["uat"]
+  github_repositories = ["payforlegalaid", "payforlegalaid-tests"]
+  github_environments = ["uat", "acceptance-tests"]
   github_actions_prefix = "FILE_STORE_UAT"
 
 }
