76e14f6d48993f9caa48669ad1ebc8d4f6909978
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-09-01 13:13:53 +0100

    chore: create test case for hmpps template

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/tim-development/resources/hmpps-test.tf
@@ -0,0 +1,15 @@
+module "dev_env" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-template?ref=1.0.0" # use the latest release
+  github_repo                   = "hmpps-template-kotlin"
+  application                   = "hmpps-template-kotlin"
+  github_team                   = "hmpps-sre"
+  environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"]
+  selected_branch_patterns      = ["main", "release/*", "feature/*"]
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
