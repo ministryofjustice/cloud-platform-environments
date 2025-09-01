2b23b6c0a07429a01539476ad6b04e61d1c33d05
cloud-platform-moj <cloudplatform@justiceuk.onmicrosoft.com>
2025-09-01 16:04:13 +0000

    concourse: correcting rds version drift

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dpr-fake-dps-service/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dpr-fake-dps-service/resources/rds-postgresql.tf
@@ -26,7 +26,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine              = "postgres"
-  db_engine_version      = "16.2"
+  db_engine_version      = "16.8"
   rds_family             = "postgres16"
   db_instance_class      = "db.t4g.micro"
   vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
