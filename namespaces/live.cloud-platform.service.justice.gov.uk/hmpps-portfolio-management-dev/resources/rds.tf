f616ad32648e7c2bbf721e2fceb2f3254ec1da23
james Hart <james.hart@digital.justice.gov.uk>
2025-09-02 11:58:41 +0100

    Bump RDS instance to 9.0.0

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-dev/resources/rds.tf
@@ -23,7 +23,7 @@ module "hmpps_service_catalogue" {
 }
 
 module "hmpps_service_catalogue_strapi5" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=8.1.0"
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.0.0"
   db_allocated_storage        = 10
   storage_type                = "gp2"
   vpc_name                    = var.vpc_name
