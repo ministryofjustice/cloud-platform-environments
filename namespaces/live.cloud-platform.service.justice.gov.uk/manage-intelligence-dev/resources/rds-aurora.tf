dba743366f184b52f72d98fb9b784dcc59bbdca2
gareth.davies <gareth.davies@digital.justice.gov.uk>
2025-09-02 15:27:40 +0100

    IMS - Update db parameter group to default name

--- namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/rds-aurora.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-intelligence-dev/resources/rds-aurora.tf
@@ -34,7 +34,7 @@ module "rds_aurora" {
 
 resource "aws_db_parameter_group" "default" {
   name   = module.rds_aurora.db_cluster_identifier
-  family = "aurora-postgresql17"
+  family = "default.aurora-postgresql17"
 
   lifecycle {
     create_before_destroy = true
