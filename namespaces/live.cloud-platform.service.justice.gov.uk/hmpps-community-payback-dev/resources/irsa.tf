d2dc421c4b0a8a59fe00228ac314fb1acd405299
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-09-03 12:00:03 +0100

    Domain evt topic config for community payback dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-payback-dev/resources/irsa.tf
@@ -0,0 +1,27 @@
+locals {
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+module "irsa-api" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0" # use the latest release
+  application            = var.application
+  business_unit          = var.business_unit
+  eks_cluster_name       = var.eks_cluster_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  service_account_name   = "hmpps-community-payback-api"
+  role_policy_arns       = local.sns_policies
+}
