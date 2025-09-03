b35858693435f7e29753e741eef10bb1d302a920
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-09-03 11:40:29 +0100

    Add OpenSearch configuration variables to Terraform

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-preprod/resources/variables.tf
@@ -163,3 +163,28 @@ variable db_parameter {
     }
   ]
 }
+
+variable "opensearch_engine_version" {
+  description = "The OpenSearch engine version to use"
+  type        = string
+  default     = "OpenSearch_2.19"
+}
+
+variable "opensearch_instance_type" {
+  description = "The instance type to use for the OpenSearch cluster"
+  type        = string
+  default     = "t3.medium.search"
+}
+
+variable "opensearch_instance_count" {
+  description = "The number of instances to use for the OpenSearch cluster"
+  type        = number
+  default     = 3
+}
+
+variable "opensearch_ebs_volume_size" {
+  description = "The EBS volume size to use for the OpenSearch cluster"
+  type        = number
+  default     = 50
+  
+}
