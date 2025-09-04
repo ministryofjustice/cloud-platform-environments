4cb023d739520f82bcd1564c6eb39d37a8f6f264
rajindersinghsandhu <102977930+rajindersinghsandhu@users.noreply.github.com>
2025-09-04 19:26:26 +0100

    Override default queue properties

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-dev/resources/modules/sqs/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-dev/resources/modules/sqs/main.tf
@@ -2,6 +2,10 @@ module "queue" {
   source     = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
   sqs_name   = var.queue_name
   fifo_queue = var.fifo_queue
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds = var.message_retention_seconds
+  max_message_size = var.max_message_size
+  delay_seconds = var.delay_seconds
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.dlq.sqs_arn
     maxReceiveCount     = var.dlq_max_receive_count
