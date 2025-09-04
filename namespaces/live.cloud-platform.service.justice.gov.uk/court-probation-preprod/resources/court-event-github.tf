3251e1c0fac25e7bc11c0003b0332120f363fd0d
michael.pitt <michael.pitt1@justice.gov.uk>
2025-09-04 15:54:42 +0100

    add module and dns name on preprod for hmpps-court-case-service-api

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/court-event-github.tf
@@ -29,3 +29,19 @@ module "crime-portal-gateway" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "hmpps-court-case-service-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-court-case-service-api"
+  application                   = "hmpps-court-case-service-api"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = var.environment-name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
