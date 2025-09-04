1e812d98405a750a9a69db4693b6a7d79f437efc
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-09-04 15:08:47 +0100

    MRD-2780 Prep ppud-automation namespace for GHA

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ppud-automation-prod/resources/hmpps-ppud-automation-api.tf
@@ -0,0 +1,15 @@
+module "hmpps-ppud-automation-api" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-ppud-automation-api"
+  application = "hmpps-ppud-automation-api"
+  github_team = var.team_name
+  environment = var.environment
+  reviewer_teams                = [var.team_name]
+  selected_branch_patterns      = ["main"]
+  is_production                 = var.is_production
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ppud-automation-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ppud-automation-prod/resources/variables.tf
@@ -26,7 +26,7 @@ variable "team_name" {
 
 variable "environment" {
   description = "The type of environment you're deploying to."
-  default     = "production"
+  default     = "prod"
 }
 
 variable "infrastructure_support" {
