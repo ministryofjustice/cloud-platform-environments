668c4e32a2b5d75700e73b73bb11b81e1bcae5a9
Peter Hunt <naseberry@users.noreply.github.com>
2025-09-05 15:59:58 +0100

    DSTEW-364: Improve github actions environment secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-access-api-uat/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-access-api-uat/resources/serviceaccount.tf
@@ -1,15 +1,14 @@
 module "serviceaccount" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
-  namespace = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
   serviceaccount_name = var.serviceaccount_name
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
   github_repositories = ["laa-data-access-api"]
-  github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
-  github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
-  github_actions_secret_kube_token     = var.github_actions_secret_kube_token
-  github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
+  github_environments = ["uat"]
 }
