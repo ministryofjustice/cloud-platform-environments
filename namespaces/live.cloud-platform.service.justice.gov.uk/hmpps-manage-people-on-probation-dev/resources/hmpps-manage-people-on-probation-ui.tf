c247ee8eb39e3863f87014fd2b24bd072e052c28
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-09-05 14:44:20 +0100

    add new kotlin service for manage people on probation

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-people-on-probation-dev/resources/hmpps-manage-people-on-probation-ui.tf
@@ -0,0 +1,48 @@
+module "hmpps_template_typescript" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-manage-people-on-probation-ui"
+  application = "hmpps-manage-people-on-probation-ui"
+  github_team = "probation-integration"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
+  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
+  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set FOR PROD
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "elasticache" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.0.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  node_type            = "cache.t4g.small"
+  engine_version       = "7.0"
+  parameter_group_name = "default.redis7"
+
+  enable_irsa = true
+}
+
+resource "kubernetes_secret" "elasticache_redis" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.elasticache.primary_endpoint_address
+    auth_token               = module.elasticache.auth_token
+    member_clusters          = jsonencode(module.elasticache.member_clusters)
+    replication_group_id     = module.elasticache.replication_group_id
+  }
+}
