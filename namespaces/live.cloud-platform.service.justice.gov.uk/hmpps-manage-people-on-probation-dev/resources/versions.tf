b0d4a7cfbda044d99556985cc41acbe473699426
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-09-05 16:36:52 +0100

    update github version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-people-on-probation-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-people-on-probation-dev/resources/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.78.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.6"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
@@ -12,10 +16,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5.1"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.6"
     }
   }
 }
