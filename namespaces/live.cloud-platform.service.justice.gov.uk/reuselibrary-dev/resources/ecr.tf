bd089631d201793fdce6d9ef475e5e715e9f854a
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2025-09-09 19:05:02 +0100

    Updated the github repository to reuse-library and removed the environment to correct the build errors

--- namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/ecr.tf
@@ -2,7 +2,7 @@ module "container_repository" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
 
   # Repo & OIDC
-  repo_name           = var.namespace
+  repo_name           = var.namespace                     # e.g. "reuselibrary-dev"
   oidc_providers      = ["github"]
   github_repositories = [var.namespace]
 
@@ -13,7 +13,6 @@ module "container_repository" {
   is_production          = var.is_production
   team_name              = var.team_name
   namespace              = var.namespace
-  environment_name       = "dev"
   infrastructure_support = var.infrastructure_support
 
   # Optional: allow querying ECR via IRSA from the namespace (read-only)
