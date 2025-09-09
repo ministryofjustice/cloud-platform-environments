e0f0b92013a777e808d7fad12428df3f107e6777
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2025-09-09 17:16:45 +0100

    Pushing 2 alerts for FailedCreatePodSandBox - failed assign IP, and Pull QPS exceeded

--- namespaces/live.cloud-platform.service.justice.gov.uk/monitoring/resources/opensearch-alert.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/monitoring/resources/opensearch-alert.tf
@@ -129,3 +129,142 @@ module "opensearch_alert_error_loading_seccomp" {
     }
   )
 }
+
+module "opensearch_alert_error_qps_exceeded" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2"
+
+  secret_name                    = "live-monitoring-3ca3cc4085f66db3"
+  secret_key                     = "url"
+  slack_channel_name             = "lower-priority-alarms"
+  slack_channel_name_description = "Cloud Platform lower priority alarms channel"
+  opensearch_alert_name          = "Pull QPS exceeded alert"
+  opensearch_alert_enabled       = true
+  monitor_period_interval        = "1"
+  monitor_period_unit            = "MINUTES"
+  index                          = ["live_eventrouter*"]
+
+  trigger_name           = "Pull QPS exceeded trigger"
+  serverity              = 1
+  query_source           = "ctx.results[0].hits.total.value > 0"
+  action_name            = "Pull QPS exceeded action"
+  slack_message_subject  = ":alert: Error: Pull QPS exceeded :alert:"
+  slack_message_template = "Monitor {{ctx.monitor.name}} just entered alert status.\n Investigate logs. \n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}"
+  alert_throttle_enabled = true
+  throttle_value         = 60
+  throttle_unit          = "MINUTES"
+  environment_name       = var.environment
+  alert_query = jsonencode(
+    {
+      "sort": [
+        {
+          "@timestamp": {
+            "order": "desc",
+            "unmapped_type": "boolean"
+          }
+        }
+      ],
+      "size":0,
+      "query": {
+        "bool": {
+          "must": [],
+          "filter": [
+            {
+              "multi_match": {
+                "type": "phrase",
+                "query": "pull QPS exceeded",
+                "lenient": true
+              }
+            },
+            {
+              "range": {
+                "@timestamp": {
+                    "from": "{{period_end}}||-1m",
+                    "to": "{{period_end}}",
+                    "include_lower": true,
+                    "include_upper": true,
+                    "format": "epoch_millis",
+                    "boost": 1
+                }
+              }
+            }
+          ],
+          "should": [],
+          "must_not": []
+        }
+      }
+    }
+  )
+}
+
+module "opensearch_alert_failedcreatepodsandbox_assign_ip" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2"
+
+  secret_name                    = "live-monitoring-3ca3cc4085f66db3"
+  secret_key                     = "url"
+  slack_channel_name             = "lower-priority-alarms"
+  slack_channel_name_description = "Cloud Platform lower priority alarms channel"
+  opensearch_alert_name          = "FailedCreatePodSandBox - failed to assign an IP address to container"
+  opensearch_alert_enabled       = true
+  monitor_period_interval        = "1"
+  monitor_period_unit            = "MINUTES"
+  index                          = ["live_eventrouter*"]
+
+  trigger_name           = "FailedCreatePodSandBox - failed to assign an IP address to container trigger"
+  serverity              = 1
+  query_source           = "ctx.results[0].hits.total.value > 0"
+  action_name            = "FailedCreatePodSandBox - failed to assign an IP address to container action"
+  slack_message_subject  = ":alert: FailedCreatePodSandBox - failed to assign an IP address to container :alert:"
+  slack_message_template = "Monitor {{ctx.monitor.name}} just entered alert status.\n Investigate logs. \n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}"
+  alert_throttle_enabled = true
+  throttle_value         = 60
+  throttle_unit          = "MINUTES"
+  environment_name       = var.environment
+  alert_query = jsonencode(
+    {
+      "sort": [
+        {
+          "@timestamp": {
+            "order": "desc",
+            "unmapped_type": "boolean"
+          }
+        }
+      ],
+      "size":0,
+      "query": {
+        "bool": {
+          "must": [],
+          "filter": [
+            {
+              "multi_match": {
+                "type": "phrase",
+                "query": "FailedCreatePodSandBox",
+                "lenient": true
+              }
+            },
+            {
+              "multi_match": {
+                "type": "phrase",
+                "query": "failed to assign an IP address to container",
+                "lenient": true
+              }
+            },
+            {
+              "range": {
+                "@timestamp": {
+                    "from": "{{period_end}}||-1m",
+                    "to": "{{period_end}}",
+                    "include_lower": true,
+                    "include_upper": true,
+                    "format": "epoch_millis",
+                    "boost": 1
+                }
+              }
+            }
+          ],
+          "should": [],
+          "must_not": []
+        }
+      }
+    }
+  )
+}
