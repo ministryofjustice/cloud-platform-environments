15b637eeedc7cddc7cea1c1ddf0290e74c9fc9c8
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-09-09 09:32:41 +0100

    update service pod version

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-event-service-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-event-service-uat/resources/irsa.tf
@@ -34,7 +34,7 @@ resource "kubernetes_secret" "irsa" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
 
   # Configuration
   namespace            = var.namespace
