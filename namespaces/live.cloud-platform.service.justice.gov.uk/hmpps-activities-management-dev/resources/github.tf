a4cbd5e452c20d6098d4fc8879b68cf1850bde16
munslowl <lance.munslow@digital.justice.gov.uk>
2025-09-10 11:41:53 +0100

    SAA-3606: Use latest template

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/github.tf
@@ -1,5 +1,5 @@
 module "hmpps-activities-management-api" {
-  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo                   = "hmpps-activities-management-api"
   application                   = "hmpps-activities-management-api"
   github_team                   = var.team_name
