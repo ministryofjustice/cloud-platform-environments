15c79311b2eb04a8c5b3f46b9b9708b87e88bb4e
Peter Hunt <naseberry@users.noreply.github.com>
2025-09-10 14:10:44 +0100

    DSTEW-364: Create new namespace laa-data-access-api-staging

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-access-api-staging/resources/serviceaccount.tf
@@ -0,0 +1,14 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  serviceaccount_name = var.serviceaccount_name
+
+  serviceaccount_token_rotated_date = "01-01-2000"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories = ["laa-data-access-api"]
+  github_environments = ["staging"]
+}
