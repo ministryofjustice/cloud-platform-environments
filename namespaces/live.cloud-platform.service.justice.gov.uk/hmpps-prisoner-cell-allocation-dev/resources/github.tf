81c23e043babcc7a4b9e159a882e0fbbccb934d4
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-09-10 16:51:46 +0100

    MAP-2678: add github actions to change somes cell in dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-dev/resources/github.tf
@@ -1,5 +1,5 @@
 module "hmpps-change-someones-cell" {
-  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo                   = "hmpps-change-someones-cell"
   application                   = "hmpps-change-someones-cell"
   github_team                   = var.deployment_team_name
