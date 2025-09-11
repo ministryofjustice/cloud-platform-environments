b7e0f0922687f1a10e339ae505b4b056b903fe17
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-09-11 13:32:21 +0100

    Added CDN and Alias. Alias requires certificate via ACM so tf file added to cover that off as well

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
@@ -0,0 +1,36 @@
+module "cloudfront" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=version" # use the latest release
+
+  # Configuration
+  bucket_id          = module.s3.bucket_name
+  bucket_domain_name = "${module.s3_bucket.bucket_name}.s3.eu-west-2.amazonaws.com"
+  aliases = [var.cloudfront_alias]
+  # SSL certificate for the CloudFront alias.
+  aliases_cert_arn     = aws_acm_certificate.cloudfront_alias_cert.arn
+  # An array of public keys with comments, to be used for CloudFront. Includes an optional entry for an expiring key
+  # !IMPORTANT! This value should NEVER be exactly equal to null. If it is, the CloudFront distribution & S3 bucket will be public.
+  trusted_public_keys  = local.expiring_trusted_key.encoded_key == null ? [local.trusted_key] : [local.trusted_key, local.expiring_trusted_key]
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  depends_on = [aws_acm_certificate.cloudfront_alias_cert]
+}
+
+resource "kubernetes_secret" "cloudfront_url" {
+  metadata {
+    name      = "cloudfront-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    cloudfront_alias       = var.cloudfront_alias
+    cloudfront_url         = module.cloudfront.cloudfront_url
+  }
+}
