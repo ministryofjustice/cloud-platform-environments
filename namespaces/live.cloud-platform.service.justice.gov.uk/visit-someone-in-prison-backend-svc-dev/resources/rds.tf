47d58738c80855cec55640955019307eb81cd9f1
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-09-11 12:52:27 +0100

    Update visit_scheduler_rds instance to 16.8 on visit-someone-in-prison-backend-svc-dev namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-dev/resources/rds.tf
@@ -9,12 +9,12 @@ module "visit_scheduler_rds" {
   infrastructure_support = var.infrastructure_support
   namespace              = var.namespace
 
-  allow_major_version_upgrade = "false"
-  prepare_for_major_upgrade   = false
+  allow_major_version_upgrade = "true"
+  prepare_for_major_upgrade   = true
   db_engine                   = "postgres"
-  db_engine_version           = "15.8"
-  rds_family                  = "postgres15"
-  db_instance_class           = "db.t4g.micro"
+  db_engine_version           = "16.8"
+  rds_family                  = "postgres16"
+  db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "500"
   db_password_rotated_date    = "2023-03-22"
 
