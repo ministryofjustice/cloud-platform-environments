47c95919049c20d5ce1b44baf10613df420ae72f
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-09-12 14:49:58 +0100

    Added prod dns zone

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/route53.tf
@@ -1,34 +1,20 @@
+# Get the kubernetes secret 'route53-zone-output' from hale-platform-prod namespace.
+# This secret contains the zone_id of the hosted zone.
 
-
-resource "aws_route53_zone" "cdn_route53_zone" {
-  name = "dev.websitebuilder.service.justice.gov.uk"
-
-  tags = {
-    business-unit          = var.business_unit
-    application            = var.application
-    is-production          = var.is_production
-    environment-name       = var.environment
-    owner                  = var.team_name
-    infrastructure-support = var.infrastructure_support
-  }
-}
-
-resource "kubernetes_secret" "cdn_route53_zone" {
+data "kubernetes_secret" "route53_zone_output" {
   metadata {
-    name      = "cdn-route53-zone-output"
-    namespace = var.namespace
-  }
-  data = {
-    zone_id = aws_route53_zone.cdn_route53_zone.zone_id
+    name      = "websitebuilder-route53-zone-output"
+    namespace = "hale-platform-prod"
   }
 }
 
 # Create an A record in the hosted zone for the CloudFront alias.
-# Note, the zone_id parameter is set to the zone_id from the hale-dev kubernetes secret.
+# Note, the zone_id parameter is set to the zone_id from the hale-platform-prod kubernetes secret.
 # And the alias.zone_id parameter is set to the CloudFront hosted zone id.
 
+
 resource "aws_route53_record" "data" {
-  zone_id = kubernetes_secret.cdn_route53_zone.data["zone_id"]
+  zone_id = data.kubernetes_secret.route53_zone_output.data["zone_id"]
   name    = var.cloudfront_alias
   type    = "A"
 
@@ -45,7 +31,7 @@ resource "aws_route53_record" "data" {
 resource "aws_route53_record" "cert_validations" {
   count           = length(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options)
 
-  zone_id         = kubernetes_secret.cdn_route53_zone.data["zone_id"]
+  zone_id         = data.kubernetes_secret.route53_zone_output.data["zone_id"]
 
   name            = element(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options[*].resource_record_name, count.index)
   type            = element(aws_acm_certificate.cloudfront_alias_cert.domain_validation_options[*].resource_record_type, count.index)
