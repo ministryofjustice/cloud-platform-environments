89bd9f9fce83238a656a467bff12a8b33c1e9902
srowlands-moj <145464259+srowlands-moj@users.noreply.github.com>
2025-09-12 16:10:03 +0100

    RR-1859 Turn off prepare for upgrad * Turn off the prepare-for-major-upgrade flag

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-support-additional-needs-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-support-additional-needs-dev/resources/rds-postgresql.tf
@@ -17,7 +17,7 @@ module "rds" {
   db_max_allocated_storage     = "500"
   enable_rds_auto_start_stop   = true # Dev database is stopped overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
 
   # PostgreSQL specifics
   db_engine         = "postgres"
