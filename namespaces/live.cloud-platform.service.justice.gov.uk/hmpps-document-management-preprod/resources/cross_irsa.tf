f927ee27674110bb65d68b38b2e4c4d337367fd3
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-09-12 07:59:39 +0100

    Create the s3 sync policy resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-preprod/resources/cross_irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-preprod/resources/cross_irsa.tf
@@ -45,3 +45,17 @@ data "aws_iam_policy_document" "s3_sync_policy" {
     resources = [ "${module.s3.bucket_arn}/*" ]
   }
 }
+
+resource "aws_iam_policy" "s3_sync_policy" {
+  name   = "s3_sync_policy"
+  policy = data.aws_iam_policy_document.s3_sync_policy.json
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
