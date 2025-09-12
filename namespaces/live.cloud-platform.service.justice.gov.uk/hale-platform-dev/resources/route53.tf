556d5241445d3c24456e26cd2afeff7796ec9ec3
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-09-12 13:24:54 +0100

    route53 zone was missing, added it now

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/route53.tf
@@ -1,5 +1,18 @@
 
 
+resource "aws_route53_zone" "cdn_route53_zone" {
+  name = "dev.websitebuilder.service.justice.gov.uk"
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+  }
+}
+
 resource "kubernetes_secret" "cdn_route53_zone" {
   metadata {
     name      = "cdn_route53-zone-output"
