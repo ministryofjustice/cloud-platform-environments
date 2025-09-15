bd9a0893e36fcfaf2f910223d1f35edefcaa06f3
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-09-15 14:17:50 +0100

    dlq policy

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-preprod/resources/irsa.tf
@@ -0,0 +1,45 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "irsa-sqs-${var.namespace}"
+  namespace            = var.namespace
+
+  role_policy_arns = {
+    sqs     = module.sqs.queue_irsa_policy_arn
+    sqs_dlq = module.sqs.dlq_irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "${var.namespace}-irsa"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+    rolearn        = module.irsa.role_arn
+  }
+}
+
+resource "kubernetes_secret" "sqs_queue_arn" {
+  metadata {
+    name      = "${var.namespace}-sqs-arn"
+    namespace = var.namespace
+  }
+  data = {
+    arn = module.sqs.sqs_queue_arn
+  }
+}
