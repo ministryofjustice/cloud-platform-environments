0d33acfae3f1b0852343f3213f4805db170a5d88
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-09-18 12:45:32 +0100

    Removed Strapi4 postgres15 RDS instance

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/rds.tf
@@ -1,27 +1,3 @@
-module "hmpps_service_catalogue" {
-  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.0.0"
-  db_allocated_storage        = 10
-  storage_type                = "gp2"
-  vpc_name                    = var.vpc_name
-  team_name                   = var.team_name
-  business_unit               = var.business_unit
-  application                 = "hmpps-service-catalogue"
-  is_production               = var.is_production
-  namespace                   = var.namespace
-  environment_name            = var.environment
-  infrastructure_support      = var.infrastructure_support
-  allow_major_version_upgrade = "false"
-  db_instance_class           = "db.t4g.micro"
-  db_max_allocated_storage    = "500" # maximum storage for autoscaling
-  db_engine_version           = "15"
-  rds_family                  = "postgres15"
-
-  providers = {
-    aws = aws.london
-  }
-
-}
-
 module "hmpps_service_catalogue_strapi5" {
   source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.0.0"
   db_allocated_storage        = 10
@@ -47,21 +23,6 @@ module "hmpps_service_catalogue_strapi5" {
 }
 
 resource "kubernetes_secret" "hmpps_service_catalogue" {
-  metadata {
-    name      = "rds-instance-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.hmpps_service_catalogue.rds_instance_endpoint
-    database_name         = module.hmpps_service_catalogue.database_name
-    database_username     = module.hmpps_service_catalogue.database_username
-    database_password     = module.hmpps_service_catalogue.database_password
-    rds_instance_address  = module.hmpps_service_catalogue.rds_instance_address
-  }
-}
-
-resource "kubernetes_secret" "hmpps_service_catalogue_strapi5" {
   metadata {
     name      = "rds-instance-output-strapi5-prod"
     namespace = var.namespace
