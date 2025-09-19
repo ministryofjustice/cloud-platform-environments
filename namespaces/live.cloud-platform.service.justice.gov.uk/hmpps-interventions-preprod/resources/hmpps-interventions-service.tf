1f1a741120a74af7d5da2457ecd5d17e42c28319
Davansah <david.ansah@digital.justice.gov.uk>
2025-09-19 14:25:58 +0100

    IPB-1308: Added hmpps template for hmpps-interventions-preprod (#36372)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-interventions-preprod/resources/hmpps-interventions-service.tf
@@ -0,0 +1,14 @@
+module "hmpps_template_kotlin" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-interventions-service"
+  application = "hmpps-interventions-service"
+  github_team = "hmpps-interventions-dev"
+  environment = var.environment
+  selected_branch_patterns      = ["main", "hotfix/*"]
+  is_production                 = var.is_production
+  application_insights_instance = "preprod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
