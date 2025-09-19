2a96b351e35f55cbfc360706957519537c7f1397
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-09-19 14:30:08 +0100

    Update rds-launchpad-auth-postgresql.tf
    feature-set-delete-protection-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-preprod/resources/rds-launchpad-auth-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-preprod/resources/rds-launchpad-auth-postgresql.tf
@@ -16,6 +16,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   prepare_for_major_upgrade  = false
   performance_insights_enabled = true
+  deletion_protection          = true
   #db_max_allocated_storage     = "500"
   enable_rds_auto_start_stop = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
