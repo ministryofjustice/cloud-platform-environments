37c00c2afa1da8d690741db9460e727ad36ad03a
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2025-09-21 21:41:17 +0100

    remove duplicate required_providers block from main.tf to fix Terraform validation error

--- namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/main.tf
@@ -1,15 +1,5 @@
 terraform {
-  backend "s3" {}
-  required_version = ">= 1.2.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.64.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
+  backend "s3" {
   }
 }
 
