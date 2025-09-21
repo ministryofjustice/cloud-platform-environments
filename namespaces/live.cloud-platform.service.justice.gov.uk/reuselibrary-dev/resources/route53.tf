2b427d786ad2edfc869f6e1d90dbba1fab0f2503
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2025-09-21 21:46:44 +0100

    fix route53 zone and kubernetes secret configuration for reuselibrary-dev (RFC1123-compliant secret name)

--- namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/reuselibrary-dev/resources/route53.tf
@@ -15,12 +15,13 @@ resource "aws_route53_zone" "dev_reuselibrary_team_route53_zone" {
 
 resource "kubernetes_secret" "dev_reuselibrary_route53_zone_sec" {
   metadata {
-    name      = "dev_reuselibrary-route53-zone-output"
+    name      = "${var.namespace}-route53-zone-output"
     namespace = var.namespace
   }
 
-  data = {
-    zone_id = aws_route53_zone.dev_reuselibrary_team_route53_zone.zone_id
+  # Use string_data so we can provide plain strings (provider encodes as needed)
+  string_data = {
+    zone_id     = aws_route53_zone.dev_reuselibrary_team_route53_zone.zone_id
     nameservers = join("\n", aws_route53_zone.dev_reuselibrary_team_route53_zone.name_servers)
   }
 }
