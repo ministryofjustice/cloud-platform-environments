5b45495d0b8e740128133e48e0a6e98146c741e5
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-09-22 15:27:03 +0100

    wrong argument used

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
@@ -5,7 +5,7 @@ module "cloudfront" {
   bucket_id          = module.s3_bucket.bucket_name
   bucket_domain_name = "${module.s3_bucket.bucket_name}.s3.eu-west-2.amazonaws.com"
   aliases           = [var.cloudfront_alias]
-  acm_certificate_arn = aws_acm_certificate.cloudfront_alias_cert.arn
+  aliases_cert_arn     = aws_acm_certificate.cloudfront_alias_cert.arn
 
   # Tags
   business_unit          = var.business_unit
