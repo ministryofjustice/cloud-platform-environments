15d68efa87ddb9c7e2d9cc1eed37eba4b9604e5c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-09-22 08:10:01 +0100

    CPR-906 remove subscription to offender events topic

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/data.tf
@@ -6,10 +6,6 @@ data "aws_ssm_parameter" "hmpps-domain-events-topic-arn" {
   name = "/hmpps-domain-events-preprod/topic-arn"
 }
 
-data "aws_sns_topic" "probation-offender-events" {
-  name = "cloud-platform-Digital-Prison-Services-dbe10e8d9c1f4d100f0c723d5d9b754e"
-}
-
 data "aws_ssm_parameter" "prod-large-court-cases-s3-bucket-arn" {
   name = "/court-probation-prod/large-court-cases-s3-bucket-arn"
 }
