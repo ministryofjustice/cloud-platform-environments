6ad0d991b157504ea3b41ffe20a356d4a9785dab
Ryan Kendall <ryankendallt@gmail.com>
2025-09-23 12:04:44 +0100

    Added service_pod to MPC staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-staging/resources/irsa.tf
@@ -29,3 +29,10 @@ module "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arn_sns_domain_events" {
   name = "/hmpps-domain-events-dev/sns/cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573/irsa-policy-arn"
 }
+
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
+
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name
+}
