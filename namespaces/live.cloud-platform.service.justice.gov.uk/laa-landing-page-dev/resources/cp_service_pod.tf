5493e8021d9c995a629d765220dc28bd8f906808
Ben Ashton <ben.ashton@justice.gov.uk>
2025-09-23 10:05:48 +0100

    Change service_pod to latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/cp_service_pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/cp_service_pod.tf
@@ -1,5 +1,5 @@
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=version"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
 
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name
