6d94ec94a4a6a8c295b9588c2b3f4a32d73c86ab
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-09-24 15:50:49 +0100

    DSTEW-463: add security-group.tf to create a security group that allows ingress from reports service security group.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/service-group.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/service-group.tf
@@ -6,7 +6,16 @@ data "aws_vpc" "selected" {
   }
 }
 
-resource "aws_security_group" "source_rds" {
+#Get reporting service's RDS security group
+data "aws_security_group" "reporting_service_rds" {
+  filter {
+    name   = "group-name"
+    values = ["laa-data-claims-reporting-service-uat-rds-sg"]
+  }
+  vpc_id = data.aws_vpc.selected.id
+}
+
+resource "aws_security_group" "rds" {
   name = "${var.namespace}-rds-sg"
   description = "RDS VPC Security Group to allow Reporting Service Ingress Traffic"
   vpc_id      = data.aws_vpc.selected.id
@@ -14,11 +23,13 @@ resource "aws_security_group" "source_rds" {
   lifecycle {
     create_before_destroy = true
   }
+}
 
-  ingress {
-    from_port                = 5432
-    to_port                  = 5432
-    protocol                 = "tcp"
-    source_security_group_id = aws_security_group.laa-data-claims-reporting-service-uat-rds-sg.id
-  }
+resource "aws_security_group_rule" "allow_ingress_from_reporting_service" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = data.aws_security_group.reporting_service_rds.id
 }
