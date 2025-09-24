d85949fd48dd202c617a1cd03f6385d05fa03495
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-09-24 09:06:21 +0100

    MAP-2786: gha config for pre-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-preprod/resources/variables.tf
@@ -35,7 +35,7 @@ variable "environment" {
 variable "deployment_environment" {
   type = string
   description = "Environment code used when deploying, e.g. dev, preprod or prod"
-  default = "prod"
+  default = "preprod"
 }
 
 variable "infrastructure_support" {
