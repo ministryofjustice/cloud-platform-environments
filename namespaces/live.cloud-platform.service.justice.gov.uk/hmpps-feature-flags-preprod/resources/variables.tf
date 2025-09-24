6b7a55d17fff6f858798d04f0a61a9b8376d140b
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-09-24 12:14:15 +0100

    hmpps-feature-flags preprod missed preprod tag

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-feature-flags-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-feature-flags-preprod/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "hmpps-feature-flags-dev"
+  default     = "hmpps-feature-flags-preprod"
 }
 
 variable "business_unit" {
