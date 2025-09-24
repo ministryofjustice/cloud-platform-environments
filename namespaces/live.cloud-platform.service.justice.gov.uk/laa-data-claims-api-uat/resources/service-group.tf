785568d81e2644967ac25a6271c88b892845e7d6
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-09-24 14:47:54 +0100

    DSTEW-463: add security-group.tf to allow ingress from reporting service database.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/service-group.tf
@@ -0,0 +1,24 @@
+# Get VPC id
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name == "live" ? "live-1" : var.vpc_name]
+  }
+}
+
+resource "aws_security_group" "source_rds" {
+  name_prefix = "${var.namespace}-rds-sg"
+  description = "RDS VPC Security Group to allow Reporting Service Ingress Traffic"
+  vpc_id      = data.aws_vpc.selected.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ingress {
+    from_port                = 5432
+    to_port                  = 5432
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.laa-data-claims-reporting-service-uat-rds-sg.id
+  }
+}
