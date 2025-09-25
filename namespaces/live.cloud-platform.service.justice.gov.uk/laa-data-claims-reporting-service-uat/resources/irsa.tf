35041b22fb84c643f70661b914bc4c3a468b3cff
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-09-25 11:59:34 +0100

    remove redundant ref to sqs

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
@@ -4,9 +4,6 @@ module "irsa" {
   service_account_name = "irsa-sqs-${var.namespace}"
   namespace            = var.namespace
 
-  role_policy_arns = {
-    sqs = module.sqs_queue.irsa_policy_arn
-  }
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
