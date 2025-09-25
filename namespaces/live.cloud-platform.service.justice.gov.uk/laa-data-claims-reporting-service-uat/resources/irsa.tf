7d9bc324aa61ad5ae9ad11088be94347b62442c9
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-09-25 16:00:29 +0100

    DSTEW-463: shorten the irsa service account name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
@@ -24,10 +24,3 @@ environment_name       = var.environment
 infrastructure_support = var.infrastructure_support
 }
 
-module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
-
-  # Configuration
-  namespace            = var.namespace
-  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
-}
