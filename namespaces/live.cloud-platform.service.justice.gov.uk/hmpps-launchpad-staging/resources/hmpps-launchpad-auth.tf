f9a70f7e53f63a90a80ae40a5a44ccff15f575e4
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-09-25 13:44:44 +0100

    add settings for github actions ci in launchpad staging

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-staging/resources/hmpps-launchpad-auth.tf
@@ -0,0 +1,16 @@
+module "hmpps_template_kotlin" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-launchpad-auth"
+  application = "hmpps-launchpad-auth"
+  github_team = "hmpps-launchpad"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-launchpad"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main", "**/**", "**"] #["main", "release/*", "feature/*"]  Optional
+  # protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
