974dd97539907ea2ca8262b3eebe6be091eb94fd
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-09-25 15:09:32 +0100

    BAVL-1057 move the BVLS UI prod deployment over to github actions.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-a-video-link-prod/resources/hmpps-book-a-video-link-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-a-video-link-prod/resources/hmpps-book-a-video-link-github.tf
@@ -1,7 +1,24 @@
-module "hmpps-activities-management-api" {
+module "hmpps-book-a-video-link-api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo                   = "hmpps-book-a-video-link-api"
   application                   = "hmpps-book-a-video-link-api"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = [var.team_name]
+}
+
+module "hmpps-book-a-video-link-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-book-a-video-link-ui"
+  application                   = "hmpps-book-a-video-link-ui"
   github_team                   = var.team_name
   environment                   = var.environment
   is_production                 = var.is_production
