123ef642ed5331bb600ce1be0b20b3e8bf6d7806
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-09-25 15:42:55 +0100

    DSTEW-463: add irsa in UAT

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
@@ -0,0 +1,33 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = var.irsa_serviceaccount_name
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the appropriate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = {
+    rds = module.rds.irsa_policy_arn
+  }
+
+# Tags
+business_unit          = var.business_unit
+application            = var.application
+is_production          = var.is_production
+team_name              = var.team_name
+environment_name       = var.environment
+infrastructure_support = var.infrastructure_support
+}
+
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+}
