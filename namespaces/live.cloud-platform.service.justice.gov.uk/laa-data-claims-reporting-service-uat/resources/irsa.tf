c4b487aede4adf6f79c4ce723c94326ee1a1083f
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-09-26 11:40:38 +0100

    merge main changes into feature

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/irsa.tf
@@ -1,9 +1,21 @@
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
-  eks_cluster_name = var.eks_cluster_name
-  service_account_name = "${var.namespace}-${var.environment}"
-  namespace            = var.namespace
 
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = var.irsa_serviceaccount_name
+  namespace            = var.namespace # this is also used as a tag
+
+  # Attach the appropriate policies using a key => value map
+  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
+  # provide an output called `irsa_policy_arn` that can be used.
+  role_policy_arns = {
+    rds = module.rds.irsa_policy_arn
+  }
+
+  # Tags
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
@@ -12,14 +24,3 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
-resource "kubernetes_secret" "irsa" {
-  metadata {
-    name      = "${var.namespace}-irsa"
-    namespace = var.namespace
-  }
-  data = {
-    role           = module.irsa.role_name
-    serviceaccount = module.irsa.service_account.name
-    rolearn        = module.irsa.role_arn
-  }
-}
