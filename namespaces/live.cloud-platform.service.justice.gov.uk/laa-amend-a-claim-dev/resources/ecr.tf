d9bf15d36e1c80b09cc3ec8f445d9624e65db946
suma.challa <suma.challa@justice.gov.uk>
2025-09-26 16:56:53 +0100

    BC-95 generated missing ecr and service account files

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/ecr.tf
@@ -1,11 +1,5 @@
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
 module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=7.1.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
 
   # Repository configuration
   repo_name = var.namespace
@@ -22,9 +16,4 @@ module "ecr" {
   namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-
-  # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for read only queries,
-  # uncomment below:
-
-  # enable_irsa = true
 }
