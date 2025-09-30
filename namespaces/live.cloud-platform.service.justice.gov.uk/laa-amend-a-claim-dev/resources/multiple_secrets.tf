208b0e2788762471ccf1a3383b284f4244a9dd31
suma.challa <suma.challa@justice.gov.uk>
2025-09-30 12:38:50 +0100

    BC-95 Added entra secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/multiple_secrets.tf
@@ -0,0 +1,34 @@
+module "secrets_manager" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "laa-amend-a-claim-azure-client-id-dev" = {
+      description             = "Azure client id",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-azure-client-id" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "laa-amend-a-claim-azure-client-secret-dev" = {
+      description             = "Azure client secret",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-azure-client-secret" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "laa-amend-a-claim-azure-tenant-secret-dev" = {
+      description             = "Azure tenant secret",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-azure-tenant-secret" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },
+    "laa-amend-a-claim-base-url-dev" = {
+      description             = "Dev landing page base url",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-base-url-secret" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    }
+  }
+}
