67f74cf68adcff464d64285967f9235873556d10
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-01 10:42:58 +0100

    chore: create mysql

--- namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.78.0"
+      version = "< 5.0.0"
     }
     github = {
       source  = "integrations/github"
@@ -11,7 +11,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+
     }
   }
 }
