e6bcd52a11f804a4670fd3baeb6035866d0bc7b5
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-10-02 08:58:16 +0100

    RCLL-512 Migrate recalls to github actions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-record-a-recall-dev/resources/hmpps-record-a-recall-github.tf
@@ -0,0 +1,29 @@
+module "hmpps_record_a_recall" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo = "hmpps-record-a-recall"
+  application = "hmpps-record-a-recall"
+  github_team = var.team_name
+  environment = var.environment
+  selected_branch_patterns      = ["main"]
+  is_production                 = var.is_production
+  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "hmpps_remand_and_sentencing_api" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo = "hmpps-remand-and-sentencing-api"
+  application = "hmpps-remand-and-sentencing-api"
+  github_team = var.team_name
+  environment = var.environment
+  selected_branch_patterns      = ["main"]
+  is_production                 = var.is_production
+  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
