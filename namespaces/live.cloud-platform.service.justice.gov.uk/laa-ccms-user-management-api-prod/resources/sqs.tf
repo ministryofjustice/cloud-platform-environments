65d4185c391cf685eac81972df60d312866b43ad
EricH-MOJDigital <eric.heaton@digital.justice.gov.uk>
2025-10-03 15:23:50 +0100

    Update production environment to mirror other environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/sqs.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/sqs.tf
@@ -3,11 +3,18 @@ module "sqs" {
   queue_name                      = "${var.namespace}-sqs-queue"
   sqs_queue_subscriber_namespaces = ["laa-landing-page-prd"]
   fifo_queue                      = true
-  business_unit                   = var.business_unit
-  application                     = var.application
-  is_production                   = var.is_production
-  team_name                       = var.team_name
-  namespace                       = var.namespace
-  environment                     = var.environment
-  infrastructure_support          = var.infrastructure_support
+  dlq_max_receive_count           = 28
+  visibility_timeout_seconds      = 30
+  message_retention_seconds       = 1209600
+  max_message_size                = 262144
+  delay_seconds                   = 1
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment            = var.environment
+  infrastructure_support = var.infrastructure_support
 }
