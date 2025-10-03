e3b41a5c03c40d57a60f4ecdc767ac4e3f92435b
Natalie Rolph <natalie.rolph@justice.gov.uk>
2025-10-03 11:05:36 +0100

    Use the -live github team for pre and prod activities

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-preprod/resources/variables.tf
@@ -19,7 +19,7 @@ variable "business_unit" {
 
 variable "team_name" {
   description = "The name of your development team"
-  default     = "activities-and-appointments"
+  default     = "activities-and-appointments-live"
 }
 
 variable "environment" {
