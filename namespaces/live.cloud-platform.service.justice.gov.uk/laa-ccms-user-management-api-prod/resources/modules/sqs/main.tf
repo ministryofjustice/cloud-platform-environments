f9509a45909eb54799625f1a719005347492b5d6
Eric Heaton <95351145+EricH-MOJDigital@users.noreply.github.com>
2025-10-03 17:14:33 +0100

    Update laa-ccms-user-management-api production environment to mirror other environments (#36873)
    * Update production environment to mirror other environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/modules/sqs/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/modules/sqs/main.tf
@@ -2,10 +2,16 @@ module "queue" {
   source     = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
   sqs_name   = var.queue_name
   fifo_queue = var.fifo_queue
+
+  message_retention_seconds = var.message_retention_seconds
+  max_message_size          = var.max_message_size
+  delay_seconds             = var.delay_seconds
+
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.dlq.sqs_arn
     maxReceiveCount     = var.dlq_max_receive_count
   })
+
   encrypt_sqs_kms = var.encrypted_queue
 
   # Tags
