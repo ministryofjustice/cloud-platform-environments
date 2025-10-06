ad4dbecb827c4b9682cbe9ee0dece48fadafe2df
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-10-06 10:53:19 +0100

    SDIT-3046 Increase queue's max receive count

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-prisoner-from-nomis-visits-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-prisoner-from-nomis-visits-sub-queue.tf
@@ -10,7 +10,7 @@ module "prisoner_from_nomis_visits_queue" {
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.prisoner_from_nomis_visits_dead_letter_queue.sqs_arn
-    maxReceiveCount     = 3
+    maxReceiveCount     = 5
   })
 
   # Tags
