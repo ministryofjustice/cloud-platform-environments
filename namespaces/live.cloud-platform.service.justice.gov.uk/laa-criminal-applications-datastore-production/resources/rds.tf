112bbde9cd3ac2bc6ba6176bc2f194788fd97406
hiboabd <51047911+hiboabd@users.noreply.github.com>
2025-10-07 11:10:29 +0100

    Enable RDS Logging to XSIAM Cortex

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/rds.tf
@@ -35,6 +35,9 @@ module "rds" {
   }
 
   enable_irsa = true
+
+  # Enables Cloudwatch logging for this RDS instance and sends them to Cortex XSIAM
+  opt_in_xsiam_logging = true
 }
 
 resource "kubernetes_secret" "rds" {
