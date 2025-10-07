cf7b0a09d63e14a27a1ae9e95bd039ab39c5c347
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-07 13:28:48 +0100

    chore: changed business unit variable to lowercase

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-litigator-fees-staging/resources/variables.tf
@@ -23,7 +23,7 @@ variable "namespace" {
 variable "business_unit" {
   description = "Area of the MOJ responsible for this service"
   type        = string
-  default     = "LAA"
+  default     = "laa"
 }
 
 variable "team_name" {
