2858fba097415e3e14beedb2616ea83a176d4977
Igor <igor.ushkarev1@justice.gov.uk>
2025-10-07 16:04:28 +0100

    Replace service accounts to swap CircleCI deployment for Github Actions in `hmpps-prisoner-profile-prod`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-profile-prod/resources/variables.tf
@@ -5,7 +5,6 @@ variable "domain" {
 variable "vpc_name" {
 }
 
-
 variable "application" {
   description = "Name of Application you are deploying"
   default     = "hmpps-prisoner-profile"
@@ -52,6 +51,7 @@ variable "slack_channel" {
 variable "number_cache_clusters" {
   default = "2"
 }
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
