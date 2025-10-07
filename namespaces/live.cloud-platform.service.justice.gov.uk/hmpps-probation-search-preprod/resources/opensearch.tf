a5c9fb9c3dec9c3fc9bc46e4baf938ec02e2719c
Marcus Aspin <maspin@unilink.com>
2025-10-07 17:24:05 +0100

    PI-3498 Enable automatic OpenSearch patching (preprod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-search-preprod/resources/opensearch.tf
@@ -12,6 +12,8 @@ module "opensearch" {
   vpc_name               = var.vpc_name
 
   engine_version      = "OpenSearch_3.1"
+  auto_software_update_enabled = true
+
   snapshot_bucket_arn = module.opensearch_snapshot_bucket.bucket_arn
   cluster_config = {
     instance_count           = 6
@@ -23,8 +25,8 @@ module "opensearch" {
   proxy_count = 3
   ebs_options = {
     volume_size = 600 # we can reduce this to 300GB after removing keyword search
-    iops        = 10000
-    throughput  = 1000
+    iops        = 3000
+    throughput  = 250
   }
 }
 
