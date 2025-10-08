1740deec7b09d68e58c8b171bff59984999a5c0c
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2025-10-08 13:58:39 +0100

    Move hot standby db param to replica MPC preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-management-preprod/resources/rds.tf
@@ -57,11 +57,6 @@ module "allocation-rds" {
       name         = "max_slot_wal_keep_size"
       value        = "40000"
       apply_method = "immediate"
-    },
-    {
-      name         = "hot_standby_feedback"
-      value        = "1"
-      apply_method = "immediate"
     }
   ]
 
@@ -161,6 +156,11 @@ module "allocation-rds-read-replica" {
     {
       name         = "max_slot_wal_keep_size"
       value        = "40000"
+      apply_method = "immediate"
+    },
+    {
+      name         = "hot_standby_feedback"
+      value        = "1"
       apply_method = "immediate"
     }
   ]
