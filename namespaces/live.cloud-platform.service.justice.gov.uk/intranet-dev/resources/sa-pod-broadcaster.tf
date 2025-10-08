9a18e505bb78a59f0cee2635b15740dbf8d2f349
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-10-08 12:58:45 +0100

    Update sa-pod-broadcaster.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/sa-pod-broadcaster.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/sa-pod-broadcaster.tf
@@ -1,7 +1,7 @@
 # The Service Account for the pod broadcaster container(s).
 # The broadcaster service will listen for requests and forward them to multiple pods.
 # This is used for clearing nginx's (sidecar container, not ingress) cache that's on multiple pods.
-module "serviceaccount" {
+module "sa_pod_broadcaster" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
 
   namespace          = var.namespace
