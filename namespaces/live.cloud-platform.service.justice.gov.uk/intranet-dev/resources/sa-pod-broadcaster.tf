f7905a8940bf4325950dc0ac8056bf33410c3381
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2025-10-08 13:27:43 +0100

    `intranet-dev` Create a service account for the pod broadcaster container(s). (#36991)
    * `intranet-dev` Create a service account for the pod broadcaster container
    
    * Update sa-pod-broadcaster.tf

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-dev/resources/sa-pod-broadcaster.tf
@@ -0,0 +1,24 @@
+# The Service Account for the pod broadcaster container(s).
+# The broadcaster service will listen for requests and forward them to multiple pods.
+# This is used for clearing nginx's (sidecar container, not ingress) cache that's on multiple pods.
+module "sa_pod_broadcaster" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_rules = [
+    {
+      api_groups = [""]
+      resources = [
+        "deployment",
+        "pods",
+      ]
+      verbs = [
+        "get",
+        "list",
+        "watch",
+      ]
+    },
+  ]
+}
