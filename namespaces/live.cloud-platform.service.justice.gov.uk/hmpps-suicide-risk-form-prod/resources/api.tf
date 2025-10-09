06c6cf7dba75e5e6603745aebd15121d3e42e47f
Marcus Aspin <maspin@unilink.com>
2025-10-09 18:45:26 +0100

    Bump module (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/api.tf
@@ -1,4 +1,5 @@
 module "hmpps_template_kotlin" {
+<<<<<<< SRF-40-infra-prod
   source         = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
   github_repo    = "hmpps-suicide-risk-form-api"
   application    = "hmpps-suicide-risk-form-api"
@@ -8,6 +9,14 @@ module "hmpps_template_kotlin" {
   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
   #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+=======
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-suicide-risk-form-api"
+  application                   = "hmpps-suicide-risk-form-api"
+  github_team                   = "unilink"
+  reviewer_teams                = ["unilink"]
+  environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+>>>>>>> local
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "prod" or "prod"
   source_template_repo          = "hmpps-template-kotlin"
