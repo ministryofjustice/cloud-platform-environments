f4ecf3c448b0995ba3f14ae82df128afd7a71a47
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-10-09 10:19:39 +0100

    Add GHA SA to SLM prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/send-legal-mail-to-prisons-prod/resources/serviceaccount-gha.tf
@@ -0,0 +1,50 @@
+module "send-legal-mail-to-prisons" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "send-legal-mail-to-prisons"
+  application = "send-legal-mail-to-prisons"
+  github_team = "hmpps-send-legal-mail-live"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-send-legal-mail-live"] # Optional team that should review deployments to this environment.
+  # selected_branch_patterns      = ["main", "**/**", "**"] # Optional
+  protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "send-legal-mail-to-prisons-api" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "send-legal-mail-to-prisons-api"
+  application = "send-legal-mail-to-prisons-api"
+  github_team = "hmpps-send-legal-mail-live"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-send-legal-mail-live"] # Optional team that should review deployments to this environment.
+  # selected_branch_patterns      = ["main", "**/**", "**"] # Optional
+  protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "hmpps-send-legal-mail-staff-ui" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-send-legal-mail-staff-ui"
+  application = "hmpps-send-legal-mail-staff-ui"
+  github_team = "hmpps-send-legal-mail-live"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-send-legal-mail-live"] # Optional team that should review deployments to this environment.
+  # selected_branch_patterns      = ["main", "**/**", "**"] # Optional
+  protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
