6e4722da5dfdf66de2f083b79ecd4af143ed13ac
Marcus Aspin <maspin@unilink.com>
2025-10-09 18:45:16 +0100

    Bump module (preprod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-preprod/resources/ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-preprod/resources/ui.tf
@@ -1,5 +1,5 @@
 module "hmpps_template_typescript" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-suicide-risk-form-ui"
   application = "hmpps-suicide-risk-form-ui"
   github_team = "unilink"
