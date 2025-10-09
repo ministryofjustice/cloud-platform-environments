e5444bbd9fcc611d83c230a2d75b9c12fbaf1fed
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-10-09 12:08:21 +0100

    Configure IRSA for Launchpad-preprod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-preprod/resources/irsa.tf
@@ -0,0 +1,22 @@
+module "irsa" {
+  #always replace with latest version from Github
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "${var.team_name}-${var.environment}"
+  namespace            = var.namespace # this is also used as a tag
+  role_policy_arns = {
+    rds = module.rds.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
