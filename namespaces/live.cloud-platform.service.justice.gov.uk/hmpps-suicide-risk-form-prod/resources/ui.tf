743c628d401fbea54ac06553f080f59e284a5fae
Marcus Aspin <maspin@unilink.com>
2025-10-10 09:45:43 +0100

    Update ui.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
@@ -4,7 +4,7 @@ module "hmpps_template_typescript" {
   application                   = "hmpps-suicide-risk-form-ui"
   github_team                   = "unilink"
   reviewer_teams                = ["unilink"]
-  environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  environment                   = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-typescript"
