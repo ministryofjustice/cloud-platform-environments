5e09c197a811fff37978eeca90301e50a5c7104c
Marcus Aspin <maspin@unilink.com>
2025-10-10 09:18:28 +0100

    Fix merge conflict

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
@@ -1,21 +1,10 @@
 module "hmpps_template_typescript" {
-<<<<<<< SRF-40-infra-prod
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
-  github_repo = "hmpps-suicide-risk-form-ui"
-  application = "hmpps-suicide-risk-form-ui"
-  github_team = "unilink"
-  environment = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
-  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
-=======
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo                   = "hmpps-suicide-risk-form-ui"
   application                   = "hmpps-suicide-risk-form-ui"
   github_team                   = "unilink"
   reviewer_teams                = ["unilink"]
   environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
->>>>>>> local
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-typescript"
