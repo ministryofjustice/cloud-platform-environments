2e3476c76e374228d0afedb3376c2f0d29875506
Marcus Aspin <maspin@unilink.com>
2025-10-10 09:41:40 +0100

    SRF-40 Add RDS, SQS, and IRSA for suicide risk form service (preprod) (#37047)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-preprod/resources/api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-preprod/resources/api.tf
@@ -1,9 +1,9 @@
 module "hmpps_template_kotlin" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-suicide-risk-form-api"
   application = "hmpps-suicide-risk-form-api"
   github_team = "unilink"
-  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  environment = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
   #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
