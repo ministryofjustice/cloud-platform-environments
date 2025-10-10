85922ffeec550625bc30772f2d97005946661121
Igor <igor.ushkarev1@justice.gov.uk>
2025-10-10 11:34:53 +0100

    Replace service accounts to swap CircleCI deployment for Github Actions in `hmpps-alerts-dev`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-dev/resources/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam = "connect-dps"
+      GithubTeam = var.team_name
     }
   }
 }
