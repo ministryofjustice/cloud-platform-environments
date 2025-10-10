105c9b6c33f5351b4f1778594675d072c801a601
Igor <igor.ushkarev1@justice.gov.uk>
2025-10-10 11:35:32 +0100

    Replace service accounts to swap CircleCI deployment for Github Actions in `hmpps-alerts-preprod`

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-alerts-preprod/resources/github.tf
@@ -0,0 +1,33 @@
+module "github-automation-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-alerts-api"
+  application                   = "hmpps-alerts-api"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  reviewer_teams                = [var.team_name, var.team_name_replacement]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "github-automation-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-alerts-ui"
+  application                   = "hmpps-alerts-ui"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  reviewer_teams                = [var.team_name, var.team_name_replacement]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
