18783750f5129db8ef1422cf5000a8d9a56df483
Paul Miller <paul.miller@bconline.co.uk>
2025-10-10 14:14:56 +0100

    Update ui.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-prod/resources/ui.tf
@@ -3,7 +3,7 @@ module "hmpps_template_typescript" {
   github_repo                   = "hmpps-suicide-risk-form-ui"
   application                   = "hmpps-suicide-risk-form-ui"
   github_team                   = "unilink"
-  reviewer_teams                = ["unilink-admin"] # Optional team that should review deployments to this environment.
+  reviewer_teams                = ["unilink_admin"] # Optional team that should review deployments to this environment.
   #  environment                   = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
