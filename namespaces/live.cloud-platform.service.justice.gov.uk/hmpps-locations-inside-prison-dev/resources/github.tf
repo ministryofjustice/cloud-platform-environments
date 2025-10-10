c04a0294073ca955bbbf569a1aeebb4f887b30a7
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-10-10 10:18:16 +0100

    MAP-2858: dev. Add non residential locations to the resi namespace and gha details

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
@@ -29,3 +29,19 @@ module "hmpps-locations-inside-prison" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "hmpps-non-residential-locations-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-non-residential-locations-ui"
+  application                   = "hmpps-non-residential-locations-ui"
+  github_team                   = var.deployment_team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
