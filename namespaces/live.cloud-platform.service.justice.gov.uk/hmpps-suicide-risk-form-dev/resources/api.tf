886459a2b9e7a950a470be1715a347947e20c3e5
Paul Miller <pmiller@unilink.com>
2025-10-10 13:57:04 +0100

    Adding Suicide Risk form Environment protection.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-dev/resources/api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-suicide-risk-form-dev/resources/api.tf
@@ -4,9 +4,9 @@ module "hmpps_template_kotlin" {
   application = "hmpps-suicide-risk-form-api"
   github_team = "unilink"
   environment = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
-  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  reviewer_teams                = ["unilink-admin"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main"] # Optional
+  # protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-kotlin"
