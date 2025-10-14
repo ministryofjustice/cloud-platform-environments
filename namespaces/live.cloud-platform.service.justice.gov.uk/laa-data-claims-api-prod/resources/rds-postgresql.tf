b51a62518edfdae860c368e4ca36ca99027b44d0
Peter Hunt <naseberry@users.noreply.github.com>
2025-10-14 16:14:22 +0100

    DSTEW-xx: Enable deletion_protection

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/rds-postgresql.tf
@@ -17,6 +17,7 @@ module "rds" {
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+  deletion_protection          = true
 
   #Enable logical replication to reporting database
   db_parameter = [
