849b0ee08f8d8e720df5343dacef2dd57bdec001
Clement Ibuanokpe <clement.ibuanokpe1@justice.gov.uk>
2025-10-14 12:40:30 +0100

    Add db name for prod.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-court-message-processor-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-court-message-processor-prod/resources/rds-postgresql.tf
@@ -26,7 +26,7 @@ module "rds" {
   rds_family        = "postgres17"
   db_instance_class = "db.t4g.micro"
 
-  db_name = var.namespace
+  rds_name = var.namespace
 
   # Tags
   application            = var.application
