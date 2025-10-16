1b2cbfb50ebaff11df2915e01abdacd46d837efa
Fergal Brennan <fergal.brennan@justice.gov.uk>
2025-10-16 16:10:46 +0100

    rotate db key in dev env

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-dev/resources/rds-postgresql.tf
@@ -16,7 +16,7 @@ module "rds" {
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+  db_password_rotated_date     = "2025-10-16" # Uncomment to rotate your database password.
 
   # PostgreSQL specifics
   db_engine         = "postgres"
