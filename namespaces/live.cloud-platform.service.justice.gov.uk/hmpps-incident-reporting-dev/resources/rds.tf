84c1da45f5993099c5aa03bd1d5f315d58abc6db
Josh Jones <165799926+jonesj2193@users.noreply.github.com>
2025-10-17 14:46:54 +0100

    updated to correct upgrade values across both main and replica dbs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/rds.tf
@@ -18,8 +18,8 @@ module "dps_rds" {
 
   prepare_for_major_upgrade = true
   db_instance_class           = "db.t4g.small"
-  rds_family                  = "postgres16"
-  db_engine_version           = "16"
+  rds_family                  = "postgres17"
+  db_engine_version           = "17.6"
   deletion_protection         = true
   allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
@@ -86,8 +86,8 @@ module "read_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "16"
-  rds_family        = "postgres16"
+  db_engine_version = "17.6"
+  rds_family        = "postgres17"
   db_instance_class = "db.t4g.small"
   # It is mandatory to set the below values to create read replica instance
 
