a9f0842028b11cbb64ce58a9f087b8b2214e7243
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-10-20 15:51:47 +0100

    Update ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-user-management-api-prod/resources/ecr.tf
@@ -1,77 +1,20 @@
-/*
- * Make sure that you use the latest version of the module by changing the
- * `ref=` value in the `source` attribute to the latest version listed on the
- * releases page of this repository.
- *
- */
 module "ecr" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
 
   # Repository configuration
-  team_name = var.team_name
-  repo_name = "${var.namespace}-ecr"
-  namespace = var.namespace
+  repo_name = var.namespace
 
   # OpenID Connect configuration
   oidc_providers        = ["github"]
   github_repositories   = ["laa-ccms-user-management-api"]
-  github_actions_prefix = "prod"
-
-  # OPTIONAL: GitHub environments, to create variables as actions variables in your environments
-  github_environments = ["prod"]
-
-  # Lifecycle policies
-  # Uncomment the below to automatically tidy up old Docker images
-
-  lifecycle_policy = <<EOF
-    {
-      "rules": [
-        {
-          "rulePriority": 1,
-          "description": "Expire untagged images older than 14 days",
-          "selection": {
-            "tagStatus": "untagged",
-            "countType": "sinceImagePushed",
-            "countUnit": "days",
-            "countNumber": 14
-          },
-          "action": {
-            "type": "expire"
-          }
-        },
-        {
-          "rulePriority": 2,
-          "description": "Keep last 30 prod and staging images",
-          "selection": {
-            "tagStatus": "tagged",
-            "tagPrefixList": ["prod"],
-            "countType": "imageCountMoreThan",
-            "countNumber": 30
-          },
-          "action": {
-            "type": "expire"
-          }
-        },
-        {
-          "rulePriority": 3,
-          "description": "Keep the newest 100 images and mark the rest for expiration",
-          "selection": {
-            "tagStatus": "any",
-            "countType": "imageCountMoreThan",
-            "countNumber": 100
-          },
-          "action": {
-            "type": "expire"
-          }
-        }
-      ]
-    }
-    EOF
+  github_actions_prefix = "CCMS-MANAGEMENT-API"
 
   # Tags
   business_unit          = var.business_unit
   application            = var.application
   is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
