2973f43d5d9a482784f0ddc08ae574a615b26b24
Jon Brighton <brightonsbox@hotmail.com>
2025-10-20 16:10:24 +0100

    CDPS-1636: Remove health-and-medication (dev) logical replication

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-dev/resources/rds-postgresql.tf
@@ -41,16 +41,6 @@ module "rds" {
   # Add parameters to enable logical replication
   db_parameter = [
     {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
       name         = "max_wal_size"
       value        = "1024"
       apply_method = "immediate"
