960c64dd8b8471c322ada824b9d7b96147c76c1f
Samia Khan <214997586+samiakhan2@users.noreply.github.com>
2025-10-21 11:08:58 +0100

    Adding Safe 2 Route53 record

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-emergency-containment-platform/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-emergency-containment-platform/resources/route53.tf
@@ -50,3 +50,16 @@ resource "aws_route53_record" "cwa-prod-db3" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "cwa-prod-db2" {
+  depends_on = [aws_route53_zone.aws-prd-legalservices-gov-uk]
+  zone_id = aws_route53_zone.aws-prd-legalservices-gov-uk.zone_id
+  name    = "cwa-prod-db2"
+  type    = "A"
+
+  alias {
+    name                   = "cwa-production-db-nlb-safe2-54d001c6914e7379.elb.eu-west-2.amazonaws.com"
+    zone_id                = "ZD4D7Y8KGAS4G"
+    evaluate_target_health = false
+  }
+}
