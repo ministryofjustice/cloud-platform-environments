b0e34dbf2ca78b221d4315905a38fcc5398ff134
Ben Carter <ben.carter@justice.gov.uk>
2025-10-21 15:54:35 +0100

    Add GitHub dev environment for hmpps-electronic-monitoring-crime-matching-algorithm

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/hmpps-electronic-monitoring-crime-matching-algorithm.tf
@@ -0,0 +1,15 @@
+module "hmpps_electronic_monitoring_crime_matching_algorithm" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-electronic-monitoring-crime-matching-algorithm"
+  application                   = "hmpps-electronic-monitoring-crime-matching-algorithm"
+  github_team                   = "hmpps-em-probation-devs"
+  environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = [] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main"]
+  is_production                 = var.is_production
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
