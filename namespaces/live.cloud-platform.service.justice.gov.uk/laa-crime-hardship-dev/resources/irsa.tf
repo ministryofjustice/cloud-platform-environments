92e599ec54164e799ed7a0830ff7719da1145105
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2025-10-22 12:03:16 +0100

    [LCAM-1838] Remove empty role_policy_arns map

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-hardship-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crime-hardship-dev/resources/irsa.tf
@@ -8,11 +8,6 @@ module "irsa" {
   service_account_name = var.application
   namespace            = var.namespace # this is also used as a tag
 
-  # Attach the approprate policies using a key => value map
-  # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
-  # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = {}
-
   # Tags
   business_unit          = var.business_unit
   application            = var.application
