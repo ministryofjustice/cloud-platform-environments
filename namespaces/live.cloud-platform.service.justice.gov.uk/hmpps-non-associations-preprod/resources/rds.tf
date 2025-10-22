f2a94623fcbaaeab5efccf40d7538c3301734635
Josh Jones <josh.jones4@digital.justice.gov.uk>
2025-10-22 16:55:53 +0100

    Upgrade non-associations preprod db to v17.6

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-preprod/resources/rds.tf
@@ -11,10 +11,10 @@ module "dps_rds" {
 
   enable_rds_auto_start_stop = true
 
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
   db_instance_class           = "db.t4g.small"
-  rds_family                  = "postgres16"
-  db_engine_version           = "16"
+  rds_family                  = "postgres17"
+  db_engine_version           = "17.6"
   deletion_protection         = true
   allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
