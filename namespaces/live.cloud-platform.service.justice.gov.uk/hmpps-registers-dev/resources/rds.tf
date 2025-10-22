e225be7a8e89658088203a8be80b78094bdbefda
Ash <ashley.gyngell@justice.gov.uk>
2025-10-22 13:12:44 +0100

    MAP-2909: hmpps-registers-dev temporarily remove db_parameter for major db upgrade (#37365)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-registers-dev/resources/rds.tf
@@ -25,33 +25,33 @@ module "prisons_rds" {
 
   vpc_security_group_ids       = [data.aws_security_group.mp_dps_sg.id]
 
-  db_parameter = [
-      {
-        name         = "rds.logical_replication"
-        value        = "1"
-        apply_method = "pending-reboot"
-      },
-      {
-        name         = "shared_preload_libraries"
-        value        = "pglogical"
-        apply_method = "pending-reboot"
-      },
-      {
-        name         = "max_wal_size"
-        value        = "1024"
-        apply_method = "immediate"
-      },
-      {
-        name         = "wal_sender_timeout"
-        value        = "0"
-        apply_method = "immediate"
-      },
-      {
-        name         = "max_slot_wal_keep_size"
-        value        = "40000"
-        apply_method = "immediate"
-      }
-    ]
+  # db_parameter = [
+  #     {
+  #       name         = "rds.logical_replication"
+  #       value        = "1"
+  #       apply_method = "pending-reboot"
+  #     },
+  #     {
+  #       name         = "shared_preload_libraries"
+  #       value        = "pglogical"
+  #       apply_method = "pending-reboot"
+  #     },
+  #     {
+  #       name         = "max_wal_size"
+  #       value        = "1024"
+  #       apply_method = "immediate"
+  #     },
+  #     {
+  #       name         = "wal_sender_timeout"
+  #       value        = "0"
+  #       apply_method = "immediate"
+  #     },
+  #     {
+  #       name         = "max_slot_wal_keep_size"
+  #       value        = "40000"
+  #       apply_method = "immediate"
+  #     }
+  #   ]
 }
 
 resource "kubernetes_secret" "prisons_rds" {
