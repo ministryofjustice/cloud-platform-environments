44f199a1160dfc015ae211252cd8072788332e3e
Jon Brighton <brightonsbox@hotmail.com>
2025-10-23 15:48:21 +0100

    Revert "CDPS-1681: Reverting health-and-medication-dev db changes"
    This reverts commit 312cd34a88ed4332b2e70e1d1a91df495633c751.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-dev/resources/rds-postgresql.tf
@@ -11,7 +11,7 @@ module "rds" {
   vpc_name = var.vpc_name
 
   # RDS configuration
-  prepare_for_major_upgrade = false
+  prepare_for_major_upgrade = true
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
@@ -22,8 +22,8 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "16"
-  rds_family        = "postgres16"
+  db_engine_version = "17"
+  rds_family        = "postgres17"
   db_instance_class = "db.t4g.micro"
 
   # Tags
@@ -37,36 +37,6 @@ module "rds" {
 
   # Add DPR security group.
   vpc_security_group_ids       = [data.aws_security_group.mp_dps_sg.id]
-
-  # Add parameters to enable logical replication
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "max_slot_wal_keep_size"
-      value        = "40000"
-      apply_method = "immediate"
-    }
-  ]
-
 
   enable_irsa = true
 }
