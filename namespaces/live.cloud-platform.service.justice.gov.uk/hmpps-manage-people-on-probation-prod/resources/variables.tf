051a3f6fef576fbf24b63b26f50c787f56a491f0
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-10-23 18:00:28 +0100

    revert email change for now

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-people-on-probation-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-people-on-probation-prod/resources/variables.tf
@@ -50,7 +50,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "mpop-digital-team@justice.gov.uk"
+  default     = "probation-integration-team@digital.justice.gov.uk"
 }
 
 variable "environment-name" {
