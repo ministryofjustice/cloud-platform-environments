1a71d741a95f033e0fb21e8f353b51edae1497ee
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-10-24 10:39:17 +0100

    Add policy to demo cd-sa

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-dev/resources/serviceaccount.tf
@@ -50,7 +50,6 @@ module "serviceaccount" {
         "monitoring.coreos.com",
         "rbac.authorization.k8s.io",
         "autoscaling",
-        "policy",
       ]
       resources = [
         "deployments",
