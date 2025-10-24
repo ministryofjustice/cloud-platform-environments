1c24793be29cf3b94e57f268175c90611ef696d1
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-10-24 12:21:40 +0100

    Updated s3 module version

--- namespaces/live.cloud-platform.service.justice.gov.uk/website-builder-assistant-dev/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/website-builder-assistant-dev/resources/opensearch.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.1.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0" # use the latest release
 
   # Tags
   business_unit          = var.business_unit
