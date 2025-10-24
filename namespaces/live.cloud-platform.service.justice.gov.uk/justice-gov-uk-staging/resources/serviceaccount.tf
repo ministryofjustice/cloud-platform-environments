97d9233046640bc17c4a577b1fe382ec3592b380
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2025-10-24 12:18:07 +0100

    justice-gov-uk-staging - Amend service account (#37462)

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/serviceaccount.tf
@@ -50,6 +50,7 @@ module "serviceaccount" {
         "monitoring.coreos.com",
         "rbac.authorization.k8s.io",
         "autoscaling",
+        "policy",
       ]
       resources = [
         "deployments",
