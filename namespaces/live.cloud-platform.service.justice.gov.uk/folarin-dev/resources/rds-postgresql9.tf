bd05d10a6fe2bc9b376fc2b57eaa3a28ec94bc6d
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-10-27 09:23:39 +0000

    fix: bump replica rds module version

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/rds-postgresql9.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/rds-postgresql9.tf
@@ -49,7 +49,7 @@ module "rds" {
 module "read_replica" {
   # default off
   count  = 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
   vpc_name = var.vpc_name
 
