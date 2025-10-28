3c950fbf391a3787e604ed0896fc511d6600b973
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-10-28 13:04:43 +0000

    `intranet-staging` Update RDS to 10.11.10

--- namespaces/live.cloud-platform.service.justice.gov.uk/intranet-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-staging/resources/rds.tf
@@ -12,7 +12,7 @@ module "rds" {
 
   # general options
   db_engine                   = "mariadb"
-  db_engine_version = "10.11.8"
+  db_engine_version           = "10.11.10"
   rds_family                  = "mariadb10.11"
   db_instance_class           = "db.t4g.large"
   environment_name            = var.environment
