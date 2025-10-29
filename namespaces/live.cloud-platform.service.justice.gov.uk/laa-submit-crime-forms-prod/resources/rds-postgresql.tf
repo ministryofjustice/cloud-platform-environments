14f42977f6a3603906dbfa6545e28b8949d5ba50
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-10-29 14:32:17 +0000

    NSCC: Enable XIAM Logging on RDS Instance (Submit Prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-prod/resources/rds-postgresql.tf
@@ -15,6 +15,7 @@ module "rds" {
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
   deletion_protection          = true
+  opt_in_xsiam_logging = true
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
