9f939238bd978b0a27e307366610ba1210826e84
Jon Brighton <brightonsbox@hotmail.com>
2025-10-30 11:36:13 +0000

    CDPS-1636: health-and-medication (prod) removing read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-prod/resources/rds-postgresql.tf
@@ -67,78 +67,7 @@ module "rds" {
     }
   ]
 
-
   enable_irsa = true
-}
-
-module "read_replica" {
-   # default off
-  count  = 1
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
-
-  vpc_name               = var.vpc_name
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
-  # If any other inputs of the RDS is passed in the source db which are different from defaults,
-  # add them to the replica
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "16"
-  rds_family        = "postgres16"
-  db_instance_class = "db.t4g.small"
-  # It is mandatory to set the below values to create read replica instance
-
-  # Set the database_name of the source db
-  db_name = null # "db_name": conflicts with replicate_source_db
-
-  # Set the db_identifier of the source db
-  replicate_source_db = module.rds.db_identifier
-
-  # Set to true. No backups or snapshots are created for read replica
-  skip_final_snapshot        = "true"
-  db_backup_retention_period = 0
-
-  # Add DPR security group.
-  vpc_security_group_ids       = [data.aws_security_group.mp_dps_sg.id]
-
-  # Add parameters to enable logical replication
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "max_slot_wal_keep_size"
-      value        = "40000"
-      apply_method = "immediate"
-    }
-  ]
-
 }
 
 resource "kubernetes_secret" "rds" {
@@ -154,24 +83,6 @@ resource "kubernetes_secret" "rds" {
     database_password     = module.rds.database_password
     rds_instance_address  = module.rds.rds_instance_address
     url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-  }
-}
-
-resource "kubernetes_secret" "read_replica" {
-  # default off
-  count = 1
-
-  metadata {
-    name      = "rds-postgresql-instance-read-replica-output"
-    namespace = var.namespace
-  }
-
-  # The database_username, database_password, database_name values are same as the source RDS instance.
-  # Uncomment if count > 0
-
-  data = {
-    rds_instance_endpoint = module.read_replica[0].rds_instance_endpoint
-    rds_instance_address  = module.read_replica[0].rds_instance_address
   }
 }
 
