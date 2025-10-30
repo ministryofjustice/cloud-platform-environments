5da2f7b32776f3f3a2b85f781375b52042e09f4d
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-10-30 11:23:31 +0000

    MAP-2960: add ui and api github config and certificates for Cell Sharing Risk Assessment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cell-sharing-risk-assessment-dev/resources/github.tf
@@ -0,0 +1,31 @@
+module "hmpps-cell-sharing-risk-assessment-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-cell-sharing-risk-assessment-api"
+  application                   = "hmpps-cell-sharing-risk-assessment-api"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "hmpps-cell-sharing-risk-assessment-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-cell-sharing-risk-assessment-ui"
+  application                   = "hmpps-cell-sharing-risk-assessment-ui"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
