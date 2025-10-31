e53dcb10fb022ebc2a6ed92c1e90dce923145978
Rick Choi <rick.choi@justice.gov.uk>
2025-10-31 10:56:30 +0000

    [ESWE-1289] Configure RDS for DPR on `hmpps-jobs-board-prod` (#37603)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-prod/resources/variables.tf
@@ -81,3 +81,9 @@ variable "number_cache_clusters" {
 variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
 }
+
+variable "mp_dps_sg_name" {
+  type        = string
+  description = "Required for MP DPR Traffic ingress into CP DPS"
+  default     = "cloudplatform-mp-dps-sg"
+}
