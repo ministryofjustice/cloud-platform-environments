4dfbc77a5e59887278852587c3849437850de450
rickchoijd <rick.choi@justice.gov.uk>
2025-10-31 12:29:46 +0000

    [ESWE-1507] Service pod in `hmpps-jobs-board-prod`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-prod/resources/rds-postgresql.tf
@@ -17,6 +17,7 @@ module "rds" {
   performance_insights_enabled = false
   db_max_allocated_storage     = "10000"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  enable_irsa                  = true
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
   deletion_protection          = true
 
