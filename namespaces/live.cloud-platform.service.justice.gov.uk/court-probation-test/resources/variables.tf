0f3192dd46d748ccf3c59fc4b011b6af5c2d9cd6
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-11-03 12:21:26 +0000

    change underscore to dash

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "Probation_in_Court"
+  default     = "Probation-in-Court"
 }
 
 variable "namespace" {
