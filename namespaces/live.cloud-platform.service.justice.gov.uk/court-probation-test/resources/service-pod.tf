1ab5312e7f9fc7c253d6443c14910ebcfd9a8049
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-11-03 12:44:35 +0000

    change underscore to dash

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/service-pod.tf
@@ -5,7 +5,7 @@ module "court-facing-api-service_pod" {
   service_account_name = module.court-facing-api-irsa.service_account.name
 }
 
-module "court-case-service_pod" {
+module "court-case-service-pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
 
   namespace            = var.namespace
