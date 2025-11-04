a341ff6af744df07dcfe88529e92a04daf42393e
james Hart <james.hart@digital.justice.gov.uk>
2025-11-04 16:45:35 +0000

    move to template 1.1.0 all over

--- namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/hmpps-james-bootstrap.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/hmpps-james-bootstrap.tf
@@ -1,5 +1,5 @@
 module "hmpps_james_bootstrap" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-james-bootstrap"
   application = "hmpps-james-bootstrap"
   github_team = "hmpps-sre"
