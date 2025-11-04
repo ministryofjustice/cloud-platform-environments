60216cd4960a9033b49d02a7f018989f4ae44232
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-11-04 15:45:27 +0000

    `justice-gov-uk-staging` Create CloudFront alias

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/cloudfront.tf
@@ -4,8 +4,10 @@ module "cloudfront" {
   # Configuration
   bucket_id          = module.s3_bucket.bucket_name
   bucket_domain_name = "${module.s3_bucket.bucket_name}.s3.eu-west-2.amazonaws.com"
-
-  # Can we get a custom domain for the CDN?
+  # The cloudfront module accepts a list of aliases, but we only need one.
+  aliases              = [var.cloudfront_alias]
+  # SSL certificate for the CloudFront alias.
+  aliases_cert_arn     = aws_acm_certificate.cloudfront_alias_cert.arn
 
   # Tags
   business_unit          = var.business_unit
@@ -15,7 +17,17 @@ module "cloudfront" {
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+
+  depends_on = [aws_acm_certificate.cloudfront_alias_cert]
 }
+
+data "kubernetes_secret" "cloudfront_input_secret" {
+  metadata {
+    name      = "cloudfront-input"
+    namespace = var.namespace
+  }
+}
+
 
 resource "kubernetes_secret" "cloudfront_url" {
   metadata {
@@ -24,6 +36,7 @@ resource "kubernetes_secret" "cloudfront_url" {
   }
 
   data = {
-    cloudfront_url = module.cloudfront.cloudfront_url
+    cloudfront_alias = var.cloudfront_alias
+    cloudfront_url   = module.cloudfront.cloudfront_url
   }
 }
