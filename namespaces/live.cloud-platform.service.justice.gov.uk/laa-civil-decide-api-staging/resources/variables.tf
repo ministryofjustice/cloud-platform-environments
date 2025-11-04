4f58d88ca8d743890e877a9ad14ee1317dac8271
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-11-04 16:45:55 +0000

    add eks_cluster_name to uat vars

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-api-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-api-staging/resources/variables.tf
@@ -38,11 +38,6 @@ variable "environment" {
   default     = "staging"
 }
 
-variable "eks_cluster_name" {
-  description = "The name of the eks cluster to use, a secret prefix"
-  type        = string
-}
-
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
