00969639ca8237c0f2f52e256815225c264aad7a
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-11-04 15:52:34 +0000

    Update cloudfront.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-staging/resources/cloudfront.tf
@@ -1,5 +1,5 @@
 module "cloudfront" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.3.1"
 
   # Configuration
   bucket_id          = module.s3_bucket.bucket_name
