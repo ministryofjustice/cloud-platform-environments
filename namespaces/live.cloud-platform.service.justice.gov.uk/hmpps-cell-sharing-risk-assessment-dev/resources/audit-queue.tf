e8f233ed95d13a2ad6be27c45d3c253e5f0fe399
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-11-04 17:23:34 +0000

    MAP-2959: audit queue

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cell-sharing-risk-assessment-dev/resources/audit-queue.tf
@@ -0,0 +1,14 @@
+locals {
+  audit_queue_name = "Digital-Prison-Services-${var.deployment_environment}-hmpps_audit_queue"
+}
+
+resource "kubernetes_secret" "hmpps_audit_config" {
+  metadata {
+    namespace = var.namespace
+    name      = "hmpps-audit"
+  }
+  data = {
+    sqs_queue_url  = "https://sqs.eu-west-2.amazonaws.com/754256621582/${local.audit_queue_name}"
+    sqs_queue_name = local.audit_queue_name
+  }
+}
