c4f52d1957dcfaa487973b3c5365a8977097c75a
adam <brown-a2@users.noreply.github.com>
2025-11-05 12:20:29 +0000

    Update the module version

--- namespaces/live.cloud-platform.service.justice.gov.uk/website-builder-assistant-dev/resources/servicepod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/website-builder-assistant-dev/resources/servicepod.tf
@@ -1,6 +1,6 @@
 # set up the service pod
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
 
   # Configuration
   namespace            = var.namespace
