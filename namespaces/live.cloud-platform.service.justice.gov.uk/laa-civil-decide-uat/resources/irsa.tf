113321704035bd4e1e62b7094f3a6c4a25b13970
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-11-05 11:14:24 +0000

    add irsa policy to access kubernetes secrets

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-uat/resources/irsa.tf
@@ -0,0 +1,33 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "${var.namespace}-irsa"
+  namespace            = var.namespace
+
+  role_policy_arns = {
+    s3 = module.authorized-keys.irsa_policy_arn
+  }
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "irsa-output"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+    rolearn        = module.irsa.role_arn
+  }
+}
