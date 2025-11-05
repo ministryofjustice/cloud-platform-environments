ad7022a5f23d03147df4dcea89e6b4b53a29169d
Francis O'Brien <francis.obrien@justice.gov.uk>
2025-11-05 14:46:07 +0000

    update github provider target version

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-support-dev/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-support-dev/resources/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.5.0"
+      version = "~> 6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
