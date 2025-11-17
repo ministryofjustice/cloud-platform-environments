89befa527074d78eae44d9ea766b42244f64ad85
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2025-11-17 10:41:09 +0000

    changing allow  major version upgrade to false

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-support-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-support-dev/resources/rds-postgresql.tf
@@ -12,7 +12,7 @@ module "rds" {
 
   # RDS configuration
   allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = true
+  allow_major_version_upgrade  = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
