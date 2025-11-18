a370ad8d0bc391399dbabaa1fe8ff0835ddad5e9
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-11-18 15:09:13 +0000

    HIA-858: Add GHA service account for hmpps-integration-events preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/hmpps-integration-api-gha.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/hmpps-integration-api-gha.tf
@@ -14,3 +14,20 @@ module "hmpps_integration_api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
+
+module "hmpps_integration_events" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "hmpps-integration-events"
+  application = "hmpps-integration-events"
+  github_team = "hmpps-integration-api"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
+  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
+  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "preprod" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
