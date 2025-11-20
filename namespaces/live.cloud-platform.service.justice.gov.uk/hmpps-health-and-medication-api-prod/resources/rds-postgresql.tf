f2806600d20c8faa8fca0f7d72eac01a4b0ccc91
Jon Brighton <brightonsbox@hotmail.com>
2025-11-20 09:11:44 +0000

    CDPS-1636: Removing health-and-medication (prod) rds parameters

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-prod/resources/rds-postgresql.tf
@@ -35,38 +35,6 @@ module "rds" {
   namespace              = var.namespace
   team_name              = var.team_name
 
-  # Add DPR security group.
-  vpc_security_group_ids       = [data.aws_security_group.mp_dps_sg.id]
-
-  # Add parameters to enable logical replication
-  db_parameter = [
-    {
-      name         = "rds.logical_replication"
-      value        = "1"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "shared_preload_libraries"
-      value        = "pglogical"
-      apply_method = "pending-reboot"
-    },
-    {
-      name         = "max_wal_size"
-      value        = "1024"
-      apply_method = "immediate"
-    },
-    {
-      name         = "wal_sender_timeout"
-      value        = "0"
-      apply_method = "immediate"
-    },
-    {
-      name         = "max_slot_wal_keep_size"
-      value        = "40000"
-      apply_method = "immediate"
-    }
-  ]
-
   enable_irsa = true
 }
 
