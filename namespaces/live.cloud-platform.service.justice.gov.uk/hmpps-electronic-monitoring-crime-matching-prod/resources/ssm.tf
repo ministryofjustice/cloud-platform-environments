4831bda902cd074eb733a6c6d40c79aa7dbdbe5c
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-11-21 12:34:55 +0000

    Adding placeholder parameter for athena role

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/ssm.tf
@@ -3,10 +3,10 @@ data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
 
-data "aws_ssm_parameter" "athena_general_role_arn" {
-  name = "/${var.namespace}/athena_general_role_arn"
-  with_decryption = true
-}
+# data "aws_ssm_parameter" "athena_general_role_arn" {
+#   name = "/${var.namespace}/athena_general_role_arn"
+#   with_decryption = true
+# }
 
 resource "aws_ssm_parameter" "athena_general_role_arn" {
   name        = "/${var.namespace}/athena_general_role_arn"
