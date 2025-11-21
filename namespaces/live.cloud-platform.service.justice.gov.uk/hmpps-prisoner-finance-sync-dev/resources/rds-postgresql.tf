c351d85f530d5a8c0e4a4d7f13c273ee6396b7eb
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2025-11-21 14:05:55 +0000

    Add RDS

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-dev/resources/rds-postgresql.tf
@@ -0,0 +1,57 @@
+module "rds" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+  db_allocated_storage = 20
+  storage_type         = "gp3"
+
+  # VPC configuration
+  vpc_name = var.vpc_name
+
+  # RDS configuration
+  allow_minor_version_upgrade  = true
+  allow_major_version_upgrade  = false
+  performance_insights_enabled = false
+  db_max_allocated_storage     = "500"
+
+  # PostgresSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "17"
+  rds_family        = "postgres17"
+  db_instance_class = "db.t4g.micro"
+
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+}
+
+resource "kubernetes_secret" "rds" {
+  metadata {
+    name      = "rds-postgresql-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_name         = module.rds.database_name
+    database_username     = module.rds.database_username
+    database_password     = module.rds.database_password
+    rds_instance_address  = module.rds.rds_instance_address
+  }
+}
+
+# Configmap to store non-sensitive data related to the RDS instance
+resource "kubernetes_config_map" "rds" {
+  metadata {
+    name      = "postgresdb"
+    namespace = var.namespace
+  }
+
+  data = {
+    database_name = module.rds.database_name
+    db_identifier = module.rds.db_identifier
+  }
+} 
