7a43dce76e33006ccfda942807d3e217340ca9d5
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-11-21 10:20:37 +0000

    Update `hmpps-feature-flags` dev namespace roles

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-feature-flags-dev/resources/hmpps-feature-flags.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-feature-flags-dev/resources/hmpps-feature-flags.tf
@@ -2,7 +2,7 @@ module "hmpps-feature-flags" {
   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-feature-flags"
   application = "hmpps-feature-flags"
-  github_team = "hmpps-lead-developers"
+  github_team = "hmpps-feature-flag-admins"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
