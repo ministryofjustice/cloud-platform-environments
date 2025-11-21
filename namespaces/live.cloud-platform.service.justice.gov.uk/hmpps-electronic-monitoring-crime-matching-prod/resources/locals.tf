7ab19538a3f311fd0ad5a51c307ff041f6c2080a
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-11-21 11:24:54 +0000

    Typos

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/locals.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/locals.tf
@@ -10,7 +10,7 @@ locals {
   }
   
   sqs_queues = {
-    "Digital-Prison-Services-preprod-hmpps_audit_queue" = "hmpps-audit-prod"
+    "Digital-Prison-Services-prod-hmpps_audit_queue" = "hmpps-audit-prod"
   }
 
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
