b728a068ce8d1b0933ac8097304c96b1fe3e8501
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-11-21 11:03:39 +0000

    Initial setup for crime matching prod namespace and api

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/cross-iam-role-sa.tf
@@ -0,0 +1,20 @@
+module "irsa" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = var.namespace-short
+  namespace            = var.namespace
+  role_policy_arns = merge(
+    local.sqs_policies,
+    {
+      rds = module.rds.irsa_policy_arn
+    }
+  )
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-prod/resources/variables.tf
@@ -0,0 +1,76 @@
+variable "vpc_name" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "HMPPS Electronic Monitoring Crime Matching"
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
+variable "namespace" {
+  default = "hmpps-electronic-monitoring-crime-matching-prod"
+}
+
+variable "namespace-short" {
+  description = "Short-form version of namespace name to get around string-length issues"
+  type = string
+  default = "hmpps-em-crime-matching-prod"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "hmpps-em-probation-devs"
+}
+
+####################################################################################################################
+### Change this environment to the environment name corresponding to this namespace (as per helm/values-ENV.dev) ###
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "prod"
+}
+####################################################################################################################
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "public-ac-crime-mapping"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
+variable "domain" {
+  default = "crime-matching-prod.electronic-monitoring.service.justice.gov.uk"
+}
+
+variable "email_bucket_name" {
+  default = "electronic-monitoring-police-emails-prod"
+}
