17896dce3e1f99151399de33885e2a610d4bf616
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-11-25 14:44:27 +0000

    HAAR-4703: hmpps-manage-users-dev add gihub action and cert for hmpps-manage-users-temp-rewrite

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-users-dev/resources/hmpps-manage-usersi-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-users-dev/resources/hmpps-manage-usersi-github.tf
@@ -8,7 +8,7 @@ module "hmpps-manage-users" {
   protected_branches_only       = true
   is_production                 = var.is_production
   application_insights_instance = var.environment-name
-  source_template_repo          = "hmpps-template-kotlin"
+  source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
@@ -25,6 +25,22 @@ module "hmpps-manage-users-api" {
   is_production                 = var.is_production
   application_insights_instance = var.environment-name
   source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "hmpps-manage-users" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-manage-users-temp-rewrite"
+  application                   = "hmpps-manage-users-temp-rewrite"
+  github_team                   = "haha-live"
+  environment                   = var.environment-name
+  selected_branch_patterns      = ["main"]
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = var.environment-name
+  source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
