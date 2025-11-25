650cf71d0d1184bc47afeb15e2a5ba729d50add8
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2025-11-25 15:47:14 +0000

    add rds and replica to hmpps-prisoner-pay-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-pay-dev/resources/rds.tf
@@ -0,0 +1,152 @@
+# Retrieve mp_dps_sg_name SG group ID
+data "aws_security_group" "mp_dps_sg" {
+  name = var.mp_dps_sg_name
+}
+
+module "prisoner_pay_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business_unit               = var.business_unit
+  application                 = var.application
+  is_production               = var.is_production
+  namespace                   = var.namespace
+  environment_name            = var.environment
+  infrastructure_support      = var.infrastructure_support
+  prepare_for_major_upgrade   = false
+  rds_family                  = "postgres18"
+  allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
+  db_instance_class           = "db.t4g.micro"
+  db_engine_version           = "18"
+  storage_type                = "gp3"
+  db_max_allocated_storage    = "50"
+
+  # Add security groups for DPR
+  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
+
+  # Add parameters to enable DPR team to configure replication
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+     {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "5000"
+      apply_method = "immediate"
+    }
+  ]
+
+  providers = {
+    aws = aws.london
+  }
+
+  enable_irsa = true
+}
+
+resource "kubernetes_secret" "prisoner_pay_rds" {
+  metadata {
+    name      = "prisoner-pay-rds"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.prisoner_pay_rds.rds_instance_endpoint
+    database_name         = module.prisoner_pay_rds.database_name
+    database_username     = module.prisoner_pay_rds.database_username
+    database_password     = module.prisoner_pay_rds.database_password
+    rds_instance_address  = module.prisoner_pay_rds.rds_instance_address
+  }
+}
+
+module "prisoner_pay_rds_read_replica" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business_unit               = var.business_unit
+  application                 = var.application
+  is_production               = var.is_production
+  namespace                   = var.namespace
+  environment_name            = var.environment
+  infrastructure_support      = var.infrastructure_support
+  rds_family                  = "postgres18"
+  allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
+  db_instance_class           = "db.t4g.micro"
+  db_engine_version           = "18"
+  storage_type                = "gp3"
+  db_max_allocated_storage    = "50"
+  replicate_source_db         = module.prisoner_pay_rds.db_identifier
+  db_backup_retention_period  = 0
+
+  # Add security groups for DPR
+  vpc_security_group_ids      = [data.aws_security_group.mp_dps_sg.id]
+
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+     {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "5000"
+      apply_method = "immediate"
+    },
+    {
+      name         = "hot_standby_feedback"
+      value        = "1"
+      apply_method = "immediate"
+    }
+  ]
+
+  providers = {
+    aws = aws.london
+  }
+
+  enable_irsa = true
+}
+
+resource "kubernetes_secret" "prisoner_pay_rds_read_replica" {
+  metadata {
+    name      = "prisoner-pay-read-replica"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.prisoner_pay_rds_read_replica.rds_instance_endpoint
+    rds_instance_address  = module.prisoner_pay_rds_read_replica.rds_instance_address
+  }
+}
