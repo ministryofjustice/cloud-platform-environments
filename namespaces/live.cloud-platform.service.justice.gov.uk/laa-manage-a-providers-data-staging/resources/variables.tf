39bb6d6f2013f2d4e20cfee7ec5dc504c2c7b900
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-11-26 13:02:53 +0000

    add tags and new variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-staging/resources/variables.tf
@@ -67,3 +67,9 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
+
+variable "service_area" {
+  description = "Service area responsible for this service"
+  type        = string
+  default     = "Information & Advice"
+}
