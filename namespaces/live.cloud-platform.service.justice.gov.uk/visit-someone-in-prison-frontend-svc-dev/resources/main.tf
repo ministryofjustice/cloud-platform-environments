5aa24c6bc4ecc8659297d1c906e18f586a342d3d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-11-26 11:51:03 +0000

    Add tags to all aws resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-dev/resources/main.tf
@@ -27,7 +27,15 @@ provider "aws" {
 
   default_tags {
     tags = {
-        GithubTeam = "hmpps-prison-visits-booking-devs"
+      application   = var.application
+      business-unit = var.business_unit
+      GithubTeam    = var.team_name
+      is-production = var.is_production
+      namespace     = var.namespace
+      owner         = var.team_name
+      service-area  = var.service_area
+      slack-channel = var.slack_channel
+      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
     }
   }
 }
@@ -38,7 +46,15 @@ provider "aws" {
 
   default_tags {
     tags = {
-        GithubTeam = "hmpps-prison-visits-booking-devs"
+      application   = var.application
+      business-unit = var.business_unit
+      GithubTeam    = var.team_name
+      is-production = var.is_production
+      namespace     = var.namespace
+      owner         = var.team_name
+      service-area  = var.service_area
+      slack-channel = var.slack_channel
+      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
     }
   }
 }
