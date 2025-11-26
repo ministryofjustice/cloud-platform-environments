9da81af4585a297c54a4b5b1881eaab23febc9cd
Denis <Denislav.Davidov@justice.gov.uk>
2025-11-26 13:39:06 +0000

    adding additional ui projcet for esup service'

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-dev/resources/probation-check-in-ui.tf
@@ -0,0 +1,51 @@
+module "hmpps_probation_check_in_ui" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo = "hmpps-probation-check-in-ui"
+  application = "hmpps-probation-check-in-ui"
+  github_team = "stg-pathfinders"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+
+# Note, redis is a requirement for hmpps-template-typescript application.
+module "probation_check_in_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.0.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = module.hmpps_probation_check_in_ui.application
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  number_cache_clusters = var.number_cache_clusters
+  # sized for micro in dev, preprod, suggest small for production
+  node_type            = "cache.t4g.micro"
+  engine_version       = "7.0"
+  parameter_group_name = "default.redis7"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "probation_check_in_elasticache_redis" {
+  metadata {
+    name      = "${module.hmpps_probation_check_in_ui.application}-elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.probation_check_in_elasticache_redis.primary_endpoint_address
+    auth_token               = module.probation_check_in_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.probation_check_in_elasticache_redis.member_clusters)
+    replication_group_id     = module.probation_check_in_elasticache_redis.replication_group_id
+  }
+}
