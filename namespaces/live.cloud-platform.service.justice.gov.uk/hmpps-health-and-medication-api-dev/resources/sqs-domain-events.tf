7cffa6507eb211d8c877bc2791b11caf23c11afd
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-11-27 11:06:24 +0000

    CDPS-1706 Add SQS queue to health-and-medication-api-dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-health-and-medication-api-dev/resources/sqs-domain-events.tf
@@ -0,0 +1,77 @@
+module "domain_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+
+  sqs_name = "domain_events_queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.domain_events_dlq.sqs_arn
+    maxReceiveCount     = 3
+  })
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "aws_sqs_queue_policy" "domain_events_queue_policy" {
+  queue_url = module.domain_events_queue.sqs_id
+  policy    = data.aws_iam_policy_document.sqs_queue_policy_document.json
+}
+
+module "domain_events_dlq" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+
+  sqs_name                  = "domain_events_dlq"
+  message_retention_seconds = 7 * 24 * 3600 # 1 week
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+resource "aws_sns_topic_subscription" "domain_events_subscription" {
+  topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  protocol  = "sqs"
+  endpoint  = module.domain_events_queue.sqs_arn
+  filter_policy = jsonencode({
+    eventType = [
+      "prisoner-offender-search.prisoner.received",
+      "prisoner-offender-search.prisoner.released",
+      "prison-offender-events.prisoner.merged",
+      "prison-offender-events.prisoner.cell.move",
+    ]
+  })
+}
+
+resource "kubernetes_secret" "domain_events_queue_secret" {
+  metadata {
+    name      = "sqs-domain-events-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    queue_url  = module.domain_events_queue.sqs_id
+    queue_arn  = module.domain_events_queue.sqs_arn
+    queue_name = module.domain_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "domain_events_dlq_secret" {
+  metadata {
+    name      = "sqs-domain-events-dlq-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    queue_url  = module.domain_events_dlq.sqs_id
+    queue_arn  = module.domain_events_dlq.sqs_arn
+    queue_name = module.domain_events_dlq.sqs_name
+  }
+}
