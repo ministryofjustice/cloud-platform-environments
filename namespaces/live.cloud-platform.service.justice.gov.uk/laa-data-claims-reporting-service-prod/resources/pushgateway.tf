903480aca59c29bd5aab6b1bb2a01c34fccc71d4
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-11-27 13:02:19 +0000

    run concourse

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-prod/resources/pushgateway.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-prod/resources/pushgateway.tf
@@ -1,4 +1,4 @@
 module "pushgateway" {
-    source    = "github.com/ministryofjustice/cloud-platform-terraform-pushgateway?ref=1.4.0" # use the latest relase
+    source    = "github.com/ministryofjustice/cloud-platform-terraform-pushgateway?ref=1.4.0" # use the latest release
     namespace = var.namespace
 }
