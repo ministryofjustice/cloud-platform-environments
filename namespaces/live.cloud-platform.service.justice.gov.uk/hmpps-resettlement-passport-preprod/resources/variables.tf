21b3795d209fcb746a956eac9be50ad90c891f29
zahid.khan <Zahid.Khan1@justice.gov.uk>
2025-11-28 15:24:20 +0000

    [ESWE-1534] add mandatory aws tags for NS hmpps-resettlement-passport-preprod.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-resettlement-passport-preprod/resources/variables.tf
@@ -26,6 +26,12 @@ variable "business_unit" {
   default     = "HMPPS"
 }
 
+variable "service_area" {
+  description = "Service area responsible for this service"
+  type        = string
+  default     = "Education, Skills & Work"
+}
+
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
