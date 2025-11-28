cc63be608d2ded81ddc2c57c1c8518710099957c
neil-stephen-mcgonigle <neil.mcgonigle@justice.gov.uk>
2025-11-28 09:03:42 +0000

    CICA Create namesapce for cica-review-case-documents-uat

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cica-review-case-documents-uat/resources/opensearch.tf
@@ -0,0 +1,57 @@
+module "s3_snapshot_bucket" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0" # use the latest release
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+# Create the domain
+module "opensearch" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch?ref=1.8.1" # use the latest release
+
+  # VPC/EKS configuration
+  vpc_name         = var.vpc_name
+  eks_cluster_name = var.eks_cluster_name
+
+  # Cluster configuration
+  engine_version      = "OpenSearch_2.19"
+  snapshot_bucket_arn = module.s3_snapshot_bucket.bucket_arn
+  proxy_count         = 2
+
+  # Non-production cluster configuration
+  cluster_config = {
+    instance_count = 2
+    instance_type  = "t3.small.search"
+  }
+
+  ebs_options = {
+    volume_size = 10
+  }
+
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+}
+
+# Output the proxy URL
+resource "kubernetes_secret" "opensearch" {
+  metadata {
+    name      = "${var.team_name}-opensearch-proxy-url"
+    namespace = var.namespace
+  }
+
+  data = {
+    proxy_url = module.opensearch.proxy_url
+  }
+}
