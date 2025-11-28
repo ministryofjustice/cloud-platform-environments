70240f2bc98733d3894328340c976918e10280d6
zahid.khan <Zahid.Khan1@justice.gov.uk>
2025-11-28 15:18:13 +0000

    [ESWE-1534] add mandatory aws tags for NS hmpps-prisoner-base-location-preprod.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-base-location-preprod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-base-location-preprod/resources/main.tf
@@ -8,6 +8,12 @@ provider "aws" {
 
   default_tags {
     tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
       # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
       GithubTeam = var.team_name
     }
@@ -20,6 +26,12 @@ provider "aws" {
 
   default_tags {
     tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
       # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
       GithubTeam = var.team_name
     }
@@ -32,6 +44,12 @@ provider "aws" {
 
   default_tags {
     tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
       # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
       GithubTeam = var.team_name
     }
