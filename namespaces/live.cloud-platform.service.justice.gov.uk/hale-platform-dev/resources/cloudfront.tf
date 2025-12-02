05d54c73426afce4f6d9fe03bba8866d5a7fec1d
Daniel Glen <daniel.glen@digital.justice.gov.uk>
2025-12-02 16:08:28 +0000

    Added cache policy id

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
@@ -21,7 +21,8 @@ module "cloudfront_with_ordered" {
   enable_ordered_cache_behavior = true # default is false
 
   ordered_cache_behavior = {
-    path_pattern = "/uploads/sites/*/*colours.css"
+    path_pattern = "/uploads/sites/*/*colours*.css"
+    cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
   }
 
   depends_on = [aws_acm_certificate_validation.cloudfront_alias_cert_validation]
