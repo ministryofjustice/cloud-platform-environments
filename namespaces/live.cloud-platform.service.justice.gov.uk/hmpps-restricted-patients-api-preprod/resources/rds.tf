c6229b3955059485989c2408e12aa7330b2b8dea
karenmillermoj <karen.miller3@justice.gov.uk>
2025-12-02 10:12:40 +0000

    SDIT-3181: 🗃️ Finish upgrade to Postgres 18 for hmpps-restricted-patients-api-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-api-preprod/resources/rds.tf
@@ -17,7 +17,7 @@ module "rp_rds" {
   rds_family                = "postgres18"
   db_password_rotated_date  = "15-02-2023"
   deletion_protection       = true
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
 
   providers = {
     aws = aws.london
