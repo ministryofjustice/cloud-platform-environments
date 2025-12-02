353ec147ef5bddae83e5b8e23337839242bdaccf
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-12-02 18:53:28 +0000

    resource naming does not like &'s

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-a-providers-data-uat/resources/variables.tf
@@ -71,6 +71,6 @@ variable "github_token" {
 variable "service_area" {
   description = "Service area responsible for this service"
   type        = string
-  default     = "Information & Advice"
+  default     = "Information and Advice"
 }
 
