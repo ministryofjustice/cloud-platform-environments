80c7eac062f78d4427556bca4d8876ab9b981ab0
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-12-02 20:13:52 +0000

    resource naming does not like &'s

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/variables.tf
@@ -53,7 +53,7 @@ variable "is_production" {
 variable "service_area" {
   description = "Service area responsible for this service"
   type        = string
-  default     = "Information & Advice"
+  default     = "Information and Advice"
 }
 
 variable "slack_channel" {
