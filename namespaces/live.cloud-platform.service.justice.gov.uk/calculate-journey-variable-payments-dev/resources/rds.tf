0630a56df36783afc081b317cc1146c018b42bde
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2025-12-02 10:46:46 +0000

    MAP-3053 creating the user

--- namespaces/live.cloud-platform.service.justice.gov.uk/calculate-journey-variable-payments-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/calculate-journey-variable-payments-dev/resources/rds.tf
@@ -48,8 +48,8 @@ resource "kubernetes_secret" "rds-instance" {
   }
 
   data = {
-    access_key_id     = module.aws_iam_access_key.dso_s3_rds_user_key.id
-    secret_access_key = module.aws_iam_access_key.dso_s3_rds_user_key.secret
+    access_key_id     = aws_iam_access_key.dso_s3_rds_user_key.id
+    secret_access_key = aws_iam_access_key.dso_s3_rds_user_key.secret
     database_name     = module.rds-instance.database_name
     database_host     = module.rds-instance.rds_instance_address
     database_port     = module.rds-instance.rds_instance_port
