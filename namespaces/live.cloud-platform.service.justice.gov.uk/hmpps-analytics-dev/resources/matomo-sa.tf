17f7b258cd9fc5e334947fc1ea636e1fb212f26f
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-09 12:00:52 +0000

    Fix source template repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-analytics-dev/resources/matomo-sa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-analytics-dev/resources/matomo-sa.tf
@@ -9,7 +9,7 @@ module "hmpps_matomo_dev" {
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
-  source_template_repo          = ""
+  source_template_repo          = "none"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
