74650f0d7f475156308227a6bc30653f5c552570
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-12-10 09:09:10 +0000

    `intranet-staging` Add pingdom

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/intranet-staging/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "intranet-staging" {
+  type                     = "http"
+  name                     = "intranet-staging"
+  host                     = "staging.intranet.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 4
+  notifyagainevery         = 0
+  url                      = "/"
+  encryption               = true
+  port                     = 443
+  tags                     = "businessunit_${var.business_unit},application_${var.application},component_ping,isstaging_${var.is_staging},environment_${var.environment},infrastructuresupport_${var.application}"
+  probefilters             = "region:EU"
+  integrationids           = [133317]
+}
