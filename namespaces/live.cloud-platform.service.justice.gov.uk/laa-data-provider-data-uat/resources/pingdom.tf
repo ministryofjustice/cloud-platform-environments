07546279429a01566c654c253cfa399c452f310b
UshaPanneer <usha.panneerselvam@justice.gov.uk>
2025-12-10 16:25:15 +0000

    feat: DSTEW-679 Set up Pingdom for uat environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-provider-data-uat/resources/pingdom.tf
@@ -0,0 +1,40 @@
+provider "pingdom" {
+  api_key = var.pingdom_api_key
+}
+
+resource "pingdom_check" "laa_data_provider_data_uat" {
+
+  name                     = "LAA Provider Details API UAT Healthcheck"
+  host                     = "laa-provider-details-api-uat.apps.live.cloud-platform.service.justice.gov.uk"
+  type                     = "http"
+  url                      = "/actuator/health"
+  encryption               = true
+  resolution               = 1                # Check every minute
+  notifywhenbackup         = true
+  sendnotificationwhendown = 2                # Notify after 2 consecutive failures
+  tags                     = "laa,uat,api,healthcheck"
+  integrationids           = [145876]
+
+  # Optional: expected HTTP status code
+  # This ensures Pingdom marks the check as "up" only when it gets a 200 response
+  requestheaders = {
+    "Accept" = "application/json"
+  }
+
+  # You can optionally restrict to specific Pingdom regions:
+  # probe_filters = "region:EU"
+  # --- Operational Hours: 07:00–21:30 daily ---
+  hoursofoperation {
+    days_of_week = [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday"
+    ]
+    from = "07:00"
+    to   = "21:30"
+  }
+}
