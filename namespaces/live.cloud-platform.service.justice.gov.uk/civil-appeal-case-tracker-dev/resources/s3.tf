8ceacefdae34e0c6b8eacba88a8626393561ea5d
Timi Olubusi <timiolubusi@icloud.com>
2025-12-11 10:09:57 +0000

    Removing vpc code as its not needed with CP s3 gateway

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/s3.tf
@@ -2,7 +2,6 @@
  * Make sure that you use the latest version of the module by changing the
  * `ref=` value in the `source` attribute to the latest version listed on the
  * releases page of this repository.
- *
  */
 module "s3_bucket" {
   source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
@@ -16,24 +15,10 @@ module "s3_bucket" {
 }
 
 # --------------------------------------------------------
-# VPC ENDPOINT FOR S3 BUCKET ACCESS FROM WITHIN VPC
-# --------------------------------------------------------
-resource "aws_vpc_endpoint" "s3_gateway" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.s3"
-  vpc_endpoint_type = "Gateway"
-
-  route_table_ids = [
-    aws_route_table.private.id,
-  ]
-}
-
-# --------------------------------------------------------
-# BUCKET POLICY (external user + VPC restriction)
+# BUCKET POLICY (Allow the external user)
 # --------------------------------------------------------
 data "aws_iam_policy_document" "merged_bucket_policy" {
 
-  # Allow the external user access
   statement {
     sid    = "AllowExternalUserToReadAndPutObjectsInS3"
     effect = "Allow"
@@ -54,50 +39,28 @@ data "aws_iam_policy_document" "merged_bucket_policy" {
       "${module.s3_bucket.bucket_arn}/*"
     ]
   }
-
-  # Deny everything unless request used the VPC endpoint
-  statement {
-    sid    = "DenyUnlessViaVpcEndpoint"
-    effect = "Deny"
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:*"
-    ]
-
-    resources = [
-      module.s3_bucket.bucket_arn,
-      "${module.s3_bucket.bucket_arn}/*"
-    ]
-
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:SourceVpce"
-      values   = [aws_vpc_endpoint.s3_gateway.id]
-    }
-  }
 }
 
-# --------------------------------------------------------
-# MERGED POLICY TO THE BUCKET
-# --------------------------------------------------------
 resource "aws_s3_bucket_policy" "restricted_policy" {
   bucket = module.s3_bucket.bucket_name
   policy = data.aws_iam_policy_document.merged_bucket_policy.json
 }
 
 # --------------------------------------------------------
-# 4. EXTERNAL USER FOR S3 BUCKET ACCESS
+# IAM USER FOR EXTERNAL S3 ACCESS
 # --------------------------------------------------------
 resource "aws_iam_user" "user" {
   name = "external-s3-access-user-${var.environment}"
   path = "/system/external-s3-access-user/"
 }
 
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
+}
+
+# --------------------------------------------------------
+# IAM POLICY FOR THAT USER (same permissions as bucket policy)
+# --------------------------------------------------------
 data "aws_iam_policy_document" "external_user_s3_access_policy" {
   statement {
     sid = "AllowExternalUserToReadAndPutObjectsInS3"
@@ -111,10 +74,6 @@ data "aws_iam_policy_document" "external_user_s3_access_policy" {
       "${module.s3_bucket.bucket_arn}/*"
     ]
   }
-}
-
-resource "aws_iam_access_key" "user" {
-  user = aws_iam_user.user.name
 }
 
 resource "aws_iam_user_policy" "policy" {
