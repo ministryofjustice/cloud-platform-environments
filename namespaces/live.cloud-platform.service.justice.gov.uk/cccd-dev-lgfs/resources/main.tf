bc7a9a4886354d0767728892a70342958cd26e9e
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2025-12-12 11:20:38 +0000

    Update mandatory tags in cccd-dev-lgfs

--- namespaces/live.cloud-platform.service.justice.gov.uk/cccd-dev-lgfs/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cccd-dev-lgfs/resources/main.tf
@@ -7,6 +7,12 @@ provider "aws" {
   region = "eu-west-2"
   default_tags {
     tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
       GithubTeam = "laa-claim-for-payment"
     }
   }
@@ -18,6 +24,12 @@ provider "aws" {
   region = "eu-west-2"
   default_tags {
     tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
       GithubTeam = "laa-claim-for-payment"
     }
   }
@@ -26,6 +38,16 @@ provider "aws" {
 # To be use in case the resources need to be created in Ireland
 provider "aws" {
   alias  = "ireland"
+  default_tags {
+    tags = {
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
+    }
+  }
   region = "eu-west-1"
 }
 provider "github" {
