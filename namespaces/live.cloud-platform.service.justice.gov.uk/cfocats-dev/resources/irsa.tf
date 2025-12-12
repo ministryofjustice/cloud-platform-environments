1a5e90cd489ac995416667526eaaecdbdd8d4312
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-12-12 12:20:59 +0000

    Removes unused resources (#38563)
    Removes SNS, SQS, and Redis resources that are no longer required.

--- namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/irsa.tf
@@ -11,20 +11,6 @@ module "irsa" {
   role_policy_arns = {
     s3 = module.s3_bucket.irsa_policy_arn
     rds_mssql = module.rds_mssql.irsa_policy_arn
-    redis = module.redis.irsa_policy_arn
-
-    sqs_overnight = module.sqs_overnight.irsa_policy_arn
-    sqs_payment  = module.sqs_payment.irsa_policy_arn
-    sqs_tasks     = module.sqs_tasks.irsa_policy_arn
-
-    sns_activity_approved_event        = module.activity_approved_event.irsa_policy_arn
-    sns_hub_induction_created_event    = module.hub_induction_created_event.irsa_policy_arn
-    sns_objectivetask_completed_event  = module.objectivetask_completed_event.irsa_policy_arn
-    sns_participant_transitioned_event = module.participant_transitioned_event.irsa_policy_arn
-    sns_pri_assigned_event             = module.pri_assigned_event.irsa_policy_arn
-    sns_pri_ttg_completed_event        = module.pri_ttg_completed_event.irsa_policy_arn
-    sns_sync_participant_command       = module.sync_participant_command.irsa_policy_arn
-    sns_wing_induction_created_event   = module.wing_induction_created_event.irsa_policy_arn
   }
 
   # Tags
