59c53d5a50bfe515194a26ac3d8afe6a8dbdd448
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2025-12-18 12:41:07 +0000

    remove tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-contribution-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-crown-court-contribution-prod/resources/main.tf
@@ -11,12 +11,6 @@ provider "aws" {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
       GithubTeam = var.team_name
-      business-unit = var.business_unit
-      application = var.application
-      is-production = var.is_production
-      owner = var.team_name
-      namespace = var.namespace
-      service-area = var.service_area
     }
   }
 }
@@ -30,12 +24,6 @@ provider "aws" {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
       GithubTeam = var.team_name
-      business-unit = var.business_unit
-      application = var.application
-      is-production = var.is_production
-      owner = var.team_name
-      namespace = var.namespace
-      service-area = var.service_area
     }
   }
 }
@@ -49,12 +37,6 @@ provider "aws" {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
       GithubTeam = var.team_name
-      business-unit = var.business_unit
-      application = var.application
-      is-production = var.is_production
-      owner = var.team_name
-      namespace = var.namespace
-      service-area = var.service_area
     }
   }
 }
