38c71a45f738c6534d08fbc7b1a5fc69e204895f
denislav-davidov-moj <denislav.davidov@justice.gov.uk>
2025-12-19 11:39:43 +0000

    adding probation-checkin-ui settings for preprod (#38710)
    * adding probation-checkin-ui settings for preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-preprod/resources/github-environment-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-preprod/resources/github-environment-api.tf
@@ -1,5 +1,5 @@
 module "hmpps_template_kotlin" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-esupervision-api"
   application = "hmpps-esupervision-api"
   github_team = "stg-pathfinders"
