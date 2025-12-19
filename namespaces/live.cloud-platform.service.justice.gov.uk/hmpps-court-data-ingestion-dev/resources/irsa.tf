46a7cc86214234555f7321baf0b8eab85f8db8b3
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-12-19 14:02:21 +0000

    CDIA-4 Sns Topic for Court Data Ingestion Service Dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-data-ingestion-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-data-ingestion-dev/resources/irsa.tf
@@ -1,5 +1,9 @@
 
 locals {
+  sns_topics = {
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
+  }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
   sqs_policies = {
     hmpps_court_data_ingestion_queue             = module.hmpps_court_data_ingestion_queue.irsa_policy_arn,
     hmpps_court_data_ingestion_dead_letter_queue = module.hmpps_court_data_ingestion_dead_letter_queue.irsa_policy_arn,
@@ -12,7 +16,7 @@ module "irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = "hmpps-court-data-ingestion-api"
-  role_policy_arns     = merge(local.sqs_policies, { rds_policy = module.hmpps-court-data-ingestion-api-rds.irsa_policy_arn })
+  role_policy_arns     = merge(local.sqs_policies, local.sns_policies, { rds_policy = module.hmpps-court-data-ingestion-api-rds.irsa_policy_arn })
   # Tags
   business_unit          = var.business_unit
   application            = var.application
@@ -20,4 +24,10 @@ module "irsa" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+}
+
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
