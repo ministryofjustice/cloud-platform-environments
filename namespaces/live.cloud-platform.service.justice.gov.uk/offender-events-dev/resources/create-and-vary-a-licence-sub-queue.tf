120b5fc5e3a0e7aafd6d54e29e971515016896aa
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2025-12-19 14:40:29 +0000

    Revert "CVSL-3664: Remove probation events queue from CVL dev."

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/create-and-vary-a-licence-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/create-and-vary-a-licence-sub-queue.tf
@@ -27,6 +27,34 @@ module "cvl_prison_events_queue" {
   }
 }
 
+module "cvl_probation_events_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+
+  # Queue configuration
+  sqs_name                  = "cvl_probation_events_queue"
+  encrypt_sqs_kms           = "true"
+  message_retention_seconds = 1209600
+
+  redrive_policy = <<EOF
+  {
+    "deadLetterTargetArn": "${module.cvl_probation_events_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+  }
+  EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 module "cvl_prison_events_dead_letter_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
 
@@ -47,6 +75,28 @@ module "cvl_prison_events_dead_letter_queue" {
     aws = aws.london
   }
 }
+
+module "cvl_probation_events_dead_letter_queue" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+
+  # Queue configuration
+  sqs_name        = "cvl_probation_events_queue_dl"
+  encrypt_sqs_kms = "true"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the queue
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
 
 resource "aws_sqs_queue_policy" "cvl_prison_events_queue_policy" {
   queue_url = module.cvl_prison_events_queue.sqs_id
@@ -75,6 +125,33 @@ resource "aws_sqs_queue_policy" "cvl_prison_events_queue_policy" {
    EOF
 }
 
+resource "aws_sqs_queue_policy" "cvl_probation_events_queue_policy" {
+  queue_url = module.cvl_probation_events_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.cvl_probation_events_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.cvl_probation_events_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+                {
+                  "aws:SourceArn": "${module.probation_offender_events.topic_arn}"
+                }
+            }
+        }
+      ]
+  }
+   EOF
+}
+
 resource "aws_sns_topic_subscription" "cvl_prison_events_subscription" {
   provider  = aws.london
   topic_arn = module.offender_events.topic_arn
@@ -88,6 +165,40 @@ resource "aws_sns_topic_subscription" "cvl_prison_events_subscription" {
   })
 }
 
+resource "aws_sns_topic_subscription" "cvl_probation_events_subscription" {
+  provider      = aws.london
+  topic_arn     = module.probation_offender_events.topic_arn
+  protocol      = "sqs"
+  endpoint      = module.cvl_probation_events_queue.sqs_arn
+  filter_policy = "{\"eventType\":[\"OFFENDER_MANAGER_CHANGED\"]}"
+}
+
+resource "kubernetes_secret" "create_and_vary_a_licence_prison_events_queue" {
+  metadata {
+    name      = "create-and-vary-a-licence-prison-events-sqs-instance-output"
+    namespace = "create-and-vary-a-licence-dev"
+  }
+
+  data = {
+    sqs_id   = module.cvl_prison_events_queue.sqs_id
+    sqs_arn  = module.cvl_prison_events_queue.sqs_arn
+    sqs_name = module.cvl_prison_events_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "create_and_vary_a_licence_probation_events_queue" {
+  metadata {
+    name      = "create-and-vary-a-licence-probation-events-sqs-instance-output"
+    namespace = "create-and-vary-a-licence-dev"
+  }
+
+  data = {
+    sqs_id   = module.cvl_probation_events_queue.sqs_id
+    sqs_arn  = module.cvl_probation_events_queue.sqs_arn
+    sqs_name = module.cvl_probation_events_queue.sqs_name
+  }
+}
+
 resource "kubernetes_secret" "create_and_vary_a_licence_prison_events_dead_letter_queue" {
   metadata {
     name      = "create-and-vary-a-licence-prison-events-sqs-dl-instance-output"
@@ -98,5 +209,18 @@ resource "kubernetes_secret" "create_and_vary_a_licence_prison_events_dead_lette
     sqs_id   = module.cvl_prison_events_dead_letter_queue.sqs_id
     sqs_arn  = module.cvl_prison_events_dead_letter_queue.sqs_arn
     sqs_name = module.cvl_prison_events_dead_letter_queue.sqs_name
+  }
+}
+
+resource "kubernetes_secret" "create_and_vary_a_licence_probation_events_dead_letter_queue" {
+  metadata {
+    name      = "create-and-vary-a-licence-probation-events-sqs-dl-instance-output"
+    namespace = "create-and-vary-a-licence-dev"
+  }
+
+  data = {
+    sqs_id   = module.cvl_probation_events_dead_letter_queue.sqs_id
+    sqs_arn  = module.cvl_probation_events_dead_letter_queue.sqs_arn
+    sqs_name = module.cvl_probation_events_dead_letter_queue.sqs_name
   }
 }
