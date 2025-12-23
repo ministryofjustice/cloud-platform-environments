c4dfff997dba471cfd2552340b9cb4502ce56db4
Ikenna Ibeanu <ikenna.ibeanu@justice.gov.uk>
2025-12-23 12:12:38 +0000

    Upgrade rds version to 17.6 for preprod court-probation

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-preprod/resources/rds.tf
@@ -10,10 +10,10 @@ module "court_case_service_rds" {
   infrastructure_support      = var.infrastructure_support
   is_production               = var.is_production
   prepare_for_major_upgrade   = true
-  db_engine                   = "postgres"
-  db_engine_version           = "14.17"
-  db_instance_class           = "db.t4g.xlarge"
-  rds_family                  = "postgres14"
+  db_engine                   = var.db_engine
+  db_engine_version           = var.db_engine_version
+  db_instance_class           = var.db_instance_class
+  rds_family                  = var.rds_family
   db_allocated_storage        = "35"
   enable_rds_auto_start_stop  = true
 
