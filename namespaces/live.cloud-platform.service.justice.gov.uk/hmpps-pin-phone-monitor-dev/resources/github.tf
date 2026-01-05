4f6d0a57123c1fc69cc382e237cc03d8af2e7042
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-01-05 10:52:54 +0000

    fixed typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/github.tf
@@ -1,12 +1,12 @@
-module "offender-categorisation" {
+module "hmpps-prisoner-communication-monitoring" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo                   = "hmpps-prisoner-communication-monitoring"
   application                   = "hmpps-prisoner-communication-monitoring"
   github_team                   = "secure-estate-digital-team"
-  environment                   = var.environment_name
+  environment                   = var.environment-name
   is_production                 = var.is_production
   protected_branches_only       = true
-  application_insights_instance = var.environment_name
+  application_insights_instance = var.environment-name
   source_template_repo          = "none"
   github_token                  = var.github_token
   namespace                     = var.namespace
