ec428044f266a1f9afa263db0d90fe8f7e4deeaa
Peter Hunt <naseberry@users.noreply.github.com>
2026-01-07 16:10:10 +0000

    DSTEW-902: OpenSearch Alert - uat

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/opensearch.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-uat/resources/opensearch.tf
@@ -1,14 +1,14 @@
 module "opensearch_alert_app_log" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-opensearch-alert?ref=1.0.2" # use the latest module
 
-  secret_name                    = "live-laa-data-claims-api-uat-40b01d3ef57692f3" 
+  secret_name                    = "live-laa-data-claims-api-uat-40b01d3ef57692f3"
   secret_key                     = "slack-alert-webhook"
 
   slack_channel_name             = "laa-data-stewardship-payments-nonprod-alerts" 
-  slack_channel_name_description = "LAA Data Claims Non-prod alerts"
+  slack_channel_name_description = "LAA Data Claims non-prod alerts"
 
   environment_name               = var.environment
-  opensearch_alert_name          = "LAA Data Claims API - Suspicious SQL-like pattern - Alert"
+  opensearch_alert_name          = "laa-data-claims-api-uat-sql-alert"
   opensearch_alert_enabled       = true
   monitor_period_interval        = "1"
   monitor_period_unit            = "MINUTES"
@@ -20,34 +20,20 @@ module "opensearch_alert_app_log" {
                 "filter": [
                     {
                         "multi_match": {
-                            "query": "Suspicious SQL",
-                            "fields": [],
                             "type": "phrase",
-                            "operator": "OR",
-                            "slop": 0,
-                            "prefix_length": 0,
-                            "max_expansions": 50,
-                            "lenient": true,
-                            "zero_terms_query": "NONE",
-                            "auto_generate_synonyms_phrase_query": true,
-                            "fuzzy_transpositions": true,
-                            "boost": 1
+                            "query": "Suspicious SQL",
+                            "lenient": true
                         }
                     },
                     {
                         "match_phrase": {
-                            "kubernetes.namespace_name": {
-                                "query": "laa-data-claims-api-uat",
-                                "slop": 0,
-                                "zero_terms_query": "NONE",
-                                "boost": 1
-                            }
+                            "kubernetes.namespace_name": "laa-data-claims-api-uat"
                         }
                     },
                     {
                         "range": {
                             "@timestamp": {
-                                "from": "{{period_end}}||-10m",
+                                "from": "{{period_end}}||-15m",
                                 "to": "{{period_end}}",
                                 "include_lower": true,
                                 "include_upper": true,
@@ -115,13 +101,13 @@ module "opensearch_alert_app_log" {
         }
     }
   )
-  trigger_name                   = "LAA Data Claims API - Suspicious SQL-like pattern - Trigger"
+  trigger_name                   = "laa-data-claims-api-uat-sql-trigger"
   serverity                      = "1"
   query_source                   = "ctx.results[0].hits.total.value > 1"
-  action_name                    = "Suspicious SQL-like pattern detected - Action"
-  slack_message_subject          = ":alert: Suspicious SQL-like pattern detected in LAA Data Claims API Uat :alert:"
-  slack_message_template         = "Monitor {{ctx.monitor.name}} just entered alert status. Please investigate the issue.\n- Trigger: {{ctx.trigger.name}}\n- Severity: {{ctx.trigger.severity}}" 
+  action_name                    = "laa-data-claims-api-uat-sql-action"
+  slack_message_subject          = ":alert: Suspicious SQL-like pattern detected in *laa-data-claims-api-uat* :alert:"
+  slack_message_template         = "Monitor {{ctx.monitor.name}} just entered alert status. Please investigate the issue.\n- *Trigger*: {{ctx.trigger.name}}\n- *Severity*: {{ctx.trigger.severity}}\n- *Dashboard*: https://app-logs.cloud-platform.service.justice.gov.uk/_dashboards/app/data-explorer/discover#?_a=(discover:(columns:!(log),isDirty:!t,sort:!()),metadata:(indexPattern:bb90f230-0d2e-11ef-bf63-53113938c53a,view:discover))&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_q=(filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:bb90f230-0d2e-11ef-bf63-53113938c53a,key:kubernetes.namespace_name,negate:!f,params:(query:laa-data-claims-api-uat),type:phrase),query:(match_phrase:(kubernetes.namespace_name:laa-data-claims-api-uat)))),query:(language:kuery,query:'Suspicious%20SQL'))" 
   alert_throttle_enabled         = true
-  throttle_value                 = 60
+  throttle_value                 = 5
   throttle_unit                  = "MINUTES"
 }
