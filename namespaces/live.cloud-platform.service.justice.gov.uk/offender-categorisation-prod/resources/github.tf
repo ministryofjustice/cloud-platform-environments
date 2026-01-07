54c93ab4cdbb71ce7ef9dbc1226445fb93ca01ae
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-01-07 15:37:33 +0000

    added in github config for cat tool api prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-prod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-categorisation-prod/resources/github.tf
@@ -14,3 +14,20 @@ module "offender-categorisation" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "hmpps-offender-categorisation-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-offender-categorisation-api"
+  application                   = "hmpps-offender-categorisation-api"
+  github_team                   = "secure-estate-digital-team"
+  environment                   = var.environment_name
+  reviewer_teams                = ["secure-estate-digital-restricted-team"]
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.environment_name
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
