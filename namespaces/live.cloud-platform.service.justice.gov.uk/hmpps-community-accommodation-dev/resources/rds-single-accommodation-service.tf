bb574ed865f7fd24dc891b6929591b59389aaada
daveawc <168838504+daveawc@users.noreply.github.com>
2026-01-08 11:25:53 +0000

    added postgres to SAS DEV

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-accommodation-dev/resources/rds-single-accommodation-service.tf
@@ -0,0 +1,67 @@
+module "single_accommodation_service_rds" {
+  source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+  db_allocated_storage        = 10
+  storage_type                = "gp2"
+  vpc_name                    = var.vpc_name
+  team_name                   = var.team_name
+  business_unit               = var.business_unit
+  application                 = var.application
+  is_production               = var.is_production
+  namespace                   = var.namespace
+  environment_name            = var.environment
+  infrastructure_support      = var.infrastructure_support
+  allow_major_version_upgrade = "true"
+  allow_minor_version_upgrade = "true"
+  db_instance_class           = "db.t4g.small"
+  db_engine_version           = "17"
+  db_engine                   = "postgres"
+  rds_family                  = "postgres17"
+  db_password_rotated_date    = "08-01-2026"
+  prepare_for_major_upgrade   = false
+
+  enable_irsa = true
+
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
+      apply_method = "immediate"
+    }
+  ]
+}
+
+
+resource "kubernetes_secret" "single_accommodation_service_rds" {
+  metadata {
+    name      = "single_accommodation_service_rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.single_accommodation_service_rds.rds_instance_endpoint
+    database_name         = module.single_accommodation_service_rds.database_name
+    database_username     = module.single_accommodation_service_rds.database_username
+    database_password     = module.single_accommodation_service_rds.database_password
+    rds_instance_address  = module.single_accommodation_service_rds.rds_instance_address
+  }
+}
