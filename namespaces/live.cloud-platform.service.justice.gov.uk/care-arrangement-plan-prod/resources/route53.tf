1fd173d76a89a4c4b28cd4f2e7b100765a71572f
Damien Wilson <damien.wilson@justice.gov.uk>
2026-01-08 11:53:52 +0000

    Update route53.tf - to include new zone for domain name (#39036)
    Adding: propose-child-arrangements-plan.service.gov.uk

--- namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/route53.tf
@@ -24,3 +24,29 @@ resource "kubernetes_secret" "cap_route53_zone_sec" {
     nameservers = join("\n", aws_route53_zone.cap_route53_zone.name_servers)
   }
 }
+
+resource "aws_route53_zone" "pcap_zone" {
+  name = "propose-child-arrangements-plan.service.gov.uk"
+
+  tags = {
+    business_unit          = var.business_unit
+    application            = var.application
+    is_production          = var.is_production
+    team_name              = var.team_name
+    namespace              = var.namespace
+    environment_name       = var.environment
+    infrastructure_support = var.infrastructure_support
+  }
+}
+
+resource "kubernetes_secret" "pcap_zone_sec" {
+  metadata {
+    name      = "pcap-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id = aws_route53_zone.pcap_zone.zone_id
+    nameservers = join("\n", aws_route53_zone.pcap_zone.name_servers)
+  }
+}
