d15193a150967a720f64e18ee12adb21e0e75805
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-01-09 13:28:04 +0000

    MIA-333 switch to live support team

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-preprod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-allocate-key-workers-preprod/resources/main.tf
@@ -10,33 +10,7 @@ provider "aws" {
     tags = {
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
-      GithubTeam = "hmpps-move-and-improve"
-    }
-  }
-}
-
-provider "aws" {
-  alias  = "london"
-  region = "eu-west-2"
-
-  default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-      GithubTeam = "hmpps-move-and-improve"
-    }
-  }
-}
-
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
-
-  default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-      GithubTeam = "hmpps-move-and-improve"
+      GithubTeam = var.team_name
     }
   }
 }
