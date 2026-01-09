605cd15c9adff6c2b1eab6bcf108a004af4b1841
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2026-01-09 15:10:58 +0000

    reverted change to service_pod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-launchpad-preprod/resources/irsa.tf
@@ -35,5 +35,5 @@ module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
   # Configuration
   namespace            = var.namespace
-  service_account_name = module.irsa.service_account_name # this uses the service account name from the irsa module
+  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
 }
