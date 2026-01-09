6d4a1c924374aaf8f3e186eb19df93800dad70fd
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-01-09 10:19:50 +0000

    RASS-1749 add audit sqs queue and service account

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-dev/resources/irsa.tf
@@ -8,9 +8,13 @@ locals {
     hmpps_remand_and_sentencing_prisoner_queue             = module.hmpps_remand_and_sentencing_prisoner_queue.irsa_policy_arn,
     hmpps_remand_and_sentencing_prisoner_dead_letter_queue = module.hmpps_remand_and_sentencing_prisoner_dead_letter_queue.irsa_policy_arn,
   }
+  ui_sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_ui_sqs : item.name => item.value}
+  ui_sqs_queues = {
+      "Digital-Prison-Services-${var.environment_name}-hmpps_audit_queue" = "hmpps-audit-${var.environment_name}"
+  }
 }
 
-module "irsa" {
+module "irsa-api" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
 
   eks_cluster_name     = var.eks_cluster_name
@@ -26,7 +30,28 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "irsa-ui" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = "hmpps-remand-and-sentencing"
+  role_policy_arns     = ui_sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_ui_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
