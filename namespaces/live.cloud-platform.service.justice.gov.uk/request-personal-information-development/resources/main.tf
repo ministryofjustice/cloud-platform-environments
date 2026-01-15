8f029e210cacc053ceafd4214f58ab729b51f92c
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-01-15 14:38:24 +0000

    Amend Dev configuration to mirror Staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/request-personal-information-development/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/request-personal-information-development/resources/main.tf
@@ -8,12 +8,6 @@ provider "aws" {
 
   default_tags {
     tags = {
-      business-unit = var.business_unit
-      application   = var.application
-      is-production = var.is_production
-      owner         = var.team_name
-      namespace     = var.namespace
-      service-area  = var.service_area
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
     }
@@ -26,12 +20,6 @@ provider "aws" {
 
   default_tags {
     tags = {
-      business-unit = var.business_unit
-      application   = var.application
-      is-production = var.is_production
-      owner         = var.team_name
-      namespace     = var.namespace
-      service-area  = var.service_area
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
     }
@@ -44,12 +32,6 @@ provider "aws" {
 
   default_tags {
     tags = {
-      business-unit = var.business_unit
-      application   = var.application
-      is-production = var.is_production
-      owner         = var.team_name
-      namespace     = var.namespace
-      service-area  = var.service_area
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
     }
