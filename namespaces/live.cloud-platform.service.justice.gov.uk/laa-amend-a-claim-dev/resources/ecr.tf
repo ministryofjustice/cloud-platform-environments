2a39c68c61e5414428235dc1b587516c553e505d
spencer.howe <spencer.howe@justice.gov.uk>
2026-01-19 17:11:50 +0000

    Revert "feat: remove dev ECR"
    This reverts commit 4ea40940e35cff03d606e804f65d9c1cbd78514e.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/ecr.tf
@@ -0,0 +1,20 @@
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
+
+  # Repository configuration
+  repo_name = var.namespace
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github"]
+  github_repositories = ["laa-amend-a-claim"]
+  github_environments = ["dev"]
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
