c812d000090c8afb2ad1950c4e0125c34946c253
spencer.howe <spencer.howe@justice.gov.uk>
2026-01-19 15:49:32 +0000

    feat: add amend a claim production environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-prod/resources/elasticache.tf
@@ -0,0 +1,31 @@
+module "elasticache_redis" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.0.0"
+
+  vpc_name               = var.vpc_name
+  application            = var.application
+  environment_name       = var.environment
+  is_production          = var.is_production
+  infrastructure_support = var.infrastructure_support
+  team_name              = var.team_name
+  namespace              = var.namespace
+  business_unit          = var.business_unit
+
+  engine_version          = "7.0"
+  parameter_group_name    = "default.redis7"
+  number_cache_clusters   = "2"
+  node_type               = "cache.t4g.micro"
+}
+
+resource "kubernetes_secret" "elasticache_redis" {
+  metadata {
+    name      = "elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.elasticache_redis.primary_endpoint_address
+    auth_token               = module.elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.elasticache_redis.member_clusters)
+    url                      = "rediss://:${module.elasticache_redis.auth_token}@${module.elasticache_redis.primary_endpoint_address}:6379"
+  }
+}
