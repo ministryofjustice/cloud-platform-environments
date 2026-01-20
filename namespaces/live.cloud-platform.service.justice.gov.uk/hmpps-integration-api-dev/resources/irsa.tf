6097e6a8600adbe32922af3207b029cf9575ef33
Toby Batch <toby.batch@digital.justice.gov.uk>
2026-01-20 12:33:49 +0000

    CPB-337: Adds config for community payback queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
@@ -4,13 +4,14 @@
 locals {
   # The names of the queues used and the namespace which created them.
   sqs_queues = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue"                                 = "hmpps-audit-dev",
-    "education-skills-work-employment-dev-hmpps_jobs_board_integration_queue"       = "hmpps-jobs-board-integration-dev",
-    "book-a-prison-visit-dev-hmpps_prison_visits_write_events_queue"                = "visit-someone-in-prison-backend-svc-dev",
-    "book-a-prison-visit-dev-hmpps_prison_visits_write_events_dlq"                  = "visit-someone-in-prison-backend-svc-dev",
-    "locations-inside-prison-development-update_from_external_system_events_queue"  = "hmpps-locations-inside-prison-dev"
-    "hmpps-farsight-reduce-re-offend-development-eawp_assessment_events_queue"      = "hmpps-education-and-work-plan-dev",
-    "activities-and-appointments-dev-update_from_external_system_events_queue"      = "hmpps-activities-management-dev"
+    "Digital-Prison-Services-dev-hmpps_audit_queue"                                    = "hmpps-audit-dev",
+    "education-skills-work-employment-dev-hmpps_jobs_board_integration_queue"          = "hmpps-jobs-board-integration-dev",
+    "book-a-prison-visit-dev-hmpps_prison_visits_write_events_queue"                   = "visit-someone-in-prison-backend-svc-dev",
+    "book-a-prison-visit-dev-hmpps_prison_visits_write_events_dlq"                     = "visit-someone-in-prison-backend-svc-dev",
+    "locations-inside-prison-development-update_from_external_system_events_queue"     = "hmpps-locations-inside-prison-dev"
+    "hmpps-farsight-reduce-re-offend-development-eawp_assessment_events_queue"         = "hmpps-education-and-work-plan-dev",
+    "hmpps-community-payback-devs-dev-community_campus_course_completion_events_queue" = "hmpps-community-payback-dev",
+    "activities-and-appointments-dev-update_from_external_system_events_queue"         = "hmpps-activities-management-dev"
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_topics = {
