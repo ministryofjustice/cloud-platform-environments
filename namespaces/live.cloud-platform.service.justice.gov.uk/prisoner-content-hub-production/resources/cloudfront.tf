bfa0dadf5604701d02797666701fd2ca473c01d2
Emterry <emma.terry@digital.justice.gov.uk>
2026-01-20 13:19:03 +0000

    testing bucket policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/cloudfront.tf
@@ -1,10 +1,11 @@
 module "cloudfront" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=s3-policy"
 
   # Configuration
-  bucket_id                     = module.drupal_content_storage_2.bucket_name
-  bucket_domain_name            = module.drupal_content_storage_2.bucket_domain_name
-  ip_allow_listing_environment  = var.environment-name
+  bucket_id                    = module.drupal_content_storage_2.bucket_name
+  bucket_domain_name           = module.drupal_content_storage_2.bucket_domain_name
+  ip_allow_listing_environment = var.environment-name
+  attach_bucket_policy         = false
 
   # Tags
   business_unit          = var.business_unit
