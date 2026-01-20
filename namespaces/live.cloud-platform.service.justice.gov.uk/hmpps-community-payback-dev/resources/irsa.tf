66594619b5dd8a5ee931f4d798cd7ff40a6be101
dhruv.patel <dhruv.patel@justice.gov.uk>
2026-01-20 17:20:48 +0000

    Added irsa hmpps audit config for community payback UIs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-payback-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-community-payback-dev/resources/irsa.tf
@@ -1,14 +1,13 @@
 locals {
+  sqs_queues = {
+    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev"
+  }
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
 
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
-}
-
-data "aws_ssm_parameter" "irsa_policy_arns_sns" {
-  for_each = local.sns_topics
-  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
 module "irsa-api" {
@@ -32,4 +31,42 @@ module "irsa-api" {
     },
     local.sns_policies
   )
+}
+
+module "irsa_ui" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-community-payback-ui-account"
+  namespace            = var.namespace
+  role_policy_arns = local.sqs_policies
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+module "irsa_supervisors_ui" {
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name     = var.eks_cluster_name
+  service_account_name = "hmpps-community-payback-supervisors-ui-account"
+  namespace            = var.namespace
+  role_policy_arns = local.sqs_policies
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sns" {
+  for_each = local.sns_topics
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
