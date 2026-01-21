d1cc709bec430fcd5d7cf118172a3e1801c4fc1b
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-01-21 10:54:12 +0000

    RASS-1517 setup analytical platform access for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-dev/resources/analytical-platform-access.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-remand-and-sentencing-dev/resources/analytical-platform-access.tf
@@ -23,7 +23,7 @@ module "ap_irsa" {
 
 # set up the service pod
 module "ap_service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
 
   # Configuration
   namespace            = var.namespace
