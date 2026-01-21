299a66eebb60365b93a0004492f5dc3a99d8a4f3
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2026-01-21 14:53:53 +0000

    LNP-592/CP #7845: update Content Hub  cloudfront module to 1.6.0 and explicitly tell it not to overwite the s3 bucket policy.

--- namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/cloudfront.tf
@@ -1,10 +1,11 @@
 module "cloudfront" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-cloudfront?ref=1.6.0"
 
   # Configuration
   bucket_id                     = module.drupal_content_storage_2.bucket_name
   bucket_domain_name            = module.drupal_content_storage_2.bucket_domain_name
   ip_allow_listing_environment  = var.environment-name
+  attach_bucket_policy          = false
 
   # Tags
   business_unit          = var.business_unit
@@ -14,4 +15,5 @@ module "cloudfront" {
   namespace              = var.namespace
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
+  service_area           = var.service_area
 }
