904463611d3bc6df3b853660b5b31ee860a31844
dhruv.patel <dhruv.patel@justice.gov.uk>
2026-01-21 11:14:10 +0000

    Removed changes to hmpps integration api

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
@@ -10,7 +10,6 @@ locals {
     "book-a-prison-visit-dev-hmpps_prison_visits_write_events_dlq"                     = "visit-someone-in-prison-backend-svc-dev",
     "locations-inside-prison-development-update_from_external_system_events_queue"     = "hmpps-locations-inside-prison-dev"
     "hmpps-farsight-reduce-re-offend-development-eawp_assessment_events_queue"         = "hmpps-education-and-work-plan-dev",
-    "hmpps-community-payback-devs-dev-community_campus_course_completion_events_queue" = "hmpps-community-payback-dev",
     "activities-and-appointments-dev-update_from_external_system_events_queue"         = "hmpps-activities-management-dev"
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
