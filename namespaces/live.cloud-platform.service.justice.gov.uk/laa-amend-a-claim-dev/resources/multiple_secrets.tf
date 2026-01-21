55b9e369438ba26b5fd3e0fd094b3bfc703c3428
suma.challa <suma.challa@justice.gov.uk>
2026-01-21 13:53:01 +0000

    BC-353 added secrets to use for e2e tests

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/multiple_secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/multiple_secrets.tf
@@ -45,5 +45,15 @@ module "secrets_manager" {
       recovery_window_in_days = 7
       k8s_secret_name         = "laa-amend-a-claim-claims-api-token" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
     }
+    "laa-amend-a-claim-e2e-claims-api-token" = {
+      description             = "Claims Api Mock Token used for e2e tests",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-e2e-claims-api-token" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    }
+    "laa-amend-a-claim-e2e-github-token" = {
+      description             = "Github Token used for e2e tests",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "laa-amend-a-claim-e2e-github-token" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    }
   }
 }
