79f1bc0f9309c6f6afff9d2ad3962979f5a4d3da
Damien Wilson <damien.wilson@justice.gov.uk>
2026-01-21 16:27:10 +0000

    Fix app_repo + namespace (#39573)
    * Fix app_repo + namespace
    
    * Remove app from repo name

--- namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/variables.tf
@@ -11,7 +11,7 @@ variable "eks_cluster_name" {
 variable "application" {
   description = "Name of Application you are deploying"
   type        = string
-  default     = "Connecting Services Prototype"
+  default     = "PFL Connecting Services App"
 }
 
 variable "namespace" {
@@ -77,5 +77,5 @@ variable "oidc_name" {
 variable "app_repo" {
   description = "Name of application repository"
   type        = string
-  default     = "connecting-services-prototype"
+  default     = "pfl-connecting-services"
 }
