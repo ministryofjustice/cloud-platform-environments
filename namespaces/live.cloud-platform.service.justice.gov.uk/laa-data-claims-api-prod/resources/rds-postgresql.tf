ba4dd3c7c2e9e65bca837433f2b08b7def7b3fad
Phil Payne <phil.payne@digital.justice.gov.uk>
2026-01-23 09:42:51 +0000

    Upgrade db instance size

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-prod/resources/rds-postgresql.tf
@@ -14,7 +14,7 @@ module "rds" {
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
+  db_max_allocated_storage     = "10000"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
   deletion_protection          = true
@@ -42,7 +42,7 @@ module "rds" {
   db_engine         = "postgres"
   db_engine_version = "17.4"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
   rds_family        = "postgres17"
-  db_instance_class = "db.t4g.small"
+  db_instance_class = "db.m6g.large"
 
   # Tags
   application            = var.application
