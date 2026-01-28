aa368197079f6f1a15849417f69d39abddfa83e9
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-01-28 17:25:51 +0000

    extended pathfinder events in domain events queue

--- namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/pathfinder-api-domain-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pathfinder-dev/resources/pathfinder-api-domain-events-queue.tf
@@ -107,7 +107,15 @@ resource "aws_sns_topic_subscription" "pathfinder_api_queue_for_domain_events_su
   endpoint  = module.pathfinder_api_queue_for_domain_events.sqs_arn
   filter_policy = jsonencode({
     eventType = [
-      "adjudication.report.created"
+      "adjudication.report.created",
+      "prison-offender-events.prisoner.released",
+      "prison-offender-events.prisoner.received",
+      "OFFENDER_BOOKING-REASSIGNED",
+      "OFFENDER_UPDATED",
+      "BOOKING_NUMBER-CHANGED",
+      "BED_ASSIGNMENT_HISTORY-INSERTED",
+      "SENTENCE_DATES-CHANGED",
+      "CONFIRMED_RELEASE_DATE-CHANGED"
     ]
   })
 }
