28a209f01c0eda9eaac325f3dcba0904533ba768
Jon Brighton <brightonsbox@hotmail.com>
2026-01-28 13:27:00 +0000

    CDPS-1784: Updating person communication needs github team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-communication-needs-api-prod/resources/template-kotlin.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-communication-needs-api-prod/resources/template-kotlin.tf
@@ -1,15 +1,16 @@
 module "hmpps_template_kotlin" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
-  github_repo = "hmpps-person-communication-needs-api"
-  application = "hmpps-person-communication-needs-api"
-  github_team = "connect-dps"
-  environment = var.environment
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-person-communication-needs-api"
+  application                   = "hmpps-person-communication-needs-api"
+  github_team                   = var.github_review_team
+  environment                   = var.environment
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
   application_insights_instance = "prod"
-  reviewer_teams                = ["connect-dps","hmpps-sre"]
+  reviewer_teams                = [var.github_deployment_team]
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
 }
