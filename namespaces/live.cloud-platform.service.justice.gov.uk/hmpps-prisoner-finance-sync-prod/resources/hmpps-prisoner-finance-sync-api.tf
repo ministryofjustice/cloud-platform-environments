a89ef13dc651f466423fc6b23c0485775cf67c93
Pietro Convalle <pietro.convalle@madetech.com>
2026-01-28 11:37:11 +0000

    Prisoner-finance-sync fixed reviewer team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-prod/resources/hmpps-prisoner-finance-sync-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-prod/resources/hmpps-prisoner-finance-sync-api.tf
@@ -4,7 +4,7 @@ module "hmpps_prisoner_finance_sync_api" {
   application                   = "hmpps-prisoner-finance-sync-api"
   github_team                   = "hmpps-prisoner-finance-live"
   environment                   = var.environment
-  reviewer_teams                = [var.team_name]
+  reviewer_teams                = ["hmpps-prisoner-finance-live"]
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
   application_insights_instance = "prod"
