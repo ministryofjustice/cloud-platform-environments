5e9a5b81c083cf81c5983f06b1064a52dbb214e5
Marcus Aspin <maspin@unilink.com>
2026-01-29 12:29:28 +0000

    PI-3784 Re-add missing SNS permissions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
@@ -52,7 +52,8 @@ module "hmpps-integration-event-irsa" {
       s3                                              = module.certificate_backup.irsa_policy_arn,
       truststore                                      = module.truststore_s3_bucket.irsa_policy_arn,
       secrets                                         = aws_iam_policy.secrets_manager_access.arn,
-      sqs                                             = aws_iam_policy.integration_events_sqs.arn
+      event_topic                                     = module.hmpps-integration-events.irsa_policy_arn,
+      event_queues                                    = aws_iam_policy.integration_events_sqs.arn
       subscription_management                         = aws_iam_policy.subscription_management.arn
     }
   )
