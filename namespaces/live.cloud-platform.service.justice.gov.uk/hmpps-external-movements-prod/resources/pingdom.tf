6826993c2213858e40530242f92a79e7bfa46eb9
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-01-29 18:38:51 +0000

    update pingdom info

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/pingdom.tf
@@ -2,7 +2,7 @@ provider "pingdom" {}
 
 resource "pingdom_check" "pingdom_api" {
   type                     = "http"
-  name                     = "HMPPS external-movements API"
+  name                     = "HMPPS External Movements API"
   host                     = "health-kick.prison.service.justice.gov.uk"
   port                     = 443
   url                      = "/https/external-movements-api.hmpps.service.justice.gov.uk"
@@ -11,14 +11,14 @@ resource "pingdom_check" "pingdom_api" {
   sendnotificationwhendown = 6
   notifyagainevery         = 0
   encryption               = true
-  tags                     = "hmpps,cloudplatform-managed,hmpps-external-movements-api,hmpps-external-movements-api,isproduction_true"
+  tags                     = "hmpps,cloudplatform-managed,hmpps-external-movements-api,isproduction_true"
   probefilters             = "region:EU"
   integrationids           = [136330] # move-and-improve-alerts
 }
 
 resource "pingdom_check" "pingdom_ui" {
   type                     = "http"
-  name                     = "HMPPS external-movements UI"
+  name                     = "HMPPS External Movements UI"
   host                     = "health-kick.prison.service.justice.gov.uk"
   port                     = 443
   url                      = "/https/external-movements.hmpps.service.justice.gov.uk"
@@ -27,7 +27,7 @@ resource "pingdom_check" "pingdom_ui" {
   sendnotificationwhendown = 6
   notifyagainevery         = 0
   encryption               = true
-  tags                     = "hmpps,cloudplatform-managed,hmpps-external-movements-ui,hmpps-external-movements-ui,isproduction_true"
+  tags                     = "hmpps,cloudplatform-managed,hmpps-external-movements-ui,isproduction_true"
   probefilters             = "region:EU"
   integrationids           = [136330] # move-and-improve-alerts
 }
