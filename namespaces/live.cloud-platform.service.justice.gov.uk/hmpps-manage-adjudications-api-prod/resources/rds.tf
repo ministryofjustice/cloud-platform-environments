5722ff28e085f51b92ab3a8b294d228967fcdbf9
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-02-04 15:36:16 +0000

    MAP-1437 Add read-replica configuration and update RDS parameters for HMPPS Manage Adjudications prod environment, including security group integration.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/rds.tf
@@ -1,6 +1,5 @@
 module "ma_rds" {
   source                      = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
-  storage_type                = "gp2"
   vpc_name                    = var.vpc_name
   team_name                   = var.team_name
   business_unit               = var.business_unit
@@ -9,24 +8,137 @@ module "ma_rds" {
   namespace                   = var.namespace
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
-  allow_major_version_upgrade = "false"
+
   db_instance_class           = "db.t4g.small"
-  db_engine_version           = "15"
   rds_family                  = "postgres15"
+  db_engine_version           = "15"
+  deletion_protection         = true
   db_password_rotated_date    = "15-02-2023"
+  allow_major_version_upgrade = "false"
   allow_minor_version_upgrade = "true"
+
+  storage_type                = "gp2"
+  db_allocated_storage        = "1500"
+
   db_engine                   = "postgres"
+  prepare_for_major_upgrade   = false
+
   backup_window               = var.backup_window
   maintenance_window          = var.maintenance_window
-  deletion_protection         = true
-  prepare_for_major_upgrade   = false
-  db_allocated_storage        = "1500"
 
 
   providers = {
     aws = aws.london
   }
 
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
+      apply_method = "immediate"
+    }
+  ]
+  # Add security groups for DPR
+  vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
+}
+
+# To create a read replica, use the below code and update the values to specify the RDS instance
+# from which you are replicating. In this example, we're assuming that rds is the
+# source RDS instance and read-replica is the replica we are creating.
+
+module "dps_rds_replica" {
+  # default off
+  count  = 1
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+
+  vpc_name               = var.vpc_name
+
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  # If any other inputs of the RDS is passed in the source db which are different from defaults,
+  # add them to the replica
+
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "15"
+  rds_family        = "postgres15"
+  db_instance_class = "db.t4g.small"
+  # It is mandatory to set the below values to create read replica instance
+
+  # Set the database_name of the source db
+  db_name = null # "db_name": conflicts with replicate_source_db
+
+  # Set the db_identifier of the source db
+  replicate_source_db = module.ma_rds.db_identifier
+
+  # Set to true. No backups or snapshots are created for read replica
+  skip_final_snapshot        = "true"
+  db_backup_retention_period = 0
+
+  # If db_parameter is specified in source rds instance, use the same values.
+  # If not specified you dont need to add any. It will use the default values.
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
+      apply_method = "immediate"
+    },
+    {
+      name         = "hot_standby_feedback"
+      value        = "1"
+      apply_method = "immediate"
+    }
+  ]
+
+  # Add security groups for DPR
+  vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
 }
 
 resource "kubernetes_secret" "dps_rds" {
@@ -36,6 +148,8 @@ resource "kubernetes_secret" "dps_rds" {
   }
 
   data = {
+    db_identifier         = module.ma_rds.db_identifier
+    resource_id           = module.ma_rds.resource_id
     rds_instance_endpoint = module.ma_rds.rds_instance_endpoint
     database_name         = module.ma_rds.database_name
     database_username     = module.ma_rds.database_username
@@ -43,4 +157,26 @@ resource "kubernetes_secret" "dps_rds" {
     rds_instance_address  = module.ma_rds.rds_instance_address
     url                   = "postgres://${module.ma_rds.database_username}:${module.ma_rds.database_password}@${module.ma_rds.rds_instance_endpoint}/${module.ma_rds.database_name}"
   }
+}
+
+resource "kubernetes_secret" "dps_rds_replica" {
+  # default off
+  count = 1
+
+  metadata {
+    name      = "dps-rds-read-replica-output"
+    namespace = var.namespace
+  }
+  # The database_username, database_password, database_name values are same as the source RDS instance.
+  # Uncomment if count > 0
+
+  data = {
+    rds_instance_endpoint = module.dps_rds_replica[0].rds_instance_endpoint
+    rds_instance_address  = module.dps_rds_replica[0].rds_instance_address
+  }
+}
+
+# Retrieve mp_dps_sg_name SG group ID, CP-MP-INGRESS
+data "aws_security_group" "mp_dps_sg" {
+  name = var.mp_dps_sg_name
 }
