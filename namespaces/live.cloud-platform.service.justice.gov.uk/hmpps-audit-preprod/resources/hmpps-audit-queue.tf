96fed4f143c96d6625b409837789304870bbcb06
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-02-05 10:25:11 +0000

    HAAR-4131: delete preprod prisoner-audit-queue/dlq so that queue can be renamed

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-preprod/resources/hmpps-audit-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-preprod/resources/hmpps-audit-queue.tf
@@ -76,83 +76,84 @@ resource "kubernetes_secret" "hmpps_audit_dead_letter_queue_secret" {
   }
 }
 
-module "hmpps_prisoner_audit_queue" {
-
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
-
-  # Queue configuration
-  sqs_name                  = "hmpps_prisoner_audit_queue"
-  encrypt_sqs_kms           = "true"
-  message_retention_seconds = 1209600
-
-  redrive_policy = <<EOF
-  {
-    "deadLetterTargetArn": "${module.hmpps_prisoner_audit_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
-  }
-
-EOF
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
-  environment_name       = var.environment-name
-  infrastructure_support = var.infrastructure_support
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-
-module "hmpps_prisoner_audit_dead_letter_queue" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
-
-  # Queue configuration
-  sqs_name        = "hmpps_prisoner_audit_dlq"
-  encrypt_sqs_kms = "true"
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the queue
-  namespace              = var.namespace
-  environment_name       = var.environment-name
-  infrastructure_support = var.infrastructure_support
-
-  providers = {
-    aws = aws.london
-  }
-}
-
-resource "kubernetes_secret" "hmpps_prisoner_audit_queue_secret" {
-  metadata {
-    name      = "sqs-prisoner-audit-queue-secret"
-    namespace = var.namespace
-  }
-
-  data = {
-    sqs_queue_url  = module.hmpps_prisoner_audit_queue.sqs_id
-    sqs_queue_arn  = module.hmpps_prisoner_audit_queue.sqs_arn
-    sqs_queue_name = module.hmpps_prisoner_audit_queue.sqs_name
-  }
-}
-
-resource "kubernetes_secret" "hmpps_prisoner_audit_dead_letter_queue_secret" {
-  metadata {
-    name      = "sqs-prisoner-audit-queue-dl-secret"
-    namespace = var.namespace
-  }
-
-  data = {
-    sqs_queue_url  = module.hmpps_prisoner_audit_dead_letter_queue.sqs_id
-    sqs_queue_arn  = module.hmpps_prisoner_audit_dead_letter_queue.sqs_arn
-    sqs_queue_name = module.hmpps_prisoner_audit_dead_letter_queue.sqs_name
-  }
-}
+# commented out so that queue will be deleted before rename
+# module "hmpps_prisoner_audit_queue" {
+#
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+#
+#   # Queue configuration
+#   sqs_name                  = "hmpps_prisoner_audit_queue"
+#   encrypt_sqs_kms           = "true"
+#   message_retention_seconds = 1209600
+#
+#   redrive_policy = <<EOF
+#   {
+#     "deadLetterTargetArn": "${module.hmpps_prisoner_audit_dead_letter_queue.sqs_arn}","maxReceiveCount": 3
+#   }
+#
+# EOF
+#
+#   # Tags
+#   business_unit          = var.business_unit
+#   application            = var.application
+#   is_production          = var.is_production
+#   team_name              = var.team_name # also used for naming the queue
+#   namespace              = var.namespace
+#   environment_name       = var.environment-name
+#   infrastructure_support = var.infrastructure_support
+#
+#   providers = {
+#     aws = aws.london
+#   }
+# }
+#
+#
+# module "hmpps_prisoner_audit_dead_letter_queue" {
+#   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
+#
+#   # Queue configuration
+#   sqs_name        = "hmpps_prisoner_audit_dlq"
+#   encrypt_sqs_kms = "true"
+#
+#   # Tags
+#   business_unit          = var.business_unit
+#   application            = var.application
+#   is_production          = var.is_production
+#   team_name              = var.team_name # also used for naming the queue
+#   namespace              = var.namespace
+#   environment_name       = var.environment-name
+#   infrastructure_support = var.infrastructure_support
+#
+#   providers = {
+#     aws = aws.london
+#   }
+# }
+#
+# resource "kubernetes_secret" "hmpps_prisoner_audit_queue_secret" {
+#   metadata {
+#     name      = "sqs-prisoner-audit-queue-secret"
+#     namespace = var.namespace
+#   }
+#
+#   data = {
+#     sqs_queue_url  = module.hmpps_prisoner_audit_queue.sqs_id
+#     sqs_queue_arn  = module.hmpps_prisoner_audit_queue.sqs_arn
+#     sqs_queue_name = module.hmpps_prisoner_audit_queue.sqs_name
+#   }
+# }
+#
+# resource "kubernetes_secret" "hmpps_prisoner_audit_dead_letter_queue_secret" {
+#   metadata {
+#     name      = "sqs-prisoner-audit-queue-dl-secret"
+#     namespace = var.namespace
+#   }
+#
+#   data = {
+#     sqs_queue_url  = module.hmpps_prisoner_audit_dead_letter_queue.sqs_id
+#     sqs_queue_arn  = module.hmpps_prisoner_audit_dead_letter_queue.sqs_arn
+#     sqs_queue_name = module.hmpps_prisoner_audit_dead_letter_queue.sqs_name
+#   }
+# }
 
 module "hmpps_audit_users_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
