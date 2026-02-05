2d04a362db4f016f447ba6c78f2fee80d66b035e
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-02-05 10:01:51 +0000

    added github actions config and updated pingdom for pcms api

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-prod/resources/pingdom.tf
@@ -4,6 +4,7 @@ provider "pingdom" {
 # Integration IDs
 # 96624 = #dps_alerts
 # 96628 = DPS Pager duty
+# 126838 = #dps_sed_alerts
 
 resource "pingdom_check" "pcms-production-check" {
   type                     = "http"
@@ -18,6 +19,22 @@ resource "pingdom_check" "pcms-production-check" {
   port                     = 443
   tags                     = "dps,hmpps,cloudplatform-managed"
   probefilters             = "region:EU"
-  integrationids           = [96624, 96628]
+  integrationids           = [96624, 96628, 126838]
+}
+
+resource "pingdom_check" "pcms-production-check" {
+  type                     = "http"
+  name                     = "DPS - ${var.application} API"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  url                      = "/https/pcms-api.prison.service.justice.gov.uk"
+  encryption               = true
+  port                     = 443
+  tags                     = "dps,hmpps,cloudplatform-managed"
+  probefilters             = "region:EU"
+  integrationids           = [96624, 126838]
 }
 
