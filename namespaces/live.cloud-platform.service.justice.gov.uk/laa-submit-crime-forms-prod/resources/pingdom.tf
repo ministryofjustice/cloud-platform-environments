71fe9c80c39d2a64e508c3a92f97fa74b44075fd
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-02-09 15:06:54 +0000

    add pingdom dashboard tag

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-submit-crime-forms-prod/resources/pingdom.tf
@@ -12,7 +12,7 @@ resource "pingdom_check" "laa-submit-crime-forms-prod" {
   url              = "/ping"
   encryption       = true
   port             = 443
-  tags             = "businessunit_${var.business_unit},application_${var.namespace},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  tags             = "businessunit_${var.business_unit},application_${var.namespace},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name},laa_production_environment_dashboard"
   probefilters     = "region:EU"
   integrationids   = [135343]
 }
