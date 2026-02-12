2d1abd3a3f4213400ed9bfa83b1c138a403b7aee
suma.challa <suma.challa@justice.gov.uk>
2026-02-12 14:17:35 +0000

    BC-441 pingdom for dev environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-dev/resources/pingdom.tf
@@ -0,0 +1,24 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-amend-a-claim-dev-pingdom" {
+  type             = "http"
+  name             = "${var.application} ${var.environment} - ping"
+  host             = "laa-amend-a-claim-dev.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  sendnotificationwhendown = 6
+  notifyagainevery = 0
+  url              = "/actuator/health"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_${var.business_unit},application_${var.namespace},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  probefilters     = "region:EU"
+  integrationids   = [146737]
+
+  # Optional: expected HTTP status code
+  # This ensures Pingdom marks the check as "up" only when it gets a 200 response
+  requestheaders = {
+    "Accept" = "application/json"
+  }
+}
