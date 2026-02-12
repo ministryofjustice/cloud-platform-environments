f0bb73451e5abd817fcbdf805e7f82b995b5b51b
mukesh-moj <mukesh.kumar@justice.gov.uk>
2026-02-12 13:00:25 +0000

    files added to create a namespace for laa-fee-scheme-ui

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-fee-scheme-ui-dev/resources/secret.tf
@@ -0,0 +1,24 @@
+module "secrets_manager" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.6"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "fee-scheme-api-secrets" = {
+      description             = "Fee Scheme UI secrets for DEV",
+      recovery_window_in_days = 7
+      k8s_secret_name         = "fee-scheme-ui-secrets"
+    },
+    "fee-scheme-api-slack-webhook" = {
+      description             = "Fee Scheme UI slack webhook url",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "fee-scheme-ui-slack-webhook"
+    },
+  }
+}
