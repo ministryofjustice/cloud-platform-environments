3b1db07bd1a0ec6688bac890a1f83bf123a82f2d
Murray Lippiatt <murray.lippiatt1@digital.justice.gov.uk>
2026-02-12 10:29:47 +0000

    Update variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-redact-prototype/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-redact-prototype/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "justiceredact-prototype"
+  default     = "justice-redact-prototype"
 }
 
 variable "service_area" {
