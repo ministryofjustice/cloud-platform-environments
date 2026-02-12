772a06d3caa38c6b6156ff8b0c09cb35c132422c
suma.challa <suma.challa@justice.gov.uk>
2026-02-12 17:41:46 +0000

    BC-441 pingdom for production environment

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-production/resources/pingdom.tf
@@ -0,0 +1,18 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-amend-a-claim-prod-pingdom" {
+  type             = "http"
+  name             = "${var.application} ${var.environment} - ping"
+  host             = "laa-amend-a-claim-prod.apps.live.cloud-platform.service.justice.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  sendnotificationwhendown = 6
+  notifyagainevery = 0
+  url              = "/"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_${var.business_unit},application_${var.namespace},component_ping,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.team_name}"
+  probefilters     = "region:EU"
+  integrationids   = [146738]
+}
