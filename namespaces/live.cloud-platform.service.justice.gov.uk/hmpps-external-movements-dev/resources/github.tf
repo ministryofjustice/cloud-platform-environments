0fec12a57beff4f498c80be4d70b84238f981ee3
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-02-12 15:14:04 +0000

    Setup github environments for hmpps-document-generation services

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-dev/resources/github.tf
@@ -29,3 +29,35 @@ module "external-movements-ui" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "document-generation-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-document-generation-api"
+  application                   = "hmpps-document-generation-api"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "document-generation-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  github_repo                   = "hmpps-document-generation-ui"
+  application                   = "hmpps-document-generation-ui"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
