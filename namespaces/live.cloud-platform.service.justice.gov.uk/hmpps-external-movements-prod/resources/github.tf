c1f5134b5896c4c8b99a9f002dd4fcba2fab13f3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-02-13 10:23:32 +0000

    MIA-1380 doc gen service infra

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/github.tf
@@ -31,3 +31,37 @@ module "external-movements-ui" {
   github_owner                  = var.github_owner
   reviewer_teams                = [var.team_name, "hmpps-move-and-improve"]
 }
+
+module "document-generation-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-document-generation-api"
+  application                   = "hmpps-document-generation-api"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = [var.team_name, "hmpps-move-and-improve"]
+}
+
+module "document-generation-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-document-generation-ui"
+  application                   = "hmpps-document-generation-ui"
+  github_team                   = var.team_name
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = [var.team_name, "hmpps-move-and-improve"]
+}
--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/pingdom.tf
@@ -31,3 +31,35 @@ resource "pingdom_check" "pingdom_ui" {
   probefilters             = "region:EU"
   integrationids           = [136330] # move-and-improve-alerts
 }
+
+resource "pingdom_check" "doc_gen_api" {
+  type                     = "http"
+  name                     = "HMPPS Document Generation API"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  port                     = 443
+  url                      = "/https/document-generation-api.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  encryption               = true
+  tags                     = "hmpps,cloudplatform-managed,hmpps-document-generation-api,isproduction_true"
+  probefilters             = "region:EU"
+  integrationids           = [136330] # move-and-improve-alerts
+}
+
+resource "pingdom_check" "doc_gen_ui" {
+  type                     = "http"
+  name                     = "HMPPS Document Generation UI"
+  host                     = "health-kick.prison.service.justice.gov.uk"
+  port                     = 443
+  url                      = "/https/document-generation.hmpps.service.justice.gov.uk"
+  resolution               = 1
+  notifywhenbackup         = true
+  sendnotificationwhendown = 6
+  notifyagainevery         = 0
+  encryption               = true
+  tags                     = "hmpps,cloudplatform-managed,hmpps-document-generation-ui,isproduction_true"
+  probefilters             = "region:EU"
+  integrationids           = [136330] # move-and-improve-alerts
+}
