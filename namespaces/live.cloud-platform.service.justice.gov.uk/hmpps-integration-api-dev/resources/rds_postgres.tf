b90c1bb348e51cb70b6e68391194a18c0951233d
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-02-16 15:04:49 +0000

    HIA-1261: Upgrade to integration events db to Postgres 16 - dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/rds_postgres.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/rds_postgres.tf
@@ -19,9 +19,10 @@ module "rds" {
   db_max_allocated_storage     = "500"
 
   # PostgresSQL specifics
+  prepare_for_major_upgrade = true
   db_engine         = "postgres"
-  db_engine_version = "15"
-  rds_family        = "postgres15"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
   # Tags
