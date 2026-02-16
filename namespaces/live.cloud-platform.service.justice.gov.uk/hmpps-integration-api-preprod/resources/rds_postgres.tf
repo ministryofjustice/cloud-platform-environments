c717a13cbe06aff619695a3365a3b6b1399cb672
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-02-16 16:36:24 +0000

    HIA-1261: Upgrade version and size of preprod events DB

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/rds_postgres.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-preprod/resources/rds_postgres.tf
@@ -19,10 +19,11 @@ module "rds" {
   db_max_allocated_storage     = "500"
 
   # PostgresSQL specifics
+  prepare_for_major_upgrade = true
   db_engine         = "postgres"
-  db_engine_version = "15"
-  rds_family        = "postgres15"
-  db_instance_class = "db.t4g.micro"
+  db_engine_version = "16"
+  rds_family        = "postgres16"
+  db_instance_class = "db.t4g.small"
 
   # Tags
   application            = var.application
