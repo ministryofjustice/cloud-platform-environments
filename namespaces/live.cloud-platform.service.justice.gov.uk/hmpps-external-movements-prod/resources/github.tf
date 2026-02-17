f6ad2f30580f361859160e323570409a0fc156bd
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-02-17 17:23:10 +0000

    Set source template repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/github.tf
@@ -58,7 +58,7 @@ module "document-generation-ui" {
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
   application_insights_instance = var.environment
-  source_template_repo          = "none"
+  source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
