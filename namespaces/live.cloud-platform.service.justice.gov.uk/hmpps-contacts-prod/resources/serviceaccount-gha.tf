bb3f1be9c11acda511f6c4a0a91fe6c030fcf55d
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-02-18 08:12:57 +0000

    Update serviceaccount-gha.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-prod/resources/serviceaccount-gha.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-contacts-prod/resources/serviceaccount-gha.tf
@@ -9,7 +9,7 @@ module "hmpps-contacts-ui" {
   protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
-  source_template_repo          = "none"
+  source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
