3881fccd89b2040e837e7107d425af1c2aa12906
David Teeling <david.teeling1@justice.gov.uk>
2026-02-18 12:41:21 +0000

    Typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-prod/resources/irsa.tf
@@ -10,7 +10,6 @@ module "irsa" {
     s3        = module.s3_bucket.irsa_policy_arn
     migration = aws_iam_policy.migration_policy.arn
     athena    = aws_iam_policy.athena_allow_irsa.arn
-
   }
 
   # Tags
