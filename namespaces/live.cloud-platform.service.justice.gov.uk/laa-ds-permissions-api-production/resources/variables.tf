13399d5872e75e360949f3eb20a3d7d0714e1942
Gabriele Bernotaite <gab.bernotaite@outlook.com>
2026-02-23 11:42:49 +0000

    AD-1210 LAA Permissions API production - rename repo and add sercive account module

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-ds-permissions-api-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ds-permissions-api-production/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "laa-data-stewardship-permissions-api-production"
+  default     = "laa-ds-permissions-api-production"
 }
 
 variable "service_area" {
