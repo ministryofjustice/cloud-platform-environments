17da8ede0def66fbc6e36ee66158a13213b5d3d4
david-stuart-justice <David.Stuart@justice.gov.uk>
2026-02-23 09:27:40 +0000

    Add irsa terraform for AWS S3 and DynamoDB in uat

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-access-api-uat/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-access-api-uat/resources/irsa.tf
@@ -24,7 +24,7 @@ module "irsa" {
 
 # set up the service pod
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
 
   # Configuration
   namespace            = var.namespace
