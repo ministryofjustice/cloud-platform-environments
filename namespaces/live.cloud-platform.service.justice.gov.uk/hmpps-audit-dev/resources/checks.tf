9cbcc4d8796f3c9035ecadf1d6fea963e4bd38df
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2026-02-24 09:38:51 +0000

    HAAR-5037: Removed check block for client list validation.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/checks.tf
+++
@@ -1,9 +0,0 @@
-check "approved_prisoner_clients_exist" {
-    assert {
-        condition = alltrue([
-        for client in var.approved_prisoner_audit_clients :
-        contains(keys(data.kubernetes_secret.approved_prisoner_audit_client_arns.data), client)
-        ])
-        error_message = "The approved client list does not match the keys in the approved_prisoner_audit_client_arns Kubernetes secret."
-    }
-}
