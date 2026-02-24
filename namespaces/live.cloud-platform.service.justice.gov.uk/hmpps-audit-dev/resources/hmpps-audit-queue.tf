66923f50edc9c275aa209b2422f7f1882162fb1c
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2026-02-24 09:12:13 +0000

    HAAR-5037: Check blocks moved into a separate checks.tf file.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-audit-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-audit-dev/resources/hmpps-audit-queue.tf
@@ -248,16 +248,6 @@ data "kubernetes_secret" "approved_prisoner_audit_client_arns" {
   }
 }
 
-  check "approved_prisoner_clients_exist" {
-    assert {
-      condition = alltrue([
-        for client in var.approved_prisoner_audit_clients :
-        contains(keys(data.kubernetes_secret.approved_prisoner_audit_client_arns.data), client)
-      ])
-      error_message = "The approved client list does not match the keys in the approved_prisoner_audit_client_arns Kubernetes secret."
-    }
-  }
-
 locals {
   prisoner_audit_client_arns = [for approved_client in var.approved_prisoner_audit_clients : data.kubernetes_secret.approved_prisoner_audit_client_arns.data[approved_client]]
 
