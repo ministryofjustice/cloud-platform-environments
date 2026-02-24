4d21d469e89ca6e5917c324effa599f19b2d59d1
Drew Clayton <drew.clayton@justice.gov.uk>
2026-02-24 16:43:18 +0000

    updated serviceaccount_name

--- namespaces/live.cloud-platform.service.justice.gov.uk/record-controlled-work-uat/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/record-controlled-work-uat/resources/serviceaccount.tf
@@ -5,6 +5,7 @@ module "serviceaccount" {
   kubernetes_cluster = var.kubernetes_cluster
 
   serviceaccount_token_rotated_date = "01-01-2000"
+  serviceaccount_name = var.serviceaccount_name
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
--- namespaces/live.cloud-platform.service.justice.gov.uk/record-controlled-work-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/record-controlled-work-uat/resources/variables.tf
@@ -73,3 +73,9 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
+
+variable "serviceaccount_name" {
+  type        = string
+  description = "Name of the service account used by GitHub Actions to deploy the applications to cloud-platform"
+  default     = "rcw-github-action-service-account"
+}
