3766d948bc5c6afa4d5665d8a89f48611824a9f6
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-02-25 10:02:45 +0000

    Add GHA service account for PVB dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prison-visits-booking-dev/resources/serviceaccount-gha.tf
@@ -0,0 +1,33 @@
+module "prison-visits-public" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "prison-visits-public"
+  application = "prison-visits-public"
+  github_team = "hmpps-prison-visits-booking-live"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-prison-visits-booking-devs"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main", "**/**", "**"] # Optional
+  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
+module "prison-visits-staff" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo = "prison-visits-2"
+  application = "prison-visits-staff"
+  github_team = "hmpps-prison-visits-booking-live"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-prison-visits-booking-devs"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main", "**/**", "**"] # Optional
+  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
