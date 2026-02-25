73cbd71e64e8f8f17d312655a4f7ed14f14824f7
Aron-v1 <aron.marriottsmith@justice.gov.uk>
2026-02-25 16:10:51 +0000

    Add module hmpps-user-preferences to prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-prod/resources/court-event-github.tf
@@ -47,3 +47,20 @@ module "pre-sentence-service" {
   github_owner                  = var.github_owner
   reviewer_teams                = ["probation-integration"]
 }
+
+module "hmpps-user-preferences" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-user-preferences"
+  application                   = "hmpps-user-preferences"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+  reviewer_teams                = ["probation-integration"]
+}
