bc6dbbc8035307df3cd4b88b58a2426dab5262f4
Eve Bramley <eve.bramley@digital.justice.gov.uk>
2026-02-26 14:15:19 +0000

    updated is_migration to false

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
@@ -70,7 +70,7 @@ module "rds" {
   namespace                   = var.namespace
   environment_name            = var.environment
   infrastructure_support      = var.infrastructure_support
-  is_migration                = true
+  is_migration                = false
 
   enable_rds_auto_start_stop = true
 
