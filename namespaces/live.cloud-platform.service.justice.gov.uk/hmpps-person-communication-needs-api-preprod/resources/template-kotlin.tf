6519d9eb86a1f5facf774e1bdad8e55a76ced161
Igor <igor.ushkarev1@justice.gov.uk>
2026-02-26 11:07:42 +0000

    Use new Connect DPS review team for `hmpps-person-communication-needs-api-preprod`

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-communication-needs-api-preprod/resources/template-kotlin.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-communication-needs-api-preprod/resources/template-kotlin.tf
@@ -1,12 +1,14 @@
 module "hmpps_template_kotlin" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
-  github_repo = "hmpps-person-communication-needs-api"
-  application = "hmpps-person-communication-needs-api"
-  github_team = "connect-dps"
-  environment = var.environment
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "hmpps-person-communication-needs-api"
+  application                   = "hmpps-person-communication-needs-api"
+  github_owner                  = var.github_owner
+  github_team                   = var.github_review_team
+  environment                   = var.environment
   selected_branch_patterns      = ["main"]
   is_production                 = var.is_production
   application_insights_instance = "preprod"
+  reviewer_teams                = [var.github_deployment_team]
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
   namespace                     = var.namespace
