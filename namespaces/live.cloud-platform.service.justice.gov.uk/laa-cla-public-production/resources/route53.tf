7e89dc8ea7b71bb5e75a3ae3d774b36540796bfb
joselopez-moj <jose.lopez@justice.gov.uk>
2026-02-26 16:53:44 +0000

    refactor: add indentation to match file format

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-public-production/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-public-production/resources/route53.tf
@@ -23,11 +23,11 @@ resource "kubernetes_secret" "route53_zone_sec" {
 }
 
 resource "aws_route53_record" "checklegalaid_route53_txt_asvdns" {
-  zone_id = aws_route53_zone.route53_zone.zone_id
-  name = "_asvdns-31fc0209-db21-495d-8c86-9ef0e91341e4.checklegalaid.service.gov.uk"
-  type = "TXT"
-  ttl  = 300
-  records = [
+  zone_id   = aws_route53_zone.route53_zone.zone_id
+  name      = "_asvdns-31fc0209-db21-495d-8c86-9ef0e91341e4.checklegalaid.service.gov.uk"
+  type      = "TXT"
+  ttl       = 300
+  records   = [
     "asvdns_d3ab4a6c-a469-4338-8164-1b646903014a"
   ]
 }
