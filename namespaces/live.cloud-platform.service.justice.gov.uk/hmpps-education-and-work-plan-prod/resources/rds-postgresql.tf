b32069b26e1725cb023c12e9d4c2a016c893dcba
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-02-26 14:44:30 +0000

    add read replica for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-education-and-work-plan-prod/resources/rds-postgresql.tf
@@ -77,15 +77,21 @@ module "read_replica" {
   infrastructure_support = var.infrastructure_support
   is_production          = var.is_production
   namespace              = var.namespace
-  team_name = var.team_name
+  team_name              = var.team_name
 
   # RDS configuration
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
-  db_max_allocated_storage = "500"
+  db_max_allocated_storage     = "500"
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-  prepare_for_major_upgrade    = false
+  prepare_for_major_upgrade = false
+
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "17"
+  rds_family        = "postgres17"
+  db_instance_class = "db.t4g.small"
 
 
   # It is mandatory to set the below values to create read replica instance
@@ -101,6 +107,39 @@ module "read_replica" {
   db_backup_retention_period = 0
 
   vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
+
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
+      apply_method = "immediate"
+    },
+    {
+      name         = "hot_standby_feedback"
+      value        = "1"
+      apply_method = "immediate"
+    }
+  ]
 }
 
 resource "kubernetes_secret" "read_replica" {
