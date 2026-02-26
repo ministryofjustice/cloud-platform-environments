0242755493c548a4ce1072e2e3f9b3f58f862a1e
Ky <ky.truong@digital.justice.gov.uk>
2026-02-26 06:38:39 +0000

    testing service_pod module in ky-dev-test

--- namespaces/live.cloud-platform.service.justice.gov.uk/ky-dev-test/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/ky-dev-test/resources/service-pod.tf
@@ -1,5 +1,5 @@
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=optional_override_deployment_name" # use the latest release
 
   # Configuration
   namespace            = var.namespace
