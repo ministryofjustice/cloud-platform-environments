dd1b9ec65743a951fe7f013ac88e685cdbc55b4e
Eve Bramley <eve.bramley@digital.justice.gov.uk>
2026-03-02 16:14:27 +0000

    added new secrets resource

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
@@ -141,6 +141,24 @@ resource "kubernetes_secret" "dps_rds" {
   }
 }
 
+resource "kubernetes_secret" "dps_rds" {
+  metadata {
+    name      = "test-rds-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    db_identifier         = module.ma_rds.db_identifier
+    resource_id           = module.ma_rds.resource_id
+    rds_instance_endpoint = module.ma_rds.rds_instance_endpoint
+    database_name         = module.ma_rds.database_name
+    database_username     = module.ma_rds.database_username
+    database_password     = module.ma_rds.database_password
+    rds_instance_address  = module.ma_rds.rds_instance_address
+    url                   = "postgres://${module.ma_rds.database_username}:${module.ma_rds.database_password}@${module.ma_rds.rds_instance_endpoint}/${module.ma_rds.database_name}"
+  }
+}
+
 # This places a secret for this preprod RDS instance in the production namespace,
 # this can then be used by a kubernetes job which will refresh the preprod data.
 resource "kubernetes_secret" "dps_rds_refresh_creds" {
