f84e073f8c37f72780f6334bcf99b4e43845cb6d
Munish Jalota <Munish.Jalota@justice.gov.uk>
2026-03-02 17:18:30 +0000

    update

--- namespaces/live.cloud-platform.service.justice.gov.uk/track-my-case-automation/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/track-my-case-automation/resources/variables.tf
@@ -23,7 +23,7 @@ variable "namespace" {
 variable "business_unit" {
   description = "Area of the MOJ responsible for this service"
   type        = string
-  default     = "Central Digital"
+  default     = "HQ"
 }
 
 variable "team_name" {
@@ -35,7 +35,7 @@ variable "team_name" {
 variable "environment" {
   description = "Name of the environment type for this service"
   type        = string
-  default     = "dev"
+  default     = "automation"
 }
 
 variable "infrastructure_support" {
