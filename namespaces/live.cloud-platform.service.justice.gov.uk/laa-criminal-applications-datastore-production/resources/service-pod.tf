cc92dbcdb1a6bc7680232b7557ae709c16f04351
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2026-03-02 10:03:32 +0000

    set resource name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-criminal-applications-datastore-production/resources/service-pod.tf
@@ -1,6 +1,7 @@
 module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
 
-  namespace            = var.namespace
-  service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+  namespace                = var.namespace
+  service_account_name     = module.irsa.service_account.name # this uses the service account name from the irsa module
+  override_deployment_name = "laa-criminal-applications-datastore-staging-service-pod"
 }
