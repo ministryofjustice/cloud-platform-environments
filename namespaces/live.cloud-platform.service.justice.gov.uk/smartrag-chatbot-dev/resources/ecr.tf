59efb742c4230cd35c631f709d2a3e9849abd0b5
Kiran Chalasani <kiran.chalasani@justice.gov.uk>
2026-03-03 16:33:36 +0000

    Fix repository name for GitHub Actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/smartrag-chatbot-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/smartrag-chatbot-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["genai_data_engineering_assistant"]
+  github_repositories = ["moj-analytical-services/genai_data_engineering_assistant"]
 
   # Tags
   business_unit          = var.business_unit
