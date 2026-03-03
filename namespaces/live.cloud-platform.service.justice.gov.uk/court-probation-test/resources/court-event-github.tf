78c648abd17e504c04830a1c0c488534c2231804
Lisa Morison <lisa.morison@justice.gov.uk>
2026-03-03 16:18:54 +0000

    add gha service account for prepare-a-case test

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-test/resources/court-event-github.tf
@@ -93,3 +93,19 @@ module "hmpps-probation-in-court-automation-tests-service" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "prepare-a-case" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "prepare-a-case"
+  application                   = "prepare-a-case"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = "test"
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = "dev"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
