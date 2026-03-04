b269832a1dae6bfb91c02b9b77cc5dea3f427382
Raby Whyte <raby.whyte@justice.gov.uk>
2026-03-04 15:06:03 +0000

    add getobjectverion tagging multipart support to s3 upload bucket

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-preprod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-preprod/resources/s3.tf
@@ -42,7 +42,11 @@ resource "aws_s3_bucket_policy" "upload_s3_bucket_policy" {
           "s3:DeleteObject",
           "s3:GetBucketLocation",
           "s3:GetObjectAcl",
-          "s3:GetObjectVersion"
+          "s3:GetObjectVersion",
+          "s3:GetObjectTagging",
+          "s3:ListBucketMultipartUploads",
+          "s3:ListMultipartUploadParts",
+          "s3:AbortMultipartUpload",
         ]
         Resource = [
           module.upload_s3_bucket.bucket_arn,
