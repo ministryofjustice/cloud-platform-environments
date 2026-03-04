82fd7668f1856495294a0ce0f1205031355276a8
Prem Basumatary <prem.basumatary@justice.gov.uk>
2026-03-04 12:01:08 +0000

    TM-128 adding resources in training env

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-training/resources/s3.tf
@@ -0,0 +1,98 @@
+module "s3_bucket" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  namespace              = var.namespace
+  versioning             = var.versioning
+
+  lifecycle_rule = [
+    {
+      id      = "${var.environment_name}-lifecycle-rule"
+      enabled = true
+
+      noncurrent_version_transition = [
+        {
+          days          = var.s3_lifecycle_config["noncurrent_version_transition_days"]
+          storage_class = "STANDARD_IA"
+        },
+        {
+          days          = var.s3_lifecycle_config["noncurrent_version_transition_glacier_days"]
+          storage_class = "GLACIER"
+        },
+      ]
+    },
+    {
+      id      = "Expire current objects"
+      enabled = false
+
+      expiration = [
+        {
+          days = 1
+        }
+      ]
+
+      # expire previous versions
+      noncurrent_version_expiration = [
+        {
+          days = 1
+        }
+      ]
+    },
+    {
+      id      = "Clean up delete markers"
+      enabled = false
+
+      expiration = [
+        {
+          expired_object_delete_marker = true
+        }
+      ]
+    },
+    {
+      id      = "Abort incomplete uploads automatically"
+      enabled = false
+      abort_incomplete_multipart_upload = [
+        {
+          days_after_initiation = 1
+        }
+      ]
+    }
+  ]
+}
+
+resource "aws_s3_bucket_accelerate_configuration" "this" {
+  bucket = module.s3_bucket.bucket_name
+  status = "Enabled"
+}
+
+resource "aws_iam_user" "alfresco_user" {
+  name = "${var.namespace}-alfresco_user"
+  path = "/system/${var.namespace}-alfresco_user/"
+}
+
+resource "aws_iam_access_key" "alfresco_user_access" {
+  user = aws_iam_user.alfresco_user.name
+}
+
+resource "aws_iam_user_policy_attachment" "alfresco_user_policy" {
+  policy_arn = module.s3_bucket.irsa_policy_arn
+  user       = aws_iam_user.alfresco_user.name
+}
+
+resource "kubernetes_secret" "s3_bucket" {
+  metadata {
+    name      = "s3-bucket-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    BUCKET_ARN  = module.s3_bucket.bucket_arn
+    BUCKET_NAME = module.s3_bucket.bucket_name
+    ACCESSKEY   = aws_iam_access_key.alfresco_user_access.id
+    SECRETKEY   = aws_iam_access_key.alfresco_user_access.secret
+  }
+}
