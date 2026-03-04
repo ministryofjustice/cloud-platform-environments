6580f880eac0ffc859dbf94c3c6abc33d55a05e3
sankhajuria <sankhajuria@gmail.com>
2026-03-04 11:37:18 +0000

    Update domain-events-subscription.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-prod/resources/domain-events-subscription.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-prod/resources/domain-events-subscription.tf
@@ -71,7 +71,6 @@ resource "aws_sns_topic_subscription" "official_visits_domain_events_subscriptio
       "prisoner-offender-search.prisoner.released",
       "prisoner-offender-search.prisoner.received",
       "prison-offender-events.prisoner.merged",
-      "prison-offender-events.prisoner.received",
       "prison-offender-events.prisoner.booking.moved",
     ]
   })
