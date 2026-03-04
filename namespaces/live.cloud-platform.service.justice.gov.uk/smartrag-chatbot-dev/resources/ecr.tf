15bf7a5499ad122a2762f07b2c94595de4829e6c
Kiran Chalasani <kiran.chalasani@justice.gov.uk>
2026-03-04 14:09:25 +0000

    Add GitHub repository access to ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/smartrag-chatbot-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/smartrag-chatbot-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["genai_data_engineering_assistant"]
+  github_repositories = []
 
 
   # Tags
