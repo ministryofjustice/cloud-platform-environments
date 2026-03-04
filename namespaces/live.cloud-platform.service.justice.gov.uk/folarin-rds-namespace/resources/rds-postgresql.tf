fe905669ff0fce2975b99bb85e779ad34c9febe0
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-03-04 08:28:32 +0000

    revert upgrade preparatory changes (#40635)

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-rds-namespace/resources/rds-postgresql.tf
@@ -13,7 +13,7 @@ module "rds" {
   # RDS configuration
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
-  prepare_for_major_upgrade    = true
+  prepare_for_major_upgrade    = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
