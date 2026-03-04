c8d8079cdce57b861d9b145ae6506627fa7d8632
Prem Basumatary <prem.basumatary@justice.gov.uk>
2026-03-04 12:15:44 +0000

    TM-128 adding resources in training env

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-training/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfresco-training/resources/service-pod.tf
@@ -1,5 +1,5 @@
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1"
 
   # Configuration
   namespace            = var.namespace
