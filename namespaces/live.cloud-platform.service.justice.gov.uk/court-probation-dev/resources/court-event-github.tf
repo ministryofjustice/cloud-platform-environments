53e1c1e191f5e3612d215aad031f0bfc395018e9
Richard Taylor <33220256+RichT900@users.noreply.github.com>
2026-03-05 11:48:40 +0000

    add court-case-service to dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/court-event-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/court-probation-dev/resources/court-event-github.tf
@@ -141,3 +141,19 @@ module "prepare-a-case" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "court-case-service" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
+  github_repo                   = "court-case-service"
+  application                   = "court-case-service"
+  github_team                   = "hmpps-probation-in-court"
+  environment                   = var.environment
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = var.environment
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
