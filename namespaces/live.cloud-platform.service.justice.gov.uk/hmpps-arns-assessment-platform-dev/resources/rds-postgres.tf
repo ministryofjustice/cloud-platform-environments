497a65c73e63ac67f80ac9b5e140d390cf973e05
Alex Warhurst <174349012+alexw-moj@users.noreply.github.com>
2026-03-05 13:39:30 +0000

    ARN-3110: Create RDS for new ARNS application

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-assessment-platform-dev/resources/rds-postgres.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-assessment-platform-dev/resources/rds-postgres.tf
@@ -39,7 +39,7 @@ module "read_replica" {
   count  = 1
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
-  vpc_name               = var.vpc_name
+  vpc_name = var.vpc_name
 
   # Tags
   application            = var.application
@@ -114,7 +114,6 @@ resource "kubernetes_secret" "read_replica" {
 
 
 # Configmap to store non-sensitive data related to the RDS instance
-
 resource "kubernetes_config_map" "rds" {
   metadata {
     name      = "rds-postgresql-instance-output"
@@ -136,5 +135,63 @@ resource "kubernetes_config_map" "read_replica" {
   data = {
     database_name = module.read_replica[0].database_name
     db_identifier = module.read_replica[0].db_identifier
+  }
+}
+
+# RDS for other users to access AAP data
+module "arns_assessment_view_rds" {
+  source                       = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+
+  # VPC configuration
+  vpc_name                     = var.vpc_name
+
+  # RDS configuration
+  allow_minor_version_upgrade  = true
+  allow_major_version_upgrade  = false
+  prepare_for_major_upgrade    = false
+  performance_insights_enabled = false
+  deletion_protection          = true
+  db_max_allocated_storage     = "500"
+  enable_rds_auto_start_stop   = true
+  db_password_rotated_date     = "2026-03-05"
+
+  # PostgreSQL specifics
+  db_allocated_storage         = 10
+  storage_type                 = "gp3"
+  rds_family                   = "postgres18"
+  db_engine                    = "postgres"
+  db_engine_version            = "18"
+  db_instance_class            = "db.t4g.micro"
+
+  # Tags
+  team_name                    = var.team_name
+  business_unit                = var.business_unit
+  application                  = var.application
+  is_production                = var.is_production
+  namespace                    = var.namespace
+  environment_name             = var.environment
+  infrastructure_support       = var.infrastructure_support
+
+  # Name
+  rds_name                     = "hmpps-arns-assessment-view-db-${var.environment}"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "arns_assessment_view_rds" {
+  metadata {
+    name      = "rds-arns-assessment-view-instance-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.arns_assessment_view_rds.rds_instance_endpoint
+    database_name         = module.arns_assessment_view_rds.database_name
+    database_username     = module.arns_assessment_view_rds.database_username
+    database_password     = module.arns_assessment_view_rds.database_password
+    rds_instance_address  = module.arns_assessment_view_rds.rds_instance_address
+    url                   = "postgres://${module.arns_assessment_view_rds.database_username}:${module.arns_assessment_view_rds.database_password}@${module.arns_assessment_view_rds.rds_instance_endpoint}/${module.arns_assessment_view_rds.database_name}"
   }
 }
