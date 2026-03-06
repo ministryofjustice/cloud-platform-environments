b51502744322a56c307d7fbedf05a4367c276cef
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2026-03-06 14:10:38 +0000

    update cache module to latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-dev/resources/ui-elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-dev/resources/ui-elasticache.tf
@@ -1,11 +1,11 @@
 # Note, this resource is for use with the template app hmpps-template-typescript.
 
 module "elasticache_redis" {
-  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.0.0"
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.1.0"
   vpc_name               = var.vpc_name
   team_name              = var.team_name
   business_unit          = var.business_unit
-  application            = var.application
+  application            = module.hmpps_managing_prisoner_apps_staff_ui.application
   is_production          = var.is_production
   namespace              = var.namespace
   environment_name       = var.environment
@@ -24,7 +24,7 @@ module "elasticache_redis" {
 
 resource "kubernetes_secret" "elasticache_redis" {
   metadata {
-    name      = "elasticache-redis"
+    name      = "${module.hmpps_managing_prisoner_apps_staff_ui.application}-elasticache-redis"
     namespace = var.namespace
   }
 
@@ -33,5 +33,41 @@ resource "kubernetes_secret" "elasticache_redis" {
     auth_token               = module.elasticache_redis.auth_token
     member_clusters          = jsonencode(module.elasticache_redis.member_clusters)
     replication_group_id     = module.elasticache_redis.replication_group_id
+  }
+}
+
+module "prisoner_ui_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.1.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = module.hmpps_prisoner_apps_ui.application
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  number_cache_clusters = var.number_cache_clusters
+  # sized for micro in dev, preprod, suggest small for production
+  node_type            = "cache.t4g.micro"
+  engine_version       = "7.0"
+  parameter_group_name = "default.redis7"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "prisoner_ui_elasticache_redis" {
+  metadata {
+    name      = "${module.hmpps_prisoner_apps_ui.application}-elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.prisoner_ui_elasticache_redis.primary_endpoint_address
+    auth_token               = module.prisoner_ui_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.prisoner_ui_elasticache_redis.member_clusters)
+    replication_group_id     = module.prisoner_ui_elasticache_redis.replication_group_id
   }
 }
