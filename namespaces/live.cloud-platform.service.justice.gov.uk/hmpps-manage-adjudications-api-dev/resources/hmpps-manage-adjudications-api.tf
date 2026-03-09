345a3df024513246de245b9494fe54fb2e28e795
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-03-09 10:21:24 +0000

    Bump versions of cloud-platform-terraform-hmpps-template to 1.1.0
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/hmpps-manage-adjudications-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/hmpps-manage-adjudications-api.tf
@@ -1,5 +1,5 @@
 module "hmpps_template_kotlin" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.0.0"
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-manage-adjudications-api"
   application = "hmpps-manage-adjudications-api"
   github_team = "hmpps-prisons-digital-live-support"
