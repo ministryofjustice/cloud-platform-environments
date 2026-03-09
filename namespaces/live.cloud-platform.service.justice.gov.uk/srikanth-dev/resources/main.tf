8f276625ff05b56ee1f42dc412cde33af783c046
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-03-09 13:45:08 +0000

    changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/main.tf
@@ -66,7 +66,7 @@ module "irsa" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1"
 
   namespace                = var.namespace
   service_account_name     = module.irsa.service_account.name
