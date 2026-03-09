b93baa5732d3e53aae7ada24954ba14ec2450964
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-03-09 10:29:40 +0000

    service account creation

--- namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srikanth-dev/resources/main.tf
@@ -46,7 +46,7 @@ provider "github" {
 provider "kubernetes" {}
 
 module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
 
   eks_cluster_name = var.eks_cluster_name
 
