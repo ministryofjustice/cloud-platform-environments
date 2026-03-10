ac046a4d7f68d2b27b3f0f46bb070f2f01d186fd
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-03-10 15:24:22 +0000

    Update main.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/request-personal-information-staging/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/request-personal-information-staging/resources/main.tf
@@ -38,6 +38,22 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      business-unit = var.business_unit
+      application   = var.application
+      is-production = var.is_production
+      owner         = var.team_name
+      namespace     = var.namespace
+      service-area  = var.service_area
+      GithubTeam    = var.team_name
+    }
+  }
+}
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
