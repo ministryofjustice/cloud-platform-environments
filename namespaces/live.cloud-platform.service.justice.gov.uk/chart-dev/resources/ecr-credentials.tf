6542ee9cc8200b8ec4fc83fdd8bf24c9f80c4ae5
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-03-11 12:46:18 +0000

    aws policy  added

--- namespaces/live.cloud-platform.service.justice.gov.uk/chart-dev/resources/ecr-credentials.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chart-dev/resources/ecr-credentials.tf
@@ -3,7 +3,7 @@ module "ecr_credentials" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
 
   # ECR Repository name
-  repo_name = "laa-generic-app"
+  repo_name = "laa-sre-admins/laa-generic-app"
 
   # OpenID Connect configuration for GitHub Actions
   oidc_providers      = ["github"]
