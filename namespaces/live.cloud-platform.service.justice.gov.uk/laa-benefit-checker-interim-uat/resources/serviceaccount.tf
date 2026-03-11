0f5f006acbfc6d9676810ef32ba201f155888173
savvysoftben <ben@savvysoftware.com>
2026-03-11 10:25:37 +0000

    naming the repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-benefit-checker-interim-uat/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-benefit-checker-interim-uat/resources/serviceaccount.tf
@@ -6,7 +6,5 @@ module "serviceaccount" {
 
   serviceaccount_token_rotated_date = "01-01-2000"
 
-  # Uncomment and provide repository names to create github actions secrets
-  # containing the ca.crt and token for use in github actions CI/CD pipelines
-  # github_repositories = ["my-repo"]
+  github_repositories = ["laa-dwp-benefit-checker"]
 }
