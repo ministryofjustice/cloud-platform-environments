ed98e17d4b9cec06fb8770065854217c5aa272f9
aamircodes <aamir.patel@justice.gov.uk>
2026-03-12 08:53:08 +0000

    remove api for now

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/github.tf
@@ -13,17 +13,3 @@ module "hmpps-ems-cemo" {
   kubernetes_cluster            = var.kubernetes_cluster
 }
 
-module "hmpps-ems-cemo-api" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
-  github_repo = "hmpps-electronic-monitoring-create-an-order-api"
-  application = "hmpps-electronic-monitoring-create-an-order-api"
-  github_team = var.team_name
-  environment = var.environment 
-  is_production                 = var.is_production
-  selected_branch_patterns      = ["main"]
-  application_insights_instance = var.environment
-  source_template_repo          = "hmpps-template-kotlin"
-  github_token                  = var.github_token
-  namespace                     = var.namespace
-  kubernetes_cluster            = var.kubernetes_cluster
-}
