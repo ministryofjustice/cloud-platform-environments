e732c67d813c3c9fbc12d1febca9d69e9d7b1303
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-03-13 12:04:58 +0000

    Updated github actions prefix to use underscores

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-event-service-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-event-service-prod/resources/ecr.tf
@@ -18,7 +18,7 @@ module "ecr" {
   # REQUIRED: GitHub repositories that push to this container repository
   # E2E repository does have trailing "-" character, this is not a typo.
   github_repositories = ["laa-data-claims-event-service", "laa-submit-a-bulk-claim", "bulk-submission-and-fee-scheme-tests-"]
-  github_actions_prefix = "laa-data-claims-event-service"
+  github_actions_prefix = "laa_data_claims_event_service"
 
   # Lifecycle policies
   lifecycle_policy = <<EOF
