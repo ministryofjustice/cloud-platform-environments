d2f8e7b6e7e9aa15eb9dbb0d8c3429f74462fd79
Drew Clayton <drew.clayton@justice.gov.uk>
2026-03-18 15:27:57 +0000

    added github and rcw repo to CCQ ecr

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/ecr.tf
@@ -9,11 +9,11 @@ module "ecr_credentials" {
   team_name = var.team_name
   repo_name = "laa-check-client-qualifies-ecr"
 
-  # enable the oidc implementation for CircleCI
-  oidc_providers = ["circleci"]
+  # enable the oidc implementation for CircleCI and Github
+  oidc_providers = ["circleci", "github"]
 
-  # specify which GitHub repository your CircleCI job runs from
-  github_repositories = ["laa-check-client-qualifies"]
+  # specify which GitHub repository your CircleCI and Github job runs from
+  github_repositories = ["laa-check-client-qualifies", "laa-record-controlled-work"]
 
   # set your namespace name to create a ConfigMap of credentials you need in CircleCI
   namespace = var.namespace
