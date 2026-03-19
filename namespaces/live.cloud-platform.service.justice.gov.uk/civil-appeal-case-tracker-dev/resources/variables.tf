5239ff04a1399bb10413c8b2b24e86f5c0b3717d
Mark Butler <mark.butler@solirius.com>
2026-03-19 12:11:07 +0000

    Add the null_resource  provider and adjust format of rotation trigger string

--- namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/civil-appeal-case-tracker-dev/resources/variables.tf
@@ -87,5 +87,5 @@ variable "service_area" {
 variable "access_key_rotation_trigger" {
   description = "External S3 Access Key Rotation Trigger"
   type        = string
-  default     = "19/03/2026"
+  default     = "20260319#1"
 }
