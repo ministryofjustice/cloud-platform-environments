ae99ad7a8e18363efef3d69c467f868261a867f2
Ben Howe <ben.howe1@justice.gov.uk>
2026-03-19 09:14:45 +0000

    Add assessment-view-api service and RDS to integrations dev namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/arns-assessment-view-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/arns-assessment-view-api.tf
@@ -1,14 +1,14 @@
-module "hmpps_assess_risks_and_needs_coordinator_api" {
+module "hmpps_arns_assessment_view_api" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.0"
-  github_repo                   = "hmpps-assess-risks-and-needs-coordinator-api"
-  application                   = "hmpps-assess-risks-and-needs-coordinator-api"
+  github_repo                   = "hmpps-arns-assessment-view-api"
+  application                   = "hmpps-arns-assessment-view-api"
   github_team                   = "hmpps-assessments"
   environment                   = var.environment
   is_production                 = var.is_production
   application_insights_instance = "dev"
   source_template_repo          = "hmpps-template-kotlin"
   github_token                  = var.github_token
-  namespace                     = var.namespace
   selected_branch_patterns      = ["*"]
+  namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
