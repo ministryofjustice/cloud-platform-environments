4ec5322e760a836b0201eb069dc9d6dc7b37065d
Ben Millar <ben.millar@justice.gov.uk>
2026-03-20 15:40:23 +0000

    Set appropriate IRSA tags

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-staging/resources/service_pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-amend-a-claim-staging/resources/service_pod.tf
@@ -1,14 +1,28 @@
 module "irsa" {
-  service_account_name = "laa-amend-a-claim-staging-service-account"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
 
+  # IRSA configuration
+  service_account_name = "laa-amend-a-claim-staging-sa"
+  namespace            = var.namespace
   role_policy_arns = {
-        elasticache_redis  = module.elasticache_redis.irsa_policy_arn
-    }
+    elasticache_redis = module.elasticache_redis.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
 }
 
-module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1"
+# set up the service pod
+module "ap_service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1" # use the latest release
 
+  # Configuration
   namespace            = var.namespace
-  service_account_name = module.irsa.service_account.name
+  service_account_name = module.ap_irsa.service_account.name # this uses the service account name from the irsa module
 }
