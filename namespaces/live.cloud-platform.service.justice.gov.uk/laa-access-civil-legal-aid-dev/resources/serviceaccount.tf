f2e15c4c041a1d69511c61906e74e1551aa082f5
joselopez-moj <jose.lopez@justice.gov.uk>
2026-03-20 15:52:08 +0000

    fix: update 2-digit month format

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-access-civil-legal-aid-dev/resources/serviceaccount.tf
@@ -4,7 +4,7 @@ module "serviceaccount" {
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
-  serviceaccount_token_rotated_date = "20-3-2026"
+  serviceaccount_token_rotated_date = "20-03-2026"
 
   # Creates Kubernetes' GitHub actions secrets in the repository
   github_repositories = ["laa-access-civil-legal-aid"]
