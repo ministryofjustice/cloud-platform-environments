1094d7bbc50d2197f6b673b1ffdbec47ea3b0108
Raby Whyte <raby.whyte@justice.gov.uk>
2026-03-20 14:05:08 +0000

    comment out data importer change for emergency hot fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-dev/resources/api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-dev/resources/api.tf
@@ -1,5 +1,5 @@
-module "api" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.0"
+module "hmpps_template_kotlin" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.1.0"
   github_repo = "hmpps-accredited-programmes-manage-and-deliver-api"
   application = "hmpps-accredited-programmes-manage-and-deliver-api"
   github_team = "hmpps-accredited-programmes-manage-and-deliver-devs"
@@ -15,20 +15,21 @@ module "api" {
   kubernetes_cluster            = var.kubernetes_cluster
 }
 
-module "data_importer_service" {
-  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.0"
-  github_repo = "hmpps-accredited-programmes-manage-and-deliver-data-importer-service"
-  application = "hmpps-accredited-programmes-manage-and-deliver-data-importer-service"
-  github_team = "hmpps-accredited-programmes-manage-and-deliver-devs"
-  environment = var.environment
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
-  protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
-  is_production                 = var.is_production
-  application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
-  source_template_repo          = "hmpps-template-kotlin"
-  github_token                  = var.github_token
-  namespace                     = var.namespace
-  kubernetes_cluster            = var.kubernetes_cluster
-}
+#module "api" { rename module hmpps_template_kotlin
+# module "data_importer_service" {
+#   source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.0"
+#   github_repo = "hmpps-accredited-programmes-manage-and-deliver-data-importer-service"
+#   application = "hmpps-accredited-programmes-manage-and-deliver-data-importer-service"
+#   github_team = "hmpps-accredited-programmes-manage-and-deliver-devs"
+#   environment = var.environment
+#   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
+#   #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
+#   protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+#   is_production                 = var.is_production
+#   application_insights_instance = var.environment # Either "dev", "preprod" or "prod"
+#   source_template_repo          = "hmpps-template-kotlin"
+#   github_token                  = var.github_token
+#   namespace                     = var.namespace
+#   kubernetes_cluster            = var.kubernetes_cluster
+# }
 
