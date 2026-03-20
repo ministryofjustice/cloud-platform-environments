52861de1eeafd280a3decbb73672f495af8374a1
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-03-20 10:54:29 +0000

    Update config for dev.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-dev/resources/main.tf
@@ -5,11 +5,23 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+
   default_tags {
     tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-      GithubTeam    = var.team_name
+      # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
+      GithubTeam = var.team_name
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      # see https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/accessing-the-cloud-console.html
+      GithubTeam = var.team_name
     }
   }
 }
