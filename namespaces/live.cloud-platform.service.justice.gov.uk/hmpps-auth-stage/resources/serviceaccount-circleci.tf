7b107c076c68d8bca6621ff0239fb26ddb1bd6f8
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-03-23 08:58:28 +0000

    HAAR-3945: ⚰️ remove serviceaccount-circleci as moved to GitHub action - hmpps-auth-stage

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/serviceaccount-circleci.tf
+++
@@ -1,8 +0,0 @@
-module "serviceaccount" {
-  source              = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
-  namespace           = var.namespace
-  kubernetes_cluster  = var.kubernetes_cluster
-  serviceaccount_name = "circleci"
-  role_name           = "circleci"
-  serviceaccount_token_rotated_date = "20-03-2026"
-}
