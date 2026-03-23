b0acfcf285ab3778636c36d1dc72c691f460ed74
matt heery <matt.heery@justice.gov.uk>
2026-03-23 15:56:37 +0000

    fix: data_store_general_role_arn

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/ssm.tf
@@ -1,9 +1,9 @@
-data "aws_ssm_parameter" "athena_general_role_arn" {
-  name = "/${var.namespace}/athena_general_role_arn"
+data "aws_ssm_parameter" "data_store_general_role_arn" {
+  name = "/${var.namespace}/data_store_general_role_arn"
   with_decryption = true
 }
 
-resource "aws_ssm_parameter" "athena_general_role_arn" {
+resource "aws_ssm_parameter" "data_store_general_role_arn" {
   name        = "/${var.namespace}/data_store_general_role_arn"
   type        = "SecureString"
   # This value must be replaced with a genuine role ARN using AWS CLI
