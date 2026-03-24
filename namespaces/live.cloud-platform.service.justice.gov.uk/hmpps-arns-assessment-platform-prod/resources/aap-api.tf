357145a125e1e8a42e2593718172aaff8308ddc2
devkokov <dm.kokov@gmail.com>
2026-03-24 14:51:42 +0000

    Create a Redis cache and service pod in AAP [PROD]

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-assessment-platform-prod/resources/aap-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-arns-assessment-platform-prod/resources/aap-api.tf
@@ -1,7 +1,7 @@
 module "hmpps_arns_assessment_platform_api" {
-source                          = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
-force_rotate_token = true
-custom_token_rotation_date = "2026-03-20"
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token            = true
+  custom_token_rotation_date    = "2026-03-20"
   github_repo                   = "hmpps-arns-assessment-platform-api"
   application                   = "hmpps-arns-assessment-platform-api"
   github_team                   = "hmpps-assessments-live"
@@ -12,4 +12,41 @@ custom_token_rotation_date = "2026-03-20"
   github_token                  = var.github_token
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
+}
+
+# Note, redis is used for the cache of hot assessments
+module "assessment-query-cache-elasticache-redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.1.0"
+  vpc_name               = var.vpc_name
+  team_name              = var.team_name
+  business_unit          = var.business_unit
+  application            = module.hmpps_arns_assessment_platform_api.application
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  number_cache_clusters = var.number_cache_clusters
+  # sized for micro in dev, preprod, suggest small for production
+  node_type            = "cache.t4g.micro"
+  engine_version       = "7.0"
+  parameter_group_name = "default.redis7"
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "assessment-query-cache-elasticache-redis" {
+  metadata {
+    name      = "assessment-query-cache-elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.assessment-query-cache-elasticache-redis.primary_endpoint_address
+    auth_token               = module.assessment-query-cache-elasticache-redis.auth_token
+    member_clusters          = jsonencode(module.assessment-query-cache-elasticache-redis.member_clusters)
+    replication_group_id     = module.assessment-query-cache-elasticache-redis.replication_group_id
+  }
 }
