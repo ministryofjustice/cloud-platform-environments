592abef6e8732a5d6c5bf70e54f29d26c9629251
dan-criddle <dan.criddle@justice.gov.uk>
2026-03-24 16:54:26 +0000

    Delete origninal RDS. Create new RDS with is_migration = true
    Updated snapshot identifier for RDS instance and added migration flag. Removed unused RDS output resources.

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
@@ -1,45 +1,3 @@
-module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
-
-  # VPC configuration
-  vpc_name = var.vpc_name
-
-  # RDS configuration
-  allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "15" # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres15"
-  db_instance_class = "db.t4g.micro"
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
-  # Testing RDS Restore
-  deletion_protection = false
-  snapshot_identifier = "cloud-platform-47d4c6e80a73418c-pre-is-mig-test-2026-03-24"
-
-  # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for CLI queries,
-  # uncomment below:
-
-  # enable_irsa = true
-
-  # If you want to enable Cloudwatch logging for this postgres RDS instance, uncomment the code below:
-  # opt_in_xsiam_logging = true
-}
-
 module "copy" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
@@ -83,8 +41,6 @@ module "copy" {
   # opt_in_xsiam_logging = true
 }
 
-
-
 module "rds_with_is_migration" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
@@ -117,7 +73,8 @@ module "rds_with_is_migration" {
 
   # Testing RDS Restore
   deletion_protection = false
-  snapshot_identifier = "cloud-platform-47d4c6e80a73418c-pre-is-mig-test-2026-03-24"
+  snapshot_identifier = "cloud-platform-47d4c6e80a73418c-finalsnapshot"
+  is_migration        = true
 
   # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for CLI queries,
   # uncomment below:
@@ -127,9 +84,6 @@ module "rds_with_is_migration" {
   # If you want to enable Cloudwatch logging for this postgres RDS instance, uncomment the code below:
   # opt_in_xsiam_logging = true
 }
-
-
-
 
 module "red" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
@@ -257,28 +211,6 @@ module "blue" {
   # opt_in_xsiam_logging = true
 }
 
-
-resource "kubernetes_secret" "rds" {
-  metadata {
-    name      = "rds-postgresql-instance-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.rds.rds_instance_endpoint
-    database_name         = module.rds.database_name
-    database_username     = module.rds.database_username
-    database_password     = module.rds.database_password
-    rds_instance_address  = module.rds.rds_instance_address
-  }
-  /* You can replace all of the above with the following, if you prefer to
-     * use a single database URL value in your application code:
-     *
-     * url = "postgres://${module.rds.database_username}:${module.rds.database_password}@${module.rds.rds_instance_endpoint}/${module.rds.database_name}"
-     *
-     */
-}
-
 resource "kubernetes_secret" "copy" {
   metadata {
     name      = "rds-postgresql-instance-copy-output"
@@ -355,18 +287,6 @@ resource "kubernetes_secret" "blue" {
 }
 
 # Configmap to store non-sensitive data related to the RDS instance
-
-resource "kubernetes_config_map" "rds" {
-  metadata {
-    name      = "rds-postgresql-instance-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
-  }
-}
 
 resource "kubernetes_config_map" "copy" {
   metadata {
