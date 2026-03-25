51160696256db3448dc64b61aa10f2c42ea5ca83
Kiran Chalasani <kiran.chalasani@justice.gov.uk>
2026-03-25 16:15:59 +0000

    Add Lambda deployment permissions to GitHub Actions role

--- namespaces/live.cloud-platform.service.justice.gov.uk/genai-data-eng-assistant-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/genai-data-eng-assistant-dev/resources/ecr.tf
@@ -29,25 +29,6 @@ module "ecr" {
   # enable_irsa = true
 }
 
-module "ecr" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
-
-  # Repository configuration
-  repo_name = var.namespace
-
-  # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = ["genai_data_engineering_assistant"]
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-}
 # Add Lambda deployment permissions for GitHub Actions
 resource "aws_iam_role_policy" "github_lambda_deploy" {
   name = "lambda-deployment-policy"
