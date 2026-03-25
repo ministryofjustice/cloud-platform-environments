1b09bdb80ab3cb388b4e75a670dd3f3f4bd6aa26
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-03-25 11:28:21 +0000

    Update athena role

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-preprod/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-preprod/resources/ssm.tf
@@ -10,19 +10,15 @@ data "aws_ssm_parameter" "athena_general_role_arn" {
 # This SSM parameter stores the MOD Platform IAM role ARN that the application
 # will assume when querying Athena.
 #
-# IMPORTANT:
-# - Terraform only creates the parameter and manages its name/lifecycle.
-# - The *value* is intentionally set to a placeholder and must be updated
-#   manually (or via platform automation) to the real MOD role ARN.
-#
 # The application reads this parameter via namespace Terraform and exposes it
 # to the pod as ATHENA_GENERAL_IAM_ROLE.
+#
+# Note: this is currently set to point at dev data.
 # -----------------------------------------------------------------------------
 resource "aws_ssm_parameter" "athena_general_role_arn" {
   name        = "/${var.namespace}/athena_general_role_arn"
   type        = "SecureString"
-  # This value must be replaced with a genuine role ARN using AWS CLI
-  value       = "arn:aws:iam::0000000000000:role/general-placeholder"
+  value       = "arn:aws:iam::800964199911:role/emdi_read_emds_data_dev"
   description = "ARN of the role used to query Athena for general EM order data"
   tags        = local.tags
   lifecycle {
