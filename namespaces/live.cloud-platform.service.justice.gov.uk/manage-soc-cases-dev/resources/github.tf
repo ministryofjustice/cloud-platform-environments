a6d7410bad08187deddb398e9d2427c652725986
Evald <evald.spedzhev@gmail.com>
2026-03-27 11:44:37 +0000

    Add gha for manage-soc-cases dev and manage-soc-cases-api dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/manage-soc-cases-dev/resources/github.tf
@@ -0,0 +1,33 @@
+module "manage-soc-cases" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-template?ref=1.2.1"
+  github_repo                   = "manage-soc-cases"
+  application                   = "manage-soc-cases"
+  github_team                   = "secure-estate-digital-team"
+  reviewer_teams                = ["secure-estate-digital-restricted-team"]
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.environment_name
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
+
+module "manage-soc-cases-api" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-template?ref=1.2.1"
+  github_repo                   = "manage-soc-cases-api"
+  application                   = "manage-soc-cases-api"
+  github_team                   = "secure-estate-digital-team"
+  reviewer_teams                = ["secure-estate-digital-restricted-team"]
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.environment_name
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
