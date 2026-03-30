286a2a7fe623d43be47af7414dbfaefbfe4ee818
dan-criddle <dan.criddle@justice.gov.uk>
2026-03-30 15:13:34 +0100

    Create new RDS demo-dan1-rds-parm-is-migration-true

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
@@ -29,42 +29,42 @@ module "rds_pg15_source" {
   team_name              = var.team_name
 }
 
-#module "rds_with_is_migration" {
-#  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
-#
-#  # VPC configuration
-#  vpc_name = var.vpc_name
-#
-#  # RDS configuration
-#  allow_minor_version_upgrade  = true
-#  allow_major_version_upgrade  = false
-#  performance_insights_enabled = false
-#  db_max_allocated_storage     = "500"
-#  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-#  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-#
-#
-#  # PostgreSQL specifics
-#  db_engine         = "postgres"
-#  db_engine_version = "15" # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-#  rds_family        = "postgres15"
-#  db_instance_class = "db.t4g.micro"
-#  rds_name          = "demo-dan1-rds-with-is-migration"
-#
-#  # Tags
-#  application            = var.application
-#  business_unit          = var.business_unit
-#  environment_name       = var.environment
-#  infrastructure_support = var.infrastructure_support
-#  is_production          = var.is_production
-#  namespace              = var.namespace
-#  team_name              = var.team_name
-#
-#  # Testing RDS Restore
-#  deletion_protection = false
-#  snapshot_identifier = "cloud-platform-47d4c6e80a73418c-finalsnapshot"
-#  is_migration        = true
-#}
+module "rds_parm_is_migration_true" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
+
+  # VPC configuration
+  vpc_name = var.vpc_name
+
+  # RDS configuration
+  allow_minor_version_upgrade  = true
+  allow_major_version_upgrade  = false
+  performance_insights_enabled = false
+  db_max_allocated_storage     = "500"
+  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
+  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
+
+
+  # PostgreSQL specifics
+  db_engine         = "postgres"
+  db_engine_version = "15" # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  rds_family        = "postgres15"
+  db_instance_class = "db.t4g.micro"
+  rds_name          = "demo-dan1-rds-parm-is-migration-true"
+
+  # Tags
+  application            = var.application
+  business_unit          = var.business_unit
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  is_production          = var.is_production
+  namespace              = var.namespace
+  team_name              = var.team_name
+
+  # Testing RDS Restore
+  deletion_protection = false
+  snapshot_identifier = "demo-dan1-rds-pg15-source-20260330"
+  is_migration        = true
+}
 
 module "red" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
@@ -207,20 +207,20 @@ resource "kubernetes_secret" "rds_pg15_source" {
   }
 }
 
-#resource "kubernetes_secret" "rds_with_is_migration" {
-#  metadata {
-#    name      = "rds-postgresql-instance-rds-with-is-migration-output"
-#    namespace = var.namespace
-#  }
-#
-#  data = {
-#    rds_instance_endpoint = module.rds_with_is_migration.rds_instance_endpoint
-#    database_name         = module.rds_with_is_migration.database_name
-#    database_username     = module.rds_with_is_migration.database_username
-#    database_password     = module.rds_with_is_migration.database_password
-#    rds_instance_address  = module.rds_with_is_migration.rds_instance_address
-#  }
-#}
+resource "kubernetes_secret" "rds_parm_is_migration_true" {
+  metadata {
+    name      = "rds-postgresql-instance-rds-parm-is-migration-true-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    rds_instance_endpoint = module.rds_parm_is_migration_true.rds_instance_endpoint
+    database_name         = module.rds_parm_is_migration_true.database_name
+    database_username     = module.rds_parm_is_migration_true.database_username
+    database_password     = module.rds_parm_is_migration_true.database_password
+    rds_instance_address  = module.rds_parm_is_migration_true.rds_instance_address
+  }
+}
 
 resource "kubernetes_secret" "red" {
   metadata {
@@ -281,18 +281,17 @@ resource "kubernetes_config_map" "rds_pg15_source" {
   }
 }
 
-#resource "kubernetes_config_map" "rds_with_is_migration" {
-#  metadata {
-#    name      = "rds-postgresql-instance-rds-with-is-migration-output"
-#    namespace = var.namespace
-#  }
-#
-#  data = {
-#    database_name = module.rds_with_is_migration.database_name
-#    db_identifier = module.rds_with_is_migration.db_identifier
-#  }
-#}
+resource "kubernetes_config_map" "rds_parm_is_migration_true" {
+  metadata {
+    name      = "rds-postgresql-instance-rds-parm-is-migration-true-output"
+    namespace = var.namespace
+  }
 
+  data = {
+    database_name = module.rds_parm_is_migration_true.database_name
+    db_identifier = module.rds_parm_is_migration_true.db_identifier
+  }
+}
 
 resource "kubernetes_config_map" "red" {
   metadata {
