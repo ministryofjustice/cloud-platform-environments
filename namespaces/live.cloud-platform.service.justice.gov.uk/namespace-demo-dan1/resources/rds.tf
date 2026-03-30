646889199b21364f1dcc40d6b14a579fb6774a17
dan-criddle <dan.criddle@justice.gov.uk>
2026-03-30 10:29:27 +0100

    Removing copy RDS
    Removed the copy module and its associated resources, retaining only the rds_with_is_migration module and its resources.

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/rds.tf
@@ -1,46 +1,3 @@
-module "copy" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
-
-  # VPC configuration
-  vpc_name = var.vpc_name
-
-  # RDS configuration
-  allow_minor_version_upgrade  = true
-  allow_major_version_upgrade  = false
-  performance_insights_enabled = false
-  db_max_allocated_storage     = "500"
-  # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
-  # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
-
-  # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "15" # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres15"
-  db_instance_class = "db.t4g.micro"
-  rds_name          = "demo-dan1-copy"
-  snapshot_identifier = "temp-manual-snapshot-cloud-platform-47d4c6e80a73418c"
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
-  # Testing RDS Restore
-  deletion_protection = false
-
-  # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for CLI queries,
-  # uncomment below:
-
-  # enable_irsa = true
-
-  # If you want to enable Cloudwatch logging for this postgres RDS instance, uncomment the code below:
-  # opt_in_xsiam_logging = true
-}
-
 module "rds_with_is_migration" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
@@ -211,20 +168,6 @@ module "blue" {
   # opt_in_xsiam_logging = true
 }
 
-resource "kubernetes_secret" "copy" {
-  metadata {
-    name      = "rds-postgresql-instance-copy-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    rds_instance_endpoint = module.copy.rds_instance_endpoint
-    database_name         = module.copy.database_name
-    database_username     = module.copy.database_username
-    database_password     = module.copy.database_password
-    rds_instance_address  = module.copy.rds_instance_address
-  }
-}
 
 resource "kubernetes_secret" "rds_with_is_migration" {
   metadata {
@@ -287,18 +230,6 @@ resource "kubernetes_secret" "blue" {
 }
 
 # Configmap to store non-sensitive data related to the RDS instance
-
-resource "kubernetes_config_map" "copy" {
-  metadata {
-    name      = "rds-postgresql-instance-copy-output"
-    namespace = var.namespace
-  }
-
-  data = {
-    database_name = module.copy.database_name
-    db_identifier = module.copy.db_identifier
-  }
-}
 
 resource "kubernetes_config_map" "rds_with_is_migration" {
   metadata {
