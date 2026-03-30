4dbe6b2f546932dac270aaaf5c5ad4bc28d648b1
ismail-walsh-justice <ismail.walsh@justice.gov.uk>
2026-03-30 10:31:17 +0100

    Add care-arrangement-plan-prototype-staging namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prototype-staging/resources/service-account.tf
@@ -0,0 +1,9 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  github_repositories = [var.app_repo]
+  github_environments = [var.environment]
+  serviceaccount_token_rotated_date = "30-03-2026"
+}
