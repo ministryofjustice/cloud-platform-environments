5721d0a2a2ca1b4c07cc93f861e81743cf9c302c
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2026-03-31 17:16:06 +0100

    WRA-45 - Create namespace for hmpps-warrant-risk-assessment (preprod) (#41373)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-warrant-risk-assessment-preprod/resources/api.tf
@@ -0,0 +1,18 @@
+module "hmpps_template_kotlin" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-31"
+  github_repo = "hmpps-warrant-risk-assessment-api"
+  application = "hmpps-warrant-risk-assessment-api"
+  github_team = "unilink"
+  environment = var.environment_name # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["unilink_admin"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main"] # Optional
+  # protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  is_production                 = var.is_production
+  application_insights_instance = "preprod" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
