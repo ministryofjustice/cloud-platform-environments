a8be37a14e563033dc465dc58ef4534ae8a48f0c
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-04-01 10:51:18 +0100

    Add new test namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-insights-test/resources/kubernetes-secrets.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_secret" "irsa" {
+  metadata {
+    name      = "${var.namespace}-irsa-output"
+    namespace = var.namespace
+  }
+  data = {
+    role           = module.irsa.role_name
+    serviceaccount = module.irsa.service_account.name
+  }
+}
+
+resource "kubernetes_secret" "athena_roles" {
+  metadata {
+    name      = "athena-roles"
+    namespace = var.namespace
+  }
+  type = "Opaque"
+  data = {
+    general_role_arn = "arn:aws:iam::800964199911:role/emdi_read_emds_data_dev" //TODO replace with test role
+  }
+}
+
