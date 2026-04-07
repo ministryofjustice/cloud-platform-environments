51fa1c0eb80e0a2105efb19a0d07d9e1ef44a899
Martin <martin.georgiev@justice.gov.uk>
2026-04-07 16:03:05 +0100

    PI-3894 - resolved script issues

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-appointment-reminders-dev/resources/hmpps-appointment-reminders-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-appointment-reminders-dev/resources/hmpps-appointment-reminders-github.tf
@@ -1,14 +1,14 @@
 module "hmpps-appointment-reminders-ui" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
   force_rotate_token = true
-  custom_token_rotated_date = "20-03-2026"
+  custom_token_rotation_date = "2026-03-20"
   github_repo                   = "hmpps-appointment-reminders-ui"
-  application                   = var.application
+  application                   = "hmpps-appointment-reminders-ui"
   github_team                   = var.team_name
   environment                   = var.environment_name
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
-  application_insights_instance = var.environment_name
+  application_insights_instance = "dev"
   source_template_repo          = "hmpps-template-typescript"
   github_token                  = var.github_token
   namespace                     = var.namespace
