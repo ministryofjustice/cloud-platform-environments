c766305cb1a6d27aed32cb1378fd68f3db98827a
Martin <martin.georgiev@justice.gov.uk>
2026-04-07 15:38:36 +0100

    PI-3894 - Revert deletion of github-actions.tf

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-appointment-reminders-dev/resources/github-actions.tf
@@ -0,0 +1,9 @@
+module "github_actions_service_account" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace           = var.namespace
+  kubernetes_cluster  = var.kubernetes_cluster
+  github_repositories = ["hmpps-appointment-reminders-ui"]
+  github_environments = [var.environment_name]
+  serviceaccount_token_rotated_date = "20-03-2026"
+}
