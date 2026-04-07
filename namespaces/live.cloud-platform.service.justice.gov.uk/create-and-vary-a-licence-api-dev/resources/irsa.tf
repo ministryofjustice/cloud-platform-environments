28b72ca7f557f033134942493652070a43a91f32
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2026-04-07 11:07:21 +0100

    fix

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-dev/resources/irsa.tf
@@ -2,16 +2,12 @@ locals {
   sqs_queues = {
     "Digital-Prison-Services-dev-cvl_domain_events_queue" = "hmpps-domain-events-dev",
     "Digital-Prison-Services-dev-cvl_domain_events_dead_letter_queue" = "hmpps-domain-events-dev",
-    "hmpps_hdc_api_queue"                 = "licences-dev",
-    "hmpps_hdc_api_dead_letter_queue"     = "licences-dev",
   }
   sqs_policies = {
     cvl_domain_events_queue             = module.cvl_domain_events_queue.irsa_policy_arn,
     cvl_domain_events_dead_letter_queue = module.cvl_domain_events_dead_letter_queue.irsa_policy_arn,
     cvl_prison_events_queue             = module.cvl_prison_events_queue.irsa_policy_arn,
     cvl_prison_events_dead_letter_queue = module.cvl_prison_events_dead_letter_queue.irsa_policy_arn,
-    hmpps_hdc_api_queue                 = data.aws_ssm_parameter.hmpps_hdc_api_queue_irsa_policy.value,
-    hmpps_hdc_api_dead_letter_queue     = data.aws_ssm_parameter.hmpps_hdc_api_dlq_irsa_policy.value,
   }
 
   sns_topics = {
@@ -39,12 +35,4 @@ module "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
-}
-
-data "aws_ssm_parameter" "hmpps_hdc_api_queue_irsa_policy" {
-  name = "/licences-dev/sqs/hmpps_hdc_api_queue/irsa-policy-arn"
-}
-
-data "aws_ssm_parameter" "hmpps_hdc_api_dlq_irsa_policy" {
-  name = "/licences-dev/sqs/hmpps_hdc_api_dlq/irsa-policy-arn"
 }
