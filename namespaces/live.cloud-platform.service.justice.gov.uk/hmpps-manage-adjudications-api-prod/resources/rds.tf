cccee0b2887b97ceadf69c8b68c4ee286bb14259
Eve Bramley <eve.bramley@digital.justice.gov.uk>
2026-04-07 12:39:34 +0100

    updated db version to 17.6 for ma_rds and read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-prod/resources/rds.tf
@@ -10,8 +10,8 @@ module "ma_rds" {
   infrastructure_support      = var.infrastructure_support
 
   db_instance_class           = "db.t4g.large"
-  rds_family                  = "postgres15"
-  db_engine_version           = "15"
+  rds_family                  = "postgres17"
+  db_engine_version           = "17.6"
   deletion_protection         = true
   db_password_rotated_date    = "15-02-2023"
   allow_major_version_upgrade = "false"
@@ -22,7 +22,7 @@ module "ma_rds" {
   db_allocated_storage        = "1500"
 
   db_engine                   = "postgres"
-  prepare_for_major_upgrade   = false
+  prepare_for_major_upgrade   = true
 
   backup_window               = var.backup_window
   maintenance_window          = var.maintenance_window
@@ -88,8 +88,8 @@ module "dps_rds_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "15"
-  rds_family        = "postgres15"
+  db_engine_version = "17.6"
+  rds_family        = "postgres17"
   db_instance_class = "db.t4g.large"
   # It is mandatory to set the below values to create read replica instance
 
