6820db6caafded79a9c81955e41f06789b80779a
Josh Jones <josh.jones4@digital.justice.gov.uk>
2026-04-08 17:53:25 +0100

    Revert "Adding tags to incident reporting preprod"
    This reverts commit 7448cadb6b836ce681a0d023b2a3e1d13b6203b9.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-preprod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-preprod/resources/main.tf
@@ -7,7 +7,9 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags =  local.default_tags
+    tags = {
+      GithubTeam = var.team_name
+    }
   }
 }
 
@@ -16,7 +18,9 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags =  local.default_tags
+    tags = {
+      GithubTeam = var.team_name
+    }
   }
 }
 
@@ -25,7 +29,9 @@ provider "aws" {
   region = "eu-west-1"
 
   default_tags {
-    tags =  local.default_tags
+    tags = {
+      GithubTeam = var.team_name
+    }
   }
 }
 provider "github" {
@@ -33,17 +39,3 @@ provider "github" {
   owner = var.github_owner
 }
 
-locals {
-  default_tags = {
-    "business-unit"    = var.business_unit
-    "service-area"     = var.service_area
-    "application"      = var.application
-    "is-production"    = var.is_production
-    "environment-name" = var.environment
-    "owner"            = var.team_name
-    "namespace"        = var.namespace
-    "source-code"      = "github.com/ministryofjustice/cloud-platform-environments"
-    "slack-channel"    = var.slack_channel
-    "GithubTeam"       = var.team_name
-  }
-}
