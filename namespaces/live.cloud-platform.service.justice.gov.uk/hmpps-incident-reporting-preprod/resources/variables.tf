c4169a99b5641fa9da9a11dd285db0f1d36ef923
Josh Jones <josh.jones4@digital.justice.gov.uk>
2026-04-08 17:21:57 +0100

    Revert "Adding tags to incident reporting preprod"
    This reverts commit 7448cadb6b836ce681a0d023b2a3e1d13b6203b9.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-preprod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-preprod/resources/variables.tf
@@ -20,12 +20,6 @@ variable "team_name" {
   default = "hmpps-incident-reporting"
 }
 
-variable "service_area" {
-  description = "Service area responsible for this service"
-  type        = string
-  default = "Manage Safety"
-}
-
 variable "environment" {
   default = "preprod"
 }
