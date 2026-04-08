b9b5ab1890bc1ab215594493b30c16ce5983d98e
Josh Jones <josh.jones4@digital.justice.gov.uk>
2026-04-08 16:17:41 +0100

    Added missing tags to incident reporting dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/main.tf
@@ -7,9 +7,7 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags = {
-      GithubTeam = var.team_name
-    }
+    tags =  local.default_tags
   }
 }
 
@@ -18,9 +16,7 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags = {
-      GithubTeam = var.team_name
-    }
+    tags =  local.default_tags
   }
 }
 
@@ -29,9 +25,7 @@ provider "aws" {
   region = "eu-west-1"
 
   default_tags {
-    tags = {
-      GithubTeam = var.team_name
-    }
+    tags =  local.default_tags
   }
 }
 provider "github" {
@@ -39,3 +33,16 @@ provider "github" {
   owner = var.github_owner
 }
 
+locals {
+  default_tags = {
+    "business-unit"    = var.business_unit
+    "service-area"     = var.service_area
+    "application"      = var.application
+    "is-production"    = var.is_production
+    "environment-name" = var.environment
+    "owner"            = var.team_name
+    "namespace"        = var.namespace
+    "source-code"      = "github.com/ministryofjustice/cloud-platform-environments"
+    "slack-channel"    = var.slack_channel
+    "GithubTeam"       = var.team_name
+  }
