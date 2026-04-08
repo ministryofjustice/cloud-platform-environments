1eb2b6bc538571e9a7135fe78e6d39b5c5526367
james.macbeth <james.macbeth@justice.gov.uk>
2026-04-08 14:26:50 +0100

    PI-3894 - Add terraform module to appointment reminders ui prod.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-appointment-reminders-prod/resources/hmpps-appointment-reminders-github.tf
@@ -0,0 +1,17 @@
+module "hmpps-appointment-reminders-ui" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo                   = "hmpps-appointment-reminders-ui"
+  application                   = "hmpps-appointment-reminders-ui"
+  github_team                   = var.team_name
+  environment                   = var.environment_name
+  is_production                 = var.is_production
+  selected_branch_patterns      = ["main"]
+  application_insights_instance = "prod"
+  source_template_repo          = "hmpps-template-typescript"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
