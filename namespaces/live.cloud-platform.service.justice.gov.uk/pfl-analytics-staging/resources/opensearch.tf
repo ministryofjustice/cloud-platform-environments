b909f6bb9b42298e6d635c7be7c429c5c79fb8a8
nina-knowles-justice <nina.knowles@justice.gov.uk>
2026-04-09 13:28:04 +0100

    Add opensearch

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pfl-analytics-staging/resources/opensearch.tf
@@ -0,0 +1,17 @@
+data "kubernetes_secret" "cap_opensearch" {
+  metadata {
+    name      = "care-arrangement-plan-opensearch-proxy-url"
+    namespace = "care-arrangement-plan-staging"
+  }
+}
+
+resource "kubernetes_secret" "cap_opensearch_url" {
+  metadata {
+    name      = "cap-opensearch-url"
+    namespace = var.namespace
+  }
+
+  data = {
+    proxy_url = data.kubernetes_secret.cap_opensearch.data["proxy_url"]
+  }
+}
