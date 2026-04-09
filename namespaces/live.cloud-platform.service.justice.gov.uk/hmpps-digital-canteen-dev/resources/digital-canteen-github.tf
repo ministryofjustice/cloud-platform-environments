eed25f44105ee7678d1b156046071725b3c6681c
drummondjm <mark.drummond@justice.gov.uk>
2026-04-09 11:51:17 +0100

    create namespace for digital canteen dev

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-canteen-dev/resources/digital-canteen-github.tf
@@ -0,0 +1,16 @@
+module "hmpps_template_kotlin" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo = "hmpps-digital-canteen-api"
+  application = "hmpps-digital-canteen-api"
+  github_team = "hmpps-digital-canteen-devs"
+  environment = var.environment
+  selected_branch_patterns      = ["main"] 
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
