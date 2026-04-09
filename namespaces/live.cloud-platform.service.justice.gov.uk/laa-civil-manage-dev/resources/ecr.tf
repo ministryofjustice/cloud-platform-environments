513ee89cc5ca7eda7cb8662fee3edf1237aea4b0
Lucas Main <lucas.e.main@gmail.com>
2026-04-09 14:16:10 +0100

    Add e2e test repo to laa-civil-manage-dev namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["laa-civil-manage"]
+  github_repositories = ["laa-civil-manage", "laa-civil-manage-e2e"]
 
   # Tags
   business_unit          = var.business_unit
