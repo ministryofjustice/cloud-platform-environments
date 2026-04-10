2e2340bc1d2b1d7126f5f0cfee2a9268adf959ab
matt heery <matt.heery@justice.gov.uk>
2026-04-10 14:09:58 +0100

    feat: add github env to dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/ecr.tf
@@ -16,6 +16,7 @@ module "ecr" {
   namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+  github_environments    = ["development"]
 
   enable_irsa = true
 }
