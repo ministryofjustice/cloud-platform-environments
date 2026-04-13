adcbc1c337f3ddba7eaf93c6c62ff6051473bb6b
vvartakMoJ <vidyanand.vartak@justice.gov.uk>
2026-04-13 14:03:12 +0100

    DSTEW-556: Postgres upgrade to v18 in Claims-rporting-service UAT.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-reporting-service-uat/resources/rds-postgresql.tf
@@ -28,11 +28,11 @@ module "rds" {
   ]
 
   # PostgreSQL specifics
-  db_engine         = "postgres"
-  db_engine_version = "17.4"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
-  rds_family        = "postgres17"
-  db_instance_class = "db.t4g.micro"
-
+  prepare_for_major_upgrade = true
+  db_engine                 = "postgres"
+  db_engine_version         = "18.1"   # If you are managing minor version updates, refer to user guide: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/deploying-an-app/relational-databases/upgrade.html#upgrading-a-database-version-or-changing-the-instance-type
+  rds_family                = "postgres18"
+  db_instance_class         = "db.t4g.micro"
   # Tags
   application            = var.application
   business_unit          = var.business_unit
