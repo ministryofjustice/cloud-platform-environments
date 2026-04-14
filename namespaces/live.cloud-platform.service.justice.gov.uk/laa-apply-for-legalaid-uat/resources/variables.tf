ea3ff79841715a2d7a779900c7e14b0d9b5dcf7a
Katharine Ahern <katharine.ahern@justice.gov.uk>
2026-04-14 08:39:43 +0100

    ap-6796: laa-apply-for-legalaid-uat add missing tags and service account variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-apply-for-legalaid-uat/resources/variables.tf
@@ -129,3 +129,8 @@ variable "serviceaccount_rules" {
 variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
 }
+
+variable "service_area" {
+  description = "Service area responsible for this service"
+  default     = "LAA Civil Apply"
+}
