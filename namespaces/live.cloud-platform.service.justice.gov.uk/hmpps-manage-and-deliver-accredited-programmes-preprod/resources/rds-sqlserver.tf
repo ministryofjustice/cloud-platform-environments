06cef5fdd63937c868452c081647aacd343ad13c
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2026-04-16 16:32:53 +0100

    APG-2047 Set backup retention to 0 and add DB drop step before restore in HMPPS manage-and-deliver-accredited-programmes preprod namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-preprod/resources/rds-sqlserver.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-and-deliver-accredited-programmes-preprod/resources/rds-sqlserver.tf
@@ -19,6 +19,7 @@ module "sqlserver" {
   team_name                  = var.team_name
   vpc_name                   = var.vpc_name
   character_set_name         = var.character_set_name
+  backup_retention_period    = 0
 
   enable_irsa = true
 
