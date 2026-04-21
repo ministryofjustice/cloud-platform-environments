232aa0e467d8d1b71ae123413d6fb4f79edafd20
joselopez-moj <jose.lopez@justice.gov.uk>
2026-04-21 13:40:59 +0100

    fix: remove unintentional changes

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-staging/resources/elasticache.tf
@@ -6,7 +6,7 @@
  */
 module "cla-frontend-redis-elasticache" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.2.0"
-  preferred_cache_cluster_azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  preferred_cache_cluster_azs = ["eu-west-2a", "eu-west-2c"]
   
   vpc_name               = var.vpc_name
   team_name              = var.team_name
