e4f1f1d064c72a1e154c7cf22c823dff86e199f3
folarin oyenuga <folarin.oyenuga@digital.justice.gov.uk>
2026-04-21 09:19:56 +0100

    Re-add snapshot_identifier to rds4 to allow in-place disable of deletion_protection

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/rds-postgresql5.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/rds-postgresql5.tf
@@ -45,6 +45,7 @@ module "rds" {
 module "rds4" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
+  snapshot_identifier = "folarin-dev-db1-3rows-20260416"
   rds_name            = "folarin-dev-db4-deletion-protection-test"
 
   # VPC configuration
