69e54813f5959f1b8ebe813b4ced4224d1a4f9fc
Luke Harper <luke.harper4@justice.gov.uk>
2026-04-22 09:24:52 +0100

    CDIA-36 KMS & SM for prod court data ingestion.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-court-data-ingestion-prod/resources/secrets.tf
@@ -0,0 +1,66 @@
+module "secret_ingestion_api_auth_token" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.3.1"
+
+  name        = "${var.namespace}-hmac-token"
+  description = "Shared secret/token used by the MP Lambda Authorizer to verify incoming requests."
+  kms_key_id  = module.secrets_kms.key_id
+
+  ignore_secret_changes = true
+  secret_string         = "populate-manually"
+}
+
+data "aws_iam_policy_document" "secret_ingestion_api_auth_token_policy_data" {
+  statement {
+    sid    = "AllowCrossAccountAccess"
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.modernisation_platform_autorizer_lambda
+      ]
+    }
+
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_secretsmanager_secret_policy" "secret_ingestion_api_auth_token_policy" {
+  secret_arn = module.secret_ingestion_api_auth_token.secret_arn
+  policy     = data.aws_iam_policy_document.secret_ingestion_api_auth_token_policy_data.json
+}
+
+
+resource "aws_iam_policy" "secret_ingestion_api_auth_token_irsa_policy" {
+  name        = "${var.namespace}-secret-auth-irsa-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue"
+        ]
+        Resource = module.secret_ingestion_api_auth_token.secret_arn
+      }
+    ]
+  })
+}
+
+resource "kubernetes_secret" "secret_ingestion_api_auth_token" {
+  metadata {
+    name      = "secret-manager-hmac-auth"
+    namespace = var.namespace
+  }
+
+  data = {
+    secret_id  = module.secret_ingestion_api_auth_token.secret_arn
+  }
+}
