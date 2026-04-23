99b241c98ea9b08cf7b9d52a3829db69cac2f54b
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-23 14:26:33 +0100

    PI-3926 consuming policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
@@ -1,3 +1,3 @@
 data "aws_ssm_parameter" "court_hearings_large_messages_bucket_policy" {
-  name = "/court-probation-${var.environment_name}/large-court-cases-s3-bucket-policy"
+  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-policy"
 }
