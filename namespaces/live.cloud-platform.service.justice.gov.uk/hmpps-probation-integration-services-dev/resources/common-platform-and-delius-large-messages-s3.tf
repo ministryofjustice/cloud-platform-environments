0e95dd89c75352e490e18f00b8bab35199bdfab4
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-23 16:17:58 +0100

    PI-3926 standardise approach

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
@@ -1,3 +1,21 @@
-data "aws_ssm_parameter" "court_hearings_large_messages_bucket_policy" {
-  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-policy"
+
+data "aws_ssm_parameter" "court_hearings_large_messages_bucket_name" {
+  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-name"
 }
+
+data "aws_ssm_parameter" "court_hearings_large_messages_bucket_arn" {
+  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-arn"
+}
+
+data "aws_iam_policy_document" "court_hearings_large_messages_bucket_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${data.aws_ssm_parameter.court_hearings_large_messages_bucket_arn.value}/*",
+    ]
+  }}
