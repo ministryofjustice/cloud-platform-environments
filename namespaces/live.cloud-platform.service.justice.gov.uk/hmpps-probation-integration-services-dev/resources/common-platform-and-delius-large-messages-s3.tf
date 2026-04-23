f2f5f1c1a2635629487cf2b36dbf7d94e3d0a5a3
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-23 16:27:29 +0100

    PI-3926 correct names

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-dev/resources/common-platform-and-delius-large-messages-s3.tf
@@ -1,10 +1,10 @@
 
 data "aws_ssm_parameter" "court_hearings_large_messages_bucket_name" {
-  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-name"
+  name = "/hmpps-person-record-${var.environment_name}/cpr-large-court-cases-s3-bucket-name"
 }
 
 data "aws_ssm_parameter" "court_hearings_large_messages_bucket_arn" {
-  name = "/hmpps-person-record-${var.environment_name}/large-court-cases-s3-bucket-arn"
+  name = "/hmpps-person-record-${var.environment_name}/cpr-large-court-cases-s3-bucket-arn"
 }
 
 data "aws_iam_policy_document" "court_hearings_large_messages_bucket_policy" {
