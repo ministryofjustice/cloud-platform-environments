a4f556c210254208e27e6b762055072ab1f29901
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-04-24 11:47:31 +0100

    MAPB-279: add tags for uat and github actions

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-uat/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-uat/resources/main.tf
@@ -10,6 +10,20 @@ provider "aws" {
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
+      slack-channel = var.slack_channel
+      GithubTeam = var.team_name
+      business-unit = var.business_unit
+      application = var.application
+      is-production = var.is_production
+      owner = var.team_name
+      namespace = var.namespace
+      service-area = var.service_area
+    }
+  }
 }
 
 provider "github" {
