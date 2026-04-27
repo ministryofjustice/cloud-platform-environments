05f15b713290f0af07355c7343f0bc67e6e68acf
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-04-27 13:43:58 +0100

    MAPB-380: add tags, github config basm frontend preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-preprod/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-frontend-preprod/resources/github.tf
@@ -2,8 +2,8 @@ module "hmpps-book-a-secure-move-frontend" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
   force_rotate_token = true
   custom_token_rotation_date    = "2026-03-20"
-  github_repo                   = "hmpps-book-secure-move-api"
-  application                   = "hmpps-book-secure-move-api"
+  github_repo                   = "hmpps-book-secure-move-frontend"
+  application                   = "hmpps-book-secure-move-frontend"
   github_team                   = var.github_review_team
   environment                   = var.environment-name
   is_production                 = var.is_production
