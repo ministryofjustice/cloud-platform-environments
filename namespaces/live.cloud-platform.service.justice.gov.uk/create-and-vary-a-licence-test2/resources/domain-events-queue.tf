9835a6ce14405ecbf49a4015a2cbca77dc226931
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2026-04-27 11:21:08 +0100

    CVSL-3999: Process recall events in CVL API test 2 environment.

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test2/resources/domain-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-test2/resources/domain-events-queue.tf
@@ -108,7 +108,9 @@ resource "aws_sns_topic_subscription" "cvl_domain_events_subscription" {
   filter_policy = jsonencode({
     eventType = [
       "person.community.manager.allocated",
-      "prisoner-offender-search.prisoner.updated"
+      "prisoner-offender-search.prisoner.updated",
+      "recall.inserted",
+      "recall.updated",
     ]
   })
 }
