a4f53d14ea123114fc47f9d02b9b2d7c5b8c7178
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-04-27 16:40:45 +0100

    MAPB-380: add tags, github config to prisoner cell dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-cell-allocation-dev/resources/variables.tf
@@ -27,6 +27,17 @@ variable "deployment_team_name" {
   default = "move-a-prisoner"
 }
 
+variable "github_review_team" {
+  description = "The name of the GitHub team that can review and merge PRs."
+  default     = "map-developers-devs"
+}
+
+variable "service_area" {
+  type        = string
+  description = "Service Area"
+  default     = "Manage Safety"
+}
+
 variable "environment" {
   description = "The type of environment you're deploying to."
   default     = "development"
