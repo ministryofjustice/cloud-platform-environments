276eb5cfc486d969e0becbcdbf1561f8caa663d4
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2026-04-29 11:17:51 +0100

    Fix to single namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-sds-stg/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-sds-stg/resources/variables.tf
@@ -143,7 +143,7 @@ variable "serviceaccount_rules" {
 
 variable "bucket_names" {
   type = list(string)
-  default = ["laa-sds-internal", "laa-data-access-api", "laa-inquests-api"]
+  default = ["laa-sds-internal", "laa-data-access-api"]
 }
 
 variable "service_area" {
