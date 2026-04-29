c6d26917f253ad9129f898a3c46caebb8e50c972
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-04-29 16:44:03 +0100

    RASS-2143-sns queue removed

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-immigration-detention-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-immigration-detention-dev/resources/irsa.tf
@@ -1,12 +1,4 @@
 locals {
-  sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
-  }
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
-  sqs_policies = {
-    hmpps_remand_and_sentencing_prisoner_queue             = module.hmpps_remand_and_sentencing_prisoner_queue.irsa_policy_arn,
-    hmpps_remand_and_sentencing_prisoner_dead_letter_queue = module.hmpps_remand_and_sentencing_prisoner_dead_letter_queue.irsa_policy_arn,
-  }
   ui_sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_ui_sqs : item.name => item.value}
   ui_sqs_queues = {
       "Digital-Prison-Services-${var.environment}-hmpps_audit_queue" = "hmpps-audit-${var.environment}"
@@ -27,11 +19,6 @@ module "irsa-ui" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-data "aws_ssm_parameter" "irsa_policy_arns_sns" {
-  for_each = local.sns_topics
-  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
 
 data "aws_ssm_parameter" "irsa_policy_arns_ui_sqs" {
