a6a7c382576f4a9b41473ecc1eca3666e37f86e9
s99mj <sandhya.gandalwar@justice.gov.uk>
2026-04-30 12:14:30 +0100

    Replaced trivy discovery with synk in hmpps-portfolio-management-prod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-portfolio-management-prod/resources/hmpps-synk-discovery.tf
@@ -0,0 +1,18 @@
+module "hmpps_trivy_discovery" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo                   = "hmpps-synk-discovery"
+  application                   = "hmpps-synk-discovery"
+  github_team                   = "hmpps-sre"
+  environment                   = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  reviewer_teams                = ["hmpps-sre"]
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = "prod"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
+
