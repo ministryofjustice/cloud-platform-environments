f9eaa6d0e6307f4826fe949fef1e20e8be61282a
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-05-05 09:04:05 +0100

    MAPB-380: prod csra aws tags and github config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cell-sharing-risk-assessment-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cell-sharing-risk-assessment-prod/resources/main.tf
@@ -6,36 +6,21 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 
-  default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
-  }
+  tags = local.default_tags
 }
 
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
 
-  default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
-  }
+  tags = local.default_tags
 }
 
 provider "aws" {
   alias  = "ireland"
   region = "eu-west-1"
 
-  default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
-  }
+  tags = local.default_tags
 }
 
 provider "github" {
@@ -44,3 +29,18 @@ provider "github" {
 }
 
 provider "kubernetes" {}
+
+locals {
+  default_tags = {
+    "business-unit"    = var.business_unit
+    "service-area"     = var.service_area
+    "application"      = var.application
+    "is-production"    = var.is_production
+    "environment-name" = var.environment
+    "owner"            = var.team_name
+    "namespace"        = var.namespace
+    "source-code"      = "github.com/ministryofjustice/cloud-platform-environments"
+    "slack-channel"    = var.slack_channel
+    "GithubTeam"       = var.review_team_name
+  }
+}
