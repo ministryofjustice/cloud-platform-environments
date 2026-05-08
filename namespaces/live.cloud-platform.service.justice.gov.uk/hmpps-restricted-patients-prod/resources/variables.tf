08a5a493e61ea6e816d3ebdab1760bcbbb773378
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-05-08 13:52:44 +0100

    SDIT-3675: ✨ Addition of restricted patients api to ui namespace for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-prod/resources/variables.tf
@@ -49,6 +49,10 @@ variable "slack_channel" {
   default     = "dps_adjudications"
 }
 
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
 variable "number_cache_clusters" {
   default = "2"
 }
