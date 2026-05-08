eac0a0fcb657b7b2b7d100557c67e55081e25e9e
Alex Warhurst <174349012+alexw-moj@users.noreply.github.com>
2026-05-08 14:40:03 +0100

    Re-add resource now the policy has been applied

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/rds-assessment-view.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/rds-assessment-view.tf
@@ -95,13 +95,12 @@ resource "random_password" "ro_password" {
   }
 }
 
-# Temporarily commented out, IAM policy (PutSecretValue + kms:GenerateDataKey) must propagate
-# resource "aws_secretsmanager_secret_version" "db" {
-#   provider  = aws.secrets
-#   secret_id = local.secret_arn
-#
-#   secret_string = jsonencode(local.db_secret)
-# }
+resource "aws_secretsmanager_secret_version" "db" {
+  provider  = aws.secrets
+  secret_id = local.secret_arn
+
+  secret_string = jsonencode(local.db_secret)
+}
 
 resource "kubernetes_secret_v1" "db_credentials" {
   metadata {
