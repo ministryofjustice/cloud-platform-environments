65ff8b221882ce359d78fae7f09e740f78047ec4
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-05-11 19:16:08 +0100

    Added service account to laa-data-claims-notify-service-dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-notify-service-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-notify-service-dev/resources/variables.tf
@@ -77,3 +77,9 @@ variable "github_token" {
 variable "eks_cluster_name" {
   description = "The name of the eks cluster to retrieve the OIDC information"
 }
+
+variable "serviceaccount_name" {
+  type        = string
+  description = "Name of the service account used by GitHub Actions to deploy the applications to cloud-platform"
+  default     = "claims-notify-service-uat-service-account"
+}
