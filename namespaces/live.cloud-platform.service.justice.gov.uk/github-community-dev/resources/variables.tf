d43401792e1e92bdbb8c25e3aaed2b3546606491
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-05-11 13:36:02 +0100

    push test change to terraform

--- namespaces/live.cloud-platform.service.justice.gov.uk/github-community-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/github-community-dev/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "github-community"
+  default     = "github-community-dev"
 }
 
 variable "namespace" {
