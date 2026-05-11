36dcbf0a26d7b8e9806496b6b9e3b580f5ac5fce
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-05-11 19:35:51 +0100

    HAAR-4425: 🔧 New namespace hmpps-prison-users-preprod

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prison-users-preprod/resources/variables.tf
@@ -0,0 +1,73 @@
+variable "domain" {
+  default = "manage-restricted-patients-preprod.hmpps.service.justice.gov.uk"
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "prison-users-api"
+}
+
+variable "namespace" {
+  default = "hmpps-prison-users-preprod"
+}
+
+
+variable "vpc_name" {}
+
+variable "kubernetes_cluster" {}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "service_area" {
+  description = "Service area responsible for this service"
+  default     = "Foundations"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "haar"
+}
+
+variable "environment-name" {
+  description = "The type of environment you're deploying to."
+  default     = "preprod"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "preprod"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "dps_adjudications"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
