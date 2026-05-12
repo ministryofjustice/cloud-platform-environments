805b02795d4292f53c5912da8d45beaa0ce5c2ae
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-05-12 09:27:54 +0100

    HAAR-4425: 🔧 add hmpps-prison-users-api to hmpps-auth-stage namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/hmpps-auth-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-auth-stage/resources/hmpps-auth-github.tf
@@ -105,3 +105,21 @@ module "token-verification-api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
+
+module "hmpps-prison-users-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo                   = "hmpps-prison-users-api"
+  application                   = "hmpps-prison-users-api"
+  github_team                   = "haha-live"
+  environment                   = var.environment-name
+  selected_branch_patterns      = ["main", "**/**", "**"]
+  protected_branches_only       = true
+  is_production                 = var.is_production
+  application_insights_instance = "dev"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
