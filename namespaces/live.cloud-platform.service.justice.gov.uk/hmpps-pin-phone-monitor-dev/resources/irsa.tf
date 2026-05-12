eca309c8bed30cb9dd81a3f4116eb1ec8cb39234
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-05-12 09:27:59 +0100

    adding domain events queue config to pcms dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-pin-phone-monitor-dev/resources/irsa.tf
@@ -1,3 +1,10 @@
+locals {
+  irsa_policies_api = merge(local.sqs_policies_api, {
+      pcms_api_queue_for_domain_events                   = module.pcms_api_queue_for_domain_events.irsa_policy_arn
+      pcms_api_queue_for_domain_events_dead_letter_queue = module.pcms_api_queue_for_domain_events_dead_letter_queue.irsa_policy_arn
+    })
+}
+
 module "irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
 
@@ -11,12 +18,14 @@ module "irsa" {
   # Attach the approprate policies using a key => value map
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
-  role_policy_arns = {
+  role_policy_arns = merge({
     rds        = module.rds_aurora.irsa_policy_arn
     s3         = aws_iam_policy.irsa_s3_policy.arn
     s3_sqs     = module.hmpps_pin_phone_monitor_s3_event_queue.irsa_policy_arn
     s3_sqs_dlq = module.hmpps_pin_phone_monitor_s3_event_dead_letter_queue.irsa_policy_arn
-  }
+   },
+   local.irsa_policies_api
+  )
 
   # Tags
   business_unit          = var.business_unit
@@ -37,4 +46,9 @@ resource "kubernetes_secret" "irsa" {
     serviceaccount = module.irsa.service_account.name
     rolearn        = module.irsa.role_arn
   }
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs_api" {
+  for_each = local.sns_topics_api
+  name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
 }
