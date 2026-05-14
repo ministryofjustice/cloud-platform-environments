0b1aca006952074417c25eb2e006a8878ee86b7a
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-05-14 10:26:28 +0100

    SDIT-3675: 🔧 Switch back to standard name for environment (#42514)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-preprod/resources/hmpps-restricted-patients-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-restricted-patients-preprod/resources/hmpps-restricted-patients-github.tf
@@ -24,7 +24,7 @@ module "hmpps-restricted-patients-api" {
   github_repo                   = "hmpps-restricted-patients-api"
   application                   = "hmpps-restricted-patients-api"
   github_team                   = "syscon-devs"
-  environment                   = "preprod-ui"
+  environment                   = "preprod"
   is_production                 = var.is_production
   selected_branch_patterns      = ["main"]
   application_insights_instance = var.environment
