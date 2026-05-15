563f33f6310354996ff240d67962d7c0aeac797a
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2026-05-15 09:58:51 +0100

    add service area variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/soc-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/soc-production/resources/variables.tf
@@ -26,6 +26,11 @@ variable "business_unit" {
   default     = "HQ"
 }
 
+variable "service_area" {
+  description = "The service area of the MOJ responsible for the service."
+  default     = "Central, Victims and Vulnerability"
+}
+
 variable "team_name" {
   description = "The name of your development team"
   default     = "central-digital-product-team"
