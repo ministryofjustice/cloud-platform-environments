f9ed18cb53c76e336b3f5344170895216e9a0f15
kashif.malik@justice.gov.uk <kashif.malik@justice.gov.uk>
2026-05-15 20:22:46 +0000

    added mandatory tags.

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-prod/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-prod/resources/main.tf
@@ -7,10 +7,7 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
+    tags =  local.default_tags
   }
 }
 
@@ -19,10 +16,7 @@ provider "aws" {
   region = "eu-west-2"
 
   default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
+    tags =  local.default_tags
   }
 }
 
@@ -31,10 +25,7 @@ provider "aws" {
   region = "eu-west-1"
 
   default_tags {
-    tags = {
-      source-code   = "github.com/ministryofjustice/cloud-platform-environments"
-      slack-channel = var.slack_channel
-    }
+    tags =  local.default_tags
   }
 }
 
@@ -44,3 +35,18 @@ provider "github" {
 }
 
 provider "kubernetes" {}
+
+locals {
+  default_tags = {
+    "business-unit"    = var.business_unit
+    "service-area"     = var.service_area
+    "application"      = var.application
+    "is-production"    = var.is_production
+    "environment-name" = var.environment
+    "owner"            = var.owner
+    "namespace"        = var.namespace
+    "source-code"      = "github.com/ministryofjustice/cloud-platform-environments"
+    "slack-channel"    = var.slack_channel
+    "GithubTeam"       = var.team_name
+  }
+}
