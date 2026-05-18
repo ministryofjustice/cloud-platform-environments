55320b1d6d988ad3ec4b7180c3507f9d937ba726
Rohit Gokhale <rohit.gokhale@justice.co.uk>
2026-05-18 14:21:51 +0100

    chore: adding autoscaling

--- namespaces/live.cloud-platform.service.justice.gov.uk/stg-track-my-case-service-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/stg-track-my-case-service-dev/resources/serviceaccount.tf
@@ -43,6 +43,7 @@ module "serviceaccount" {
         "rbac.authorization.k8s.io",
         "external-secrets.io",
         "policy",
+        "autoscaling",
       ]
       resources = [
         "deployments",
@@ -57,6 +58,8 @@ module "serviceaccount" {
         "rolebindings",
         "poddisruptionbudgets",
         "externalsecrets",
+        "hpa",
+        "horizontalpodautoscalers",
       ]
       verbs = [
         "get",
