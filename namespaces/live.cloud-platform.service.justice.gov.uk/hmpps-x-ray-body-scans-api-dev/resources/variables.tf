50b339eb55405bcc9eb0034964c1fb96a41c8511
Ewan-Donovan <ewan.donovan@digital.justice.co.uk>
2026-05-18 16:38:31 +0100

    Add namespace and config + attempt to fix unverified commit?

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-x-ray-body-scans-api-dev/resources/variables.tf
@@ -0,0 +1,68 @@
+variable "vpc_name" {
+}
+
+variable "kubernetes_cluster" {
+}
+
+variable "application" {
+  description = "Name of Application you are deploying"
+  default     = "hmpps-x-ray-body-scans-api"
+}
+
+variable "namespace" {
+  default = "hmpps-x-ray-body-scans-api-dev"
+}
+
+variable "business_unit" {
+  description = "Area of the MOJ responsible for the service."
+  default     = "HMPPS"
+}
+
+variable "team_name" {
+  description = "The name of your development team"
+  default     = "connect-dps"
+}
+
+variable "service_area" {
+  description = "Service area responsible for this service"
+  default     = "Foundations"
+}
+
+variable "environment" {
+  description = "The type of environment you're deploying to."
+  default     = "dev"
+}
+
+variable "infrastructure_support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form team-email."
+  default     = "dps-hmpps@digital.justice.gov.uk"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "slack_channel" {
+  description = "Team slack channel to use if we need to contact your team"
+  default     = "connect-dps"
+}
+
+variable "number_cache_clusters" {
+  default = "2"
+}
+variable "github_owner" {
+  description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
+  type        = string
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Required by the GitHub Terraform provider"
+  default     = ""
+}
+
+variable "github_review_team" {
+  description = "The name of the GitHub team that can review and merge PRs."
+  default     = "connect-dps-devs"
+}
