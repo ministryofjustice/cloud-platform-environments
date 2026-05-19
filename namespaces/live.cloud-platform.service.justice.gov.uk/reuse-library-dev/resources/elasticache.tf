84a96b1193e2ff5a208a1c1cfd79cef511c2b1b3
Aron-v1 <aron.marriottsmith@justice.gov.uk>
2026-05-19 15:57:00 +0100

    Add redis cluster

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/reuse-library-dev/resources/elasticache.tf
@@ -0,0 +1,34 @@
+module "reuse_library_elasticache_redis" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-elasticache-cluster?ref=8.0.0"
+  vpc_name               = var.vpc_name
+  application            = var.application
+  environment_name       = var.environment-name
+  is_production          = var.is_production
+  infrastructure_support = var.infrastructure_support
+  team_name              = var.team_name
+  number_cache_clusters  = var.number_cache_clusters
+  business_unit          = var.business_unit
+  node_type              = "cache.t4g.micro"
+  engine_version         = "9.0"
+  parameter_group_name   = "default.redis9"
+  namespace              = var.namespace
+
+  providers = {
+    aws = aws.london
+  }
+
+  enable_irsa = true
+}
+
+resource "kubernetes_secret" "reuse_library_elasticache_redis" {
+  metadata {
+    name      = "reuse-library-elasticache-redis"
+    namespace = var.namespace
+  }
+
+  data = {
+    primary_endpoint_address = module.reuse_library_elasticache_redis.primary_endpoint_address
+    auth_token               = module.reuse_library_elasticache_redis.auth_token
+    member_clusters          = jsonencode(module.reuse_library_elasticache_redis.member_clusters)
+  }
+}
