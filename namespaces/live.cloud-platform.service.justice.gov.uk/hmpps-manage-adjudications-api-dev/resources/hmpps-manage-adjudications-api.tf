7d774e57d4b4f6b5a581dcbd1c827af395ce602c
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-05-21 13:51:25 +0100

    Allow dev deployments from ticket branches
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/hmpps-manage-adjudications-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-dev/resources/hmpps-manage-adjudications-api.tf
@@ -7,8 +7,8 @@ module "hmpps_template_kotlin" {
   github_team = "hmpps-prisons-digital-live-support"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
   reviewer_teams                = ["hmpps-prisons-digital-live-support"] # Optional team that should review deployments to this environment.
-  selected_branch_patterns      = ["main"] # Optional
-  #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
+  selected_branch_patterns      = ["main", "LSM-*"] # Optional
+  #protected_branches_only      = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
   source_template_repo          = "hmpps-template-kotlin"
