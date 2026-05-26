1f1db69dc53609031d4b7ec2e7d3106df12ae62b
Mark Butler <mark.butler@solirius.com>
2026-05-26 14:33:14 +0100

    Fix syntax errors

--- namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-dev/resources/apex-s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-dev/resources/apex-s3.tf
@@ -1,10 +1,8 @@
 module "apex-migration-s3" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
 
-  # S3 configuration
   versioning = true
 
-  # Tags
   business_unit          = var.business_unit
   application            = local.application
   is_production          = var.is_production
@@ -17,18 +15,18 @@ module "apex-migration-s3" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
           AWS = [
-              "arn:aws:sts::754256621582:assumed-role/access-via-github/Tim97eng",
-              "arn:aws:sts::754256621582:assumed-role/access-via-github/mark-butler-solirius"
+            "arn:aws:sts::754256621582:assumed-role/access-via-github/Tim97eng",
+            "arn:aws:sts::754256621582:assumed-role/access-via-github/mark-butler-solirius"
           ]
-        },
+        }
         Action = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject"
-        ],
+        ]
         Resource = [
           "$${bucket_arn}/*"
         ]
@@ -37,8 +35,8 @@ module "apex-migration-s3" {
         Effect = "Allow"
         Principal = {
           AWS = [
-              "arn:aws:sts::754256621582:assumed-role/access-via-github/Tim97eng",
-              "arn:aws:sts::754256621582:assumed-role/access-via-github/mark-butler-solirius"
+            "arn:aws:sts::754256621582:assumed-role/access-via-github/Tim97eng",
+            "arn:aws:sts::754256621582:assumed-role/access-via-github/mark-butler-solirius"
           ]
         }
         Action = [
@@ -50,19 +48,19 @@ module "apex-migration-s3" {
       }
     ]
   })
+}
 
-  resource "kubernetes_secret" "s3_bucket" {
-    metadata {
-      name      = "s3-bucket-output"
-      namespace = var.namespace
-    }
+resource "kubernetes_secret" "s3_bucket" {
+  metadata {
+    name      = "s3-bucket-output"
+    namespace = var.namespace
+  }
 
-    data = {
-      bucket_arn           = module.s3_bucket.bucket_arn
-      bucket_name          = module.s3_bucket.bucket_name
-      s3_access_user_arn   = aws_iam_user.s3_user.arn
-      s3_access_key_id     = aws_iam_access_key.s3_user.id
-      s3_secret_access_key = aws_iam_access_key.s3_user.secret
-    }
+  data = {
+    bucket_arn           = module.apex-migration-s3.bucket_arn
+    bucket_name          = module.apex-migration-s3.bucket_name
+    s3_access_user_arn   = aws_iam_user.s3_user.arn
+    s3_access_key_id     = aws_iam_access_key.s3_user.id
+    s3_secret_access_key = aws_iam_access_key.s3_user.secret
   }
 }
