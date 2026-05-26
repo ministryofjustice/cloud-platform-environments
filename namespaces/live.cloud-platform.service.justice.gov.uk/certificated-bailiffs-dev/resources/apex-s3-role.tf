dd2dbf121c08251c7222e363883576989a230837
Mark Butler <mark.butler@solirius.com>
2026-05-26 15:42:52 +0100

    Fix syntax errors

--- namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-dev/resources/apex-s3-role.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-dev/resources/apex-s3-role.tf
@@ -2,55 +2,45 @@ resource "aws_iam_role" "rds_s3_integration" {
   name = "apex-rds-s3-integration-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow",
+      Effect = "Allow"
       Principal = {
         Service = "rds.amazonaws.com"
-      },
+      }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-policy = jsonencode({
-  Version = "2012-10-17"
-  Statement = [
-    {
-      Effect = "Allow"
-      Principal = {
-        AWS = [
-          aws_iam_role.rds_s3_integration.arn
-        ]
-      }
-      Action = [
-        "s3:ListBucket"
-      ]
-      Resource = [
-        "$${bucket_arn}"
-      ]
-    },
-    {
-      Effect = "Allow"
-      Principal = {
-        AWS = [
-          aws_iam_role.rds_s3_integration.arn
-        ]
-      }
-      Action = [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "s3:PutObject",
-        "s3:DeleteObject"
-      ]
-      Resource = [
-        "$${bucket_arn}/*"
-      ]
-    }
-  ]
-})
+resource "aws_iam_role_policy" "rds_s3_policy" {
+  name = "apex-rds-s3-policy"
+  role = aws_iam_role.rds_s3_integration.id
 
-resource "aws_iam_role_policy_attachment" "rds_s3_attach" {
-  role       = aws_iam_role.rds_s3_integration.name
-  policy_arn = aws_iam_policy.rds_s3_access_policy.arn
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = [
+          module.apex-migration-s3.bucket_arn
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "${module.apex-migration-s3.bucket_arn}/*"
+        ]
+      }
+    ]
+  })
 }
