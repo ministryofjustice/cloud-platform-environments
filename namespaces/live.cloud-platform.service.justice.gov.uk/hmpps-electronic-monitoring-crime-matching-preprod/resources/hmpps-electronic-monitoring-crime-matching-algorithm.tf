5015efd85ca8022b0282defd067addb3d44afefc
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2026-05-27 11:04:29 +0100

    Removing ECR config for now

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-preprod/resources/hmpps-electronic-monitoring-crime-matching-algorithm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-preprod/resources/hmpps-electronic-monitoring-crime-matching-algorithm.tf
@@ -19,23 +19,3 @@ module "hmpps_electronic_monitoring_crime_matching_algorithm" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
-
-module "hmpps_electronic_monitoring_crime_matching_algorithm_container_repository" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
-
-  # Repository configuration
-  repo_name = local.crime_matching_algorithm_repo
-
-  # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = [local.crime_matching_algorithm_repo]
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the container repository
-  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-}
