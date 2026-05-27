380fd9f4f046dfde25eb9c3907844ea2d22e172e
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2026-05-27 12:19:07 +0100

    Migrate pingdom alert configuration to hmpps SRE management.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-dev/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-adjustments-dev/resources/pingdom.tf
@@ -19,19 +19,3 @@ resource "pingdom_check" "hmpps-adjustments-dev-check" {
   probefilters             = "region:EU"
   integrationids           = [141508]
 }
-
-resource "pingdom_check" "hmpps-adjustments-api-dev-check" {
-  type                     = "http"
-  name                     = "DPS - HMPPS Adjustments API - Development"
-  host                     = "health-kick.prison.service.justice.gov.uk"
-  resolution               = 1
-  notifywhenbackup         = true
-  sendnotificationwhendown = 6
-  notifyagainevery         = 0
-  url                      = "/https/adjustments-api-dev.hmpps.service.justice.gov.uk"
-  encryption               = true
-  port                     = 443
-  tags                     = "dps,hmpps,cloudplatform-managed"
-  probefilters             = "region:EU"
-  integrationids           = [141508]
-}
