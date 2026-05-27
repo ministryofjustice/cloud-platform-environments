3291308e3f9d25cdc765acbb72a42b6f6393b430
Alex Warhurst <174349012+alexw-moj@users.noreply.github.com>
2026-05-27 09:27:44 +0100

    terraform fmt

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/serviceaccount.tf
@@ -1,8 +1,8 @@
 module "serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
-  namespace = var.namespace
-  kubernetes_cluster = var.kubernetes_cluster
-  serviceaccount_name = "circleci"
+  source                            = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+  namespace                         = var.namespace
+  kubernetes_cluster                = var.kubernetes_cluster
+  serviceaccount_name               = "circleci"
   serviceaccount_token_rotated_date = "20-03-2026"
   github_repositories = [
     "hmpps-assess-risks-and-needs-handover-service",
