918a81945a14a7ff7cebd70f4069cc6ba68b3e3c
Venkat-Anumula-MoJ <Venkat.Anumula@justice.gov.uk>
2026-06-01 15:24:46 +0100

    Add ecr config with deletion protection disabled to bypass pipeline crash

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-redact-staging/resources/ecr.tf
@@ -0,0 +1,24 @@
+module "ecr" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
+
+  # Repository configuration
+  repo_name = "${var.team_name}/${var.namespace}"
+
+  # ✅ This flag tells the pipeline it's safe to destroy this duplicate resource
+  deletion_protection = false
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github"]
+  github_repositories = ["justice-redact-frontend", "justice-redact-backend"]
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name 
+  namespace              = var.namespace 
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  enable_irsa = true
+}
