874afdfb4d0bd741ae9781582c53a1eca87a968b
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2026-06-01 15:12:47 +0100

    CVSL-4096 remove event from preprod (#42892)

--- namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-preprod/resources/domain-events-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/create-and-vary-a-licence-api-preprod/resources/domain-events-queue.tf
@@ -111,7 +111,6 @@ resource "aws_sns_topic_subscription" "cvl_domain_events_subscription" {
       "prisoner-offender-search.prisoner.updated",
       "recall.inserted",
       "recall.updated",
-      "restricted-patients.patient.supporting-prison-changed",
     ]
   })
 }
