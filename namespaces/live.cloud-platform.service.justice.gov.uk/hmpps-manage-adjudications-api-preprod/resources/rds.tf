77b02a6dae62481119efc89a70ccfaa90b8fd61a
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-06-02 12:14:06 +0100

    Remove DB Engine minor version
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-api-preprod/resources/rds.tf
@@ -14,7 +14,7 @@ module "ma_rds" {
 
   db_instance_class           = "db.t4g.large"
   rds_family                  = "postgres17"
-  db_engine_version           = "17.6"
+  db_engine_version           = "17"
   deletion_protection         = true
   db_engine                   = "postgres"
   db_password_rotated_date    = "15-02-2023"
