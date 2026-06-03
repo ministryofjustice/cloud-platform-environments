55a6a1e8dc40391a4e48e0a7b7f1fd1329afe605
said-moj <45761276+said-moj@users.noreply.github.com>
2026-06-03 16:04:40 +0100

    Set environment-name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/secretsmanager.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/secretsmanager.tf
@@ -13,6 +13,6 @@ module "secret" {
   is_production          = var.is_production
   team_name              = var.team_name
   namespace              = var.namespace
-  environment_name       = var.environment
+  environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
 }
