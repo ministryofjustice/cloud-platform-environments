1a21629faea4120dae3de6664a2b6e21852d8098
dharmendrak <dharmendra.kumar@justice.gov.uk>
2026-06-05 09:47:04 +0100

    Correct team name

--- namespaces/live.cloud-platform.service.justice.gov.uk/srrc-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srrc-dev/resources/variables.tf
@@ -35,7 +35,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "SRRC-Dev-Team"
+  default     = "srrc-tech"
 }
 
 variable "environment" {
