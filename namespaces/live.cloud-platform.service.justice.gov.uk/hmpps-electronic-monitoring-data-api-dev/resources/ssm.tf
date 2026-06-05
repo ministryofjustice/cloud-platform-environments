2d38f23de2bf3f2f5d72030d45fd2b154dae5641
“kraihanmoj” <khristiania.raihan@justice.gov.uk>
2026-06-05 16:17:58 +0100

    fix api data role ssm pointer

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-api-dev/resources/ssm.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-api-dev/resources/ssm.tf
@@ -3,8 +3,8 @@
 #   with_decryption = true
 # }
 
-resource "aws_ssm_parameter" "emd_validation_db_read_data_prod" {
-  name        = "/${var.namespace}/emd_validation_db_read_data_prod"
+resource "aws_ssm_parameter" "data_store_general_role_arn" {
+  name        = "/${var.namespace}/data_store_general_role_arn"
   type        = "SecureString"
   # This value must be replaced with a genuine role ARN using AWS CLI
   value       = "arn:aws:iam::0000000000000:role/general-placeholder"
