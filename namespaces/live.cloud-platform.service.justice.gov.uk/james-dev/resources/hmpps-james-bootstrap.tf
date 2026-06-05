c108bd1b5f95722197fbefd377605667657b9522
Matt <34448412+mattops@users.noreply.github.com>
2026-06-05 15:09:35 +0100

    fix test egress changes (#43059)
    * Update, testing egress rules
    
    * terraform fmt

--- namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/hmpps-james-bootstrap.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/hmpps-james-bootstrap.tf
@@ -5,7 +5,7 @@ module "hmpps_james_bootstrap" {
   github_team = "hmpps-sre"
   environment = var.environment # 
   #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] #
-  selected_branch_patterns      = ["main", "**/**", "**", "fix/**"] # Optional
+  selected_branch_patterns = ["main", "**/**", "**", "fix/**"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
