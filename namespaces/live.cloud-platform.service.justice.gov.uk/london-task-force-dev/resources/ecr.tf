845332843d1315f27e854e8cadcdca4eea0dbfc8
Stuart James <stuart.james1@justice.gov.uk>
2026-06-05 16:21:03 +0100

    Org swap london task force

--- namespaces/live.cloud-platform.service.justice.gov.uk/london-task-force-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/london-task-force-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["performance-information-intelligence-system"]
+  github_repositories = ["performance-information-intelligence-system-demo"]
 
   # Tags
   business_unit          = var.business_unit
