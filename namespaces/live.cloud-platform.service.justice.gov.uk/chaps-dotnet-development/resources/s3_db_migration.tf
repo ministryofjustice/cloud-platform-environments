c6689c49de139bbe2c91907dfa50ee9d0cbf508c
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2026-06-05 15:21:44 +0100

    tidy dev db migration resources

--- namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-development/resources/s3_db_migration.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-development/resources/s3_db_migration.tf
@@ -1,7 +1,7 @@
 locals {
   db_migration_environment_short_name = "dev"
   db_migration_prefix = "native-backups/${local.db_migration_environment_short_name}"
-  db_migration_bucket_name = "${var.namespace}-db-migration-${data.aws_caller_identity.current.account_id}"
+  db_migration_bucket_name = "chaps-${local.db_migration_environment_short_name}-db-migration-${data.aws_caller_identity.current.account_id}"
 
 
   db_migration_tags = {
