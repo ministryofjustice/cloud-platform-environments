943248c3bed1399e001ef544318aadf0d7e382fb
Elavarasan Pandiyan <elavarasan.pandiyan@justice.gov.uk>
2026-06-08 18:54:19 +0100

    change HMPPS  SQS queue and create kubernetes secret for queue URL

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-people-on-probation-dev/resources/hmpps-auth-secrets.tf
@@ -0,0 +1,10 @@
+resource "kubernetes_secret" "hmpps_person_on_probation_audit_queue_secret" {
+  metadata {
+    name      = "sqs-hmpps-audit-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url = "https://sqs.eu-west-2.amazonaws.com/754256621582/Digital-Prison-Services-dev-hmpps_person_on_probation_audit_queue""
+  }
+}
