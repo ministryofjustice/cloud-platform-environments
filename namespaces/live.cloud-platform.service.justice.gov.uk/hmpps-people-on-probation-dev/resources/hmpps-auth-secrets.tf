898b0310dce2731a5a0a06f2eb4275bf3613680d
Elavarasan Pandiyan <elavarasan.pandiyan@justice.gov.uk>
2026-06-08 18:56:39 +0100

    fix typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-people-on-probation-dev/resources/hmpps-auth-secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-people-on-probation-dev/resources/hmpps-auth-secrets.tf
@@ -5,6 +5,6 @@ resource "kubernetes_secret" "hmpps_person_on_probation_audit_queue_secret" {
   }
 
   data = {
-    sqs_queue_url = "https://sqs.eu-west-2.amazonaws.com/754256621582/Digital-Prison-Services-dev-hmpps_person_on_probation_audit_queue""
+    sqs_queue_url = "https://sqs.eu-west-2.amazonaws.com/754256621582/Digital-Prison-Services-dev-hmpps_person_on_probation_audit_queue"
   }
 }
