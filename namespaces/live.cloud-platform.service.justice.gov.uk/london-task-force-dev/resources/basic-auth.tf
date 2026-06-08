776218146bde4972ec8a80656137598dc9e8d088
Stuart James <stuart.james1@justice.gov.uk>
2026-06-08 11:28:15 +0100

    Added auth for LTF app

--- namespaces/live.cloud-platform.service.justice.gov.uk/london-task-force-dev/resources/basic-auth.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/london-task-force-dev/resources/basic-auth.tf
@@ -1,4 +1,4 @@
-# Username and password for the prototype kit website's http basic
+# Username and password for the demo website's nginx ingress http basic
 # authentication
 resource "kubernetes_secret" "basic-auth" {
   metadata {
@@ -6,8 +6,15 @@ resource "kubernetes_secret" "basic-auth" {
     namespace = var.namespace
   }
 
+  # nginx ingress basic auth expects a single "auth" key in htpasswd format:
+  # "<username>:<bcrypt-hashed-password>"
   data = {
-    username = var.basic-auth-username
-    password = var.basic-auth-password
+    auth = "${var.basic-auth-username}:${bcrypt(var.basic-auth-password)}"
+  }
+
+  lifecycle {
+    # bcrypt() generates a new salt on every run, which would otherwise force
+    # the secret to be recreated on each apply.
+    ignore_changes = [data]
   }
 }
