b9cd25b5fc57eeb1b8b8a900d1a3d67b0da296fc
Alex Warhurst <174349012+alexw-moj@users.noreply.github.com>
2026-06-08 11:47:57 +0100

    Remove overloaded variables

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/irsa-arns-assessment-view-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-dev/resources/irsa-arns-assessment-view-api.tf
@@ -1,14 +1,3 @@
-locals {
-  arns_coordinator_api_sqs_queues = {
-    "hmpps-assessments-dev-arns_coordinator_queue" = "hmpps-assess-risks-and-needs-integrations-dev"
-  }
-
-  arns_coordinator_api_sqs_policies = {
-    for item in data.aws_ssm_parameter.arns_coordinator_api_irsa_policy_arns_sqs :
-    item.name => item.value
-  }
-}
-
 module "irsa_arns_assessment_view_api" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
 
@@ -24,9 +13,4 @@ module "irsa_arns_assessment_view_api" {
   team_name              = var.team_name
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-}
-
-data "aws_ssm_parameter" "arns_coordinator_api_irsa_policy_arns_sqs" {
-  for_each = local.arns_coordinator_api_sqs_queues
-  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
