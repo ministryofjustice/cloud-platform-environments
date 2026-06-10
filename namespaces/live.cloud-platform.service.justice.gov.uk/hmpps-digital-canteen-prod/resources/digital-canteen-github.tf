717b957436a3b3ab026395f5a116d264a5915f96
drummondjm <mark.drummond@justice.gov.uk>
2026-06-10 16:46:30 +0100

    add DB for prod update medusa config

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-canteen-prod/resources/digital-canteen-github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-digital-canteen-prod/resources/digital-canteen-github.tf
@@ -15,3 +15,21 @@ module "hmpps-digital-canteen-api" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
+
+module "hmpps-digital-canteen-medusa-service" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo = "hmpps-digital-canteen-medusa-service"
+  application = "hmpps-digital-canteen-medusa-service"
+  github_team = "hmpps-digital-canteen-live"
+  environment = var.environment
+  selected_branch_patterns      = ["main"]
+  reviewer_teams                = [var.github_deployment_team]
+  is_production                 = var.is_production
+  application_insights_instance = "prod"
+  source_template_repo          = "none"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
