70d4cb291e63a089f400d4ec44bcc99bf249f1ff
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-06-10 14:03:31 +0100

    MAPB-451: add rds connection parameters for DPR

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-book-secure-move-api-preprod/resources/rds.tf
@@ -14,6 +14,10 @@ module "rds-instance" {
   backup_window      = var.backup_window
   maintenance_window = var.maintenance_window
 
+  # Add security groups for DPR
+  vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
+
+
   # this isn't possible with a read replica
   enable_rds_auto_start_stop = false
 
@@ -63,6 +67,11 @@ module "rds-instance" {
     {
       name         = "wal_sender_timeout"
       value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
       apply_method = "immediate"
     },
     {
@@ -143,6 +152,38 @@ module "rds-read-replica" {
     aws = aws.london
   }
 
+  db_parameter = [
+    {
+      name         = "rds.logical_replication"
+      value        = "1"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "shared_preload_libraries"
+      value        = "pglogical"
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "max_wal_size"
+      value        = "1024"
+      apply_method = "immediate"
+    },
+    {
+      name         = "wal_sender_timeout"
+      value        = "0"
+      apply_method = "immediate"
+    },
+    {
+      name         = "max_slot_wal_keep_size"
+      value        = "40000"
+      apply_method = "immediate"
+    },
+    {
+      name         = "hot_standby_feedback"
+      value        = "1"
+      apply_method = "immediate"
+    }
+  ]
 
   # Add security groups for DPR
   vpc_security_group_ids = [data.aws_security_group.mp_dps_sg.id]
