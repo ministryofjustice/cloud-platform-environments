a77087fe70fd742eb870804a01657686dca179e3
Alan Sage <alan.sage@madetech.com>
2026-06-11 10:33:32 +0100

    Remove e2e from dev namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-dev/resources/ecr.tf
@@ -12,7 +12,7 @@ module "ecr" {
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["laa-civil-manage", "laa-civil-manage-e2e"]
+  github_repositories = ["laa-civil-manage"]
 
   # Tags
   business_unit          = var.business_unit
