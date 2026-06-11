58426f2b6553f2ea6178bdc39768548c93925d63
mikereiddigital <mike.reid@justice.gov.uk>
2026-06-11 14:03:22 +0100

    Adds the modernisation-platform-slackbot-app-prod namespace & aws resources.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/modernisation-platform-slackbot-app-prod/resources/ecr.tf
@@ -0,0 +1,40 @@
+module "container_repository" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.0"
+
+  # Repository configuration
+  repo_name = "${var.namespace}-ecr"
+
+  # OpenID Connect configuration
+  oidc_providers      = ["github"]
+  github_repositories = ["modernisation-platform-slackbot"]
+  github_environments = ["production"]
+
+  # Lifecycle Policy
+  lifecycle_policy = <<EOF
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Only keep latest 12 images",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 12
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+    EOF
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name # also used for naming the container repository
+  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
