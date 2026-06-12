41071ab7954544e20aa305c8e4cd280b875b4ebd
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-06-12 10:25:42 +0100

    MAPB-452 Add hmpps-prisoner-property-api CD service account (dev)
    Creates the GitHub Actions deploy service account and KUBE_* environment secrets so
    hmpps-prisoner-property-api can deploy to the shared hmpps-locations-inside-prison-dev namespace.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-locations-inside-prison-dev/resources/github.tf
@@ -51,3 +51,19 @@ module "hmpps-non-residential-locations-ui" {
   kubernetes_cluster            = var.kubernetes_cluster
   github_owner                  = var.github_owner
 }
+
+module "hmpps-prisoner-property-api" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  github_repo                   = "hmpps-prisoner-property-api"
+  application                   = "hmpps-prisoner-property-api"
+  github_team                   = var.team_name
+  environment                   = var.deployment_environment
+  is_production                 = var.is_production
+  protected_branches_only       = true
+  application_insights_instance = var.deployment_environment
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+  github_owner                  = var.github_owner
+}
