d778fb398a955e7f0b3c1ad99ece82da83dcf7c7
Alberto Aguilera Juarez <alberto.aguilerajuarez@justice.gov.uk>
2026-06-12 11:36:49 +0100

    fix: revert MCC Elasticache Redis to 7.1 (latest available)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-your-civil-cases-uat/resources/elasticache.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-manage-your-civil-cases-uat/resources/elasticache.tf
@@ -9,8 +9,8 @@ module "elasticache_redis" {
   number_cache_clusters  = var.number_cache_clusters
   business_unit          = var.business_unit
   node_type              = "cache.t4g.micro"
-  engine_version         = "9.0"
-  parameter_group_name   = "default.valkey9.cluster.on"
+  engine_version         = "7.1"
+  parameter_group_name   = "default.redis7"
   namespace              = var.namespace
 
   auth_token_rotated_date = "2023-02-14"
