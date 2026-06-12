341e77ba883e4bedc796980bceb445e80612f13c
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-06-12 11:29:15 +0100

    Set enable_egress_controls to false (oops)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-forge-dev/resources/egress-controls.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-forge-dev/resources/egress-controls.tf
@@ -2,7 +2,7 @@ module "hmpps_egress_controls" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-egress-controls?ref=0.0.3"
 
   enable_envoy_setup     = true
-  enable_egress_controls = true
+  enable_egress_controls = false # Stage 2: set to true once the app is confirmed to route via the proxy
   namespace              = var.namespace
   vpc_name               = var.vpc_name
 }
