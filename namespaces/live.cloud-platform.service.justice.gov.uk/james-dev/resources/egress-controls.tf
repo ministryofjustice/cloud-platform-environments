1a58d107366440a049a698b0c2d953503623175b
Matt <34448412+mattops@users.noreply.github.com>
2026-06-15 10:13:10 +0100

    Update egress controls of james-dev namespaces (#43208)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/egress-controls.tf
@@ -0,0 +1,8 @@
+module "hmpps_egress_controls" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-egress-controls?ref=0.0.3"
+
+  enable_envoy_setup     = true
+  enable_egress_controls = true
+  namespace              = var.namespace
+  vpc_name               = var.vpc_name
+}
