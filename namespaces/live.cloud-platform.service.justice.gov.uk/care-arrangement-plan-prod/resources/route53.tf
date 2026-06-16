0952e601213b3d747d6f17d966d372542919849f
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-06-16 12:39:43 +0100

    Reorder Route53 TXT record resource for domain verification.

--- namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/route53.tf
@@ -13,17 +13,6 @@ resource "aws_route53_zone" "cap_route53_zone" {
   }
 }
 
-# add a txt record to the zone to verify ownership of the domain with Microsoft Entra ID
-resource "aws_route53_record" "entra_id_verification" {
-  zone_id = aws_route53_zone.pcap_zone.zone_id
-  name    = "propose-child-arrangements-plan.service.gov.uk"
-  type    = "TXT"
-  ttl     = 300
-  records = [
-    "MS=ms47915806"
-  ]
-}
-
 resource "kubernetes_secret" "cap_route53_zone_sec" {
   metadata {
     name      = "cap-route53-zone-output"
@@ -48,6 +37,17 @@ resource "aws_route53_zone" "pcap_zone" {
     environment_name       = var.environment
     infrastructure_support = var.infrastructure_support
   }
+}
+
+# add a txt record to the zone to verify ownership of the domain with Microsoft Entra ID
+resource "aws_route53_record" "entra_id_verification" {
+  zone_id = aws_route53_zone.pcap_zone.zone_id
+  name    = "propose-child-arrangements-plan.service.gov.uk"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "MS=ms47915806"
+  ]
 }
 
 resource "kubernetes_secret" "pcap_zone_sec" {
