04cea5f1c270a3f44e30a86c9114c6e8f093fb9f
James Reed <james.reed1@justice.gov.uk>
2026-06-16 16:36:36 +0100

    fix queue names

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-mi-ui-dev/resources/domain-events-lao-queues.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-mi-ui-dev/resources/domain-events-lao-queues.tf
@@ -1,7 +1,7 @@
 module "hmpps_probation_mi_domain_events_queue" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
 
-  sqs_name = "domain_events_queue"
+  sqs_name = "lao_domain_events"
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.hmpps_probation_mi_domain_events_dlq.sqs_arn
     maxReceiveCount     = 3
@@ -45,7 +45,7 @@ resource "aws_sqs_queue_policy" "hmpps_probation_mi_domain_events_queue_policy" 
 module "hmpps_probation_mi_domain_events_dlq" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.2"
 
-  sqs_name                  = "hmpps_probation_mi_domain_events_dlq"
+  sqs_name                  = "lao_domain_events_dlq"
   message_retention_seconds = 7 * 24 * 3600 # 1 week
 
   business_unit          = var.business_unit
