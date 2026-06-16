b9cc2d34af79fe203b7e9550e13df0359d584219
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2026-06-16 17:24:40 +0100

    add the specific sentryDNS as only acceptable sentry egress endoint

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/egress-controls.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-activities-management-dev/resources/egress-controls.tf
@@ -12,11 +12,8 @@ module "hmpps_egress_controls" {
 
   # Add your service's external dependencies
   envoy_extra_allowed_hosts_exact = [
-    "www.gov.uk"
-  ]
-
-  envoy_extra_allowed_hosts_suffixes = [
-    "ingest.sentry.io"
+    "www.gov.uk",
+    "283af212ca8bb8bf0d6aac43a6b83046@o345774.ingest.sentry.io"
   ]
 
 }
