5444cb7b72ae314d72a6b4986b29693cbfdd751a
Dean Longstaff <dean.longstaff@justice.gov.uk>
2026-06-16 21:46:44 +0100

    Create VS-Code Marketplace Namespace
    Created a new namespace to host vscode private marketplace with a custom curator. This is a proof-on-concept product that allows us to control the list of vscode extensions that are available for installation across the estate.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/vscode-marketplace/resources/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  alias  = "london"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.eks_cluster_name
+}
