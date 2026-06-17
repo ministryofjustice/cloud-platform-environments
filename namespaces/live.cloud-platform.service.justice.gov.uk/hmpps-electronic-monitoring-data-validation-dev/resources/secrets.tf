165e0a73324c1753c93482e20689e1382e1b174d
gwionap <gwion.aprhobat@justice.gov.uk>
2026-06-17 15:16:57 +0100

    Add additional secret.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/secrets.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-data-validation-dev/resources/secrets.tf
@@ -22,3 +22,28 @@ module "secrets_manager_multiple_secrets" {
     },
   }
 }
+
+module "secrets_manager_dns_resolver" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.7"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "dns_resolver_domain" = {
+      description             = "DNS Resolver Domain",
+      recovery_window_in_days = 0
+      k8s_secret_name         = "dns-resolver-domain"
+    },
+    "dns_resolver_ip" = {
+      description             = "DNS Resolver IP",
+      recovery_window_in_days = 0
+      k8s_secret_name         = "dns-resolver-ip"
+    },
+  }
+}
