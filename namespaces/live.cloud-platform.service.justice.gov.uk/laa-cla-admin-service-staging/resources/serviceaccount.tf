04286ae040563c455699bc132c5a253b1100b346
Will Clarke <william.clarke@digital.justice.gov.uk>
2026-06-18 20:54:43 +0100

    add CAS STG service account

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-admin-service-staging/resources/serviceaccount.tf
@@ -0,0 +1,12 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
+
+  namespace = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+
+  serviceaccount_token_rotated_date = "19-06-2026"
+
+  # Creates Kubernetes' GitHub actions secrets in the repository
+  github_repositories = ["laa-cla-admin-service"]
+  github_environments = ["staging"]
+}
