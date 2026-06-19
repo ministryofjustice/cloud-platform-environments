6b9432ea0c53513156231c8043fbd1747e100d17
charles <chalston.pinnock@justice.gov.uk>
2026-06-19 10:30:22 +0100

    updating pr to match cloud platfom

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/variables.tf
@@ -64,28 +64,5 @@ variable "db_identifier" {
   type        = string
 }
 
-variable "db_subnet_group_name" {
-  description = "RDS subnet group name"
-  type        = string
-}
-
-variable "instance_class" {
-  description = "RDS instance class"
-  type        = string
-  default     = "db.t4g.small"
-}
-
-variable "engine" {
-  description = "Database engine"
-  type        = string
-  default     = "postgres"
-}
-
-variable "source_db_instance_identifier" {
-  description = "Source RDS instance to restore from"
-  type        = string
-  default = "laa-cla-backend-staging-pitr"
-}
-
 
 
