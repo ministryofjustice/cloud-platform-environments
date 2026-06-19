858178c55032ff9914df0d83c458ce8891f72bd8
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2026-06-19 08:51:22 +0000

    Add GithubTeam Tag to enable read only access via console

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-development/resources/main.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-development/resources/main.tf
@@ -34,6 +34,7 @@ provider "aws" {
       service-area  = var.service_area
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam    = "data-platform-engineering"
     }
   }
 }
@@ -52,6 +53,7 @@ provider "aws" {
       service-area  = var.service_area
       source-code   = "github.com/ministryofjustice/cloud-platform-environments"
       slack-channel = var.slack_channel
+      GithubTeam    = "data-platform-engineering"
     }
   }
 }
