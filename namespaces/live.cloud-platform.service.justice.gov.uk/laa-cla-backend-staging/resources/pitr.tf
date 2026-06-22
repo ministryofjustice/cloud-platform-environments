8f1a59f8cc9d5d338d67d9ba89008c41ec0edcfb
charles <chalston.pinnock@justice.gov.uk>
2026-06-22 14:15:26 +0100

    remove back secuity group

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/pitr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/pitr.tf
@@ -3,8 +3,6 @@ resource "aws_db_instance" "restored" {
   instance_class         = var.db_instance_class
   engine_version         = var.db_engine_version
 
-  
-  vpc_security_group_ids = var.vpc_name.vpc_security_group_ids
 
   restore_to_point_in_time {
     source_db_instance_identifier = module.cla_backend_rds_postgres_14.db_identifier
