67369f261f03e409415b2774b053a75b64a1c483
charles <chalston.pinnock@justice.gov.uk>
2026-06-22 13:55:56 +0100

    reference the variables for the information

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/pitr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/pitr.tf
@@ -1,30 +1,20 @@
-resource "aws_db_instance", "restored" {
+resource "aws_db_instance" "restored" {
+  identifier             = var.db_identifier
+  instance_class         = var.db_instance_class
+  engine                 = var.engine
+  engine_version         = var.db_engine_version
 
-  identifier           = var.db_identifier
-  db_subnet_group_name = var.db_subnet_group_name
-  instance_class = var.instance_class
-  engine = var.engine
-
-
-  identifier ="cla_backend_rds_postgres_14_pitr", 
-  instance_class = "db.t4g.small", 
-  engine="postgres"
-
-
-  db_subnet_group_name = ""
-  vpc_secruity_group_ids =[]
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   restore_to_point_in_time {
-
-    source_db_instance_identifier = "", 
-    use_latest_restoreable_time = true 
-
+    source_db_instance_identifier = module.cla_backend_rds_postgres_14.db_identifier
+    use_latest_restorable_time    = true
   }
 
   skip_final_snapshot = true
 
   tags = {
-    name= var.namespace
+    Name = var.namespace
   }
-
 }
