8f0e699c7f77b4f28ac154f253a81b5596f820f6
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-22 14:05:37 +0100

    Remove production environments

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-development/resources/locals.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-development/resources/locals.tf
@@ -3,11 +3,9 @@ locals {
     "013433889002", # data-platform-development
     "259787491607", # data-platform-test
     "386657846332", # data-platform-preproduction
-    "209019631331", # data-platform-production
     "883644721336", # data-platform-governance-development
     "584108277778", # data-platform-governance-test
     "960979851195", # data-platform-governance-preproduction
-    "190028274816", # data-platform-governance-production
   ]
   data_platform_monitoring_role_name = "data-platform-monitoring"
 }
