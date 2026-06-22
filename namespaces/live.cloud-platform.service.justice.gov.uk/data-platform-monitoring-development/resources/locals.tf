46c10c8842fa9454846789d60fff56cec6c7e21a
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-22 12:40:44 +0100

    Add in the iam resources for data platform development monitoring

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-development/resources/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  data_platform_accounts = [
+    "013433889002", # data-platform-development
+    "259787491607", # data-platform-test
+    "386657846332", # data-platform-preproduction
+    "209019631331", # data-platform-production
+    "883644721336", # data-platform-governance-development
+    "584108277778", # data-platform-governance-test
+    "960979851195", # data-platform-governance-preproduction
+    "190028274816", # data-platform-governance-production
+  ]
+  data_platform_monitoring_role_name = "data-platform-monitoring"
+}
