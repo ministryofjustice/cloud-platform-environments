0bb9afb5cce9e793102f476a92e80570ffb5a02d
Pietro Convalle <pietro.convalle@madetech.com>
2026-06-22 11:28:51 +0100

    Added secrets for db restore

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-general-ledger-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-general-ledger-preprod/resources/rds-postgresql.tf
@@ -153,7 +153,27 @@ resource "kubernetes_config_map" "rds" {
   }
 
   data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
+    database_name         = module.rds.database_name
+    db_identifier         = module.rds.db_identifier
+    rds_instance_endpoint = module.rds.rds_instance_endpoint
+    database_username     = module.rds.database_username
+    database_password     = module.rds.database_password
+    rds_instance_address  = module.rds.rds_instance_address
+  }
+}
+
+# This places a secret for this preprod RDS instance in the production namespace,
+# this can then be used by a kubernetes job which will refresh the preprod data.
+resource "kubernetes_secret" "dps_rds_refresh_creds" {
+  metadata {
+    name      = "dps-rds-instance-output-preprod"
+    namespace = "hmpps-nomis-mapping-service-prod"
+  }
+
+  data = {
+    database_name        = module.dps_rds.database_name
+    database_username    = module.dps_rds.database_username
+    database_password    = module.dps_rds.database_password
+    rds_instance_address = module.dps_rds.rds_instance_address
   }
 }
