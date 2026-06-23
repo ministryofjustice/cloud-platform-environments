b2a8d367c525bd70979a8ef8ad5cbec18bd70752
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2026-06-23 09:39:46 +0100

    Alter DNS record name value pairs

--- namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/care-arrangement-plan-prod/resources/variables.tf
@@ -60,6 +60,10 @@ variable "domain" {
   default = "child-arrangements-plan.service.justice.gov.uk"
 }
 
+variable "alias" {
+  default = "propose-child-arrangements-plan"
+}
+
 variable "github_owner" {
   description = "The GitHub organization or individual user account containing the app's code repo. Used by the Github Terraform provider. See: https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/ecr-setup.html#accessing-the-credentials"
   type        = string
