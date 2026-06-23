9b8316d846b1318b3c00bce38bc40b3e093e82d1
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-06-23 13:26:35 +0100

    remove unneeded irsa policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/irsa.tf
@@ -12,7 +12,6 @@ module "irsa" {
   # If you're using Cloud Platform provided modules (e.g. SNS, S3), these
   # provide an output called `irsa_policy_arn` that can be used.
   role_policy_arns = {
-    rds = module.rds.irsa_policy_arn
     s3  = module.s3_bucket.irsa_policy_arn
   }
 
