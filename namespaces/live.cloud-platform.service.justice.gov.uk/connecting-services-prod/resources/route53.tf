eab7d776b154c5f35093d512d08f6315130149d8
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2026-06-24 21:00:55 +0100

    Add verification records

--- namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/route53.tf
@@ -13,17 +13,6 @@ resource "aws_route53_zone" "pfl_cs_route53_zone" {
   }
 }
 
-# add a txt record to the zone to verify ownership of the domain with Microsoft Entra ID
-resource "aws_route53_record" "entra_id_verification" {
-  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
-  name    = var.domain
-  type    = "TXT"
-  ttl     = 300
-  records = [
-    "MS=ms72370887"
-  ]
-}
-
 resource "kubernetes_secret" "pfl_cs_route53_zone_sec" {
   metadata {
     name      = "pfl-cs-route53-zone-output"
@@ -34,4 +23,30 @@ resource "kubernetes_secret" "pfl_cs_route53_zone_sec" {
     zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
     nameservers = join("\n", aws_route53_zone.pfl_cs_route53_zone.name_servers)
   }
+}
+
+
+# Microsoft Entra/Outlook related
+resource "aws_route53_record" "pfl_cs_route53_cname_record_autodiscover" {
+  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
+  name    = "autodiscover"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["autodiscover.outlook.com"]
+}
+
+resource "aws_route53_record" "pfl_cs_route53_mx_record_outlook" {
+  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
+  name    = var.domain
+  type    = "MX"
+  ttl     = "3600"
+  records = ["0 findchildarrangementoption-service-gov-uk01be2e.mail.protection.outlook.com"]
+}
+
+resource "aws_route53_record" "entra_id_verification" {
+  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
+  name    = var.domain
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["MS=ms72370887", "v=spf1 include:spf.protection.outlook.com -all"]
 }
