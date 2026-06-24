0e430dc2700a6e4e7b157c2b706a615a6f5ec860
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-06-24 14:09:15 +0100

    athena infra for copilot credits dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-dev/resources/athena.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-dev/resources/athena.tf
@@ -5,28 +5,8 @@ locals {
     ]
 }
 
-resource "aws_athena_workgroup" "coat_cur_report" {
-  name = "coat_cur_report"
-
-  configuration {
-    result_configuration {
-      output_location = "s3://coat-${local.environment}-cur-v2-hourly/athena-results/"
-      encryption_configuration {
-        encryption_option = "SSE_S3"
-      }
-    }
-
-    enforce_workgroup_configuration    = true
-    publish_cloudwatch_metrics_enabled = true
-  }
-}
-
-resource "aws_glue_catalog_database" "cur_v2_database" {
-  name = "cur_v2_database"
-}
-
-resource "aws_iam_role" "glue_cur_role" {
-  name = "glue_cur_role"
+resource "aws_iam_role" "copilot_credits_dev_glue_role" {
+  name = "copilot_credits_dev_glue_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -40,10 +20,11 @@ resource "aws_iam_role" "glue_cur_role" {
   })
 }
 
-resource "aws_iam_role_policy" "glue_s3_policy" {
+resource "aws_iam_role_policy" "copilot_credits_dev_glue_s3_policy" {
   #checkov:skip=CKV_AWS_355: "Ensure no IAM policies documents allow "*" as a statement's resource for restrictable actions"
   #checkov:skip=CKV_AWS_290: "Ensure IAM policies does not allow write access without constraints"
-  role = aws_iam_role.glue_cur_role.name
+  role = aws_iam_role.copilot_credits_dev_glue_role.name
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -51,19 +32,9 @@ resource "aws_iam_role_policy" "glue_s3_policy" {
         Effect = "Allow",
         Action = ["s3:GetObject", "s3:ListBucket"],
         Resource = [
-          "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/*",
-          "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly",
-          "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly-enriched/*",
-          "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly-enriched"
+          "${module.s3_bucket.bucket_arn}/*",
+          module.s3_bucket.bucket_arn
         ]
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "kms:Decrypt",
-          "kms:GenerateDataKey"
-        ],
-        Resource = module.cur_s3_kms.key_arn
       },
       {
         Sid    = "AthenaAccess",
@@ -88,23 +59,24 @@ resource "aws_iam_role_policy" "glue_s3_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_service_role_attachment" {
-  role       = aws_iam_role.glue_cur_role.name
+resource "aws_iam_role_policy_attachment" "copilot_credits_dev_glue_service_role_attachment" {
+  role       = aws_iam_role.copilot_credits_dev_glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
-resource "aws_glue_crawler" "cur_v2_crawler" {
+resource "aws_glue_catalog_database" "copilot_credits_dev_database" {
+  name = "copilot_credits_dev_database"
+}
+
+resource "aws_glue_crawler" "copilot_credits_dev_crawler" {
   #checkov:skip=CKV_AWS_195: "Ensure Glue component has a security configuration associated"
 
-  name          = "cur_v2_crawler"
-  database_name = aws_glue_catalog_database.cur_v2_database.name
-  role          = aws_iam_role.glue_cur_role.arn
+  name          = "copilot_credits_dev_crawler"
+  database_name = aws_glue_catalog_database.copilot_credits_dev_database.name
+  role          = aws_iam_role.copilot_credits_dev_glue_role.arn
 
-  dynamic "s3_target" {
-    for_each = local.glue_s3_targets[local.environment]
-    content {
-      path = s3_target.value
-    }
+  s3_target {
+    path = "s3://${module.s3_bucket.bucket_name}/reports/"
   }
 
   configuration = jsonencode({
@@ -117,4 +89,20 @@ resource "aws_glue_crawler" "cur_v2_crawler" {
   })
 
   schedule = "cron(0 7 * * ? *)"
+}
+
+resource "aws_athena_workgroup" "copilot_credits_dev_workgroup" {
+  name = "copilot_credits_dev_workgroup"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${module.s3_bucket.bucket_name}/athena-results/"
+      encryption_configuration {
+        encryption_option = "SSE_S3"
+      }
+    }
+
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+  }
 }
