0c6a267e4073786d280beb3d1b6af8c345734f46
kashif.malik@justice.gov.uk <kashif.malik@justice.gov.uk>
2026-06-25 16:07:20 +0000

    increased RDS minor version.

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-prod/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-prod/resources/variables.tf
@@ -152,7 +152,7 @@ variable db_engine {
 variable db_engine_version {
   description = "The database engine version to use for the RDS instance"
   type        = string
-  default     = "17.7"
+  default     = "17.9"
 }
 variable rds_family {
   description = "The RDS family to use for the RDS instance"
