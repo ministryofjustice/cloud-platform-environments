11687fb6915361183e7ab08205824fdc1af93a88
muhammad9970 <88196037+muhammad9970@users.noreply.github.com>
2026-06-25 15:04:27 +0100

    Add dev/test/preprod accounts to data-platform-monitoring-production (#43598)
    Signed-off-by: Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-production/resources/locals.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-monitoring-production/resources/locals.tf
@@ -1,5 +1,11 @@
 locals {
   data_platform_accounts = [
+    "013433889002", # data-platform-development
+    "259787491607", # data-platform-test
+    "386657846332", # data-platform-preproduction
+    "883644721336", # data-platform-governance-development
+    "584108277778", # data-platform-governance-test
+    "960979851195", # data-platform-governance-preproduction
     "209019631331", # data-platform-production
     "190028274816", # data-platform-governance-production
   ]
