652bf76b78f90fcc22fc169e89402d1c316bf535
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2026-06-26 14:33:21 +0100

    Update irsa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-preprod/resources/irsa.tf
@@ -3,8 +3,12 @@ locals {
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd" = "hmpps-domain-events-preprod"
   }
-
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
+  
+  sqs_queues = {
+    "Digital-Prison-Services-preprod-hmpps_audit_queue" = "hmpps-audit-preprod",
+  }
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
 module "irsa" {
@@ -38,6 +42,22 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
+module "esupervision-ui-service-account" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.0.0"
+
+  eks_cluster_name       = var.eks_cluster_name
+  namespace              = var.namespace
+  service_account_name   = "hmpps-esupervision"
+  role_policy_arns       = local.sqs_policies
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
 # IRSA role used to authenticate bucket requests
 resource "kubernetes_secret" "irsa" {
   metadata {
@@ -53,4 +73,9 @@ resource "kubernetes_secret" "irsa" {
 data "aws_ssm_parameter" "irsa_policy_arns_sns" {
   for_each = local.sns_topics
   name     = "/${each.value}/sns/${each.key}/irsa-policy-arn"
+}
+
+data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
+  for_each = local.sqs_queues
+  name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
 }
