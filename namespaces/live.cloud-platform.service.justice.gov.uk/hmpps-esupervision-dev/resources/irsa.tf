7ec6b65f59d72448d50c7a010e2179dfe392dc13
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2026-06-26 14:58:24 +0100

    Fix typo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-esupervision-dev/resources/irsa.tf
@@ -3,7 +3,7 @@ locals {
   sns_topics = {
     "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
+  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
   
   sqs_queues = {
     "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
