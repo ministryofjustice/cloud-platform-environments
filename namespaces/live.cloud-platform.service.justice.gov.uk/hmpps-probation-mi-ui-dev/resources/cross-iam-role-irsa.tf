c4f5eb24c1474fe67f129c4ba7b67cc9de45ebbd
James Reed <james.reed1@justice.gov.uk>
2026-06-30 14:25:08 +0100

    use latest ver

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-mi-ui-dev/resources/cross-iam-role-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-mi-ui-dev/resources/cross-iam-role-irsa.tf
@@ -94,7 +94,7 @@ module "irsa" {
 }
 
 module "service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.0.0" # use the latest release
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1"
 
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name
