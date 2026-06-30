a3f7ed4bd5f1c54885409168c14459855ce326ec
Peter Hunt <naseberry@users.noreply.github.com>
2026-06-30 16:11:12 +0100

    DSTEW-1908: Remove laa-data-cca-uat Read Replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-certificated-api-uat/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-certificated-api-uat/resources/rds-postgresql.tf
@@ -59,7 +59,7 @@ module "rds" {
 
 module "read_replica" {
   # default off
-  count  = 1
+  count  = 0
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
   vpc_name               = var.vpc_name
@@ -126,7 +126,7 @@ resource "kubernetes_secret" "rds" {
 
 resource "kubernetes_secret" "read_replica" {
   # default off
-  count = 1
+  count = 0
 
   metadata {
     name      = "rds-postgresql-read-replica-output"
@@ -136,10 +136,14 @@ resource "kubernetes_secret" "read_replica" {
   # The database_username, database_password, database_name values are same as the source RDS instance.
   # Uncomment if count > 0
 
+  /*
   data = {
-    rds_instance_endpoint = module.read_replica[0].rds_instance_endpoint
-    rds_instance_address  = module.read_replica[0].rds_instance_address
+    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
+    rds_instance_address  = module.read_replica.rds_instance_address
+    access_key_id         = module.read_replica.access_key_id
+    secret_access_key     = module.read_replica.secret_access_key
   }
+  */
 }
 
 
