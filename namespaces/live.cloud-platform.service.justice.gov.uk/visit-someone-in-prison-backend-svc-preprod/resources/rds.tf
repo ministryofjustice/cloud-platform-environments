3a1ca93660a2c430d8d1b0028ca4899e372ecd11
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-07-01 12:31:31 +0100

    Update DB minor version from 17.4 to 17.9 for all 3 visits databases - preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-preprod/resources/rds.tf
@@ -10,9 +10,10 @@ module "visit_scheduler_pg_rds" {
   namespace              = var.namespace
 
   allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
   prepare_for_major_upgrade   = false
   db_engine                   = "postgres"
-  db_engine_version           = "17.4"
+  db_engine_version           = "17.9"
   rds_family                  = "postgres17"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage     = "200"
@@ -102,9 +103,10 @@ module "prison_visit_booker_reg_rds" {
   namespace              = var.namespace
 
   allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
   prepare_for_major_upgrade   = false
   db_engine                   = "postgres"
-  db_engine_version           = "17.4"
+  db_engine_version           = "17.9"
   rds_family                  = "postgres17"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "50"
@@ -132,9 +134,10 @@ module "visit_allocation_rds" {
   namespace              = var.namespace
 
   allow_major_version_upgrade = "false"
+  allow_minor_version_upgrade = "true"
   prepare_for_major_upgrade   = false
   db_engine                   = "postgres"
-  db_engine_version           = "17.4"
+  db_engine_version           = "17.9"
   rds_family                  = "postgres17"
   db_instance_class           = "db.t4g.small"
   db_max_allocated_storage    = "50"
