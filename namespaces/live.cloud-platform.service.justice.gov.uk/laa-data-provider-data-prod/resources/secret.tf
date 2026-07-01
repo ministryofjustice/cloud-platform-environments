3e02829560d35ce9c549f6e982f4e79a41461a4a
David Conneely <899881+dconneely@users.noreply.github.com>
2026-07-01 13:28:23 +0100

    chore: Add new PDA secrets (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-provider-data-prod/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-provider-data-prod/resources/secret.tf
@@ -11,14 +11,24 @@ module "secrets_manager" {
 
   secrets = {
     "app-secrets" = {
-      description             = "[app-secrets] App secrets for Provider data"
+      description             = "[app-secrets] App configuration secrets for PDA-r1"
       recovery_window_in_days = 7
       k8s_secret_name         = "app-secrets"
     }
-    "app-secrets-secondary" = {
-      description             = "[app-secrets-secondary] Secondary app secrets for Provider data"
+    "pda-r1-configuration" = {
+      description             = "[pda-r1-configuration] Configuration secrets for PDA-r1"
       recovery_window_in_days = 7
-      k8s_secret_name         = "app-secrets-secondary"
+      k8s_secret_name         = "pda-r1-configuration"
+    }
+    "pda-r1-novation-data" = {
+      description             = "[pda-r1-novation-data] Novation data secrets for PDA-r1"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "pda-r1-novation-data"
+    }
+    "pda-r2-configuration" = {
+      description             = "[pda-r2-configuration] Configuration secrets for PDA-r2"
+      recovery_window_in_days = 7
+      k8s_secret_name         = "pda-r2-configuration"
     }
   }
 }
