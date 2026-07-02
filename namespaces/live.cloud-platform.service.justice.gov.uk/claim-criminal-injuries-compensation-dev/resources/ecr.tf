5e6d56374904ee66e791313b771618bf617e598f
Barry Piccinni <Barry.Piccinni@justice.gov.uk>
2026-07-02 15:31:10 +0100

    Claim-criminal-injuries-dev update ECR vars

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-dev/resources/ecr.tf
@@ -13,8 +13,9 @@ module "cica_ecr_credentials" {
 
 
   # OpenID Connect configuration
-  oidc_providers      = ["circleci"]
+  oidc_providers      = ["circleci", "github"]
   github_repositories = var.repo_name
+  github_environments = [ "dev" ]
 
   # Tags
   business_unit          = var.business_unit
