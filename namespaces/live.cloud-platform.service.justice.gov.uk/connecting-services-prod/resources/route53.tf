3f082fc8406086699a6713214ad7a8d976f9db5f
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2026-07-03 10:44:20 +0100

    Remove TXT DKIM records

--- namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/connecting-services-prod/resources/route53.tf
@@ -51,23 +51,6 @@ resource "aws_route53_record" "entra_id_verification" {
   records = ["MS=ms72370887", "v=spf1 include:spf.protection.outlook.com -all"]
 }
 
-# TXT based DKIM records
-resource "aws_route53_record" "entra_id_verification_dkim1" {
-  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
-  name    = "selector1._domainkey.find-child-arrangement-option.service.gov.uk"
-  type    = "TXT"
-  ttl     = "3600"
-  records = ["selector1-findchildarrangementoption-service-gov-uk01be2e._domainkey.JusticeUK.w-v1.dkim.mail.microsoft"]
-}
-
-resource "aws_route53_record" "entra_id_verification_dkim2" {
-  zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
-  name    = "selector2._domainkey.find-child-arrangement-option.service.gov.uk"
-  type    = "TXT"
-  ttl     = "3600"
-  records = ["selector2-findchildarrangementoption-service-gov-uk01be2e._domainkey.JusticeUK.w-v1.dkim.mail.microsoft"]
-}
-
 # CNAME based DKIM records
 resource "aws_route53_record" "entra_id_verification_dkim1_cname" {
   zone_id = aws_route53_zone.pfl_cs_route53_zone.zone_id
