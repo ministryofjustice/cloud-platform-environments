9986532258c15952495797fae2d280a91429d9cd
as-mt-26 <alex.stockford@justice.gov.uk>
2026-07-07 14:24:43 +0100

    Add secrets manager with first secret

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-monies-dev/resources/secrets-manager.tf
@@ -0,0 +1,20 @@
+
+module "secrets_manager" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.7"
+  team_name              = var.team_name
+  application            = var.application
+  business_unit          = var.business_unit
+  is_production          = var.is_production
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+  eks_cluster_name       = var.eks_cluster_name
+
+  secrets = {
+    "govuk-notify-callbacks-bearer-token" = {
+      description             = "The API token used by the Legacy MTP project",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "govuk_notify_callbacks_bearer_token" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
+    },    
+  }
+}
