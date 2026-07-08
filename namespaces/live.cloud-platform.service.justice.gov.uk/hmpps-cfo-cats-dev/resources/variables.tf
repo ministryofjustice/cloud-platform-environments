9e1bf823e31347890139892be20178ef0c63bffb
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-07-08 13:22:28 +0100

    Rename cfocats-dev to "hmpps-cfo-cats-dev"

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-cats-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-cats-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "cfocats-dev"
+  default     = "hmpps-cfo-cats-dev"
 }
 
 variable "business_unit" {
