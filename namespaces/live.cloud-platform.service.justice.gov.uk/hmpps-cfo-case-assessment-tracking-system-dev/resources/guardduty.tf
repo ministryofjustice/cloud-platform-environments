945129c55b7e9df4ea7867fea17ac6295e0d0a4c
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-07-08 14:33:00 +0100

    Shorten guardduty role/policy names

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/guardduty.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/guardduty.tf
@@ -21,7 +21,7 @@ resource "aws_guardduty_malware_protection_plan" "malware_plan" {
 }
 
 resource "aws_iam_role" "guardduty_malware_protection_role" {
-  name = "${var.namespace}-guardduty-malware-protection-role"
+  name = "${var.namespace}-guardduty-role"
   path = "/"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -48,7 +48,7 @@ resource "aws_iam_role" "guardduty_malware_protection_role" {
 ## IAM policy
 ## Policy reference: https://docs.aws.amazon.com/guardduty/latest/ug/malware-protection-s3-iam-policy-prerequisite.html
 resource "aws_iam_policy" "guard-duty_s3_custom" {
-  name        = "${var.namespace}-guard-duty-s3-policy"
+  name        = "${var.namespace}-guardduty-policy"
   description = "Policy allowing GuardDuty to manage S3 events, used for malware protection."
 
   policy = jsonencode({
