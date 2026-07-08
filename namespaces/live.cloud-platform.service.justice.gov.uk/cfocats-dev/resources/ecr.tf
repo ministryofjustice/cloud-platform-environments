5bcc8f68eb460a598508eacb4209d84b9b47cc22
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-07-08 09:39:22 +0100

    Disable deletion protection

--- namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfocats-dev/resources/ecr.tf
@@ -22,4 +22,5 @@ module "ecr" {
   namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+  deletion_protection    = false
 }
