b336eff987ab286a281b12ec28f57c992779a29c
Ryan Kearsley <49034987+ryankearsley-moj@users.noreply.github.com>
2026-07-09 11:44:55 +0100

    Update variables.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/variables.tf
@@ -29,7 +29,7 @@ variable "business_unit" {
 variable "team_name" {
   description = "Name of the development team responsible for this service"
   type        = string
-  default     = "hmpps-creating-future-opportunities-live"
+  default     = "hmpps-creating-future-opportunities-devs"
 }
 
 variable "environment" {
