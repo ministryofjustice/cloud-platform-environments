16681c3abe32e3905a616ecf5919dbd27846ddfc
James Reed <james.reed1@justice.gov.uk>
2026-07-09 10:21:55 +0100

    fix irsa test

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dpr-tools-test-probation/resources/cross-iam-role-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-dpr-tools-test-probation/resources/cross-iam-role-irsa.tf
@@ -3,12 +3,12 @@ locals {
 
   accounts_map = {
     development = "771283872747",
-    test = "203591025782"
+    test        = "203591025782"
   }
 
   environments_map = {
     development = "development",
-    test = "test"
+    test        = "test"
   }
 }
 
@@ -18,14 +18,14 @@ data "aws_eks_cluster" "eks_cluster" {
 
 # Cluster OIDC
 module "dpr_mi_assume_role" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.13.0"
-  create_role                   = true
-  role_name                     = "dpr-tools-${var.environment}-cross-iam-${var.eks_cluster_name}"
-  provider_url                  = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
-  role_policy_arns              = [aws_iam_policy.cross_iam_dpr_oidc.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:dpr-tools-${lookup(local.environments_map, lower(var.environment))}-cross-iam"]
-  oidc_fully_qualified_audiences= ["sts.amazonaws.com"]
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "5.13.0"
+  create_role                    = true
+  role_name                      = "dpr-tools-probation-${var.environment}-cross-iam-${var.eks_cluster_name}"
+  provider_url                   = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  role_policy_arns               = [aws_iam_policy.cross_iam_dpr_oidc.arn]
+  oidc_fully_qualified_subjects  = ["system:serviceaccount:${var.namespace}:dpr-tools-${lookup(local.environments_map, lower(var.environment))}-cross-iam"]
+  oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
 }
 
 data "aws_iam_policy_document" "cross_iam_dpr_oidc" {
@@ -53,11 +53,11 @@ resource "aws_iam_policy" "cross_iam_dpr_oidc" {
 
 # IRSA
 module "irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  source               = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
   eks_cluster_name     = var.eks_cluster_name
   service_account_name = "dpr-tools-${var.environment}-cross-iam"
   namespace            = var.namespace
-  role_policy_arns     = {
+  role_policy_arns = {
     secrets = aws_iam_policy.cross_iam_policy_mp.arn
   }
 
@@ -117,7 +117,7 @@ resource "kubernetes_secret" "irsa" {
     namespace = var.namespace
   }
   data = {
-    role = module.irsa.role_name
+    role           = module.irsa.role_name
     serviceaccount = module.irsa.service_account.name
   }
 }
