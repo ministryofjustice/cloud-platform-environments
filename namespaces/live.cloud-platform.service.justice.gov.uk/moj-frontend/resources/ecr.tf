8e4b8c38970777549f40cbaf3e0eee61587de8c4
Johan D. <johan.dindaine@justice.gov.uk>
2026-07-10 12:11:24 +0000

    got the correct repo names after checking other namespaces configuration.

--- namespaces/live.cloud-platform.service.justice.gov.uk/moj-frontend/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/moj-frontend/resources/ecr.tf
@@ -4,7 +4,7 @@ module "ecr-repo" {
   repo_name = "${var.namespace}-ecr"
 
   oidc_providers      = ["github"]
-  github_repositories = [var.namespace, ministryofjustice / moj-frontend-token-fetcher]
+  github_repositories = ["moj-frontend", "moj-frontend-token-fetcher"]
 
   # Tags
   business_unit          = var.business_unit
