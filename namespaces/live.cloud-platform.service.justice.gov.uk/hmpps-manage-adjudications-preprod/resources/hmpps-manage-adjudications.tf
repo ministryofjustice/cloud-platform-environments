9897af02757ac9a1dd43886728e2b0363177f6d7
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-07-13 14:25:07 +0100

    Allow merge from prefixed branches

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-preprod/resources/hmpps-manage-adjudications.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-manage-adjudications-preprod/resources/hmpps-manage-adjudications.tf
@@ -7,7 +7,7 @@ module "hmpps_template_typescript" {
   github_team = "hmpps-adjudications"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
   reviewer_teams                = ["hmpps-adjudications"] # Optional team that should review deployments to this environment.
-  selected_branch_patterns      = ["main"] # Optional
+  selected_branch_patterns      = ["main", "IR-*"] # Optional
   #protected_branches_only      = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "preprod" # Either "dev", "preprod" or "prod"
