0e0bef9a375b2209a971b0497d1554a9d55b8f17
Rohit Gokhale <rohit.gokhale@justice.co.uk>
2026-07-13 12:42:15 +0100

    chore: updated infra support contact details

--- namespaces/live.cloud-platform.service.justice.gov.uk/stg-vsp-subscription-service-test/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/stg-vsp-subscription-service-test/resources/variables.tf
@@ -47,7 +47,7 @@ variable "environment" {
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
-  default     = "victimspathfinder@justice.gov.uk"
+  default     = "rohit.gokhale@justice.gov.uk"
 }
 
 variable "is_production" {
