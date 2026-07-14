965dec275d0283d86c58cc86439e24279786aeec
Katharine Ahern <katharine.ahern@justice.gov.uk>
2026-07-14 08:33:28 +0100

    ap-6980: add new serviceaccount_github module to cfe-civil-production
    Upgrade db_engine_version to reflect actual version and prevent concourse failure.

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cfe-civil-production/resources/github-serviceaccount.tf
@@ -0,0 +1,18 @@
+module "serviceaccount_github" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.2.0"
+
+  namespace            = var.namespace
+  kubernetes_cluster   = var.kubernetes_cluster
+  serviceaccount_name  = "github-actions"
+  serviceaccount_rules = var.serviceaccount_rules
+  role_name            = "github-actions-role"
+  rolebinding_name     = "github-action-rolebinding"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  github_repositories                  = [var.repo_name]
+  github_actions_secret_kube_cert      = var.github_actions_secret_kube_cert
+  github_actions_secret_kube_token     = var.github_actions_secret_kube_token
+  github_actions_secret_kube_cluster   = var.github_actions_secret_kube_cluster
+  github_actions_secret_kube_namespace = var.github_actions_secret_kube_namespace
+}
