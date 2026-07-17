a0622792795435fd2379f17b77af46ea4806b96a
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-07-17 10:46:48 +0100

    PI-4323 change offender events topic name preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/topic.tf
@@ -14,9 +14,8 @@ data "aws_ssm_parameter" "probation-offender-events-policy-arn" {
   name = "/offender-events-${var.environment_name}/sns/${data.aws_sns_topic.probation-offender-events.name}/irsa-policy-arn"
 }
 
-# Temporary - reading from production topic into pre-production
 data "aws_sns_topic" "probation-offender-events-prod" {
-  name = "cloud-platform-Digital-Prison-Services-c2d997878cd24eef94e60f1404977153"
+  name = "cloud-platform-Digital-Prison-Services-dbe10e8d9c1f4d100f0c723d5d9b754e"
 }
 
 data "aws_ssm_parameter" "probation-offender-events-prod-policy-arn" {
