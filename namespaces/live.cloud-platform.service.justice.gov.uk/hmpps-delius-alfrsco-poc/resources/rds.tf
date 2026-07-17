799e3cd011bb11db29ca7114dedfa00012b70578
dunc56 <duncan.whitham@justice.gov.uk>
2026-07-17 13:32:00 +0100

    Update rds.tf
    correct min size

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-alfrsco-poc/resources/rds.tf
@@ -11,7 +11,7 @@ module "rds_alfresco" {
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
   db_max_allocated_storage     = "500"
-  db_allocated_storage         = "200"
+  db_allocated_storage         = "400"
   # enable_rds_auto_start_stop   = true # Uncomment to turn off your database overnight between 10PM and 6AM UTC / 11PM and 7AM BST.
   # db_password_rotated_date     = "2023-04-17" # Uncomment to rotate your database password.
 
