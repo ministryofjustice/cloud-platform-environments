2220ab103100944a3c5ee88fcf6ff01696ef7bbe
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2026-07-20 14:19:45 +0100

    Bump proxy module version

--- namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/egress-controls.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-backend-svc-staging/resources/egress-controls.tf
@@ -2,7 +2,7 @@
 # For guidance see: https://tech-docs.hmpps.service.justice.gov.uk/how-to-guides/retrofitting-egress-controls-with-envoy-proxy
 
 module "hmpps_egress_controls" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-egress-controls?ref=0.0.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-egress-controls?ref=0.0.6"
 
   enable_envoy_setup     = true
   enable_egress_controls = false 
