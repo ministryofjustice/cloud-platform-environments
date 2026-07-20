42fc545041b971407371c00a81b6c318ce1cef40
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-07-20 12:15:01 +0100

    HIA-1543: Added GHA tokens for external api config repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/hmpps-integration-api-gha.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/hmpps-integration-api-gha.tf
@@ -36,3 +36,19 @@ module "hmpps_integration_events" {
   namespace                     = var.namespace
   kubernetes_cluster            = var.kubernetes_cluster
 }
+
+module "hmpps_external_api_config" {
+  source      = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  force_rotate_token = true
+  custom_token_rotation_date = "2026-03-20"
+  github_repo = "hmpps-external-api-config"
+  application = "hmpps-external-api-config"
+  github_team = "hmpps-integration-api"
+  environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
+  is_production                 = var.is_production
+  application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
+  source_template_repo          = "hmpps-template-kotlin"
+  github_token                  = var.github_token
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
