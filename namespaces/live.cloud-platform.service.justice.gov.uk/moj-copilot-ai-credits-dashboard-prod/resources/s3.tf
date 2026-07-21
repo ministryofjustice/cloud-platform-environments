f3e4459dc2150f66dbd7bc4c24fd6a78d6bc6b01
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-07-21 11:33:47 +0100

    add backfill role to s3 bucket policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/s3.tf
@@ -40,7 +40,10 @@ module "s3_bucket" {
       "Sid": "S3DataSyncAccessAPAirflowprod",
       "Effect": "Allow",
       "Principal": {
-        "AWS": "arn:aws:iam::593291632749:role/airflow-production-coat-get-copilot-data"
+        "AWS": [
+          "arn:aws:iam::593291632749:role/airflow-production-coat-get-copilot-data",
+          "arn:aws:iam::593291632749:role/airflow-production-coat-backfill-copilot-data"
+        ]
       },
       "Action": [
         "s3:PutObject",
