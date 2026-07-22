9d667d4c4b5cfbf86873543671364fda363abec1
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-07-22 16:33:59 +0100

    Remove unneeded irsa PFMA prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-prod/resources/irsa.tf
@@ -66,22 +66,6 @@ module "irsa" {
   infrastructure_support = var.infrastructure_support
 }
 
-module "pfma_irsa" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
-
-  eks_cluster_name       = var.eks_cluster_name
-  namespace              = var.namespace
-  service_account_name   = "hmpps-prisoner-apps-ui"
-  role_policy_arns       = local.sqs_policies
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-}
-
 data "aws_ssm_parameter" "irsa_policy_arns_sqs" {
   for_each = local.sqs_queues
   name     = "/${each.value}/sqs/${each.key}/irsa-policy-arn"
