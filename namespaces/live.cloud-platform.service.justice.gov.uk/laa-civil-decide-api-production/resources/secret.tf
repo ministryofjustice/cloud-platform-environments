dee132ec67480fe305e2663559052ae3fa454f31
Ant Roy <anthony.roy@madetech.com>
2026-07-22 16:48:35 +0100

    Auth secrets and naming standardisation for civil-decide-api production

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-api-production/resources/secret.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-decide-api-production/resources/secret.tf
@@ -11,31 +11,31 @@ module "secrets_manager" {
 
   secrets = {
     "auth-client-id" = {
-      description             = "Auth client ID from Entra for production environment",
+      description             = "Auth client ID from Entra for Production",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "auth-client-id-production"
+      k8s_secret_name         = "auth-client-id-prod"
     },
 
     "auth-client-secret" = {
-      description             = "Auth client secret from Entra for production environment",
+      description             = "Auth client secret from Entra for Production",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "auth-client-secret-production"
+      k8s_secret_name         = "auth-client-secret-prod"
     },
 
     "auth-directory-url" = {
-      description             = "Auth directory URL from Entra for production environment",
+      description             = "Auth directory URL from Entra for Production",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "auth-directory-url-production"
+      k8s_secret_name         = "auth-directory-url-prod"
     },
 
     "base_url_map" = {
       description             = "API Base URL Map for Production",
       recovery_window_in_days = 7,
-      k8s_secret_name         = "base-url-map-production"
+      k8s_secret_name         = "base-url-map-prod"
     },
 
     "gov-notify-api-key" = {
-      description             = "API key for GOV Notify",
+      description             = "API key for GOV Notify for Production",
       recovery_window_in_days = 7,
       k8s_secret_name         = "gov-notify-api-key-prod"
     },
@@ -44,8 +44,13 @@ module "secrets_manager" {
       recovery_window_in_days = 7,
       k8s_secret_name         = "allow-auto-grant-prod"
     },
+    "api-entra-auth-config" = {
+      description             = "API Entra Configuration for Production",
+      recovery_window_in_days = 7,
+      k8s_secret_name         = "api-entra-auth-config-prod"
+    },
     "grant_email_template_id" = {
-      description             = "Template ID for sending grant emails",
+      description             = "Template ID for sending grant emails for Production",
       recovery_window_in_days = 7,
       k8s_secret_name         = "grant-email-template-id-prod"
     },
