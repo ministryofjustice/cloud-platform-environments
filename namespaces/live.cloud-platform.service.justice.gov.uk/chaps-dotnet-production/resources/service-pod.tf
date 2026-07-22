fef98c98128bb12852ad1db0a1e49fdf072a2ddd
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-07-22 17:49:22 +0100

    feat(service-pod): rename IRSA module to irsa_service_pod and update service account reference

--- namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/service-pod.tf
@@ -1,6 +1,7 @@
 # irsa configuration is required to use the service pod
-module "irsa" {
+module "irsa_service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
   role_policy_arns = {
     # Here you must provide the policy arn(s) for the AWS resources you want to access via the service pod
     rds  = module.rds_mssql.irsa_policy_arn
@@ -14,5 +15,5 @@ module "service_pod" {
 
   # Configuration
   namespace            = var.namespace
-  service_account_name = module.irsa.service_account_name.name # this uses the service account name from the irsa module
+  service_account_name = module.irsa_service_pod.service_account_name.name # this uses the service account name from the irsa module
 }
