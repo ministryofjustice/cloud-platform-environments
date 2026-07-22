b7b0c65a60442d126d1df795f1cff3ad007bb6f1
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-07-22 17:05:39 +0100

    feat(service-pod): add IRSA configuration and service pod setup

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/service-pod.tf
@@ -0,0 +1,17 @@
+# irsa configuration is required to use the service pod
+module "irsa" {
+  role_policy_arns = {
+    # Here you must provide the policy arn(s) for the AWS resources you want to access via the service pod
+    rds  = module.rds_mssql.irsa_policy_arn
+    ecr = module.ecr.irsa_policy_arn
+  }
+}
+
+# set up the service pod
+module "service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1" # use the latest release
+
+  # Configuration
+  namespace            = var.namespace
+  service_account_name = module.irsa.service_account_name.name # this uses the service account name from the irsa module
+}
