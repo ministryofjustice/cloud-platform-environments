4c1ccd9c9662642cd3c77734dec53d0f02bbad62
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-07-22 16:35:59 +0100

    Add prisoner audit queue manage apps staging

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-staging/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-managing-prisoner-apps-staging/resources/irsa.tf
@@ -31,7 +31,8 @@ resource "aws_iam_policy" "combined_local_sqs" {
 locals {
   # The names of the queues used and the namespace which created them
   sqs_queues = {
-    "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
+    "Digital-Prison-Services-dev-hmpps_audit_queue"          = "hmpps-audit-dev",
+    "Digital-Prison-Services-dev-hmpps_prisoner_audit_queue" = "hmpps-audit-dev",
   }
 
   # The names of the SNS topics used and the namespace which created them
