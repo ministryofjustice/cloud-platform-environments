63dea78dd9f22a48a93f5237b5e6e54c38176b3e
Marcus Aspin <maspin@unilink.com>
2026-07-23 14:55:31 +0100

    PI-4365 Add hmpps-supervision namespaces (prod) (#44315)
    * PI-4365 Add hmpps-supervision namespaces (prod)
    
    * Add reviewer_teams

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-supervision-prod/resources/domain-events-topic.tf
@@ -0,0 +1,26 @@
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08"
+}
+
+data "aws_ssm_parameter" "hmpps-domain-events-policy-arn" {
+  name = "/hmpps-domain-events-${var.environment_name}/sns/${data.aws_sns_topic.hmpps-domain-events.name}/irsa-policy-arn"
+}
+
+# Policy to allow SNS -> SQS
+data "aws_iam_policy_document" "sqs_queue_policy_document" {
+  statement {
+    sid     = "DomainEventsToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_sns_topic.hmpps-domain-events.arn]
+    }
+    resources = ["*"]
+  }
+}
---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-supervision-prod/resources/github.tf
@@ -0,0 +1,16 @@
+module "hmpps-supervision" {
+  source                        = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.1"
+  github_repo                   = "hmpps-supervision"
+  selected_branch_patterns      = ["main"]
+  source_template_repo          = "none"
+  reviewer_teams                = [var.team_name]
+  application                   = var.application
+  github_owner                  = var.github_owner
+  github_team                   = var.team_name
+  github_token                  = var.github_token
+  environment                   = var.environment_name
+  application_insights_instance = var.environment_name
+  is_production                 = var.is_production
+  namespace                     = var.namespace
+  kubernetes_cluster            = var.kubernetes_cluster
+}
