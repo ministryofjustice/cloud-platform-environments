84fa84151a62236adf05c195f79c4e13848ac7dd
Pietro Convalle <pietro.convalle@madetech.com>
2026-07-23 14:00:42 +0100

    Updated to correct domain event service for prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/irsa.tf
@@ -1,7 +1,7 @@
 
 locals {
   sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events"
+    "cloud-platform-Digital-Prison-Services-97e6567cf80881a8a52290ff2c269b08" = "hmpps-domain-events-prod"
   }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
