100057ee1b504cd1c08e01d50f1765876c0dabe1
Marcus Aspin <maspin@unilink.com>
2026-07-23 14:55:20 +0100

    PI-4365 Add hmpps-supervision namespaces (preprod) (#44314)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-supervision-preprod/resources/domain-events-topic.tf
@@ -0,0 +1,26 @@
+data "aws_sns_topic" "hmpps-domain-events" {
+  name = "cloud-platform-Digital-Prison-Services-15b2b4a6af7714848baeaf5f41c85fcd"
+}
+
+data "aws_ssm_parameter" "hmpps-domain-events-policy-arn" {
+  name = "/hmpps-domain-events-${var.environment_name}/sns/${data.aws_sns_topic.hmpps-domain-events.name}/irsa-policy-arn"
+}
+
+# Policy to allow SNS -> SQS
+data "aws_iam_policy_document" "sqs_queue_policy_document" {
+  statement {
+    sid     = "DomainEventsToQueue"
+    effect  = "Allow"
+    actions = ["sqs:SendMessage"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      variable = "aws:SourceArn"
+      test     = "ArnEquals"
+      values   = [data.aws_sns_topic.hmpps-domain-events.arn]
+    }
+    resources = ["*"]
+  }
+}
