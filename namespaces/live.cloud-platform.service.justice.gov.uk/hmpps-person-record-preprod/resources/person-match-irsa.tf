b274ac0c4d658b4b9cd2d7e41015ec51e24d58c0
Luke Bevan <6418390+lbevan@users.noreply.github.com>
2026-07-24 15:47:41 +0100

    Update person-match-irsa.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/person-match-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-person-record-preprod/resources/person-match-irsa.tf
@@ -5,7 +5,7 @@ module "hmpps_person_match_irsa" {
   eks_cluster_name = var.eks_cluster_name
 
   # IRSA configuration
-  service_account_name = "person-record-service"
+  service_account_name = "person-match-service"
   role_policy_arns = {
       rds = module.hmpps_person_match_rds.irsa_policy_arn
   }
