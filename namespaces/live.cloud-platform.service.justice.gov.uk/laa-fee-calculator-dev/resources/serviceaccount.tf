5f7525ded8ef695dd8e9f771d192035470a92695
Roni Colling <veronica.colling@justice.gov.uk>
2026-07-27 15:52:34 +0100

    fix(config): amend to add gh role_name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-fee-calculator-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-fee-calculator-dev/resources/serviceaccount.tf
@@ -3,10 +3,10 @@ module "serviceaccount" {
 
   namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
+  role_name = "serviceaccount_role_gh"
 
   # default value
   serviceaccount_token_rotated_date = "01-01-2000"
-  serviceaccount_name = "feecalc-dev-serviceaccount"
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
