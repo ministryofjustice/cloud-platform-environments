a1cdfacb56107b2c69f6121a5cde46bc072092f8
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2026-07-27 15:12:49 +0100

    Provision mock datastore in crime matching dev (#44386)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/iam-policies.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-electronic-monitoring-crime-matching-dev/resources/iam-policies.tf
@@ -4,7 +4,8 @@ data "aws_iam_policy_document" "athena_policy" {
       "sts:AssumeRole"
     ]
     resources = [
-      var.datastore_role
+      var.datastore_role,
+      aws_iam_role.mock_datastore_role.arn,
     ]
   }
 }
