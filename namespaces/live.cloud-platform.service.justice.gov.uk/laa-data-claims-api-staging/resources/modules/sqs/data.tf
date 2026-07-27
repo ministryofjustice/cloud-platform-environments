749408c860e121fc8fa6b9161414ecdd3680db58
siddharthshroff-MoJ <Siddharth.Shroff@justice.gov.uk>
2026-07-27 16:52:54 +0100

    feat: updated iam policies for queue and dlq staging
    Signed-off-by: siddharthshroff-MoJ <Siddharth.Shroff@justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-staging/resources/modules/sqs/data.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-claims-api-staging/resources/modules/sqs/data.tf
@@ -1,6 +1,7 @@
 /* --SQS Access Control--
 The two data sources below control access to SQS by looking up the value in the "namespace" tag
 for a specific namespace. */
+# data.tf
 
 data "aws_iam_roles" "sqs_subscriber_roles" {
   name_regex = var.sqs_subscriber_roles_regex_filter
@@ -11,23 +12,46 @@ data "aws_iam_role" "sqs_matching_roles" {
   name     = each.value
 }
 
-#--DLQ Policy
+# Resource-based policy on the DLQ
 data "aws_iam_policy_document" "dlq" {
+  # Only the source queue can redrive messages into the DLQ
   statement {
-    sid    = "Allow"
+    sid    = "AllowRedriveFromSourceQueue"
     effect = "Allow"
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
+    actions   = ["sqs:SendMessage"]
+    resources = [module.dlq.sqs_arn]
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [module.queue.sqs_arn]
+    }
+  }
+
+  # Only IRSA roles tagged with an allowed namespace can read/manage the DLQ
+  statement {
+    sid    = "AllowIRSAAccess"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [for role in local.sqs_roles_with_namespace_tag : role.arn]
+    }
     actions = [
-      "sqs:*"
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:ChangeMessageVisibility",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:PurgeQueue",
     ]
     resources = [module.dlq.sqs_arn]
   }
 }
 
-#--This policy will be constructed from the data sources above and some local transformations to grant SQS access
+# Resource-based policy on the main queue
 data "aws_iam_policy_document" "queue" {
   statement {
     sid    = "AllowSend"
