cf45444c6e63c212e1e69b82ca1d2dc2bbbf11f3
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-07-28 17:34:00 +0100

    feat(service-pod): add IRSA configuration to service pod setup

--- namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/irsa.tf
@@ -21,30 +21,3 @@ module "irsa" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
-
-# Setting up a service pod
-# irsa configuration is required to use the service pod
-module "irsa_service_pod" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
-
-  # EKS configuration
-  eks_cluster_name = var.eks_cluster_name
-
-  # IRSA configuration
-  service_account_name = "${var.namespace}-service-account"
-  namespace            = var.namespace # this is also used as a tag
-
-  # Policies for AWS resources accessed via the service pod
-  role_policy_arns = {
-    rds = module.rds_mssql.irsa_policy_arn
-    ecr = module.ecr.irsa_policy_arn
-  }
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-}
--- namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/chaps-dotnet-production/resources/service-pod.tf
@@ -1,4 +1,30 @@
-# set up the service pod
+# Setting up a service pod
+# irsa configuration is required to use the service pod
+module "irsa_service_pod" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # IRSA configuration
+  service_account_name = "${var.namespace}-service-account"
+  namespace            = var.namespace # this is also used as a tag
+
+  # Policies for AWS resources accessed via the service pod
+  role_policy_arns = {
+    rds = module.rds_mssql.irsa_policy_arn
+    ecr = module.ecr.irsa_policy_arn
+  }
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
 module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.2.1"
 
@@ -6,3 +32,4 @@ module "service_pod" {
   namespace            = var.namespace
   service_account_name = module.irsa_service_pod.service_account_name # this uses the service account name from the irsa module
 }
+
