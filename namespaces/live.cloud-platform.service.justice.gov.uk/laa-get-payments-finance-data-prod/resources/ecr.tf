24ce6eaa7b35b64da4f11b918c7f7e4e7377f026
DevarshBarotOC <264167205+DevarshBarotOC@users.noreply.github.com>
2026-07-28 15:09:51 +0100

    LPF-1536 - Removed prefix from prod ECR

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-get-payments-finance-data-prod/resources/ecr.tf
@@ -24,9 +24,6 @@ module "ecr" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
-  # For ECR creation to differentiate between environments
-  github_actions_prefix = "prod"
-
   enable_irsa = true
 }
 
@@ -51,7 +48,7 @@ module "data_ecr" {
   infrastructure_support = var.infrastructure_support
 
   # For ECR creation to differentiate between environments
-  github_actions_prefix = "prod_data"
+  github_actions_prefix = "data"
 
   enable_irsa = true
 }
