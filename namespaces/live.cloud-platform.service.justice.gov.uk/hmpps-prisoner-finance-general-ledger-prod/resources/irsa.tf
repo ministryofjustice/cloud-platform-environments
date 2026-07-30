365e7824f04e7fe2f389bd289b80c404927478ef
Matt Smith <mattonfoot@gmail.com>
2026-07-30 09:29:36 +0100

    Add S3 bucket for NOMIS synchronised database backups (#44479)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-general-ledger-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-general-ledger-prod/resources/irsa.tf
@@ -2,10 +2,11 @@
 # The value of each item should be the namespace where the SQS was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
-  irsa_policies ={
-    gl_calculcated_balance_queue             = module.prisoner_finance_general_ledger_queue_for_calculated_balances.irsa_policy_arn
-    gl_calculcated_balance_queue_dlq         = module.prisoner_finance_general_ledger_queue_for_calculated_balances_dead_letter_queue.irsa_policy_arn
-    rds                                      = module.rds.irsa_policy_arn
+  irsa_policies = {
+    gl_calculcated_balance_queue     = module.prisoner_finance_general_ledger_queue_for_calculated_balances.irsa_policy_arn
+    gl_calculcated_balance_queue_dlq = module.prisoner_finance_general_ledger_queue_for_calculated_balances_dead_letter_queue.irsa_policy_arn
+    rds                              = module.rds.irsa_policy_arn
+    s3                               = module.s3.irsa_policy_arn
   }
 }
 
@@ -16,6 +17,7 @@ module "hmpps_prisoner_finance_gl_irsa" {
   namespace            = var.namespace
   service_account_name = var.service_account_name
   role_policy_arns     = local.irsa_policies
+
   # Tags
   business_unit          = var.business_unit
   application            = var.application
