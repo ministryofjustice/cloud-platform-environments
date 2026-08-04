6b08344c464f8d5d8ffa606aa2f471caa8692a8b
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-08-04 10:58:39 +0100

    Add a name to the sql backup bucket (#44596)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/s3.tf
@@ -61,6 +61,7 @@ resource "kubernetes_secret" "s3_bucket" {
 module "sqlserver_backup_s3_bucket" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
 
+  bucket_name            = "hmpps-cfo-case-assessment-tracking-system-backup-${var.environment}"
   team_name              = var.team_name
   business_unit          = var.business_unit
   application            = var.application
