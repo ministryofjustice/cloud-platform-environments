9caddab899a05ff6e93fe279de6d2328e59efb4d
Ollie Evans <ollie.evans1@justice.gov.uk>
2026-08-05 11:22:33 +0100

    cis-pp-changing-test-account-email

--- namespaces/live.cloud-platform.service.justice.gov.uk/cis-pp/resources/cognito.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cis-pp/resources/cognito.tf
@@ -181,11 +181,11 @@ data "aws_secretsmanager_secret_version" "cognito_test_user" {
 
 resource "aws_cognito_user" "test" {
   user_pool_id = aws_cognito_user_pool.main.id
-  username     = "ollie.evans1@justice.gov.uk"
+  username     = "ollie.evanstest@justice.gov.uk"
 
   attributes = {
     cis-role       = "viewer"
-    email          = "ollie.evans1@justice.gov.uk"
+    email          = "ollie.evanstest@justice.gov.uk"
     email_verified = "true"
   }
 
