b0a64d1e35e80d3b2fb60f6b8a970a4f152bd4e1
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-08-05 17:18:57 +0100

    Configure GitHub deployment approval for CFO train (#44674)
    * Configure GitHub deployment approval for CFO train
    
    * Add flag to determine if environment requires a review to deploy

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-train/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-train/resources/variables.tf
@@ -38,6 +38,18 @@ variable "environment" {
   default     = "training"
 }
 
+variable "github_environment_name" {
+  description = "Name of the GitHub Actions / helm-values environment this namespace serves"
+  type        = string
+  default     = "train"
+}
+
+variable "require_deployment_reviewers" {
+  description = "Whether deployments to this GitHub Actions environment require approval from team_name reviewers"
+  type        = bool
+  default     = true
+}
+
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
