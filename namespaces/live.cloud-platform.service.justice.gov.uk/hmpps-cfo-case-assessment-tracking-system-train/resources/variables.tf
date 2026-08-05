12733f670f406addddee1b23e373c8c6236a1cdc
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-08-05 16:47:50 +0100

    Configure GitHub deployment approval for CFO train

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-train/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-train/resources/variables.tf
@@ -38,6 +38,12 @@ variable "environment" {
   default     = "training"
 }
 
+variable "github_environment_name" {
+  description = "Name of the GitHub Actions / helm-values environment this namespace serves"
+  type        = string
+  default     = "train"
+}
+
 variable "infrastructure_support" {
   description = "Email address of the team responsible this service"
   type        = string
