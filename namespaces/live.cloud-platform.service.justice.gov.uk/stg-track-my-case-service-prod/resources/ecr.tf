ec24013dd75df3c3ea4d97ee8750dd3c1096ff23
Rohit Gokhale <rohit.gokhale@justice.co.uk>
2026-08-06 12:33:28 +0100

    chore: preparing to decommissioning namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/stg-track-my-case-service-prod/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/stg-track-my-case-service-prod/resources/ecr.tf
@@ -15,6 +15,7 @@ module "ecr" {
   github_repositories = ["stg-track-my-case-service"]
   github_environments = ["prod"]
   github_actions_prefix = "prod"
+  deletion_protection = false
 
   # Tags
   business_unit          = var.business_unit
