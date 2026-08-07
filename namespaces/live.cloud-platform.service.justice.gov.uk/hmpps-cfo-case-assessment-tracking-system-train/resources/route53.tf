fdffb7ea579aa07511db1745e394e99c64d8545c
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2026-08-07 09:08:44 +0100

    Use a custom domain for train (#44703)

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-train/resources/route53.tf
@@ -0,0 +1,25 @@
+resource "aws_route53_zone" "cats_train" {
+  name = "training.manage-external-funded-offender-provision.service.justice.gov.uk"
+
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    environment-name       = var.environment
+    owner                  = var.team_name
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "cats_train_route53_zone" {
+  metadata {
+    name      = "cats-train-route53-zone-output"
+    namespace = var.namespace
+  }
+
+  data = {
+    zone_id     = aws_route53_zone.cats_train.zone_id
+    nameservers = join("\n", aws_route53_zone.cats_train.name_servers)
+  }
+}
