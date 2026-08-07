ac6f6b0cd41cafbe948964183a8194387908cb3f
Kristian Gregory <kristian.gregory@digital.justice.gov.uk>
2026-08-07 14:47:19 +0100

    HMPPS Visits frontend prod - deploy envoy proxy

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/visit-someone-in-prison-frontend-svc-prod/resources/egress-controls.tf
@@ -0,0 +1,22 @@
+# Deploys Envoy HTTPS forward proxy and creates proxy environment secret
+# For guidance see: https://tech-docs.hmpps.service.justice.gov.uk/how-to-guides/retrofitting-egress-controls-with-envoy-proxy
+
+module "hmpps_egress_controls" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-egress-controls?ref=0.0.7"
+
+  enable_envoy_setup     = true
+  enable_egress_controls = false 
+
+  namespace = var.namespace
+  vpc_name  = var.vpc_name
+
+  # Add your service's external dependencies
+  envoy_extra_allowed_hosts_exact = [
+    "oidc.integration.account.gov.uk",
+    "home.integration.account.gov.uk"
+  ]
+
+  envoy_extra_allowed_hosts_suffixes = [
+  ]
+
+}
