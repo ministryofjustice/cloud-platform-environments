b64b4712b028f6781b1f92153e57d01931d6be4e
Kurt Plaschke <kurt.plaschke@digital.justice.gov.uk>
2026-08-10 16:08:15 +0100

    updated version to correct latest

--- namespaces/live.cloud-platform.service.justice.gov.uk/mandatory-drug-testing-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mandatory-drug-testing-dev/resources/rds-postgresql.tf
@@ -1,5 +1,5 @@
 module "rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.3.0"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=9.2.0"
 
   # VPC configuration
   vpc_name = var.vpc_name
@@ -28,29 +28,8 @@ module "rds" {
   namespace              = var.namespace
   team_name              = var.team_name
 
-}
-
-# To create a read replica, use the below code and update the values to specify the RDS instance
-# from which you are replicating. In this example, we're assuming that rds is the
-# source RDS instance and read-replica is the replica we are creating.
-
-module "read_replica" {
-  # default off
-  count  = 0
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=7.3.0"
-
-  vpc_name = var.vpc_name
-
-  # Tags
-  application            = var.application
-  business_unit          = var.business_unit
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  is_production          = var.is_production
-  namespace              = var.namespace
-  team_name              = var.team_name
-
   enable_irsa = true
+
 }
 
 resource "kubernetes_secret" "postgres" {
