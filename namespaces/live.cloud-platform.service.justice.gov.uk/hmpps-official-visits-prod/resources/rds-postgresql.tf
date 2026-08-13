a6009cdb9d3b7426299678b9870233b67c65a880
prabash_moj <prabash.balasuriya@justice.gov.uk>
2026-08-13 17:38:42 +0100

    OV-862 : Configure the OV database for DPR read replica production

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-prod/resources/rds-postgresql.tf
@@ -131,11 +131,11 @@ resource "kubernetes_secret" "read_replica" {
   }
 
   data = {
-    database_name         = module.read_replica.database_name
-    database_username     = module.read_replica.database_username
-    database_password     = module.read_replica.database_password
-    rds_instance_endpoint = module.read_replica.rds_instance_endpoint
-    rds_instance_address  = module.read_replica.rds_instance_address
+    database_name         = module.read_replica[0].database_name
+    database_username     = module.read_replica[0].database_username
+    database_password     = module.read_replica[0].database_password
+    rds_instance_endpoint = module.read_replica[0].rds_instance_endpoint
+    rds_instance_address  = module.read_replica[0].rds_instance_address
   }
 }
 
