91b4c301107e160726402858f4fb10e626b8ed0d
said-moj <45761276+said-moj@users.noreply.github.com>
2026-08-17 14:01:41 +0100

    laa-cla-frontend-training - Update S3 bucket acl to private

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-training/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-training/resources/s3.tf
@@ -1,6 +1,6 @@
 module "cla_frontend_static_files_bucket" {
   source                        = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
-  acl                           = "public-read"
+  acl                           = "private"
   enable_allow_block_pub_access = false
   team_name                     = var.team_name
   business_unit                 = var.business_unit
