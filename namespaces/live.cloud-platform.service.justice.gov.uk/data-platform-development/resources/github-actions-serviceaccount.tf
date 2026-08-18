8ff278d3f7b3b1c80696bc6d123ed367bc704737
muhammad9970 <88196037+muhammad9970@users.noreply.github.com>
2026-08-18 11:33:49 +0100

    Data Platform: Remove development namespace- #44974 (#44974)
    Signed-off-by: Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-development/resources/github-actions-serviceaccount.tf
+++
@@ -1,8 +0,0 @@
-module "github_actions_serviceaccount" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.1.0"
-
-  kubernetes_cluster  = var.kubernetes_cluster
-  namespace           = var.namespace
-  serviceaccount_name = "github-actions"
-  serviceaccount_token_rotated_date = "20-03-2026"
-}
