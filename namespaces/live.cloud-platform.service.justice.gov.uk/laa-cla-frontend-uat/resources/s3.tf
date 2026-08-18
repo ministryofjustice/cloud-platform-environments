739194781999b5c45603fc6dc3c1c0b091292db1
said-moj <45761276+said-moj@users.noreply.github.com>
2026-08-18 14:41:11 +0100

    Remove unused cors rules now that bucket is private

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-uat/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-frontend-uat/resources/s3.tf
@@ -13,15 +13,6 @@ module "cla_frontend_static_files_bucket" {
   providers = {
     aws = aws.london
   }
-  cors_rule = [
-    {
-      allowed_headers = ["*"]
-      allowed_methods = ["GET"]
-      allowed_origins = ["https://*.apps.live-1.cloud-platform.service.justice.gov.uk", "uat.cases.civillegaladvice.service.gov.uk"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-    }
-  ]
 }
 
 
