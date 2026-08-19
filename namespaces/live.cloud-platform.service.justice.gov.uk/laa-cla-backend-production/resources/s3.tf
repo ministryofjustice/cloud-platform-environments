9c2c951e25e0806e2ce7ec922dce2aa7e8a08056
said-moj <45761276+said-moj@users.noreply.github.com>
2026-08-19 14:38:08 +0100

    Enable allow block public access

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-production/resources/s3.tf
@@ -59,7 +59,7 @@ module "cla_backend_static_files_bucket" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.0"
 
   acl                           = "private"
-  enable_allow_block_pub_access = false
+  enable_allow_block_pub_access = true
   team_name                     = var.team_name
   business_unit                 = var.business_unit
   application                   = var.application
@@ -71,15 +71,6 @@ module "cla_backend_static_files_bucket" {
   providers = {
     aws = aws.london
   }
-  cors_rule = [
-    {
-      allowed_headers = ["*"]
-      allowed_methods = ["GET"]
-      allowed_origins = ["https://laa-cla-backend-production.apps.live-1.cloud-platform.service.justice.gov.uk", "https://fox.civillegaladvice.service.gov.uk"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-    }
-  ]
 }
 
 
