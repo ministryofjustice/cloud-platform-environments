d8151acb543a81dd738a84856973d426f1e11815
Lewis P <lewis.prescott@justice.gov.uk>
2026-08-20 09:13:56 +0100

    Upgrade RDS PostgreSQL from version 14 to 15

--- namespaces/live.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds-postgres14.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/pact-broker-prod/resources/rds-postgres14.tf
@@ -11,10 +11,12 @@ module "pact_broker_rds_postgres14" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
-  rds_family                  = "postgres14"
-  db_engine_version           = "14"
+  prepare_for_major_upgrade   = true
+  rds_family                  = "postgres15"
+  db_engine_version           = "15"
   db_instance_class           = "db.t4g.small"
-  allow_major_version_upgrade = "false"
+  allow_major_version_upgrade = false
+  allow_minor_version_upgrade = true
 
   providers = {
     aws = aws.london
