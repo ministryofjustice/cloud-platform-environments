6cba232e97a863c3b613aba32bcfe4e8f372049a
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-08-21 14:50:57 +0100

    Amend CORs policy so fonts and scripts can be loaded from CDN

--- namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hale-platform-dev/resources/cloudfront.tf
@@ -20,6 +20,9 @@ module "cloudfront_with_ordered" {
 
   # Managed-SimpleCORS: adds "Access-Control-Allow-Origin: *".
   default_cache_behavior = {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+
     response_headers_policy_id = "e61eb60c-9c35-4d20-a928-2b84e02af89c"
   }
 
