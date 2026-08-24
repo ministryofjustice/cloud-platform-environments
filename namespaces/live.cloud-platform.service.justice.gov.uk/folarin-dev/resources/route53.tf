79fc7285d15678c9db6fc3dc45c17905864f0a1b
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-08-24 14:42:42 +0100

    Remove octo-nonlive custom domain test DNS records (#45108)
    Deletes the t3 _acme-challenge and traffic CNAMEs added for the #7877 custom-TLS acceptance test. The test is complete and evidence captured, removing the records eliminates the dangling-CNAME subdomain-takeover risk.

--- namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/folarin-dev/resources/route53.tf
@@ -24,28 +24,3 @@ resource "kubernetes_secret" "folarin_dev_route53_zone_sec" {
     nameservers = join("\n", aws_route53_zone.folarin_dev_route53_zone.name_servers)
   }
 }
-
-locals {
-  custom_host = "t3"
-
-  octo_nonlive_acme_target = "${local.custom_host}-acme.octo-nonlive.container-platform.service.justice.gov.uk"
-  octo_nonlive_nlb         = "octo-nonlive-envoy-default-7f403de9005876a3.elb.eu-west-2.amazonaws.com"
-}
-
-# ACME DNS-01 delegation: cert-manager (octo-nonlive) follows this CNAME and writes the
-# challenge TXT at the target, which sits in a zone its Pod Identity IAM can write.
-resource "aws_route53_record" "custom_acme_challenge" {
-  zone_id = aws_route53_zone.folarin_dev_route53_zone.zone_id
-  name    = "_acme-challenge.${local.custom_host}.${var.domain}"
-  type    = "CNAME"
-  ttl     = 300
-  records = [local.octo_nonlive_acme_target]
-}
-
-resource "aws_route53_record" "custom_traffic" {
-  zone_id = aws_route53_zone.folarin_dev_route53_zone.zone_id
-  name    = "${local.custom_host}.${var.domain}"
-  type    = "CNAME"
-  ttl     = 300
-  records = [local.octo_nonlive_nlb]
-}
