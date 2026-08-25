b09a4d7360bbe6af5732edb34013dd84df2bbcf4
dan-criddle <dan.criddle@justice.gov.uk>
2026-08-25 15:55:29 +0100

    Add IRSA module configuration in irsa.tf

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/irsa.tf
@@ -0,0 +1,15 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+  namespace            = var.namespace
+  service_account_name = var.application
+  role_policy_arns     = {}
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
