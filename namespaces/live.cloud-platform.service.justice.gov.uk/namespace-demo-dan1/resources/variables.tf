805e6c28cab0e9123088afbac76cd257e4bb7f1b
dan-criddle <dan.criddle@justice.gov.uk>
2026-08-25 16:10:26 +0100

    Add missing eks_cluster_name variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/variables.tf
@@ -67,3 +67,9 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
+
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+  default = "live"
+}
+
