4f3b6937acd4aa669f8f87f5807d8f2d0a2bc7c4
Szabina K <szabina.kovacs@justice.gov.uk>
2026-08-25 11:03:43 +0100

    Add rolebinding github actions,(prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-prod/resources/serviceaccount-githubactions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/claim-criminal-injuries-compensation-prod/resources/serviceaccount-githubactions.tf
@@ -1,10 +1,14 @@
 module "serviceaccount_githubactions" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.2.0"
 
-  namespace          = var.namespace
+  namespace = var.namespace
   kubernetes_cluster = var.kubernetes_cluster
 
   serviceaccount_name = "github-actions"
+  role_name = "github-actions-role"
+  rolebinding_name = "github-actions-rolebinding"
+
+  serviceaccount_token_rotated_date = "25-08-2026"
 
   github_repositories = ["cica-apply-web"]
   github_environments = ["prod"]
