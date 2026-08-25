991cc038079cb3d38135a997f95bb8e0fe8caa34
dan-criddle <dan.criddle@justice.gov.uk>
2026-08-25 16:18:29 +0100

    REmove spaces from app name

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/variables.tf
@@ -11,7 +11,7 @@ variable "kubernetes_cluster" {
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
-  default     = "Dans First Namespace"
+  default     = "dan-application"
 }
 
 variable "namespace" {
