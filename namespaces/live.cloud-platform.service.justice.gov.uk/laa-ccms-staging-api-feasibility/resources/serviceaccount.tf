19c02e82b9a9b6d4e2bc4aecefff8b6ec2b0fe66
farrell-m <michael.farrell@justice.gov.uk>
2026-08-26 12:34:55 +0100

    add ccms staging api feasibility namespace

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-ccms-staging-api-feasibility/resources/serviceaccount.tf
@@ -0,0 +1,15 @@
+module "serviceaccount" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-serviceaccount?ref=1.2.0"
+
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
+  
+  serviceaccount_token_rotated_date = "26-08-2026"
+
+  # Uncomment and provide repository names to create github actions secrets
+  # containing the ca.crt and token for use in github actions CI/CD pipelines
+  # github_repositories = ["my-repo"]
+  github_repositories = ["laa-ccms-staging-rest-api"]
+  github_environments = ["feasibility"]
+
+}
