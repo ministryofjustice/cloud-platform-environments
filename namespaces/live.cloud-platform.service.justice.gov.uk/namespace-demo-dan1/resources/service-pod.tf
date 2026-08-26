0fd7a7be6270abe58e19d711bca4c894370b19e0
dan-criddle <dan.criddle@justice.gov.uk>
2026-08-26 16:29:32 +0100

    Add unique deployment names to service pod modules

--- namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/namespace-demo-dan1/resources/service-pod.tf
@@ -4,6 +4,7 @@ module "service_pod_100" {
   # Configuration
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+  override_deployment_name = "sp-alpine"
 }
 
 module "service_pod_121" {
@@ -12,6 +13,7 @@ module "service_pod_121" {
   # Configuration
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+  override_deployment_name = "sp-debian12"
 }
 
 module "service_pod_debian13" {
@@ -20,5 +22,5 @@ module "service_pod_debian13" {
   # Configuration
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+  override_deployment_name = "sp-debian13"
 }
-
