5c3d7a1eec4ee5dc496e2fa1cf68cf300b836630
Brian Carr <brian.carr@justice.gov.uk>
2026-08-26 15:44:31 +0100

    Fix to irsa module policy_arns ref

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/irsa.tf
@@ -56,7 +56,7 @@ module "irsa_rds_backup_check"{
   namespace            = var.namespace
   
   role_policy_arns = {
-    rds_backup_check = data.aws_iam_policy_document.rds_backup_check_assume_role_policy.arn
+    rds_backup_check = aws_iam_policy.rds_backup_check_policy.arn
   }
   business_unit          = var.business_unit
   application            = var.application
