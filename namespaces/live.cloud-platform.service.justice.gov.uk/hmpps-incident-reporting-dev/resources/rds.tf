f054393a7e24032bfe5811a14b20a57e12486722
Michael Willis <michael.willis2@justice.gov.uk>
2026-08-26 11:45:28 +0100

    IR-1883 Pin hmpps-incident-reporting-dev read replica to Postgres 17.9 (#45187)
    The primary was pinned to 17.9 in an earlier PR but the read replica
    block was missed and still specified 17.6. A replica must run the same
    engine version as its primary, so the apply still fails on this
    namespace.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incident-reporting-dev/resources/rds.tf
@@ -86,7 +86,7 @@ module "read_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "17.6"
+  db_engine_version = "17.9"
   rds_family        = "postgres17"
   db_instance_class = "db.t4g.small"
   # It is mandatory to set the below values to create read replica instance
