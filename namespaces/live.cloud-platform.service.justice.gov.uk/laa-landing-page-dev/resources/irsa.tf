e53e710504d6ca06e6763ed94fd804a17c6add27
Brian Carr <brian.carr@justice.gov.uk>
2026-08-26 14:16:12 +0100

    Adding IRSA policy to allow checks for RDS snapshots and removing redundant AWS secret.

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/irsa.tf
@@ -33,3 +33,35 @@ module "irsa_temp" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 }
+
+data "aws_iam_policy_document" "rds_backup_check_assume_role_policy" {
+  statement {
+    sid = "DescribeDBSnapshots"
+    effect = "Allow"
+    actions = ["rds:DescribeDBSnapshots"]
+    resources = ["arn:aws:rds:*:*:snapshot:*"]
+  }
+}
+
+resource "aws_iam_policy" "rds_backup_check_policy" {
+  name        = "${var.namespace}-rds-backup-check-policy"
+  description = "Read-only permission for the rds-backup-check k8s cronjob to list DB snapshots and check their age"
+  policy      = data.aws_iam_policy_document.rds_backup_check_assume_role_policy.json
+}
+
+module "irsa_rds_backup_check"{
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name = var.eks_cluster_name
+  service_account_name = "irsa-laa-landing-page-${var.environment}-rds-backup-check"
+  namespace            = var.namespace
+  
+  role_policy_arns = {
+    rds_backup_check = data.aws_ssm_parameter.rds_backup_check_policy_arn.value
+  }
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
