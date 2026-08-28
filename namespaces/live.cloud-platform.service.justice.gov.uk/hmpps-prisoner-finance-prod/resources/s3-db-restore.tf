73b0198dd817fbd9c71726cf2cee021d997312d8
emilyfbgov <emily.bennett@justice.gov.uk>
2026-08-28 15:00:12 +0100

    update cron job restore name and secret

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/s3-db-restore.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/s3-db-restore.tf
@@ -1,10 +1,10 @@
-module "s3" {
+module "holds-restore-s3-bucket" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-s3-bucket?ref=5.3.1"
 
   # S3 configuration
 
   # Tags
-  application            = var.application
+  application            = "hmpps-prisoner-finance-holds-api"
   business_unit          = var.business_unit
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
@@ -15,7 +15,7 @@ module "s3" {
 
 resource "kubernetes_secret" "s3-db-restore" {
   metadata {
-    name      = "s3-db-restore-bucket"
+    name      = "holds-restore-s3-bucket"
     namespace = var.namespace
   }
 
