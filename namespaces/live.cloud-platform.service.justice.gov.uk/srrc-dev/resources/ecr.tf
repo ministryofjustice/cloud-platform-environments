579b6d855c46800ff8aff384acac0e0d73106c78
scottbdavey-MOJ <scott.davey@justice.gov.uk>
2026-09-01 12:43:04 +0100

    undo dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/srrc-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/srrc-dev/resources/ecr.tf
@@ -14,8 +14,6 @@ module "ecr" {
   oidc_providers      = ["github"]
   github_repositories = ["srrc-poc-sample-application","srrc-verify-biometric-scans-api","srrc-capture-finger-prints","srrc-soap-ws-stub-endpoint"]
 
-  github_environments = ["development"]
-
   # Tags
   business_unit          = var.business_unit
   application            = var.application
