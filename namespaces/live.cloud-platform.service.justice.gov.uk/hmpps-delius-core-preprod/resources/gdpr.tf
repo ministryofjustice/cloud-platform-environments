7d72510fa50d40dc25e40bc8df87130a7c9a8f26
james.macbeth <james.macbeth@justice.gov.uk>
2026-09-01 13:43:00 +0100

    DST-22918 - Enable prepare_for_major_upgrade flag for Merge and GDPR DBs

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-preprod/resources/gdpr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-delius-core-preprod/resources/gdpr.tf
@@ -17,7 +17,7 @@ module "gdpr_rds" {
 
   # PostgreSQL specifics
   db_engine                 = "postgres"
-  prepare_for_major_upgrade = false
+  prepare_for_major_upgrade = true
   db_engine_version         = "16"
   rds_family                = "postgres16"
   db_instance_class         = "db.m5.large"
