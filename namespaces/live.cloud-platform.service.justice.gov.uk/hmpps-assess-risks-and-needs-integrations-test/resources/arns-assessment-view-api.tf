9c938c36a608f93ab3aedac7509daeab34bcb0e1
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-09-03 12:45:37 +0100

    Swap hmpps-assessments team for new teams in hmpps-assess-risks-and-needs-integrations-test

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-test/resources/arns-assessment-view-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-assess-risks-and-needs-integrations-test/resources/arns-assessment-view-api.tf
@@ -4,7 +4,7 @@ module "hmpps_arns_assessment_view_api" {
   custom_token_rotation_date = "2026-03-20"
   github_repo                   = "hmpps-arns-assessment-view-api"
   application                   = "hmpps-arns-assessment-view-api"
-  github_team                   = "hmpps-assessments"
+  github_team                   = "hmpps-assessments-devs"
   environment                   = var.environment
   is_production                 = var.is_production
   application_insights_instance = "dev"
