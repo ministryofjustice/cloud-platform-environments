94aeee5547ee7b2779c4a63fbad9d9ca2711df00
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2026-09-04 13:48:16 +0100

    `justice-gov-uk-production` delete resources part 1 (#45345)
    * `justice-gov-uk-production` Delete resources - part 1
    
    * Remove K8S resources
    
    * Update versions.tf
    
    * Delete cloudfront.tf
    
    * Delete acm.tf
    
    * Update main.tf
    
    * Update variables.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Create 04-networkpolicy.yaml‎
    
    * Restore network policy

--- namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-production/resources/versions.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/justice-gov-uk-production/resources/versions.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "integrations/github"
       version = "~> 6.6.0"
     }
-    pingdom = {
-      source  = "DrFaust92/pingdom"
-      version = "~> 1.3.1"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23.0"
