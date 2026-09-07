5fd2a8b1beafc8492da236a8d106905723f7a270
Ollie Evans <ollie.evans1@justice.gov.uk>
2026-09-07 12:41:50 +0100

    cis-prd-adding-test-user

--- namespaces/live.cloud-platform.service.justice.gov.uk/cis-prd/resources/cognito.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cis-prd/resources/cognito.tf
@@ -189,7 +189,7 @@ resource "aws_cognito_user" "test" {
     email_verified = "true"
   }
 
-  password       = jsondecode(data.aws_secretsmanager_secret_version.cognito_test_user.secret_string)["CIS_PRD_COGNITO_TEST_USER_PASS"]
+  password       = jsondecode(data.aws_secretsmanager_secret_version.cognito_test_user.secret_string)["CIS_PP_COGNITO_TEST_USER_PASS"]
   message_action = "SUPPRESS"
 }
 
