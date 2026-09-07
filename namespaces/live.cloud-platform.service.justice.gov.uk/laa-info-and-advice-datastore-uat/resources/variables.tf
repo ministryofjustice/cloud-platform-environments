de068a993fe332c3caec06b687512135fe349c9d
Ian King <ian.antking@gmail.com>
2026-09-07 15:31:35 +0100

    add uat entra secrets

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-info-and-advice-datastore-uat/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-info-and-advice-datastore-uat/resources/variables.tf
@@ -8,6 +8,11 @@ variable "kubernetes_cluster" {
   type        = string
 }
 
+variable "eks_cluster_name" {
+  description = "EKS cluster name, used by the Secrets Manager module to create IAM role trust policies"
+  type        = string
+}
+
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
