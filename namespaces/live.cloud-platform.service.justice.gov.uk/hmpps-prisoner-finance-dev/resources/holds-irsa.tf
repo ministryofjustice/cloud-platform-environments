0f3b7653fc686d64be29648b0c5d7d0d97bc62c4
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-08 15:06:51 +0100

    Split into multiple service accounts

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/holds-irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/holds-irsa.tf
@@ -9,7 +9,7 @@ module "hmpps_prisoner_finance_holds_irsa" {
 
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
-  service_account_name = var.service_account_name
+  service_account_name = var.service_account_name_holds
   role_policy_arns     = local.holds_irsa_policies
   # Tags
   business_unit          = var.business_unit
