bf41a4b49dfaac17014859de7de826b940f29290
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-08 15:27:09 +0100

    Added a service pod for each service

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/service-pod.tf
@@ -2,6 +2,34 @@ module "service_pod" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
 
   # Configuration
+  override_deployment_name = "service-pod"
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
+}
+
+module "service_pod_holds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
+
+  # Configuration
+  override_deployment_name =  "holds-service-pod"
+  namespace            = var.namespace
+  service_account_name = module.holds_irsa.service_account.name # this uses the service account name from the irsa module
+}
+
+module "service_pod_advances" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
+
+  # Configuration
+  override_deployment_name =  "advances-service-pod"
+  namespace            = var.namespace
+  service_account_name = module.advances_irsa.service_account.name # this uses the service account name from the irsa module
+}
+
+module "service_pod_subscriptions" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
+
+  # Configuration
+  override_deployment_name =  "subscriptions-service-pod"
+  namespace            = var.namespace
+  service_account_name = module.subscriptions_irsa.service_account.name # this uses the service account name from the irsa module
 }
