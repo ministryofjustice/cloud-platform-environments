d31a3fa8663de10c2b16befa79495772ec6d13fd
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-08 15:43:49 +0100

    updated service names

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/service-pod.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-dev/resources/service-pod.tf
@@ -1,8 +1,8 @@
-module "service_pod" {
+module "service_pod_pf_api" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
 
   # Configuration
-  override_deployment_name = "service-pod"
+  override_deployment_name = "service-pod-pf-api"
   namespace            = var.namespace
   service_account_name = module.irsa.service_account.name # this uses the service account name from the irsa module
 }
@@ -11,25 +11,25 @@ module "service_pod_holds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
 
   # Configuration
-  override_deployment_name =  "holds-service-pod"
+  override_deployment_name =  "service-pod-holds"
   namespace            = var.namespace
-  service_account_name = module.holds_irsa.service_account.name # this uses the service account name from the irsa module
+  service_account_name = module.hmpps_prisoner_finance_holds_irsa.service_account.name # this uses the service account name from the irsa module
 }
 
 module "service_pod_advances" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
 
   # Configuration
-  override_deployment_name =  "advances-service-pod"
+  override_deployment_name =  "service-pod-advances"
   namespace            = var.namespace
-  service_account_name = module.advances_irsa.service_account.name # this uses the service account name from the irsa module
+  service_account_name = module.hmpps_prisoner_finance_advances_irsa.service_account.name # this uses the service account name from the irsa module
 }
 
 module "service_pod_subscriptions" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-service-pod?ref=1.3.0"
 
   # Configuration
-  override_deployment_name =  "subscriptions-service-pod"
+  override_deployment_name = "service-pod-subscriptions"
   namespace            = var.namespace
-  service_account_name = module.subscriptions_irsa.service_account.name # this uses the service account name from the irsa module
+  service_account_name = module.hmpps_prisoner_finance_subscriptions_irsa.service_account.name # this uses the service account name from the irsa module
 }
