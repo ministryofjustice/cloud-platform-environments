f8173ee105c150b337b7c2c68b4f4350de839d59
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-09 14:08:24 +0100

    Added reviewer teams

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/hmpps-prisoner-finance-subscriptions-api.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-prod/resources/hmpps-prisoner-finance-subscriptions-api.tf
@@ -6,8 +6,8 @@ module "hmpps_prisoner_finance_subscriptions_prod" {
   application                = "hmpps-prisoner-finance-subscriptions-api"
   github_team                = "hmpps-prisoner-finance"
   environment                = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  selected_branch_patterns = ["main"] # Optional
+  reviewer_teams             = [var.team_name] # Optional team that should review deployments to this environment.
+  selected_branch_patterns   = ["main"] # Optional
   #protected_branches_only       = true # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "prod" # Either "dev", "preprod" or "prod"
