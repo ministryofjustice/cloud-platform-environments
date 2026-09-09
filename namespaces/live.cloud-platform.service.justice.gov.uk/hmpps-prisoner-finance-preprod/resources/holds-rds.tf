c12c8fe7b7633c4a9f97cdffe09a52a1bf9df8dc
emilyfbgov <emily.bennett@justice.gov.uk>
2026-09-09 11:43:14 +0100

    adds refresh creds from pre prod to prod namespace

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-preprod/resources/holds-rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-preprod/resources/holds-rds.tf
@@ -85,3 +85,18 @@ resource "kubernetes_config_map" "rds" {
     db_identifier = module.holds_rds.db_identifier
   }
 }
+
+
+resource "kubernetes_secret" "dps_rds_refresh_creds" {
+  metadata {
+    name      = "holds_rds-postgresql-instance-output-preprod"
+    namespace = "hmpps-prisoner-finance-prod"
+  }
+
+  data = {
+    database_name        = module.holds_rds.database_name
+    database_username    = module.holds_rds.database_username
+    database_password    = module.holds_rds.database_password
+    rds_instance_address = module.holds_rds.rds_instance_address
+  }
+}
