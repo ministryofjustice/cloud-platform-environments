9f4b30c980691c04b13171e513cdca39f11e0661
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-09 15:23:16 +0100

    fixed var reference

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-prod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-prod/resources/irsa.tf
@@ -14,7 +14,7 @@ module "hmpps_prisoner_finance_sync_irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = sqs_policies
+  role_policy_arns     = local.sqs_policies
   # Tags
   business_unit          = var.business_unit
   application            = var.application
