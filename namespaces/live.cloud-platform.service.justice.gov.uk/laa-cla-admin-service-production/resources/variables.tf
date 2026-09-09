a9a0a5343c2a879bccdaa27578c57453d3f61e79
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-09 16:00:48 +0100

    laa-cla-admin-service-production - Add s3 (#45418)
    * laa-cla-admin-service-productio - Add s3
    
    * Rename service account to shorter name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-admin-service-production/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-admin-service-production/resources/variables.tf
@@ -8,6 +8,10 @@ variable "kubernetes_cluster" {
   type        = string
 }
 
+variable "eks_cluster_name" {
+  description = "The name of the eks cluster to retrieve the OIDC information"
+}
+
 variable "application" {
   description = "Name of the application you are deploying"
   type        = string
