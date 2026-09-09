4266bc9b4096c9615faa7552dd3bd2f4dd028c51
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-09 15:14:12 +0100

    updated irsa

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-dev/resources/irsa.tf
@@ -2,13 +2,10 @@
 # The value of each item should be the namespace where the SQS was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
-  irsa_policies = merge(local.sns_policies, local.sqs_policies,)
  sqs_queues = {
     "Digital-Prison-Services-dev-hmpps_audit_queue" = "hmpps-audit-dev",
   }
-
+  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
 }
 
 module "hmpps_prisoner_finance_sync_irsa" {
@@ -17,7 +14,7 @@ module "hmpps_prisoner_finance_sync_irsa" {
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = local.irsa_policies
+  role_policy_arns     = local.sqs_policies
   # Tags
   business_unit          = var.business_unit
   application            = var.application
