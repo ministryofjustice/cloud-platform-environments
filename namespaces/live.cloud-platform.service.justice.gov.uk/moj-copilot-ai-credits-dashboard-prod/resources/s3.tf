62027003075d59911f001c2ead3751fe859c6c99
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-09-09 13:12:38 +0100

    remove uneeded lifecycle rule

--- namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/s3.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/moj-copilot-ai-credits-dashboard-prod/resources/s3.tf
@@ -145,24 +145,6 @@ EOF
   oidc_providers = ["github"]
   github_repositories = ["moj-copilot-ai-credits-dashboard"]
   github_actions_prefix = "PROD"
-
-  lifecycle_rule = [
-    {
-      enabled                                = true
-      id                                     = "expire-copilot-credits-auth0-tf-state-prod"
-      abort_incomplete_multipart_upload_days = 90
-      expiration = [
-        {
-          days = 30
-        },
-      ]
-      noncurrent_version_expiration = [
-        {
-          days = 30
-        },
-      ]
-    },
-  ]
 }
 
 resource "kubernetes_secret" "s3_bucket" {
