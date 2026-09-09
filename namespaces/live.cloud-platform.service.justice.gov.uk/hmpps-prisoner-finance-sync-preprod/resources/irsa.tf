8525c43c1432883edb63442209a4db827ec71b1f
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-09 14:46:05 +0100

    removed sns and sqs vars

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-finance-sync-preprod/resources/irsa.tf
@@ -2,21 +2,17 @@
 # The value of each item should be the namespace where the SQS was created.
 # This information is used to collect the IAM policies which are used by the IRSA module.
 locals {
-  sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
-  sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
-  irsa_policies = merge(local.sns_policies, local.sqs_policies,)
-sqs_queues = {
+  sqs_queues = {
     "Digital-Prison-Services-preprod-hmpps_audit_queue" = "hmpps-audit-preprod",
   }
 }
 
 module "hmpps_prisoner_finance_sync_irsa" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
-
   eks_cluster_name     = var.eks_cluster_name
   namespace            = var.namespace
   service_account_name = var.application
-  role_policy_arns     = local.irsa_policies
+  role_policy_arns     = local.sqs_queues
   # Tags
   business_unit          = var.business_unit
   application            = var.application
