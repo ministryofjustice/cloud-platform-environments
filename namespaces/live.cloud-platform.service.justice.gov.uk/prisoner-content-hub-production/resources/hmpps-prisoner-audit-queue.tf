1a6b1be2882379f3d643a6a9af69570b7eb302fa
Curtis Young <Curtis.Young@justice.gov.uk>
2026-09-10 15:15:11 +0100

    Add IRSA for content hub production

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/prisoner-content-hub-production/resources/hmpps-prisoner-audit-queue.tf
@@ -0,0 +1,10 @@
+resource "kubernetes_secret" "hmpps_prisoner_audit_queue_secret" {
+  metadata {
+    name      = "sqs-hmpps-prisoner-audit-secret"
+    namespace = var.namespace
+  }
+
+  data = {
+    sqs_queue_url = "https://sqs.eu-west-2.amazonaws.com/754256621582/Digital-Prison-Services-prod-hmpps_prisoner_audit_queue"
+  }
+}
