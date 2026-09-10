3ac011b1870b8a82e0de2bbd681d4a893ed42755
Steve Calcott <steve.calcott@justice.gov.uk>
2026-09-10 10:57:12 +0100

    Set reviewer team and branch patterns.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/hmpps-sre-observability-dev.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/hmpps-sre-observability-dev.tf
@@ -10,8 +10,8 @@ module "hmpps_template_typescript" {
   application = "health-kick"
   github_team = "hmpps-sre"
   environment = var.environment # Should match environment name used in helm values file e.g. values-dev.yaml
-  #reviewer_teams                = ["hmpps-dev-team-1", "hmpps-dev-team-2"] # Optional team that should review deployments to this environment.
-  #selected_branch_patterns      = ["main", "release/*", "feature/*"] # Optional
+  reviewer_teams                = ["hmpps-sre"] # Optional team that should review deployments to this environment.
+  selected_branch_patterns      = ["main", "release/*", "feature/*", "bug/*", "**"] # Optional
   protected_branches_only       = false # Optional, defaults to true unless selected_branch_patterns is set
   is_production                 = var.is_production
   application_insights_instance = "dev" # Either "dev", "preprod" or "prod"
