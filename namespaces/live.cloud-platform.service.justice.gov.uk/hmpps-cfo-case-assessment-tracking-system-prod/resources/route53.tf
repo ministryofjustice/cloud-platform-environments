4f2ec4a96b8a4f9e79be405b19aa12e8d167fa21
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-09-10 18:08:41 +0100

    Replicate legacy DNS records into new prod Route53 zone
    Add Route53 records replicating the current legacy AWS account's DNS for manage-external-funded-offender-provision.service.justice.gov.uk exactly, so the zone can be safely delegated to Cloud Platform with no change in traffic.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-prod/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-prod/resources/route53.tf
@@ -23,3 +23,158 @@ resource "kubernetes_secret" "cats_prod_route53_zone" {
     nameservers = join("\n", aws_route53_zone.cats_prod.name_servers)
   }
 }
+
+# --- apex/production start ---
+# NOTE: the domains team suggested converting our ALIAS records to CNAME for consistency
+# (applied to "dev" below). This one is kept as an ALIAS instead, since a CNAME record is
+# not permitted at a zone apex/root (RFC 1034), and this name is the apex of the cats_prod
+# hosted zone above - ALIAS is the only valid way to point it at CloudFront.
+resource "aws_route53_record" "apex" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "A"
+  alias {
+    name                   = "d3tubljexqypxg.cloudfront.net"
+    zone_id                = "Z2FDTNDATAQYW2" # CloudFront's fixed hosted zone ID
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "apex_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_e423c56d23dc0dce73d7af3276cfd0e6.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_e74deb9ca8229527592f88459265f1ef.kirrbxfjtw.acm-validations.aws."]
+}
+# --- apex/production end ---
+
+
+# --- dev start ---
+resource "aws_route53_record" "dev" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "dev.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d3gjnixuqv4h4f.cloudfront.net"]
+}
+
+resource "aws_route53_record" "dev_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_d8e015eb77621a22d1bb4fe339ec1ebd.dev.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_f45d3bd1f2f3aa9f672facf92f66d56a.sdgjtdhdhz.acm-validations.aws."]
+}
+# --- dev end ---
+
+# --- training start ---
+resource "aws_route53_record" "training" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "training.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d2xa34imt80u6b.cloudfront.net"]
+}
+
+resource "aws_route53_record" "training_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_82cde6f94d128501499619acb1b642f4.training.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_556342720a60ba23735a0b41ba123f7b.djqtsrsxkq.acm-validations.aws."]
+}
+# --- training end ---
+
+# --- preprod start ---
+resource "aws_route53_record" "preprod" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "preprod.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["dnsmo1f6n0f63.cloudfront.net"]
+}
+
+resource "aws_route53_record" "preprod_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_8f2a1999a9e110af3685208ee3875799.preprod.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_319d4c72bdfe59b5b6d40f8409b78e65.sdgjtdhdhz.acm-validations.aws."]
+}
+# --- preprod end ---
+
+# --- legacy CFO3/CATS start ---
+resource "aws_route53_record" "legacy" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "legacy.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d2p66dhqonjkjj.cloudfront.net"]
+}
+
+resource "aws_route53_record" "legacy_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_d77aef5e838aac4d3d7761ad92401c78.legacy.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_414290eebad8535f016569d8e6055684.sdgjtdhdhz.acm-validations.aws."]
+}
+# --- legacy CFO3/CATS end ---
+
+# --- uat start (todo: remove as unused) ---
+resource "aws_route53_record" "uat" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "uat.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d35an6ch35u5jb.cloudfront.net"]
+}
+
+resource "aws_route53_record" "uat_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_5bbd77a4d881573bfe10b4fcca0d4579.uat.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_7ea920d16b055881c2a4dae81f31ca6c.xlfgrmvvlj.acm-validations.aws."]
+}
+# --- uat end ---
+
+# --- accessibility start (todo: remove as unused) ---
+resource "aws_route53_record" "accessibility" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "accessibility.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d1odphpda4fk81.cloudfront.net"]
+}
+
+resource "aws_route53_record" "accessibility_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_2cf3aee75260c043ab7bf7625aa4b527.accessibility.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_a7dcc4b9fda9896900ae733516c6193d.xlfgrmvvlj.acm-validations.aws."]
+}
+# --- accessibility end ---
+
+# --- preprod.visualiser start (todo: remove as unused) ---
+resource "aws_route53_record" "preprod_visualiser" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "preprod.visualiser.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["d3jlsovjv477z1.cloudfront.net"]
+}
+
+# NOTE: source spreadsheet truncated this name to "...manage-external-funded-offender-provision"
+# with no ".service.justice.gov.uk" suffix - verify the exact FQDN against the legacy zone
+# before applying, otherwise ACM validation for preprod.visualiser will fail.
+resource "aws_route53_record" "preprod_visualiser_acm_validation" {
+  zone_id = aws_route53_zone.cats_prod.zone_id
+  name    = "_bdcfe721a7cbf0bcfed8b2dc6a8a6512.preprod.visualiser.manage-external-funded-offender-provision.service.justice.gov.uk"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_4a0d681394880a4236091bb3f5030875.jkddzztszm.acm-validations.aws."]
+}
+# --- preprod.visualiser end ---
