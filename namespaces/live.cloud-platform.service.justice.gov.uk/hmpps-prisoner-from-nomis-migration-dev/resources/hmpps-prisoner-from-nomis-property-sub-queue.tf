95f9d63f04922b7979af997f3852b7da765d6e32
steverendell <steve.rendell@justice.gov.uk>
2026-09-10 17:22:54 +0100

    SDIT-3285 Handle CSRA booking.moved SDIT-3284 Handle CSRA merges SDIT-4010 Property: Handle booking.moved

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-prisoner-from-nomis-property-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-dev/resources/hmpps-prisoner-from-nomis-property-sub-queue.tf
@@ -44,7 +44,10 @@ resource "aws_sqs_queue_policy" "prisoner_from_nomis_property_queue_policy" {
             {
               "ArnEquals":
                 {
-                  "aws:SourceArn": "${data.aws_ssm_parameter.offender-events-topic-arn.value}"
+                  "aws:SourceArn": [
+                    "${data.aws_ssm_parameter.offender-events-topic-arn.value}",
+                    "${data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value}"
+                  ]
                 }
             }
         }
@@ -112,6 +115,18 @@ resource "aws_sns_topic_subscription" "prisoner_from_nomis_property_subscription
       "PRISONER_PROPERTY-INSERTED",
       "PRISONER_PROPERTY-UPDATED",
       "PRISONER_PROPERTY-DELETED"
+    ]
+  })
+}
+
+resource "aws_sns_topic_subscription" "prisoner_from_nomis_domain_property_subscription" {
+  provider  = aws.london
+  topic_arn = data.aws_ssm_parameter.hmpps-domain-events-topic-arn.value
+  protocol  = "sqs"
+  endpoint  = module.prisoner_from_nomis_property_queue.sqs_arn
+  filter_policy = jsonencode({
+    eventType = [
+      "prison-offender-events.prisoner.booking.moved",
     ]
   })
 }
