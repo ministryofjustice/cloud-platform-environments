dd47c1bd1bc2799a96badbbd7c94260e890c0e36
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2026-09-11 16:17:37 +0100

    Remove access to offender_events_topic for hmpps-prison-offender-events app, not required

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/prisoner-offender-events-sub-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-dev/resources/prisoner-offender-events-sub-queue.tf
@@ -162,8 +162,7 @@ module "prison-offender-events-irsa" {
     local.sns_policies,
     {
       prisoner_offender_events_queue = module.prisoner_offender_events_queue.irsa_policy_arn,
-      prisoner_offender_events_dlq   = module.prisoner_offender_events_dead_letter_queue.irsa_policy_arn,
-      offender_events_topic          = aws_iam_policy.sns_topic_irsa_publish[module.offender_events.topic_name].arn
+      prisoner_offender_events_dlq   = module.prisoner_offender_events_dead_letter_queue.irsa_policy_arn
   })
 
   # Tags
