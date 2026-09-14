31ed529bccf32d2b08e3cd3ff206cc567b90b256
David Conneely <899881+dconneely@users.noreply.github.com>
2026-09-14 14:15:44 +0100

    chore: Update PostgreSQL RDS version (prod)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-provider-data-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-data-provider-data-prod/resources/rds-postgresql.tf
@@ -22,7 +22,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "17"
+  db_engine_version = "17.11"
   rds_family        = "postgres17"
   db_instance_class = "db.t4g.small"
 
