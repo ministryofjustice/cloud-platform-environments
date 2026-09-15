ff8c0499b82237c62757206b93f179ccf2674c9c
Brian Carr <brian.carr@justice.gov.uk>
2026-09-15 15:27:55 +0100

    SiLAS upgrade postgres in Dev to 16.15

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/rds.tf
@@ -318,7 +318,7 @@ module "unleash_rds" {
   deletion_protection          = false
 
   db_engine         = "postgres"
-  db_engine_version = "16"
+  db_engine_version = "16.15"
   rds_family        = "postgres16"
   db_instance_class = "db.t4g.micro"
 
