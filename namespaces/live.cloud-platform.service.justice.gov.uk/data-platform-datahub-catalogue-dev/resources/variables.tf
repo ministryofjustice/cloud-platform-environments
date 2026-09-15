4144e2892df1b27e623bdc86e7be6c5400e89262
Mark McKinlay <mark@mckinlay.io>
2026-09-15 15:55:04 +0100

    Changed Db_enmgine_version to 17.11 To protect against CVE-2026-6471

--- namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/data-platform-datahub-catalogue-dev/resources/variables.tf
@@ -134,7 +134,7 @@ variable db_engine {
 variable db_engine_version {
   description = "The database engine version to use for the RDS instance"
   type        = string
-  default     = "17.7"
+  default     = "17.11"
 }
 variable rds_family {
   description = "The RDS family to use for the RDS instance"
