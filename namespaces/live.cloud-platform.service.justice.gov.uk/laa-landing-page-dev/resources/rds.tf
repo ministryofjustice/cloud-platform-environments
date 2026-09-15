8d7287eca38b46e25f31cc823d2edebc0a79967f
Brian Carr <brian.carr@justice.gov.uk>
2026-09-15 15:31:24 +0100

    SiLAS upgrade postgres in Dev to 16.15 for other DB

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-dev/resources/rds.tf
@@ -94,7 +94,7 @@ module "rds_temp" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "16"
+  db_engine_version = "16.15"
   rds_family        = "postgres16"
   db_instance_class = "db.t4g.medium"
 
