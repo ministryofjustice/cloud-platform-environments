62ee5dd20642a616a18b193b0a105d2c64c24dfb
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2026-09-15 16:32:59 +0100

    OV-902 : upgrade minor db version dev - read replica (#45706)
    * OV-902 : upgrade minor db version dev - read replica
    
    * OV-902 : upgrade minor db version prod - read replica

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-dev/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-official-visits-dev/resources/rds-postgresql.tf
@@ -98,7 +98,7 @@ module "read_replica" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "17"
+  db_engine_version = "17.11"
   rds_family        = "postgres17"
   db_instance_class = "db.t4g.micro"
   vpc_security_group_ids       = [data.aws_security_group.mp_dps_sg.id]
