bcbd6ebf7d22e1c27e47ad8d6b48d45949f01f0e
joselopez-moj <Jose.Lopez@justice.gov.uk>
2026-09-15 16:42:05 +0100

    infra: bump db version for main training db + metabase (#45699)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-training/resources/rds.tf
@@ -18,7 +18,7 @@ module "cla_backend_rds_postgres_14" {
 
   db_name = "cla_backend"
   # change the postgres version as you see fit.
-  db_engine_version      = "14"
+  db_engine_version      = "14.24"
   db_instance_class      = "db.t4g.small"
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
@@ -96,7 +96,7 @@ module "cla_backend_metabase_rds" {
   namespace     = var.namespace
 
   db_name                  = "metabase"
-  db_engine_version        = "15"
+  db_engine_version        = "15.19"
   db_instance_class        = "db.t4g.micro"
   db_allocated_storage     = "5"
   db_max_allocated_storage = "500"
