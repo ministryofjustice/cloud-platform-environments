f3708d2f28988c084dee1c09019ccb286471faf4
Sam Gibson <Sam.Gibson@justice.gov.uk>
2026-09-15 15:51:57 +0100

    Adopt hmpps subdomain and environment suffix

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/route53.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-cfo-case-assessment-tracking-system-dev/resources/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "cats_dev" {
-  name = "dev.manage-external-funded-offender-provision.service.justice.gov.uk"
+  name = "manage-external-funded-offender-provision-dev.hmpps.service.justice.gov.uk"
 
   tags = {
     business-unit          = var.business_unit
