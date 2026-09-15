9dc39d44a007ef1afdf02545dcfbb0e7339873cf
joselopez-moj <Jose.Lopez@justice.gov.uk>
2026-09-15 15:49:03 +0100

    infra: update replica db staging (#45687)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-cla-backend-staging/resources/rds.tf
@@ -53,7 +53,7 @@ module "cla_backend_rds_postgres_14" {
 
   db_name = "cla_backend"
   # change the postgres version as you see fit.
-  db_engine_version      = "14"
+  db_engine_version      = "14.24"
   db_instance_class      = "db.t4g.small"
   environment_name       = var.environment-name
   infrastructure_support = var.infrastructure_support
