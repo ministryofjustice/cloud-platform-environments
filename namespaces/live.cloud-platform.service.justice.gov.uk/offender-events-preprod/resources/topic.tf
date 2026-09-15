6a4b7c760f2e9a56551ae0d40c950014d381c3fb
Matt <34448412+mattops@users.noreply.github.com>
2026-09-15 11:43:18 +0100

    fix(offender-events): restrict preprod SNS access (#45550)
    Use scoped SNS policies and remove the unused assessments topic.

--- namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/offender-events-preprod/resources/topic.tf
@@ -49,34 +49,52 @@ module "probation_offender_events" {
   }
 }
 
-module "offender_assessments_events" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=5.1.2"
+data "aws_caller_identity" "current" {
+  provider = aws.london
+}
 
-  # Configuration
-  topic_display_name = "offender-assessments-events"
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the topic
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-
-  providers = {
-    aws = aws.london
+data "aws_iam_policy_document" "offender_events_topic_policy" {
+  statement {
+    sid    = "AllowTopicPublish"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+      "sns:GetTopicAttributes",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    resources = [module.offender_events.topic_arn]
   }
 }
 
-resource "kubernetes_secret" "offender_assessments_events" {
-  metadata {
-    name      = "offender-assessments-events-topic"
-    namespace = var.namespace
+resource "aws_sns_topic_policy" "offender_events_topic_policy" {
+  provider = aws.london
+  arn      = module.offender_events.topic_arn
+  policy   = data.aws_iam_policy_document.offender_events_topic_policy.json
+}
+
+data "aws_iam_policy_document" "probation_offender_events_topic_policy" {
+  statement {
+    sid    = "AllowTopicPublish"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+      "sns:GetTopicAttributes",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    resources = [module.probation_offender_events.topic_arn]
   }
-  data = {
-    topic_arn = module.offender_assessments_events.topic_arn
-  }
+}
+
+resource "aws_sns_topic_policy" "probation_offender_events_topic_policy" {
+  provider = aws.london
+  arn      = module.probation_offender_events.topic_arn
+  policy   = data.aws_iam_policy_document.probation_offender_events_topic_policy.json
 }
 
 resource "kubernetes_secret" "offender-events-and-delius-topic-secret" {
