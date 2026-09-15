b751f8b1187a009fad3ba30d24aedacbbcb7208b
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2026-09-15 18:06:51 +0100

    🔐 Upgrade Postgres to 18.6 in `laa-sre-prod` (#45735)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-sre-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-sre-prod/resources/rds.tf
@@ -6,7 +6,7 @@ module "rds" {
 
   # Database configuration
   db_engine                   = "postgres"
-  db_engine_version           = "18"
+  db_engine_version           = "18.6"
   rds_family                  = "postgres18"
   db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500"
