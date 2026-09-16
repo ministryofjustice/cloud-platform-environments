71dc7ee8ccc818ac2b8696bea774f59767476b35
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2026-09-16 10:03:03 +0100

    Adding pg_dump write policy and role etc (#45728)
    * Adding pg_dump write policy and role etc
    
    * Empty commit

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-test/resources/s3-backup.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-landing-page-test/resources/s3-backup.tf
@@ -38,6 +38,51 @@ resource "aws_s3_bucket_object_lock_configuration" "backup" {
   }
 }
 
+data "aws_iam_policy_document" "pg_dump_backup_write" {
+  statement {
+    sid       = "PutPgDumpObjects"
+    effect    = "Allow"
+    actions   = ["s3:PutObject", "s3:AbortMultipartUpload"]
+    resources = ["${module.backup.bucket_arn}/pg-dump/*"]
+  }
+
+  statement {
+    sid       = "ListMultipartUploadsForPgDumpPrefix"
+    effect    = "Allow"
+    actions   = ["s3:ListBucketMultipartUploads"]
+    resources = [module.backup.bucket_arn]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values   = ["pg-dump/*"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "pg_dump_backup_write" {
+  name        = "${var.namespace}-pg-dump-backup-write-policy"
+  description = "Write-only access for the test namespace's weekly pg_dump CronJob, scoped to this bucket's pg-dump/ prefix. No get/list/delete."
+  policy      = data.aws_iam_policy_document.pg_dump_backup_write.json
+}
+
+module "irsa_pg_dump_backup" {
+  source                = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+  eks_cluster_name      = var.eks_cluster_name
+  service_account_name  = "irsa-laa-landing-page-${var.environment}-pg-dump-backup"
+  namespace             = var.namespace
+
+  role_policy_arns = {
+    pg_dump_backup_write = aws_iam_policy.pg_dump_backup_write.arn
+  }
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name               = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
+
 resource "kubernetes_secret" "backup_bucket" {
   metadata {
     name      = "backup-bucket-output"
