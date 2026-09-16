11c5f32ece456a6c7f4df548c0e33a7b91834098
Edwin Kruglov <edwin.kruglov@justice.gov.uk>
2026-09-16 17:01:19 +0100

    [CRIMAPP-2532] Upgrade Postgres to v17.11 - Crime Review prod (#45796)

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-review-criminal-legal-aid-production/resources/rds.tf
@@ -25,7 +25,7 @@ module "rds" {
 
   # PostgreSQL specifics
   db_engine         = "postgres"
-  db_engine_version = "17"
+  db_engine_version = "17.11"
   rds_family        = "postgres17"
   db_instance_class = "db.t4g.small"
 
