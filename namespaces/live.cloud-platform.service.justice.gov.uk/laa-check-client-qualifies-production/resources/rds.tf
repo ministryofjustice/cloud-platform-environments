80c1af41121ceb0b93bca15dbf7acd9b1aefa7ad
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-09-16 12:44:25 +0100

    chore: Add service pod and IRSA module configurations for CCQ prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-check-client-qualifies-production/resources/rds.tf
@@ -26,7 +26,7 @@ module "rds" {
   performance_insights_enabled = true
 
   # change the postgres version as you see fit.
-  db_engine = "postgres"
+  db_engine         = "postgres"
   db_engine_version = "17.4"
 
   # change the instance class as you see fit.
@@ -57,6 +57,7 @@ module "rds" {
     aws = aws.london
   }
 
+  enable_irsa = true
 }
 
 # To create a read replica, use the below code and update the values to specify the RDS instance
