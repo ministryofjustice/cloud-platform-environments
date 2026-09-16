b45c4ea029ddb5c8ff6f13673e351e350feb39c6
Kurt Plaschke <kurt.plaschke@digital.justice.gov.uk>
2026-09-16 15:56:09 +0100

    updated mdt namespace to use the hmpps prefix for dev env

--- namespaces/live.cloud-platform.service.justice.gov.uk/mandatory-drug-testing-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/mandatory-drug-testing-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "mandatory-drug-testing-dev"
+  default     = "hmpps-mandatory-drug-testing-dev"
 }
 
 variable "service_area" {
