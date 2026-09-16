52f36bb499b76f754d81e3e5e97da99afef1f3e4
Steve Calcott <steve.calcott@justice.gov.uk>
2026-09-16 14:38:56 +0100

    Add minor version upgrade and rds maintenance window.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/variables.tf
@@ -64,3 +64,9 @@ variable "github_token" {
   description = "Required by the GitHub Terraform provider"
   default     = ""
 }
+
+variable "rds_maintenance_window" {
+  type        = string
+  description = "When to schedule maintenance for RDS DBs (e.g. minor version upgrades)"
+  default     = "Tue:02:20-Tue:03:30"
+}
