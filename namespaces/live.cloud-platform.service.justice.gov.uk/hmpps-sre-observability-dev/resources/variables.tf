5e730519164c2693daff8dec6cde2b4e08d89327
Steve Calcott <steve.calcott@justice.gov.uk>
2026-09-16 14:59:17 +0100

    Add missing eks_cluster_name var.

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-sre-observability-dev/resources/variables.tf
@@ -70,3 +70,8 @@ variable "rds_maintenance_window" {
   description = "When to schedule maintenance for RDS DBs (e.g. minor version upgrades)"
   default     = "Tue:02:20-Tue:03:30"
 }
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "The name of the eks cluster to be used with IRSA"
+}
