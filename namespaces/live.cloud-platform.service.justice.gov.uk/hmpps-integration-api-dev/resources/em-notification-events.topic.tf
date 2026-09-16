92829d012a29fea919b29e3139c356d42dfad194
James Francis <james.francis@madetech.com>
2026-09-16 14:32:02 +0100

    add secret and ssm param

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/em-notification-events.topic.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/em-notification-events.topic.tf
@@ -1,4 +1,4 @@
-module "sns_topic" {
+module "em-notification-events" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=5.1.2"
 
   # Configurations
@@ -13,4 +13,34 @@ module "sns_topic" {
   namespace              = var.namespace
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
+}
+
+resource "kubernetes_secret" "hmpps-integration-em-notification-events-secret" {
+  metadata {
+    name      = "hmpps-integration-em-notification-events-topic"
+    namespace = var.namespace
+  }
+
+  data = {
+    sns_arn      = module.hmpps-integration-events.topic_arn
+    sns_name     = module.hmpps-integration-events.topic_name
+    sns_irsa_arn = module.integration_api_domain_events_queue.irsa_policy_arn
+  }
+}
+
+resource "aws_ssm_parameter" "em-notification-events-topic-arn" {
+  type        = "String"
+  name        = "/${var.namespace}/em-notification-events-topic-arn"
+  value       = module.em-notification-events.topic_arn
+  description = "SNS topic ARN for em notification events; use this parameter from other namespaces"
+
+  tags = {
+    business_unit          = var.business_unit
+    application            = var.application
+    is_production          = var.is_production
+    team_name              = var.team_name
+    namespace              = var.namespace
+    environment_name       = var.environment
+    infrastructure_support = var.infrastructure_support
+  }
 }
