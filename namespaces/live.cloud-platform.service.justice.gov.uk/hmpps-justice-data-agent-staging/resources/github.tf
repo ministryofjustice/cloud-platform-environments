1d6769624e0bec7a80164dd6b64e5eb07ec4f39f
Zainab Daria <zainab.daria@justice.gov.uk>
2026-09-18 10:37:15 +0100

    fixup

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-justice-data-agent-staging/resources/github.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-justice-data-agent-staging/resources/github.tf
@@ -14,18 +14,3 @@ module "hmpps-justice-data-agent-worker" {
   kubernetes_cluster            = var.kubernetes_cluster
 }
 
-module "hmpps-justice-data-agent-client-api" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-hmpps-template?ref=1.2.2"
-  github_repo                   = "hmpps-justice-data-agent-client-api"
-  application                   = "hmpps-justice-data-agent-client-api"
-  github_team                   = var.team_name
-  environment                   = var.environment # Should match environment name used in helm values file e.g. values-staging.yaml
-  reviewer_teams                = [var.team_name]
-  selected_branch_patterns      = ["main"]
-  is_production                 = var.is_production
-  application_insights_instance = "dev" # Either "dev", "staging", "preprod" or "prod"
-  source_template_repo          = "hmpps-template-kotlin"
-  github_token                  = var.github_token
-  namespace                     = var.namespace
-  kubernetes_cluster            = var.kubernetes_cluster
-}
