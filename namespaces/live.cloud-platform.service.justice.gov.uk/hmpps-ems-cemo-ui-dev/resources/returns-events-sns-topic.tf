8f01c7b590834a8c1f6fdb604456485aac618878
James Francis <james.francis@madetech.com>
2026-09-18 14:43:55 +0100

    sns topic with subscriptions

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-ems-cemo-ui-dev/resources/returns-events-sns-topic.tf
@@ -0,0 +1,84 @@
+module "returns_events_sns_topic" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sns-topic?ref=5.1.2"
+
+  topic_display_name          = "returns-events"
+  encrypt_sns_kms             = true
+  fifo_topic                  = true
+  content_based_deduplication = true
+
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+
+  providers = {
+    aws = aws.london
+  }
+}
+
+resource "kubernetes_secret" "returns_events_sns_topic" {
+  metadata {
+    name      = "returns-events-sns-topic"
+    namespace = var.namespace
+  }
+
+  data = {
+    topic_name = module.returns_events_sns_topic.topic_name
+    topic_arn  = module.returns_events_sns_topic.topic_arn
+  }
+}
+
+resource "aws_ssm_parameter" "returns_events_sns_topic_arn" {
+  provider    = aws.london
+  type        = "String"
+  name        = "/${var.namespace}/returns-events-topic-arn"
+  value       = module.returns_events_sns_topic.topic_arn
+  description = "Returns events SNS topic ARN"
+  tags = {
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    owner                  = var.team_name
+    environment-name       = var.environment
+    infrastructure-support = var.infrastructure_support
+    namespace              = var.namespace
+  }
+}
+
+resource "aws_sns_topic_subscription" "returns_events_fifo_subscription" {
+  provider             = aws.london
+  topic_arn            = module.returns_events_sns_topic.topic_arn
+  protocol             = "sqs"
+  endpoint             = module.returns_events_fifo_queue.sqs_arn
+  raw_message_delivery = true
+}
+
+resource "aws_sqs_queue_policy" "returns_events_fifo_queue_policy" {
+  queue_url = module.returns_events_fifo_queue.sqs_id
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Id": "${module.returns_events_fifo_queue.sqs_arn}/SQSDefaultPolicy",
+    "Statement":
+      [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS": "*"},
+          "Resource": "${module.returns_events_fifo_queue.sqs_arn}",
+          "Action": "SQS:SendMessage",
+          "Condition":
+            {
+              "ArnEquals":
+              {
+                "aws:SourceArn": "${module.returns_events_sns_topic.topic_arn}"
+              }
+            }
+        }
+      ]
+  }
+EOF
+}
