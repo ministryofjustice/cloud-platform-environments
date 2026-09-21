278d57db20a6efd294b5798e7882c5b37aca7331
dmaggio-jd <david.maggio@justice.gov.uk>
2026-09-21 16:57:57 +0100

    RASS-2667 upgrade document management db to 18.6

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-document-management-preprod/resources/rds-postgresql.tf
@@ -8,12 +8,12 @@ module "rds_postgres" {
 
   # Database configuration
   db_engine                  = "postgres"
-  db_engine_version          = "16"
-  rds_family                 = "postgres16"
+  db_engine_version          = "18.6"
+  rds_family                 = "postgres18"
   db_instance_class          = "db.t4g.small"
   db_max_allocated_storage   = "500"
   enable_rds_auto_start_stop = true
-  prepare_for_major_upgrade  = false
+  prepare_for_major_upgrade  = true
 
   # Tags
   business_unit          = var.business_unit
