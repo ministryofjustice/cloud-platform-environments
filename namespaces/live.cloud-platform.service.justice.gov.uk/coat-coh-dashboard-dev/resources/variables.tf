60e5207c3fb9be4bce61eb8c86915a5106cdd68d
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-09-21 10:55:04 +0100

    update terraform namespace variable

--- namespaces/live.cloud-platform.service.justice.gov.uk/coat-coh-dashboard-dev/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/coat-coh-dashboard-dev/resources/variables.tf
@@ -17,7 +17,7 @@ variable "application" {
 variable "namespace" {
   description = "Name of the namespace these resources are part of"
   type        = string
-  default     = "moj-copilot-ai-credits-dashboard-dev"
+  default     = "coat-coh-dashboard-dev"
 }
 
 variable "service_area" {
