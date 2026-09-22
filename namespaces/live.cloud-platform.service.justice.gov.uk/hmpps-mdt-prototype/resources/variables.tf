b585358666c93864f8313221c6bca733d8f91fe8
jackmitchellwinter <jack.mitchellwinter@version1.com>
2026-09-22 10:32:31 +0100

    Added my prototype to cloud platform

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mdt-prototype/resources/variables.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-mdt-prototype/resources/variables.tf
@@ -59,7 +59,7 @@ variable "is_production" {
 variable "slack_channel" {
   description = "Slack channel name for your team, if we need to contact you about this service"
   type        = string
-  default     = "mdt-prototype-alerts"
+  default     = "mandatory-drug-testing-support"
 }
 
 variable "github_owner" {
