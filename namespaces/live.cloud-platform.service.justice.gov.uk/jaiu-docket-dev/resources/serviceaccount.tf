f0e38f83e60a177c139c65ee5b10436fe1c1b4cd
Luke Plausin <luke.plausin@justice.gov.uk>
2026-09-22 14:56:37 +0100

    chore: comment out external git repo

--- namespaces/live.cloud-platform.service.justice.gov.uk/jaiu-docket-dev/resources/serviceaccount.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/jaiu-docket-dev/resources/serviceaccount.tf
@@ -8,5 +8,7 @@ module "serviceaccount" {
 
   # Uncomment and provide repository names to create github actions secrets
   # containing the ca.crt and token for use in github actions CI/CD pipelines
-  github_repositories = ["JusticeAIUnit/Docket"]
+
+  # This is not possible in Cloud Platform at present.
+  # github_repositories = ["JusticeAIUnit/Docket"]
 }
