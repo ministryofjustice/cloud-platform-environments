52e9835b63c4b94367748f39564141aa0f9f84f8
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-09-23 14:50:33 +0100

    MAPB-891: Uof preprod enable read replica irsa

--- namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/use-of-force-preprod/resources/irsa.tf
@@ -14,7 +14,7 @@ module "irsa" {
   role_policy_arns = {
     rds   = module.dps_rds.irsa_policy_arn
     redis = module.uof_elasticache_redis.irsa_policy_arn
-    read_replica = module.dps_rds_replica.irsa_policy_arn
+    read_replica = module.dps_rds_replica[0].irsa_policy_arn
   }
 
   # Tags
