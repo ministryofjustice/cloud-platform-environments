7597d679a7d800f618736ff5121b484d5a1d4ff6
james Hart <james.hart@digital.justice.gov.uk>
2026-09-23 17:17:45 +0100

    enable tidying up the old ECR repositor

--- namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/ecr.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/james-dev/resources/ecr.tf
@@ -5,17 +5,17 @@
  *
  */
 
-# One module instantiation for each repo
-
-module "ecr-james-typescript-test" {
+module "ecr" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.2"
 
   # Repository configuration
-  repo_name = "james-typescript-test"
+  repo_name = var.namespace
 
   # OpenID Connect configuration
   oidc_providers      = ["github"]
-  github_repositories = ["james-typescript-test"]
+  github_repositories = ["james-typescript-test", 
+                         "james-kotlin-test",
+                         "hmpps-james-bootstrap"]
 
   # Tags
   business_unit          = var.business_unit
@@ -25,54 +25,7 @@ module "ecr-james-typescript-test" {
   namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
-
-  # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for read only queries,
-  # uncomment below:
-  # enable_irsa = true
-}
-
-module "ecr-james-kotlin-test" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.2"
-
-  # Repository configuration
-  repo_name = "james-kotlin-test"
-
-  # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = ["james-kotlin-test"]
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the container repository
-  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-
-  # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for read only queries,
-  # uncomment below:
-  # enable_irsa = true
-}
-
-module "ecr-hmpps-james-bootstrap" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.2"
-
-  # Repository configuration
-  repo_name = "hmpps-james-bootstrap"
-
-  # OpenID Connect configuration
-  oidc_providers      = ["github"]
-  github_repositories = ["hmpps-james-bootstrap"]
-
-  # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name # also used for naming the container repository
-  namespace              = var.namespace # also used for creating a Kubernetes ConfigMap
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
+  deletion_protection    = false
 
   # If you want to assign AWS permissions to a k8s pod in your namespace - ie service pod for read only queries,
   # uncomment below:
