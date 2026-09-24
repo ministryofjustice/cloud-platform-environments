deedecbdc0d0b096cb867218080a0ab15c8b7dd2
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-09-24 11:31:06 +0100

    SDIT-4259: 🔥 Remove official visits migrations for hmpps-prisoner-from-nomis-migration-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-preprod/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-prisoner-from-nomis-migration-preprod/resources/irsa.tf
@@ -47,14 +47,10 @@ data "aws_iam_policy_document" "combined_local_sqs_migration" {
       module.migration_prisonerbalance_dead_letter_queue.sqs_arn,
       module.migration_property_queue.sqs_arn,
       module.migration_property_dead_letter_queue.sqs_arn,
-      module.migration_officialvisits_queue.sqs_arn,
-      module.migration_officialvisits_dead_letter_queue.sqs_arn,
       module.migration_staff_queue.sqs_arn,
       module.migration_staff_dead_letter_queue.sqs_arn,
       module.migration_transfermovements_queue.sqs_arn,
       module.migration_transfermovements_dead_letter_queue.sqs_arn,
-      module.migration_visitslots_queue.sqs_arn,
-      module.migration_visitslots_dead_letter_queue.sqs_arn,
     ]
   }
 }
