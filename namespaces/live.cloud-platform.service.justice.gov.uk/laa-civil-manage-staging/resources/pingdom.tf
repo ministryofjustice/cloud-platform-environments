e7cd6d37ac5b5155f7b6be99c6e27e541404aa54
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2026-09-24 15:22:18 +0100

    Add pingdom monitoring

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-staging/resources/pingdom.tf
@@ -0,0 +1,17 @@
+provider "pingdom" {
+}
+
+resource "pingdom_check" "laa-civil-manage-staging" {
+  type             = "http"
+  name             = "LAA Civil Manage UI - Staging"
+  host             = "laa-civil-manage-staging.cloud-platform.service.justice.gov.uk"
+  resolution       = 1
+  notifywhenbackup = true
+  notifyagainevery = 0
+  url              = "/health"
+  encryption       = true
+  port             = 443
+  tags             = "businessunit_${var.business_unit},application_${var.application},component_frontend,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.application},laa,civil-manage"
+  probefilters     = "region:EU"
+  integrationids   = [149337]
+}
