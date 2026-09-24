22d62afcb41e224928eca507de5b5cf0621196fe
Anthony Britton <anthony.britton@justice.gov.uk>
2026-09-24 12:16:44 +0100

    MIA-2311 add backup window

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-release-scheduler-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-release-scheduler-prod/resources/rds-postgresql.tf
@@ -27,6 +27,7 @@ module "rds" {
   is_production          = var.is_production
   namespace              = var.namespace
   team_name              = var.team_name
+  backup_window          = var.backup_window
   maintenance_window     = var.maintenance_window
 
   vpc_security_group_ids     = [data.aws_security_group.mp_dps_sg.id]
