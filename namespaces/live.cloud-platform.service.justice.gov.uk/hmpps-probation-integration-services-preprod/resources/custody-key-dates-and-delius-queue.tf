01699749122c91a2f108866eb37915dbafe55045
Marcus Aspin <maspin@unilink.com>
2026-09-24 10:50:55 +0100

    PI-4400 Add access to SNS (preprod) (#46268)

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/custody-key-dates-and-delius-queue.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-probation-integration-services-preprod/resources/custody-key-dates-and-delius-queue.tf
@@ -104,5 +104,8 @@ module "custody-key-dates-and-delius-service-account" {
   team_name              = var.team_name
 
   service_account_name = "custody-key-dates-and-delius"
-  role_policy_arns     = { sqs = module.custody-key-dates-and-delius-queue.irsa_policy_arn }
+  role_policy_arns     = {
+    sqs = module.custody-key-dates-and-delius-queue.irsa_policy_arn
+    sns = data.aws_ssm_parameter.hmpps-domain-events-policy-arn.value
+  }
 }
