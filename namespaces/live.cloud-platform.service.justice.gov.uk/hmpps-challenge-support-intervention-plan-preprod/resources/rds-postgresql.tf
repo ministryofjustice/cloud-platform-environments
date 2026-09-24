dee2334c701403c79e21977f19e5a98bcaee5ed8
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-09-24 10:42:34 +0100

    IR-1982 Reset prepare_for_major_upgrade on hmpps-challenge-support-intervention-plan-preprod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-preprod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-challenge-support-intervention-plan-preprod/resources/rds-postgresql.tf
@@ -11,7 +11,7 @@ module "rds" {
   vpc_name = var.vpc_name
 
   # RDS configuration
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   allow_minor_version_upgrade  = true
   allow_major_version_upgrade  = false
   performance_insights_enabled = false
@@ -121,7 +121,7 @@ module "read_replica" {
   db_engine                = "postgres"
   db_engine_version        = "18.6"
   rds_family               = "postgres18"
-  prepare_for_major_upgrade = true
+  prepare_for_major_upgrade = false
   db_instance_class        = "db.t4g.small"
   db_max_allocated_storage = "100"
 
