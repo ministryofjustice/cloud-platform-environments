3dc9820c235a4ee934f0ed7e0b368ff620817b9d
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-09-24 20:25:05 +0100

    CRS-2849 Upgrade to fix postgres vulnerabilities - IR tool

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-identify-remand-periods-dev/resources/rds.tf
@@ -14,7 +14,7 @@ module "identify_remand_rds" {
   allow_major_version_upgrade = "false"
   db_instance_class           = "db.t4g.micro"
   db_max_allocated_storage    = "500" # maximum storage for autoscaling
-  db_engine_version           = "17.4"
+  db_engine_version           = "17.11"
   enable_rds_auto_start_stop  = true
   prepare_for_major_upgrade = false
 
