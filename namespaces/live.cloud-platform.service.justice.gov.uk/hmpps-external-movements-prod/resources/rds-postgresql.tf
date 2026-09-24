c3c26a69ea0acaffdce82a68980aa550fa283c74
Anthony Britton <anthony.britton@justice.gov.uk>
2026-09-24 08:54:42 +0100

    MIA-2311 add maintenance window

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/rds-postgresql.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-external-movements-prod/resources/rds-postgresql.tf
@@ -36,6 +36,7 @@ module "rds" {
   is_production          = var.is_production
   namespace              = var.namespace
   team_name              = var.team_name
+  maintenance_window     = var.maintenance_window
 
   db_parameter = [
     {
