c2b0f24746e28909046103bd1c78c26510e7fa99
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2026-09-24 15:06:27 +0100

    Update pingdom name

--- namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-api-dev/resources/pingdom.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/laa-civil-manage-api-dev/resources/pingdom.tf
@@ -3,7 +3,7 @@ provider "pingdom" {
 
 resource "pingdom_check" "laa-civil-manage-api-dev" {
   type             = "http"
-  name             = "LAA Manage a Civil Application API - Dev"
+  name             = "LAA Civil Manage API - Dev"
   host             = "laa-civil-manage-api-dev.cloud-platform.service.justice.gov.uk"
   resolution       = 1
   notifywhenbackup = true
@@ -13,5 +13,5 @@ resource "pingdom_check" "laa-civil-manage-api-dev" {
   port             = 443
   tags             = "businessunit_${var.business_unit},application_${var.application},component_api,isproduction_${var.is_production},environment_${var.environment},infrastructuresupport_${var.application},laa,civil-manage"
   probefilters     = "region:EU"
-  integrationids   = [149337] 
+  integrationids   = [149337]
 }
