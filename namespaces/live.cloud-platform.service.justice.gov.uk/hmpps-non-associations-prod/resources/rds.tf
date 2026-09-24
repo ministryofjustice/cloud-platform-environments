73151f46cbde8c53515feb6e46de88fc3610b946
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-09-24 11:55:38 +0100

    IR-1982 Reset prepare_for_major_upgrade on hmpps-non-associations-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-non-associations-prod/resources/rds.tf
@@ -14,7 +14,7 @@ module "dps_rds" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   db_instance_class           = "db.t4g.large"
   rds_family                  = "postgres18"
   db_engine_version           = "18.6"
@@ -90,7 +90,7 @@ module "dps_rds_replica" {
   # add them to the replica
 
   # PostgreSQL specifics
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   db_engine         = "postgres"
   db_engine_version = "18.6"
   rds_family        = "postgres18"
