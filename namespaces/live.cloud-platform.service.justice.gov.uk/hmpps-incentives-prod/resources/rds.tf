b953f54d4e94ba868acfd0e43eba012c95be84e2
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-09-24 11:55:36 +0100

    IR-1982 Reset prepare_for_major_upgrade on hmpps-incentives-prod

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-prod/resources/rds.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-incentives-prod/resources/rds.tf
@@ -14,7 +14,7 @@ module "dps_rds" {
   environment_name       = var.environment
   infrastructure_support = var.infrastructure_support
 
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
   db_instance_class           = "db.t4g.large"
   rds_family                  = "postgres18"
   db_engine_version           = "18.6"
@@ -85,7 +85,7 @@ module "dps_rds_replica" {
   # If any other inputs of the RDS is passed in the source db which are different from defaults,
   # add them to the replica
 
-  prepare_for_major_upgrade   = true
+  prepare_for_major_upgrade   = false
 
   # PostgreSQL specifics
   db_engine         = "postgres"
