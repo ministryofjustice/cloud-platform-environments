b93171dbd9174b5eae5c633d19b76f6af2016cfb
Paul McPhee <paul.mcphee@justice.gov.uk>
2026-09-25 15:06:10 +0100

    HIA-1548: Add cemo returns event sns topic secret - ext api dev

--- namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-integration-api-dev/resources/irsa.tf
@@ -14,8 +14,7 @@ locals {
   }
   sqs_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sqs : item.name => item.value }
   sns_topics = {
-    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev",
-    "cloud-platform-hmpps-electronic-monitoring-2df41de5fd9a2640f879551b653cc8cd.fifo" = "hmpps-ems-cemo-ui-dev",
+    "cloud-platform-Digital-Prison-Services-e29fb030a51b3576dd645aa5e460e573" = "hmpps-domain-events-dev"
   }
   sns_policies = { for item in data.aws_ssm_parameter.irsa_policy_arns_sns : item.name => item.value }
 }
