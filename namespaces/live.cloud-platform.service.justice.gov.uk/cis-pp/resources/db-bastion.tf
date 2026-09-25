90fbd182fc8099f31e19f72e52024907371efa76
Stefan Hristov <stefan.hristov@justice.gov.uk>
2026-09-25 11:46:01 +0100

    One more update to SG

--- namespaces/live.cloud-platform.service.justice.gov.uk/cis-pp/resources/db-bastion.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/cis-pp/resources/db-bastion.tf
@@ -76,15 +76,6 @@ resource "aws_vpc_security_group_egress_rule" "db_bastion_oracle" {
   referenced_security_group_id = aws_security_group.db_bastion_oracle_access.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "db_bastion_oracle" {
-  security_group_id            = aws_security_group.db_bastion_oracle_access.id
-  description                  = "Allow Oracle connections from the database bastion"
-  ip_protocol                  = "tcp"
-  from_port                    = 1521
-  to_port                      = 1521
-  referenced_security_group_id = aws_security_group.db_bastion.id
-}
-
 resource "aws_instance" "db_bastion" {
   ami                         = data.aws_ssm_parameter.windows_server_2022.value
   instance_type               = "t3.large"
