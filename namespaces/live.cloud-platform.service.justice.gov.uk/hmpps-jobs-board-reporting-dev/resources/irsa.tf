90358888b4a22f2b647dbb32a48463a0e2c0c164
awatson-moj <adam.watson2@justice.gov.uk>
2026-09-25 15:09:44 +0100

    ESWE-2136 Add IRSA to jobs board reporting dev (#46418)
    * ESWE-2136 Add IRSA to jobs board reporting
    
    * ESWE-2136 Remove piece not present in guide
    
    * ESWE-2136 Add EKS cluster name

---
+++ namespaces/live.cloud-platform.service.justice.gov.uk/hmpps-jobs-board-reporting-dev/resources/irsa.tf
@@ -0,0 +1,18 @@
+module "irsa" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-irsa?ref=2.1.0"
+
+  eks_cluster_name     = var.eks_cluster_name
+
+  namespace            = var.namespace
+  service_account_name = "hmpps-jobs-board-reporting"
+
+  role_policy_arns     = {}
+  
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  environment_name       = var.environment
+  infrastructure_support = var.infrastructure_support
+}
