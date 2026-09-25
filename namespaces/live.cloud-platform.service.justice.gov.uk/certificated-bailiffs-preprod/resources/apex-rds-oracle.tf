facfba220d5493d8643aceff8c675a642b938798
Timi Olubusi <81569910+Tim97eng@users.noreply.github.com>
2026-09-25 15:31:07 +0100

    RST-8605: cbrs preprod postgres version bump (#46391)
    * Update rds-postgresql.tf
    
    * Update apex-rds-oracle.tf
    
    * Update rds-postgresql.tf
    
    * Update rds-postgresql.tf

--- namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-preprod/resources/apex-rds-oracle.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/certificated-bailiffs-preprod/resources/apex-rds-oracle.tf
@@ -61,7 +61,7 @@ module "rds_apex" {
 
   # Oracle specifics
   db_engine                = "oracle-se2"
-  db_engine_version        = "19.0.0.0.ru-2026-04.rur-2026-04.r1"
+  db_engine_version        = "19.0.0.0.ru-2026-07.mrp-2026-07.r1"
   rds_family               = "oracle-se2-19"
   db_instance_class        = "db.t3.small"
   storage_type             = "gp2"
